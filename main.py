656e496fc2c2c09430a73f995880beb76fd23561
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:32:48 +0000

    powerful bear loss brown trade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import sys
-def exit_program():
-        sys.exit()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
