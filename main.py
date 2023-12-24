0235f6b36deb1c26b7fcc6c98d7726ebaf73f2af
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:29:47 +0000

    giveback ordinary rush direct 179bite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
