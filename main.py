5b74a8bf5ca75272ca2b95777837c9a5578db7bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:21:16 +0000

    list argue software open worried

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
