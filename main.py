bc446ac6b3757f410c62b23054c9586fc72675f9
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:00:09 +0000

    takeoff over pronunciation I mysterious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_length(array):
+        return len(array)
