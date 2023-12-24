3f974c128b18790ec3f9256e2db18d1d63bc85f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:54:00 +0000

    with ignore straight single comment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def square_number(x):
+        return x**2
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
