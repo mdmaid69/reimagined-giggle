93fe01f1697372fefc620b01d16efb180f2f553c
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:00:22 +0000

    normal used thank keepfrom wander

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_set(array):
-        return set(array)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import json
+print(json.dumps({"name": "John", "age": 30}))
