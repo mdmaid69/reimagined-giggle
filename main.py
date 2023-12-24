77bbf7feb24244c15f0faee4f4c1f1d58717e097
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:48:57 +0000

    computer suit leave interview great

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+name = "Python"
+print("Hello,", name)
