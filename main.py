e5411cea5e96d5a93dbdf43e58d43b78c5d1d0e8
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:24:09 +0000

    clue round attempt estimate almost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
