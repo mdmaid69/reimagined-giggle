ab5c49ad848fce702dbeea3afc2e97f82069859b
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:24:58 +0000

    nothing fall intelligent turnoff pride

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-print(math.pi)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
