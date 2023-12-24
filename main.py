23462bf018d9a5c430e4ab9f6b3555ee2595844c
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:09:42 +0000

    advice interview university test mass

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
