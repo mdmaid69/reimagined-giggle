e86ce021956a90f04ba2608fb1e00b5228a21534
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:06:32 +0000

    public holiday union better safety

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-for i in range(10): print(i)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def sort_list(lst):
+        return sorted(lst)
