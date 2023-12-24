11cb0b4b6b495d6fcd8d0d253943ec222aa786b8
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:02:22 +0000

    answer split close estimate sound

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def cube_number(x):
-        return x**3
-def is_odd(n):
-        return n % 2 != 0
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
