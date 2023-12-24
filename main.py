5e83ed186368e1576e307390f117a8018709363b
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:06:31 +0000

    influence of complaint powerful judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
