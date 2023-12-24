9bb427c000937610bc3e95ae71f269879d84de45
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:14:40 +0000

    want trade dig any proud

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import sys
+  def get_python_version():
+        return sys.version
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
