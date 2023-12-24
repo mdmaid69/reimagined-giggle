8de13003ac676354b7fb1fca8f13732c3c6e3b58
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:54:33 +0000

    burst coat change film write

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
