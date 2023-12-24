53b6d78d0cbf05b5085ca89f662cbd74d6be49c9
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:51:17 +0000

    experiment height eye demand weather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_blocks_allocated(file_name):
         return os.stat(file_name).st_blocks
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
