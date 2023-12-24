ee22d1e4b18921d143416f51cf5585ceee89d55e
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:02:17 +0000

    plenty basic matter lot above

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
