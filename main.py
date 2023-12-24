d598188b077962646af0b9aa2d9ebad949078f7b
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:45:00 +0000

    judge each brother reaction create

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_itemsize(array):
+        return array.itemsize
