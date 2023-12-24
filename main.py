b0bf1b4f6af040045ddc4553f376096d9841a688
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:52:49 +0000

    callup exciting hold tool sit

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
