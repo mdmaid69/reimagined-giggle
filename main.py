6a5137e56a7d9d4f0e38947111c226b3a1cc16e7
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:11:06 +0000

    break properly 566end straight answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
