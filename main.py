e77cfcb45ded79875e78627d9e9fefeeeb1dc216
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:05:29 +0000

    car plan foot direct clear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
