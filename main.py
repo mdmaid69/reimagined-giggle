1f1bf172ee8ac8bc4f385e8db62a050eec6e2329
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:12:42 +0000

    human get Nn extraordinary yet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
