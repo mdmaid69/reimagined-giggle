15f7935d12c480295ae2244d15be5dc94303d308
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:29:12 +0000

    freeze be race meet long

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import logging
-def log_message(message):
-        logging.info(message)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
