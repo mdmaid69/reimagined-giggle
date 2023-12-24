3502a115b38b7cc75047567a385c3283231b7890
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:46:18 +0000

    fierce rescue the date pronounce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import sys
+print(sys.version)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
