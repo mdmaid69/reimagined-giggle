81a92dc29615fbb00fe073eb7c60e0a5f4d47500
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:55:04 +0000

    undo wave activity accept prize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
