c8ae55257f67ae98c50e0224702a39ac698ab9d2
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:28:16 +0000

    tidy race wet Hh second

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
