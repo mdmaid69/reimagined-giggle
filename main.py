c8f2199524787922643cfdb2a017737858bdda86
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:51:19 +0000

    release fly weekend mind cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
