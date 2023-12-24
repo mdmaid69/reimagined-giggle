cfa2dcfdcbb6479582571619f35b46fe66c58b77
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:48:43 +0000

    legal mysterious uniform finger work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
