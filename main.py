7f780ee353c91325ce90e1d148f67ac74eee7f39
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:19:51 +0000

    birthday bag drive taste wrap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
