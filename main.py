af9f9f3d0d299d1749e2be52d35e6ef907bcec04
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:29:13 +0000

    conversation bear do union the

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
