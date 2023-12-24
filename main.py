ec248c02172ed7bf38e0f571371b574c36c0cd69
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:04:34 +0000

    opponent present reply thing answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import collections
+def create_queue():
+        return collections.deque()
