0375aa3d6ef1c9885e5d5655614cf023986f5682
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:38:27 +0000

    artificial break health hurry failure

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_density(mass, volume):
+        return mass / volume
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
