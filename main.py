be180ece5c0008f42e62738c5d6fba74b1841b51
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:39:06 +0000

    emphasis science suckme gas precise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
