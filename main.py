9456381b6221978d5c559d70097ad11c48761e33
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:50:17 +0000

    pick talkback stick low especially

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def convert_to_binary(n):
+        return bin(n)
