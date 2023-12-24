99885cf57d8acb17b377d755bde249a694bbed4b
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:37:27 +0000

    snow storm furniture battery exact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
