561409b212bda243c84262becc77bd8521ba6797
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:25:31 +0000

    nigga hit criticize marry force

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
