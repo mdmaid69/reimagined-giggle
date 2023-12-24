e6209f1a40352deb7468ef0c62e55c23e1e63f91
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:25:29 +0000

    limit violence out shall occur

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
