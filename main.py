df708877be18b0c34d362b243175ad933111be39
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:12:55 +0000

    intend confident provide final natural

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
