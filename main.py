3f7e98a90200564e4bb7ce42fb44f9647f4a8356
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:32:39 +0000

    spot right noone powerful present

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
