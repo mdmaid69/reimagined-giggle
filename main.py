4612080540f4076f9a5968bb960cfafb46be3902
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:51:10 +0000

    no deliver settle disagree identify

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_unique_words(sentence):
-        return set(sentence.split())
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+name = "Python"
+print("Hello,", name)
