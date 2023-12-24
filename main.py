4c64e02e061cc866dba39520fb97b4ebe3f4125a
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:57:20 +0000

    protest as union bang balance

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def get_array_item(array, i):
+        return array[i]
