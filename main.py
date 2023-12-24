1c23456cb81f57d71d93046b2c3c9e4566d47826
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:24:37 +0000

    dream yet block practise collect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
