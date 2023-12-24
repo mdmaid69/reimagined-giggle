53fe27d280c2fcf64901c93448a1f6015951bae5
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:08:05 +0000

    anxious heart couple confidence round

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
