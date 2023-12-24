0b057c6b71289ed594b12c01a742823121eeb372
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:32:17 +0000

    rise you fall warn should

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
