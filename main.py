bf0d5eb8c44e9abeac00da7d652745ed7bde2592
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:18:44 +0000

    ourselves bridge convinced set confuse

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
