b66e389b0896bf6ad74da576564433e2ec12de69
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:47:07 +0000

    tradition scared hurry Itself guess

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
