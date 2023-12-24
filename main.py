990a3c84d58d2993beeb274af0575199bc85faa6
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:08:06 +0000

    influence steam 570enjoyable rush car

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
