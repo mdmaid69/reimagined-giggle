4a0be3214bd9a3d8b40dfe690236511718018dfa
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:20:17 +0000

    line message fortunately miss independent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
