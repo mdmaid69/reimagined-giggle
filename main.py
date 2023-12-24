49fb582ecab843e260260732bf718e8a09d59d2c
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:16:24 +0000

    bit personal mix mistake history

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
