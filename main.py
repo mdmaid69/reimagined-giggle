b716e8b56bd788708a84ee3a7c77c93c146880ec
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:29:07 +0000

    floor flow take hurt tough

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
