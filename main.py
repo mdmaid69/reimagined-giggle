00fde8ed19cf740d9ea1b3daf787723714d4ca9b
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:02:34 +0000

    get normal completely Bb unite

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
