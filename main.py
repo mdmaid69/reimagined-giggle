e99221d2790e55bce38409734f07724cbe9cac04
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:04:12 +0000

    score comment embarrassing pretend talkinto

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
