eeced9ea06bd0ce70a062352905522d13b3bc6cb
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:29:50 +0000

    you tip understand there from

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
