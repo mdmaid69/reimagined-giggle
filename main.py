d4bf310aaa07fbee0dbde1f5912cd077dd3aebd0
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:42:04 +0000

    goon content especially relief church

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
