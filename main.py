b2db2450bb12e0bfee8e5bba49de2dcf6a07cdb5
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:44:31 +0000

    community wind demand shop oil

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
 import csv
-def save_csv(data, filename):
+def write_csv_file(filename, data):
         with open(filename, "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerows(data)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
