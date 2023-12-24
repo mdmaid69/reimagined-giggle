402cfd301eac8a0983d85e50c635b059f49f4ac6
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:40:32 +0000

    spring repeat contact closed before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
