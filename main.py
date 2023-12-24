8cc0eba515a1fc5adadd83a58d1457fd7d36bc02
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:06:33 +0000

    order translate classroom sweep smile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import numpy as np
+print(np.array([1, 2, 3]))
