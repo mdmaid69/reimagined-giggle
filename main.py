054d59984de2e76e0504295d900204c78d0c0426
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:50:37 +0000

    market lock active lock offer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def find_min(numbers):
+        return min(numbers)
+  def convert_to_hex(n):
+        return hex(n)
