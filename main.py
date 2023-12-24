508ee688c71aa8a0b79b8a51225d20cd5659fd77
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:02:31 +0000

    fight dance error mother sudden

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
   def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+        return os.path.getsize(file_name)
