c9d8556662125c0bc8e4a540ab8bac586ffe8ab1
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:35:52 +0000

    raw warn pot compare seat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
