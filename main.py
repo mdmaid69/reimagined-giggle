4dd8d1d8ffbc76a5f41386b0816cf0092fc86806
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:33:06 +0000

    second entrance board private knowledge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+x = 10
+y = 20
+print("Sum:", x + y)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
