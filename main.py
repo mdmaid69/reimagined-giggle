364a2ddff420c4c260a7e686ec536fca6dc24286
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:57:21 +0000

    apply hand far attract usual

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import numpy as np
+print(np.array([1, 2, 3]))
