6cb55170c5b38bd3ea5552b2b7da60e2a51b9cfb
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:28:13 +0000

    party style shopping guilty club

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
