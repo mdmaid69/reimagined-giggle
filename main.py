d2ca92708d7e6885c50f4fc481cf463e499caaf9
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:47:19 +0000

    pressure vote bringback total neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
