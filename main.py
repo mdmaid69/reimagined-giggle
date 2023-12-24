f9ae2f846862bb7d8248a342b7babac3d9b546a6
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:44:33 +0000

    below spill talk born hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
