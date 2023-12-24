f61ab4023bbf7b34fe30aeabfe2feede1e014748
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:37:17 +0000

    careful direct pleasure break last

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-print([x**2 for x in range(10)])
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def append_to_array(array, item):
+        array.append(item)
