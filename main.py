b3d81d741ba90d41373d9d75d01ae26779ffaa0c
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:11:07 +0000

    address about patience stone bring

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def square_number(x):
+        return x**2
+def is_even(n):
+        return n % 2 == 0
