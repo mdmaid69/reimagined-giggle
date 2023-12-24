0aaed5074eee469bb1ea8887e4ef502a823d2a46
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:59:23 +0000

    accommodation formal danger risk tidy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  def convert_to_hex(n):
+        return hex(n)
