0362eb70ff265e8b943343da21935b0244448f2a
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:51:04 +0000

    prevent old fond pause war

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import platform
+def get_os_info():
+        return platform.uname()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
