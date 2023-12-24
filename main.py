4423bad5a65857e297fb7b18a1ef8e7b6af98bba
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:44:10 +0000

    branch complaint by skirt greeting

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
