5f85848df2cf0ee1f04607942b83c36ed5f519ec
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:32:44 +0000

    let snow tongue study concerned

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
