fbfd3d4bddc6a5534424aa9de575afda5f68a09f
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:28:28 +0000

    describe cause treat comedown refuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
