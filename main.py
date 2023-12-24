2dc46cef42b67613e36fbcd238d6656a99699a7c
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:10:25 +0000

    bang cruel like calm detail

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
