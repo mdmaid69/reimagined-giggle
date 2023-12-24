573683205a2a2596494b106f446549c14ba463d2
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:34:50 +0000

    mad switch buy your sweet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
