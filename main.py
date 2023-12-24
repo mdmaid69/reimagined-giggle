c866841931ee8f37536978675977aeef2242ed69
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:27:56 +0000

    degree impressive Ee except pretend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def find_unique_words(sentence):
-        return set(sentence.split())
+for i in range(5):
+        print(i)
+  import os
+  def get_current_directory():
+        return os.getcwd()
