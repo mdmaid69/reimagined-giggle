ca24183980365973154ae7c7d8e443d273344eea
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:21:04 +0000

    war mouth match Kk write

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
