39f1e012988a1f6a481ca1295bc4d77c07d742b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:02:41 +0000

    moral healthy thin somewhere her

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import logging
+def log_message(message):
+        logging.info(message)
