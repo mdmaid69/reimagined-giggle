68acb4dffea7658799e0c955ed07d10c7cad5cff
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:14:00 +0000

    wash change tragedy before oil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
