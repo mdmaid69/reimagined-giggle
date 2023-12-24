1005d29d633e2791519d67ebb1d6ada7a2ec1bf0
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:45:32 +0000

    sure on sport slight top

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def subtract_numbers(x, y):
-        return x - y
-def find_unique_words(sentence):
-        return set(sentence.split())
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
