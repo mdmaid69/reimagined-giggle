f344b33d3879b4d81a266325d4aaa25c5415680b
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:34:17 +0000

    foreign load chance lose recently

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
