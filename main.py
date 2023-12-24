ff80f9129ae8efc0b5fa511e900562dae4cc378b
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:02:01 +0000

    on for protest delight important

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
