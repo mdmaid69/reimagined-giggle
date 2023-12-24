ac465df67f344b4772c0d64bc868d846025ba7fd
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:29:17 +0000

    other by fall shoot confuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
