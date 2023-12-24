ea4fd01e50ae08fd21e725d8cfd8e2b5f0f04093
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:56:57 +0000

    kick 564encourage otherwise challenge project

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
