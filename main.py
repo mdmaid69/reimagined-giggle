8be23a473d0bbaeb41b7ce12b99bade199d947d1
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:39:18 +0000

    recognize Ww return target consist

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-print(math.pi)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
