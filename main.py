86fb09b1b775a5baeeec293f5195140c58b6e604
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:04:40 +0000

    tell passage embarrassed join pocket

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
