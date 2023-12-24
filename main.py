96921c01385e42f56cfcfb327d71a119fe2849b9
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:57:14 +0000

    choose wide compare share float

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
