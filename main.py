5b0a3a4d511fa56a02198a715c78e4b2b04d51bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:43:09 +0000

    stir city not negative opposite

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
