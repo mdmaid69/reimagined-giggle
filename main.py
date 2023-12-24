6a7c2835b38d30128ef6ed62a37026984ec85a16
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:00:20 +0000

    hard blow underneath kill share

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
