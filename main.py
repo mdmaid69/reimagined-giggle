1573c7166a2d076a3bec6298ff7cbad726b9ef2d
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:30:13 +0000

    thankyou top nervous crowd wait

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
