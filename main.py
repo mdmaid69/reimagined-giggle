3a9d2b9d026de7984286823f9c01dc6bcf3294ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:32:08 +0000

    poison himself plain no onto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_current_directory():
+        return os.getcwd()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
