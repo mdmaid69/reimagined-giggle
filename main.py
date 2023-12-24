f1422da549fa797af4d76705c171e010f77d52fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:09:27 +0000

    thankyou team equal rank lead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_pressure(force, area):
+        return force / area
