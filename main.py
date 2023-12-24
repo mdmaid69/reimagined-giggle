418c935f5b5db3b7262a134e7a2fce876d287dc6
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:00:46 +0000

    creature drop avoid reach private

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def is_odd(n):
+        return n % 2 != 0
