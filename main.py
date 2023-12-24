052f5d84743c9beba1f2a72f09d1adf9bc6b8e6e
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:24:44 +0000

    always ought round workupto cum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def reverse_list(lst):
+        return lst[::-1]
