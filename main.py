3f5b2e33b45d68b555aeb2fb4e778e043490e91c
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:09:45 +0000

    precise answer arrive plastic better

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_max(lst):
+        return max(lst)
