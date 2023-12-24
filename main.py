29574d9d9ca2b4096191e225d7bf0c508ce2396a
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:50:48 +0000

    about urgent recovery stiff second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
