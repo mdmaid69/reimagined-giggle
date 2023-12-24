382af08153e8bedb6382a42dd5ec153004632203
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:04:07 +0000

    sense clear argue together disappear

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
