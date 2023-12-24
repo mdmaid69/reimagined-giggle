f86764488a1945c8605e418349060cb9f6b8d1d6
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:51:44 +0000

    connection press interview keep especially

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import logging
+def log_message(message):
+        logging.info(message)
