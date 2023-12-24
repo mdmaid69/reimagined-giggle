edaaa1e84cde0b433963f5ba81309663bd088556
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:44:58 +0000

    fun quiet come more Ww

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
