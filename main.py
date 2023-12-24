ad23f9022e9daf9574db0d0f13b3e635203150fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:58:27 +0000

    swim century comfortable worth moral

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
