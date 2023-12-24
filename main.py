935f3647140663689cee3f92d269205fce08d407
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:19:34 +0000

    natural sleep often board friend

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
