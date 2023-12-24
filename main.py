e4cf4db507d4e2494d94baa357348322765aac6a
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:41:27 +0000

    keepup break section consult turnover

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
