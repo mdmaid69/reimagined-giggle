2e06999da2017411fa7f5871d7587bbed13d8cb7
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:07:32 +0000

    today car convince exam wild

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
