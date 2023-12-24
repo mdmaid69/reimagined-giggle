f0a40314c62f07a4413fb73baa02a82cdec958d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:21:19 +0000

    hardly plan hear themselves most

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
