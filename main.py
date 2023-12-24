7beb8ea6ed47e16214ead19cdcca692995e6dd2e
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:44:54 +0000

    whistle check finally matter test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
