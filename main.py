74855bad08863fe7fd569553b012d4664582168a
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:45:19 +0000

    properly instant marry upstairs answer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_array_item(array, i):
+        return array[i]
