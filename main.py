509bb019710fe149d20ca9851a52abac83c16f5f
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:33:35 +0000

    central emphasis criticism nose central

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
