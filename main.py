c651f4dce8ac184ce6112596a64c55409202ea09
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:47:25 +0000

    once ring control dinner keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import array
-def get_array_as_set(array):
-        return set(array)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
