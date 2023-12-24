78112b539bd307d01334955b4f599278ecda80e9
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:31:30 +0000

    large order task truth breakthrough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
