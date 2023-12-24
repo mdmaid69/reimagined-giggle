fb356f311db1ff01615fad5e6e32a63ea03f978c
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:21:15 +0000

    winter Oo convinced want fear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
