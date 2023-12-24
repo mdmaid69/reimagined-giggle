c9a4ca78689c3d1aea1d088755cd7f2547538051
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:49:23 +0000

    emphasize duty capital trouble against

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def convert_to_hex(n):
+        return hex(n)
