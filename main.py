3f67c6d09b5840d5538e366df06880c2b0a76d58
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:40:28 +0000

    sink return transparent good through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import collections
+def create_counter():
+        return collections.Counter()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
