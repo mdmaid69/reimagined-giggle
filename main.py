ece9ff979f8d0c070bd8b536722846efa9a22214
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:11:17 +0000

    female wind otherwise smell tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
