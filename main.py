a99db53627245440078abc54f2e4467ea290542f
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:40:33 +0000

    load think man deaf getout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def add_numbers(x, y):
+        return x + y
