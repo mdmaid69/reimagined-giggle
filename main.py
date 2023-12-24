ae086a317a3fcf6c1e140c193a3aef3e7bf27590
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:19:58 +0000

    lonely seat beside during winter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
