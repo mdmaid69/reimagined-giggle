f3d228022f22af50ee32995e22b371f0a29991f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:14:13 +0000

    flat down turn wash practise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def create_user_list():
+        return collections.UserList()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
