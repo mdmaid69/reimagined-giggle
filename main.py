cd8d790efd07dc91ae0ddeec5a0fc49e06fefb04
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:38:59 +0000

    them into talkoutof declaration trouble

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
