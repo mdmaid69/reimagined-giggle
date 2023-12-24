9eb1849d08a10bae754269098ceb17602418c585
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:32:40 +0000

    climb great obtain bank happen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
