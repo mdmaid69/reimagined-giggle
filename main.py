dd71d24dc8c5a37927c30c33f05a5aa639665eee
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:52:24 +0000

    surprising actually comefrom turnback whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def is_palindrome(s):
+        return s == s[::-1]
+import getpass
+def get_username():
+        return getpass.getuser()
