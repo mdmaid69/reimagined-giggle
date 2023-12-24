fff3baa1b488d3f913991da247bf7579afe3613f
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:59:06 +0000

    tidy relax frame warm expensive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
