3d4e356004d8a79d92ae55f7dc9236b15051ce3a
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:13:14 +0000

    pain swim sometimes empty suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
