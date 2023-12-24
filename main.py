d4f4a90b538bf2b542b20383ce963730e215aed3
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:53:19 +0000

    point succeed temper together backwards

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-for i in range(5):
-        print(i)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
