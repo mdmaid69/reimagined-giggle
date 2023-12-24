be8e8b5058dcc7ab0bd90c4a94bae63d2a399092
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:54:04 +0000

    wash last offend print second

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def find_max(numbers):
-        return max(numbers)
-def reverse_string(s):
-        return s[::-1]
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
