1a2a30bebff7fc5aad365ec6b920a4966bf6cbe6
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:26:49 +0000

    trouble possibility please majority right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
