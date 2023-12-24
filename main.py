40f48ec783cce7c8a8f426cc935892891e2e0120
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:26:06 +0000

    deep excuse interrupt coat fairly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+print(time.time())
