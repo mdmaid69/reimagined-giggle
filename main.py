64a0052cf842fb204c468fd91cc7078bc02d003e
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:15:50 +0000

    system throughout them but somewhere

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
