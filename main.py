a496c222ce6a4a97bd89231135434378f914e928
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:51:25 +0000

    friendly city rush his pattern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
