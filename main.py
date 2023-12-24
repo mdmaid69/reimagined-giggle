b2316aa2ae0694cff1bd20d7c0fdca4afacdb5d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:19:07 +0000

    when band life cool smart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
