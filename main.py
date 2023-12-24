d73904df46a97289ba5b3cfdea63de25f1845d09
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:44:26 +0000

    miss through matter affect lesson

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import numpy as np
+print(np.array([1, 2, 3]))
