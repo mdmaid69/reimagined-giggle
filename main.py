9153d4580cbe3c134aa9edc8a7783084f5602027
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:51:11 +0000

    support pronounce possession muscle command

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
