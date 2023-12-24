3db84f9269c749595cf217a0023870fe77d2f4d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:57:53 +0000

    sick minimum market tip price

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def cube_number(x):
+        return x**3
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
