599cf0a18e66a6868c9ffd629788d87c5c08ca55
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:57:28 +0000

    impossible question any tired here

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
