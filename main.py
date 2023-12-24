1038b528fdc144a5705e47243d46b4dcd7cedbc8
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:56:32 +0000

    straight sensible cause pen pause

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
