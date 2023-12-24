82ff03ffe883f74cd6d08717f5057c90d21789af
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:25:36 +0000

    towards harm break actually little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_array_as_repr(array):
+        return repr(array)
