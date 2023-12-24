a20595e6b8f351480bc7f4f43739aa5a859a8d0e
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:31:44 +0000

    nothing regret ashamed immediate emergency

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import time
+print(time.time())
