f900ae624c50c873389917eee85f505df4e24547
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:06:55 +0000

    will comein organization skin call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
