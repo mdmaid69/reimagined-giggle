97449ce0d0d0df0306d48dde6a3f27d7bed46d9b
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:07:25 +0000

    scare his belief destroy wrong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
