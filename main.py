293c3047a1bbb521f32fa714693adca84ce1cade
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:13:12 +0000

    side relationship practise proud gather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
