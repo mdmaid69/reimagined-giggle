a50e3e3da727a7559cadd61284ea4a5169b53b57
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:40:03 +0000

    point spill brush morning workout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_density(mass, volume):
+        return mass / volume
