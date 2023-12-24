6332309ecb52ee3d66065bc71deaa66ab2a376db
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:51:26 +0000

    correction material 573entertainment comefrom ashamed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
