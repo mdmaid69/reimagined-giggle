a50c44e14a23e668b228810f2cea177c500bb208
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:34:57 +0000

    invite goback(to) discover hope phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
