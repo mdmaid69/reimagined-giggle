419274bd2bf21ed40a7d9a97da6898102156f52a
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:17:47 +0000

    advanced medium pattern wind smoke

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
