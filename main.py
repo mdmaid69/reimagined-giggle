378f64fa594402e6cffbf0392e3c28008c7099ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:37:14 +0000

    emotion both ignore film issue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
