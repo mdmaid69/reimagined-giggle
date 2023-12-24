13217ac3dc70af0119ab72a67c52e7f674913db9
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:27:10 +0000

    over create 565end music music

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import time
+def get_current_time():
+        return time.time()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
