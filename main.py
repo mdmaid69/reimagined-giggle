647cdc120faeff6ea9d2f72667d31032a3412f40
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:58:52 +0000

    refer shrink field translate instrument

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
