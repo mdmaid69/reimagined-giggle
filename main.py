5c8a484445eda099d5c08232b28002e6c7a57313
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:07:24 +0000

    find reduce any less count

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
