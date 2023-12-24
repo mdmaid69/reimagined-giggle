45c488433a2bc26ca5b434b5e1fc9191523b88ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:45:06 +0000

    attitude poison original time field

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def square_number(x):
-        return x**2
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
