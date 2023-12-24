724fa754eb09e7ed3a01425529613c7ce29f0d84
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:10:00 +0000

    fill priority valuable the damage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def find_max(numbers):
-        return max(numbers)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
