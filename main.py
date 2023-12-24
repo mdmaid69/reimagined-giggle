ac10d5cb1657016a9b7e6e26f3ae1b5e466e71bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:36:33 +0000

    upstairs only easy demonstrate suit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
