56f55431de03068ca1c5cfa0e3bc867507f5ed86
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:56:56 +0000

    development meeting rich lead needle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import collections
-def create_user_list():
-        return collections.UserList()
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
