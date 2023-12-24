594e112d465397217fb1e5b3733beb3b98227ad9
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:14:13 +0000

    experiment hope our size trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
