7eb6566ebc51a2e68e71fb5ef54729298389c364
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:38:24 +0000

    waste 568energy health follow offer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
