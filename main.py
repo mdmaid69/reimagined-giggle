05046927e81e99b47f9be3eadd767a3df132b0a5
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:26:01 +0000

    easy shake formal exchange breakout

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
