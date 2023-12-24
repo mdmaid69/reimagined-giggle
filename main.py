de2b73d623a1baff462637213afa889ed4ac66dd
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:16:01 +0000

    declaration process passenger meaning holdon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
