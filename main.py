dd3b35ca3a46d825e772e1472c7b655e4d9a7c9c
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:13:25 +0000

    amount university mail last on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
