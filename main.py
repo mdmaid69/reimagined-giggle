f704a09c0c7353ed12d93bed2427428235f79819
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:00:05 +0000

    copy sick freeze emotion rush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import sys
+def print_python_version():
+        print(sys.version)
