c1f75d588433b3247fe450065a0181ad8c137972
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:50:19 +0000

    suit example thinkover family crowd

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_index(array, item):
+        return array.index(item)
