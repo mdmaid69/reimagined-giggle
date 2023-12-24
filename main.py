32181b56a4c7b788196f64644f141296b8087f80
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:43:19 +0000

    simple pause trip climb spare

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
