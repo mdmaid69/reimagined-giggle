50b76e89a64b12aca77a60d72db800d021dae3d8
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:55:15 +0000

    system my noone question birthday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
