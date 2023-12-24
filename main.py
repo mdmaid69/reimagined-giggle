e7846dcf471834db68e9705df63ee8c34c92de3e
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:44:17 +0000

    brief commit stop direct hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
