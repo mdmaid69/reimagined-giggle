65e91d51c6017f5cd21c0bef7aeb10e9f47faaad
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:20:22 +0000

    after thinkover type punishment right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
