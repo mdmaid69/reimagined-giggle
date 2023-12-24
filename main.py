127f71eaa38efffaa4a951cacb5442d9446889dd
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:41:17 +0000

    inform although undo top beneath

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def find_min(lst):
+        return min(lst)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
