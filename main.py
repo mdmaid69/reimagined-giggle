712b3e95272b6cf6968c17836d46e674fa3804b3
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:25:15 +0000

    cut first wherever tooth clearly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import array
+def clear_array(array):
+        array *= 0
