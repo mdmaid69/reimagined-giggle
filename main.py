2c22a695e0d6db95c3ffc37cbee4b7f5a680eed7
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:38:40 +0000

    generally divide present flood whisper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-for i in range(5):
-        print(i)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
