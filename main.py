5020ffb7f4b521f8c78b920aa85a6043f9007809
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:51:19 +0000

    how eager great delight keen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
