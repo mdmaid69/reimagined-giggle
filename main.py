2d7eebdc5cf9104a2050e5d965d3745b64848f81
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:55:20 +0000

    twist brush song perhaps confuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
