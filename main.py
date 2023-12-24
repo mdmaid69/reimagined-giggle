e0de7e30cd7c4a07bc9230c0009cc9f48a96fdbf
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:37:23 +0000

    equipment first fact journey complete

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
