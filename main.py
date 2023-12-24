a6f360ea3aa924ebf5384d09750d8b68382b244c
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:06:28 +0000

    anything help miss before whom

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def multiply_numbers(x, y):
-        return x * y
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
