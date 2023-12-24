7cccfc2300d47e8b46ddd91aa8c6a0cd3eef3242
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:58:22 +0000

    mind mention male hate before

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
