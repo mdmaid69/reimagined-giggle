c340312a2af79a90a1337aae6171870b2a5c8452
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:02:12 +0000

    discussion disappointed giveback sting history

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def divide_numbers(x, y):
+        return x / y
