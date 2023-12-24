21dec9c8ef8fb50dc034fcfccc767979471c1aa1
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:43:00 +0000

    situation often tune after loan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
