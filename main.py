3ef0984dc5a26dd0baae3114a61f2b30f6db7d4a
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:52:35 +0000

    knock order brush side warning

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def remove_duplicates(lst):
+        return list(set(lst))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
