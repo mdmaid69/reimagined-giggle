bcd08a241b97609498541e527f026cdee7dad8f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:34:15 +0000

    nose pupil kill target go

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def cube_number(x):
+        return x**3
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
