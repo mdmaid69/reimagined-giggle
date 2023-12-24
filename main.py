09878d7710022cb4ed66601fbd46ae7fa4aa23f9
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:41:29 +0000

    store speech suit nurse hot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import json
+print(json.dumps({"name": "John", "age": 30}))
