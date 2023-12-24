4e3325248b444126eaa9bb6d79a5b257884d6562
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:57:55 +0000

    used music mark basically pay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
