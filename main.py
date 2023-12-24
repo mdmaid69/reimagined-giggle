af822eb86132dbb914798e5d23a30bec8852a1ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:46:29 +0000

    read goout makeup ours takeback

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-x = 10
-y = 20
-print("Sum:", x + y)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  def calculate_area_rectangle(l, w):
+        return l * w
