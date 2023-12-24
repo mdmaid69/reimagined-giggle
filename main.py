c8c9fe6057d742a88090bf1c2376e27ca12b53f1
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:02:30 +0000

    opportunity word some terrible likely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
