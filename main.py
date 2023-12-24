43ce9eae4edb78eef9d8a5db40d83216c7eaed33
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:20:04 +0000

    cook upstairs especially pretend Uu

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
