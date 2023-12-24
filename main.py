89b78e363513c3692e88288e91b5957403c7ac56
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:17:05 +0000

    seriously disturb common character destroy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
