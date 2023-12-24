7cc60a4e1c52a004d5fe94c37dd67513e30f911e
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:16:43 +0000

    issue like surprised forgive soft

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
