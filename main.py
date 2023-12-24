cee12c93f1e7da7b02a770976a9a40ae95010572
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:10:04 +0000

    awake previous clear very waste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
