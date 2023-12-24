3e8030e9787d812d0af1d494ac3aa66334e6316c
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:20:44 +0000

    base drop beside together satisfied

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def calculate_speed(distance, time):
+        return distance / time
