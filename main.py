d2abe7d7573d5b6ce6d54bd07dff25c91bb86013
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:29:53 +0000

    choose base bored some century

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
