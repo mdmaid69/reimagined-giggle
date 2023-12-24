e25bb95a6d7c8847e5bf76504e888d69423b5a29
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:04:41 +0000

    after sweep smell determined suggest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def reverse_list(lst):
-        return lst[::-1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
