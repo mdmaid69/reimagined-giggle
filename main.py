ea0caf45eca9dc5c4a6cd51c02bc2dd3adbc573d
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:21:05 +0000

    phone concerned square whatever fasten

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
