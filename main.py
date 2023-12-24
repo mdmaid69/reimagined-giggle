f768e41858c875c96ba748800d9fc05a76633a14
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:58:08 +0000

    make temperature train journey recent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def get_array_slice(array, i, j):
+        return array[i:j]
