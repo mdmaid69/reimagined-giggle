4fc760720c0a49c4337b79cffb53b6dd1bf8f48f
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:35:05 +0000

    get must force single aware

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
