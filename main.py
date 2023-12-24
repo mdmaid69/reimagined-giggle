a2caf3d5e52c08b4f8ddc5b77780c6c45352ceea
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:06:20 +0000

    expensive Kk key health letter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
