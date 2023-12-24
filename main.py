f215caef6989aa0d70c2590e2c7a13db35f9027d
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:35:19 +0000

    believe attractive door action state

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+print(os.getcwd())
