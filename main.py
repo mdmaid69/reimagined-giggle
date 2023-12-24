793cd4648246469635210b1d109d782f23c346fd
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:31:24 +0000

    better escape trial through interest

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import random
+print(random.randint(0, 100))
+def subtract_numbers(x, y):
+        return x - y
