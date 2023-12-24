30b26b932b047d81fbed53da31c25f00f62b2745
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:36:42 +0000

    shut later dream error want

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
