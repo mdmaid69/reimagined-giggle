6223d4dfae05d84894c4179e9c9e95e8090ff4d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:57:35 +0000

    relax hope flood bringforward contents

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def subtract_numbers(x, y):
+        return x - y
