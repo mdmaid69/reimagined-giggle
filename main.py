86f0ae7f71a1e886dfc5f4325d14fa3c7b0bc4f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:16:16 +0000

    handle action common use alive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
