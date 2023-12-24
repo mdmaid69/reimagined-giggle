865fd9347d161509655385a0262a9a190567c183
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:50:15 +0000

    report purpose tradition live mood

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def count_words(sentence):
+        return len(sentence.split())
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
