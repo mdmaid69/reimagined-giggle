434b38367099fa960f52c09a486db1e2f6ff95ee
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:04:39 +0000

    rain close result rough very

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
