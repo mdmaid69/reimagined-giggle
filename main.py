21166963b3b892e48e6278040232eb19a73919ff
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:44:55 +0000

    steep spot term mistake tidy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
