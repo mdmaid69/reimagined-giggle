d46540b56ad60f465f95bb55bdf3d0daa9124c82
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:33:03 +0000

    mess raise combine angry holiday

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
