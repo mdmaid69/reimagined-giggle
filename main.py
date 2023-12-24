c9c29683210ab9eec3d757469c9763e8edf17750
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:45:40 +0000

    search grateful hand character slowly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import sys
+print(sys.version)
