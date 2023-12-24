909b9444e69c77c53e81b9f2af6ab066043ba3f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:33:08 +0000

    grateful correct answer private modern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def sort_numbers(numbers):
+        return sorted(numbers)
