5e1c7b2020d45f0a0973954a5762d50e28e5516e
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:54:43 +0000

    dig valuable payment post movement

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import time
+print(time.time())
