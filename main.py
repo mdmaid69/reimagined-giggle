1f5c3974d074be2d9b59ec997e23b738472441f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:50:45 +0000

    regret here wild smile must

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_current_working_directory():
+        return os.getcwd()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
