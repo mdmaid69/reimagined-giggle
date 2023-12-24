7048d79e8b0693597de1f6b35d7b81dab6673bb2
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:07:47 +0000

    condition any 572enter judge share

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_string_from_array(array):
+        return array.tobytes()
