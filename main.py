1c3aa86a3f1f1cb7b8663a6d68b96e41bc1c574a
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:35:09 +0000

    hate proof pretty wave clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
