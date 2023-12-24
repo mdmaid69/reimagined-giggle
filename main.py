59cfa26c81b2b000f7a8599e9d68f2cb33f78ae5
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:22:41 +0000

    software complete both intelligent cool

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def count_elements(lst):
+        return len(lst)
