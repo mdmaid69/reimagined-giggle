07e0f13983ed53e8b52ed1c754db91202d5852f9
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:51:53 +0000

    stone behind wheel Mm listenfor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
