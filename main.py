6fba5cd01570ad72c3d6b2b76a5b306b58571551
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:14:42 +0000

    simple cool plastic cheat clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
