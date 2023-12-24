a1b1a9e935024d20be63be8dda24595a5436785b
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:13:46 +0000

    switch appeal play reasonable people

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
