ddeb45c8c2987963cc410dc99f6a4d639af7a48a
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:52:10 +0000

    drive anxious convenient to rubbish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
