acd825f47842357a8345e1d2b9004b4a7a9cbd09
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:15:05 +0000

    allow urgent hand explain physical

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
