6ebd82029a15a6307f5254d3549de9333abedc6d
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:42:36 +0000

    total chase lonely table forward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
