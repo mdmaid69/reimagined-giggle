53e77f0dad1565da45e507f855df5ad3ad257ed4
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:32:42 +0000

    immediately open purpose relax emergency

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def subtract_numbers(x, y):
+        return x - y
