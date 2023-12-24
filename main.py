0c2329b4608f3a586d1db33fc6d374bd1fdc7eb8
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:47:19 +0000

    laugh free dream correct OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
