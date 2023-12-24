413cf03a018a2a75475c6e75ac78c74f91d33b08
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:20:47 +0000

    press treatment since far lose

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def square_number(x):
+        return x**2
+def calculate_power(work, time):
+        return work / time
