785c47d3b00507be8579517270141672999fe7a9
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:29:24 +0000

    want force me home name

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_tangent(x):
+        return math.tan(x)
