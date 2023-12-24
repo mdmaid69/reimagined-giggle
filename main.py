c8bb8f0f89915dc9552a63ce5f2e15088417438a
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:27:41 +0000

    lend sight you gentle custom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
