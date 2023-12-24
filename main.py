d61846e73bf075268001d2863d2fe571fc4698a7
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:38:17 +0000

    middle survive mention brother protest

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
