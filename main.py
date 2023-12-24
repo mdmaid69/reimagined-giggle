0b8556ea773d9298c57547492eb3625bcb946aff
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:20:12 +0000

    mad region release tidy debt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
