428da2397316ced0392d0f433b406b5dc1617409
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:39:20 +0000

    probably store replace suggest tough

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
