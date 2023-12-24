c26a44547a2869c6804174bbf3fd868b47011800
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:13:27 +0000

    somebody swim empty good block

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
