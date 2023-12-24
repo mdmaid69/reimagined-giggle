c9eda9c403099922dc6d06f7a9fea6790dbe273b
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:55:32 +0000

    gooff Hh surprising complete process

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
