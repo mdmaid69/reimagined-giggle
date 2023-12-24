4a50e05ece9d76dded63f6767ff09114dd0cc17b
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:00:53 +0000

    interfere mixture treat talkoutof poor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  def convert_to_hex(n):
+        return hex(n)
