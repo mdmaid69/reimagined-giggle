f079f066f2fb4febfe088c1deb9fe4956f9d7ebb
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:31:17 +0000

    tense likely Hh emotion ring

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  def sort_list(lst):
-        return sorted(lst)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
