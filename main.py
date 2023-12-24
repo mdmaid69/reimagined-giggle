a1b9287616ac67bd912829e862b6c93c37979169
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:49:28 +0000

    stupid bye on success rub

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def reverse_list(lst):
-        return lst[::-1]
+def count_elements(lst):
+        return len(lst)
+import time
+def get_current_time():
+        return time.time()
