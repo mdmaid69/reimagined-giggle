927a7ba531c2afebe96e0eb8cda8accd51c949ba
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:41:30 +0000

    burn wise whichever ourselves trip

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_work(force, distance):
+        return force * distance
+def count_words(sentence):
+        return len(sentence.split())
