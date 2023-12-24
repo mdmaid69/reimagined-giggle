c0a1a835eee0e5e09c86a7985b3ff174d3dd1ebb
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:45:26 +0000

    scare neither nerve loan pupil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
