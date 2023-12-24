54249a1f1075d9a8f4a2852659b73ba65bf32c76
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:59:30 +0000

    into run whole ground about

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
 def is_prime(n):
-        if n < 2: return False
         for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
+                if n % i == 0:
                 return False
         return True
