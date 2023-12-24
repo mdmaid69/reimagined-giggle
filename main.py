7c05ffdb73c6f22ec19adb9bc64b0cfae2676956
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:57:46 +0000

    journey vote eager under present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def convert_to_binary(n):
-        return bin(n)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
