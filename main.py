99edc987620745c3a6156fcecc6942a715c5707f
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:28:37 +0000

    slightly somebody prevent area appeal

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
