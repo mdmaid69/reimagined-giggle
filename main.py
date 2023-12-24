3089e63e723b5add5801e7ae0e1bfc796f56e99b
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:44:07 +0000

    inquire theirs foot much supply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
