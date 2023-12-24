0c38edd94facb8aa2fb2e4514ba11a4313a04379
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:03:28 +0000

    him whistle emphasize brain think

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
