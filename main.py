7c39346c92714517da5b4c25466870f370473a5e
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:50:33 +0000

    base accordingto grow attitude block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
