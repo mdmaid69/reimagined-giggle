c556ad26caa3840abff7a638116ba251750bbc02
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:02:26 +0000

    target wise react feed announce

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import sys
+def print_python_version():
+        print(sys.version)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
