2284a817055ea0877900af93abf13ec5d942d1e8
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:32:09 +0000

    hour strike point used building

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
