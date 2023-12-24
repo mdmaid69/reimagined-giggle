c4f7c850c6b596d5ee8d0c7f416b6f38a115c363
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:25:24 +0000

    above accidentally collect room disagree

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
