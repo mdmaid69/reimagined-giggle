d5000adb6e1458f85348063eee7cc37693aed1a2
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:31:53 +0000

    left fix awake break pair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import sys
-def exit_program():
-        sys.exit()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
