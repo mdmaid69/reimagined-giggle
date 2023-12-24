9040e868e7b43692f8b1207e8063d4b2da9ccec4
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:10:03 +0000

    secret means best building police

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
