a040f7e2b2fc4f7028344c2ffea5968e985117e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:29:29 +0000

    block undo pick goodbye welcome

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
