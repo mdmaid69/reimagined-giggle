e9a3c8e6330ce68058165af8cb51df8bad88b707
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:59:20 +0000

    mention about petrol disappear wherever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def extend_array(array, iterable):
+        array.extend(iterable)
