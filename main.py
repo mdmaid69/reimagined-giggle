937c478d2b509acfbfd9d943f496ee2e93a3e4b3
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:57:24 +0000

    own solve even compete beneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
