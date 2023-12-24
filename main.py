d5095ff7549cc007dd25385e9cced8e6d3b56c97
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:31:08 +0000

    live welcome meet married wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
