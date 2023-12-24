ab5bc8bbb6b02bca756183e118a112a954f31374
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:21:10 +0000

    prove punishment save interfere hear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import collections
+def create_user_string():
+        return collections.UserString()
