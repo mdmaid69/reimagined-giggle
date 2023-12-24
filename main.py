0a2035922c089111042603ffc99f4b20ff3ec506
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:27:38 +0000

    letter important join bury exercise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_pressure(force, area):
+        return force / area
