61de49fc9c83e078126433359e5674ffe416b32b
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:25:58 +0000

    string split comein most connection

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_area_circle(r):
+        return 3.14 * r**2
   def remove_duplicates(lst):
         return list(set(lst))
