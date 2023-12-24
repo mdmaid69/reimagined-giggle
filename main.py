b148c508595cfd90f0b0cacc3b220e67807b899c
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:55:18 +0000

    pen separate shout finish you

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def sort_list(lst):
+        return sorted(lst)
