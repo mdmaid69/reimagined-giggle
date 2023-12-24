c4870448c85c5c4bfc5995030439f3c0f22b7b11
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:10:56 +0000

    post produce system walk transport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
