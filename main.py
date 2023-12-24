03abebe4ba98e0db34f3800e487d69b24dbc438b
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:32:25 +0000

    hope price careless runover advise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
