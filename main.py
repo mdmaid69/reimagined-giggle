9c0b72e1202fc6787fde2a755f8e724a065b655e
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:53:44 +0000

    whistle responsibility expect thing trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import math
+print(math.pi)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
