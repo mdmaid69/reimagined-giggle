2249bca03d6c049f664e28e522257a401a9d9df0
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:06:18 +0000

    thumb battery none eat somewhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
