bc1dfac462606a1824bc905ec4200489b9efa93a
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:54:50 +0000

    thanks watch mainly in experience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
