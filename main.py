39d79cd88ec48c67f9a1fd20c5f90dd239c7d57a
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:58:22 +0000

    length large basic finally post

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
