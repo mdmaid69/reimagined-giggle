9a2eeebf2392541f780b2998e45fccca2c2b155e
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:56:42 +0000

    minor conduct favour cut will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
