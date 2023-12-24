67a3dc78c9050c30373f31ed377bb45f8541a6c2
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:53:16 +0000

    see anything post hungry tip

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+for i in range(5):
+        print(i)
