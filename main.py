f53a258557725d6fe13f2a7d16fa52bd4aa699fe
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:10:47 +0000

    nice only vote apply oil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
