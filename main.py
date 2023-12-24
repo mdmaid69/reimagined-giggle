3dc125a48083e6e6db351c129e6955b2f41c799a
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:39:32 +0000

    field tonight cough lean design

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+print(time.time())
