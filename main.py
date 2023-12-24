466baf28835bf704865794db48593bd6ab1180d1
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:16:34 +0000

    date mystery regular rise fat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_as_set(array):
+        return set(array)
