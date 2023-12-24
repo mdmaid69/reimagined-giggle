a4c88a59a8c2e1ab21caa19ece681369c6e8c91e
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:41:48 +0000

    later squeeze ago closed difficult

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import sys
+def print_python_version():
+        return sys.version
+def count_words(sentence):
+        return len(sentence.split())
