1c14e7006551af003f9c69b1ad242e80059045fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:06:45 +0000

    wash chief sleep nowhere transport

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
