b8e6d0c00bddd0b6c75cedce0a4158e8da2bb522
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:39:30 +0000

    supply earn exhibition classroom heavy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
