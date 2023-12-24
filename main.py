806911c801654c7dd5e9fabc428c6c877eb3f9b5
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:06:19 +0000

    Uu inform definite family teach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
