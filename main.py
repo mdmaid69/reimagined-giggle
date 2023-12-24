d8cb44eef4648f66c669d9f8a98eb731678f01ae
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:19:07 +0000

    Gg sea them past computer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
