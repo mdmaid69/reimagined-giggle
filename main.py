a5c094c502d4495c0b01a817f7c7e559d01487fd
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:47:38 +0000

    wonder address badly terrible embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
