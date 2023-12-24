e4cd45003a4b34b3026b016d4a46a7eb30e3e2ee
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:38:25 +0000

    bored pass proper crash answer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
