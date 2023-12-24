cfe5b70b24a4803305da022c5ed825ae70a6b23a
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:06:15 +0000

    position information land direction later

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+print("Hello, world!")
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
