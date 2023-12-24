691befca870e77ccdf550da4501e76758ac1adb9
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:01:12 +0000

    recovery debt many forget come

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def square_number(x):
+        return x**2
