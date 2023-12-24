0974a6e2f24b6dae1674746deb40ade73b8d0d0f
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:52:25 +0000

    foreign store gate discover swallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_min(lst):
-        return min(lst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
