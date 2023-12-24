56163651db5d642d61e7c49d2206f7bfd7fd15e8
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:42:06 +0000

    physical priority accident judge care

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
