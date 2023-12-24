57ed0fe663de23d1985d2182c5521f034f5aa4f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:57:22 +0000

    bin free row on factory

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
