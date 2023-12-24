4eb1a812cb231ae0bf6de02109c280e5fefe7e5e
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:49:58 +0000

    first settle friend material pity

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
