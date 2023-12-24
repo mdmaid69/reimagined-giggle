7b50d12cff82f44f0da8deb7dacbbdc6fc1e5236
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:36:57 +0000

    injury before switch business offence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
