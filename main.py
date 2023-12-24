afca4d8eb2504e75f58df08f6265a52f3b44dfe0
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:58:30 +0000

    talk table expensive stamp region

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import platform
+def get_python_version():
+        return platform.python_version()
