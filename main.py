a21525b47896bba141578d6cd018a9a17d38e7f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:48:47 +0000

    chance sad police above lookaround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
