af56f094e2c2f7b3f53acb0d1d2ad719533483e7
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:02:38 +0000

    storm corner accidentally exactly between

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import sys
-print(sys.version)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
