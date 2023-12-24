36cfd4ceba478bc69101802aa106841d905f5631
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:20:44 +0000

    pretty friend bad event minute

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
+for i in range(5):
+        print(i)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
