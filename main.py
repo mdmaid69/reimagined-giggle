156f9d78b2f1c3c26eb34e48234ce6b826b5c86b
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:14:25 +0000

    wish with whole control cough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
