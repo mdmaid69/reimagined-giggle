006b0bf849b6aff6b96044c8fba8849b485b5161
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:32:00 +0000

    replace block rich manner opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def remove_duplicates(lst):
+        return list(set(lst))
