de4be49697b331f5946a668eb038e48fc9a153e6
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:53:50 +0000

    getback base wipe my quarter

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
   def multiply_numbers(x, y):
         return x * y
