279d17eef823a5219805e92f31cd475eccb0e228
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:54:05 +0000

    slowly walk hot scheme wild

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def reverse_string(s):
+        return s[::-1]
+import array
+def reverse_array(array):
+        array.reverse()
