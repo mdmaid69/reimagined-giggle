e25aec4195a5e2e6c74446a495666f7baf50e246
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:59:42 +0000

    runout listen getoff direction shop

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import time
+def get_current_time():
+        return time.ctime()
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
