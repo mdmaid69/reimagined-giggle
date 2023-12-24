680875fb380240af936a51ee7a76efc7ea201728
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:33:30 +0000

    obvious certainly professional quickly better

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
 import array
-def get_bytes_from_array(array):
+def get_string_from_array(array):
         return array.tobytes()
+def calculate_density(mass, volume):
+        return mass / volume
