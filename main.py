1cc78bec9b89c80cb0f17309562e24947bc848ba
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:44:41 +0000

    splash quantity happen careless characteristic

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
 import tempfile
 def create_temp_file():
         return tempfile.NamedTemporaryFile(delete=False)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
