decd1447f2a0a8b54723806b4f9c8fece67cce8e
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:11:57 +0000

    unable thumb mess environment later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import array
-def get_array_as_list(array):
-        return list(array)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
