410ffac43a730a042fe75da1c68543638d11af49
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:28:31 +0000

    organize many angry hope comeround

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
