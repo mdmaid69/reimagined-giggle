9f6b1560d13d8e7fcedfac90b50b8725c8cbbe50
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:49:33 +0000

    concerned risk pure dead empty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
