7a1fc5cfcfb6c5682016db04b9080b75d0395093
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:55:15 +0000

    pinch film region before clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
