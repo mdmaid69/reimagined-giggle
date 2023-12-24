903f264fd15ed2015bfdaae25c7425618cc7c25e
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:41:31 +0000

    big spring preparation turnover stir

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
