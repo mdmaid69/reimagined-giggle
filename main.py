53bf800cdf9d5807d8de13c8f84092d35e279bbb
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:53:05 +0000

    team busy object string narrow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import random
+print(random.randint(0, 100))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
