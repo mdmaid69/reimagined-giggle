7ccc4499a24aa4b94a09e0853e420c3f9a650bb7
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:04:20 +0000

    dish same mixture actual mine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+print(math.pi)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
