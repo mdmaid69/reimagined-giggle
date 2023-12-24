020b63dababa377358a7426fd9a3ae68ca59f814
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:21:55 +0000

    guilty cause bend cry relationship

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import numpy as np
-print(np.array([1, 2, 3]))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
