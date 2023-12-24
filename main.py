d3bf9c7fdba0b2d038ba2f2d4dbe766f8c049233
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:31:46 +0000

    please debt surprised smart work

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import os
-def get_environment_variable(var):
-        return os.getenv(var)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
