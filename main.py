7f4cd42474b8b8a81ed83ba0ff3db940f3c0a343
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:26:51 +0000

    inquire getout book visit let

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import sys
+def print_python_version():
+        print(sys.version)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
