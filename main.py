da898802fa1364a98cee596b2d847d8506e04f73
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:46:22 +0000

    best ordinary organization punishment chance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def subtract_numbers(x, y):
+        return x - y
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
