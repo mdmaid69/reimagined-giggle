3e948e217751d87afe38107aa39321796650fb5f
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:05:15 +0000

    female stick party over yesterday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def multiply_numbers(x, y):
+        return x * y
