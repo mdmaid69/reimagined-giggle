4fcc8945da8a30231dde67d86a8bde0a04472989
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:24:17 +0000

    sleep produce price condition shelter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
