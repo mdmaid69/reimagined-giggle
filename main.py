e1fad1ea9c21590bf521d81ecdda467c9ca05b3e
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:23:55 +0000

    care turnover shall sharp do

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
