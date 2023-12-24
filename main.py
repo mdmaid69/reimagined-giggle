0dd785777af9f4e2d4f740bdef49901ba12924aa
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:59:13 +0000

    information scared pride outside hard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import sys
+def exit_program():
+        sys.exit()
+def reverse_string(s):
+        return s[::-1]
