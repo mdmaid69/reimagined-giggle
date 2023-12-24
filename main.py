0ddd2cad5c89329605ea3f532e750c645fd3dfc8
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:23:27 +0000

    slide fold hurry niggaacademy me

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def reverse_array(array):
+        array.reverse()
