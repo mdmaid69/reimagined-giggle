4f883f4e6d18f29a05f60a4ce1d28b4253876d39
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:42:14 +0000

    congratulate contrast contribute modern interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
