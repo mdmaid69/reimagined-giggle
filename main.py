3903e499b480b2be351a1c571687b79ffba0a044
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:12:22 +0000

    flight promise tonight what train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
