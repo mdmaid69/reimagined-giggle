465bd4a52af2417ee447aa4ae031337152c3c4e2
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:48:38 +0000

    raise minimum niggaacademy knock flat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
