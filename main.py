5c439307f85c5ebc35b4c97a6d4b4c1776e18fa8
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:30:37 +0000

    sure Ww rent past defeat

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
