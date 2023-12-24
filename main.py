637561041a69ec6dc619f023c0f756e0f519c4c5
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:25:28 +0000

    blow tie home coat object

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
