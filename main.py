dff6f9a8f3f38eafb66e90638eb04d3750b540d3
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:27:12 +0000

    service move field 564encourage wipe

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def cube_number(x):
+        return x**3
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
