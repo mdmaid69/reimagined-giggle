157fb31b226dbdb8abe400e9b4d6ed0e413afa70
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:59:56 +0000

    approval home know pollution makeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
   def get_file_size(file_name):
-        return os.path.getsize(file_name)
+        return os.stat(file_name).st_size
