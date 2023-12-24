204d6f72852c3a661acb4e2be020960bc1aa8e0d
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:04:55 +0000

    certain ground Pp contact excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import datetime
-def get_current_date():
-        return datetime.date.today()
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+print(datetime.datetime.now())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
