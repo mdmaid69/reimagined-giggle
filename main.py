e7689e2c6768fcee4a350da3fc11bafc35c93b8b
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:33:02 +0000

    extra through confuse tomorrow to

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
