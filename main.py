67409fe6d1b1b1253fcefcbdaff3a7bfac25274a
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:40:09 +0000

    process necessary goodbye promise understand

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+print([x**2 for x in range(10)])
