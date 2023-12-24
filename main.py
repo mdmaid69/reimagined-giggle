b3661fc1ac38e76697c3e053d54e08f970e4c24e
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:51:34 +0000

    careful minimum lock juice present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
