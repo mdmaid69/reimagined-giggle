73a6a9403f80489b00b42bc92e83f6202cefe2b2
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:20:12 +0000

    trick foot lead half command

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
