51b223002fff7f12d14f0dc1512a34ddc0a534b0
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:13:34 +0000

    message whistle clever chief behave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-text = "Hello, world!"
-print("Characters:", len(text))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import os
+def remove_directory(path):
+        os.rmdir(path)
