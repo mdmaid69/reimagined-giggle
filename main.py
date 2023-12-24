7a1d3ce97e2ff137125cb655bbf546201d68b68f
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:26:20 +0000

    weekend loan round to spin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def get_array_as_list(array):
+        return list(array)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
