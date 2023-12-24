21f9065956cf60b5f9490b665a1657b4e5abfb59
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:14:28 +0000

    you battery happy grow since

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
