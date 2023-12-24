f8ed4aaa76de4b3b3dcc4fd55e14f75bfa70032f
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:00:02 +0000

    proper plate sit birth follow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
