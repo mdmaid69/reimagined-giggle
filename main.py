564e05c5630fa9875f5db63acefae70e8f46848c
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:03:49 +0000

    slight welcome description home order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
