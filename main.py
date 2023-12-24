4df16e36ca0f4f106ab2e90a0a3ea00754f0588a
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:24:21 +0000

    quantity mess rate total disappear

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
