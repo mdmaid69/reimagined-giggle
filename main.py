c7e562ab2515c3eee502c8dea1c26ae6c71544b8
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:05:38 +0000

    makeinto constantly bridge stand sore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
