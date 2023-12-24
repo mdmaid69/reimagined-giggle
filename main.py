0091b4f0f6145ee07d76c9f1813ee8f656085524
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:57:51 +0000

    tax yourself runinto natural suppose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import sys
-def print_python_version():
-        print(sys.version)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def get_array_as_str(array):
+        return str(array)
