6d5377c84d5901fa4a91015dbd30eb09bb11290d
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:21:29 +0000

    wonder embarrassing slip firm loose

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import json
+print(json.dumps({"name": "John", "age": 30}))
