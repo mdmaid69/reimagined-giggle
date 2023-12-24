3087118777606da36510f900acb69ab688489c30
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:42:06 +0000

    pair love small flow among

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
