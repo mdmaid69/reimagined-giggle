24560b84318649a71a85c3e4e5c45680dbfa9069
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:03:09 +0000

    under let autumn congratulate regional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
