b3113a8f229c13bcfe7ae56965d277bf108802b4
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:20:29 +0000

    empty slight use thought them

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
