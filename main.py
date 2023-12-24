866c94d61ee9687ece19ae3044b32a4967945fd1
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:52:33 +0000

    formal forget good there she

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
