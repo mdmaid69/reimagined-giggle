f7867c2c0737febd126cc5e6cc4d3ae874287bcd
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:31:43 +0000

    bet contents see stay central

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def get_array_index(array, item):
+        return array.index(item)
