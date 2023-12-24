3d22ace331939f2740e9b76929da656190569e67
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:21:40 +0000

    wander house fairly stay still

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
