da9a33360c298f0cda0aabf19e11c50f30c1f113
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:41:32 +0000

    excuse plate nearly drive tap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import math
+print(math.pi)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
