708cc9da744a3341086671a1f79f24364edefb30
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:06:00 +0000

    sport share ought reply heavy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
   def get_file_size(file_name):
         return os.path.getsize(file_name)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
