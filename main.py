e3fc0b7126caefa21eb9f7eef7289e7642816fe5
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:14:37 +0000

    mention interview plan we nor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def convert_to_octal(n):
+        return oct(n)
