ebbdcf475cf3018c4645856c358b161a9d63749b
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:57:26 +0000

    passage frighten suggest little right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
