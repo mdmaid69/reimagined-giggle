18a03805b4b105511de5c5fedcb025a2bdb5829b
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:22:05 +0000

    offence fork her noisy clever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_typecode(array):
+        return array.typecode
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
