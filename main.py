bf5addad82f329c8702966919c8e326b105acd59
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:57:02 +0000

    joke short recently edge increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
