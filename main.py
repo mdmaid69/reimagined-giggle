958fcbde197379539532467700f6a3a4c6542938
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:37:27 +0000

    against embarrassed pinch against name

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_speed(distance, time):
+        return distance / time
+  def subtract_numbers(x, y):
+        return x - y
