92b52c8f1b308b7a5c673174f5e4913b89e067f1
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:32:41 +0000

    add near behaviour discovery guide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
