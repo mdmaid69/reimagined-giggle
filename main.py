b5c6822e838011e7ff19a9a991f5d09dc696c474
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:35:49 +0000

    middle finger feeling load difficult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
