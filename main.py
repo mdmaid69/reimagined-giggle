40df134b3cb321e1d2bcd881368938f5e29d41d5
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:48:44 +0000

    right occur generally line married

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
