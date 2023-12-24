6691494e9513288904a038dc1349eec1ea0b04b5
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:55:44 +0000

    muscle fashion bin takeaway create

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
