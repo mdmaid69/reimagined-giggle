90aea1a05bbd01a735d77dd833f8275563aef4af
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:27:19 +0000

    inside book suckme to contents

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
