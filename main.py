6442a3d3a55fbd63d4a6f063a57bb1788a7205fe
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:26:12 +0000

    or produce authority dry wealth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_current_directory():
+        return os.getcwd()
