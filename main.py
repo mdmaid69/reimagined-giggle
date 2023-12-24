9892650e78ab7608e4ee77f62199d726cb120fb9
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:32:18 +0000

    perfect other soon 570enjoyable loud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def count_characters(sentence):
+        return len(sentence)
