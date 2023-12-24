60d7cf80c0ab0448128d20e340e5ec4cea6faf2e
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:48:09 +0000

    somehow music matter ignore basic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
