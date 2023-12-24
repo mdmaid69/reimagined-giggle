c5882adda8ac3fa7d12a4abdea77af4721571446
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:43:12 +0000

    actually comeover tie open save

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def sort_numbers(numbers):
-        return sorted(numbers)
+def count_elements(lst):
+        return len(lst)
+text = "Hello, world!"
+print("Words:", len(text.split()))
