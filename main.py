0cbc441ce72680f95066f8a63c55e0e97c5d6b1c
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:21:33 +0000

    little affect recognize familiar burn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
