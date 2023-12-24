1584dad9910bd21b59864365be9ee983d29f9368
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:31:29 +0000

    science group safety help rubbish

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_pressure(force, area):
-        return force / area
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
