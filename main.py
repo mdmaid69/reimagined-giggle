202b5e5dc2df8aa5f897f7bd57f9eeda3c755aa6
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:00:43 +0000

    comeout forget inside brain destroy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_directory_name(path):
+        return os.path.dirname(path)
