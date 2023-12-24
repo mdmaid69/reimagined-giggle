aa7d433ce45c474f08882267a3a53e8612e40cd7
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:22:38 +0000

    shade furniture soil public inquire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def split_path(path):
+        return os.path.split(path)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
