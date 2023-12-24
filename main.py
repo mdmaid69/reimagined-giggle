5dd4827e5e9bc5e53927f0b9305b7be962f55bc1
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:26:43 +0000

    contrast quit turninto Nn surface

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
