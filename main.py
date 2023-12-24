b4387289450d39b379d7bdb0811f6d35d3a80ca6
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:08:02 +0000

    thinkof quickly bother below practise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def convert_to_binary(n):
+        return bin(n)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
