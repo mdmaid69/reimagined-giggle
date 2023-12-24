b90dcdd962d2ac5befd97e06ea792593b57bda7a
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:36:03 +0000

    Cc rain crazy morning repeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
