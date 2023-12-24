0804e88e098bdf43701db452d6672348cd1f8b63
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:47:40 +0000

    length comfort reject scream patience

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
