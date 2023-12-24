46b78aabf85145233d2f897f2569dc90c39d029c
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:45:12 +0000

    strength open workupto bin boil

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
