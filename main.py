9af10a5ed585024f1d59e429d47ced96e464db27
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:17:38 +0000

    complain thick be move represent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
