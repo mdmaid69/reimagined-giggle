2a4e6d96b842b6eb7c2c586d5b8540fa9efa2292
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:02:18 +0000

    young sorry for wet judge

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def sort_list(lst):
+        return sorted(lst)
