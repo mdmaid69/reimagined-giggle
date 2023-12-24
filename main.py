c370c2fa84cfd158598c8d4293839652753bd80a
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:21:00 +0000

    valuable finish money uncertain company

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
