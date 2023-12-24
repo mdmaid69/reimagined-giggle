5c32ecbb251eb8fb9da53dc7a3d7f50318572b4a
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:34:15 +0000

    further explain error onto us

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
