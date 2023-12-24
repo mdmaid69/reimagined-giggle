4d7014510b354e965b6f3defa4c31877b9dea0c8
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:29:41 +0000

    professional offend delight protect disagree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import tensorflow as tf
+print(tf.__version__)
