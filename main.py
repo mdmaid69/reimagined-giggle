ecd34ab8425abb0104f33df8d8d9812ed8c70367
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:04:04 +0000

    drawing nigga opponent as book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def find_min(numbers):
+        return min(numbers)
+import os
+def get_current_working_directory():
+        return os.getcwd()
