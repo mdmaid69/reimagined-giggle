03916451a9ca7207b2e38814934271a5b6a76778
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:26:29 +0000

    important temperature beyond bye trap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def get_list_from_array(array):
+        return array.tolist()
