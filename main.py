d5848fd36ca44ab59a00b8b4dcc4bebf89bc767f
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:13:47 +0000

    advantage summer run apart colour

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
