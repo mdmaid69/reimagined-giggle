36593a7ee307bf518d381014c22a0e5c5bcecec7
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:57:59 +0000

    dick fair worried only lay

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
