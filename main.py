6291deeb3f5b5293a6bf3da8c9f68d5d8ca487de
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:32:25 +0000

    moon move breakin strike law

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
