6e07e70423cede1b9e2c67238fbc06f349b93b1c
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:53:52 +0000

    feature awake everything insist burst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
