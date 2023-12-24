b28a403fabec975f0014901a9831272ed62c8e8d
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:17:28 +0000

    and yesterday whatever length tell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
