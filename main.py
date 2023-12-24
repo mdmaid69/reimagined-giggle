dde24fc0bf2c88365bd6b36d8c6b73068237f150
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:43:34 +0000

    company independent into colour apologizeapologise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
