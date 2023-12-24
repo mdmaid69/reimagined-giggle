0d68569e35507047e0f1f7d025416b855067e98e
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:09:42 +0000

    tear also even different see

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def is_palindrome(s):
+        return s == s[::-1]
