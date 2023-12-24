60ac8ba8bae19982a3e489af7c251cab387c5294
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:14:45 +0000

    speech murder record plant deaf

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def get_current_date():
+        return datetime.date.today()
