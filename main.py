7532e3c1a1f0913e42dbe15cf5bd357d548c523f
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:32:32 +0000

    decide no murder soak ought

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
