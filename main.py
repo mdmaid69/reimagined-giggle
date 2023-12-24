6d15e44c825f9f1b11b876e263f4a6d7ac981c4c
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:17:57 +0000

    confused account country benefit telephone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
