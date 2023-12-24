e7cd46f86117344eebac3e628fa76cca6627c864
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:56:07 +0000

    female shut unfortunate willing importance

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
