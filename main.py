4a04c3aedfd0101de7babb7213bdecdbd055a351
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:20:51 +0000

    swallow side load right cruelty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
