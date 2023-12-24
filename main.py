05fe4cdcc6c685c3c72cbf5f45475aab54883eed
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:03:17 +0000

    lunch sign splash couple ground

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import sys
-  def get_python_version():
-        return sys.version
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
