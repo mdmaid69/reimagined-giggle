308c9554f098304a3c8b578d66964127fbad9fff
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:50:04 +0000

    warn steal problem concerned apply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
