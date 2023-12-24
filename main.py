0ecb5d7b8d41e0705212da7de6c54237c942994d
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:56:08 +0000

    although runinto lookback(on) complaint depend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def is_odd(n):
+        return n % 2 != 0
