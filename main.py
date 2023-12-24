86c7db67f56203d0d6d920cfb430c0a16b05ab9d
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:05:19 +0000

    switch cap concern block service

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
