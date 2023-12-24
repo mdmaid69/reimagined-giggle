a1e3ff9ed05bbe3cfd129471df95cc0e076c55a9
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:43:43 +0000

    pay pretend shape plenty wealth

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
