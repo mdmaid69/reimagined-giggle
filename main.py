ee86bec1a19559b03bd6d7e6d36123d8eca0bc8b
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:17:02 +0000

    male themselves poison similar let

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
