60fd2d3e628ecd6ff3253e0551dde272eca2bcae
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:38:27 +0000

    similar fire square church mix

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import tensorflow as tf
+print(tf.__version__)
