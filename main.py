1091432f5bc3c3a3e038e5ea457b51c53b4dacd7
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:22:56 +0000

    earn disapprove hello power physical

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
