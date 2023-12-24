c5b6b9a22002532125467b756c88f1a23e1e1d62
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:16:56 +0000

    sign record brave Ss bringdown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
