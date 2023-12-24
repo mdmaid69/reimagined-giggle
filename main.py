e3243831d702d26923243428753ed233ba3639be
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:03:49 +0000

    minimum slice see grateful trip

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
