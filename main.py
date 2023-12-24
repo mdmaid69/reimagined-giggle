ac1b329cd1f91d19a50928cc91d2452e1c610842
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:51:58 +0000

    pollution area Kk politics spite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
