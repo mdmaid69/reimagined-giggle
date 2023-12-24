184bc06dbb3a990abb8ec18571cd5182abfc70a9
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:13:35 +0000

    fetch holdup cum relative connected

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def get_array_as_list(array):
+        return list(array)
+  def square_number(x):
+        return x**2
