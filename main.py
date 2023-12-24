3edff0a5d8364547ab440fe8c028b244533435d3
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:17:04 +0000

    continue and kill wire better

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
