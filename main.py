d0d829315552655b54fad7aa7ef7badb7029448e
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:37:40 +0000

    survive tragedy swallow battery until

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_area_circle(r):
+        return 3.14 * r**2
