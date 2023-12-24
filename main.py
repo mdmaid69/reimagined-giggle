4d3bbd2eb83c6c75dab1ee351e4cc280ebcc1ec5
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:40:22 +0000

    tip ability price wind last

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
