cebb314bbc550ea69d5cddd4fc6f6ffcc77bc0af
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:50:26 +0000

    shock goaway doctor public fairly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
