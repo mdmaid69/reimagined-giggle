cc3e6283f1baad87b1d8cada1b7d87c110b8a128
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:49:25 +0000

    whether pause lower godown dream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def reverse_list(lst):
+        return lst[::-1]
