57620ae40e8fac570050c06a0513aa5964bfbb41
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:22:19 +0000

    sign return join train workout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
