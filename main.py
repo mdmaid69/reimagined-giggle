a800af27f979673b1f711263ba50296ef887ba90
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:10:49 +0000

    sale centre objection uponon abroad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
