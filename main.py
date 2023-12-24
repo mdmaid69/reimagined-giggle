75d63c509960428741b071d5dacb3ecd3ac8e97e
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:02:32 +0000

    in never morning test negative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def find_min(numbers):
-        return min(numbers)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
