fd8a62ed778f41161fd8b75cf3bdc26bc4af6131
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:34:09 +0000

    quiet public definite match comeup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
