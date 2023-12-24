ff14b96d6530b119ff8700168e788fa2cd2f2013
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:52:36 +0000

    bet mail wrong ought mess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
