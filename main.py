1dc21cf559833c940295fd993f60cc5140a5a245
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:13:00 +0000

    blow soil because nurse shrink

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
