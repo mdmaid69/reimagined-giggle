2a4c8f862cf291c22e1d3b3adb6b18e23ce722df
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:07:07 +0000

    accidentally lead her system miss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
