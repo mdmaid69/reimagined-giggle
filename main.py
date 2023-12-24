b7f7e74f55a01b8011d11eb8c8a4839f8a7c2f50
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:06:57 +0000

    lock breathe expression example slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
