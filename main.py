eb7a34ae13290508a25ab833899bc74890ead5c3
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:41:43 +0000

    loose factory really glass every

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def multiply_numbers(x, y):
-        return x * y
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
