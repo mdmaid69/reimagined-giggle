dce67fcca51d8c569c7c19198f455473f9f6ba55
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:45:27 +0000

    memory behaviour cheque usual frame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def is_even(n):
+        return n % 2 == 0
