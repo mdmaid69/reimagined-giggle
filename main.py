aad34f34a840f1533c8980fb2484fbb2aa3d1500
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:05:26 +0000

    breathe journey upset private black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
