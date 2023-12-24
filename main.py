ec65be63c719f5e1c581924159c161dc9ac46f9b
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:41:23 +0000

    offence quiet sleep section process

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
