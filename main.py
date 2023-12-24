36efa72b52a477222334718f818f72342861741a
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:49:16 +0000

    mine talkback cover suffer that

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
