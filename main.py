78cc87b210e4ed47d28aa09b74f25de23fbb186a
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:51:59 +0000

    rely wide keepup coat anyone

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import array
-def get_array_as_repr(array):
-        return repr(array)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
