aa4e9713f497f876731685bccead14f38acd9f21
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:27:57 +0000

    fast sorry system public problem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import platform
-def get_os_info():
-        return platform.uname()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import time
+print(time.time())
