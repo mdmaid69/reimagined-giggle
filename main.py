283a5ec9c7d6de213a16f92548c064fe7f34fdc2
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:06:40 +0000

    side birth breakinto chin base

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import tensorflow as tf
+print(tf.__version__)
