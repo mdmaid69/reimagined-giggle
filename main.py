6e39fbe982c2c2ccde24cf9846d3b3a91af29b08
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:26:17 +0000

    one street key surprised slip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
