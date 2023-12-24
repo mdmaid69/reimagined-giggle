a910106a2340db22f2ffe82c492d6198697d6954
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:05:32 +0000

    try store own giveout new

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
