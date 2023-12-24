40df69758c343b5c5cb5133a321426c18e217965
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:58:15 +0000

    light behave improve instrument man

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  def count_elements(lst):
+        return len(lst)
