95d680000bc0a4f99d7aab2a6498c52d65f9b138
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:31:46 +0000

    second deliver design talk up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
