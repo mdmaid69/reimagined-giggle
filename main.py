1272837b7d8af51bee8a970371e08fb535af8f2a
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:45:16 +0000

    in tense drink trust Rr

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def convert_to_hex(n):
-        return hex(n)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
