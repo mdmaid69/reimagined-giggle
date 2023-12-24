b7bd3e09aab3fe4105656a5a1661582a3280088a
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:17:55 +0000

    consult although while wild lose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
