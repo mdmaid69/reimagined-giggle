ec1f0f329505c67f5e70b9cb6c80741d05a863ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:04:26 +0000

    slight suckme like tooth behind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-def is_even(n):
-        return n % 2 == 0
+def calculate_arc_sine(x):
+        return math.asin(x)
