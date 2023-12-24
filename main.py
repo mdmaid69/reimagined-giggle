e48d566fdab4588003dfab268606e1ca2c3bc08c
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:00:39 +0000

    makeinto desperate mark travel thumb

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
