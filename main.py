85deee903a638d90bb56e99a5ae70304179ab979
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:02:43 +0000

    personal bear sort with outside

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
