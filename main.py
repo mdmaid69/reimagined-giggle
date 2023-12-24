7114a4d9b6b1d2c349ad1cb62deada631171f3f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:50:31 +0000

    cook trousers separate on sit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
