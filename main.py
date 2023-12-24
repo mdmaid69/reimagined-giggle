7f40b839023c35b97e33fb6e0941ab26a2a0251d
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:11:47 +0000

    black knowledge to brief steam

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
