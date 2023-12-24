070024e5c1b90cf296434065237b5f085099db4b
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:00:31 +0000

    another comeout shine arm region

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
