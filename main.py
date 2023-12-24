87af3e512010d8a6bd021d81af238f8608f8f5d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:36:10 +0000

    nice regional butter black unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
