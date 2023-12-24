780d9410a3391ef522f49afe5269e02f1655fe88
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:50:16 +0000

    skirt argument similar recovery kind

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
