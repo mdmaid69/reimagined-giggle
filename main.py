fe84d0ad5cd8f0da1ae7b9d01e1cada7ebb53b43
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:22:50 +0000

    grow past mark side difficulty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+text = "Hello, world!"
+print("Characters:", len(text))
