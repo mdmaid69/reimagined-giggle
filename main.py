51e75a04c32d7de663d4add50d7566545fabd469
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:21:15 +0000

    fresh hurt answer reduce fear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
