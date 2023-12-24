e91154844f0a458f6f72dc58d782cb1f36b42572
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:42:37 +0000

    radio comeoff transport discover trial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
