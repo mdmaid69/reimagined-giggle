7c6f0f4e13392c14c3676469d767912f9e3f0451
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:33:05 +0000

    custom walk above fat equipment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
