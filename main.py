8f8809665cb63d3c7c541f3458a8404c4708ed84
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:33:45 +0000

    port confident luck workupto unite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
