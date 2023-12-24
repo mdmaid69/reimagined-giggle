45a04e4c7c9ba3f31911556d3db2deeae2628dc7
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:51:03 +0000

    part heavy reserve frightened reliable

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
