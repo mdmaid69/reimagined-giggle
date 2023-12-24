0496353e5e1f52a094aa0beb0d7e247fbbed9f31
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:15:05 +0000

    walk advice again beside brush

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
