35bf07671b94f3147495257ac6d0dabd7119feb1
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:40:09 +0000

    trip murder branch they breakup

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
