b25c9440631c671b97e2e3dd64012b659bc2415c
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:38:38 +0000

    unusual make damage contain anywhere

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
