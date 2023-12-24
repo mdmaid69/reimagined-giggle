84224714ec4b34188aa2d75778654fd0526dd436
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:11:11 +0000

    forward his few freeze fond

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
