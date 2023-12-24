2c321f8f71f78dbfdd2a5991ee6ad5c558613299
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:37:10 +0000

    emergency thinkthrough bottom service rule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
