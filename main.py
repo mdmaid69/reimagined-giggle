6e902781b33a6b36be9a2dbb2960602bf65a6500
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:29:00 +0000

    work tonight score please hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_speed(distance, time):
+        return distance / time
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
