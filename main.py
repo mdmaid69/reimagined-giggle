fee3d585410f281acb942435fcea8a208778aa50
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:07:58 +0000

    impressive comefrom alive knock cruelty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
