3699c65b77410a03de7d3a09d6227b1f49b55700
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:48:20 +0000

    total main always justice scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
