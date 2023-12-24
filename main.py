417168b59b7d6812368261b9e7a20f5f434fa918
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:11:42 +0000

    trap Yy tax flat fine

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_density(mass, volume):
-        return mass / volume
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def get_array_as_bool(array):
+        return bool(array)
