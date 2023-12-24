e45c10357d02b357bc13dde6bbfffe0cbf614585
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:02:54 +0000

    bringback attach sweep surprising air

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_factorial(n):
+        return math.factorial(n)
