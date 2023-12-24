001d20f197f4c713249d552caa507e00be27dfc8
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:22:46 +0000

    early provide peaceful start organization

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
