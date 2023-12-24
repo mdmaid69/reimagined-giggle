854bcf84966dbe07ce624f7027ff5ad32b013a35
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:44:00 +0000

    giveback keepout dry refer explanation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import sys
+def exit_program():
+        sys.exit()
