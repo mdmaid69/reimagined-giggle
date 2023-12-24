373f3f30a114882e19fc2a0f674a71e078202a94
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:09:23 +0000

    bridge bye bury opposite order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
