1f86ed038e9380ad9515c25ee5116ce5d4f85efb
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:40:37 +0000

    side thinkof cope whenever minimum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def convert_to_octal(n):
-        return oct(n)
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def append_to_array(array, item):
+        array.append(item)
