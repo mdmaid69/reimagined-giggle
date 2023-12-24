432a7afd8ecbd6eb81925def928cfcde3b4721fa
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:41:18 +0000

    dick neither worth build Gg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_union(list1, list2):
+        return set(list1) | set(list2)
