21920e4cdb2ebb72d3c0555e61599a0d591eceeb
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:13:25 +0000

    middle share decide between afternoon

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
