4795dcdfe9260a1dfa6d4d414677a4abcfe6f9df
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:37:13 +0000

    trade sail escape attract flood

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-print(sum(range(10)))
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
