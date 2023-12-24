c14f340bdd5aadb4e8ea45ffa727f9ab5c36553b
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:10:05 +0000

    shadow put comfort limit second

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
