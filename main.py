d60ba3535a7782ff609d63714fc9ab90179911b7
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:18:51 +0000

    earn boring rush foreign later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def reverse_string(s):
+        return s[::-1]
