37d034dbcb1bd514ddaa196e83ec2931790ed5f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:06:53 +0000

    offer show authority door consult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import platform
+def get_os_info():
+        return platform.uname()
