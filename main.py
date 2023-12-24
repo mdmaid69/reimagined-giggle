e26d6858f2ff440dcecc54d5464788243cf74df4
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:47:56 +0000

    agree share lookafter section difference

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
