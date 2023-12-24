624270a9216be091fb51f73564e4abce544aae5f
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:49:19 +0000

    swing late artificial crowded journey

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import time
-def get_current_time():
-        return time.time()
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
