f28edf56f1104c66a9ca5e19421b50a8152b7147
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:22:02 +0000

    actual attach tidy busy band

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
