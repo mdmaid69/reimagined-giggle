5bca141fcc4a6d456c29391dfef3ade41f44daa3
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:44:53 +0000

    peace dry crisis welcome exact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import sys
+def exit_program():
+        sys.exit()
