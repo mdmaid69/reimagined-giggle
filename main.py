d143afec320ce913cad20a9e216c6a03e803acab
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:28:49 +0000

    might rely surprise fire duty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
