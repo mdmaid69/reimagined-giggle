c7991b507e8fc90986d90541a757d10f7a58dc5e
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:03:20 +0000

    contrast tomorrow flow deserve mention

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
