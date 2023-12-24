ba809b45738ba1cb29eaaa9147a4fdabd3a77818
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:10:20 +0000

    phone rough receive runaway account

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-name = "Python"
-print("Hello,", name)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
