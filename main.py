6002355a526412ec83b50744f6181c91c2da2507
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:49:29 +0000

    regard soil afterwards suppose goup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
