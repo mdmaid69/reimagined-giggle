8ce116254f1ba7c46128afc236734cbd988428e8
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:36:10 +0000

    force achieve describe what repeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
