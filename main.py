fcd99489f01f166dd522f2d850fa9ad68d0c36d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:41:53 +0000

    estimate careful injury study arrange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
