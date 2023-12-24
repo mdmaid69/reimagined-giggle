9644be75bf12c9ca75f89ec94e640e147499edb9
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:33:36 +0000

    want problem television turnout long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_counter():
+        return collections.Counter()
