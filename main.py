6daf890ad535bb7d5cb7c57dcf4f3f9de4029e51
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:18:26 +0000

    start accommodation beat instant free

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
