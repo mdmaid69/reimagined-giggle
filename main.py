9607d0e2eaaff97b4479b9420fbaa3692cb05757
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:46:22 +0000

    represent constantly apart burn remain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
