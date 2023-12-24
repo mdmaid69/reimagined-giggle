1190d6ee977bf9b5e3d49751e7a97d4c6339619d
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:25:38 +0000

    fear measure divide damage recently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
