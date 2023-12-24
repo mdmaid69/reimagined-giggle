f9abceaa2a9e1aeddf4811f75493b9747db51206
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:33:25 +0000

    good television confuse exactly do

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
