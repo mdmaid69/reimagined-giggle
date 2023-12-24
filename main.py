27dda619ab508dd4fda10603045c3d8deb25e0cf
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:46:09 +0000

    convinced treat translate trust stir

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
