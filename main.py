4a1196badba48dc68bd38e0382d15ff2bd0111aa
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:10:00 +0000

    match doctor would clever uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def append_to_array(array, item):
+        array.append(item)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
