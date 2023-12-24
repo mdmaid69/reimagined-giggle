92e24f5f84f883ed755d1b231683455a9ee08ed9
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:24:10 +0000

    go confidence secret exercise correction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import collections
-def create_queue():
-        return collections.deque()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
