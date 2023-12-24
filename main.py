2e333e53cbf081f8e0fb1d6115e66986a5befedc
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:41:43 +0000

    everyone birthday sure spring on

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
