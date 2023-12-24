91d94fce9b3f5fed6af2f332b4cfdb57760d40ed
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:16:20 +0000

    or final fasten light practical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import platform
+def get_os_info():
+        return platform.uname()
