460dc435e9f4d21755375aa7bf4942e2639e59e9
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:41:47 +0000

    black shrink cut instead maintain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
