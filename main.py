234f570e7dbd0237d7910d3db607d37c96b6fa8b
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:21:18 +0000

    see learn goon report please

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def square_number(x):
-        return x**2
-def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_acceleration(speed, time):
+        return speed / time
