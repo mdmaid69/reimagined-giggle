1f6b3390855b4e5a6aa35b926f888facad792d66
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:19:20 +0000

    instruction support whenever disagree smile

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import tensorflow as tf
+print(tf.__version__)
