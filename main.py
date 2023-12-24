1696001cb61cd1a3dfec13b0b9ccd5666af6921f
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:54:52 +0000

    night top behave deliver recently

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
