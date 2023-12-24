919880de2f89117076147d8bd2c418843f5600bd
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:07:32 +0000

    mix join tend discovery crime

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
