0223d9e2721ef29d6284b1389a28f6ac951c244b
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:24:08 +0000

    region this against getout phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import collections
+def create_queue():
+        return collections.deque()
