39e2fc29719808c11d357c219711b4951571baaf
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:01:36 +0000

    rarely remark question work keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
