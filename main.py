044fe40d63c9460e56899a76dca56a82e5f38c52
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:44:41 +0000

    comeover contrast sensible plenty silent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_directory_name(path):
+        return os.path.dirname(path)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
