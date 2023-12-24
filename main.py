edb53b923649c18977c6bb0f9ef2d253fd46151a
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:53:19 +0000

    full performance tone early clear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_area_rectangle(l, w):
+        return l * w
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
