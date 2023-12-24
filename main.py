4c5a1923ce2e04a27438f48ddd2ead68e5edf006
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:43:52 +0000

    position cover profit needle rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def get_array_as_int(array):
+        return int(array[0])
