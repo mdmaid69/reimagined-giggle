b59ab87b9a5d078f8203937ecf6821b5f8936ac7
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:17:20 +0000

    possible group bath sound expression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
