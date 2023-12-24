9e1abc43940974ab015a5a3d9e34859bb08dbffd
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:38:36 +0000

    duty wind attempt shadow delicate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
