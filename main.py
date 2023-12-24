1f03c6b4adc45f2f4527276f0645df8c51335f12
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:55:13 +0000

    colour row fasten stage boil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
