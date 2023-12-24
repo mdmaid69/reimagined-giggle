dd424790a61f3c761e359f173a235f9420b1ba52
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:20:24 +0000

    repair spend mood on slight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import time
+def get_current_time():
+        return time.ctime()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
