9760f9a1f3ba70507221e03eed31dd2ab0edaba3
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:08:06 +0000

    none count laugh unhappy fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
