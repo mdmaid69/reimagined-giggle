d1d7979bb785aecb1750cd71049c84a2fd5c88a2
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:07:28 +0000

    concentrate day medium live interfere

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
