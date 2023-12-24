4e1409d904c36d8b5084f42e2ce39c251bcb5b2a
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:53:34 +0000

    accident play hang makeup keepup

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
