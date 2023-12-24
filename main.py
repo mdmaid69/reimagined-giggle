c36645c1da346e9933383252b691dbef1f21794f
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:30:30 +0000

    attention rise programme store draw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
