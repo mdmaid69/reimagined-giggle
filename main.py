543e08f988cf7afc5de2e75bc1de37e7b584a032
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:26:53 +0000

    tap goout carry smoke boring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import os
+print(os.getcwd())
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
