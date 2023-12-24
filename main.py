982b9664da580bfd5168b0aff320198102f8a410
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:16:48 +0000

    slide stiff difference dick suggestion

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
