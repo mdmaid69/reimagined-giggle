066309a32a361765fb204fa65886f1bd50763e87
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:05:09 +0000

    whom needle impression work little

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
