c94d5d27605ce0787b4b98908cd07c37ee76a0b0
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:51:54 +0000

    acceptable price hurt good bang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
