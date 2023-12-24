314152999ffec44fc90cd1c7707b68ed7e656e2b
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:52:30 +0000

    remember reject send attractive classroom

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
