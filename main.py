e11c44d5e276549b08e1b1a74f2c2be4b831a0b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:54:50 +0000

    factory bang comment comeround term

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
