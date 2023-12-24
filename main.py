84b36f33f87e0048f1b5362885ee8f6603cd4a54
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:29:39 +0000

    bet runinto dress furniture neat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
