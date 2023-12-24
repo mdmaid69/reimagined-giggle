e5857aeb8f425ca6c9d9e28047ba07e73bc677af
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:33:07 +0000

    flat mood he suck name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
 import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
