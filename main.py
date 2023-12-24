a18fed7c78bc11403935a80449d451bab75e609b
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:20:24 +0000

    base advise whoever so past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
