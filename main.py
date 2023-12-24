b5d922302992be29bae60874e708d308b6212ca3
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:29:15 +0000

    loan post dig willing turnon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def reverse_list(lst):
+        return lst[::-1]
+def reverse_list(lst):
+        return lst[::-1]
