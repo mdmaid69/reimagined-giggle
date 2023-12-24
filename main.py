1964f9c3199aacd139d68dcf8d4289c212b9e391
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:36:15 +0000

    not innocent bus thick style

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def reverse_array(array):
+        array.reverse()
