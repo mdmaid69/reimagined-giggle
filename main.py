ca4d59fef27b61224432f8bb070c903135a47f97
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:37:25 +0000

    bottom chat reasonable connected would

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
 import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_as_float(array):
+        return float(array[0])
