ecc35761bfbde19f458ad97ab05870f339432dca
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:21:59 +0000

    pull badly from right power

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_speed(distance, time):
+        return distance / time
