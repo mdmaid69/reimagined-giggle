a5edac1dd7990dd360283b7499efd902b6cec038
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:01:09 +0000

    suffer 564encourage pair contribute nothing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
