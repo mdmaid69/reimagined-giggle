7e2a38ddc77dfb80c069ee48e5078bec16a5f998
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:15:11 +0000

    string other famous nor persuade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
