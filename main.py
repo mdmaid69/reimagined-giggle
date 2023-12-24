3382e7462b1bf9aaed853dd34ef35d993ff5a04a
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:53:42 +0000

    whistle add cool fill and

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
