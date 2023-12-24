213c9359eebba5c493fbf584d4e79026816d4c1f
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:21:28 +0000

    onto coffee used waste them

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def clear_array(array):
+        array *= 0
