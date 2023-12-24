50a281337b67274b15329a181df02717dc841731
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:54:24 +0000

    warm long would hers pleasure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
