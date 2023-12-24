2f3ec9e1b169f2f8357282b7d5e08bbd45f0224b
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:36:22 +0000

    noone sit luck stay turnup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
