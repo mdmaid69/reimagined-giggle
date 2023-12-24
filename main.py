0214154e8cbbe6083b237b4a67e8dd8c89b49e17
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:47:08 +0000

    project one feed steal unit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_cosine(x):
+        return math.cos(x)
