02e2e18476bef7a9f18bc58c304d4c152b4136ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:10:12 +0000

    part weigh soft keepup speech

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_binary(n):
-        return bin(n)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
