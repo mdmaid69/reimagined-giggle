d800d6e0085f6f20f7e3f0692fe5ebc9b2f6c15d
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:32:44 +0000

    meal exercise keep Qq copy

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import getpass
+def get_username():
+        return getpass.getuser()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
