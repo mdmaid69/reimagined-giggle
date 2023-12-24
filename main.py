1d2c25a1a2ba4f1e5ca7fc8aa1d8e71cdbebab43
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:48:52 +0000

    little rule emphasis point so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+print(json.dumps({"name": "John", "age": 30}))
