e9a8adec9feb1eabfa0f4d3c75743910c6783ea5
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:21:57 +0000

    professional name later reach exhibition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
