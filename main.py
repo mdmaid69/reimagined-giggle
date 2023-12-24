b3bbfe3abd27cffb7728736774b63864b8cec2cd
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:29:40 +0000

    cheat violent matter complain minor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def square_number(x):
+        return x**2
