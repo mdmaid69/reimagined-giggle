cd642de3b462f06e72c89a8249ddba0b0f351714
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:37:06 +0000

    rough store loud spray definite

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+print([x**2 for x in range(10)])
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
