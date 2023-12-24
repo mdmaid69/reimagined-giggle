38f1241e7653d0b94d27e13d84ebad85e4dd371f
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:27:08 +0000

    pupil law secret upset sorry

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def get_array_as_int(array):
+        return int(array[0])
