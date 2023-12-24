e55bac74b820f74bbae9f7041277d2973873759f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:18:35 +0000

    future raw indeed ear issue

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def get_array_as_repr(array):
+        return repr(array)
