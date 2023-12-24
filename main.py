0fe9ea5c7a54dd2224c729cce47b8cab298118d7
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:21:35 +0000

    wear afterwards back can destroy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
