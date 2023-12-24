904bf0dd1f03d1965ec037991b2d4ce75f882552
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:06:19 +0000

    breakdown turndown takeback much head

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
