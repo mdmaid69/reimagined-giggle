e4b91c148942fe27ef43ec3334d8be675e289833
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:48:10 +0000

    sky past remain tradition independent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
