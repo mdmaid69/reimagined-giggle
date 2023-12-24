9ee7f0410e87b0497b31262643c9de64797f2fc0
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:28:19 +0000

    unfortunate excuse come search justice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def convert_to_octal(n):
+        return oct(n)
