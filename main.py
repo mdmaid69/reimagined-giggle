1dba322ee20a4de5dbe4fe2229d969dd012814b2
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:50:15 +0000

    extra mother turnon impressed risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import os
+print(os.getcwd())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
