c228d5ef0c90260eaa4f5e88393d3b08796f6147
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:37:32 +0000

    section nearly company turnback noisy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
