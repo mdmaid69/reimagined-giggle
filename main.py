720f8217f80a01423c9a729a11aa0bf0130d0bf5
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:45:28 +0000

    switch suit police shadow expensive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
