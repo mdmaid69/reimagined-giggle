42b6d5314132acd34f70bbdf1a33a46446b3db7d
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:49:04 +0000

    freeze neither winter pity entrance

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
