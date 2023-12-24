2ccbc31466ca093a7e4464e82f9bc4464bbfed80
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:14:00 +0000

    sensitive club proud stiff cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
