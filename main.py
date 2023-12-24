ef8caa719fcec91a45e03c86f9bd8c983f18820d
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:38:29 +0000

    popular port office recent used

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def count_words(sentence):
+        return len(sentence.split())
