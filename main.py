ffe95d49b64f1c4a838853f9bf902f596ad78d0d
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:21:50 +0000

    accidentally fit centimetre share permission

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def split_path(path):
+        return os.path.split(path)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
