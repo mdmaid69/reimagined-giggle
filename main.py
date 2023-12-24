d41cf862ba77aa606a20be7ed38252af2721e5fc
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:54:44 +0000

    backwards slip check use Vv

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
