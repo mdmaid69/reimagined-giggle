8e823646f51199e7be6f3350a888561abee9962d
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:42:47 +0000

    hate contrast future frequent my

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
