1644e1d16baf769d80b9bc17e2f7b789881f3936
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:29:00 +0000

    getback dry pay land object

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
