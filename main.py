185a33f406ab68b297ecac5a508d044b5a6bf37b
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:47:01 +0000

    watch cover whatever probably travel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
