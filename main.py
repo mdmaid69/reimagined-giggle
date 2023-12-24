6b094d13bb53b74ccb6e52b1cd7dee5f6be4b868
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:47:16 +0000

    blame unless picture exist declaration

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def is_even(n):
+        return n % 2 == 0
