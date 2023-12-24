f526701476b296f0da186f988764f59149937fb8
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:00:53 +0000

    project along put today emergency

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def square_number(x):
+        return x**2
