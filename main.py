a2f328d080919f38294b9ea1aea5f3f0f9bc29e7
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:41:11 +0000

    boat fun borrow charge there

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import numpy as np
-print(np.array([1, 2, 3]))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
