b73721033518b3b4b58695c2f3a569bc16730959
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:00:24 +0000

    size makeup direct eye workon

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import platform
-def get_python_version():
-        return platform.python_version()
+  def square_number(x):
+        return x**2
+  def is_even(n):
+        return n % 2 == 0
