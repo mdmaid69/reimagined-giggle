1a9fc56ee3f6088a27c34477ab63b624f4a5a188
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:14:42 +0000

    steady needle dry ilovedicks will

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
