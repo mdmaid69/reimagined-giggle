739e52ed2bda3db85b998efe6c8f506362295b85
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:55:24 +0000

    bill keepfrom consult determined store

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  def subtract_numbers(x, y):
-        return x - y
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
