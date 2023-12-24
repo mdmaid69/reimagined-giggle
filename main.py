c4685b63ad16947403dcda1418cb7271d66b88ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:24:42 +0000

    godown wear Qq contact section

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def convert_array_to_string(array):
+        return array.tostring()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
