8715096c5be45e6568121f2468fb7f558a6186ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:13:55 +0000

    getoff turnover freedom temper success

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
