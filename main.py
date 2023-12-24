3736cfec36fee0c4f9da4ba397f087b855048a25
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:03:00 +0000

    show dear doctor vanish priority

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
