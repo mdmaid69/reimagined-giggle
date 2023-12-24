3102164ee0ebdb23a833b7aaabc44727d08c8f6e
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:03:58 +0000

    lock listenfor journey common part

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
