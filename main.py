723e41c2b5591bd7c39007522d44936d5006d914
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:20:57 +0000

    appearance pull busy culture honest

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
