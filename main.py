73058d6325d2efa44ced2239cb27d7737af09ed4
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:32:26 +0000

    base telephone suffer regular throw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def clear_array(array):
+        array *= 0
