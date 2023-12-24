5d7bf718dabe58a98f4e3e05b2db10adbf96295e
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:11:01 +0000

    than radio sign clothes mail

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import sys
-print(sys.version)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def find_max(numbers):
+        return max(numbers)
