ca088693014e072952ab4f7182c58e8c187f98e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:25:09 +0000

    fence argue explain convince rather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def convert_to_octal(n):
+        return oct(n)
