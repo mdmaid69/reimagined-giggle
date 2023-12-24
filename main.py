c627c1ff48d8fa1a35d8652137c1c375252edfa5
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:55:28 +0000

    embarrassing damp upstairs load shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
