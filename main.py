e5b648709de0159f0ac5cb5bb8c722cab0cf8023
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:23:55 +0000

    change contain crisis delight comein

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+def print_python_version():
+        print(sys.version)
