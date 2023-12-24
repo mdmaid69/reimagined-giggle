f4a60fe81ee58c806aa7995a73bef4c337a4dc4c
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:30:49 +0000

    bit shop lookaround bringback reduce

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
