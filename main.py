7a4204001a3987865baab5dd4fd64dff61d5fa79
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:43:38 +0000

    too nerve last legal area

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
