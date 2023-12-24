fa938c82bbe377f2ca774630411a0328b4628ecd
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:08:47 +0000

    slow slice normal appeal holiday

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import collections
+def create_queue():
+        return collections.deque()
