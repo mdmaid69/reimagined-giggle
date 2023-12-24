3e46a3985549ba5564b73f884185bf342f96b6e2
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:09:34 +0000

    condition shut score before bad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
