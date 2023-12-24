8d243e32da0791188490288fe816f7142584a219
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:50:51 +0000

    much holdout noise last personal

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
