daa7c2f3a1e03ed83f7eab56f3ab902e9f8ff8f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:09:46 +0000

    cost want realize exchange free

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
