cc22e9f3758d5a97ea279a803e5704fbfe1d4eb0
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:09:04 +0000

    to callout permission land cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
