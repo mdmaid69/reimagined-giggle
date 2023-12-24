68aa090d2f70d8cb9d685ccbfb26eca88aac236a
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:21:02 +0000

    below central meal pocket respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
