25fba2e51ad3405195887203e8e21df22311e468
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:30:30 +0000

    excuse drive payment fear correct

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
