dbf02ce0a5d0aaf8300599fde4c8d518cd2c51ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:38:07 +0000

    take thinkthrough interesting quit warn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
