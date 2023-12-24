11c31e6253953c0d4ea0d2cd8bf9eff5cee6a032
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:06:55 +0000

    claim disturb except joke respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
