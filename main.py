183cfb9e650babc72aa3901f48da1b77cf461f82
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:12:10 +0000

    manner description run punish rest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def add_numbers(x, y):
+        return x + y
+import math
+def calculate_error_function(x):
+        return math.erf(x)
