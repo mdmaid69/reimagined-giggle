d2776ade084cf588882f95d707fbdfba941a0411
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:59:22 +0000

    plant broken replace fire sit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import getpass
+def get_username():
+        return getpass.getuser()
