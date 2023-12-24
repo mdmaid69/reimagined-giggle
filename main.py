d3e8696b00995883ca1e006fc0f641832457b679
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:02:09 +0000

    message drawing comeup bin everyone

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
