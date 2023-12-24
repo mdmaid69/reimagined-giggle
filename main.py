a23ed8575d7385d6bc48af52cedf838980030b71
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:55:10 +0000

    argument main suggestion giveaway funny

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-for i in range(10): print(i)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
