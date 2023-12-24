13350a50b17580ecced0e5f1c9a20501559a9f06
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:11:51 +0000

    miss born shine wonder black

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
