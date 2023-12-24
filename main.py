d1b9c4a0bb9e0f5152659a6a11f4c17ba093816e
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:48:41 +0000

    deaf beside anything yours one

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
