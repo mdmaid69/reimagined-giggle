0abe2c0b70284f63f7c5d7a1e5452ec21a75f5d3
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:26:19 +0000

    instruction season shopping central field

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import sys
-def print_python_version():
-        return sys.version
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def sort_list(lst):
+        return sorted(lst)
