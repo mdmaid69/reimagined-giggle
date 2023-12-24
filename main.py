755d72788d2aaca1699d78df87be9269e993fb92
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:02:42 +0000

    anything disappointed discussion spring we

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
 import array
-def convert_string_to_array(string, typecode):
+def get_array_from_file(filename, typecode):
         a = array.array(typecode)
-        a.fromstring(string)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
         return a
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
