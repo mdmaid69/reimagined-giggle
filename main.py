437a163d08215634ad43673f47051bd52f62dcaa
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:10:41 +0000

    praise goback(to) yourself crisis but

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
