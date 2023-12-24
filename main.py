ce980a8339300407016f4b387131db36353c45d6
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:33:27 +0000

    turnon butter forgive lose row

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def remove_duplicates(lst):
-        return list(set(lst))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_index(array, item):
+        return array.index(item)
