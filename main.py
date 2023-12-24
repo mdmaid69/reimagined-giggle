3be956cdfde76b73bf1aef0f08d492671c6d71d5
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:50:19 +0000

    question matter scale 568energy work

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_speed(distance, time):
+        return distance / time
