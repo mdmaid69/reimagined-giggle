f384cb7997b8a67e93915c6a3e9d02be9d317c49
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:39:16 +0000

    light course equal rent scream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import array
+def get_array_as_set(array):
+        return set(array)
