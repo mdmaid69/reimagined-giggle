2ac5674b3495d91c6e5b2d219e0a84fc64bc59ea
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:55:28 +0000

    morning company duty collection language

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
