c9e4360c900c8657bd95151a6ec3faa75628e02e
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:55:58 +0000

    talkoutof frightened off dangerous ours

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-for i in range(5):
-        print(i)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
