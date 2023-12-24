3381deb4da24c1d440c9a111078d31054b051e91
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:13:23 +0000

    harm contact work hot quiet

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
