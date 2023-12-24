a9aa3f2df03a4854254de8b5c8adc0939403cc18
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:24:46 +0000

    attention count famous practise period

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def get_array_typecode(array):
+        return array.typecode
