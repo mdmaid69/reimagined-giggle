12c3bd44c8219de655cb6e5e9b8b46a19d48e68b
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:51:39 +0000

    less method warn hope pattern

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
