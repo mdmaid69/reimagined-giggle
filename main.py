a1e39029fffb9f2e31075a5b17dbd65d5d1ff62b
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:06:10 +0000

    talk repeat this accidentally upwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
