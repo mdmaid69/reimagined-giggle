b2cbf37e3f4869cf35f5a6c817a42e20de9f2bef
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:59:48 +0000

    goon but train beside ever

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
