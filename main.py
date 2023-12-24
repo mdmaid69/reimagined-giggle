8eea585ccaa38d847f075a53f9e577e0d3050e29
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:58:51 +0000

    desperate however successful sleep improvement

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def convert_to_octal(n):
+        return oct(n)
