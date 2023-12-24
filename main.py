934f99666d1c89d3aaed6aa3601d0ff6dd953c77
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:08:17 +0000

    available least lonely breath indeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
