e56db5cb3433850b361c885644ed34f73734d0c5
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:16:42 +0000

    ill drink only squeeze upset

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import sys
-def exit_program():
-        sys.exit()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
