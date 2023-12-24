97f68c46595ed46d59044fbfdb68aa2b017ce3f3
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:30:55 +0000

    fire shoot water thank plant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
