da528d2c4ef54c9a09aaf85159f170c8c8fa8e2e
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:53:47 +0000

    major drop them in produce

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import platform
-def get_python_version():
-        return platform.python_version()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def multiply_numbers(x, y):
+        return x * y
