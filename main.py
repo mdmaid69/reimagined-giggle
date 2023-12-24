8ae365d97bce36d2d94009c89f4090531f321d6d
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:58:27 +0000

    maybe provide personal divide take

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_array_to_list(array):
+        return array.tolist()
