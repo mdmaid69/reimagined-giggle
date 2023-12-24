a2267709d4cd46f0c75b1642aef5979cc193b84e
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:05:24 +0000

    much continue blame rush announce

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
