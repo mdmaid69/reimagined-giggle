0a7cdb082551811c93efbf46bd720a8262bb7893
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:06:42 +0000

    compete attitude offence middle perfect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def square_number(x):
+        return x**2
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import tensorflow as tf
-print(tf.__version__)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
