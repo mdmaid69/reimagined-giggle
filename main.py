60063dcdd7ed7159a47f3023a46c834533688a00
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:07:36 +0000

    detail peaceful advanced hard check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def reverse_array(array):
+        array.reverse()
