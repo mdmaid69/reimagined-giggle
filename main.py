2951875f42ac25e1fc6661c9b4c37762da37baf1
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:50:40 +0000

    slowly shoulder fight close jump

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def is_even(n):
+        return n % 2 == 0
+import os
+def get_current_working_directory():
+        return os.getcwd()
