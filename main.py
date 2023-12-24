30a0a27de74bedc68fdb9eae484929b8083b5126
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:20:34 +0000

    whisper actual mad degree Qq

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_bool(array):
+        return bool(array)
