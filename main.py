8b1884c41378200c653d2c849d048fad5b912b38
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:07:07 +0000

    snow morning bend deliver proof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
