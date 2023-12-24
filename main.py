1d4085201f899d58622a03c39476563980222ff4
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:06:29 +0000

    tight neither block cheek artificial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
