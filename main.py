6bc54733cf3ef626f273cc1bda7214a08a1e0e32
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:35:38 +0000

    pin listen now field content

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_as_list(array):
+        return list(array)
