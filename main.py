1c5a389319fe23365300a1409f68c685172b0473
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:10:24 +0000

    chin discover by dare represent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
