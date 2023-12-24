2773031335a4b2d6e7fec1f30ece4fcfa1ec82ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:56:15 +0000

    train meanwhile lock mouth proud

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
