4e9af0637d6ce2310f1cfb749fdb971004916c49
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:42:52 +0000

    rent reaction nothing may phone

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def multiply_numbers(x, y):
-        return x * y
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
