41b5e94987d27734ad3986ffe57799b65e5005ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:41:21 +0000

    either know better suggest talkback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
