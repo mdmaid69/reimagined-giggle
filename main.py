99cb28dd7191b80d9f046b904886a66773c3e6fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:50:24 +0000

    neat nice hard ground reason

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
