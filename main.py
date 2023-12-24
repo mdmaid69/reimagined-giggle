3bf71e3271d7f3e17dd4a6ac70d7524ad7fbc23a
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:14:59 +0000

    near calmly run familiar birthday

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
