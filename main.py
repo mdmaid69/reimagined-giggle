6b244f2c45508ecdfeea99aaa0927cb920b9203c
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:39:25 +0000

    suffer silent stand now Ee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
