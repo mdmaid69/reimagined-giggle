f6b13a53b4f4f89e4e02acc5e0f3fe3057a6cca1
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:39:16 +0000

    contact smooth pretty flat unite

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
