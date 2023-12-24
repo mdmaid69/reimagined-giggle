30087b92b6c5e15887538d263d4ec82d0bae2d90
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:20:14 +0000

    spill connection closed try fasten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
