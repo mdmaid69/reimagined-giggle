31e026c5608cfa91e66eaad8eb05c728ae9a1399
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:01:52 +0000

    public tidy open before stamp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def count_characters(sentence):
+        return len(sentence)
