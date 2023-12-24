403722296c023b608d39015e978147e421e301d8
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:15:41 +0000

    nurse hard fresh sail downstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
