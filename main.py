c72f42f462da84a3c9f8d554525626e6bdee9d73
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:00:35 +0000

    produce name abroad roll skirt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
