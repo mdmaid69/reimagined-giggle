36943ad1bb9d7953b7850f5eadefdc32f325964d
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:12:38 +0000

    shrink theirs inquire compete usually

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
