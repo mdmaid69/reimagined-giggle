94b9937d5354b248fc2039fcf89ff7adc784b9fd
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:45:28 +0000

    hope morning equal steady ticket

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_acceleration(speed, time):
-        return speed / time
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
