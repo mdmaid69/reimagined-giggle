e8c6d5d66c5c279c91f79ed6595009b60ad9f13a
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:49:01 +0000

    work tap raw alone tap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
