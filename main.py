00e3e00425f70da34e8e5b048bcace6da15441b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:39:11 +0000

    grade correct twist way science

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
