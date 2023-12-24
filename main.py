55dd79fa1fc8160c8c51100b274f75f0e12abbf5
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:49:05 +0000

    tidy become wait cool step

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
