c43645a80795697aef7590af178308969dd1a064
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:39:13 +0000

    light eager send home fasten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
