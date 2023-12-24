542a7d967c9072aaa26259433afa2948eb257de1
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:08:23 +0000

    recently turnoff point shame eye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
