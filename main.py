58f59bb1cefd312a647dce27c3aba932569ebdcc
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:05:17 +0000

    perfect pull belong collection react

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
 def calculate_energy(mass, c=3*10**8):
         return mass * c**2
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
