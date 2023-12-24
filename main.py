10cf44363ba95c23230bfa72a89d9a92e4439fc7
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:50:17 +0000

    lock national we unfair up

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
