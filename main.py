e8f0d7e247f9ec6ca1cb0d7edba390925d75be1e
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:38:23 +0000

    sting travel freedom pinch breathe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+x = 10
+y = 20
+print("Sum:", x + y)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
