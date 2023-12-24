e0ea45f96fe40f8020e62be415cf9fe6fe55790b
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:45:42 +0000

    street so know in then

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def is_palindrome(s):
+        return s == s[::-1]
