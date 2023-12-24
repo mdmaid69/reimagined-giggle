c4bd7d104136dbbad06b2ca1c49724058498671b
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:32:59 +0000

    winter schedule love real mysterious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
