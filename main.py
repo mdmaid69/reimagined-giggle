0b97f4b7ada477c9330eb1a3a893339f9cce7429
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:57:46 +0000

    main house band luck formal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def convert_to_octal(n):
+        return oct(n)
