5dba18de26f9d152f4fbfda11321f15ce67634fd
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:46:09 +0000

    the wait result unpleasant bend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import time
+print(time.time())
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
