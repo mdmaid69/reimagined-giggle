50b2583ba78776a6dad6cce16242fedae5f4ee50
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:47:08 +0000

    expression press steam buy male

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_area_rectangle(l, w):
+        return l * w
