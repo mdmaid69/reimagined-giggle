91e8d55eee7372b3322c87cc2ed455213baff306
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:06:43 +0000

    message abroad rule future reduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
