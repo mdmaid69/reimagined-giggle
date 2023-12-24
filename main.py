9a71ec7875c6432af9a65c09bd02798f44079867
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:09:25 +0000

    with alone sail us willing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
