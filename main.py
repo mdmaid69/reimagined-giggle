6efdfa624ff32918ae64db9b2d9a20c455974992
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:35:05 +0000

    trial fix famous relief Cc

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def convert_to_hex(n):
+        return hex(n)
