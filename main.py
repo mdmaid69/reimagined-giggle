3d20dc2ecddc1b095082d5612f75e2f3cae9de29
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:32:20 +0000

    grow apart move if muscle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
