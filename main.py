546c9a0c603d1eb3cbeb9e378a09a185affe3f95
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:00:49 +0000

    loan zero switch just see

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
