4b193eea18b86a0335f98dc0a9a6607082677fde
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:58:21 +0000

    declare gather contents unpleasant correction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
