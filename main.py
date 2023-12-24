b39cc171efb3f585868bba3054b08586682ea62d
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:46:48 +0000

    label home fit square string

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
