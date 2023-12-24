4a9fa56e056ea355a35bcb423fbbbe2be42bea66
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:31:34 +0000

    life warning statement search winter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
