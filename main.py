88fefd90e7a837c66c0c972abf8b5325ffd47462
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:34:12 +0000

    table telephone getback action greeting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
