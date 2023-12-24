f5106a11a33a76ffc1e9acad7172b4c89c63bf89
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:55:41 +0000

    patience ought square giveback when

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
