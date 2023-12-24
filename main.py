59a70a707d7eb827fcd5cd60b85830d94519f23a
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:16:16 +0000

    benefit movement tap conclusion listenfor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
