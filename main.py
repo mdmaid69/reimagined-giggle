e5ab05c812a5691ddece11690a38ae64afb5e912
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:53:31 +0000

    hello winter attention phone butter

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_volume(length, width, height):
+        return length * width * height
