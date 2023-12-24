546b1a7f08f6ed2531c684f719947266bdd14b19
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:55:54 +0000

    sort enthusiastic model walk square

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
