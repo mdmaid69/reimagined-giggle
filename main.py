6fe8bbae019981c664fbfffa3e79e587ed9aeb64
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:27:13 +0000

    belief clean ourselves area wave

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
