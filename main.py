f6a1851bb4a750f2ea024b96f176303f16bd63e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:08:55 +0000

    arm fly possession search equal

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
