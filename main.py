9d05e0445cb36701f51e343dbf908d7ae936f70e
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:34:10 +0000

    back Cc accept little along

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
