e38cccfa72aa014c97b7ca52e307cf5e6b1b5883
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:55:08 +0000

    object run not shout experiment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_max(numbers):
+        return max(numbers)
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
