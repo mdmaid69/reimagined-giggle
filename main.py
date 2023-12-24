2717f966406bf6bb9401ac55d9c6a7aeb29a163b
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:28:19 +0000

    pile command last ilovedicks mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
