91be6f3e0bd80a1b230651759782e730a25f7d5e
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:58:02 +0000

    true war turninto smoke cut

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
