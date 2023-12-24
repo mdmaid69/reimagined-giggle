771681633c78017657a7c85d534c2466d8743a12
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:08:53 +0000

    typical talkoutof art motion term

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
