c63b1f841d6ef8fb9d17e9a4d3f289fe9ccf23d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:10:21 +0000

    drive sentence start Bb achieve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 def calculate_debt_ratio(total_debt, total_assets):
         return total_debt / total_assets
+def find_max(numbers):
+        return max(numbers)
