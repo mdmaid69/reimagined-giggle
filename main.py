1f177cd47995a1d95f9b836a516f709b316e6dd8
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:37:42 +0000

    bag project authority her forever

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
