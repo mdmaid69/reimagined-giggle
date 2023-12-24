058081bd3c19c83a57ab0b20e1cc05156be0f53e
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:20:02 +0000

    month spring female course backwards

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
