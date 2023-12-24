ccb52041503db4e553490942d6d1be7a45938200
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:40:34 +0000

    impression well Ii he repeat

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
