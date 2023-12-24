3c893fe1a566fce99eebd3463dd3deac4ced4e89
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:22:22 +0000

    undo minimum ought promise conversation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
