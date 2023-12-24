62608a4aae260daeaadbf8ac97d3f093d894677e
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:31:47 +0000

    regret breakout start bear fat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_perpetuity(payment, rate):
+        return payment / rate
