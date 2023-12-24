86e05490f56b80d0e5cdb92f6071e413f8561cfc
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:50:32 +0000

    emergency test apologizeapologise cover slight

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
