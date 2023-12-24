8a60cfc6df06630dee756e388e9aef97d70e3026
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:33:50 +0000

    block agree set steep past

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
