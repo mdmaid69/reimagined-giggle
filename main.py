cd76828c0727bcfe04cca1f407d3ab0a07f55d78
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:24:59 +0000

    approve rough university may mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import collections
-def create_priority_queue():
-        return collections.deque()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import os
+def remove_directory(path):
+        os.rmdir(path)
