17c0b520d6e026a1f80a16111c789827a32eb83f
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:10:26 +0000

    callat unlucky old makeinto mouth

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
