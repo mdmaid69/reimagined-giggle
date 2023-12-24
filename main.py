913d0eb5c376f741bb3b8ad7bca163c8e1d90205
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:16:37 +0000

    issue advice introduction city and

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
