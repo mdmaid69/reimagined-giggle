1a2c6e57ea4c7807f384e60235ff3f1575ee58d2
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:45:30 +0000

    rare use geton pure condition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_force(mass, acceleration):
+        return mass * acceleration
