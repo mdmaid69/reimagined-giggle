3656b80044d251335fba3d1f0e7c1eee946961dc
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:10:43 +0000

    custom proof mysterious we where

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
