a9e7e108d1f4f8bcf9122867477bf90249345b08
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:44:31 +0000

    previous stand child height love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sys
+def print_python_version():
+        return sys.version
