975742db077634287c47c561f66dead36871dbbe
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:59:25 +0000

    base steal accidentally typical impressive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
