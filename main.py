3fa879d6343ae7fdda0a7be11e557bf109d1c84c
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:12:21 +0000

    opportunity crisis file art busy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def find_max(numbers):
+        return max(numbers)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
