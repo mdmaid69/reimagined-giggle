7b3c78369d6cb70e8c402fdf26cf2b4bb0bb4af7
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:11:47 +0000

    anywayanyhow lesson goout lend count

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
