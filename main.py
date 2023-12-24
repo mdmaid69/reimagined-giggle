0ada1b8db71567a9cec88d149cf842f53331e76b
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:43:37 +0000

    part answer bring involve positive

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
