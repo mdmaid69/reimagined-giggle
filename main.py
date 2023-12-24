5f0d6cdd50ffb2b81e4ccd2280cbb0042301fa5c
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:41:07 +0000

    crime cheap stage music transparent

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
