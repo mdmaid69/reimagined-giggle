691c3fc5a3cbd3b26aa34448bab875a2af89b6c8
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:07:30 +0000

    however otherwise heavy eye her

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
