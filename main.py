2f918312087791eb1ebb1da6702dd05013fe6fa2
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:50:03 +0000

    vanish fold damage stir birth

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
