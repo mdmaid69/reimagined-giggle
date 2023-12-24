689eb3d44946ae4cd5a38c508ffe7707b1061a0e
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:55:43 +0000

    chief getout silent hurry present

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
