39126cba9139dfb94fdbc02668f9dfdc533121ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:46:14 +0000

    weak street win crash doctor

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def multiply_numbers(x, y):
+        return x * y
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
