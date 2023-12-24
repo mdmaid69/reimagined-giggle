bc8465ca6593316a97cef69200065edaa95252af
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:56:32 +0000

    image workout various suit along

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
