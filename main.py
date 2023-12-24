47d6b7bd1de86c7b9a77e96212e1bc76bf3bffe7
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:56:27 +0000

    responsible confuse about difference back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_current_working_directory():
+        return os.getcwd()
