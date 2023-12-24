3013cf6035b758d175d9f21a4bcfc0120099706a
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:56:56 +0000

    still busy do callat off

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
