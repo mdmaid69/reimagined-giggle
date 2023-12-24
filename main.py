33b585d664342b4ad529ca7fbc6178793c14bd23
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:19:28 +0000

    comeback chief rent spray apply

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_pressure(force, area):
+        return force / area
