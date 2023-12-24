92855bcee0b758d7b4b1073a860630a7ae7f8ba3
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:42:44 +0000

    question needle snow understand practice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
