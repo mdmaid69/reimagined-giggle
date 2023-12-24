9f0e94301d9fbc41cdcc2b89d57651854671f6ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:52:53 +0000

    raw communicate may shoulder usual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_acceleration(speed, time):
+        return speed / time
