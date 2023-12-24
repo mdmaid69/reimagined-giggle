df748214a4e9a3f868f7e5dbaeea1f78a9c03742
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:00:54 +0000

    ashamed noise press free walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
