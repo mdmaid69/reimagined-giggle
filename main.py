e8a8ddd60d4b1e82c4625e2094bdb1bdc16719d1
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:05:34 +0000

    commit activity recognize tap hospital

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import datetime
+def get_today_date():
+        return datetime.date.today()
