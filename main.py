32bdaef5467be27ed80efd63dee06bb442324164
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:02:37 +0000

    drop constantly possession argument themselves

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_unique_words(sentence):
+        return set(sentence.split())
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
