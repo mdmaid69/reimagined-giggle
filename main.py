41de81f1c077e03563ea01a1fdd88bb209fae645
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:19:14 +0000

    past turn result shoulder familiar

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
