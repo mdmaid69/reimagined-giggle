5124c958a9188be00368c9adf6e26916af656186
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:05:27 +0000

    whichever goup path work disappear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
