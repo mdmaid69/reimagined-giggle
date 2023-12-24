9086f60776914683ef3d43dd63f85162e7c81715
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:31:27 +0000

    description continue set after steep

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  def convert_to_binary(n):
+        return bin(n)
