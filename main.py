46d912048a6da3ea2f180ca8c8e68f8e4e755455
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:21:10 +0000

    may bound Ww steam to

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-print([x**2 for x in range(10)])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
