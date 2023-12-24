606847b25877e82b6beb3b0cd4e93655a635b82f
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:52:15 +0000

    connection material where clearly fill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
