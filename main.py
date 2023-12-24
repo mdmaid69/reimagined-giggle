7998299867972c9cc5ef0b159f5c46791add2254
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:42:12 +0000

    cruel listenfor mind air represent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
