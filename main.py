21912bc9772841a0dea9ab75e9372a17aeaa9e79
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:38:09 +0000

    apologizeapologise shut card know mouth

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_as_str(array):
+        return str(array)
