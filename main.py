4e0a655a23dd961ecdb72c6e82ac82f98836b752
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:50:50 +0000

    border owe gooff trip smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
