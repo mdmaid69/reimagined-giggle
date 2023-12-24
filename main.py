d18a5cf7c064f0ff1f0c13d5bec04eedafb3b763
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:27:10 +0000

    but point suckme blind illness

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
