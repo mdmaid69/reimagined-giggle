139ccdd715a0cabadabb2dced0315bf7e89f4f75
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:52:22 +0000

    lucky spill dream division comefrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_sine(x):
+        return math.sin(x)
