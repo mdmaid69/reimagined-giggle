6ccbaa7bda9848008c2c1c1ff52cb0fd08514bf3
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:09:18 +0000

    leave organize threaten most truth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import sys
+def print_python_version():
+        print(sys.version)
