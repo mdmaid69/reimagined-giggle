05ce5bbfd50b7669b12a7b47ef972bc099e2e4e9
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:55:49 +0000

    content police care each concerning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
