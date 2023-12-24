12538f20be6f2e7901ce1303022b6a69ad9638be
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:05:04 +0000

    meet material unpleasant action question

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
