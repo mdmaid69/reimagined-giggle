08b74f7caab86a8781b57dfb9151291937a11be4
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:01:40 +0000

    basic which use jump tie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def append_to_array(array, item):
+        array.append(item)
