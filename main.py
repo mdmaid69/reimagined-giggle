2bfc547208919e39af95c3ba420baf3e6b4ce319
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:59:38 +0000

    activity natural advantage order unit

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import math
-print(math.pi)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+name = "Python"
+print("Hello,", name)
