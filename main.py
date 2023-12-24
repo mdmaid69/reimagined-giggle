590886b73adcb63b6f6d4d435894a1e487a180a4
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:58:06 +0000

    Nn second takeaway blind future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
