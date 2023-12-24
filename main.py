b66641e8379e2030c4c37f73901f11ec898db317
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:26:43 +0000

    worry whenever boil funny bang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+from collections import Counter
+print(Counter("hello world"))
