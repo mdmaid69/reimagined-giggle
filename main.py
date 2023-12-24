ca6a2201646c97f25874bffa1f1326b27ccfd020
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:59:52 +0000

    bang breakfast fill snow scene

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
