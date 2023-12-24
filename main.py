661003997a0eba1f74f357f20967972ab918ac09
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:05:06 +0000

    listen display both field dead

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
