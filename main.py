e2679293c84cb0c5ec2e34810c580c6aad5421c0
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:25:19 +0000

    minimum whatever keepaway recommend law

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
