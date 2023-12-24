a9355e34e1cf2405aa8d3b1abb6cd2953d8f078c
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:47:58 +0000

    upset 570enjoyable group probably dry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
