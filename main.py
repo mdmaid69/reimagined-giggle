e690969b50815d039b62d9d6ab21046f567ba91f
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:01:52 +0000

    who hate reliable regional height

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def reverse_array(array):
+        array.reverse()
