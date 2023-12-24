370188b983ac75ff30bdff2cacb5b350a32644eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:47:08 +0000

    switch doctor embarrassing bringback pupil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
-def remove_duplicates(lst):
-        return list(set(lst))
+def get_array_as_int(array):
+        return int(array[0])
+def add_numbers(a, b):
+        return a + b
