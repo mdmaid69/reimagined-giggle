0b6ef2879a321eb8f5c00d94948a6b7806385c99
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:47:14 +0000

    strength another anxious brief acceptable

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-print([x**2 for x in range(10)])
+import array
+def clear_array(array):
+        array *= 0
+def calculate_speed(distance, time):
+        return distance / time
