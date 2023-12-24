e1173dada4fcb34a4a997653aaf1f6044f451c40
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:54:57 +0000

    up tip fortunate waste plenty

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
