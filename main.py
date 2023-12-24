84d8b5896533e21be50669debdd237addc7f3d9b
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:27:10 +0000

    reply constantly base quit pour

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
