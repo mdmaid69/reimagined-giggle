d61710fac0fa312d738b038e2d560b437c81ae4d
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:59:44 +0000

    law weather freeze book race

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def remove_duplicates(lst):
+        return list(set(lst))
