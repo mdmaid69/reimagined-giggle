6783e83e6bb549d2bb9bcb1c608ee1d5ec2d4472
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:36:38 +0000

    Oo kick action respect emotion

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def count_elements(lst):
+        return len(lst)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
