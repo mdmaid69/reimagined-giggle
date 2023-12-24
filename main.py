3e65669a3097cd6d7b4ee32c50d9e7946a3bab91
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:22:06 +0000

    of Pp fetch effect shrink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
