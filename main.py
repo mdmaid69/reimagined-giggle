896f7762dd73672456ee2d8d2df2f02be4aa958f
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:47:12 +0000

    organize curious warn afraid answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import time
-def get_current_time():
-        return time.time()
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
