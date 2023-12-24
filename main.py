0793fd1a8e449907359eaef1cdfda7d7bb67d23e
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:11:32 +0000

    quiet protect once forever noone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
