5ca3de13b75a7241c39ed4816b16b3d187972aaf
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:14:09 +0000

    now damage big comefrom detail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
