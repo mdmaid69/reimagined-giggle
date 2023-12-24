2d0d4f8258cbac3ea2a341a3baa0b8bb640b483f
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:52:20 +0000

    insist trip whistle copy challenge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
