fe759c0be40344a095d5a0d3092980a1d39c39f1
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:14:47 +0000

    tune marry crash traffic introduction

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def subtract_numbers(x, y):
+        return x - y
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
