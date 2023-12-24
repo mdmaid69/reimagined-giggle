9a660f07fb25b98c14f67304553d5e198e590441
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:50:06 +0000

    wealth wander major sort brain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   def find_max(lst):
         return max(lst) if len(lst) != 0 else "List is empty"
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
