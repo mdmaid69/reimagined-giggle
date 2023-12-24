88dd3dbb8f9cc4ae8afd36aed74f791056edf54d
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:04:10 +0000

    later mend anxious acceptable zero

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
