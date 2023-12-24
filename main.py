72c7cea04766d5a0dbd0a67665e1a6df7d6aa558
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:46:53 +0000

    rise roof well ordinary telephone

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
