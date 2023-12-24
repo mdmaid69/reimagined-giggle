cba52722bb9f01f50251c73a715380b0598d293f
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:17:01 +0000

    duty mine record forever scrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
