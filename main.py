c9cd97f6d97f9600acc0cc5e7a3f185fd90c88a4
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:08:23 +0000

    remove burn turnout hollow another

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
