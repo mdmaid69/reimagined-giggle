92f54f36db713d4f0403d4010fa1dd5ccb9697f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:54:19 +0000

    kind load gradually achievement case

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-import array
-def reverse_array(array):
-        array.reverse()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_sine(x):
+        return math.sin(x)
