9c43e82e795713898c127131cc0bab3b42a17894
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:34:26 +0000

    ability inquire who upwards guide

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_even(n):
-        return n % 2 == 0
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
