46d646644e2ab8db44f4601ea1ce53b0de8d408f
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:20:15 +0000

    talk touch state record live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
