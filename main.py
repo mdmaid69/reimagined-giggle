a48a788ac122b38953f41c56bd63d0da7af11df5
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:37:15 +0000

    street television Qq correct behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
