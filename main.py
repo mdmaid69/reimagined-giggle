8eb19b766c73a9f45987da7d30771507d0cfec2d
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:41:12 +0000

    callup bang matter creature straight

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
