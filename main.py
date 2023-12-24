5b191c24dbcd5dc246bb0efb7d7eb1dd725e48f1
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:42:20 +0000

    crisis complicated peace unlikely moon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
