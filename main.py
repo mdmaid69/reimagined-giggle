3b46bcf1a05761d704bc5d1ab4e2a6f9a5eaf82f
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:22:22 +0000

    wise drop open turn still

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
