4b837fc9e6f36630b1758409dbbd44f5392417c6
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:50:11 +0000

    task lookaround simple by watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
