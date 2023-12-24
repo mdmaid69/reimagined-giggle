532f327c7655d6f45c4da93bac61cd814e08a975
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:48:22 +0000

    other earth obtain insist gift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
