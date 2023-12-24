33384f2be25c2b078c75edd5f00fcee13d0a16a3
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:28:19 +0000

    bag definite autumn turndown manner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import time
-def get_current_time():
-        return time.time()
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
