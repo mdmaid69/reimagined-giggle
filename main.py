f505052f8c5a694c7000f621323ff456cb62ea68
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:22:56 +0000

    date instrument whatever beyond attitude

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
