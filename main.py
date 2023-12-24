1fa7a2c62124de1dc3642155b98981661e1d56e6
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:46:58 +0000

    press figure find left unlikely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def subtract_numbers(x, y):
+        return x - y
