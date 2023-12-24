066eaea1ff67b8b15ede113d8c11b294e5b75761
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:24:57 +0000

    allow crisis available excellent skirt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import datetime
+def get_today_date():
+        return datetime.date.today()
