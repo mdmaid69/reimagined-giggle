9ed2726eb9546ac21adebe2cbe3da71b89ad6d35
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:26:28 +0000

    case name gooff load coat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import random
+print(random.randint(0, 100))
 import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def get_array_typecode(array):
+        return array.typecode
