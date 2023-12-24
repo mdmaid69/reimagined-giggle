9f8ce5b69c6fe4fa417d0cadc0299d88115d7bb7
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:59:38 +0000

    sore worse march teach introduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
