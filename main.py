16de70b11d29d4991f9b5253d4d72a03dcb32baf
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:06:01 +0000

    allow scheme manage ability harm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
