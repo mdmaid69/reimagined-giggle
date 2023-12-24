726f4fc1ba83efaaaa85ec4c7c1c9a60833be8ea
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:24:56 +0000

    actual distant reduce that its

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
