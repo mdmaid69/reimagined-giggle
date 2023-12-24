69e996df80d8b9af937eec716729b74e9ecda5e5
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:12:39 +0000

    why sure concentration summer tune

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
