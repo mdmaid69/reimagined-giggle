a5e0b91e16d88333b68c31b3ed1b9a4b8415f981
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:14:41 +0000

    unkind another advise bridge beat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
