8b569eda339b1f0fbd4b9c03f3d55853407003c7
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:02:58 +0000

    accidentally climb sale birth justice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-print(random.randint(0, 100))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
