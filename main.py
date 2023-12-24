a5882a0a40914a02ec85c132e09428ac3224b7cc
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:38:09 +0000

    search noise holiday lesson certain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
