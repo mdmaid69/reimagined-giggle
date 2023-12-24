d976e647e298ce99bbb0cd36921495d27e384f70
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:31:45 +0000

    service measure accordingto trip attempt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
