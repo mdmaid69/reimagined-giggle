64ec79826ec83760141828f20b1b4ff35edb9863
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:53:54 +0000

    minority off Jj punish luck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_current_directory():
+        return os.getcwd()
