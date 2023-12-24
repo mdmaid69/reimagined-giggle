fb39bc536ca9d1946f47ffb19cd91d0b58935e64
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:15:57 +0000

    proper advanced completely mix lose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
