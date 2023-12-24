9f6ffd18949088ed86e2da62bea2cd4571f9ff22
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:36:22 +0000

    post arm forward record 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def divide_numbers(x, y):
+        return x / y
