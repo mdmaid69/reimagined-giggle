3710b8c831c75a65d5643980dae254cd1c6a24ba
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:33:33 +0000

    care notice sea carry we

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def calculate_power(work, time):
+        return work / time
 import time
-print(time.time())
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
