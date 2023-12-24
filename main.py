a016685021264e23ebfba6bceffff925b789cf7e
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:11:05 +0000

    last mend task bread substance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
