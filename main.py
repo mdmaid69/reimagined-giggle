8b22707ca1689b265021b01525bb042f51797e24
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:45:16 +0000

    schedule sink innocent sentence mysterious

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
