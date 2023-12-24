9340bb38346aa8d5a05c50b7cd058b0585b637bd
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:34:36 +0000

    charge play bunch space up

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
