7b1d918e9777c6265a3294369f2dadbd194337e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:48:49 +0000

    but dig book might often

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def get_current_time():
-        return time.time()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import os
+print(os.getcwd())
+def calculate_power(work, time):
+        return work / time
