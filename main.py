66c65c262afc51b88a3213371116c2435e5d5257
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:21:58 +0000

    hide recover upstairs meeting tune

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
