7b9474991a32431bb5e493964204bb8861d4c8c2
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:33:07 +0000

    love final answer mainly mess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def square_number(x):
-        return x**2
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
