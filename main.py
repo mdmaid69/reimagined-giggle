e14c67ffd462dfe4fc11e169cddf4d6a00f0d3f3
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:16:03 +0000

    remove moment play drop Oo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
