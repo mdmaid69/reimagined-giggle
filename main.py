307967888682d72c9c7bc550146ca84ca6d2075a
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:43:17 +0000

    rest risk public slide quantity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
