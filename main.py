c9d19fdbce4622f30d2fa4ff10fdd764ddb9bb29
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:01:54 +0000

    burn error outside manage exhibition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
