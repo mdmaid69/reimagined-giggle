a82cfe60332d2934813010d66d1fd0ba51eabd16
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:03:55 +0000

    authority voice insist say head

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
