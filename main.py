6f4c93edeaed38fee058ff00e443f070ec562224
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:00:57 +0000

    disagree climb floor deliberate otherwise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import re
+print(re.match("h.*o", "hello world"))
+from collections import Counter
+print(Counter("hello world"))
