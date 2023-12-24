94dbc5f2d97992759b6ae50245c617c1c9ea00bd
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:29:58 +0000

    base clever today reply rise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
