03bd6436877cfb0d255114ffa77030f6fd790293
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:10:53 +0000

    find water pot prepare apparently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
