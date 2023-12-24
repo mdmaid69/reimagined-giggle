add2ee8ed5f9bc64d0bfe384c13cdf51bc9a0c05
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:30:55 +0000

    contrast second receive all seriously

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_list(array):
+        return list(array)
