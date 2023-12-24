7bb5ed91a257466beb2eb16591a7e9dab6ec174c
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:54:53 +0000

    full impatient comeover drink street

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
