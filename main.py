26df1045f3433b13ca6a205c50b32224bcfa8ce2
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:30:01 +0000

    shoulder behave score cheek space

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import collections
+def create_stack():
+        return collections.deque()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
