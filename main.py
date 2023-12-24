e670fc1663f3e384f2abdd76df4ee9a6a71a82ee
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:40:33 +0000

    print independent nigga warn turninto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
