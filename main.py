e9c7e4c27ca6bc4709aed1c39d85b258b33cbda1
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:50:42 +0000

    frightened walk cross bin priority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import sys
+print(sys.version)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
