607ba5918ab2ea8cbf61482689093c7c47cdf18e
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:05:53 +0000

    undo stay heavy even push

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_index(array, item):
+        return array.index(item)
