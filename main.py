1899c1094de22af9f74c0a7ab573ed1cfd82a248
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:23:50 +0000

    shoot rest interfere prevent punish

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
 import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
