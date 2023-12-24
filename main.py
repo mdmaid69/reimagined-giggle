631f23e955ece03a94ac23fcfbb0196d9072b7c0
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:42:17 +0000

    beat back combine lookaround attitude

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_typecode(array):
-        return array.typecode
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
