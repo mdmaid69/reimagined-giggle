23a51424f04347298d09ff857cedd6fd4ea5f1f9
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:10:34 +0000

    up night damage behaviour stare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
