11fae118610876871916b0990938e91ed3ac5761
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:09:45 +0000

    insist bringforward very make control

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
