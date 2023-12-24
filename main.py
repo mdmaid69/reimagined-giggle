5775cbf86181eeefd20931b04321bec0516c2b48
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:53:43 +0000

    nigga kind radio breakdown bin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
 import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
