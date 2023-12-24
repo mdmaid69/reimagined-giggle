fc2ac5a94582dba0a5bc441edd09a8c321ed88b5
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:22:25 +0000

    free capital talkinto Vv treat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
