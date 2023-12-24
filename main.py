ae2406fe95eee9540ab3a530539034f6999b5405
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:23:15 +0000

    slow clearly space usual everywhere

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def cube_number(x):
-        return x**3
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
