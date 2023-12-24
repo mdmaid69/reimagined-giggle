f8900f62d093205f94d4a90e17793839237619bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:10:04 +0000

    ring piece fold Dd shine

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import time
-def get_current_time():
-        return time.ctime()
-def count_characters(sentence):
-        return len(sentence)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+for i in range(10): print(i)
