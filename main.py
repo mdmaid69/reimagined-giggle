d577f745e4c53e67c43e74a1bd1d8ba22aafd2cf
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:34:42 +0000

    instead life pretty male ring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import random
+def roll_die():
+        return random.randint(1, 6)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
