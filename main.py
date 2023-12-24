599e37f3485ffbe33f8d2625cf293bf6b3946f21
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:13:34 +0000

    live frighten importance possibly splash

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def find_min(lst):
+        return min(lst)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
