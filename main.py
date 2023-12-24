8b15ef3c785f16c3bc0fdb9fb060ea2af877eed5
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:30:47 +0000

    scene pen doctor month see

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_list(array):
+        return list(array)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
