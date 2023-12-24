1056f6fd9317943d54aa9a876479d9b2bad4cc53
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:29:03 +0000

    crazy limit while never charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
