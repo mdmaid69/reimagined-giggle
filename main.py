ce118ff782fc96f7bef6e2eac4f006bde231b301
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:43:34 +0000

    everywhere suggest mark minute independent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
