cd0d6e43b5322c365391abeba627268d61ff4189
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:06:12 +0000

    arm stand total plain Kk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
