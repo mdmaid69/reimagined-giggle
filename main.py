127b8967195200c9b126669851cbdfd3b58789e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:04:59 +0000

    brown runinto somebody comfort conduct

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-for i in range(10): print(i)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
