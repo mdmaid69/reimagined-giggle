ceb1e11d7df127cc5b033ad6dcbc359bcb20732a
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:43:15 +0000

    mass nigga old further walk

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def multiply_numbers(x, y):
+        return x * y
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
