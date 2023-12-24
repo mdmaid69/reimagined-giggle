cddd63e63f693521dfbe41aca735ead9e3cb9f1e
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:02:44 +0000

    burn man charge software through

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
