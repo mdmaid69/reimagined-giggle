e1cff4125f50ec74d95f4df84328bdeea6e71366
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:17:57 +0000

    badly runinto spoil true usual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
