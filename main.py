9073f6ad8081865f5887526979b40913ae9857a4
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:38:10 +0000

    part follow scrape Mm bringforward

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def count_characters(sentence):
-        return len(sentence)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
