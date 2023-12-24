8b252d720d666afd1523f6eff79bdf4c6686c728
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:05:15 +0000

    nurse comein spill branch replace

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
