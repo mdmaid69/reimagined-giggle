22a6ff642c7402475cd97a999486bbe9ca45507e
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:05:42 +0000

    switch cinema bit suit twist

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+name = "Python"
+print("Hello,", name)
+text = "Hello, world!"
+print("Words:", len(text.split()))
