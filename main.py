af57b98e92484e4a1cd7bd48b1dc4efeded1849c
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:43:58 +0000

    off responsible 566end communicate block

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
