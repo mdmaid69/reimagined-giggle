0f22d51646d8f74ee27debb83a3e9242d180c460
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:36:31 +0000

    notice shallow arrangement advise from

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
