711c0efeea675ef3ac1d287ac021123611c203f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:42:08 +0000

    child last team fix skin

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
