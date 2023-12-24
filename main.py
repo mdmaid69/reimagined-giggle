c55df9e7521fa6a2cc0734aedf45109e60de8d9e
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:55:00 +0000

    lift comeoff soft neither record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
