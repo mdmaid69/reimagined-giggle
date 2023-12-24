7b212ca045eb68343ce7c029bcbba34fb3958171
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:51:35 +0000

    taste copy send wish press

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def find_min(lst):
+        return min(lst)
