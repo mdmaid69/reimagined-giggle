e43df7ed3626f14879e2dd25423a6653d98958f8
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:07:07 +0000

    able clearly something rest march

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
