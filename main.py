e9b5838bcc2e740dab30d4f3dc9c8bcfa9b4526e
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:00:29 +0000

    lower considering Oo form undo

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  def add_numbers(x, y):
+        return x + y
