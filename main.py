375a2ca7e8efb58b6cec16b1d2a777251fcce44f
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:10:43 +0000

    money taste warm complicated shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_palindrome(s):
+        return s == s[::-1]
 import array
-def get_array_item(array, i):
-        return array[i]
+def iterate_over_array(array):
+        for item in array:
+        print(item)
