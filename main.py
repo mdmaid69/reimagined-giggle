b0a4f30d077c2400fd71f670000a2dc0842a8d92
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:10:57 +0000

    broad true raw treat weak

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+import collections
+def create_counter():
+        return collections.Counter()
