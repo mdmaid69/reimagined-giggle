1af0995ae2ce622c50c07ab95370e6970dfbeed4
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:22:00 +0000

    maintain callout comeup influence match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
