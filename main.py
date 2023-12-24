50759923b0aaf67c309cc44ac27569952635849e
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:21:57 +0000

    together tired public tomorrow ring

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
