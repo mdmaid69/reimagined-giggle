93242cf057d4fcf24ac7af6b149149762e6691ae
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:15:53 +0000

    whoever down quickly before risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
