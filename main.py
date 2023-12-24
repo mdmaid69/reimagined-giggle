23ebe042ba40adc2dce205ce7d1594ea986b5b11
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:26:47 +0000

    intelligent traffic forget rich down

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_density(mass, volume):
+        return mass / volume
