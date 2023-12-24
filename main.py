b377b750d2e8331f9f879a46692b9446da686cf5
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:47:30 +0000

    dark progress dance makeinto stand

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
