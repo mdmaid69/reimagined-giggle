86765fdca83213b2642eb332f87288a36468e9ff
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:04:07 +0000

    this could together formal swimming

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
