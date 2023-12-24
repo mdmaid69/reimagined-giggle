40d46c7018539734f6087af3a48b29164596fd1c
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:08:12 +0000

    slice waste distant neither platform

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
