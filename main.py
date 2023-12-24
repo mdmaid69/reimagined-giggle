92aece6cda59b5e2f3f50ff4510a1c4c1f3eae02
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:15:40 +0000

    young turnout moment declare thought

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
