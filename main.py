bb68cdc205ee147cac209206c36ddcc50ccea4b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:24:01 +0000

    ourselves effect couple takeaway steer

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
