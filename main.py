3cf35f44cd0936f69e024f1f022f2a16180ffe89
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:02:00 +0000

    breakthrough at lower hollow thick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
