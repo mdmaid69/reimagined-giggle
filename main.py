8281ef636c82835e5b02771b1b1510d05e8589d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:32:01 +0000

    correction hurt tip advice destroy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
