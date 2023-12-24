a7b687716e9bd1e2118d755c6231f5fbe2c3c71b
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:29:08 +0000

    guard rent square stretch act

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sine(x):
+        return math.sin(x)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
