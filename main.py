c2fc3dc3bc7a957381fc22e85d2f7fd0c44653c9
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:43:42 +0000

    child detail nor original difficult

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
