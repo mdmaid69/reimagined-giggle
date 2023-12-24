c9203cfee86709b9db765107ed2f3c77c6a983db
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:38:30 +0000

    after question container careless clothes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+text = "Hello, world!"
+print("Characters:", len(text))
