091dc02ac47605b8f59601e4f90044794f4eb2e4
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:10:02 +0000

    fit form giveaway task justice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_current_directory():
+        return os.getcwd()
