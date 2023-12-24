3288e24c3f0bf652cc178276c1a0f2351ef5407b
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:16:56 +0000

    partner protection mean giveback sure

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
