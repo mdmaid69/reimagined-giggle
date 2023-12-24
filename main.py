5712360917721ad2279f806c67ef1c6cda6380ba
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:08:45 +0000

    recommend certainly excuse breakout guilty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
