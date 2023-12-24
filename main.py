d1a141ab8c3f19da7f8c8a9a36287360cf5d6e74
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:37:44 +0000

    write skirt left honest interested

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def find_unique_words(sentence):
+        return set(sentence.split())
