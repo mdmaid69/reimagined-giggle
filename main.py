09c26ed3d15314b7e5661c48b6dca8f52a1ab945
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:17:15 +0000

    worse store geton intelligent hit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
