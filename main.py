12d90211e9a4637ac179cf066364f68b863d8cac
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:47:31 +0000

    anybody brush heart any form

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-print(os.getcwd())
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
