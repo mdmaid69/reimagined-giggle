d9801027e6d9d31eeaacbab7259f7e88051eae5f
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:39:49 +0000

    culture goup smooth luck empty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
