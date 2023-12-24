1eeddbff2769377172948eb094ffa2e26a63f4d1
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:05:53 +0000

    with sense favourite tired field

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
