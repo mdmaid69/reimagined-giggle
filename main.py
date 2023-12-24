38c39e875d032c58f5116731ed12eeb1d9c081fe
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:14:25 +0000

    previous mind receive 179bite 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
