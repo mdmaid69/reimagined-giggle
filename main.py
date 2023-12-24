51ccc3c01346a67c9cdf963e6cd966dd63b6d028
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:49:29 +0000

    wind callup clear wish narrow

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def find_min(lst):
+        return min(lst)
+  def sort_list(lst):
+        return sorted(lst)
