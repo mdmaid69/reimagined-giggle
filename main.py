952ac668865a494fa9097cc96653b65c0def81a4
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:21:55 +0000

    kick shop clothes colour entrance

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
