2c3c2bd58b74df8bbf641137d5a18934e524fa88
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:00:27 +0000

    trip sleep unless cheap broad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
