9d1a1c70cee2a4c63f24b750c85f77adac1a773f
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:35:01 +0000

    platform busy workupto behind language

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import collections
-def create_priority_queue():
-        return collections.deque()
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
