1256cac72a13c2dbc06061e193b53cf943f74d93
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:45:42 +0000

    tell correct recommend height runinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
