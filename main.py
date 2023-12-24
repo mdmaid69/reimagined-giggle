f5f40d1e9c9aca2cea95af54188ee728c6959063
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:14:47 +0000

    possibility pronounce live skill spin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
