71e18370a487bae52acd17c1828aedaa6955e8e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:42:07 +0000

    workout dream pause along press

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import collections
-def create_queue():
-        return collections.deque()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_density(mass, volume):
+        return mass / volume
