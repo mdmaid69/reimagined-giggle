3ebebdbe7bdf4f598dc79ae2d25fae9096c4a82c
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:08:48 +0000

    emotion post in poison motion

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
