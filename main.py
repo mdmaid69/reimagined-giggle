fbe9ca207cd4b24a0222dddebfddc4d9e03e838f
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:13:15 +0000

    Ff wide excited no on

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import time
-def get_current_time():
-        return time.ctime()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+def exit_program():
+        sys.exit()
