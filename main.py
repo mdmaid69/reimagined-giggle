900cd6265b325d964115a07a18a9344524304e39
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:23:24 +0000

    holdout wind culture plain block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
