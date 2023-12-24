31d1b60a3c86df73aeaf36cb49f8f8e6873f38ba
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:51:26 +0000

    begin quit emergency risk keepaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
