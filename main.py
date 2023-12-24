31a7a5ab926dda98c752169fdeb3d363f7ed4059
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:29:34 +0000

    sign possible rise neither left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
