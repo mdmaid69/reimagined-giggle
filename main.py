9ad3898023f58e04945226257c761971737499ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:29:51 +0000

    clue comfort shut most spend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def convert_to_octal(n):
+        return oct(n)
