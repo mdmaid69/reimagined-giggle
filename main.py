cb1d12db940ddfd20fc28eccb234add0180fe784
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:38:53 +0000

    afterwards rich correct anywayanyhow nigga

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
