b7a1d233f7843ca7660f6a744d72eef9d871b5e9
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:20:18 +0000

    listenfor respect nurse shut dick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
