9e8ede2ff2288a4c774f26cde0b41c0d80155327
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:23:20 +0000

    past goup dry pleasant feature

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
