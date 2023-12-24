70ab60fde90c4f220270f72c5b1992bbf73f1042
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:55:53 +0000

    dull ball respect murder Oo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
