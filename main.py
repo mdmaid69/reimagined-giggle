6449ad3a76d6a9229b35b0126a7e12446ea0d322
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:33:45 +0000

    total promise guide weather charge

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
