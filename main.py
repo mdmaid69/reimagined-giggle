8f2053fe07cd7895d58f4004e4e2d53ee9f47cf1
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:36:06 +0000

    comeround noone allow definite woman

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
 import array
-def reverse_array(array):
-        array.reverse()
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
