3869d35157fcab4390853cd6f6458abf9c7ad4d3
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:43:52 +0000

    result us talkback Oo how

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
