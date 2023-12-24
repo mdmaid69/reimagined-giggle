e307d353caeb422b35b007c737a9be60ed1ee80a
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:26:57 +0000

    excellent callback one build organization

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print(sum(range(10)))
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
