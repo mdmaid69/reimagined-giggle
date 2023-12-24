d2a7c902c24567b68149dac45be73b36d964bd8b
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:41:39 +0000

    their thumb old persuade emphasis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+def exit_program():
+        sys.exit()
