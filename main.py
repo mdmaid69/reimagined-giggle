bae2a97a21e61f01bf87a348ee4227e6c8b36925
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:55:52 +0000

    lean visit really safe treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
