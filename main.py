cef90635fa311c4f76b615ada7d5167faf1880e0
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:42:03 +0000

    it dig friendly run single

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
