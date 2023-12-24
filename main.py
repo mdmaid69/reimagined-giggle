71c3011d8e30212a6e7f41e8d165945d2263daf0
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:40:28 +0000

    factory lend shrink emergency surprised

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
