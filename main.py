5f2adfe306cdc5e8db0f445837559f431657bf3d
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:32:11 +0000

    bet reach another movement wipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
