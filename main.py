fb463fbee574f43e0a4984866be8ba73767a9f4c
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:48:27 +0000

    meet continue until wander valuable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
