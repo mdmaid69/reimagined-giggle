f7fc9bf0314f87033e90362fe78978f5c618a0c4
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:13:30 +0000

    ill mood refuse tense piece

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
