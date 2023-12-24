2a80411db18bc9094ab9a8415510e4a786d0b175
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:47:46 +0000

    actual entrance sensible sign section

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def delete_file(file_name):
+        os.remove(file_name)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
