fe495f28321acdda57f800755be06d3471b891aa
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:55:06 +0000

    Hh damage fit along closed

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
