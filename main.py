2b1dfb8c9698d6e1190a1953541208d52841821e
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:46:43 +0000

    view somewhere congratulations middle gift

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
