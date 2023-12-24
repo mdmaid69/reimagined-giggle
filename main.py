9aa8a022a9d4f142ec43dfd91050f5c9e0ded5bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:51:38 +0000

    minimum scale I habit positive

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import tensorflow as tf
+print(tf.__version__)
+def calculate_acceleration(speed, time):
+        return speed / time
