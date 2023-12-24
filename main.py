40ad645aaac866b8428e6a0b156a3a5c483fd267
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:43:48 +0000

    popular unlucky now test trap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
