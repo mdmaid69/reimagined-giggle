782c819d59c449bde5cc00df7604a5d3bedbe02f
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:12:40 +0000

    569enjoy standard temper slow scared

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import sys
+print(sys.version)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
