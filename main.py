fd14afa9998695f95a4c9f5d09c188899510e189
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:02:55 +0000

    during tonight ilovedicks shallow life

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
