8268c2e56036d2a21c2c126d4c8bf21d54811aa1
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:59:37 +0000

    against in negative maintain lend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def count_elements(lst):
+        return len(lst)
