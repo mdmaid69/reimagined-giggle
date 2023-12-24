4e2a17f8fc54a33e101b79f0413aa4f25f9e7122
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:12:43 +0000

    wash trousers offer shadow increase

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
