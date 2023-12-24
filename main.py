0f1c335a998b9b2c3e415ae7bd383a65304684e2
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:56:44 +0000

    conversation matter turndown telephone vote

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  def cube_number(x):
+        return x**3
