5486a039338ebd6565965128b579afa1d5c938ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:16:28 +0000

    fix square gather tomorrow book

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def convert_to_binary(n):
+        return bin(n)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
