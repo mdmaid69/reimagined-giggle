3885c53a397dab8d39f7f54c314e0b32e386136a
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:07:23 +0000

    formal there substance some sore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-print(sys.version)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  def convert_to_binary(n):
+        return bin(n)
