b73e636cf8a3f63e3c0b0ee89072c17969dcc9db
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:22:09 +0000

    heat cheat lost extremely possibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_floor(x):
+        return math.floor(x)
