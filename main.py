51ef462c7e282203aa1d68f1cc5e983dfc91d70c
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:50:34 +0000

    expect interest not appear on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import sys
+  def get_python_version():
+        return sys.version
+import array
+def get_array_as_list(array):
+        return list(array)
