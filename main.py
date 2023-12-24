8594395971f175b1d80bd7f32036c4c988e309d8
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:53:30 +0000

    pity control unfair process normal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
