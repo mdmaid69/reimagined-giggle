ed3648490985aa4c30d3e7e10ee40369c2f8173e
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:14:04 +0000

    comeover desperate fight steam their

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_repr(array):
+        return repr(array)
