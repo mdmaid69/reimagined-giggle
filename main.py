0bf9bd7ad5f25f0a8a7534646de0ecd7ec0dc59e
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:23:23 +0000

    care treat excuse along beat

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def check_if_array_contains_item(array, item):
+        return item in array
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
