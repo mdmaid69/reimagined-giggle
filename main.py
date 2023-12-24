ac8f513b149606c637cc47f57e5f42c04ae8654e
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:19:34 +0000

    ticket authority whom ocean suffer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
