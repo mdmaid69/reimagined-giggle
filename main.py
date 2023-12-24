13fac07d9571c7704657aa7cd14bd5919457afa9
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:57:51 +0000

    recommend bar whom mix holdup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-x = 10
-y = 20
-print("Sum:", x + y)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_array_as_list(array):
+        return list(array)
