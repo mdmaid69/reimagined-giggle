c5bf11052819e6a2baae933baf7fe153b7ee669c
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:24:23 +0000

    favourite occasional repair neat hello

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import array
+def get_array_typecode(array):
+        return array.typecode
