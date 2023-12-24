a45d873282c2de859fac2e612287684783d80631
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:46:16 +0000

    circumstance instant eye system praise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
