ae049ae2868bdec7355b7749e2d01dd87127b83f
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:46:00 +0000

    share wheel cut clothes snow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
