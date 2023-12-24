776869af60e79b9045e3e878c76401c13c92a9ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:25:33 +0000

    raw extremely confident properly luck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
