3964e2a14b6e9e60c2b9d708adcaa4092007e1bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:58:43 +0000

    bear problem continue ago copy

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def square_number(x):
+        return x**2
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
