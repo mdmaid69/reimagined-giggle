8d2dcaf80c3d57d91d3378322f847e4ce1debb50
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:43:15 +0000

    impression contrast issue remember mean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
