9483daddf1a9f157191eba14381dba4beb8bcc95
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:09:59 +0000

    use punish disturbing post plenty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
