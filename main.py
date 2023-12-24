4d4894cbe8b586f0f8e13ae1bfc946d4a3cc25bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:09:50 +0000

    most town weather drop pain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_min(numbers):
-        return min(numbers)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
