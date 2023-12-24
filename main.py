67a5903711656f5373527f33549dbba64b8abb66
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:23:58 +0000

    autumn keepup pump correction common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_index(array, item):
+        return array.index(item)
