883d59a9023867d3b03abcef7ed7dba49631f692
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:14:19 +0000

    skirt out wash force warning

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
