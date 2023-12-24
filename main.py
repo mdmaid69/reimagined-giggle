bbe2cedddc2cca9a42137c54babb3bcc902ab3e6
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:48:29 +0000

    goon admire official keepout level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
