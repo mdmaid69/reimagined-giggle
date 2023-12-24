73a3db0cc729c20ff78f5a5ae08b74c87ac46033
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:31:47 +0000

    whistle satisfied position repeat look

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
