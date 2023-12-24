2cd6a251e60182e82f1d6e488b2626cd3eb5b15f
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:31:28 +0000

    company range exact getoff error

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
