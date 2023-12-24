9191e4606f9958dc0bf3c7ff99d3012b28d22fe7
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:03:05 +0000

    offence mix accidentally pleased wipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def calculate_area_circle(r):
+        return 3.14 * r**2
