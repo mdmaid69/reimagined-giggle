91fd4efb1509a212c3ab9318c7d94fe5075b7eb3
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:13:53 +0000

    me passage for warm scared

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
