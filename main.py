f926eb69fe3647cd507500d44aa6215b759779c9
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:13:44 +0000

    apart town forward object suffer

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
