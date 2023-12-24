cabaedfaed267a81915b3f8c3bc6e21a1a20b9a8
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:04:53 +0000

    dress pick every hour company

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
