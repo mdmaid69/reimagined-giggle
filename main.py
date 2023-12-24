2d3bb225154cccad668f2b4beb2652e77f615e7f
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:28:16 +0000

    curious game private yesterday dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
