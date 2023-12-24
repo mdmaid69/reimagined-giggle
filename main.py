0b63a926c581e4efe3810b46a255e95b57ba6525
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:40:09 +0000

    hollow lie move bother this

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def get_array_as_str(array):
-        return str(array)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def is_even(n):
+        return n % 2 == 0
