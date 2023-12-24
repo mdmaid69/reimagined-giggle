f22f3484a3b5d459853191a3d6d4d03d25fcc97e
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:34:27 +0000

    represent experience bring across dry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import os
+def change_working_directory(path):
+        os.chdir(path)
+print("Hello, world!")
