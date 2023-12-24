442bf78a660f9070cca95d55baa193a6790e081e
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:25:26 +0000

    impressed getback insist measure lot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
