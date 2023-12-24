b6be80fc09be28436a5e3e20b87ca61cc20b4011
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:16:51 +0000

    comeup mouth whenever mystery beat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
