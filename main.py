bf116655e612ca85daa5f2a3be79af9b054ca0bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:56:48 +0000

    recommend many completely callup impatient

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
