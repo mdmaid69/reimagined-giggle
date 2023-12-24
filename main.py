54b971b56abc8f0288710c5864065a6a3421e07d
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:38:54 +0000

    judge shape reliable matter education

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
