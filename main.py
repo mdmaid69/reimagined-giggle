c35f83a047b49a98003384e11cbf013ec784b0d2
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:38:02 +0000

    process disgusting carry however burn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  def sort_list(lst):
-        return sorted(lst)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
