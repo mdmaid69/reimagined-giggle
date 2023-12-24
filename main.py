6d8fd23640aca9a8549fc1c250f9a5f37bf19405
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:52:36 +0000

    finger determined matter fly steady

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import platform
-def get_python_version():
-        return platform.python_version()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
