a1849f8ae990f571954ad95176eb8368cae5228d
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:10:28 +0000

    traffic relation comeout purpose gradually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
