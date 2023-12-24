ffd80bebbdafb1f930d00852cf707117f7f9ad4f
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:02:19 +0000

    scale on raise this line

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def is_even(n):
-        return n % 2 == 0
-def remove_duplicates(lst):
-        return list(set(lst))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
