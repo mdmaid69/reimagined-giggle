4c5f8b8f405a329756ff698201fb25b26fb34ab4
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:32:55 +0000

    run bye year project you

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
