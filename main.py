14da7bec2c92aba886ce3a31bd9a4aea3f12b16d
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:55:54 +0000

    care business taste getout system

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
