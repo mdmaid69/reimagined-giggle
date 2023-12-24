7d8817241518257d6705ec9491d720ff77648117
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:54:12 +0000

    brush butter produce everywhere skill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_density(mass, volume):
-        return mass / volume
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
