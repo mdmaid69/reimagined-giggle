6d7e919b18227f19c937695e00e7fe33467ff7c6
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:25:44 +0000

    far switch completely lead rare

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def reverse_string(s):
+        return s[::-1]
