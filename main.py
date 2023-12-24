5732bff990f3c6c210ed3e6c1c6d13ba4a4252e5
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:17:25 +0000

    black cause need worse fight

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
