50751cb47f8e0371e040b22105d357c7dec205a8
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:34:14 +0000

    suggestion waste narrow commit capital

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
