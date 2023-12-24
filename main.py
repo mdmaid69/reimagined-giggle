7354d70c91311e2f0dd20f5a8fcfb27f89d627d1
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:16:44 +0000

    convincing lie matter belief friendly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
