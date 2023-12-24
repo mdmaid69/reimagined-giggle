350150fb5e139f448679ef0a2175c3232325ebc6
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:06:30 +0000

    traffic chat straight opposite ignore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
