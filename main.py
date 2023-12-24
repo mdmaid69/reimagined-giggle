51d13cf8055dadbdc2d9328dfd4b8e79e32ca561
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:25:44 +0000

    size fold reduction stare like

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
