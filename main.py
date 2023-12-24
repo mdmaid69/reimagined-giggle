5bd24cbdf8140dbc483dd95a78cec629c38ad257
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:15:51 +0000

    should plain Itself official ball

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def clear_array(array):
+        array *= 0
+  def cube_number(x):
+        return x**3
