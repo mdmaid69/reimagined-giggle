b85529ae7f5b09d2d0e58e6dd0dc1e0ffca9fd53
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:32:57 +0000

    demonstrate discussion emphasize pay modern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+import sys
+def print_python_version():
+        print(sys.version)
