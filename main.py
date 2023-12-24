291ad7454fe36d4f9b062d273077096825ffba3e
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:10:50 +0000

    hold after press relax tell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def find_union(list1, list2):
+        return set(list1) | set(list2)
