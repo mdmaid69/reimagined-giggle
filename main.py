7ef79decdd1daa3174ebe5b5b2ca5832ce4c1ade
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:56:15 +0000

    cross development feeling kind gather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
