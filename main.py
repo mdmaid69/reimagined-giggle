82d94ddc794d5f619fd4d086a47c05952a33a7bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:32:55 +0000

    considering promise plant thorough name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_stack():
+        return collections.deque()
