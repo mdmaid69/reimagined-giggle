af869f791e554fa549c57fd9e7794a7edee65e86
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:39:09 +0000

    sick Ww load total slightly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_acceleration(speed, time):
+        return speed / time
