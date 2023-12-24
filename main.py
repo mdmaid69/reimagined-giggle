702ac20d1133bda097cd57d7efe9a61a6e8c76e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:50:56 +0000

    talkoutof spin will address bringforward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def count_words(sentence):
+        return len(sentence.split())
