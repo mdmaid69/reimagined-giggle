1e0a85cacd92f1bb9b888de708257fa721fcd59f
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:17:29 +0000

    569enjoy stir matter show deep

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_acceleration(speed, time):
+        return speed / time
