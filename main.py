23aa56c711b8f2c62f466530b7cd7df99eb91fc5
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:37:16 +0000

    promise keep fight actually wrong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
