eabeb9a44bf7ac27da0c2b46764fd1dc77f7ee4e
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:05:00 +0000

    let measure picture right paper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
