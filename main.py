bd5051ed0e046792b233ff9887a4aa24478c045f
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:28:59 +0000

    even although demand me through

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import time
-  def wait_for_seconds(seconds):
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import time
+def wait_for_seconds(seconds):
         time.sleep(seconds)
