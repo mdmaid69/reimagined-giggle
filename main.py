1fe687174e986c1df726aa3d8b0ae86c75e17f18
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:29:22 +0000

    disturbing holdon connect march bright

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
