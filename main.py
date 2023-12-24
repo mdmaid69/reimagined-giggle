55837cffb26c9f1670adf6d74879e61bda815df5
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:29:06 +0000

    already worried free gradually difficulty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
+import platform
+def get_python_version():
+        return platform.python_version()
