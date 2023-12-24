ff39f79d664c068cd7139b3228d77bf63bdfe31d
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:34:08 +0000

    platform club affect ill store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
