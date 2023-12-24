53eca962c04e84374a263399788ff289a94a2892
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:00:22 +0000

    peaceful comeout safety live attention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
