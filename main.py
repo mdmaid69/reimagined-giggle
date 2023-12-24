8f593af992f38bc47c843555ca5934d257173454
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:03:34 +0000

    callback suppose rough nigga born

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
