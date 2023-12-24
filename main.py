6409e065664feb8a8e76056055f4af62c0ba1112
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:56:05 +0000

    suck recently closely broad patient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
