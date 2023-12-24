5b5bdf4403da2cd4460230b909120bc6306ba231
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:33:38 +0000

    least blue clean congratulate change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
