a6fa7e218d98d5f262e959f2a7dd394997c89f0c
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:43:34 +0000

    part will guilty fall standard

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
