b4ce3ef91accb9fe588afe7b02b5db2d3dc22304
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:52:36 +0000

    drive who apply reduction cover

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-for i in range(10): print(i)
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
