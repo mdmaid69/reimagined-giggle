dcb3e9acfab57830dd10b61103e692f3c1318bf0
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:54:22 +0000

    ought declaration stamp bush best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
