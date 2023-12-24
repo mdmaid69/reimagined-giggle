f4f4a6c3cad286f4dba306f00b4955a6edd33a74
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:42:06 +0000

    detail steady use pride delicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
