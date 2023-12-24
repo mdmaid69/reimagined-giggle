aa8e802df26ebc4068155238379f2fbc8a5b78fc
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:22:38 +0000

    sight board outside pleased book

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
