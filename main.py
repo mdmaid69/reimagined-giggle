38c77e340763dc648610da5553c213fb8c994a75
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:15:51 +0000

    burst wear shelter fear forgive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def sort_list(lst):
+        return sorted(lst)
