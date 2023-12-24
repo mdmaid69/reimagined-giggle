18bfddecca944c080a81eacbcb480d3e52ba4ebd
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:27:15 +0000

    charge generally room agreement offence

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def convert_array_to_unicode(array):
+        return array.tounicode()
