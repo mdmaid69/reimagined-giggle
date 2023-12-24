a16c669a6c44876ebf917db53cc34bd9ae96ec9a
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:52:59 +0000

    thumb stranger sure tool wear

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import random
-print(random.randint(0, 100))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
