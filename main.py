b699c82c1f23b5e0e6a45a6ec3a4992bf75e3221
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:18:20 +0000

    plant by score relief child

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
