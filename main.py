305aa7eef4af6bae62944361bdc60a8da1ef4a14
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:08:08 +0000

    gap early children language music

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
