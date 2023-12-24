94128d987c6fa087a4c0655442e6dd8bb63da5a6
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:24:45 +0000

    greeting luck plate fall such

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def is_odd(n):
-        return n % 2 != 0
+def remove_duplicates(lst):
+        return list(set(lst))
+  def calculate_area_rectangle(l, w):
+        return l * w
