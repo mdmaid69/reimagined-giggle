6a08e318dea7417e1133bb13c6ac3dc0ef3868d2
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:20:39 +0000

    loan especially disgusting neither time

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
