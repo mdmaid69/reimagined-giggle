c3175c294c74ac9fa4348a1ef7595b4f42e2d3e0
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:08:07 +0000

    hand limit match past petrol

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
