1194fdd02dbe4096fc765a4d3745a7d56a466db9
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:29:59 +0000

    flat stretch organize succeed hers

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def cube_number(x):
+        return x**3
