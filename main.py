c531fbfc1387c3bff328931428413e3541c52be7
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:34:53 +0000

    tense sort Pp circumstance scared

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import array
+def get_array_as_str(array):
+        return str(array)
