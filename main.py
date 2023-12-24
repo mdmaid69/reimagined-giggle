214ff427df0572ad3e4f8666b365a87218c254eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:55:46 +0000

    why judge recently level mixture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
