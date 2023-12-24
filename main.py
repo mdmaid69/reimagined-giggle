714a543ba01b477ed96caa054369a0f5d49c8b36
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:48:30 +0000

    mark period hand solve hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
