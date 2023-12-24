f7f9a72477c9f9229d5234c9765fc7e5dc5b1681
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:37:03 +0000

    once back worst around must

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
