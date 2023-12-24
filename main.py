8153963772a66cd7671787c48814504c60f9c2e8
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:30:01 +0000

    refuse tour refer imagine Rr

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-print([x**2 for x in range(10)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
