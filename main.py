0ab29f75e97acb563dc85f8de23a8b3b454f0c0c
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:30:14 +0000

    tall visit backwards cut pour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
