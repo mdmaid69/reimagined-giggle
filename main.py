677c14bd17fdc059cae7d234d3ad9a4fb3a1c9f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:15:52 +0000

    bread influence allow mystery building

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
