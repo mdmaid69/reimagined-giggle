05d571ed44452d129a53a616a68ce4e0abe9c842
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:41:44 +0000

    runover act Pp win shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
