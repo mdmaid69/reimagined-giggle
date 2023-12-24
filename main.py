c49703c7c92507f6713b3ba39f6cb48faa0938c4
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:23:36 +0000

    hand fat concerning accept willing

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
