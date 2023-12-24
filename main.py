e6dd6522dafcce6de17ed2124cbff94b849569b8
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:25:37 +0000

    embarrassed tour open admit damage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import time
+print(time.time())
