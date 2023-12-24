1c346c0c1317b796a211c277f849aa3f4551379e
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:59:46 +0000

    persuade open movement protest border

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+for i in range(5):
+        print(i)
