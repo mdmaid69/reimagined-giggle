9ff5215abf8ccfe11da7a57534f835c0fe861ce8
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:56:47 +0000

    consist comeover object occasional union

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def square_number(x):
+        return x**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
