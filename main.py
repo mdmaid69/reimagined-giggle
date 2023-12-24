4f3922b253c19766d5295a79453daff3c6cdbd1c
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:34:21 +0000

    state defeat average will bin

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
