900346c6ecd8266d190fda7c432d496c980cb5f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:46:59 +0000

    sign guard along 571enough pattern

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
