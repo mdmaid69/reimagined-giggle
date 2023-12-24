d2d4c713dc78be2b1f966f6ef566a0888fa69a6b
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:58:00 +0000

    commit mainly bin free here

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
