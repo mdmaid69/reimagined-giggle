d7506eb64e0bcad5d6491f69fabd6517cbd9f9ed
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:37:11 +0000

    earn average everyone good neither

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def is_odd(n):
+def is_odd(n):
         return n % 2 != 0
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def count_characters(sentence):
+        return len(sentence)
