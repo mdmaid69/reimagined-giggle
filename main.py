c758ad2a4f3ea6b8c7056cb33d43ea66e8cb7104
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:25:40 +0000

    into plenty please comment dress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def add_numbers(x, y):
+        return x + y
