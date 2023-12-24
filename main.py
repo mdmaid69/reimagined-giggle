3d7a3d6da1c823aadaa2f9d4ec92d1958483b0b9
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:11:25 +0000

    OKorokay copy now more bank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
