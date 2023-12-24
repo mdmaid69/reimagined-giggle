66ff1b7fcb9cb7aad481b0eb5a4efd54fd73631a
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:43:39 +0000

    style by job Oo why

--- main.py
+++ main.py
@@ -2,6 +2,5 @@
   def write_to_excel_file(file_name, data):
         df = pd.DataFrame(data)
         df.to_excel(file_name, index=False)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
