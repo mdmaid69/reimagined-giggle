ea6fd1b6203cd4af97d29733b8f2ac5521932ce8
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:32:01 +0000

    enthusiastic Qq ashamed scrap face

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
