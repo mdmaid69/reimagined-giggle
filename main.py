32211cfa8cd6dee313189cca3d697b9630a2af23
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:57:20 +0000

    solve wall influence suit pressure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
