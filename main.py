408a3d728ba7e3ff8862e2a56d9ca0fbbfbce190
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:20:00 +0000

    major know minimum upstairs tip

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
