e9e457aacb90a5b87eb2f21576b0edf30b209af0
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:46:24 +0000

    terrible exploration them mess patience

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_average(lst):
+        return sum(lst) / len(lst)
