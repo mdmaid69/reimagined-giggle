8bbe2e97450d502fbfb5fc75a8599e7dd694a1d6
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:52:05 +0000

    that disturb union depend bound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
