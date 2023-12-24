153abf63e51807245b1aea277b3df929d17fcbb0
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:01:42 +0000

    generous burst taste either godown

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+from collections import Counter
+print(Counter("hello world"))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
