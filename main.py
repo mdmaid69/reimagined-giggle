bf77e3a072e0a67769e054c89b954316479b7abf
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:21:17 +0000

    pour argument prove match season

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-def subtract_numbers(x, y):
-        return x - y
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_area_circle(r):
+        return 3.14 * r**2
+print("Hello, world!")
