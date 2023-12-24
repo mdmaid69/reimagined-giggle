f9880b3159ca34e61d2498709d1741b28f93635d
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:48:50 +0000

    somewhere past entry remark justice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
