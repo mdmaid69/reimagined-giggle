35a660563b51ccf8a3cb184290cb32ee92933f12
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:50:57 +0000

    cross stupid natural debt pleasant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import datetime
-print(datetime.datetime.now())
+  def get_current_directory():
+        return os.getcwd()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
