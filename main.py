6cd80f3ceae56823f2d542a1d24a5a6843bd100b
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:31:08 +0000

    press flight past remark cut

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def is_palindrome(s):
+        return s == s[::-1]
