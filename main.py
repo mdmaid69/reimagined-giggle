c86675c0b8f1271bb37f05c40d7b75358c595219
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:38:05 +0000

    breakdown divide maybe judge bush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
