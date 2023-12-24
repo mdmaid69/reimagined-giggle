c1321168c706f749521b9a6cff8c594338c4a1cc
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:40:34 +0000

    jump zero whisper police aware

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
