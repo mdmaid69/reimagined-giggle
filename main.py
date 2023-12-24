5eb2cd5c35c641eb99ea89dc1c8f2d55b9d9e2e5
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:45:49 +0000

    bet never load at afraid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
