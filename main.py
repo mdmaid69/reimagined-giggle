c75c24d1390d9a7f1c8645b6170271bae92e8f6a
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:57:55 +0000

    pride after below throw idea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
