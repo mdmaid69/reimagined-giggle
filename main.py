83eb98320033114a2fab60ddf03ba129546f985f
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:35:01 +0000

    run touch himself travel example

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-print(random.randint(0, 100))
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def reverse_list(lst):
+        return lst[::-1]
