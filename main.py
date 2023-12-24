3c832ae5ee37709778700055e7944f97b80b4324
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:16:16 +0000

    single gooff pride bear tonight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
