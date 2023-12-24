3f5e8ee9f3d102a107340e482a2197b8653520bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:27:43 +0000

    little right line check reply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
