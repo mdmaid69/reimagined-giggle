a7a0ac0fd853aaa2bb2041cded307fe298400ae0
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:51:56 +0000

    back grade correct rub beginning

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
