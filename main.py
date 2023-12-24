c778cbfbb4eeed433351a7c3de5b25d9c2e47b53
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:09:55 +0000

    offer thinkover operation thing manage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
