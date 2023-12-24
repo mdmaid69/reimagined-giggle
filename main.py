82ae7be17c9da1905116325c6756ab8c3336658e
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:49:56 +0000

    phone value sink winter service

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-def multiply_numbers(x, y):
-        return x * y
+def subtract_numbers(x, y):
+        return x - y
+import os
+def remove_directory(path):
+        os.rmdir(path)
