c35c0976fe44761d5a97f9ba7100577c8c38c503
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:32:20 +0000

    couple drive write division extraordinary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
