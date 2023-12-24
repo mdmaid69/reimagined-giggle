6a8ee95c7b0cbb5842e7a3c56e2c330f25039046
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:22:07 +0000

    workupto swimming job convenient up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
