d5da62d69450a555f59c63e6df397f4cb31a5d23
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:03:19 +0000

    middle tone I ought lock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def find_max(lst):
+        return max(lst)
