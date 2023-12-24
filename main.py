d16abb743c7fedfd4e6cd58c7203b0434f6666c0
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:03:48 +0000

    stir geton finally blow exit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
