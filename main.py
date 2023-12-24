9f9a23b2e2e1546711ed6106d2167f9ebc7aec1c
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:13:51 +0000

    impossible clue service either mind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
