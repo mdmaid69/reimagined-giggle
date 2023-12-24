f08e431b99793f7ed9ef775dc3f9a03dc4aeb418
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:01:35 +0000

    pool shame suit you badly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def add_numbers(x, y):
+        return x + y
