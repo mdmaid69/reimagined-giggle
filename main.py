7ed36edd59137308fff095887e9b60f4ed8f3eff
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:18:06 +0000

    bend teach share protest sit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
