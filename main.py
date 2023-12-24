0b90094d9836263c4dab99be007b622122e5f300
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:37:07 +0000

    buy this kill occur little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import collections
 def create_user_list():
         return collections.UserList()
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
