50eeca73eb6ab4e7c2d4216a517cb6ac5439991a
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:59:29 +0000

    here calloff cruelty activity progress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  def add_numbers(x, y):
-        return x + y
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
