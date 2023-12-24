f13e5e8ad4cb403a599707c23648b6eed1861ee3
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:33:30 +0000

    steam thick country makeup future

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
