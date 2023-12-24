24f0409a982f4995aad86c2ba95a3e9da25b6e1c
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:54:17 +0000

    my recognize belief like accidentally

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_average(lst):
+        return sum(lst) / len(lst)
