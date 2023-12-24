95b015304d6bb4e68002b7e70d11eb9506fcb7ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:31:15 +0000

    regard confuse near Jj jump

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
   def get_absolute_path(file_name):
         return os.path.abspath(file_name)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
