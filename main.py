4b3b518a77bb4e4dea31d48e6c12b52de926d8bb
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:13:34 +0000

    concern passenger style get damage

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_acceleration(speed, time):
+        return speed / time
