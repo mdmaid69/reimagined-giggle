a9071812d973b6572bab132b9667edce16675e07
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:46:48 +0000

    worth collection tight such swim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
