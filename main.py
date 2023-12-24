0fd4835ee10b1577e232197a24884ffef1fe7e8f
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:12:45 +0000

    tend successful natural kind produce

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import datetime
+def get_current_date():
+        return datetime.date.today()
