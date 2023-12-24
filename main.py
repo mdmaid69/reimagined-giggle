a1f27f3ab58208a010450f0f3602d3d71545645f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:31:30 +0000

    match exploration appearance difficult we

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_size(file_name):
+  def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
