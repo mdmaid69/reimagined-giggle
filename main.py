6516031e0ef6eadd2d031f1bb77e553502146a81
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:25:24 +0000

    or behave whenever left favour

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
