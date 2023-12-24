2d4d7e463709430f3e0d2713dc53bc9038dc709a
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:30:57 +0000

    time belong her everywhere soft

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_int(array):
+        return int(array[0])
