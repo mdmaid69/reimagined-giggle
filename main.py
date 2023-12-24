f62699a77ce03728f6b627b652a85e106841f5c2
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:10:02 +0000

    talkback late mystery personal scheme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_exponential(x):
+        return math.exp(x)
