4102ea2a5a1dea9f2fe92fccad4fcbfefa3f8720
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:35:48 +0000

    true drive sell impatient whisper

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
