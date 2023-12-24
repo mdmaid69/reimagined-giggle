2ac3703e74cfead3b59392863b2a5f5857b1dc11
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:13:07 +0000

    pause spring defend tired friendly

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import math
+def calculate_sine(x):
+        return math.sin(x)
