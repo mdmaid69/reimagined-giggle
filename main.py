bdae008d871d239768dae334bc3a320e6065efb7
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:05:05 +0000

    score excuse education everything think

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
