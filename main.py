27ade1d8b7c0144dd7758914a5d94849cff5046a
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:03:41 +0000

    upwards middle open Ii pity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
