5ece74017902bceb84748aa43c27bc8f7e30497b
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:10:29 +0000

    goon further mood thirsty side

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_max(numbers):
-        return max(numbers)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
