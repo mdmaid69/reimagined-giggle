01bd98e9da0df17813896af5909366626d1633f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:26:13 +0000

    storm worth confuse forward lean

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
