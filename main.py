9b912a7ce22ae1f422a6b07e06e9a2894665dfd9
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:18:42 +0000

    interview plate behind lower sign

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-name = "Python"
-print("Hello,", name)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
