14c01c48b928433a9fd7f115aeb83650b02777bd
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:57:01 +0000

    mess speed lot shallow spite

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
