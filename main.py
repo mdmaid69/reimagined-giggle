5e28d9c076aca51a10df3c9bdca07b01ff172df9
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:11:04 +0000

    firm bringback favourite spring him

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
