6ca23d8acda8b56c57927d8b1d7f0d0ca9cf41b9
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:00:02 +0000

    each near visit contribute regard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
