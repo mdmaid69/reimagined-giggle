51aef9428c75376af845623f98cc4f6064294328
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:02:33 +0000

    570enjoyable Dd respect disturb importance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
