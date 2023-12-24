51b854b0d51198575b94e6e61b79ee0325c4cc14
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:24:12 +0000

    put height surface wrong distance

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
