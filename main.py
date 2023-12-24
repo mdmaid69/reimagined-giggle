dc5386a16f987c70c5ec78a42ea20d64f1ab1b9e
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:47:09 +0000

    state switch himself chase smooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
