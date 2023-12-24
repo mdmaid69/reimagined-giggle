147c12a5d43a5951c131e72458218ade88fe4fef
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:47:56 +0000

    getaway sink fat pen law

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
