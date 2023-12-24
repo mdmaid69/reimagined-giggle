0fffae1247d6c7cc1bbea4e205663fefe2df7a30
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:53:36 +0000

    whom beautiful this quickly almost

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
