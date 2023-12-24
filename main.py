f7aeb949ef7373aab19bec0019082f7685a82d5a
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:27:02 +0000

    570enjoyable sea listenfor deal slide

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
