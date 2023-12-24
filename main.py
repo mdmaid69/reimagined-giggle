56a8dc24d4a1398be0045f1f5b5b781aaeda5a37
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:44:13 +0000

    general manner bear fly grand

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_bytes_from_array(array):
+        return array.tobytes()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
