4bee7d94114d2c541c577791c6a0c4faec0126b3
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:31:11 +0000

    seem protest blame disturb matter

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
