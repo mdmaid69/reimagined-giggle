892c6299577fa1ac857e82063adbb30e8c810373
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:23:06 +0000

    note preparation callat trap whole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
