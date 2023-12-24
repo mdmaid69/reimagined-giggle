14f37f7375c55b0f39b9d3b6de0cf802173fcdbf
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:53:06 +0000

    fence ask land plain stick

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
