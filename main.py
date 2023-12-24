2e1c24b3c1b7876bf3a2b068d853da075bc860e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:03:57 +0000

    wet brush suggest nor wipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  def is_odd(n):
-        return n % 2 != 0
+def get_array_as_frozenset(array):
+        return frozenset(array)
