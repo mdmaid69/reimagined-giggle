e8246d7360b74130ee68573190aa82f5687f01a4
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:46:00 +0000

    they university smooth grand exist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
