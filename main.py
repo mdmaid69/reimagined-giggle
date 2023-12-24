9b3bc6823f85082e37070b8ec4653dd92cfa6747
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:46:54 +0000

    hold basic shadow far steal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
