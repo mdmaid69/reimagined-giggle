17b59534616342643e823dd1a03b034d9e8af147
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:56:38 +0000

    low basic least century hole

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import time
+print(time.time())
