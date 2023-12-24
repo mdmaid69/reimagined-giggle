795df7c23b5351713e69ed5ab89a979ae536804b
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:18:14 +0000

    unite goon means feeling connection

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def count_elements(lst):
-        return len(lst)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_work(force, distance):
+        return force * distance
