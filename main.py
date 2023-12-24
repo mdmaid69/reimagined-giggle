216d593c73ae777ff44fef08c964dc34331eff17
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:54:11 +0000

    politics bend keepfrom invite medium

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from collections import Counter
-print(Counter("hello world"))
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
