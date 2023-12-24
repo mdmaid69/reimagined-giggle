2305c2bbf7fe94e0de1422eaa0415f2df7e0bc7d
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:04:09 +0000

    waste damage emotion terrible real

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import json
+print(json.dumps({"name": "John", "age": 30}))
