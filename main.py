793964335a4420cb92c1426cb7fcd55ab4697716
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:29:42 +0000

    slice family leg sweep pour

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
