6b321368bd2d4fa9232c52348e0f5f0b263770aa
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:47:10 +0000

    behind feed actually food season

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_as_repr(array):
+        return repr(array)
