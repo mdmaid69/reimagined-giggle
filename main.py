28f7fb9d5218a8929075a4ff7bfec7fd674e6222
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:05:51 +0000

    search Ww once include weather

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-def find_max(numbers):
-        return max(numbers)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
