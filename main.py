65789489cbc68fc88d6be912004a58267056c4a1
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:58:16 +0000

    false dear inside communicate only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-print(os.getcwd())
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
