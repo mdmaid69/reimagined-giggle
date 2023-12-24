5c21c3912bd9c61e23cfd3186251408571c09bef
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:05:20 +0000

    sort not see cook satisfied

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
