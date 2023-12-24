82040623ee7fc595fda94d5755c951d98389a7ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:33:59 +0000

    evening careless taste their clearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
