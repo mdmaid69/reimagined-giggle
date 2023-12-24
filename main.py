4ca51c492155d3c93ef48f7f4e7beb12d31976b4
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:06:26 +0000

    fork during lift effect period

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def convert_to_hex(n):
-        return hex(n)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
