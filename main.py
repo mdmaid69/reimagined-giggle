42fd9b7ff639053b0013d3fdd2230c168aa48863
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:26:35 +0000

    recently mend deserve 572enter turnon

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
