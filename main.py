10c5ace2262ae0a7100e16c463363a21306d5a22
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:16:28 +0000

    gooff repeat Kk difficulty straight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
