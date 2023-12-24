e2fefe0049ed5dc40e43a8ab6e9305ea37eee19c
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:29:34 +0000

    comefrom amount still handle bang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
