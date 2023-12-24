89f845a7246d10b8d60112f718403a3acc63e347
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:47:16 +0000

    broken difficulty one polite wealth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import random
-def roll_die():
-        return random.randint(1, 6)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
