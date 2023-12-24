af759dd66a915ce0936fcdad2a2fd852e195334c
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:25:35 +0000

    flight fast wet will container

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def count_elements(lst):
-        return len(lst)
+import random
+print(random.randint(0, 100))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
