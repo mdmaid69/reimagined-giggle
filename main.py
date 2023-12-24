212c693b40f90dcf07c12e097596e1237e61779d
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:35:20 +0000

    interview on immediate circle delight

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def find_unique_words(sentence):
-        return set(sentence.split())
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import re
+print(re.match("h.*o", "hello world"))
