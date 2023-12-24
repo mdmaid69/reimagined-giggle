d14a880720a4a270f6f98a01d006bf7185fd8f52
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:26:55 +0000

    reasonable upstairs convenient duty male

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def convert_to_octal(n):
+        return oct(n)
