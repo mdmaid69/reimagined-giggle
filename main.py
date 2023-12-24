a50a7908fb70b31a8d5f1a7c5a7ead3cf2a97e23
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:32:37 +0000

    comment confuse only kick command

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
