54412aa6539883b7b536f697cc8d3fe6adb1f1ae
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:13:27 +0000

    successful tip patience there recommend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
