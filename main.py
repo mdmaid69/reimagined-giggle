420a503f53288ee4dff48c58f8f4a473a366f514
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:48:55 +0000

    remember raw serve impression getout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
