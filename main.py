1739a22a80678848ae338dcdaae5388e493e3b71
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:18:58 +0000

    dress their breakin quick year

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_array_item(array, i):
+        return array[i]
