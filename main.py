9e037b756566df543c2177b6823b61765c7a8b49
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:54:51 +0000

    temperature suppose occasional upwards then

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def get_array_as_int(array):
+        return int(array[0])
