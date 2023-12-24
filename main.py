fca275c81534ec376f82ae542c5d86c75cf25956
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:29:41 +0000

    clear question grow bringback learn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import math
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
