68a6ea776146540039bb0e907680e105c837a0b3
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:48:16 +0000

    ring row effect move common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def get_array_typecode(array):
+        return array.typecode
+import platform
+def get_python_version():
+        return platform.python_version()
