2643b279f5ff8f40714f4597f03f2497340b1d3c
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:52:19 +0000

    speech turnout himself politics talk

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
