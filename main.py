47af9501f69ea895ef9fb62d158145acae18fbac
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:00:54 +0000

    crowd steer usually from get

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
