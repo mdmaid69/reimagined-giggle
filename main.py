ddc234f47c95f0eb1582947e197208a4f177d4d6
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:59:07 +0000

    country fight crash big quiet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
