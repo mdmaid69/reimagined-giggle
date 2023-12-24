63cb09e6a9092c6db757096c99d81b664e1d3a2a
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:45:59 +0000

    drawing collect detail unless uponon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+for i in range(10): print(i)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
