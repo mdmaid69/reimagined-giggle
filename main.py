6afe099582b51a87b2cf516429a6fb2b0ffb0675
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:15:46 +0000

    mine progress bringforward advantage possible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
