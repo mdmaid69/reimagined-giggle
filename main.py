8184ca46b415db1daf74a4df071f2d734e65cdcd
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:59:18 +0000

    behave pollution runover match snow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
