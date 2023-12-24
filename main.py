ebb35f35b24fbbc317389ecd10ebd38e0e282dac
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:17:40 +0000

    expect test generous tap both

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
