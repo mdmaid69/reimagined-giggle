a4ec71954a0a4525bfa0412839e00d83132d60db
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:09:53 +0000

    switch method beside Yy certainly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import collections
+def create_user_dict():
+        return collections.UserDict()
