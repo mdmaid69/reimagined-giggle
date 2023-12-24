1d402072cd13063b930b561ce05384f691c52a8f
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:07:49 +0000

    male promise hold film press

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_speed(distance, time):
+        return distance / time
