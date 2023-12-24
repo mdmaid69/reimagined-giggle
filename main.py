9a55c756efea8d18a8971505e6e8ccf6568a46ee
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:29:29 +0000

    than if talkinto by hers

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_priority_queue():
-        return collections.deque()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+print([x**2 for x in range(10)])
