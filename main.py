621c40d406075ff1e5844d03a969dcf2cc76d736
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:03:18 +0000

    custom steer fence difference watch

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
