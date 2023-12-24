40e9ab8889690837a11a43dea33873650ddb2068
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:37:46 +0000

    level anybody warn godown operation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
