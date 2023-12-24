f9f9e1dd6794471c02f7c7b0b4ebd702cc27a7e2
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:39:32 +0000

    claim office warm full none

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import os
+def change_working_directory(path):
+        os.chdir(path)
