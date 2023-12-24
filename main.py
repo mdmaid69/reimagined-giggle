3db1fdfb84b7b189daa37520a7cc055974be4284
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:45:59 +0000

    future little point meal clearly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import math
+print(math.pi)
