5ab424efabadecb20a15c2453cd9fcaf289050f8
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:51:14 +0000

    workout mark slowly trouble bringforward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
