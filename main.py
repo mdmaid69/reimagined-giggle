db65bc76a415a50124371937c0420e171db3c34d
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:50:48 +0000

    aim chin quantity repair another

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
