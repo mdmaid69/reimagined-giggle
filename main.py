7bb1d30af772aee49b5ca2f70d9ad0ef280d9789
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:42:57 +0000

    bush land workon getback lay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
