e427f2974fda16435c4d3d1a2ee50778348f5149
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:33:25 +0000

    trip familiar callat invitation weigh

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
