7302df03d834ccb34a7e02d231c29c59fcc2869d
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:33:44 +0000

    small lift holdup callup yours

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def clear_array(array):
-        array *= 0
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def split_path(path):
+        return os.path.split(path)
