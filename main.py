282fa10e39b25db0b033a206b8229ce5f3886aec
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:30:47 +0000

    risk mix successful tall border

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
