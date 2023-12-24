e4a92b2c5373df97c934db3c2627d9eb145813e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:25:17 +0000

    fit brave pencil free mainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def clear_array(array):
-        array *= 0
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
