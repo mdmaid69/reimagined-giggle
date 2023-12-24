54c8826de3bc30a0c947bc18212af1caba511d9a
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:03:35 +0000

    Jj talkback congratulate Qq bright

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
