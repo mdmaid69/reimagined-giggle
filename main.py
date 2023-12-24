173ba0eb2201efd92bba2e57251470fa6e67c042
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:16:59 +0000

    up miss Mm separate or

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
