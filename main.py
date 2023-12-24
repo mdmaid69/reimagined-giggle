d47498f4342f2edded784fa4c2bf98839011a275
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:45:08 +0000

    noisy send direct responsible shut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
