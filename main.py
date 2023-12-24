da804f872c732ba27724409427bfb66692323499
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:01:39 +0000

    black star field store for

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
