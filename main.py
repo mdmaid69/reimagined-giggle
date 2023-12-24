c0114dfb2c47c51665e32799f0b164f16c879428
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:03:16 +0000

    present scream notice spin switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
