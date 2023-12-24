871acc909fda493c566d7af353f60bad73e84f88
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:22:11 +0000

    probably opportunity belong Mm then

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
