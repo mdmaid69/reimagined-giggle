70dbc504ed4a2bbd798508ed758af610717ad65e
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:25:03 +0000

    main makeinto left feature worse

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import platform
+def get_os_info():
+        return platform.uname()
