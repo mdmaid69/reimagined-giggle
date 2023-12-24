565866a6def558951bed9fece706f7f0c2866ad1
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:21:06 +0000

    approve crisis much trick smart

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
