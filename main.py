6787395049c4d78b697f9b79bdd410c423b01bce
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:31:57 +0000

    getoff face notice birthday hate

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import sys
+print(sys.version)
