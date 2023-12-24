bad23e6d82fb36aa56cb3a1139f6fb19c80268ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:12:11 +0000

    church uncomfortable onto dinner work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
