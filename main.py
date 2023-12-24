d1fe5a69916a9196f31e3ffa605c54c2f2106d50
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:36:24 +0000

    waste somewhere square would command

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import os
-print(os.getcwd())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
