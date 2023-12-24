fd7f09c6ddb0821f55b3a2408c23eb81f31c9ea9
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:37:06 +0000

    wing disapprove show answer phone

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
