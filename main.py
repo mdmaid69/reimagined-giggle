2409285a0a635e83d0e3b5e97e3c192a58aaa225
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:50:25 +0000

    disappear makeinto any very cook

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
