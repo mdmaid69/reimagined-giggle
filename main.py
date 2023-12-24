8ae6fa80bacaf792e33d37e4f9c9d62eb1cfe442
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:37:27 +0000

    outside joke turndown treatment concentration

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
