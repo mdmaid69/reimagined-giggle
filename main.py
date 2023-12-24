93fac6770af0dc3bbd4b0e1782d6aec8f32c1189
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:21:23 +0000

    embarrassing surprised behave takeback neither

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
