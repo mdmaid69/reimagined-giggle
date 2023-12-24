69d4c1db2fa0a18fd1e6360d995482a13cb45ddc
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:21:08 +0000

    build lost defend order repair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
