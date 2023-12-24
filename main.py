9b7b4dc8667426d4c8cdb459537160c063aa2da6
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:57:29 +0000

    announce getback exam keepoff maybe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
