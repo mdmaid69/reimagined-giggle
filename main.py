3f50ec3874edfe660f400566e99d3cacb7158c64
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:25:09 +0000

    house poison joke accordingto pin

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_str(array):
+        return str(array)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
