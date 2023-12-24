03f4ac0adc9cc0026fa4e57f8475db215120efac
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:35:36 +0000

    speed treatment fear performance discovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
