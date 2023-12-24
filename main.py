8d2400f03a23633a568db4c7a9d2c3fa0e301dab
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:15:26 +0000

    level spoil club weather quantity

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
