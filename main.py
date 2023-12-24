65cd1893535f6e06b3d57e9dea734aa47c0947d2
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:56:15 +0000

    narrow attitude afternoon confident appeal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
