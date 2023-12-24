8743db1e4e77c7b7284d44eb0c2bd1e2ad9b28c6
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:50:24 +0000

    door computer welcome on occur

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def is_odd(n):
+        return n % 2 != 0
+import sys
+print(sys.version)
