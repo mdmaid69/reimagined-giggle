949d3908df1eccccb86dafb8c2fb62766bfaf9a0
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:51:30 +0000

    delicate game laugh sale trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
