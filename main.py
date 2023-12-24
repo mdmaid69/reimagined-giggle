625c9c1d7e57189862d5beefd65a383efdfa8422
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:59:54 +0000

    inform lift question other fetch

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
