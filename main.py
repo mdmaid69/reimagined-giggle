d4836ff3d711f06e0ecd9ae441b3f4696c9cfddc
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:31:07 +0000

    praise press runover type write

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import os
+def remove_directory(path):
+        os.rmdir(path)
