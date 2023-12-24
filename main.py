36a88e5e366afdbb2f94774388e4268e9ed2c120
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:16:33 +0000

    protest cross fill could hold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
