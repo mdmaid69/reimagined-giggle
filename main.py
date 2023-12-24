38c02e51028ec01b43a3a7c6f1b1e577b83f7074
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:31:37 +0000

    dish sight especially sweep splash

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
