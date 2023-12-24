5572e9e76e7abce18d67bccb9c35250d4f3d9983
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:01:25 +0000

    unlikely Ii claim they injury

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
+def calculate_power(work, time):
+        return work / time
 import array
-def get_array_from_string(string, typecode):
+def convert_string_to_array(string, typecode):
         a = array.array(typecode)
         a.fromstring(string)
-        return a
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
         return a
