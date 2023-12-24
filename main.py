fbaca267f09ed4cca8a74777b0073a506bb6a2c8
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:58:15 +0000

    break how thirsty still wipe

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import os
+print(os.getcwd())
