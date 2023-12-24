900f0145fc22001ac23afb7ce0a0e57c79c7e0b4
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:37:15 +0000

    however little dream approve example

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
