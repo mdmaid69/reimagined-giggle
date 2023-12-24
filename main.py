a6b1500deabbc1a57e87565943fcdb25905122f2
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:17:18 +0000

    coffee herself answer impressive concerned

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
