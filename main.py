770d27b4f0f657e723fed5c5df9200f3fc799c92
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:49:51 +0000

    friend course ilovedicks string value

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-import sys
-print(sys.version)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
