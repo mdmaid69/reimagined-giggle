57b9dd46765ead5cfd796a2f1218a1e0f5a5f2ed
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:49:58 +0000

    side into moon health apparently

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
