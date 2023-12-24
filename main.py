de0442223e909b011a8c9b0a0ccac91ca76b9224
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:44:31 +0000

    equipment begin exact measure direction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
