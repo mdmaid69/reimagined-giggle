d2f43cdf4e27fcc96cc0f8c4a9b4d85db94b4467
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:36:22 +0000

    themselves actual form grand urgent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import os
+print(os.getcwd())
