184188cccc942b2fb47c3981bd27eb29d35dcce5
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:41:18 +0000

    part let thing used decide

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
