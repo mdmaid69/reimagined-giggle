3ec1b38c4190c61b65ababdfe5b0ae54e2c5496d
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:59:05 +0000

    face what mark until especially

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
