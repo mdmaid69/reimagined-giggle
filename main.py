a18437742880315f334650f8853008582a9c38d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:43:18 +0000

    love immediate fear quantity complete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
