f27be0247e6ec90f1ef1dded0f338cd1cc5dea91
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:19:38 +0000

    include funny area meal blind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
