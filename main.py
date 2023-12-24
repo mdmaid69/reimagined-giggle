5e4ecf0d37ed32302790d69d9ade3601ae87cb8b
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:06:58 +0000

    translate group importance sink since

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_pressure(force, area):
+        return force / area
