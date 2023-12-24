a88b496757f1a86f485cd6d29559afdc5568842a
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:50:41 +0000

    circle hello frequent satisfied practical

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
