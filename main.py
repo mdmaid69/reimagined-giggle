024b710d6cec0ac185d59f2012cab2b9b4ac17f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:09:09 +0000

    plenty rich steep wander job

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
