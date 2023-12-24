7fd717e50b30468de8aa9e01f12498e61c63bfe4
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:06:45 +0000

    properly rent sorry discussion guess

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_list(array):
+        return list(array)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
