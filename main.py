38398372856bacddfcc9a139560921e2d6eccb00
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:23:27 +0000

    course importance meet up dick

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def convert_to_binary(n):
+        return bin(n)
