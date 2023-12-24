1fe3a7eb7b9aa950cad53dee6585000878a77075
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:26:31 +0000

    health all ring concerned impressive

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
