779f22f7df2045be455fbcecb558556e07b16f8b
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:41:35 +0000

    earn deaf thinkthrough turnaway anything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
