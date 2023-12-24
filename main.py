5a61fba1e8e4fe262d0bf8959863698576bd9009
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:20:15 +0000

    comeoff method goodbye reaction workout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
