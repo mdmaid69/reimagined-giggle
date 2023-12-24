7462b640686de411a75c21cece55332f92c9fa87
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:41:03 +0000

    street yourself feed cut reply

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
