0dbafcb3b230695e50fefa45279ee5cb22df0ad6
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:40:33 +0000

    error loud point contact mainly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_work(force, distance):
+        return force * distance
