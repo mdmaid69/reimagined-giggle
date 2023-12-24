309ba7448cb0420104917e2f9e6242b13f6beceb
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:29:57 +0000

    burst block well prefer mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
