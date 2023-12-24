c7177e15847cadea113ab306d69dc6a545e703fa
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:14:13 +0000

    there rather during congratulations realize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
 import glob
 def find_files(pattern):
         return glob.glob(pattern)
-import array
-def get_array_as_set(array):
-        return set(array)
