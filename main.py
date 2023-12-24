956043f25fc704a10ac0c010db3acdd0fd61c37f
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:31:42 +0000

    before come cost exam build

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import random
+print(random.randint(0, 100))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
