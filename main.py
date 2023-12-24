f70fe3ea28c0174f4478f769c0bce73d9708f162
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:22:34 +0000

    light thinkthrough taste interest runover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
