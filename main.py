b3dd24554fdd1b1b66f323dab0ccfb1693db3540
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:11:09 +0000

    eye worth complaint also convenient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
