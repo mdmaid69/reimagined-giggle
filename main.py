81369eae980a195e97907edf882b40c88c589dc5
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:59:40 +0000

    unable important cry simple pin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Characters:", len(text))
