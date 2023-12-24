f1ed91d160a4d4cadebf29c54d3c6f4d6d45bd17
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:10:42 +0000

    let road search suddenly front

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
