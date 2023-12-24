50ee75bdb79ebce773f577731ef67e9de972799e
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:19:08 +0000

    drive loss side notice defend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def clear_array(array):
+        array *= 0
