f8691998ef0dbe6cb7b4530168a100f12f5b3ae0
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:56:11 +0000

    whenever organization handle passenger feed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
