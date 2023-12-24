08962b5878a7c79b29599a95516f56f0049a2cc0
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:43:17 +0000

    sort sweet fork spoil exam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_typecode(array):
+        return array.typecode
