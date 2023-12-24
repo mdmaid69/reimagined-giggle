30871aad45a36e048e03044d81bc18f5c00d39a8
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:16:57 +0000

    spring authority but interview already

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_typecode(array):
+        return array.typecode
