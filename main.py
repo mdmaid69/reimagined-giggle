6a51857c14afce9d05c490f29770b7d8f9a753f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:50:51 +0000

    development relative direct environment violent

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  def count_elements(lst):
+        return len(lst)
