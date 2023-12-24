3bc9d7dad3a8c3175b7f0f779528965c11406cad
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:25:54 +0000

    emergency warm plenty since ourselves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
