e8b8253fe8404e8d53e12551f96c590810608b5b
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:38:39 +0000

    explanation empty rush lift main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
+import math
+print(math.pi)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
