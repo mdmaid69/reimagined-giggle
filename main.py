faf4546858fe2158dd8fcaa2f208b782afe62bb2
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:31:45 +0000

    organize bin warn total repeat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
