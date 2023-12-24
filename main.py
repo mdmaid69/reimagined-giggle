7987c475db141d795c10860f15b4b58d85958ec8
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:56:36 +0000

    fail comeround concerning behave charge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_inode(file_name):
+  def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
