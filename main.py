1a28eacbd581bc750d1da34a12411fbb4b72872f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:49:50 +0000

    scene arrange it slightly drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
