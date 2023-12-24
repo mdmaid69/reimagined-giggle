4159e542fecd99b3be4c72e6017c234d75196c91
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:27:23 +0000

    deliver especially dance greeting team

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
