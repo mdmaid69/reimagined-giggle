77d65b5b460a07f9aa49c6c1ba431ba1588caa34
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:34:38 +0000

    right break serious thankyou pay

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  def is_even(n):
+        return n % 2 == 0
