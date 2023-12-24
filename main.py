2eedcb3a75c3515a77c16f7e48cda5a83095007e
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:37:58 +0000

    scream share straight offence discussion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
