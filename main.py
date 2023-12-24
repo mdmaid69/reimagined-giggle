7a62d50e5b2c800aff07b66b7b2b97412c8ae863
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:40:43 +0000

    hit complaint replace schedule cover

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
