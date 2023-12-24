5f91db32419731a96ce9edb3bcaddcf21d0d699e
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:02:53 +0000

    corner always suit adjective prove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-for i in range(10): print(i)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
