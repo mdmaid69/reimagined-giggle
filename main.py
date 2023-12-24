6a42edeae3d0442c34ec0bbc8dc2950eedecf05e
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:39:31 +0000

    copy ticket complete emphasize shelter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
