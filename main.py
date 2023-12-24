c8478289f0ce0ba6470c431ffacd07b25d2f6ebe
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:59:38 +0000

    nigga reply takeoff sticky towards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_current_directory():
+        return os.getcwd()
