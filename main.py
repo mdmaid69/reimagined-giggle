dcfea7c42f7d40b625bed4c4ca8bc312fce7b885
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:34:07 +0000

    distance bet mark he trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
