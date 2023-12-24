da4bd3fab59e6911dc97c6bdffcd4e415372ea17
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:09:35 +0000

    ilovedicks objection memory listen alive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import sys
+def exit_program():
+        sys.exit()
