31fd8a2a1ef41e2b150cfaea21453c886a4100ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:18:10 +0000

    sad correction company concentration through

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
