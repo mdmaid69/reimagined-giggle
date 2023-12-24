8c6fd0a7bcd2fcc9c6765d5db4916fc8747f167e
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:47:21 +0000

    play special hit mood winter

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
