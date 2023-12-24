36dc866e900bd1ffd68e86e48daa455af3fbe659
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:24:19 +0000

    cruel cross knock thinkover central

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
