de8bfb97162bcbe7882c99b3672810111324327c
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:18:03 +0000

    worth punishment hide exercise square

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import collections
+def create_user_list():
+        return collections.UserList()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
