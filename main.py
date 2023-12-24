567de97496bd626cc044a21b55551c8d3a4eb993
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:25:27 +0000

    birth finger tidy where kill

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-print(os.getcwd())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
