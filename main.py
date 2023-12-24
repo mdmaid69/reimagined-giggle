8cc949dec9ee39f445f73189d79541cc9c66f9f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:41:56 +0000

    personal female expression wake range

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
