d4fc296c420b0bddaaeb1138f3c1c4668e94afb1
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:51:21 +0000

    nice strike unfair only manage

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
