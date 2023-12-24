9a9870fa53417677fe6c47fee7dcdf62a5444469
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:46:13 +0000

    region them rank secret reaction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
