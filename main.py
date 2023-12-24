e1cf311bf81ea0cfc1cce0193d4d9953a0408685
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:20:04 +0000

    tradition mess future position healthy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
