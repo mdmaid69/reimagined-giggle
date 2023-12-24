527faccb0bb3f2ee7807ee184a7467459986103b
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:52:52 +0000

    form breakthrough myself old even

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
