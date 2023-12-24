5cb8946b5168db4c3115bf4216bbc04244c00ef9
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:38:38 +0000

    disturbing vision state or giveaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
