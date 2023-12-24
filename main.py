2c19a12dfcaf7f802e5e48be4fdecbee405187f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:04:47 +0000

    war press maybe breakout art

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def reverse_array(array):
+        array.reverse()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
