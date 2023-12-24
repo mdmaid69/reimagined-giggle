c15b4d81d3f40139196d30f4d45bd4dd3eebe5d7
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:53:27 +0000

    cover burst equal spread course

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
