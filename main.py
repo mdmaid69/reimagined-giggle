28efcf689a642f70b48fa596846ffd7480d743a6
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:59:52 +0000

    spin guess speak fight chin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
