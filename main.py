c9d900fed54799a18493765ed5d51a36acf29511
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:58:35 +0000

    previous exit book keepout discuss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
