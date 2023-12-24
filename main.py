5b26434c3e5b312046f5cbdf492461e4fed729c0
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:03:34 +0000

    convinced smile concerning breakin ahead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def get_array_as_list(array):
+        return list(array)
