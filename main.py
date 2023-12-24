73ee60610d970f3b59be56da18b1243e5f85ce38
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:54:24 +0000

    season Rr that forgive transport

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  def cube_number(x):
-        return x**3
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
