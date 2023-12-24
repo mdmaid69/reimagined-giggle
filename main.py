31ff16e497db676900ca72690d285e73dc0b53bb
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:16:26 +0000

    closely calmly contrast shrink hear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-print(sys.version)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import os
+print(os.getcwd())
