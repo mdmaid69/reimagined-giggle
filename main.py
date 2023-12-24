081a15afeb92bf04793356e913cd5414de1817be
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:50:23 +0000

    absolutely make light clear up

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
