31138684cf0ae4d860e4e122c7c49868953c5ce5
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:34:55 +0000

    factory pupil limit just Vv

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
