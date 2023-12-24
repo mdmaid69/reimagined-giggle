26bc12f51a21459b17a46a84c8c458d0e04b6d5d
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:54:23 +0000

    total sense firm record by

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
