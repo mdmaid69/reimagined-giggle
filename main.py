a3cc2889457e0752f8d69b6704817894ecaef4be
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:09:10 +0000

    clean admit arrange war card

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  def sort_list(lst):
-        return sorted(lst)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
