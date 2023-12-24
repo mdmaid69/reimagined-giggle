771d0c4f6d109c53f1fd2b8f9553ee4b5456fb0f
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:36:49 +0000

    manage loud earth so confidence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
