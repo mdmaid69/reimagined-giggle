6af085cd38f37327a56479c65a564a1c057640c6
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:17:28 +0000

    invitation rate order entry shall

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
