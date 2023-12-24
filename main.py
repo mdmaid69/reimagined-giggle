4280704fba4ecc297fd59a7b729a3304458710f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:59:30 +0000

    push recovery record reject settle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
