69f1a1f083d07f2730f48786beddd6d05a96ea03
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:19:32 +0000

    variety Kk joke ago bored

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
