872b0b7499fb417a9408472761d9ca0f2dbc0676
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:46:25 +0000

    name choose base hour open

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def count_words(sentence):
-        return len(sentence.split())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_as_bool(array):
+        return bool(array)
