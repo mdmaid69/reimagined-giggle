a60ca881a4474cc780454ddd471f05ebf24653ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:07:49 +0000

    care swing prove dry heavy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
