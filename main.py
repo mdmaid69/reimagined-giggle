7d67f8ff99b2146fc46540c57a414ee9eb219b82
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:26:14 +0000

    false them reduction picture sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  def multiply_numbers(x, y):
+        return x * y
