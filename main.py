421af1c90ad156ded91900018d77ce825fcfd2b8
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:05:35 +0000

    rent spare speech wander substance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
