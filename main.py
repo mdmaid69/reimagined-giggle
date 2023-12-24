9836bea41ce851ff35f612fffbc94a857a6d53f2
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:01:50 +0000

    fair lead book where write

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
