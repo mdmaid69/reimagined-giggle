5a6e1d90a7704add4be27519a0c0347e56b5434a
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:17:48 +0000

    when belief work nice fetch

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def count_words(sentence):
+        return len(sentence.split())
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
