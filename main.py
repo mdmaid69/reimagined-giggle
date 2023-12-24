f2f3930a080edae6798498c75599796d9832c272
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:13:10 +0000

    turnon head direct character hungry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import sys
+print(sys.version)
