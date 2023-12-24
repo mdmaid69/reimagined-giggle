a3228ac05094edf1016ca8491cbc892dac9874ea
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:45:28 +0000

    shallow bringup few square world

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def split_path(path):
+        return os.path.split(path)
