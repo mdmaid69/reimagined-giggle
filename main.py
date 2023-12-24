2f24368f3b26584dbd84c4c69fdff26bb9df13d3
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:36:02 +0000

    talk splash underneath target characteristic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def count_elements(lst):
-        return len(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
