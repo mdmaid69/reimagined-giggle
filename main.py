3a6fcb3a2785e82fb5c89c9d8ad15e317c7e27b9
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:00:57 +0000

    doctor report achievement book destroy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
