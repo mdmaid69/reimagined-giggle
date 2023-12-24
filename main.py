a9898603931c61a97be73ed6ae267403a18478fa
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:38:30 +0000

    note moon treatment wander raise

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
