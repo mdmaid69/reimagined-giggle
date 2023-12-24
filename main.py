5c8a8b26d3510894fae3acc415449fe84edc9f04
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:23:42 +0000

    healthy block whistle there worth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import time
+print(time.time())
+def sort_numbers(numbers):
+        return sorted(numbers)
