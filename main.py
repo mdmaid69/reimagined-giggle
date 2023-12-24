5d5e89d7e82ec33bd978ed4772cdbed0fe3eb855
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:04:09 +0000

    behaviour happy play area mine

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
