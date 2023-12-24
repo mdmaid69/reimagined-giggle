fd4b61354bf7b635e6f97ff3eac694489b880639
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:14:45 +0000

    past ride sign slice soil

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_bytes(array):
+        return bytes(array)
