4b61d29bb4fbd3fd344bac4e35eacf581e3102ae
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:55:48 +0000

    finally turn them gooff term

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def is_odd(n):
+        return n % 2 != 0
