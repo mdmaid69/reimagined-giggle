d037f022236e29f2a9ede7df759c7bdcdb072e80
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:33:01 +0000

    form divide Ll holdup correction

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
