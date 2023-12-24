719330eebd5d612ca5d9ea732fcc0f077af6318e
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:07:56 +0000

    disagree note nervous account morning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def reverse_array(array):
+        array.reverse()
