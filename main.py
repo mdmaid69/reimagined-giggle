c5ad64506fae379de707dee3f6953a167b3f6c71
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:46:07 +0000

    shine dress makeup habit listen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
