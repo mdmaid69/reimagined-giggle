1ac65663886b4faae48f8def0ffdd6a9e2725c31
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:42:54 +0000

    nature fire special dance middle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
