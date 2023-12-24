6b6d86c2ebce8bca1749b29808db01027915597f
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:06:11 +0000

    lonely float whom level explain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
