285d0aeee5f69eb0d0b3b1179653052f4cbd272e
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:58:00 +0000

    minor beneath uniform idea recovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def square_number(x):
+        return x**2
