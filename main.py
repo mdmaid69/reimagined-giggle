d5e5b3bd030938af5229a774f1ac9f0c541600ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:47:19 +0000

    that study glass performance positive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
