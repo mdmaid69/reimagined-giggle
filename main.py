1f9509b67f0bee0b0d993c131cd8c82deecc990f
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:12:32 +0000

    praise shoot apply Tt remove

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
