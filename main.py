d9c3e1f08d39994dc5816e440cafff484a62890a
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:18:37 +0000

    suggestion nature substance food cook

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def square_number(x):
+        return x**2
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
