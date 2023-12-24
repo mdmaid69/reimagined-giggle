f9193b48fe8cdba1a24651d87fe5d3bae0175e39
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:48:22 +0000

    makeup century sore everyone everyone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import numpy as np
+print(np.array([1, 2, 3]))
+  def square_number(x):
+        return x**2
