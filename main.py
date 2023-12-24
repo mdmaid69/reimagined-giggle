f86c6b6db12aa9a6f6ae6954436b5b09b6310b2f
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:14:13 +0000

    finish meanwhile difference age divide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
