8a64de221f5c72758c1d9e564f20b536b82a8741
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:26:42 +0000

    down off ring question onto

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
