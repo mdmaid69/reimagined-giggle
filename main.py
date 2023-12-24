cbe9b43da05f63f4e827fe60c8c10eda68d0feb3
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:19:48 +0000

    admire politics plan ride shelter

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
