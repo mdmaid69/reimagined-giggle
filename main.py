63740a2422971a16a94db72f79de2ca90fb85fc9
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:24:27 +0000

    try comein weak slide afford

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_int(array):
+        return int(array[0])
