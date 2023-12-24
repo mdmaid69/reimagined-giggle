adaa9ec45b961c74750ddf77be9dfcbed91a5bcc
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:35:13 +0000

    scheme nearly ill measure damage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print("Hello, world!")
