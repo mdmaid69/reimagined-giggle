6a5bef1315166093ed36edfb2fc4c7b6834c4e3c
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:31:43 +0000

    breakup workout eager emphasis dear

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
