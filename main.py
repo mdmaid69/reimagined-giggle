e13903cba6074ca38831ae58a0ab0211ce2d1c32
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:50:34 +0000

    hurt breakout attention book correction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
