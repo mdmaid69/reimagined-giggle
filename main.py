92cd4865f073a2cc5ed6d4dff88464a36b798eec
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:59:59 +0000

    ability unable boat drop comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def sort_numbers(numbers):
+        return sorted(numbers)
