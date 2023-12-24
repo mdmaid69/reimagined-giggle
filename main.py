656f7f60e3f922dae09c5a49e1b5de28264ef231
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:00:22 +0000

    guess stamp everybody have sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def count_elements(lst):
-        return len(lst)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
