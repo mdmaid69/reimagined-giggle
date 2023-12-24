174b005fdb595fbab64891a728d58cd29e5710cd
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:17:11 +0000

    comment course case estimate secret

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
