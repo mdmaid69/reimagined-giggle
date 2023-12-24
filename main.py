71f1bd43e57f6f1001a98c8bd475f65321207168
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:27:29 +0000

    unusual free whichever backwards object

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
