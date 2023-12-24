ddb6f997c66f7e1bfebfa782def7e5ac73de79b2
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:34:36 +0000

    worth twist rule declare stretch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_str(array):
-        return str(array)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def calculate_area_rectangle(l, w):
+        return l * w
