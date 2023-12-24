5eeec663a7cea56a6c27d1d8ff3e0aea09c9c0a1
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:18:02 +0000

    extra getaway keen catch age

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
