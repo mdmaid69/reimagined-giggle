a7feaa5a81c502bbd0e5a52397509838ff96c188
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:34:16 +0000

    question Pp bunch that nose

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
