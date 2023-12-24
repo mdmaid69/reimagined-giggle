0b3b8c7c5516ce82e2d25464eae33612884a3cbf
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:13:41 +0000

    not mine course object Qq

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def find_max(lst):
+        return max(lst)
