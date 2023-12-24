f3c8fd41ae1995c9945c5eb647a205217cf18f71
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:23:42 +0000

    force possibility come sleep refuse

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
