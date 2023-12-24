420c005a073656ac292bcc216d0965f9f533ca3a
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:22:13 +0000

    pretty steer scheme rare may

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def remove_duplicates(lst):
+        return list(set(lst))
