fdff476717e1af049fcd3747535b06ee6647ecc1
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:03:00 +0000

    material brown extra pour average

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
