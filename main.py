74544c54851c45ff8f12d518a23042b03d18f317
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:35:38 +0000

    fall blame ahead cry suggest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
