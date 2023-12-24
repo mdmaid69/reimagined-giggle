749ce0798344d7ada7035db6d2c5cff6ba4cfc41
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:25:14 +0000

    post exaggerate dare shout ocean

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
