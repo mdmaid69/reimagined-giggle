6ed72ab27ed3877fdf569e5124c042e9f6d80e6a
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:19:43 +0000

    since outside progress holdout bus

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_to_hex(n):
+        return hex(n)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
