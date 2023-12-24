aec0b91fba581835eb7da79808cf3ce3f4f31f6b
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:38:46 +0000

    file warm stay relief order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
