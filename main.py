6b15e29ddc19b9d9ea61ada338fb4f582d9b0443
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:22:23 +0000

    fire even common sting muscle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
