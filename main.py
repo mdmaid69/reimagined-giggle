f6cbaef5c1cce76f1e04024fd609ed3717167b5d
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:45:12 +0000

    pen suggest pride what pretend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
