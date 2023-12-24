70525210204093276942002864460f6b0a964b04
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:57:29 +0000

    bury law pinch build simple

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import os
-print(os.getcwd())
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
