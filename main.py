4981e3451aa1f218f195e9d35fcd54d0eb11f43a
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:17:45 +0000

    sleep brown rise mark least

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+text = "Hello, world!"
+print("Characters:", len(text))
