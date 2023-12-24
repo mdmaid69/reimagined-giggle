449bcf7abddcbb5013d85d8b7ce9428b0761369d
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:44:36 +0000

    doubt base cheap change speed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
