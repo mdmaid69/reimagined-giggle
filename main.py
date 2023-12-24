037b0d3e42e61fb3e24cc91a30be6a01c4b6991c
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:30:15 +0000

    popular brown frame none lend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
