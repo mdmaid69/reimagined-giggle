56cec5a9f041f499df0060314dfda333480060b4
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:06:25 +0000

    save pattern gift fill must

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
