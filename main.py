57d4b6e9b5b792aaf67a9848e75a9f3aba2fd597
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:29:46 +0000

    sharp important glass question plenty

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def count_characters(sentence):
+        return len(sentence)
