78fb346dbda7ac094a33b51bb5b35e465932ab85
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:08:06 +0000

    advice ask instead laugh offer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def find_max(numbers):
+        return max(numbers)
