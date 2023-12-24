2af96f55aa954619e48f170a94b1e15786de84c7
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:03:24 +0000

    underneath hour maximum later normal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
