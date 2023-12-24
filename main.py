4173addb22e99f6d3c0eaa4455c75ad627f54d0c
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:34:30 +0000

    zero turnup brave trouble choose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
