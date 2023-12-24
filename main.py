adbce1dc4b4e79ef218d5151950fd1a586600817
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:17:57 +0000

    fasten worried experience gas breakfast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
