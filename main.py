459f961dbad0edcea0fe6887de5a64348a17f421
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:01:38 +0000

    frequent exist match total print

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_work(force, distance):
+        return force * distance
