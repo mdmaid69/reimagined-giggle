421a46498a6ab4239ca005f39bf2da4af49a27db
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:33:02 +0000

    handle concern turnout put recovery

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
