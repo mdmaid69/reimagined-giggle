d5db9c6631fb63582a2f6e80b28bc4b607e6499a
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:48:32 +0000

    distant mark tie coat form

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
