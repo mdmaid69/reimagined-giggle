3554755f02b4db7471782ae8b4cc885287f960b2
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:33:13 +0000

    provide most successful build programme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
