59f5da4da8dfec27bb2c21248c17e2f1bc4912fc
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:41:07 +0000

    open dear shut cost Cc

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
