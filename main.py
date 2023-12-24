82049d77df0341ebba69aa7862137c9550456a75
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:55:10 +0000

    succeed hardly prefer store letter

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def count_elements(lst):
-        return len(lst)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import datetime
+def get_current_date():
+        return datetime.date.today()
