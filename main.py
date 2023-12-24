d53bcce28c27b54cfa641be03444d98e731bab44
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:27:33 +0000

    notice important correct confident match

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
