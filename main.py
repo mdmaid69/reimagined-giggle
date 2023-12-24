841d9fdd923257cddd82e5ab9bb6364996bf2341
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:48:08 +0000

    direction hot beautiful black treat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import array
-def get_array_length(array):
-        return len(array)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def get_array_as_list(array):
+        return list(array)
