f5bf3bf33e71faa16c952342dec8992bc59b479a
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:23:02 +0000

    second search seriously heat slow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def append_to_array(array, item):
+        array.append(item)
