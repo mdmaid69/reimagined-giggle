9a1da8d85237ca8f7adb2711dae02a0e0cc58d93
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:23:54 +0000

    shake paper react realize fall

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
