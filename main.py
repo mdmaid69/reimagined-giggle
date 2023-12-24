e8bc480d82e1c9fec4e9b2e64f91c1e08c343590
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:07:57 +0000

    bye now paper gooff whistle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
