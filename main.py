100c2ce2d58e6e1d086eba12b598d7363abb85e9
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:03:14 +0000

    dance university yesterday affect Zz

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-print(math.pi)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
