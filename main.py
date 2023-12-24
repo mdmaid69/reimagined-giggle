4e0348d2fc571bb35648c559b40e80aa2b22d8c1
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:06:37 +0000

    rest table nonsense someone instant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
