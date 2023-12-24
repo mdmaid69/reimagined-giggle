27909429810b6e13cf8b6bb19aecad98d7adb76f
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:40:37 +0000

    interview interview desperate cruel Dd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
