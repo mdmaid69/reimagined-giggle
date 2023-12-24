8f91911fc0625f6210dd769446730ac88b16a82e
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:29:27 +0000

    circle confident tear country trade

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
