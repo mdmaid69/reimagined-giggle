a80bc9bbd83a82824beaac2130d328f8bf1dae8d
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:55:32 +0000

    tidy their final set mess

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
