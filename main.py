b73d1c2abfd3ad638b13d43df6a4faf9b7c06ca8
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:02:48 +0000

    stretch last poor model matter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
