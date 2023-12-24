34d3d9d4a14e52ac3310d9af584b6f5b39c50535
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:13:45 +0000

    dead pause also wish power

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
