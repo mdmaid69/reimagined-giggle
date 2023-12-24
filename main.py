074d2e4203b17e43f72a171153ac3419f3223933
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:37:44 +0000

    second breakfast touch criticize warning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
