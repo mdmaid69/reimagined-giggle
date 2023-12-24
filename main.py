1364da044ca94b6397ee600b19d4dee9d0bf2aff
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:56:00 +0000

    rough fix clue zero take

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
