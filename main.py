e8788a2b2a08a3ffa426313e7f2bfa6afd718448
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:54:04 +0000

    disturb possibility gift time explain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import sys
+def exit_program():
+        sys.exit()
