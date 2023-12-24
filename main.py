646f4b5ab215cc0afba1187c43e99b8f158c8f20
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:45:27 +0000

    arrival breath share chin force

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_item(array, i):
+        return array[i]
