8b06812268346b5be83f685c1755f1d80b5ce4f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:48:06 +0000

    mark news person roof various

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
