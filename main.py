08de8438b4c2bc0e25d76b38b553ffadb6124dae
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:39:40 +0000

    each claim eat send concern

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-print(sum(range(10)))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
