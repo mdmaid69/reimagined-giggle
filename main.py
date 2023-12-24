7fc49a0ed75027707dcf509fb344afc9afcadea1
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:40:43 +0000

    stiff mess pocket holdon custom

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-print([x**2 for x in range(10)])
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
