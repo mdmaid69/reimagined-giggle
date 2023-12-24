3c53fa17c9901d9fe01ad6e50b8cd45adbaa6681
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:28:07 +0000

    tap tomorrow probably tap actually

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
