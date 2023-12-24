828e886e7ecd8c0986343e2dbc64959401e9341e
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:28:43 +0000

    cinema me return bury noise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
