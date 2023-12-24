b78c4cb89d3dc152e9bfefa92d947fd2f24c7bb6
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:37:38 +0000

    shoot extra turninto risk mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
