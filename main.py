8cb7e484678614c45b46cb13ec74e33909e226eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:22:17 +0000

    contact threaten opposite throw catch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def cube_number(x):
-        return x**3
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
