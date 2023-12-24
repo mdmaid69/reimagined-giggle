6d6209b1c0ac1006d18572e9c75d8a583091c657
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:37:52 +0000

    Dd high pause smart name

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+print(sum(range(10)))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
