1e4e571c295714647bae453140956ad97edd878f
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:25:01 +0000

    secret coat turnout born breakout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import datetime
+print(datetime.datetime.now())
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
