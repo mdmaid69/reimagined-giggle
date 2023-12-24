e451305ca58f92e3d715d2f92d3fef3f3eecfd67
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:02:56 +0000

    hungry know behind during through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import collections
-def create_priority_queue():
-        return collections.deque()
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
