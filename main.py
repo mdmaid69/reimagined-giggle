db39d7533d9be08dae28b5f94cf8e6bc9ca10476
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:54:39 +0000

    hot love lay completely turnup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
