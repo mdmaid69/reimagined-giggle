c0423b4043765b7e6eb099f7b7d9214b9b87dc38
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:05:51 +0000

    set house claim across comment

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import requests
   def get_web_page(url):
         response = requests.get(url)
