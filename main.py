cfa7614af26032e822c4ae26eece041662569507
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:11:32 +0000

    pleased communication attitude there bringback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
-def find_min(numbers):
-        return min(numbers)
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
