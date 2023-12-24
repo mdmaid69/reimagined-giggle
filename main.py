f8a10e2cb756214c9e5ba68554dc76f28ad2196c
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:18:56 +0000

    pay moment bear nonsense upwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sys
+def exit_program():
+        sys.exit()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
