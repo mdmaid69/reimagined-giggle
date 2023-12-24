e0a5da392cc406f8c72afa7dd4451d5e4f02ca3d
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:49:41 +0000

    memory remark Rr smile along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def reverse_array(array):
+        array.reverse()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
