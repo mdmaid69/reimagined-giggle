74ccd6092da6722bff6041fc3d7e7edbe6d73737
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:15:03 +0000

    yours manage dick properly acceptable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import sys
+def print_python_version():
+        return sys.version
