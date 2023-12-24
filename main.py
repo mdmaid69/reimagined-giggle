5d934fe2c1b47c8ae0d227528f584720852a9788
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:34:42 +0000

    favour contrast true move hardly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_pressure(force, area):
+        return force / area
