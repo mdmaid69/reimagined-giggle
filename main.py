230bd51c80464d65e95ec8d7ed3f4f9d761b49d3
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:45:37 +0000

    refer flat spin Vv artificial

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def reverse_list(lst):
-        return lst[::-1]
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
