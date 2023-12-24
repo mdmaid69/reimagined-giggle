2b947e1c665df9806c7f483478c78b74046ffb3e
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:09:05 +0000

    church bush married convincing tape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  def calculate_area_rectangle(l, w):
-        return l * w
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
