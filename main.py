99e014d149912fd456a4844ea53a5163ade40f49
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:18:23 +0000

    message she world group herself

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import datetime
+def get_current_date():
+        return datetime.date.today()
