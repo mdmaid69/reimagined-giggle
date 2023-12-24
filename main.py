0b5dc82fa16627b6299f9dc27d037f5d2574950f
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:09:48 +0000

    clear share sweep boat trouble

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 def find_min(numbers):
         return min(numbers)
