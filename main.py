556d68294d7d2996b12d81d4f2b45e5b0f89f67c
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:52:25 +0000

    home disappointed advice share exact

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
