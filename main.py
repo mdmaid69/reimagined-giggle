730168b532b2c13db51cc73774bc5f92d013478c
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:42:35 +0000

    wake head rare 567enemy and

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
