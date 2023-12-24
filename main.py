ac98417ac95ec43025770b836bf1530bb1afa254
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:48:00 +0000

    giveout also train unite obtain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import os
+print(os.getcwd())
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
