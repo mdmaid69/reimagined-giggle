25b9e354bf5fb2f7c9f72133dc6431edae91407d
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:08:01 +0000

    they strike perform correction crowd

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import logging
+def log_message(message):
+        logging.info(message)
