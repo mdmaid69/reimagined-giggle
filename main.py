e8640db44181276aeeef29512480837acf73c384
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:20:14 +0000

    yes now ring switch everybody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
