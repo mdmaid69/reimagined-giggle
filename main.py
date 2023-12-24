329892b83c2e2d405db9daadd2c5443d8e0be761
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:22:50 +0000

    cross exciting rubbish believe stupid

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def is_odd(n):
+        return n % 2 != 0
