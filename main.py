c36ea938c0ddb89e99747e6602dd5dacf1cefab2
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:57:52 +0000

    unlucky music now comeoff afterwards

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
