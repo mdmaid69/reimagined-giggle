b34fd635efb2c5e89854a47aba3b8cf140261fe3
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:24:13 +0000

    wrong scale 571enough convince blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
