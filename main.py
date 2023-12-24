c5ce51816b5900e5d714b014133aa4dbc584c3f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:43:23 +0000

    marry first use pen offence

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
