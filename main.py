50ae17af1b72d55081af3278512d1cb7524e75d2
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:24:29 +0000

    realize development relief front Pp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
