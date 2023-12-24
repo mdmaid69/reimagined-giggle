e99fe47df2603c54133c2bf9f2878f4a4a274e22
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:41:57 +0000

    somewhere pause than natural team

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
