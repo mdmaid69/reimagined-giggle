698de8a13666f405d0abbe4bd254e6cb135c3085
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:45:14 +0000

    estimate least stamp detail flood

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def convert_to_hex(n):
+        return hex(n)
