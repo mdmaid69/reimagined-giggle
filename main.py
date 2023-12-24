f536fcafab36a2ff577a60755da0a1a036fab97f
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:25:59 +0000

    environment dangerous nearly mystery easy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
