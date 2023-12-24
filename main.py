a7a96a8ebc216ab541aaee2c212a68ebeddc845a
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:58:33 +0000

    about gate form guard yours

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import os
+def get_current_working_directory():
+        return os.getcwd()
