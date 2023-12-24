452d9d5d40ed595052001e1dbe98c448575d264d
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:04:47 +0000

    family bright Hh smart possibility

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_length(array):
+        return len(array)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
