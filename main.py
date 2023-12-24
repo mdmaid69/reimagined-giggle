c4e89feb38a6c3f1eeb488be6be689c9eda66be5
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:29:42 +0000

    bet getback sick keepout information

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_average(lst):
+        return sum(lst) / len(lst)
