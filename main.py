cb27397aeea34102b968e58565911bae2b92f8e6
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:14:21 +0000

    fresh accommodation soon thorough either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
