9b5984b64d7f221a0d4611af97b6629d12493b61
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:43:12 +0000

    floor that tomorrow turnon family

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
