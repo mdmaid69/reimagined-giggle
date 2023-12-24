e28fcb2c7633df342bb2bc831e8b05567e1ce12e
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:25:24 +0000

    dead popular base accidental key

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import sys
-def exit_program():
-        sys.exit()
+def square_number(x):
+        return x**2
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
