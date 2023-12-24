aaf2e3e53b5c94a5daec2a06b44aad65024d41a6
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:58:14 +0000

    powerful account stranger steal treat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
