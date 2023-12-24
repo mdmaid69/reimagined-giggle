e4f7dc23f1cd143e532e130dbc92e906837a7d43
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:32:14 +0000

    thanks path waste switch comment

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
