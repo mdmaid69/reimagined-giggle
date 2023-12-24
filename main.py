1f569f44b0d1b5a4772a1dd79ec2dabde0c54196
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:28:59 +0000

    fierce on word that minimum

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
