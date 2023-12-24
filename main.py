a46d1d382e3968c8192bdbf0829046750e30cc34
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:19:40 +0000

    jump rest disappointed usually act

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
