ede60222a0fa548c9930e1a4c3d2e5e74772c459
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:01:44 +0000

    unfair the music major any

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
