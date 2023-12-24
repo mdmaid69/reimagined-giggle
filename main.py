ce8acac2b0f5add078c20140ce0af83a784c5c9e
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:20:23 +0000

    fashion sale my matter mother

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
