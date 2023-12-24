5cbaf54ab5f271d6091daaeac714ee449b6bc1f8
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:03:26 +0000

    confuse relative sweep brave pipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
