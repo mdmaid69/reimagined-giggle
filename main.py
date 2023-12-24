855d6b6e3d58e74f851c86b6270548a18fb21b4e
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:04:43 +0000

    knock clear sink handle translate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
