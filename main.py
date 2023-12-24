201049af485ef86f68e5f48ca7a67e94bf311791
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:32:48 +0000

    equal me tie train justice

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
