9f7e86326f7e146784e0de42ac7944c835b4cd35
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:00:16 +0000

    pause temper branch tip mark

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def count_characters(sentence):
+        return len(sentence)
+  def reverse_list(lst):
+        return lst[::-1]
