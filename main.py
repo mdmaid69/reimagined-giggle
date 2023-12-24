7ea7982318010493bad15cf56ba69b27353b0a72
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:22:38 +0000

    receive permission connect journey circle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import json
-print(json.dumps({"name": "John", "age": 30}))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def multiply_numbers(x, y):
+        return x * y
