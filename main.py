8f06ede9de660edf6aa184700d2c2efd1bef5a76
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:37:20 +0000

    shopping laugh careful comeover cold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
