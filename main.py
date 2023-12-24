b502590028cd92d718f88a6dddc0850df0220a42
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:11:38 +0000

    constantly basic finally club direct

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def count_characters(sentence):
-        return len(sentence)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
