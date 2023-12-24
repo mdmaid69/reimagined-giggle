5c89aa96f7ba98cf8ee521cffb4dc85102e505ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:56:30 +0000

    cover tone become luck miss

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-for i in range(10): print(i)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
