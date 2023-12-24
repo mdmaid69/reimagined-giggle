d134db7b38764cba4bebac134114f1f6a0dae765
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:49:01 +0000

    recently pause sensible considering match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_string_from_array(array):
+        return array.tobytes()
