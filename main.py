d3a47dc0c3ed5750d1c6cbe11b204545d794905c
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:46:06 +0000

    own above speak consult speak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
