b8d6a69d246e9154d507b32e303c2f05d4da382d
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:54:47 +0000

    concerning picture ticket statement laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
