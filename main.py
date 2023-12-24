5608b436774552a1babeb5c25f66cc7fdf70ff72
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:18:11 +0000

    cost present doubt prefer information

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import array
+def clear_array(array):
+        array *= 0
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
