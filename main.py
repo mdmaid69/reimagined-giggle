c81005b23aae537416f5f928e7ebe9fe2452aec0
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:30:11 +0000

    prevent thinkof exaggerate Ee formal

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def add_numbers(a, b):
+        return a + b
+def calculate_work(force, distance):
+        return force * distance
