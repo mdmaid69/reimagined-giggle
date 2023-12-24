b3a88bdafc1e16f77eef39155f8478e80908f5ba
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:12:20 +0000

    tidy train through pin breakdown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
