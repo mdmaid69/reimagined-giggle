b8bc449abb0427fb1f04ece8daf6fd30f316acb3
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:55:42 +0000

    grand children bang distance Qq

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
