bb18ed30b550189e851ecba970618e9889fe36b3
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:09:02 +0000

    intelligent extremely physical smell find

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
