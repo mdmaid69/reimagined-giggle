e942c85dc69671ab9c2598c8f50ccc8757a8e6f3
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:10:37 +0000

    laugh break boil achieve close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_speed(distance, time):
+        return distance / time
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
 print("Difference:", set(list1) - set(list2))
