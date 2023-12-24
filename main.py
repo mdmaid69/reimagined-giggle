e0145cbd77693214ae58e9712bfadcf0edc18496
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:30:32 +0000

    skin gate Pp bye provide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
