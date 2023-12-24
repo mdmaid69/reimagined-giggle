807bc28c92f328455da62d46088978d18408526c
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:40:36 +0000

    dinner demand conclusion related change

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def divide_numbers(x, y):
+        return x / y
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
