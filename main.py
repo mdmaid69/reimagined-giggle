10638b1116ca5dc3ce3c0082d7df8eb87e2c70a8
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:29:14 +0000

    conversation behind choice hardly tradition

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_work(force, distance):
+        return force * distance
