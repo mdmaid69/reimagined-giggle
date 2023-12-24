0c522513ef62b809e6ad857da647a425b81505aa
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:33:09 +0000

    yes getback trust light point

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
