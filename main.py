37d0d78edbce18d6f733fbdd848f8c9cbeed6333
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:45:10 +0000

    fear run middle food her

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
