240732a1830e8481e0b91304df6e5e2ec7d9613f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:48:07 +0000

    thought high along branch stage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-print(sys.version)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
