040d4e88d5dffb7ede752d3f5f11938cacae9b1f
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:14:53 +0000

    rare recognize bite character heavy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def greet(name):
-        print(f"Hello, {name}!")
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
