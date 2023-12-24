00724a07f2a70ae9ea1167f194216795e1a217ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:57:31 +0000

    act hit classroom place direct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def square_number(x):
+        return x**2
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
