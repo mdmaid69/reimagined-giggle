d09a04820efea6a62c424b63581bf0f5fb8b09d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:10:46 +0000

    invite make by satisfied money

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
