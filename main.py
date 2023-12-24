6651beb87bcaaaed1177daa7089f81c5dac6b320
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:10:38 +0000

    pronounce sing bad butter silent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
