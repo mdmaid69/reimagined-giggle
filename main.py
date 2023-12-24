b04cfc49a3fd916b94409c3e4f029c1315aece09
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:56:51 +0000

    must pay issue grateful narrow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+for i in range(10): print(i)
