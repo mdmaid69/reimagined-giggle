a5730f197b923b1dacc429375ddd94425d76db2a
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:55:59 +0000

    report comment combination entrance downstairs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
