8e0f40f4dd162420e9829ff734f15fe9dd1fc546
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:38:08 +0000

    there whoever turnback admire start

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
