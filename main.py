a3ff4ebc674ab39c51d382a60ef12816ae405b91
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:29:46 +0000

    runover complicated turnout avoid call

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+print("Hello, world!")
