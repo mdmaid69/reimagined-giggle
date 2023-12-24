1bfa53df942d92f7dfdb739ba0ae417dd046fd83
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:32:28 +0000

    record main be Ii comeover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def append_to_array(array, item):
+        array.append(item)
