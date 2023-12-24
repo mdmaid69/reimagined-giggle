90b33173d880d7660afc46ceef6482ba8271c7bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:02:19 +0000

    guess pleasure runaway heat keen

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def cube_number(x):
+        return x**3
+def is_odd(n):
+        return n % 2 != 0
