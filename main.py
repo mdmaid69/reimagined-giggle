cc4769fa19046a10d1c8e571ed969c23b048c21e
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:23:42 +0000

    history beyond runout fast appear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
