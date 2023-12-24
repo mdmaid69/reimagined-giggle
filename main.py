e70cbc44664b7ae2a50aeeda075ffdbfb891fc76
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:23:28 +0000

    young sing declaration worry before

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
