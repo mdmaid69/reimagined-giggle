b748377ce98e1c78763b2ad01b5d8cee6e776432
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:24:25 +0000

    wish wrap ear place finger

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def convert_to_binary(n):
+        return bin(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
