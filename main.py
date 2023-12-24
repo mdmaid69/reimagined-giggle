2b5a115f3d22ad1fbfb9fd01f7a8294c0b1dc88d
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:28:37 +0000

    except total raise cover callout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  def convert_to_binary(n):
-        return bin(n)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def convert_to_octal(n):
+        return oct(n)
