03bc80e325e545385fcd077cb65efd4f22f2c40b
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:05:22 +0000

    shall mind drive window breath

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
