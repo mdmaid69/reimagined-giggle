75d6020b1fd4c25de6c23b093bdde45ff9036084
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:26:22 +0000

    summer pretty connect slice boil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
