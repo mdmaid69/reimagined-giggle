6e932a2b0ca310fc536402e96d48df22ad7d9687
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:21:44 +0000

    blame artificial turn travel future

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import tensorflow as tf
-print(tf.__version__)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
