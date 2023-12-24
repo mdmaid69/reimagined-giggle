7bcd78764d91679ffb3ffd60c0f838b2d207b83d
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:05:54 +0000

    arrangement several run achieve memory

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
