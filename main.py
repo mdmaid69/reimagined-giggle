f83a26f428f4b06c477c73ab33ad2c2fc1d71819
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:04:58 +0000

    stick Gg ashamed ask through

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_density(mass, volume):
+        return mass / volume
