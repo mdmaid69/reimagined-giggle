b81d79533482adb87e9b54e2fc35305863f75ccc
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:32:22 +0000

    delight mark cough to car

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
