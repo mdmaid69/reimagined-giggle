1611aa28bd265338fd834ae96e8e35e7b97c2a64
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:24:54 +0000

    exchange compare complaint time succeed

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
