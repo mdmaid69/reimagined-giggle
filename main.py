33ba3792d176839eb301886d882c919b9624627f
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:44:15 +0000

    order waste distance cool slice

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
