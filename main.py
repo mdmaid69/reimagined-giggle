1054ba5a2014b0098cd47a019c34bc9676273323
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:14:43 +0000

    preparation shall design pinch command

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_acceleration(speed, time):
-        return speed / time
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
