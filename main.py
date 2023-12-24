d7c755df6312372c7911d038dea0ebea61180ceb
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:50:06 +0000

    pay brave pretty owe except

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import os
+def change_working_directory(path):
+        os.chdir(path)
