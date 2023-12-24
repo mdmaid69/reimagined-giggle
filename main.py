91cc326a599b4858e7a4ff786ef0904aa38f69a5
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:01:49 +0000

    whenever spare thing flood imagine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
