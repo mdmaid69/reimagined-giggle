f5908a29c8ca602ef04980b9e5d7d86b66709a6c
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:31:59 +0000

    variety obvious tap statement long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
