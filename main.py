c33e16f5845a35721e1186abf17d9a67699a1bb3
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:47:42 +0000

    trip degree suit left evidence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  def convert_to_hex(n):
+        return hex(n)
