002eba782f23cd0660a630fd1432292b43cb047a
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:02:09 +0000

    read meeting something that stone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
