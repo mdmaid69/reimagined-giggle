9f8c209307fd5eb82723bba4b70eb1fe26d3670e
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:12:42 +0000

    hard punishment book alive anybody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def count_words(sentence):
-        return len(sentence.split())
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
