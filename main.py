d33fe3f30d8134424d23b0f947a48739c3398cd9
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:09:58 +0000

    advanced centimetre patience keepout giveaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
