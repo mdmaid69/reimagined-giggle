8bb703308c4fb12bb4c956db4d18fd8a0046b5c6
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:41:02 +0000

    anything bringforward challenge past collection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import platform
+def get_python_version():
+        return platform.python_version()
