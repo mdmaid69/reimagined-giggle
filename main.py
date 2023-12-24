430162c5ffc2abaac61c5d5cc24689c921e0056f
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:23:43 +0000

    dull treat develop wild great

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+text = "Hello, world!"
+print("Words:", len(text.split()))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
