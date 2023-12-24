064cd037a0ff8051a807512dba27816c35c0e698
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:26:17 +0000

    unpleasant sense move talk apart

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
