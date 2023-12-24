3b353a4ead7ddc186ea007548b3d408a46104bab
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:06:00 +0000

    light many board seriously shame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
