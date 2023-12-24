aff73ec2d8341e53c2e0f05639a17223f19c28f2
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:32:27 +0000

    care cross disagree my play

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
