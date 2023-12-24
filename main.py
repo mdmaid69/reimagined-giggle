58284580914787826918372895b5e2bdbeb5d38a
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:22:37 +0000

    already fetch comfortable teach visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
