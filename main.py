fdf357ba7192b8265bf1a96d392fa9db345210c6
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:32:21 +0000

    alone slight near there suck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
