b7bc080844fe2798e001641dada6207df6a3e75c
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:43:25 +0000

    period result past alone cap

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  def convert_to_octal(n):
-        return oct(n)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import os
+def remove_directory(path):
+        os.rmdir(path)
