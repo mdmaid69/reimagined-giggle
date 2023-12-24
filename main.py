74ccf231c121658781fdaaff4e9748ccc85a0e07
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:58:16 +0000

    trousers present recover desperate present

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def find_min(lst):
-        return min(lst)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def set_array_item(array, i, item):
+        array[i] = item
