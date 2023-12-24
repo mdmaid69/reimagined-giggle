c237c0ee4a76fd341eae17806be34bf89b576864
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:10:19 +0000

    any wide entry Nn properly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_stack():
+        return collections.deque()
