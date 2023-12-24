61d3696d0a8ff9d8456cdddc077f49645782b335
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:17:00 +0000

    more base wait pocket business

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
