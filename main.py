5a186daabacc4d1438226628567180bc8b916cf4
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:31:22 +0000

    variety continue fire company near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import array
+def convert_array_to_list(array):
+        return array.tolist()
