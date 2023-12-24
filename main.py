5504369e19549e5239d2c72028bb4f1ac18f54ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:24:44 +0000

    against used effect genuine accidentally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
