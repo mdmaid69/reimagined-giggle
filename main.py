93bf3c29ad0cd69f393bf066a7d564886ffb3661
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:13:35 +0000

    study compare undo concern taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_density(mass, volume):
+        return mass / volume
