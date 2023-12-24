72a2f80b7fe6b5907c12e234ff750e207dad62e5
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:51:10 +0000

    sign comfortable vision announce print

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
