b9b3bcbf11401e44b6ad0fda63ca92d3c345eb48
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:48:47 +0000

    bush punish drink frequent various

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def sort_list(lst):
+        return sorted(lst)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
