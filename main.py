4305cd497d3cf674408ea492923c766a9d66a72b
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:02:35 +0000

    vision impressive threaten success standard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def square_number(x):
+        return x**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
