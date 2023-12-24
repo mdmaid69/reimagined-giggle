dfffb314c1952a9c070a97b0574a1165edb23dcb
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:14:26 +0000

    turn gradually turn performance lonely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+print("Hello, world!")
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
