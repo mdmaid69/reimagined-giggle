d25dc5f37b25e83aad254cad522b3128d2333971
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:46:31 +0000

    question learn estimate television battery

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def multiply_numbers(x, y):
-        return x * y
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
