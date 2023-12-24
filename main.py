0515f9e3527c76a7cfc4f8950d07ad7978f7a0d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:20:14 +0000

    start set human travel move

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def find_max(lst):
-        return max(lst)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
