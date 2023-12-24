c10a220077a6b37cb71e67cce2c392a898483d77
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:27:08 +0000

    active wall on exploration pull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
