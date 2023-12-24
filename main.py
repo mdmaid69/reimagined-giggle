d0d0067723d1674c78942429ae2f4fff40fa47a8
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:18:39 +0000

    foot control tip 571enough path

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def get_array_as_list(array):
-        return list(array)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
