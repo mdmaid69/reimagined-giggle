54662dab6413161381941c359710c97f17c48141
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:40:03 +0000

    count mixture ring argument who

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
