628ed1aa62da6f685610098617f064d6bc1b8b21
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:21:43 +0000

    similar road spend any callback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
