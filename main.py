7189150ed8ac3c925d4baa9a0d21805a2bb08fb2
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:24:11 +0000

    bush easy wild failure before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-print(os.getcwd())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
