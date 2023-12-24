0b69931d35a49d7c9a50f8ad1f417fa52ea2107d
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:02:56 +0000

    whoever laugh freedom active moon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import random
+print(random.randint(0, 100))
