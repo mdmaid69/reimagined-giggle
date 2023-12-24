e38cf2ac80d5bfd3a46139dc6ca69216452fb7b4
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:48:20 +0000

    somewhere complete none operate suit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_as_set(array):
+        return set(array)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
