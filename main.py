e66ab17f49b5173f8cfa510d8de9ec0af5b6b54f
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:41:30 +0000

    rank attract disturbing coat exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
