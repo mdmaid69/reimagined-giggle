9f428c343dac794fb1ce14de20e607ae2e83610d
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:57:43 +0000

    suppose quarter suddenly admit cut

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+print(math.pi)
