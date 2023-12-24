73932e4269f81547a316a934e8125bf14e848b79
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:49:20 +0000

    pen contrast extra peace soil

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import getpass
+def get_username():
+        return getpass.getuser()
