ab096af08cd058ff324a726bff5c8a4795f9a91b
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:07:02 +0000

    decision trick lift profit anything

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  def convert_to_binary(n):
-        return bin(n)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
