c2e4090f9772f3ed06a47f287e97330be0576366
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:18:32 +0000

    fence variety difficulty rid married

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
