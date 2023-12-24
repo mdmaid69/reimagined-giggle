59f39adefb38ae8f5487603739892125c948311a
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:36:10 +0000

    company clear charge flat among

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
