8904fe7b43556941d298c35db28636418ee20ac5
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:11:24 +0000

    sort star extraordinary bill complete

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sys
-def print_python_version():
-        print(sys.version)
+import numpy as np
+print(np.array([1, 2, 3]))
+  def convert_to_octal(n):
+        return oct(n)
