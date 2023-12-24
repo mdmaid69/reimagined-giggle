8df1336f2eb691ac7df8e4a8ba9e52896e0cb782
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:31:54 +0000

    able able other face gradually

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
