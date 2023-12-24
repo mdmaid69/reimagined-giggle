6e68d3d33530f2eeeddcfa8bf62d551c1deb49a9
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:01:09 +0000

    machine pipe free favour frequent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
