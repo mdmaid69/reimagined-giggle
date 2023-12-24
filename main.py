ed9d063a8f0dddc478474b2f0c9907b761d3a161
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:49:23 +0000

    increase authority herself steady couple

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import array
-def reverse_array(array):
-        array.reverse()
+  def is_odd(n):
+        return n % 2 != 0
+  def is_odd(n):
+        return n % 2 != 0
