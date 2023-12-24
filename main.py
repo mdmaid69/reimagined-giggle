8e0834786355a5135878d589fd372fdeb259942d
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:00:39 +0000

    complete rush copy mistake worse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
