673d88b957778e86cad011cf18f65dfa9056a1d7
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:29:41 +0000

    least lead pass dear nose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
