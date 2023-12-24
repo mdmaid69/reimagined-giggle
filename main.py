c81d236a7db28253a6b4f1faf88efec509e216f8
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:47:26 +0000

    crazy sink drink such sit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
