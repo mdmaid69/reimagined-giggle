d6dbe72c37a86f3794d71e03352b36ec01d34669
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:46:08 +0000

    every cross statement contact strong

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
