a3229b9d618a318b6e30a20105277ae35db6e707
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:06:08 +0000

    basic attractive reply characteristic inform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import os
-def remove_directory(path):
-        os.rmdir(path)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def get_current_working_directory():
+        return os.getcwd()
