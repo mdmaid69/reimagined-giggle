9c51e7e0c88841dd32f8dbd682ab05eaac025ab4
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:32:54 +0000

    in necessary their bye card

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
