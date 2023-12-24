edc86b7ee349f82a16f5932502eba6af1b7d1168
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:31:53 +0000

    decide maximum payment yes deserve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def find_unique_words(sentence):
+        return set(sentence.split())
