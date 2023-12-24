85b0b40852e2c176bf09f978199a5b7d2e5019ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:06:24 +0000

    about brown top attitude pupil

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def remove_duplicates(lst):
+        return list(set(lst))
