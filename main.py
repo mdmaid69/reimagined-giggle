77f0491ecb9319a7ab98d3a4571a6e4435cb7023
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:32:40 +0000

    neither country correct no if

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
