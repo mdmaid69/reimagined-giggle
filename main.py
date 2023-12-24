1f8e871cc430505c7102d132f402387935d2aa80
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:39:39 +0000

    afterwards clear minimum left priority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import time
-print(time.time())
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
