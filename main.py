c2743c7b89468ddfc467e1f9b7b6ef294bafc841
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:33:40 +0000

    help part difference takeover note

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import datetime
+def get_current_date():
+        return datetime.date.today()
