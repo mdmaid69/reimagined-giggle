54816473dc1e9eac08f743ee2ac7c4741fa092a7
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:51:17 +0000

    squash middle Tt nature surprise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
