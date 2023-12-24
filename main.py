2aa84fdd34ab07fde7579f05a07778fb3369a050
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:54:01 +0000

    near holdup kind sweep finally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_power(work, time):
+        return work / time
