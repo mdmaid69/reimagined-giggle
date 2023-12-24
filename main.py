78db8a4d2c6face1071d72ed9a07099e52e4cd7d
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:05:29 +0000

    follow getback habit rest need

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
