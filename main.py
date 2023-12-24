8d36df77f978093941d0fecb5f8cc834f0aea700
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:50:12 +0000

    brother straight rest appeal bringup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def calculate_return_on_equity(net_income, total_equity):
         return net_income / total_equity
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import time
+def get_current_time():
+        return time.time()
