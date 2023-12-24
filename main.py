b2ec7ae6f8fc438a4051d73a39d6894594cd3fc6
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:59:26 +0000

    could play priority rubbish add

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
