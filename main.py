3dab9b5a10b7d0b0b252cca2ccbc84f2a2e75078
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:14:56 +0000

    search produce bury special sign

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
