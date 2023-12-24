43f0d1e9de89d438e26208af731fb2d3827cc877
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:17:44 +0000

    pick pipe police store Mm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
