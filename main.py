181ceb7256cd3a6e4d8749307d0fc7ef049820b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:48:19 +0000

    substance film commit rain born

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def sort_numbers(numbers):
+        return sorted(numbers)
