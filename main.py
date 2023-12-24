151ba5877be86bff2595ba6e20c7b38f0065e38e
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:57:23 +0000

    back noise hold advice other

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
