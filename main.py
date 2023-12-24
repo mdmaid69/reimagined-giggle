a1acbe94aa7762de67ca2f654f654ee77de81d69
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:08:24 +0000

    Jj certain oil right hope

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
