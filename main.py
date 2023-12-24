6136115d91c77f388fd552944d4b1bd251521072
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:48:17 +0000

    considering foreign project callat give

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print(sum(range(10)))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_pressure(force, area):
+        return force / area
+def calculate_density(mass, volume):
+        return mass / volume
