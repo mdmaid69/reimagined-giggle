04bd384e13af4d14bf46a8df8745b76b2aee5e52
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:01:18 +0000

    correction pretend recovery day switch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-def find_max(lst):
-        return max(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
