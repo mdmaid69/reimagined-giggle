6915eb1bee9f20f36b268982f9f3a26b8afc912e
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:58:33 +0000

    turn scare noise raw surprised

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import numpy as np
-print(np.array([1, 2, 3]))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
