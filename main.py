fc5b1dd9715e0cad62ce9ad1e10d4b10630c73fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:34:39 +0000

    convinced lean should hers bin

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
