52c1652934f4965f2950b9305a19261bd0d51964
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:39:13 +0000

    worry wheel effect like criticize

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
