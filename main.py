b7b0d1b12d48e94ec5a88ee62c952cd6e8a6069f
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:28:04 +0000

    soft inform tune on smile

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
