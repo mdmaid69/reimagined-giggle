b326ad521e5d3c6594032ec493951037cb121178
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:53:06 +0000

    clean tip silent he swing

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def remove_duplicates(lst):
+        return list(set(lst))
