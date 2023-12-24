6821696a5d45fcbfb735e027f3ef8492974290fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:38:57 +0000

    tough bringforward or owe chief

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  def remove_duplicates(lst):
+        return list(set(lst))
