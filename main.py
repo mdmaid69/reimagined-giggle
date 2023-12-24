6258723f4fcaa0c5c00a6a5c15794f46967ee760
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:00:13 +0000

    temper tool expensive comfort hold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def is_odd(n):
+        return n % 2 != 0
