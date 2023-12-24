ef9db6ca8228de1adc935e2e5468406601a73595
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:41:39 +0000

    protest behaviour noisy weather drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import array
-def reverse_array(array):
-        array.reverse()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
