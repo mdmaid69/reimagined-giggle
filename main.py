fc29a80a7a31ed3b349e5c98540c96c5155bf5df
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:59:23 +0000

    ashamed tall go meal smell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
