9a8ee84832646d5f9a9b07fe4f2ccc5ef56013f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:00:14 +0000

    formal true shop takeout disappear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
