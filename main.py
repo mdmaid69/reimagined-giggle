a3afc217909fd341fe2ecb50abcc3fa9dea64275
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:54:09 +0000

    excuse thing yours fashion sticky

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
