27bc5bb5fe242486b4bb58db29e6f90ac364db7e
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:50:19 +0000

    anxious passenger hope cross declare

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_speed(distance, time):
+        return distance / time
+  def remove_duplicates(lst):
+        return list(set(lst))
