9b99231a175d5887aa8dcc1b691043de4a540625
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:20:18 +0000

    fairly honest off opponent hard

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
