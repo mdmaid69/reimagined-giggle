307ed5b245f9fd0c1540ae8aa24f30e91272192c
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:24:35 +0000

    recovery lift Rr cry scheme

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
