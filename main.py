bc1cbdd1f0dcde43c617af66b821101682e7012b
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:39:54 +0000

    pure formal try fight lesson

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def sort_numbers(numbers):
+        return sorted(numbers)
