1f4a6a76fed33af3f853ef605ac7d376b976ff06
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:48:00 +0000

    far store definite reply ago

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import numpy as np
+print(np.array([1, 2, 3]))
