3178d16c843085293813ec2fc5e298807ef8d993
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:39:22 +0000

    problem away take order practise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def count_words(sentence):
+        return len(sentence.split())
