cf6b64756ac3b19c3dcf3700786851e7569a1769
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:23:14 +0000

    nurse once inquire comfortable beneath

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
 import tensorflow as tf
 print(tf.__version__)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
