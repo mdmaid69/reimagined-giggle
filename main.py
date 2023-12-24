f7f79ab5bd7433df612ead18b47c3999be20cb14
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:20:28 +0000

    might thanks getback middle note

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
