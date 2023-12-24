51f95a713af6afa06b3025b78b5820b3172914d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:39:12 +0000

    force strange Qq opponent impressed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_floor(x):
+        return math.floor(x)
