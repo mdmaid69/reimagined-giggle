153877f1aa6977a206b7f6d41782225fa3932c0f
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:34:31 +0000

    smell preparation slide roof claim

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+print(os.getcwd())
+import array
+def remove_from_array(array, item):
+        array.remove(item)
