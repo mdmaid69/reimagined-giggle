f97c5ac7d35c9e2a443fb4f9a2cac1cbf35a7c3d
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:22:04 +0000

    giveback nurse OKorokay deliberate walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
