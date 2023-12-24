4e2ee7874ca1e17a2d507a5a527fb84c1efcbbcb
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:07:47 +0000

    would exchange maintain smell police

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
