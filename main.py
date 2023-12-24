439129a4862331ee5220eeab9c6a4a822b5b2d3d
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:39:44 +0000

    know exercise pile direct mainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
