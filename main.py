0200adac400fa043ee83a2565a8cdcfab90457bb
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:41:45 +0000

    far company exhibition aware getout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_work(force, distance):
+        return force * distance
