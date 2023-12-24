18537f9255d99ccd1f6184d7c12bad703dfec79d
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:07:15 +0000

    computer creature die steer family

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
