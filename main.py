7cceb6995ff88231e6a48b5b5cf52e5a93db7aff
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:26:45 +0000

    about fence divide offer exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
