e8902426e5c0b61f2a893fe271105a9173e35e8c
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:09:18 +0000

    condition connected cinema ilovedicks pupil

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def is_odd(n):
+        return n % 2 != 0
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
