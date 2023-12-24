4ec7fb0b1166b13da6af3126b103375d183fda88
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:19:45 +0000

    team prove pollution where base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def find_unique_words(sentence):
+        return set(sentence.split())
