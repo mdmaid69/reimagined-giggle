3793b8591166a6879afebf25f2c67ade86e720a1
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:33:58 +0000

    ought up silent mind up

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
