820408d1c3a9b0c3669075a57db9ef150930c317
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:45:32 +0000

    much feeling steer loan heart

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
