a8becba50b11418cbc628f4f5e722db1386acd6c
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:09:41 +0000

    suggestion force take please find

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
