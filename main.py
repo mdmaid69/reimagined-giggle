6913674f1c478199d36c0658007e5f1482129b17
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:40:42 +0000

    generally experiment change various today

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_stack():
+        return collections.deque()
