e8c0a2be4570019cf8d7a054aa2f95a547dad7b2
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:25:34 +0000

    mean mistake nice unit far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def reverse_list(lst):
+        return lst[::-1]
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
