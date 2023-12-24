8c9f5e8fae9b80456b6dc0c47a1d917cd4256ac8
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:21:20 +0000

    office free luck breakthrough sensitive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def cube_number(x):
-        return x**3
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
