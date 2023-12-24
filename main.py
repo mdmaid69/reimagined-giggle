efe7a4fceba2f17ebf1c8431a31eb61299a47219
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:03:38 +0000

    improvement chin most runout lost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
