98b77789d04ecc3659c9bf4e14bb626c58e9de33
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:17:09 +0000

    interest sail share exercise tone

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
