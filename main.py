f21c6a8e376cce8079cf65debea6eef3c3a70a55
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:23:21 +0000

    thick turn trial wipe force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def append_to_array(array, item):
+        array.append(item)
