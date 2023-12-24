d099d4e39ccecfe84c1cf73f44f03b3c1626b00f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:20:22 +0000

    either ticket drop tone above

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import sys
+def exit_program():
+        sys.exit()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
