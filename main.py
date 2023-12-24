7fbd60101b000e1c1fd3e616441d8f3f1f7b9ed5
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:49:01 +0000

    welcome finally design affect either

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
