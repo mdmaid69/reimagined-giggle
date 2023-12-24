b06c42ab9dde45f8c75ffd1bd99c6c3b9a63b57e
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:14:36 +0000

    effect upwards sign strength step

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
