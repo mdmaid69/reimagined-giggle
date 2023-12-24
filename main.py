f3b99bb59e2250dcffdf86c5ac2db92d0274afff
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:34:55 +0000

    apologizeapologise thin reduction what maintain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
