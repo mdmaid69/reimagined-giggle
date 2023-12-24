341ef6a51a59582a0eb11dbf6ead4d17c89a0a5c
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:06:21 +0000

    good scream compete pattern turnback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
