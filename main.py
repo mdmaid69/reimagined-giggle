b9bf1ddb5415b27999693ed7d175f549aa0b3892
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:54:04 +0000

    play unlikely urgent mine care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+print(os.getcwd())
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
