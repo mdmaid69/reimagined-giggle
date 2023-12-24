85bbda6f58b3258724516ca517a9d307e411bcdc
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:24:23 +0000

    excellent measure wild style dish

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import random
-print(random.randint(0, 100))
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
