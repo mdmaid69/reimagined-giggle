eb5e2ef69160e63afad07c690a7c8cc620871c71
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:54:39 +0000

    frighten spoil song consider damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_bool(array):
+        return bool(array)
