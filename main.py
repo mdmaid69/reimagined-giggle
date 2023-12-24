3ce4161ad75409fbce84413e345de398c995e96b
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:16:52 +0000

    destroy difficulty ordinary type term

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-def multiply_numbers(x, y):
-        return x * y
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
