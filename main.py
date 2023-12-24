f426f4b82ac9b01f7bfecdcb34a239e4d808bae8
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:24:02 +0000

    finally Rr strong kind free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
