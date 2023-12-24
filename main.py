5bb61803f740257d1c7981c2d1a15e1fabd76669
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:02:30 +0000

    middle success plant translate prevent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
