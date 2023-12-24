07a17576ac7564d75002f244de616726ccbb83d7
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:34:10 +0000

    division thinkthrough difference spring offence

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
