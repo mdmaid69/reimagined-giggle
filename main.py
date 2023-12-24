35b88dc0d300380cc067e98cfb4a3492fd92a98d
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:45:10 +0000

    we distant drink bend instant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
