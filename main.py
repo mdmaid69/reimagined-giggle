fcc61b263b627a48df3b380ec207b5501ac095a1
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:15:32 +0000

    beat means feed refuse then

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def divide_numbers(x, y):
+        return x / y
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
