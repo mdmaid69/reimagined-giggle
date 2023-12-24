83b63698d9f1f99f88918a02ff82f0f91861cc74
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:16:49 +0000

    bunch this satisfied rare trick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
