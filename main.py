3136070d428aae1db3b5a80899cee874a7145a77
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:23:09 +0000

    scared not shoot if mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  def add_numbers(x, y):
+        return x + y
