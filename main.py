7de3087b996278ee832a446fe14dbc8e8d940a42
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:53:04 +0000

    provide fail Bb row order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
