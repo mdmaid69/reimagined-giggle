d670c2d49c83a943d4292df2eeeb9dd7894c6338
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:41:54 +0000

    success second object judge accommodation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+text = "Hello, world!"
+print("Uppercase:", text.upper())
