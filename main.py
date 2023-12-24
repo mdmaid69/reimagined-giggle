1c62ff9c1b7853832c404306415379203a8c40d6
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:58:32 +0000

    spring ability straight butter majority

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def is_palindrome(s):
+        return s == s[::-1]
