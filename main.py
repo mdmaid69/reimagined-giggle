0ead65d829c879ffb59efdb4406e6577ceea8135
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:06:35 +0000

    weekend lookup pain hot will

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
