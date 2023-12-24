5677727264d31e27e593477ee35d5952045b50f8
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:54:23 +0000

    wind attach regularly punishment copy

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
