f141b36e870f062fab4dcf1c56dca90be3d43b5b
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:04:30 +0000

    love giveout vision flat pour

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
