91045125657292e0455952c99b9903a52bf6100f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:00:43 +0000

    pair pupil dig what on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def clear_array(array):
+        array *= 0
+import array
+def get_array_length(array):
+        return len(array)
