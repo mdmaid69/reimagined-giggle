4739505db74f369e701d3cbae1b31f86272ac591
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:52:14 +0000

    tone cum equipment too practical

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
