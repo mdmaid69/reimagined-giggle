7ea4d58a3f0f09e3ad9df341bf958c81e50751f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:19:58 +0000

    train pretty hate entry level

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_typecode(array):
+        return array.typecode
