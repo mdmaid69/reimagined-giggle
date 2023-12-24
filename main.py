773b46ae3c7f799b9d12563a8c098f890fedcc65
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:11:51 +0000

    miss dish before yet pencil

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_power(work, time):
+        return work / time
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
