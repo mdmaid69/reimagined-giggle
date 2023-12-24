c31f88229c0932ecf69d4f10e2fe0895e10019d7
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:28:13 +0000

    lend uncomfortable shall centre mind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_exponential(x):
+        return math.exp(x)
