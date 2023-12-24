7a1206eb092f49d697405a9df27233ad0ec2b620
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:10:50 +0000

    imagination centimetre trick somebody drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
