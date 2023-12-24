1fa06b5904a22f458b3e6d54d3d3a1a4297c32cb
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:40:59 +0000

    call smile mark I skirt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
