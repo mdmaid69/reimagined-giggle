7f4d7075f7b731ed94c97ccdd78168a9ca3fdc54
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:16:04 +0000

    roof month breakinto beyond best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
