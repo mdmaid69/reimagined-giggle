bc6d16e1916ad5c3301561a7e9d4fe82a759ee42
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:02:04 +0000

    hide from slide speed run

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_acceleration(speed, time):
-        return speed / time
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
