cf7a18657df380249624a5398f8cfe2216f3fd20
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:22:04 +0000

    activity possible declare relative exploration

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def divide_numbers(x, y):
+        return x / y
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
