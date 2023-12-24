3320f79c56b8aa09635d8895c6e4477db76153bb
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:08:45 +0000

    pause connection pump sensible organization

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
