e7e008392bf5362b2c2393a391936c40ed5eeb73
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:07:44 +0000

    plastic without trade thin uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
