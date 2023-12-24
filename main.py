842c68fba78fa46da57223fac723c7d58958869e
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:05:13 +0000

    precise pass callup soft protest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import os
+def change_working_directory(path):
+        os.chdir(path)
