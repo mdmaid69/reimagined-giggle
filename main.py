2e6a995c3be53bac289ebd6a87c8dab178cf7b62
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:48:24 +0000

    mistake runaway expect physical suffer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import sys
-print(sys.version)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
