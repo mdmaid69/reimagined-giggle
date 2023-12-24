9f9921d06144db121d4daded8d4782a835e621cb
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:29:38 +0000

    frighten likely question moral hold

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
 import sqlite3
 conn = sqlite3.connect(":memory:")
 c = conn.cursor()
 c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
