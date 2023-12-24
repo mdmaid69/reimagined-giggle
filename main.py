b39aee7b4b5fc30b692cda4d85906cdd5ad45ea3
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:04:29 +0000

    left machine noisy worth him

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_work(force, distance):
-        return force * distance
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
