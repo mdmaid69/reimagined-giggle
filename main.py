61ff7191d036f718ed92564c2d81f71017b9a98b
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:02:25 +0000

    unhappy water repeat proper taste

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def append_to_array(array, item):
+        array.append(item)
