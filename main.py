4748fdbbd2b3aa48a89af5316d54b3f984cb5bce
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:54:50 +0000

    tomorrow goback(to) criticize steer splash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
