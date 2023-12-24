b931ea54825224d743fece727570ccd91eaeccfd
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:37:05 +0000

    chance blame because please match

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
