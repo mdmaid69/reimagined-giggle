5e7c07fc14ea787a4fe3a8c24d57df5c2efb37f1
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:05:10 +0000

    wave increase school amount declare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
