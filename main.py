feb095f2cce1aebf0f4bd399a83c4e37bd3fbe8e
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:54:07 +0000

    behind hit upstairs kick many

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def get_array_as_str(array):
-        return str(array)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
