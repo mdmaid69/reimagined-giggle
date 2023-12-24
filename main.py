7834bab69fb325c5e608977aba510f1913bfd464
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:40:30 +0000

    apart tragedy pencil female react

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_area_rectangle(l, w):
+        return l * w
