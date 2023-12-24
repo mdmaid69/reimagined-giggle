643e665f64bef82d291162b1ad56feb89e69b245
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:20:10 +0000

    tight used theirs equipment murder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sys
+def exit_program():
+        sys.exit()
