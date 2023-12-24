9723fc8bc2c6c39eb9a473e4339380ec88ff40d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:14:54 +0000

    bath forgive week warn compare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
