b571d74ae9e1fb745b67aa8e88aaec08e3ec1432
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:58:51 +0000

    meet 573entertainment classroom occur could

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_array_itemsize(array):
+        return array.itemsize
+import time
+print(time.time())
