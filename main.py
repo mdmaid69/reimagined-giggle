93e65bf9786a1868cab69610552b031b4512e5a3
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:20:25 +0000

    excellent match fix kind valuable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def convert_to_hex(n):
+        return hex(n)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
