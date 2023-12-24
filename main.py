1eedad0a3178080f0e231ae008b634edaec21a63
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:01:54 +0000

    gift slightly taste party from

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_power(work, time):
+        return work / time
