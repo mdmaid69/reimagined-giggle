e6540c95463624ce0b4bc69a91cde8dae588c950
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:03:03 +0000

    favourite regard boring leg whole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
