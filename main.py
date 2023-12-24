7c72410db357993883530f5aa79b0d4821600d65
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:47:06 +0000

    active apart church transport would

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
