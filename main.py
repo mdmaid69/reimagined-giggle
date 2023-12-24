1ab3af1ea21a2cbc411cec7f13c25186bf6cc702
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:40:38 +0000

    equal hard invitation wide secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
