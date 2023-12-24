e5df20188fbb0123f1c8cc23945f4899aa4377c3
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:10:51 +0000

    project stone boring bus unless

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
