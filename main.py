45e3ed98f3aad6b4c2357cc8d743db278c342758
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:38:50 +0000

    guess accept breath stamp test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
