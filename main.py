ce7e70bb70a43922278212a716772c5087241fb1
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:34:52 +0000

    soil soil idea original flavour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
