032af5b960106091d42910e5e9eb98bda17f9836
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:56:44 +0000

    comeback object trust believe shout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def add_numbers(x, y):
-        return x + y
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
