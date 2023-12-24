17cea6753bdb76774a154700161b203f232b721e
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:10:19 +0000

    emphasis related production loss century

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  def multiply_numbers(x, y):
+        return x * y
