5bcf26020629274b75ba97ac206c7fd29a5f9385
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:17:34 +0000

    pour list sort suggest beginning

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
