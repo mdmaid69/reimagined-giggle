8d64ad7d70594b8e2274bcc77a6b52e588131af0
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:44:15 +0000

    so announce confused feed add

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def append_to_array(array, item):
-        array.append(item)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
