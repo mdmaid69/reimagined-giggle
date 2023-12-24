46f316b73d1bd2d153939b8a16982bb7889b8c9a
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:26:39 +0000

    innocent better doubt surprising top

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import os
-print(os.getcwd())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
