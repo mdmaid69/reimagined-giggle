3d3004acfc42eb14d12f0620ff80bf97f8d945d7
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:54:24 +0000

    spare work poison crash everybody

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def get_array_as_set(array):
+        return set(array)
