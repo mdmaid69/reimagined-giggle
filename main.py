c7da1ad55c9623792ecb3ce92d758e7b63f3de22
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:28:49 +0000

    goaway identify I her mouth

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
