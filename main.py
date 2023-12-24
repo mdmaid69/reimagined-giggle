ee6946df7021fcd83a6411a213f19271e9091b3d
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:57:06 +0000

    record muscle stand instant child

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import sys
-print(sys.version)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
