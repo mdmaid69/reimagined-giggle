be1ef5ad71a4a9f89f08b31a4c0bfe1a45edb0e0
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:02:31 +0000

    positive number you set play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_set(array):
+        return set(array)
