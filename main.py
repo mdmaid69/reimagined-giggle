347f867ba5f3889f3f98e8db16ec7834df77f2ce
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:52:13 +0000

    friendly undo care past conversation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def subtract_numbers(x, y):
+        return x - y
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
