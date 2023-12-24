cc12c68b5b503fa2eafc51848327d0677be3316e
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:54:12 +0000

    wind such keep tell deal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
