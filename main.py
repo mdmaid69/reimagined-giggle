a69b4e95047678bfcfa394723c413128b64a3622
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:33:01 +0000

    already thumb limit generally neither

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
