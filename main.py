41b7d927fadeaf2fee803283d86e42b26398f5f2
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:41:09 +0000

    nonsense your job unpleasant activity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def convert_to_binary(n):
+        return bin(n)
