785e082989b0633853f8b45fdbde3a11f228eeac
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:47:20 +0000

    note window responsible will result

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
