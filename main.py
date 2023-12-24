5c52a51ad409f881cbb7f14a78855a44908f526c
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:18:16 +0000

    takeoff shop comment physical keen

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_index(array, item):
+        return array.index(item)
