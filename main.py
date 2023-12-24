f739d5866565050799093dcb7390f6760ad6f92b
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:14:23 +0000

    doctor independent rest treat true

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
