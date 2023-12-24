4e33e792a87b5c5062ccb102621a2c52c02fd30e
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:41:48 +0000

    bang smoke powerful exit once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
