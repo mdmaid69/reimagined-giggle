7d2ee4ee34583f2314393690054f39ef82ac4851
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:17:42 +0000

    lay soon wheel reply write

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
