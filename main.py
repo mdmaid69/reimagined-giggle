17e5514780875fc220d95cafe9879e7434896ff0
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:57:45 +0000

    purpose getoff cover minority workupto

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import sys
-print(sys.version)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
