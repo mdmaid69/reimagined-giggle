ba5339db6732ca73f3220f3620d8502bac910dd3
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:08:49 +0000

    switch collection interruption male scrap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
