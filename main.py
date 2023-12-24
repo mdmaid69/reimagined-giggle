c3d5879dc4cc2629021eeb4c0e9de5b02402b8be
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:09:25 +0000

    nowhere interesting behind fun relationship

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
