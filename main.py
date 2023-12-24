2707700e79003deb096e780ce811b280c694b318
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:07:04 +0000

    dance wide hope pity feed

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-def sort_list(lst):
-        return sorted(lst)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
