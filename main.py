0dba978160f869dae4d2b461cab55e9efd7dc7a7
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:46:29 +0000

    getoff basically job soft course

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def cube_number(x):
+        return x**3
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
