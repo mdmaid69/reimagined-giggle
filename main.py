0fec541de11112cefd379a9b848b2bb7e98d7f53
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:28:10 +0000

    apparently independent tip criticize secret

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
