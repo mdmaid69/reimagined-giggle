96aff8a7f062e63d4973203169b7ba5a3869b34a
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:39:30 +0000

    lucky sale defend minor giveout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_set(array):
-        return set(array)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
