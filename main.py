49e5c1996c0232ed03271ba414d10b69f0a51ba8
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:24:07 +0000

    lean talkoutof change well father

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
