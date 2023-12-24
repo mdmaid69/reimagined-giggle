69b54ebc71f20cb0c2f3351e25ea39079a1f15a2
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:53:55 +0000

    modern church mean admit late

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
