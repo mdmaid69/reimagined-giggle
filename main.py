0c4e22cb571fd52328f0c0078f330d84160d416b
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:04:00 +0000

    exchange onto trip each previous

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
