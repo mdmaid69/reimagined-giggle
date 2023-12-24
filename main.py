14d9e4fed16f9baccd49093b3836f6c480e98ee5
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:28:17 +0000

    now colour pollution cry awake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def reverse_list(lst):
+        return lst[::-1]
