d27ba7b4b3ed8666a3d0bcb5c7d2ffb2257b3fdb
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:37:50 +0000

    fire at language joke expect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def get_list_from_array(array):
+        return array.tolist()
+  def count_elements(lst):
+        return len(lst)
