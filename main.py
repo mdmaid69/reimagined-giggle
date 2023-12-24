d27769883e35e93e007661d67a7629a56955765d
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:47:40 +0000

    side plan control collection suit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+print([x**2 for x in range(10)])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
