6220ac11bb595c712e570794a5d895c86793ce13
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:11:07 +0000

    rich inside everyone certain occur

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
