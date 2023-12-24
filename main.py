606f52b0e039bc7693628e6cfcca4a6afee11fad
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:21:44 +0000

    bite prefer print whoever generous

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
