561de6de7e8af6d40ac57cc908be9e1668eeb9b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:49:53 +0000

    urgent stare cool carry furniture

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
