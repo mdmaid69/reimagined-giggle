9eb053e42343de40d496f1c9830043f7c5cabe34
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:04:17 +0000

    offer raw able lookafter consider

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
