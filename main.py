7841e95b8741e3db4990c9230a51dba46cfc0c56
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:03:41 +0000

    damp cross stamp mix Ee

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+print(sum(range(10)))
