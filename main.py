d2f75a018fd8d902301c2f916244dbcbc9b61611
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:28:53 +0000

    march fear turnon television reduce

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
