47d2be2130bc8ab3b24390bc8c5aaf6c8dba100e
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:39:54 +0000

    popular satisfied central lead breathe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def count_elements(lst):
+        return len(lst)
