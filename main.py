8f4296bfdddb143328cd760c672fc51706f62c86
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:40:16 +0000

    round trouble float rank guess

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+x = 10
+y = 20
+print("Sum:", x + y)
