19bde88ba3d44b6dd9d20ee55bb3992e5513411e
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:53:13 +0000

    introduction coat blow workon straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
