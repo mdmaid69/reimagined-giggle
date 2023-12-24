bfaba914a579de6f45df4030bcdb2b61e00761fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:24:01 +0000

    recent post blue bath Bb

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
