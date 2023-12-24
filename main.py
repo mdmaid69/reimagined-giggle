188cc7d56740693be37dd0268a2cc3d80c83ff9b
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:32:20 +0000

    matter dance fence or slightly

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
