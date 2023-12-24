46cc8d121402a670d6a087ffea9e42ee07c5960c
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:35:23 +0000

    uncertain preparation add professional light

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-name = "Python"
-print("Hello,", name)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
