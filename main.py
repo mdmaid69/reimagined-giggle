e0560f569f025551fa6d0a245eb4a79874937186
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:17:03 +0000

    mixture trap mouth drop difference

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
