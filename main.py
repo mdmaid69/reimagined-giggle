854acc4dd3bbed9972b328a4c6fd8e7d0a03bf83
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:17:59 +0000

    sight smoke ill history hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def find_max(lst):
+        return max(lst)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
