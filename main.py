c28a2eed25288ea727031e09cf6596d4d38c24f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:39:12 +0000

    loss air free justice Dd

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
