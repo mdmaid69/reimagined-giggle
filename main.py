fd16ffe53a5a8356134505e809dee6668c9c2105
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:02:22 +0000

    demonstrate Bb switch central poor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
