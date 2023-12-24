c9108114c8865e2f46fd42b20249cd40137e6229
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:58:29 +0000

    accordingto maximum ear score temper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
