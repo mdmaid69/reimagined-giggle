8a289c3c12e5270e877452f1ae981a3ce495e3a5
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:54:14 +0000

    Nn goon wild centre Mm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
