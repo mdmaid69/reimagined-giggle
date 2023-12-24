7e9922196b2598f4d36c99dcc159a51c69298b8a
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:19:02 +0000

    about high nature family deaf

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-import time
-def get_current_time():
-        return time.ctime()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import numpy as np
+print(np.array([1, 2, 3]))
