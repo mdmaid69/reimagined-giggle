3f5ca84b829a02dafa37e19f789e26c026866359
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:41:28 +0000

    family content owe workon row

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import time
+def get_current_time():
+        return time.ctime()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
