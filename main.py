0f45d980ee843efb3c55a6df10f312af259941ae
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:06:26 +0000

    print limit neck general refuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
