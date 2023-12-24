355c742d69c7c8627638cabc37d64aa2e93d6f7b
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:25:26 +0000

    lucky limit I less once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
