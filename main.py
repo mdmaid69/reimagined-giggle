a525dc77d30f7cc9a4d0f9920f807da64f4d23d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:13:22 +0000

    once copy subject strong board

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
