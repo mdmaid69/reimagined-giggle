5caf08010163fb5dd019131f656ead5fd4fe12ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:59:56 +0000

    shall our squash neck least

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_stack():
+        return collections.deque()
