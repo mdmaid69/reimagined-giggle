59a6b4ad732474ef3600c38930aa0a9752e898dd
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:22:50 +0000

    autumn mess sometimes mind quit

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
