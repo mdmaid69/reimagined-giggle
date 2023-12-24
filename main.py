e72ff21005fefed76e5734e0fef0b314a3b903e0
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:00:32 +0000

    takeout sign keepfrom mood offer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_array_as_float(array):
+        return float(array[0])
