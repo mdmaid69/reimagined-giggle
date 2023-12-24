850541dbd2886cb45b230d7dfe20584899c0b814
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:03:05 +0000

    quality science anything weak appear

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-def square_number(x):
-        return x**2
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
