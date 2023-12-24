b4f8d59d9d3b8575a6edc5374aa347e2a092429f
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:13:37 +0000

    turnup nigga hurry cool moment

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
