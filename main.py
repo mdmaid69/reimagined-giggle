5edef8ff26f7329e7e42789691d6623e5869bbff
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:10:17 +0000

    preparation wind wing light provide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
