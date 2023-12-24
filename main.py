12b3f2bb2e640b0dcbe7b7dce2f83b0184a8bcf3
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:31:06 +0000

    rule wise church round special

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
