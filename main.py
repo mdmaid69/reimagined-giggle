144a2cf5c1660e037555bf626384c7be68d4d57d
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:43:31 +0000

    else after matter pretty breakout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def multiply_numbers(x, y):
+        return x * y
+def find_max(numbers):
+        return max(numbers)
