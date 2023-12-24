2284f719b5320d8d92ac6d4c54d5f2fdb7212951
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:02:59 +0000

    across tour spend hurry deserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print(sum(range(10)))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
