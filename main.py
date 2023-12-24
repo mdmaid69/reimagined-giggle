8f726529a20482ab8f88ab6bece6d032b02d66ec
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:36:19 +0000

    temper awake order old card

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def is_even(n):
+        return n % 2 == 0
