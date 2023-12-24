3d22ed94e4e073b1dc2cc670ab2afb52af918720
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:12:49 +0000

    whom arm treat angry takeout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
