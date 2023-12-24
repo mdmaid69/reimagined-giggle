876d9bf31465f5ec832dee1d27bee0078febb9d1
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:47:53 +0000

    Itself shade fork personal row

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
