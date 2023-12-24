1491eb9f4906303be543ca44001f5145388d1dec
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:38:06 +0000

    woman owe game drink sing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+print(math.pi)
