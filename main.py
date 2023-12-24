6bbaea98fd1ca59a05e1027ebad7a236c58089d5
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:21:15 +0000

    share tired theirs detail dig

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
