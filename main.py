b8f38110e97eb81ea7c4dd1eeb7f5544281320d7
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:47:57 +0000

    comeround still wake child suit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_tangent(x):
+        return math.tan(x)
