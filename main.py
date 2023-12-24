70116e41f601a161a5fbf029d6db1c4cd35a052c
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:07:39 +0000

    pupil congratulate tragedy seat software

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
