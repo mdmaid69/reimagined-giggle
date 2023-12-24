b4e76f670b21bda75456add90eec79f6a8572952
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:10:46 +0000

    better value single objection complete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
