2dc04ba4f58ae3c40b7696f2c8cebe7990e8557e
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:03:59 +0000

    face satisfied crowd final punishment

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
