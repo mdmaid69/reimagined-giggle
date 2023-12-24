76eb655fb7ca2db306d6054521d1a98d53960ec7
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:26:49 +0000

    tax speed Ee turnback such

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
