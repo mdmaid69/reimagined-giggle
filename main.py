e899bfbe2d41c786f826cc3cb42136ac5baf93df
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:25:54 +0000

    principle action reply conduct kill

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
