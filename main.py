d3a7d3267db63befeae876a7ef85d53f26bd0e69
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:07:55 +0000

    Mm raw cost office interested

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
