cf986d6765062fd655b5735d69445912fde6ae30
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:33:07 +0000

    freedom thirsty through much contents

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
