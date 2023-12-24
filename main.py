bfc8e6330ccb13284980f2df0c711dadb82ac483
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:40:04 +0000

    lot exchange crisis quick willing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
