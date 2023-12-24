594a84d9b73972c88e34e5c9de709a01aacc836b
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:46:31 +0000

    standard strike very chance right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_index(array, item):
+        return array.index(item)
