6d9cd6c39e0f1b67562d727dd6430ee330f6fc94
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:58:00 +0000

    shop start long immediate scrape

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
