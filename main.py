dd10f6cec728f5a1b1c66600937c3607ffac38e6
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:11:20 +0000

    ability satisfied bright include empty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
