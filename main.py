711f4eddcc6963c7210ba7fdc80bf0256de1518a
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:49:21 +0000

    turnout at back live kind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
