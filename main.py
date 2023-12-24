ed18af0e673e710bf086a7fe883310c21329bb46
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:37:19 +0000

    goback(to) original use negative regional

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def append_to_array(array, item):
-        array.append(item)
+import collections
+def create_user_string():
+        return collections.UserString()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
