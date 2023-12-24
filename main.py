d66baf3df44e12b8e239ea67064ef75e7882dc58
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:04:15 +0000

    Gg people Vv blue passenger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
