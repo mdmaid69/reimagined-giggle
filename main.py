0600ee2449eac0c06e46786793a816b62d8d0195
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:02:35 +0000

    crazy 179bite lead themselves row

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import time
+def get_time_since_epoch():
+        return time.time()
