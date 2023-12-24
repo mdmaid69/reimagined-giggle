d5ce0114c31c2eed4a02c4f46b7a5099dae0e6a1
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:09:14 +0000

    scrape introduction and collection contribute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import collections
-def create_user_list():
-        return collections.UserList()
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
