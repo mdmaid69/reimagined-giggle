c97fe3330d162095bf2f27cfa80094c2e2d277ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:47:01 +0000

    whistle destroy rich dinner completely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def split_path(path):
+        return os.path.split(path)
