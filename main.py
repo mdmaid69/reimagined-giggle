8f65f7ef445d2000a70524150dcbd9a00d0f54fa
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:35:43 +0000

    start fault increase Jj storm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_power(work, time):
+        return work / time
