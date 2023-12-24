46e3f804e5c1dee3111b9056d1ce0574f45296fa
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:10:57 +0000

    call apply cross division shut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
