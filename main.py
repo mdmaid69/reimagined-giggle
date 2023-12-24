36d6086d07f352c8e79ddc7f6cfdc8dd88c7f1ae
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:22:44 +0000

    use tie intend dear dig

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
