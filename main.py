7b059d86d26b4a5dc7e23cfb138f191c9400c79c
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:56:04 +0000

    skill cause ask advise spring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import math
+print(math.pi)
