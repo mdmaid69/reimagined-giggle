c26b4b773e066da04f5a89e0b3255184c5f0308d
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:59:34 +0000

    reply computer respect deaf nose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
