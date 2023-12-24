e582265d67e44e5a0668b7c08466b411412b8389
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:57:49 +0000

    quantity want tip children phone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import sys
+def print_python_version():
+        print(sys.version)
