2e0481ff211e2645a35658f55578045328d4a31f
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:35:49 +0000

    hand common square block callback

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
