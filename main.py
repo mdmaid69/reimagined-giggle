05604a251ac738659ab3a2703502753d16f94a86
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:52:03 +0000

    slow lie 571enough surprise payment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def find_min(numbers):
-        return min(numbers)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
