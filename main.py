554b25876f39ccf209667669c8698b0bf8da443a
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:55:42 +0000

    false programme information mystery conduct

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
