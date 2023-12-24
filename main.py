c07303c4c341ad20713590f3ac7a5cd16930e55b
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:47:54 +0000

    between dish excuse building what

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
