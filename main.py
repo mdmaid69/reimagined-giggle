ec78b0f1b89965c195cd61b6f87c92150950e81f
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:26:01 +0000

    coat song boil wait type

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_acceleration(speed, time):
-        return speed / time
+def get_array_as_list(array):
+        return list(array)
+import datetime
+print(datetime.datetime.now())
