bfbc4ecf15ef851b7ae37fce63ed8ce09e4ffc29
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:12:29 +0000

    pump crowded clue artificial live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
