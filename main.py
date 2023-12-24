0e4483c5ce27c2bac5dfabacaf3c7d64ee13f434
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:26:04 +0000

    praise success unkind fall still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_as_tuple(array):
+        return tuple(array)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
