989636c9ea6967470fc43e03629f60f3ccd328eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:32:18 +0000

    sore firm famous flat Kk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
