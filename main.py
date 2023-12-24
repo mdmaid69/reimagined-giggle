84932c4996fe6b332cb32218f4c2fe14cb2517eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:01:15 +0000

    closely spray small prefer subject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
