7a7d5e7edbbd32130426489bf203f442c75f9a9f
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:16:35 +0000

    lookafter character successful willing minority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
