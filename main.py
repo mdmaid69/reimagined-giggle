a8b3ba53e43d8746c9087d84bed2d8aedd5e3d97
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:05:59 +0000

    warn Ff respect spin thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_area(radius):
+        return 3.14 * radius * radius
