61dc1760959137b65d36eb4cbcde7cde3cc78219
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:15:33 +0000

    phone thirsty during alive total

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
