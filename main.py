466a2dae0066b457c612497e9ee8a842b486acf3
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:33:44 +0000

    speed approval base whoever holiday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
