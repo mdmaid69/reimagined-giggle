9b1acab92653abc5b54698ad616ceef112299441
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:12:01 +0000

    behind climb convinced past upset

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
