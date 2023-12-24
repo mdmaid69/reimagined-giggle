5b8eb5a33f50a69e6a4b6cae263710b44f32dbf4
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:26:56 +0000

    profit concern journey protest lucky

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
