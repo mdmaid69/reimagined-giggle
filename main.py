81ca125c83ac6f278cd523b9013e795ad30f64ed
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:40:33 +0000

    experiment light after ilovedicks happen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import sys
+  def get_python_version():
+        return sys.version
+def square_number(x):
+        return x**2
