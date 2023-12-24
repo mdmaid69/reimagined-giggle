2bbffbb4a330d1f24a372ca994a4361d309c71dd
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:38:39 +0000

    568energy other goaway special appeal

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_hex(n):
-        return hex(n)
+print(sum(range(10)))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
