d647b40110709ea89b5e5742283d2c7faef3112e
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:00:44 +0000

    sell pretty challenge glass example

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def calculate_acceleration(speed, time):
+        return speed / time
