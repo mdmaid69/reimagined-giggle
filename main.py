3d0bdb95625aa79b44f49868a37eb8e401277c15
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:27:17 +0000

    proud right price ring school

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
