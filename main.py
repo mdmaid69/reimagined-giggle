b7d1db2a79e765b98228779387322cc2e1c0425a
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:55:30 +0000

    them wander personal brief dig

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
 def convert_array_to_bytes(array):
         return array.tobytes()
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
