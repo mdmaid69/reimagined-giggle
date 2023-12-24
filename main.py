36691b491ca4638ac0d258e4c0cb3fa941f94e43
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:56:31 +0000

    want you thin shout turnup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
