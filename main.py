354334be615169463a8084d044d251b09596273f
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:31:51 +0000

    limit develop crowded tongue commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
