a0a62533cd4d9b7ae171ffa8c8d4cc6605fccb18
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:01:38 +0000

    bringup especially model anywhere difficulty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import time
+print(time.time())
