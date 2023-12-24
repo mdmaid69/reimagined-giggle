a60d39afe3f9ea8d2874e6fae660929294ef08f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:00:46 +0000

    afraid wide tax proud vote

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def get_array_as_float(array):
+        return float(array[0])
