f25c56e45a3e35bba524ebd4095315b1cfe02fc7
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:20:23 +0000

    phone deliver concerning represent young

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
