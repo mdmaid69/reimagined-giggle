0eba5cc63eb0238580a91ad40e101b67935bb6a2
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:16:06 +0000

    rid reach pass turnaway slightly

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
