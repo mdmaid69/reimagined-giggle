8601e88c008633b73c185f9e8d9476e004ccf467
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:15:28 +0000

    which goaway age build quiet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-for i in range(5):
-        print(i)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
