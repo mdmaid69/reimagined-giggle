28b660b346f4cb4f6ddfeb7da182380e7ab75177
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:36:34 +0000

    unusual scream bear urgent female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
