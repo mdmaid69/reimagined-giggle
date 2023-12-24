b6766a7fdd1cb48182b6dd6ad091836d4ab2522d
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:10:22 +0000

    act defeat getup blame wave

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import datetime
+def get_current_date():
+        return datetime.date.today()
