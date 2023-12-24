d598b452a7d71d415b8847e33ad878d20a696195
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:43:58 +0000

    during hurry search shrink separate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
