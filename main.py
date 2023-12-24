6c96b6f48cd04b533ff6b90a6fa0e4ea42f4d377
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:53:52 +0000

    creature Itself burst trip judge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import collections
+def create_queue():
+        return collections.deque()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
