a1d85dd632caa88d9d82bf07ec003bb9ec49dc9a
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:52:31 +0000

    forward happy trade joke suck

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
