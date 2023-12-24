e3e08feda8a941c96eb28ef1065a4c0315449b13
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:39:49 +0000

    case taste swimming building progress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
