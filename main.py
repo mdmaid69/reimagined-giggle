c89bb58a61d4f38d103fe33f964316da7dcdaa6b
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:47:18 +0000

    clean bend worried shock trick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_power(work, time):
+        return work / time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
