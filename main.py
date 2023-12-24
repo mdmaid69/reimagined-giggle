1aff439232872ff6cff291f1be1a3fbcbb752a3f
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:57:42 +0000

    game train unfortunate hit succeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
