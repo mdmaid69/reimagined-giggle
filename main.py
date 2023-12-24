0b61eee829f5100aa559cceb8fcf49555fbb895d
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:20:43 +0000

    swimming thirsty half put air

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
