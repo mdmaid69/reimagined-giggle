c4d445ff07f17f36448b7c2353930f604b44f45e
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:53:19 +0000

    you partner strength passage every

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
