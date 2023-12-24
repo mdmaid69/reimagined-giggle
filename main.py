0fedf647eab23bde98f41af22df9a76c4c3a6a66
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:20:20 +0000

    need exam stare speech form

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
