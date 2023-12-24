f2e82b198694a6a59c3916c1f8b50ac0347db124
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:25:29 +0000

    concerning complain lie pretend properly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
