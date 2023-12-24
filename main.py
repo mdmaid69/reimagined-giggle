9c1d33be7759a62423fb570dbee283a66a10d1f1
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:54:07 +0000

    pump short left 564encourage place

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def count_elements(lst):
-        return len(lst)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
