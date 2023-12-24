679c553fe1f4021e67dca7b6dc3f1acf93806ec4
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:55:47 +0000

    interview completely mark process aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
