2a84fd347d2f33bf7b4f1e9eebe61ef7e7c09b7f
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:16:31 +0000

    wake about building never look

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import random
+print(random.randint(0, 100))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
