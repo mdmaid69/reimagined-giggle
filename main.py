f928a46dbd710b396a7798cd73683efc69dd33a0
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:59:25 +0000

    pleasant suck direct good throughout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
