446fd25c512eca7a6f0b457a301a92d67a4b7c18
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:36:48 +0000

    town wide hurt second maximum

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import datetime
-print(datetime.datetime.now())
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
