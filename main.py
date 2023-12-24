48d732c66f6d328609479df5ae91e6a8c4fe47e6
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:14:32 +0000

    much sharp duty factory swimming

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
