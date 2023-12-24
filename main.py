1e37c070e549195dbec91f3c4dcdef5adc52a779
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:16:30 +0000

    holdup hold strange passage event

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
