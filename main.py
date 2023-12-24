7d36607a0786b9ae9f9ce22bd42a64dd95288790
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:03:57 +0000

    worth pronounce popular detail holdup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def multiply_numbers(x, y):
+        return x * y
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
