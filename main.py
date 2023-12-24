3e10060727a9c32f50e4b8f239b6f2b7b5067536
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:36:47 +0000

    tax like sit ball excuse

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+from collections import Counter
+print(Counter("hello world"))
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
