eeb1ab4db688c983f14a62d3b09ba321aeb2f4e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:24:25 +0000

    warning eager Ii edge burst

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_speed(distance, time):
+        return distance / time
