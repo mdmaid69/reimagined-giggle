7aaea54d1297539e0818188adad1e9d1ae0fb7c0
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:17:56 +0000

    sick interfere Yy male yes

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
