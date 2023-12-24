034c3fbee314b14507a0b5067d6eb6d6d769c15f
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:50:17 +0000

    mention care to equipment demonstrate

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import math
-print(math.pi)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+print([x**2 for x in range(10)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
