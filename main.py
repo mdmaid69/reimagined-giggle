841077b83cefb55b12a7d15bd606b0f2998fd6c8
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:37:39 +0000

    pay dick interrupt inside false

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
