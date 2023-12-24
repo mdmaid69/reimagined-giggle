c94ddb3709621b01696a41c2c88fad5ac97589e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:34:40 +0000

    touch tell border open uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
