4ce90b04b22c514150425a8874b0eaf2d6b6655b
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:11:38 +0000

    print next settle temper border

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
