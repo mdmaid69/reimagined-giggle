5480a61cb9ad8e1b7c43accd1d3c2fe654c65f1c
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:38:31 +0000

    plain play contrast question thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def find_min(lst):
+        return min(lst)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
