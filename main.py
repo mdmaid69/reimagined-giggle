5a69960ffa4aa3c9fb30e61d52b327c24b5d2fd8
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:45:27 +0000

    aim song also than butter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-print([x**2 for x in range(10)])
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import tensorflow as tf
+print(tf.__version__)
