71771d4064cd3c913f426e1fe3b1b703e7dab2fd
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:34:53 +0000

    confused success estimate pretty uponon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def subtract_numbers(x, y):
-        return x - y
+  def split_path(path):
+        return os.path.split(path)
