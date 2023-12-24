41fed8d74dc82717641e910447de391f97e64f09
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:08:41 +0000

    usual tidy his blame sad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
