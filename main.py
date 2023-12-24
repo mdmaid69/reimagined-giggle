6204f000a87b6d7e0a8303c3f73bed5105dc9d42
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:56:54 +0000

    sharp favourite pupil history surprised

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
