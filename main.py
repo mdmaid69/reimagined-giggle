bdd3bc675d5e664299328b1fcb5b492d4828a25f
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:39:50 +0000

    runinto get lay recovery impressive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def convert_to_binary(n):
+        return bin(n)
