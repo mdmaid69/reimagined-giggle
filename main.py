8ced48c41f3659909ca364f6c471194b3bead7ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:18:17 +0000

    degree public Vv far day

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
