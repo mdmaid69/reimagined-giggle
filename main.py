9f9b0b0b96b2d79b78afd666d65510b2fa383577
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:32:37 +0000

    lie computer importance copy visit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
