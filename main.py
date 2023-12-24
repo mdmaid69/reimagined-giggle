44b6f62e1e004210fe63c16b04466968fe409539
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:13:49 +0000

    sink towards neither partner sister

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_work(force, distance):
+        return force * distance
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
