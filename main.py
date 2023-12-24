55e115bf993b3317f47f4019a920bf63ded910b9
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:26:42 +0000

    climb mouth recent refer centre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def convert_to_octal(n):
+        return oct(n)
+import os
+def remove_directory(path):
+        os.rmdir(path)
