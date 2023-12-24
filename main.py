1c9d971025b0e20a520eec9d65346e3f8ff03502
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:49:58 +0000

    brown near where vote shall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
