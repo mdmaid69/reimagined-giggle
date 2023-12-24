8fe58635fb76b4dfc0118674b582111f21bceb60
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:18:50 +0000

    rest limit available fit lookup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
