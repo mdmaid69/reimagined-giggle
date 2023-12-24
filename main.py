76772804a5f39330da797d56c5faf09f0794085c
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:48:35 +0000

    lock finally rough none judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def convert_to_hex(n):
+        return hex(n)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
