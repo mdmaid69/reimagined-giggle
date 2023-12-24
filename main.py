8fc8ea7c19b73d172ebd7361167338bc988e0072
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:34:27 +0000

    town ilovedicks branch hope much

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-print(sum(range(10)))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_hex(n):
+        return hex(n)
