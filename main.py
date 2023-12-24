74846ff59e5bbfb011c361fb7f6f742f3bb4f133
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:46:30 +0000

    estimate wrap nerve fine carry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
