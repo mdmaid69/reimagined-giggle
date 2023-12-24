9611ad5d88722ffa14769b0f1b1473520a19835b
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:33:17 +0000

    principle expect building twist fresh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
