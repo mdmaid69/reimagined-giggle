6f14e65c1b0d34560bb548f0fa6dd885e0fefe0f
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:27:49 +0000

    accident pure battery too safety

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
