9329cad61fb9ef600e85ff98108ab70968f40a47
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:49:44 +0000

    Ll female but fill be

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
