332f40aaacf64d0dbc2797b2bd72567107c9284f
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:00:53 +0000

    confused else rate letter form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_power(work, time):
+        return work / time
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
