2ca809a72d3e75680df56f92016ae0e7a146fb8e
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:57:55 +0000

    thank band term bringup announce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
