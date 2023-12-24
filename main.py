b738762c5d58ec55da422c4cd5443eeb872cb967
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:52:34 +0000

    ill disapprove getback relief rank

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_typecode(array):
+        return array.typecode
+for i in range(10): print(i)
