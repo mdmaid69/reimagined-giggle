ded4f1fbb1d6efd82e3600933c6b65363414690a
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:04:02 +0000

    damp square contribute hold turn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
