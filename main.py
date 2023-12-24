b6c0337c3ea54b7173af134dc65855e8568a2bcc
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:08:08 +0000

    closed common tear reasonable responsible

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_area_circle(r):
+        return 3.14 * r**2
