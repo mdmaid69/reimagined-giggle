c797adb6238b2ea3354dd4dc5070623275cc431a
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:49:59 +0000

    attractive disagree pause job operate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
