e3a2c607480fea341fb5c7bc787886455d9eed6b
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:40:52 +0000

    our definite attach warn hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
