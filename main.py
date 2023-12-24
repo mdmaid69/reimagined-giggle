b771fea446b4ad7a6d36f8a2708d7354c6ea0de9
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:30:10 +0000

    organize her thorough moral disgusting

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_area_circle(r):
+        return 3.14 * r**2
