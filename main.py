71d405c8390498766dcb2ae61cfcea9bd9583667
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:18:56 +0000

    brief hide matter always plenty

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
