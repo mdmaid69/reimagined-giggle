1ef9ad4dd065ab171b85cbc9140e3b2746bb8cc6
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:46:52 +0000

    concerning before boat where join

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
