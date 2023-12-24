3ec0c6f8f5286ed3241c678c93cd502cdac75bc6
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:53:21 +0000

    whistle below persuade custom feature

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
