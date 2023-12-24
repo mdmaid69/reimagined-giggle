509cb4276ad8f3a0f36bb3e4976da40004231b0e
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:24:02 +0000

    exam share discover catch tight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
