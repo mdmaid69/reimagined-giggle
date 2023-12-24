38b68a2b3ecd7e3e3fed14b8785f59f579400650
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:38:19 +0000

    tight grand alone represent limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
