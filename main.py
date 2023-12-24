bd6e6676bf73b31a461033d7a72bf4912cd60b8e
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:03:13 +0000

    petrol holiday base eye tonight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_sign(x):
+        return math.copysign(1, x)
+for i in range(5):
+        print(i)
