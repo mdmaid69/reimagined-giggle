cbbe5bfb5210a9781daf578c07139475e7095e29
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:28:32 +0000

    target anything until dry trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
