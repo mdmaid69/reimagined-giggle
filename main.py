0f2b0e23b37152106d6ac10fd2ec6b146c0e1408
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:44:15 +0000

    pile anything somewhere murder convinced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import time
+def get_time_since_epoch():
+        return time.time()
+def reverse_list(lst):
+        return lst[::-1]
