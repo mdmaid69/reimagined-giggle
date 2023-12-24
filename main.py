8822dd04e78b79efb0500d4ead643a1b4d5e89a5
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:05:26 +0000

    dance pretty fold creature plant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
