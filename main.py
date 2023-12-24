14b31fc7305af31858c3d1abdc291669f1b6a109
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:04:05 +0000

    discuss amount delight turn could

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
