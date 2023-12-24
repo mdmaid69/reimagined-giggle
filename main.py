9d35e8a814e9932735df4b1aebb8b650e2e401a5
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:04:34 +0000

    goout offend number gradually otherwise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
