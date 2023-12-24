b8615a20bd8de1b9d8aee400d4a2b6b8177d3b34
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:23:15 +0000

    cruel dangerous variety modern proper

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
