8d9a5e5659b0ab7a0a6b5cbf12aec18cb74cd0ed
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:31:42 +0000

    sing runout trust provide over

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def get_array_typecode(array):
+        return array.typecode
