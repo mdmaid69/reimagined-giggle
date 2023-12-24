6594f5b1986e00261b19d26143c4ff124b5f3956
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:16:39 +0000

    use undo reply middle finger

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
 import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def create_queue():
+        return collections.deque()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
