8971e3e375ed6c92af1fbbac1612b50ecba83eb7
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:27:06 +0000

    if bread pleased soak border

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
