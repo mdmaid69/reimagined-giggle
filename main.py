c981554dbf2ef65c2cfc81b80804d40ef26a6c73
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:32:00 +0000

    narrow hope drawing year activity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
