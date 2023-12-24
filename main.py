a9ba403be4410193511120f9c3783eb2654d58e8
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:25:26 +0000

    container keen range pressure out

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
