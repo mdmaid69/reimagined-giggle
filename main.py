d2bee2eeb0bf33f353e0016926fcdbd291a3a6b5
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:49:00 +0000

    sort lost quantity bottom interest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
