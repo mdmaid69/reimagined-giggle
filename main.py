51e9bc4e0c5eed8ec35b0ca2b5dec85e8b5d7ba6
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:08:51 +0000

    speed use wheel war raw

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
