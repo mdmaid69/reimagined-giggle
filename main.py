f72ed06a765c5349cf456ae71b87a910c862ad1f
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:13:49 +0000

    Oo some example motion food

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
