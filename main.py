19f16304f98d968362e196235fc553ba88439345
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:02:04 +0000

    snow connect after rough success

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def get_array_item(array, i):
+        return array[i]
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
