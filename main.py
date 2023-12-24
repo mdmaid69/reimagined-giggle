a348bf957a99dc30e21070f458e46e7fe308e0da
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:41:34 +0000

    ill letter repeat move attract

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
