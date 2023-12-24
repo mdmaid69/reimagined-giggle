17ca3ec30d753c782d9b7c51671f9a6264c3fbac
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:32:30 +0000

    between describe combination active mass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
