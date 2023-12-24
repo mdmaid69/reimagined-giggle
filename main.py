07f379fa6f7ade53ffc611e72c1eb8db5bd23033
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:32:38 +0000

    their best test correction picture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_pressure(force, area):
+        return force / area
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
