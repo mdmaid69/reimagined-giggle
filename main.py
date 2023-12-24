f0845b169beca3203182f46402032e90cb3ae6f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:42:59 +0000

    speed objection interview shout about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
