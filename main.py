d8aa552f81917379a66e20b8e92815a07edd64e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:56:12 +0000

    smile sad mistake yourself wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
