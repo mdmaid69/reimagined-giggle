ec7b49819d59a5721c7395c5714ca8612c9b9a67
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:21:43 +0000

    shock pupil great square taste

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def sort_list(lst):
+        return sorted(lst)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
