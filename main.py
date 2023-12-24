52520875857c35f2f04d4c4cc81df3a09003f92c
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:32:16 +0000

    though voice Cc drive attractive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
