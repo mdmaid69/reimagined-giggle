8a0d3e81dd6c948e93a8cac20ab21b157c091d48
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:01:30 +0000

    surface short trousers outside complete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
