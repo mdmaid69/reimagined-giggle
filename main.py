fd59b7fb1dfc4af270e28cd0492ad3f93d6b5128
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:35:55 +0000

    comedown callout closely about measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def reverse_array(array):
+        array.reverse()
