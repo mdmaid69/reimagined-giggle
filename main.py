3317aeea63b23c58a1656f7d045a2d5a743be489
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:57:29 +0000

    top name steady cry bus

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
