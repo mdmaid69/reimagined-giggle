8a4a58f51b668ce78a4ec63807c4ebdbf58c5ba2
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:23:41 +0000

    bad private aware violence region

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import platform
-def get_os_info():
-        return platform.uname()
+def multiply_numbers(x, y):
+        return x * y
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
