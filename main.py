069c582dbb98621d9a9b5548976e198734b8680a
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:26:59 +0000

    put freeze clean challenge petrol

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_speed(distance, time):
-        return distance / time
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  def calculate_area_rectangle(l, w):
+        return l * w
