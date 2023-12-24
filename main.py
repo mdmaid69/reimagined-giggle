513d1028fd0149aae6220254aea27e4b6261d425
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:50:20 +0000

    repair everybody support spill popular

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
