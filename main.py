6742c83bb34742fe53ace51dfdd30d25265a001a
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:56:04 +0000

    immediately help several let free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
