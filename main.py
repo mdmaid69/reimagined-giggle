d58636dda2247ce089640226f7e7408bfe8d5bb5
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:56:45 +0000

    large pronounce free when cross

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def calculate_power(work, time):
-        return work / time
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
