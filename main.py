1952911e52af929a1ddbc09013f3a3301eb03f78
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:22:26 +0000

    identify blow store bright continue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
