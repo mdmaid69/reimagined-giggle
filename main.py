ec02b49dcd30bcf8901707c9ef8975a200da390a
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:30:52 +0000

    progress change house exact noisy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import array
-def get_array_length(array):
-        return len(array)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
