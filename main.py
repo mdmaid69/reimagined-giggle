d171a5c9c38cf4f10524ca520dff11ad0a3197ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:17:54 +0000

    approval until mysterious rain mention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def get_array_as_set(array):
+        return set(array)
