f7dc0b018e5ece855f36ef3b2bea1e20877df284
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:31:51 +0000

    terrible exactly responsible rely previous

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def subtract_numbers(x, y):
+        return x - y
+def find_unique_words(sentence):
+        return set(sentence.split())
