a4de50757900e4778191761c6a246b02411c6e64
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:22:49 +0000

    tidy translate unlucky measure fit

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+print(sum(range(10)))
