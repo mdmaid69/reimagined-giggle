dade9ef753ed2d266e0c34c995c1500355650304
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:07:05 +0000

    effect into someone light combine

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
