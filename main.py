f2dcb113ecdb141bc54b25fee386da971e0518f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:29:54 +0000

    enthusiastic report cost first television

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def find_max(numbers):
+        return max(numbers)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
