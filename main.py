b2101c3e197183daf5a1455d192b21711f81de2b
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:49:25 +0000

    by mix suit possibly available

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
