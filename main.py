68a6d5f00a8803a3bf628c48c5161e6fb0f53c12
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:59:23 +0000

    mistake repeat handle politics almost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
