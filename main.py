33f7e388a2c669ebc17d43e7ba7dfb281e3db31a
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:08:47 +0000

    usually bury pattern command over

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
