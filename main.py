ca186e177a6c92f98742551689d840bb9b6fa25a
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:33:46 +0000

    Zz central fall gather boring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def reverse_string(s):
+        return s[::-1]
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
