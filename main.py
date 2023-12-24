3d4ab43767a0ba21a02f616fa7468baa1aeee4bd
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:04:49 +0000

    value friendly arrange unite successful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
