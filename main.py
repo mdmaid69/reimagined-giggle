4c4b2bf5b45d4aa58ed09709988a6c7739518dd8
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:12:22 +0000

    train argument 569enjoy schedule share

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import sys
+def exit_program():
+        sys.exit()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
