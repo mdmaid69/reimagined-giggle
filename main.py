7400a528234100e081b4a05003f7cfa3a9bd3f50
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:07:11 +0000

    rush fold experience recommend under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
