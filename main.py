cb968535760d0a3cca09d4ed72b81a3285ed9009
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:01:01 +0000

    quick free trouble peace turnoff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  def count_elements(lst):
-        return len(lst)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
