de63a656d70661a3eb10e3b922ad70a52c0b45aa
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:53:20 +0000

    giveout squeeze shake unite vision

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
