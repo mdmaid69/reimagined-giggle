8e1532830473345503f376d8dbe542317a84ee5b
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:08:30 +0000

    deep count path water getup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_str(array):
+        return str(array)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
