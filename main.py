b78dd04078a3bc4440206e58b553cce4c8d4b5fd
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:57:38 +0000

    wait seat plan respect 566end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
