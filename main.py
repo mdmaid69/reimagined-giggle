62ea83ec4f8d5d9422fc2c4507d81461550e20ce
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:53:56 +0000

    embarrassing piece friend battery must

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
