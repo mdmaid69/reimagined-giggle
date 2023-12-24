d7a0564d6c6829ae9a23fb364b9d8044bc91d1fe
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:23:42 +0000

    accommodation company other immediately mainly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-name = "Python"
-print("Hello,", name)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
