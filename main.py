03a364891e2cb37fae0db6809571ccc9062da22c
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:00:36 +0000

    basic general division workon slow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
