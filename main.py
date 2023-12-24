56067a4a141ba15ee6762fb32d0a04f5404afbe7
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:59:24 +0000

    bag original before open maybe

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
