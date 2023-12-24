9beb173bdca2c0182ec0668699fc8051b2ba7268
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:15:33 +0000

    lunch extremely moon him why

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def cube_number(x):
-        return x**3
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
