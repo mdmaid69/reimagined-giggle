c50442a834c459d7d17d4e367a2281e77d2fd6f1
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:37:18 +0000

    switch church someone often dry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
