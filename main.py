fc14974cb827d0f98ce386008e98564ff91c2c61
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:02:13 +0000

    cross opponent still avoid crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_work(force, distance):
+        return force * distance
