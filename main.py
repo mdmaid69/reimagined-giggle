24d05731fcd6fb8dd85249c4542422d673ba458c
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:24:19 +0000

    conclusion interrupt without surprised communication

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
