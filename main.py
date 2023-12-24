93f27c3c9ab57ead68ee801504df0d47fe84e405
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:41:19 +0000

    middle defend telephone cover when

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
