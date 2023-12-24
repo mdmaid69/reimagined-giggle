b835d2be8ac80a92beb0ed8b7ab47fb658752795
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:33:50 +0000

    thin sound beside emphasis deliver

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
