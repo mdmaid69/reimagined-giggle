58c002333f1d1107f3af9b16c23beef59921cd27
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:38:56 +0000

    so be exactly development post

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
