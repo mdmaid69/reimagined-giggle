c2d13ff955b5e78afebe29e53af4ba092ad500ce
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:01:41 +0000

    dish sail demand beginning line

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import sys
+def print_python_version():
+        print(sys.version)
