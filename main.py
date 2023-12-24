1901961077e3164dec78a1f469be1641bbb61613
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:31:53 +0000

    noone Cc allow Nn guilty

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
