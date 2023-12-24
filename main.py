1d51b60567e2b0a3fd73c30f240f18755fc77282
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:17:36 +0000

    pattern press lock pay behave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
