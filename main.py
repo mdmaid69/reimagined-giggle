bb9e563117d328cad27dadf82ecd195e6843e5bd
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:57:19 +0000

    switch post forever power pinch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_bytes_from_array(array):
+        return array.tobytes()
