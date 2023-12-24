2c408b4684e73b960801856ea890b2886fc794a7
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:38:51 +0000

    nose preparation work gentle fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_length(array):
-        return len(array)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import collections
+def create_user_dict():
+        return collections.UserDict()
