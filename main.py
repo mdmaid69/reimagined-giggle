9d36d84125bd2cecd2996507278440a9de859b51
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:41:08 +0000

    567enemy forget accidentally unusual rub

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
 print("Hello, world!")
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
