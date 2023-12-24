33b6b242451b1dcbb74ad3efde8bae9855690882
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:33:58 +0000

    supply state professional closely claim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
