1ae01687fd5c6299c0de7d3f815f86bf9810b2ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:52:50 +0000

    row memory slow hole runinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
