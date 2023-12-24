17433bfe2ba4e8148fd4103e171614b059eceb55
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:58:46 +0000

    total secret feed cover high

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
