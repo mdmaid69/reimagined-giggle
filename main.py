0f52ec40cfc203504e067331a73d26ec5655dd9a
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:45:12 +0000

    look godown workout exploration beyond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
