7990707a3f1e3407eb19c8291241595ffec84b71
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:44:52 +0000

    tread summer reaction comeback satisfied

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import random
+print(random.randint(0, 100))
+import math
+def calculate_cosine(x):
+        return math.cos(x)
