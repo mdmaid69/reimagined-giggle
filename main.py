8c4733a892e5ab8be117d044a9fc3074345bc450
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:12:42 +0000

    add gentle circumstance thinkover pollution

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
