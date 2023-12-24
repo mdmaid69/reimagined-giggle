0f950c7685c3f7c6cde3d0f1cd9e85a34c1b4620
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:05:27 +0000

    important count whisper branch special

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_string_from_array(array):
+        return array.tobytes()
