169e98ace60b6c01f0c7ff28294af7a7c1a5365f
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:59:13 +0000

    speed thick afterwards any you

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
