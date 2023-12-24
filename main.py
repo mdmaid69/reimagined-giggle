82910f40e6c5fb8963e5091fed8225a673a44335
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:20:27 +0000

    too pinch bury character properly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
