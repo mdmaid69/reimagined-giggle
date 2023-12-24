5787e8ec9ae2bc770a8ca208113fd23376313692
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:02:23 +0000

    promise tongue further average punishment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
