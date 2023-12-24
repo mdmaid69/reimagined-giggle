97336a2ebb9b3e88feba8d5b4fa2bf0529859aba
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:25:52 +0000

    add surprising importance same wide

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
