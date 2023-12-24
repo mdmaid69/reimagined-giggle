68848c910a42e1712b92fc4c549f5265fe8527d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:57:25 +0000

    rank dish direct strange valuable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
