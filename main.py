f711f2d297e3b6def4bf410e7eb336ed8c2dadb8
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:40:39 +0000

    praise whether most pool evidence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def sort_numbers(numbers):
+        return sorted(numbers)
