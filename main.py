0caaacd5cb2e91e5a4567e86be29250eaa9e3e5b
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:38:22 +0000

    voice office lift off its

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-for i in range(5):
-        print(i)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
