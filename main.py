b217a83f93a7a9f530b59b1d2b4d4bb6b3b7fd76
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:26:39 +0000

    blow body once broken hide

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  def sort_list(lst):
+        return sorted(lst)
