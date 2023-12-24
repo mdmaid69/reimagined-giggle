244ba8aef15c325541bfed0e265624f1e6cf1483
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:37:05 +0000

    thinkover autumn past instead unlucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_volume(length, width, height):
-        return length * width * height
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
