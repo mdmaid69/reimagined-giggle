8dca672252f0505fcf900008ae11231a9d0908f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:27:43 +0000

    there must nice tooth disgusting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def get_array_index(array, item):
+        return array.index(item)
