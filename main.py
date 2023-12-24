298634c937c477d349415bb432d5c93fa73fdb9c
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:40:07 +0000

    569enjoy direct task production uncomfortable

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def convert_to_binary(n):
+        return bin(n)
