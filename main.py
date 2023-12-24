612a6fba76bb7159ad4bd64364f3094c51f63551
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:46:43 +0000

    comedown surprised hospital crash fold

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def find_max(numbers):
+        return max(numbers)
+def count_characters(sentence):
+        return len(sentence)
