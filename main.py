c944bec39d39ec15eee48be88ab34a6a86ae4e0d
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:58:14 +0000

    bottom she however outside underneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
