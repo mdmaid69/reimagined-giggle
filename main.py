fcffa05a8872a71009903cca07fcc02d5d89119e
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:06:20 +0000

    where clever cry long on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
