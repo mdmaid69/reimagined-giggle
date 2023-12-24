3c44632c41662bf545723f42a295d13cd6532684
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:10:07 +0000

    old sense bury his sensible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
