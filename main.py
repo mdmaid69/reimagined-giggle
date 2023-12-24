dcd1352c752c131e9095ab5b6b515ceec9f9c5cb
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:17:20 +0000

    pump doubt trip lower that

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
