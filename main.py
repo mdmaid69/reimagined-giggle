86f5bff05ed9b38a58b8fbd6718e3243c6f681e7
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:09:39 +0000

    feeling slice mean brown charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
