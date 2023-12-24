54033de91d1806cece74092c6b12bd29ca754fb4
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:52:28 +0000

    adjective statement calm watch hour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
