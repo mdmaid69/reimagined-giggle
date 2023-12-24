7ab42bc5fa9a0a16c02d3a69fe00fb451d70c029
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:57:06 +0000

    back discover replace immediately crash

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+print("Hello, world!")
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
