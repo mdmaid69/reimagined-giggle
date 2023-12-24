2b1ff6f317c53f6c2a64a334d2d091f9db8eb9fc
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:13:31 +0000

    fence find give sore Kk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
