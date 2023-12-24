f44952aecbc739044fa4dbe5dae42b2b28a7c7c6
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:56:34 +0000

    extra moon alive difference speed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
