13dadb3d475055203dbe28533a643608407fa6e4
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:51:32 +0000

    onto substance if rare mind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
