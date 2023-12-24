0c5ac5f57ca14d19f91246dae8b06b22d83f9f2d
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:29:36 +0000

    priority instrument about exactly lend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
