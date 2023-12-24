63338d5c5e81d5de658a50b3f44784d5735974e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:34:04 +0000

    rely top defeat ever damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
