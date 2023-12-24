08e1189a3e1a7c8709a5c07bd3eeabb729ee432b
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:39:31 +0000

    dream hollow thumb pretty subject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
