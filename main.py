2c6334904c2e2805a3fe07cfe789a8ead16b5eff
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:50:54 +0000

    section brown be easy absolutely

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
