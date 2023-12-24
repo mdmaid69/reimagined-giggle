651e7eb70d40bf557e84de947771cc874b924a18
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:16:47 +0000

    568energy move temperature thinkthrough date

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
