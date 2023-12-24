5e36e196cb3c6ae19217635752ebe511f0afcd41
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:30:28 +0000

    answer blame long uncertain spare

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
