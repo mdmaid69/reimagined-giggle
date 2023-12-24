59a5eceb33880a7ed20c6e28a756e58ca06cf194
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:35:23 +0000

    speed question difficulty you invite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
