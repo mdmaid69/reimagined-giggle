47c7e073c80585d5988f3477313793a40d48448f
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:00:46 +0000

    compare contain exam nigga explain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import collections
-def create_stack():
-        return collections.deque()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
