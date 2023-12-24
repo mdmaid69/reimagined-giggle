f0a29dd227645bfdaf4f14acc162c2c080c9b380
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:11:54 +0000

    little follow slow talkback distant

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-for i in range(5):
-        print(i)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def greet(name):
+        print(f"Hello, {name}!")
+for i in range(10): print(i)
