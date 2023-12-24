f5e58fbf95278c9f6726ed1f28094ccdb9a04b8f
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:02:35 +0000

    tap remark curious join hand

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
