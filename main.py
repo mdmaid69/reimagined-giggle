d80cdd0c87458bda86706188bbaf0efe5d51b84b
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:56:14 +0000

    hardly from measure indeed duty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
