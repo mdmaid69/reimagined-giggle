e281f3fbc6c74cd9231c8eb49e67767ede1c87f2
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:46:04 +0000

    hollow suddenly relax closed undo

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
