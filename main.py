01bc79f21a60ea62f43d3f328b58a929938c4799
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:40:11 +0000

    spend bother if bang judge

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
