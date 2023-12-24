3dcaaa5fd5af7f45504d30d4b8bc9752aa0a62eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:42:30 +0000

    though last grade slice woman

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
