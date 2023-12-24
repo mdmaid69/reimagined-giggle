59eb22dcddc669e30b347d51d51535a8d4c7a3c2
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:42:40 +0000

    lower connection OKorokay race imagination

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
