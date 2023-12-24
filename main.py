f7ff03570160beb78d980387f92b36294845ba5c
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:07:47 +0000

    load official ordinary criticize call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
