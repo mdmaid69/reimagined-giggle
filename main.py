12a7c32298ca48ef1c2e3807720c1278d6632bd6
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:35:47 +0000

    him feeling stage pot method

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
