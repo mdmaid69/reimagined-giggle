80498314dd98d35d35a9db5cbdc53a98e7d55cf7
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:46:23 +0000

    issue emphasize sometimes shout other

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
