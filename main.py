caea652d95a5d637a9239ca84d0f9c6b02ab3ef2
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:14:07 +0000

    backwards mind able turninto style

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+from collections import Counter
+print(Counter("hello world"))
