37f381997546ce3975596f67f9c4b139ca4c6a39
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:38:30 +0000

    lay juice represent become someone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
