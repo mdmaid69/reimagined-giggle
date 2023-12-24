44e2b6f8e97ad7095f1b65485493961ccb7d5801
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:57:16 +0000

    nothing appear advice already noone

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
