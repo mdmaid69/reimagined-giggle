bc75c93f48b5810d0d71270d141691288c400198
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:46:21 +0000

    dress often group born debt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def greet(name):
+        print(f"Hello, {name}!")
