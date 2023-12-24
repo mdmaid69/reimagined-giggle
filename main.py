469cc9853ed8c602e5a08da1f722c9a2b21ee57c
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:05:37 +0000

    exist depend remark small occur

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+print(os.getcwd())
