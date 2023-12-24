f013d68bb1bb882f8899e797a36d39bf542e3d92
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:37:59 +0000

    politics Pp protection example persuade

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_as_set(array):
+        return set(array)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
