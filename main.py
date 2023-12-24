79a3e92114ff60e8242a74c00a984f0e77f92df1
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:50:10 +0000

    recognize experience anybody recent shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
