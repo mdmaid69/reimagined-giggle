6c8a9db8a938d1b0a96303346e00bacc601a9348
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:01:05 +0000

    crime attempt possibility cruel buy

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
