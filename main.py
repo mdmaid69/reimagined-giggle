fdf322496983285798df0fa1ebeb99181627ec19
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:29:26 +0000

    film long describe hollow factory

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_work(force, distance):
+        return force * distance
