7a6bb9363f8b9c46e54da1a954355f084eae39f8
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:18:50 +0000

    catch unhappy seem section impatient

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
