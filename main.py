67860831a6a2cfb090c9dec603fdbd6704266003
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:10:49 +0000

    nervous stop few takeout little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def get_array_as_bool(array):
+        return bool(array)
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
