a9a2ece9920e0490431ccf979f2cf21648bcdf15
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:40:55 +0000

    circle care fierce clearly holiday

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
