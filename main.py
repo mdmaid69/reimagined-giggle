7e2af99d550d585e718c041c01dd1a566caf32a3
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:45:51 +0000

    extremely colour head blind exam

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
