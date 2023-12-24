7246aa7ffab5c1cf3a6821ffd9247abddfccab40
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:08:52 +0000

    rule minimum slip what something

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
