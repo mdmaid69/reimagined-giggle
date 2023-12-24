e3490d3f2ef7a63220b90736e101e09e013959be
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:19:22 +0000

    operation advice continue guilty power

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
