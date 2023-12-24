4f9be5e66939a79c50f9d939c6d7469d6afd44b0
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:59:39 +0000

    stiff state operate attention bank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_current_working_directory():
+        return os.getcwd()
