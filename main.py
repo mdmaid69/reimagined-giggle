2c247ec9c4e34c45f9b349999bfaf10b38af1aa6
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:55:55 +0000

    actually have unkind pollution lock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
