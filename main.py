690870faa533ca444ac41fc458cf8baeb18f562f
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:35:27 +0000

    improve downstairs complicated beginning at

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
