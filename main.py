e7de67682403d858aafa382fc4139819f1f772dc
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:49:31 +0000

    love near surface shallow sweet

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  def calculate_area_rectangle(l, w):
+        return l * w
