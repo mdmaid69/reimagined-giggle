a76f690e9bb98b5c57550c76033a475f3c85cc10
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:00:11 +0000

    rest confusing breakfast shame declaration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
