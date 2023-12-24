ce8986116cbf50456499cab5f0acf7f9f843882d
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:55:50 +0000

    new anything position worth shine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_current_working_directory():
+        return os.getcwd()
