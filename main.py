aea26a8a6d2523efdbd8aa6f27c6f8a3d714005c
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:01:58 +0000

    weigh style transparent ill 566end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
