a9469ca83c56c72c6039e8c603fdc981943bdb0d
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:52:47 +0000

    rough precise shoot exactly flight

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
