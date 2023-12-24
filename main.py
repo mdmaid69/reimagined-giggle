e51b0d9185d8f42f6d7721ca0b646dd2cc4a008a
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:17:40 +0000

    ago everything chase after finger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
