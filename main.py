41e0072801bbcbd978694e5aaff0538d50a890b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:53:56 +0000

    succeed country clear walk talk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import time
-print(time.time())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
