8e62b582a685b5e31220e8d8634eaaeefd68dc79
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:19:22 +0000

    persuade keepoff underneath improvement land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
