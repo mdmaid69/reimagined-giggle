9c725de72cb93d033ee7eefef98dd6ac22d63eb7
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:01:11 +0000

    build dark scream world turnout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
