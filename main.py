48ca25a39f45ed4eb46a3f9c73bacb63074aa3a4
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:56:11 +0000

    rely hungry important ability turnoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
