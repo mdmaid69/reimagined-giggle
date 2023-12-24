909109a839b2d0aa59f53a54f8676745c6824e85
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:24:57 +0000

    choose smell hard post lookup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import tensorflow as tf
+print(tf.__version__)
