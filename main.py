d4620f79dde870c9fb2d593785ec09cb333cd98e
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:10:54 +0000

    prevent note comein may much

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-x = 10
-y = 20
-print("Sum:", x + y)
+import array
+def get_array_as_set(array):
+        return set(array)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
