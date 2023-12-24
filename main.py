5b4015b674768c610dfdedde136230f5ae09b103
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:41:30 +0000

    past whichever value well on

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
