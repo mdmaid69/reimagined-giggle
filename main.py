30cad205e1c3091444f7e1d43dc6d21fc7d22902
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:11:23 +0000

    spray recover family lay copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
