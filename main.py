522bffb6e2050be3668a6f80a1550422c192e848
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:44:19 +0000

    contain coffee threaten sense precise

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
