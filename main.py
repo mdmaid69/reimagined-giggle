fce0b84698c5fd9d424e62fba9d8639645790ecb
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:37:21 +0000

    favourite throw apologizeapologise giveback under

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import time
-def get_current_time():
-        return time.time()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def reverse_array(array):
+        array.reverse()
