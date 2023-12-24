d308acf2a58615193378772418248450f4904403
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:47:34 +0000

    equal test female break that

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
