a11095ed74bd6b6f63e2b3b5ff321d58e3c3b021
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:40:03 +0000

    couple likely about interested picture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
