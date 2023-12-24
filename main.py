ff62ef5f58553c516ee21fc8541fea51e4d800df
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:07:14 +0000

    below through wake accordingto discussion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
