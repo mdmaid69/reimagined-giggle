0f8212c1e303cba668e69f2b3ba712d834ef482a
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:59:31 +0000

    meanwhile about transport certain style

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import os
+print(os.getcwd())
