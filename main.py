079f21950d8cb9d40c5261bc249c3147bcbfc88e
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:55:29 +0000

    bank against purpose meeting score

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def multiply_numbers(x, y):
-        return x * y
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_power(work, time):
+        return work / time
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
