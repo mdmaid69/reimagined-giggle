6109f0c99ad6ec6a5b8745b563ee6238cf1cafc5
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:43:53 +0000

    odd another time feel lookup

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
