2f88f8ca39332f307eb13aab776dafffb699104b
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:17:54 +0000

    stupid comeround kick aware period

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def convert_to_octal(n):
-        return oct(n)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
