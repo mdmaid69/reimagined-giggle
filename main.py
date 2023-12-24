1704f4e9fd3fb534752423d7ab386daba59b2631
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:26:48 +0000

    complicated wealth gentle lot whether

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
