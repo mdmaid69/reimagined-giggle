a8d39dc36b5869dfa20889724e09d3e19dedb491
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:48:48 +0000

    snow welcome same legal crime

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def divide_numbers(x, y):
-        return x / y
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
