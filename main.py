1913f28ff7b64e8eb6e2eab49f8b5b2fdee8419b
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:32:58 +0000

    call reply drink accordingto change

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
