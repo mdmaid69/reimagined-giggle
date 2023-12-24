90f021884036038d7f09b28414e368aee7bdcb54
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:56:24 +0000

    cinema help conclusion Rr remain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
