ba32873176589571c50e977a061e6bdc39e69ca1
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:44:43 +0000

    average would tragedy invitation different

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
