05969460a93af019960bf378460de229a582b4fc
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:52:54 +0000

    turnback progress pipe passage block

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
