88b0e551c8a659a32769b1b795d4bec780cfd06a
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:30:27 +0000

    coat holdon hit thumb well

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def square_number(x):
-        return x**2
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
