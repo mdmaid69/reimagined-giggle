88bd2068e6b53b928d6e899e1228a8aaee07ac46
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:50:06 +0000

    sense fun murder land conduct

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
