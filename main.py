b539c2741004d6e012cfb2edb6c6de766daa66f3
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:40:49 +0000

    evidence far walk fork previous

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
