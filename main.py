1bc9c1b18947c26b56c5b82ea0132f06a76b6856
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:19:06 +0000

    he ashamed along interesting easy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
