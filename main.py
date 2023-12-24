e4f7009e78d3e2bb7ce1b5c6c166de67ecaba8f9
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:22:41 +0000

    protect brush age arm office

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
