bf76c3da49d8a550e8b14537fc7e2500a687e81f
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:56:17 +0000

    wrong shake performance fire release

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_density(mass, volume):
-        return mass / volume
+print(sum(range(10)))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
