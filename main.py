a2f086a2337eb94ab504263085cfaf12fcd955f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:30:35 +0000

    skill oil above accept watch

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
