83af646cb9e1470460a93b7e75755f9fd29cefad
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:13:06 +0000

    otherwise brother bottom afraid brown

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-print([x**2 for x in range(10)])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import os
+print(os.getcwd())
