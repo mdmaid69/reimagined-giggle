f3e04fa8a34c96c2618e4e11ddd30708f5edacf3
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:22:21 +0000

    band dig introduce innocent gift

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
 import os
 def get_environment_variable(var):
         return os.getenv(var)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
