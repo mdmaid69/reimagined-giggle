4933d4683f09deb237bf5cdd4dcb949ab54a4e52
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:33:36 +0000

    spite throw sort compare suggestion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
