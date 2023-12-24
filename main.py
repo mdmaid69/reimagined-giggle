ede2a2631d81042705c3d258fe8c8e82093ee48e
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:22:09 +0000

    stranger face over drop send

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
