7700e8de86f36889d559bfd34fb5e2898341b8fa
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:16:41 +0000

    promise onto suggest ahead direction

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
