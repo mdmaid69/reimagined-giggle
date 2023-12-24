a1bb048bc30fbd2d1beb0c91b3ec60475d1faa17
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:39:51 +0000

    beautiful plan say reply no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
