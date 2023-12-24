99eeeb15dc708d6c15f1304fc70c55a5299894d5
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:32:30 +0000

    bunch walk shoulder mark blind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
