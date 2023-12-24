800fd3d5db0cf69bfb598ea82756798cf9ba6723
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:12:07 +0000

    spring turnaway base prefer content

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
