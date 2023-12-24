83fb362a0299a69d4c5646188b435733d6627775
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:04:37 +0000

    relation manner pride wash risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def square_number(x):
+        return x**2
