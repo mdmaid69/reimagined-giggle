6b402b150ad75abf890db6a29fe54e9a09cf4b87
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:58:47 +0000

    careless narrow manner reliable because

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
