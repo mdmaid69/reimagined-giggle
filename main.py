e23c0141c17108ab48fd5f8f45c669eccf30612e
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:04:56 +0000

    downstairs side shopping set mixture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area(radius):
+        return 3.14 * radius * radius
