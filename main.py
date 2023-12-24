938f2af89206a3d02f4c4926ab80e4e100b4f2f3
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:41:22 +0000

    damage feed matter common contact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
