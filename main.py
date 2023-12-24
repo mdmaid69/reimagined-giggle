36bf78983599a5cf6c874bfde7644a72307c1cd7
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:33:52 +0000

    bread owe block church measure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
