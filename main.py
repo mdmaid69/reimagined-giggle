af8fb18b9f9ba96cde649c318ab98f4d9b6826ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:33:43 +0000

    sink snow might sit course

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
