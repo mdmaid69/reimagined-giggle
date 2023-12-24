841495e0cf85d03e4668f5f735211ba26e374d65
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:01:02 +0000

    convincing tough improvement half note

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
