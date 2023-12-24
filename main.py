9cc89dd60d3accafde1b1cca041a203f6411b37c
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:05:28 +0000

    past provide left study Qq

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import platform
+def get_os_info():
+        return platform.uname()
