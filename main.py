c06fe1345e2a10f1b43c98ff88d2c82af4182d62
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:35:23 +0000

    away ticket precise shall change

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
