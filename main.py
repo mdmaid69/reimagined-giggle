2ec110ffa10c552f4ec182bf1287dd9a99a1ed85
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:22:58 +0000

    calmly love wish wash talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def divide_numbers(x, y):
+        return x / y
