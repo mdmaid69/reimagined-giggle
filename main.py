defb812321a977677c864b646d09414c82f51f90
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:20:34 +0000

    keep point wide that range

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import collections
+def create_user_dict():
+        return collections.UserDict()
