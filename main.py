e4d0d59fae00165ceb32beae974026f4adc848ce
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:09:01 +0000

    formal supply tense takeover talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
