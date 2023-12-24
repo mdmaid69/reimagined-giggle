ae8459f738c91c8aaeeb1c6ed7a470730b0ee80a
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:56:52 +0000

    slowly as up fit find

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
