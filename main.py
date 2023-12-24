92d762a407865c3cff59e63d656c09b79b82474a
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:39:17 +0000

    across car image then how

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def square_number(x):
+        return x**2
+import array
+def get_array_as_repr(array):
+        return repr(array)
