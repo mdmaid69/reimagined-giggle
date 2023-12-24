0b77f8dbf2451513f29a5b13a33b332daba0f9a1
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:17:43 +0000

    last speak loud ours simple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_density(mass, volume):
+        return mass / volume
