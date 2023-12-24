30d2675c4c6e9f2fcf3fb328908832b600d1a3fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:32:24 +0000

    concentration child taste runover scene

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+print(math.pi)
