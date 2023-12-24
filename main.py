1d12c1f7ab609be0ad4ecc156a1b226d5081b588
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:02:34 +0000

    on round neither roll fix

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
