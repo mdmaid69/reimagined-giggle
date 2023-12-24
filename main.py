a1ba3aeccc4d8988b04fcf043b31f7b410cdba14
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:35:37 +0000

    row view shoot unlikely disturb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
