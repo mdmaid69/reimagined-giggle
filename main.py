c873f7ec40dad90d474c15a711e9b7472e9da1d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:15:12 +0000

    order justice kick through change

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
