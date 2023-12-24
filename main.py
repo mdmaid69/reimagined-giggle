03eea83d24e9a796edf2bef76bc39c31f4255c7f
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:57:23 +0000

    influence hit wish check performance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def remove_duplicates(lst):
+        return list(set(lst))
