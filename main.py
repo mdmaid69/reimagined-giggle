9c062bff4d725480a12076153560efec7a6c1e21
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:08:17 +0000

    chin height different breakup task

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def find_max(lst):
+        return max(lst)
+import array
+def clear_array(array):
+        array *= 0
