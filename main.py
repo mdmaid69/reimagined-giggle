9978b6556d2ed5549292562cd1bb6c5d99bd8a82
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:14:16 +0000

    performance gather damage badly time

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
 def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
