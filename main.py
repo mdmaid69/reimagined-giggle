857b6faebb25d5d336e79c0febcef00d30c97d20
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:55:34 +0000

    now they shut protection sad

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
