4107b67639578fa12c898a24716abc8e64bb514b
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:48:28 +0000

    drink narrow hungry street area

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
