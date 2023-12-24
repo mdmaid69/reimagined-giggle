7d44e0dd94de057e9b129a611d013af99614de7c
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:09:08 +0000

    sad food store content rob

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+for i in range(10): print(i)
