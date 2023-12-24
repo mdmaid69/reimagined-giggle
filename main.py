56e5d775b3956114f57e86ceb9a832fcec760bfb
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:15:05 +0000

    himself famous involve as Ll

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
 import math
 def calculate_logarithm_base_e(x):
         return math.log(x)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
