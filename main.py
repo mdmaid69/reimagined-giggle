cd9eda89c3cd9ac4cbce89ba183a6eaa6fff0c9b
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:50:02 +0000

    border turnon within greeting answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def reverse_string(s):
-        return s[::-1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
