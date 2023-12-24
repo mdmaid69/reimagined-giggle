6efc99a3e11fe8e9a9cfceba8df051e2c972adc2
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:10:41 +0000

    content bet pull drop section

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
