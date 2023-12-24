6add6716bd75bde7d9ebfc137a8b051ff3fe234b
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:24:03 +0000

    grand usual actually with trick

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
