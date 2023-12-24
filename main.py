bca37a6f17997d5b7edec79035ed2d8812a84a39
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:05:43 +0000

    eager tired drop ear scheme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import datetime
+print(datetime.datetime.now())
