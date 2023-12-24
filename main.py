f2095c2fd04245ff5665b6c649108501b81283de
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:03:21 +0000

    disapprove wave turnout imagination stupid

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import getpass
+def get_username():
+        return getpass.getuser()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
