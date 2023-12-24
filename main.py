cda5893797c4c80cf82168a4c65699efc68d1d4f
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:25:49 +0000

    poison behave lookafter thumb this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_speed(distance, time):
+        return distance / time
