b57ad9fef22d1ca4ce49ce7974506e925e57d40c
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:25:51 +0000

    shout furniture neither bill temperature

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-print(math.pi)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
