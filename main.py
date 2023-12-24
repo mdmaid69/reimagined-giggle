bb1dc5203b6561a855ff119b36c2fd2eb47d3a6e
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:39:01 +0000

    round right before equal sky

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
