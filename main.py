89442a4b07abb999cc237a1135d1eb803b49de75
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:43:49 +0000

    ride tall house copy Ss

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
