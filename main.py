5382331deefec1e86e3fa431e6a938622591eb1d
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:39:22 +0000

    mixture bunch possibility politics task

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
