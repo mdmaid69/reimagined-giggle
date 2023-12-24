199bfd83ff18b354eaa42779a6c0242be11d2a78
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:59:02 +0000

    comein rich convenient difficult clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
