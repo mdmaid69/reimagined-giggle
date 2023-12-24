9e7828337531652cf2afdcb50e5a0fc3282b17d8
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:02:41 +0000

    179bite cap opinion press command

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
