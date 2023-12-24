7ebd9b3260e1f8c4815f1502d9c1ee9d058efd25
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:04:33 +0000

    money solve steep cut win

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
