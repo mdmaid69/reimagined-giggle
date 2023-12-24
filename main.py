fc15846a0719ac073a817779a1c5818d5e4a461b
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:13:59 +0000

    closely talkinto pour chance convenient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
