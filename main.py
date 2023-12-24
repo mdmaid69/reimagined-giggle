02b9891bb36011c9684021a589d693266c75c369
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:24:54 +0000

    temper desperate doctor obvious keepfrom

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
