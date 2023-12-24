8ea085669c08114f45345262bf7752433da97be8
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:38:16 +0000

    dream sense independent frightened software

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_permissions(file_name):
+  def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
