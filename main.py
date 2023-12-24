f08ee666706df40a7be9c9c46bd617548909022e
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:47:47 +0000

    tradition brave with imagine bridge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
