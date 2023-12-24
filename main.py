b452806af3dbf890839a675c4d84cc9e8c189d21
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:40:46 +0000

    always improve public crash home

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import json
+print(json.dumps({"name": "John", "age": 30}))
