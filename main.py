40f56ed43c87124aaa9ba65f8cc3b7e784006f2d
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:14:17 +0000

    track fly lost trip sense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_density(mass, volume):
+        return mass / volume
