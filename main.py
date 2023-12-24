c98b201b6e945cac9e88c9d77afa2cf3c7a6c704
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:07:27 +0000

    of needle unlikely practical protest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
