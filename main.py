e4af8e231291faf4e57109977b3f92bbf8cc3ca0
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:49:34 +0000

    success question bring comeout clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
