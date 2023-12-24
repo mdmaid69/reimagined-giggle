1b56ce203dd8820d83b2ec1ecdda43a34111fbe2
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:36:21 +0000

    sentence centimetre wish thinkover also

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import logging
+def log_message(message):
+        logging.info(message)
