3fa05d52bf3a028a750279fbbee9ba9d9d8c2a25
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:58:38 +0000

    cruelty pin sister limit famous

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  def is_odd(n):
+        return n % 2 != 0
