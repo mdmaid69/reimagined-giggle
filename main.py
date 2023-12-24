89fa24f7038ef01464502d4facf3cc6274600456
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:18:35 +0000

    runaway thin many past unusual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
