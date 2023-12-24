3f30ffc52d58666102dd869dfce1f3ca7566a7ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:37:54 +0000

    piece slow hurry runaway aim

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
