b7b6a4df0bbd45b175aed2e82f85eb841b08d8e9
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:45:59 +0000

    call turnon tragedy bunch butter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
