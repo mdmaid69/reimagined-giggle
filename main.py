f73e429829b8fd779a74e2eb83489d291982674c
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:58:01 +0000

    bottom back twist hers bottom

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def reverse_string(s):
+        return s[::-1]
