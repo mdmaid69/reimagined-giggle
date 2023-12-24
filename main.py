afc7a9f6e5995ff1480328e7e2f8e9f8637104b4
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:57:39 +0000

    communication various whole alone think

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+name = "Python"
+print("Hello,", name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
