01562a314c6f152c703e48037b40814221e4540d
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:34:55 +0000

    taste accidental warm start among

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
