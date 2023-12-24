c3edf39a44889edaad11c995b256220d9bd3b3f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:14:38 +0000

    Itself OKorokay sticky deserve shadow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
