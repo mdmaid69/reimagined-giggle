bf5103d8ac2573bfec976daab8b338e14142bdd5
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:58:12 +0000

    nothing equal importance term honest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
