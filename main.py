b2497f9227db17b952545e4bf2df0dc93634fe6c
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:06:37 +0000

    platform steer print after sharp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def count_elements(lst):
-        return len(lst)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_stack():
+        return collections.deque()
