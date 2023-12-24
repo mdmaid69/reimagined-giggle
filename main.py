b1e204dc92400f5d599b761fcbcf0838de93dde8
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:24:36 +0000

    call contain occasional aware break

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import re
+print(re.match("h.*o", "hello world"))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
