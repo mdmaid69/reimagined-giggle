189156be22b7a9eddeb0f6e0cd8677d2522619bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:25:47 +0000

    stir doubt shake hold company

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
