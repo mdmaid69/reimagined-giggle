9f48c41a5d8f7617aef0465b768e075ca7a12b13
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:11:01 +0000

    name splash complaint limit computer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
