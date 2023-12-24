cd04dbe0ed821e4af311129945658a1c6f507ef4
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:33:23 +0000

    advantage programme towards time ashamed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
