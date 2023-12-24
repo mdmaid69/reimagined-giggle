63996ca6e47757147c07e92bba9b042eaee3dba2
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:42:56 +0000

    free date smoke young wave

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def square_number(x):
+        return x**2
