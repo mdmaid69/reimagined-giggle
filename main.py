cafb9ad17a7df2a030171561f6ac53646f1419f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:52:31 +0000

    hard dry rush bet rather

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_area_circle(r):
+        return 3.14 * r**2
