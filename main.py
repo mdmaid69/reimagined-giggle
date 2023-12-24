b104f649f2329a01bf1dc5ccca1cebc4abc447d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:34:32 +0000

    call friendly clever calloff feel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
