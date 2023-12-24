ce94b444967b69d7fab7cb94301012e9fc566640
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:07:32 +0000

    566end cut apply platform getoff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+def reverse_list(lst):
+        return lst[::-1]
