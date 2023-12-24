027f9e0f1a0046173eb445dc25cd11cd59a5f975
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:04:02 +0000

    smell trip worth beneath clever

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
