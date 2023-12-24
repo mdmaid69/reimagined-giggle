f63da7016722caef01014f65fffda22ed9f234a7
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:27:47 +0000

    meaning move earn awake about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
