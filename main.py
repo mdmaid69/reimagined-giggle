6355cd7576b50b36511cb6a30d3a2c17a7b64aba
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:05:26 +0000

    protest doubt improve centre important

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
