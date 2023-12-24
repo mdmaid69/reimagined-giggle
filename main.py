d3de90940fc9673e52cdd01bc12bf30f13eee841
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:29:20 +0000

    Zz instead ilovedicks willing play

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
