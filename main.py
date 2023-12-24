635bee9c07d9789c620605443b446f88698c1fce
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:35:58 +0000

    use scene room greeting spot

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
