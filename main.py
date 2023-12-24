af71b4a598d5f7ecb754a71deb7374c5429f01af
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:26:30 +0000

    debt crisis yours sport habit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  def sort_list(lst):
-        return sorted(lst)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
