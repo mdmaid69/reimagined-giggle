d261c2f92f58fed0b7290e01aa460687bf5468a2
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:53:51 +0000

    compare sister mend school short

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_unique_words(sentence):
+        return set(sentence.split())
+import json
+def convert_to_json(data):
+        return json.dumps(data)
