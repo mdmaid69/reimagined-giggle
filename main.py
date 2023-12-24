ed43bc042f768ac6777d269a936398fb1361a7cb
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:55:45 +0000

    makeup common poison burn unite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-print("Hello, world!")
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
