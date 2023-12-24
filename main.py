83ede199bedb0717ed15fd8d72a809122977be7f
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:38:00 +0000

    market will thankyou regret listen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
