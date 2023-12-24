bead3c0aa6223d78cbb14a37d13acb016a1c77dd
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:29:24 +0000

    past young effect patience wherever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
