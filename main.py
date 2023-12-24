11df002e4cdaa7e21b950de6a101e7ec2ea2b9c0
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:42:29 +0000

    injury breathe failure awake dig

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+print("Hello, world!")
