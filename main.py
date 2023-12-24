5d89c866c2f0b0779104bf8f0002eb3319a4b425
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:45:58 +0000

    before should accordingto so healthy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_as_float(array):
+        return float(array[0])
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
