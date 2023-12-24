8e4a64911e11dec144dfbc7d6e9b4c064e5d5d93
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:18:29 +0000

    shoot bang trade hard disappear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_power(work, time):
+        return work / time
