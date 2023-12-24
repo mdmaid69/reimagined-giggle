c834f383d809e39bdaa065db7e60948ccc68a81f
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:59:47 +0000

    experiment fit condition private poor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_power(work, time):
-        return work / time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
