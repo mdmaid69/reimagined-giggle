341e6b1f83cb6fb93c20dd9c463a92434c7de17d
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:41:55 +0000

    year police minor test catch

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import datetime
+print(datetime.datetime.now())
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
