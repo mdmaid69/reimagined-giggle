576c99bee351b95ffc5346f966f2871cb42ec1f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:24:08 +0000

    search keen solve connection sea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def count_words(sentence):
+        return len(sentence.split())
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
