5b31824dcb016804c5b8dc6f83932d9b3b67db5c
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:16:34 +0000

    substance work calmly related half

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
