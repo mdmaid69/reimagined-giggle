06a845575021ae09b04a77a6ca0c4ff899ba031f
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:00:56 +0000

    their damage further guilty judge

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
