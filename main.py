c2f23e9a68028ecc5806421121ce53272c29b9b9
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:15:36 +0000

    fight school used circle family

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+x = 10
+y = 20
+print("Sum:", x + y)
