3aa70a62a83550959af16de8ba1bf7f87d29a047
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:08:35 +0000

    edge good important impossible she

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def reverse_list(lst):
+        return lst[::-1]
