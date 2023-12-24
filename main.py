ef54d78fd08109b91eaaf7fcb109cb672acc348c
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:42:11 +0000

    careless once play embarrassed strong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
