9bbe0eb6fac0b018fe86f410369b337747956589
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:25:52 +0000

    keepup unpleasant excited chin weak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def find_max(numbers):
+        return max(numbers)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
