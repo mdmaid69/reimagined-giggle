d3092a7d0003ff19fb3f517f0473fbb6e31248d2
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:01:30 +0000

    side outside track history hide

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def square_number(x):
-        return x**2
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
