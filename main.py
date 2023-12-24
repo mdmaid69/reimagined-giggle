7c8d7b751eb95d6e8fcc4a9fe121493d96767157
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:42:12 +0000

    connection class interesting suit justice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def split_path(path):
+        return os.path.split(path)
