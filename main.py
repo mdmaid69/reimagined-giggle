6333394ba27514638d142c410735f9a0ab147dec
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:47:59 +0000

    unite keepoff far wander pleased

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
