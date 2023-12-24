3eb0605dbb6f4471ae9a37686f7dd14beb6d61be
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:22:30 +0000

    interview compare gas giveout start

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
