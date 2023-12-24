ec00443033df38727e6d234008d68745d411c6f1
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:09:59 +0000

    inquire recovery probably only Cc

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+print("Hello, world!")
+def find_max(numbers):
+        return max(numbers)
