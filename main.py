1b25023ff2d63413ef5a8c10ad95283e977dcc52
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:05:23 +0000

    press bus apply progress future

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
