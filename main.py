9dd34aff4467f8d45565e9a9d172d7defb38b39b
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:43:47 +0000

    wrap treat fix callup healthy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  def square_number(x):
+        return x**2
