fc695b5d11c8149d47eaefd62836fd64ae1ac089
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:43:56 +0000

    ordinary slip comeround date wild

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+i = 0
+while i < 5:
+        print(i)
+        i += 1
