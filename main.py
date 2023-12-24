a048f44b47261ce753a3e1a5b7b441cd30ad23b4
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:01:09 +0000

    Oo other that come bunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import array
+def get_array_itemsize(array):
+        return array.itemsize
