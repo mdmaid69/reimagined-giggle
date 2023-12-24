c8ef43fa32c3b141cc33b9e4930563c8302ec1d5
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:47:31 +0000

    interview row skirt expression begin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def reverse_string(s):
+        return s[::-1]
