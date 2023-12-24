0149f287ac05a591228094d6405f72da2556f79f
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:53:22 +0000

    past production dance cheek consult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
