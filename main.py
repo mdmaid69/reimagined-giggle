b329b326cae4680bb28a2ab7448bd7b6b26efb02
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:09:47 +0000

    certainly careless cost sound happen

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
