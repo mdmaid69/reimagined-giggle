c11f022c7dc61ab9fc9f0020971de7f7666c2014
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:36:02 +0000

    charge lose protection exam may

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
