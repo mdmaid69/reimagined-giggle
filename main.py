30fe5a37e1fc218b27f5a7f11be6f952757ef0a0
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:49:24 +0000

    calloff period always substance disgusting

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
