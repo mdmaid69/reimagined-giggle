81fadcee8e3b3eee83aaf70cd686d4c9715a2a04
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:27:00 +0000

    Zz so callback seem discovery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
