2e4f613bbedbf7f349f454befe22a5337d626ab4
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:34:01 +0000

    wise birth partner extra create

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+for i in range(10): print(i)
