a7596a6a283cdc34af345d6fd5f4a67029fe3b9a
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:50:13 +0000

    himself used miss eye brave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-  def sort_list(lst):
-        return sorted(lst)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
