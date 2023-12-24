b1aaac96b0d61b689eec7077311a107fea5df9d3
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:54:06 +0000

    pair they somewhere science Yy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
