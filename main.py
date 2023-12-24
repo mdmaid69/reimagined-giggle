7b9fef7b3422d1d4ccb5be98928451a1335f99ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:22:19 +0000

    into nothing collection both she

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
