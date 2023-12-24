ca60c6ab2d05e7c63f3880f5beb6fb418638df91
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:59:19 +0000

    absolutely spring do condition clue

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_speed(distance, time):
+        return distance / time
