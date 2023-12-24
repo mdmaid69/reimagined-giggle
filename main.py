62b3890916caf66bfc9c09c04b31a0e57cf994f3
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:35:53 +0000

    concentrate loan leg repair achievement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def convert_to_binary(n):
+        return bin(n)
