590c663287d03f12fe4b52bbb86ec68066efcb45
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:39:42 +0000

    will backwards active statement remark

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
