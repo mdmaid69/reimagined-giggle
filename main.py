a253f51ec76ac6042d79736475d9148d7a06d51a
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:18:53 +0000

    well study better delight hour

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def reverse_string(s):
-        return s[::-1]
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
