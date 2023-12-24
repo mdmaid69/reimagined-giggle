76a4bf1a638f54b2d299f4b486d6f4fdaf0d5e1e
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:52:29 +0000

    mean offence between object take

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
