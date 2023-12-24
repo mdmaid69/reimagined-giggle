3e734262bc603f715d329a171b21743a7210d60a
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:07:09 +0000

    alive term since plant immediate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
