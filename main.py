d0d2b9f93a552644f51feb4bec7d83b8de85140b
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:22:38 +0000

    between pump mind storm row

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
