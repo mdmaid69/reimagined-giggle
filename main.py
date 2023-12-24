db11dd5891c5d3afdf56dfc84f459bca6b0443e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:12:20 +0000

    morning fun fortunate note season

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
