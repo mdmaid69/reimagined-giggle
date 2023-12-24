9f8ace21e29dc6d3e61acad1b64e6b2550375085
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:11:36 +0000

    train state young platform vision

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
