2c7479ee5cf60f48083362bd5ef8cc42f6169c2e
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:06:33 +0000

    put principle determined careless survive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
