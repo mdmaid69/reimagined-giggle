4a60e2b5a9e5d85affec8541f0d260083684b51d
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:58:24 +0000

    material destroy quit issue teach

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_priority_queue():
+        return collections.deque()
