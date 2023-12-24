e484c7288b10e6a5f4ad5d35fa828cb04b1e5925
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:52:46 +0000

    boil 567enemy sense bringdown except

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def convert_to_octal(n):
+        return oct(n)
