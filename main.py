60696c3a78d5b65d54abc236e4550652b8911d24
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:44:20 +0000

    help passenger against learn climb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import os
+print(os.getcwd())
+import sys
+def exit_program():
+        sys.exit()
