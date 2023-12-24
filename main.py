b8047021029a5fa0bb8871b619a4dcda5524f2b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:00:00 +0000

    theirs goback(to) once slightly loan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
