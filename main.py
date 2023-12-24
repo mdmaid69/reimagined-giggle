b4ab06a7040d14f352bc7437d531915ceacf0029
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:37:33 +0000

    cruel main brave turnover wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
