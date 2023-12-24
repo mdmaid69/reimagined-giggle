097d50ee6aa72ee9b640ac8020d649b4061d8b54
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:58:22 +0000

    abroad giveback patience probably unkind

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
