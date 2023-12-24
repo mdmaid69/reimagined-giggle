38d8c2650ba949346e8a05c215df70459b0fe3fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:51:20 +0000

    line behave blame official shame

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
