ac2ebdfbf170280efb29cc1a0f31bcfdd07c06b1
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:23:17 +0000

    circumstance other adjective lock such

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def is_odd(n):
-        return n % 2 != 0
+def calculate_power(work, time):
+        return work / time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
