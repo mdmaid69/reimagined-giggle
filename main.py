36e5cd300c87e4a689cbf942b568c0d17fbcdacc
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:02:02 +0000

    cinema firm such organization object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
