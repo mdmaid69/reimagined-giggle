c02022ee12676d5bfbcc83d763569d825ad222e7
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:20:27 +0000

    head contain difficult strike offer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
