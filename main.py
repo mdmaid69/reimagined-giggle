cfdced8f6758a440077c0f3323f744ecea8e2670
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:11:04 +0000

    closed fold emotion pause good

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
