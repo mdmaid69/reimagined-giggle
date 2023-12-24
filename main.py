73f26c0dd2fe7ff8bcc02c6919caaf364b9ae00c
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:01:11 +0000

    hers properly still excuse bringup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def clear_array(array):
+        array *= 0
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
