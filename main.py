0a807602ee855303b02b6c6df01880f9ac332331
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:30:36 +0000

    exist promise excuse Gg produce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
