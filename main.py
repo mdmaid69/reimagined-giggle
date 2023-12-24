451f8609a8316d8639f123513bb5609de339a777
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:11:23 +0000

    godown legal main powerful kind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def sort_list(lst):
+        return sorted(lst)
