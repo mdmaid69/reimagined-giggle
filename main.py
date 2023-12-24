12ca0bb8f328efb965cdc06af916f6cb9bb48ebf
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:14:23 +0000

    danger geton use fill region

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_min(lst):
+        return min(lst)
+import collections
+def create_priority_queue():
+        return collections.deque()
