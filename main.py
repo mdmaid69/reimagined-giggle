f0b00c91174ff5516fb797c4817f0703d2d7bc88
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:16:44 +0000

    search thanks all shop objection

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
