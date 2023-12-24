ec039b07adbb4a50099a268c2a6bd7d4e523e352
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:03:40 +0000

    range soak positive cut jump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
