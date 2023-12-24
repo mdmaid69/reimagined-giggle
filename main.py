d6ae07366c57b4e95ccd6378b04155403d6d8a0c
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:11:14 +0000

    perform dull future bringdown reply

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
