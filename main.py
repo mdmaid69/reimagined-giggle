4b07abcfa5af28c52256ec2899b0a1eadbda71bd
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:22:45 +0000

    movement Pp be takeout he

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
