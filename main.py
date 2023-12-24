94f27943f6c9f75cad6cad38ec5167ce196718cf
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:28:16 +0000

    class rare takeover kind blame

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
