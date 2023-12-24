3c918cb8784eb7557ddd04031949ff19950a7642
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:11:38 +0000

    convenient ring plastic ashamed chief

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def convert_to_octal(n):
+        return oct(n)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
