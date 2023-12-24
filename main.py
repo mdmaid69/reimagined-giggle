ac871c2f7046ce6c3d4e0e8d39701f6645877f61
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:00:08 +0000

    action range can criticize depend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
