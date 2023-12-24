c2d44f7fef3640e7aabddaf34558292a73ad3383
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:20:48 +0000

    nearly over grade blue win

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
