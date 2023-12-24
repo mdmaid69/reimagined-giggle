f5745f1e4cec997bad6e585eb6d32f05475f1294
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:23:30 +0000

    even poison carry estimate content

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def set_array_item(array, i, item):
+        array[i] = item
