b6b9230a7c40b97085a01559aad49c9e639fea20
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:38:26 +0000

    smell excuse importance transport free

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
