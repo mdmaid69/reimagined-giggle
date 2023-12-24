d69e02d099e3b006c02d8103e168f0c66826554c
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:59:18 +0000

    guess repeat lock head account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
