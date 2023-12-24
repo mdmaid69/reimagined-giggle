fe7e94afaae0b0e52c0f2d179d4db0245d3b0d78
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:38:32 +0000

    even pair stage workon certainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
