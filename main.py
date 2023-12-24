cc939d18629f148c0d5f40a5a0cf02aa001c29d3
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:13:36 +0000

    answer Uu load ourselves appeal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
