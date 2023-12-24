82d142677abd1339bfcaed5b62a378610383ebf7
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:01:17 +0000

    container market return expression positive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
