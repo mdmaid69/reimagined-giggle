6ecfcfab8cae32a32f7b140ee3a63ebb94cde949
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:27:27 +0000

    reliable night drop better distance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
 import collections
-def create_user_string():
-        return collections.UserString()
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
