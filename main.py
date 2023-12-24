80f37beca13fb3c6c1637c926acd0e219deabf8f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:33:12 +0000

    ever up used phone discussion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
