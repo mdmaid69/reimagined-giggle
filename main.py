03014801f3a06d14ac22884fcf56c33569a0a222
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:54:58 +0000

    into sleep wear short tap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def multiply_numbers(x, y):
+def multiply_numbers(x, y):
         return x * y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
