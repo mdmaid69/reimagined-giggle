261ca3e79497234fe094a6557c7ba25322b0d4ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:42:00 +0000

    matter between valuable happen tidy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_max(lst):
+        return max(lst)
+def find_max(lst):
+        return max(lst)
