aff702373f8b08ded85325a8515c167eeaf1fdc8
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:31:10 +0000

    scream process born attention quit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
