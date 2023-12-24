2aa2c65317787cc10b5314437e00914dd8598100
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:57:51 +0000

    long regional close busy eat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-for i in range(10): print(i)
+def find_max(lst):
+        return max(lst)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
