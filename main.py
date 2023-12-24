97ae819af31a142346e400dd4aacbaa23cdbb8da
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:22:45 +0000

    warn behind burst keepup react

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
