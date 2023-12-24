59316553c1682d165a02f1351b64f971581189ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:40:12 +0000

    never partner urgent town respect

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def multiply_numbers(x, y):
-        return x * y
-for i in range(5):
-        print(i)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
