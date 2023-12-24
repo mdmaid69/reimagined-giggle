94de5ce03d68a04b4d108e8f922bc58784e485f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:41:55 +0000

    plate model leave contrast pick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def exit_program():
+        sys.exit()
