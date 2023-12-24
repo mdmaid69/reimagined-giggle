dc2504af9ef304239e46632a6003331b4145e456
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:02:29 +0000

    suggest rush exactly row accordingto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def split_path(path):
+        return os.path.split(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
