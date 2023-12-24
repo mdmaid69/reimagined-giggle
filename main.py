cf1d862bd28a6e027bf2b42705233cd87e7b1225
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:28:21 +0000

    office pollution bottom male important

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import sys
-def print_python_version():
-        return sys.version
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def reverse_array(array):
+        array.reverse()
