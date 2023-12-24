533ac880aa2112b32ce35b675d395f366811fedc
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:38:29 +0000

    noisy central perfect direct flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
