4f9143c75bdf4c4f2be7265ad339eb1946976d1c
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:50:02 +0000

    healthy work translate brush personal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-print("Hello, world!")
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+text = "Hello, world!"
+print("Words:", len(text.split()))
