de67b0dd25be3ec42c15f25a796efc772a9bb7f1
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:45:17 +0000

    cover damage back peaceful capital

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
