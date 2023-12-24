d50dcf6adb0e3bf8dc7a44bc911a916db907300c
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:16:26 +0000

    cope accommodation poison software bottom

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-from collections import Counter
-print(Counter("hello world"))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
