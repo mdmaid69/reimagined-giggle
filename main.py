b91cb76196e36d5a8ca2855871e39c44261d41d3
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:54:44 +0000

    which thing breakfast left usual

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import os
+print(os.getcwd())
