d1a1600b5fdd41c505dec7806ed8fad87198dfd0
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:57:41 +0000

    woman steam public go breakout

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_max(lst):
-        return max(lst)
-def multiply_numbers(x, y):
-        return x * y
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def sort_numbers(numbers):
+        return sorted(numbers)
