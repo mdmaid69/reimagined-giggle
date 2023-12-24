72d45d9c19beda17124dbc65b8b38e0c20321529
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:31:41 +0000

    relationship impatient runover pressure hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
