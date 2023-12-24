8268f6da3c1d26dec6c83e4c1f06b49e56acff48
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:52:33 +0000

    record public skin under manner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
