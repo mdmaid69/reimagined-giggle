1d6ef0f13ae388d8e8c2da1413a474e34158c4bb
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:50:55 +0000

    gift clean wash goaway human

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
