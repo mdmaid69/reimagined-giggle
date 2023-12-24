a9f7691afb688cf5edf8af86be35a01adceec89e
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:10:13 +0000

    wall dance injury will keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import platform
-def get_os_info():
-        return platform.uname()
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
