f0193849782991f127ce37c90bb165462a5f76b3
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:06:53 +0000

    equal address inside properly there

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
