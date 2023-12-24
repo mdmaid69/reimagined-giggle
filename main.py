3039dc76c7331e11cff3a51c98f05f970aaceca6
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:41:15 +0000

    medium where breakthrough argue getout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
