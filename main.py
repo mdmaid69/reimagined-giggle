01cc2e812467f399bb281c63cc8a8e6d151a3cb5
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:27:04 +0000

    hide keepup rarely refuse season

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
