44790175ae55312edb42cdc542fe83be54373625
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:36:40 +0000

    agree if beneath battery joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import getpass
-def get_username():
-        return getpass.getuser()
+def count_words(sentence):
+        return len(sentence.split())
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
