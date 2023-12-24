ca6fd7fed6b3c622a6f1ec21a5d43afaf7dfa521
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:58:38 +0000

    unless command talkinto pupil past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
