617e676cbeb2faebd5e128dc947c0013aa0b2bf8
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:47:17 +0000

    meeting replace send fit commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import platform
-def get_python_version():
-        return platform.python_version()
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import sys
+print(sys.version)
