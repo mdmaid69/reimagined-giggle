3f44556d618b4bbd10fdc556afe22b574de6a6df
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:31:14 +0000

    contact hang correct tear protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
