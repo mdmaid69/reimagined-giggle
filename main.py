c8eb5393108d0c2fc6579ace21065c1fbd13831c
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:02:29 +0000

    cool fear 179bite light beat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def reverse_list(lst):
+        return lst[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
