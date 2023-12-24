fa844fa1e8c4ef93e1685783f2c9a469d164bd19
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:23:41 +0000

    marry great better none record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
