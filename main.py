a0e6d5e0a16f51304d3b2d6223059ee7024533a8
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:51:46 +0000

    everything comment glad alive instead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
