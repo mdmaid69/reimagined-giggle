fe542912dbd1fa78cdfcbe558d648b1d50e6e03f
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:12:14 +0000

    traffic wet precise plastic sometimes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_circle_area(radius):
+        return math.pi * radius**2
