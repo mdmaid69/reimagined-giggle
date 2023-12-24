fb9f6513d658394c9635a143205e2656a4cca541
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:24:50 +0000

    phone hold decision ready roll

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
