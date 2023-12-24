9aa8c24326e4bfea3b7567ac9b84a3f49a16b956
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:22:34 +0000

    bored arrange sense attractive bin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
