ca209e32b103c57762c20ec876aba83d8a73caa2
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:09:32 +0000

    comfortable welcome snow field degree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
