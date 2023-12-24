b8834cdc08599c045f78804b2155201fa626b5d6
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:53:47 +0000

    by blame attract pocket mystery

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
