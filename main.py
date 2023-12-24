41bddfecc494387bada79246ffc6d0664ccc6724
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:31:09 +0000

    moment admit beginning still nonsense

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
