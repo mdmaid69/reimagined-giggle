e6544546f621aed483fbd34fde22b87c4f42199b
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:09:59 +0000

    cum choose advise sail material

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_pressure(force, area):
-        return force / area
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
