8c475e9ecffd6d65b01017d45dd02c2e10ced355
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:26:37 +0000

    thick small fine tip past

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
