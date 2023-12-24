42b95750413ae9d86b044e822bbda09d34d8b058
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:42:06 +0000

    instant march eat behind while

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
