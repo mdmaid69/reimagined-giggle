9690bf8e6cd99b4b643a657eef44c8eb205e3b55
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:51:40 +0000

    message home hospital ocean church

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
