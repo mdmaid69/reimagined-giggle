a4ad21f4cc5adb49e52800de8e85408e544f0485
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:45:10 +0000

    joke ring delight keepup favourite

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
