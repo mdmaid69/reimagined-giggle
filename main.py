fa3479a0fb16cd79a2f2fd3ffc3a6eedfc7f34da
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:31:55 +0000

    suggest prize hold whether bye

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
