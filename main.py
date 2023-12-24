f025dffd654f65e659b120850df8c9cd1cb66594
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:41:19 +0000

    burst pocket set maximum less

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def count_characters(sentence):
-        return len(sentence)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
