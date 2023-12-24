def971d4fa753c3dccc50faa4bb1dbd8cef582eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:54:45 +0000

    fire single solve value takeback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-for i in range(10): print(i)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
