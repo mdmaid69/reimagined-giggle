ab86da3e828ce40c35cef8f60f5aca4dd069f8d6
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:27:28 +0000

    fence as runaway punish deal

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import getpass
-def get_username():
-        return getpass.getuser()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
