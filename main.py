f2195dd8721379514c6c583ce0eaa84097ae9b39
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:52:58 +0000

    pool Ww second car central

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-for i in range(10): print(i)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
