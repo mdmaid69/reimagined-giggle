a954fc4d8749dd4928931c3de6812418fd799516
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:10:43 +0000

    software side their cheap vote

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def find_min(numbers):
+        return min(numbers)
+def calculate_area_rectangle(l, w):
+        return l * w
