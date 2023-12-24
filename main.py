68647bcc1adf728f036cb391f18c7569edd5fa20
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:51:56 +0000

    building sticky thinkover hate please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import time
+def get_time_since_epoch():
+        return time.time()
