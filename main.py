87be4ee182df049d80b4aa474182ea19df64edf3
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:21:52 +0000

    pretty space shade common shoot

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
