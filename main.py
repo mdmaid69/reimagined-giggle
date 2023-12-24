b2916836f63182030a71611949438013346b7797
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:13:21 +0000

    reliable deep 567enemy tie meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
