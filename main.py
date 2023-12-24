f524c7700cd229cb6e0ba42838e373c7d5004be8
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:19:28 +0000

    anyone lunch hollow top city

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
