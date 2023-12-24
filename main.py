f651a6cac86716973f3c46a3358637c2bacab2e5
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:33:00 +0000

    rush scale muscle pretend letter

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def convert_to_hex(n):
+        return hex(n)
