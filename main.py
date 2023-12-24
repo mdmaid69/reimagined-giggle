dc6f9cb92f0d6af130c3fddbbb96a64731914db6
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:22:14 +0000

    drive talkoutof shock all worst

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
