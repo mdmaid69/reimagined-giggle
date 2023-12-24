cff513d735d9d44beafaad46f76ca97283333b87
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:33:00 +0000

    again food one scream performance

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-for i in range(10): print(i)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
