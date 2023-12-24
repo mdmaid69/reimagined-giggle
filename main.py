d70d0caee2bcb48651a820e059789ec91ee089a1
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:47:34 +0000

    description general write apparently firm

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
