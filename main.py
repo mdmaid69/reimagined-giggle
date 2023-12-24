46767dcc4b9a3943655d66d419f6451e689c5c2d
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:03:13 +0000

    payment bound holiday petrol distant

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
