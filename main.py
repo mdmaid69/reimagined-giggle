137f133d076e659347eee6e316e2e3b5e655a5ba
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:07:13 +0000

    brush criticize impressed language discovery

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_power(work, time):
-        return work / time
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
