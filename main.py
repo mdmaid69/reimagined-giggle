a4b685bef130cec60f70de0c90a18e2f0f9ba518
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:25:55 +0000

    direct goon flow left necessary

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
