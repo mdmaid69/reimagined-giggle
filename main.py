f337c8dbf9f929b882e20e871d6fd64f636520f9
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:11:59 +0000

    cost always future turnout guilty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
