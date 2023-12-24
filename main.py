ae7902c776bdc9b0e82a96000cf106159828fc4e
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:26:14 +0000

    them recommend negative bag around

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
