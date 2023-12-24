ab552c7d4e68c69e08568824f891d81f98cdd743
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:31:04 +0000

    anything born content steal in

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
