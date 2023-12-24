9c5d8d52ce8966a2f8d762cd95c21d0ebe20654d
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:44:09 +0000

    matter criticism drop married before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
