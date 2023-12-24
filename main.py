559d5000b69ec5eba958a02923b90ab1a1340573
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:55:05 +0000

    intend strong bend Oo opposite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
