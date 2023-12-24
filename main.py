46de00a6cfb3508ed7bc1eb4509a976ea19c92ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:54:03 +0000

    need everything your turninto complain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
