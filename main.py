f03b01e5d82fa07136b99d0b17394e52add95119
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:33:35 +0000

    hang surprised sweet uponon connect

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
 import array
-def get_list_from_array(array):
-        return array.tolist()
-def square_number(x):
-        return x**2
+def get_array_as_bytes(array):
+        return bytes(array)
