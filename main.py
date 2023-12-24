76445ac2114499de3c7e1e0bab27ea90c5fc4fc3
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:19:50 +0000

    afraid lookback(on) display mark only

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
