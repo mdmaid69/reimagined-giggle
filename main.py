12bbac1b2d73c656aec4c9be66c78e807e88b5ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:04:46 +0000

    mark declare experiment Yy figure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_memoryview(array):
+        return memoryview(array)
