c3b66206af60b504bd47560ec281d8f1186a0177
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:12:22 +0000

    sound strength fault suck centimetre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import datetime
-def get_current_date():
-        return datetime.date.today()
