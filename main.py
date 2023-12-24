ba50aa38c74b68d62e1858394e5f6ef00200fd2c
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:24:19 +0000

    find hear accordingto anything talkinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
