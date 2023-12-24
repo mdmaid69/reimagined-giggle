6c2f97f7f94877517463b2870f597b0c6dfef09c
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:34:44 +0000

    and detail high hour know

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_list(array):
+        return array.tolist()
