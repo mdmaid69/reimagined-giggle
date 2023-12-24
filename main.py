e8ea510e22d0a67cefdf0d1ea6717e7931777829
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:45:45 +0000

    innocent swing near worried measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
