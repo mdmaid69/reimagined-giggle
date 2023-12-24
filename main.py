ca14d817628032a0723833c4adbfaf56e8e18d4e
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:36:58 +0000

    find over show sign Dd

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def is_odd(n):
+        return n % 2 != 0
