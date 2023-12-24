d166f4160b3e8bf0b3a67b2f27ba9447ea624746
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:10:06 +0000

    release clean bringback scream loud

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-for i in range(10): print(i)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
