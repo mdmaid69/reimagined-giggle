fc04a9ee47361f88f2f078489d79890bbb69ce5b
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:46:00 +0000

    embarrassed possibility mine move recent

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import numpy as np
-print(np.array([1, 2, 3]))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def convert_array_to_list(array):
+        return array.tolist()
