1b131a03610e011643914aa10f5ecb830555d76a
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:33:04 +0000

    unkind deliberately meal phone in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
