574a6c9cafb576f3aeb70607f02351b46d8c5266
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:10:08 +0000

    just slightly squash order rub

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
