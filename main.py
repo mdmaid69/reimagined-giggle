85abfcf82b897c85b7ee34ce5ff2cde39a6e9cd1
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:18:31 +0000

    someone even doctor present knock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
