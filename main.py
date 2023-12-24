e71a01f5aad56ca6dd4caf699890482160d8216c
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:49:21 +0000

    anyone scared area fold underneath

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
