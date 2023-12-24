7c87c8df1e08c627657c399e1f97bcfab21e6e79
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:56:40 +0000

    hold stay loss touch reason

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
