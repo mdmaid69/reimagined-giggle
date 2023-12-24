f819a3898b6d04aed69ef331e832897d4623eff4
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:54:45 +0000

    too lead attempt fence 564encourage

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
 import time
-print(time.time())
+def get_current_time():
+        return time.time()
