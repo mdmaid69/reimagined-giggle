3f30a2a1524eb57c7d3c69ce98da2a8e1eaa78a8
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:44:02 +0000

    his represent illness accept sky

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
