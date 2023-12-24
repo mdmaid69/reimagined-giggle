57346ff3a9988afcc35d6f573fb9d72e7126116e
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:18:24 +0000

    fence gas compare say copy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def reverse_list(lst):
+        return lst[::-1]
