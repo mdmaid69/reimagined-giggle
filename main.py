5504b35fbef1fe83a2f1b4c80f2d975715005e6f
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:42:18 +0000

    able 179bite exciting healthy post

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
