1bb30bc39fbaa1a45c80fc0f11bc2a67202c0a43
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:26:57 +0000

    hair family race holdout hope

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
