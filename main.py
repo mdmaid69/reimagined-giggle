75bbfe1f5c6bd320ffc1a0d62f3777f7c53c689e
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:43:49 +0000

    angry scale side muscle accordingto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
