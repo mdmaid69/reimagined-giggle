13688dc6f894f72e94c1a1cc4fd3d03e954260ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:29:27 +0000

    tired pick giveback interview refuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
