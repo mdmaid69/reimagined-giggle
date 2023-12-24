d9dc9e5518e2b0d7a26e7cb43211ebf421f7374e
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:12:04 +0000

    whole that importance usually send

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import datetime
+def get_current_date():
+        return datetime.date.today()
