e1ef6c591746076727f3afd277304b6814da83e6
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:42:56 +0000

    aware accident Nn government whistle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
