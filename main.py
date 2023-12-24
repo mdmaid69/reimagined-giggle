1a6f7bdb22ded6d9635fc2fb0b857727c3ea1edb
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:51:06 +0000

    size name drive character science

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
