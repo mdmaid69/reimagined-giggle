63886949d7fa4fda331a67f27287e3f3e6207d89
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:09:34 +0000

    single cum central most himself

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_work(force, distance):
+        return force * distance
