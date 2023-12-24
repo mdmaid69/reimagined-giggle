c7a5e485c596fcf320393460b96f375faa6e3d89
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:52:40 +0000

    unhappy holdup interview side better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
