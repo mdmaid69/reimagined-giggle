5891894346d19a5634225e5006e0f2229880ec9a
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:00:01 +0000

    active distant snow somehow past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
