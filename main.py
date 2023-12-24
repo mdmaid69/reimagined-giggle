2be7c8b5b3cff2c59137dff7388d4a60c23bd51c
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:36:25 +0000

    steady measure breakin unkind only

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+name = "Python"
+print("Hello,", name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
