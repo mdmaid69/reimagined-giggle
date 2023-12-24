329fcd2483fe963c791953d1b19890778c536aa7
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:41:49 +0000

    dark what abroad trust careless

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  def convert_to_binary(n):
-        return bin(n)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import math
+def calculate_exponential(x):
+        return math.exp(x)
