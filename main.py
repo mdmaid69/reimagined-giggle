f1abc77461faa92a8531c3e0b56a2aa1bb36fa00
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:46:27 +0000

    hungry physical mess male hour

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
