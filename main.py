ffb89dc72df81565d4ca7baafe763fb5f1facff5
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:50:49 +0000

    claim attempt mood fail joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
