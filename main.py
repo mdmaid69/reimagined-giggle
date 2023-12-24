740c3574fc508d6a4b8eb4310a86cbdc4035755f
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:04:20 +0000

    inquire guess thing comeround rub

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def is_odd(n):
-        return n % 2 != 0
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
