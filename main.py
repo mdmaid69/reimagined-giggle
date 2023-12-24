ef8700dc3db3313c6d7908c95e0085c22a385a75
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:24:01 +0000

    turnon very possession eye nurse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
