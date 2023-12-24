731b120ea1d3548d7f2b823df274d36796e158d5
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:17:29 +0000

    hour limit escape half delight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
