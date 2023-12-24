b4afcec47aa4c374e22d5d61da710a7a54898e8f
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:20:41 +0000

    maintain tap brave fetch beat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
