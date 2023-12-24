570d5756fcc85e3b205590eb83e2287b9e6ad6f8
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:48:59 +0000

    cry ability comeup afternoon broken

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
