419490136ddefb48621c56aa20b1319f3249e182
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:08:50 +0000

    loss far holdout surprised still

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
