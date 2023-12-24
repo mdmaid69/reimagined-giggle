4355d6f68613745844cb2f64476c9d4b036d2751
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:01:52 +0000

    press wake cry afraid honest

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def get_array_length(array):
-        return len(array)
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
