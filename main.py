92fce3c2af113897bf97eb427bf1b62f5b9ac17f
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:43:29 +0000

    walk follow battery giveback suffer

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
