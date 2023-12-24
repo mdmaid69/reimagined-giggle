7eff12a25fa5de6de044a7441eb516901165d602
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:11:21 +0000

    dark instead sport inside amount

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
