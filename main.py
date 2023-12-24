d7be467b79dd5b4e5391344486e6c245e7ac0c50
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:13:29 +0000

    chat pleased war Uu failure

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
