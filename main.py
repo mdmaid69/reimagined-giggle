8535d7485da2c75993aa50a225b21a7fe8e4db03
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:48:19 +0000

    hide control be connection fresh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
