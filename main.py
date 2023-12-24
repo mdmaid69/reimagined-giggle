be0f8250606b6ff42d8a3d38926864c161896fef
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:43:04 +0000

    us fail frame stick payment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-print("Hello, world!")
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
