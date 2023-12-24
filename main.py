1299fc0fecaed10ed7dafff6029c49cea99041f8
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:41:40 +0000

    perform tone powerful tip oil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def clear_array(array):
+        array *= 0
