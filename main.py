eefa458cbae7d9db7c818577579854dad35aee9b
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:37:34 +0000

    claim without name swing mine

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
