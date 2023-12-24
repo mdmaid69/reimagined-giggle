b7aa9dbc1b293f3173d7fdd433e306bec427de71
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:44:36 +0000

    welcome 567enemy suggestion heart appeal

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def subtract_numbers(x, y):
+        return x - y
