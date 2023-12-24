87afa7e60fff833c2ffb6ec5042774da3b2aeef4
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:46:02 +0000

    except holdout gradually circle vanish

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_int(array):
+        return int(array[0])
