cf54dab2b429c2ed575aaec6188a370e93a7d2f3
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:40:00 +0000

    himself Rr produce geton fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
