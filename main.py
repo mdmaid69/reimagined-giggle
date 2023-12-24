fdd7fb22bca15d20ea012d6a8c928c980d5d22d6
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:43:40 +0000

    either to nor step blind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
