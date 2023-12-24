bf2f013c31faa7b299b3381f0fdc2dc8954a5e4c
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:06:37 +0000

    building certain art independent enthusiastic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 def calculate_amortization(principal, rate, time):
         return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
