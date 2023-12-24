f89eac7ba72fbee64e7d7fa784b895d16be20cfa
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:25:54 +0000

    side now mass passenger actually

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
