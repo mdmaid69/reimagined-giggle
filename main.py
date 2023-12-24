82ced2a08650c58beed963a63c639c430bc4f0c0
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:40:11 +0000

    recent disapprove lead practical smooth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_density(mass, volume):
-        return mass / volume
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
