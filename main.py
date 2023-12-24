aea6a07598d6ad6955f295fe74fb1da439a9fc38
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:51:39 +0000

    ill lot admit benefit shall

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
 def calculate_present_value(future_value, rate, time):
         return future_value / (1 + rate)**time
