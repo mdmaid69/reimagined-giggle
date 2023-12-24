a7806c0dd70fb87aafb1776fd1090f2e85bbe0b9
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:18:32 +0000

    can sell run flavour track

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import collections
+def create_queue():
+        return collections.deque()
