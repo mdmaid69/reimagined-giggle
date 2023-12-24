3c1adb70f7d6db5b4c74d7ed4f3c18a40898b351
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:28:51 +0000

    remember idea clean interview poor

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
