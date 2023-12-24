3d2954bb596263f5e84e5e16cfbe7ba95e3c3604
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:34:32 +0000

    takeoff national out report so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
