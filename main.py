fbc4d41135c5724813f7d44d739e2b334fbe65af
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:01:34 +0000

    back turninto hit exploration high

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
