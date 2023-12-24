57ab27a2e62605918ba4b63cdc926b9f601366eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:23:59 +0000

    your include pinch suffer maximum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
