7c05c69c04724a3b079d9b22fcc5934f3ef15bb1
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:06:39 +0000

    best account fun summer at

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
