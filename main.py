c7ffbcaaf67d2f9ded0f949f75047b675ed1cd69
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:44:01 +0000

    better original father lonely statement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
