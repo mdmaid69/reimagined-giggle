a696e91f24dcd3b7146814632a4d14285c319646
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:58:39 +0000

    recognize order light cost increase

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def multiply_numbers(x, y):
+        return x * y
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
