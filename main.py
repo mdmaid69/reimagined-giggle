8902a398d52a9f480d842f9577dfe22db823a253
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:54:08 +0000

    pride gap debt children accept

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def get_array_as_frozenset(array):
+        return frozenset(array)
