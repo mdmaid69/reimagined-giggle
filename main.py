d04d0548a1af44f7f0da83ceb479dc39c1556515
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:16:09 +0000

    shrink getback pressure rub temper

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
