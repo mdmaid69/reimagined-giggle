4545c00e041d4d97b1560418a63d9f6b98eae4de
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:09:41 +0000

    search hang progress wing prize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import getpass
+def get_username():
+        return getpass.getuser()
