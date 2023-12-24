2f598a1f128b1c681c822e86eaf87911e7b55704
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:14:07 +0000

    safe approval peaceful famous relation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import datetime
-print(datetime.datetime.now())
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
