6404c0c3af5322bdb0ad0632462baabb2d6c8f58
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:48:37 +0000

    pour seem attitude joke table

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
