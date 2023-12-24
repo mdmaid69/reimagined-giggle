fc10bdb3354c095ab85bff4b8fdc46508625653e
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:46:18 +0000

    nowhere difference convinced file taste

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-print("Hello, world!")
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
