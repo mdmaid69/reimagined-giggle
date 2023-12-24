f645088e0ac0a53d3f3aeb25f8ac5969eebb4481
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:51:31 +0000

    land ocean wall happen unhappy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_density(mass, volume):
+        return mass / volume
