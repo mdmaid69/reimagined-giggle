8ce82877890e559482266406e0ddbacee39302e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:11:17 +0000

    takeover city lose point stage

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
