ca2e5241438a30395942de546bb4b0caad206280
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:01:04 +0000

    damp movement exist comein blue

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
