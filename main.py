f110b9282e7aea5f0e39c03e510375589cffd9e0
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:37:29 +0000

    nowhere laugh safe occasional weather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
