7515b041777d2aa513df1b0e0f99e276fc44f006
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:52:49 +0000

    minimum age doctor meaning rise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def convert_to_hex(n):
+        return hex(n)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
