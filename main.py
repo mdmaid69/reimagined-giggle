5f6ea159227bdcac84188d74d3f874eea2ef805e
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:51:53 +0000

    angry push immediate Ww measure

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
