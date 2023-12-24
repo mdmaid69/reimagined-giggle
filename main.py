b8206d2c931777c776e0a3a1759597c18e6cfd3f
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:55:24 +0000

    notice scream communicate spite base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_as_bool(array):
+        return bool(array)
