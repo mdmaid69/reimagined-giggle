2ef42059a5a82d1cfac4bd1e3fce44cbf4ccb490
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:45:01 +0000

    love dinner search foreign interest

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
