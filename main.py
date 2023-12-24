f8ef733a30d968f31151ab0bcfe4383ea26d8594
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:05:35 +0000

    remember rate risk however tradition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
