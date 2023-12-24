401350f518d1a4e9b851d2105ae0d447e867c785
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:01:52 +0000

    print respect broad stop careless

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
