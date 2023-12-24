ec41cd9c5907afdc4bbe6763fd05f78f9b2dd280
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:53:13 +0000

    opposite finish run similar material

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
