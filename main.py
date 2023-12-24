7c0648ec65289523980516c84b95145fae084b61
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:06:28 +0000

    many study likely tend fact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def convert_to_octal(n):
+        return oct(n)
+import os
+def change_working_directory(path):
+        os.chdir(path)
