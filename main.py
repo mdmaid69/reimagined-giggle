a481f3d9ec3a713685ad6026a63dc62ae7e3d803
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:31:57 +0000

    poor refuse accident fast if

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
