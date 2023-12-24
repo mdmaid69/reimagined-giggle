22b0decbb7ea0c81cfdac7f3f41a0e13871862c3
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:22:44 +0000

    pool hard promise press complaint

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def is_odd(n):
+        return n % 2 != 0
