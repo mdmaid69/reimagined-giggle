155af9ce36e8efe90abc3cb46bc3c711dbb3fa3f
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:02:02 +0000

    familiar season storm light at

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
