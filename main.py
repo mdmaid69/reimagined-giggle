c7cdfd58473cbdd9d1c7b1052c8f0c8e54bab2c4
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:59:06 +0000

    let smile history demand nowhere

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import os
-print(os.getcwd())
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def reverse_array(array):
+        array.reverse()
