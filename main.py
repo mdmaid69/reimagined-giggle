eef033d3ed95dc7844216a023c76ef311698692d
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:43:16 +0000

    thinkover stupid rain famous water

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
