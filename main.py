584bae72d5eb4221a429536134c0ea0666b004a0
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:45:36 +0000

    attach advanced frightened spread decide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
