7fdc8b3bfd0ed57e3a414f6dc2990db59e2bf121
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:49:29 +0000

    fit spend accommodation cruel turninto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
