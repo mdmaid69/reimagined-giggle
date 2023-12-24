1d9fa69727ac2658f0e728da900f7b8cd0a8a49d
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:12:30 +0000

    concerned sign country muscle wherever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def multiply_numbers(x, y):
+        return x * y
