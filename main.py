a18ac62fb0f69e1b9fadd1489d52cae55b2a0a91
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:03:11 +0000

    spill shock strict stretch reserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
