9013931513adfb6101d38e48dcc30cca0e12252d
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:50:26 +0000

    defeat unlikely post takeaway shine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-print([x**2 for x in range(10)])
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
