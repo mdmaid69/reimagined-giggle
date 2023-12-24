222eaeb2ee90f7d1218a30cefa358ca309eab8a8
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:56:18 +0000

    holiday criticize talk shoulder authority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
