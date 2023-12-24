8b2f23a753778766fd30a003d5374e97bad2d101
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:38:54 +0000

    worst tomorrow original suffer sing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+name = "Python"
+print("Hello,", name)
