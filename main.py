ad7316446493591d31860e61d6d68fd32b1768ff
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:58:59 +0000

    bend half road environment goon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
