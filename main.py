baebeb05ff7f6e8142e1a26ce5622f457b5d3283
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:04:13 +0000

    contact no breakfast play love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
