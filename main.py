033e8e81a749414cdcc3ca15087967c92bea346e
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:58:24 +0000

    sleep push interview confidence getoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
