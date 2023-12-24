40f28abf01b5ed42cace486b37aec7babf6e272d
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:28:08 +0000

    warm appear warm match better

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import os
+def remove_directory(path):
+        os.rmdir(path)
