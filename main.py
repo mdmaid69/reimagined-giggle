800ee7767fa1869ff11eaa8ca5beeb968b44711d
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:39:37 +0000

    cap accordingto whisper welcome deep

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def square_number(x):
-        return x**2
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
