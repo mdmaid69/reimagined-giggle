eab382de8b70aacc506ce4281f49e24f6e9401e8
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:00:23 +0000

    since pause spare above sleep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_power(work, time):
+        return work / time
