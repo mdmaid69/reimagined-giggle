5c7f6a001b7733f04a446dcc04852acac2c37f79
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:32:59 +0000

    march 568energy emphasize strong hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
