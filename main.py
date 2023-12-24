66e934dda37501ac8413c0a103426e9d9f718fce
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:25:03 +0000

    taste occasion bottom display statement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
