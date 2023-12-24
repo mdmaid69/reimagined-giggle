afe9ebcb6e8b0df3d86096fa38445eb4642e34a2
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:07:15 +0000

    right rest children scare common

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
