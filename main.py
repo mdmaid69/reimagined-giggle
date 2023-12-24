44cfd20416cd61ea9485c00c6cb7b09e2b297499
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:05:20 +0000

    along breakout inform office squash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
