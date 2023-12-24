108ff2fa40d8d5484af630acb527e1b4b0af06c2
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:48:31 +0000

    568energy 572enter convenient closely imagination

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def find_min(numbers):
-        return min(numbers)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
