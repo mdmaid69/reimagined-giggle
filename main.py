25ae646190aeb7291693940bce91288b392d2309
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:19:21 +0000

    opposite anywayanyhow art comeround party

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  def is_odd(n):
+        return n % 2 != 0
