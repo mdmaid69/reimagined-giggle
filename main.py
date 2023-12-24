a7d0ce0126e84ed9fd5579b59fc57b5669c8d008
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:04:11 +0000

    harm production space present problem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
 import heapq
 def push_pop_heap(heap, item):
         return heapq.heappushpop(heap, item)
