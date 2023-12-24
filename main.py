769478c048d5c7ede27673b5c3bbf93a07cf54df
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:34:46 +0000

    quantity where design rare right

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def find_min(numbers):
+        return min(numbers)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
