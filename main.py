975fc5257bb16b56da8f855cb446ac70fbfbbb77
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:49:07 +0000

    actually murder over balance complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
