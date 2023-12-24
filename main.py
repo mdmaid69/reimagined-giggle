304a5fb82041ec9e2ec61e7eac1452d556c80c24
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:28:07 +0000

    sink suggestion smooth admire correct

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import os
-print(os.getcwd())
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def find_min(lst):
+        return min(lst)
