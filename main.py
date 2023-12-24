951543984c09c54b4e302ad31cde6a192544aba9
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:02:54 +0000

    freedom impatient to slow scale

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
