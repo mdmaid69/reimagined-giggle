ecd8fdafa8b94325d0382bf9a7f93524c43f6d19
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:19:37 +0000

    than occasional demonstrate borrow murder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
