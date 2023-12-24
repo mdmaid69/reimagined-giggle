3606f7b318019403199c16402068bf1ed99a1656
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:45:48 +0000

    team behave lunch goon condition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
