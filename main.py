7156a5a2b59840362ff96d7ecdd2d74ee46781b9
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:48:01 +0000

    telephone trust sign hurt book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
