379e9cb116141318846c7bc2dfcdcfd4e5859465
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:19:33 +0000

    male its sail reject hers

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_power(work, time):
+        return work / time
