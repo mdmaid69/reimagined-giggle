d38bf36d39db698ca82adbf203ff48c840f86f4e
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:51:48 +0000

    off remark want so goon

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_area_circle(r):
+        return 3.14 * r**2
