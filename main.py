fc01992ed8287e403d87d63a2ba7a47b75930741
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:41:32 +0000

    spend gather win search aware

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
