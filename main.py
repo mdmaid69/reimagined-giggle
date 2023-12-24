e2d6fcd40feef4abbf6e42c684ee3a38acf9061d
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:20:06 +0000

    plan like he brush reliable

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import sys
+  def get_python_version():
+        return sys.version
