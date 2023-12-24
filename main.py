3ca8b76d95824a7eba53a9e6caea98320580b18b
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:19:39 +0000

    nerve patient rubbish gather substance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import platform
-def get_os_info():
-        return platform.uname()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
