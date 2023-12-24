f0d170f4af98694daac6168b5bf6377f77f49acf
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:18:22 +0000

    in generous punishment gradually traffic

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  def cube_number(x):
+        return x**3
