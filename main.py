89ab48fc1e5c2f971f287024798ff1f3bbc43965
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:34:09 +0000

    afford just grateful level tune

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
