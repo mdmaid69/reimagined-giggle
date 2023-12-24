e84c5fbaf917046bcf45b59ffc8555abfc42f262
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:29:36 +0000

    talk compete discovery goaway buy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+name = "Python"
+print("Hello,", name)
