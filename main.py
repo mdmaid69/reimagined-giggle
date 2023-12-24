67487aef75cb7978013b4c7b93302bec836964c8
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:49:20 +0000

    family visit bar show bend

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_min(lst):
-        return min(lst)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
