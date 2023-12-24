11e2d196a5fba489fd31bf4027e59458c5c13b18
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:25:23 +0000

    bottom scrap system turnaway meanwhile

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
