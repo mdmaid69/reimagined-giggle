4223ca39007ce2c6e758a75368535ccc274d0045
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:24:17 +0000

    keepout knowledge ill under meet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
