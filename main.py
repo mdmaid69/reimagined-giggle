85eb3f55c45e1d4c43504ee8d758c36eb623b85c
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:03:51 +0000

    train ours cook how murder

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-def divide_numbers(x, y):
-        return x / y
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
