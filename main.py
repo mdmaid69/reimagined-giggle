841036f98c0406c972118164bb25ae622a56069b
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:35:50 +0000

    further boring takeoff peaceful contrast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
