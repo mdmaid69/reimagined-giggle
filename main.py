fc19b8c690fe5a90fb5f7cef6d8a3b5d5cb12ec4
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:21:24 +0000

    choice thank owe bet damage

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
