74c8ed8330786dbcae6594f30a7955aabc7a236b
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:08:50 +0000

    murder drive side during might

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
