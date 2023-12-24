9321d35cdd4ec4cbfb9fb336b7af2b496fd878bb
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:37:23 +0000

    face relationship cut from empty

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def square_number(x):
+        return x**2
