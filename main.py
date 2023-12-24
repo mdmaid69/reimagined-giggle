ab232a11ac527478e3a750c067b266333967bf58
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:07:26 +0000

    frightened makeup city repeat fine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
