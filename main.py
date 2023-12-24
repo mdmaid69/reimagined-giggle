dd2d9116f9fe23802514570bf071e369c21f5e30
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:26:40 +0000

    talk only slow material game

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
