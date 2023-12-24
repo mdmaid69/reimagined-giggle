45acfacbb150526550c20484e807ffdb69930c2f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:22:41 +0000

    rain unable force paper favourite

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
