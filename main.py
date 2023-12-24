5b891aa5fdb1b1872d6ea4d5f93bbe6ba965ef2b
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:28:40 +0000

    face surprised hope crazy board

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
