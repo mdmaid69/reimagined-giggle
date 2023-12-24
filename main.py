2fd2a603ae53a4168f92ca17654e34f3d89a28d1
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:59:27 +0000

    565end weekend final risk drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
