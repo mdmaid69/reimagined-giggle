a443e4f486cfd8772d9ba87afba2970f0b1ec68d
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:23:30 +0000

    load warning Cc criticism must

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
