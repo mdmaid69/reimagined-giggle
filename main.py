9aba5a9f36bdb9353308e6d8ff2764951809b702
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:46:38 +0000

    cost holiday refer politics keepout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def cube_number(x):
+        return x**3
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
