1fdd81bfd1db0d0003028b3b26c1ee2bf30b1421
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:07:38 +0000

    raw market rich admit keepfrom

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def change_working_directory(path):
+        os.chdir(path)
