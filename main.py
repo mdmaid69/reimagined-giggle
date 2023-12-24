eab94832d939da567dc6e4bcf0f4d5f9ad49e826
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:44:09 +0000

    cap combine trip young theirs

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
