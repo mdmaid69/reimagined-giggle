ced75c854f9f02974cdf9415527f433b871bf91e
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:32:54 +0000

    promise friend performance breakfast Jj

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
