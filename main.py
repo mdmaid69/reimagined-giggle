425656409a3d4c32d08c5dd043d609190b050c78
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:59:12 +0000

    set mind excited this confuse

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
