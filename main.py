ad9ec347218e5acb0a1610b9a89274304d259d3c
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:56:33 +0000

    correct scrape walk legal usually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import numpy as np
+print(np.array([1, 2, 3]))
