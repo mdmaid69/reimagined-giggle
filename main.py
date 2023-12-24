3c10177833d54247e525570379e5f051f6bc5b97
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:33:42 +0000

    trap early pinch tidy thanks

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def clear_array(array):
+        array *= 0
