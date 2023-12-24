cc2eb4822ef4a7e14370b10a93ebfa0bdab2231a
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:49:05 +0000

    giveaway able bringforward figure complete

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def convert_to_octal(n):
+        return oct(n)
