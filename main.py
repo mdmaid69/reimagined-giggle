b7f08bce77932fc04a03f2a4949b80abd4272d55
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:54:25 +0000

    modern event can lunch pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
