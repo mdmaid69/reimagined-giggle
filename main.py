1839249cf8bf738709a6675d20f43c795fc2e5f1
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:49:26 +0000

    responsibility refuse reach unfortunate well

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print([x**2 for x in range(10)])
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+print("Hello, world!")
