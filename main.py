8fef598796f049dc990b2bd9648faae57c9d61c8
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:18:03 +0000

    ask too exploration imagination out

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
