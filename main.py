e8d34780283a7b404d5353ef1521befbc81ce023
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:54:36 +0000

    row stamp lot where practical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
