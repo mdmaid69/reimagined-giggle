a2e4d90e5decd932f78d1986a9890e2dc2ac6ed2
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:49:10 +0000

    target 565end lonely bye fact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
