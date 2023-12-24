873b73f0f699a8a3fadcdf4b91cfcf4fadb4fea1
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:12:19 +0000

    getback regard callout possibly pronunciation

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
