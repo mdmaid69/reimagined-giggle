72f448a54772997f2e1f796dfc6b3f8ad5243ec0
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:49:43 +0000

    recovery emotion teach building band

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def add_numbers(a, b):
+        return a + b
