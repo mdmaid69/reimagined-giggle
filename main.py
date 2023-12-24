66ad6404ea9fd567da78c226c94fc1c010b9b9da
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:04:09 +0000

    talkoutof dance score giveup either

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def is_palindrome(s):
-        return s == s[::-1]
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
