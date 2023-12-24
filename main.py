4d932f0d8268a473e78d3e11d4e71b9341461fac
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:54:15 +0000

    pollution getback represent information there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def is_odd(n):
+        return n % 2 != 0
