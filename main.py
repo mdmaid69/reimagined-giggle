e0cdd923b3e22c5e7bfb9c3446b44f476d53288d
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:14:08 +0000

    kick myself immediately clue fierce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
