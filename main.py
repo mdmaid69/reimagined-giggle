ee6c7d466447871da31da66c1476434cc8b8b333
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:13:27 +0000

    challenge proof mail myself action

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
