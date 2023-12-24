d321de50d083982444849cc9ee99e961c60ea5e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:10:52 +0000

    record bother plenty plate kind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
