ced6093422aacae70bbe0635426dc558fb74f1b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:35:30 +0000

    well balance boil minute expect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_user_list():
-        return collections.UserList()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_union(list1, list2):
+        return set(list1) | set(list2)
