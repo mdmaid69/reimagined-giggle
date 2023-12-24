fffbabfeba2a90831f379401e8d982002e0f00e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:48:27 +0000

    road turninto cry press least

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-print(sum(range(10)))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
