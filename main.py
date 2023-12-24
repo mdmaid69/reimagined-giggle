c51c41edf01ef95987723d64dff4d70469d879b2
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:00:49 +0000

    name argument ride check finally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import platform
+def get_os_info():
+        return platform.uname()
