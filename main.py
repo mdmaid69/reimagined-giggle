c897c9b52b42f0d731423b6ee5e136aa5def4c75
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:40:31 +0000

    frame government before interesting official

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def multiply_numbers(x, y):
-        return x * y
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
