7a73fc3d020cc35e1bc9c55366343199132671d7
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:42:50 +0000

    common thick love behave respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
