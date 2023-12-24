951844ff5b5b99de1c3a23be8b905ec9ecafd46f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:10:35 +0000

    geton relief basic future eager

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
