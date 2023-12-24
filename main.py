4ca749443ba882eaea2fd09964b717bc235f76bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:34:26 +0000

    refer separate pinch side either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
