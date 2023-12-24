cdd4f6282484be5ccb988fe11a6475a084240f8c
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:00:34 +0000

    stage prevent basis final guide

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import os
-def remove_directory(path):
-        os.rmdir(path)
+print("Hello, world!")
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
