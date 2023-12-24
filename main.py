1df726e0132fad46e56ffb30d5e6b9f410bee260
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:09:13 +0000

    constantly send listenfor call ahead

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
