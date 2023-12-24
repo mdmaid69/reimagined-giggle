972700997b073d8ca215eb482b285b90e41cc1dc
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:17:37 +0000

    hide tear disgusting offend order

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_area(radius):
+        return 3.14 * radius * radius
