dc31ad14d90849a1052195b47c90bdf5136aaba5
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:06:25 +0000

    future only breakdown compete there

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-name = "Python"
-print("Hello,", name)
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
