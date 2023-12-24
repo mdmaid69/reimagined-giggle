a911a7b86fdf44986172d0c7c4fc46395e7cd602
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:31:56 +0000

    sport spring star progress rough

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
