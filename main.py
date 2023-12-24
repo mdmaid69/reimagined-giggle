e622fcf5cb3ae1ff81ee1837fa78ea1a61e51ee2
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:19:05 +0000

    fall pretty pool keepfrom business

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
