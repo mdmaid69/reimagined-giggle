c93f725c55ba46aa12e964d56c3c06f6667a7dfb
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:02:37 +0000

    doubt rich load cross natural

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
