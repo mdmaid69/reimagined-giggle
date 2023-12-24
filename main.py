a0a7f89c96b326e9b75bb43668b6ed1b0b919916
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:20:59 +0000

    quick callat awake priority watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
