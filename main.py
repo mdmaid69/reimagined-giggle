6424c2b2224405ff556b5ef89d85ec53ec2b1f6c
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:24:32 +0000

    object word connect length generous

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def cube_number(x):
-        return x**3
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
