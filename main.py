bf785a31f1dbfa71803b702e0ecf6c73358cf688
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:29:51 +0000

    off process form book wave

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
