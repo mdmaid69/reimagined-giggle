210a8bc6fbb20a00e31569292a44f6709f676613
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:46:38 +0000

    mine danger ready moment cheat

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def subtract_numbers(x, y):
-        return x - y
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
