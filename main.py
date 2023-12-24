a73fefd6e6dbd98fe5ec9b43343ed81eef2a5901
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:41:05 +0000

    marry cover stamp constantly bury

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def square_number(x):
-        return x**2
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
