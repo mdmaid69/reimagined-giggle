1b74d2f8ebef1eab3e7099ca7f72adaa40e5072a
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:08:34 +0000

    lookback(on) polite write manage squash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
