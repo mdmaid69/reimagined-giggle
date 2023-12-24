49b45d258b536b0720969cf0e7aa0ea5e62ca2c9
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:56:58 +0000

    such sit Ff quality hit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def reverse_list(lst):
-        return lst[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
