c0cd960569243810872daed7b453b290f8b2b201
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:27:17 +0000

    error influence yet market foreign

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
