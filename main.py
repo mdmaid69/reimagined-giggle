07237c4306be9cb816392e1e322b3ec1c14e1146
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:00:04 +0000

    meeting depend upstairs beginning bringback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
