6a2b3793d6d38f56fae72d07278300c379db490f
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:37:36 +0000

    callup left famous long record

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import re
+print(re.match("h.*o", "hello world"))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
