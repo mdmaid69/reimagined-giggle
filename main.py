3c3cf913d2255fe69ebc2ab1f5381de3b7e5c619
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:34:13 +0000

    negative over test frequent late

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
