b7ab7a498588122c92364604e64561fbfdd9cccf
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:14:25 +0000

    load try telephone takeover without

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
