2795cc2045b63ffcf90c0600d2ff759da25c19dd
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:13:40 +0000

    male instruction row secret interruption

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
