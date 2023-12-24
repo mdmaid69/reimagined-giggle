876a4ad07b41e4b61a5426c88cac0e2ea1a7ce5e
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:20:51 +0000

    grade rise male glass friend

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
