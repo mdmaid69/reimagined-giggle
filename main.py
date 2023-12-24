6fd87187b70814325f103cec9ef18e21d623381f
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:03:56 +0000

    anywayanyhow change concentration family precise

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
