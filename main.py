5cb86cb6ad58cde4210f77a95651623e5786543e
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:51:12 +0000

    full Yy mean terrible breakin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
