d655b1cd078faa8a74ff95aba686830a6ea7f30e
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:26:01 +0000

    passage culture fierce action holdon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def convert_to_hex(n):
+        return hex(n)
