bcdbfe5e15c9fc23da2eb21798ae430158da2903
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:03:45 +0000

    friend build underneath ball number

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
