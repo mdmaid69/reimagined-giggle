ff16073e9175c0daf5a0a08bef5e61b2bcca811e
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:14:27 +0000

    error guilty tip disappear pot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
