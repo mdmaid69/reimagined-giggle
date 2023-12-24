982ccbe9a1b995af7b62c69c6ea83ee2982a8cd1
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:53:01 +0000

    prepare succeed public appearance blow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def convert_to_octal(n):
+        return oct(n)
