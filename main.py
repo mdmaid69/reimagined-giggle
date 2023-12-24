258a3da9c66afcf37bdfc7d328b33b9cc29b81c0
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:15:41 +0000

    fail Yy discussion formal cause

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_list(array):
+        return list(array)
