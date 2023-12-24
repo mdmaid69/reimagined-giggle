4fd3a74863449d1cc68243d31acf4009cf1fe0ba
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:30:51 +0000

    direction reaction attitude drop smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
