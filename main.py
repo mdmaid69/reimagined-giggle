f55d72d52b081807434fd7fdf36d22764a8af79e
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:12:28 +0000

    busy mind peace earth expression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
