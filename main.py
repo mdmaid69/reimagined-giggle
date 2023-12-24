2a9b9d9c6f3a82cf0cae40c8255e2c1fcdf0a7e6
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:30:22 +0000

    hers turnon lower tired pile

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-def convert_to_hex(n):
-        return hex(n)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
