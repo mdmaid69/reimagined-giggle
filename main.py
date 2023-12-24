a3bd773b8fef894d733cf1a81b27efe52bbfac6b
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:40:33 +0000

    goup genuine community over adjective

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
