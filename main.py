86205c0e95a624735fbe0ba613bfcb9ea5cc9827
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:41:37 +0000

    open your cost splash generous

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+print("Hello, world!")
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
