1f59d23704eca16e816a34c69cc4ef615591fa0e
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:52:25 +0000

    die duty unable live report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
