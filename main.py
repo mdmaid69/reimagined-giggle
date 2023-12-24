6361e4716b603cc09e588c6df36cbc4235ce3978
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:08:46 +0000

    state appear gooff popular fold

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+print([x**2 for x in range(10)])
