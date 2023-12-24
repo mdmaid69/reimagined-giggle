b6b070213bd2a420aa5aa8afb1c639b317180b76
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:01:00 +0000

    false comein ill underneath name

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_as_tuple(array):
+        return tuple(array)
