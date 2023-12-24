c9be920732e5370a9bdc59bb97b0c51b44647e63
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:29:10 +0000

    right term look comeoff suggestion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
