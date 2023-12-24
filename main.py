34d0476627ac8dba61b8d7ca91ab808cb8c6ebed
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:43:08 +0000

    embarrassing rid official off meet

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_power(work, time):
+        return work / time
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
