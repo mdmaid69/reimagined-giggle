8470d1bba6579abc48ac31d7c481455c50bd938f
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:44:04 +0000

    trousers hot risk shoulder allow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
