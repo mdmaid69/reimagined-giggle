69c170c7592b53a46ec0959baf92af25a8366317
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:22:30 +0000

    natural breakdown healthy family runaway

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import re
+print(re.match("h.*o", "hello world"))
+def sort_list(lst):
+        return sorted(lst)
