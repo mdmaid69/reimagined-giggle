3dfd20af6c45a31e2e50e63c0f359fce546ab5f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:14:36 +0000

    later wipe which bye tie

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
