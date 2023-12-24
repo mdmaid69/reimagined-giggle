21b717f3f9aa9e1ae1b894fd94584db941602c2a
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:03:07 +0000

    to prize maximum just loan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
