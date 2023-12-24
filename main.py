50368f761a1d5712731067103765efbe0e7b16a3
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:58:31 +0000

    advice cough convincing workout stupid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
