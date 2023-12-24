4ad415e2009638a934f5a88b789c365b57a8364b
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:02:16 +0000

    cost visit represent though rare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
