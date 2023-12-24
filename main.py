3948d1dd20230371d7c3c62d76177a078f28da94
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:38:54 +0000

    level silent plan level listenfor

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
