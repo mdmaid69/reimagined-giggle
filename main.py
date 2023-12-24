7fcc2db84b4f8be1474e340cb5feab164a2c12ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:15:22 +0000

    scream room search course concentrate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  def convert_to_binary(n):
+        return bin(n)
