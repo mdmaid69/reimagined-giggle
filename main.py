512e5227f8beec17796235448944af1ca3ed5cd3
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:08:42 +0000

    sink through until roof market

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_current_directory():
+        return os.getcwd()
