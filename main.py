0d4289994f4feb5b951634515a484cd2dba9c008
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:45:49 +0000

    help pretend sweep figure factory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
