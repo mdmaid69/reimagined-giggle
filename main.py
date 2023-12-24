01520c8aace664d2d134da7d1c67de8a0c314fdc
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:03:03 +0000

    rid used explanation female greeting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
