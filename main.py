7e948cfde884fb9f671dd1827a579ac6d7e26c2b
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:22:14 +0000

    word short takeoff month record

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
