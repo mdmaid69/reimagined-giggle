74be91de7f7be3ce6a51ef680024a58a94eaf466
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:43:22 +0000

    nothing exit judge do buy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_speed(distance, time):
-        return distance / time
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
