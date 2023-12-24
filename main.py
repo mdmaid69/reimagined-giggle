f302aa31be88bfc59f8684d80119cf2057756600
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:29:07 +0000

    lock less inform pot the

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
