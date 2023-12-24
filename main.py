097df821823266fe477e3e306ffa1b87393efd97
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:31:58 +0000

    wonder towards care front attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
