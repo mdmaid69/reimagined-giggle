82405df4b0f322995ba46dfc5c316578cc253455
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:23:12 +0000

    only name marry move should

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
