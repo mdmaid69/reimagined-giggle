831a8dc146e20f59783fb024dd84b44ea4866725
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:21:00 +0000

    chief noise want confused alone

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
