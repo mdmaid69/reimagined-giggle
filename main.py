44d687f0b5df4f439438e59231d3f226db594755
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:28:37 +0000

    sell steep help include share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
