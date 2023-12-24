4b69045e825f3964a19ccb4cb6a04a01948851e8
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:05:56 +0000

    centimetre wealth address equipment clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
