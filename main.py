c848faa822be9051108732c254e8a3f4b2864de4
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:04:31 +0000

    cross shut furniture language block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
