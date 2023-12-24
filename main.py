7c25bd5ed772ca25d201e589bc8e69045fe0cbf8
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:58:44 +0000

    for worry disappear somehow dinner

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
