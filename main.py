1284b813cc2f0ac4a216c9858f8d59c52b1aed01
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:16:45 +0000

    regular lift sweep fence university

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
