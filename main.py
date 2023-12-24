cc648ba12cd3dd2e96bc2db875ac31058ee5e65b
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:06:12 +0000

    post combination stamp control cut

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def find_max(lst):
+        return max(lst)
+def calculate_work(force, distance):
+        return force * distance
