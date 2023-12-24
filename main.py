bbb0fec3b75ada5fc63e7be96486f5d9c50e2864
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:54:10 +0000

    rest blame keepfrom perform rid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
