ab51a12a9e0ef1f23bd83cbd2c2b7ee6b4b835a8
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:07:48 +0000

    within ability cook copy boat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def subtract_numbers(x, y):
+        return x - y
