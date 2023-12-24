66a18db90ab2760eb738465664365d27db596208
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:57:41 +0000

    pretend fit immediate past away

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def count_elements(lst):
-        return len(lst)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import getpass
+def get_username():
+        return getpass.getuser()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
