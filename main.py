cec9ec57856e0ed70aa7ba7ed8eca8b7c4ef3670
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:37:35 +0000

    holdout fast warn appearance yours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
