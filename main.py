780e13c87ddef2ec982ba03602c1772813a5e20f
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:27:16 +0000

    here slightly rent turn shade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
