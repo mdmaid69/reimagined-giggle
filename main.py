489abc06eee83a8e9b58050803f94a741e19c6cd
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:26:18 +0000

    large turnup turnoff flow relation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
