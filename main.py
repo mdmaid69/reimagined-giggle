e22ebe63918cf3fb16971a38bce9122e019955a9
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:53:40 +0000

    attract deaf rush move lose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
