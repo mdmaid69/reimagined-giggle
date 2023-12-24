69ee4db6cd5c58289207d0cb474e24ada8a752b3
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:46:02 +0000

    plenty guilty worried down rob

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def convert_array_to_bytes(array):
+        return array.tobytes()
