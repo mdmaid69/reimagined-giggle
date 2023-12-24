bfca20e38dcfca6f24b9b49a4c9c8887d4bbf615
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:39:07 +0000

    stop cut pleasant town difficult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
