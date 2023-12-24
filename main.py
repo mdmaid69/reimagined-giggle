eac54c86f23d8c19fb840d350717f1813516624c
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:06:02 +0000

    accept pleasure alone comein zero

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
