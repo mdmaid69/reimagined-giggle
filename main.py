7690e084ba5feeb20035577bc080718149c1cd52
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:30:34 +0000

    comein weak feature lookaround wise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_floor(x):
-        return math.floor(x)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import array
+def get_list_from_array(array):
+        return array.tolist()
