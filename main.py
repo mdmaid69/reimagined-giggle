ac63e3b4438315efa72f1506742917567927d613
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:46:55 +0000

    plastic young regional Itself recent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
