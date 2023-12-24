a092f15d634f1f3bdd238ef7b8fe33f56c302060
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:48:11 +0000

    within cry attempt safety brief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_queue():
-        return collections.deque()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import os
+def get_current_working_directory():
+        return os.getcwd()
