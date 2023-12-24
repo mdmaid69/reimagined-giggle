514558a20955a315fc72e9bde0717918a92d3308
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:14:01 +0000

    sharp pure city slide achievement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
