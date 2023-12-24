9644dc167dd69ee9a75ac6f3fea4f45ad2097b70
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:52:29 +0000

    out treat stamp near taste

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-import array
-def reverse_array(array):
-        array.reverse()
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
