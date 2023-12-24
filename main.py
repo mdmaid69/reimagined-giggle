4cacc476198716722f31f2673f51cbd0a48014b9
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:41:05 +0000

    new young moon clean danger

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_work(force, distance):
+        return force * distance
