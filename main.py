b5fadd0306113ba031ced9453c794f1245273f70
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:46:29 +0000

    appeal sure period so reply

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
