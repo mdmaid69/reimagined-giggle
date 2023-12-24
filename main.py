d0f3fb766791febfb555628d03fd62ca6caeec79
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:46:59 +0000

    ourselves shut shut exhibition tool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def get_array_index(array, item):
+        return array.index(item)
