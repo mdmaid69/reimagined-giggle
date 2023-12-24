9ace6ad9e48acd830b09ee7659d67c06b34a7f4a
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:36:07 +0000

    occasional wrong goon pressure fall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
