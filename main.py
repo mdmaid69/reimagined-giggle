a11ec7e064363132a1e8afd28b3c08eda39d2f4b
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:12:13 +0000

    consist weigh formal destroy expect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
