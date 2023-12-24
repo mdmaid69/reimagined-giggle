e2aca42c3763b008a020f556260f9bb1bd0e1ef6
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:11:21 +0000

    house false instrument firm point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
