e11841072d25a7e00b2e67d4da572d43b8c303cc
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:40:51 +0000

    slowly fetch underneath fall model

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
