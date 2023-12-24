a912b5ac621ddd3f079b5f9f53bdb5a323edea2d
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:16:34 +0000

    Nn convincing appeal nowhere repair

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def sort_list(lst):
+        return sorted(lst)
