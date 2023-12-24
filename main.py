0d25cd11ebf7c761a4d5ca083f8d3844f6f978b5
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:32:53 +0000

    black recover warn myself high

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import matplotlib.pyplot as plt
   def plot_scatter_graph(x, y):
         plt.scatter(x, y)
