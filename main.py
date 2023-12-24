9b3de40b2638f9b613b9f68a9646566d7967cb37
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:41:51 +0000

    spill interested part comeover operation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
