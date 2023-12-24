020c5d1c447c166f45f2a245ee7c30f1c00d9537
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:05:03 +0000

    inform heavy dare machine fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
