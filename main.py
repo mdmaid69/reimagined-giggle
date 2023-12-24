18a4834af0abd8c8d405448463c4fe54d4abda46
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:30:27 +0000

    lesson agreement visit break target

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
