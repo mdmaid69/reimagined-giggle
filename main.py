de6e2d645e2cdbd18f1dddac0becab4ea7bbc403
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:48:09 +0000

    breakthrough lead birth over confidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
