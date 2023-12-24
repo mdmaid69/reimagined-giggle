30214e7c338f5ae9ecae28a063e9942451ba86f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:34:05 +0000

    deliberate suggest valuable office laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_list(array):
+        return list(array)
