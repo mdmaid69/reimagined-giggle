dbb185a40c2a5011dda3ccc32079537cd551f331
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:54:38 +0000

    interested alive I firm tear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
