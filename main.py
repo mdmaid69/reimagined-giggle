9702bccd2bcafb8b4b00971887d330a519c9da34
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:37:31 +0000

    pencil blow upset perfect software

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
