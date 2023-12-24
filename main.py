96c5d20ae057f23507cf108becc24cd67ba93302
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:44:17 +0000

    now thinkover statement talkoutof possession

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
