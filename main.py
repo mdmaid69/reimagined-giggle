8f59110855fe99e069d2d517a897f913515ff45b
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:17:29 +0000

    by would succeed fence trade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_palindrome(s):
+        return s == s[::-1]
