4d90ce5bf393d266c0bddeed0ffdf4345e97903e
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:26:05 +0000

    child water picture tidy mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
