298bca8b7a073ed15d20b093dbc534accc996f55
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:16:55 +0000

    mention church speech possibility pity

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-def find_min(lst):
-        return min(lst)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
