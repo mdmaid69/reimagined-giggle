07a6070a13841bd8e8220171fb7c87cf89da6050
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:49:01 +0000

    design sick knowledge straight insist

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def remove_duplicates(lst):
+        return list(set(lst))
