59e3b396bac5fd2e7bb178fef30f8c1056eb84d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:26:29 +0000

    right still convince juice polite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
