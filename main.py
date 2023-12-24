981abad3f6d8989de2d7223a3acf6d987b6df3a7
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:44:17 +0000

    plenty nose shame software responsibility

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
