b53bffbb743c6a59f39d384ddf49f4665fcfdd4f
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:16:43 +0000

    though usually reasonable Bb hard

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
