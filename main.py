0e07608df5cf073057b1289374a1dc7d0c92a372
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:38:47 +0000

    corner sound decide get introduction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
