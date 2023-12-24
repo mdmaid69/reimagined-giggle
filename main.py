cc44ad9d4bd6676f7f491d58db49fb2840b4fdfd
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:53:02 +0000

    opposite fortunately trick hope still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
   def is_odd(n):
         return n % 2 != 0
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
