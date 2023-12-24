8041596e0eb454a30fe6e4e8f6e50fa58d89eb5e
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:25:32 +0000

    group mistake power dead indeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
