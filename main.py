f804e7c774124d6a209f27a62e86fa22dbee995b
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:26:21 +0000

    I right seem getup contribute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
