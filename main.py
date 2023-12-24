7dd0986b3279e4cacf5dc22d867193405c597f78
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:21:34 +0000

    guess beyond interesting discussion gather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
