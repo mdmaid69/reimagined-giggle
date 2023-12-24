b66fcfc13f06b0f0163fcfdfc358815905f73e53
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:47:43 +0000

    getaway interruption godown trust custom

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
