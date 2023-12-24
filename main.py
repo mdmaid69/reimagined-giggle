62a43779c469ea17aabb3d0f5439691b75b359fc
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:13:43 +0000

    excuse receive flavour badly pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
