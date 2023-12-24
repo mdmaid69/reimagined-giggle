91e1edce6fa391164e73874d7de96ce8b2c47834
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:20:57 +0000

    shame remove event uncertain convincing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def reverse_list(lst):
+        return lst[::-1]
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
