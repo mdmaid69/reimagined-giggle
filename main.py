f5ac514f3347fddc6b2a0d0c1895051bf285e140
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:15:43 +0000

    sing top than peaceful feature

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def is_even(n):
+        return n % 2 == 0
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
