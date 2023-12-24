a702a02dee35ae8dc6997fef938bf956d61b3d93
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:27:42 +0000

    long secret basic bin destroy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
