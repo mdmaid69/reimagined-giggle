59ff1a40c110b8f05935c86c14ca6ad2f4d6f925
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:05:19 +0000

    crowd noise length under release

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
