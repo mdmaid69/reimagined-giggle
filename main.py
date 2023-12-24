a92762f1709de444d83ae006ed50f9d8e196bd96
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:46:39 +0000

    Vv crash twist none beginning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
