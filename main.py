7d6e4fbf699b024cb31b2c346b0b39f6478461c8
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:12:22 +0000

    mood floor match truth most

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
