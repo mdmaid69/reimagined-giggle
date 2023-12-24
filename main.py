e46dceb45eaac24b1c64af0885ef04447724cdc9
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:24:04 +0000

    window keep book far for

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def convert_to_octal(n):
+        return oct(n)
