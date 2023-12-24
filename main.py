241bf852b275ccd9ec9dad1491b9bed8c5ff6561
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:15:10 +0000

    concentration hear natural load yourself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
