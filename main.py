d18bc9045b50703e1defe0f50d6e3fd00bfa6d8f
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:25:01 +0000

    Rr definite flat sky loud

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
