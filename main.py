06754289c133348a6c90ff1eb5b332e3e8235d44
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:49:10 +0000

    radio thinkover communication within note

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
