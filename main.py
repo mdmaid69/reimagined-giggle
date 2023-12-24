5fb764e7c9814d475911a38fdf974b9f7aaf5471
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:31:55 +0000

    along release promise eager they

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def is_even(n):
+        return n % 2 == 0
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
