e231ca2aa991a0d8196bab233ee92941b4c6b118
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:56:05 +0000

    petrol manner calm undo direct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
