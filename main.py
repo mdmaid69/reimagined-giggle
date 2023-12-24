c885f92e8b21a44001b89c14ca75d5aac5b895cf
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:22:45 +0000

    proof conduct line hers suckme

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
