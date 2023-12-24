0ae394ed268b409b9ee11ebad3564a30525cc887
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:14:56 +0000

    father off tongue getout take

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_error_function(x):
+        return math.erf(x)
