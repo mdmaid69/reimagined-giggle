ccc43af31501bfd26b1cdd9103b093c31e6b56af
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:34:43 +0000

    cheap sometimes burn ill male

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def count_characters(sentence):
+        return len(sentence)
