3e63570afe4c895715f85fc7c9075772488e2b97
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:08:30 +0000

    note stiff remain computer everyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_density(mass, volume):
+        return mass / volume
