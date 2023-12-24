f2c024acadc16c0299ee409e3dfbd7e972d1c383
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:35:27 +0000

    slide among mass square unable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def reverse_list(lst):
+        return lst[::-1]
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
