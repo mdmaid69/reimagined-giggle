7acf458f3382d3cd3dbd0d7b0d2b41f45274f980
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:08:23 +0000

    forward punishment stamp excuse distant

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
