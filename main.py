efbf09fd9630e450b407f267e65a2f1fae6f78dc
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:23:11 +0000

    that accordingto sick doubt insist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
