4718e0336e3666365c88d6d42b7d7d54e103897b
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:38:12 +0000

    circle uponon worry language agreement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
