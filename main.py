b6159b89957cfac796893adda95076d34685f0da
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:16:25 +0000

    slight forward beat error can

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
