0d3101d5b7979f9f7e2eb3d9bf625d0b20ab145e
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:13:56 +0000

    shock Ee term car hello

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_density(mass, volume):
+        return mass / volume
