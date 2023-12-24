5ce1d84a2285e11cc399b988a614cb844a3aa2ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:53:44 +0000

    custom beyond positive afford thick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
