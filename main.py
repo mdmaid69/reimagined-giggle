e85c3266c7d6ac1ec83a0a6d94eb1ac40fefe7d3
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:29:24 +0000

    goback(to) vision rob hand minute

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import sys
-def exit_program():
-        sys.exit()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
