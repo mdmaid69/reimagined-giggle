007823859d6e6a2c7bb7d6918ebd28997a2e6cdd
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:39:10 +0000

    main suck difficulty sick power

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-print([x**2 for x in range(10)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
