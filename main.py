eea9e111fd5a09d4b06cb0401a17cbbe5ca2720f
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:56:15 +0000

    complicated slow shut march announce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
