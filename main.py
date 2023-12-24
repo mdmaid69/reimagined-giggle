5dbd1067e1f2f22b87546fe0a192efd05db4f41c
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:09:18 +0000

    work principle stay all many

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
