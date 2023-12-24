d16c1e4e2a5dd33e91845cd86c7db9b80dca4c98
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:12:05 +0000

    sky fit concerning steep complicated

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
