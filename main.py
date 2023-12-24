f4c79a04d0c11ec02ab6c1c7a9d56d6aa686908c
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:44:32 +0000

    extra used gentle sing receive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
