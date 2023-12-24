90abbdb2d6fe3fa4020d74ad53cd6854b74c24cc
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:27:16 +0000

    turnoff split laugh plant polite

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
