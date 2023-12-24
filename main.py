6bf6be412faa2b9eb9386a41a9b6bf244fd900d2
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:28:52 +0000

    appear spin bang clothes neat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
