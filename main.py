c9d43ae3b6c3d737c7d19602c305139e18a041b2
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:37:43 +0000

    aware relief knowledge fierce responsible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_current_working_directory():
+        return os.getcwd()
