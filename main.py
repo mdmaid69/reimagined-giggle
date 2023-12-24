b8d5d551d5be6b202b62510caec890f7ff15ee7c
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:57:43 +0000

    union scream now final post

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import sys
+print(sys.version)
