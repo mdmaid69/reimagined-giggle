da7eca5229e0ade12b418080deb639d75cbdff9b
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:03:02 +0000

    but roof appearance mass hello

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
