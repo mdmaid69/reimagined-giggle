eb931b648eccf988d544e3b7bc6f65684ab7d96d
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:07:49 +0000

    artificial repeat behind ago damp

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
