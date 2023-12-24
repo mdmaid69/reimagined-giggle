73109a2e7bad14e743938aa962b4bfead1038e35
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:24:38 +0000

    off wish muscle getback Zz

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def subtract_numbers(x, y):
+        return x - y
