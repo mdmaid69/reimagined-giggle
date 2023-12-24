240d8bcc50b1f022259a38dbd896ea568ef08d9d
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:12:24 +0000

    advise table underneath nowhere offend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def calculate_density(mass, volume):
+        return mass / volume
