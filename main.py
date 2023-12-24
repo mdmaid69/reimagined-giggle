f54be3552686bdceaf7172f6e66ae7bc788cb05b
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:29:58 +0000

    complete post trade free test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
