2db059b04c528459dd549f02f0ddcda68d2d97ee
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:58:40 +0000

    performance weather mother right birthday

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  def is_odd(n):
-        return n % 2 != 0
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_pressure(force, area):
+        return force / area
