27101a7c054d76ca52154a08ccf107870c0f4579
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:12:34 +0000

    recently want police evidence affect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
