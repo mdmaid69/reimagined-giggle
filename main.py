68a46d38becc4abbcd334f691733b49402bd00cf
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:12:25 +0000

    comein least whole own personal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_current_directory():
+        return os.getcwd()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
