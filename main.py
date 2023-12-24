d49c5f5bce0169fa989220376701f4ac0bb60bed
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:12:26 +0000

    copy happen instruction unlucky bush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
