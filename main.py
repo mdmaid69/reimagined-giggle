4ab2d8524684cf94f625d2aa282574f45c8bf92b
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:27:21 +0000

    easy cross 564encourage makeinto film

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_area_circle(r):
+        return 3.14 * r**2
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
