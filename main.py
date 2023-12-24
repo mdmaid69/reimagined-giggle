39f1cdc1387a7c33788c6e02481a3fb2bd64a066
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:21:53 +0000

    ask so wash visit perfect

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import array
+def reverse_array(array):
+        array.reverse()
