80df26e1a07c3c3f259602168d88bbfddf992492
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:59:50 +0000

    naturally callat invitation opportunity throw

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
