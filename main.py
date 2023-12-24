d089a4631fcc918be3ac1dfa296a23e05586ac87
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:06:27 +0000

    Tt means smile answer corner

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
