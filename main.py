990697d5eb7df008a83d75787151f97ff7eff751
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:16:25 +0000

    callback tidy example and star

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
