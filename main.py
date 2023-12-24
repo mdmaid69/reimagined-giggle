ecd7ee0689b7e981dc51ea94f49c758b03d01b50
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:11:38 +0000

    confuse improve catch physical beginning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
