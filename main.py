90080941ccf529e7e4542c18afb1fa7094fa2d5b
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:21:07 +0000

    discover swimming last circumstance old

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
