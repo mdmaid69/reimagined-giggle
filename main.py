dde3e253aa29ba0bedeab6f82d2050db7d8afc8b
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:15:40 +0000

    cost hour fall push beginning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import time
+def get_time_since_epoch():
+        return time.time()
