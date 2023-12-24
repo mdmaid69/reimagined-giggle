14c10a767685eb47cc0565c35d5adfb58ad3b0fd
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:16:51 +0000

    ignore flight about polite born

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
 import os
-print(os.getcwd())
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
