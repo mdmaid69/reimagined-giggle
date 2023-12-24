9ad48826beee042804b81469e54da2970df2016f
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:43:01 +0000

    grateful boring powerful complain conversation

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
