4eb29decf31af778e869c8502cda07d0de2f99d5
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:14:18 +0000

    run buy fire yet wander

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
