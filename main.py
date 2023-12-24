f21e2ae8c25c189efb34ee06ac47c5615c878521
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:44:42 +0000

    listen bang on turnon fork

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
