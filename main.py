d0a9156538594886b09376453b697eb530b3193e
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:33:34 +0000

    sale spin cross violence true

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
