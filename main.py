eb3c1efd9d326a909adaaa6cdac97f4638c5a15b
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:10:59 +0000

    side represent middle politics memory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
