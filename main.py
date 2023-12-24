7d4f82feb4b077ff648eab37f643c74b6a1d5371
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:57:23 +0000

    national above first pin season

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
