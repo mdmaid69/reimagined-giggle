41bacaa598a0b26966311cbb3504e271d4cf2df2
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:16:38 +0000

    ring length choice intelligent exit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
