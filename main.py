a7f9938156f53082b18a61c82c663f7c2815d5fc
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:27:41 +0000

    hardly total neat address undo

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
