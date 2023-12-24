ed6e0836f8574a8e9247ab62e0959c9d6ab212b4
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:19:57 +0000

    principle damp upwards stare colour

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-  def count_elements(lst):
-        return len(lst)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
