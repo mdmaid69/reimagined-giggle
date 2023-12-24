ea06204b2b0a9c368f1480a7a80de2a79d6a49af
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:01:48 +0000

    pour proper still quality thinkof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
