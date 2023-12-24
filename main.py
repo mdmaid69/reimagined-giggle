143f4e41438cca6ed9a4850e74864c15d5597589
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:40:31 +0000

    crowd active cut opponent hungry

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
 import random
-def roll_die():
-        return random.randint(1, 6)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
