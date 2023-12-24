21950539b20ffc8a38e1040ed8871d7b5d81362c
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:16:51 +0000

    culture software thinkover doubt contrast

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def greet(name):
+        print(f"Hello, {name}!")
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
