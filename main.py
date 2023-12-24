6e3756669788d1c14b579bbcf9b4c178504da635
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:46:19 +0000

    able wish amount through type

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
