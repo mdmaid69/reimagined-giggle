55e650733720690f6c793d3da2b1001888175677
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:42:26 +0000

    threaten score sale which various

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
