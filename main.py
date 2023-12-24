52cc0e52a1a325eea52288d4f90cb66f69842d8d
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:02:23 +0000

    bound mad compare wipe absolutely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
