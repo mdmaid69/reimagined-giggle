102d6a6a9f23e4441d235894a84e6266091179f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:10:16 +0000

    range post record base warm

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
