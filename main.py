cccde0df6522dd13d40c0d7050b42ec177e1fe68
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:43:44 +0000

    principle estimate bringforward loan exaggerate

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def is_odd(n):
+        return n % 2 != 0
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
