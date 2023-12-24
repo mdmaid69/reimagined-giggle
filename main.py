ce1fee2f92c1e9ce7147bf4f3eb53d96af6aa126
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:16:59 +0000

    violence whistle absolutely twist face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
