5d9dc42931b01b052216147d2c50e4de341635a3
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:34:16 +0000

    load drive vanish gate priority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
