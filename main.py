64bc71914bb609589a66c986f781c31d31455195
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:05:17 +0000

    corner attitude tense lookafter train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import time
+print(time.time())
