f76e107b2304319df525899b4fc18d79bf9ba35a
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:27:14 +0000

    neck comedown injury obvious Oo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
