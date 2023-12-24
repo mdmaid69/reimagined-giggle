36fca6c3f7a01881b36bf225dbe72fe0b4c4ff89
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:54:26 +0000

    mark mail central still breakin

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
