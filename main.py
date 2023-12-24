1f32443f2c858e8d8fd693f4d88a4f7d0cebbf85
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:45:28 +0000

    day shopping in rate talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def convert_to_octal(n):
-        return oct(n)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import getpass
+def get_username():
+        return getpass.getuser()
