4063f90da4ba1de856911d389b8249760aac17dc
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:41:30 +0000

    complete everyone pay mess uniform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_words(sentence):
+        return len(sentence.split())
