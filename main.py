98c49d0d036a47b80d1792fc16290b25c2c11c9a
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:17:03 +0000

    tragedy back blame fairly would

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
