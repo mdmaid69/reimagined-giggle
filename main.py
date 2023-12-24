8ef9775c05f0f049650f032d9eaae4049c2f3690
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:26:32 +0000

    store benefit 179bite whichever speech

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
