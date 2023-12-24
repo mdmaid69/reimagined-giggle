afb2f3ba6ce9e47cec7b2ab8f668419d57e5e15d
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:38:03 +0000

    tomorrow clearly will print drop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
