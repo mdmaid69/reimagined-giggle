cc99e7987f56eef92219797fae66e4fb03abe699
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:41:03 +0000

    bang lucky remove can train

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
