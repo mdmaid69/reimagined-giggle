2fa15dcf002a520d0cc01f8451624c82c0191e4c
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:58:11 +0000

    constantly like move generally actually

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
