64478418297923d8ec34d9f889a8c8007eb001f2
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:31:21 +0000

    lookaround talk Uu clear cross

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
