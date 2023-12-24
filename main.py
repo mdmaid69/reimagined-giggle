9fceced7a0720d5ce2ef507bf38b60e0183629be
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:39:30 +0000

    detail intelligent shoot minimum breath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
