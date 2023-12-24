d19f35ee6f659c0afa3016705d2917f49c9f60a3
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:19:27 +0000

    vanish emphasis possibly generous suppose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def clear_array(array):
+        array *= 0
