2952344cecdf3dfa5533c9f69ac9a514c9424677
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:16:01 +0000

    teach wonder unkind guess fast

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
