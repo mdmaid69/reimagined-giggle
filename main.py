dee8716b154552ae290f6c41d28389860714c357
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:38:46 +0000

    talkback proper pay consider accidentally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def reverse_list(lst):
+        return lst[::-1]
