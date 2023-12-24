4e65e32f1f841f1f13b9fe09c818692321ac2dc0
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:24:05 +0000

    open holdout move apart aim

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
