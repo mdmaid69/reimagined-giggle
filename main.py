b89cb98c9f8470bc9dd98da8028cc25bdaf2b3c4
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:52:45 +0000

    sting announce hour system traffic

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import array
-def append_to_array(array, item):
-        array.append(item)
+def set_array_item(array, i, item):
+        array[i] = item
