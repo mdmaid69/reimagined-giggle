f7c0e5ee2ceb7af9947a3d91df8fd2f53d8a063b
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:18:39 +0000

    treatment convenient sense else crisis

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def square_number(x):
-        return x**2
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
