31044c4ddc122a3898cff693602359a5b021c7f1
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:22:35 +0000

    Gg uncertain rest leave takeoff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
