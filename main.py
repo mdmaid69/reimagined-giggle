6d4a0ebe5fd87bc221649c6542afad5be5080414
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:26:51 +0000

    wind surface peace money breakdown

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
