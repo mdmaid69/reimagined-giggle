7db434325938376a9fe32f39663301914f12768b
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:26:30 +0000

    trust ask up that noisy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+name = "Python"
+print("Hello,", name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
