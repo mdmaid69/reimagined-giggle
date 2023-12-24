3d569e77944b287c27f73421285609294672c54e
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:34:50 +0000

    scale search safety train my

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
