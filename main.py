7d7fe641324fe20e01228e2d04a479a33fce4855
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:50:45 +0000

    cross holdout reason wait close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
