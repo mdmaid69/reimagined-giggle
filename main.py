8dcaed2f72d82f4e6176696a4c50d320d39c30db
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:26:47 +0000

    for under window write part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_force(mass, acceleration):
+        return mass * acceleration
