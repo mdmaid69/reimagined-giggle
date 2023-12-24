83b2355c2515974edcd5a42a23deca17bdd6244c
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:44:57 +0000

    spend stone praise behind common

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
