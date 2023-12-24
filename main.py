963cfe7b8d0a00c1881a354c4d24af70aada0283
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:55:08 +0000

    free under pure private earn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
