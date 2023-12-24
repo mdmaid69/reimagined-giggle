cf2d0cd80496939f51c5c1a2f3347635f99f9f38
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:19:35 +0000

    wish minimum correct grand around

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import getpass
+def get_username():
+        return getpass.getuser()
