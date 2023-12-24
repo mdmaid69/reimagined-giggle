4b9f697b91c57c335352dbebbfae7ce6848baf94
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:10:40 +0000

    knowledge paper experience correct across

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
