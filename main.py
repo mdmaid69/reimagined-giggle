797c8714cfd3f3194cfdd60197498f457c408a77
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:31:47 +0000

    thinkover feed nature lend borrow

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_work(force, distance):
+        return force * distance
