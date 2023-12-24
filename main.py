7f965f04a902c60e1ecc7cd2620daebfe57d5959
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:14:11 +0000

    school related work communicate ground

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_pressure(force, area):
+        return force / area
