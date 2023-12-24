62ce0a47b4122914349ff96cc7b3bb07d81cc67d
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:48:36 +0000

    over handle mother create mixture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
