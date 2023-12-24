d0b1c50ab122ed2befbf29d5861eb523be937a4f
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:37:58 +0000

    tomorrow surprising combination tooth account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
