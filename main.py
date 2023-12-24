bc4cfbd49d4ca8ec06bb51f39990892d26178f3a
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:21:02 +0000

    convenient wish argue book 179bite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
