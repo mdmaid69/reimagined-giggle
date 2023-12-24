03f7e7c188dacffd9f99bd767315929b08452985
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:15:16 +0000

    somehow cum identify wash there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_as_list(array):
+        return list(array)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
