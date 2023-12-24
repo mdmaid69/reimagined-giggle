24925cddf2ec2cbd917045591335940589a21c89
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:50:19 +0000

    sick rank and keep hurt

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
