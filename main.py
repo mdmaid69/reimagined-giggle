282ce5faae8e6c6c2c13b66b6e265f3f2e96c320
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:17:03 +0000

    shut precise proud weekend perhaps

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
