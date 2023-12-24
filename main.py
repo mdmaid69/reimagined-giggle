b7fd13ac3a69d7ed462fdb46620c00142781a2fe
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:59:22 +0000

    opposite couple suppose present far

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
