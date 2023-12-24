1d27ac0f88ba0831ff8f45f6ea84cf511b22ccb4
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:04:17 +0000

    tip drop fall us slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
