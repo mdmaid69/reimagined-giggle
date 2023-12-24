a34d96b07a3e87bb95d099a83be22447cac67f70
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:07:32 +0000

    correct necessary disappear wish geton

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
