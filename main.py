7de388d9e130f1a429b9dca583cf20f52ed09474
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:18:52 +0000

    complaint holdon interest runout wet

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def divide_numbers(x, y):
-        return x / y
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def sort_list(lst):
+        return sorted(lst)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
