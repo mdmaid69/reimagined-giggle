600e4c27e81d3aa974e69e5af194ed65d9daca2e
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:06:02 +0000

    each challenge summer properly delight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import os
+print(os.getcwd())
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
