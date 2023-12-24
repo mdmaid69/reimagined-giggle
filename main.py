67749e1676512515ee493333cdbc357516b4d28e
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:43:27 +0000

    imagine judge shade spring part

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
