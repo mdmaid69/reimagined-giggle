0cc64f709b64ada6b13c3a91c07e8ac5f3b48c7b
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:45:47 +0000

    for door turnaway runinto this

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import getpass
+def get_username():
+        return getpass.getuser()
