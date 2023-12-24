fc7de0197b90a423b5f115d64833974dbbdf259a
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:14:15 +0000

    they every keepout report recover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
