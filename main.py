9ed79206761485f4e5dc46a886ed147a8f093c4a
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:58:54 +0000

    turnover long prefer tall cinema

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
