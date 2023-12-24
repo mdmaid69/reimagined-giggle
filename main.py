d8e392d7470cac8053b15e29a79383e5e41bb66b
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:10:06 +0000

    price uncomfortable disappear along load

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import sys
-  def get_python_version():
-        return sys.version
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_acceleration(speed, time):
+        return speed / time
