56e9ea08268cb4e27f658ce1b57c1af89c5a2083
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:02:41 +0000

    than something him black print

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
