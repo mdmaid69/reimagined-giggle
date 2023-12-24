9377915658e87e690407161bb1e9c35265bce817
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:31:54 +0000

    right target welcome chat shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
