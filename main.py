1349b82cfa9d0524d89e0f98f65d734420ef3f41
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:14:07 +0000

    until write score dream runaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
