051e3adbbe0a4f6e56652ab221bc8720f5c13f15
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:47:47 +0000

    close pair seat ride wave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
