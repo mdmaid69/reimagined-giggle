a12f05a0a91fe6c8a2c30cfdd8cf1dea3c06852b
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:01:29 +0000

    letter stupid schedule make immediately

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def cube_number(x):
-        return x**3
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import datetime
+print(datetime.datetime.now())
