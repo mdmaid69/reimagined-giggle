dc7d72ea2a6666f045d1db5621a5391cb56f17d1
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:14:46 +0000

    excuse reduce able cruel evidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
