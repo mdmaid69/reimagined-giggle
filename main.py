1c3bd4ca86f1b8e454293d7caab140c2e3e6d363
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:44:36 +0000

    fly danger share mess sensible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
