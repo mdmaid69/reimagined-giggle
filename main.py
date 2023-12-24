f7470badfd28b0e17a7b36978dc04b04f449c159
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:21:15 +0000

    which second letter zero front

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
