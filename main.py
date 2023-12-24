0332b35931d141fd5e145acf8a2c57743ddc12e0
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:38:44 +0000

    row lock you rarely but

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def is_even(n):
-        return n % 2 == 0
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def find_min(numbers):
+        return min(numbers)
