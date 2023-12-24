89ae9f566346970009a4f01a1cf0a8bf942991f2
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:16:40 +0000

    left strong nose left second

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
