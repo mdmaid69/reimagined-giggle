eb92cd9a63ab16de9b04aaf3f00022443c789fca
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:36:09 +0000

    fork early issue harm organization

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
