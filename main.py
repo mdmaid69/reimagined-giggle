11f93e838ee7a1601cc8677789c654cb7484da46
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:49:52 +0000

    clean afraid free month keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
