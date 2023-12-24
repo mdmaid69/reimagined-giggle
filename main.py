19e95ba8cf48c43257ae557d0d5f76bd493f5786
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:26:49 +0000

    just advise continue attempt 565end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_repr(array):
+        return repr(array)
