15000209bd32fd4126606b6e6525fa7c2873f18d
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:55:47 +0000

    risk market embarrassing through confident

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
