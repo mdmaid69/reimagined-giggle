2815bece0e51e280d1300cf3425090e53149a273
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:42:45 +0000

    blame skill reach cause image

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import logging
+def log_message(message):
+        logging.info(message)
