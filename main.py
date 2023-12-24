c9cbece6b03e07221baad98674bb1d6edb303bae
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:42:49 +0000

    interfere cope may usual dry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def divide_numbers(x, y):
-        return x / y
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
