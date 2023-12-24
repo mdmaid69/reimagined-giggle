59c53c6df100574e4d4c58c99aed3bf26f78f54d
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:36:17 +0000

    game familiar date chief burn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import collections
-def create_stack():
-        return collections.deque()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
