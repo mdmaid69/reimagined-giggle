c77575dfcadcf0a36174410c9309e918c9e5079c
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:44:28 +0000

    autumn look not treat minimum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-print("Hello, world!")
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
