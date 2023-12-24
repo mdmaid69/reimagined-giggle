940afd87f7b79e10df7d9491c2d181568097f937
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:48:46 +0000

    mind argument name again store

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import datetime
+print(datetime.datetime.now())
+import tensorflow as tf
+print(tf.__version__)
