5a0c8f0d0001d38be57f469c617a3ceb06fcb756
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:25:10 +0000

    square goback(to) story pretty excuse

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-print(sum(range(10)))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+text = "Hello, world!"
+print("Uppercase:", text.upper())
