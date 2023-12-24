41202f7a17bd31cbdee7ce2b7a20bec2711ca25b
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:26:38 +0000

    cruel reaction getaway definite prefer

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
