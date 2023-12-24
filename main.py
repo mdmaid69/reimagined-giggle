f817e4cbee7b8aa243acc5aeb59b6cbdd7b416e4
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:36:10 +0000

    special shut feel sea event

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
