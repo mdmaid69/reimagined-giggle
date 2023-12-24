f4e3b428b544090bde00e11598a2bf638021e672
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:36:34 +0000

    wind goup easy their buy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
