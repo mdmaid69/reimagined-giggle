c376b61fd3740298d3dfb964768820cbdb45369e
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:18:18 +0000

    treatment keepaway one around and

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
