62ea34dd5c7b595532def7e03e68d343de19957e
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:50:15 +0000

    copy better standard workon interrupt

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-print("Hello, world!")
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
