048a6d8bbf4ea451c4df44b28c2616080fcea788
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:55:22 +0000

    now bet for any weekend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def add_numbers(x, y):
+        return x + y
