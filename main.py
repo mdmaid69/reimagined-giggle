16ee795d19a666f33609cddefabd4792e462b25a
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:03:19 +0000

    play police unite show difference

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
