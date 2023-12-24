b84afce295ff4e7d9059e968f764a48e6b6945b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:02:57 +0000

    movement basically expect butter but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
