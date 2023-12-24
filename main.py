b2acf426d3b8a52151d3456854b0959ebc97aa55
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:58:03 +0000

    present ability right in noise

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
