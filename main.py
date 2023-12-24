dab0797b769d47b6ac284e1e53cc3ce741f9f700
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:27:09 +0000

    behind behaviour preparation underneath recover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
