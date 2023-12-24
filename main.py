d762536c6f0cd1c999842a4406f43561946bf131
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:03:26 +0000

    march exploration pause reliable quarter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
