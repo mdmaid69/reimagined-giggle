694d3b5766e9e99e2ea5b9a27942c9557b7d3201
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:21:41 +0000

    stick quarter nose so beginning

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
 def is_prime(n):
-        if n < 2: return False
         for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
+                if n % i == 0:
                 return False
         return True
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
