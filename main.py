c4a3c9214f7342b4393f4960d5b8db97c746430e
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:32:21 +0000

    environment flow wild discussion tone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import getpass
-def get_username():
-        return getpass.getuser()
+import sys
+def print_python_version():
+        return sys.version
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
