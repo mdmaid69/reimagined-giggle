8b0a3a6fadd27571a14395cc893e71bfa84d17a9
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:56:36 +0000

    mix basic touch common temper

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-print([x**2 for x in range(10)])
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
