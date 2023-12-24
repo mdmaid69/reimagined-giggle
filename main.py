bd794105e4dd9abc7074166f0bfb8e190440c95a
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:40:22 +0000

    disturb hers rank organization judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
