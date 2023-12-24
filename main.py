2f68532cac00e8e79a0d333734cf45709f6e16cf
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:10:21 +0000

    lucky machine destroy can free

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
