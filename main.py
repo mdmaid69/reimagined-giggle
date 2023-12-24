b578c483e487c2593382ba4f045e934fcd14dc64
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:18:54 +0000

    often back right great debt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
