849da40f07b1499e11b3b2323fee0f819c872bde
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:40:09 +0000

    flood uncomfortable total throughout goout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
