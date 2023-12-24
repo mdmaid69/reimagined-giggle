851c831e47bc5d7bd6e1bc67a3841b3ffba579ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:42:55 +0000

    impossible door hope future turn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def get_array_index(array, item):
+        return array.index(item)
