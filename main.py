a02f369104aa69be9d1a34e74c0195a7afa44fcd
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:53:21 +0000

    waste light turndown contain complaint

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def convert_to_binary(n):
+        return bin(n)
