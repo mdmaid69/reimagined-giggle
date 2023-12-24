e2b94dbf890bd5e865de15ec23eee5d125395db4
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:54:09 +0000

    train smile unpleasant reasonable fix

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
