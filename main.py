a82575885c2a21789d598911145781d3d9373ecb
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:41:40 +0000

    help turnon less high tired

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_pressure(force, area):
+        return force / area
