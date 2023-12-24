188703773a34387228c53602c7e921cac7d75c96
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:31:20 +0000

    suddenly regret rob walk sick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
