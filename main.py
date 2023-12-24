388b11c7a2cd959e157bcbecde000e71f7e7f52c
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:42:04 +0000

    address effect ticket reduction high

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
