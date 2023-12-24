077380385f92e8c69427bced862710a90364b913
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:18:55 +0000

    pleasure second clean base display

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+for i in range(10): print(i)
