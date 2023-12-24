0b1718fad4b01e60d31dd8b9b45040c0c858525e
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:17:59 +0000

    wire write door teach shoot

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def add_numbers(x, y):
+        return x + y
