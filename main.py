c37bb63fe12bb19803037e285c1951b6f139e3bb
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:04:10 +0000

    that profit together attempt kill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
