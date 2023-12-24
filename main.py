37d0fc0d00859a7a3ecfa1b13085c298f525b294
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:29:57 +0000

    mind alive happen language bet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
