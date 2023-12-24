d35ff64780651d653db69d14470c089b5f6ea7a0
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:09:26 +0000

    free available keen light boil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
