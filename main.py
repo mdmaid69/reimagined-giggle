a7f1d06f38e219b4cb7546ef45c02577581ac775
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:55:13 +0000

    shadow with cap comein bit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import collections
-def create_user_string():
-        return collections.UserString()
+def square_number(x):
+        return x**2
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
