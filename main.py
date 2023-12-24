8dc23a7b253f2d7982756d27c87eb041a2603708
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:18:24 +0000

    judge when address most welcome

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
