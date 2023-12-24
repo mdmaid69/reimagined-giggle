11023a805e6aebccb933ef1f0edba5a5ad661ed3
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:43:17 +0000

    false pressure it whoever unlikely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  def calculate_area_rectangle(l, w):
+        return l * w
