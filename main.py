c30864c09f2446c9ceb5284ab12f6e3574ecbf80
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:32:15 +0000

    butter belief bridge deliberate pay

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
