75903b076218bed072c9dc7f1eaede0b46ed3dc2
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:13:12 +0000

    569enjoy zero mixture thumb comfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def add_numbers(x, y):
-        return x + y
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
