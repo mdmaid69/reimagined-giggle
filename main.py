a76d0d662f1150307be398ab7d3c223e6902cf21
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:21:32 +0000

    anything common could only practice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
