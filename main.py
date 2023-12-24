c514957fc4a80b2c51754c37a01ff9bd68ca5ef6
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:17:03 +0000

    567enemy action possibly compare part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
