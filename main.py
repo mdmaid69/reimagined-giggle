17000868aeeed6c68f9601d10a721d93d7578dca
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:01:59 +0000

    figure ignore eat defeat correct

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
