099545610ee5663d005b52bc58ad6a27b408d243
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:21:28 +0000

    appear beside serious ground straight

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
