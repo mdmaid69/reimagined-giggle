2d81d730a547f15b9c40585a34738caeb7a797a0
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:16:18 +0000

    dick minute gentle lesson left

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+for i in range(10): print(i)
