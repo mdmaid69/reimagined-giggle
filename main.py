b08b9e9ff0b86791dd62e5076c50da1da00e8dfd
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:59:32 +0000

    compete basically beyond total church

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
