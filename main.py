b38952a90117eae6ef945ae836224ca78de9cea0
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:08:41 +0000

    point quickly television poison office

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
