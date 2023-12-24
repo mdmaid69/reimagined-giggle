e064cbac44267978d9b0946f42a871c2e574fa78
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:44:40 +0000

    rest rich accommodation boring bag

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
