b5d41a1bb7476a7b598660786b77057e86e71285
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:18:41 +0000

    punishment place through determined yesterday

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
+print("Hello, world!")
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
