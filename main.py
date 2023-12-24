3ad6977f659dd9ccaa148ec0b65abc10103ff8eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:42:18 +0000

    trip hold until call loose

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
