fcbee7997c8f19eef857ed373221de1b9f2207b1
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:29:32 +0000

    bottom sorry laugh breakinto modern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import numpy as np
+print(np.array([1, 2, 3]))
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_as_bytearray(array):
+        return bytearray(array)
