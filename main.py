2d926da4d0d489362cdd938b20067f8b9c9eeabd
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:46:41 +0000

    sign workon could loose female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
