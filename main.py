68251c7ea4d3b58423dc3da753fb29ebd1f3e837
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:44:16 +0000

    workon deaf callat divide worth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def subtract_numbers(x, y):
-        return x - y
+def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
