4dff98e429dab485d3724d1842402bfab5188306
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:35:49 +0000

    see reliable refuse up part

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_pressure(force, area):
+        return force / area
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
