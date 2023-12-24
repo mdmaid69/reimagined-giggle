a469fd22f541d0b93bc03b5d2d25047383277d85
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:50:26 +0000

    male famous wake holdon discussion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_index(array, item):
+        return array.index(item)
