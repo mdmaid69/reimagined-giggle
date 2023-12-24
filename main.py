80f5950563c05062c68cbb2d9ad848c812966b29
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:35:15 +0000

    satisfactory tidy distant nearly subject

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
