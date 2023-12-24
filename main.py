9a7c3990729ebfecff1bafa17dedba4c3cc177fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:33:03 +0000

    translate 565end 566end involve among

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
