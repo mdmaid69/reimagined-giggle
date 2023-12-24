6787489a4bb18a0a25e89c4abb6d3f42f5b1c154
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:35:53 +0000

    myself cover achieve Bb after

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def cube_number(x):
-        return x**3
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
