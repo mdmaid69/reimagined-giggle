aad0c9574336f367659c7947cad10e5b1eae5764
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:37:29 +0000

    autumn also crisis satisfactory slightly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
