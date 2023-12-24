74b328421e97aee1cb2379aaa31e197a7e6b9813
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:35:21 +0000

    wall quick discover Ff edge

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import platform
+def get_python_version():
+        return platform.python_version()
