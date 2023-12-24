ba1e7ff02a52a9db9c1099bf0ce66a23b57c7d6f
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:28:42 +0000

    nose alone earn workon arrival

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
