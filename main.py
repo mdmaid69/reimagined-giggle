ae55b5fe2d1631f2683bfe84bedbb50e69161acc
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:25:12 +0000

    shape otherwise recovery fail cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
