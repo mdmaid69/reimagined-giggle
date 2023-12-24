d0e776b6dafcdf5ee0253ccca69806877c7f1c63
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:44:35 +0000

    stage purpose poison do flavour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_length(array):
+        return len(array)
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
