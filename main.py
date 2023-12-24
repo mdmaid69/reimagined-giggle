474c19b76f89e3f9941d0b4e701e207d9888a3e5
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:01:49 +0000

    age absolutely whether interest similar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def add_numbers(x, y):
-        return x + y
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
