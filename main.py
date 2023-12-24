f2df04e4c648019c13954f42da682b085fc4407d
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:59:15 +0000

    cover support order mouth mass

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
