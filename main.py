830564e4bcfb8ec52fd398e0a02137b51532067d
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:25:38 +0000

    otherwise birthday discover stick increase

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
