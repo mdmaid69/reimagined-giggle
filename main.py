8b3817ffe956a9ba55e8105c677247182b2b1a7b
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:54:58 +0000

    activity transparent bush stir strike

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_set(array):
+        return set(array)
