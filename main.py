fc0f120d0f5d9c255679b32dc90751bc1916da2e
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:28:53 +0000

    water taste fetch push medium

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def count_elements(lst):
-        return len(lst)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
