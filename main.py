8fce5acbc564cd2ba5f3bcf6fcbdf45bc1d8934a
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:05:11 +0000

    team message rain story for

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import datetime
+print(datetime.datetime.now())
