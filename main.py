2e4410904844356e5fc4f7b328122ad37d8d26dc
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:34:33 +0000

    sad shame much order extremely

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
