27e375805d2d104f891f5f827350b284206f2e20
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:22:03 +0000

    know choice bar care remark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
