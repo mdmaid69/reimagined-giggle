97cd2f7f99f900959a72f1f01ffb1e3d32c5e4fc
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:55:51 +0000

    equal familiar sleep further speech

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
