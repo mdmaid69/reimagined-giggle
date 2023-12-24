2f616b6c2ece1148711f3657d6777148452f0654
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:09:23 +0000

    confusing as normal interesting reject

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def add_numbers(x, y):
-        return x + y
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
