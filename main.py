76cdbbb46da24fdc6596843be24bd5baf1cc7e96
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:42:32 +0000

    rid confused suit room criticism

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
