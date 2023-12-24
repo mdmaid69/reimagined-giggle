2b6c4d22a8516e9936ec95d1fb7a669d8a32e06f
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:20:34 +0000

    comfort past evidence negative Mm

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
