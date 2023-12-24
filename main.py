462a2ef74042292b71068684ef521042acb9979b
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:45:21 +0000

    fortunately tip battery comeround brown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
