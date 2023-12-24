51015d0b2ef79af21b013508bd07050f76b56055
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:16:51 +0000

    violence blame blame goout unite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_density(mass, volume):
-        return mass / volume
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
