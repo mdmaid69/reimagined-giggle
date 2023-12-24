9d3e656159981d0430eb12fe4695b3badd26e0ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:03:54 +0000

    blow laugh whole problem crowded

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
