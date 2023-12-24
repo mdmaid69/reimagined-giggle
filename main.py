18bc97a569432e9463df414b1b832e78fab9dbf3
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:16:27 +0000

    warn stone freeze field light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import platform
+def get_python_version():
+        return platform.python_version()
