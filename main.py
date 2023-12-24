c9119542f9fd534631e0fa2d8f3c76df3dc5f3e5
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:27:48 +0000

    amount smoke wide slide takeaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
