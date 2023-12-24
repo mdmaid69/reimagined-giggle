2bab85c46dc59f9aec34e9b1bb7b9b265f92a203
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:19:18 +0000

    trip keepup divide onto total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
