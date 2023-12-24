3e930ea83ea5325d3f62be2d64c482efe39afe2e
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:30:51 +0000

    account onto pronunciation hour approval

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
