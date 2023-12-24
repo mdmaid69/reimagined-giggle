33c4cab65d81293c8e5bb27cbd292f76b169013a
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:08:25 +0000

    me point ready her friend

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+print("Hello, world!")
