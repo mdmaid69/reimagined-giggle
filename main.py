2fb794e554d57fa96a4774d78c0a0ce757377e22
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:47:56 +0000

    peace special else holdout protest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def sort_list(lst):
+        return sorted(lst)
