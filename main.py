1ccb51133daa95d04517d9c7d562bd1fb12ed600
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:45:14 +0000

    soak train tape strict angry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
