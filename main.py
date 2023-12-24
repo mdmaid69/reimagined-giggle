6d87040997499ea373d6b46829e9adef49c37caf
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:30:57 +0000

    Ss send sight trap cum

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def get_array_as_list(array):
-        return list(array)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
