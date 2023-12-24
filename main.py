7a66960be9f555456cc66ad62995ba7aebb43105
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:31:02 +0000

    interest artificial base act limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def split_path(path):
+        return os.path.split(path)
