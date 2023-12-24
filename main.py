54f1fe9ed1ccccc3582631ff39db812d236bbd0b
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:20:52 +0000

    immediately badly unkind exercise guess

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def square_number(x):
-        return x**2
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
