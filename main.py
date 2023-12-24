275379b9fed8ab4b02be7eb4c4f05f799ec9ad1c
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:02:45 +0000

    course murder hit promise copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+print(os.getcwd())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
