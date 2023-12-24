32750121b73121b70af8456a585d6d0d9358e339
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:19:40 +0000

    crowded steam Ww continue conduct

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import collections
-def create_stack():
-        return collections.deque()
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
