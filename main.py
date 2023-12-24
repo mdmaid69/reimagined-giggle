793dad6d94754d56d753d2b70cb5dcfd7a2abd6c
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:24:34 +0000

    date view Uu rough turnon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
