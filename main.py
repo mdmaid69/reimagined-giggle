2f57f9019950d635415c3111206e173b956c0a09
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:10:58 +0000

    ring game weather getaway popular

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
