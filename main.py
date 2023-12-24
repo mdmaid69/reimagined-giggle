5a3216e58664218e1c92cc62e3dcdf452fd34b0b
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:27:10 +0000

    home demonstrate breakinto minor taste

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import random
+def roll_die():
+        return random.randint(1, 6)
