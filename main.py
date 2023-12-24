0b24718584fefa22ba9e3c04e0840d49e3bf51a5
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:50:15 +0000

    company true attitude combine Nn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
