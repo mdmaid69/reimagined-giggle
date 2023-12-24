e800642339017ecc9e424f11e5868647ea32a605
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:12:55 +0000

    turnback excited stamp confusing yesterday

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
