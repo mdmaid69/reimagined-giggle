6a2a83ae1204aef1ea4ea8e011be3ed8171caab1
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:55:37 +0000

    miss scene nowhere game black

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
