547a1baf6802daa12a359eecee71010309e7f50f
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:35:40 +0000

    estimate crazy sudden imagine preparation

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
