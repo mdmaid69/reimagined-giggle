7bde1edf97e7b23fe202d45fc0484d33875252d6
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:53:53 +0000

    keepaway Rr comfortable opposite both

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_string_from_array(array):
+        return array.tobytes()
+import os
+def get_current_working_directory():
+        return os.getcwd()
