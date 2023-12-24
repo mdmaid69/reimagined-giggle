0fb74941401ca977e8e5fb57caa5fdb31b891539
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:28:38 +0000

    free food street professional Jj

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
