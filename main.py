aeb656b2048ea01afd12bab71b726bc86aa0a78f
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:59:31 +0000

    problem goup equal thankyou thanks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
