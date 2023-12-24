355c8fce84727c925a8aee684c0bcd5d4a1ff271
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:43:14 +0000

    pity fence hope unhappy reliable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def get_string_from_array(array):
+        return array.tobytes()
