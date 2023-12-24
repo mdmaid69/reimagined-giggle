58ede4c623d9e61fc2ca97013a22e9e3be228be7
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:10:39 +0000

    strict thirsty coffee pain within

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
