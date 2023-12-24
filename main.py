021619f4b4ad8ccf23315f7cf94f0a495ebbc5b5
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:15:23 +0000

    platform name scream lookaround shoot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
