9543a47303b5d16e28789ee49aa601a827274c1c
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:58:47 +0000

    unless wise or finger split

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-from collections import Counter
-print(Counter("hello world"))
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
