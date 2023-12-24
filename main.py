a4d8b98bb2fcfe33059af4cf00d9964d3b6c1d25
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:26:42 +0000

    almost entrance nowhere few expression

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
