3e808bf1e9c2cc8c9be48aa29ba20da7ddfe2acf
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:45:49 +0000

    independent absolutely left complicated permission

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_slice(array, i, j):
+        return array[i:j]
