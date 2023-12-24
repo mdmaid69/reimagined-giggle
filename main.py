596785e756c4c71cc9796abfd3d84cc9257f7f52
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:41:19 +0000

    lock neither sister crowd fit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
