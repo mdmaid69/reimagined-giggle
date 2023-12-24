e31d135b69675b476c839b95ad02b9624e71f232
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:54:28 +0000

    positive tooth half pay excellent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_max(lst):
+        return max(lst)
