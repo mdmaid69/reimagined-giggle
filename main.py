72c4f419e1a8c702843ae30ace1cf90620811a5d
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:09:21 +0000

    possible avoid impatient year breakin

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
