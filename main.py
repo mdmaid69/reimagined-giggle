b1d4c64e0d26fa707cb99e71278a35ceea36a6a3
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:55:48 +0000

    sure pile string combination figure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_area_rectangle(l, w):
-        return l * w
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
