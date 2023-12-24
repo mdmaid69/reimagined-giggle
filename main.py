e940dddd9791e07f38d852030a839857ce2d4a49
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:52:08 +0000

    firm worth own pressure return

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
