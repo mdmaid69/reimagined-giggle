5887a5134e1c2a683d4c53abf9f91fcad06dba31
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:01:49 +0000

    cruelty human tip blue become

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
