ee38b92aadd02038dd0beb73dd0a7e5e59de5914
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:22:45 +0000

    trick entry schedule present breakout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
 def calculate_exponential(x):
         return math.exp(x)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import os
+def remove_directory(path):
+        os.rmdir(path)
