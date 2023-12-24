e7d0b6f0804e0d0334d38bb8a6d097cf6212d5d5
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:42:12 +0000

    various equal hard condition follow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_size(file_name):
+  def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
