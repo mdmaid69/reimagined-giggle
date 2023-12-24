fc762e6b00d9e7c14751118a38f2cd03b68f191c
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:34:02 +0000

    valuable face grateful fork breakthrough

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
