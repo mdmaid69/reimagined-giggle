ce33aef84dc04db0ce2a63968c27e2f275f60ac4
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:06:49 +0000

    surface possession plain fast heat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
