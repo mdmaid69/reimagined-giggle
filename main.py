ba0d5fc84c606f9120486b0cbf1c7bee81852ed8
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:30:25 +0000

    nerve intend she common underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
