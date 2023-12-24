269541031d9cf2432bf406c8f8901cf3ad8b5519
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:36:32 +0000

    education public live complete extremely

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_density(mass, volume):
+        return mass / volume
