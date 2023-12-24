3c4bbfb2b29088aed810e47a7b138de0777a0a66
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:15:10 +0000

    acceptable bag sight official weigh

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import time
+print(time.time())
