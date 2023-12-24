2e83711cb72c22667b6188dfce1a2af5a4a3dae6
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:16:44 +0000

    lost cold list here respect

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
