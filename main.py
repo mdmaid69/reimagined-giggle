e6c3d3d0d39c4d8a884aeda9e73bfeec3366e38a
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:31:53 +0000

    drive regular successful trust arm

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
