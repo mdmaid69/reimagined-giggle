a76e45f77d146fed8594d236f52cae3171df726f
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:07:46 +0000

    release of under keepaway strike

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
