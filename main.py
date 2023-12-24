0584b5fa27dd339d84073b8b6b86113a3978e694
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:18:47 +0000

    shop position little Rr interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
