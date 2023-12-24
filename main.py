b4ae314eaa9446ec17fb4c6fe372e0a99707b4e5
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:55:01 +0000

    tape cause interview limit lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
