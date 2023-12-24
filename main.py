ab29dc43f6a1bc57f7fc00caebcd6ef11a4d0246
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:55:34 +0000

    way minor below rub why

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
