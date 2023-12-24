46607a4b15e3e3e52380c05b117d92be37881ce1
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:20:16 +0000

    below odd ordinary someone holiday

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
