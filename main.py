10d144bd3c67b9e210657e8f5716b7a1583b69bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:09:33 +0000

    comedown everybody delight important attract

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
