2fc000710048c221f83ac1bd959bfcc1c7a5b1a5
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:07:33 +0000

    deserve crisis uponon job sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def convert_array_to_string(array):
+        return array.tostring()
