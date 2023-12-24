ad7c45283dc36d95af223f969695b7f869384a50
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:44:15 +0000

    179bite shade room intelligent bag

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_work(force, distance):
+        return force * distance
