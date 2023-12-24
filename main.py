5de972aa67717e8c6a060141b0199c278a4cb1a5
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:21:27 +0000

    prize pause plastic ashamed heart

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import platform
+def get_python_version():
+        return platform.python_version()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
