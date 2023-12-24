4d3284e4d51fe8ecd6fe4b8f047b18c307d17cc0
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:20:58 +0000

    fierce beyond distance house side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
