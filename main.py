8f05f9220efe0f1de0950c41f9a060cdb8d78945
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:39:58 +0000

    start life course real dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import getpass
+def get_username():
+        return getpass.getuser()
+import sys
+def print_python_version():
+        return sys.version
