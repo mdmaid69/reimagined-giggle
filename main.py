1e92562b634cb4cf86154fc58b015fc623e2aec0
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:01:15 +0000

    minor help congratulate thing estimate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_speed(distance, time):
+        return distance / time
