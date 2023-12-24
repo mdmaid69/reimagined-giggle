8958f917c250eb2b54f7d9e97531ecd48066932b
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:13:41 +0000

    giveaway reach desperate when job

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
