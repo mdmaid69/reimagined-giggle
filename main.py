6dfe6b43648d14fe8580f5d605b83b329e4777cd
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:44:19 +0000

    recover comfort allow bottom afterwards

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+print("Hello, world!")
