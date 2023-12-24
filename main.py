53084aeae739f0f3e2d3605f53f0b24114ac671a
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:56:43 +0000

    high keep whistle winter calloff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_float(array):
+        return float(array[0])
