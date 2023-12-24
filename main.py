dbc299bf42b3f3f9140ef65529bd0b8e171e2a76
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:24:07 +0000

    grade rid declaration reasonable theirs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def get_array_typecode(array):
+        return array.typecode
