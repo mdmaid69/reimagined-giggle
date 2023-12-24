3675b6e9de06a810f11b0bb9a54788818b114389
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:13:40 +0000

    effort blind willing near experience

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_acceleration(speed, time):
+        return speed / time
