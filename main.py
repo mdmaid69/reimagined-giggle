6391839fae66d04e80ba3f291304afcd35e14f07
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:21:01 +0000

    independent conduct road comeover turnoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
