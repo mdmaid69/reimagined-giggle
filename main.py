1878c19d0b32f4fa0dce510cf64de9781eb65207
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:43:08 +0000

    sore solve not getout Qq

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import datetime
+def get_today_date():
+        return datetime.date.today()
