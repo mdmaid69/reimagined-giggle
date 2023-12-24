baaa0988f04769afbf1c2323a12890df7046fbc8
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:03:29 +0000

    coat suckme quit first build

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
