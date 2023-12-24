a446167c5bb06e2e770d35b108e96ec410f83751
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:09:47 +0000

    chance black and bar post

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def find_min(lst):
-        return min(lst)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
