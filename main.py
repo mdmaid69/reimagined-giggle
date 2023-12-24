3644a5160787c83ec5cdba87fadfdb6e58db487b
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:28:25 +0000

    instead second show mainly lead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
