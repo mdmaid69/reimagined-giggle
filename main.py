028002ee644542c22f969f84bd24e6c91b2133c6
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:07:48 +0000

    scare home floor frighten string

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
