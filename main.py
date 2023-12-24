ff8453c29d6fb8325dc27dd3dbe9a4af07accf76
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:58:32 +0000

    yours stay statement would female

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
