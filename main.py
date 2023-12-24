96ce9f253de3efb6f483b08a69278378cbc832b8
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:38:12 +0000

    travel we centre habit Ll

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+print([x**2 for x in range(10)])
