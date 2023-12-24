837a56bc02e723e7606b641fbb3f8ac9c1560ad1
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:54:16 +0000

    concentrate tradition listenfor entry tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
