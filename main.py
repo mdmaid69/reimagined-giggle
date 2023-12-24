9968415bdf6b9833997a563b57c26124dbbc1d1a
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:30:45 +0000

    throughout appearance thinkthrough urgent nothing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def reverse_string(s):
+        return s[::-1]
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
