222fc4db76abba8cac8f0a160a975e3a61601212
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:57:50 +0000

    way return father play meal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
