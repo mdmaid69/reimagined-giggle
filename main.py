1bec46b0e5b16ce051975b84a1381da6db910d19
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:57:19 +0000

    people phone case though size

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def cube_number(x):
-        return x**3
-def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
