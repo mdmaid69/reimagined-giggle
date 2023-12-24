7d99d5a952e8ad66d37fc24301e82f718b187b64
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:16:41 +0000

    win slightly rarely this godown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
