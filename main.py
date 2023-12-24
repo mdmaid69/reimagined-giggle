8f33e57de106b65d107c021be01f877cd734261b
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:20:17 +0000

    shop square depend block workon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import random
+print(random.randint(0, 100))
