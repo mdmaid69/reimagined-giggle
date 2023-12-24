d0abbe8a48b3268d7c3a715f4b63069d8dd13701
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:46:23 +0000

    post foot under pay 179bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def sort_list(lst):
+        return sorted(lst)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_item_count(array, item):
+        return array.count(item)
