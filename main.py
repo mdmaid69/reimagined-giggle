d077e74aff0e653ddbf05fdbe34abb7186abc103
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:37:18 +0000

    call correct should solve clean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
