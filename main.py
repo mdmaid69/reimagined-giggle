c6d9b30369d2992e72df7563394061217429aa29
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:11:25 +0000

    trick underneath compare it pull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
