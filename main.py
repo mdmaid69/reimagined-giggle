9807d1406dd42b6ee96a4d465e652f985f6856d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:19:19 +0000

    bus likely plant ordinary traffic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def clear_array(array):
+        array *= 0
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
