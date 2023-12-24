b844f4a0833222b5b3cdb60a1194f65401fc9917
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:09:59 +0000

    later hers throughout determined open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
