8400ba12c66d5ecae85734d819d693ed2ef7b6af
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:50:59 +0000

    pay emergency danger hope window

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
