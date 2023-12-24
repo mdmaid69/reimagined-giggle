42815608161856b0d4e775a10c71576128cef2b7
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:02:21 +0000

    cinema explanation read Gg case

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
