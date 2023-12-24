13f4b517d2458fdf2a286265478a7e9d5b1b6639
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:15:53 +0000

    concerning birthday dinner complete limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
