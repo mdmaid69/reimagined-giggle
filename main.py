8e1a2b3d8c1ee7c3f61e1975426eb7c1de69b9f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:45:29 +0000

    thumb embarrassing building naturally how

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+text = "Hello, world!"
+print("Uppercase:", text.upper())
