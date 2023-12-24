5919d4b31b5bd3723380afe0c368a5846eac46b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:43:25 +0000

    round bury edge sad healthy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
