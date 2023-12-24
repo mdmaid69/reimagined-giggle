5a2bb693a7c8a8dc9c11725e0646242f7fc90167
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:07:45 +0000

    story suit breath hit fasten

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
