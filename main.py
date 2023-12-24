f081f2d7dbdbe86a42a64fec05fa373ca038e207
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:50:58 +0000

    not holdup prepare legal single

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
