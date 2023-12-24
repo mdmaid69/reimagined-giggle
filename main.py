bd6f4aa3d840bba82160fb39feadd975459f1792
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:40:47 +0000

    otherwise permission actually lot show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
