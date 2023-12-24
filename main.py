f43049f3859085a2733e420e04df86a7329a13ce
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:43:03 +0000

    ago lift reply mystery give

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def cube_number(x):
-        return x**3
+import time
+print(time.time())
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
