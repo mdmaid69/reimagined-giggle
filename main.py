131229306066ed991598c4cceebd076806cc63c1
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:11:09 +0000

    depend move opinion example fill

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def convert_to_hex(n):
+        return hex(n)
