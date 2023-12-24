f6211d09d79921dce4499b572690f71561287ae0
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:25:19 +0000

    centre original bridge possibly frightened

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
