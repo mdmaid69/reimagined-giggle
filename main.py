03b9efaa0fe7e32e090a94e95d62b7e6f09ac45d
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:25:59 +0000

    community cry century she half

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
