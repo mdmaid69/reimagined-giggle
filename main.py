519d2a07963a6222aa9b1276977133218ed8aea3
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:19:03 +0000

    relative Ww responsibility approve bottom

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import random
+print(random.randint(0, 100))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
