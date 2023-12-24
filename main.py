528eac17556d68de2d1e987e9bdd5d48d9b970bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:48:02 +0000

    twist like thinkover entry bringdown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
