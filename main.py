6b1b752a8eb5c63f2d938436cd5e748eaa04dd1e
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:11:45 +0000

    value sort fine brush share

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def find_max(lst):
-        return max(lst)
+def calculate_pressure(force, area):
+        return force / area
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
