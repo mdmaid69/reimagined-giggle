79cc1f7ceb9aeab893d9391791da9c36ee40535f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:17:07 +0000

    natural deliberately lock maintain naturally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def add_numbers(a, b):
-        return a + b
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
