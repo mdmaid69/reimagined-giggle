f98348209c111e6a3918eb5212f4cff180c048dd
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:24:44 +0000

    transparent quickly drink turnout original

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
