d5f42dcf1e826936aa7dbae4fafe22c9c053bc25
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:48:43 +0000

    machine breakin there wheel unable

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import collections
-def create_priority_queue():
-        return collections.deque()
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
