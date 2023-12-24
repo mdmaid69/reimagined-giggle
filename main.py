8ec1fbc50a0b77cae8f4d6275f64ba343e3c28fe
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:48:17 +0000

    lot little promise traffic herself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_average(lst):
+        return sum(lst) / len(lst)
