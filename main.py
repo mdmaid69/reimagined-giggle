87147039fa1bfaffe1709f2944d9c1e478165420
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:01:08 +0000

    head crime situation year his

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
