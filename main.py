dbf9f1cdf418e638c083283bba55f21f2af08f8e
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:31:35 +0000

    undo difficulty another public keepfrom

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def add_numbers(x, y):
-        return x + y
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
