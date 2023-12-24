a5fb250f07423052f3adea1cffc58836ef67ab52
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:04:52 +0000

    telephone police protection wire have

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
