9030a606e202f8a48f396d61c8ac3ccb0c03a710
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:49:58 +0000

    alive breakthrough prevent big steam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_length(array):
+        return len(array)
