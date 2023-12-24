1185e481e42bf89c96b9f5fe2fad89467a8d4415
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:19:15 +0000

    listenfor bin straight say road

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
