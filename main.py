cb5240eb2e7928f4b3b891cd11d852600fa21415
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:53:36 +0000

    shape various finally game advise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
