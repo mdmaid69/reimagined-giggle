4365fba12760d30378ed800ef94d8b9beee44bf9
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:42:45 +0000

    march try man ability cinema

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_density(mass, volume):
+        return mass / volume
