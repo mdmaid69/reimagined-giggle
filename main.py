0952ddd59d2edbf3dbd8f29cbe29b5f17e0c72d6
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:20:34 +0000

    state care sink exit watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
