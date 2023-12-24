9828eee5e83f0949afec2660c908e4e8fbb9053f
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:25:58 +0000

    recover independent accidental belong reply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
