b02628303c4be42afb66a7b4edde4a7b9182dc81
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:26:41 +0000

    maybe drive statement powerful they

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
