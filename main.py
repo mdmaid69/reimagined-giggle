092cc9498c3dc111ef4eaec05f3aaa2f1817ac8a
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:12:53 +0000

    always fortunate characteristic spring invitation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def is_odd(n):
+        return n % 2 != 0
