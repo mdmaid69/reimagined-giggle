dbc5c4848da37d520038790b10cea6b909c3d375
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:24:37 +0000

    trap lead strong name suggestion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
