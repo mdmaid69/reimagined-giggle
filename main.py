2e9c72649f0c96cf2a5b9e482e186550e59fd3a5
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:47:12 +0000

    official geton closely giveup report

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
