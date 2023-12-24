ef3e10071f0b3b062f68f73803f8fe3fca8659d2
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:24:18 +0000

    onto except low friendly straight

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_volume(length, width, height):
-        return length * width * height
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def count_characters(sentence):
+        return len(sentence)
