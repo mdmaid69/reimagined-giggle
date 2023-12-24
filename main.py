0fc549c32c3e06d8441de4f21dccf52cc054454f
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:25:12 +0000

    person 566end Mm attention quantity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  def count_elements(lst):
+        return len(lst)
