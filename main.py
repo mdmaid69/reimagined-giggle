0e2083635c278d47db039b65d4171bf88e4df1b2
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:55:51 +0000

    greeting feeling patient not shallow

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def count_elements(lst):
+        return len(lst)
