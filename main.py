50c934637a6b1c6abfc00d83f088a5d835bfcf4c
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:59:09 +0000

    Ll giveout spin importance surprised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
