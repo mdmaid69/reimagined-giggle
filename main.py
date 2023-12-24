5d377c341fed7aaf39b86a01c6ab888eb5c9af67
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:25:12 +0000

    union us into friendly stone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import math
-print(math.pi)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
