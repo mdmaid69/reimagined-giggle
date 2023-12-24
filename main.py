70b87932d600adaa9664f88d6481fe48682d6727
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:03:25 +0000

    computer skill clear concentration must

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import json
+print(json.dumps({"name": "John", "age": 30}))
