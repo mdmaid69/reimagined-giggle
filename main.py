5922dfca0d99a3317b698f4a636b75cb8d78ea45
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:12:24 +0000

    translate trip someone disgusting sing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
