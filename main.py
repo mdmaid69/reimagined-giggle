5385fa327ca81a07d388452fab55e85693f5fdd7
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:04:42 +0000

    suddenly perform pure complete sensible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def convert_to_binary(n):
+        return bin(n)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
