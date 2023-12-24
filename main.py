3b89f8a5e2e47eb71eab15ba98dabb20c1e9b00f
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:27:52 +0000

    detail invitation effort violence fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
 import os
-def get_current_working_directory():
-        return os.getcwd()
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
