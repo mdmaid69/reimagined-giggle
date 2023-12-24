03a95985661b1f52e23327e14a893c7ad506c608
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:48:19 +0000

    history neither concerning Ss now

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
