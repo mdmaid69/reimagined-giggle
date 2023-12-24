2efa29393baeec14d8f4c0b8cb57542b3fbc2cbc
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:31:33 +0000

    attract downstairs radio book train

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
