8b008f47eada6d64dd463480d6fd1b6ba0bad5fd
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:45:20 +0000

    rid platform pen command its

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
