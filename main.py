87ab5f7bc772a92d6fca8b4c6315badc16307d1c
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:11:38 +0000

    sea punish first production imagination

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import datetime
+print(datetime.datetime.now())
