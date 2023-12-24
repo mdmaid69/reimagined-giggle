0681ef0f0db3946fe18bb8ba2b8c275b51faaf4b
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:29:58 +0000

    cause cheat call broad question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
