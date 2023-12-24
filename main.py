a937b7ba773448e32674340e659eba605edf7d99
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:15:49 +0000

    long yes crisis lookup apparently

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_power(work, time):
+        return work / time
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
