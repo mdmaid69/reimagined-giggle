e3ff3df3e29044414b0fd6ab83092fab5a817704
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:52:40 +0000

    leave week clear appeal proper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
