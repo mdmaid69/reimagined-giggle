fe360c7ca15aa8653a634ecb14c7efe3687631dc
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:53:05 +0000

    bunch swing him meet run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
