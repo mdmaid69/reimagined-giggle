d106b799cf2fce9a90cd614b290e4479fbd93210
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:00:51 +0000

    look scared sister suggestion group

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import platform
-def get_os_info():
-        return platform.uname()
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def cube_number(x):
+        return x**3
