afe7f21b7f86bcefc61cea065b929870a9422864
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:00:24 +0000

    home get equal high steep

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def is_odd(n):
+        return n % 2 != 0
