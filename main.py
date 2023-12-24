b406e8a28ea8719733017d5d8e5273217def83e8
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:20:03 +0000

    weekend raw practise young organize

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
