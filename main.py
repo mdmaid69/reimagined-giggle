2163d3dd4e1c6832114660cae8fd3ba5287bc16e
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:22:54 +0000

    well usual delicate every wall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
