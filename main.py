e8b29464295cacbc144ed2ccd4f70b890dde2f83
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:13:50 +0000

    father brave fashion exaggerate use

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
