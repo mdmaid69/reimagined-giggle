4eca715657a51ba181bb7579dad1514756bf3ffd
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:14:10 +0000

    giveout it condition issue emotion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
