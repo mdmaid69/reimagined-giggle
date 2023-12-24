c27292763a8954a6643cfae41baff5e4025f150a
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:37:07 +0000

    breakfast fill certainly tax regular

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
