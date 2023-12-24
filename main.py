b714057aee11ebda0ac725884d193254101914d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:18:28 +0000

    finally our rely keen sale

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+text = "Hello, world!"
+print("Characters:", len(text))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
