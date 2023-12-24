501afed696c293811a0975d7ec16115e3f1e51e9
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:39:44 +0000

    person conversation inside lot small

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_array_as_list(array):
+        return list(array)
