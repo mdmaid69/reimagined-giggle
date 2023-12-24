b77ac5e425dccd97836812d084b20b5ee5c62ae1
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:40:12 +0000

    mainly size explain coat taste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
