8c45936425d3d982c2bbba37bf02e24d050fdabe
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:07:07 +0000

    calm bang accidental brain charge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
