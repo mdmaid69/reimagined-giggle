2182d06a1397041d8e2773a2631a1a3ccc1135f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:24:50 +0000

    quality 569enjoy public suggest freeze

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def count_elements(lst):
-        return len(lst)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
