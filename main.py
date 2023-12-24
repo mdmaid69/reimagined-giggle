3c4b2c66f701fcb249caf10979af725b4c566904
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:14:02 +0000

    telephone settle personal safe my

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
