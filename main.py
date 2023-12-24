8108d83aa1c91d9d3de98884ba0a82a236194ad2
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:22:15 +0000

    right passenger comment settle meal

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-print(sum(range(10)))
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
