1497400e81ea61dee60be7442c59ebe5d57ea17f
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:08:29 +0000

    bottom mix protest family legal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
