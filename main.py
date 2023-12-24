baf5f5ec111b4fceadecbe3cfdd78f7f850e17f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:17:12 +0000

    street near keepup come culture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
