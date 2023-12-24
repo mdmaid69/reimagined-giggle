2d7e920d297ef66d10110c1546f263834f14c182
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:29:22 +0000

    development pinch giveout healthy bringforward

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
