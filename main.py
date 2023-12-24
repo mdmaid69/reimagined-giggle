dffb867de14ba7aec6416d5a7cc22fb7b15a071a
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:11:04 +0000

    yes strike dry measure unit

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
