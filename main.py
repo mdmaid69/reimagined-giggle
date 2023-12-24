34bfb3b82ad09df27734ce0bd9577bc30fdc42c6
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:40:55 +0000

    create store suckme scare which

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
