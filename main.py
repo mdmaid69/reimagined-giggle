634547dce233ea5393293ee26b53fc977786c088
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:18:04 +0000

    advantage precise switch value live

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
