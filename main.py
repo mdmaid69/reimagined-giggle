0d38eed4d61f4cc8f9a521b1a875b06644a1dbfe
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:28:42 +0000

    down know purpose behind discussion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
