23f990a10b39d9b5b2bb4736b0d1726508b4b940
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:03:12 +0000

    total possibly reply book scheme

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
