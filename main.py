21e8a291d04b2ea8b367af5e80541f4381bb9903
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:24:17 +0000

    569enjoy unfair workupto test quit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
