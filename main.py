2636d716caaaf3a0f189799f97ada0c5c41af178
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:01:54 +0000

    best together computer so whisper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
