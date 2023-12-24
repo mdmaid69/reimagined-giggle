de972545e8f8380a51a4f8045724ea08413847bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:28:42 +0000

    black our children smooth wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
