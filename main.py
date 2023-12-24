24ea679cf49e2264c1eb3d898b3d82c842566e85
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:21:34 +0000

    surprise thank patience direct her

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
