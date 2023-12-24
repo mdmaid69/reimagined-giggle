a70f715e3b186ef2b545c6d1f42521addcac01e6
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:59:10 +0000

    marry stretch factory knowledge concerning

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
