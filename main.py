6ab1dd7adb08597dde2f3de16c6aa405a1a9b390
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:50:11 +0000

    difficult accept complete progress shock

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
