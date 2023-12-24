94c2d2b027804368c1db766b568c1b3f902e9600
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:18:28 +0000

    wear tidy beat rule meaning

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import random
+print(random.randint(0, 100))
