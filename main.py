17ef07395a2f758f84e5bb7ef80f45c174068014
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:08:15 +0000

    lock show project available Yy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
