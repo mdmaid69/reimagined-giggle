bff5ab646391c0812e26279fac6c96406cf3f6b5
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:59:05 +0000

    switch thing nerve ask soon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
