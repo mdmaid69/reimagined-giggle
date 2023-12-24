bedad8b75e12e7edc419dffbf75bb9fc289d8997
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:23:35 +0000

    suit strike benefit shop fit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
