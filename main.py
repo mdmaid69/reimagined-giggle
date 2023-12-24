4f87d4671ad19a8669b7d906078d6ee871b44a51
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:00:33 +0000

    blind delicate smoke exaggerate splash

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import os
+def get_current_working_directory():
+        return os.getcwd()
