29722b7a88076b23ff052bad063c3119bff435f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:17:35 +0000

    any correct relationship declaration successful

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
