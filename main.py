a265aec530d5420196bac56e54186114e0affc1b
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:34:25 +0000

    instant generally boil priority phone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_density(mass, volume):
+        return mass / volume
