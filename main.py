48e3fdb24ab7f36e423d5cfefa0691604cf92fb5
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:30:44 +0000

    expensive gift suppose will at

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
