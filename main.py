b9628b48918bd8129232d1d6e64efd03e886c03b
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:19:33 +0000

    pain smile back dig union

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
