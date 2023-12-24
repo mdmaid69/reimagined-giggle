100538d6ec61fe8bd88351d7fa313a3e9ec117de
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:01:34 +0000

    dark talkoutof carry frequent point

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
