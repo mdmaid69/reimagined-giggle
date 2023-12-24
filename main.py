772c9155296c37c753f304a9b1425c4a478cc4d6
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:39:05 +0000

    crowd threaten convenient mine uncomfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
