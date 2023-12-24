2d8fb98d56121ce1728f6971aa1379508ff89410
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:20:04 +0000

    shame mouth taste formal remember

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
