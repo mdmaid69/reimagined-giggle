e043ce08c0c66527f01f8c2d2cffbc4565f91939
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:30:50 +0000

    urgent spin constantly prefer downstairs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def square_number(x):
+        return x**2
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
