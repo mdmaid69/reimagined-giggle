41e1616a952129eb967f75c6454ae9d2a6951ebf
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:14:26 +0000

    positive worse our takeaway obtain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import sys
+print(sys.version)
