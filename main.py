0eba8356d868ab1f13b3b1d73ccaf27745f75f48
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:06:55 +0000

    pupil but age fence heat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def square_number(x):
+        return x**2
