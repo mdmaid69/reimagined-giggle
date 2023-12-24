2a5401fe5ff19e1c8ca22ca535fe3d9e0573d4ea
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:57:05 +0000

    decide off aim unkind argue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
