5ed3054d660419d241cb8f13b3402186cddbdec8
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:31:09 +0000

    produce leg coffee divide either

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
