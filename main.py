9fc490bb323ba6838b68fc92f560032e83d4c4e7
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:39:13 +0000

    normal treatment miss talkback modern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
