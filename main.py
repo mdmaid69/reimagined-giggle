8a4bfa0e4f287b0df98046581b9d0ff5bb1ac7f1
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:00:03 +0000

    basically holdout office special eager

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
