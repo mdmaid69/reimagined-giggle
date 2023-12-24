6bc0a7a6bbf282577952202e8ea66fe37d5fe525
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:12:15 +0000

    afterwards strong aware still 565end

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import collections
+def create_user_dict():
+        return collections.UserDict()
