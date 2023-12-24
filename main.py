5422849f5a125d89143e82a1f536ca9656f24319
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:47:12 +0000

    cut getup they grow pause

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
