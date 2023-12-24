3ad547475edf79cbbc4bca37dc507e0ae1c2bb74
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:46:57 +0000

    miss offer somewhere replace smell

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
