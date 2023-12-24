99931c88b0331f100e9e12f6d0ff7531ea1cc107
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:16:42 +0000

    nowhere basically cut thinkover no

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
