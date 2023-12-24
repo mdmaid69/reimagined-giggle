112b4ddb84f3c97faf7778579c9ec69122b1dd70
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:39:58 +0000

    usual bridge satisfactory development rub

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
