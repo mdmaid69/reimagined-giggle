966c38ebfccc80f15a4722b78a086597dbe51674
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:30:29 +0000

    face target hit effort by

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def add_numbers(x, y):
+        return x + y
