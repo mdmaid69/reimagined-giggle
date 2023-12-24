657b37e46e767c74d3c856d3ec2cb0ec4caefce3
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:23:40 +0000

    division practical deliberate fork then

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
