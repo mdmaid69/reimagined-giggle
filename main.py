5bd7c0af1985841478b411cf400be3a1b09e04bb
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:55:55 +0000

    makeinto backwards or forward decision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_acceleration(speed, time):
+        return speed / time
