cde9dc84ab0a75b1cc64bb6b16b6aefdbf7c4e55
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:03:19 +0000

    rank operation conduct school cross

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
