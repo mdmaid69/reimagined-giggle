c6a6b29167f9215883908bea9c6dc6142e4ba158
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:06:28 +0000

    scare commit protest sound gooff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
