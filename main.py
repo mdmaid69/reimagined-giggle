6fe19e4a7856f3ab2a300a0167c763f1e1791f29
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:41:41 +0000

    may prove underneath settle surprised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
