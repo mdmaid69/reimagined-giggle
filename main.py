80b9eb8cc536a888faefee103392cd8908d3b5de
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:17:23 +0000

    ready certainly mistake bringforward stage

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
