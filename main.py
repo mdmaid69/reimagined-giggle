898be8a65173ca969bd342ac29e27fb082e67ab6
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:55:26 +0000

    book expression check beautiful big

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import math
+print(math.pi)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
