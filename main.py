f7a8806e08339b14a53af21961ce765417edd151
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:29:39 +0000

    eager case any variety basically

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_list(array):
+        return list(array)
