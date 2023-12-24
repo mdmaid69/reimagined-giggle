56179359a4eb9fb61a766d78900c4020720b4a7e
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:08:28 +0000

    care still friendly people whole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
