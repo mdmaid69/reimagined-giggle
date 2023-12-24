ae257505ae502f1b9471c076f18f659868e0405f
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:48:48 +0000

    urgent quality 564encourage watch check

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
