e0dd9313342badb31cfd5a8d14a27746ffbcfd83
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:29:00 +0000

    family wander clever indeed wide

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
