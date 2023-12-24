714674a92e12ff2a90210aa33c96e7d0bf2b591a
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:15:22 +0000

    mistake voice whenever influence pressure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
