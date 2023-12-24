84e7ea2b963563ab54a398a46d1d688142b25e65
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:04:24 +0000

    regret comeup touch trap sensitive

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
 import math
-print(math.pi)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
