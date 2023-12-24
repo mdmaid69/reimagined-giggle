3db5a41e2918916ac76f52dfc71f661bc7687a4d
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:19:37 +0000

    club lose rob scene Ff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  def square_number(x):
+        return x**2
