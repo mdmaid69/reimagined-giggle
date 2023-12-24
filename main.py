e929626f1031c2f19d527acba7a884d1a789c22c
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:42:05 +0000

    shape really noisy difference slight

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
