42c20a62de0ec9ba3f6fdc2817c64a62633108fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:18:26 +0000

    perhaps copy immediately comment like

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
