0371800e21dd0147ea198297a60728f0d08f548f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:45:41 +0000

    clean eye themselves passenger warning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-print([x**2 for x in range(10)])
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
