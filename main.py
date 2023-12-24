7969a32c0a31afbe0b1f4c3a6876ab9d9da55213
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:18:25 +0000

    switch else issue tape broad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
