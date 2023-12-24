c151c93606075ae217d5a7e25a73a1516923cfb3
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:34:05 +0000

    marry Ss wait here left

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import collections
+def create_priority_queue():
+        return collections.deque()
+for i in range(10): print(i)
