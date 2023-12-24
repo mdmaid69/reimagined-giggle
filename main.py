3df6b07f14e5be9d1b4359400e08f5d4d91a0340
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:42:10 +0000

    announce shout attitude petrol recently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def remove_duplicates(lst):
+        return list(set(lst))
