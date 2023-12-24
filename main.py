8843ba26f8c207f1057286f3b8959844f41d4efd
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:31:27 +0000

    uniform ball pattern repeat pen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
