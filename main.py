320386946174c40a538b3b7e1a05a68d22ec9a7c
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:18:29 +0000

    comeover achievement afterwards be promise

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_set(array):
+        return set(array)
