572be066f3388e857800eb4860607f2e28c7c173
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:18:40 +0000

    contrast worst pin air minor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def convert_to_hex(n):
-        return hex(n)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
