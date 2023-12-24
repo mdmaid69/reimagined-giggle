0d1a7f8af5c4f72b6a192bf54b7561b2afa669b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:45:27 +0000

    material equipment part cheat ability

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
