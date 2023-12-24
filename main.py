24759bc9085ac12985ceab95e07154ab22cc8201
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:30:44 +0000

    hardly beside suggest either giveout

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  def reverse_list(lst):
-        return lst[::-1]
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
