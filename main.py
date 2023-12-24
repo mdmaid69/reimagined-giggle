2bab9654bab2ee22656e27605c6a026db5671998
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:56:26 +0000

    yourself again prize at one

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
