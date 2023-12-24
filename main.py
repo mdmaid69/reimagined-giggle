9a82cccf9930f2fc582532d4aa18748112cc2ec9
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:52:46 +0000

    wind light runaway raise nature

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  def is_even(n):
+        return n % 2 == 0
