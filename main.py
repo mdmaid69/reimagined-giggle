f29f6070171f77eeb25eff2f5e55432536eb3b1a
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:59:47 +0000

    unlucky about blue bite night

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
