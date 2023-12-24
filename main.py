770a12aefd708ed6c37bc6e72afc87b511d1a909
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:34:48 +0000

    suddenly you myself during club

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
