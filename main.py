6f99a5085c81dda9a1c8881695e9159cd00bb160
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:53:25 +0000

    quality compare winter level ashamed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import time
+def get_current_time():
+        return time.ctime()
