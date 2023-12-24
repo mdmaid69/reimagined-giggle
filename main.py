cd1a03c396d2ae4b16eec84ead97abce130af5e5
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:10:57 +0000

    cross classroom slide keepup better

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
