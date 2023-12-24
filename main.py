ef78e82736cfc8d4c466ddd59261e85ef4244540
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:18:26 +0000

    eager something successful final permission

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_array_as_float(array):
+        return float(array[0])
