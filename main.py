fb14de92d191eff5f8321b1588db7414f133e42a
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:43:23 +0000

    emotion shelter last peace across

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
