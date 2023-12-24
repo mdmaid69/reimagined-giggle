5f1948f1cf1045e3b913b68a7a31603de88ba260
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:51:17 +0000

    serious sister although shock offer

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def find_max(numbers):
-        return max(numbers)
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
