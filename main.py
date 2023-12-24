ee6c6049b568f3136be0354fa0bd945b34aeee70
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:05:16 +0000

    callup completely lend lead patient

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
