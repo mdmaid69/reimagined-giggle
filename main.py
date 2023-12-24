7c01115d6dd60b44c061173303faeb7bc6897164
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:18:28 +0000

    nerve church pipe hospital too

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
