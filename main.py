f723d6d267fff723621ae63af28e5966706749a6
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:20:03 +0000

    illness style its scared backwards

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-print(math.pi)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def find_max(numbers):
+        return max(numbers)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
