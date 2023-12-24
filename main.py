66b9f5d6de4d25b6d207764db21c1b08373d55b0
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:22:50 +0000

    pot turndown earth care do

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
