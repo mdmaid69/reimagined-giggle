1f44b94451bfcd1e795a35c3070b1dac32422c96
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:31:57 +0000

    congratulations name error will development

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
