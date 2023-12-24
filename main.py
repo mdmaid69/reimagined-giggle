b01764be30920673d96e5e601f97fb354fa6283f
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:24:40 +0000

    breakinto runout task unpleasant split

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
