8a4d35075ed16a605dd9f3d1eb5d54943339689b
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:23:56 +0000

    expression always turnover rob frightened

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
