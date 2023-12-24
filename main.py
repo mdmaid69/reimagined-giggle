27b887ed2c914d4726480a449c43718e4b2366a8
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:13:44 +0000

    shop 565end sometimes maybe reserve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def calculate_density(mass, volume):
-        return mass / volume
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
