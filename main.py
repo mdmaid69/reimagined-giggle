580e869f5a2f4cd4f5684e6fa391048193973406
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:12:19 +0000

    ashamed uncomfortable born patience spare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
