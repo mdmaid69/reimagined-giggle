5472ba75d251b4688700597d4eb79a7c6062d302
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:45:56 +0000

    takeback pay properly everything clothes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
