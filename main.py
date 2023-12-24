88ae5c69f1af1341796c84db700cf156536217a0
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:29:57 +0000

    weigh common do possibility but

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import re
+print(re.match("h.*o", "hello world"))
