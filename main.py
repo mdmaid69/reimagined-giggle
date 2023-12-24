90357ddd77a036350cba671bbc4f6319a2b7ab59
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:19:49 +0000

    sport Itself holiday clean high

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+for i in range(10): print(i)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
