decbc5e7fe4fcf65bc2de38b0619612079bf89c6
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:06:04 +0000

    water minor centimetre influence once

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_pressure(force, area):
+        return force / area
