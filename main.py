d9160ac656a756b3e5ca7ab1157d06567eb7fb7e
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:40:45 +0000

    free offer size phone pause

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def square_number(x):
+        return x**2
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
