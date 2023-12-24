b9aae2ef7fdbf7f8a1c75034638c08342729dea4
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:06:00 +0000

    OKorokay desperate level hello under

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
