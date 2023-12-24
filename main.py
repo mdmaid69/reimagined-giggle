9da6f6d5233f6b3dd5858adbf3d2b1710ce374e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:41:52 +0000

    comein material age disappear live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
