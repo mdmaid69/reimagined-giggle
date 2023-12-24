c96e82b190b726259d66ad9d32c51295cf6d13d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:31:37 +0000

    badly write block demonstrate like

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
