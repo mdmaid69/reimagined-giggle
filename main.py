9684e83ad2a41923cc278cff623c27b78bf3e905
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:02:18 +0000

    Hh together so holdout urgent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import tensorflow as tf
+print(tf.__version__)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
