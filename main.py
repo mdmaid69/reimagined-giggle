dff0f7fb76bcfb0aefe2aa632d5603da2677ba90
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:40:20 +0000

    care congratulate ashamed stay joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import re
+print(re.match("h.*o", "hello world"))
