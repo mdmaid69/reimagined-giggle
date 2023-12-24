1532c0ba1509aebe3923479190d9e307b044c0d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:44:19 +0000

    load listenfor worst violent radio

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
