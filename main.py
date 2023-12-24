c2cb68d155db338e2ba665307da53391ffc4126b
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:33:25 +0000

    suddenly dance phone so considering

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
