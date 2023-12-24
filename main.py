456f1ab27269a76377dc05c1ba60733aba659f88
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:19:59 +0000

    thick quarter soak roll offend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
