9e3b5dd3d5d7d4809aa3b2541f204058d22afec2
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:21:18 +0000

    nature goout unit miss used

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
