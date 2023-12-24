1a749835c1e80fac3b1767ef0c6b23ace930f0f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:50:23 +0000

    fire lead warn bin home

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_slice(array, i, j):
+        return array[i:j]
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
