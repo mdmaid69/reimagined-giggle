ae00fa464158ab1e05cd136a68b1aa19335a4a9d
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:30:35 +0000

    pile collection situation extremely note

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
