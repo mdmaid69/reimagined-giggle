5688a6f151df78eb08e01800074dd692e219589e
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:33:52 +0000

    marry quickly pay cut pretend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def square_number(x):
+        return x**2
