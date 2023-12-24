18a0422e4a510f0557df2afd62f55a6bb6d0f83d
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:53:50 +0000

    experience raw deal share shoulder

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
