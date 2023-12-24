d22509192fce78f5546518c12f6c8f8120cfb0c1
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:02:28 +0000

    hardly narrow less autumn hit

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
