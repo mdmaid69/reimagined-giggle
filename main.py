b09b4b6cf237168ae588989a8366a8d4613dd7d2
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:04:58 +0000

    decision same tragedy say aim

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_float(array):
+        return float(array[0])
