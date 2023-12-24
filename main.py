94bfb5ff5f2bdb0e322c904abbbffa2f84d4d612
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:58:25 +0000

    allow wind Kk shock smooth

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
