da8cb2eb1a9afcd99599cd9ca83f5f0bcfdcd319
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:23:44 +0000

    fire error suit lie measure

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
