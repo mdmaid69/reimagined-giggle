050c8068100f666d678e15b85ee44306574889eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:20:01 +0000

    curious brother free recent violence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import sys
+def exit_program():
+        sys.exit()
