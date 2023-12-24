1fd0b91fe0a1a9165deeac59b5197f42cd6de56f
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:53:25 +0000

    computer concentration deep life yes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_union(list1, list2):
+        return set(list1) | set(list2)
