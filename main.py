7bdb4c142682962f48ed274a7091369f169b85b7
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:09:19 +0000

    brush probably pleasure quick long

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def convert_to_octal(n):
+        return oct(n)
