e2d30348a4f3fce5e472fb496af5d13e611bf677
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:38:54 +0000

    callat stamp happy brush hungry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
