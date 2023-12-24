5fbda36bf030ab46d6fd97f9483b227d9b32052f
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:47:15 +0000

    less moment usual climb dangerous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Characters:", len(text))
