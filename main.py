25ed83dd0dbcd4b606050e75257f8afde262711c
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:37:05 +0000

    flow fight contact minor else

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
