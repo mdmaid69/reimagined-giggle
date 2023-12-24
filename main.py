2e7bbd3594370c3851d5d2ea79d93a87f2d0fb7f
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:43:54 +0000

    kind impression perfect ticket under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
