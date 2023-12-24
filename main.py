3887ef959ceef599be2c572311ca2ebfc41397cf
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:47:45 +0000

    yesterday occasion heavy weigh future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
