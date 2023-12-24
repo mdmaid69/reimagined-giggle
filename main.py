63d999e38014092b9ea62f00a7fdaff50ff04004
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:27:12 +0000

    spring many account takeover protest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
