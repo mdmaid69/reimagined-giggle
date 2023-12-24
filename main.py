dee0ab7764c55af9bed8bcfa0fb4cbb793666afb
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:40:59 +0000

    well accordingto many hard 568energy

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
