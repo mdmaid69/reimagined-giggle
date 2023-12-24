8d91d466558cea734bf4d6ed52c9451345fb86b7
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:39:46 +0000

    mail proper temperature bury workon

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def get_array_as_list(array):
+        return list(array)
