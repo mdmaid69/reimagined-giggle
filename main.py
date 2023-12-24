a8ebcb66a676f8e210cfab385890ee65b080823d
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:06:58 +0000

    chat rain law today quit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
