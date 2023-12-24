52d6757a53663de0eaf1c857c674d6669817ae35
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:27:57 +0000

    separate alive story geton direction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
