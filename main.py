a8cc636cc642cfcd4dedb8afd1d8cd97682cb301
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:10:30 +0000

    or true character take perhaps

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_typecode(array):
+        return array.typecode
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
