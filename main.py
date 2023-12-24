928e47b770ca30525da0ca192b3400801736a767
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:12:47 +0000

    provide hope country over takeoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_volume(length, width, height):
+        return length * width * height
