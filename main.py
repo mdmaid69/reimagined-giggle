be138cba0dfebc7a1c939c5b39b321fab45be7fc
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:11:15 +0000

    move contrast murder Ii OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import tensorflow as tf
+print(tf.__version__)
