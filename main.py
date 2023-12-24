eb9270cb5123f95e573098ccd318ec10b4926ad1
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:41:13 +0000

    able proud soil convincing pile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
