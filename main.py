012c30cb29b2a879cd54037b4d15945e54de9c90
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:22:08 +0000

    typical feed stand swimming miss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
