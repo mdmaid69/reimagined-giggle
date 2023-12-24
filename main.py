fd67ad7be85b0d72f846df692cf01f4487ba7001
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:58:46 +0000

    there unlucky edge message rest

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import tensorflow as tf
-print(tf.__version__)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
