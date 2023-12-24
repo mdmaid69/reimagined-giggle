87a72a10383b0a478c248106668f13373dc35557
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:26:19 +0000

    proper prevent OKorokay steer proof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import sys
-print(sys.version)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
