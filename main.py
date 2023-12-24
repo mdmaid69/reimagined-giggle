d33ada548f1e78ddd4dbd47b0e14a70b5d30bc2f
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:08:25 +0000

    plate basically blame attractive flood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import collections
+def create_stack():
+        return collections.deque()
