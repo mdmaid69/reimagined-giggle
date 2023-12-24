9661d46cce9dbb19800196819225b0f379afc11b
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:04:27 +0000

    sit sting type tall criticize

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
