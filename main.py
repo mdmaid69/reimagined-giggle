e75125d9ae2d957a43ca1a289a8f43d06328673a
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:11:00 +0000

    pretty can block flat sign

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+for i in range(10): print(i)
