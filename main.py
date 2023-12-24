58e299ab8f77e2ec8a09adef9dbc60610996cdf5
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:47:34 +0000

    lie introduction return scheme usual

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import collections
+def create_stack():
+        return collections.deque()
