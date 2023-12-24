9fd01c6bf04d2851955c522716b88644d039e857
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:12:07 +0000

    cold hot conversation price comfort

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
