27da31c1716067fe771bdbb1b1f14506c4c2fd9b
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:07:05 +0000

    extraordinary until still half open

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
