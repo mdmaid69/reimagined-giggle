22ca9bf7ebdfdb448150ae2078a8c7da3ea985e7
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:32:20 +0000

    drawing most store guide week

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import getpass
+def get_username():
+        return getpass.getuser()
+def add_numbers(x, y):
+        return x + y
