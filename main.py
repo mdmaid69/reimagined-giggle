4c6c81dba16e81022735fc43ad2b9d30645afdb7
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:17:50 +0000

    burst black basis many repair

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def add_numbers(a, b):
-        return a + b
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_acceleration(speed, time):
+        return speed / time
