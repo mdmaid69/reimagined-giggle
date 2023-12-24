edca21be511606487da2d78adb1c7603094495b7
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:45:39 +0000

    brave form far takeoff hurt

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
