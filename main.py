1481ac9039bf92317095ab28a2e074a431f3bf77
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:55:40 +0000

    same effort question gift talk

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import array
+def get_array_as_repr(array):
+        return repr(array)
