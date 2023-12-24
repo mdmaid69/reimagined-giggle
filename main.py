26cab8b76834ffda19e022b683f65a5995a8f789
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:42:43 +0000

    add fetch sort bottom thinkover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
