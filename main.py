2d0bc76012448fb9137cdd15ff54bb3c953e2663
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:44:16 +0000

    mad where benefit scheme smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  def cube_number(x):
+        return x**3
