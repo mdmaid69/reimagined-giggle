5d1ed7aac9ded8cea25ec789a878f48f3e71019b
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:02:00 +0000

    sign Dd measure tomorrow nonsense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
