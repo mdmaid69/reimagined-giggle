415ee29a71fc6650c09c9db62df4365d76dd51ce
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:35:35 +0000

    bringback really truth snow direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
