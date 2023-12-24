4b2a5381f142952c63603759c40e911ae4013481
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:37:29 +0000

    preparation through much wave actual

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
