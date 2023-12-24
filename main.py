abce7fcef0d60601db9a43622ec8093b32f17c2a
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:18:19 +0000

    proper opponent powerful damp wash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
