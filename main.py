258512e7a5189f92315c2e52e780e12add4df48e
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:36:04 +0000

    find add slice afford light

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def calculate_work(force, distance):
+        return force * distance
