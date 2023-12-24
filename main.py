8b1f04932256787fdb34708a77e780288061f92b
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:24:18 +0000

    wide feed straight grade Ll

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
