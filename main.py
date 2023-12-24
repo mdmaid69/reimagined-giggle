f32c5be02fe9e1b5ccc82aaf2047c98b1fcc876b
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:03:01 +0000

    art soon famous cough principle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-def exit_program():
-        sys.exit()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
