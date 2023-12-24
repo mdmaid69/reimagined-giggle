26fa5800b24414d2e5ee8e598b2e5132e698d35b
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:25:25 +0000

    natural comment likely mean tall

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+print(os.getcwd())
