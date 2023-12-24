a12cfa662ed1fde2353b87c6281a14284c9782e7
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:03:49 +0000

    uponon put form Ee snow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_density(mass, volume):
+        return mass / volume
