8e9a638bc9a559c360a1614cbfefa8acdc1638c8
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:58:17 +0000

    nurse gift environment hurt kick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import os
-print(os.getcwd())
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
