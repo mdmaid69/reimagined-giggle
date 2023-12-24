36957bfd9fccd6cf8a77706ea2efb53a001b98dc
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:42:20 +0000

    heavy equal holdup benefit Ii

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
