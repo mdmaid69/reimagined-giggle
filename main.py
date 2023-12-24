c8ca220262cce8aba9fe422d1acd88dffc8c3eee
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:34:08 +0000

    wise usual dull intelligent deliberately

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
