a558c7b682fcb511e177cb6b625c15a35d2f298c
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:07:22 +0000

    walk question punishment well smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
