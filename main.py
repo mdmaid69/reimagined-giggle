20e37fc6b03206ff1a9feae6993bdef994c3d3e7
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:48:32 +0000

    act single runover prepare gift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
