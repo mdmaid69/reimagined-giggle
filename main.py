d23b04de7b9a999de13f40c7bc2136ceae0b94af
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:19:16 +0000

    plenty ordinary communicate stare Ww

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
