17a533ce004de9e36a42bd663a356db12b4e2527
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:05:25 +0000

    sort related left score wake

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
