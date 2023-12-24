2ad63d70b3623216606e70aa1e4a30bd41540a20
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:21:03 +0000

    especially possibly interested share hold

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+for i in range(10): print(i)
