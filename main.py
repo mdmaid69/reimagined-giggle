87fc9e1e0ba6136bdfd3a532887bc3293bc0f1f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:09:47 +0000

    sick unfortunate tap frame holdon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_str(array):
+        return str(array)
