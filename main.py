74ced89c2173688d3e4d0ead19d3a5be4b338cf6
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:11:12 +0000

    advise eye train old spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
