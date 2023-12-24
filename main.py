2bfe09a6c817035d4f1aa46e4529e4663b3ecc24
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:20:37 +0000

    cry consult evening charge respect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+print([x**2 for x in range(10)])
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
