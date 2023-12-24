79c4030284d092379acb36fd3920897e51765db5
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:49:15 +0000

    angry Mm change rescue usually

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import sys
+def exit_program():
+        sys.exit()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
