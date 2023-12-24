2f5e57d7a6ea463de99ec13f727eb1cd6de70695
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:20:22 +0000

    fire luck win thanks Ee

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
