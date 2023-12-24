f9b8cdedf17bf7a8974b395a05f8deeabd2f305b
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:51:46 +0000

    thirsty wing think chat communication

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
