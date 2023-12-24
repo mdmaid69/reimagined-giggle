84b02922a45a49a6a4698735eb5ddbd2ca56cbf6
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:08:43 +0000

    good public bread goaway test

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
