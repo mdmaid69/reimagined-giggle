4b1688ab41888b8a4989ad77fc4462ac7d8fe1ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:28:57 +0000

    greeting conduct mood soft mistake

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
