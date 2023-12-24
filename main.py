9673c0b087659a99e8d42f323b6b37e24a767f95
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:34:13 +0000

    suck under worst swing future

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
