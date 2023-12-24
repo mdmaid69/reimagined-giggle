5c5da68446f3cfa025555b5fa79e83eaf3af5394
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:55:02 +0000

    tonight male unhappy protest like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import math
+print(math.pi)
