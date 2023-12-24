d9266f2a1c2f127a9909f26e89a7812aa8334a3d
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:07:40 +0000

    Qq class welcome usually lean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
