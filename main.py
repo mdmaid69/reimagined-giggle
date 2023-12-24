c47df9ce5c36baab108b7cc848c1937ec506ba33
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:12:55 +0000

    violence ordinary story square butter

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
