7f4a3d41de548c1f5f58eca80ddce42b10dc6176
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:02:13 +0000

    manner contrast wide cum scrap

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
