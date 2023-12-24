a90ef73c3a3cfac57a778cc664acabebb3e4790c
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:18:33 +0000

    sting land record interesting go

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
