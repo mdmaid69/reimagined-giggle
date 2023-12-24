feb06aa29b520cc5db35429c892e198dbae9596e
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:32:56 +0000

    will mean treatment flow chance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
