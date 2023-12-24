8f7ecc3d8ff7b9a6b68aa69aa7ed2e5570f8a990
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:33:21 +0000

    indeed roll movement getup government

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
