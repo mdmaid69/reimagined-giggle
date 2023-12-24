6b97823310cda98874742076363f459237c9a620
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:51:12 +0000

    565end wake mistake murder murder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
