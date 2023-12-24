7e7d9293aefcbeff333f562e630260262fed8201
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:04:46 +0000

    create lost concerned ill hide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import getpass
+def get_username():
+        return getpass.getuser()
