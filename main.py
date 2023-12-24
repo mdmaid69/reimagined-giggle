90958a283af6e898a55f0efd8732b9b012f87c78
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:31:16 +0000

    correct strong confusing thinkover stage

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def find_unique_words(sentence):
         return set(sentence.split())
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
