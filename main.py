de06458a17adde357b1b5d458718d85ae54b3a20
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:00:28 +0000

    divide development embarrassing lot agreement

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
