d6aecda7c99ec3102065aa056db2fc7257b083b1
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:56:32 +0000

    over anywhere crowded guess shade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
