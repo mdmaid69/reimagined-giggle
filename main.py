960c4328284668c4ac8f5088a7f3f16262532bcb
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:54:05 +0000

    place but time slow allow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  def subtract_numbers(x, y):
+        return x - y
