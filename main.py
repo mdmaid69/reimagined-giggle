4745eb0b20c7d78d031a53ea59eef9bfb2bca0ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:48:57 +0000

    takeaway aim holdon run order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
