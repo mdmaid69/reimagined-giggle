85c221b4ec14310078432166bab16b7af3054798
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:11:05 +0000

    send other wave responsibility wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
