2da4fe4acb23f73d539b8490e69e31274e916501
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:18:20 +0000

    central exhibition embarrassing thumb shoulder

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
