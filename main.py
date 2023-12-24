7c5d111a284a53ac33b06fe4e6669891b1353106
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:23:29 +0000

    cause movement fear swim alone

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_speed(distance, time):
-        return distance / time
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
