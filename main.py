b2b66847479948c56e8078ff541caa8f31a8fd60
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:09:04 +0000

    whistle sort for early question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_speed(distance, time):
+        return distance / time
