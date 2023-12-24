13ec638b224132a477f26f5161c3cb31235893e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:41:22 +0000

    question share letter murder exploration

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def cube_number(x):
+        return x**3
+def remove_duplicates(lst):
+        return list(set(lst))
