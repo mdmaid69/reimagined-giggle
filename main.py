71bbaea78a875483a0472e659192a30ad46b3168
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:50:44 +0000

    boring score fall change object

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def find_min(lst):
-        return min(lst)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
