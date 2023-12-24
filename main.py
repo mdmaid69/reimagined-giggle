434b44ce5fee3a01cb5993490d2bbf708fe12c5d
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:27:42 +0000

    shallow promise still file squeeze

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
