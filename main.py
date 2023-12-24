f173bb96141ebba50a2898362a63ef41eb22cd0e
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:13:38 +0000

    fork season suffer collection comeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
