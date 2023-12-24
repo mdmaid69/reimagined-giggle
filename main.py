c2b57f5089df29410d70f4ff8cbe66961fae31d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:43:51 +0000

    chin sore tidy comment write

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-def calculate_speed(distance, time):
-        return distance / time
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
