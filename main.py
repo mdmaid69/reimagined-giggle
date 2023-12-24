136fe44936865a437c43f92e2b2e4ea61d1e00ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:18:51 +0000

    rest possibly way recommend pump

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+for i in range(10): print(i)
