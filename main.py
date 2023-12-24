e4342b908af215db01c8c9c8b456e7b5d549d1db
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:39:49 +0000

    ground angry brain regard fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
