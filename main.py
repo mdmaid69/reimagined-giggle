41234eb49fab6e0cdcb535246cc8c57323ddba01
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:23:34 +0000

    recovery smooth programme form rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
