5cf1d7adf8213c2049e31b768fe5bb7e0c823271
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:39:12 +0000

    trap mean past similar sweet

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def reverse_array(array):
+        array.reverse()
