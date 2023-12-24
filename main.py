de30d149ef76696f5b3edae9ddf99f1a1fe5812d
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:13:25 +0000

    country collect necessary bar thinkover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
