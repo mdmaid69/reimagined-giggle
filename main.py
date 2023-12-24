1b2736ab1bb7a15f4c7f94074a8de9d3611c97b8
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:40:56 +0000

    suck bad fun powerful impression

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_pressure(force, area):
+        return force / area
