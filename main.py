fb3d1023bc019ba93aca9a8d5addbafcaff3f5d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:09:35 +0000

    anything broad worth hard turnover

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_power(work, time):
-        return work / time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
