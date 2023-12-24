bdd1f391645ce444a993970336d26387e93d5f12
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:09:53 +0000

    destroy once stare sink normal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
