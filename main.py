403d78035f93b649373b98e354e422fd581c4a69
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:41:53 +0000

    watch manner contain connection mean

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+for i in range(10): print(i)
