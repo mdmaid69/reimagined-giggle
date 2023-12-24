7bf5a8e18de27e90d6cb2573d9a73cf2bd7bcf00
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:25:47 +0000

    everybody sure Yy recognize ever

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import datetime
+def get_today_date():
+        return datetime.date.today()
