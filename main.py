40fb6e55d8afc1e9a51ccb5c988967edb42ccb58
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:50:23 +0000

    pattern seriously buy borrow tense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
