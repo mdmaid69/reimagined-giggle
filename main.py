77b8ab6e768fe328528c41296990a579a9280660
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:29:44 +0000

    shut film exam small rubbish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
