5411dfdc574bb5c4dc6c812fa5d14c8d169b4c15
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:21:48 +0000

    pronunciation shout how trousers undo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
