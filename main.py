466bc6b5b14b0db87ecfc1221232a021bbddc233
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:09:08 +0000

    read corner properly strength measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
+  def get_file_block_size(file_name):
         return os.stat(file_name).st_blksize
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+x = 10
+y = 20
+print("Sum:", x + y)
