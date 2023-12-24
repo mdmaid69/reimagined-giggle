ef01897ec7b88e53c5bd537b4aa21dbed523e90b
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:11:35 +0000

    punish constantly giveaway mysterious section

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import random
-print(random.randint(0, 100))
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
