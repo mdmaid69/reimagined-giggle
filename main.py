596456f1a83f406865aef7fc4415ffe0f5ad7eb2
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:04:24 +0000

    bound private takeover change night

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
 import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def get_array_as_bytes(array):
+        return bytes(array)
