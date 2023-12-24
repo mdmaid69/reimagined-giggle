bc8df7fd35cf80860fc665bae4385bf6413395ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:21:15 +0000

    discuss repeat he provide scream

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
