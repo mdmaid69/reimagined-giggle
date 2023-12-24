978c5c1c8c98cf386c574cf1bdf8805a568d8400
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:56:43 +0000

    bear somebody as invite thank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+print(os.getcwd())
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
