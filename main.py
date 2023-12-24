900d14a81af03afc3d23188e44116e80ad7b1112
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:32:35 +0000

    century piece use stop communicate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
