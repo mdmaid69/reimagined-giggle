0c922ae9a95075b10eda9f49612a3af9d2030a79
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:56:49 +0000

    stranger burn shop scare opportunity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
