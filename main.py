02a0cef3122609a9bd1ecab0918fdb82cc19df0b
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:48:15 +0000

    dish my in strength estimate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def get_array_index(array, item):
+        return array.index(item)
