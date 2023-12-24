2a0966b64be68d121442b48aa7f24025de6dac9f
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:01:21 +0000

    broken flow vote appear club

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+print(sum(range(10)))
