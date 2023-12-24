7b2c0f267a72bf1aa0edf203263be0d6c02e5015
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:50:48 +0000

    frame confuse company power serve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
