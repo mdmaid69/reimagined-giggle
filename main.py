0a6a674563b73bacad4f0c8cc3f5151f49d7e833
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:54:32 +0000

    conclusion once sight it comfort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_int(array):
+        return int(array[0])
