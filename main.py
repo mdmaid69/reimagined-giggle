8319952152544141763953c75409f5bc53fa5810
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:10:40 +0000

    loose never allow threaten mention

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  def is_even(n):
-        return n % 2 == 0
+  def count_elements(lst):
+        return len(lst)
+import json
+print(json.dumps({"name": "John", "age": 30}))
