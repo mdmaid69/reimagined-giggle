fa296ccf2217f6f60c76be7e72691b4bd0484fae
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:20:42 +0000

    relax crowd air rush communication

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
 import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import os
+def change_working_directory(path):
+        os.chdir(path)
