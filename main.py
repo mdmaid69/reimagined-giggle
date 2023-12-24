9857ee54e5867ffe4ac903324e43e6995d31988f
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:30:23 +0000

    balance train brother press its

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
