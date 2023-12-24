2359f3199e70d0c87e7bd0c5ff99b28ecdf4bce0
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:35:49 +0000

    appeal rub feeling nothing shout

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import sys
+print(sys.version)
