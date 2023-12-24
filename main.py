2c55d0979e78a4d448f38026c38c71b4337e364d
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:21:33 +0000

    bringdown along waste home side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
