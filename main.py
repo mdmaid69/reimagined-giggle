a91b31a28981281e5eaf2bfb4207ce992a9cae88
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:56:50 +0000

    factory succeed ours under thumb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
