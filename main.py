8155b84ef455b48c4382dc7bd7e47c49a1a1c896
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:24:19 +0000

    problem serve common pause lead

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
