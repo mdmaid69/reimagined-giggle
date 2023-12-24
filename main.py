82641b6b01d7dd10d3fce603d4891ad58a3c893f
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:15:10 +0000

    comment total square here convenient

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_as_set(array):
-        return set(array)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
