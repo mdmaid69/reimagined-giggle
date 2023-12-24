7c001c66fcba57cfb786120ee7a19bef8399b0d3
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:07:54 +0000

    see name complete and principle

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
