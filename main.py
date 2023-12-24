20947d00249fad7c70249d61be2f9d79c493fdc7
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:40:47 +0000

    day full must minimum worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
