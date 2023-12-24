ca7f3b652d5022bd2bd47fdf6b8cc822a0d5a520
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:32:10 +0000

    book children hold land split

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
