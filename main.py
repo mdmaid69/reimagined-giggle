8e3864c593a8d94ab117b74f015079766d23e601
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:54:48 +0000

    crowded clue breath between choice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_queue():
+        return collections.deque()
