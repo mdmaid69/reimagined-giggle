795da2d1c1d7fa48a05a6eeffc94c44341693a94
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:36:12 +0000

    square whether shop soak dream

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
