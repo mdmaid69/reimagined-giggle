1067569f5083dcf6e57ffab197cdeac4a98b9bb9
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:02:53 +0000

    shock who listen correct they

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
