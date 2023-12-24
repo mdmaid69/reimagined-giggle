713644b409595a6c737443aaf01613dce274ae2a
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:38:59 +0000

    among pollution drive light risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_int(array):
+        return int(array[0])
