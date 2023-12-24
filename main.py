ce481073c38b22b0ded4015f176f43bdf9cb1912
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:46:39 +0000

    process me out car pair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def greet(name):
+        print(f"Hello, {name}!")
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
