a609e47c1b8bc717b4e9a0a32f4a12f479d0b429
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:39:31 +0000

    pronunciation shop trousers neither thinkof

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
