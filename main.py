9bafd718ab5221d2841a18931d2642aafb7b5182
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:51:39 +0000

    mainly popular beat minor either

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def clear_array(array):
+        array *= 0
