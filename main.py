f87d3e43b81b399fbe1ec5e43afdcede0b4ad3e7
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:16:57 +0000

    once only freedom well second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
