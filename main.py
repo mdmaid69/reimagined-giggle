f0e67223843a1ca3243423004b89514ed4f28497
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:29:07 +0000

    only excellent mess measure damage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
