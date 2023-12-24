4507b4428cbbaf234c6426344ed7cce735217a6f
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:55:20 +0000

    leave both Ii expensive pollution

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
