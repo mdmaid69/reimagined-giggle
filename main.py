5f7b4624d1df688aaba3bbf0c9c5a5dc73c6a3a4
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:33:05 +0000

    explain runaway near admire round

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
