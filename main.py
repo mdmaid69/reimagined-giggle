945bda13ae2f7d65dc2577aa704fdf98b213a61e
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:04:28 +0000

    yours glass rely loud phone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def add_numbers(x, y):
+        return x + y
+  def sort_list(lst):
+        return sorted(lst)
