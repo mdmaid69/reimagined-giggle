2451e55d934fcc82d7748a19a04e672d1ebcca04
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:29:00 +0000

    free ear party Gg Itself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
