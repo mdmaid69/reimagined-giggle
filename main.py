edf1a4aae294cb6a25c27debcd91f9c9699a3202
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:35:07 +0000

    put Dd expression before ground

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
