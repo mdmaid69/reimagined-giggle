aa77ab23f0c18bf29ba9d638ace3375922d7379f
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:23:26 +0000

    roll before programme quick thick

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
