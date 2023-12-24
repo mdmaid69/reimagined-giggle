cdce29df6783f08045ffcf6744c59d7be5888b1d
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:37:00 +0000

    replace thank nervous innocent question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
