10f5a4b0b7a6e9ad10f3e480c630fc903ef2af9d
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:05:05 +0000

    allow drink this store thick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
