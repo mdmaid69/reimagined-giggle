8024431bb64c86ecb34fc82158e77e3541e2f7fd
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:37:50 +0000

    after check such born factory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
