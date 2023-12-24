a3014c6945ee3c5ea154fe3b87708f22a31e6e00
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:08:27 +0000

    conduct bread crisis hollow traffic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
