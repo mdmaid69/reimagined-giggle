3364156cb97fe11537aa8d059bdd6a3d03ac46fd
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:53:00 +0000

    begin introduction prove up stand

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def is_odd(n):
+        return n % 2 != 0
