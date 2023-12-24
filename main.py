0e72813fe04244f4ecc0016c07a592bd6f31152a
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:46:52 +0000

    speak Ee we funny wide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
