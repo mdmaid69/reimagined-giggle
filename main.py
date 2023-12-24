07d079875bbe99e6c9b7d1dabd5a06b813e616d7
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:55:42 +0000

    field allow gooff minute arm

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
