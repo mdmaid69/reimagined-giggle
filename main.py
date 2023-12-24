f3126d71f53b7770cd0e92b4e4164b1d312f33df
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:08:54 +0000

    constantly blind fact deliberately everybody

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
