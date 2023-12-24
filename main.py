374f43b73e4fe0225175d19ac66a22dbcff73772
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:06:24 +0000

    border cinema total hit if

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import array
+def get_array_as_int(array):
+        return int(array[0])
