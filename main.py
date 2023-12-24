91d34ffd69e3ef5ee7d634abc8d73b0385a323c9
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:40:32 +0000

    call pen willing alive fashion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def find_min(numbers):
-        return min(numbers)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
