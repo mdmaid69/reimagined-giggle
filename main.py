da98092268fa052ea2162641a30690694044a1bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:30:35 +0000

    replace female in tidy float

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
