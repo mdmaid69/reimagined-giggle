602fff2737c03f42f06cd23dda88dd5e639c62a2
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:11:39 +0000

    unit worried market shade argue

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
