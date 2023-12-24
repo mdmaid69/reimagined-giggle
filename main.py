1cc2567e8704fe352df44c187f95601e56e9e87a
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:05:39 +0000

    any train read receive path

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def count_elements(lst):
+        return len(lst)
+import collections
+def create_counter():
+        return collections.Counter()
