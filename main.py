3dc29d9fddf1e44ba48f79884b02d8c13f027e67
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:12:11 +0000

    past would shout mysterious finally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
