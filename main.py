210df8e2e8561eac969a353ab8a0082bd2e9e616
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:14:45 +0000

    ever evening price your grand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
