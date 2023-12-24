5afc0a0f6fa7b1189517c0f9561ea62dd77579f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:42:38 +0000

    book yours fond hit admire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
