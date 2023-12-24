2f16c811119fe65794efb0bde3f9bd8513363c71
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:49:50 +0000

    increase boat contact hers stick

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_acceleration(speed, time):
+        return speed / time
