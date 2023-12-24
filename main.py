91a16b001600a52b5eff9fa397a50dd42dc022d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:43:02 +0000

    his total workupto promise divide

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+print("Hello, world!")
+def add_numbers(a, b):
+        return a + b
