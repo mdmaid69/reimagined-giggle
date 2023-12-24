4708e26c2c899b2c770c4ad518f44f063bfe38fc
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:01:56 +0000

    illness old Nn bath design

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
