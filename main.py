3b90ae785d8c3636b5e53d124387d88e2f89a725
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:05:57 +0000

    public damage brain trap gentle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import os
-print(os.getcwd())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_max(lst):
+        return max(lst)
