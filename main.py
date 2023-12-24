17915af55bb0a08c7fd2d516856da6f5a8e8d0bd
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:03:20 +0000

    realize meet middle noise off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import sys
-def exit_program():
-        sys.exit()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import getpass
+def get_username():
+        return getpass.getuser()
