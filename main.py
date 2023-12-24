03d87dabc1ea8335bed79734df08d80d99b21afd
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:50:59 +0000

    pen willing least conversation lookafter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
