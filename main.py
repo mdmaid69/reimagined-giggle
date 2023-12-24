cdede0c4056970c6071c18583e46537177131d9a
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:20:16 +0000

    pot cruelty sting object against

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
