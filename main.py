cb4011d27f2b774eb00cde77ebcdb6d391bb02fd
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:27:47 +0000

    decision poison embarrassing pleased mend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
 def get_array_as_list(array):
         return list(array)
-def calculate_speed(distance, time):
-        return distance / time
