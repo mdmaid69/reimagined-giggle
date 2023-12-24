a91734b9dc712392f4629ec4fe97c5fb05a6c4eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:31:14 +0000

    control correction pin this lost

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
