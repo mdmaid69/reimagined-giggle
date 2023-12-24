f1e56e0ab8dad0c3fe13db16320f6c5ea04fc563
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:48:03 +0000

    straight giveaway with holdup wish

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import sys
+def print_python_version():
+        print(sys.version)
