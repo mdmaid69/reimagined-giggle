c3494ea0ae939f38826be3dd9acde28846ad3aa1
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:26:07 +0000

    watch march satisfied strike phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def convert_to_binary(n):
+        return bin(n)
