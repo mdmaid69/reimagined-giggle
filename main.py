ae5c7fb41e1e8247e4b934e7a891e3243a64653b
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:19:09 +0000

    any bury glass plastic careful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def convert_to_octal(n):
+        return oct(n)
