b33dfe89fd1446759ee09c5b5bfae7c63da85d39
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:55:10 +0000

    knowledge follow score final proof

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
