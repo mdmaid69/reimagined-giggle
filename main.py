55ee65cf12d88b26bdafa509db5c3bf545787a4f
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:21:38 +0000

    hour yes shop school argument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
