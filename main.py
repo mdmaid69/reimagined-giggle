5e29bf944f12be7032fae53b2913c3e41b6bb590
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:10:23 +0000

    dance on test stupid stage

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
