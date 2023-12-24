bb19bf662f8becb46f48b5cd254111104040788c
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:29:47 +0000

    along to comeover Cc decision

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  def is_odd(n):
-        return n % 2 != 0
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
