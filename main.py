cca35d6bf83a0d1ce33e810aeb39cd3b2ab7cccb
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:54:30 +0000

    reply purpose single light concentrate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
