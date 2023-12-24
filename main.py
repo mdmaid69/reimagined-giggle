c65cd95a4c084520a5fbc75cddc60da176ff3c5e
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:29:32 +0000

    contrast wipe communication try circumstance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
