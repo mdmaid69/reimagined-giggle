3a06507fe6511d5c70bfae4f87afbb76641daee4
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:49:16 +0000

    collect colour snow appear reduce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
