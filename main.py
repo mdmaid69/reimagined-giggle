7bc68b98f9ecd47af8af87321865662a33f9d780
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:00:17 +0000

    centre down on hers juice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import logging
-def log_message(message):
-        logging.info(message)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
