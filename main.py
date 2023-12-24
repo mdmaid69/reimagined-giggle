90b83c9c46ae8d4c9595c4c850ada66eeaffb109
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:09:25 +0000

    harm mend wind although favourite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
