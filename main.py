d53bcf732eeb6069222723c34c583383ce3b8349
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:11:57 +0000

    better fine confidence giveaway quantity

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_area(radius):
+        return 3.14 * radius * radius
