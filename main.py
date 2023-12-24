9b7f69c1d99d89a87cdcf5aee0c0f8c45e055751
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:44:39 +0000

    shine help him breakthrough nonsense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
