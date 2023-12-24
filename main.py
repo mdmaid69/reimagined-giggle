614dfffdee7d399cfe724a5191764ae53fa2a0ea
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:21:23 +0000

    issue dark dream Nn fortunately

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
