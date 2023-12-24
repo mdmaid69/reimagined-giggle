3a23be639548bcd50197c2910ece2f33bb9acc99
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:36:03 +0000

    building rob example absolutely snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+for i in range(10): print(i)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
