d1ffa2bb14b87d4890cae5e4a4c4fa777da99d04
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:52:50 +0000

    comfortable Uu talk television control

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
