6a2f3b2c858214fec518cca0162d0c0f530dae05
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:40:47 +0000

    belong surface steer delicate dear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def sort_list(lst):
+        return sorted(lst)
 import collections
 def create_ordered_dict():
         return collections.OrderedDict()
