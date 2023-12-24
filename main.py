3f10883ac536ab4e3d42cc15650a7a0ec3a8b9d6
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:38:27 +0000

    also late land tidy market

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import time
-def get_current_time():
-        return time.time()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
