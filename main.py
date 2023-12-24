5278bb5e5eee90b69c8cf57494be67061dcd1c92
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:37:26 +0000

    land talkinto half take plenty

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import sys
+def exit_program():
+        sys.exit()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
