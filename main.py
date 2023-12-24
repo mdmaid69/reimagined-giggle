7e0284afae4f843f46faa35fff94a32a5590a420
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:44:09 +0000

    weigh exhibition provide certain find

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_array_length(array):
+        return len(array)
