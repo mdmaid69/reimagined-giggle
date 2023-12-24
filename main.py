e0e4fc193eb5038fa2fedef4cbb242571ab28b8a
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:06:04 +0000

    chance spoil train tear upstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
