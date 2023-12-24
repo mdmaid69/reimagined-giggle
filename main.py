2a7ecba4f9e6c0e9610671760bb4b28ebcd8e841
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:30:00 +0000

    black record waste official risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-x = 10
-y = 20
-print("Sum:", x + y)
+def calculate_work(force, distance):
+        return force * distance
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
