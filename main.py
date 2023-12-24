2cf6a83f41693cbfd9c41301bcd22f4e436d8f4e
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:21:58 +0000

    spring bring famous effort bridge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def find_max(lst):
+        return max(lst)
