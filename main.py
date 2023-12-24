16440e0bc299771a2e60905759c197506347d497
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:15:36 +0000

    book later actually available same

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
