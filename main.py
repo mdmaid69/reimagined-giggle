a7e9d4e5b0464feb6217f95f07a950e9cce0fd22
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:21:13 +0000

    season plastic sense thinkover push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import array
+def get_array_as_str(array):
+        return str(array)
