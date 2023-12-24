200b05bf93ce418bf25644f2696bfc24f3045d1c
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:37:19 +0000

    throw rest down deaf pity

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def get_list_from_array(array):
+        return array.tolist()
