ad5e37708129ba7a12d29a0847c07c038df7388e
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:33:02 +0000

    middle goback(to) kick increase please

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
