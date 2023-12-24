d3576519cf980fcb12c9052d5ec006d040ac86c0
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:13:58 +0000

    hope ever danger buy gas

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
