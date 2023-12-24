888701c19797e67ec9219f60ab336f580c73de50
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:11:57 +0000

    burst majority poison under excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
