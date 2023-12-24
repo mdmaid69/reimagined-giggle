cf32a4a78f1525a99f376fcaa8b79c3e22f010a9
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:33:14 +0000

    Zz hers spill cheap available

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
