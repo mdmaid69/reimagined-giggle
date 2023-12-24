087684947f603182b7b2356e7a4e93309fc9f4b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:14:08 +0000

    cause different gather minute count

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def convert_to_binary(n):
+        return bin(n)
