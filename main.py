d09f862a6b0f5e39af3cbeb921264789422f4fac
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:47:22 +0000

    downstairs into classroom hospital confident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_as_set(array):
+        return set(array)
