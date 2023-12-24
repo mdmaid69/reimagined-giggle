50450be4f8c2409f29da6e3ebbdaf9596d5a74ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:43:08 +0000

    quick shock expensive indeed reserve

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
