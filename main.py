9fa0ad0acfb6b2e4c5a79af6b340de84c4fddf6a
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:09:22 +0000

    mail trust same foot main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def square_number(x):
+        return x**2
