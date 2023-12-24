1cbeefb3fe5c4b3c1f6abac53d00bf1b1f99d5f3
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:46:40 +0000

    my welcome sharp copy foot

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
