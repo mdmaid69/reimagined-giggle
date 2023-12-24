62d086f10395dbf166a9ed7ba67963e9a157b847
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:07:02 +0000

    climb Ll then improvement around

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
