ad70d8c16cfee47949c24f2ea3ab14f3ae0cb6be
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:53:58 +0000

    congratulate sell play wise custom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
