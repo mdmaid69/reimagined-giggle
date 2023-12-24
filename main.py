431144bac5e1301efd7989fc12c7b808a073964f
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:08:46 +0000

    call bad smell circumstance fault

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def multiply_numbers(x, y):
+        return x * y
