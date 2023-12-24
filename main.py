67b78d15800ffc5d729895ccf74de5d1e167d759
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:58:33 +0000

    value movement formal exit walk

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import time
+print(time.time())
