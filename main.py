096f6270497a184c9bc52631cbc2b6ce5bea1e84
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:49:13 +0000

    float risk can character careful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
