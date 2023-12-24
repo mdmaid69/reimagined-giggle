b48216fd42a20f1a2320d7c4fc8bd4559eba7a48
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:03:03 +0000

    offer meet destroy pronounce him

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
