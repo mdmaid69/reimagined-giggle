c49087bf6077171981dcb10d91acd7e1e704020f
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:42:26 +0000

    money shake pass Ii cheque

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
