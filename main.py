7c71f263c3fefa088563db2086d264e502e8b55e
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:40:29 +0000

    increase switch over bar be

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
