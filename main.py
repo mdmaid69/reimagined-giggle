74fe94edea1e08ba728cf7e31cce8c07536e312c
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:16:20 +0000

    rule even boil furniture calmly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
