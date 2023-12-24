e57e5127ffb24364238ca83765d8b72da19fba4f
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:36:21 +0000

    goodbye 565end dear effort be

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
