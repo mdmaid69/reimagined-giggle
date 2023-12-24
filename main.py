b1c1acbac9f22038fbc8c766034b1775e0350385
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:53:50 +0000

    live rid swim judge autumn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_exponential(x):
+        return math.exp(x)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
