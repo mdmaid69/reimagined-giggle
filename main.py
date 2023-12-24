46ac400dbb59aa5b3233af2503a32943c5b3cfac
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:28:09 +0000

    press kind 568energy failure exploration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
