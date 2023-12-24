47754eb51b6daea95267743804b0c778dc45dc80
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:12:09 +0000

    slide badly charge rarely consider

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
