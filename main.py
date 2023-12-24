8399437dc75c81b110d888c7f5441bce2247610d
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:42:52 +0000

    body flood proud may own

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
