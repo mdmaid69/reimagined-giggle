2f5c3033041c1496c51e892b8e59c3a42869e792
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:10:45 +0000

    bringdown comfort exhibition official hard

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
