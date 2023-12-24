a977f99d6d1f928cf18634db9214108bf722741d
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:22:53 +0000

    difference ought shine foreign Oo

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def convert_to_hex(n):
+        return hex(n)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
