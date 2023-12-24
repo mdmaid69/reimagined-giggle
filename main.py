911cf193bf45f459ed571f49a3b62895d9fc6362
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:18:07 +0000

    face pretty method light war

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
