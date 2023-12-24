35249a9a9b5ae7c532da5d9d1298c4dfc1c1e608
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:13:53 +0000

    everybody plenty middle test that

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
