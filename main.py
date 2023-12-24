47e156d495630575212010bb4a03ef7ed3b233f3
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:47:33 +0000

    public up it confused majority

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import time
+print(time.time())
+def is_even(n):
+        return n % 2 == 0
