db9ca9bfa7347f03f72acac01d9d9130c41c4698
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:08:10 +0000

    smile radio such own escape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
