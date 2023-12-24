137f28df6e47c75ebd10df769ced39a5bc7b3f86
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:02:50 +0000

    somehow future seriously age from

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
