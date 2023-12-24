8b7a9b3d9d23744bfdffe43cfa44da8d5d1d2121
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:37:52 +0000

    share yourself suffer bank marry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import numpy as np
+print(np.array([1, 2, 3]))
