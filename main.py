c0d55a030e222023e5cfd9885e18a240e93ab639
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:48:31 +0000

    too passage half class company

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import numpy as np
   def calculate_standard_deviation(arr):
         return np.std(arr)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
