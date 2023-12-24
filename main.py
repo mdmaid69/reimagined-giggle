388639b664618e4693ec7ee3210340991ce5c456
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:34:27 +0000

    glad careless apologizeapologise interested until

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_square_root(x):
+        return math.sqrt(x)
