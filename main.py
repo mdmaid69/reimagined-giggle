9fa61ea62a6682ffadc215d57837b6b6226c8c6d
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:56:58 +0000

    black choose family passage try

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import tensorflow as tf
+print(tf.__version__)
