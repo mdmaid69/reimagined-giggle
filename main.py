c6c3ddba9aeb273dc1fe84b0afbcc649f5d9ce1b
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:03:27 +0000

    table free before contribute interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_combinations(n, k):
+        return math.comb(n, k)
