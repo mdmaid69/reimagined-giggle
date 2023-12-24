6f9e36c5563d61a25853e22930db84405456a365
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:13:42 +0000

    but make stand soft impressive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
