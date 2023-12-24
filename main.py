148d8413b6104153e76fd680ce336a24f40ef8e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:19:57 +0000

    speak strong 570enjoyable light reply

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
