50f47f37ee3cd05ef6614ff1c770824f24bac76c
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:17:45 +0000

    fence brush battery Ff share

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
