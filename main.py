cb79de87100c88555f651fe9e1030e4d96fd153f
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:20:23 +0000

    match set tall mark wire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
