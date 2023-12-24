cfbf718c395ea78a4081674f7d4bf9725ae143e8
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:07:18 +0000

    gap extraordinary range passenger chin

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+for i in range(5):
+        print(i)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
