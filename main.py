4b40ed2c9d69c85d7467a98d91d8bfc6fc747ebf
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:52:37 +0000

    return big point mouth pipe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def greet(name):
+        print(f"Hello, {name}!")
