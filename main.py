52380623b663e5f95a0a34a43ec806b0f2fce5e9
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:44:36 +0000

    century attempt plate gift sport

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def add_numbers(a, b):
+        return a + b
