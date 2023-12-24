7d37dba67998b617508cf02d2f3122f96b07b6e6
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:22:23 +0000

    hurry only responsible 566end middle

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
