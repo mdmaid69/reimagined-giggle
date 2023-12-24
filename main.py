0ebe9b958e57c788e7082f4f22a1a54568662582
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:56:35 +0000

    damage travel squash suit active

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
