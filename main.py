db4242ea8975db7ea715a216cef2e590f279bb09
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:01:09 +0000

    fill common stamp add basis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
