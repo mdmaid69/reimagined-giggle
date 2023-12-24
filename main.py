da2525f4db695647f47befea4013511e5e256740
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:51:14 +0000

    prevent kill speed interesting confuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def is_even(n):
+        return n % 2 == 0
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
