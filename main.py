95849da22c8c6961e96b1bf95260888dcf0fbe68
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:48:14 +0000

    chase yesterday compete crisis whenever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
