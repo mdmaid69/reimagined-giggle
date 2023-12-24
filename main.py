146654c753b065544c65bc42b69bea3727f368e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:53:15 +0000

    correct show change store wherever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
