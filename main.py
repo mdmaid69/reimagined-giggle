a27147957c1bdb57a8e3c75fd434409f685257c2
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:02:09 +0000

    certain shoot capital still concern

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import getpass
+def get_username():
+        return getpass.getuser()
