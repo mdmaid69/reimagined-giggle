7a76b21a730c3500af0374b749087b1de8e29b06
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:37:29 +0000

    walk look prefer wild be

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
