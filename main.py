41cbdce2fc1ce41c971d66b558a6f80952037af6
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:35:41 +0000

    go battery position test shock

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
 import array
-def get_array_as_list(array):
-        return list(array)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
