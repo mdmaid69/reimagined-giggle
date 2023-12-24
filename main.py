25dcc242e570888def2fc1ea51eab0971b63f2d8
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:37:10 +0000

    fix wild flat plain every

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def is_even(n):
+        return n % 2 == 0
