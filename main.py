20ee045a8b35ea203c76f729a034a66aa2222a9c
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:25:34 +0000

    at give turndown regard sensitive

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-for i in range(10): print(i)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
