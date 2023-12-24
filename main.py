3553cb195c4db40f3a20fcc11dc41c6b3abb40bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:42:15 +0000

    clearly talk tear lot across

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
 import array
-def reverse_array(array):
-        array.reverse()
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def get_array_as_list(array):
+        return list(array)
