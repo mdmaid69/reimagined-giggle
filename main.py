3ada7b97d31f1f2b4c7b9079ffe639d22c866a92
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:12:40 +0000

    profit straight stare organize apart

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
