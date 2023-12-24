18216a2e9203c75fd94cb0a06a2f0040df042d59
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:57:20 +0000

    treat letter condition hope pay

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
