bd3d8d1e3779bcc1bca9992a81805c8be6f68673
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:20:19 +0000

    motion spring late opposite politics

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
