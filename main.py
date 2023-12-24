22319479a3fa1e4187640135ede49bb6f587ef3c
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:02:01 +0000

    jump each possibility attract question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
 def get_array_item(array, i):
         return array[i]
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
