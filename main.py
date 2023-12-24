858409fa20b202bcd8f319c05ee28d8d9ce4bc88
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:27:18 +0000

    wide blue mass cruel style

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
