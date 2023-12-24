c6e84094c52bfcfc3092fbf46efd7b7e1fdf307f
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:52:06 +0000

    over comeoff call myself appear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
