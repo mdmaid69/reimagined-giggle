b272e298440d04fdbba741559f4ddc3131ab31ce
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:48:05 +0000

    miss later maintain back chat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
