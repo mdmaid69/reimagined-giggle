de16d626fc90823c7736b896acd2d1e47fc97fef
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:17:19 +0000

    summer rest even direction soft

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
