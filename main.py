2fc5deb136dfbb400fdb5cdbeac70316e4d1d292
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:05:26 +0000

    least mine neck relative burn

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
