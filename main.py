2f0c86da3c9df188f8e9120b8cb27170f1c9942b
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:49:38 +0000

    attitude clear square disagree bound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_array_as_set(array):
+        return set(array)
