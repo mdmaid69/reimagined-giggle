4c3ef40891bbb9409f18904d8e0bce43941fac3d
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:31:47 +0000

    affect charge keep platform alive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+print(sum(range(10)))
