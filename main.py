c54fccb679255dc85dabcc65f6542a96b4e20fd1
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:15:38 +0000

    difference capital pain accept answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import sys
+def exit_program():
+        sys.exit()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
