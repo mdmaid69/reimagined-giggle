08e8c7a376d2da724449bd9a680bcaf06665d7f2
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:58:57 +0000

    expression friend somehow bound direct

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_gamma_function(x):
+        return math.gamma(x)
