7be150fb64d11567f3a886a9e443bd123fe31f06
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:47:39 +0000

    speed increase mother discovery provide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import datetime
+def get_today_date():
+        return datetime.date.today()
