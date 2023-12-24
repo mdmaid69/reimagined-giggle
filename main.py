e1325fe73aeaff8022108a0c4270aa7ccd6f5d23
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:53:14 +0000

    true hold wear attract cough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def convert_array_to_string(array):
+        return array.tostring()
   import os
   def get_file_inode(file_name):
         return os.stat(file_name).st_ino
