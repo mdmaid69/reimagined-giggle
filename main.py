35897176970cef8436a2bd91da24fba9e646f8c4
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:42:04 +0000

    immediately either shall cut anybody

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
