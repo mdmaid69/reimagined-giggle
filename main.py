2fa25532e835b8ce4b390c71d3f30af9707d1b01
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:27:34 +0000

    even wrong trust progress train

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
