1092de7ca8595fefbf638372e053bc55a258131f
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:47:42 +0000

    produce interesting improve always afraid

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-print([x**2 for x in range(10)])
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import platform
+def get_os_info():
+        return platform.uname()
