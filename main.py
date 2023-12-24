5ec48ab2c6964fd6736cbbd013054f3809b58e62
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:52:04 +0000

    fortunately it tidy turnaway then

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
 import os
-def get_environment_variable(var):
-        return os.getenv(var)
+print(os.getcwd())
