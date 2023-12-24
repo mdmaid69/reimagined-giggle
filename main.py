c4d12b786a04b4e836459def6908d93c23a147e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:16:36 +0000

    warm prefer develop proof order

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
