c8180636d204136775d748ccd1be63e1d1e36a9e
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:02:48 +0000

    cinema company blow smoke off

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
