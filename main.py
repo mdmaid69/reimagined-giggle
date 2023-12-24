e2c089f774e3460307a41f19def44ceabca026d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:47:38 +0000

    front post throughout somebody yesterday

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
