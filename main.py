ef4faf85eceb14e93fbfde634fba73e1e1a005a6
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:16:40 +0000

    justice empty file twist belief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
