18c41f7c72d368383e8af26756c2c82e30cbd74b
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:35:10 +0000

    holdout fun stir hit fall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
