633b9db6267e0aab3129395935c83bf1da69430c
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:35:51 +0000

    shallow game base shut middle

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_index(array, item):
+        return array.index(item)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
