9ccde9852ed50bbf8fea34a50c39c3adfc30c917
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:48:01 +0000

    miss praise unkind key bar

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_work(force, distance):
+        return force * distance
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
