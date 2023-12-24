3b65beb4277dffe6f7c91bb4c900810ac3d09464
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:04:35 +0000

    situation teach cum damage trip

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
