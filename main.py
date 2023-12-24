419b40ada8da2ec6e430effbac46bab129ec2e50
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:28:56 +0000

    coat model free until fight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import datetime
+def get_today_date():
+        return datetime.date.today()
