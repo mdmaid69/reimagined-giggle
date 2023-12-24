9c65c2b3beb99e71264ca9add9964b05753f4456
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:35:26 +0000

    post put confident leave accidental

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
