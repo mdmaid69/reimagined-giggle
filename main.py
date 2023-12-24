3dbf82144619e4c4f41cfcb5f6115a5fada7f52c
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:15:36 +0000

    Ss friendly cinema section roll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
