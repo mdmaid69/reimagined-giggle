d0a3820349ba6fb04dbba872075a5af40e0dfb96
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:49:37 +0000

    especially petrol estimate tone entry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
