ae4c882d73e6a3d76438adc0caf34688b97d8d71
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:57:44 +0000

    music tip cope board listen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_speed(distance, time):
+        return distance / time
