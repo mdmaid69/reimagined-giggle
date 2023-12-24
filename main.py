86685320d1f2706fc65e8ebf53d308f4de44f525
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:41:24 +0000

    wet deserve around string march

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def cube_number(x):
-        return x**3
-def remove_duplicates(lst):
-        return list(set(lst))
+for i in range(10): print(i)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
