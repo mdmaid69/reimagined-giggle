3bac1ccba73c30d665e5d0dbef273a0a372b5e69
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:01:43 +0000

    invite wave window runover region

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def convert_to_binary(n):
+        return bin(n)
