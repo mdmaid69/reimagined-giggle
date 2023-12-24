c567d5cd4fc18993805fa8023812eb3e762a2003
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:13:45 +0000

    comein 564encourage warn space enthusiastic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
