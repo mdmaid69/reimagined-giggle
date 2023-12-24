399d51eaa36ecd399ec9bec9d6aae7b763aaf10a
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:04:21 +0000

    our exploration impression substance flight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import os
+print(os.getcwd())
