e2a8f6889de233865e6f7d045296fda0ecc6daa1
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:14:03 +0000

    detail question female turnback gate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def add_numbers(a, b):
+        return a + b
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
