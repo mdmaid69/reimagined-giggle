9d907766946bb9b5954243fb71a811a6dddabe7a
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:48:18 +0000

    contribute speed reduce order open

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
