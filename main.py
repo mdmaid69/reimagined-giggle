7583a8548b87d987b0db11c289e693b69cebc779
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:57:39 +0000

    account reliable me recommend holiday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def greet(name):
+        print(f"Hello, {name}!")
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
