784dfd5ad96ed7dbac73b2a05ec611135f670d5d
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:28:04 +0000

    speech instead string give wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
