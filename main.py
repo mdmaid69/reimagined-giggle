34c14ccfece24b3a345bac5b087088779d2d2af4
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:15:27 +0000

    alive buy gas introduction spite

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_words(sentence):
+        return len(sentence.split())
