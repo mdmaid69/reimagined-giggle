9139bf569f0d86227408253f2842df37b4992afe
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:16:34 +0000

    drive compare activity flood statement

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import random
-print(random.randint(0, 100))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
