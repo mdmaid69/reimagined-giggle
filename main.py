821cda71041859c65e2e2a50eb7a1c23e6acded5
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:55:59 +0000

    belong good hospital schedule branch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def cube_number(x):
+        return x**3
