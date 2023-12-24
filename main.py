5185a98fb67a1b0512575ea4068a9bf8b40be93d
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:57:11 +0000

    worry dull out hold steer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
