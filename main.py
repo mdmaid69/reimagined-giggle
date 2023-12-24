cea857aae54d6589e0da2e1655cba90f3d93e147
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:43:03 +0000

    blue keepoff climb table Pp

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
