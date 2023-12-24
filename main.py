72ebf7aba4ac4cfd054c3632c07279440d8d3309
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:43:36 +0000

    deserve neither busy classroom crazy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_work(force, distance):
+        return force * distance
