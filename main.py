ed5b38e2a90603c0022d2809928a463c910d823b
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:26:58 +0000

    never crowd safe out fit

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-  def add_numbers(x, y):
-        return x + y
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
