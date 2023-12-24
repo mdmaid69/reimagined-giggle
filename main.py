8eb4f94b8ba14da47e4cda338d3d553ed553e350
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:10:34 +0000

    think own slight tomorrow delicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_array_as_list(array):
+        return list(array)
