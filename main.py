26338556ed0bc28b6221d8c56353f99fbbdc1595
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:54:19 +0000

    hospital bottom success purpose deliver

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_min(numbers):
+        return min(numbers)
