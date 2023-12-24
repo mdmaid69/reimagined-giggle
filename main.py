9c09d38b246b24c1f0d10ae3f3433c4e54eb9099
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:10:10 +0000

    kill us dangerous sort still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def add_numbers(x, y):
+        return x + y
