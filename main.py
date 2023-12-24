5e7692d94fa2a2fb75cb39541befe102a7ac99b5
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:47:44 +0000

    true close fear make flavour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
