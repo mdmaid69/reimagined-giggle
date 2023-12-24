14fb59f834d081d97b4f1c9979b277435040801d
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:12:38 +0000

    common disturb blue willing separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
