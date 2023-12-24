f8b06abbfb27df891551a27badf526f385d2d4ae
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:04:25 +0000

    pencil she less convincing bad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+print(os.getcwd())
