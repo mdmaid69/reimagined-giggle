5552297bdc74b651e5862c07ecf29b405e84deb3
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:09:14 +0000

    final voice war lay keepup

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
 import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
