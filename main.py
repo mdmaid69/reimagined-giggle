69ccf032effa20820aa2a07ea915f386766ebf39
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:25:13 +0000

    woman stiff large service dry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import time
-print(time.time())
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
