5c0dc9aba2ce972099cd400f928264cd92e8a8ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:21:12 +0000

    still control along hide little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
