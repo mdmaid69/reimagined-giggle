20c1cc8f03c15fe4a4aabee3b10795229dd9b92c
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:27:33 +0000

    murder platform involve else stage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
