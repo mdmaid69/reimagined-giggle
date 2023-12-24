2c8913431315bd4c4710c477ca21ebc2a14c70f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:29:03 +0000

    impossible past whenever name face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
