3e47ed5b10d0d8bc21a1bf648f759e4449e68158
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:05:37 +0000

    radio patient prefer Cc take

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
