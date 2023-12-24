e341fca9e8e63c4bfcbccbd779d74eec08b4d4df
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:49:42 +0000

    sleep pretty phone occur face

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
