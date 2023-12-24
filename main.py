82f3f07c3d524d56c60bf1f5953671a6382e3095
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:03:31 +0000

    increase pull fold lean present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def get_array_index(array, item):
+        return array.index(item)
