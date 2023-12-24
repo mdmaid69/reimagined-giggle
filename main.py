fc939537e124e1a94a70baa85ff4e55c40b9559a
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:07:36 +0000

    congratulate promise disgusting equal language

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import sys
+def exit_program():
+        sys.exit()
+import os
+def get_current_working_directory():
+        return os.getcwd()
