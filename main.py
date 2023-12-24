e65e7e3b5918b7eed918d092f90d5a4416cd3494
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:27:46 +0000

    why sense sure glass exam

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
