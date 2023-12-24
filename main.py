602b8a648f3e2fb1bdde9c60eb9a75e8f799d9b8
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:09:33 +0000

    reply unless burn invitation who

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
