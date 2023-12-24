b497c6068d2600aec439a3c6c9a2ae1dd158441e
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:17:11 +0000

    sort turnon neat discovery workupto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
