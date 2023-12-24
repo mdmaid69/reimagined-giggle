556d0a1ca6910d09140f36a9bccfec06be79f263
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:50:59 +0000

    tooth special shoulder obtain wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
