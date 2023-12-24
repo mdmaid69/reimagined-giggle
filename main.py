4d4084ea91e22133204a1ac68470ca6977ac4993
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:43:14 +0000

    him even ashamed figure offence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
