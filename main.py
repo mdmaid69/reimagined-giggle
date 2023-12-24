6b5001532d4992dafe2732851d4ae7fee2eb0114
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:04:05 +0000

    stone teach shape ill fast

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+print("Hello, world!")
