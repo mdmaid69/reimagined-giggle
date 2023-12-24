63ea31e5b4c8846d0e4e563433b10e118c7264f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:43:01 +0000

    getup case fine especially anybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
