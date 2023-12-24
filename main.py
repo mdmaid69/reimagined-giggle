f8f100fd5dd3259618e010343f638e0fedfee5b9
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:04:59 +0000

    giveup begin short along discussion

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def convert_to_hex(n):
-        return hex(n)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import logging
+def log_message(message):
+        logging.info(message)
