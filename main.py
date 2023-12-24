aac1f9e13135e2791450795f02a62034dc922fc2
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:55:55 +0000

    false probably discovery rest what

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
