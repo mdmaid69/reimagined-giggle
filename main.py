da856780f9042f967a2d6884aa9b0ecfccec1b0c
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:18:38 +0000

    interrupt convince accept our child

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  def is_odd(n):
+        return n % 2 != 0
