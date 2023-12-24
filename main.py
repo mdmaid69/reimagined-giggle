d9afddc4de261fdf378f326fc1d5eb21187890a6
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:07:37 +0000

    previous shake reach as skill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_area_rectangle(l, w):
+        return l * w
