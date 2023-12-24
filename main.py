8a8ca3ec83da2287b896a81645c3e2696e1533be
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:35:36 +0000

    uncomfortable can plate famous decide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_as_list(array):
+        return list(array)
+def calculate_work(force, distance):
+        return force * distance
