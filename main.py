6e4c2eeecf85a8b59876ab5e8425fee16fe87eb4
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:35:54 +0000

    dinner crash hear pretty cap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def add_numbers(x, y):
+        return x + y
