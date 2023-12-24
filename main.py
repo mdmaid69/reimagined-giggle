be09ed3d1bb300128a5de267fa9549739d77fd0a
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:37:38 +0000

    give although quality average talkinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def reverse_string(s):
+        return s[::-1]
+import sys
+def exit_program():
+        sys.exit()
