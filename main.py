2f3932851c29eedaacef0e48a8439f3dc7ba083d
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:37:27 +0000

    while building right closely sight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
