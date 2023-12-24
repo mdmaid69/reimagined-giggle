70e43087e8349be4787c82b450b3daae5794804c
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:28:59 +0000

    custom whom house education respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
