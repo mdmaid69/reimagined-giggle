f6f5ba3efc965b663b550bccb681c522dc59077f
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:07:58 +0000

    aim 572enter steal 572enter tone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
