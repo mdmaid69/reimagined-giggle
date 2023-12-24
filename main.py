d960d3fb2d9546c0553d35520e4c4d4ea903803c
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:53:33 +0000

    basic company duty soft regularly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
