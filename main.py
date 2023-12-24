40d9994eb9c7977d0eceb42749a3e1e0d75d19d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:11:55 +0000

    majority debt pencil port single

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def find_max(lst):
+        return max(lst)
+import array
+def get_array_as_list(array):
+        return list(array)
