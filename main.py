6ef85d76acbac7b315f757c69ec8da06cc8271e5
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:21:39 +0000

    strike develop our anything sell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
