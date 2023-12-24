a36ba2601a8db3d14f7b6327e565a9117aff3596
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:12:32 +0000

    sink cross balance soil talk

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import threading
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
 
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+{body}")
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
