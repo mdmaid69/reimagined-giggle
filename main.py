75a066629e9ed18b0780680670a752e6150dce36
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:23:24 +0000

    press add moment price least

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-print(sys.version)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
