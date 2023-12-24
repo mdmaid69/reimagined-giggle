bcb96e6cf5ebc3043c5fc34409882d064082fa0f
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:58:04 +0000

    damage send distance swimming file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def remove_duplicates(lst):
+        return list(set(lst))
