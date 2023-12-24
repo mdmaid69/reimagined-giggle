3dd7008f4516ab8ca8372f2a99bd042a31ebb74d
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:00:01 +0000

    marry description arrange eye refer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
