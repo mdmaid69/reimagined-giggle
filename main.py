0ab1e363ca3adccac97456c596b15412d4301366
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:44:16 +0000

    valuable slice gift cry colour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_floor(x):
+        return math.floor(x)
