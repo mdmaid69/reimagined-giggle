8200e3e1616283137f1f8765f63d204bd6565a5b
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:07:12 +0000

    sister custom sea real anybody

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-for i in range(5):
-        print(i)
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
