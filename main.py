5a845acacb7bf81892039a907391961884833e6a
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:58:28 +0000

    ignore anywhere attractive vision damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  def is_even(n):
-        return n % 2 == 0
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
