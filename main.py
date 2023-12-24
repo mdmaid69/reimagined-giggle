28b910d69be9baf7cb35d4ec53aae886226fa1a2
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:06:03 +0000

    time use deep disturb friend

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
