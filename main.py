707a329283672ab62b687c4af96b78974202c855
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:03:11 +0000

    giveaway proper cool where handle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
