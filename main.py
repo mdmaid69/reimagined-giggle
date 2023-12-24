3eeebeeba3e80c2f68761039d7445534cfd8d410
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:07:27 +0000

    train attractive money illness phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
