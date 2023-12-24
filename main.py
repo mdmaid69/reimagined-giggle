b08eb9b2be80620a6b2f137d13a1881dad3493ae
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:04:28 +0000

    burst ought custom now talk

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 def find_difference(list1, list2):
         return set(list1) - set(list2)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
