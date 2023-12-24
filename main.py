0119e8bae5653e2553bf811425df84120d4c6c8f
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:02:58 +0000

    flavour old side common us

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
