33435f32b7ef1ea34640c1efb1252ab7e650efd1
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:24:16 +0000

    turnover getout gas well with

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import os
-print(os.getcwd())
+import time
+print(time.time())
+import time
+def get_time_since_epoch():
+        return time.time()
