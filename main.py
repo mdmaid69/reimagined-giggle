04b398624bd06cebca7212aa44e229b9842310a2
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:29:00 +0000

    male speed string light minute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def sort_numbers(numbers):
-        return sorted(numbers)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
