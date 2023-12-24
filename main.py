5d5e8d8c7c57bc67596c96f5896bd2eb555a5fe4
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:09:01 +0000

    activity spite bringforward increase children

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
