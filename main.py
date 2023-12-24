8cb590bb53dbb41d057ed5dbbc6951cdb71bcd23
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:43:44 +0000

    demand point all left boring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
