f5dc44369dcdbf6c25be784deae94b3bbf579017
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:34:55 +0000

    contact statement treat mind present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_min(numbers):
-        return min(numbers)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def clear_array(array):
+        array *= 0
