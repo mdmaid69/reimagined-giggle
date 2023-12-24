e0dec6059f8af822a132120f2fa8e7f32adf4c61
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:34:44 +0000

    short flavour design reply wander

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_speed(distance, time):
+        return distance / time
