d8291652bbede1e7149e54479e5e02b6a9969b16
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:12:20 +0000

    compete impressive direct raise pass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
