7058f31ce045b1a130b3d000001a840954d11882
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:38:41 +0000

    heart ordinary unhappy rule live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+print(json.dumps({"name": "John", "age": 30}))
