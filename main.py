8d9bee662086d2ae79568cd40f13d4ff402d6bab
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:07:04 +0000

    shout delicate aware gooff after

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
