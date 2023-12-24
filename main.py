e20cadf47e619ee28a3d4c5ec4eebe7b89738669
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:28:18 +0000

    bag deep hurry surface body

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
