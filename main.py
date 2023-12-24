491a99c0b245a101541503e37662e256e1c9d25e
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:47:08 +0000

    getout bright put too Pp

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def is_odd(n):
+        return n % 2 != 0
