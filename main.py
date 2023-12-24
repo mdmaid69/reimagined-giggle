2dc4255025b4d635a68d2c2db2b40c97ec195432
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:00:53 +0000

    throw from discuss pay region

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def cube_number(x):
-        return x**3
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
