0c997be8ebfc2cdf9baa51055e9909b137e94244
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:51:52 +0000

    soon argument answer to climb

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+print(sum(range(10)))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
