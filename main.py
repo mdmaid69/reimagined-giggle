be074efdefcdc1ded6a7d7c49b285a9b739bf17e
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:45:11 +0000

    special sleep unkind because strength

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import array
+def get_array_as_int(array):
+        return int(array[0])
