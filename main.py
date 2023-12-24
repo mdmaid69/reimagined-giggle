fd27e4f31776de20fba81bb27bf78489e12c3702
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:21:09 +0000

    discovery sound far fear turnup

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import collections
+def create_user_dict():
+        return collections.UserDict()
