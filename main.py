c78d4a9d804105c5ad6e87a166d1dc60e56228c2
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:50:04 +0000

    Qq break somewhere mother event

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  def convert_to_binary(n):
+        return bin(n)
