0f9d796ca6179df044b0a754850bddd6c0d94523
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:34:34 +0000

    satisfied steer basically rest content

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def get_array_as_str(array):
+        return str(array)
