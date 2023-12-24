63851a7a261cac7a1177dcd2a9633bdb28dc18d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:45:32 +0000

    appearance street influence lock cheque

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
