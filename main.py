409427d87e1996599abff4d45fd3615bb38c10f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:21:53 +0000

    tense organize exaggerate law vote

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
