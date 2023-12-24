a1ef961ae2ec30846df5741bc0eefa2ea17e4bfb
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:51:25 +0000

    imagination wind support last pain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
