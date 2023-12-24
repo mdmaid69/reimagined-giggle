b8125b002d3054fa86bce5560b1c625031f5c765
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:42:27 +0000

    female whistle advice concern nervous

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def get_array_index(array, item):
+        return array.index(item)
