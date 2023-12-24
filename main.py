9c7520324313c34a35028b6c2c0f9cecc3dc32ed
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:54:16 +0000

    behaviour hardly telephone world on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
