676a1020b03494d204119208080fe9b76030b91f
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:07:31 +0000

    classroom artificial some permission reply

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
