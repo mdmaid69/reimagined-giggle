215e99a5c9f3a867c620dc092157d2a6fac26c54
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:52:35 +0000

    brain advise solve argument object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import math
 def calculate_inverse_hyperbolic_sine(x):
         return math.asinh(x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
