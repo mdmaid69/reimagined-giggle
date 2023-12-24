c7df87a638fd299373b9a739991ef12e2e7aef8f
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:25:11 +0000

    thick callback behaviour late frequent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
