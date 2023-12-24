3f274573fbf47cff9eefd3e9eb9f2de6ca624e10
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:20:53 +0000

    but bound by actual reaction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
