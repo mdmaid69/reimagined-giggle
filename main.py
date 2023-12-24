5ec3ed815586b40237a0262a804791c243b9a687
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:28:11 +0000

    adjective fetch boring picture type

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
