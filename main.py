885f7b0f1983f85f7fcbafce02d4a5da885d2c4f
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:56:14 +0000

    mysterious mark chat much goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
