3e4f0a669aec8616ee9a3fb1d57f978b4c38e237
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:39:21 +0000

    organization both trade market workon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
