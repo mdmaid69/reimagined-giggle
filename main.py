f95429a65790ece098f9e74ad669cc4a05333014
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:30:19 +0000

    tool reach afford soil hospital

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
