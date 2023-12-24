d515f160f613090c346d86701950c6d016687e22
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:22:42 +0000

    snow person Nn rent open

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
