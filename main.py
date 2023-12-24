c1657de35d76148807c39072b5c557bd1d85bf99
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:16:46 +0000

    law door coat hungry hole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
