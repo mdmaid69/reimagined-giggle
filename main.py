eef50cab0ab6a1a25fe96fb9f5d9ac5db2d0b36a
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:38:39 +0000

    agreement steal once hole key

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def cube_number(x):
-        return x**3
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
