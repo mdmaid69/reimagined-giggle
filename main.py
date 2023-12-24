952e6573ef6e34f3fa5662dd4198903c6b915e25
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:21:43 +0000

    wherever medium section fold false

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def multiply_numbers(x, y):
+        return x * y
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
