e93304333dbfd00601462f77d7a98592074b2439
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:39:19 +0000

    Mm shout represent pen by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
