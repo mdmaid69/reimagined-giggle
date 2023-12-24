6b2ef79db0e5290bd181125ed8e856ad831a236d
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:20:30 +0000

    rich twist scream petrol in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
