750b07cc133cb3b90238a16fd597c1dd04830333
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:19:11 +0000

    tight scale they close wander

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
