9f8421dc29f159ef161859b5c390b6cb2fb71161
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:17:43 +0000

    benefit goaway none use march

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-def calculate_density(mass, volume):
-        return mass / volume
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
