863b62a7f25baeba57e5aaa9422c2e31d1dbc15d
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:52:11 +0000

    even past bang plain friendly

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
