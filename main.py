9535bc02d321fc2c931fa525a25e622216e9bb49
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:26:56 +0000

    protest swing turnaway ear impression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
