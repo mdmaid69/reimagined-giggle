2e24affc011b73b74d6577ffb4abe0537d2372fe
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:14:53 +0000

    pair smile fix pattern feed

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_length(array):
+        return len(array)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
