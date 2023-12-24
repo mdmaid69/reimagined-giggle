9a561aa6b208b8e75ab75d6c74bb224c24e2f1b2
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:00:50 +0000

    concentration understand seriously fence kick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
