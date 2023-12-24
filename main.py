a0e768034f87d5d7c662abb72bb63a74e929f9df
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:43:33 +0000

    cheek public plenty proud fall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import sys
+print(sys.version)
