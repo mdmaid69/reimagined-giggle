8b09f1081c14266c719026d6604b0e473269b744
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:06:22 +0000

    promise string passage edge possession

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+for i in range(5):
+        print(i)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
