6b165e26c2ea23c94451866f2a48eeff758b9d00
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:44:05 +0000

    callup afford contrast section spill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
