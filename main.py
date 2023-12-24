887cce94b12f6f0420157db91a302e85bb9ef738
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:09:50 +0000

    getback uniform raw cost her

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import collections
 def create_counter():
         return collections.Counter()
