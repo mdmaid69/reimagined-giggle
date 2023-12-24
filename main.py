2068fed478dda8728ea73844a7d3255d588816f3
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:51:31 +0000

    doctor act exciting 568energy private

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def sort_list(lst):
+        return sorted(lst)
+def calculate_speed(distance, time):
+        return distance / time
