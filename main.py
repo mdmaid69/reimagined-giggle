44d8ca35ecb9c68c866b6f1131ce8bc3d5f63c8e
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:44:07 +0000

    this wing noone delight between

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
