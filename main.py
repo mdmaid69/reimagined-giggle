d830e6c789b85acffa62f720ba0f8176bd7bce67
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:50:28 +0000

    bridge summer rain trade continue

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
