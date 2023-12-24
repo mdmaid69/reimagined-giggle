ea73ce2aae526d30f53653504d5515e343201187
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:13:20 +0000

    sensible tap talkoutof respect consider

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
