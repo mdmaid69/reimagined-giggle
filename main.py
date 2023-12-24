d1ef82c4e70fa1898eb2f9735ad04253a37f6154
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:09:51 +0000

    lean accident negative 565end still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
