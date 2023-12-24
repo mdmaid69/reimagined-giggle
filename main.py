bebd04aa5f3953b21550825dae093c152bf644f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:07:31 +0000

    reach getback unfair turnout minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def find_min(lst):
+        return min(lst)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
