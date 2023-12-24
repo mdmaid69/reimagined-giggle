e8b0a3862e3d87aec5f9cad97e9107bbd4231de3
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:55:41 +0000

    destroy pen pleasure breakthrough suck

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import sys
+print(sys.version)
+import tensorflow as tf
+print(tf.__version__)
