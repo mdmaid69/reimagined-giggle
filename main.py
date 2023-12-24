30d1b6836f30e7c657914df6ab4b4eeb0baeb77c
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:57:39 +0000

    short thinkof fun second house

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
