9c65c9c04ebafe60f5a7a33314f0e5d23bd2b7ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:43:43 +0000

    base cut boil practical regularly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import os
+def remove_directory(path):
+        os.rmdir(path)
