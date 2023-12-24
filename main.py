c8587de07571ea08bb1945e18806ff00e30b930a
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:13:30 +0000

    storm winter wipe search flow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
