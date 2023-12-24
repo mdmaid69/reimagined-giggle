ba9157d080614554d45c50297ec1d6a1a224eee2
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:51:38 +0000

    balance programme begin sit secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
