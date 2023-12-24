9488f7dbe014ab34e793ae8edab535986c3f87c0
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:35:06 +0000

    deaf ahead praise clearly Ww

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
