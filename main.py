a664afbcefb85d46fef3254185000bdf7cf8a7a7
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:35:55 +0000

    past then pool stand with

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
