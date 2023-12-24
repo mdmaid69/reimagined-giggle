40fb271bf0387216d2a0b444557e626b9f76957a
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:27:24 +0000

    sudden bank whistle cross concerning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
