b00ffb0544634ae45a51f07336d442f614695f72
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:55:05 +0000

    supply disapprove hear include upstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
