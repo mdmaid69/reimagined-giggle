46ded59b1fea7bfdf022210e0da662f3845eb0de
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:24:35 +0000

    command judge boat confusing freedom

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
