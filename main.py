f521745462acb3b7bc17db5ff0994e0cc4afcdfc
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:53:11 +0000

    notice square exact class callup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
