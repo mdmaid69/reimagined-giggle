0fb8163e76c01526cf0eebc13fd36e89a617037a
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:17:14 +0000

    square stare bright square region

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_floor(x):
+        return math.floor(x)
