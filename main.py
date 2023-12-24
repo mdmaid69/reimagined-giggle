7d982c79076dabb13dae3679a816446c33a860bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:53:23 +0000

    imagine education friend feeling rescue

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import array
-def clear_array(array):
-        array *= 0
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import platform
+def get_python_version():
+        return platform.python_version()
