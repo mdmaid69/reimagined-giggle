6e0de9a9e27ee6f104719280725bf429c478b5ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:06:06 +0000

    do character cool all precise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_max(numbers):
+        return max(numbers)
