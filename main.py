fa934e5148a8e2c999ac8966166f83f2a337270e
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:20:48 +0000

    fashion judge your react lie

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def is_palindrome(s):
+        return s == s[::-1]
