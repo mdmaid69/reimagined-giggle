e762d90184951b38e16f8b4267d0a6d6bd337d80
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:11:11 +0000

    sport otherwise guess play complicated

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+for i in range(10): print(i)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
