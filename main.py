ec4523630d886c04a3ad1cab5c8fa8fa4357fc69
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:19:32 +0000

    understand onto makeinto environment let

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
