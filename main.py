3cae8ad999b5e074a5b9c5784a7b4d3cf6e5e8d5
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:58:13 +0000

    attitude return luck drop unfortunate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
