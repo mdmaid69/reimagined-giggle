acdaff8aa54ffc66b27dd86c7b4abe084ecce173
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:56:03 +0000

    adjective transport Dd murder whole

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
