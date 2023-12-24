f547f044d3221f5e963be0db240a6ffa7b455c23
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:02:51 +0000

    city will mainly visit convinced

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
