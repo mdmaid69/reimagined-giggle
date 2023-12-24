14d1b7295c6ba96ea3978c4678f2817023faf011
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:15:44 +0000

    tough throw narrow news exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_work(force, distance):
+        return force * distance
