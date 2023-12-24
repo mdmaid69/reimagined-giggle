2d869a2e0d3c43fea76d596445752860d5a54953
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:31:26 +0000

    frightened grow news regard summer

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
