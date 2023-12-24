f96f6d47a0652ab25324c92035fcb0b039dee528
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:55:55 +0000

    influence shut damage you surprised

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def count_elements(lst):
-        return len(lst)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
