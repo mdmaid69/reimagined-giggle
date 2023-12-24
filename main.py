714064f11dff1238530e1dbf70ba819a0b1e3835
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:50:37 +0000

    such situation comefrom guard cry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
