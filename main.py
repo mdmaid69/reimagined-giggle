4807fbaacb1be19ef9a8c8258fe61bf103b6d2db
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:47:47 +0000

    unfortunate value comeout train against

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_volume(length, width, height):
-        return length * width * height
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
