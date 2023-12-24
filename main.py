5f8c01cfbff805ba2ff76660c142d427a5aa50ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:49:11 +0000

    decision cry come ordinary track

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
