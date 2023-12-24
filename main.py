6fc9020b6a1ee31996a620174a9077d57311dd83
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:20:56 +0000

    wave honest say brief public

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
