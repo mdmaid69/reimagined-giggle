041455953cdab5c420c37637d1195cde6d1a667d
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:16:54 +0000

    trust behaviour team sensible sister

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
