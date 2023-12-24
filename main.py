5c4d8a0eb203d477e4af8c2a4322f98aae956826
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:46:20 +0000

    callup contain partner apologizeapologise expensive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
