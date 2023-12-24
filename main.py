c171b37f26003c31fdf787fc1725adc6dfb2709c
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:44:53 +0000

    choice allow wind tune achievement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def reverse_array(array):
-        array.reverse()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
