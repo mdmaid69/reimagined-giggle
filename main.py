9f96e27736c08b48f42523642cd84b7424a625a5
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:28:53 +0000

    friend date meeting unit clean

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import sys
-def print_python_version():
-        return sys.version
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
