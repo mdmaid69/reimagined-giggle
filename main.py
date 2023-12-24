283617a775432bb01d697e2e7b896a6764758c22
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:09:05 +0000

    call different life danger thirsty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
