e3e371246df804e0512f964338bb4241ec41f1fd
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:23:05 +0000

    vanish rare feed you exciting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
