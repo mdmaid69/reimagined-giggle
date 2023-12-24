3ce1dcafd3c1160f702c503d9299513350ceaeb6
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:38:50 +0000

    heart once boring fine object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
