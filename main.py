36a1c3d1ccfdcc11856b53903f38c576874d300c
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:39:55 +0000

    take drive class direction character

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
