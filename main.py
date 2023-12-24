01110c5cb2edad1b550ee53fb20f8a3260e1cbc4
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:45:11 +0000

    popular recognize personal middle comment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
