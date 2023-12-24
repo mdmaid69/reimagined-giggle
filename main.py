d9dc709aab5414f4221e731411a1b86c7bf66e2f
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:50:25 +0000

    fall hit this our several

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
