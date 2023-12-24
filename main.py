29f690307f920d6ad67cbf82d0dd9e33a4a7a20e
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:22:42 +0000

    brave print mad scream mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
