8ae104b4f42b3e3b7457335402212b46b2b838bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:58:09 +0000

    hole extra murder kick sharp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import math
-print(math.pi)
+import array
+def get_array_item(array, i):
+        return array[i]
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
