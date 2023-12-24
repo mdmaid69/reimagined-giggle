77290ec26874511d3fbd4d295c45a096ad066706
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:54:05 +0000

    event distance block miss call

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import time
-def get_current_time():
-        return time.time()
+def get_array_as_str(array):
+        return str(array)
