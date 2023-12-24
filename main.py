f0efe9518179d69debfa4fdfd838464dd52e79a3
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:30:22 +0000

    count rough himself length escape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
