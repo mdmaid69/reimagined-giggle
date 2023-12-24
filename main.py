529640538ea6d500d3f62d274c55125753f7c0e6
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:29:30 +0000

    ourselves rise niggaacademy wipe tired

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
