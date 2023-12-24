671849353cd43a479995a84a9bc71ff6b4796bdc
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:07:56 +0000

    smile dance wild pin slice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
