6a6a4b98d0c7422f49d3589fefafdc88c4272604
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:28:18 +0000

    myself condition me later your

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
