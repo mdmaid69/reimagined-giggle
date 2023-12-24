9fb85f16adfd9463895ff4e217a3eca749b47b25
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:42:40 +0000

    demonstrate present high wide imagine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_frozenset(array):
+        return frozenset(array)
