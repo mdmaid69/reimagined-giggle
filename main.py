d700fba4646d6bfc8214d73e483ad2e17a9cdc68
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:53:55 +0000

    threaten offer coffee protest violence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
