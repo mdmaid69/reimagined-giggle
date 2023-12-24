ef8d2b48585b8888f6cb96e469c0af20568bad5c
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:35:17 +0000

    quickly hungry interfere sure contain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def find_max(lst):
-        return max(lst)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
