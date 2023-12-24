2a72f0c34163d3070c20fb5a57bd87a6c3709cf4
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:39:38 +0000

    move port buy solve neither

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+print(sum(range(10)))
