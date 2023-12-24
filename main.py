b1290711cf2396325a3e7c9f369d640173e097e9
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:26:24 +0000

    brain bury describe many dead

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_perpetuity(payment, rate):
+        return payment / rate
