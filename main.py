1fd8477fd7fda06a3ab1adfbe5feae3b941774b4
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:01:10 +0000

    bury lookup weather afternoon absolutely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
