72f8f9279702d4e3ffd3bc8cc3fef4a732f33bca
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:30:18 +0000

    favour forget poison spray problem

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
