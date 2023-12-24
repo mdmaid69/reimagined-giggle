a6c3beb3f47eda087d1039958f8f99ce8ed4cda4
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:27:30 +0000

    force contact process unless female

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import sys
-def exit_program():
-        sys.exit()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
