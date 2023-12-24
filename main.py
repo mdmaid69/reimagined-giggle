18913c7b9e691f3a84caf204c1496bb950724bee
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:09:34 +0000

    across live truth object new

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_user_dict():
+        return collections.UserDict()
