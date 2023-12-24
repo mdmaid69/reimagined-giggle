5b1f1a3aff3a0bbdf33a60b8f84be2d6f67650c1
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:37:19 +0000

    back get interview bear mind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def find_min(numbers):
-        return min(numbers)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
