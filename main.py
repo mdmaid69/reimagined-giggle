9a59b8ca408bc83a6cab607d6e3531f1c0f3ea35
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:32:04 +0000

    law mention fasten model ticket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import getpass
+def get_username():
+        return getpass.getuser()
