50f4436b648da45a5bb0f0fd6bc8656ce2a0dae7
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:53:22 +0000

    glad distant miss afford take

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
