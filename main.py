c709c145be32082e6da009c9ebb30da6d98e07b7
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:08:33 +0000

    calm ought fall Itself spill

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def divide_numbers(x, y):
-        return x / y
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
