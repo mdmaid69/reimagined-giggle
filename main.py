02ade320d1555abf9ef3bcdcf3d75243862f6709
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:40:57 +0000

    stick factory big fairly nothing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+print(sum(range(10)))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
