65116e5fb0e58556015aa9b8b3f980e16d116fe5
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:19:12 +0000

    Gg anybody play Tt window

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
