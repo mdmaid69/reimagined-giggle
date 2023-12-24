58b61c0229d12b42623c2e46ac96ee3d52f6fc53
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:46:36 +0000

    length sky government most absolutely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def count_elements(lst):
+        return len(lst)
