ac8cfb13784a7d5194c5770c94f0df759701757e
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:51:18 +0000

    swim limit safe funny straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
