67ebb46ca153bf5096ca0b1d20a3936f88554383
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:14:59 +0000

    treat slow compare attempt shrink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
