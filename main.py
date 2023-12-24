cdd2b6f4fc8701f22e6394927c88ad2b9cf0afd2
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:59:57 +0000

    believe object trade first connect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+print(time.time())
