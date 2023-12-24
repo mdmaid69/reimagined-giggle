40577d121a4a113591e51fd4602e4a99da87b995
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:38:08 +0000

    permission mess runinto interview team

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
