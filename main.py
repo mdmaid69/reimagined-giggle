2674572bd5ea729317e478ace83e5a1b54ee35ec
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:46:29 +0000

    by Nn lucky nor turnaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
