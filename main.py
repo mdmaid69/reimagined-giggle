d3902545335c9eeaa287799576b3cb3dea29eed2
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:41:03 +0000

    case cost opinion meanwhile manner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_unique_words(sentence):
+        return set(sentence.split())
