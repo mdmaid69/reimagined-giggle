421b7c98074da532b8bcf19641914b54a439c401
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:39:56 +0000

    cook luck thinkover constantly switch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def count_elements(lst):
-        return len(lst)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
