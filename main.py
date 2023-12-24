d5d855f357c6f11915592fad83c76037d6ecc5cb
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:53:24 +0000

    tear wait share characteristic it

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def find_min(lst):
+        return min(lst)
+def count_elements(lst):
+        return len(lst)
