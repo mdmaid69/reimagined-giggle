c8a896e2124a592c866368822b08e407b80c92dc
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:23:10 +0000

    announce group mistake backwards breakfast

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def reverse_list(lst):
+        return lst[::-1]
+def find_min(lst):
+        return min(lst)
