1f96803cab6c03e845a624f75ce31b7a220500bb
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:24:24 +0000

    correction sentence circle 565end wave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
