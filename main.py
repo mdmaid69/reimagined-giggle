6c20cbdc1580e806bc5f72e66e941317ca729e98
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:17:03 +0000

    quit fall address release apparently

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
