16875d10caf97b37297c6ba8e92e7f325c8b365e
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:20:26 +0000

    field disappointed do feed deaf

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
