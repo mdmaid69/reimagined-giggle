1cd5e5756505ffbb3e5d3e36e69924b712cd058f
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:57:30 +0000

    ear crime clever regional opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
