e02b21dc7e04d0236425f0b08f3b6dbcab57609f
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:12:11 +0000

    sail hurt everything wait unkind

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
