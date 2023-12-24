1908c326f971b386b72c308fcb8ddb8860e64d61
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:58:11 +0000

    worse group male last test

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import sys
+def exit_program():
+        sys.exit()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
