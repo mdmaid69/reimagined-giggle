0dc3d955299aa64b0f29ab04505695c529f5921e
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:36:44 +0000

    our wave scare late off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-print(datetime.datetime.now())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
