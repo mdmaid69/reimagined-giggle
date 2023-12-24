be8c8abf1f114dd249a6646eb2bbbee497dfda99
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:44:03 +0000

    embarrassing decide operation trust smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
