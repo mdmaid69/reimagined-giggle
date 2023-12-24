5ec92081dbb66311742c743590cb9970dbf8d8eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:15:38 +0000

    long argue behind weak married

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
