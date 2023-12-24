b9d5ff397a2bd26e7e53c0e568a6b084a8a2669e
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:16:10 +0000

    approve interested acceptable coffee sport

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
