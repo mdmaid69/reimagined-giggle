a99e7ce9f8bef1ec3f7abba57e27a610ea2f6e53
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:54:17 +0000

    object achievement delight it advise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
