d2c555b226d510732356108b6c3af9471b3253da
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:08:19 +0000

    shut string stop save miss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_current_directory():
+        return os.getcwd()
