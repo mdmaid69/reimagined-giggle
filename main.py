6ba8a979d0d21636280425d752a4de38143e52d3
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:44:56 +0000

    spite accidental sport date neat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
