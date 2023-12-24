c2d6b3edb02cc0eb4013c7788146c248b8b65e15
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:08:32 +0000

    gas dry just obvious house

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_as_bytes(array):
+        return bytes(array)
