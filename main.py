0a2f2bb67d277be79e2780257ba554933d010ed4
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:22:29 +0000

    night him slow slightly world

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
