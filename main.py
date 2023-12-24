14a9e3155c601ac88386ed7ae3459e97adec2fe8
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:10:56 +0000

    type hide later cold discussion

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def is_palindrome(s):
+        return s == s[::-1]
+def subtract_numbers(x, y):
+        return x - y
