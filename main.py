57cd39704e849d8ec2413da7a8aa0e90b1fab75c
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:22:07 +0000

    familiar 568energy pretty no official

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
