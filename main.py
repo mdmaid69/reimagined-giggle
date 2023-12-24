9ca48d05653231962c2f48acddcbe09bbbf6b0d3
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:14:43 +0000

    before press chance single art

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
 import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
