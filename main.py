39d1b31668f8240e756acfde142f5c3c9576a119
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:20:47 +0000

    refuse patience female reply correction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
