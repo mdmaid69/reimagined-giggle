7f0ce5d4a991f6542b03aa074529d2392ed82d80
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:31:10 +0000

    design maybe thin activity special

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
