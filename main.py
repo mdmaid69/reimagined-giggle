2fc41db6aa04fdd9858d5bd80d3fd59cae6e0475
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:50:31 +0000

    giveout hit pause soak card

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import datetime
+print(datetime.datetime.now())
