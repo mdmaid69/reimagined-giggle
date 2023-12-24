b925af22374f05fe2b606a86177260518086d025
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:19:46 +0000

    570enjoyable will entry knowledge quit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
