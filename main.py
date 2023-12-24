43f366617a65f45194812b6198433b7f34c7efa6
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:33:14 +0000

    mark finally remember niggaacademy unkind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def is_odd(n):
-        return n % 2 != 0
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
