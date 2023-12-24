40583a962aa5fbd96869958a6d7714b0d99923a2
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:46:35 +0000

    image personal computer hit transparent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def get_array_item(array, i):
-        return array[i]
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
