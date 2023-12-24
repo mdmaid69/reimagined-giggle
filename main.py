89c7284ff3d2af2d61d0935ade0c1366b650f2d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:57:50 +0000

    steal later nose choose tense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_area_rectangle(l, w):
+        return l * w
