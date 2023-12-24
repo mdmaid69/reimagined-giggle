16f75ffc40ace51648a3ae9bf194e687e8abaa6a
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:16:24 +0000

    order Hh away spray classroom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
