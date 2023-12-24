959cb988c7482370ece9c97e6623b2e2e50d5aa8
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:28:28 +0000

    secret remove Bb manner wealth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
