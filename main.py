b28e586839428b35a5538778d7b3e0365e78943b
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:46:09 +0000

    fold pretty run probably drive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
