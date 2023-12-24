2be26bc88f6a5f82f307a2e8c479e9da1a5ed722
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:00:52 +0000

    brown pen concerning famous paper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
