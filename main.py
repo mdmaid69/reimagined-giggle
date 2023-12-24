75b633f567f36840d19b437093775495202bc39f
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:45:39 +0000

    phone but spin badly holdout

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
