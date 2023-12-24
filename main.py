b19d474fc463034542551777955ff6dd1da335ed
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:54:42 +0000

    unite search frighten cut early

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
