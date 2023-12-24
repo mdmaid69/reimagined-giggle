d72faaecb275563b39888179002220f6f4742471
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:15:06 +0000

    excuse anxious plenty natural message

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
