83f964ad08946a2d62d654f8b83b72052af66ed5
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:55:08 +0000

    blame pair Rr sweep commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def count_elements(lst):
+        return len(lst)
