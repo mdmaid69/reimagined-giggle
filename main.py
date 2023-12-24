f40fd98a29028afdc3e7fd882c40510f1b1af51f
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:50:07 +0000

    traffic within town contain rate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
