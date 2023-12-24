760f288e4b06f1eb63fc5668187ed483efb8e6da
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:15:50 +0000

    sensible fall autumn healthy his

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
