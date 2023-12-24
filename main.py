6952a3f0f9a84032a75bc10d8f358ceeab8ff3f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:10:22 +0000

    favourite form please platform noisy

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  def add_numbers(x, y):
-        return x + y
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def split_path(path):
+        return os.path.split(path)
