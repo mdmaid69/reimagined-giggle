13b041673c65791f1add0452b9690af75eb329e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:12:09 +0000

    own sign cry sign picture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_bool(array):
+        return bool(array)
