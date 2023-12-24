a80c4bafe9bd930e5500781a5e19bb650f09fad3
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:47:25 +0000

    authority size Gg bend tough

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import getpass
+def get_username():
+        return getpass.getuser()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
