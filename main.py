778ae9790b1201604ac4426109cd950f3de1bc35
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:19:24 +0000

    nothing exhibition mine people press

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
