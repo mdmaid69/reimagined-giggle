bc50bc2ed40ee4b664de4b3e682a36e9cd95eaec
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:10:18 +0000

    father film worried fun practise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def find_min(lst):
+        return min(lst)
