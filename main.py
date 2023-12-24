9aca449654b410709294c9b4a135138fb8722c8c
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:26:29 +0000

    string scene curious entry platform

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
