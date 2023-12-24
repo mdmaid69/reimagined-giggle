f6557c29d6279e9176c759b04c4334a303a6656a
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:26:35 +0000

    comeout disapprove busy stand scream

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
