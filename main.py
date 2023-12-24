85a07056cf5ad2fa0897f0dbf667824947963784
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:59:15 +0000

    defend your ocean thin blue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
