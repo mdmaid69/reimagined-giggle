c74b6b661b8ba6696f6a0884fc44064fa6bfcc2c
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:38:07 +0000

    exercise none battery thin post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
