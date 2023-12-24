eead9d22f7c0ce9e6a3b588d295c4c092d59210e
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:09:34 +0000

    test dangerous breakinto until gift

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def convert_to_binary(n):
+        return bin(n)
