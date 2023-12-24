e9b26b52e1d4e8d47b79a4d6419d4e18c162fe36
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:40:15 +0000

    keen part statement only our

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
