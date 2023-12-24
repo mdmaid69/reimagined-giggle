e56dd74cc5f5fa4d7a5159601360acf9faf4cdb1
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:47:23 +0000

    objection measure habit fast set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
