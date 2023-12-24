81d8bb14831d795f079c485a44bec649430b6c55
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:59:22 +0000

    oil visit blind aware sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
   def delete_file(file_name):
         os.remove(file_name)
+import array
+def get_array_length(array):
+        return len(array)
