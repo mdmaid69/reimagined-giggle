8147f75e6020403e2864b3c446e091e82e3628ff
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:15:02 +0000

    body suddenly brave exist keepup

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import os
+def change_working_directory(path):
+        os.chdir(path)
