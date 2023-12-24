ca2746a9365358a90f3af7de26254eba7127c544
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:14:28 +0000

    keep project news trousers regard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def is_even(n):
-        return n % 2 == 0
+def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
