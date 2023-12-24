a3b25379220c270511ee06771581fbccc9013abb
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:16:49 +0000

    target tour agree guard indeed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import os
+print(os.getcwd())
