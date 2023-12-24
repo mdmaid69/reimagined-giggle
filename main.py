d409b21404078a4a7dda6d18ddf35d78ce7d4fb0
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:47:57 +0000

    confuse shut Ee 566end unkind

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
