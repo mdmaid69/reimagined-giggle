07bdb06040d1cf160ebeab3dc050d99c1530c445
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:46:54 +0000

    choose reduction neither it glad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def subtract_numbers(x, y):
-        return x - y
+import sys
+def print_python_version():
+        print(sys.version)
+  def sort_list(lst):
+        return sorted(lst)
