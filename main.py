bf61cec170e366edd08fb7efec70f8a1671d7cd2
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:23:59 +0000

    difficult his spare themselves distant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
