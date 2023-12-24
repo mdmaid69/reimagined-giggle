f48ec6087724b70e6ec022fc6b7809b573b47c1a
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:15:32 +0000

    stir stamp neither spread tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def is_odd(n):
+        return n % 2 != 0
