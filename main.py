92f4b4025ba870ec5e45ec6bdfc51e431f14a089
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:31:39 +0000

    listenfor pure damp complete calloff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def find_max(lst):
+        return max(lst)
