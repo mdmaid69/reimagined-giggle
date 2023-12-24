5a2f05fc2a1ef6115f20c76898f66b46f66e4dd1
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:50:33 +0000

    round mother moment operation well

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
