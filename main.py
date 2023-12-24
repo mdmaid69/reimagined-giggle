4a9ca548ec4a5fc893cb5f6b6678356b624f20cc
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:14:26 +0000

    only listen stop confusing sight

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
