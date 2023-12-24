e519508ed8317b1c5f73f2d5d59525686361cdec
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:34:45 +0000

    carry cross that colour disgusting

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def square_number(x):
+        return x**2
