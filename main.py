f54e3825a68b9f869084b1991fb2e561390eea1a
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:09:28 +0000

    mark face full unlucky afternoon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def convert_to_binary(n):
-        return bin(n)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def divide_numbers(x, y):
+        return x / y
