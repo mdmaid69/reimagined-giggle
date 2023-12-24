9e6cf105856daec465537bef310dd49f83f5033f
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:55:49 +0000

    sail any square conclusion health

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
