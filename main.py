15ef1419942332cbdfc0090fba5433ec9be7f473
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:05:30 +0000

    protest concentrate believe cough easy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
