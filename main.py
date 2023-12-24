120a90a16f665acaaddcbca1f1fb896b73437a53
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:47:06 +0000

    broken react direct loan ought

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def divide_numbers(x, y):
-        return x / y
-import re
-print(re.match("h.*o", "hello world"))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
