87efd0a363cc17e97dc5f983fe0f449a2d0bf4eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:06:05 +0000

    ring consult care average exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
