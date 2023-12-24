cf114ac1f04974f6499f7e471d63bb38d13ffa7e
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:29:17 +0000

    kill agreement bringback boat hair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
