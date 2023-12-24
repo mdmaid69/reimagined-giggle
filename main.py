d7566365b641bbbd33179850bd1ad5817450b0be
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:12:02 +0000

    stand ability glass bound peaceful

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
