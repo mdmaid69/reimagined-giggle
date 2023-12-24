8556cf7ce56cad5fa3f210fd4b504d157fdf8f56
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:51:21 +0000

    glass side stranger damage choice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def is_even(n):
+        return n % 2 == 0
