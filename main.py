5de26c82919a26533babfb5f2826d0e9334633a0
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:02:23 +0000

    strange similar manner time least

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
