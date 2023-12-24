6f05789e9edcfd1a3f0410a14d31e0c1396ba085
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:47:36 +0000

    action this art land pretty

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def sort_list(lst):
+        return sorted(lst)
