aa33f7a5375f9eab99f8f0d7c532438b9e2476aa
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:54:38 +0000

    nurse able ignore turninto exist

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
