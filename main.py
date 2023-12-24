c72d513caea56d1f587390e7671ae89e5c039f75
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:32:38 +0000

    hot offence nice contain if

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import collections
+def create_priority_queue():
+        return collections.deque()
