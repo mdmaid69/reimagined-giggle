280f64ae03c97d07c255e29ed452a23ebd212744
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:58:33 +0000

    regularly there love OKorokay edge

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
