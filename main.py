770e3c94e93f0d25ef07e77839f85b273bab916a
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:18:32 +0000

    getup soon door affect free

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import math
-print(math.pi)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
