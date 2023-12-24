ca8711e77e62d6f2d42b25cfba1a3f264eec569f
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:19:28 +0000

    hurry cry where fear bill

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def divide_numbers(x, y):
-        return x / y
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
