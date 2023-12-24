e070afe4278cd07751445a86cc9ed88271839066
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:32:44 +0000

    have far their takeback left

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def subtract_numbers(x, y):
-        return x - y
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
