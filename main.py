ad5ed58fab44a72ee87e58352459e9e9625822a6
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:18:49 +0000

    eye late someone bunch bush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
