2d2dbd8df379cdb0147190ef741560383889742a
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:38:37 +0000

    record represent recommend law create

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
