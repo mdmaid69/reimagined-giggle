7cc9ec1a0365dc062e6a1025068c7a45df253f1d
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:47:56 +0000

    wipe in grow sink necessary

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def cube_number(x):
+        return x**3
