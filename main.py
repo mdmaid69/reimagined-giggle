fd50f621f7c2aa3dbc25cf498203437353f3286c
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:46:11 +0000

    glad noise chief protect listen

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
