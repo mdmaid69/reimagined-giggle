c1fdf5db098ef669b9249831000cad7b2a812c1e
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:31:37 +0000

    operate stage usual apart force

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
