3dd08ee0469028959c3cc7aba5c85465a5a5303b
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:57:11 +0000

    best force split relation ashamed

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def calculate_density(mass, volume):
+        return mass / volume
