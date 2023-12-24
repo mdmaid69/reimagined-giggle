efa3f855ac60508a2c7bbc4ef981e42bd2fff0a1
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:34:50 +0000

    lead right challenge die slow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def append_to_array(array, item):
+        array.append(item)
