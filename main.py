7658a072fae57f202a0f48ffe7e67ddbfdb15bba
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:01:59 +0000

    turn smell worried exploration exact

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import array
-def get_array_typecode(array):
-        return array.typecode
+def find_max(numbers):
+        return max(numbers)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
