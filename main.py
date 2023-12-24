6220955ec31b21afc19778089f3afcc02bc41fbd
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:25:49 +0000

    or disturb ask shade order

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def count_characters(sentence):
-        return len(sentence)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
