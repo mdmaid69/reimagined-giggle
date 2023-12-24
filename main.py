78fcc5409095bb1da5cf0b2eb7beb8c3e43ade8c
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:17:17 +0000

    upset release improvement on achieve

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+from collections import Counter
+print(Counter("hello world"))
