f8ed640997860b57e46f2878674066bdbfce60e8
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:12:59 +0000

    fail prepare next head explain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
