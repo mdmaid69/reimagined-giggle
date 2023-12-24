8f52d201557a6240af952fc38846b8f2add24b1b
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:18:46 +0000

    have school wealth stiff suffer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
