fa9a97e1dcc98196d6991b366b518a374310453a
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:32:00 +0000

    occur back window thick tax

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def multiply_numbers(x, y):
-        return x * y
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+for i in range(10): print(i)
