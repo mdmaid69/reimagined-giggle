aea8f2788f4b23dbf5788d91cdaaaf68cef129c0
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:33:38 +0000

    passage file it appeal Jj

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
