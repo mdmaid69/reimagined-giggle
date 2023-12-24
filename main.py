610cfdc8d6c0e668c085c6b1d41804155a07f3ae
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:06:17 +0000

    home collect petrol sweet voice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
