fa13af1be664dad0e0deafc4e8c71af0a0d30fc1
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:40:57 +0000

    neither means consist book excellent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
