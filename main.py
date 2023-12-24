58b978ac3a2bd04665cd7e5d9d8a419c50fd4bbc
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:17:51 +0000

    comeout chance coffee cause free

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
