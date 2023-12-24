60fceaaac15d4bcaedd3ca2cd8302ce7370e77c1
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:02:50 +0000

    mess cost low pen everything

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_item(array, i):
+        return array[i]
