988baa961b0f7a78b469be3fe9c4800a73d540ea
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:49:21 +0000

    underneath further balance excuse although

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import array
+def reverse_array(array):
+        array.reverse()
