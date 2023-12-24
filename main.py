e39a64cc38182d6c79c89e8d5fc086304bf9e71b
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:44:00 +0000

    practise properly passage probably name

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_cosine(x):
+        return math.cos(x)
