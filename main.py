380b0a95ba533f3ba48fc4dd71fe78485a516a33
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:45:42 +0000

    blame confuse roll turninto nigga

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import collections
+def create_queue():
+        return collections.deque()
