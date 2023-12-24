e531460d89b347f6808027d64301a1f8b975d650
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:19:29 +0000

    dare in hang separate see

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
