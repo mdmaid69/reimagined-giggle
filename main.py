a4039d7e65936f4aaf9469e7d2f8ef5c7add56bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:07:09 +0000

    luck upstairs image result special

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
