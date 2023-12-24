36d78ac56619517b38e660f28ae5a11c2d7985db
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:08:47 +0000

    find challenge trust whether with

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
