3f2850f79fbccb3448616870ea262fa649f935e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:14:52 +0000

    fast combine well excuse hair

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
