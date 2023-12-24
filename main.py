a1f1768e2602ceae7b8ea230f31b51a582643c05
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:36:19 +0000

    aim positive at century Bb

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
