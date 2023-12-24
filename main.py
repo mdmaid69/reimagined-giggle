3aac0c1aeca498fde9317841aecae0293049744e
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:58:48 +0000

    should impression ground around relation

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
