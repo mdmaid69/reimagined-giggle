366510526de8ae39884a12d4af21dcf915503405
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:54:20 +0000

    like silent content vote teach

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def sort_list(lst):
+        return sorted(lst)
