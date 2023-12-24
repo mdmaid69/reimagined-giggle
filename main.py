eb9d50ed4d004badff54789b37187a4f53be3698
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:11:18 +0000

    separate crowded couple care commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_acceleration(speed, time):
+        return speed / time
+import collections
+def create_queue():
+        return collections.deque()
