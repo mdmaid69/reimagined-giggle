f506e6a22fde8c5131d0971ad86ea06567616c45
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:08:46 +0000

    pool begin politics shoot opinion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
