a0077aa422d4f2d4f2215b39a1685d93f3c61f3f
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:23:51 +0000

    cinema block zero fetch material

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_stack():
-        return collections.deque()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def clear_array(array):
+        array *= 0
