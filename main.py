c0f5e99541ff1d6c48643252f34c17869f57e67b
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:57:31 +0000

    true disappear crazy thick moment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
