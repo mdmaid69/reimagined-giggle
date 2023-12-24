422bc74d04d2b17f0a0e221a5bedd9e28ea46f13
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:05:00 +0000

    section miss entry taste secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def count_words(sentence):
+        return len(sentence.split())
