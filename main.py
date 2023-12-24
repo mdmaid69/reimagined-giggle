88f3b447221384a7e7542b09cf8c45cd3ed81611
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:12:48 +0000

    right aim rise terrible lesson

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
