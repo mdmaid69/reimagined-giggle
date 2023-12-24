91d6cea70b37574e2f37529ce47a5aadd6f7271b
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:14:12 +0000

    freeze copy uponon direct fire

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
