ae4552d2e29ed97067b5833ca94245355349863c
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:49:09 +0000

    comeup famous near sensible child

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
