7846d24955f2b66751cc6425897a162a66a1a560
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:35:37 +0000

    prize instant value to programme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import os
-print(os.getcwd())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
