bce34b843da1c53197a7b1ecbddc11f6d6adc426
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:41:29 +0000

    its doubt judge run giveaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
