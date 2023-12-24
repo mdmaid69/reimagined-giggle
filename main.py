5859d9c7a3de6dc70b7536f15836170ad33b5a19
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:43:01 +0000

    owe my true bank next

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
