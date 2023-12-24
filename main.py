74af5356f8f4ca2fdeb6d2624adef3d21978e659
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:49:02 +0000

    opposite explanation part lead exist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
