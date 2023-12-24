67abe4620a7db384a325263f214fec7247bb306b
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:12:57 +0000

    mind fix train afterwards pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
