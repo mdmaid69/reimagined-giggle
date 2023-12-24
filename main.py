602adbdb1fc20001006a93f1a9d21f5dcc7ed35e
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:49:57 +0000

    before dress measure live space

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
