fb10a38042f7bd7a4ee8c39554716e3469efb078
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:25:30 +0000

    advantage his creature arrange take

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_work(force, distance):
-        return force * distance
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
