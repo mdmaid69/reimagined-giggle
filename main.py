b441651d1b20d449bf1377c233bb4b29aec7c1e0
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:16:08 +0000

    statement tonight ilovedicks world reduction

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-  def is_even(n):
-        return n % 2 == 0
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
