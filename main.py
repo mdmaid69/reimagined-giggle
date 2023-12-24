c7096223b093511eafe06498c95ae9bfa86d147f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:37:51 +0000

    opposite takeback slice free example

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
