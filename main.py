39bda42d8322951ab5bcbde55a9311b8e176f808
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:00:46 +0000

    trick bet workupto put band

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-print([x**2 for x in range(10)])
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
