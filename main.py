902d93af77365e505707f60c0f96fd724478d0a8
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:07:11 +0000

    where unite official mark touch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
