c5fd8961b19c91b64911ed9cc846966f7d147ad5
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:39:54 +0000

    wind wipe wide use flat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
