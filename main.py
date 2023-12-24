b9e4919a7fe3f8e55ec53aefebf2e371dbd45d51
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:16:41 +0000

    point success arrange split them

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
