79a99d8f0893c6a988811785c77c7a81416d1628
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:09:11 +0000

    thick exhibition remember obtain operate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+for i in range(5):
+        print(i)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
