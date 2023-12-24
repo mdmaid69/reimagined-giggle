e4dc89df174bd060d01dc7f2e1050a2c97a67355
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:35:38 +0000

    hair book treat represent load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
