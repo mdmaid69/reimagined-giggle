aef688f15e0b599516e3244a7a7bd1896167c06c
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:01:07 +0000

    wet uncomfortable juice extra motion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_float(array):
+        return float(array[0])
