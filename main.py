53841a3e9430f064c927567feca040c576522d67
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:40:50 +0000

    great onto bag tool science

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
