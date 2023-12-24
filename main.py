c6939cefc4e00d144cddf7efbf53089160602716
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:27:17 +0000

    thirsty attract interview instead advantage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
