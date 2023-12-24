23815f17f08c4f833e4a8489f48dcf7cc9378531
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:31:36 +0000

    responsibility neither mistake right runover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-print(math.pi)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
