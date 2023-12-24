4eb66ef2f58941fffcb7b3effd1805feda426748
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:56:03 +0000

    fail hard understand illness soak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
