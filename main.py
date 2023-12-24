f28c280a87dcd83be3a8e36325e2bbebed5f0f8d
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:26:26 +0000

    hour mind thinkthrough basically interest

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
