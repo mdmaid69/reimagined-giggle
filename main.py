d0d9060c016a6573728fb9249f570d2622c83de1
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:29:12 +0000

    roof discover total modern here

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
