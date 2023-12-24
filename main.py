4b36ccfeea41af00493153fa0ceb9cd29aebcd9b
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:31:16 +0000

    past before form flavour mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  def square_number(x):
+        return x**2
