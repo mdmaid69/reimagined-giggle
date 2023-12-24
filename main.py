548e74b06fe0700eb3b4befb3875c4c98ead8664
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:17:20 +0000

    tidy extra angry scene comfortable

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def divide_numbers(x, y):
-        return x / y
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
