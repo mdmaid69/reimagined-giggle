9f72440e68b669dfe549ff2bf5ec0d4dbf2b48df
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:53:02 +0000

    so and date road slight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sys
+def print_python_version():
+        return sys.version
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
