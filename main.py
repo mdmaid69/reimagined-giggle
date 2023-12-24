a029903c9ee4c1298a8a6d10c52742350f91e7f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:41:26 +0000

    Ii satisfactory plastic most release

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
