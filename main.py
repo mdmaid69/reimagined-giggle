ac40f6df0ab271de3162f7e942b371e3063921ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:26:21 +0000

    previous help check crisis month

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_power(work, time):
+        return work / time
+import array
+def append_to_array(array, item):
+        array.append(item)
