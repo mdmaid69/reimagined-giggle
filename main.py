8be706ddc1db5926f5b5bccce18c79c369fd6d34
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:22:57 +0000

    fire ring but present capital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import math
-print(math.pi)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
