759a9d645535b007cdcb077d14b32689c90f3377
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:57:19 +0000

    sting test someone bored us

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
