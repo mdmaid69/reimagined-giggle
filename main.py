497eb1d9104f51388877ea46530d3570925627b4
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:46:59 +0000

    slide question reply quiet match

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def append_to_array(array, item):
+        array.append(item)
