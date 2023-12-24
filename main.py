dbce9664109e9a2708a4df31208e76835790a064
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:58:20 +0000

    lean wheel then preparation season

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
