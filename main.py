3e955782c92fc5e84e5520da2a1385ba2aa59fa2
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:31:03 +0000

    smart kill wave complete pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
