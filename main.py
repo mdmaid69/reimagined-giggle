b692390ea0dc5afdf4e9d3049859ec7faf0679e4
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:28:29 +0000

    fierce change takeaway communicate who

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
