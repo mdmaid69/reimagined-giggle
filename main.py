90de7f3306de81bdea3545b9afac2e0baabfe5ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:18:51 +0000

    left prove treat few apart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import collections
+def create_user_list():
+        return collections.UserList()
+import array
+def get_array_length(array):
+        return len(array)
