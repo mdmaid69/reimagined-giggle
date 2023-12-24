e95ad2de9b112b758e50cd9e33cbb139791622e5
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:12:11 +0000

    certain minimum key care favour

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def append_to_array(array, item):
+        array.append(item)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
