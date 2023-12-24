7d0cb6e5433bee2cb1c158e51c70ecfe4188c1d3
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:33:22 +0000

    transparent somehow smooth temperature sail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+def get_current_time():
+        return time.time()
