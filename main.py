aea2a88d7e08cc1892bbf3a0403c134e9af1889a
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:04:14 +0000

    exit fashion disappointed instant contrast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
