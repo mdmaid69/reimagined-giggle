1c4f38973c09e6a36d91cee47244078fb7d6f236
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:11:42 +0000

    talk criticize burst live excellent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
