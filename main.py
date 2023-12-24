57d25c197e248915f3ff6ae9fec1c92986dc393f
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:11:01 +0000

    over straight whisper either live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
