8d851ac3643d2a88ae652330729a307db28b70d8
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:00:36 +0000

    excited rob warm ought tell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def subtract_numbers(x, y):
+        return x - y
