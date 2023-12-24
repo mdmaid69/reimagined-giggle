106c806db15693e0fe8899350f343c8f9c34b6c8
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:33:08 +0000

    hot square wise ever cover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
