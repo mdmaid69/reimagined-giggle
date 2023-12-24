0d31525a74586d728b207ff6b81c3c22fb90ce0e
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:03:36 +0000

    slip win summer instrument cheque

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
