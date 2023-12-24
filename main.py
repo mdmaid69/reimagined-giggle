2f3d0959334198effb5792aa5b2b20556ebde5ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:37:02 +0000

    centimetre before ground do cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
