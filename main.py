7bdd20c81e656236a3f219d1b14b805bb33e229f
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:42:29 +0000

    couple on tall pattern simple

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+print(math.pi)
