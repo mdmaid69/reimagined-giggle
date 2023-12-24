78449d188bb956b057d8e25e2ddb3e59cf60aebc
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:44:27 +0000

    borrow permission live past she

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
