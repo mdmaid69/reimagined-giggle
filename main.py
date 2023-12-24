ee2d544f2344b32873bba01854ed10cbdc858a9b
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:21:59 +0000

    stone influence goback(to) Hh knowledge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
