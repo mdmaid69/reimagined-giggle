11414335f2430152dbeda8bad284f0b41039f01c
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:19:35 +0000

    pick tool spring autumn risk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
