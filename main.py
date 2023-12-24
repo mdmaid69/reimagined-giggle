5935be4b48b69ba56f730c20ad5e15b3fddd7ab2
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:06:51 +0000

    private moon content spare new

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_area_circle(r):
+        return 3.14 * r**2
