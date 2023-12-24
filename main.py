3c91f9c65f257513c5c6902b87cf0ee910c84bcf
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:44:51 +0000

    sensitive container top listen ours

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
