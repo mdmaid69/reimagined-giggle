7a7184cc902d1840a3eed6fd01a417eb204bb902
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:28:22 +0000

    necessary wrap comeback care bang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_acceleration(speed, time):
+        return speed / time
