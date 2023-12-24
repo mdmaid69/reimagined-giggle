5581a48fd4d7a94ed3d4063adef03b8eaf7ad094
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:59:42 +0000

    support nigga hers rich scare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def cube_number(x):
+        return x**3
