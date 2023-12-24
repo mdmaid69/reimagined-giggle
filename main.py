8b32c1550f59b2c5f0db9c5313d2caed9709633c
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:49:04 +0000

    thing frame tonight rub tour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
