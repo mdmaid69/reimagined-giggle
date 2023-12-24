e96871b71cec8a62e63e893852a64d2ce7e11c60
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:55:51 +0000

    physical neither worried cruel usual

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_force(mass, acceleration):
+        return mass * acceleration
