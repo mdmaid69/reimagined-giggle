08c9e15abb56f4a84b1927373ab73c08f7d54371
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:06:41 +0000

    turnup black without agreement back

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def find_max(lst):
-        return max(lst)
+for i in range(5):
+        print(i)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
