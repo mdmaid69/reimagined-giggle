eae5d935058f1766234828c7b22166b54f2ea453
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:42:59 +0000

    bored popular exciting low treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+for i in range(5):
+        print(i)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
