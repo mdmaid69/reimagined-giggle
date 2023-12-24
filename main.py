0d6810cadb2da2df6bdad4428e3e3c7bbd11e003
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:12:16 +0000

    condition glass mend disturbing breakfast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
