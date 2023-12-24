51969c5ee80ee9df9f724578d56fa9e7b1173a61
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:33:49 +0000

    contribute pass show turnout destroy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_as_int(array):
+        return int(array[0])
