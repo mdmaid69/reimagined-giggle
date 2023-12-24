0a4e95ead1a756544c55f924668f1ad07cf7a37b
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:25:25 +0000

    allow by getoff shut charge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
