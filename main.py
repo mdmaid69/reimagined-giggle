c0236963ed685d69892a98f082411d80bb63fb84
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:53:15 +0000

    nowhere mark disapprove usually move

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
