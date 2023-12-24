89ccc5d28d76d25b51e5097e51ad5c0d08d3df3f
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:28:13 +0000

    neither finger secret my goout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
