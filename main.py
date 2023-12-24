cfb4667b039d0427f16b06ce2cc95b0f6a7fd7fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:09:33 +0000

    desperate early rub anyone turninto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
