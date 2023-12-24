39738a5ef7aa9d5daa6aec8dd94cb3f6ce33d882
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:51:34 +0000

    rely hospital weather sort often

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-name = "Python"
-print("Hello,", name)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
