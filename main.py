0bdb503820ef6348c4acb5f765c967f044475410
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:13:06 +0000

    patient standard beneath stranger whisper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
