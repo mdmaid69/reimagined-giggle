7bc3b9a3650efda5eb499f5642f05e3e6e4a0113
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:17:31 +0000

    fix trust theirs consist angry

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
