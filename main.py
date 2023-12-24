6f072351af7dd4732c72132c95ba098dafb920c8
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:39:27 +0000

    divide operate correction keep impressive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_current_directory():
+        return os.getcwd()
+import sys
+def exit_program():
+        sys.exit()
