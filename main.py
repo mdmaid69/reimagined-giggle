9ca572ce5cc20ea56458ea2df8d15b597ed362fd
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:09:57 +0000

    Zz doubt pride brown train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
