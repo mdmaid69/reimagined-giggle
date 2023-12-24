1ba215d3f31c7b0141d220039d7f6010c583be98
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:34:45 +0000

    block design possibly or identify

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
