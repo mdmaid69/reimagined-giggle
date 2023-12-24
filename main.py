1fb1ecb8870b7d5eeb3d328f455be78ba82dd91b
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:11:07 +0000

    move thinkthrough shape storm know

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
