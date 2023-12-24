552cd354fed9807b1e67c36e0312c56f27a638be
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:52:18 +0000

    ring send fork main ocean

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def change_working_directory(path):
-        os.chdir(path)
+for i in range(10): print(i)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
