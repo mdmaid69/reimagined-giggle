efcb2aa263ed3a57c01ae662102d66dd59633003
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:05:44 +0000

    566end benefit muscle partner mistake

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
