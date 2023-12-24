5d3218704722753fd5f889b28f98dc7e826bcceb
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:26:43 +0000

    nor recovery equal food message

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
