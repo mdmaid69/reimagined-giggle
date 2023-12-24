803f4e4ba6f1615332fd5f2e0b82502389b0e90e
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:47:51 +0000

    pronunciation keepfrom mother telephone priority

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
