c972acad111ceaa07c577d96ce89cdaec564716b
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:21:54 +0000

    except rise outside stranger answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
