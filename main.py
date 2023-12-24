7c53987779797bfe1706d5eae6e28d241d0f2b1b
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:50:36 +0000

    cause situation bath cut spite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
