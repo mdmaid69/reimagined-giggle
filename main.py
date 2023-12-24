c211a7b7634537bfff4138328e67a2e16ea3c632
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:57:28 +0000

    happy breath choose amount attention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
