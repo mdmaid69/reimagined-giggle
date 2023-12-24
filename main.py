22192831ff0bcfa0e620739dadc8739a4ac21f57
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:30:33 +0000

    object effect contrast thank throw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
