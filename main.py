c194295862a97f6d57044e10d24ca095b9864dce
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:03:55 +0000

    calm respect shelter schedule interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def sort_numbers(numbers):
-        return sorted(numbers)
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
