bd8738a36b1391d124c22f7d4d96ceb8cf887e78
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:21:06 +0000

    after well worse entrance stone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
