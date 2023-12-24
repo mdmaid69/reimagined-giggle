fdd8bb9e9824b2d08de02ca1e0c7ade89136e05b
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:50:54 +0000

    whistle issue Hh feature question

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
