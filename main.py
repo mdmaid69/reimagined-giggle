279da3422f12c35078cbab77d5bdadff31a44608
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:40:00 +0000

    deliberately stay party seem still

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
