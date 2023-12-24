5b7d3a64948176f01291e246668f071b70d788b1
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:37:41 +0000

    last OKorokay suffer talkinto sport

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import time
-print(time.time())
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def convert_to_octal(n):
+        return oct(n)
