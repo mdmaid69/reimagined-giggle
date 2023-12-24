06c1b37be13d4e509baef2bba31532035788805a
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:03:42 +0000

    calmly characteristic theirs getup change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
