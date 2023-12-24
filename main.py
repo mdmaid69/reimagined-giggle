27b5e811e8a9dd6a5e9307c2cde8e1e453b70336
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:42:15 +0000

    travel travel remark tend human

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
