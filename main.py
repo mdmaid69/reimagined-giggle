75bb2d03a335a871b98c8efb6241ee5ff0730a77
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:28:33 +0000

    deserve hang mass blind battery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import sys
+print(sys.version)
