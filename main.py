abb74803abd62b5cebb5de4ac043d481259b8a65
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:01:12 +0000

    invite undo turn benefit shut

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
