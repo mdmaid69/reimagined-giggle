be292a6cb0fbdb36d1f0da4f5a2033810a90b6df
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:06:30 +0000

    wave long visit temper until

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
