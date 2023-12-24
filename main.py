75c04282c357bee95b0e2dd9d045d64ed83c8f26
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:48:02 +0000

    steep correct lookup because genuine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def get_list_from_array(array):
+        return array.tolist()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
