54aa0ac9f2790f4e31c91334b10fe2354c68a5a5
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:36:36 +0000

    band air test schedule peace

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+print(sum(range(10)))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
