5d21ad5180a6480790c06404b134bfc248f0084d
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:58:39 +0000

    demand sea immediately print sister

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def calculate_density(mass, volume):
-        return mass / volume
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
