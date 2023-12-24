d1864e7c6da0efa83668c976c9429a883fcb4b7f
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:50:03 +0000

    capital show pressure takeover willing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
