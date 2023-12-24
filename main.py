0ae848e979d98b2beb172f574cd3f93a9c08412f
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:04:57 +0000

    recent play swimming present tough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_hex(n):
+        return hex(n)
