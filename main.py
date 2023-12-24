2de725be06dea447d7899475e3a13673d81a41f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:11:40 +0000

    news affect juice cruel generous

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def subtract_numbers(x, y):
-        return x - y
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
