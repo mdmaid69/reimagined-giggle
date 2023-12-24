876aa5892761c9b70555167382fa20a4ab268f2a
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:20:55 +0000

    turnup turndown generally pretty plan

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
