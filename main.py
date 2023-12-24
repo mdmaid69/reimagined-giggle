4bd69024a5edf77014346ee207acba5a12e40e18
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:21:42 +0000

    finish mark near care jump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def find_max(lst):
+        return max(lst)
