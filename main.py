b3919f7456c123ef2dfe8b6731b40e5530238d44
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:33:56 +0000

    note hollow secret maybe mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def convert_to_hex(n):
+        return hex(n)
