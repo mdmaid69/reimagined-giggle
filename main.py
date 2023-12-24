1f7bd293441c694a87ada2ed53ed724523c7b014
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:02:56 +0000

    colour bath appeal slide gradually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
