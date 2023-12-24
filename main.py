b261c3092b3f53a024d2b5a9d321751962eff659
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:40:39 +0000

    meet attitude card trip music

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
