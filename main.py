a8885a47834281a8391dfdf075a77e139ee422b4
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:27:04 +0000

    to change occasion ours Oo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_even(n):
-        return n % 2 == 0
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
