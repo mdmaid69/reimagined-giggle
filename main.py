865abd3ae5a496f7d1b90381187d259d7b820713
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:36:58 +0000

    recently display anxious unlikely order

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import random
+print(random.randint(0, 100))
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
