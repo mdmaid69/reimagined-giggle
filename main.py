399399e2afe16f9513df704ca8271279c3867082
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:49:19 +0000

    achieve detail turndown include sound

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def reverse_string(s):
-        return s[::-1]
+def is_odd(n):
+        return n % 2 != 0
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
