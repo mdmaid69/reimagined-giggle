8d9386ee10a0e1043d4d7e00baefb9678da8c6b2
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:10:35 +0000

    treat development land point activity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  def reverse_list(lst):
+        return lst[::-1]
