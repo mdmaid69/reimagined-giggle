39552975b315b272d31e7bba87887fdfabf2bf3e
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:36:50 +0000

    thick price equipment finally boat

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
