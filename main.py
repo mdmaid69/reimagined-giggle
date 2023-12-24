eec552be62b88a8cd1c964fae0e23925243b49ee
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:26:35 +0000

    translate better regular post pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def split_path(path):
+        return os.path.split(path)
