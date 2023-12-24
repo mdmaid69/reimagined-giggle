49f3f81ffa1e0adb14894d73f0840532070a0678
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:07:46 +0000

    handle until deserve town through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_length(array):
-        return len(array)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
