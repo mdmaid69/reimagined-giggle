2e56564b42a1c3c01adb1f3452834bbcad647bcd
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:36:41 +0000

    partner tread behind fortunately pity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
