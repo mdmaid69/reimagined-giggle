3962647ec2cd074339a4a4134d3d2acbe1bb99dd
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:45:18 +0000

    confusing suppose accommodation activity flavour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_gamma_function(x):
+        return math.gamma(x)
