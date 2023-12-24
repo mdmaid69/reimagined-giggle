1c3157aae727832abe1f39f349d3c668010ae92d
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:51:21 +0000

    pick keepfrom happy target it

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
