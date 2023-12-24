26f33d0c2c0922599ac2460ce0fefb31a785c7ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:04:10 +0000

    who simple name evidence worry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
