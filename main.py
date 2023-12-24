0bd7400a41f52bd8578e17620fc4a16545930bea
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:50:43 +0000

    child upset touch walk speed

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
