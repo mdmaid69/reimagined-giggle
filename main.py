74e82c8b3b9d0fe05522dc0466c797c3d7339b93
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:05:58 +0000

    judge challenge use date as

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def convert_to_hex(n):
+        return hex(n)
+import sys
+def print_python_version():
+        print(sys.version)
