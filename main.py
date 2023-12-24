6191d18d1c46d2391877db8a77a7bf52694bb236
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:43:40 +0000

    brave reaction damage hot fat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
