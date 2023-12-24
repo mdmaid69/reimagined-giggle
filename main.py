4dcd201e4731e5126f6e615c15350247962b8a08
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:56:10 +0000

    certain bit cost breakin dead

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def is_odd(n):
+        return n % 2 != 0
