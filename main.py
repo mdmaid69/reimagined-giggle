c6fa8c1cb6df5b726ea11c6c166bf840705d7563
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:26:58 +0000

    sensible pleasure rub turnup old

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
