1823ff941ba7dfcc2eddf6cee937ba5991e55db5
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:22:42 +0000

    along manner perfect practice escape

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+for i in range(5):
+        print(i)
