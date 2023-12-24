4f0cb6f2bf3baf24c4ee9f09a5251a66981fa735
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:30:25 +0000

    terrible do control pinch birthday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
