8ef11c336e88fa5aec1d28c3b949dbf054b8d10b
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:03:55 +0000

    unite sink unless back cost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Characters:", len(text))
