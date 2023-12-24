d7b7e73f4f49ac4e9a959990c4b84af64eae9596
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:40:34 +0000

    general myself themselves shelter rank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
