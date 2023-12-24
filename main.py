5f66cb3060f65a6bf190ae9f166dc85df546a2f9
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:55:27 +0000

    pick hospital defeat image introduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
