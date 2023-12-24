787d8ec9cc6443feb34fa176cb1529d7b93bcd53
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:04:33 +0000

    glass usual spite on music

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
