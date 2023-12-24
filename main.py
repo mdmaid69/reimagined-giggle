c795227174b81b5d86a1ed9912b878952cec9bef
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:14:45 +0000

    mean thinkover comeup hardly call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
