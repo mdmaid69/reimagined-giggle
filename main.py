4536bc42940137a2b7468bf03edf019fe3135f7c
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:45:31 +0000

    turnover lay forget come quantity

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import time
+print(time.time())
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
