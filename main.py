a45e8965a4802df3ce0dea87ab6bf589dc528863
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:57:51 +0000

    edge sale admit bit Jj

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
