9b3bc52ae08699f6fb944dd85f6eb8619d18da7f
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:50:57 +0000

    steep fortunately under whisper noisy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
