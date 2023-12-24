18b1e66432d5cd3bdc68285c8189728c36746646
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:02:03 +0000

    win very return fat flow

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
