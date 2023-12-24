b4578d015b36209f22510a7c4dcded93d56bc4f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:06:37 +0000

    shake address sky cheek substance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_user_list():
+        return collections.UserList()
