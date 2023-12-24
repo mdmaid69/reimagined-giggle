7c79c1811cf14799851b00a7968517dfbfdee8dc
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:58:07 +0000

    strong runinto nor unit whenever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
