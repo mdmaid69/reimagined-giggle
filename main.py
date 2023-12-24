872e29be574e7729e07fe55ad64f4b5d49e33820
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:11:07 +0000

    pause anywhere pile chin unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_array_as_repr(array):
+        return repr(array)
