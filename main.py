822a3a1e3e2468042c349bef8cd36a55e861a205
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:13:16 +0000

    takeback female country fault busy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
