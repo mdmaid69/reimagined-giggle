8506532a184bbd18ad85f07f8412700f3b3fe3e9
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:42:12 +0000

    collect mark Mm damp mark

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
