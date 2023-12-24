eeed300a7155135931ff9352dedd26a4fe9a7353
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:36:34 +0000

    corner trial record project agreement

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
