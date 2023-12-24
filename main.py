0ba93a0067564ef406f3bdfdddf6149fc3e9b89d
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:01:10 +0000

    payment head pass colour power

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
