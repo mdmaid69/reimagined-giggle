2bfd735d33e21e8695c29af19b2c8a549d48b2c6
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:18:47 +0000

    practice survive answer year speech

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
