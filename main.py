1a0aefce5131bc5f3072ce8115b24c192928b945
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:46:52 +0000

    give stop fasten reason bread

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
