eecca5631717571e5578f666f8360517503446ee
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:33:26 +0000

    theirs keepoff bringforward office whenever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
