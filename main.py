bdab5ab1ebe14a53d93505b0d485a351a4869b1a
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:28:44 +0000

    sting lock shop cold any

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
