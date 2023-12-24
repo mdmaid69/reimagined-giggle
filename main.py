c73960a3793dabd3637222fd194865eeb7d14025
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:30:08 +0000

    reduction ordinary company dinner thinkof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_logarithm(base, x):
+        return math.log(x, base)
