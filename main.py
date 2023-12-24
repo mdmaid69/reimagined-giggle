e8cd8c21d870f5b6a181fff7b77ea5bc7d423abf
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:16:18 +0000

    petrol out guilty bear present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
