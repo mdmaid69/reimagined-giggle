3b7c618b551bd5a6bac18b3fb13c8f73faee6a74
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:08:24 +0000

    hold emphasize possible miss account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_item(array, i):
+        return array[i]
