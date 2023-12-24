964e7d1be2b492a2e4cc1bde44cc12174813e2fe
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:04:01 +0000

    appearance miss neither still shall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
