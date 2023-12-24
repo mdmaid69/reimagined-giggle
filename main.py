83c9e7bc35cd91d66e154de264d83379e95a3a98
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:25:38 +0000

    cut possible undo interesting flow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
