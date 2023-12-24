b3268f30b64582bfc5852ebada99a0fa4246feaa
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:08:13 +0000

    explanation exact tax valuable artificial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
