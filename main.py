17008740e43586281ecaf1fb6cb03651d1bb8fee
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:29:09 +0000

    check recent watch table guess

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+print([x**2 for x in range(10)])
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
