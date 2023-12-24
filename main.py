18784e9fb66b3b439b874292e2074802c2ceb3e8
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:01:54 +0000

    welcome project remark deep tired

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
