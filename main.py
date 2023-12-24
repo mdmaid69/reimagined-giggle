28a26d0c52a9c13336863dc7923b964e74651e9b
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:31:10 +0000

    gate various complete exit complain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_min(numbers):
+        return min(numbers)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
