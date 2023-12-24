00124eb2d5dd786b981207a2dda3439d3f3fc195
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:00:09 +0000

    study ourselves deliberately suggestion difficulty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
