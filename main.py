9c3e77b0e4e1d7c760822ab5cc027011a101d26f
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:22:30 +0000

    health feature involve deep angry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
