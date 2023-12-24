de5e7c58e38afac77290e193e38f1ada5a051aa1
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:48:18 +0000

    underneath generally speak unusual maximum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
