2baa628ea622b35b44558ab93b0c93b06add1ff8
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:36:18 +0000

    trial follow picture buy there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
