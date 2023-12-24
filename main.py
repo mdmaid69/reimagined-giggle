caab470adad098fcb96a07fa66cbe8602ddbff2d
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:00:46 +0000

    love profit teach reasonable face

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def convert_to_binary(n):
+        return bin(n)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
