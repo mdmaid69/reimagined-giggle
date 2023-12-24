26e03a6fd207d80762e53df6b739252b43539746
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:16:26 +0000

    party somewhere flat friend crowd

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  def sort_list(lst):
+        return sorted(lst)
