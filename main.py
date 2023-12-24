4543383dbbc7b91aef75e13e9a19ed32f4f54250
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:39:47 +0000

    swing apparently ignore unfortunate typical

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
