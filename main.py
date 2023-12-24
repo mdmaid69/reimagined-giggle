e29a02b06f52faa76f8a61e7d735fb9abf6a400a
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:57:11 +0000

    skirt finger comedown temper fair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  def count_elements(lst):
+        return len(lst)
