893e241eb263cf8a3bff5c9c75cb8286b519e4f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:21:51 +0000

    whistle runaway touch high meeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_average(lst):
+        return sum(lst) / len(lst)
