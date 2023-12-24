e24b81fd966531a83e12e17e2e433c1bf79b0ab8
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:01:21 +0000

    begin gap distant mistake unable

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
