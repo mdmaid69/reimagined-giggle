97864361b59cbb7b4cfbc0e10ca8c5bb83197423
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:19:47 +0000

    smart brain hate finally obvious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
