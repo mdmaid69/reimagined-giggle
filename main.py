e10ef43b757dfbfa94ab8fd9550981b400e6b1f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:25:40 +0000

    sensitive negative noisy speech alive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
