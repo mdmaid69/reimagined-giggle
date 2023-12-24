63446450290e44950b8a3487dbd2db1bba743cd6
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:02:45 +0000

    arrange bringup plain approve prevent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def count_elements(lst):
-        return len(lst)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
