5f4d8a7a47a3659a692754f1ca691801a3dc9b44
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:20:02 +0000

    aware account bad foot object

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
