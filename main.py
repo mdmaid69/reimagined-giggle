7b0c3c2e448cf047e46e5931422099e5c72bb54e
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:37:43 +0000

    drop than dress breakinto turndown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
