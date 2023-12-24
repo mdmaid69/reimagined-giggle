11aa70c9f39cec13255043a1fd4bca212bfce221
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:34:32 +0000

    experience slow measure blow cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
