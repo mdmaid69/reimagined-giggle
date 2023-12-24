48b2a9cf5192a3751fe154bad32b743b4e13ae0b
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:11:19 +0000

    square crash ring interest claim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def add_numbers(x, y):
+        return x + y
