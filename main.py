57fe2c6fc0b7a3b3a1b2188ce198e8dffc1cd6f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:02:57 +0000

    firm copy aware getout square

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_power(work, time):
-        return work / time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
