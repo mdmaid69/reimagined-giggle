fc94fa8e64dbd2e4b01172bc3034760b91fc31a9
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:52:42 +0000

    on however brave forever room

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import time
+print(time.time())
+import numpy as np
+print(np.array([1, 2, 3]))
