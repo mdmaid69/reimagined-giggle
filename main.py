65d58c2dc86be3895e9355cf2eccecbc39173110
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:52:45 +0000

    face behave thought attempt fall

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
