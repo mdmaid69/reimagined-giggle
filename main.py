0c3db6c769d4cb556ae510b4ae7b5858db41d508
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:09:52 +0000

    rescue failure still close event

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
