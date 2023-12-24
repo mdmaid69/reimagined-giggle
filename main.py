c4a8d0373e310dc618df5db29cd9b67bb281252c
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:37:24 +0000

    most boil skirt left advice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
