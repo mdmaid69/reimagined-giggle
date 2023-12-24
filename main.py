775cbe08093961552e571de96e7bf7141d24ecf5
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:53:08 +0000

    doubt recent open lost somewhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
