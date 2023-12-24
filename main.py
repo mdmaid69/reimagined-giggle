4cd59bf0398dc4bc5040c59f2755ff6008ea0ab2
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:32:48 +0000

    very file police satisfied strange

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
