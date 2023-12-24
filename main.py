cdd3e4734f39cd0cd559b4ee19edbbeee349e741
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:19:10 +0000

    stamp home bunch principle gooff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
