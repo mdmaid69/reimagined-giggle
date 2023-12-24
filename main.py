edbed3d2c5f71ff6c9664bf85601e800cba0e953
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:35:27 +0000

    convenient getaway clothes arrangement me

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def convert_to_hex(n):
+        return hex(n)
