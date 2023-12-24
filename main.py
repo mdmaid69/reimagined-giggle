3392166a2cc5d1ce111e5f8204e1fcb66d73b676
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:41:06 +0000

    miss thing call description test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
