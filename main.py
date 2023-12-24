476bec61e06121e106852be2c24ae19b623334c0
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:23:26 +0000

    swallow mess lock free advantage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  def calculate_circumference_circle(r):
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_circumference_circle(r):
         return 2 * 3.14 * r
