43251571b65f666e113dab1990c556de64a4c37c
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:56:30 +0000

    mark present hard whistle shock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def is_odd(n):
-        return n % 2 != 0
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
