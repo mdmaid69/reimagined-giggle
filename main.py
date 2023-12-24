4b4bff1313bed2283e1e72226b4a22c752e468b1
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:46:24 +0000

    equal everybody mark most keepout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import platform
-def get_os_info():
-        return platform.uname()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
