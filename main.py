e1dbff13067931886d7ccc04fc848acaa79f3dfa
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:53:43 +0000

    explanation wind ring exercise blind

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def add_numbers(x, y):
-        return x + y
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
