85372408cfbdcc12b553bd83eb6f270d12a52327
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:46:55 +0000

    stupid name basic exciting manner

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
