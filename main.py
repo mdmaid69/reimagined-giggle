97022657c407e33d99beabf20331d58670fc9011
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:46:13 +0000

    present sensitive closed fast happy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
