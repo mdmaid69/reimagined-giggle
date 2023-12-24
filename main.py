15509570aa2edd68281366a63e8a5979ff6b012d
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:54:21 +0000

    shut various quick everything even

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-for i in range(5):
-        print(i)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
