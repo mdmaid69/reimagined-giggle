6154a8910dc2bde44c2ab0eb2101fd6e707becb5
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:01:05 +0000

    zero nose murder themselves sort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_area_circle(r):
+        return 3.14 * r**2
