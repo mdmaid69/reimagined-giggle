43b0ccd8e60acc6b36d81a0e006e34f24c88617b
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:15:58 +0000

    then port organization sleep race

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
