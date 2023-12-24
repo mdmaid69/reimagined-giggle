0129e29cb9df4d97c5a6c860feeb8bd56280cd1b
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:23:59 +0000

    turnup mend sink over meeting

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-print(sum(range(10)))
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
