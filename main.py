cec0554acf065d27e69638a8520784be9e47f148
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:47:15 +0000

    suffer correction arrive each afford

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  def count_elements(lst):
+        return len(lst)
