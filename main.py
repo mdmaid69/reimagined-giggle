bdc7486bb1176f06df5350ceb812453d0323bebf
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:10:30 +0000

    listenfor communication pay tour progress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
