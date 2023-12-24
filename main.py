ad90cc88a8a4752c6bdb7f3bffd6654eab6a50ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:28:02 +0000

    late any responsibility attention objection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
