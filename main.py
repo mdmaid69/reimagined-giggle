9c569abc3c5837cdc9a7795b36178b59898e66d2
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:44:23 +0000

    drink noisy tap partner sky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print([x**2 for x in range(10)])
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
