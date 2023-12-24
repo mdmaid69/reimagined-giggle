936cf2686292ef636f50b69f76bc4ad85884cfe1
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:28:59 +0000

    breakout clue holiday cheap today

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_queue():
+        return collections.deque()
