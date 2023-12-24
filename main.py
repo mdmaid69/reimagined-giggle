e68a33387b14772a7cb1979b68b17e94fb8db562
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:36:27 +0000

    dear conclusion excellent wrong accept

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
