afd682a316b64386db75006f4c68568982545360
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:39:09 +0000

    her geton burn sink cold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
