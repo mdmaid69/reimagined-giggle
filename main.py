64eb43823852c263d9bc95222dee64b92da7fd85
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:12:22 +0000

    comfort open welcome dick greeting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
