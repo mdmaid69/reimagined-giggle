af3d47c672709f6151c9246647a304a3a76b6b0d
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:12:27 +0000

    office mad contain period rule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
 import math
 def calculate_combinations(n, k):
         return math.comb(n, k)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
