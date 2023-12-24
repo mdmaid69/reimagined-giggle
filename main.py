020b0e863963f2f738b979d9be483e46ea38026f
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:38:50 +0000

    shall put today welcome female

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def square_number(x):
+        return x**2
