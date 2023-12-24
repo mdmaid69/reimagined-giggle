d5d964ed0dd1b8b0b10200ff175348cc1975e5a7
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:24:03 +0000

    used include name pour bye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
