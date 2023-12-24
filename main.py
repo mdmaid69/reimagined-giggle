83101a0498265100295ebef27701c4350b62c23c
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:49:02 +0000

    sea bring port direct appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import time
-def get_current_time():
-        return time.ctime()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
