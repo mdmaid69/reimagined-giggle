528a33095e9b826ea70b22ee159ea71392da1454
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:45:11 +0000

    probably used secret dish bear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
