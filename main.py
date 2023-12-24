488ffcf8a2479cb8c3b18e650992600a843b1d7a
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:11:11 +0000

    common few uniform view education

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
