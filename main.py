9e3566e7b767211455e1747fccf6d56157648248
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:45:35 +0000

    better force simple heavy unfair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
