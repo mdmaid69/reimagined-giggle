5c2e58b9353707ac034499b578e0a737f27cd66a
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:43:48 +0000

    apologizeapologise nonsense somewhere holdout water

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
 import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
