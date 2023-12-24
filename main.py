0b68ba992cf7a00a6b1992490c4815cf597e6c3b
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:18:01 +0000

    air kind busy interesting worth

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def add_numbers(x, y):
-        return x + y
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  def multiply_numbers(x, y):
+        return x * y
