a235f5a9a111ddf89187c03f6993e9a3aa397816
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:15:51 +0000

    main shrink hit step obvious

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_sine(x):
+        return math.sin(x)
