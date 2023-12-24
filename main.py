3aacfb1ece38782ff70e210e0af27f9f950838f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:35:30 +0000

    affect female ball spite serve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
