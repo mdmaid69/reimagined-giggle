8ac0bf21ec903ea5556dcf0931a4a279d9c990f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:27:04 +0000

    ticket worried brave freedom morning

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import time
-print(time.time())
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_speed(distance, time):
+        return distance / time
+import logging
+def log_message(message):
+        logging.info(message)
