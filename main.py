51c12215e6b65061f7b7ddbb16ef17acb9069822
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:48:53 +0000

    practise art godown winter complaint

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def square_number(x):
+        return x**2
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
