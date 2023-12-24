b5d549a06470b11ecede5da85b975554eb25e47c
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:32:33 +0000

    almost copy pronounce walk stretch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
