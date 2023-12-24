cc4d01f95692bb53dad94d677fbd38dea753d681
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:46:05 +0000

    unfortunate wealth conduct impressed stare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+print(json.dumps({"name": "John", "age": 30}))
