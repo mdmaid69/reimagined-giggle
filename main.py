c5b1ba4316b2708c8d5be70fd855bcc6c2eff75c
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:17:58 +0000

    never if book slice stick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
