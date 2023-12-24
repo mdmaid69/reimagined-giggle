05f6172bb31eb1a03d23be868cce2867e17d9a56
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:46:27 +0000

    shrink recently question workon pretend

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_power(work, time):
+        return work / time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
