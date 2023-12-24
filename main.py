d2e2dd8a5d547705589eb3a2e4e2fa29365243c6
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:29:13 +0000

    along admit hang she hardly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
