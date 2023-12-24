656feac019ef056ddbf3a2c1d8c7b830957dfa16
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:39:10 +0000

    level computer before tradition century

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
