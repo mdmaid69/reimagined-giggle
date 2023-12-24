3a39c330fa4baa3012855ee348b52963e36abcdf
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:45:29 +0000

    realize attempt mean right compare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
