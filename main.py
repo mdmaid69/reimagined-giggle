844d7ca96610325e4d846fb9f2d405911a3df7f2
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:02:33 +0000

    edge hardly cruel line uncertain

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
