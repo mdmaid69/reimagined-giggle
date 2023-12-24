005a5cc2a135b21ade51ece3fd3edbf5674031cf
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:47:43 +0000

    side occasion somebody extra flow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
+print([x**2 for x in range(10)])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
