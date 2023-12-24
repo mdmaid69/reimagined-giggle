d107171591b33dc069ffb204976835ad617e9149
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:36:56 +0000

    comeoff ticket basis yourself gradually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
