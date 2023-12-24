aa4f85495093589cc69f789f7be428995e73ecb3
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:22:56 +0000

    thank city warning congratulations classroom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def subtract_numbers(x, y):
+        return x - y
