21ddfff842a8d6fe3a4b05fac7a14c05d33e8a37
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:28:39 +0000

    kind operation let convince rank

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
