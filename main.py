285b33421147519ad772d237dd24628546af187c
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:52:03 +0000

    behaviour influence within argument around

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
