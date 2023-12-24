b036ba7aec17da5bf9e5f97580ab711afd8da1f9
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:29:41 +0000

    child disturb Ww deliberately strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
