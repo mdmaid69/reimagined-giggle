b7492072d356d2bd3074accad3fb787936344a4e
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:08:19 +0000

    behind want occur shout impossible

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-import array
-def clear_array(array):
-        array *= 0
+def calculate_pressure(force, area):
+        return force / area
+def sort_list(lst):
+        return sorted(lst)
