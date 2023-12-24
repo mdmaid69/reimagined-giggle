56a33cbed121577eb71170133ecaabcc7f609bbc
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:00:49 +0000

    I bite it scrape bad

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def convert_to_binary(n):
+        return bin(n)
+import collections
+def create_queue():
+        return collections.deque()
