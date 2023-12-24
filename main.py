5f2890b997b22e7de3eea54c6097b7ab652b6e09
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:51:33 +0000

    fetch fast display music Cc

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+print(os.getcwd())
