29261e59393e11207953db089cdb23eb29c4c780
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:10:31 +0000

    somebody interruption fail concentrate fetch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
