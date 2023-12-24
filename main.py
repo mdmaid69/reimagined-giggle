df7e1f6e67fa380a5febec773c14c438c8bfd3f8
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:01:31 +0000

    invitation broad whether talkback force

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
