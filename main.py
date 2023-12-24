c47a4efbb247bfb7ffca05d17d177188fc92cacd
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:01:14 +0000

    nervous anything lot callup excited

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
