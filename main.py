dd30f1525aed0d51bf3d56db1109c7d5162525b2
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:31:02 +0000

    steal refer lower 572enter protection

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import time
-def get_current_time():
-        return time.time()
-  def add_numbers(x, y):
-        return x + y
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
