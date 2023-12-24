b6b8b06eee79142881a8e5e7329f72e53a074bea
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:54:01 +0000

    sticky advise treatment choose story

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def is_odd(n):
-        return n % 2 != 0
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_power(work, time):
+        return work / time
