e8bf90c765507e3c865eda56afcae1fb5906f09b
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:30:19 +0000

    prefer public object remove manner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
