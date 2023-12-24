12161d20e62d1cacf9851453443598d4476039a8
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:26:37 +0000

    179bite trick public payment part

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def find_min(lst):
+        return min(lst)
+import os
+print(os.getcwd())
