5793cd01ea5299d262710ab3cc84626fdab48d06
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:36:52 +0000

    name accident female through error

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import math
+print(math.pi)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
