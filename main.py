1b9d374296624ce9e546fc154fec4d9f1c4fd674
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:16:03 +0000

    deserve Hh watch flood near

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
