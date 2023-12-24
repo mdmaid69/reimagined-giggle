f7c1c317480d4ce6b333d12bf9447c7eab881b98
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:06:25 +0000

    pretty traffic tell trick forever

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import datetime
+print(datetime.datetime.now())
