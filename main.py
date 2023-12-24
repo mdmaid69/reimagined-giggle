a3fe4d2f01a29f5505595960d1817f3800dec2db
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:44:54 +0000

    congratulations guide instant only slightly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
