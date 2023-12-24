2d4d6b322016f64b56ab843b0d4ceeb3b5b0b3d7
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:28:17 +0000

    fly name damage comment maximum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
