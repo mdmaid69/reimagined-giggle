f349e498413a56bbaae7b382d27b7c166e177c7e
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:44:30 +0000

    across owe Vv birthday Ss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def change_working_directory(path):
+        os.chdir(path)
