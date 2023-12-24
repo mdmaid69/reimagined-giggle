3c683212ed98b98c3e0dc00792ca8557796ed940
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:20:27 +0000

    effect scrap die finally stranger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_str(array):
-        return str(array)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
