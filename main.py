7a08305c79fbda81e7057a2238f1f47ed68249b4
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:05:00 +0000

    room declare today sense wave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
