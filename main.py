fb269779cf4afe881bcadc41a50e0b31faa870ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:23:31 +0000

    store judge Ss set pleasure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
