3479784a9e54ae2b2c317df708588b846d23b34d
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:31:12 +0000

    matter stop determined exciting left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import getpass
-def get_username():
-        return getpass.getuser()
+import platform
+def get_os_info():
+        return platform.uname()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
