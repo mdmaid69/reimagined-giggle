7de80c1f200f3edd4c096b551e047f34b4630905
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:39:33 +0000

    kind nose quickly part discussion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_absolute_value(x):
+        return math.fabs(x)
