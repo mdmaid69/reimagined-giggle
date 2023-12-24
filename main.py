eb57d71bf799b124b5413fd5e6da519635244d32
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:47:17 +0000

    serve still control flow to

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
