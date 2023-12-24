ead6b44b33e268152a65e10ea295ed57c76492b4
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:18:56 +0000

    fast minority moon Tt eat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
