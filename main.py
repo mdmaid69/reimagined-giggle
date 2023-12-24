2acd90d4b52040bd60bf218a19207969d700da5b
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:30:01 +0000

    attach pride which hope roof

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
