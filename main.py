881f5d62a5545879a58e12f51f311eb9258bd1b8
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:25:57 +0000

    never form getout produce fence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import platform
+def get_python_version():
+        return platform.python_version()
