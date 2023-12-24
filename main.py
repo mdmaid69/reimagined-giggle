7921a70145daaa6a6df41f790360573d8f8196a7
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:10:15 +0000

    hit lead build journey your

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
