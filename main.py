1b2e8152003fb5ca931c46cb34af1fb5d7ee3b36
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:38:02 +0000

    classroom breakinto ill hour certain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
