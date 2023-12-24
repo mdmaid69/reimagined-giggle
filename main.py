a887ff3138c4123878f4a46779e2f878560792ff
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:09:45 +0000

    taste bringback please record example

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_density(mass, volume):
+        return mass / volume
