f0a5114e6775e91acb1f7b458bded00eff9ae6d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:34:31 +0000

    objection majority exchange cry figure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
