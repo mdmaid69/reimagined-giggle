8192859af3a3714d94dd385b9163a6916040176d
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:37:12 +0000

    part but soft pride responsibility

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
