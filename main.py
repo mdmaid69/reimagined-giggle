babf349e413543218c1bb223b22f0cde9ee3ce77
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:47:35 +0000

    wall letter serious breakout section

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import sys
+def exit_program():
+        sys.exit()
