b7af977ab041b2fee2cff3165e2d4c729aba1304
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:27:31 +0000

    die fence arrive track pour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
