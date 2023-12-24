658742abb8892867586a2dc70f3bfeca25a1b75d
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:26:36 +0000

    some destroy narrow steer lift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
 import array
 def create_array(typecode, initializer):
         return array.array(typecode, initializer)
-import os
-print(os.getcwd())
