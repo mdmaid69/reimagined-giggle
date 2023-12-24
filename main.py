f24776fb72e360b750eb36271bb92df506c853c6
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:13:31 +0000

    answer tight crash goback(to) opposite

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
