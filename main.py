e6385294f00a0766bdd81f3e5bc796467594885a
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:56:08 +0000

    art thick offence turn butter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
