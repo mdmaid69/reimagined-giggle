e74f0667e8e454880e35b44aecc0e2da03a3dab0
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:12:15 +0000

    trade unpleasant crash distance aim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
