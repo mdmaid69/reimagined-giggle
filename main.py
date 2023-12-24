892c21393cd2e345b4ab9cb4b37df09f04111dbf
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:33:18 +0000

    last object dress section condition

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
