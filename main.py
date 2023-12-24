4fab8b0dbb9b2abb574fa26e1d794d030250adff
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:56:23 +0000

    spot wide sign reserve ahead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
