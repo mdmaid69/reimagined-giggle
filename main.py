18df3800cb7b5c914bb933a3a18c02ebd160a0f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:18:09 +0000

    takeout forgive keepaway thankyou argument

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
