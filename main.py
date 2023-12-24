7dc80f0d3c91acb00aff3c8ad2753a69b5cbd328
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:47:00 +0000

    between beside card giveaway sensible

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def is_even(n):
+        return n % 2 == 0
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
