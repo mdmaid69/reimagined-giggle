5c2a1860b1a3af38eb78c998fb1e3287a475b5a5
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:32:46 +0000

    shop another hot slow sight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
