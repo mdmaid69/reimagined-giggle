8121de74e8bdd86b761679f5bbdba8334a07a1c0
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:19:59 +0000

    trust unlikely zero battery place

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
