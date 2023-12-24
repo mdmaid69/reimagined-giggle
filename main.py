0ae976dd2fadde362fe04c199b35b7ee5c19adfe
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:05:04 +0000

    holdup plenty control mad urgent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
