1a13e99cc4e88ff9434e37b508bada9f657d3f3b
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:50:51 +0000

    past description tip travel shake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
