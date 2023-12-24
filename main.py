9115d6730e4f19207da909c127c58742d5f4ac94
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:05:07 +0000

    worse damage dish insist rush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
