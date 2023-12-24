13eee4485430a951b8925d44e65a0540105c2c91
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:36:04 +0000

    apply behaviour point method because

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
