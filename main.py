ca719280922c326788304348f63a43a1ad8ef888
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:01:02 +0000

    offer task goback(to) tragedy dig

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_itemsize(array):
+        return array.itemsize
