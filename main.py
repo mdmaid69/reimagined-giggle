0302014058095b80349e5ccd775bfa1247700f0f
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:21:26 +0000

    goout concentrate reduce increase calmly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
