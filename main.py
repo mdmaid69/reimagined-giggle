4a6b5874d7355955e7faa0155cb350d7f05ae68d
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:44:38 +0000

    turnoff slice future food breakup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
