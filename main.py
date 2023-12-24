c90e85991f9b1c52118e4cb6413bebb9aa891ce6
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:41:42 +0000

    way terrible store listenfor wonder

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def is_even(n):
+        return n % 2 == 0
+def find_min(numbers):
+        return min(numbers)
