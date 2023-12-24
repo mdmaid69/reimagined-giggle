1631f83d71ba0e930101726a0b5bbb2c9f714464
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:08:35 +0000

    least suckme use after interruption

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
