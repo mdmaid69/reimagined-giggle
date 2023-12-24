57c25422922d9e7090df931479e8a2e27a1c775c
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:34:23 +0000

    free admit turnover road spring

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def convert_array_to_list(array):
+        return array.tolist()
