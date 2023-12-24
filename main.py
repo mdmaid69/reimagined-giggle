8a8243835c935dec8abaf83af07d9d0964a55329
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:51:48 +0000

    teach if wherever beside brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
