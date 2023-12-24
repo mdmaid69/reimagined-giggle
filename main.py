0e78637296231f78aa3325f57442f5eb503fbef8
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:18:09 +0000

    her label perform big not

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
