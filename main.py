234b3336f8e29115011faaaa17a6dbbcd6afbcd1
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:20:48 +0000

    walk ring pin turnoff afternoon

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import time
-def get_time_since_epoch():
-        return time.time()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import json
+print(json.dumps({"name": "John", "age": 30}))
