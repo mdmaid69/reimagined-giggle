9cab726fee2a1139712d8a81ba85e86c653f4166
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:20:00 +0000

    choose great enthusiastic Rr control

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_typecode(array):
-        return array.typecode
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
