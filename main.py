3a0456d0542ffc249672208628f6e65e95fcdfed
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:01:17 +0000

    same figure right rescue substance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
