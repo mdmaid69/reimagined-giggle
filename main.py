84c8434cdb357bdfd1fd6b46b1cbefa59f34841b
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:04:30 +0000

    part admit soon generous anywhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
