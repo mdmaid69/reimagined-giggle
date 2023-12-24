48f3b26d10c4e0f9d1030a1db7045355c8bf9675
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:21:07 +0000

    almost train her file substance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+print(time.time())
