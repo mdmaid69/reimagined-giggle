9504d9c55418fb6b8c6b656493712e66a10afdcb
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:14:43 +0000

    majority needle congratulations estimate environment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
