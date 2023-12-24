396d7e6c589bd674035fe9fceb3b1158403abe05
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:54:04 +0000

    form Ss slowly bringdown run

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
