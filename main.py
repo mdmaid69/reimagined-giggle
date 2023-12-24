48bba9b03b526018e629832801d2b58ce78ecb57
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:57:27 +0000

    choice plain instant switch today

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import os
+print(os.getcwd())
