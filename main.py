ef5ec6534a6317a23e1ced88fd5438dc3d5e91a1
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:11:48 +0000

    gap union protest shopping come

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
