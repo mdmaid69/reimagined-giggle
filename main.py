32ef49395777f481e32b719093a852dd56682660
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:29:16 +0000

    person pretend go safety pin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def convert_to_octal(n):
+        return oct(n)
