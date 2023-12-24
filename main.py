37e3bebe0dd54a3eebf41212b6e81c6deeda9128
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:16:58 +0000

    broad else century grow ball

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
