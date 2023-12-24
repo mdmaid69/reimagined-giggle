2b5673533703a138dcfcc890892efc2c470ee236
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:31:00 +0000

    real taste cry experiment nearly

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
