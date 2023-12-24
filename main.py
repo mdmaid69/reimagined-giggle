07c8eddb801b7e11a5422ca274a05c9d81c77393
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:22:00 +0000

    equal forever beyond unit quick

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-print("Hello, world!")
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
