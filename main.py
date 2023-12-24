5330e7ed25fd49c76a234f74d57aeffcfc349be1
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:23:10 +0000

    before joke Bb rank breakthrough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
