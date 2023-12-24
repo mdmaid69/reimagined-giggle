d59daedeb3054b4fc4004ca3fea6a4ccf65da05c
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:53:20 +0000

    other profit dance previous look

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
