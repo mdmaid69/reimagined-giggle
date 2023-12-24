1a82999eaa7d389781bdc6324e4cf869679d1e41
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:24:12 +0000

    any pretty bear table makeup

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
