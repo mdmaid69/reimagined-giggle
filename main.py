fe9a6f83ada2fe345482175ff8cef8531b99f80e
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:20:39 +0000

    aware hole modern thumb shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
