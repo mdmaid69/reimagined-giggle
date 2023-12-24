b8849cad7f3a4e94b65fb36a9bc5b4fa912a9901
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:21:25 +0000

    nerve force sweet conclusion Jj

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import array
+def get_array_as_list(array):
+        return list(array)
