9bad0ad17aa50cd1c4b84dd31351a532d8d98895
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:44:31 +0000

    approve talkback march far sport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
