0918b418826452ee3825dcbb74591a6fbd4018c6
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:18:32 +0000

    sleep place peaceful badly lift

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-def find_min(lst):
-        return min(lst)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
