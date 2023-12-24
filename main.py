e8264f2015e7c6087561c6ed7031cebc5a748b95
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:43:58 +0000

    wet how mixture card argue

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
