fa0447cae30ab26551be644070d146f8a69599ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:18:07 +0000

    narrow hospital dress later sensitive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import array
+def clear_array(array):
+        array *= 0
