8a003489fb90ab718fed9c4aa6f2085277e2adc9
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:53:27 +0000

    national likely basic apply foot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
