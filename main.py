72ee917ea6b429ae843f7dcc0000cfa751539aa2
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:29:19 +0000

    various all announce difficult argument

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-print(math.pi)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_max(lst):
+        return max(lst)
