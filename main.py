e36c9c6625462117aa628367d91b8e5f0177c848
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:49:40 +0000

    compare to ashamed point image

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
