60199e6a8e46b6a41be77b2e29c0c6bbe3f78527
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:43:04 +0000

    gas excuse scene about track

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-def add_numbers(a, b):
-        return a + b
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
