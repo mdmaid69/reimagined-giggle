f1bbc09962c6fa526eaa9334b106e3b962dc288a
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:01:09 +0000

    remark slightly hard realize result

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
