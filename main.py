4986e8bdca7949fc9359ccd3c97bd7b1f18c294b
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:42:33 +0000

    unfortunate wheel comedown wheel boat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
