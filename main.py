fa5cc76d322ce7b91d66e4194a0b56827a8c6b25
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:50:18 +0000

    flood advanced body high protest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
