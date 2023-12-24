328ba9c24ff80cda6db17740b1692a06fe651cb4
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:15:44 +0000

    top past so cut trouble

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_even(n):
+        return n % 2 == 0
