573eadab264e18dc58f9f62660f6507d530de142
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:37:46 +0000

    hair breakout quit female my

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import platform
-def get_python_version():
-        return platform.python_version()
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
