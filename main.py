0af6d10b45770565ce2ea1a9bab9bc389cec1675
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:39:48 +0000

    unkind insist afraid national loud

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+for i in range(5):
+        print(i)
+import array
+def get_array_length(array):
+        return len(array)
