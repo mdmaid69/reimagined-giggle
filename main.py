3576a71def4612a0dc6db9671677d7058024b6d7
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:01:34 +0000

    limit thanks car body hide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
