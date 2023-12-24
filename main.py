3ee0c7a6a232c928e941b0a3b45821d62a7c25b3
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:22:31 +0000

    sweep pair turn service image

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
