d22eeaf246f53e2911e08ff49cd239c8bece35c7
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:42:22 +0000

    to notice fork anyone rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
