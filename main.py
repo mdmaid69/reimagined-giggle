6bef55df4bad116a8c17b7bdfb2e5001b8721b3a
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:58:46 +0000

    Yy Zz narrow phone visit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
