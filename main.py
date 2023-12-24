cae0d04c75492a1e07772319772ec7de7f523d3e
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:41:04 +0000

    brother contact tonight brief slow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
