1cbb37ff5c4ee445f128309122edbcabe1d288ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:57:56 +0000

    better stamp cut bang store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
