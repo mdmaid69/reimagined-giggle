29270182011f7bee1d3f2311b0509c8ce0cfbec8
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:47:01 +0000

    out first impressed congratulate learn

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
