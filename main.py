24b860e0ed39df32e9dff2c499f3d575fb258236
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:31:55 +0000

    ever record goon do rise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
