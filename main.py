0db1cb3ab8b77d98c2352c5fb179497394e2cb88
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:13:52 +0000

    climb smell possession wait level

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  def sort_list(lst):
+        return sorted(lst)
