075ca73c9f4b30ce04ab5a2530963b15fd496080
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:10:49 +0000

    turndown exist solve dinner copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
