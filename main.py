b767418d2f6265fe729f08e4f7b15b08c0553d30
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:33:50 +0000

    direction doubt word makeup late

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
