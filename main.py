5801567eca745236ade374ec93f2137d16570894
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:00:36 +0000

    hide no illness mess dress

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
