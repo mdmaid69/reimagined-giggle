6375f4ed66f758a180d912248fe9fd86f2db92c2
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:21:28 +0000

    slide else judge hide success

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import sys
-def print_python_version():
-        print(sys.version)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
