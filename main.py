faef9e3fc770dd88d95fe40d4ef58ccaa71c175b
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:58:17 +0000

    price Qq wipe programme spring

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def find_max(lst):
-        return max(lst)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
