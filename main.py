80677f7903d46f98d511468ef684d24fbd11f36c
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:30:41 +0000

    borrow transparent at every pattern

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
