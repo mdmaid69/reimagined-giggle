e68589b48782c0095835583230e948a7a1de11ee
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:51:55 +0000

    call fairly loud copy comment

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import sys
+  def get_python_version():
+        return sys.version
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
