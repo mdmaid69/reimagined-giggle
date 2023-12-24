8952f67dc7d2e0ac816cc65911d379fc1a7c2d97
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:16:20 +0000

    pause interview this objection 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
