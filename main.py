fc436a01d870566ea300bc9e030ef87deef47f36
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:45:11 +0000

    stir runout sensitive move bank

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def reverse_array(array):
+        array.reverse()
