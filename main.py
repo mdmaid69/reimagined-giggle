96bb47d0f2947d1e1aa1321ec7dc402bdc870051
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:43:15 +0000

    chance bottom sort coffee agree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
