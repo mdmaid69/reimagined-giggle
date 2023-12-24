e1ec439d508f3d6824465a65490fc32b60392d74
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:45:25 +0000

    business now food speech goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
