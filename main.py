f7ebb5a51bb2a9a1be4d8bdb150afa25919bbaf7
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:16:30 +0000

    start journey task report deserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
