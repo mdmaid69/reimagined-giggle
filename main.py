0168f968b0b1eec3ec42cf6313f3e75323a5ac66
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:30:16 +0000

    quickly damp many just aim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
