26fe0aa9a0d124e3e6e31e122cb3864d2b0a84ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:30:19 +0000

    and shame substance meanwhile cook

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def count_words(sentence):
+        return len(sentence.split())
