da11876bd293b03ef342f80748d5be85f5a2b6ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:22:02 +0000

    accidental hurt common her really

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
