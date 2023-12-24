50d50cf7c997a76e070102de3ae6874cd7998152
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:10:54 +0000

    colour precise lose count there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
