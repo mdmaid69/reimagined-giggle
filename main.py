f830ae7f466669d9bc92be712a2ecfdfea258676
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:29:47 +0000

    make discovery give satisfied pot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
