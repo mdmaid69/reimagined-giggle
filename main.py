595912d2f23fed7236929055e2f57a853abd1274
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:41:01 +0000

    offence takeaway then spread opportunity

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def multiply_numbers(x, y):
+        return x * y
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
