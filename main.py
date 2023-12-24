933a647c7a72f0827546655743ac708f416517b7
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:18:03 +0000

    standard mine past shout ring

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
