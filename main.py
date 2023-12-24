aa292affa76a22518aab4d2377a840e152dbae5d
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:45:59 +0000

    minority adjective heavy protect damp

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
