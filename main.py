dc8fb4d46040a2aac7a1f39f7a3229113c12e20f
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:11:50 +0000

    race comein pocket dance shout

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def convert_to_octal(n):
+        return oct(n)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
