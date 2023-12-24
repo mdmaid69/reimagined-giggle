847357eb1ce193e593f37ffaa8f0e064f4f5bc79
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:09:43 +0000

    slow seat adjective chat against

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
