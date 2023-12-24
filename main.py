98aa9c7692dd0c90bec5ed148aa6101952576693
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:41:24 +0000

    ability damage such lean issue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
