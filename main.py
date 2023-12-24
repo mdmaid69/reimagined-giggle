d9db17058f3ec49ddf608910cb8d8ae6811a46af
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:34:08 +0000

    perfect which slice appeal clear

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_acceleration(speed, time):
+        return speed / time
