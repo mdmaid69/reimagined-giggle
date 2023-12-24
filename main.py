cef168d561ecdff7ba666e2b231495639d044c47
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:49:35 +0000

    stone they priority hole fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
