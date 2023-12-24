92dc5d0975901038904b40d94d47ad96401bcef8
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:02:26 +0000

    furniture family deep joke secret

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def subtract_numbers(x, y):
+        return x - y
+def find_max(lst):
+        return max(lst)
