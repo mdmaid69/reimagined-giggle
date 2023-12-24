b4db13c75d3871d360c428b6d060c2a203d151d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:37:08 +0000

    gift delight exciting world again

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def is_odd(n):
+        return n % 2 != 0
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
