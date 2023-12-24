453c374bbd06477a8e2b0927faabc884663d67fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:07:42 +0000

    news attempt trick repeat unlucky

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
