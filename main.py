d78d186b9e538517da53e92124692dde283cc4de
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:32:31 +0000

    crime murder content respect decide

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
