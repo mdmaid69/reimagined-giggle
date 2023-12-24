dad082b7b6f773f2089af2502921b662535487bd
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:15:10 +0000

    skill print long slice take

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
