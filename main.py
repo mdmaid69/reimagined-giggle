10c2fa4860384fbf9bcc2ce74ec12f256b8665f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:58:05 +0000

    free return though frightened so

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
