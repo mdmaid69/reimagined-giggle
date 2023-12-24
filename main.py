4544c3dab5923f15c6a3c9e9dfab80524be2c595
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:32:04 +0000

    wander strange surprise vision design

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
