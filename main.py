e8006c8cfb3b59da20cebe81b35c7c453494e0d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:15:14 +0000

    afraid nigga range container across

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
