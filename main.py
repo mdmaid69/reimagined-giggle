2b5b1e6beb428318191472540b0c7165d863b873
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:44:01 +0000

    change conversation lift experiment fix

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
