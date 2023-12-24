71d1366e1eff3d19119e33bf76332a1413e72c11
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:31:32 +0000

    train finger runout blame answer

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-for i in range(10): print(i)
+import sys
+print(sys.version)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
