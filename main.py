73eea378a98dd01cb48f5dbac27f01c5eb0f6162
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:32:17 +0000

    provide interview trouble after record

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
