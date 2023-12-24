50ff7ca5d366a833f6886d73bb15c82d4cff6cb3
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:56:55 +0000

    underneath movement drink but vote

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def convert_to_binary(n):
-        return bin(n)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
