b99dc5502ab2c65b0afdb542826094536200647b
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:40:58 +0000

    whistle quiet excuse behind worried

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import os
+print(os.getcwd())
