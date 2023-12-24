47b5dacc4b4ea3a3045b9a5c79c7b53e31e8072d
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:48:39 +0000

    guilty straight finally development attract

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
