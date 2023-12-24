0da0b6972ad4e011a1c148100996ff6a8e86be01
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:25:00 +0000

    can style action believe stone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
