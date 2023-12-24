a4eeb21a896c5367f51cf06e2bae0567ba0c73d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:10:17 +0000

    stop unfair may example course

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
