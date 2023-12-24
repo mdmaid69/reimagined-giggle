43d558e13df01c330638eb7f7cef098933b25d6e
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:13:45 +0000

    worth mark normal our getoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
