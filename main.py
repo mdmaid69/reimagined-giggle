fc6e4ac88a3bd84b84ca38cfc28f365cf23cab26
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:21:04 +0000

    truth reduce public judge excited

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def square_number(x):
+        return x**2
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
