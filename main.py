ac9544504c75951c1cb8157732d26171c19845d6
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:01:01 +0000

    project brother exit steer anyone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Characters:", len(text))
