fd234bdb1ee5450cde86d1b8cee40ccc73ff199d
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:23:56 +0000

    scream centimetre process thing giveout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_density(mass, volume):
-        return mass / volume
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
