d8f6aef03e3da51a28f632ed585cc5eb39ffca4a
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:26:31 +0000

    tomorrow step meaning dig further

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
