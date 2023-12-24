917fa75e30132f6a0135cf35ec9c1a5f56a5d81b
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:52:49 +0000

    lesson send involve everything convinced

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import tensorflow as tf
+print(tf.__version__)
+for i in range(5):
+        print(i)
