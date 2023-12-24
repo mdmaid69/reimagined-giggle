14c328e650708d849c8a26676e9267665032ffd2
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:10:54 +0000

    church splash mass watch platform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
