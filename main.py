da12eaa1278a65c840c16435506ef9db814b508c
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:11:06 +0000

    bank thank beginning prepare shallow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_power(work, time):
+        return work / time
+def is_palindrome(s):
+        return s == s[::-1]
