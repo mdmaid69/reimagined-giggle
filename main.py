0e46ddbc0d709b469d3d0e2e5a0c5e3cc825912f
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:58:45 +0000

    bear bet event successful shut

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def is_odd(n):
-        return n % 2 != 0
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def append_to_array(array, item):
+        array.append(item)
