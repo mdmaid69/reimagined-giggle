72480f6a59b64e06a276ca2061a363744268f5b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:43:42 +0000

    go importance block inform lay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import re
+print(re.match("h.*o", "hello world"))
   import os
   def get_file_size(file_name):
-        return os.stat(file_name).st_size
+        return os.path.getsize(file_name)
