fbcc3a676ad832fe951febf2e0ed85d85a166593
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:04:36 +0000

    correct add get extremely image

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-print(sys.version)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
