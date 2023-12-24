ebe0acb77efec0b85ec667dbedd4471dfbf56b90
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:53:35 +0000

    place free commit light police

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
