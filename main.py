1c0bd3fd18c4e9baaa933c0c0256d324b3cb2d53
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:58:45 +0000

    might doubt one distance closely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
