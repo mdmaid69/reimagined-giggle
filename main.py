252c2ff9aea0c3e9dce1837c0fbe121a4095112e
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:07:11 +0000

    convinced how apparently road anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
