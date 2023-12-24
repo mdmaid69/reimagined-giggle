2069669e2ce70640d5a05961ba75f15cb7dcdfd4
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:15:18 +0000

    juice teach interested outside ground

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import random
+print(random.randint(0, 100))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
