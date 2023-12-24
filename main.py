d88e89fcff22f158c8182a75e11ad54990a9b708
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:46:20 +0000

    usual breakinto try when play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
