228dd76d76fe9c7cdbe9eb7cd131f1e70209f043
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:46:14 +0000

    somewhere command welcome only to

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_acceleration(speed, time):
+        return speed / time
