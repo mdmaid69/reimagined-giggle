8da58cfeb02d3ebe3609b89c673fd3b9f781519c
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:21:43 +0000

    refer sure mouth wrong wing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
