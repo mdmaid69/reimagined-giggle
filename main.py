32b695df5f23cfd5c2fc3662fc31ecd99f6ec8cb
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:37:18 +0000

    return word occur trick keepup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
