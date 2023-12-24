3a02bdbf1810a19ca6b5c0e7056a96d25bb271b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:09:22 +0000

    floor confused opinion onto delicate

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
