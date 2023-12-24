866f352207cd5434359cf37d0238bc4477ae6049
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:30:40 +0000

    press separate couple mistake off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
   import os
-  def get_file_owner(file_name):
+  def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
