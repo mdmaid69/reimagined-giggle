f5d613a4af1bff3bcfe9a11fc64ad49e1fa04928
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:37:35 +0000

    delicate programme opponent bringup straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
