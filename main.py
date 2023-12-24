4dff6ded5b015d010b365518f513d1c118fca13c
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:54:03 +0000

    opportunity near absolutely polite file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_power(work, time):
-        return work / time
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
