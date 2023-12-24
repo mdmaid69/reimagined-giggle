5b686ef2adeff6182ffd06d6d3f48a3ef0b86bad
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:09:53 +0000

    win consider responsible cut smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
