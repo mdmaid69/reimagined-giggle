ab32e77d340c0b92032dbf2a6c9acfcaf1275813
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:48:25 +0000

    fence safety block undo forgive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
