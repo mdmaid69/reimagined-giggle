16feae558acec6f0522d09773d8d1d68cfc1176c
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:54:53 +0000

    main trap firm physical surprise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import tensorflow as tf
+print(tf.__version__)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
