375886c9abaea9569dfcb6c8ca5f067143527b8d
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:27:54 +0000

    off belong unite cough hurry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_index(array, item):
+        return array.index(item)
