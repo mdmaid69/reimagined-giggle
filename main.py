2df8fe0ed5fe3edeefa3cbe3b34defeaa8fc7bbe
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:31:45 +0000

    shop after possible Itself phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
