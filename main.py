f634729a78e2895dc4c909c0b5c3ca230754c772
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:22:46 +0000

    teach distance meal without fetch

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
