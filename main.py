3f5dd62c21e5f83dff86e6f03cbb226808cd6933
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:24:48 +0000

    walk collect spot fight cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
