262a605220ecf0a01fd639fa983800b397cdabe6
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:55:02 +0000

    arrive simple pick rare manage

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import os
+print(os.getcwd())
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
