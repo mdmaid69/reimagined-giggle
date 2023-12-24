9c04a333f453015e4fd508e82460a7acb086ee65
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:09:33 +0000

    black school be fear belong

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
