0933a03d6b045c10d9427d6713e5ff95505a0dc5
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:57:31 +0000

    cruelty cause main family perfect

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
