628028e430a5e65f8f0425e2b3fa2d0820b213e7
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:30:29 +0000

    story kind strike fork high

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_speed(distance, time):
+        return distance / time
