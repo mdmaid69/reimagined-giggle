8e6fb593d58ae24f2615bab06d9a9059449a72e8
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:45:33 +0000

    condition race successful trap progress

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
