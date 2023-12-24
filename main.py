3229ea9750f0d933631de496f41a8d471c110d09
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:13:11 +0000

    570enjoyable smell seriously watch nothing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
