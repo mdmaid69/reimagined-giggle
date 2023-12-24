23c8b5e2acbc1228df7834cbbb928930ccc8fecb
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:25:05 +0000

    car profit spare blame imagination

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+i = 0
+while i < 5:
+        print(i)
+        i += 1
