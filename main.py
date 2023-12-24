276fff075403d9834c04e09f50adba3e3fbef8ec
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:00:54 +0000

    smart guess now matter excellent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
