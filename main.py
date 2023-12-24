b240bde68bc4ed464a16ee3396553fec0b1c496e
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:11:18 +0000

    foreign centimetre weekend the rid

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
