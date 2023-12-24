fcd9b6210c361d579adec2eb84d2991e53fba387
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:05:27 +0000

    crazy sign talk wide precise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
