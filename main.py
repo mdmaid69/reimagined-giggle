04315a9c46b44f538f7c691db2726230c033058d
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:34:26 +0000

    crowd tidy evening food fit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-for i in range(5):
-        print(i)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
