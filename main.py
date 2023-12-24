3773521b860213a6a6afd86bfbb50961f82de538
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:33:39 +0000

    exist turn dangerous bound count

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
