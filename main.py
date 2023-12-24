ae4439e0dfa2203e920a7c00d8fed556b5445673
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:41:47 +0000

    sweep float neither save cost

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
