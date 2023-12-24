16308481962d00a979f1725d0ed49c5b71767b71
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:43:30 +0000

    scream operate Ii final trip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def convert_to_binary(n):
+        return bin(n)
