65dd49493334f34746f58541bc181fd3fb3cbb2f
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:20:52 +0000

    bite term act Ww wise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
