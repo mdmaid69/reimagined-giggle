c987ee01bec71f9e710cdb2b87f2a07f30008f10
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:18:04 +0000

    swimming apart glass operation last

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
