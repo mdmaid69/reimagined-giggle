5fc1c4bc506f48fbf52db422a2f5a41abaeec64c
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:18:01 +0000

    score Uu hair peace change

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
