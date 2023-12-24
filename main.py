8550bcf82994b86e8f971d565d848a7c128a9b01
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:52:48 +0000

    refer picture clear protest wait

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
