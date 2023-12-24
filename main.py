7864bd9f685d91633782c0667840a1204a4d1ebe
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:48:16 +0000

    describe load lay cruelty female

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+print(sum(range(10)))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
