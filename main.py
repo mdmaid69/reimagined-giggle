14bfa4110db27fcf36828d313a3c64257e0749af
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:57:46 +0000

    sudden port born gooff ocean

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
