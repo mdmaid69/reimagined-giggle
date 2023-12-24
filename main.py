09957b84dc9b7a410421f8af0ed705f493fceb14
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:48:01 +0000

    wash interrupt doctor before ring

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
