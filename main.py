da2d2d3af6af2b5f14a4ba45df1b44d1bff60096
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:20:52 +0000

    neck important everybody bad never

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_current_working_directory():
+        return os.getcwd()
