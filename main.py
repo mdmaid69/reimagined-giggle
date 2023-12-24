e14608853a6769915de125656b0ef0e823a45df4
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:15:44 +0000

    knock extra excuse standard live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
