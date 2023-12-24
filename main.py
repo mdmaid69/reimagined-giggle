854ec9711b4ee13d4d8463fc1c0d29b69e689cc9
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:03:16 +0000

    sting ring famous everybody contrast

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import platform
+def get_os_info():
+        return platform.uname()
