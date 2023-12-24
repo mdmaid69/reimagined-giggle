4df65660cd16540032f9d9ff7c3769e4729feb65
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:42:42 +0000

    please sharp development your no

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
