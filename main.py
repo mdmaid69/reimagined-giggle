0526fcb694bfe2e9e42ba80376a3c3db4883b5b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:48:26 +0000

    shoot money search conduct adjective

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
