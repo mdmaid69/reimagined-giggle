45f0b89af3eac4ddd45c8ecc9f58c86f5c1aff11
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:42:23 +0000

    worse total underneath workupto room

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
