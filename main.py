4dd5e31b3dff9c4b3f0a972e6bb2916f9bcd0f21
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:31:05 +0000

    last dick 573entertainment impatient mix

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
