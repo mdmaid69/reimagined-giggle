b6bfafb88d6549d4a0e83bb469fd20eb55257fa3
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:05:24 +0000

    blame give active goback(to) cinema

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def square_number(x):
+        return x**2
