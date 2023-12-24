6ab16d3fd3fc50cb190cf1a27fcf0c151e6f5909
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:45:05 +0000

    suckme murder callup shoot turnup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
 import shutil
 def delete_directory(path):
         shutil.rmtree(path)
