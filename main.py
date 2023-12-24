1f44a04fc10690ddc2a3b7842d533e2159338152
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:08:12 +0000

    spring behaviour raw miss base

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
