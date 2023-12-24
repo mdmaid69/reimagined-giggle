4c5c6179e028a41d4ae47322a66898ab601f9722
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:16:05 +0000

    react interested over money preparation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def count_words(sentence):
-        return len(sentence.split())
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
