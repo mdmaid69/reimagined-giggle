6deabea81f8f645ffef06c04d4ae03ee6a11b0e5
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:35:09 +0000

    wrong measure open correct present

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
