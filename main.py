4e7584ea5082e50d7b8b09e3f741ee091405ef86
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:52:31 +0000

    brain about fire period drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
