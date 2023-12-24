0012a3baa2d024af82d80678f840cddd9f0b18a9
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:49:57 +0000

    goon upwards adjective forward keepoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
