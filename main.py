25c38229ca0bef75337f61629f36b9eabe8c4953
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:56:03 +0000

    back gas shape century unfair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def get_array_slice(array, i, j):
+        return array[i:j]
