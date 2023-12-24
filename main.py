800f15af94d18c03fbc8b31f29ac67f8e4204c47
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:39:38 +0000

    point do buy ask nice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
