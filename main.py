0e13919b2af3795c07c387d5a9f1e10ef0a4f1f3
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:23:02 +0000

    smile classroom contrast payment stone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def add_numbers(x, y):
+        return x + y
