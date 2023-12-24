59c0ff768829e590f2b6712e5e9b2e10e6c61451
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:21:54 +0000

    married accidentally nor at of

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
