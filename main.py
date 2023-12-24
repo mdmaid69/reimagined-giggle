f2e38ccd515d93d95be93da498d9084285bc73d6
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:34:34 +0000

    better hungry stick share round

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def subtract_numbers(x, y):
+        return x - y
