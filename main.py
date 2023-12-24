0fdb08c04f2c284e72a05797146cf77660b093a3
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:21:11 +0000

    ought different angry sea fashion

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def create_user_list():
+        return collections.UserList()
+import platform
+def get_python_version():
+        return platform.python_version()
