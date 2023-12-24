faf8d24ffff6f136fdf259663e5c79305e23797f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:07:31 +0000

    illness bury illness fond spill

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import os
+def remove_directory(path):
+        os.rmdir(path)
