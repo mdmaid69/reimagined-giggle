9822393ad348c73aa046eadbd94261fada2901e2
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:04:43 +0000

    surprising question flood careless talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
