60b4f09d3f7dd3c6184a4a4629bc315c60e2f556
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:23:25 +0000

    beautiful thanks case nowhere like

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_tuple(array):
+        return tuple(array)
