02ed3990a093c754fcff0e4d66fbe14714822c6f
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:21:40 +0000

    solve necessary word excellent punish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_acceleration(speed, time):
+        return speed / time
