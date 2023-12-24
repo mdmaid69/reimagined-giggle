78b150e4643e763cf0d20208152c0b4b9af64441
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:18:36 +0000

    cinema proper sing little yesterday

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
