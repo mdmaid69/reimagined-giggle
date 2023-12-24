a9d428b26aae530915957ca3680ce8f61dc8c320
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:17:05 +0000

    stamp believe continue benefit blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
