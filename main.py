980f1a3e6a97544cf0c2d14165c3fdd608fc59f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:47:55 +0000

    fine sense hide best few

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
