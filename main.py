fc02de423975cc9d3d0ca37159afe0526c9079d8
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:53:35 +0000

    through wealth receive already minor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_pressure(force, area):
-        return force / area
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
