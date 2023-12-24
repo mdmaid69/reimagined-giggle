e98c0f52387a1b2bf7b9d19133afea636846b48e
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:33:17 +0000

    pour before regional accommodation receive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def convert_array_to_list(array):
+        return array.tolist()
