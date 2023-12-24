6e1440467a377ace1ff74e2291c57aa320197a30
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:40:17 +0000

    state happy material weather rush

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
