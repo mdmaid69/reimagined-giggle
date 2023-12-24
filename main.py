d0bf8408841ac2d43e538bbc78a4d0f1151592fd
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:38:27 +0000

    mystery along disturbing concentration pupil

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_bytearray(array):
+        return bytearray(array)
