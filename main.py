14d97910b5356b01c4da5951c7384e3e98b280e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:48:11 +0000

    dream dress moon lean little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import getpass
+def get_username():
+        return getpass.getuser()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
