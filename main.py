a37f8a1df09664f08446bf9b06e73645d9bef752
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:04:07 +0000

    thankyou say accept free increase

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 def is_palindrome(s):
         return s == s[::-1]
