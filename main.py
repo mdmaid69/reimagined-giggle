75daff00be4e4fcdd32cb61580f0327d28c51cdc
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:52:26 +0000

    lead wherever exist main flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
