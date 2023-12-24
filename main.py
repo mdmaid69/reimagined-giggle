dce2ba3dc159807153e5be682a7768409e8c3ce1
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:15:53 +0000

    last callback across question replace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def convert_to_binary(n):
+        return bin(n)
