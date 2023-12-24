0baf8e23757fb1c720538892e5393dd48bcba518
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:59:50 +0000

    present partner spill circle fence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
