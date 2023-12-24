b65a79a3d7cb78c57094509d73cbf725da9e7165
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:33:11 +0000

    narrow bag press before like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
