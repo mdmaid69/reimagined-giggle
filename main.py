9a157ffa1dca02976a98296b3ddf8f5f4abc60a2
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:18:17 +0000

    impatient comfort only tense squeeze

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
