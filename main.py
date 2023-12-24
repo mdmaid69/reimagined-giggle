a94ef8791c425fada1e54c1ed46361fcfb8f7e90
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:45:26 +0000

    surface die ours though wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
