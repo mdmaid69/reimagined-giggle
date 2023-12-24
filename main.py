9f75cc7a07781eaab8a02d99dafddbe08353a51c
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:07:22 +0000

    transparent present ear wish bend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
