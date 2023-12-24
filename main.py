65ca1565b22f21b35f1c1af197b57cecd42aa720
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:29:28 +0000

    impression hate idea appear cap

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
