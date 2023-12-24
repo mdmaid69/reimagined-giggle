5e1332fd38f5bd39c3f03490a432a4faaa310587
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:05:23 +0000

    catch ground nurse slice object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+name = "Python"
+print("Hello,", name)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_float(array):
+        return float(array[0])
