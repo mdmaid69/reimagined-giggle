df12472bb454cce365ec278b7c6760551b1be70a
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:19:03 +0000

    offence muscle goback(to) detail free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
