1f28ca9f05fe9aafb4517fa458348185ee547853
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:33:13 +0000

    upset wall tap artificial listenfor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_work(force, distance):
+        return force * distance
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
