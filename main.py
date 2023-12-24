0deac8c82ddac2520fb8bce1500dff2fefad66a3
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:21:11 +0000

    range none reduction exhibition cover

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
