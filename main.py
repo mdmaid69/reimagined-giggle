1d07cb353e3c4d658220c62c8daaed4b931195e2
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:18:57 +0000

    light allow priority benefit less

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
