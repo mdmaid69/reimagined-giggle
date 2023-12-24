571b0735d7827681783c0ef7a295b4e129b82bf9
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:18:12 +0000

    manage no they arrange fit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
