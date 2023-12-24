e97e27ac148d7e076db8b0b44bc6e06f296c5acd
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:50:03 +0000

    scale mood theirs price damage

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def square_number(x):
+        return x**2
