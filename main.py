9367c9b71bae3737cfa0b8992ad0260ae3bab30c
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:24:22 +0000

    hospital string figure Ee frightened

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
