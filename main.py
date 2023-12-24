d8a6d867203ef0a4bbc2d6f7960161a13f7a4095
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:19:30 +0000

    beside generous makeinto OKorokay bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
