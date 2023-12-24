7501a0e88655cd2e2422cc989bf8147c40040b46
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:30:11 +0000

    breakdown straight push score tune

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_acceleration(speed, time):
-        return speed / time
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
