33c6e018ec2c4cc972fa71310efd3bd2427d6910
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:02:06 +0000

    other spend your hide set

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
