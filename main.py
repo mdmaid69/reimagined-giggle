309dc95aba464f7daa132f6ebdd09a322f53472c
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:22:33 +0000

    cost usual well sign makeup

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
