87588d0568740f622432cceb0dd41a7b6d54c52e
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:17:04 +0000

    promise authority urgent worth plenty

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
