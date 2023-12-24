f4b8c52e90addd012ba2d499bb9ec711dc6806bd
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:14:51 +0000

    sleep star away shut habit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
