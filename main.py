a29ec59fdaab7b3d14bb00f29235dba167fa5093
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:33:00 +0000

    cold forever wash splash into

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def square_number(x):
+        return x**2
