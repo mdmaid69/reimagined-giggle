1b995e2290c0f9f732db5fb76009e2f57b50b26c
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:15:38 +0000

    relative small 179bite tool general

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
