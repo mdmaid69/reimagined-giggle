6379d87cf5d2f4e5befd9b5e1498df04d4efd615
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:55:07 +0000

    contents right save pinch print

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
