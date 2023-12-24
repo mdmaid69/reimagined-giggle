f2bc90ff70a64f78a50f9bd83e169953fa059914
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:34:52 +0000

    back so breakup rent general

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_cosine(x):
+        return math.cos(x)
