a363117128dd94d78b1fb32f8b39ff585bf2904b
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:18:50 +0000

    same base guard address thinkof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
