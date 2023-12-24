efe0cb0e0e8d5a8391f70139d736f0714c64de8e
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:41:16 +0000

    load war active depend appeal

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
