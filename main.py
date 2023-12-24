b3e32ac24c6a54b6a0a23962c6841ddece29a514
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:14:52 +0000

    frighten secret weather drop smooth

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
