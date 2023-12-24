f1846c1c37bf33949fd78032535c36efe8764034
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:46:49 +0000

    organize protest talkoutof choose translate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+x = 10
+y = 20
+print("Sum:", x + y)
