a4496bf4e7bf9ba1f32652461996a31fcc2ce96d
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:53:25 +0000

    generally work rule interview 565end

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import re
+print(re.match("h.*o", "hello world"))
