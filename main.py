df73c2ac470992edad8174c3b4d3646ef8d914c9
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:28:27 +0000

    sort yourself beside keep problem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
