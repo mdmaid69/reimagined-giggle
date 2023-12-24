43a361a3b3bf65e66c3949734b23e6cedd92825e
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:57:06 +0000

    building deal arrive pleasure tie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def convert_to_hex(n):
+        return hex(n)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
