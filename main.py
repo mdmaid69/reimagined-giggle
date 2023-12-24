187b5884330429350e03901c39c72a84aa258daa
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:49:47 +0000

    breakfast nowhere themselves wake suggest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_sine(x):
+        return math.sin(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
