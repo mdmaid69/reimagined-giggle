bee177c4cc383388b042cd6a7633bae0edd5f2ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:51:57 +0000

    excuse sit father nowhere whole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import getpass
+def get_username():
+        return getpass.getuser()
