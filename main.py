e5dfd273f2167766b7c0e7195a7b641936f9ad79
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:40:55 +0000

    much whether remove week feel

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def is_palindrome(s):
+        return s == s[::-1]
