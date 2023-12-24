9de1185fe1d4f5c86f74b349c6834cd36b2b665e
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:02:16 +0000

    mysterious fall tonight club strange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def calculate_speed(distance, time):
-        return distance / time
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def split_path(path):
+        return os.path.split(path)
