ff83a5cffd9ad74ea67a3d1d03aeb2881875c625
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:22:36 +0000

    weather instrument involve sight piece

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def cube_number(x):
-        return x**3
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
