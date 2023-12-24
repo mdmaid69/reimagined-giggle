a50aefdee4c29805b3bfa077b9b729ea4c946656
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:37:20 +0000

    delicate importance this now meet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
