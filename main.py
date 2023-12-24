a139b0b317b6951ea1b3ef5ae2f2c2be00a00a75
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:45:11 +0000

    holdup over unable cold write

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def get_array_as_memoryview(array):
+        return memoryview(array)
