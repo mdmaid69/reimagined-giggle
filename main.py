616ed3ffd557e79dbf842a980f812affe8b95eba
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:12:46 +0000

    community declaration completely offer petrol

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
