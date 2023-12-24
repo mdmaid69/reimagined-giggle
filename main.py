7b15fcd7d554a3719f98927d2906695775abf2f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:57:04 +0000

    bridge culture impressive under uponon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
