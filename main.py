03c1ff6f5458e30793c4a9f3803f05dea584611e
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:07:54 +0000

    bring measure brush almost declaration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
