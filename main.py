e0955f890b795c2d71abd9699bdeedec08cb5f34
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:19:25 +0000

    like press except for breath

--- main.py
+++ main.py
@@ -3,5 +3,5 @@
         plt.pie(sizes, labels=labels)
         plt.show()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
