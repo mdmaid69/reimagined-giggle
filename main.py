05a71712e7a2d14719c193c6be0434e088fe50ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:48:04 +0000

    correct wing software degree nerve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
