f8d05a263a947d42a0468fd55335e584b917f15a
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:31:52 +0000

    pupil rule along rare shake

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_floor(x):
+        return math.floor(x)
