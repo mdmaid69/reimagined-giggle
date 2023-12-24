f9ecff5c83f669fc99973b8edf5629ce9d21bc71
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:37:23 +0000

    stage time open sort set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import array
+def convert_array_to_string(array):
+        return array.tostring()
