2a46b65db9314bba0546473baaab6a06ab731fbc
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:11:49 +0000

    breakout damage activity prefer loose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
