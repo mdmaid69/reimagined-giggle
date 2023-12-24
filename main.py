f12991eb1c25d4720536db4656c6f41ea15dcd0c
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:00:01 +0000

    runaway total century closely sweet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
