4e6c460c777a87579c5cbec750abe62045c64257
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:03:35 +0000

    birthday arm immediate recover kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
