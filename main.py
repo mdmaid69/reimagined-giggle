a3bc725cdf817e990185c00579317154b416e741
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:49:16 +0000

    gas frightened uncertain juice direct

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
