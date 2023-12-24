8f32f8e95ed198ed474787bd16596a8359f477ee
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:45:32 +0000

    plan its feed sink needle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
