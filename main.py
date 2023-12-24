2b489e8225ea176aa2f30370cea15fbf82b5b6b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:52:46 +0000

    style advantage fat exciting geton

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def subtract_numbers(x, y):
+        return x - y
