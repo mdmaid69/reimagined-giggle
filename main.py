67850b3cb128dcd96f59bdf4b7b01b186f682afd
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:08:18 +0000

    limit Ss wing blow in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
