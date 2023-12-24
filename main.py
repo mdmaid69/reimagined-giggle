f31b5b7d176d4c1cb6af757f102e6735aac4e005
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:55:46 +0000

    official branch hurry eat mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
