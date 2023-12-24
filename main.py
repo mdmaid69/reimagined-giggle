38d3f9c8df34469b9b98c0323f0c25000ce0d45b
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:10:13 +0000

    add either busy passenger worth

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import array
-def get_array_item(array, i):
-        return array[i]
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
