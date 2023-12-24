365581d5e29e7b127ee021f88408eb1bb7c214de
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:08:00 +0000

    tune modern whoever suck course

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
