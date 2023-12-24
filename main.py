b1a32954a3552fdbf0ee694517b44fe4f7dc95cb
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:42:23 +0000

    worth after difficult edge bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
