c71e4d7e6526119329597b872917ff070ab46119
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:07:13 +0000

    at top reply seriously ball

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
