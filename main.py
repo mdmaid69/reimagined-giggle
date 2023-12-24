631177193c4b3894216945718b1d0264af99f1ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:48:53 +0000

    dance harm suffer cruel responsible

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
