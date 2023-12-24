aee15c2c1efd4f3ee1f056431bc1dabb1f41cb1a
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:18:57 +0000

    concentration wall order real normal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def add_numbers(a, b):
+        return a + b
