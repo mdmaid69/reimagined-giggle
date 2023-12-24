09967d1676c18f167d86247ad6dbdf96ca3fcad2
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:07:23 +0000

    onto mind she change steer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import collections
-def create_user_dict():
-        return collections.UserDict()
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
