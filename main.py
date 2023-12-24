fece9a78786163332d17f65dcce6649ba35c8fba
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:36:04 +0000

    Itself part noone deliver object

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import datetime
+print(datetime.datetime.now())
