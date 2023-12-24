de1828655f2f14283bc23d0f745bad6b507173cd
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:09:39 +0000

    everywhere stamp cry special someone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
