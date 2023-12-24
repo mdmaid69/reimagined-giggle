b14209ce7a4d6e1fe2d84b33dbba86428cef8122
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:44:20 +0000

    left powerful some admire fall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def is_even(n):
+        return n % 2 == 0
+import array
+def clear_array(array):
+        array *= 0
