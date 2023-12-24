34a22a8b1b4ca31375886b6ea7f3d09202e22f5e
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:52:17 +0000

    anywhere near until such cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
