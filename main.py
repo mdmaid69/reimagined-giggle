a947a535af26b4b44ab0c2c0fe99593a62c78268
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:46:47 +0000

    like future sort left post

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import math
+print(math.pi)
