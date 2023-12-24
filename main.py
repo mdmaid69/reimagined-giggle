bfbcd42b6d332c310a499ebb89390711e98f0310
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:37:10 +0000

    lesson proper lie private hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import random
+def roll_die():
+        return random.randint(1, 6)
