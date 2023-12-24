e36e399ff3dc69740e6da191c2cd1d00c6c28da7
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:23:30 +0000

    people picture loan out alive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
