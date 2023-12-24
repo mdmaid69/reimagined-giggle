5675049920243cb68243df9256626544f8e9ee21
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:09:21 +0000

    any mend suggest damp influence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def convert_to_octal(n):
-        return oct(n)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
