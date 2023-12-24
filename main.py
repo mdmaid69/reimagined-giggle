338275edc0464db5147d1d52ba64d5ab2cff8d4d
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:23:53 +0000

    suggestion term call judge introduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
