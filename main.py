0a9050f565c9aebf6fefd0cbdccfba6df0a107c9
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:47:32 +0000

    load roof holiday switch regional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
