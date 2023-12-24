45c97a0604f491ffb97868996fcdf46eca96b7e8
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:17:02 +0000

    Pp thankyou law face height

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def cube_number(x):
-        return x**3
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
