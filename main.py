2d138b3fed6ccdee0d74789947b941051746d1ee
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:12:13 +0000

    confident attract only clear house

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
