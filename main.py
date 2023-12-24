389be96513757b196c33bad57f8a48196aad66b8
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:41:59 +0000

    contact machine tight call over

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
