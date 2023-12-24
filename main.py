e055012e0730dcecc20ef274f3444bae9dccebb2
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:30:39 +0000

    group several pick change stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_slice(array, i, j):
+        return array[i:j]
