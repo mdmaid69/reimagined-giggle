2e147d986eb28abd6e0ac0b8fa6d67ff8d0c70c3
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:26:51 +0000

    lean over shadow without head

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
