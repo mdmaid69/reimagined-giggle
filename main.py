3e89c49726bee5d0403f96857baa563f451438f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:30:40 +0000

    address wild pen lean exciting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
