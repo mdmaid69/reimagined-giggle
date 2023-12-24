b24180a081d295def399c08029c037a12d6a7a7a
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:14:08 +0000

    mean suffer allow keep too

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import logging
+def log_message(message):
+        logging.info(message)
