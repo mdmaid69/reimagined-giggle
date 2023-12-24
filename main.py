0de3e6a5c8506f669638c9553c3e7e8dbfe6f9a4
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:25:51 +0000

    steep apparently creature water turndown

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
 def calculate_mortgage(principal, rate, time):
         return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
