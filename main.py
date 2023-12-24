153766ff21f56cdea473855af2f6665c84aa7f99
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:23:58 +0000

    that note tip interested exactly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
