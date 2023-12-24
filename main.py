8f3cb917024c3277244bc9e17982e56453c311ee
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:14:49 +0000

    operation refer file successful attract

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def find_max(numbers):
-        return max(numbers)
+def reverse_list(lst):
+        return lst[::-1]
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
