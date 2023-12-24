b1f5564d01cef6917c3cae2b5fb9048b7797363d
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:31:39 +0000

    curious custom conclusion challenge book

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
