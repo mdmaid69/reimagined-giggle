47061fb67bd627798454a897cad366d4c1708488
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:19:36 +0000

    fierce corner medium murder trip

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
