e262910a51b0f97614c57c56a77f306117c804ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:33:43 +0000

    news shrink 568energy shadow fire

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
