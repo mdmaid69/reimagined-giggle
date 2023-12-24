034d8cc5bcbc89a7837f66a067a6b0974e630ede
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:53:29 +0000

    geton meal tooth off keepout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
