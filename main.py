f380f3ad5e7fda55837893c290d299b351e09b56
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:03:53 +0000

    admit seriously plan block range

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_work(force, distance):
+        return force * distance
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
