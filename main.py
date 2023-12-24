46aaa39a99450e2a2ce7120b434340bd87ba239c
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:26:05 +0000

    runaway beyond oil badly fit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
