3f92f75c12355aef24f5b52634cbb22db5bd3ed1
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:33:01 +0000

    minor please immediately unpleasant unfortunate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
