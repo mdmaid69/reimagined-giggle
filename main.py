c73bb8a52d788d505056f4bc15414ca8226f94f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:43:51 +0000

    message ring generally play likely

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def add_numbers(x, y):
-        return x + y
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def find_max(numbers):
+        return max(numbers)
