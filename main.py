af7ff31e423e60670332475c9b5b03316017dc38
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:20:08 +0000

    whatever culture content press rate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_speed(distance, time):
+        return distance / time
+  def convert_to_hex(n):
+        return hex(n)
