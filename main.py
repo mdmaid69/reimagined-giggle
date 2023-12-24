71c95a47bcbd5a539fd3200284fefb927054d506
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:58:26 +0000

    land together greeting switch quickly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
