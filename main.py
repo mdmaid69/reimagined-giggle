6423fae40a53e8b25a0d34db000ced5397e7bc78
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:09:32 +0000

    unless somewhere combine table front

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
