4a198ed3e571d7f062b576ce3b57ded5c7515d41
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:54:37 +0000

    behind exaggerate drop nervous weak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
