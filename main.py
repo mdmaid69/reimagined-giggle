c7995b8c692176ade9c1f691111be377aaa07cc2
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:45:58 +0000

    criticize criticize favourite love perfect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
