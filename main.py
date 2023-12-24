f9ab0fc3d5bdcaeefa10b2e6f7c729d03fc2107b
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:57:20 +0000

    upset emphasis dull with offer

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_length(array):
-        return len(array)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
