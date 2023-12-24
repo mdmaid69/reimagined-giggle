2f02133883735a45c40b96e90e8501051f8f7dd4
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:08:30 +0000

    stamp able risk claim suckme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
