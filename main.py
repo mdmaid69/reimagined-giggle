df59792c1f1fbb79f20d652218192ffba5855927
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:40:24 +0000

    use example tip beginning suddenly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
