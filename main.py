85b5e9103d03804886757347a91efc4d59f027fd
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:03:50 +0000

    warn whole introduction run tradition

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
