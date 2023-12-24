10d736dde21aecc85d6d3e5c992df386a009eb75
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:13:54 +0000

    possibly succeed temper takeoff serious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_bool(array):
+        return bool(array)
