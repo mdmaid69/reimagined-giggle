f37583915c76fdf2277cb814ba315337e8f59500
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:12:35 +0000

    then bath in switch send

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import array
+def get_array_as_int(array):
+        return int(array[0])
