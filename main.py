7e0ef7088a76b8e8e067431a56edf46ca929eb8d
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:58:40 +0000

    as fetch world likely please

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def square_number(x):
-        return x**2
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
