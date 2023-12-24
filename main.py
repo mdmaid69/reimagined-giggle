e77a26c2de3176579fb3d2385e3b1447a91432b3
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:23:53 +0000

    far giveup bang bill embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_elements(lst):
-        return len(lst)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
