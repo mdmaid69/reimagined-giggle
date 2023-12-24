d7a8c6dd5db71985cba11f344c0e1e8e077f201b
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:14:39 +0000

    century commit proof yours repeat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def is_odd(n):
+        return n % 2 != 0
