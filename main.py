a36f75ca2937980bb5d65c2ee9f367afebb39463
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:21:53 +0000

    might Vv away all suck

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import numpy as np
+print(np.array([1, 2, 3]))
