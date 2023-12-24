44cc39aee1cddf1d1671fd7765ecb1765b890b84
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:34:42 +0000

    whoever him direct influence pinch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
