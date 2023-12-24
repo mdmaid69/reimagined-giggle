adc227064f11815c41f40bd9847978f1bf6e7148
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:05:31 +0000

    goaway length relation national absolutely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def count_words(sentence):
+        return len(sentence.split())
