bbca3ebcee02049b1e2c2cf3fddb0b42f48fcd7f
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:42:37 +0000

    mixture on idea Ee cause

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import tensorflow as tf
-print(tf.__version__)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+print(sum(range(10)))
