fc17f2a96cb1c181b7973fd61f84722adac6b7b3
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:12:57 +0000

    plan period petrol brown bring

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
