bea2128d697e1063201b80eaaa93003b3fcd8654
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:22:31 +0000

    beyond buy ought bottom left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def is_palindrome(s):
+        return s == s[::-1]
