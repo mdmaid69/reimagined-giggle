e39ea876bc29f16f2b5cbef5b33a69ad842dd596
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:45:28 +0000

    return car closed practice love

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
