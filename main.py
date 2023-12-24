5fdd9b78dacff9f01bd81eb5977bb0912fe8779c
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:16:40 +0000

    fair back pause public night

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def convert_to_hex(n):
+        return hex(n)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
