beb451591d5a444c0f61604164bdbaad740177f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:10:38 +0000

    sign special arrange relation wealth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
