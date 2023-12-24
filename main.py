541acc2e3669713efffef8768bf1faa11bd689c8
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:26:40 +0000

    book interest comeback contents tongue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
