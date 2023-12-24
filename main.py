bb70197e6e4c26d0b65bc107db86e845a172bac6
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:45:51 +0000

    escape just over stay production

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
