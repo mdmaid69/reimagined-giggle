832af690e109a2d38bb74cbc6342698cf3b08383
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:55:06 +0000

    push contact mind opposite group

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
