c7504ab1859a072b49a12eda7cca4bab6d79c143
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:09:27 +0000

    suffer promise reply they long

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def convert_to_binary(n):
-        return bin(n)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
