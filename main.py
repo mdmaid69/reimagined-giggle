184989a7a491c717f18ad2aeb76e59288d11cc04
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:57:44 +0000

    side demand memory find failure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
