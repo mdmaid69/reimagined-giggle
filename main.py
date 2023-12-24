437d3f234f93381edc2552d0a7cba94f77ada1db
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:54:50 +0000

    available independent behind earn share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
