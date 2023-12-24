ced9246c6622680ad72ada4743a9191e75e73099
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:27:57 +0000

    furniture there else frightened strength

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
