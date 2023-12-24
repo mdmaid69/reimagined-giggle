d6dbf733ef9dba42c506b81c43c07bbfb0416bab
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:39:07 +0000

    read history aim ignore check

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
