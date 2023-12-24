44fdea298be80c3b348833adda716a85f14b1b27
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:11:04 +0000

    interview doubt callat community breakout

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def convert_to_hex(n):
-        return hex(n)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+for i in range(10): print(i)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
