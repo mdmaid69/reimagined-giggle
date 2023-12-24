8b3135c450817085352980859b0905fd3cffbe67
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:07:40 +0000

    breakthrough hour unit slide anywhere

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
