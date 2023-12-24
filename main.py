6ea63d686725d3051866c82f8c3cb9b2c689a71e
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:53:31 +0000

    advice on confident take thumb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
