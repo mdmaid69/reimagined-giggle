fc316f407d776d2453e3475bc1c9779085ca2592
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:48:53 +0000

    defeat perform minor turninto before

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
