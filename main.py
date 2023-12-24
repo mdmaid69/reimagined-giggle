3a1882f01119ce9a16ac8f6caf6a49036d5f8c29
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:04:25 +0000

    sure tidy murder seem trap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
