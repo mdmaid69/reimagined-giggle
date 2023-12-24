60f3daccef9d4dbc879f76d7eeaa2ce391c52b64
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:14:38 +0000

    union charge getaway fight event

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
