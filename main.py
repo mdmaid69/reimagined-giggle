9f65174cedf5796623207c7df69528d55bd2bd5e
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:50:40 +0000

    point whichever figure safe fasten

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def convert_to_binary(n):
+        return bin(n)
