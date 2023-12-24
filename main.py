90c2e272ae762c25c1acbb66663ac9c7069ba92f
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:59:43 +0000

    joke twist lucky group way

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
