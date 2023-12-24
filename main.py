28ee404746a672bbba1c700096603d30b2051023
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:05:28 +0000

    classroom impressive tend well almost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
