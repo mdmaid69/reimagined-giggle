c3c25b08ce14a4535bf1f16b332855f6e03b70eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:14:29 +0000

    back gentle class lay busy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def subtract_numbers(x, y):
+        return x - y
