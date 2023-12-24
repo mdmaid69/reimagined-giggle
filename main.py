6cc25132c886fe608a12fa456dfa154c12834ee0
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:53:21 +0000

    such smell shake post turndown

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import array
+def clear_array(array):
+        array *= 0
