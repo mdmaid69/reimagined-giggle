e41f59f05b33a87c1aa71bf77019cfb824eedfbf
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:51:09 +0000

    target boring mainly nor skill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import getpass
+def get_username():
+        return getpass.getuser()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
