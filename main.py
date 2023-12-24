7cadba9fe7db978a1a3bd84f0d99f856c2531e95
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:19:15 +0000

    recently search club shout traffic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
