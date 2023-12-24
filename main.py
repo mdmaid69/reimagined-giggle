08dc901ad415ce8884ea4e348040e6ce4828c360
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:34:18 +0000

    trial holdup brave into reason

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
