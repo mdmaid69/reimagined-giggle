3222ddd3821231ab62a1bfd7fdf9e38bae4f454e
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:34:21 +0000

    against first runover patience whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
