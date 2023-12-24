70ac86733581f45545862c251fd6ac1edaa41b42
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:27:37 +0000

    dance them holdon thinkthrough accidentally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
