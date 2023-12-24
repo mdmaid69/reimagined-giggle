ba6dd7125053c82c0707aa6810b50696fd14a206
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:31:28 +0000

    age step goback(to) see ignore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_index(array, item):
+        return array.index(item)
