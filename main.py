fdae95129493dba9b12ac0bfb9173febf6056617
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:10:50 +0000

    such no exactly someone score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import random
-print(random.randint(0, 100))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
