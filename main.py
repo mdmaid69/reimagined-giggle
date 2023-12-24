b88cd1f4abd3e78daf7a0715c6bb7d65ff9a9835
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:56:09 +0000

    congratulate honest save port scale

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
