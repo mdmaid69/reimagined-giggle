9096852720d4419609066a6a42bac482186a1cbc
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:29:18 +0000

    off light consist complaint afraid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
