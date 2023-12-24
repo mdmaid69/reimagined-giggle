8f0c42078425c2177ca51870e4bdd3c135ea87f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:49:38 +0000

    alive mother rescue low big

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
