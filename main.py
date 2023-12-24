3672e5200ace31df78468f34b32425624ff32f26
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:41:39 +0000

    crowd offend soon achieve noone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
