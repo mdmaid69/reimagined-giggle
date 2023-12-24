1c11275cfd882610ed07a6d58ffc222b38ac8237
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:36:12 +0000

    pipe order direct material attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import array
+def get_list_from_array(array):
+        return array.tolist()
