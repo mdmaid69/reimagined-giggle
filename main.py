e1f70448c77967bd9b00e5b6fda3726a6f60c8bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:30:10 +0000

    boring history dear active wise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
