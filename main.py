18ddc070f03604f750954bb468812662527619b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:15:46 +0000

    journey delicate fine contents whatever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def sort_list(lst):
+        return sorted(lst)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
