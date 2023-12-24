28421147244b423c3e3804664f58e10fe2019352
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:50:47 +0000

    himself turninto awake burst believe

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
