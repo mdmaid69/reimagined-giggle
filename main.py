ff499a13841de65563f92979f4d41d27844cb038
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:56:43 +0000

    callback they state bet offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_power(work, time):
+        return work / time
