a2d45cab132ad179406e28f6186dfa432209e923
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:47:21 +0000

    Pp afraid bunch where tense

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def multiply_numbers(x, y):
+        return x * y
+from collections import Counter
+print(Counter("hello world"))
