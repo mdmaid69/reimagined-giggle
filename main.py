a099b57e46a1fdd9ef3aaec41b3fbdff1e1eeb53
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:08:23 +0000

    rent extra glad correct protest

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_power(work, time):
+        return work / time
