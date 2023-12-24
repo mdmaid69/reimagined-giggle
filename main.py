8d7b1332ec8bac20b083c48c755d34fe9a9f398e
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:17:05 +0000

    still high form formal case

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def find_unique_words(sentence):
+        return set(sentence.split())
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
