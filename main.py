2f3f3968b1a4f5d70578d12d11d517f5e46dbd4d
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:35:06 +0000

    curious live emergency very repeat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+print(os.getcwd())
