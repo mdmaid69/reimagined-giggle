79a065b7b8de26de69eae8aefc98b7aa365e1ac7
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:40:52 +0000

    slice read clear favourite part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
