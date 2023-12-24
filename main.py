083562d95b1df0df17782253286c3bc3ed403328
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:26:47 +0000

    gradually piece command rain turn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
