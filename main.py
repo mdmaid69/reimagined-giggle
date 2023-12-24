c0011a5c245e03aadd020e94f71b527e42c75236
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:22:07 +0000

    land instead provide ready bet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
