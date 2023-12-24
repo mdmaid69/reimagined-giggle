57dceef8a3d73d0533c86d37eaf22e6df44d143f
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:35:18 +0000

    our consider supply side difference

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def is_odd(n):
+        return n % 2 != 0
