f348d96cb64312efcd3d2ef42dee8ef98aa01ad0
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:35:13 +0000

    sorry war mixture public this

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def calculate_speed(distance, time):
-        return distance / time
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
