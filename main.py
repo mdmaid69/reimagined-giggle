b6cf3142c260eafeaa1cead075368d6a6ffdef32
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:57:22 +0000

    bang commit imagination successful fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_length(array):
-        return len(array)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
