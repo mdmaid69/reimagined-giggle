24314b8d169b710cd443db0cf0ec3341e839c4a3
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:36:57 +0000

    deal breakup wake patient influence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
