deda9d94b72b9810de6d31865ec25afe62231b61
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:18:11 +0000

    myself every history hollow comeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def reverse_list(lst):
-        return lst[::-1]
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
