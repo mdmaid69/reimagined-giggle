04caefbaca322a7a019031b00847ff7e1893df73
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:27:50 +0000

    tired practical discovery determined thinkof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_base_name(path):
+        return os.path.basename(path)
