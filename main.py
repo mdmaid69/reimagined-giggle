56e250402a0fef0c7476a3a2cfc9e0f4ee1f4bc1
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:31:58 +0000

    getup lay properly fill available

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
