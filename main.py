a1580126f325d51832fb04df9935a461f1afeeb6
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:28:39 +0000

    introduction ill holdup food reasonable

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
