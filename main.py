d4443c84fc461ec17501ac22de4f274a00493560
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:49:18 +0000

    claim date greeting space noise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
