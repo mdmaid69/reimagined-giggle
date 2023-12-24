861475c353c1cdb73ac8255a06907dcd5e848d3d
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:59:18 +0000

    bite check snow culture apart

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
