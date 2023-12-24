469fc42db5892083f35f5bbdbbbff3091d0022e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:31:45 +0000

    below meaning line danger rush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-def find_max(numbers):
-        return max(numbers)
+  def is_odd(n):
+        return n % 2 != 0
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
