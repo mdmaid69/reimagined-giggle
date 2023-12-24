98e724e524610fa642b9159d83f96de6a4521dfa
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:58:27 +0000

    famous ball recent ticket ask

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
