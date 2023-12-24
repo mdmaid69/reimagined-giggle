85cf0f0efd384b79d1dbd6ba7427f5fac45ebf6e
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:25:51 +0000

    fear immediately almost deep wind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
