c7d19b87ccfdd80159a6f60f5d951606cf70379b
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:33:49 +0000

    pollution that passenger normal compete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
