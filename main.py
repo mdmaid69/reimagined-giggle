7f1cf8470b50d39fde67efe4587d22eda3f03a7a
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:36:05 +0000

    quick familiar goaway place go

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
