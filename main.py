2e141736dc5744651e7015badd0a998b8f52fc17
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:55:38 +0000

    rank after drive Cc machine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
