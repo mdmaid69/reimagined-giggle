19030be6bfff8ac39aa3449329e527b07e2e7a7a
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:30:05 +0000

    shape standard flavour hold lesson

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def print_python_version():
-        return sys.version
-def count_characters(sentence):
-        return len(sentence)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
