f89253c1d73152b9570f1377afbe0c6cca6f49b7
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:14:28 +0000

    club national hurry care however

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
