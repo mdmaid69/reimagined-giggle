d3e83eb73a53a76d2cf43652e4f1b745e79528df
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:59:23 +0000

    set woman stupid quality quickly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def square_number(x):
-        return x**2
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
