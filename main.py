9bbb569ebf3e03cbf7f04b2c17af9ebf5b47924f
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:32:19 +0000

    relative complete pressure well slow

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
