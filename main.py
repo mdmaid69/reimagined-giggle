fa32ccb57beb4c9435375317c85cd361d1b2e82d
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:56:20 +0000

    before female eye wind intend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
