a94800ec0ca4b3646569ca3caf664d5fb22f4f49
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:05:07 +0000

    wait suggestion so wealth talkback

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
