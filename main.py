99de1feb44ac1c850b2ec6b4ee392caa5cc1b93f
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:09:47 +0000

    rich treat smell turndown Bb

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def convert_to_hex(n):
+        return hex(n)
