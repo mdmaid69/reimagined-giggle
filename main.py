154e7e778caa1f91a89e904c1a29e696be462e7d
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:31:23 +0000

    report in fashion turnback lost

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
