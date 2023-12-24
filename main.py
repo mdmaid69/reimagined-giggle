a3b0f6eb61473095fc5dab1e0d7059f3993e55f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:34:22 +0000

    home any pride press lucky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_density(mass, volume):
+        return mass / volume
