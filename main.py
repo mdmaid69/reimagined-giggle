7d8bb2bc7c1f95b6d8995b4bfff56287b5235f71
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:23:40 +0000

    mystery score repair light swimming

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
