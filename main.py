a5566b34cf1522aa795ca93978ff9ebd26434079
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:05:15 +0000

    flat rank announce ring mood

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
