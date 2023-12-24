eec37c749f5f88f1e93ce5c1bc11e7d915d10911
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:04:12 +0000

    absolutely lot mark care file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import sys
+def exit_program():
+        sys.exit()
