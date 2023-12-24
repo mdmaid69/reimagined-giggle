8060796c77c7fcd9b62f55c80cf73f4a8972a37e
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:01:45 +0000

    full broad fold nerve thought

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def convert_to_binary(n):
-        return bin(n)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
