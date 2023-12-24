7304b9ebc74c8862bf053ca0f20fe25167022471
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:05:43 +0000

    return ask workupto prepare complicated

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_power(work, time):
+        return work / time
