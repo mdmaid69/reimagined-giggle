0f9a73aa5cc686a8c5c0c4eaaf15519404671d25
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:28:35 +0000

    greeting ride extra face speak

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
