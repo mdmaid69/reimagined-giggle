bdb4c0da94fc05c1ab7615ccab43322e4ea33be3
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:34:49 +0000

    guide dark maybe unfortunate statement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def divide_numbers(x, y):
-        return x / y
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_density(mass, volume):
+        return mass / volume
