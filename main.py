8a42b46c525603f8e49b6cfb953d2155fca42291
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:04:45 +0000

    state payment feeling aim Gg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
