9cc8e68134506f035d5211f7aa65716d120f8389
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:12:32 +0000

    572enter waste succeed maximum might

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
