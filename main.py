d894537767295fac652802a0550ac2bcce0fead8
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:29:56 +0000

    hear town guide or strike

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
