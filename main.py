f2ea7c1c38c5b3f5d9db88199136e76c487a4b06
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:02:53 +0000

    whistle whichever study light hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
