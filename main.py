fecf88fd670754d0cd8d1fda178032d2f5bda20d
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:46:52 +0000

    reply nature bored organization check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def subtract_numbers(x, y):
+        return x - y
