c73bcb1386fc54a0cf8f3c848a4c8370f62b113f
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:03:01 +0000

    escape priority display plenty alive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_volume(length, width, height):
+        return length * width * height
