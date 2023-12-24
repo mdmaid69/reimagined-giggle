2f65a1832c9f662b0879f166e67e9ed4cffec3a4
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:56:56 +0000

    truth spot corner wherever behind

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("Hello, world!")
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
