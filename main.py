92742c18c32e0d1cd469a7404110429c38debeb8
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:24:41 +0000

    exactly Oo praise of attempt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  def calculate_area_rectangle(l, w):
+        return l * w
