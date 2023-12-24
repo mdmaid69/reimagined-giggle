b31be20f145b8b35155066bc89a28ef861848f87
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:44:11 +0000

    contrast uncertain post solve space

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
