b8db14451f1e4ac542c19325e968336d5e5e84aa
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:33:37 +0000

    pipe unhappy steer that goodbye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import numpy as np
   def calculate_standard_deviation(arr):
         return np.std(arr)
-import random
-print(random.randint(0, 100))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
