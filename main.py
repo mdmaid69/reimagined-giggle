26bd24b6b9a699985d4e28c080b8bc68eb549e98
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:34:45 +0000

    well unit off true common

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def convert_to_octal(n):
+        return oct(n)
