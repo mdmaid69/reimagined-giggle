bb925b775649399de2bd073644e571dcd9639fd2
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:15:56 +0000

    lean accidental breakin attach protest

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_pressure(force, area):
+        return force / area
+def calculate_acceleration(speed, time):
+        return speed / time
