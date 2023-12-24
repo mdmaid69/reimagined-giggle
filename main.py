7a92bf941bb1952a5edeb2c8f59080dd08a355e8
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:36:01 +0000

    temperature usually could stare in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
 import re
-print(re.match("h.*o", "hello world"))
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
