3134926140f824be8042406b5dcd5605e04da34d
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:55:30 +0000

    fight hang life patience worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
