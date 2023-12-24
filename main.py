89987ae01fb17c3214dcec4633b5ecfa43c85517
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:54:56 +0000

    only correction Qq action fierce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import platform
-def get_python_version():
-        return platform.python_version()
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
