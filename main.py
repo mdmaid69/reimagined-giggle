ad29b9956eac75d1978f832397bb0f05712b48e9
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:10:27 +0000

    hurry excuse path suffer hungry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
