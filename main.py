14c8e6e9029fd565210501a60134ecc35e31faf0
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:00:33 +0000

    advice exercise future person produce

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_as_bool(array):
+        return bool(array)
