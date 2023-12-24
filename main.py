a63a671eeaedda1c7d85cb873ba7c0cc339a2c8b
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:18:29 +0000

    silent mass draw run bottom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
