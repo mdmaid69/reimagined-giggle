84ecc964596c1d16974b3d3652da825729804d2e
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:25:33 +0000

    priority deep that bank criticism

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
