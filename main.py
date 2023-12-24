c5528c412808b3723c791e3b580ca6fd7b094bc7
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:04:52 +0000

    pressure situation similar marry shock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+i = 0
+while i < 5:
+        print(i)
+        i += 1
