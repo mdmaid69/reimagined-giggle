af095cff7684abc20e443d142ac93be1e0ddb16f
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:25:23 +0000

    book ground you measure hot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_acceleration(speed, time):
+        return speed / time
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
