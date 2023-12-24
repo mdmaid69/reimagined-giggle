bd0b4d7b6b643f8cc1b57d5229a1bbe45c6ef899
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:48:03 +0000

    wall dance interesting naturally still

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
