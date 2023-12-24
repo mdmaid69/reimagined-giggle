b3699872d4924c5d76e5b66a9caed2e198c7f843
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:00:23 +0000

    mean small draw provide refer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
