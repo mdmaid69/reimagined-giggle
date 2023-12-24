01c7089ba155230eeb1285adee6761e81cd30ad6
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:25:36 +0000

    guide study tongue several bottom

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_power(work, time):
+        return work / time
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
