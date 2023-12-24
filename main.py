fa79f2763fd26a4f9c59cf888cfe23f7ffb13b6d
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:53:42 +0000

    tap say project high make

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
