5ac6e63a98b8edcfbd9a8d7a3b5b70c8977b77f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:02:10 +0000

    occasional nurse foot bank address

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
