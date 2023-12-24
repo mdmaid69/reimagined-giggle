ede3589b3c80306baf4c34d493461262de6cfbc7
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:24:11 +0000

    exhibition doubt table steer her

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def count_words(sentence):
+        return len(sentence.split())
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
