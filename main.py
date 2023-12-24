2fd7019250c3966fa449b4e280fc80e82c04fd45
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:38:30 +0000

    love bring fact influence stone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
