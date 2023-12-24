67d4c48aaea635c28279a359fecf6040bb1e807e
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:23:40 +0000

    scene cheque ability avoid sort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import platform
+def get_python_version():
+        return platform.python_version()
