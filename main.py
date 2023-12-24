e153b1b2f10acf08d83c9a7b8bdd0f361d109556
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:37:36 +0000

    common convincing swim home owe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
 import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def convert_to_binary(n):
-        return bin(n)
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
