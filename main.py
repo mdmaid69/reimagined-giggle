283a9df9d909f3ed318f58efdcd7df04d6f20a4f
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:46:15 +0000

    too politics runaway extra cinema

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
