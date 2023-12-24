de626d4230a0a7d42aafdbf0fb7a23fd93e6d737
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:36:24 +0000

    progress stir house attention track

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
