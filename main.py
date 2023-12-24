3e3532b685989b4c78089e1a240e258d8d3b4e60
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:38:56 +0000

    owe attach ahead wear course

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
