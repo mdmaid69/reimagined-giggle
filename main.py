483e1d416e139c174ea813a2c494cdd379720e45
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:51:29 +0000

    search choose stupid full silent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
