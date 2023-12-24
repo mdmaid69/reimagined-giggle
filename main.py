d0d7e0e3421070c34d50ad4ac362ce2e987587ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:40:23 +0000

    bar quiet thumb used different

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_acceleration(speed, time):
+        return speed / time
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
