65396ab5a6dbd91c37a2c358bde173852c93a1e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:46:47 +0000

    anxious store lock include brain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
