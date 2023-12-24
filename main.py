24386840ebffbc46b38b0258f7fc10383a19a8de
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:56:38 +0000

    sink control whether fair wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_as_tuple(array):
+        return tuple(array)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
