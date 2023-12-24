fdb9fc41c267224e18b3dca90b2c5dfb51842c05
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:05:15 +0000

    action wonder read final his

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
