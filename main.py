b6e3259de15622284b166f62b2049694f3d035ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:08:32 +0000

    load combination rescue flow pretty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
