6eaeb32a259084637b98edfb995b836f79ac4abc
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:51:32 +0000

    congratulate muscle wire earn impatient

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
