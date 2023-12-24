59150827316ae2ba2cc9f009e960935597da05d5
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:37:57 +0000

    sight schedule only but practice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
