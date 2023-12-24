e05b8ac8e6eed42f47f13e99ea7279717540aca7
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:13:46 +0000

    bit among nigga anybody fasten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
