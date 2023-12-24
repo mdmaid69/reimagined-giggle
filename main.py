045869a6f2edd40c3ecee655e6034cba6e0f7d13
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:14:17 +0000

    pretty suck close week level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_area_rectangle(l, w):
-        return l * w
+import sys
+print(sys.version)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
