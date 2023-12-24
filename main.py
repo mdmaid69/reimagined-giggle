3669cf363e6b30bf6f6abbd6b2bcb5b6cf7e32fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:18:32 +0000

    brother brother advise further completely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
