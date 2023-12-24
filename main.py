17e2be865eeea831b299d02d4b7fb4f730f34491
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:56:42 +0000

    direct tip relation whoever comein

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
