a54c16232be0265c82314633ccb6c99f426e2791
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:08:04 +0000

    holdup find speak comein rid

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import datetime
+def get_current_date():
+        return datetime.date.today()
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
