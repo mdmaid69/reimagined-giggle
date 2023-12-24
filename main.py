ca9f89b303ab9b36c934cc33684b1479dcc5ef98
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:52:26 +0000

    frequent job smell provide fork

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
