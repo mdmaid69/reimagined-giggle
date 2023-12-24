71efe9703d0eadb69d4b448ec03fc2ba3fdfb1ea
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:09:07 +0000

    touch recognize food catch unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
