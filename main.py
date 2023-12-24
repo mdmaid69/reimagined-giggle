a7ab224e6cfaf3aa970089d4093c074427155d06
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:45:53 +0000

    long should rubbish rank much

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import tensorflow as tf
+print(tf.__version__)
