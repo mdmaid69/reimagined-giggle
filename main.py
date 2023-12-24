438c93e9803c740ac1537933c009d90090e52266
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:24:46 +0000

    lookback(on) please gooff geton brief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
