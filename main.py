09c5edfd4d371cba017ccb2568cbb30009458930
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:02:42 +0000

    table here stone keepaway explanation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import collections
-def create_user_string():
-        return collections.UserString()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
