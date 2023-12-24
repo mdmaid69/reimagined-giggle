4782727a8ee60b72dc0e9d686ad5800a996795ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:39:36 +0000

    juice giveaway shut type check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
