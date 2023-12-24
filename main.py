2c238d571baa17e8d8467cf8ea5eec390b72bf40
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:52:59 +0000

    prepare now shop trick home

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
