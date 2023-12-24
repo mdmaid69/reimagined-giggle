52ca7156b609b50dc9f074f6235cb3beb027632e
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:35:07 +0000

    572enter important future unite cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
