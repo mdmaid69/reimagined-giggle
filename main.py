45d1067a29c9673850ba8a03496b215649bb02f3
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:41:47 +0000

    blame matter advice list reaction

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_user_string():
-        return collections.UserString()
+def calculate_power(work, time):
+        return work / time
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
