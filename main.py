dca42a7b7c1d2753964e397d4ca592d1ce0f4e2e
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:35:39 +0000

    shoulder rubbish scare attempt distant

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
