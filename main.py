f3b41ef08e716f98d3f8b3d2ffbffd58d26af90a
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:25:44 +0000

    voice pump tired less regard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
