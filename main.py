06c1dda81f9b6a40866c5c615cd7e99fdd935569
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:38:22 +0000

    lead correction place Ll cheat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import sys
-def print_python_version():
-        return sys.version
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
