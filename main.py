b4a4acadb508aa3136535760695f6615068b4ef5
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:24:26 +0000

    wheel male real grateful giveout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
