135abf020b39fd31263fca2782041f005bf10785
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:46:05 +0000

    whistle murder string present drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
