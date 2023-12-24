4926aca8524cd27ed402c80adedd6fcb25eaeb61
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:46:42 +0000

    bar pay best past rather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_base_name(path):
+        return os.path.basename(path)
