ae9c35b6deba21972dcbe943bafaa2bda0a38d66
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:38:00 +0000

    worry Ww bath half brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
