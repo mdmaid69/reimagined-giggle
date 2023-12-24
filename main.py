7eefa845d329fa3d38ceeff6b6377aa3e0200cde
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:50:10 +0000

    organization before shoulder boring bringforward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
