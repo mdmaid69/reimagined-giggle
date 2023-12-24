e86966e84f6abde031aa7f222c5b7503db485ee0
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:45:55 +0000

    knowledge trial taste him contact

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def is_palindrome(s):
+        return s == s[::-1]
