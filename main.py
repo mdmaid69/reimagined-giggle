b78659e9a543deedc9a5e5141dbb1d31952f18bb
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:49:13 +0000

    giveup think fix funny her

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
