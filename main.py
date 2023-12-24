768292b99aa918e8be19016c588352b18e9f4466
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:49:02 +0000

    skin deliberate straight old tap

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
