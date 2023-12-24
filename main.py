d78def82abea71ef6a21efcc51b8cf51d76f989d
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:04:58 +0000

    breakdown temperature move tense own

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
