52db5430f0b10fb46f5038872832114abd572315
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:41:55 +0000

    worried precise female practical towards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
