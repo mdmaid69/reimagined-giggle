ac9f74dc31e36e1ea8cd1009e75b642bfaae0028
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:34:35 +0000

    they get exit official to

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from flask import Flask
-app = Flask(__name__)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+import threading
 
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
