92cf42512a9d2f8397d051466c228397a16137b4
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:22:10 +0000

    talkinto possibly set smile house

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_str(array):
-        return str(array)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
