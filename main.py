dbfb54d471ace0dc00ee9ac76e6ee8754adca926
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:35:45 +0000

    average keepout arrange aim choose

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import logging
-def log_message(message):
-        logging.info(message)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def reverse_string(s):
+        return s[::-1]
