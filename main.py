14fd04ddcbd909dcf0cfecf929a4baca76d00cc6
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:08:26 +0000

    turndown foot wait splash uncomfortable

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
