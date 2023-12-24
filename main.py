40a37c7cf296b2244e9992fc32f5f76d5d31730a
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:10:06 +0000

    opposite spare speech spray better

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import random
+print(random.randint(0, 100))
