4529ecc674fe5bf86c1f72c8bb73be9f3403bf5f
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:50:23 +0000

    region create act cause people

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+print([x**2 for x in range(10)])
+import collections
+def create_user_dict():
+        return collections.UserDict()
