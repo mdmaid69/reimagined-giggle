4461545642fccfe6a4543f7756b7382fbe0cce80
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:03:55 +0000

    job television touch trip pour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
