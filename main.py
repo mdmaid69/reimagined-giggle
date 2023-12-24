099f2a10a44cdcdad78ccffced14f36e418971b0
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:36:29 +0000

    dance relation refer never skirt

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
