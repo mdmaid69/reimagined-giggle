ffaafe60cd286a8ef28133a50f1e498cc8a1d4f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:07:08 +0000

    tired art rely split pencil

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
