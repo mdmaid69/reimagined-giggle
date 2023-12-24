fc20e1f4dbe6bd48612e52d7183e7a426a2babe7
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:14:23 +0000

    petrol recognize turnaway freedom pipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
