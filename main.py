012dc06b28d04dd135588cd6284c4ae76810c9d7
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:04:43 +0000

    discussion face clean release impression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
