22b44f210002d47b99143adb4c1e826da0f23323
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:35:43 +0000

    just interesting previous goback(to) command

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def cube_number(x):
-        return x**3
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import logging
+def log_message(message):
+        logging.info(message)
