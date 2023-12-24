2bb94c9d95877f17f5271b2e387db166064c5911
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:21:25 +0000

    stay how round stupid takeoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
