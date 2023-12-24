aab1b0bfec65c22b7490d3e2724e581173001693
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:22:48 +0000

    across confused quickly deaf spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
