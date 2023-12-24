5a016d09d4d18b4157b10ff3b9af3969ac4a6d8d
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:23:06 +0000

    callup sense receive permission weekend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
