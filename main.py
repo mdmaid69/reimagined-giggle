006cce4e41a09ce4724746fe69f9bc5842f713f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:41:05 +0000

    total shelter work undo direct

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ with open("some.csv", "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerow(["spam", "eggs", "bacon", "ham"])
         writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
