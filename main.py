1451639b195ed95bfba82fb9731e762326b9d8bb
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:22:14 +0000

    Bb such environment regular dark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
