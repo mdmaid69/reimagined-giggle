d502eea6f5d213d40086769a729b1664cff1107b
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:10:36 +0000

    mark flow happy believe start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
