17b5b4ec9286a20352dfe57c2e2582db44838984
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:54:49 +0000

    split tell plan hole emergency

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
