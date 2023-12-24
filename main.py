34128a2dd2540fa3b68b38ac230a592d7dbc640c
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:45:40 +0000

    sting better physical damp pretty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def convert_to_hex(n):
+        return hex(n)
