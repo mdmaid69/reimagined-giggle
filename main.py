e3fd17153ec768b3bae377116562cd38c21af339
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:05:19 +0000

    turninto handle matter no date

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def remove_from_array(array, item):
+        array.remove(item)
