430d85b483d7df6fb086bb30dce07610838ec36d
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:22:12 +0000

    practical possibly throw greeting constantly

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+x = 10
+y = 20
+print("Sum:", x + y)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
