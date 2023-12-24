a4814386f5e918fd8e6f142a0006614fcccbac99
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:10:34 +0000

    eye your fault increase sink

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
