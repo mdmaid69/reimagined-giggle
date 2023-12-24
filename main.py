d721a6dec077ad2b77058b0729ded4e43c66ca80
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:44:11 +0000

    under weekend do burst record

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def reverse_array(array):
+        array.reverse()
