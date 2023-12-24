6f61280a50d2be2d880ec50cc4f0e6fefa53d26a
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:16:30 +0000

    market lesson edge expression mystery

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
