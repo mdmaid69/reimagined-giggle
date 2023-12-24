23bd08f4a619fd795834f1e3995fad0efb9535c3
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:44:44 +0000

    us square yet foreign disturb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def is_palindrome(s):
+        return s == s[::-1]
