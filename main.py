a0ee789c43f58f02f75de232a8c2b24f7a1f7855
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:42:45 +0000

    damage door hate decision bright

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
