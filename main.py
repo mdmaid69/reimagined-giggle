17349ea387b10cea00bb3ff961428cb3973b0d3a
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:43:59 +0000

    arrival unfair proud noone bang

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
 import array
 def get_array_from_string(string, typecode):
         a = array.array(typecode)
