1453bfc5441a3c5fc969dd0b8b8cbbd90323d69e
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:56:56 +0000

    thick perform black breakdown emphasize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def find_max(numbers):
+        return max(numbers)
