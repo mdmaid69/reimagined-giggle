7f6d5cf18e3061b0a4cf0476aa8633b79ebbd6b7
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:40:12 +0000

    area neither trial keepaway might

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import math
+def calculate_floor(x):
+        return math.floor(x)
