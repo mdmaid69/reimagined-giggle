be36758c8c3b7d070a325c7f41e25318bb705a5d
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:31:31 +0000

    someone depend tidy crash arm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
