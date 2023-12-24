24fa47e9838c8ec9d5b1491afbe646f52e69d50b
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:18:49 +0000

    neat draw pressure confident conversation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
