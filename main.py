7ee69368a637a251e2ba7dff19b33cc1f3539b5b
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:37:45 +0000

    opportunity point name anybody build

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
