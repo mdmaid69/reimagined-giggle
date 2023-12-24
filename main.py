8c17df3c9c2b7774d43c3f02641739a62b208447
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:23:23 +0000

    run balance pupil centre tread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_as_complex(array):
+        return complex(array[0])
