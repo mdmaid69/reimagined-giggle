8fadc3cb3cebf347eb0317917319c7f943eab605
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:00:48 +0000

    bread callat money model official

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def sort_list(lst):
-        return sorted(lst)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
