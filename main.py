6e17203dc876de433c5c3f27376861314bad6655
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:50:19 +0000

    market Ss attractive trick comeback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
