349c4703b4b318445759ce86849804379e5767cb
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:06:15 +0000

    beneath cruel delicate content fire

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import sys
+def exit_program():
+        sys.exit()
