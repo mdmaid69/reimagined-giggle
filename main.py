8437dd232588f68d711f214a2a6fbb151de60648
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:46:21 +0000

    alone gooff find battery deaf

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def convert_to_octal(n):
+        return oct(n)
