e55982d4d9648d9ddeb5fa5af901d152a2ca62bb
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:28:41 +0000

    each himself judge hit final

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
