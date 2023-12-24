70466b31980b38b6469f29f8bf2bb6a6a2ab83ff
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:48:54 +0000

    complete light replace used inform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import datetime
+def get_current_date():
+        return datetime.date.today()
