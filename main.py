7bcec51774bae4b730c00be508c35af90af0e952
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:48:01 +0000

    nonsense drive card copy reason

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def get_array_typecode(array):
-        return array.typecode
+import re
+print(re.match("h.*o", "hello world"))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
