1ee03e111bbf0877fb14b98d55ce2262f8ad5db2
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:54:43 +0000

    sensible scare getback practice handle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+for i in range(10): print(i)
