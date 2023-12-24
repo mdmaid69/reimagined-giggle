ead856a8fe36d72e13587654809b518b249d3879
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:55:43 +0000

    raise corner powerful speech top

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_base_name(path):
+        return os.path.basename(path)
