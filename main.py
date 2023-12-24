ca5f0cf50359116a3f23e67ea5d7194be9d2cc70
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:55:28 +0000

    contain read beside convinced match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
