3afbe0dbe14d572f3c34b49ee7dedfe042222ac7
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:41:28 +0000

    branch depend environment surprise open

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-print(os.getcwd())
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
