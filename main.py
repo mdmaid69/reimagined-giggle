96ba36280234455fe49a50891abaf538718b78e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:30:34 +0000

    may listen crisis tape whoever

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def append_to_array(array, item):
+        array.append(item)
