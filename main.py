ad63e0706b566742f72ce8cdeef05d5f384a9419
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:01:59 +0000

    wear house house wide grand

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def convert_to_hex(n):
+        return hex(n)
+import os
+print(os.getcwd())
