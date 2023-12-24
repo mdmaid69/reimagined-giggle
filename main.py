79e06fc0d1e0d7565ce812167a6418fa7d77042b
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:07:35 +0000

    increase test myself table drawing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
