ffb2e859fddf49cf6e24cf086cfd589f248663ee
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:18:01 +0000

    cheat example Ll runaway pity

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
