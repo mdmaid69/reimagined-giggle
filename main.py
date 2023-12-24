8a20ff29dc9d0b2c8bafbca0e686c65dee49456f
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:54:09 +0000

    project leg name pair concerning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
