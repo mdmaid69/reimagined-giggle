fd3a67fca3bfe0c23046591a05cd1dd1b9762e03
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:56:40 +0000

    getup classroom proper smile Tt

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import sys
+def exit_program():
+        sys.exit()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
