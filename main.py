b9bee4bb607154a480a1ae110dbeff15b55b6d11
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:01:10 +0000

    good long along wipe common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def cube_number(x):
+        return x**3
