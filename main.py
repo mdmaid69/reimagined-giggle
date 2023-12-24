bcc7baa3ad8fa606f2c36ba26c1e72bacd4c29d5
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:42:46 +0000

    suck thing distant save beginning

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
