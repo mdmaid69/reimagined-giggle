ea7c90aad4d410ad0e724ca1e0c0874e7f5ac256
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:36:17 +0000

    expect half amount rush tone

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import math
+def calculate_floor(x):
+        return math.floor(x)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
