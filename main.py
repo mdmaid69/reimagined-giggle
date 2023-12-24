43fcbf7ed2f61df2f3c9901230b05ebbc40a51d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:34:06 +0000

    responsibility wide married cost fashion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
