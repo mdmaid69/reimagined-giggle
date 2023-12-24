af1ba393bda2ac98650cd566f42e335fd5098f2f
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:46:29 +0000

    speech without stir contain sign

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
