bcca26be7988d4b16f7fdc8e090ee7ba6ce8d079
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:26:11 +0000

    scale draw file true praise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
