d52465228ad93334e3469a2f5a71d59533df57e5
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:08:48 +0000

    message foreign lonely stone 570enjoyable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
