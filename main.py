5daa28f872aede6ba665346213e5b7d5b6cf5a39
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:07:44 +0000

    gradually downstairs tradition gentle getback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
