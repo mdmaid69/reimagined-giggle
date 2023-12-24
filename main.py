3acd5598b6fe2b518b172ab2e291d1005e84b2b3
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:30:53 +0000

    question promise crash meal apart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
