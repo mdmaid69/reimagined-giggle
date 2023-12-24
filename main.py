29f7332b338c5039583b825b076640189ad2180b
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:39:03 +0000

    national clear base picture design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import array
+def convert_array_to_list(array):
+        return array.tolist()
