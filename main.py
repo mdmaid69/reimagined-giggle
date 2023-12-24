bd685899b8334d1bc5d55c3088376c70eac5103a
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:53:49 +0000

    572enter excuse trap flat combine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
