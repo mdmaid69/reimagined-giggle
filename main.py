ae8e175b6ff8401addef8af0aaf4603b4f29bd74
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:25:56 +0000

    well act vote complete outside

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
