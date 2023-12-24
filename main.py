2189eac2f9acce7d0b3569e02fe11ee86597f5bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:49:46 +0000

    stone wide affect remember impressed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_acceleration(speed, time):
-        return speed / time
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
