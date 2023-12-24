a61ad460dcb58dbb0dc3a169153aa222619f2483
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:15:25 +0000

    go sticky development high suffer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
