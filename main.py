9cbb32bec7386c766a9aaa8ec056eb5cbd2efaea
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:18:06 +0000

    sink conversation giveup offer public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
