9fce059edd16f756176f20aea58729a3150b279b
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:05:48 +0000

    square anyone he official doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_as_repr(array):
+        return repr(array)
