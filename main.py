bbc96022d901ef8e5d3f83772f71e35aa0f5a58f
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:32:06 +0000

    grade broken meaning support runaway

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
