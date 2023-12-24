60d0a1b638cf54bf18c79b04e2ed0150d6841453
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:04:48 +0000

    silent share storm guess foreign

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_power(work, time):
+        return work / time
