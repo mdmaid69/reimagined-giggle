c8bdfc584e1e9fa495761cbbe2db9dbab3e54f65
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:27:05 +0000

    work total image bang ignore

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
