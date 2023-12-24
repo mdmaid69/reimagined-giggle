e73d6d3657e85142da808fae019a406791efa287
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:35:19 +0000

    load kind have attach sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
