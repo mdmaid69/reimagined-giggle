33e553ec18db7b11d4258128c010a96fc28b7ea8
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:06:34 +0000

    place nurse science hand in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import platform
+def get_python_version():
+        return platform.python_version()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
