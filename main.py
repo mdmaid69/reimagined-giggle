8c3f7dfbeef14d4a1c516f582d565f5ddc43c4b7
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:52:01 +0000

    rich set uniform instrument both

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
