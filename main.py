ecdde16e1ed9f07050472df51a016dde61db2281
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:33:04 +0000

    delight news embarrassed search concentration

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-print([x**2 for x in range(10)])
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
