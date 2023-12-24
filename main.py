d8e751602b8e69ece81428bb6b13f264896bb0ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:30:15 +0000

    learn play both container right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
