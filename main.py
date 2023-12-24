fcbd5b378d75e4d98a1b1bfee98121ff37613a38
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:46:36 +0000

    lose noisy involve mainly government

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
