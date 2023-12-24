f7c3e8422b7be51da5289838009a57056666ff2a
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:14:34 +0000

    mistake war hungry mainly tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
