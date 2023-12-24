8f5d294776e69227c42366cdefb488527708e534
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:35:11 +0000

    fat task shallow arrangement normal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
