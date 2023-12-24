defc384380042cc23641587f041f8b5f3f4b5edb
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:51:42 +0000

    private special satisfied argue be

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
