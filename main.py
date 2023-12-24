40864b7b6eb4a514be1ab4f7b5999fd25f167663
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:39:27 +0000

    whichever into market serious main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
