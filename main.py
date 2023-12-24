5b048693f0bef08939bd21d2bc2e228997e9b165
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:30:57 +0000

    frame glad suggestion content example

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
