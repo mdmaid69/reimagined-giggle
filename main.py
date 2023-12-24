3417435f1be6119c5d4aa8db0df28662effb41a7
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:40:35 +0000

    quickly Tt price smooth uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
