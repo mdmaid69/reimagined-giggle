6b0c45b5ede0086733600b111321951b666d918d
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:22:15 +0000

    feed turnover worry strange right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def cube_number(x):
+        return x**3
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
