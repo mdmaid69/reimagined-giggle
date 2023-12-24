4fd681c49fbdd1cb613cf1b52bd87f7be93405bd
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:20:13 +0000

    pleased roof report state moment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def find_min(numbers):
+        return min(numbers)
