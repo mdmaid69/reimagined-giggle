9fed365e4e2a1685368e5cf893cb2e8f8919a953
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:07:45 +0000

    protest manage mad wave anyone

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
