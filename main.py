ed87f1f4c85494bdde178725319810cd494ea7a2
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:55:36 +0000

    rule fierce around nonsense whichever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
