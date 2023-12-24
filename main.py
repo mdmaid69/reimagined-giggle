cc675c0c7a04af26b68cce14bc17017f396f6f6a
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:17:16 +0000

    sad tragedy why seat target

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_pressure(force, area):
+        return force / area
