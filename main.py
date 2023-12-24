da65f65511702292f63e3026fc954c73281285bb
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:55:06 +0000

    thirsty best comedown instrument rid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
