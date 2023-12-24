9ec4804f1576ec1aeb3a1585016499c1ede91216
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:18:35 +0000

    material comein dig 570enjoyable if

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 def calculate_density(mass, volume):
         return mass / volume
