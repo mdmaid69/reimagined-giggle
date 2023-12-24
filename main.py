4cad6f4196dc6fb7dc7ef6b1c8d3b28983ff29f3
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:49:49 +0000

    unite soon wrong wonder town

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
