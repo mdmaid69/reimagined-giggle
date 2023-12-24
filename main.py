4f281623e5cd39a7775013f10b45ff51ebe909f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:10:39 +0000

    get possible miss listen maximum

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_user_list():
-        return collections.UserList()
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
