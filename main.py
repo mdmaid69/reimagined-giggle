974abac0a36e7ae0f4346e27b509736585c86d83
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:41:56 +0000

    still black busy medium convincing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def remove_duplicates(lst):
+        return list(set(lst))
