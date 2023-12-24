af4788cf5586872a9c704006b3392f75e103c701
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:15:29 +0000

    tear mainly burn late soon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
