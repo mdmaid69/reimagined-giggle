36f8b6dc0ec991f07ff83a90fd1871f9dab33384
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:19:52 +0000

    slice accidental test pen skill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
