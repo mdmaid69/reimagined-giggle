d54f7372e45a2f40c78fea62271c9ed0b1cbc87a
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:08:35 +0000

    thinkthrough comeover challenge eye remain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
