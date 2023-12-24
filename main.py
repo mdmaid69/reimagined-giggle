476ea5daf15a1bc08b9ad679b7f2452285b31105
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:12:00 +0000

    he shall recently guard pot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
