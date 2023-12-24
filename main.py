16083612656e0bc826ebd3cde502b6bbc2b7a440
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:18:48 +0000

    comeoff air giveout meal emergency

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
