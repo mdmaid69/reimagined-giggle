e9cec773e2461093bf86480c75bada5e313bdda2
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:44:20 +0000

    copy recently profit accommodation rest

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
