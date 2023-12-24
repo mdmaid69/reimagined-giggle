837b97bc3c414869424abaa945a5ed5a17e921db
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:50:53 +0000

    wind true variety bye public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
