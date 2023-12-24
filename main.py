d5cfb3e9b0d1c729fb4206bbb0121a64ce0af154
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:44:37 +0000

    half tense conduct lift children

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def clear_array(array):
+        array *= 0
