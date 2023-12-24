2daf1b7e3faa7d602502091e9b9298b77db43353
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:48:24 +0000

    plan support chase cost comment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
