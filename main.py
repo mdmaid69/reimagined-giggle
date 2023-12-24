15dd23c5f432418880afe1996f03569781c1ac1a
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:59:37 +0000

    house murder bring climb middle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_itemsize(array):
+        return array.itemsize
