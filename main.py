9015fd48a4715b5f3ebff9c85d21cb619efe12a4
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:45:41 +0000

    between eager experiment turnover complicated

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
