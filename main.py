70f7aa75d041ac9f409086a09251d91f2c9a75f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:15:51 +0000

    worse look here style issue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
