c265be8ae402a80932fe10752a518f65ba4ce478
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:30:57 +0000

    number wire 571enough ability hardly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def square_number(x):
+        return x**2
