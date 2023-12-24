be1dcf45cd62f4d2762e10f33e41f1b628c69a90
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:22:48 +0000

    provide 564encourage run score drop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
