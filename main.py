ae8dc668026659c3b6340cb930c286d2bb9ba192
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:57:47 +0000

    trade workon habit cum mean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import array
+def convert_array_to_list(array):
+        return array.tolist()
