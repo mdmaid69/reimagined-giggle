a020105483bb8bc89ecbb34ca91ca5f51c9affba
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:30:26 +0000

    charge private interrupt physical previous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
