e9647ca2f06458503a011bf374db125565286cd6
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:04:08 +0000

    come bring achieve mistake table

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
 import array
-def get_array_as_set(array):
-        return set(array)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def get_array_as_bytearray(array):
+        return bytearray(array)
