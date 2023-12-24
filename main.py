ae4fda0a64f55ba2b6c6f268214d765b6c1e1741
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:04:54 +0000

    closed stage string noone smart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def convert_array_to_list(array):
+        return array.tolist()
