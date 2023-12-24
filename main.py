48984bdda1ccd3ad3f8e49dad920c43476fae819
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:31:59 +0000

    pride hope line exchange whom

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
