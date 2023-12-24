b0ff808c079965ee2e3091793668880cb526715d
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:56:40 +0000

    weigh by last whether undo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def get_array_slice(array, i, j):
+        return array[i:j]
