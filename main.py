06c1a08b0b47894d918210b227e758cdcafb2046
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:13:13 +0000

    heart determined position special sport

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
