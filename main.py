09eeac4ef74d344130e878a932a821d3eb77ec67
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:06:33 +0000

    acceptable range direct estimate section

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
