fd235ebd56376bc198a7dad73ef93fb460486bd5
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:29:36 +0000

    little giveout occur send earn

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def sort_list(lst):
+        return sorted(lst)
