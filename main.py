f90653f90798506b971215782090046cf7ac3f6d
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:18:00 +0000

    bored many equal handle return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
