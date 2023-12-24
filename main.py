5c44cb9d9c3c1a1e90b13d6635a910da04d8024b
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:35:47 +0000

    match basically square joke food

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import math
+def calculate_sine(x):
+        return math.sin(x)
