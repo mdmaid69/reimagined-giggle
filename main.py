7444853a81e235606e764e28b00bc3e594b00f9e
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:00:22 +0000

    joke maintain wipe stay sure

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_speed(distance, time):
+        return distance / time
