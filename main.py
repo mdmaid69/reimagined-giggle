d94728d68c87b47cc3b50382e0a086ce22be044c
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:59:16 +0000

    marry whistle trust brush must

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def extend_array(array, iterable):
+        array.extend(iterable)
