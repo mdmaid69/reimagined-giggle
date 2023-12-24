2d82c6117d84ff38d4c3c9c1b55171d2d723f0e4
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:54:32 +0000

    rate popular violent ball love

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
