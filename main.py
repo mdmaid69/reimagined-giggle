dd624d48a0d36786e1367999431f61b62a8615aa
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:10:35 +0000

    history smell sale off compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
