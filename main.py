8430103f068d95c4c110b44d8b79010fb52853de
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:07:55 +0000

    573entertainment possibility disappear true getaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
 import array
-def get_array_typecode(array):
-        return array.typecode
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def get_array_as_int(array):
+        return int(array[0])
