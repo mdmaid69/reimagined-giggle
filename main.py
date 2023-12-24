49a57a47c8aa5c32f69386bdb488daeadc275b96
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:50:16 +0000

    piece control lock freedom their

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def calculate_area_circle(r):
+        return 3.14 * r**2
