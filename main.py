80eb5fa7c642300cfb7d870ac9b3445f399ecbed
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:08:56 +0000

    level language regional profit smoke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
