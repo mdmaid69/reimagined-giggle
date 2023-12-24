9c4866dca82d39d9b0be08e6a33ab174abffb16f
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:28:33 +0000

    effort noone breath uponon pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
