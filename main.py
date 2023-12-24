fca41c1bdfbafccd2dba1c5f3dbc1dfcf0c6522a
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:26:46 +0000

    do tread satisfied final callback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
