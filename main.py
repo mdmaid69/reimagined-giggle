4006edaefbf5989a3ac1338c4caf92b520fcc1ff
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:40:52 +0000

    justice hole week die protection

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
