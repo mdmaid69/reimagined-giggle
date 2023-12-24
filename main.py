2618e0468385d6595fbde38c8d4c1c878a410336
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:27:19 +0000

    hole corner switch ought cheat

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  def calculate_area_rectangle(l, w):
+        return l * w
