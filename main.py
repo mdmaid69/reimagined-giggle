46006114733c131c3d600edb22324899d9ccb666
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:14:07 +0000

    address who strength payment belief

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def get_array_as_list(array):
+        return list(array)
