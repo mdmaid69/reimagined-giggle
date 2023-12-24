378e205692f9603f3d89e60501ad09191dc8d63f
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:28:20 +0000

    private excellent acceptable backwards top

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-for i in range(10): print(i)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def count_characters(sentence):
+        return len(sentence)
