8778b59adcd3f45848d6605ef17a113d9d9cc52c
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:25:34 +0000

    cost broad instrument purpose praise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import math
-print(math.pi)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
