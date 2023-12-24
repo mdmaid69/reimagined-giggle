7dadb38d6ea50162ab9c63c26fdc5227b651c6b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:59:07 +0000

    whole love however escape morning

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def get_array_as_float(array):
+        return float(array[0])
