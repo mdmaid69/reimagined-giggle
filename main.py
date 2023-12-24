b790cf80f7ae561c13cb506ba30373cf5ced4e12
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:39:46 +0000

    marry bringforward tired bang be

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
