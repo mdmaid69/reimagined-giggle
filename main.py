d83b17b801effbbe4c29a7bbc11687d35c25dd36
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:55:32 +0000

    land shelter wash base pollution

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
