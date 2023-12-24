0097630e94dbad6279fe67977284f07b79a19653
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:39:05 +0000

    564encourage protest force miss pump

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
