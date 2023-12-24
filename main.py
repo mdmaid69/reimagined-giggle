059b6b9135dd792d1f853a011e351c44b1d07b28
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:08:38 +0000

    worth holiday blow introduce fill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
