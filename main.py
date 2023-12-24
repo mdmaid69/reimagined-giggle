70c5cb61a184e9571b13fe6125480cd2976a7c50
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:16:07 +0000

    best firm steep accommodation secret

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def subtract_numbers(x, y):
+        return x - y
