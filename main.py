0e32008f44328ebeb9b7ee4f8784fbef29d92d22
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:52:38 +0000

    safety attention another race 571enough

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
