ceda184bf8e64d9d05efd2927adb95904c6ad8b9
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:46:55 +0000

    mess guess preparation schedule possession

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
