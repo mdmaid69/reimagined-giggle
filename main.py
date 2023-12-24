34e02bdcbc39f55e65a586cfe647a504032a9301
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:29:28 +0000

    better motion drawing attitude music

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
