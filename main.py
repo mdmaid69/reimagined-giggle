2ad5c8366d200b2f832c8e1d05745cde0d9461d6
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:35:41 +0000

    effect pure reply who level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def cube_number(x):
+        return x**3
