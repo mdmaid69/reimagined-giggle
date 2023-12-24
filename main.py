2ad1188631fec69bf09c0617e8c7fecae0dcd70e
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:39:55 +0000

    underneath basic sea coffee part

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import re
+print(re.match("h.*o", "hello world"))
