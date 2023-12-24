a6652b4ffb00ebe1e2e7cc4e3f695204e0a1a39c
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:09:10 +0000

    cook since deep keepout runinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import collections
+def create_user_list():
+        return collections.UserList()
