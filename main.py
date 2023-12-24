b8a7ab71b7f69c3e49eecd51fbeffeb4def72f89
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:00:19 +0000

    unite clean callback obvious become

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def reverse_list(lst):
+        return lst[::-1]
