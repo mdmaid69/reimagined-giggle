8be592ff518da26613b9e353568ad5644d046386
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:48:42 +0000

    set relation temper software arrangement

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
