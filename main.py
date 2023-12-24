88110dfef30174929338f9cb01281b425a9f7bb1
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:38:29 +0000

    result age fat time aware

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def add_numbers(x, y):
-        return x + y
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
