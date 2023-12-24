1bcbe0299975e3c749fb227d0ffd4fce2b39843a
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:47:31 +0000

    stage combination clean label Ll

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def set_array_item(array, i, item):
+        array[i] = item
