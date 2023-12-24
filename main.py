4770feca3ef5f6e8908d6fcecd6dd664098ce92e
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:03:38 +0000

    content sticky mistake mark clear

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
