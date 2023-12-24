5c0503ecee49a35a5fc0c74c5482db90ff191a25
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:30:06 +0000

    fresh treat wave keep pause

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
