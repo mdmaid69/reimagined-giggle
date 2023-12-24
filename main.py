23bee1adef3591481d0cf63a796705fba26f0a81
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:51:28 +0000

    concerned none birth lay then

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
+for i in range(5):
+        print(i)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
