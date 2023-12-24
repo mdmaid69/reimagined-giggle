093dbc8db05a9c3acb68ba7705f96101dfd57c5d
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:37:53 +0000

    put comeback holdon only nervous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
