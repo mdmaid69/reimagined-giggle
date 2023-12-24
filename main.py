959af2648443bf00fd1668fca8827846d896e8a2
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:24:59 +0000

    colour cruel first woman cheek

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
