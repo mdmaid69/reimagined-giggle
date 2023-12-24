21ef73bea732732d3acf848dc6fb528ed0a5b147
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:24:50 +0000

    mix pride legal honest mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
