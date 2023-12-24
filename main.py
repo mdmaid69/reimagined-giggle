786f5428ef40715cda380b31b5019d2a0327034d
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:13:36 +0000

    limit get tidy similar calm

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_acceleration(speed, time):
-        return speed / time
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
