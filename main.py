05b64b991d6711894ba4a6d8b3f05613662df718
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:14:00 +0000

    572enter brain prepare fresh shopping

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import math
+def calculate_floor(x):
+        return math.floor(x)
