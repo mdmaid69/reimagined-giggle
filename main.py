252c35a328a244da0ccddb096af17b08c383ec55
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:43:18 +0000

    reach fault reliable lock tough

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
