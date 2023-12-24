ee1966001314735fdfd0679728c2252538a6830c
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:16:14 +0000

    improve steam persuade everywhere happy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
