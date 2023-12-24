2ba6c1d8bba2717dd2fbe48b962eed49374422df
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:45:19 +0000

    soft proud money uncertain point

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def sort_numbers(numbers):
+        return sorted(numbers)
