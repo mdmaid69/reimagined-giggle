73900aafc1d120a6f8a67d4c9ab6f2167fdf87c0
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:38:41 +0000

    half best into proof strong

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
