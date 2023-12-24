fe22637e3a7f38a0b57879bfac6d369b38d10e9e
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:55:49 +0000

    trouble swallow place strength radio

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
