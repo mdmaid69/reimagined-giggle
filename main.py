36252a0b9946dec9319ba8c0306e1646fbc187d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:31:13 +0000

    practice along fail cover somehow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
