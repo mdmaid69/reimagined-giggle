23a1493b2b4eeffcb7ac1c85ce2b6786014d3c97
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:30:19 +0000

    afford foreign range follow holdup

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def get_array_as_list(array):
-        return list(array)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
