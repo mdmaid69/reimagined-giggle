7f9af48035a78988a18184d3d5e13f7a1ffecf82
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:38:13 +0000

    bus make best achieve achieve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_density(mass, volume):
+        return mass / volume
+import os
+def change_working_directory(path):
+        os.chdir(path)
