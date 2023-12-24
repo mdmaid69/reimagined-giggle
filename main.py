a9a6fa5f5f7b731be4bc63a5d593c2663859b58b
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:04:41 +0000

    promise rush beyond hot surprising

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import collections
-def create_queue():
-        return collections.deque()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
