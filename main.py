718236fc299e2a38670bbb27b001e060c19ddda6
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:07:48 +0000

    plenty noone arrive smooth introduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
