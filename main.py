db2b8b1adfd5765b2d0e790dd4c6f704b48f92dc
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:59:27 +0000

    decide eager equal possibly sure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def clear_array(array):
-        array *= 0
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
