5fab8005c7ec0f06085e73b1e90adcaa730fd4db
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:57:25 +0000

    judge concentration represent while hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+i = 0
+while i < 5:
+        print(i)
+        i += 1
