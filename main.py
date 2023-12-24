63b58b3347b334e3bda8903dd8d03cd4e15988f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:52:41 +0000

    any visit before interest clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
