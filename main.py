ff067b73e06ab525f2e7b20021973bfa9022f6b1
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:23:12 +0000

    run usual unit up yours

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
