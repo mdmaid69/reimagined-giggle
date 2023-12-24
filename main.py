cfd0938ee1446531d61cd2231de63cf16bd3f504
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:56:00 +0000

    type of giveaway face disturbing

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
