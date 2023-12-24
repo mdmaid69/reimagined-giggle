2dcbe99cebcc256fe5dbb000ef86aadcab4d5606
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:42:26 +0000

    light through forgive freeze minute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
