e17f2f75f562479863f6c8a20f55fa1cf818e4cd
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:01:18 +0000

    somewhere get ride house describe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
