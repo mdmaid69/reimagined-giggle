987bc83ab7a6bf150fe0a1d90accd3cbc15ec95c
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:02:41 +0000

    young firm trip sort cheat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
