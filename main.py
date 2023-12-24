1ccc6365363d5e616ceede79325966c288586b6c
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:41:22 +0000

    degree correct cope choose always

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
