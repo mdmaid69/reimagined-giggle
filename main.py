067547824f9e8b9b775c73aface75a65a5e564b7
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:13:56 +0000

    error standard cross centre its

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
