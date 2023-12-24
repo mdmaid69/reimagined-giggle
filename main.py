c85697c6df42fa78f51be6a028c38def17d002c8
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:12:34 +0000

    normal shop there pass chief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_exponential(x):
+        return math.exp(x)
