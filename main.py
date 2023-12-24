f16acfde66800aa39a42b3154296d0d0dfe697f2
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:50:26 +0000

    day control truth them provide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
