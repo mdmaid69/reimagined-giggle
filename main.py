98807585fc599b3eefdfcb8f407e0dcdd76c6d31
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:42:26 +0000

    tough onto only name mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
