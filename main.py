b0814b70c754d689717b8974af4efe96486ae2ee
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:34:07 +0000

    deliver bath memory onto defeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import os
+def change_working_directory(path):
+        os.chdir(path)
