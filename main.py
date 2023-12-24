f262ec20220d5f8ca5235ac4345bf585a813cd0c
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:05:19 +0000

    kind rise apart tidy charge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
