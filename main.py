f43b868d9c426d91b8ca277f8a726c86aef02dcd
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:45:05 +0000

    cum include dull rather discussion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
