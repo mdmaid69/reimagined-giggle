84ade4da94f39344e1a4a6270b2335ffbdcb278c
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:37:04 +0000

    572enter sink strong impressed head

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_as_tuple(array):
+        return tuple(array)
