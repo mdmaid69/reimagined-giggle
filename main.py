24b4d3bd4f0cb910f250a98cf2d4bbde94846707
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:50:36 +0000

    runout floor entrance truth leave

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def find_unique_words(sentence):
+        return set(sentence.split())
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
