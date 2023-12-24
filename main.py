70a92c92f08d6ec430490db5604c6ad446a6cafb
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:11:04 +0000

    board peace shock entrance error

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
