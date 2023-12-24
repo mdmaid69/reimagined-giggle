dd82512740a9d8ff19642a2946041ec765ce87b0
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:50:36 +0000

    age really heat television crime

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
