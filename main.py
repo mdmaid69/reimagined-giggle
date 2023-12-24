ad1e6f5f3b825f2add3a9c8a4b1fe1b914baec56
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:10:01 +0000

    interruption Uu rough should send

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
