e0e3cee2292f2d591bdd4f8565a7f01da113ba8f
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:05:20 +0000

    brief total bar regularly branch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def count_elements(lst):
+        return len(lst)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
