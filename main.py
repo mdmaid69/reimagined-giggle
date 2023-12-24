bb819223868e8311a299856cbff8b23fa31bca1f
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:36:03 +0000

    sleep spring pot trust sink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
