7b9fa0b3281181425847456656a5a1aa89a23ae4
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:15:26 +0000

    satisfactory smell goup measure fail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import logging
+def log_message(message):
+        logging.info(message)
