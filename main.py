2e7d8b0c1d0c651abf714c5fb4989cfaeea8155b
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:03:22 +0000

    buy pretty back all character

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
