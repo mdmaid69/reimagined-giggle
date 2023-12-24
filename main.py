2faeafe6c1a4abc115032f1b329d65b52b24a0bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:12:54 +0000

    hope instead beyond advice negative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import sys
-  def get_python_version():
-        return sys.version
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
