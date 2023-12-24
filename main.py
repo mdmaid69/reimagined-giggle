190d16a8f716ba42914bcc321ec15565db095e6f
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:03:16 +0000

    and minor nature use anxious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_permissions(file_name):
+  def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
