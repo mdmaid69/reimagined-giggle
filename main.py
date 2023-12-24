a41a88100a3431504e8d3f45f11ceff5ffb9a4fa
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:15:45 +0000

    themselves tragedy motion quick another

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import time
+print(time.time())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
