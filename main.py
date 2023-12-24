be7eb74e0ac9bda17be6ff2f1379887b2c1df337
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:04:39 +0000

    health store material temperature trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def is_palindrome(s):
+        return s == s[::-1]
