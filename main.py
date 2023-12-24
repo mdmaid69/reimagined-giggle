d2ad9efa6de40f14ccd91e34b02b5da9c7d8b1c9
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:53:06 +0000

    do down reserve will last

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
