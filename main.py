74eb37ba4995079dd5d163341b65fa36caedd7f2
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:11:32 +0000

    bend strike difficulty deaf frightened

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
