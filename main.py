07517c718f90b8bd56c1254c59a49fcd59c3afd2
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:42:16 +0000

    run country condition eye behind

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+print("Hello, world!")
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
