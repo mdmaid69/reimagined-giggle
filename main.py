2c707ba1db247548b0bbfc82877a815a37b78f9c
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:07:29 +0000

    spill nose exhibition communicate paper

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_cosine(x):
+        return math.cos(x)
