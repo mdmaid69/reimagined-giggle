8b6161c008898d73c48d8f0a78a296eedf696659
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:03:25 +0000

    closed upset us skirt perhaps

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
