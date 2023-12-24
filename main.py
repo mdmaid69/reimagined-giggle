2b6d410dd18c2a197e517f8dcd0ee10a30e98d98
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:58:01 +0000

    full label gift smell short

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_ceiling(x):
+        return math.ceil(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
