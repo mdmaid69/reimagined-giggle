1f69247405fe3e459e9849608a4eb9ce2afb680e
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:01:30 +0000

    especially colour ordinary block terrible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
