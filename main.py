008d3ea8b95e41a459a04e083327393b2e3402c8
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:34:55 +0000

    moral recently pretend friendly top

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_max(numbers):
-        return max(numbers)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def clear_array(array):
+        array *= 0
