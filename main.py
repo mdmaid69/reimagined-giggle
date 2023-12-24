ca1bb0da8807419c04293b1a77ef5f8d77cf2aae
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:45:47 +0000

    opposite same special getout car

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def count_elements(lst):
-        return len(lst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_bool(array):
+        return bool(array)
