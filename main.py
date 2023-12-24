c438bd0bd82f67e087c13e05155e714cf9b1b922
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:14:47 +0000

    cost main all temper recently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def find_max(numbers):
+        return max(numbers)
