e0b646d6315df9844ee8df41251aebb5f4c11158
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:41:49 +0000

    promise fight claim matter certainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_length(array):
+        return len(array)
+def is_palindrome(s):
+        return s == s[::-1]
