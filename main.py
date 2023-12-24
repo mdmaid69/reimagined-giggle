4cf35ec289c75c3b3f87bada3801cb1693cf378a
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:40:31 +0000

    opponent cruel sore centimetre table

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
