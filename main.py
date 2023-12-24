38cb1d82a63ed2638cdf0cc5c5accb14f7e6bddd
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:40:05 +0000

    goon row connected build classroom

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
