227581d815493e1dbe8d4679dba917472f0b5291
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:59:31 +0000

    range story defend prove excited

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
