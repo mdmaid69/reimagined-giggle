c6cf48bd1efcbea5127ea358ab5ae79b322a007b
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:37:18 +0000

    agree please extremely pile rare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
