104d87964172dfa7d96ada875fbf2a15ba470a2c
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:59:32 +0000

    dead reach comment 569enjoy bath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
