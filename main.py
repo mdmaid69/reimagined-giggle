7c2e25e85043c6e27fb45d73d5d8f1f091efb835
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:06:17 +0000

    jump flow sign purpose leg

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def append_to_array(array, item):
+        array.append(item)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
