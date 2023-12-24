5a941f278dd294ffa6aab763b06d84c39656e6d5
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:01:15 +0000

    price Rr direct nervous case

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_volume(length, width, height):
+        return length * width * height
+import os
+def get_current_working_directory():
+        return os.getcwd()
