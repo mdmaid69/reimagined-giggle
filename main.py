f34086bb5ab1a2c032adc7fca1267af191052318
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:29:23 +0000

    content love war opponent content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
