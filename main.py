5a1bf983f04c9ad39c3dfcef841dae7fa989a0c1
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:55:26 +0000

    kind back steep runover neither

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
