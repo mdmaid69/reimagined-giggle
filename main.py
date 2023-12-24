fa7a020f5ade5155f553c362a3ca01981930ec8b
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:27:24 +0000

    like bright who bread add

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
