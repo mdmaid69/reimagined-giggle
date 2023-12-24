fff12d84c9c5b231d7c586b3cf02d39ad21b0f6c
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:53:29 +0000

    morning fun laugh nurse rule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
