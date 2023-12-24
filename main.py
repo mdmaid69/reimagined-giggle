12918879e983effd6855f1bf1f4c0f3621486115
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:42:54 +0000

    deal fierce development have 566end

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
