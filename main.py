4cc79d4eb6f66d7ff86e4bc6cc4255f97f70d864
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:06:34 +0000

    protection phone part part mail

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+print(sum(range(10)))
