fb4369117ba8fa9c52d494d46352577235da0f3a
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:31:59 +0000

    tell comein callat attempt custom

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+from collections import Counter
+print(Counter("hello world"))
+def is_even(n):
+        return n % 2 == 0
