9fb67d11ef62269d2320ade180359f346b24422c
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:51:59 +0000

    away manage tap vanish suffer

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import getpass
-def get_username():
-        return getpass.getuser()
+print(sum(range(10)))
+name = "Python"
+print("Hello,", name)
