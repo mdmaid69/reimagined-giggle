a0f9982609ac4d784aa5f5f4554181555cbb937b
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:58:16 +0000

    natural inside such start moment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
