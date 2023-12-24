6f0707fadb69f38346f2d273c6a13de97aacc0f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:49:35 +0000

    catch will sail 567enemy before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
