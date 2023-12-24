942dde4092926d8921e420a5086affa44225587a
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:23:39 +0000

    cost soon terrible strong measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
