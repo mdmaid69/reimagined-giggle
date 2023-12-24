7098b5d38115694f75814858a090f29a4d8a1764
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:00:19 +0000

    difference relation hope final table

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
