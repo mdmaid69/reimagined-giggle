679f19d4d6bc9e4b183c93dd0862d9748b6696a3
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:33:37 +0000

    watch square way flow pure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
