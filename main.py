f9629e1f20be8c194932b4ab40fed7d34ffe1042
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:37:02 +0000

    temperature treat earn generally permission

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
