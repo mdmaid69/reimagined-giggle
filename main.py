e508cf309b154ac3cf4d78ff24775d99816da4eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:14:43 +0000

    typical hers most used lookafter

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
