97b217a4d9d0c70d6c9240d842834c8dd1c183ed
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:52:05 +0000

    rather collection damp terrible Pp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
