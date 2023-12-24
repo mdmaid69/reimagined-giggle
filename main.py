9cd05800a559fea45aea993346777803821d66a9
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:30:21 +0000

    equal rely own drink than

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
