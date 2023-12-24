ea921b02f0ce5492994f308d3f28b465157c141e
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:18:06 +0000

    introduce matter minimum height roof

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
