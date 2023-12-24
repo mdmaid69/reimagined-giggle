19627fe7aa9a6c8f9862fae13bf6f6c20f2cb84e
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:35:21 +0000

    hold sound drink quality worth

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
