c02f7ea86e903ae079844dcbc00e4b6ebdd65347
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:41:51 +0000

    remark load light prove hers

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
