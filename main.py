0159dcc43cb373215d0b59f60ab2777cb4e75b48
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:52:18 +0000

    in familiar pay day sharp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
