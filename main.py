ba6f306491ca6771f8091e7b514fade197c810cf
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:46:34 +0000

    part chance tour music real

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def sort_list(lst):
+        return sorted(lst)
