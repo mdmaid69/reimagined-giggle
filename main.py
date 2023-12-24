26209710e7ea4c6a0044296a73988c544dcec2cc
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:19:11 +0000

    design lift workupto off board

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
