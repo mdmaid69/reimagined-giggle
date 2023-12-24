de09488c9257e93b4264c00883ea8aebbe0b6fec
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:37:02 +0000

    life path left behaviour strike

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def square_number(x):
+        return x**2
+import tensorflow as tf
+print(tf.__version__)
