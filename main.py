62658028ea09cc9d05dda611d88a34b4ca661ed6
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:49:05 +0000

    fire petrol hospital visit street

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
