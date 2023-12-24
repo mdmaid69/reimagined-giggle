4b40667e9004b8808501ff10a7d4d4d4a702b542
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:58:44 +0000

    smile takeover furniture progress natural

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
