62e19ccaf9a70effe9729ba7483b0fc51dae2f1f
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:18:58 +0000

    callat drive embarrassed nerve borrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def delete_file(file_name):
+        os.remove(file_name)
+  def sort_list(lst):
+        return sorted(lst)
