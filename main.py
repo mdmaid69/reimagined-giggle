dc91fa60e2f8fdc4f5077c400085331e3ff1833c
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:43:26 +0000

    shall Hh mixture alone listenfor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
