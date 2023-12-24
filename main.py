109a3b4695fc2e009a7975c13b0977d3476b9666
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:35:13 +0000

    woman mouth achieve hurt our

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def get_current_time():
+        return time.ctime()
