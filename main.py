864be0eb84f2354d530887ac13d415ad605d84da
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:38:04 +0000

    door like about disgusting raw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
