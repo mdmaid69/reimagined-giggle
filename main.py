901b8c9aef5ac992943961b19663941b863e39c0
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:08:47 +0000

    arrangement minor phone picture time

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
