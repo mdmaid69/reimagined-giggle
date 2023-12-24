6c801befffd189691fd9a30f52d37e56b6d6b736
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:14:18 +0000

    crash common science regret move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import os
+def get_current_working_directory():
+        return os.getcwd()
