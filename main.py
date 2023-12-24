06d84dbc0e6281092e29d718d4252bd30cf36a1d
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:27:03 +0000

    spoil lead past emphasis slide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
