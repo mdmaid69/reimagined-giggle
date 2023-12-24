bbfafc64497f0b68f3e07c561f37540555890e37
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:47:34 +0000

    shallow all wide moral deep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def reverse_string(s):
-        return s[::-1]
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
