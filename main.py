749aca7c09adf6dd89a4df5b7862ce4153bdc7d1
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:58:25 +0000

    shock level such practice 179bite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_min(lst):
-        return min(lst)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
