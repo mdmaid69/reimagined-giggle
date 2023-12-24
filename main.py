df21e9750647625def48511ae84beca9b204fcff
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:17:40 +0000

    middle piece face about post

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def find_min(lst):
+        return min(lst)
