27ca95607d26a1c2676fdf05b3fb6b49aa6655c6
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:39:22 +0000

    cause sport cook lucky intelligent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
