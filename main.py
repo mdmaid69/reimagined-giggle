8b801e23f5e96d162f0f48d54339a7f183eefb85
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:32:33 +0000

    different freedom spare mind thought

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
