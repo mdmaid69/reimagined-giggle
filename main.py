96401c5bebaad5bba174415381eebc582e21d81d
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:30:38 +0000

    570enjoyable shelter neither makeup duty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
