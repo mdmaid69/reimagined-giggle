718a17d998d081adbf5759729071eec9379c8f2b
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:36:45 +0000

    deliberate on getback circumstance emphasize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
