55bccd9d0d51375579a32282b4e96535ae2302c7
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:44:34 +0000

    whether display swallow Mm backwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_exponential(x):
+        return math.exp(x)
