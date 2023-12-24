c83f5e18899fce4aa440d9f4f00ae1dd7abc1ac6
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:35:54 +0000

    example pocket play emergency compete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
