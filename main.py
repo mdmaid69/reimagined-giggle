ca47cc3d367a061d82282898e873c40a51ebc7eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:08:57 +0000

    interruption roll accommodation store extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_float(array):
+        return float(array[0])
