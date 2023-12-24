0fb698cbca8471b5e1b472cd591daced5aee380f
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:54:11 +0000

    recommend borrow rain everywhere pleasure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
