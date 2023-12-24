549469f2eece15e9f8f5a1a1f93de26976e4e3de
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:58:00 +0000

    talkoutof organization from market innocent

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import sys
+def exit_program():
+        sys.exit()
