21bdb0e83771afdf497b5320439cf787c1fc8b87
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:13:10 +0000

    hit software today everywhere positive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
