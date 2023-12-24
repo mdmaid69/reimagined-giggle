175d9ead38001446ef2e169eff14a8f540e6a441
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:32:02 +0000

    quality callup politics welcome more

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import tensorflow as tf
+print(tf.__version__)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
