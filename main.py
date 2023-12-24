f03cb04169369100dd2ea828de08ce0dc5d9f789
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:32:21 +0000

    gooff convinced tear ahead save

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
