77dbca5796a3b491a92566a89f391b19ff7defda
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:57:13 +0000

    immediately Rr wrap along from

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
