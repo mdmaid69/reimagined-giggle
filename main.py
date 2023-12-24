1bf834d2664a9c6d7788baee1dea41e281b8e08d
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:28:38 +0000

    there add off behind watch

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def sort_list(lst):
+        return sorted(lst)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
