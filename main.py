7c2f22d99bbce5e8d630c8462bd1f7596c888b2f
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:37:09 +0000

    compete file bring effort born

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_pressure(force, area):
+        return force / area
+def calculate_area_rectangle(l, w):
+        return l * w
