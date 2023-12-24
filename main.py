9280c7771e0d641bd62849c2793f818fddbd99e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:34:44 +0000

    peaceful suggestion intelligent study phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_list(array):
+        return list(array)
