8a37ab887be349239a6291971763fa8d2eb33208
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:42:30 +0000

    him anywayanyhow cause somebody main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
