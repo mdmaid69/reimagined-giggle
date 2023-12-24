cc9f5b5251ee59666bc5d9ace3e8f0b644ce057a
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:22:03 +0000

    side suckme question offer load

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_volume(length, width, height):
+        return length * width * height
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
