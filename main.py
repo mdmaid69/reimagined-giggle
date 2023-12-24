051040c3e209c5b83a4b7745b4e52f75fbcae34e
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:54:36 +0000

    and ashamed trust confusing suckme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
