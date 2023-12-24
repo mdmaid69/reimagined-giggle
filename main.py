ef752fd46ae1f661047028f07df6974e3d83ab2c
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:34:31 +0000

    natural whisper 564encourage what argue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def convert_to_hex(n):
+        return hex(n)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
