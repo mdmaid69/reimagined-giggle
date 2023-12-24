fce937794e8bc2ee6eb0a307b41b50cfa4bf8d24
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:48:32 +0000

    let replace second meal bear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import collections
+def create_priority_queue():
+        return collections.deque()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
