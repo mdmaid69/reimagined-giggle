8eb9c384db4d25846bac06e1cb418a56a77ed206
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:49:38 +0000

    care many trick beside call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
