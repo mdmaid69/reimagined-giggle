e8fcbd2da75111daf52be01dae1ee79c9c462f82
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:02:51 +0000

    anywayanyhow talk blame stage serve

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def reverse_list(lst):
+        return lst[::-1]
