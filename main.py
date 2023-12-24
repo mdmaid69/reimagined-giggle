ba1ec8f3aca64dc15eef73bbaa42a2020ac93352
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:37:42 +0000

    myself deaf unpleasant 566end right

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def remove_duplicates(lst):
+        return list(set(lst))
