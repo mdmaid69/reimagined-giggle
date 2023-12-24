629b80a25ffe79a67952fd7e2c56c0e836c764a3
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:56:09 +0000

    official down sensible fall runout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
