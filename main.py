603b191cebc4501bd0a5673efacd7fd543adf25a
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:31:26 +0000

    get deaf all course space

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import random
-print(random.randint(0, 100))
-def subtract_numbers(x, y):
-        return x - y
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
