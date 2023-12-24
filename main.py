d6b8f692d3d536f9bcb07aef8c113068e8a9fb39
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:58:42 +0000

    Zz will breakout tomorrow pleasure

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
