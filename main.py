26a68b5a5d050fa0416aac67fb89cd97555ddbdc
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:14:47 +0000

    culture fly refuse check break

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def find_min(numbers):
-        return min(numbers)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_area(radius):
+        return 3.14 * radius * radius
