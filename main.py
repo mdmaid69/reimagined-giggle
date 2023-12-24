2548653b5c77dec07df33e4323c435b7de4f1fca
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:51:25 +0000

    frame top I Itself fine

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_pressure(force, area):
+        return force / area
