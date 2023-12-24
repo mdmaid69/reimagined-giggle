a0d41ea2c755a8aaae36a9908a4ba14208b88e0d
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:53:48 +0000

    Ff space related bill used

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def reverse_list(lst):
+        return lst[::-1]
