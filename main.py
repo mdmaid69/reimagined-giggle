70936bab57d3713393278ab465a9fcd32c91af56
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:21:45 +0000

    throughout quarter noise height miss

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def multiply_numbers(x, y):
-        return x * y
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
