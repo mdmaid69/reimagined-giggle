fae2ee6476a5b4434735d9c85be1cbc16e56bab4
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:53:27 +0000

    serious count unit nothing miss

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
