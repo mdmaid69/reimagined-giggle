6014ec7e43f635d81e819e60515015c4543b071c
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:06:28 +0000

    card impressive adjective plain person

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
