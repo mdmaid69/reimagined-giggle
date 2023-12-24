0a5961a671221a613b1a10f6dca86bf4353c7ab2
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:35:29 +0000

    disappointed soft regional neat priority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
