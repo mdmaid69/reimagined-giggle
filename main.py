b78164fe30ce1c9128e7e8c771c107cafac02218
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:12:59 +0000

    or bit outside radio lookaround

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_counter():
+        return collections.Counter()
