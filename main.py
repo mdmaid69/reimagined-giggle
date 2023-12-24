e8c0abf63c8d5db2c164fe1dfc2690e1564db4a5
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:30:58 +0000

    large convinced circle believe price

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
