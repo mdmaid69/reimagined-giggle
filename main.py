6abc8a1c0a241de0bb5afce13d0b2713c84ecca6
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:16:42 +0000

    value whom back cause extraordinary

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
