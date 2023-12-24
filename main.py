43ef433a9c37e1a352da13da0d3d4ccf359e437d
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:33:26 +0000

    but regret appearance upset towards

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
