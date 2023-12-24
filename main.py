46b18618544ddbe24cf2acb9bf33f708d9134a32
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:56:06 +0000

    bunch opposite quarter again one

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import heapq
 def merge_sorted_iterables(*iterables):
         return heapq.merge(*iterables)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
