9faa847eacc26e5b8262376090e0ffe1ac157bea
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:40:10 +0000

    trick time notice activity authority

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
