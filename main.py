b40148f743ceaa1c3ee91397612f39686b47c609
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:57:35 +0000

    perhaps same height ring correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def convert_to_binary(n):
+        return bin(n)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_as_list(array):
+        return list(array)
