6c15eed74ec29306fdc48b9bb1da253a9a2b5366
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:31:12 +0000

    dance class combine movement accidentally

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
