0a4af0bca893e445ce045bb81c3c9ac5a1f53764
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:09:20 +0000

    silent shut wrong long extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_error_function(x):
+        return math.erf(x)
