740df5ef3341cda277fc575c285f22934c9bb58a
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:35:38 +0000

    uponon almost big tone design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
