1c14681d07532342df0b34e2dcb2884822726235
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:55:37 +0000

    extremely convenient disagree fresh warn

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def append_to_array(array, item):
+        array.append(item)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
