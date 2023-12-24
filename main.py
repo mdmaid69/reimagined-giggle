4ce334d6a903d1117ac3bae28bb6748d5d257c28
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:49:40 +0000

    getback emergency slice spray deliver

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
