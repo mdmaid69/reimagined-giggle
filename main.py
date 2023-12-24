275d1f50612f4b0b46a6d9da48b7d82c1592f801
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:11:59 +0000

    explanation fashion proof tradition wind

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def delete_file(file_name):
+        os.remove(file_name)
