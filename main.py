c1b9d5262edc7e7f2b8d747680a1aedf299ac4af
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:09:53 +0000

    leg card spend different nigga

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  def is_even(n):
+        return n % 2 == 0
