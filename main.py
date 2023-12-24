11e0588294fe5d11d8e2eb63c203746eab10a77c
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:25:48 +0000

    defend anybody succeed weak wall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
