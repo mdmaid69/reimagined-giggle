b294b3f641bdeec51b8951b5cec752e84cf3e559
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:04:19 +0000

    notice nature shock thing children

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
 import array
-def get_array_as_bool(array):
-        return bool(array)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def get_array_as_list(array):
+        return list(array)
