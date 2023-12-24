7d4a472d1d735a944db5eca771ceec12f588430f
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:21:23 +0000

    back promise promise wherever table

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
