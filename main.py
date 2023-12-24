d5d898bab414ea756f61a386a992b1ef0785832b
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:53:54 +0000

    disagree giveback read guess announce

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def count_words(sentence):
-        return len(sentence.split())
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_acceleration(speed, time):
+        return speed / time
