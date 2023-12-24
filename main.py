979efeef3bc1a91d12b93821594833c56da1275b
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:58:34 +0000

    take switch suffer stay like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
