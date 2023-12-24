2ae1687c458a229916758107c981b3936bae072a
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:16:45 +0000

    modern will embarrassed book backwards

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
