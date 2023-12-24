10f6ef3f3482175086350e9e757960d12b9707a5
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:12:24 +0000

    proper child brave nearly tight

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
