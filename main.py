a3aab2e124a047148a36b193ad2b05217ba3f2e5
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:33:15 +0000

    yourself listen violent uncertain every

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_area_rectangle(l, w):
-        return l * w
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
