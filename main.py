9c960c6978df5c01f2b22ea40feb24496b491249
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:14:05 +0000

    present any light so call

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import datetime
+print(datetime.datetime.now())
