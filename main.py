6dfecba5c7f629e274a50b87fc9479cb0541caa4
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:26:10 +0000

    regional seat conversation present walk

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
