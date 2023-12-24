02eda8e39f1a03c3d552cbdda66a42b91ee01013
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:52:23 +0000

    quiet rest account introduction just

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
