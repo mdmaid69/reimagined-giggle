13ba64627b54f623c196d08a2b5cd370c7778b8b
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:31:29 +0000

    serve far account sticky passage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
