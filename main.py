c56d91fda93bf74a0b329f7976e448cc9cb63ae8
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:47:39 +0000

    close build face trade quarter

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import array
-def get_array_length(array):
-        return len(array)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
