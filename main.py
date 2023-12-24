3a121dc712e58cf963294bcd2fa3037b86564fdf
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:11:50 +0000

    complicated month gentle everything onto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
