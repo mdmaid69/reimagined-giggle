ad9f88a42f10dad9dee98848e36df8bc333a6d44
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:19:49 +0000

    ahead cry complain talkinto invite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
