e7e8891564f3f403ae56ea1cff2b15c118f65f27
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:09:08 +0000

    disturb suggest tip collection off

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import random
+print(random.randint(0, 100))
