97c16033a4b1995d106e35c51c14d0e52064cce1
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:22:51 +0000

    year criticize boil period always

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_power(work, time):
+        return work / time
