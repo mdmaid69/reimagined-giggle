e81d71792313d49e0b1ff94d0a5938a94ce83797
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:58:19 +0000

    short music watch ocean history

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
