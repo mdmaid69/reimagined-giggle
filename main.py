ebc509d6b3e9e5d3212f302ef93119136aa6ba71
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:37:07 +0000

    prepare could disagree film Rr

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_power(work, time):
+        return work / time
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
