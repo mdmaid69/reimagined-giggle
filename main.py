9a2a9664b460fdd57cdfd0ce059d432ce6ae264c
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:39:07 +0000

    mother getaway sign mystery direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
