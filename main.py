5817dd40a9d2353653fe1f4f7c3c49d15649d597
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:50:27 +0000

    popular him break underneath over

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+from collections import Counter
+print(Counter("hello world"))
