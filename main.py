79c014e7292d8d3054688f954c79c55a7b5ba2c4
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:23:19 +0000

    failure his culture detail soon

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+print(sum(range(10)))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
