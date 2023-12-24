c8716e3bc63b7d60067a253730736803aaff3ed7
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:10:27 +0000

    round stay precise unpleasant improve

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
