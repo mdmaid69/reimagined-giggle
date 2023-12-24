45938f97965e77b55c518a091f9725060b8ad6a5
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:05:06 +0000

    make fortunately medium top family

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def convert_to_binary(n):
+        return bin(n)
