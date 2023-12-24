f11e50a7d71fa886782bb80ca26189db8f014845
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:43:21 +0000

    pinch success visit objection gate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
