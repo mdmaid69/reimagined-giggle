7afc2c78112df08a25724b9939fde609213757de
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:27:29 +0000

    soon contact better sister terrible

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
