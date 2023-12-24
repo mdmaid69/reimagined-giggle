59a8c2accac4bc7d136fdaa5598782f68f673c7d
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:44:30 +0000

    want now admire fat exercise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
