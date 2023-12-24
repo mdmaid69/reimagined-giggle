02d43740d93a1e32880ca506a1dc4a9b225c76ae
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:03:27 +0000

    earn bush success clearly accident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
