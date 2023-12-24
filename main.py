923c106668c21de459458778620cd10dd5a1b40f
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:26:06 +0000

    night clever transparent guilty return

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
