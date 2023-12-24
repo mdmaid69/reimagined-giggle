2f7be838cd48dc2d7b8817a1cf77a15144c3d734
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:50:24 +0000

    fair familiar official pleasure crisis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def get_list_from_array(array):
+        return array.tolist()
