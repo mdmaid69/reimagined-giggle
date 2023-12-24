385bbad5b9b96b0f449c049ea8ef11226f77ab84
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:25:29 +0000

    charge movement pattern design cruelty

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
