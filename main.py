d58fca35420ea8f27049a5b1fd326e973b490331
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:35:22 +0000

    middle care brain plan part

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
