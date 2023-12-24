8adf66f92502f2220d805c6dc1557a43c3c6bb88
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:30:19 +0000

    pleasure be terrible heart can

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def reverse_list(lst):
+        return lst[::-1]
