5b315ef5f602366ef10dca66c79c3bfedb908564
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:45:35 +0000

    wide law movement road smell

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def reverse_list(lst):
+        return lst[::-1]
