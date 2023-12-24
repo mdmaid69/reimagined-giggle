a5146efe0ae7ab51c699dd716d491a4dbd549a8c
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:10:34 +0000

    handle smile equipment deaf possible

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def reverse_array(array):
+        array.reverse()
