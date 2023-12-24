45f96beb0e1a36163a07a995025894d177771a11
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:09:57 +0000

    basically behaviour petrol comment wherever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import collections
-def create_priority_queue():
-        return collections.deque()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import time
+def get_current_time():
+        return time.time()
