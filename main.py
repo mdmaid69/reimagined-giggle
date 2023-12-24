a38d3067d6883d5f7a8c7ac6541825cd29e0fe53
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:45:32 +0000

    arrive dare lower science formal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def find_min(lst):
-        return min(lst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import time
+def get_time_since_epoch():
+        return time.time()
