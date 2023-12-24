f3a05a1d0d760835483f0db127ef05e24a8075f1
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:07:06 +0000

    uniform need plastic thirsty ourselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
