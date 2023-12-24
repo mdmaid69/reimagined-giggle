e97b00e73a1c0cdf6a6683fb97809db0b528f18c
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:41:42 +0000

    familiar stop politics completely love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_repr(array):
+        return repr(array)
