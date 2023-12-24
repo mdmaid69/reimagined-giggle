6da045f53ba08b427f64116566481509872cb8d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:22:11 +0000

    reach distant vision love reserve

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def divide_numbers(x, y):
+        return x / y
