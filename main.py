4b40658fe311ca8e8ec1e8448f7fac8f36f4e8b2
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:27:59 +0000

    grand turninto talk table constantly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
