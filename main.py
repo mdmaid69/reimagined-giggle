35fcedb4b74020d8ee2d1c0dc03bb6f1ac3ba7aa
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:01:22 +0000

    hard even past sky fault

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
