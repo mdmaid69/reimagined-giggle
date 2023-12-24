44f17f94cd0b7982a24dd79227101fb66964d219
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:34:00 +0000

    regularly there plan joke bank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
