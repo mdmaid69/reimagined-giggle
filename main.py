065b072e6265b546426e9fcf703c48dc3d9d098c
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:13:57 +0000

    popular she authority country respect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+name = "Python"
+print("Hello,", name)
