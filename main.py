a856a58816c2987b851e9c08cb4060feb41563e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:37:11 +0000

    actual level equal narrow way

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
 def calculate_quick_ratio(current_assets, inventory, current_liabilities):
         return (current_assets - inventory) / current_liabilities
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
