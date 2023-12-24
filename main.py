84c3a142743eeeb3db18096de1469b8eaef4c69e
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:06:26 +0000

    police majority insist memory design

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
