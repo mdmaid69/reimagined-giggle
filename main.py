52e2a69004889009b6a95495dd6208f1b265d102
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:35:33 +0000

    do related shallow large exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import os
+def change_working_directory(path):
+        os.chdir(path)
