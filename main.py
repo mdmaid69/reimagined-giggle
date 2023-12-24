3129e3fef2df05f2d251bd97960ea20dc713c7d7
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:51:49 +0000

    reduction neither such talkinto Gg

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
