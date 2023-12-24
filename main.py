f79483ce0f79e0d1210f61a01d76b91563678395
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:57:22 +0000

    break apart work view sting

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def is_even(n):
+        return n % 2 == 0
