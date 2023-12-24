730938e83a16e3aa90a44411b036d4990fe2de91
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:21:34 +0000

    everyone cope common one relationship

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
