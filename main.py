4345c2ccb013a7b04fbd2f6f6182a971dfaed90e
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:02:02 +0000

    substance owe continue limit trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
