6360d3d4805078b2f0dab8e3a148dac00912029a
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:11:26 +0000

    urgent freeze attractive recently spend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
