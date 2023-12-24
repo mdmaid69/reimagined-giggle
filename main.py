ee8edf75c7bf104ba4ac57448c5af01859f35547
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:15:00 +0000

    its danger any breakup drop

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_pressure(force, area):
+        return force / area
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
