83958d75904211c7cf237397bae75b12bc77ef47
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:34:16 +0000

    bright spare rob breakin container

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
