8fa01849a9a641c71916e3a2662319ee12778cec
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:51:49 +0000

    treatment light ashamed onto paper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
