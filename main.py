41ce2701ae88a5d5c97231364178ebd51e1cb65a
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:50:46 +0000

    forever convenient politics raw cool

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
