4f67faec5fcb4c704314105a9f2109c652936125
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:19:04 +0000

    lookback(on) scale eat children goon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
