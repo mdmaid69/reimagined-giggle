e1e612f1fe26f55a60e690c52dfc46bd24f0710f
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:32:36 +0000

    dangerous direct somewhere lesson building

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def count_characters(sentence):
-        return len(sentence)
+import sys
+def exit_program():
+        sys.exit()
+import random
+print(random.randint(0, 100))
