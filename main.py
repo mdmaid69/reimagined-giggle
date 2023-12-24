941562707e4979d4021071735357fe55f0951ea5
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:14:45 +0000

    pronounce loud enthusiastic back quiet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def add_numbers(a, b):
+        return a + b
