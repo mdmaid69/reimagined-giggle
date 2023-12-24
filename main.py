69c392e9fb0e28751fef05fddd901986f8738845
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:35:52 +0000

    in flight club goup his

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+i = 0
+while i < 5:
+        print(i)
+        i += 1
