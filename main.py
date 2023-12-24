fc892dba51d8704e79c27b38f3548d383b29ec21
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:28:29 +0000

    commit feel explanation break consider

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
