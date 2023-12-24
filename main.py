560d17388661cb03df953fd4c784d28bcd85f4aa
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:54:50 +0000

    base cool low operate day

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def is_palindrome(s):
+        return s == s[::-1]
