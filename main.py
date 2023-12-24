b357e490098c3c2057eb14eef09bd20265a9947d
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:37:41 +0000

    light aim sensitive trip go

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
