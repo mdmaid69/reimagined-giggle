38c16217bd43ddb7136ed4ce09a9f82ddde56a17
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:39:17 +0000

    breakinto Ww advanced promise forgive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_acceleration(speed, time):
+        return speed / time
+import datetime
+def get_today_date():
+        return datetime.date.today()
