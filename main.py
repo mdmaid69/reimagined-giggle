f0012d953d81b1cd1006f0e88ec8747d1ffa4820
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:38:53 +0000

    period uniform blame copy bang

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_exponential(x):
+        return math.exp(x)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
