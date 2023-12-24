4b50f317977b2d93f320c01fc9edcf71ae37cb38
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:26:28 +0000

    mix new instrument sometimes recovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_pressure(force, area):
+        return force / area
