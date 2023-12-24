327a070c75edc14b216dee3858dce52d8ddd59bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:09:39 +0000

    Bb angry but neck right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
