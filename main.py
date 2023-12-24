4a1048b704df55193b21cbfec0558ae33e357fad
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:04:27 +0000

    hungry prize report chat conduct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def get_array_typecode(array):
+        return array.typecode
+import array
+def reverse_array(array):
+        array.reverse()
