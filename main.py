00d9460b5e6332a2e717c092bdee06a620148ef5
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:44:00 +0000

    only reach study typical clothes

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
