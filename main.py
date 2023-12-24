f2cfa71ab87fed40d70c64daa2f6042e0e98ce39
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:35:47 +0000

    fetch present very tape shock

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
