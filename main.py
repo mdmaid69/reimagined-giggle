e5d4e10c40f6da6cb4648f3e46f410ac404e9653
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:56:54 +0000

    need section throw increase wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
