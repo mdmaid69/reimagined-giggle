d54eaacbc81c3bcc4f4a2335fd22e32f63383b93
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:40:16 +0000

    bite town mark difficult positive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def convert_to_binary(n):
+        return bin(n)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
