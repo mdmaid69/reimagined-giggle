56f924836ff9f377978c52ee304f5c82b2229427
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:37:49 +0000

    himself drink average earn material

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
