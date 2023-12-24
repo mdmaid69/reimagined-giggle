2a84e1a6815d64f1366cbfc4ea3dfeb5c2b4eaa8
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:57:59 +0000

    rid mistake stamp before attract

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
