71aac39def2f2a5e9866ef7473d04f79ba0e9449
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:03:03 +0000

    comedown neck environment ring grade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
