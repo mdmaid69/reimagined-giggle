48a247f14156d12fb43ffb287a337fec7d86f4f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:12:41 +0000

    Gg could definite related mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
