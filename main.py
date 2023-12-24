0270f9c91aceee4940b0b0b2ed043feaf7fef4c1
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:07:07 +0000

    566end cry today plate money

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
