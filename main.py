45fc66f78caa671446919c162e3b8dafb0a8fa95
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:25:52 +0000

    recently ground convincing develop appeal

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import math
+print(math.pi)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
