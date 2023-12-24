68b0ee6fb6be9df7faacea4d7b0439026da5ab35
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:07:24 +0000

    me form live cinema report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
