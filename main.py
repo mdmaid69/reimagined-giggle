985c4b4258c24a9dde94c8ccf261b8cef2e92564
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:23:50 +0000

    slowly body guide enthusiastic content

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
