9b5cc2df7d7bdba257d83657beeab287afde0d7c
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:40:48 +0000

    note clearly throw failure before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_volume(length, width, height):
-        return length * width * height
+  def sort_list(lst):
+        return sorted(lst)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
