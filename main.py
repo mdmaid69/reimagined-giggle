95ea7998f2994effc70e5abc7c1811767b58266f
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:31:18 +0000

    bad him hardly complete brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
