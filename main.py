f9e3f01ead4c2e993f8be6aeaf2db4b1cb27f54f
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:32:21 +0000

    bar post ever work willing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
   def get_environment_variable(var_name):
         return os.getenv(var_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
