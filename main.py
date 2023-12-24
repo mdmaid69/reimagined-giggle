ff1afee24c05890d631f45a5c8262f5835812994
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:22:51 +0000

    warning such search cum maybe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
