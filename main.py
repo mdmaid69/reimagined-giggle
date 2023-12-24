0bb20bf6d135bc60a91651cf28cfe313139adf8b
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:56:02 +0000

    getback appear responsibility notice anybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def square_number(x):
-        return x**2
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import time
+print(time.time())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
