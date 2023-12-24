710bf1f9430ba888b02d0d0df081202371c90ab1
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:42:10 +0000

    wash some preparation guess work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
