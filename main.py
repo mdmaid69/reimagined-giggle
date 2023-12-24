8a1071adb2593e592af10b1e6cd639eb5ce4c1a2
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:52:30 +0000

    solve lookup brave confuse female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
