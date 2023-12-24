e45e4173739809e42e6b4043cc5ab4c970462321
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:11:10 +0000

    accident show create market dish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_density(mass, volume):
+        return mass / volume
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
