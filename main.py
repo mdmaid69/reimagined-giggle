63d09911fb2f98924f8d0e3c52a57db6da0d58ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:32:56 +0000

    wish thinkof switch alone boil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def sort_numbers(numbers):
+        return sorted(numbers)
