776ecac96a356938e5983d808066efafb474b4eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:03:08 +0000

    break correct war change obvious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import os
-print(os.getcwd())
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
