b35d55b1c0da83019c80ce1d98ed5072b1bcc839
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:26:01 +0000

    major come steer claim key

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
