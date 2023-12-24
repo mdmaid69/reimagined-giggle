a2b87918c31e2690bfaa3e39e057d1232b289179
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:01:50 +0000

    sort mind several basically back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
 import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
