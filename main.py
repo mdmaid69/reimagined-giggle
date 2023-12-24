1fbb4453f39e61cbfa8cd00fe123b7797319d49b
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:01:53 +0000

    interview window circle dead nowhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_as_float(array):
+        return float(array[0])
