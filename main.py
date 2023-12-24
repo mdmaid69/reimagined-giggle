6ca7f79370f0ebcd3a509576cbf36f3e10d46a0c
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:19:37 +0000

    couple reaction drive poor knowledge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_sign(x):
+        return math.copysign(1, x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
