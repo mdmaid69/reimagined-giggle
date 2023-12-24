c039c0f0b94e105a4f627635254f6b709717de43
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:43:53 +0000

    sort bound mysterious 569enjoy suggest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
