ceaa8523722694a54236ab4c39ab3559c3c69f0b
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:29:34 +0000

    dark hospital although run surprised

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
