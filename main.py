43e484715ef66d225d8938ac946ec39533a4f27f
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:06:30 +0000

    back likely cause excuse improve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
