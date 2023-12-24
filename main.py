5e88c5fc64a5e54a879933a5ace0613b6ca450fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:46:49 +0000

    well underneath strength quickly thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
