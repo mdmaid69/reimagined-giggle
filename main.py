7a0a7cee1092f9cf9aafc3864cc9ff4668851595
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:53:45 +0000

    human could return slightly male

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import platform
+def get_python_version():
+        return platform.python_version()
