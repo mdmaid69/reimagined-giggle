6985abd87bfe86c263b78b85ec57d78ffe00bb12
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:19:05 +0000

    content ours confused valuable afford

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
