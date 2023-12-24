9e651cd909e07d201f9f034ea6e11593fd65a61b
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:36:25 +0000

    stop 566end Ss connect mainly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def square_number(x):
+        return x**2
