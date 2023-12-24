6217ab06f6201fad36c2cd33f1802ee5d176b147
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:51:09 +0000

    battery spray such worse promise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
