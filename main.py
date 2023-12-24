170f1086212274c807ee7ca2ea73e22ba3c721f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:35:01 +0000

    runaway cry copy attach fresh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
