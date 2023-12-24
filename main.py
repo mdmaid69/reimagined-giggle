490cf6295154afdaf356599defa60877cad273be
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:38:13 +0000

    divide noone extraordinary owe stay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
