e92da552307d7207d48e6218b5696c7d9aa2ca2a
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:00:08 +0000

    manner butter takeaway know hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
