f5f5e3b6dfb89dbefd3bac2a3fc63f4299a8e911
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:32:36 +0000

    bang summer achieve do inside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
