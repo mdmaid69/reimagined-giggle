3766f35243d32615d099c7b0c88a286a9dc1107c
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:17:26 +0000

    pull wave busy argue home

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
