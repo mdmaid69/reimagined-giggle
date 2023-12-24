1657ecb0dcbefde7a4bd7062d0dedd636009177b
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:53:34 +0000

    blame drink instrument spin should

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
