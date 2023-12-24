77084d13622d951acaae92cc0d944599645d8cdb
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:03:36 +0000

    stranger night properly bath worth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
