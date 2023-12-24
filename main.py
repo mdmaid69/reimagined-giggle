542396e5ca3df8132fb462e71017861814e32f19
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:37:47 +0000

    hold tongue mainly unfortunate Oo

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
