60e200ed8dcd73b745be71601ca93a75db1d237c
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:50:36 +0000

    zero away tap waste dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
