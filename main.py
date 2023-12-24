f45e01207ffe2896f620acec10bf2e3db31c0a7a
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:11:32 +0000

    somehow top change angry foreign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
