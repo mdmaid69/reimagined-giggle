6b21e4392870288536e49995c7484a7a0771408c
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:04:54 +0000

    ready hurry bye this criticism

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def square_number(x):
+        return x**2
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
