8032bf64b24922a978ab7c5c2cfdd32236790cbb
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:41:06 +0000

    uponon below back takeaway bin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+print("Hello, world!")
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
