9dc2ad62208fcf488740424b17f595c65e6efcc1
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:18:59 +0000

    little aim cost method kill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import collections
+def create_stack():
+        return collections.deque()
