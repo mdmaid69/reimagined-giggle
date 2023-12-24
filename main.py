f9f59d05a828d67a7ad2f6b1d4323506c64bc57c
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:01:21 +0000

    sore must usual free score

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
