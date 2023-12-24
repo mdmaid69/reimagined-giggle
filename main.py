be2690c4f335ceec45c1e073c515030fdb80b219
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:07:17 +0000

    door jump bear art appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  def calculate_area_rectangle(l, w):
+        return l * w
