bb99203c8a8c0475fad65ce3257cac377394e003
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:37:37 +0000

    fall branch collect soft ever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+text = "Hello, world!"
+print("Words:", len(text.split()))
