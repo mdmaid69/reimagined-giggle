6c0ca7e9441d4733a01fc6c61b9b9b0bb813fd72
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:52:17 +0000

    laugh moment influence soak off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_current_directory():
+        return os.getcwd()
