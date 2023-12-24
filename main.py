d5be3d9353e6e4302f556608bbc00629122eeba2
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:02:35 +0000

    or century calmly consider shape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
