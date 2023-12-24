a1fb0d8f8e985b24131019ed716867e6d86eb4d3
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:40:39 +0000

    sink against extraordinary smart into

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import array
-def append_to_array(array, item):
-        array.append(item)
+def count_elements(lst):
+        return len(lst)
+def count_words(sentence):
+        return len(sentence.split())
