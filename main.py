8a3127157ba50e12eefc6a07cd1f90375b4e92ed
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:25:15 +0000

    inside concern now add theirs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
