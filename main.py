3f9a888c4a0a0dc6d5b10be645ceb58803985a92
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:43:39 +0000

    mine towards glad have mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
