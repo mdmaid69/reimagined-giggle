31ffd149cf39f32c639e84c12e8e144c55482688
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:24:43 +0000

    mind live able off admire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def is_odd(n):
+        return n % 2 != 0
