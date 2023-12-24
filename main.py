07c5e7b7726d3e5b49bc66c7c70aaa1dd6a594c8
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:27:06 +0000

    legal may turnaway long breath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
