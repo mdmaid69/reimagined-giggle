7d31c31e63b7c72dccbb25c98b8ef966be7714ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:46:43 +0000

    meaning rare range promise damage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def subtract_numbers(x, y):
+        return x - y
