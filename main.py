6178c181c2a308e788e95a46781000b96b78905e
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:05:36 +0000

    enthusiastic steady direct might total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import sys
+def exit_program():
+        sys.exit()
