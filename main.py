f5d0765f32459061f80819f8329b1ef5999b1118
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:27:16 +0000

    defend could with control arrange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def convert_to_hex(n):
-        return hex(n)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
