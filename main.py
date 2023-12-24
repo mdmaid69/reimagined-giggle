067feaa5110f642d067427baaf23aa594faf12ff
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:32:53 +0000

    total exploration appear full space

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def find_min(lst):
+        return min(lst)
