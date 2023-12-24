dcee8e3c8354226cda69e2c9ee5eb5cb64361e8c
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:54:59 +0000

    drive warn mood character equal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import sys
+  def get_python_version():
+        return sys.version
