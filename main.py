043e0e0581b4137cdfae88ed5d7a7eed17e11f24
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:08:53 +0000

    second and belong software thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
