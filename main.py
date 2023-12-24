13274123f958bc33f08e2ccf4f39f2b06187475a
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:29:59 +0000

    simple return broad moment noise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
