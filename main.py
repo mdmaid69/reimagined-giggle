2ee50ecd2ddc8308dd097ce4c3fc390bf9e0370b
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:24:56 +0000

    cruelty challenge free throw border

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
