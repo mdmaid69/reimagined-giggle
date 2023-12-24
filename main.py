bc77905be1f36ef166411c1c219cdd8f4853e0fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:09:59 +0000

    occasion related flow unfair Jj

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
