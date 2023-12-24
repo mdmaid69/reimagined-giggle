92d9f1970f9e46b7729b38b563b78629a3d16faa
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:25:36 +0000

    car ball keepoff measure clear

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
