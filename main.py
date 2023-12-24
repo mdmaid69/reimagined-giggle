91e6aec5e8176e0607f86d5d27399c05b206b0c7
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:29:49 +0000

    damage opinion once watch plate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_current_working_directory():
+        return os.getcwd()
