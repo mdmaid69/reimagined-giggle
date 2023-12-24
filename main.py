edfc0728f484d62727e06afff8c0e1e2c14cd4a6
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:17:14 +0000

    like dare mystery thinkover common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
