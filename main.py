43cd7d619af72e21ea3f9eda1d2bddd9a86648dc
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:09:00 +0000

    condition open bringforward belong name

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_speed(distance, time):
+        return distance / time
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
