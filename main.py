fc59c5c980e047e508ab803c05eca920042f415c
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:02:03 +0000

    innocent change ourselves geton difficult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  def remove_duplicates(lst):
+        return list(set(lst))
