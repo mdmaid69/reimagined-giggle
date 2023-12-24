3497b50432081bb92146dd0be4550bdd2e2b6f59
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:57:58 +0000

    cruel tooth crowded shoot provide

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
