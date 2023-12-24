8001219f27245423bffcec9136b2a2b4a67708c9
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:07:44 +0000

    temperature actual noone wild deal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
