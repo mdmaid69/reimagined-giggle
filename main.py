cdce5d284c1c60698141045d2c161d80cbeaddfe
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:17:38 +0000

    plenty software naturally tall comedown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+def calculate_density(mass, volume):
+        return mass / volume
