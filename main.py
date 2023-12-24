80e43e1ab466953e15df117ae99345e7ef19b948
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:22:38 +0000

    feed study themselves born law

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def remove_duplicates(lst):
+        return list(set(lst))
