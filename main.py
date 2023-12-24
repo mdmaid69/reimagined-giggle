7a80b025ebe564e7998140c47dae452aed573d96
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:28:33 +0000

    for standard storm embarrassed acceptable

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
