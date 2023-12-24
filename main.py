27b22f8ceeddaafaf03ed0586381fff404a712fe
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:44:30 +0000

    float miss summer ever learn

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-  def convert_to_octal(n):
-        return oct(n)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
