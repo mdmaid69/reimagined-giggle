757054d4a87fdbc0abdb4af79fef16d27d3d0cb5
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:21:21 +0000

    pen somebody claim worth tight

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
