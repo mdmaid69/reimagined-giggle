2d91ac196bd4497df2a724dd245a6c55b66e07a7
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:37:18 +0000

    holdon equal takeoff reduce language

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
