44c9899dce59c147a7f22ad5e596d19df851fe32
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:55:31 +0000

    stick extremely exploration how release

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
