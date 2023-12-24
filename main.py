b944a1dbd6fa8f21bdc87b54c20d32021fae2a84
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:51:26 +0000

    write practice suggest argue year

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 n = 10
 print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
