5d569571ffca4ffc2355c9d8a961448e589df196
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:42:58 +0000

    getoff polite treat set catch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
