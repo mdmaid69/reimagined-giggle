6e375a71bb7f3d4228db770cba23341e4f80a51e
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:53:25 +0000

    even exchange holiday contribute fill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
