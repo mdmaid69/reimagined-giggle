26ac85ef3c70b82ff50a5911d088cbb4a6fc6463
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:15:09 +0000

    in alive if programme question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import time
+print(time.time())
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
