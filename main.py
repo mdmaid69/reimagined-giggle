b263af47006363486143ca9d25f5d9699b9cab46
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:33:03 +0000

    settle middle lock keepoff card

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_item(array, i):
+        return array[i]
