19788fc1b7b8323d08d94dc643470bddf4e29e8f
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:35:58 +0000

    apart history within demonstrate welcome

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def sort_numbers(numbers):
+        return sorted(numbers)
