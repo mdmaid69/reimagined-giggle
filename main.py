41a8f0f2c9345887896a5b832d2dd6181df6d9d6
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:32:08 +0000

    sleep independent listenfor talkinto reduce

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import os
-print(os.getcwd())
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
