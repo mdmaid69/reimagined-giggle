8d18de9fd359bf82b9a6940993d34e93ac0fabe6
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:30:13 +0000

    good deliberately key spray neither

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def count_characters(sentence):
+        return len(sentence)
