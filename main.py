78a94690d5bd0035ff3c1a3f98656cb1797190f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:38:09 +0000

    put stupid urgent present same

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
 def find_union(list1, list2):
         return set(list1) | set(list2)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
