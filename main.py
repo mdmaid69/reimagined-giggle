e39a953b6231b6fceb40562cd0d5209eb59f11d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:16:12 +0000

    keepfrom slice several heavy slowly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
