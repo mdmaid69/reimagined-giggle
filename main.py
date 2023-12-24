2dec20b6d926d1f2ffdec291e301c9ffb5d09625
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:16:35 +0000

    statement lead size strict friend

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
