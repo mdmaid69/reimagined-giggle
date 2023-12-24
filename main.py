344ed6580276a80fadaf8f8447b83a774fe0e8c4
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:18:28 +0000

    target but straight relationship butter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_area_circle(r):
-        return 3.14 * r**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
