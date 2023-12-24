bc5f095f8fc6f49c853e4ccd3b7a9ef5c19ddbd4
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:40:49 +0000

    spite woman concerned fortunately confident

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
