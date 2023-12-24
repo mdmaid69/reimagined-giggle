b27c6421f75673bc9f0838cf54f908ea26017a3a
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:07:54 +0000

    connection land recently top go

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
