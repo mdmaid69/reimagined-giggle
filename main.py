08277055a38317e577ade561112e9cfec0ba2a94
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:57:17 +0000

    lose the degree middle shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
