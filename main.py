3ec52e86f029e50f528a878f0d4b0f078faae5f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:38:18 +0000

    convincing cause turnup post Ii

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
