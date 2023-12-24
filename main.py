5d244cbd7782b2573802accf579e148fd1e6031b
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:09:14 +0000

    save may size back field

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def count_words(sentence):
-        return len(sentence.split())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+print(sum(range(10)))
