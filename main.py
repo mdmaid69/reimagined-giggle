7379fdb1312360e359c216677797236ae507f524
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:42:06 +0000

    base issue low they cross

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
 numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+print("Even:", [n for n in numbers if n % 2 == 0])
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
