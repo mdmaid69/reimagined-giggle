f8f12fff2791f3e0b27067d328a209cbbf702f2b
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:21:09 +0000

    about lesson temper attention weekend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
