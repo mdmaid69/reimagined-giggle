7d6d3ebb227dd3f6d24047fec7dd169116b52e2f
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:42:18 +0000

    destroy flood finish deliver pain

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
