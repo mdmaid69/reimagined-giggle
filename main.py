e308c8eeedb4435b54a58c35a3c89ef2016c9bd1
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:10:13 +0000

    cope between broad actual beginning

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
