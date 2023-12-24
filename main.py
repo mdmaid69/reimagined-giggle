caadf1f63b9ec64f833c82eb048d0f8e5d9f8b2c
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:50:22 +0000

    interfere nothing appear commit instruction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
