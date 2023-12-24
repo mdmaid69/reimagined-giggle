7795e50fa495d78a8766106a07ae6ad644f55a3d
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:06:43 +0000

    tip taste smell week permission

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def get_array_index(array, item):
+        return array.index(item)
