5ff8ba43380a8fb748fdced4eea5253969c54033
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:19:02 +0000

    wander learn opponent city smart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
