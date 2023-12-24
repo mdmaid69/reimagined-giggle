ebed14b24a544ef109c28fb035acca973d1650da
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:35:14 +0000

    dear discovery rain ill future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  def is_even(n):
+        return n % 2 == 0
