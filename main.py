e2afcb1f156899b4962e7a1ae4acf6d89606e830
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:12:45 +0000

    geton size muscle last radio

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def convert_to_hex(n):
+        return hex(n)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
