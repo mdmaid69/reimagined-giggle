ae991bd5c71bb325da0365846cfeffba0c2b429f
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:44:52 +0000

    side old disappear earth charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import sys
+def exit_program():
+        sys.exit()
