55fe7456727625490ccf96863ce690aacb1731de
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:28:45 +0000

    extra fit spoil interesting very

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def remove_duplicates(lst):
+        return list(set(lst))
+  def convert_to_octal(n):
+        return oct(n)
