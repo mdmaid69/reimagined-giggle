c78eca0230e4ecf38053d2d2f59fa7c486aab4b7
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:28:10 +0000

    least workon extra black low

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def sort_list(lst):
+        return sorted(lst)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
