8426b6f1a69428f501163379846fddd5b0cd2d20
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:19:41 +0000

    way somewhere pinch minority relationship

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def convert_to_hex(n):
+        return hex(n)
