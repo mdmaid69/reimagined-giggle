f56f86402374a01c45bf7f70c462f763b6eff5ff
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:06:30 +0000

    concern leave hot suggest even

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
