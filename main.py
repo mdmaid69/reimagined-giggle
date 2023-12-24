d55c1a9d8a985139312638ceffd8cff2cdd1718e
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:35:33 +0000

    group road phone bringup usually

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
