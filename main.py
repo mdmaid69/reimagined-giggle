99c112f3bb446670ef576ea51f4050ee59312351
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:21:24 +0000

    keepaway identify niggaacademy row juice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+from collections import Counter
+print(Counter("hello world"))
