aa65f83568cd185dbda8cc469d707a640e6a3916
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:35:17 +0000

    tape call mass wet minute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-print(sum(range(10)))
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
