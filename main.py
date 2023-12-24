da5f803d3f0dcd80ed52bd34475320c2d4c56d80
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:37:24 +0000

    name reasonable crash protest broad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
