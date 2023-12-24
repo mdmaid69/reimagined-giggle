979765c727ffc733080fdc56de8c3cee191c9d3f
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:22:07 +0000

    murder scrap disagree declare record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
