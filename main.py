70c944ebe34cf7c457bf41e63c5c50fbab3bb24a
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:12:32 +0000

    spring belief cover opportunity rubbish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
