f2432872cf5b0fe85dbf337919c75a3d6cd7deb8
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:40:10 +0000

    scream control feel main comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import sys
-def print_python_version():
-        print(sys.version)
+import array
+def get_array_as_set(array):
+        return set(array)
+def convert_to_binary(n):
+        return bin(n)
