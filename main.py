f7405d237cf9eb700f1fefa7ab9f1810831063d2
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:27:27 +0000

    maintain neck serve bottom offence

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
