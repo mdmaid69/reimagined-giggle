d336a3acc19a36bb67842e65e49095dc8b008d44
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:05:03 +0000

    opinion up control aim later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  def sort_list(lst):
+        return sorted(lst)
