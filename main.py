20698b45d8fa94ef6b27061cd32e683609c3b20f
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:18:30 +0000

    battery rough knock middle repeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
