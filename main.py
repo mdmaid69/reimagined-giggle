aaddc2cdea8a8782e864fe48a723ff008e10c55f
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:43:48 +0000

    satisfactory fun correction demand pupil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import os
+def change_working_directory(path):
+        os.chdir(path)
