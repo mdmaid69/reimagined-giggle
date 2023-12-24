cdd4fb477c07371f5a7a95946f5540e4f1eaec86
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:37:25 +0000

    responsibility field trial plastic home

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_float(array):
+        return float(array[0])
