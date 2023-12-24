5fb09fa9cbe031a09f92743e8177b8b7fc9f7aaf
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:14:31 +0000

    complaint before turnaway accommodation trap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
