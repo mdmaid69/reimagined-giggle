336322b03297e9ed08280c7ceae7512e9e39fcb8
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:14:16 +0000

    top workupto condition performance unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  def reverse_list(lst):
-        return lst[::-1]
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
