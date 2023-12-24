7723b2ef59b390eb0e113b92b471bd8d543908fd
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:47:13 +0000

    relation along fast afterwards if

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
