3f7e12713c0e1a0694b47945afbb8405ece49fe8
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:19:37 +0000

    whichever heat home lost brother

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
