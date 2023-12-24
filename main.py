4297cf186dddeb33fd46f8fc6ec6f89576f2a137
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:38:24 +0000

    power affect Tt organization arrangement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
