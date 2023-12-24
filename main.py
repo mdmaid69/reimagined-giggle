ccb900e08b824728586c5a36e929c15bb664cc18
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:03:30 +0000

    tax storm good attitude strange

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
