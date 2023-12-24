632978fc61646a88ccba1aa12ceb152e70656096
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:44:28 +0000

    finally discover trade doctor guide

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import numpy as np
-print(np.array([1, 2, 3]))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
