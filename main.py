53c3e3348b826d86be8947b65237f2828f67d289
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:58:45 +0000

    thought practice late too meal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-  def square_number(x):
-        return x**2
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
