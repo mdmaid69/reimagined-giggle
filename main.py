3a13dfb88020470ec0b9977dfcaca016ef2a575c
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:32:50 +0000

    commit open beside match murder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import os
+def get_current_working_directory():
+        return os.getcwd()
