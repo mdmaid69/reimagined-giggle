ed00fd447bcc4c12775922469325a3a0c58e9f12
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:06:57 +0000

    hurt muscle possibly finger best

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def square_number(x):
-        return x**2
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
