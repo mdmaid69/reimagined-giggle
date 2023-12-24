bb861ab01093b1e9d6bede2b0f11e08e3a691120
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:01:52 +0000

    book step bus dance upstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import time
+def get_time_since_epoch():
+        return time.time()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
