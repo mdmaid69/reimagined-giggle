192191030b2b6bcaaf4d6ace952411248a3ab46c
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:04:59 +0000

    point less talkoutof ordinary prize

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def get_today_date():
+        return datetime.date.today()
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
