7de6f5666aa8d1c34d45c85e47d5042f81d370bd
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:03:22 +0000

    complete care unhappy shop sky

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
