129922b1ec79fafe13233fdfd49a7728e1b2144a
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:08:35 +0000

    share mainly takeover pleasure intend

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
