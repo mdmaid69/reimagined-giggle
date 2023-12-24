a49e51c4c5b32903b53de7faa8365cf7781daa1a
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:04:44 +0000

    post plate rest switch thinkover

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def sort_numbers(numbers):
-        return sorted(numbers)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def divide_numbers(x, y):
+        return x / y
