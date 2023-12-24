42ca9358bd403f7a22ba9d1b63359efe4ec97aa4
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:58:58 +0000

    people kind climb excuse change

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
