40162a90b689a5c0300deeb6041cd6c9019a9718
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:21:21 +0000

    fold turninto crowd answer means

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import collections
+def create_counter():
+        return collections.Counter()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
