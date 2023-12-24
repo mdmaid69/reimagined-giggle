3c93ab5c62b947292cec136c3beb224af4c5b689
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:34:41 +0000

    not change consist satisfied pollution

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def square_number(x):
-        return x**2
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def count_words(sentence):
+        return len(sentence.split())
