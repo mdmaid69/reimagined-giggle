a70ef0c1a7be5f4782cdd01309712cd46e886315
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:01:14 +0000

    suit convincing undo snow probably

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
