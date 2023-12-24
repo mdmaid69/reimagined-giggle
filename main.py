f16cc4ba0dc4eec2a6e309510c0dd46a96612f96
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:36:47 +0000

    stage design tip excited must

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
