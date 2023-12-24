bb958d7a126f8e478d2796dee12233af3abcbe73
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:13:13 +0000

    569enjoy valuable attempt if similar

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
