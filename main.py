adbff73fd66b31ea55ef25c5c67e0254e35c5b35
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:46:51 +0000

    mark worry adjective sorry on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
