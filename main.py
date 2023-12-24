5d7ae5b0736b38b68caa933dc06a7c2faba573db
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:21:22 +0000

    lead interest belong over off

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-  def is_odd(n):
-        return n % 2 != 0
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def reverse_list(lst):
+        return lst[::-1]
