719487b621cf6d44dd958400d01be3548fd801ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:21:52 +0000

    subject sweet shallow complaint ear

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
