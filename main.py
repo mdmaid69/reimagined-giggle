f5fc01d3ddabbacc13012a938e4a8e2a0e10d6b8
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:04:50 +0000

    collection cap strange evidence damage

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
