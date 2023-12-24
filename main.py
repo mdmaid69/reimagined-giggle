607cce7aa46067b27e57ec2d25d8c749af23ff05
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:45:54 +0000

    remark below shelter share square

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
