ef0650641ea26256e1dbc75961b116a26c5b83f3
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:54:06 +0000

    giveout hello either cost nature

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
