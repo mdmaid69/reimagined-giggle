9a9774eb61c739d03a36eae072b72a21ded41eee
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:39:49 +0000

    boil Ss discussion raw announce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
