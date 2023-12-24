f07784d483ec2814c731b527f2d64e1ec85623dd
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:59:12 +0000

    fat bound force port soil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
