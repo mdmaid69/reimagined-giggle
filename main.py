e241bcaa78551d6b4e1aa59ad677de6d80dd0089
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:39:14 +0000

    study praise word what belong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
