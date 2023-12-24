51809790460fbe009693f71f1a3c5e260d3a3b6a
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:33:46 +0000

    dinner chance show field stone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def square_number(x):
+        return x**2
