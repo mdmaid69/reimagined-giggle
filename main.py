be237fdaec4b5a460f58aa9410c1b5917f2737a3
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:50:19 +0000

    keepfrom divide 179bite acceptable bit

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import getpass
+def get_username():
+        return getpass.getuser()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
