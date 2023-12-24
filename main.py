26e509ee851fbe8a43dfc23a0da466d3ab6f26c1
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:43:45 +0000

    pure fashion score display side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import getpass
+def get_username():
+        return getpass.getuser()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
