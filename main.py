89ec99938f53668bee89e86e4591ad315b723a7b
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:36:13 +0000

    excuse disagree friend bury tell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
