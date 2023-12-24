32e5ab7645dcaf4352cd25284fbe9ceb3cfe05b1
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:52:25 +0000

    show do badly floor check

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
