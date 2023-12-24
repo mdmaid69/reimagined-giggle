df99d0b03518f71bf77fe855d5ba7a09af823372
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:47:49 +0000

    properly emergency have justice around

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
