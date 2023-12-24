fcafa7c293394bd572cff25771837bd73a9bc031
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:41:04 +0000

    certain suggestion runaway mark deliver

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
