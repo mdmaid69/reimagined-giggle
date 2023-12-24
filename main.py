2711f74481aadc6de9afe208088a0f28dd2ba15e
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:28:50 +0000

    address matter traffic song where

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_current_working_directory():
+        return os.getcwd()
