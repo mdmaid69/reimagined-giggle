85678a3653b89de8c88418c514b6a212ed90cfe8
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:56:17 +0000

    error pull pull spray no

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def greet(name):
+        print(f"Hello, {name}!")
