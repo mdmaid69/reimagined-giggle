52c8d7c50a54446eeb2f555f744f84433ec3515e
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:45:03 +0000

    scared centimetre also plan height

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
