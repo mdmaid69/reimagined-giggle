9edff00753e1149d0393af3ac4062f41e3102dec
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:02:17 +0000

    nose common related total comfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import getpass
-def get_username():
-        return getpass.getuser()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
