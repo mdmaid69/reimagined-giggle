2e191c34740fc34d95b210f090fd84f83466a294
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:30:23 +0000

    train private before edge long

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_min(numbers):
+        return min(numbers)
