005914fbd615a9e35517c4c7039111f371d66f20
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:43:35 +0000

    situation set maintain country clear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def reverse_list(lst):
+        return lst[::-1]
+for i in range(5):
+        print(i)
