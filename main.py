76b90fa6ca3ab30be0bfc3acb65cf35ccbf89b90
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:55:20 +0000

    window miss back key near

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def divide_numbers(x, y):
+        return x / y
