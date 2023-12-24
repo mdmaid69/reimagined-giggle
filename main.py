cefd244d0759fe798cfa97b917da79199bb674d7
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:12:58 +0000

    mixture exchange town design attractive

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-for i in range(5):
-        print(i)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
