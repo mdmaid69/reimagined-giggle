a2a73d09fbf8c8b59528c16938349a13d31cc083
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:23:08 +0000

    record squeeze complicated after scale

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
