cbf6b30ad6d4d70b4670dfeebe182d6c10f85608
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:57:07 +0000

    expensive lookup goon pinch bound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
