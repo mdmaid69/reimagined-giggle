12e6529c0e07d3bc92a1d9302e6715dc17ee5b93
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:32:08 +0000

    behind where soil general boil

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
