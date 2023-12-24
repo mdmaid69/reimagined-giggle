ea06d4ba21d686a145a087868cae5febd54583f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:47:59 +0000

    Ii thumb division suppose hers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+from collections import Counter
+print(Counter("hello world"))
