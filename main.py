bb2c630a71ba2e175db059045e73ad836af5e0b3
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:32:18 +0000

    talkoutof statement short spin mystery

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
