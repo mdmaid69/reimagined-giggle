e4653eb9912e098b93d490e2de822f4c36aa9d5b
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:16:58 +0000

    fear back none later correct

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
