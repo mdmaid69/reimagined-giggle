500c18dafc05edf08968682f863807364ded29f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:03:50 +0000

    worse I support record stare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-name = "Python"
-print("Hello,", name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
