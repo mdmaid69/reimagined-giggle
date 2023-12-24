a02a4913672c56332607c61a19e8214c50169e73
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:13:15 +0000

    fun 570enjoyable related send on

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
