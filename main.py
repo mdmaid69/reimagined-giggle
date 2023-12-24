8a496efde404149813512f606c3a393e03436733
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:42:01 +0000

    mainly not within confusing support

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
