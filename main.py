df30bfe72c451a3da12601be3da9332c9f8ad84b
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:05:19 +0000

    issue waste insist child real

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
