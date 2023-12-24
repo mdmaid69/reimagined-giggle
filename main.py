3b72d04bd75c3fa057de0b05fa2bcb59f8acb4c6
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:44:32 +0000

    quit exchange find cheque file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
