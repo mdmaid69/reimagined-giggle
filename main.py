c822fcf9ee06f9f23b090176c48c21aec98872f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:53:37 +0000

    sometimes runaway himself connected count

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  def square_number(x):
+        return x**2
