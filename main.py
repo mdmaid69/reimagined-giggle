2e29681199845d6744b37446ec0bcbd5e11b62b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:20:01 +0000

    question goaway dry where stare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
