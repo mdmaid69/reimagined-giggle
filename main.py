4064866ed9e996692c4d30f7689ad92f342abc65
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:41:41 +0000

    material mainly system geton cheat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
