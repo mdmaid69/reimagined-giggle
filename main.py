e0773473f7ff34fcc8099b044f90aa542ca89dc7
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:15:36 +0000

    improvement only water agree mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
