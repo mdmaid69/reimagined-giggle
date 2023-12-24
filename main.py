622acf2023db54ab8c10e0d205ead6a6079d0455
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:07:15 +0000

    sell hard far date bury

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
