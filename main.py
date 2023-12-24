1e9eb3c57502f41a7c7730fb2629e518b1daf6bd
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:39:10 +0000

    area introduction national conclusion anything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
