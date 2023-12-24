ddc3ad7f840a3328682c6409db379daf1454a5ce
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:06:18 +0000

    grand stop sleep schedule every

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+from collections import Counter
+print(Counter("hello world"))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
