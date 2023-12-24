6a18e19c75a54427374107c3190b9aab766fe951
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:05:09 +0000

    collect meeting brush run rescue

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def convert_array_to_bytes(array):
+        return array.tobytes()
