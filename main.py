79f83538cb07097c0a192ba593fd81433dbfbc62
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:44:51 +0000

    live cost land frame condition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def add_numbers(x, y):
-        return x + y
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
