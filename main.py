195793745418867a9221da3975cc99899cb45ed4
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:30:02 +0000

    female concentration sticky deep behave

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
