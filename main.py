b4ed2e1ab11dfa634c650d70dab039aa7100c9db
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:49:04 +0000

    so behave club describe might

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
