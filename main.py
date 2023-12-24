336d47c3484dc21d7a821b2450f25b6bb8b2abf4
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:34:33 +0000

    minor support appear winter accept

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
