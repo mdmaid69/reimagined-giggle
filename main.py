ed3245f0be13f71e515a0dde16b6e826764136eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:59:35 +0000

    children emphasis same bet or

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
