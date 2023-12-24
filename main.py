d9b652993f6fafe7a44b57ec6e75ad2a6c5e45ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:28:26 +0000

    port comeback frighten food fashion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
