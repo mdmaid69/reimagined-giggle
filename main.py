db3f8b3a6324c1f2953d27bc9c8d8a6fb62b52ee
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:41:48 +0000

    on trouble still goon meeting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
