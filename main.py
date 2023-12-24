9ae6e3ca50e609e888c65abbb5f3c50dac33d436
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:38:28 +0000

    praise lookup bury middle loose

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
