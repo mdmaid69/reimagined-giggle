d544065493f59d8678d42af445c4981f2e679cc1
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:07:30 +0000

    seat either unfair pocket suit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
