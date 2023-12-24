25ee95964c218a39e564add7833918764491fc54
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:25:41 +0000

    snow phone calloff clean peaceful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
