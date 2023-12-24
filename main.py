f75b15fa160272e1af01019d334e80bc861bef6f
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:02:09 +0000

    side talkoutof rest control search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_user_list():
+        return collections.UserList()
