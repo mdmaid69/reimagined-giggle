4358dc404ac0b0110b4a5c1fcb12da0ebc4129e7
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:08:31 +0000

    runout climb late joke performance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def divide_numbers(x, y):
+        return x / y
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
