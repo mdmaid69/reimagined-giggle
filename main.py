9d12845667d5915b97a5c5242e844c739007364e
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:47:16 +0000

    free sort secret disappear breakout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_length(array):
+        return len(array)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
