ac25300079edf99839b0d5b5213ebaf0d40d49bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:26:50 +0000

    guess objection release traffic guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
