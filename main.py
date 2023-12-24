cfed5bd98060df5cec1df080bf297b4b9179994a
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:18:27 +0000

    pay spill demonstrate container world

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
 text = "Hello, world!"
-print("Characters:", len(text))
+print("Uppercase:", text.upper())
