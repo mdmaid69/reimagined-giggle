b4e96b40ce51a7ce572ed23625192076f35b25cc
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:46:26 +0000

    want sink owe bend suckme

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
