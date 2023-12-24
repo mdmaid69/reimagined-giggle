ca05114479fe3498836807e557f6850fb2728f5f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:02:00 +0000

    limit discovery turndown bringdown yours

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
