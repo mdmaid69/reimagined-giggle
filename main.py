d500401846a1cf3005e2d56d95899eb2e044587c
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:58:30 +0000

    trip hole under remember neither

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def add_numbers(a, b):
+        return a + b
