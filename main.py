8e65b131f5d93018260bcd8b89476bfebd8980c4
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:02:34 +0000

    road race break yesterday dangerous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def clear_array(array):
+        array *= 0
+  def subtract_numbers(x, y):
+        return x - y
