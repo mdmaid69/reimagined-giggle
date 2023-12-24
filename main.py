f92cc5d81ed4a344870af7716f7550ffc5e216f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:12:06 +0000

    takeout object fortunately explanation lookaround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
