029d541ff331a60222b30da909a7683f0eabf411
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:31:49 +0000

    disapprove grow rule high proud

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
