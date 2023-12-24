3ca58299028119be79802edaf7bb72426ce493a5
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:50:59 +0000

    march little public yesterday day

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
