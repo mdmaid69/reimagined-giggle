97089eca58a21ad56d5ba9f8af39af0e9ec50142
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:13:46 +0000

    recommend permission message conduct world

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
