304e01f183003e0408336f5f42f336c78fd90984
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:49:29 +0000

    pot during vanish imagination boat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
