a747bd562aa02654373c0deb6d42bec4753c995e
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:51:55 +0000

    ring fix shock conduct tense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
