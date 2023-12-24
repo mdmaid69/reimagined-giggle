0558ec5660ddcb7857e436e0fbdb4a613d3c3d87
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:09:12 +0000

    clean mend crisis anxious my

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
