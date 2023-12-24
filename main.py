420a0fd059e614df7e21498bf76d891664888f2d
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:49:12 +0000

    genuine invite along cope public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_max(numbers):
-        return max(numbers)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
