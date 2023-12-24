e3a8216873b230c84b52b6f1a7bef33984fa684c
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:43:56 +0000

    watch us fasten appear fear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
