f6243ae3725e9bb08cbdc464c20e4db78052f19e
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:50:05 +0000

    rob sentence butter expect active

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_speed(distance, time):
+        return distance / time
