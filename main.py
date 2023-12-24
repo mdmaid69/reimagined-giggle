64c13d44a4a5f345f7f21868b2c8dfab00bf88bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:48:25 +0000

    improvement big Cc height gooff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
