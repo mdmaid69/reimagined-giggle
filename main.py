158483188a02f5f362262f9bae5f098a31dd5d65
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:21:02 +0000

    lot rescue guess since possibility

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
