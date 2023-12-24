5eb7471b68847142a3263f9a73fe6ae2f88aeb7d
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:48:11 +0000

    power connect tour pause past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
