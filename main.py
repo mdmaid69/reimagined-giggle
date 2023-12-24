fc65c796942e41e5c08a13357f80c00fab6bd38e
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:25:11 +0000

    expensive prefer surprising best welcome

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-print(math.pi)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+print(sum(range(10)))
