848a00e5994fcc8b311c20489dc6835964a1da71
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:26:52 +0000

    move everything always match only

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
