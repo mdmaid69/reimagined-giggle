83c2ab817897b739ff4110fbaf479fecc7f3fa17
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:58:41 +0000

    desperate possible recover achievement lucky

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
