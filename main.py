d5877cc40cfbfe415326ca93d73f222cc274e234
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:38:44 +0000

    workon well value joke touch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
