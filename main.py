04d43c3fe59c13d568e6076d77ecfd95065bda47
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:46:33 +0000

    post fair pain threaten leave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
