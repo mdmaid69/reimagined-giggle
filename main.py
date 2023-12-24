4c3dad4239e43d73f5d0470b209f880784499b8e
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:13:05 +0000

    themselves owe there hurt adjective

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import math
-print(math.pi)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
