9cf3eb79ed1dfe4a0db3462ec3c79c3143dd4aa9
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:16:22 +0000

    thorough operate criticize eye to

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
