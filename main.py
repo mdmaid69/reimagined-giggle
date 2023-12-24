ab86cf7fec2944bbdb19bc7caf5dc3bef4dff0c1
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:35:29 +0000

    true nose reliable makeinto collect

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
