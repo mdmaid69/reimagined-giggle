22d34af3d44fd9946f24e547f16ee1b6cca4b824
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:51:05 +0000

    keep school change but over

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def split_path(path):
+        return os.path.split(path)
