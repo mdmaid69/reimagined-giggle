7726f81e2fb39fa02ca3b8fe5742c9b3512674bb
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:48:13 +0000

    name name belief mind patience

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-print(os.getcwd())
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
