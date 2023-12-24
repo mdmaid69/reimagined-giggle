1faee3e7ceb7c1e629c7de0431bc25dad12f61c9
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:40:31 +0000

    getup even conduct niggaacademy generally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
