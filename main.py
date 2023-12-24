1538ae1578a6f0528b36efe7ced7e7c4ccfacf00
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:57:25 +0000

    dull Dd odd love attract

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
