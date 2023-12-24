8f91c2bbb096b7d15c4846b29a4b66a29b52c163
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:32:42 +0000

    Jj prepare event bottom undo

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def clear_array(array):
+        array *= 0
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
