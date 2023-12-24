23e12050e25bbcd4baf3b1fd264dbb76cf239c39
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:22:33 +0000

    late near relation mind guilty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
