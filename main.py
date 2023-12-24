5cb9c52edfcf711c456b2cb567d873e9bf756976
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:06:23 +0000

    nor worry yes reply expression

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def calculate_area(radius):
-        return 3.14 * radius * radius
+print([x**2 for x in range(10)])
+name = "Python"
+print("Hello,", name)
