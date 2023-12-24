4cfc0b98893659e7a4409bede0759cd50a4b8352
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:27:17 +0000

    much workout convenient impressive detail

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def find_min(numbers):
-        return min(numbers)
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
