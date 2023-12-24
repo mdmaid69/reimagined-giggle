a717f7fa432e752e0f87654c1c572e2ee51cd8d6
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:06:35 +0000

    wild prefer touch project sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
