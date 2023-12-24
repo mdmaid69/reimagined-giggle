7fa60905a62e9df8174b77ecfa938dd86c92e598
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:17:08 +0000

    disturbing sensible slice material image

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-  def is_even(n):
-        return n % 2 == 0
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
