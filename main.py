e488afe323a83e441e233e0b40dc6ae72a3517a9
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:12:59 +0000

    sing callback wave patient variety

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
