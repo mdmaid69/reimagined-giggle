8eb33daab36ae4dea025714b8753d4e6fb21d9b3
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:32:16 +0000

    increase drive correct claim top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def remove_directory(path):
+        os.rmdir(path)
