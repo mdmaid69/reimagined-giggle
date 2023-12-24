007d1938f38d5130a8f266c8b4d7b7bff57ebf77
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:28:08 +0000

    circumstance mood slow never general

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Characters:", len(text))
+import array
+def get_array_typecode(array):
+        return array.typecode
