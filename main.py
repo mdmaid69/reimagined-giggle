46903baf7076ed56f008678731e2efa43a76ca5a
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:55:31 +0000

    direction butter onto eye brief

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
