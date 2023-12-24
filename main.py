be8f78c0cd8662ab2783b9a4354e3dc2d9c116bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:17:13 +0000

    correct 569enjoy love cost warning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_power(work, time):
+        return work / time
