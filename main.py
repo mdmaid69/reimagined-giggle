2f88d8062d91e9b9532c051e5367e5f99d1a28e8
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:44:10 +0000

    attractive address talk round his

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_as_bool(array):
+        return bool(array)
