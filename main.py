4e34ee73c91060505e8791b693e7044f75a09619
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:15:29 +0000

    whistle accidentally steady pronunciation likely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
