143a88bf1ded10d9e96d3cb4e1979791d8576645
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:47:54 +0000

    small church bury right drink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
