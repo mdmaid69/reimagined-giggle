b4c572f08e734b4deec71fe1d5bfad837b6fd98e
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:19:08 +0000

    shadow top further fire next

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  def sort_list(lst):
+        return sorted(lst)
