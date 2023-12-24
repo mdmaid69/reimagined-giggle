31c04e1e41b5ba99122ca93d4ac5d19c959f0bf1
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:24:34 +0000

    mind survive wise winter warn

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
