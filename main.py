2c0ca46808b12b21e2e58487e878d2bdcc00c103
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:29:11 +0000

    bring runover guide announce crazy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
