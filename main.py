a4e22960b5f62fe97b2a729ff4b60da519286ceb
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:47:25 +0000

    slowly error claim put stare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
