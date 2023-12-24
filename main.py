6286694457c878878a2f1413811dff0759261cea
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:46:23 +0000

    breathe fill sting join spread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def count_words(sentence):
+        return len(sentence.split())
