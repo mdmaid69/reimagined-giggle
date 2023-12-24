76752eddb64fa6b4bc2fe128e5cbe4a53a85e84e
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:27:23 +0000

    fit influence beat getout like

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def subtract_numbers(x, y):
-        return x - y
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
