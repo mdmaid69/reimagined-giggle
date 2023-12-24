9000c8287e3ce31f414d4fe94855ddbc8efc9a3e
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:21:13 +0000

    welcome forever evening bunch circumstance

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
