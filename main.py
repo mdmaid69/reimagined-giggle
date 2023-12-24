7e2904f9b690de45d3e1b4d5542849451443336e
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:36:19 +0000

    talkoutof kind drink on whisper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
