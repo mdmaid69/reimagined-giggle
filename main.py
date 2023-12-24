2c331656f46ab0b03c24447ae7215bb1ca237403
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:17:51 +0000

    motion unfair wide standard nose

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sys
+def print_python_version():
+        print(sys.version)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
