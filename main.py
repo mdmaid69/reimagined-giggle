5c81681a30d0fba37541825e9f3707a4a3f527fa
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:10:42 +0000

    bread gas tip now impression

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  def convert_to_hex(n):
-        return hex(n)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import os
+print(os.getcwd())
