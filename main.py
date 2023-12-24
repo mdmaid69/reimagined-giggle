8b0f479b424cd9683c96c1a742813f25a0a7460d
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:37:45 +0000

    hate turn petrol type sport

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
