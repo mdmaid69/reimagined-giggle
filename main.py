93b1bda2f651ad5365b1c3747ae6372f4c6b39ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:32:59 +0000

    goaway admire lead road lay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
