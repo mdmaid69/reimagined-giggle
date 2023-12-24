0fe758f4574febbce126c5343848adea86f45324
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:12:42 +0000

    rule mistake sound concentration substance

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+  def subtract_numbers(x, y):
+        return x - y
