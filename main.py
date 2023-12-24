5c633e75afd7507ace2a5632d76e793e5c3c0e60
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:59:34 +0000

    harm freeze fill perform nearly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def cube_number(x):
+        return x**3
