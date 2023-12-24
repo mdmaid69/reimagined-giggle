5a4091fdd8e954d367fd6d144adb2fc4e1ba7498
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:18:00 +0000

    contrast sharp fix come main

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
