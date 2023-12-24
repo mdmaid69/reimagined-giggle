48d307631f9f76876dd00fa274c5a5389950e08a
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:38:14 +0000

    visit breakdown we mark somehow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import sys
-def print_python_version():
-        print(sys.version)
+  import sys
+  def get_python_version():
+        return sys.version
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
