99b64686d8311d9b08413294c9db05112162268e
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:57:59 +0000

    eager can lie deep press

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
