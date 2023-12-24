2f3e2250dc64007ffd2e5f52bc74fedfaf750a7e
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:03:46 +0000

    sink everybody free operate along

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def add_numbers(a, b):
-        return a + b
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
