385eb8cd69178512de37ce66561993f86b90318e
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:39:55 +0000

    Hh break shut pair minority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def add_numbers(x, y):
+        return x + y
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
