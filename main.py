2ad13916a11975e075c08d075db3c7c2cb5e54d8
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:54:38 +0000

    raise bother danger formal approval

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def count_words(sentence):
+        return len(sentence.split())
