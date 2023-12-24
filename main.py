ebe931ecff407a73e1e1b40c694d670038beac08
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:08:06 +0000

    collection boring sore print shoulder

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def subtract_numbers(x, y):
+        return x - y
