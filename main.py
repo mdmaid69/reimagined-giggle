2aff76bcde8bc6afbad788f563fb2699d1ab6ab1
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:04:10 +0000

    tread anybody Dd upwards tough

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import re
-print(re.match("h.*o", "hello world"))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
