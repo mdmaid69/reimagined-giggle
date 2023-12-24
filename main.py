fce83b103bf9e8bf27c880f139fb72791bbb4d49
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:57:22 +0000

    control remark permission gift over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
