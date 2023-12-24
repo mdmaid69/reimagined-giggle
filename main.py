a14792f40bb75c7831f4203faaa86097ca696698
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:19:36 +0000

    music match other process behave

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_min(lst):
+        return min(lst)
