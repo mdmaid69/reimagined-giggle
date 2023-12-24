efa6e6e2184a74a7b642162a701c9c75f3a1be88
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:35:27 +0000

    but move takeaway vote natural

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
