c6ef2f88a27a47dc8d9f8485afef07001bc72d4a
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:43:18 +0000

    shut Zz path estimate thinkover

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
