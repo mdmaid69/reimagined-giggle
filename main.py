e7c8f26e8ff81ee00806fe2cfec56947c0c971a9
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:43:48 +0000

    friendly confused progress complete load

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-  def square_number(x):
-        return x**2
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
