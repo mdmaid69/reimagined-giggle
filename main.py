0924e57dc3afacc2e8940e1981ba4f95990ec4ea
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:15:25 +0000

    circumstance manage behaviour seriously friendly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def calculate_area_rectangle(l, w):
+        return l * w
