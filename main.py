709c2273712ea7c3a6e19c69f07e543e0fd8da83
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:19:13 +0000

    become throw mess bored tend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
