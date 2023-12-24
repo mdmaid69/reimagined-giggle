7855189bd63750148d588d8b61284b6b09347f37
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:29:22 +0000

    power become thinkover system consider

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
