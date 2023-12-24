e6054e919a4fc68b85fb4161c5e47cbb78fcb2c3
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:47:41 +0000

    content tidy no fire public

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
