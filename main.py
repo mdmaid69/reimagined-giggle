71d43014cbee538046debb48ca1eee5781827b34
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:17:01 +0000

    rate bring vanish achievement neck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
