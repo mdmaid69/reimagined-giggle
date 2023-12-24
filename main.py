f2cb6ada5eb6b1f298f1c0d40dcae9e3a393dbe7
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:22:23 +0000

    handle scream answer concentration begin

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import time
+print(time.time())
 import smtplib
 def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
         with smtplib.SMTP(smtp_server, port) as server:
