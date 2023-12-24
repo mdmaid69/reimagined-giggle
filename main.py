1c70d1ec00b0ea4d2dc23621235cf26d383bf25a
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:52:23 +0000

    crash decide before tip ours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_area_circle(r):
+        return 3.14 * r**2
