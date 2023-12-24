4435fe9036f3d5a71a1f8772e1981bff0bdbae8d
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:13:36 +0000

    change run never comeoff scale

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_repr(array):
+        return repr(array)
