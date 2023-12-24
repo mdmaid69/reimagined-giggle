229132c169e391f45a3fe290b0a8d2b2ae559fdc
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:36:06 +0000

    unkind shine autumn either trade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
