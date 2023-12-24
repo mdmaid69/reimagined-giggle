77c7d445a9e4bfe7e1ad3b6db3a7f8c75f805435
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:06:49 +0000

    day thumb then debt especially

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import tensorflow as tf
+print(tf.__version__)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
