115cd0976fd034236b9bbdbfc1d47b2d0e510d9d
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:01:14 +0000

    thinkthrough Ss blame standard thick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
