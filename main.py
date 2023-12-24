05c4e4156df0e264a9a40cdd0c213fccba933992
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:15:44 +0000

    touch exactly find person rank

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
