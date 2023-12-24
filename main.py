0f277f2edad07f36507933d786a13bfc0b8a88fe
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:05:26 +0000

    smell frighten OKorokay cost finally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
+  def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
