e949b5af188ee570f8ceae9956fd473671ff88d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:42:16 +0000

    sticky hair last silent loud

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
