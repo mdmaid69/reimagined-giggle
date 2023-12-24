169c7ca7dc2597800f108b7925fcd424211b50d1
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:43:15 +0000

    dangerous arrival more dry capital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
