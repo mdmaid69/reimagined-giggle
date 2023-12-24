ee5d9d47145f31f9b21471671a5f8d8c7c2415b2
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:52:25 +0000

    earn street shoulder music knowledge

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
