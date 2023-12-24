b1bc9b31dfe9029257b61bdd583ac52ff6a727b2
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:46:06 +0000

    sharp address card our considering

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
