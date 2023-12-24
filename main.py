13cd7312990beb6f22615c4ed8e42cbd650d6511
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:44:49 +0000

    live simple draw stiff runinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import time
+def get_current_time():
+        return time.ctime()
