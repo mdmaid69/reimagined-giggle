98e5467556348fbfd1aa35e0223175d30f41b47f
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:52:07 +0000

    break emphasis inquire burst Gg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
