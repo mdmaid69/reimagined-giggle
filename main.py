7708538297620d85fc7b4d3700b14c209fb08a43
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:52:20 +0000

    accidentally reduce name contrast evidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
