1ac1b801a06cd09c2934b02f71dcaa9e2640d520
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:13:56 +0000

    little wander takeout machine very

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
