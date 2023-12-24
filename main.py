9d318680fc24156d798d9000ce27ebdbb5a21195
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:32:21 +0000

    copy talkinto call teach somewhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
