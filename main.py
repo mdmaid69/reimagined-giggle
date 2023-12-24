f85b0c406cf677dc34f93e47b4d47c6ddcc238e2
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:27:42 +0000

    unlikely day use trip OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
