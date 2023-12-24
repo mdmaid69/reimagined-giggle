c207d1d9ff3c0372b045457e94423231694e462d
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:07:42 +0000

    harm we sense confusing basically

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def cube_number(x):
-        return x**3
+  def convert_to_hex(n):
+        return hex(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
