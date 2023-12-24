b818c9b5401544b1488a4757c588bbc23d6be602
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:33:48 +0000

    problem test charge immediate embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def square_number(x):
-        return x**2
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
