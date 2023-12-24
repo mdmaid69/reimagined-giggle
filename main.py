399a44eda71f62931e4b03653faac914215f596c
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:39:51 +0000

    tall memory inside instead Uu

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
