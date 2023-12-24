a5eb4361f56461ad406b2e5dbdc4ca1d15a9360b
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:22:44 +0000

    crazy side prefer slip trade

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def square_number(x):
-        return x**2
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import platform
+def get_python_version():
+        return platform.python_version()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
