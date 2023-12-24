274f3831fb08d24f386d2e7ec2055e4314d8639e
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:58:41 +0000

    brain giveback talk different say

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_work(force, distance):
+        return force * distance
