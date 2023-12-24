9c78d630f6f98a328bf1c17fdc2b9e05cab33439
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:36:30 +0000

    workupto interview winter once her

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
