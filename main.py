d02328563f8da982dfdaef69fff00e54b54fff77
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:15:55 +0000

    better firm buy history production

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_rectangle(l, w):
+        return l * w
