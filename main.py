5e89f3a2e923ce507a0cb4741c7dbefe2f6c0d33
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:12:03 +0000

    fetch taste holdout affect opposite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
