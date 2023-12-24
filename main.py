fe06c7ce7aa6b3c5fa8f6edf6f7c4bc973f9a9d1
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:55:11 +0000

    turnout worst life easy hand

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def convert_to_hex(n):
-        return hex(n)
-import math
-print(math.pi)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
