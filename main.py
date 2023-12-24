cc4539e6b9787a10ff4aec2c8fa2aaef10384c4b
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:08:08 +0000

    defend owe next contain takeback

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
