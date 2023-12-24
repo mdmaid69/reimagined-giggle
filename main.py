a97553370f081c739fd7c0220e4cc316b815b094
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:49:37 +0000

    strike long send science private

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import sys
+def exit_program():
+        sys.exit()
