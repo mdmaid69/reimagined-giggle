63626cff4738bb8fd80201a28b36b665b7bd32a0
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:54:44 +0000

    569enjoy kind term smile contact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def is_even(n):
+        return n % 2 == 0
