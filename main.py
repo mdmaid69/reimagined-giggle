e3966ba2abb8037f6d73744792302531fb8b267d
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:04:49 +0000

    active anxious shame work skin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import time
+print(time.time())
