e1b9890c322bec11013753327fab3ec598d0e695
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:35:33 +0000

    attention vanish noone bridge maybe

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
