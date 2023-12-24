e85a93e452def9fdefcea8c6da3135d1d1c1ad0c
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:23:13 +0000

    worth support way lend Hh

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
