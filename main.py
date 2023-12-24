d4cff72be1e3b22e1f7fa1f4fe629d47ee0ed67b
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:47:19 +0000

    dance characteristic show male surprise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
