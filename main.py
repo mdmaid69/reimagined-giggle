2e35aaab023d7bcc579cf1335fe63ddcb8b438d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:56:30 +0000

    total follow quickly steal grateful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def square_number(x):
+        return x**2
