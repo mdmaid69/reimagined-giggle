e07d2496954d18a1313a45ea48852d11fb5133ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:49:12 +0000

    gooff better stay trade doctor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def convert_to_hex(n):
+        return hex(n)
