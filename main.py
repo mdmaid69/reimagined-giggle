c390f6038687a23201f6fa182715e17efd9bab0b
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:57:06 +0000

    experiment high card read secret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
