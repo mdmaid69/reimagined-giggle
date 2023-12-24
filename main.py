a5b5a1a329928db42daa600a51cdb2c47e9b9287
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:58:30 +0000

    see born adjective tax plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
