ee05a58eb4358e04c9004718db019a52c9bce030
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:52:28 +0000

    confident fasten way married be

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
