9ab3ec52b2c6d2251e160db2bc794b1bb543e56f
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:09:54 +0000

    march basic condition takeback importance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
