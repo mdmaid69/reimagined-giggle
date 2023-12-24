952e52beabb6671fc10f60952ab600b242a90753
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:21:38 +0000

    ride quantity marry fold display

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def is_odd(n):
-        return n % 2 != 0
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
