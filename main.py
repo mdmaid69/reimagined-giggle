fb14566a762c846c37f1ec4ab322d0be27e431e4
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:15:10 +0000

    patience seem seat hers rest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
