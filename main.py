fe965b07ce305dbd0bdd22c3decae32c622bb2db
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:24:06 +0000

    chin dream film stick capital

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
