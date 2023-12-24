418c4c8900568a98adbf72fd86ab056404afcfe0
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:10:11 +0000

    laugh comment expression basic offend

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
