e950c977c157497db249afbe4d449aa60ae8d33b
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:52:36 +0000

    powerful pride return read improvement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
