9fc915554caa1eeaf39a028948ea4522fffafc87
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:52:32 +0000

    congratulations study head mainly shoulder

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def sort_list(lst):
-        return sorted(lst)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
