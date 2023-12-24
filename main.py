4ed98c14216cd19fd280148e91840f6a2b1a0126
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:00:15 +0000

    send matter track guess strict

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_item(array, i):
+        return array[i]
