6c8b74ff2747c6f3b0f0e5e5addf8f65c7e64411
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:48:51 +0000

    as trust thumb study comeround

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def calculate_power(work, time):
-        return work / time
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
