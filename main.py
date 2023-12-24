1109d327ba513306cab8fa64f6ac8ff43d0eccd5
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:24:55 +0000

    dance deep afterwards through position

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
