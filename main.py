791ab644d28f5882e0743377e6a1448aa07ac011
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:16:01 +0000

    control error have bother aim

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
