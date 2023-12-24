3f7502e352b47f35ae7da6d91f3dbb88c9c0950a
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:14:59 +0000

    badly 570enjoyable centre present bottom

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
