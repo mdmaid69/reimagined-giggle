27766884b397cd28cce2ec060f07a62e4fe95546
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:15:15 +0000

    maintain ball workon should stare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
