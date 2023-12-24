6ac762ec7d32af0234fecd4166d3a59b1612698b
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:40:23 +0000

    opposite sorry talk ago defeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
