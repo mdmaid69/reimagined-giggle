caa6e8f41d9d0e98c9b734cf53e066b3f91281a9
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:36:18 +0000

    person build combine that responsibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
