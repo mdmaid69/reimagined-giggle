5e3026738a05ee4f98be91066eb367cb01c162c4
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:30:20 +0000

    children tap power touch language

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
