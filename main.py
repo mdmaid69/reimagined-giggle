7545ac2c376c71e65cd795c73d031babcec6a569
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:34:32 +0000

    him big receive impossible deliberately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def square_number(x):
+        return x**2
