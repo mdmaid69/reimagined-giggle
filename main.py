23287eda06c0e85b4b76dbf1e7c3d725bd6b398e
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:02:49 +0000

    rush ready already open reliable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
