0b14a79d2e3e04c1c7ed17fd1d672d48c13ef8ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:39:38 +0000

    married whether welcome better awake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_max(numbers):
+        return max(numbers)
