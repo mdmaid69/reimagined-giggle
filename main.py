9866f7aed8485f1c9219febf5df83c398d076e66
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:28:35 +0000

    pleasant pleasure argue lean language

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
