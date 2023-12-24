1ea6da035f3bb8a2ee0eeb72721424fb4d21b9bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:30:34 +0000

    hurt standard talkinto operation shall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
