5ed97a23edb0101c4ed3e8ee9215698aab50ad39
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:02:35 +0000

    performance attract bound petrol fast

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
