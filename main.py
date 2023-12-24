0f9cb820f0140ff8fcc6f520c0d0bd86e397f26a
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:17:25 +0000

    operation room stamp climb interesting

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
