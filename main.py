b3fb0bbebb20c932c687d38c53b43263d2ca8daa
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:23:58 +0000

    tool development woman national calloff

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_owner(file_name):
+  def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
