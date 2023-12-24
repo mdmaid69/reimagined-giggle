980e8ba2a2e98d1d9b589d3923616a6aa4b20efe
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:57:39 +0000

    gate please underneath translate smile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-print(sys.version)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
