dfdaed680e8dd4fb0b0b0b14be20f7496f9657f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:05:08 +0000

    getout average Hh sentence punishment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_work(force, distance):
+        return force * distance
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
