278dc7ef26967e06a12348a3273b0126e6b18126
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:12:09 +0000

    book step tread bus police

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_exponential(x):
+        return math.exp(x)
