2379009d261ea8642239bc46bd18c99455028c3e
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:06:59 +0000

    hurry apparently geton door project

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
