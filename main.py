04f20b64367360e6c542b897128006f462d9656b
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:09:21 +0000

    relative factory mouth winter smart

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
