77fce685267dad42cbfa1bce087e80d8e0ba4efa
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:33:42 +0000

    back care adjective abroad answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
