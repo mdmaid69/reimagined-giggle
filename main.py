484625f70baafd9d37646e754f609594e9e396cf
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:09:22 +0000

    gooff declare chin skin takeoff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import tensorflow as tf
+print(tf.__version__)
