1eb41ba9bb24c7a796d9dc9fe880946924aacf4d
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:14:19 +0000

    Tt bringback along dry talkback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_bool(array):
+        return bool(array)
