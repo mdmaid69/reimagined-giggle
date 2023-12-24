94f7296d711ead0fa86a447ea3696a5cd653f9f9
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:44:15 +0000

    wish upstairs welcome basic many

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def cube_number(x):
-        return x**3
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
