0d1582da2371bf85722080a9a74e161421fa0561
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:38:57 +0000

    rubbish environment flat everything lie

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def is_odd(n):
-        return n % 2 != 0
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
