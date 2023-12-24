8f47aec0b70bbe8e5ae9e724b4e3136f40b3a827
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:20:47 +0000

    certainly loss row professional guess

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-def calculate_work(force, distance):
-        return force * distance
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_power(work, time):
+        return work / time
