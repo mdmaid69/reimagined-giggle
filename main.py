6eb5a8cacf58510f961b43e3fd4f06b9ae2b52c3
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:58:16 +0000

    store blow treatment turndown able

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def sort_list(lst):
+        return sorted(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
