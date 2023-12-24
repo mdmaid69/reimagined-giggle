5d18e75dbcc74a11be7573258199c80a86390504
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:23:40 +0000

    allow leave imagine emphasis heart

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
