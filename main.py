f5e298a0957c001ad60b3959c5a2e0de83f83e63
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:09:31 +0000

    weekend partner strike Hh willing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
