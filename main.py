6321cd65fcf6f4b5b96fd4369ec0351c0baa23df
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:02:06 +0000

    violence behaviour produce burn play

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
