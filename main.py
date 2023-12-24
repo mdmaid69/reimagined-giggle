b0c9ec3721c47eea07c38cb54c912e1e2390b1eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:21:25 +0000

    tend announce patience claim flow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import sys
+  def get_python_version():
+        return sys.version
