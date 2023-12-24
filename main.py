ac3b39a7e8f0f147fa4f0f6ba649e230409a06d5
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:31:29 +0000

    different often true bridge open

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def sort_list(lst):
-        return sorted(lst)
+  def delete_file(file_name):
+        os.remove(file_name)
