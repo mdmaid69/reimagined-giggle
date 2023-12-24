da08b56ec540739f42138348e480a84e6c9878a6
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:46:13 +0000

    Kk busy likely neck absolutely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
