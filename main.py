8daa887bca051d12d42aca5abd5ef90686cb88db
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:59:29 +0000

    pleased cruelty any sale brief

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def clear_array(array):
+        array *= 0
