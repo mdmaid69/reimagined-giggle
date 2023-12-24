c18cc6f91007319c69cae3061c8e05a5f9ec652b
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:33:40 +0000

    fly sleep turndown empty splash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  def convert_to_octal(n):
+        return oct(n)
