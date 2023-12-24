f12c9d202077617e9226a93dafd3e7d2fb9d72e9
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:17:34 +0000

    cross share 179bite favourite wall

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def count_elements(lst):
+        return len(lst)
