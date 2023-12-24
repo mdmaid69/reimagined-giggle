49dae7e50fd2359f9b8b015043f7a352379f92b8
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:38:34 +0000

    base best interesting hour immediate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
