1be8786767b3154c76aede9dd8c69011b27ae687
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:35:47 +0000

    operate reason nurse choose scale

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
