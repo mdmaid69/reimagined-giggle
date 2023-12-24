0fc2917a3e3fcf2598ccebd4387ccb356f5aa08a
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:17:12 +0000

    damp example fair reduction need

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import collections
-def create_priority_queue():
-        return collections.deque()
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
