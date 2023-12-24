e162054f09bc9e53e55c06ea498129aff1b71e94
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:39:59 +0000

    throw back real past their

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
