6aef866e8c2324d5f9cb005494af4d2a88334f0f
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:36:28 +0000

    law fall drink quantity insist

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
