e139ace3379ce31ccf0be6f13f5c617318356d09
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:46:20 +0000

    accident least offence computer moon

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import collections
-def create_user_string():
-        return collections.UserString()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
