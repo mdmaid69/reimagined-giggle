9d349b5297952954bb9eb5bb9d620a291aed1756
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:12:47 +0000

    port hot calmly task base

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
