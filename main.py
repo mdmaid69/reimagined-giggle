eddf919053e968e10d5629bb911fd110306f658f
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:51:39 +0000

    wind whistle miss have before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
