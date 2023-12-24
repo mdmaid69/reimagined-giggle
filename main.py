706d515108ff9dd58ea89eadc4a282e9f02f5d1a
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:31:10 +0000

    apply protection loan several worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import getpass
+def get_username():
+        return getpass.getuser()
