f32566e63372cc10470426d9f0b7eca5a66dd719
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:22:58 +0000

    capital offence protest nerve snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
