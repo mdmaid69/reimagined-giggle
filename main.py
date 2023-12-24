0395f113f9072e973d101272034d81a1791ac074
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:34:08 +0000

    row either about software discussion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def get_array_as_int(array):
+        return int(array[0])
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
