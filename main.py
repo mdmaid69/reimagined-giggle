48a8070f752266cd535b7805514fef62957843df
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:25:52 +0000

    supply wealth comfortable pleased land

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
