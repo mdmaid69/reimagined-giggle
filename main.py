bdafe3cc773a42e5f4ebeddea2caff197d8be8fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:58:54 +0000

    judge giveback Uu direct snow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_as_int(array):
+        return int(array[0])
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
