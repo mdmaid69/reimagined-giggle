67fa78e4b41b6a770369844246730960eb971109
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:08:29 +0000

    nowhere battery emphasis division perhaps

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def convert_to_octal(n):
+        return oct(n)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
