47bde5dbb60226c50bc2a175032285917b5f5286
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:48:18 +0000

    respect cover hard gentle large

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_array_item(array, i):
+        return array[i]
