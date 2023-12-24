9ec66ad0e3395b19860b4d0cd86efc0d565eab01
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:55:22 +0000

    except pick unusual scale drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
