6758d2b751aad4cb37152411e26bdff8c51a7c33
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:51:36 +0000

    godown tooth advanced fresh wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
+  def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
