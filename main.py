c91c654e6fa1c5e01a48f79146efc444d5dba971
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:11:55 +0000

    deal advice forward hospital takeaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
