73e68fe29916bee883b4e078913747ea2ec5757b
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:08:43 +0000

    upset understand exercise remark true

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
