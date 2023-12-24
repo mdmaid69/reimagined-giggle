2f10ed0d8d2931acb88221face62e2db05dcca56
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:57:31 +0000

    moment improvement switch answer fortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
