a6abd3a1033392e6556de67a56c4d5dd26f4cd67
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:14:28 +0000

    tonight collection post keepout paper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import datetime
+print(datetime.datetime.now())
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
