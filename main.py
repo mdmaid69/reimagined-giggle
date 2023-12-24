69a5e63d4e7101b7124b212c7e7f4b19e9b200cd
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:51:34 +0000

    birth tongue yours brave mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
