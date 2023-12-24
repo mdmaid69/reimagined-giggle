3a3a33695515b618ea2da04db3adf1767f715db7
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:26:42 +0000

    rate basis suit moral nothing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
