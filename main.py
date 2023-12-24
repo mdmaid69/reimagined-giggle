f1689a86fec7bf5e0df74c88d14c38117e158bbd
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:51:38 +0000

    welcome main law outside past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
