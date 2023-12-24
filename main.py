22acfa31ededbc5fe35283541f5cfffd25302a6f
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:41:08 +0000

    suit sensible sound thirsty burn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
