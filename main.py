bfb5c3656e4e68d810e35ee4705eb8cfca1bd3b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:04:14 +0000

    possession brief neck soil deserve

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def convert_to_hex(n):
+        return hex(n)
