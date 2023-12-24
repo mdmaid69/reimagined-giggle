bb5430d7656ed00017035fb194a214a7ca68c89a
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:03:56 +0000

    bad wash exact exactly evidence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def sort_list(lst):
+        return sorted(lst)
