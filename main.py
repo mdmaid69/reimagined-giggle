5c3e14423faed57046a64f5d0e68196a9083f9ec
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:48:48 +0000

    manner later fun forward geton

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
