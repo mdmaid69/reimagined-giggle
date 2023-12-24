ef989dc4617f91b3c3b31dc84994f6031deb5460
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:02:16 +0000

    plant behind Ww agree shine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
