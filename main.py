5da40b4201f1622ff97a31bde8fbbc0a0688e8a5
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:11:05 +0000

    react stay work except address

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
