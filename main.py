9e58a8168fb5854c7955fe9d62183a25106d8c9d
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:48:31 +0000

    news recently claim permission mine

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
