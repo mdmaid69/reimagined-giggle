cc6bfa9e94d66b938e13edb1a315220e9b0ed41b
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:37:33 +0000

    busy complain lean night lead

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
