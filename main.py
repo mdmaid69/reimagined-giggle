ff8288f64c631ef612c2696e841a4c5154b2e199
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:29:56 +0000

    allow suggestion regret rubbish manner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
