4ba2fc8f494df2108cc615bb7945db4e550a634d
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:43:52 +0000

    country plan shake breathe such

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
