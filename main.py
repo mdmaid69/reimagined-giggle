4c06870aed74ac810b47ca3ad48a38ae92720139
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:25:49 +0000

    scheme properly slightly custom male

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import array
+def reverse_array(array):
+        array.reverse()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
