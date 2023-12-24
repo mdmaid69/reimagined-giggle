2b327ed5280f17ee1112cc65273398638ff0dc06
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:25:28 +0000

    attempt Ss straight cost platform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def sort_list(lst):
+        return sorted(lst)
