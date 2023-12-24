de0065d35e0dae5b343772d7236506fc1d879bf3
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:20:42 +0000

    clean bottom hour calm rise

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
