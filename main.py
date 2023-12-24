a72f4c536a44daa40cba8a9c93aee68c9feffb7e
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:08:08 +0000

    loud remark except splash private

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
