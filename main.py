a2200ac8c9a91e537af0537ad68e7114115e4a3c
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:09:36 +0000

    nerve business not scream what

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_work(force, distance):
-        return force * distance
+def insert_into_array(array, i, item):
+        array.insert(i, item)
