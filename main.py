e6e79a5fc348e428519da416708d38d9480d7a66
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:58:07 +0000

    occasional violent hot length serious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_current_directory():
+        return os.getcwd()
