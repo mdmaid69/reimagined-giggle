474adb8a9ea50c52ae0ad1fbeb40050e9c04333e
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:29:35 +0000

    off equipment reduce rub put

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
