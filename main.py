94e8e2b435707c5a196bbbcb548198942694926c
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:49:17 +0000

    common tight rub quiet terrible

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def calculate_variance(arr):
+        return np.var(arr)
