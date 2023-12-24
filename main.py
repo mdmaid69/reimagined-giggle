767dc8798b8a3335781599a38eff305c75df4d43
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:41:44 +0000

    yes branch full sky stiff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_user_string():
+        return collections.UserString()
