94dc2a0d24cf517c001eb0e9a7efdbf59fe73ebf
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:54:14 +0000

    warm Zz mood unfortunate foreign

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
