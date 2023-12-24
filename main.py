6fbf5a405928cd79cfc502b32b5e49a5bed2ca0f
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:42:22 +0000

    space indeed board shock trade

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
