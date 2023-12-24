c46942769425a5e810336304aaad9a0ab5653436
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:25:02 +0000

    lot direct report settle copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
