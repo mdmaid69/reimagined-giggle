34b75807e22e2f24cd5d152b3bdbdbdac8a20978
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:05:02 +0000

    lonely coat broken she us

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
