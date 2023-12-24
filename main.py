333f69df26bc7ea5800ba9122d6e82ec19bd9e3e
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:52:24 +0000

    certainly club burst build computer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
