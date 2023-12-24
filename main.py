b58135e075020bc6108f53b5526fbc4ad2ef76bb
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:26:25 +0000

    maintain up relief hardly Cc

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
