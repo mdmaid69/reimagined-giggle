1c0bdd8607bc9c83c315e31b6fbbe4620c612f5f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:46:49 +0000

    release protection cook wind wrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import json
+print(json.dumps({"name": "John", "age": 30}))
