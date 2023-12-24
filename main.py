f5e5674e654dff3c9368548d83920d5c2fb6850d
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:43:30 +0000

    funny respect bus prefer today

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
