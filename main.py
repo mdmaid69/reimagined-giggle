dd691f9c984feb5fb37cbad0423d6b098d2412f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:46:04 +0000

    back last achieve forward ring

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import logging
+def log_message(message):
+        logging.info(message)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
