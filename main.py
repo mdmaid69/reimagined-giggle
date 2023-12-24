73d7e5293b8246eae5978db0239d85cc6118b86c
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:47:23 +0000

    operation understand dick march experiment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def get_array_as_set(array):
+        return set(array)
