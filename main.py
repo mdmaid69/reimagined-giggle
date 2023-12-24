156987535c4823ea27a52648b211f501455e22ba
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:41:21 +0000

    reduce tool state regular block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
