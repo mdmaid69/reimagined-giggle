fed546436c85b3c3d7190e1dd33f291fd537f767
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:36:52 +0000

    beyond patient long challenge correct

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_typecode(array):
+        return array.typecode
