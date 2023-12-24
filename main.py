2addb22a0120ed03b78968cd302933abb391092e
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:57:08 +0000

    hit mass back side minute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
