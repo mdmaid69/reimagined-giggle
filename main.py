71ad0338bccfa5d8839810c19daa6d978d20b6aa
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:55:14 +0000

    shut tip ill regional stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
