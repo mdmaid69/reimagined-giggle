fe635ac0091763cd641b01fee7ce74416d58298f
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:03:59 +0000

    punish decision town discuss sink

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
