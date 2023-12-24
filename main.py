0c23cf45217164cbaf7ebe6ca4df738dca8b72cc
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:45:45 +0000

    train mix correction ocean step

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sys
+def exit_program():
+        sys.exit()
