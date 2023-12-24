02e209618666c75495d2ba6335d51b1528317018
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:33:37 +0000

    skirt shake shock education rush

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
