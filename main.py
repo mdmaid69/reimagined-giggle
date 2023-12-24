85c2b571cafc759b96db7493c438b0fce1b2b639
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:21:11 +0000

    further there middle sort past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
