74690b31be6c92184e29386af61fc1a3b9f61e75
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:47:13 +0000

    mess open computer politics keen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
