95844f934d83dbd62cd9f17395e1974409dd4744
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:57:16 +0000

    objection grow ordinary why stiff

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+print("Hello, world!")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
