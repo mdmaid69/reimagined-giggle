34bae505593a62924bb3cb71c248853ff8d6b641
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:49:41 +0000

    tie heat sky furniture loose

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def multiply_numbers(x, y):
+        return x * y
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
