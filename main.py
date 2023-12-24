dc80b13370b6c523a3c5f7322cd323c1e2e7be0b
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:35:03 +0000

    maintain tip eye surprised workupto

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_speed(distance, time):
+        return distance / time
