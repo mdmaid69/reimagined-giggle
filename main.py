bc3d784d5d6084ab5f397bc101b38ac4fb3d8d69
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:22:19 +0000

    make issue skill raise say

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
