f8bef4f56685c3db45b2ed1fc1dcd0a4191f7872
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:31:08 +0000

    reaction pressure this right change

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_factorial(n):
+        return math.factorial(n)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
