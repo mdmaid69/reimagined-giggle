a8664a2b3881f1487ac0588e45db692998d6fda5
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:23:09 +0000

    suggestion entry detail small meal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_density(mass, volume):
+        return mass / volume
