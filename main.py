12104734b8f2f354dc181f58584e41e4d42a1ab5
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:00:26 +0000

    die tidy involve involve club

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
