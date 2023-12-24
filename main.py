b2ed2b1ac6a1168d0b6bf8ec6e9a513ae37eb87f
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:49:26 +0000

    visit provide sort exchange holdon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_area_circle(r):
+        return 3.14 * r**2
