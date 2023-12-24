ea62ae354074286a399f52579c823cefe608c4fe
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:41:47 +0000

    miss complete lookback(on) only below

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_work(force, distance):
-        return force * distance
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
