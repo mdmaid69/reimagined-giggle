769740916a3c9dbbc68dd4f4afa84bb5a6dcaeaa
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:17:10 +0000

    mine object Zz taste whisper

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
