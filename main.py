27a7b8f7dc104ea340ba5e3a3fca12102ba5766b
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:19:55 +0000

    mistake comeover nor peaceful own

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
