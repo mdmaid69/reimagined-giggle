816d94eb8f50dbcf5ac06b560372988cb1a0b842
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:45:03 +0000

    modern congratulate doubt arrange train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
