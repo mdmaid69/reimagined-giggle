8d4bb16e17e911aefba5c89a9359a9590f5d3478
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:43:29 +0000

    wrong effort weekend increase road

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
