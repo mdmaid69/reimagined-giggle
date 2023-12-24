d12cb6a21b1076a2077574cd9adebb347b2fbf38
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:49:09 +0000

    loud maximum smile sign study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
