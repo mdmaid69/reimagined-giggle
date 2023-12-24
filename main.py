7d9324847256e369ff5f31d12d62838c7ac08f34
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:26:44 +0000

    culture busy correction big deliberately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
