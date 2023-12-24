cbf87c703621f751055fe2f91d14ca8b41a27cf4
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:00:18 +0000

    sink towards list wall touch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+print("Hello, world!")
