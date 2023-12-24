5abecb54a403f36d982263724c94ed5f6ddbf6f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:44:53 +0000

    outside result precise break dry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+from collections import Counter
+print(Counter("hello world"))
