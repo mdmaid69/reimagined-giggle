15c6d3c40e0cafc98aa61b0ff09e3a40ba3bc431
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:33:00 +0000

    bad ill sign settle neat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import time
-print(time.time())
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
