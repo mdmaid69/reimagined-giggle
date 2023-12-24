3849cd2283869cfd86ac55683270e355674da8cd
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:22:19 +0000

    goup miss bear forever shelter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
