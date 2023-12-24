ab9e32c940f83d583f3dad3a1ee96884e95d8317
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:41:10 +0000

    concerning no deserve bin reach

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
