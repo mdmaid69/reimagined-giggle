323f4c79e4c1f1c8b8c6e037b497b18ecc720bcf
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:29:06 +0000

    patient fortunate ignore each organize

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def delete_file(file_name):
+        os.remove(file_name)
