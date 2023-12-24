b29f59dd939b0a071ed43f7a14aac840cff67d46
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:37:58 +0000

    bored slice finger 571enough rain

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
