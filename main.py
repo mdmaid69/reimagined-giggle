ca82df2104347144d8c083dea3b5bb96739cc403
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:23:39 +0000

    over generous up 568energy forward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
