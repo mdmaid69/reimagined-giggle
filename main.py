28416a89470d3da38d4116d829647dec26d38912
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:48:03 +0000

    attempt care sister soil afterwards

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
