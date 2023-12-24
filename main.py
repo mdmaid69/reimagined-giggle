0052cf29f340dd5f4f66e6dafad30a541628e197
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:20:57 +0000

    news future hungry maybe rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
