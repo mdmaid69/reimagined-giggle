87664d298838434e1ea1e34e21c0bcdb6b058b0b
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:05:39 +0000

    shape pin teach fond tape

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def subtract_numbers(x, y):
+        return x - y
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
