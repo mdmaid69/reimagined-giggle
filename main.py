4eb014bac4816c0ac1d1284758b6f19ceb92e4db
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:51:31 +0000

    profit voice term store guess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
