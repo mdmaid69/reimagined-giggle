6f888cd2e7c50bf02d3ac19624c0119c1dcf4f23
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:47:28 +0000

    whom keep fine square kind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
