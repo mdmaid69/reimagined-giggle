08aad2dcf931bbf9c54b84189060bb04d1dc4a56
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:44:24 +0000

    mainly reduction scheme slowly pour

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def cube_number(x):
+        return x**3
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
