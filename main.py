6e2b951ce58eba62548522b4c5ddf330de12c160
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:58:02 +0000

    dead relax shoulder another joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
