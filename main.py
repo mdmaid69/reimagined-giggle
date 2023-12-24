28c2dba72af1666f1e09bd8e49125485bf2206b9
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:12:57 +0000

    hot clean estimate quit upset

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 def calculate_quick_ratio(current_assets, inventory, current_liabilities):
         return (current_assets - inventory) / current_liabilities
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
