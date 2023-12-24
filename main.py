dea6db65b23a5eff15335400732bf09c4f731898
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:35:06 +0000

    weather pay crash what combine

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
