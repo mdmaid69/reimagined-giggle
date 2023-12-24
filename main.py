227a5fbb5b45c16e83758970249993b6442e6451
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:08:11 +0000

    hungry hand official table persuade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def count_characters(sentence):
+        return len(sentence)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
