43d6c9cb32c48c97bec5eb5576bc3db78ce2ed7a
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:59:20 +0000

    rise on comeround hello fasten

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def reverse_list(lst):
+        return lst[::-1]
