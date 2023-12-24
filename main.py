3d606cd4becaae7f1251d6a6356d11e7e32da53d
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:23:21 +0000

    occur woman clearly whichever long

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_min(lst):
-        return min(lst)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
