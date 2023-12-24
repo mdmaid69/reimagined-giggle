4d8978d1f25dbe6bb013dd842df5aae515d4029b
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:12:30 +0000

    exercise sound probably able model

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
