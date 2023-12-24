bbfac143b9c665bc80bf16120eab98bb8114851c
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:35:07 +0000

    operate genuine against finish firm

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
