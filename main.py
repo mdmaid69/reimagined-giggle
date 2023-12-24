1aa432032989ef95551ad147b00c38fdc79387de
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:50:15 +0000

    him recently possession naturally pair

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_power(work, time):
+        return work / time
+import time
+print(time.time())
