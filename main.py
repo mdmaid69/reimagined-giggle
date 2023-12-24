0b02c62eee35646a0a65e8047b58c88fdb0e94f9
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:46:25 +0000

    this whisper wash famous entrance

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
