2387e4e82f8f96cf915d5acf22cbf2be7db8450f
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:48:51 +0000

    special steep direction work flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def get_array_as_complex(array):
+        return complex(array[0])
