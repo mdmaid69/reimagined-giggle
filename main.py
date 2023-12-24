999ce061952f83ffd65d739352387ecbfd9c8ae3
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:58:03 +0000

    tap mix arrive announce clothes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def reverse_string(s):
-        return s[::-1]
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_int(array):
+        return int(array[0])
