e61638db305f0c762e680f38683f7ae8408e6656
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:52:37 +0000

    born petrol ill Ll polite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
