faf1247b6c890ee6d8f66c8a9856a3eb0be05657
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:01:41 +0000

    pupil bottom vote Zz various

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
