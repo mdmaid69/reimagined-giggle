a1742742591ce877cc380b3de244fad37ee3b5a9
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:22:31 +0000

    young mention raise brother serious

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def calculate_acceleration(speed, time):
+        return speed / time
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
