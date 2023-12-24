af20dfdeea3b9f573d53e6b35b3f3ac688f2b411
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:09:36 +0000

    weekend far more bottom knowledge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_current_directory():
+        return os.getcwd()
