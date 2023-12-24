272ae6be6b0052867f656cecd52320eda1f80e79
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:33:42 +0000

    arrival early list raw music

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+name = "Python"
+print("Hello,", name)
