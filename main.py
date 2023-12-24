139a808ff71718f5c540710243373251cda42cd3
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:28:31 +0000

    popular loan crash piece congratulations

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import socket
 
-{body}")
-import math
-print(math.pi)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
