c18ba32c5a6c021ce602212b5fee25755294083d
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:56:13 +0000

    sometimes tear sister middle deliberate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_power(work, time):
+        return work / time
