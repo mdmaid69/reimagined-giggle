9a833c8769e848ab04cae56a4b0e6b5d938be18f
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:34:52 +0000

    maintain study game neck frequent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
