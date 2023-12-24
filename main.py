9b1a880f034a29f7d6d989b2eda815df922d91ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:38:21 +0000

    degree curious extremely disturb spray

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
