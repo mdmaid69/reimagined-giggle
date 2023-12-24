4db8fedde53a8888b08877d73030d59a5af6f37f
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:09:26 +0000

    medium as blame get interruption

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def convert_to_binary(n):
+        return bin(n)
