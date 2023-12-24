efc36d80df3b5db344ab4a3d1a6ca4c09607f305
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:58:08 +0000

    shrink war pipe comment job

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
