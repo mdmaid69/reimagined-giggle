3a7c2c0797adefda907d761c01226cb28f1450f2
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:33:22 +0000

    design truth interfere last solve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def sort_numbers(numbers):
+        return sorted(numbers)
