338d89c692948e52d9d4ee7b7d5e73e150d14000
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:06:09 +0000

    comfort surface train small but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sys
+def print_python_version():
+        print(sys.version)
+import array
+def get_array_typecode(array):
+        return array.typecode
