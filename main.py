1909d7daa93b215c7b739dcfb6f21163ea545993
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:19:36 +0000

    shock hair generous divide hello

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
