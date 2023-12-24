63038c9d45f19f5aba31acb94dfd26b0a929f846
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:26:20 +0000

    holiday finish soak declaration walk

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
