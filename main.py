c440a361936aff4b22e8ff3b7399decc0e23e66d
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:43:37 +0000

    nice smell grade afford like

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-for i in range(5):
-        print(i)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
