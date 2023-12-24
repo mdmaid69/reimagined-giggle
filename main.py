adef4b4877bbadc5df48b5933ee833b171f54c8f
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:27:36 +0000

    importance else Yy dance comedown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
