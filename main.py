1498dcafc30fd1e28425884fabab8546a7c2e476
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:57:50 +0000

    getoff with short holdon address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
