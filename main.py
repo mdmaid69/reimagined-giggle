cb773ee880039f391995ee4536033c3e8e780539
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:54:44 +0000

    occur blame excuse fetch thirsty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import time
+print(time.time())
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
