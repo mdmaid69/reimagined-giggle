337225e064dc969d63a6f40094d874e88b2ed8c4
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:48:59 +0000

    embarrassing story quiet variety of

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def reverse_list(lst):
-        return lst[::-1]
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
