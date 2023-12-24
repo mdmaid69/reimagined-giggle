3c61e4f8b3029c9bf5415d481e749f3158f3bf98
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:28:58 +0000

    estimate shock car goaway I

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
