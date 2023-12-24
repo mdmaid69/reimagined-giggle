dc238253facbe3e77a231e562457858d6e21bc67
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:19:52 +0000

    mother legal off Itself stretch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
