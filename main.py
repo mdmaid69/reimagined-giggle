14a07043c7c998bde06cd2d0d1c01562acee6432
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:58:09 +0000

    bus secret skill shoot wheel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
