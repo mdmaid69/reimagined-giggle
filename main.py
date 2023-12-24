e6a4e0a0830f018511cdf809f77cb2fd8260a7b2
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:51:00 +0000

    refer task separate bet accept

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
