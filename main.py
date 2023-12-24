a6a3fbb0583804a7fcc452902a88a5d37ae55f8c
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:34:29 +0000

    brush room cool soon instrument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
