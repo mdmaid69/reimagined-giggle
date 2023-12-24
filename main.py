e776549b1ebd43cf742a26fc0df167c682cdf25c
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:49:25 +0000

    whether path not murder choose

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
