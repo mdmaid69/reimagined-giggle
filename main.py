45082186ac80d636162959a5b759c2e402e7b1a7
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:02:11 +0000

    uncertain organization kind slice treatment

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
