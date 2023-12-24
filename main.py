bde08be20fdbdfba6ad4baa20cb14252fcce4da8
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:52:26 +0000

    shopping gap should everything demand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
