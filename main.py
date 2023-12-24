40a88aef41034ee1251aa2dc0237afa4da9d5520
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:54:05 +0000

    characteristic apart climb practise sense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
