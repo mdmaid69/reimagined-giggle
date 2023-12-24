0eaff2b755385e89330cd29dfae2e5e146e4077f
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:14:37 +0000

    across very progress go trap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
