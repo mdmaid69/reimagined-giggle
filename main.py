f21d5a383d4e4b3dc4b18833028ff8c8c61a9918
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:19:30 +0000

    takeoff office like short rent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
