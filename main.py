4e6b5d906157cf2e148f8ea39e736b671048b27b
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:40:30 +0000

    can once shade far content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def find_min(numbers):
+        return min(numbers)
