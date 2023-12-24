1630b417b9d1bdf5a3b77e1a419743092b4da278
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:08:36 +0000

    quarter without again block charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
