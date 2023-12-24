f2bb9e23b2bd8400f6ca73ca194c8f455305ac9c
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:29:45 +0000

    tone fear sort include information

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def get_array_length(array):
+        return len(array)
