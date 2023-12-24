bfe9d1193cafa947d24ca6b831bf9b3343bc0dc1
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:23:30 +0000

    hang minimum through bus thing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import random
+print(random.randint(0, 100))
