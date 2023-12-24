cb83f92484fc06fbb1fea87d4c28b156462beb7b
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:44:42 +0000

    boil drive finish waste press

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  def sort_list(lst):
-        return sorted(lst)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
