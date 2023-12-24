c68bd4f425651cafe08a6df18aa7524a89531cd5
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:55:16 +0000

    major production half wear breath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
