74eeed0b9446ba2e99b8cdd44c85bf3f9577348a
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:31:37 +0000

    fire attention skin little upset

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_list_from_array(array):
+        return array.tolist()
