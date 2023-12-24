267379a36a7bf3153f71bac78d23f3df2a6546fe
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:47:55 +0000

    money wide crash wake breathe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import sys
-print(sys.version)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area(radius):
+        return 3.14 * radius * radius
