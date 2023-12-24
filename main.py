7d4f03d42d1bf4f36940fd239925cfac010a1690
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:41:03 +0000

    plant runover action exciting history

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import collections
+def create_queue():
+        return collections.deque()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
