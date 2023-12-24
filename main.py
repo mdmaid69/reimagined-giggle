3528060f41513b8f1da257fe999716ee87325d77
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:06:32 +0000

    travel object dream thorough less

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import time
+def get_current_time():
+        return time.ctime()
