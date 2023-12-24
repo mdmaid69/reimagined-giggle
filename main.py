49c830a33f070535f90dfd63dacbe0a0d1a78a49
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:57:39 +0000

    bet beginning kill from record

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
