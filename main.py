061f0029ea5c79da513aea6b13f12eec2b282d5f
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:54:17 +0000

    born search pause goback(to) suck

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
