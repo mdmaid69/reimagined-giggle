52e3e7a5c873babed3783b9f92131ebc8c3279ea
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:26:16 +0000

    goback(to) comeup mess block makeup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_power(work, time):
+        return work / time
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
