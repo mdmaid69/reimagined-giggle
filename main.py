010bf4fac54228281ca77662954d4efc0e45b898
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:37:42 +0000

    male inside once Zz mother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import random
+print(random.randint(0, 100))
