cd30ee8c14088400ba72b3edb465efcd209478d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:07:52 +0000

    weak instead refuse surprised grade

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
