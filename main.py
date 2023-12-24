45ae9e580802c6701384b9cb4aaf1e159a47a58c
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:18:33 +0000

    history call wrong stamp bunch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_floor(x):
+        return math.floor(x)
+for i in range(10): print(i)
