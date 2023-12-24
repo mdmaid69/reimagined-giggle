e2d1b866b553c678a57256250ff87a6364bfb408
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:33:15 +0000

    return quick typical hole long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
