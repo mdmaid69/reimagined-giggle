f67589a45a5a32dce18728380e324b57b9fd3750
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:44:46 +0000

    your opportunity practise tradition finger

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
