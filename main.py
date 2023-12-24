ba10bf29bb1fa22e26abd3a179508aff69447083
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:16:30 +0000

    trousers hurry since beat material

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+for i in range(5):
+        print(i)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
