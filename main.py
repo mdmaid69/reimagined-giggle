539c580d3050513d0a66e8bdd8809d8808bd22b8
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:31:12 +0000

    shake spray manner exactly ashamed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
