ac8688c4f5df3fbd99b41225b54a7b883216e74b
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:48:09 +0000

    but square long steal result

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  def is_odd(n):
-        return n % 2 != 0
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
