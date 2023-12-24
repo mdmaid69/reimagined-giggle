cdcd9b0d635083825960a9f875651f94829229fd
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:14:21 +0000

    concerning home no win your

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def subtract_numbers(x, y):
-        return x - y
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
