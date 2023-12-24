4dedea101426b2f67675bd8256fc284ac5d69b42
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:32:46 +0000

    compete scared stretch nice oil

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
