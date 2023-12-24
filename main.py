0c74a4a821b41c466ce46535178d9cd9067ae74c
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:41:34 +0000

    suffer nigga perfect possibility brain

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import time
 def measure_execution_time(func, *args):
         start = time.time()
@@ -8,3 +5,5 @@ def measure_execution_time(func, *args):
         end = time.time()
         print(f"Execution time: {end - start} seconds")
         return result
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
