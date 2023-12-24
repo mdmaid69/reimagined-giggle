d49ba809175377d3a4c957bd2ae2aa6381bb114c
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:09:37 +0000

    majority mouth birth building world

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  def is_odd(n):
+        return n % 2 != 0
