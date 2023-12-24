ba06de740b13fede3adf0473d79cffae20fd70d7
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:51:42 +0000

    influence spend tradition appeal opportunity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
