40291d863a12bbe0e3ef47ce7d0f56e7967835fe
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:24:34 +0000

    Rr fall himself decision wind

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
