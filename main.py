cc436b475f94a73ad44f9ad427f6fc91314a5210
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:43:02 +0000

    where responsible turn drop future

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
