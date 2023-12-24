4ba2731dadf5f7b77ac13d27e456fd3828f00905
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:46:44 +0000

    move your characteristic tragedy last

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
