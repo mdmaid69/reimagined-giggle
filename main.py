240440ad65e0e88d37cbe6b99e944055169df740
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:18:25 +0000

    grateful consist slide knock person

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
