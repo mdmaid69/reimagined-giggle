6df937f955370704c26b938444668fbc2c2e21e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:32:03 +0000

    comeup exact exercise bound tool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_work(force, distance):
+        return force * distance
