665e52fc3a1d00bf0e71c9794357b8779f391810
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:24:14 +0000

    spite runout pattern spot unhappy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import os
+print(os.getcwd())
