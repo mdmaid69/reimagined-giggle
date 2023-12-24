2c3d39c4d4acaff9774aca9b07e8030782e82d98
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:22:52 +0000

    giveup deal thank confident view

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
