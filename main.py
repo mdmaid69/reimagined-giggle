8d308b96b72b6c36ee6668f870af166169ce5b7f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:54:30 +0000

    steer only gather include secret

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
