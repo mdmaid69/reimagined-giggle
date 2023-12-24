954de438fe97e14b0070b493f08791995e4f6fac
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:33:13 +0000

    foot reply getout smile notice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
