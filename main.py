da0df12891104aa8c9997bdf807ff9b68dd6322e
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:25:09 +0000

    should act uponon frightened instruction

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
