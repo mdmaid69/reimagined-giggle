4013addc93da82f46a156030334c4e1b5fc40be9
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:31:24 +0000

    guard concerned chief truth themselves

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
