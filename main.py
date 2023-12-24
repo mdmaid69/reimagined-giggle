dbc05e1a3cb5061070398b9b0c3bf83e48992e00
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:28:22 +0000

    chin vote fierce joke Jj

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def count_characters(sentence):
-        return len(sentence)
+def calculate_power(work, time):
+        return work / time
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
