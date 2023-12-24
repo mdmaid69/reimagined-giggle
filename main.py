266862d6bad050ee287c47d0e61a5d82f0a9377d
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:09:06 +0000

    feature once least goout goout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
