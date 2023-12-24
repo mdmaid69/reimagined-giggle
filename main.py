000969a4b6b58fcb631079431715bbaffc9d0f8d
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:43:43 +0000

    pleasure entrance connected vision pipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
