7568a536c539ba79870707f116f2694713f29dfe
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:14:37 +0000

    apparently later our remark undo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
