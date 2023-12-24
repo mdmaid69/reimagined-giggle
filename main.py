cc3553a945200c8c7f8250aa61cbaf5b094a2ce1
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:43:17 +0000

    right guess right dull beginning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import time
+print(time.time())
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
