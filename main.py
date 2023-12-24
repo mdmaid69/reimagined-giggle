9390fd2d0a8e22349bc8feb3e90bed518f4bfc75
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:01:03 +0000

    determined gate room body under

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
