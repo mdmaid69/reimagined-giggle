89216221208b7eb7da2cb4a9fa25b7d3f1bb8ffe
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:04:16 +0000

    advise sweet content demonstrate feeling

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
