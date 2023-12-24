56a465fcbdcde86b90865a0ca5526c65209c8c6d
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:24:32 +0000

    convinced face unkind healthy board

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_item(array, i):
-        return array[i]
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
