898f0ee5e5256bf069bc789545c79de785ed321f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:07:47 +0000

    separate know weekend term heart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import os
+def get_current_working_directory():
+        return os.getcwd()
