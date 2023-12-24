b8455c15e7e11cf31b4b1ea94088187c73e78a7b
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:23:05 +0000

    thick dare rare art Kk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import sys
+  def get_python_version():
+        return sys.version
