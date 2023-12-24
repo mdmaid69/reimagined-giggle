33a252e27fa787635e54886ef3f704f90a1bfc78
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:31:28 +0000

    anywhere related error pin perfect

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
