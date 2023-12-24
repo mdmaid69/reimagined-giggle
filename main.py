8b8ab47930e232ab575a08c7e21e00c807a0e17c
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:14:22 +0000

    tooth cold various number dance

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
