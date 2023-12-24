04148b1b30306d1d5454c3aeb0bd0309cc33ea39
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:10:44 +0000

    confused report tall stone there

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
