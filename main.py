4ce123c55a5ac498edc24cc87b75307d592773dd
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:23:41 +0000

    secret responsible of wherever unhappy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
