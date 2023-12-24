1faacc48948a329e36e30ef1c80b36df897a9144
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:53:11 +0000

    why exciting throw show concentration

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
