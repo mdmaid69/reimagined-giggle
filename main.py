882c2960dd110558fa9a5574f2527cfbae6d3f52
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:35:16 +0000

    about may anybody violent advantage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
