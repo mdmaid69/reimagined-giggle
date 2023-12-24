e8ea8e591a40c6b48582cd17b827acfbd30c18d8
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:25:06 +0000

    attitude wish anywayanyhow confuse cheque

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
