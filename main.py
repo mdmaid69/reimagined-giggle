100db591eb8e2c14a1a1553fa40ef57807f28aed
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:08:58 +0000

    like sign load actually pool

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def cube_number(x):
+        return x**3
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
