525d5e3470e570a6f58b5d91f6d80ce915b1df2b
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:33:31 +0000

    score stamp some mark clean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
