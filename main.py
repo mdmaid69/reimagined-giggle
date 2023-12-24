9390a6db504effe02b35a75553bbb90f3f8fe54a
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:07:51 +0000

    attempt rate rescue crowd but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
