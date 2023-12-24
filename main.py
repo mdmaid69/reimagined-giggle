5f12661046f398c7195986782b1f95b4a821c938
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:19:22 +0000

    really intend easy nervous substance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
 import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
