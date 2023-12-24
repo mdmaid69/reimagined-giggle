51750462fd690e1df64c709d6ac85a7feb6737d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:32:00 +0000

    culture through underneath famous anxious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import time
-def get_time_since_epoch():
-        return time.time()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_power(work, time):
+        return work / time
