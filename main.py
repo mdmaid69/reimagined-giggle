020a7a02a57659faa64bafde17ba42b3e3964486
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:16:25 +0000

    blame human beside brown name

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
