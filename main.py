210bceef1c65f5c1d54a691f00ceb5537f48d245
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:19:08 +0000

    at every account equipment justice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def create_priority_queue():
+        return collections.deque()
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
