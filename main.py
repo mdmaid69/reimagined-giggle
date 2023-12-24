f2b9c6706b56f9e9a01f2d93010870453b7902dd
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:46:19 +0000

    hope cool however yet rain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
