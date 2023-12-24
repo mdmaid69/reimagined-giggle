1bbd9dd2670e4c0f79117c70bc35ace9814a3cea
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:57:49 +0000

    block worth back ashamed exaggerate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
