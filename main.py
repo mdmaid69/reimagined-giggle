4265e6ffa604e265e9604b36752452624842e88d
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:29:13 +0000

    hers square tone lift proof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def convert_array_to_list(array):
+        return array.tolist()
