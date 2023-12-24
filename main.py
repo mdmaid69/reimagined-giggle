5f1e5289fed70112e97bdef240f1c79a71c75de5
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:10:26 +0000

    point flow warning afterwards basically

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
