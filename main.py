c8f50a6341475827af380ea49ec38e872387c072
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:06:32 +0000

    offer giveout beat question cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
