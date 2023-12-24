b4a14449dfab395025e0c77db0bb64efaff6873a
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:35:10 +0000

    squash man prepare official intelligent

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
