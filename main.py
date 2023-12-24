d7e3591dc0b03ac3fe60419d9248b1f656d123ed
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:47:28 +0000

    I commit temperature punish string

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
