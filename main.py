93e5def7689665c3381834876571b2ac7ac33f65
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:43:38 +0000

    ball clear sense target in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_exponential(x):
+        return math.exp(x)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
