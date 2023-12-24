f3d0b52e8b20343193d85f9c12872c90a828971d
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:56:14 +0000

    punishment hope makeinto damage wait

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
