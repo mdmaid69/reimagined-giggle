c7b08cfde96673baa1ba1bc2503146c33e5256e8
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:11:58 +0000

    hurry actual before as lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
