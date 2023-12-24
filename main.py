865a2d96632e4028a4f17060cb51f20ef3ebd2a1
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:55:20 +0000

    date burn pride than destroy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
