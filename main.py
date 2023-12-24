e44187eee2eac559dbae1e62770617f59d848cc4
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:43:23 +0000

    fall artificial estimate fire sure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
