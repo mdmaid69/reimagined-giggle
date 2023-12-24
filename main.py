2a31d61539693c8c5489beac086bf9244be371a0
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:10:14 +0000

    justice match what hair ought

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
