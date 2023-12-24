a49287eb70cf6a8701ea5d618734bee0e070c7ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:31:02 +0000

    Kk judge beside outside temperature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import time
+def get_current_time():
+        return time.time()
+def calculate_acceleration(speed, time):
+        return speed / time
