13afaadb8f343f577f10b4d8354181b0c7a77041
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:57:00 +0000

    he congratulate tour balance share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_average(lst):
+        return sum(lst) / len(lst)
