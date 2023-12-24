cd19a0c013830940bd06dee5ff834b39a55db807
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:36:34 +0000

    add control reply easy smooth

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def square_number(x):
-        return x**2
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def get_array_as_repr(array):
+        return repr(array)
