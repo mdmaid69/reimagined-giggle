c422fb0e7bd8f97c916b45affb1e7cc29346be30
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:24:30 +0000

    simple prefer original brush previous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
