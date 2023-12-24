d33eb79f4a0a0cb03267ca3a8fe576d07683f07b
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:32:00 +0000

    anybody term hot past mass

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import sys
+def print_python_version():
+        print(sys.version)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
