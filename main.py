0a5677a7e19b3cd6ec1910dc0f07716e92809d28
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:06:37 +0000

    tap never kind male ready

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
