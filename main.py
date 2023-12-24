f7486c45f3ed27eef2c91719dd04d46b1703ea0e
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:03:50 +0000

    sensitive description extra task label

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_acceleration(speed, time):
+        return speed / time
+x = 10
+y = 20
+print("Sum:", x + y)
