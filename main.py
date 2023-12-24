c224a8cd76fa98c63d06ab560a2dae0cbcbd9c18
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:38:51 +0000

    steady mysterious century close dish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
