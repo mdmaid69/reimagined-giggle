6f68a4b39e8eb3fb18ab0be508c5858d4c462052
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:11:17 +0000

    air free surface straight especially

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
