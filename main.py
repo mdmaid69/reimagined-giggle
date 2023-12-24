37a1b63a1767d9859c8898d08bc9d5d7f4941741
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:39:51 +0000

    ride fine wish on broken

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_power(work, time):
+        return work / time
+import tensorflow as tf
+print(tf.__version__)
