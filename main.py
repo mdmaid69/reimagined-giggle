08bb30a23dba16e6d51223be62033ab9136116f8
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:19:23 +0000

    accept minority relation goodbye dream

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
