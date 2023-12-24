c097f6a2ad1fa5b037fcf138734d04ca900403d8
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:30:33 +0000

    calmly uncertain second drop lean

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import sys
-print(sys.version)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
