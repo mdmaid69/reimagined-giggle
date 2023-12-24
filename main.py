6869ef592771efe1c97a73c0170c6a8b945a9bf3
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:34:12 +0000

    connect first confusing thing strength

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
