d48087f371bf8603fed23fdc8fb53eb81df9b8ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:01:27 +0000

    bright talk cheat have appearance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
