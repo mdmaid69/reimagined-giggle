49f06db64afef279b07a163afed19f93b50f3675
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:29:44 +0000

    present government goodbye fresh against

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
