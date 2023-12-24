f740cdbdbca2440aa0260e5d8f9ed067709e2349
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:13:11 +0000

    danger experiment tidy mixture trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
