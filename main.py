3ec13e1ca43c8dde1c27363772f8adb9f5c67985
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:05:09 +0000

    attach forget move shopping pronounce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_speed(distance, time):
-        return distance / time
+  def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
