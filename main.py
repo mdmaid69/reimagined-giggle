74a0b4a2c370d81231453a037ed16c2b4f370af5
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:50:39 +0000

    wish definite secret borrow see

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_current_time():
-        return time.time()
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
