c5b6c80f9b1fb66ee4c3d5760e06df8b97631e58
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:49:11 +0000

    favourite commit sorry slice plant

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
