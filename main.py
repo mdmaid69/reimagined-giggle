a3333c1ddb3475017a9bae36294952b4bc5c84ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:10:53 +0000

    favour police set joke command

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
