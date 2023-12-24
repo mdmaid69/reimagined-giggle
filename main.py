b1721316707c02c6ea3b8968941ee8163eb024db
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:03:13 +0000

    soak minimum public pause colour

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def cube_number(x):
+        return x**3
