7c941a2f14c075abef6fe740ecb93007b85938be
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:29:07 +0000

    influence hide address spare loan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_tangent(x):
+        return math.tan(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
