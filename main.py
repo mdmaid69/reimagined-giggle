d2ea78125f06175d7dc3a14f712c1ef9fcb74068
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:25:18 +0000

    community cap language drive land

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
-import collections
-def create_queue():
-        return collections.deque()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
