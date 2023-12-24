aba4b40d5f4a2b18579d7698758bd5a559f608c0
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:49:13 +0000

    call makeup lock reaction wear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
