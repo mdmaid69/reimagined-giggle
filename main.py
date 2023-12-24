3a6df637fa5149965d06f4746a66e5e09858371b
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:30:15 +0000

    fear independent thing date Cc

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-print(math.pi)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
