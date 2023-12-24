2a2eeb11d1ef75f93baec077b21768b907c333aa
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:02:18 +0000

    calm grand feeling official bored

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
