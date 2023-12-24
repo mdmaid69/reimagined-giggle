f8a3a56dd79d41b20ffdb9e1e53b55d5287d3e8d
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:00:26 +0000

    follow stamp holdout immediately road

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
