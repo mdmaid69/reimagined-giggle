3008ce86be7124ee528abe60c0cba1e98ba6381b
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:31:18 +0000

    copy can willing neither turnoff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import getpass
+def get_username():
+        return getpass.getuser()
