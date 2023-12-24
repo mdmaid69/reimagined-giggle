53e943ee724f7c421f40b3b4e537c2e831faa493
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:57:15 +0000

    news yet interesting sore reserve

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
