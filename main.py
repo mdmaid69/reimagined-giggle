464762db6c64102269e49b0096bfae7ff981a5ee
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:07:32 +0000

    turnoff kind getback occur comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
