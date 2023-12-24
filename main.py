8ee80d157eb84b25aacb8a2ded2a04096944512e
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:35:03 +0000

    platform guess left eat natural

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_repr(array):
+        return repr(array)
+for i in range(5):
+        print(i)
