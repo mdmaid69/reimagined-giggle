0950a18aa84eeb0960c971fc925b5a0e46eb2fad
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:51:27 +0000

    themselves wander creature lend fit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
