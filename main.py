7eec102f70e659bfa0e56090c40f93f121d6c4be
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:08:04 +0000

    office play murder square good

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
