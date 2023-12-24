e96bfe8f9c7aa4521fc3bc1a405e3fe1c2799cf3
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:05:15 +0000

    attitude step themselves pretty forward

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
