27f1cb5658832f61e5a139e02c1c50bd41d1fb51
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:01:02 +0000

    balance wonder tomorrow region disapprove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def change_working_directory(path):
+        os.chdir(path)
