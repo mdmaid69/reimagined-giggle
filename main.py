f797bc9987d033602723e142c77ea6deb79c4f2d
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:42:01 +0000

    attention near age yesterday school

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
