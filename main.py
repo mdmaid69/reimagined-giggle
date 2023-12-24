7acdc0ec8fbadf3294a8d010b42664a9f1484305
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:08:50 +0000

    between learn slip drink content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def find_unique_words(sentence):
+        return set(sentence.split())
