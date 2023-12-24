a75144e08b8dcfe8e70156114c8ff15a330fa481
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:34:51 +0000

    exercise middle glad glass forward

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
