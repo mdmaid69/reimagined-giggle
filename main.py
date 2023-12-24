c183ba02b5edd70ab98b603a2db60b78668fecc5
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:14:14 +0000

    special short same still lookaround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
