9db60379c93bd3a1361a25b17f26531e502e2c4a
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:34:18 +0000

    sentence play neither let familiar

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
