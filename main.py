30789153f41652d43de25f13396450f21f6b4f25
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:29:42 +0000

    leave slightly fairly blind train

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def square_number(x):
-        return x**2
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
