77a8088cbc186e9ea285732d780ea47d048b118d
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:42:30 +0000

    switch thankyou forever wide rule

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_speed(distance, time):
+        return distance / time
