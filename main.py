0524ef76182131782ae93ff003462876f4bc310b
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:18:33 +0000

    glass blame determined gap coat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
