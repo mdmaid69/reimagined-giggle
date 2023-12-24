ef8262132b2103e8ba93051d29304c5e98a07f13
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:35:14 +0000

    callout stop accept convinced programme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
