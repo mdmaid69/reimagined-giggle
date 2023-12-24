027666d80ffdf1a4edbbd3b4173edf50a463f5fc
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:09:08 +0000

    exciting land sore second please

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
