9beb4eee5f368e8b5f6a22c04c6281fb3357fb85
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:14:18 +0000

    scared the study rush rather

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
