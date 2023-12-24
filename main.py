0809bf853edda0f6469e0fa72e3f9f072b7b8801
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:50:22 +0000

    class calm clothes tap force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
