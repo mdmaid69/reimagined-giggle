50934f94375308d23076fbf184828aa8df99e8bd
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:43:14 +0000

    authority still anybody plastic letter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
