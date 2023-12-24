f690bd34bfea56ed29298e3fd39af9c0574f7f97
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:49:26 +0000

    wish share building to runout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
