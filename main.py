6632b56ce40b48f55838783e596b719d64a1d68f
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:49:14 +0000

    emotion shine emphasize window example

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-x = 10
-y = 20
-print("Sum:", x + y)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
