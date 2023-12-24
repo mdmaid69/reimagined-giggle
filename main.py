7b754f0571f5f95f4f2c7fb8f9a8b094581e85b8
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:37:38 +0000

    desperate dear mark niggaacademy on

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def convert_to_binary(n):
+        return bin(n)
+  def is_odd(n):
+        return n % 2 != 0
