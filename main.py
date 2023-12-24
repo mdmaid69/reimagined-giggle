68fd4885ea86ea5fcc07485cb541f73077e0fec1
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:54:43 +0000

    completely justice then proof behave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  def remove_duplicates(lst):
+        return list(set(lst))
