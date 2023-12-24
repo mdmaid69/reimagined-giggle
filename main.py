09133bb88cb814c34c4a9dd6cfc6247dfad0b090
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:51:25 +0000

    fire receive rescue worst bend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_user_dict():
+        return collections.UserDict()
