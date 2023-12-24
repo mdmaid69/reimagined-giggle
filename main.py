9dc78ade8b7c568fdbe06c36fb0e8ef057c3181e
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:50:06 +0000

    there supply full fact declaration

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  def convert_to_binary(n):
-        return bin(n)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
