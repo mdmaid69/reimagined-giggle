94866201b8079ff83b04a974614553d7a7e4aa3a
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:12:41 +0000

    strike mysterious waste urgent both

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  def remove_duplicates(lst):
-        return list(set(lst))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import os
+def change_working_directory(path):
+        os.chdir(path)
