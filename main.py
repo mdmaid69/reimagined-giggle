877582c1ede67bcb74046ce874c64c5d7c6be4b1
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:48:59 +0000

    help afterwards brush float injury

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
