c5d2548f7a9d5fb50be4f07647171661334f0e29
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:45:15 +0000

    ordinary listenfor question beat judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def find_max(lst):
-        return max(lst)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
