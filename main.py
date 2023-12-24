b467f17fecb9b1044b69607d6c81c4e210d33e0a
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:02:05 +0000

    contrast noisy illness suit near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
