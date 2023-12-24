bd50a8b61983c7841b99d957aa146b32c4076687
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:04:06 +0000

    drink disgusting neat spare talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_error_function(x):
+        return math.erf(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
