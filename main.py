32591b83429312794f74f80582570491261d1d26
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:55:49 +0000

    opposite just length Ff deep

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print("Hello, world!")
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
