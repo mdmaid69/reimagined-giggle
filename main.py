bfb0811e427dd90f21d30e9b001233e8cb7c4fde
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:10:06 +0000

    original answer miss influence goodbye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
