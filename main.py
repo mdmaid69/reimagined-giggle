b83a2071044e9b92b69738ba4322240d79aa95c8
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:10:07 +0000

    whole successful to finger leg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
