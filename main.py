9b88b57beac8eee9e4b91942f9b992ce6aa43966
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:20:10 +0000

    walk anyone mixture authority rest

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import datetime
-print(datetime.datetime.now())
+import platform
+def get_os_info():
+        return platform.uname()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
