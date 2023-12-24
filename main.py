f67ba22119434d613f11632daaf7d0f71dac8c36
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:16:20 +0000

    bush tomorrow wrong influence herself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
