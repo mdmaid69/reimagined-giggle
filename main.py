07fe1be73d51e1eacc2d1f74878d73bc2083ea4d
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:47:37 +0000

    wash further turnaway influence tight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
