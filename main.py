5f0687d656eaa1cc0d1cb163b39d6361611c3745
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:29:19 +0000

    guess bath breakout sister image

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-for i in range(5):
-        print(i)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
