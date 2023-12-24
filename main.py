fee58e49e9c70ff19b4cf4d2cb28ada23f1b5b22
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:39:08 +0000

    excited dance wish cope answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+def get_current_time():
+        return time.time()
