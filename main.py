e1cf950b554aefcfdd911ca81830846fa21fe6ec
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:35:52 +0000

    country statement admit 570enjoyable negative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
