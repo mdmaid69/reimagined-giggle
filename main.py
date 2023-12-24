a51e0dccde6bc0237fb96d1176ad2adbf769daf3
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:47:31 +0000

    light control workupto space arrival

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
