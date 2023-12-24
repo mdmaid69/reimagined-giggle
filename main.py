5763a936b07d0d1df59324a8af4e906d57d1adcf
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:37:32 +0000

    weak keepup sometimes follow closed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
