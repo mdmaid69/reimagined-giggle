0bf3c7fa3dadbdbb7a8f06ee7015b56a17d413f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:56:18 +0000

    full course unpleasant exam suit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def convert_to_binary(n):
+        return bin(n)
