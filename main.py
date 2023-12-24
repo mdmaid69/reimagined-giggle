69e94842cc481838f562fb47d210086ad33691b2
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:52:13 +0000

    pure rub trial formal stiff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
