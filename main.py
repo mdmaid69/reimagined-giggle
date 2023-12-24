f4ca2cf065b47359714c5b463d5db827490fe514
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:06:38 +0000

    godown improvement appeal Ss love

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def remove_duplicates(lst):
+        return list(set(lst))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
