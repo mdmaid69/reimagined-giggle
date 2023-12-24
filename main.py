486cd3d7d7d50a2b3ec8f3edad28775d0fed72bd
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:54:19 +0000

    boil murder wave fight process

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import tensorflow as tf
+print(tf.__version__)
