470b1871c1b05e61d92b8af2056eb32fa1539f47
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:27:03 +0000

    hardly Vv turnup pen family

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
