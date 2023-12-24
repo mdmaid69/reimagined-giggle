6771401e83236966d429f525aa205acaf9bef5f2
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:38:28 +0000

    pile Ii introduction object equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import platform
+def get_os_info():
+        return platform.uname()
