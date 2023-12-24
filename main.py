459927d99d5790ddac0797bf76757027e086b2c6
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:16:22 +0000

    answer another less turn because

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_arc_cosine(x):
+        return math.acos(x)
