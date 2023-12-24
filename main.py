8b2e354ace3d43e2961100d7de6aa912af6d6bbc
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:55:58 +0000

    manage shout attach meanwhile comeround

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
