4896e77e0c13e65e1afc21ab9144db3afc6287b5
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:23:45 +0000

    shoulder Gg further confusing hour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
