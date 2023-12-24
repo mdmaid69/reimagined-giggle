d80165a96628f76ff92baa0e803e475dab751223
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:56:41 +0000

    laugh ball mine rise general

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
