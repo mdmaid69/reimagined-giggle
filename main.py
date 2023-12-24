20b097d43ecd6fce23ae24b52ec9b95652eca956
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:42:22 +0000

    crowd takeaway forever pure cry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+x = 10
+y = 20
+print("Sum:", x + y)
