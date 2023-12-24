b03dfb20ad89f818431497dcedbb50115b04b688
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:17:26 +0000

    deep partner table never callout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
