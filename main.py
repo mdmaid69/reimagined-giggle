d2ebe7f523158100325cdaea18280efe8d4baaef
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:36:36 +0000

    relax very thin person look

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
