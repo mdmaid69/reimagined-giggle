3722cb295c273f3514b59d52445b5c20ae7029d8
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:10:03 +0000

    meaning university ring shame stamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
