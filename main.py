9bd002538c4150055a908ba23a40d12050223e1d
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:33:05 +0000

    practice aim slide pure return

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
