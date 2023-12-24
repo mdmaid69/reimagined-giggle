d958b5a7bc315352fa398a0456acd10ea4c3f901
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:35:07 +0000

    or word rare behave uncertain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
