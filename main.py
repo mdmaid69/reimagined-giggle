3bbd6789fc931fffe34e89ec5e57a35118f29e4b
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:05:02 +0000

    heavy read fit Zz conversation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
