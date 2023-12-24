617a3a7124ccbf56ee090914536bb6636e181381
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:02:32 +0000

    impossible combination wave ring strength

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
