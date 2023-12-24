2af273c3567c1c39638681be1aa58141e28b5e63
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:57:58 +0000

    estimate hard search base address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
