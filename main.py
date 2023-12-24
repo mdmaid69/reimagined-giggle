6106940c3c456eb9da83d2a4d1de3eb62cae384d
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:45:41 +0000

    basis address treat apply spray

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
