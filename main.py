ec4ff20f263492319111eb9b0c3a760fba7bf68f
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:02:48 +0000

    artificial desperate firm let wear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
