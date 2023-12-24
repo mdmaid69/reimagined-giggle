b2173cbe9c82a5d2298376ec9213f3be20903354
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:36:19 +0000

    skin capital next cheque rain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_distance(x1, y1, x2, y2):
+  def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
