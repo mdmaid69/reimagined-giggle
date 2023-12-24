6cf8efe89d4cdd52ede9c27af888406f2e5d481a
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:11:29 +0000

    prove odd uponon company drink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
