1a221f623d2383a54240a7d4b6b4dee21653a6ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:05:04 +0000

    field mood mark back birthday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
