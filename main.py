f11f5efd1fb387135c4b810fee0b15773261afeb
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:55:49 +0000

    goout single actually Kk negative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
