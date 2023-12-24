b707ad83c9da581c57ffa0fffbe3349a35c6348a
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:17:23 +0000

    reach buy concentration neat rob

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import os
+def remove_directory(path):
+        os.rmdir(path)
