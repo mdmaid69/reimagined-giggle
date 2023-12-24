459d3f5077b60c94c8899dcd35cad84cc66e784b
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:55:34 +0000

    try expression unusual disturbing anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
