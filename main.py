d3f655b7353a145a37c10e521d54b82dd7d3c16e
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:25:16 +0000

    dry study perfect move explanation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
