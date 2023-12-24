bcbb381395c0828a01d1655a3b1647d6cbde42d8
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:47:54 +0000

    soak operation recommend smart by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
