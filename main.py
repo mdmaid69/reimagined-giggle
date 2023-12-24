c0d39e710d3792f22791f251a81aafd81ed3526a
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:37:04 +0000

    acceptable behaviour cut unless special

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def find_min(numbers):
+        return min(numbers)
