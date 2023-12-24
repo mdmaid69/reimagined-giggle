7ce7e4f264055b8b86c37b033d7b8e73d5d2a762
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:46:30 +0000

    account previous idea cook cool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
