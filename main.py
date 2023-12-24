2048fc466181b9ed08155d9665f1970889813dd7
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:49:40 +0000

    sense seriously tear protection whenever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
 text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+print("Uppercase:", text.upper())
