b5282d2224312e8e8a035641b4ac5d0dbdbd20f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:20:26 +0000

    tragedy pleasant ticket home natural

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
