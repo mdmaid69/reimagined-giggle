6604b33a657b65203eec018384abee33d2126c33
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:21:47 +0000

    push answer track seem marry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
