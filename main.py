df14babd145e063f63f9e6dc66591ee446fd2d32
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:57:53 +0000

    squeeze drive guess noisy mend

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
