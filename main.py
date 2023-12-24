9a35aa221bf18d8a689d8d799fcc8c5faa865c28
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:18:53 +0000

    typical complicated improvement path arrive

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-for i in range(10): print(i)
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
