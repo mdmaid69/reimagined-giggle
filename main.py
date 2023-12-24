102c4b3d9837aa765bab9266389dbe6756c335b3
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:10:09 +0000

    translate short group many steal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
