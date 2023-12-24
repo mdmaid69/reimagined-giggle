ae8d1c80f2886267c5f614498aeeb6b15e201dc4
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:21:30 +0000

    hear national badly away often

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
