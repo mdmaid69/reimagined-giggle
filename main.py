643d6b540c7ef1048d28e0d414d8962b90c146fe
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:19:51 +0000

    ours Gg necessary sort control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
