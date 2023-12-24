de86f73312c04d7141e971ed2a1409c418c3e881
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:45:09 +0000

    movement time friendly smile such

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_pressure(force, area):
-        return force / area
+def convert_to_binary(n):
+        return bin(n)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
