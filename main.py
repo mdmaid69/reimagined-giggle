61ab41cecd6676cd63c9f07b9738c42dcba4bb27
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:55:35 +0000

    wing possibly smooth plenty rare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
