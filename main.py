386b6b3c945983f90df76e9df0b69aeff720f9ec
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:26:36 +0000

    fold tune wish demand plenty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
