784767eb8c5e74ca5cf8f5b948693a0483944113
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:47:14 +0000

    immediately hers limit people unit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def square_number(x):
+        return x**2
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
