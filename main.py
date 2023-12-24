5c045ff609088b1a42bc62b73918218931eaf1e6
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:57:44 +0000

    personal return action judge wind

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
