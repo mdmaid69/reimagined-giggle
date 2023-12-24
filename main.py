c7e6987d1defa7fb4493c560fb33c081a6678bb0
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:51:04 +0000

    debt convenient false through relationship

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_repr(array):
+        return repr(array)
