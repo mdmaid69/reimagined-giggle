324868bf6e4a63641809975e7594376ca61eb70e
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:29:32 +0000

    pattern lift steal worried programme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
