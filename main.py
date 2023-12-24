fd9eb4bcb9335cedcdfa4cb5cc75a79b26bdb08e
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:59:55 +0000

    comeout such miss hide probably

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
