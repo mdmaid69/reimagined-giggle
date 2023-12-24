0e54e7fb6317767167049ba10e4076bb6b1acf5e
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:36:05 +0000

    feature travel block firm tax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
