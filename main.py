96bf50e67f0b20a9d978792f314ba6e8890b3a34
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:22:52 +0000

    rarely side tool flow joke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def greet(name):
-        print(f"Hello, {name}!")
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  def remove_duplicates(lst):
+        return list(set(lst))
