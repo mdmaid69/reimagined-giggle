ef80ebf7e77c63ac84196f1ac0a8a6dbb41f9e04
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:01:24 +0000

    tight food completely sure consult

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
