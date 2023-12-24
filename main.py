9d67a7743fdd583a0d9073be1293dd06dfc424ce
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:46:11 +0000

    result off display cause defend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
