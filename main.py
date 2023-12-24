09deed2401ac6d714604adac080cec07dc3dff8c
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:33:41 +0000

    difference fear Nn while door

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
