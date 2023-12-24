7516866219e4f0be86c902b5aa0f5c24cb6d81b9
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:42:58 +0000

    production hard passenger drive advise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def square_number(x):
-        return x**2
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import os
+def remove_directory(path):
+        os.rmdir(path)
