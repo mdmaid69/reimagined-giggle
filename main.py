d175ab56f2eee3f422cfc4d942b3761ad12b3cd3
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:18:16 +0000

    frightened it spring mysterious secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
