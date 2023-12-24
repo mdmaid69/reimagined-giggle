852940f79fdffba0fc1a58b3563c46c5f580fa81
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:10:46 +0000

    compete pot be Mm government

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-from collections import Counter
-print(Counter("hello world"))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
