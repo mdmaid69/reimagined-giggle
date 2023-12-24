a11fd8d5d9f0c0de40663c59d8c82eed6d8edb3e
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:36:28 +0000

    rise rain ring about beside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import collections
-def create_user_dict():
-        return collections.UserDict()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
