93635e68688ebb1c5e3e255a15a60b22c3da7cea
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:56:26 +0000

    display upwards history fail play

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import numpy as np
+print(np.array([1, 2, 3]))
