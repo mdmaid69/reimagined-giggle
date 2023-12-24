20f1392fcb1c964e347d74af9fa7ec2ca314804f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:23:00 +0000

    copy separate nervous jump innocent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def divide_numbers(x, y):
-        return x / y
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
