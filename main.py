fb3fed2077dda4f136c8ff0181315de9710d8dca
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:18:22 +0000

    before you happen team knowledge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
