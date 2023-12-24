e9a357f4d45a6992da0d848a775ab06e7b292306
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:30:51 +0000

    frame reasonable pot straight operate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  def add_numbers(x, y):
-        return x + y
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
