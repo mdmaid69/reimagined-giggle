2ff4bfcf71051253840b5ec644709fbf39c0fe22
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:50:52 +0000

    pleasure production before reply among

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
