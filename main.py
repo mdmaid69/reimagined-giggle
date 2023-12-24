2faf1708b363c1829f8ca336d87dafb29212fb51
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:04:59 +0000

    too copy juice cover proud

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
