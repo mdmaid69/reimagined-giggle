90f70c6d0658925213a8292af958e721ecbf0d04
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:49:00 +0000

    excited noise experience pick thorough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_pressure(force, area):
+        return force / area
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
