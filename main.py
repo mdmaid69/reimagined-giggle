e7fba15b5f3748cc167e78a00664f44c1581ce1e
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:10:24 +0000

    callback thorough recognize conclusion flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
