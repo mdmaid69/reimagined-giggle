5acf46ba18d58125323b695edba9a7385048c45b
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:40:31 +0000

    win also machine silent history

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
