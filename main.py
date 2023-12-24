71fed46ddd3f685f2ad7b67af68a803e548651e0
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:56:15 +0000

    sorry scene real size godown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
