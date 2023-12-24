b948bbfa5366344ce858bc8695949cfedfea48e6
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:32:35 +0000

    under means country injury build

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
