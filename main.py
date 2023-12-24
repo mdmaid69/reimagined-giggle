6a9f4519ae91ba5bf54374a229387e9cfbb829d2
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:12:32 +0000

    lead anxious entrance clear holdout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
