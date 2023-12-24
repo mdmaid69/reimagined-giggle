1a605e52468eaa81294194deaac57aaef6556988
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:56:24 +0000

    yet comefrom clue guilty treat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
