de2edc4b9a7a124485f76a671256d2d5b36e5bf2
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:30:45 +0000

    important throughout record geton pick

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def is_even(n):
+        return n % 2 == 0
