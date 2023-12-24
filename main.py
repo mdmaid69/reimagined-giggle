7197c9c587d7fa8fb81dcf41eeebd6e5db93866d
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:05:04 +0000

    scrap occur movement crime flow

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
 import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
