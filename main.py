c34dda6bd4617039f68f089e4daed1bf25a6d0b3
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:21:29 +0000

    substance takeout name thankyou card

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
