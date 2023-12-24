807ba4682393eabe025656ad33febe5d16d5af0b
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:54:45 +0000

    whatever to skirt case avoid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
