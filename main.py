20a1bc7abfcca98821c9562619e67a0777c700e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:16:38 +0000

    twist nigga skin hate connect

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
