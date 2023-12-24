2022c460c5060002b0d5c3702d58acc38931d262
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:34:38 +0000

    slide slice twist reasonable shape

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_as_int(array):
+        return int(array[0])
