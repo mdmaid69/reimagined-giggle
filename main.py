6ad9df37ee5e7dbd62d1bb09448c41fb120aca0d
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:37:40 +0000

    say bye protest thick all

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import random
-print(random.randint(0, 100))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
