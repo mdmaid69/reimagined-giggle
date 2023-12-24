320b3d403f3be1dc385c36f35b4994fc23a08304
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:30:58 +0000

    city formal best muscle live

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
 import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def get_array_as_str(array):
+        return str(array)
