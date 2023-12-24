1cf4ce47584f7a72b2c7b3dcde22a338288305ce
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:38:33 +0000

    promise rub exchange well variety

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
