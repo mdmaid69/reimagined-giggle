4ec7eed2a20bacbc2740f5c85c55101b899a09d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:36:51 +0000

    special dance carry weather dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  def count_elements(lst):
-        return len(lst)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
