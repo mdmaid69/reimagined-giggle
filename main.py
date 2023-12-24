abacf9455465285ad30146a5a34be71cce57ec98
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:12:00 +0000

    however difficult definite towards concern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
