5c0a16d99c896ecd6ff18f4bf34cf6e870fdb5e8
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:29:22 +0000

    sensible motion strength factory support

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
