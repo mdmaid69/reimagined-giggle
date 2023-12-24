2667ad89159cecfd8f8b5ab6b12404c283128f96
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:38:11 +0000

    coat more bend unite matter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
