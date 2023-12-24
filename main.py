7264568ae9b28a678014caac38ab95066f27112a
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:00:55 +0000

    future study by pleasure afterwards

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def square_number(x):
-        return x**2
+  def add_numbers(x, y):
+        return x + y
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
