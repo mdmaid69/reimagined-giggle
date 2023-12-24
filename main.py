78bc3436b64de6bae71707fb51d65f0d913edfa3
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:21:14 +0000

    high best tall degree have

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
