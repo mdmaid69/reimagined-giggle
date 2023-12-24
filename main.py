2301a54f9d81dbffde1cf2f97e7cfd8b0c0a27cc
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:25:33 +0000

    region swallow round delicate comfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
