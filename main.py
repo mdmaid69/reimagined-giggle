49bd25566dda8a1eb2306ecf5c8fbcf1acb947c3
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:04:22 +0000

    pinch fault stay Zz blind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
