d3df53718d688a86bcc62c6909927a32bce03342
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:04:45 +0000

    force turnaway breakout film praise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
