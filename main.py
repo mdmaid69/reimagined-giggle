5af1959768348ab54ef7f17f39c716de0380e390
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:26:17 +0000

    beginning add image depend organize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import sys
+def print_python_version():
+        return sys.version
