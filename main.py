4124ed26b496835d8358f4bb49212ce12301cb26
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:29:24 +0000

    familiar extremely breakfast into check

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
