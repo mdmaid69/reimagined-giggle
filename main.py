073c3120e8bfc826847df6b1c260656a2c3be7f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:32:10 +0000

    easy design cool last generous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
