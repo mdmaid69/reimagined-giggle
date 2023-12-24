f70bdf0b949e8f6db86f1bda3c1f81f6771cd4eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:17:16 +0000

    reach provide indeed fit love

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_floor(x):
-        return math.floor(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
