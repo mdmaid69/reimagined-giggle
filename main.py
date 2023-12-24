8cb493af27ceb4904037f213d62140f112e8765b
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:59:01 +0000

    tread detail meanwhile discover connect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
