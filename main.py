524f5c364b4780d62cb7d1cd6a70196fdfde4c9a
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:47:35 +0000

    neither congratulate everything suck promise

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
