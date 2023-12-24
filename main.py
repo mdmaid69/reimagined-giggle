3f1c3cd0f8e50b793265fb8f06f6e90637f7a0a4
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:12:30 +0000

    provide slight far distance middle

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
