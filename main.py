cf1a06c01d5fd1fbf475b35bb372f1dc021b80de
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:30:20 +0000

    demonstrate near sleep fat rate

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
