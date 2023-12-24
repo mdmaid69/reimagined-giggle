8684945e3f386bfac13997bd4ae05d6f5260c4cb
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:16:25 +0000

    creature free regard sensible store

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-name = "Python"
-print("Hello,", name)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
