598022ab19d7674034346267e6f069796ad6780a
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:58:53 +0000

    ask listen learn deep takeaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_array_to_string(array):
+        return array.tostring()
