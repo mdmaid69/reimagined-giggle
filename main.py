e9cea790e21bc867c564df2b5044fe3deb421348
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:55:04 +0000

    term include vanish passage contrast

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import sys
-print(sys.version)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def get_array_index(array, item):
+        return array.index(item)
