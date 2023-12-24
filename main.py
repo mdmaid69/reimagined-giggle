5ec3a539e1946f15d05d89aaefe90a8a7503ccbf
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:50:46 +0000

    child charge sharp before protect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
