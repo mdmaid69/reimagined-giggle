1c75c8808765c1c389246e0167b40b6636a28131
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:20:42 +0000

    565end lesson cry formal include

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
