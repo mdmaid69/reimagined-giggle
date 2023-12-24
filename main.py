fcab24ad3281d6ab21c8b626dcd3109178bbe058
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:19:37 +0000

    discover crowd intend brain angry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
