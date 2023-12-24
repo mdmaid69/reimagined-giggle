490a552710abfbb28796f0d3cafe2b37c7ecc150
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:35:16 +0000

    exam lend bar sharp fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
