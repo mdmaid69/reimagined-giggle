ee2affba2358938ab975f012fdecb4dd8ce35a5f
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:32:44 +0000

    honest seem mind secret suddenly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
