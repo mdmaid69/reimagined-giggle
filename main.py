efffcf1c18c059a2a9da7245da8b6770f6156d42
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:07:07 +0000

    body scream fortunately ago regular

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import time
+print(time.time())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
