c0ac9807cba592c386b622f2eb5b822331a28b85
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:24:21 +0000

    Ll meet clue charge offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def get_array_as_str(array):
-        return str(array)
+  def cube_number(x):
+        return x**3
+import json
+def convert_to_json(data):
+        return json.dumps(data)
