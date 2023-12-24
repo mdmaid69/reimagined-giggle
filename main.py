1fe68b427fecfacbfcb74d63cf68a9de7059f4f2
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:45:30 +0000

    skill matter new formal hot

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def subtract_numbers(x, y):
+        return x - y
+def find_unique_words(sentence):
+        return set(sentence.split())
