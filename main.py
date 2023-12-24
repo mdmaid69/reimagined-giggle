92c8bc84abb4791081ff8557781473952498eb2b
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:39:18 +0000

    anxious compete number godown equal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_speed(distance, time):
+        return distance / time
