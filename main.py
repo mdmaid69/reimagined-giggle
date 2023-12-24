a3a286852fc137cef2f8881eabbdf45cc7dc5c45
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:33:44 +0000

    neither way anywhere realize pollution

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def get_array_item(array, i):
-        return array[i]
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
