6fdea7f41ddd0dd26ff397dffee87c95a4f5fa71
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:41:39 +0000

    remember class appear strength extremely

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import re
+print(re.match("h.*o", "hello world"))
