974a69b7e32dd77f0eefacafe16758b28c5444da
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:01:50 +0000

    thirsty upwards laugh find upstairs

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+text = "Hello, world!"
+print("Words:", len(text.split()))
