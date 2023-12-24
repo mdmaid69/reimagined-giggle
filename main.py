0fc1f91cc614e371f623261277a22cfc7423f005
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:43:44 +0000

    direction bad drop harm tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
