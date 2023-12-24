c311742b0187470b8b5a885c86607854bad2253b
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:29:28 +0000

    emotion sting alive creature empty

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
