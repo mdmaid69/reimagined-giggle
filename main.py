f77dfa6e200bef322b4768cfb6a8f88860718e76
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:01:30 +0000

    life mention throw experience means

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
