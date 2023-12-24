4a90f0548f27660793e4a92923972221b815299b
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:12:01 +0000

    fall be rate geton forget

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
