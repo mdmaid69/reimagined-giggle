4bd81d3c5e165e4871a12630502e053e53d2a194
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:41:35 +0000

    rid software free smoke thorough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def find_min(lst):
-        return min(lst)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
