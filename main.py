1a42b26f159871ebb3afaecfc7aeb980a5c209fc
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:21:00 +0000

    hair tip travel ball friendly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
