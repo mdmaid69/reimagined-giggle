700f0afcc4c20f4957be73eb45abac7e3f48d9e0
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:38:22 +0000

    start weigh degree press hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_exponential(x):
+        return math.exp(x)
