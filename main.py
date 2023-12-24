bdb9e2826fe390865a44087161579b4a5f8e6794
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:54:40 +0000

    traffic dig murder pity happy

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def count_words(sentence):
-        return len(sentence.split())
-x = 10
-y = 20
-print("Sum:", x + y)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
