423f48894747425d52e451115a44d1be732d6c9a
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:32:40 +0000

    black consider voice stay only

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
