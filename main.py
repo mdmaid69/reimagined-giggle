139fc10cd6ee824a40023f06db2c9017da94cbc1
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:59:15 +0000

    behind hope reply song cook

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
