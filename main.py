631b63c68f2a3f135d8a67d1a3f4c30a5f5f12ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:49:15 +0000

    breath lead yours serve be

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
