b1d2311eb4026211a71bfd781e1c6721b7a8da8b
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:50:44 +0000

    ask rank sink 564encourage about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
