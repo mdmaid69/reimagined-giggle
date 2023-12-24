04a2df83099cf413ed4f76ee5f6f9eecf8837c40
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:22:53 +0000

    tear gooff slip somewhere book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
