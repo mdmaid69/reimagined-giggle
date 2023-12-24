87be257e57fda9f30cdf35de0f39ed0372fcfee2
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:59:05 +0000

    clothes makeinto exchange project tip

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
