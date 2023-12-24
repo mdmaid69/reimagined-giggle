631da0097135519b6fbf0bb74deff801e452d0ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:19:26 +0000

    fold sing activity closed otherwise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
