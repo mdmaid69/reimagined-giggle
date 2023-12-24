ea4113f12cc244ab015f5442775e4b1a691d0e48
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:21:27 +0000

    general clean search fairly sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
