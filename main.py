45c77c7a75e30091593eeb23ee89f40cd18d5659
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:17:56 +0000

    study lunch together health destroy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
