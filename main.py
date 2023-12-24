c790e1074b435438732e2687ca9f50b16656c5dd
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:17:35 +0000

    speak regularly among visit introduction

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-text = "Hello, world!"
-print("Characters:", len(text))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def find_max(lst):
+        return max(lst)
