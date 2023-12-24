dc47f856de2f7606715b2356bcd2b7ad6aa90cc3
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:49:55 +0000

    hers regret hard wind burst

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import logging
+def log_message(message):
+        logging.info(message)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
