22d49e0c2381dafaac21a53e809589012b96f529
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:10:00 +0000

    poor wake generous discuss first

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
