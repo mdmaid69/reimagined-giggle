f4e09af29e3dce32f68714d57f1832e5c725da68
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:37:55 +0000

    against challenge attractive mainly attention

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+print(sum(range(10)))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
