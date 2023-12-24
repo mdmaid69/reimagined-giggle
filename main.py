f146ad95c9b05fb2ca3e1bc8d8afea5706b5c949
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:46:05 +0000

    typical scared statement close rarely

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import os
+print(os.getcwd())
+import array
+def get_array_as_float(array):
+        return float(array[0])
