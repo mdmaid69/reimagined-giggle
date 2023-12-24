1af023db3002be5eaa928c1fc0f6f31db53c166f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:55:09 +0000

    interview further impressed luck heavy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
