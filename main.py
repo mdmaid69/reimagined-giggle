cf09fed0206a7a774f3e5e43648abeb9753fa391
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:22:33 +0000

    sticky throughout character order possible

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def calculate_area_rectangle(l, w):
+        return l * w
