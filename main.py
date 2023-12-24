9ab104c8ba7035ce00c846677f7e68923f11634c
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:10:31 +0000

    comeback with please same answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def check_if_array_contains_item(array, item):
+        return item in array
