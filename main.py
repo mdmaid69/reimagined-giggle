ace0b838928b2ea94fcd477a0ff5c35b18328fcd
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:34:48 +0000

    mind do smile effect company

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def get_array_as_frozenset(array):
+        return frozenset(array)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
