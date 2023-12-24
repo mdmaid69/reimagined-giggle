db1828d407bca3e36be513e5dd920cec215c7d61
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:07:00 +0000

    factory during birthday school word

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_pressure(force, area):
-        return force / area
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
