3d96e78f85fe44addb4a0bd2b2487c002c0daf7c
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:19:47 +0000

    battery in something charge praise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def is_odd(n):
+        return n % 2 != 0
+  def square_number(x):
+        return x**2
