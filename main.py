438c5338c7e4845a438bf9d4067503c70d5291bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:50:36 +0000

    holdout interview thirsty mind able

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+from collections import Counter
+print(Counter("hello world"))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
