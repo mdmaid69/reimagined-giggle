b2f322aeac79b274b7c7e9cb5215e6b8a43f5946
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:01:57 +0000

    fast capital over pause pressure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def find_unique_words(sentence):
+        return set(sentence.split())
