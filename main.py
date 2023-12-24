4ecc1b12fb970033cdfce1c3e4084bafcb37fded
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:16:01 +0000

    insist wrong weak pattern sharp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
