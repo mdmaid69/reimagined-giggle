9b87b9b594713a9a99d97917e3bf421d995cb4af
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:59:49 +0000

    visit previous talkback save aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def reverse_string(s):
+        return s[::-1]
