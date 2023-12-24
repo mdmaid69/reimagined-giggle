fa985e88933def838ba01c9c9e3fec9e0ca9eb24
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:46:11 +0000

    hurt ours straight usual scene

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
