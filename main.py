670809304f6ba35c4c6613805a86be3c1e22021c
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:07:26 +0000

    cheat arrange whole turnaway sound

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def sort_list(lst):
-        return sorted(lst)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
