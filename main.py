4a689fff0f9c2ae22b61d4f99f231943ac8979b7
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:24:31 +0000

    comment connection wander clean similar

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def multiply_numbers(x, y):
-        return x * y
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
