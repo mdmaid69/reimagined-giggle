33b985539c54f0d53771884e7e813d4e41535357
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:03:29 +0000

    almost sure calloff trial summer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
