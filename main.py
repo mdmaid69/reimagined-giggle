1edcf28ceae89bf4892a74d829db4013f40bd2d8
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:38:20 +0000

    say worst concerned behind production

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
