5cc7740d88b2c4928d535a5fbe2ee6ab4973dfbd
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:23:57 +0000

    ourselves Tt minor club turnaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import sys
-def print_python_version():
-        print(sys.version)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
