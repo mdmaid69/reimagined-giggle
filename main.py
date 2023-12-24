7f3d64dba2c2bdb95c2c31ed611b30d4ce8eac0d
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:49:18 +0000

    force well that treat complicated

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  def count_elements(lst):
+        return len(lst)
