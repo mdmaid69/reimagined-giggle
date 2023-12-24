38f00d18f9fca346834ef4c52221a45ff3695da3
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:11:47 +0000

    concerned entrance anywayanyhow later gradually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
