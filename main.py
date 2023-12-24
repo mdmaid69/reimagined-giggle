3b389439aa1cbb646665513d7f501ddad32adb31
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:43:31 +0000

    chat other collect laugh operate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_string_from_array(array):
+        return array.tobytes()
