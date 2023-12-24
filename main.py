f65bd698277b4c884fe1c14d3690652f5d0f6f6c
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:14:04 +0000

    motion arrive possibility bin problem

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
