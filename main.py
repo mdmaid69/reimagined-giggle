1e2241359ffe8db7f6cc4d4f4d94956e46a4a4d8
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:32:14 +0000

    confuse fire shoulder eye importance

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import math
+print(math.pi)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
