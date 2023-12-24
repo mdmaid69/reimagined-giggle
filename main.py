978c519862427790ec03ca214e422b3d1f3b27c1
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:42:00 +0000

    wet conversation far swallow angry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
