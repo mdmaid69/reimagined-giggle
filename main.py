e4f563c67807d79fb220c6fa219977b8f19bdde5
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:14:38 +0000

    front not performance statement children

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
