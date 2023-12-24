7f8834535b0d3fb6c4dce6226eac1589cf3f66b3
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:15:18 +0000

    union close tough age explain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_density(mass, volume):
+        return mass / volume
