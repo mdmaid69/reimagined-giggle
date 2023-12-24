c5a857269120b5b92ad8d2afa4874c8cc1946605
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:19:55 +0000

    always shelter gate familiar tonight

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
