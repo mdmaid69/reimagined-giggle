d4db223bd1e27ab47cf948a7a3a3596608f9e4d3
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:01:15 +0000

    swallow under grade love 568energy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
