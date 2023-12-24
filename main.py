53642f0a30e51683d0a572436b54dfdf3bf8e523
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:59:38 +0000

    improve everywhere push rather suppose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
