a76c1f276ae48ce565e1c76d304e4f0b6bb60138
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:52:13 +0000

    bush yesterday greeting cheat sticky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
