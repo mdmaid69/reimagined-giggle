4620e4dee948c172e665e8e1f31c6a0ac7845584
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:31:38 +0000

    cause road related road compete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import random
+print(random.randint(0, 100))
