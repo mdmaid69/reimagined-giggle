cac94564569e37f7765df48ec97310bb4a16672a
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:18:25 +0000

    culture smile wild comeout public

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+print("Hello, world!")
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
