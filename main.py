fc3260413c48a061fd65870280dd9bef1cbca60b
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:29:22 +0000

    drink risk road still charge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+for i in range(5):
+        print(i)
