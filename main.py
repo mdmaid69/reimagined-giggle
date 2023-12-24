2f84fdce706be7a91132c338a9971ef0fc049a00
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:07:58 +0000

    right mainly swallow poison acceptable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
