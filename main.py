6fb1c20f8c5146d635ae3279774de59bad993f45
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:58:08 +0000

    knock artificial piece smoke bit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_string_from_array(array):
+        return array.tobytes()
+print([x**2 for x in range(10)])
