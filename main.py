2ceffd983cb6eb3b7dbe226100c6c8c4f41ab8d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:33:09 +0000

    Bb test truth break case

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def sort_numbers(numbers):
+        return sorted(numbers)
