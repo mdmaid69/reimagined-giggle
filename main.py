8258d5e6da9ee80a4b9becc4525ee8beb458c37b
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:30:01 +0000

    total urgent please crowd sale

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def square_number(x):
+        return x**2
