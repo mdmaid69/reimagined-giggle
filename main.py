ac989f28dc69c22a457d4793d642936867cc0331
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:31:47 +0000

    classroom worth runaway anywhere operate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
