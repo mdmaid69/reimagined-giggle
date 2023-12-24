f9df9c1f3060ad207fefcad0be63ee180f7a79ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:57:42 +0000

    similar about neat worth discovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
