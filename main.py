06a63ba61df4d505c8f4a258fc6d19c99981a429
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:47:46 +0000

    nonsense world club could rest

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
