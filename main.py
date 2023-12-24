ec277bc8c2558ec1d074758d6f57604b7cc674f3
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:24:58 +0000

    by thinkof chase typical wish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
