4c3752ea287c66895471b6dd36ac323f307940c8
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:41:52 +0000

    especially evening powerful 564encourage but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+def exit_program():
+        sys.exit()
