ea8c1dc0784f2d36f3290f201c1fca1abdf977b0
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:45:03 +0000

    regret swimming light make confuse

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+x = 10
+y = 20
+print("Sum:", x + y)
