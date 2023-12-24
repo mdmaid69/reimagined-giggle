1f6b94a652aea7161c6f7e3bf6cd82f4ea3d66e0
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:21:19 +0000

    567enemy address usual confidence command

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
