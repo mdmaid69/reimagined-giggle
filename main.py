f0dab3520ebe95bfb40d35535e5ac789de6d8bab
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:13:21 +0000

    stupid terrible onto apart concerning

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
