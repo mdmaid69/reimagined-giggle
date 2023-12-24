853992f5f896d2ce1d1feb130d03aa1de1fce78d
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:03:18 +0000

    cap because worry hate runaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import sys
+def exit_program():
+        sys.exit()
