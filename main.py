09888a94f71159c6dbfb7d4d3606a2f68d3179e0
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:39:37 +0000

    empty road hurry accident slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def remove_from_array(array, item):
+        array.remove(item)
