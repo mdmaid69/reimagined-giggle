12db3a2de1e484bec107af8ba542568a2a5024c8
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:03:29 +0000

    finish control round excellent world

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_as_bool(array):
+        return bool(array)
