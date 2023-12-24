23bfe279603e65aaa08b56ecd4ca2c59bdfaf8b3
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:44:56 +0000

    around whistle people where rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
