e0a60e9e630f249abb897e6560e2be350bd65e38
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:58:51 +0000

    city object go money dear

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
