24708f8a3d665284d4d696c79f811470a257b390
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:56:25 +0000

    complaint ride alive for mixture

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
