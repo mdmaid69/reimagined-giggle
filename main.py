fbd2917ce1e978aeaf39fe587a5639e95d509476
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:34:42 +0000

    but give place value part

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def convert_array_to_bytes(array):
+        return array.tobytes()
