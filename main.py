a824a10e7953f7900ce2895fb96c691d189b6105
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:23:21 +0000

    breakfast uncomfortable mother medium thing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def find_min(lst):
+        return min(lst)
+  def square_number(x):
+        return x**2
