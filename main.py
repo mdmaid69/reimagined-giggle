c72ef52f94ed5447ec6411110b9be43ada037ca6
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:42:43 +0000

    free tip he too right

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import re
+print(re.match("h.*o", "hello world"))
