b6b2232d05112ce4e12266a06acf5797a7099658
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:26:38 +0000

    car rate experiment some decide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
