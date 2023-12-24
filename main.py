3f43dfdd8daabf2e658b505eb4b599d7e4755fb7
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:34:57 +0000

    miss place fly success stranger

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
