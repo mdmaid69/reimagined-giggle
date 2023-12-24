6f4b64fb5364f3806212dbe2ed2f4d7b7586188a
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:18:32 +0000

    goon ought draw mouth crazy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
