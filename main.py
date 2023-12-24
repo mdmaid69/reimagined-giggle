c13b6366aa325597bf16a41b414ebeaa7b25e70c
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:11:31 +0000

    sensible yes fetch drink aware

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
 import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+print(sys.version)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
