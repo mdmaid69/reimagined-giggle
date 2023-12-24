65db551f3b0364e177492191e929dd8e84a0bd55
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:40:00 +0000

    embarrassing answer finally deliberately die

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_work(force, distance):
+        return force * distance
