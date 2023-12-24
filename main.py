e9cdb6ff8485941c7e47c982f42cff034418627a
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:33:45 +0000

    steep apart agreement path against

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+print([x**2 for x in range(10)])
