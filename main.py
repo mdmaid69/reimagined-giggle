08a65fa7dd9ddc2fcc8b73aae2d480cf2b29e67b
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:41:20 +0000

    comfortable nigga generous runout callout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import datetime
+def get_current_date():
+        return datetime.date.today()
