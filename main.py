dfb2abaf291dfb6930bb96759f30e99a08caa364
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:58:01 +0000

    children field pull example fence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
