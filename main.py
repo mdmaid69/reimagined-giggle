fcae0a46b1697149cb39c95061bdfa8bfd5ee034
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:25:58 +0000

    centimetre operate complaint arrive machine

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
