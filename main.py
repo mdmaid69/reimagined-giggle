af9d7a36d918dede354c171f848cce8944e1b9b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:57:57 +0000

    lucky action little stop sharp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
