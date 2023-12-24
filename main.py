1b2d9023b1ef3155d68d9ea07cb0dba33420b4fc
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:02:06 +0000

    badly it especially dead height

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def multiply_numbers(x, y):
+        return x * y
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
