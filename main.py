e93beac9e9bca99f58f8b7afc453f3d9b14e6903
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:23:17 +0000

    unfortunate born risk patient food

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
