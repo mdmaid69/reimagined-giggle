b583c731d635ac452f529d121234ec5eaf5a582c
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:48:26 +0000

    giveback generally size unfortunate that

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
