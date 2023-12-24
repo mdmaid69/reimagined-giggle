8e136b7d9cf0c3ddcc015d641169c0e43b244b07
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:40:58 +0000

    vanish subject country win absolutely

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import time
+print(time.time())
