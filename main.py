76af2cc259a9dac6e461289de10eb62bf83912e2
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:33:05 +0000

    fierce nonsense drive anyone winter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_priority_queue():
+        return collections.deque()
