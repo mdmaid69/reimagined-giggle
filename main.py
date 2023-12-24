a67ea7f089b32e1267726cdac5c9ebd5b2eb0e9c
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:10:37 +0000

    makeinto book claim carry pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_user_list():
+        return collections.UserList()
