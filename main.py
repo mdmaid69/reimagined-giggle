588dd1552da748476e9c9a86410209133a407d72
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:40:41 +0000

    consider so tune unkind education

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-print(math.pi)
-import sys
-print(sys.version)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def reverse_list(lst):
+        return lst[::-1]
