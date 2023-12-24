5134391dcb790c41d9788b2175c76e349b389059
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:10:47 +0000

    voice answer concerning modern rush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def convert_to_hex(n):
+        return hex(n)
