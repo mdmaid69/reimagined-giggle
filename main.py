7c7054cbecf944d3c70cc0c54b73d1c5b673629a
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:57:53 +0000

    payment glad art unit bet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def is_even(n):
+        return n % 2 == 0
+import os
+def change_working_directory(path):
+        os.chdir(path)
