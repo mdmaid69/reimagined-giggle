248e6c3574af7971634a249f82dde6485b00af84
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:06:49 +0000

    scream start ignore measure leave

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
