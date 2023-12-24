68fb94583234e6d13e476866efa421a016bea962
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:49:42 +0000

    Rr own decision dig ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
