c1427c0ad1f83604502ca3e498ae4035abc05e2c
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:05:11 +0000

    lookback(on) thought shame poison repair

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
