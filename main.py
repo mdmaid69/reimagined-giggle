1d659b171f1f1d01deb9e6319c6d8ba9304dde8c
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:15:26 +0000

    actually borrow movement plain count

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
