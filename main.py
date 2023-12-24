ebdec71e2f099a080bb629c0ef452cfe1000b38d
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:11:19 +0000

    usual closed commit factory worried

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import sys
+def print_python_version():
+        return sys.version
