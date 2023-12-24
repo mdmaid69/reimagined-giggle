7d70046000ed59764121e6af47d34e08fc3973c3
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:12:03 +0000

    reply callup talkoutof help half

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
