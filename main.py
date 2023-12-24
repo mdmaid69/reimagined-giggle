4b092a2a6e3464be16ed3a7b693cde5f145eeb2b
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:52:34 +0000

    getback then rain suit approve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def change_working_directory(path):
+        os.chdir(path)
