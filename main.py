87ddd9920c7a5896e26fb0643d3332be1bd183cd
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:17:43 +0000

    anywhere properly uponon squeeze comeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
