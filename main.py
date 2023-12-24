5030791838f6716590a71192cf1bfec45243f10a
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:58:43 +0000

    table complaint actually manner finally

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
