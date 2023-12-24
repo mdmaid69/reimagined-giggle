f1e38d9cb9aa1a80a3ec931abbad0ca1f7ded8fe
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:40:16 +0000

    feeling far reduction holdup behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
   def get_file_size(file_name):
-        return os.path.getsize(file_name)
+        return os.stat(file_name).st_size
