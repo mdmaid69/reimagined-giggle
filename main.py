1b4e20cb8b032b306ad1c3fdc614cd37d7d90f90
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:21:27 +0000

    pen line unhappy mysterious boil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_bytes_from_array(array):
+        return array.tobytes()
