4b1a93e3667eb9c4381349be388e8459e3b1b3d1
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:21:48 +0000

    fail prize turnoff correct behave

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
