a8c8663127e498bf1f4cfa713c73a42ad147d511
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:24:57 +0000

    equal turnoff remove worried rule

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
