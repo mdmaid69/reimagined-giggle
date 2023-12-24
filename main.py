d06215385b61eda3706e16f934fe42d9b2cda7c6
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:09:48 +0000

    fat exercise charge turnaway hers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
