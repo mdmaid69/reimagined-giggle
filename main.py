98be83f6ee3f46d93f049d2a7bd520f33a85ff11
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:43:12 +0000

    cold judge spill foot brief

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
