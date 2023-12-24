d125d64dfca2e03580744352bcda4571965aa18b
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:43:29 +0000

    change snow big Ff off

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
