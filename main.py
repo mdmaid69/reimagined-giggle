27da4c001f86f42b8740c9b09201ae84ce2b577c
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:28:43 +0000

    plan tap getback battery neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
