b59939638b8f5b4f9521213087c4dd0445caefa9
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:14:42 +0000

    test Rr probably equal succeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def clear_array(array):
+        array *= 0
