4acdea3e86a0569b3759b2cce0c6088a0bdbe14d
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:34:55 +0000

    claim belong criticism defend 572enter

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def get_array_as_str(array):
-        return str(array)
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
