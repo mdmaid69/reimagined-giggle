5ca8cbebc4646999dd9ae64749c9d952c7846342
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:00:07 +0000

    beyond show brain plain warning

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
