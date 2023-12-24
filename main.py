32a74e751cbb5607449cb6ee61195fa88fccf9ff
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:35:40 +0000

    better birthday guide really valuable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
