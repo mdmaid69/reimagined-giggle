c1bcbfcacebf9b56ad723bc044a92da47989c93c
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:06:01 +0000

    house shape new border impressive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def find_max(lst):
+        return max(lst)
