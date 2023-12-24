a7ee5be42e3f0d3118819a31ff10bf4e0d8f7f73
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:36:52 +0000

    completely tooth herself march winter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_bool(array):
+        return bool(array)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
