c2fe7a830a87d12cd8d35568f5373e16bd20613d
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:36:38 +0000

    priority safety surprise month Vv

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def cube_number(x):
-        return x**3
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
