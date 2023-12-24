881a160573ef19ff1a919934fb7a5e41666e2856
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:28:36 +0000

    town pressure now Dd reliable

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
