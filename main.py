46f31b9c1b50b59356e44158e269bfb168e6a3f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:03:35 +0000

    pen set its throw suckme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def print_python_version():
+        return sys.version
