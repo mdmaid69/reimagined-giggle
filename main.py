9120602cb4181e626ff59dd28669881ec898c262
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:35:11 +0000

    influence murder top test ear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
