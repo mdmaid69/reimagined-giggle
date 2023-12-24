02654277310eef728227122d33da3cff97d7d8be
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:29:24 +0000

    half back case temperature strike

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-for i in range(5):
-        print(i)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
