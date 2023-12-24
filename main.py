7645bd23ae94b56ad6ffecf54a87a64b0d6730e2
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:13:32 +0000

    breakfast accordingto contribute disapprove through

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
