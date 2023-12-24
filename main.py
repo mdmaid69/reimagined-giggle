b301fdfc167f2ab23ead4fd1383cae8ec5ea802d
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:55:38 +0000

    doubt formal send reduction definite

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
