69020ededd290d309b932473866c32d47039ba2f
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:07:56 +0000

    intend operation swallow party mess

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
