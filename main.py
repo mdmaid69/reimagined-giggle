8d6bcff6120885f0ac30b298d8c595bda30574ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:25:21 +0000

    service aim disgusting organize fortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
