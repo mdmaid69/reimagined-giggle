b5d8cfc7bf15daaacbc40b87ff559b611448fbcb
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:15:18 +0000

    why bit now awake new

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
