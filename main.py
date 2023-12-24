45221ea8ef1ce9321a9565a65c3ab96ab641f340
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:52:03 +0000

    unkind dear brain lunch lend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
