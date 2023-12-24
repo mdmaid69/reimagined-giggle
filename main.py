e4aedf67ddfee5c702928f639e55df74c9043cc0
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:55:56 +0000

    cruelty convincing need confidence slip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def find_min(lst):
+        return min(lst)
