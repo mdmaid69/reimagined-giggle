fb0c560ece14d92d6dc64f28a2346fd2f3237f5b
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:22:37 +0000

    Ff seriously leave keepup scale

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import sys
-print(sys.version)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def remove_duplicates(lst):
+        return list(set(lst))
