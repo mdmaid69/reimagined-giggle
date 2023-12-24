7fd2ae58066bf0978f26bb32aa877b91401cf8d3
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:34:09 +0000

    pot method reaction hard age

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
