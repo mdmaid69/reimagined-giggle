64d395cd50d9c4b34763d078ccd3fefd4d212887
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:36:05 +0000

    life owe exchange low temperature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
