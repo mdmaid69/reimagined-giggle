a1bc77150f5686f05c548229313f498b4ec0f763
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:08:45 +0000

    godown occasion test healthy breakout

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def get_array_as_set(array):
+        return set(array)
