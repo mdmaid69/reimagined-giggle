a8ddf9811087c52424ff6324d00cf1c8afdaa7fd
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:45:07 +0000

    settle entry breakout test confidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def exit_program():
+        sys.exit()
