b70b6cfc7b2dc14a4bbe3f08c4367de3f6ec2c96
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:50:08 +0000

    one violent her 570enjoyable suit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_current_working_directory():
+        return os.getcwd()
