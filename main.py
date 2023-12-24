0eff9399a59b654add31902d997c7aa1ce2271dc
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:54:05 +0000

    offence right full introduce skin

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
