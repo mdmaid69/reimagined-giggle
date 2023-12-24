58eff798e291596b864322b095cc8df59149ed74
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:08:30 +0000

    relative Ll sure matter understand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_list_from_array(array):
+        return array.tolist()
