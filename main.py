d3a891fbdda6b707673c0332818b1d3382ceef18
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:39:40 +0000

    people door friendly mine bringup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-print(time.time())
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
