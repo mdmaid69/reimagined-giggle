f8e47330ddfd950df3509bec129b022eb6fc7fb7
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:38:29 +0000

    method finger various sense total

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
