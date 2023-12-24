0be4d86f9388b2aa49dfc3062090ba2c98dbd80d
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:27:22 +0000

    wrong type broad head hardly

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
