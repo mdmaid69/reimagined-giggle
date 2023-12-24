1a1c859ea7f7e54b8fbf2554de0936a1b91c4820
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:57:37 +0000

    emergency clear hold weather bored

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
