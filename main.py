7f42744cdb89731eeb8ef93f40bf031a80b1381e
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:35:37 +0000

    tell say factory upwards hot

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
