d5571801bc9f4fbd61d515b238d5075b7f77dede
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:58:07 +0000

    cough punishment takeback tense wander

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import random
-print(random.randint(0, 100))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def find_max(lst):
+        return max(lst)
