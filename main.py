df0a883f992165de9775452f851068a975ba396f
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:20:58 +0000

    software breakdown or bus early

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
