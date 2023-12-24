ab1b1a41e1d8fb4a355f93ae8f23c2e65c932ae4
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:35:56 +0000

    meanwhile uncomfortable most her everybody

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
