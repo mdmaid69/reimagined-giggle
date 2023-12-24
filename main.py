8189c8a1223e383d5f3b6f5440442ab28d6805f3
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:12:13 +0000

    little finish judge certain hardly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
