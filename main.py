f4e7ffd7af1864195bd61ef7f0fdb3beb6c25345
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:23:14 +0000

    family because empty period coat

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+for i in range(5):
+        print(i)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
