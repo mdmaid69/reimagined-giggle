b26199f4dfea7289630927376a02f3d723726b4b
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:42:53 +0000

    stamp method repeat goaway soon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_priority_queue():
+        return collections.deque()
