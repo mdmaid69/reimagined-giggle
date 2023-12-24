167ed4809bc5c0f222dcd594647492b3b49b30be
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:24:15 +0000

    that paper season complete scared

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
