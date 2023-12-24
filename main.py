5d3d493dd26f1bea68b32aa45bc65d83ffa87ae9
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:38:43 +0000

    date shout exit communication fall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
