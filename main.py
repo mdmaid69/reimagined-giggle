275052bcf55391985bbd50d1c569094781e03185
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:56:45 +0000

    rather correction may protest basically

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
