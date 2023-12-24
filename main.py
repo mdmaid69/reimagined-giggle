4c00d7be0f7ebeaf93dd2e85ec56f38df7d315b1
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:10:29 +0000

    better bottom choice apologizeapologise hungry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
