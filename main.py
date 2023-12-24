6c578bf6beb6a431d65d9c27eb60682fdc7224ae
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:12:02 +0000

    delight compare loud boil dress

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
