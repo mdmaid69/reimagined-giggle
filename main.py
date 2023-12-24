ed798cbe727932df3a26f16daa993de8e7b3e990
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:45:18 +0000

    broken since loud slice punishment

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
