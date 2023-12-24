fc28921ea26d041c5ad1023a92f9a0bafed720ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:00:46 +0000

    road tap turnup bet apart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
