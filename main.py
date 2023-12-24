48c6362c2f299e2a78f2c4d568bc86a145b610b8
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:26:09 +0000

    aware little congratulations its level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
