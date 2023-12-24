6d933b84a1b2fb02d58141a93e04ca7d4b48281d
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:21:02 +0000

    worry dream class connected pour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
