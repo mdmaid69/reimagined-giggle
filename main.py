68c39e5a95933c3d69f93d1d09b8c847bc979b60
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:55:36 +0000

    down somebody culture keen content

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import collections
-def create_user_string():
-        return collections.UserString()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def reverse_array(array):
+        array.reverse()
