ea14eef92983c545a6c0794ab50b10569c3c537d
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:23:25 +0000

    cruel seriously fear their deep

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
