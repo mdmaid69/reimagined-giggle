3dee03370a9af97e3f25d1502432e7fc82d4ad3d
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:50:38 +0000

    trap approve yes correct could

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+for i in range(5):
+        print(i)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
