fdbc5e529fcb4553634924c71ad99ccbe814c79a
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:01:24 +0000

    modern once pleasant flavour another

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
