79ebcd34f5235e46ff63702913a928f4995883a9
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:00:57 +0000

    pair punish steer anything joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
