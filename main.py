18e5019e78a8f43978a87050bf4597628305c24e
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:53:21 +0000

    usual holiday name bend crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import re
+print(re.match("h.*o", "hello world"))
