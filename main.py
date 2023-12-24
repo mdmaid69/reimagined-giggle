0041bf78c69b9078eb8f2aa9efd0af507a4cc4e5
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:58:17 +0000

    load smell obvious drop alive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
