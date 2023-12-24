5789ef752df12b55203fc078d7831b10ea490275
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:32:17 +0000

    like cover match keep light

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
