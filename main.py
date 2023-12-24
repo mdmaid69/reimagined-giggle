fd8395545ff305a28086c29d64f1ae2f27c8bb39
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:23:06 +0000

    whoever born sad scream fetch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-name = "Python"
-print("Hello,", name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
