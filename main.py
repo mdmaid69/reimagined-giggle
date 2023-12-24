3cd05453fe4586bd1df4b63e93647437099bef81
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:45:10 +0000

    fold file company under shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
