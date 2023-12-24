d970ee2c3d4b49536912fa3d08e4bcf4b0922650
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:46:36 +0000

    curious story callback poor chance

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def remove_from_array(array, item):
+        array.remove(item)
