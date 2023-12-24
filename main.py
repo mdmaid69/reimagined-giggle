9cd387508fb9459e5fc9bac337c1a48e7c7a4cb9
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:09:03 +0000

    offence disgusting control further old

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
