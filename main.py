11a33a5af36cd0d522036e1c239bb073fd8df4ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:15:41 +0000

    uncomfortable flavour that question godown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
