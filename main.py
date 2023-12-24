08f2b7c6ddd5926db097b9c1134d38ac47026faf
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:08:39 +0000

    command anywhere copy raw position

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-def multiply_numbers(x, y):
-        return x * y
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import math
+print(math.pi)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
