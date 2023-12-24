ab9022c091eaa6fba00434c03b255d79840d0efb
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:48:40 +0000

    injury delight completely track owe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
