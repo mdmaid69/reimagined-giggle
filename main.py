5365ea06660f0c345efa687772a09b2bb21fe9ba
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:10:25 +0000

    better concentrate needle worth common

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def get_array_length(array):
+        return len(array)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
