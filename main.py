a18e3a06d4fe1edc08c5b4e81bbb16b4b38725de
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:43:11 +0000

    finally danger sometimes seem among

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import tensorflow as tf
-print(tf.__version__)
+  def get_current_working_directory():
+        return os.getcwd()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
