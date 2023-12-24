6522901e8dff086fb585b57a592a1e4caa2fbd4d
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:53:24 +0000

    child narrow neck although mad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
