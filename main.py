a98a79b5a5f939d7ebe7e69d4ca3498e57c14d81
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:17:24 +0000

    goodbye mix so getback least

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def find_max(numbers):
+        return max(numbers)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
