8daa0f11264904918d9c369a87292f18076064e4
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:13:13 +0000

    better warm straight every who

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+text = "Hello, world!"
+print("Uppercase:", text.upper())
