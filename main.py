d17f7e7adbe5d06962c8b73477d706a95dada77e
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:04:16 +0000

    both second Oo before thing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
