12f9293ac37a29c60987c6324d1d4ffa38ff25d1
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:20:50 +0000

    reduction will avoid shall knock

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+print(sum(range(10)))
