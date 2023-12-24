1cc7fe515c8ac758bc236d78bada2627c96a34bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:35:09 +0000

    strike advanced prefer scale want

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
