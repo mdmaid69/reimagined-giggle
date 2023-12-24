a0ef46c57a9857469179758d3f9ce437bac6edea
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:07:54 +0000

    properly thin go deep age

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
