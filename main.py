f9141bf6700b999c4763a4172db35696097fe7ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:42:52 +0000

    how own married sail claim

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-def reverse_list(lst):
-        return lst[::-1]
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
