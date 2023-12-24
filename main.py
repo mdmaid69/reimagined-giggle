d01c88ffb488d7fd90f8cc329c3180c30e3d33b0
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:13:08 +0000

    left bill remain present close

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import os
-print(os.getcwd())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
