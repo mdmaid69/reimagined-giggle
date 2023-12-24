9f41414c34a9b1edded2854ba191612e00a5cb6b
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:50:52 +0000

    clean judge tax present in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
