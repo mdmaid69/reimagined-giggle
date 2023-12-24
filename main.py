491b4d3d1b8b249637f5fc25caa44b41a8e1ab8c
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:04:17 +0000

    before national dead group company

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import collections
+def create_priority_queue():
+        return collections.deque()
