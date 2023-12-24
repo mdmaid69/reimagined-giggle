a7b2772b54c2a73511185ec35bea58f440d463d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:01:40 +0000

    front dry deaf but strange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
