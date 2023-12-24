e9cf94bbd4b8569e684c526e702f5cc77ae9deeb
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:23:46 +0000

    window switch need capital turndown

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import json
+print(json.dumps({"name": "John", "age": 30}))
