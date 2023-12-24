22aa7b8b84fbacde637c6e7cf76aaf19123bc783
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:33:08 +0000

    there tense so face spot

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
