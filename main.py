6732c03b0fafd6ca4565c042c4520e1127359b46
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:28:27 +0000

    bye search weigh defend do

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
