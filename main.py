947d0bb0c4e933b3fdfff742e12bbdf78c0903b7
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:18:55 +0000

    cause instrument real level search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
