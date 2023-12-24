231a023f68086c0caa9298e5d66b4115df4ef354
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:33:15 +0000

    angry obtain note clue just

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
