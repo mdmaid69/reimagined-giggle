682894707a9d35da64e2e89be7ee91fbdd9db170
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:12:07 +0000

    point surprising mass sad standard

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import array
+def get_array_as_set(array):
+        return set(array)
+def sort_numbers(numbers):
+        return sorted(numbers)
