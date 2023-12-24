afdd4bef13e5c55326be22ed3395eab9c96ec55c
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:23:03 +0000

    type suggestion there exchange divide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
