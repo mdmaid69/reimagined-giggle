d56b07417e03171a9acb19eab5fc3e48f9ff5ebc
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:44:41 +0000

    standard smooth pay recent feeling

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
