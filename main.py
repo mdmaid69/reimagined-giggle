4a43606196cb56769b9dd289cb3bde2152ee7b87
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:12:51 +0000

    feel passenger transport separate history

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+print([x**2 for x in range(10)])
