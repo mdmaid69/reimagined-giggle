b0ac27f0f936be25573db1110183f8b27228eb18
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:19:56 +0000

    noone yesterday condition form favour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
