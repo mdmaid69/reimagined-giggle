8edbc1f84d9b9190e6c7894faadf17dc7efb5e01
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:15:38 +0000

    offer exhibition culture lesson offend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import sys
+def exit_program():
+        sys.exit()
