dda5429e417b6f9ba939247dad8aad0c5e073a4e
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:15:30 +0000

    home home section description regret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
