30e8dae6b94748bea9c5002befc60167589288c9
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:41:11 +0000

    exit write possession insist over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
