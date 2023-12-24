756f60b88802b6f61e7f1c40b312b33ce55bdd62
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:13:25 +0000

    under no interesting turninto direction

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
