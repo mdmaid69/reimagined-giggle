55e6679243cdffd9e76ef56078bf20dc8ba96fab
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:01:41 +0000

    interruption curious mine say flat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def remove_duplicates(lst):
-        return list(set(lst))
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
