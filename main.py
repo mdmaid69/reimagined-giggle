ef7f489d5321957289423d3995a49f4fbb7daa59
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:06:06 +0000

    wave value disappear world soft

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_factorial(n):
+        return math.factorial(n)
