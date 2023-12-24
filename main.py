b440b56bd119688d958867a6fa2a5413d17d71f1
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:35:31 +0000

    get cut statement second inquire

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
