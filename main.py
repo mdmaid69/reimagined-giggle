cc5073cc3663ea9c4927d88c2a5261fc7038a511
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:14:39 +0000

    curious score sight any careful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
