5111c40a491bdc7696bcfcb05fe17ecba53c25f3
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:10:46 +0000

    vision unkind back march control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
