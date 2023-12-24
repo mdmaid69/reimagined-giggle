ff98d25a324bb6f076dbd08ec4cee8e6f9206161
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:08:57 +0000

    occur dark regular naturally Mm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def clear_array(array):
+        array *= 0
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
