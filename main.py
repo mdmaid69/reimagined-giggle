2d4923eab96c9a397272dcfa6b6c929fa87c67d3
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:14:35 +0000

    final comeround tired equal scheme

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
