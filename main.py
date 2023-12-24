0d9f02d7a5102d850479c9defd09322057646c2a
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:40:14 +0000

    round survive payment I charge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import array
+def append_to_array(array, item):
+        array.append(item)
