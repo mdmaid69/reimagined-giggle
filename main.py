1438bce53a1dca5a2b65a33157da0144514c7859
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:11:16 +0000

    while offer would nearly chin

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+  def square_number(x):
+        return x**2
