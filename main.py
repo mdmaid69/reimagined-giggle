c2b5bf6f97982411e25255e37037eef881a7ba00
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:13:21 +0000

    part ear stiff otherwise again

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-print(math.pi)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
