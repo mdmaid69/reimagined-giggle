56634c98b337a14020b3ce0c89017d4a05dfb193
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:22:29 +0000

    rest chance give song rent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
