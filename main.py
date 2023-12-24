947c2c383c865ce50447fad186a7c4f01958f577
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:28:27 +0000

    Nn mark copy mother claim

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_priority_queue():
+        return collections.deque()
