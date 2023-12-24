ad8621dead08fade65aa19f2c6dc0a5dfe61ee30
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:48:40 +0000

    muscle cover male read habit

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
