f36e43aac37ad90dbb1983797720e84b0408ca2c
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:54:03 +0000

    yourself tip genuine finally stop

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
