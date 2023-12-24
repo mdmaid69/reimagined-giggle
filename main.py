d568da522eb24218d2f6b0c392a7d5644ac08682
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:03:09 +0000

    little we turnout deal explain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
