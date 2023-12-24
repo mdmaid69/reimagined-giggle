06dca83619a3d81dca0de0d7691152ecc5986f82
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:20:44 +0000

    neck judge limit everybody any

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+for i in range(5):
+        print(i)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
