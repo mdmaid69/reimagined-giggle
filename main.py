fa94d707a5d83451655cf400cafdc52efb93add1
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:54:07 +0000

    period move prevent kind hurry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
