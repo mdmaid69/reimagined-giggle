9e987a92340b9dc031af06f4588b6ce79a7a7f88
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:08:27 +0000

    spring slice bet law accident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import platform
+def get_python_version():
+        return platform.python_version()
