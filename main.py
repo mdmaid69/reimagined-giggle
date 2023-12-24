bbce4bae3f1002cb94261e3f7c86a1ca25fbc6ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:07:42 +0000

    bored reply settle would him

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import os
+print(os.getcwd())
+def calculate_area(radius):
+        return 3.14 * radius * radius
