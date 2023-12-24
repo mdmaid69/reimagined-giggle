47e5a4b7b6ed0674562be3853fd2d9b8e99a635f
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:08:36 +0000

    friend trial worst thorough lookup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
