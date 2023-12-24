1e68e0f84f3384ab6f15290cd407db5e7fafa04f
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:53:09 +0000

    special right pencil approval sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
