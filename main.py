ae44ca845ab8b2ef47e2a4a86958d0bb4d9990b0
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:26:14 +0000

    depend by matter description crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
