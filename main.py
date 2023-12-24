8ff55f17ef2bad0518d795514587f01c63f1a731
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:17:45 +0000

    refuse else unfair manage flight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
