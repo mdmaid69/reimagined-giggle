0e68dbf457e454ae9b4878922e7e57ed0f74ad9a
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:23:16 +0000

    word regret sail declaration bank

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-import numpy as np
-print(np.array([1, 2, 3]))
+  def convert_to_binary(n):
+        return bin(n)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
