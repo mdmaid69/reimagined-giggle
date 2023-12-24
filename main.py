ce97cc701fd2815ea0a7c4d98b19a74f4b4bf343
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:09:06 +0000

    Uu more fast shake turninto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
