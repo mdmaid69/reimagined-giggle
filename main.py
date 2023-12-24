26a889c538d7198b3b9ef84602d1a0d23f300df4
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:38:06 +0000

    interested imagine floor pure past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_set(array):
-        return set(array)
+def convert_array_to_bytes(array):
+        return array.tobytes()
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def remove_directory(path):
+        os.rmdir(path)
