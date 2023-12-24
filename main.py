bbad5d7763ec5886c85b348a4b8b7552a46626a8
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:16:13 +0000

    sing round science high war

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def subtract_numbers(x, y):
+        return x - y
+import os
+def get_current_working_directory():
+        return os.getcwd()
