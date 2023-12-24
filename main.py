4b6d012b2f59d50cf64eb73805bb3bbc4009cf90
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:29:09 +0000

    press wide doubt emphasize occasional

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def count_words(sentence):
-        return len(sentence.split())
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
