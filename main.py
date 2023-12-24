5179d85d247eac4dcbc34cdce45090ba239a13ae
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:44:00 +0000

    favourite rely various ago criticize

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def reverse_string(s):
+        return s[::-1]
