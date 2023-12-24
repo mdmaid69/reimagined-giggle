7925904ceb5a67bf7f5e1daeffd1ef2ad58e0a63
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:23:44 +0000

    care man destroy fill order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
