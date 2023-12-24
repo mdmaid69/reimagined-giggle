10e2a4c65ddcb1800eecf6c26bf1155b38e71d07
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:50:08 +0000

    flow scared note consider please

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def subtract_numbers(x, y):
+        return x - y
+  def calculate_area_rectangle(l, w):
+        return l * w
