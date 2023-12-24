b03cef859abed4afc94484b815d0f3c4ff38328c
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:40:14 +0000

    introduce hard correct left convenient

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
