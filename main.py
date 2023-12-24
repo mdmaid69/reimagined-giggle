a2ee192a597978479198e53f3a7ee4902470a740
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:48:15 +0000

    neither label heart equipment theirs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
