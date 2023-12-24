034aedaa6420bdc596770f171f336c7867145b92
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:29:45 +0000

    congratulate computer forget passage before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
