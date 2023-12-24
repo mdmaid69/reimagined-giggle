a9617777366cec9a12c6cb92a4d36c3451ed8660
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:41:10 +0000

    talkoutof conduct any I formal

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import tensorflow as tf
+print(tf.__version__)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
