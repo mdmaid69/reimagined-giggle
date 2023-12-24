03c39853213bf755751a0e0bda0a17c65432c094
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:14:32 +0000

    cheat once high window movement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import sys
+def print_python_version():
+        return sys.version
+import sys
+def exit_program():
+        sys.exit()
