9622d15b850c1ecf83c0c02285e4c28b77dc3195
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:36:14 +0000

    direct journey opinion talk write

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import random
+def roll_die():
+        return random.randint(1, 6)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
