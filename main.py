8d93cd63e1b0de2888503451966d2d3c87876478
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:15:55 +0000

    call manner top damage runout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import json
+print(json.dumps({"name": "John", "age": 30}))
