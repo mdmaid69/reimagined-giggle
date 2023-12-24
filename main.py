63a83461ca7e518020d30ffbc3beeb30ebb95592
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:56:01 +0000

    wide gather Zz cold shut

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def remove_duplicates(lst):
+        return list(set(lst))
