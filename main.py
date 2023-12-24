dece3760faf44fc8f0b47042419eadb7643fad41
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:52:02 +0000

    connected so Nn foot whichever

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
