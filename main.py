ed1f16c014586151053b291d929a2c4add007cbe
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:20:46 +0000

    disturb hear yourself intelligent find

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import time
+def get_time_since_epoch():
+        return time.time()
