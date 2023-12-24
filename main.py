9e6966a72a22ae09eb11e2a779826f84e891a033
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:49:37 +0000

    noone hello close treat slight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import sys
+def print_python_version():
+        print(sys.version)
+def reverse_list(lst):
+        return lst[::-1]
