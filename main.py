b114f9c6bc3ca632804ad6f853c3c78be1cd84c6
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:01:20 +0000

    getout town mistake giveup broad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_priority_queue():
+        return collections.deque()
