2fee0726ac3e94df22c245c9f568840889eec730
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:36:50 +0000

    ring involve top drive workupto

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
