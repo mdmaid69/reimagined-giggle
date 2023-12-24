27a2ef7ea7e638801b86b16cf6c9116ac6e1d157
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:08:37 +0000

    explanation calm throughout refuse lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
