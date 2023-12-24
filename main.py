ce1fe373b986799ad85cc58b5f27235ca273ddef
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:35:31 +0000

    sail manner funny shop pretty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
