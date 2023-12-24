503cb922dfdbe4a4631dfe2442e66955e8d57f2f
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:12:48 +0000

    wealth stranger squash touch thanks

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
