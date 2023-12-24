944f2df1ff4e10f1de6ece32f0c2652c405a7f3b
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:41:53 +0000

    frequent war joke positive cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
