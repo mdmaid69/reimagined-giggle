58aaa27a4c67686c04278772c824076f83181a79
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:13:29 +0000

    twist guard argument our in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_floor(x):
+        return math.floor(x)
