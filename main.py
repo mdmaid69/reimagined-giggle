89d43336e2b75d7c5d0edfae980465d8403f065a
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:40:14 +0000

    check win smooth criticize ago

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_max(lst):
-        return max(lst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
