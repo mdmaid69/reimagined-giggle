80c0cf6c5ff6521de23e8e8359c30aab06961f7c
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:06:19 +0000

    unhappy report disagree young reply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
