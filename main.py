0e483f7d854c73a2ed7f64b5bafb951cd8258baf
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:44:43 +0000

    runout copy arm theirs phone

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
