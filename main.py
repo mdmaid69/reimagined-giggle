6371f90171c7b544a5481259328e8338c229c8f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:04:33 +0000

    whenever would turnback house point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
