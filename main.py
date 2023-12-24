4ca99dee4d66e4a615ae942e7be159424fc73f5d
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:05:57 +0000

    side without exchange same break

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
