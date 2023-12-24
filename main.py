232174ce90541caa7abc420d06d07ebcf8f79e34
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:31:47 +0000

    computer cruelty eat tooth defeat

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def cube_number(x):
-        return x**3
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+for i in range(5):
+        print(i)
