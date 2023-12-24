2876970430fa2c4c55a22968d3140129fa36c069
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:06:59 +0000

    valuable involve cross previous ignore

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
