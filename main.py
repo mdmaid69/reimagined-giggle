47f74b43aa7d5b00ef1e9ad316ec736ccac4e8d2
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:31:14 +0000

    live instruction attractive satisfactory winter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def convert_to_octal(n):
+        return oct(n)
