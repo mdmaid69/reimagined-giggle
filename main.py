21b6fa91e4aa3f3e41f1edb4552966de7573f7d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:36:32 +0000

    would plate once brush they

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
