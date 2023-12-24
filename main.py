5f8ba92bcf6c93733d39b2920902628e7c2b9e98
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:53:43 +0000

    face goup right will reduction

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def add_numbers(x, y):
-        return x + y
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
