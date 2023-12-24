5d87171cdd44efebc7de602d711a16fa55948417
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:05:39 +0000

    backwards inside moon herself sing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
