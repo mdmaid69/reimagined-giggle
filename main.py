ab3411930ba9821b3364f095555bd57d418c9dd9
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:10:18 +0000

    rise place getup since satisfied

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def create_user_dict():
+        return collections.UserDict()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
