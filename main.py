3c1c9bf714161208f2266bdfdc8bf4edf7037d35
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:09:46 +0000

    search excuse break burn otherwise

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_as_repr(array):
+        return repr(array)
