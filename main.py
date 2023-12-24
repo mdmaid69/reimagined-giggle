114312e19d861652222018fd512d40e3651bf6c0
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:18:27 +0000

    smell joke form obtain burn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
