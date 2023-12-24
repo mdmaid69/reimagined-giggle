64342985123939744d6f616f7f30b598864e2440
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:05:01 +0000

    hope theirs real decide preparation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
