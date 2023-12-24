424d9746b1b23b190fe861d27e883ba2b9964bfc
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:18:18 +0000

    public vote 565end bread worry

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
