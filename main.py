d0528aa6a7fe75840c6cd0a3cd7bae1136e3243d
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:16:26 +0000

    runout shrink hospital tax field

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def append_to_array(array, item):
+        array.append(item)
