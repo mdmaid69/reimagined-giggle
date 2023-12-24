be69b4ce9bcf3f6241701bb7ae5135d4345f2bcb
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:24:32 +0000

    leave mix hot home vision

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  def remove_duplicates(lst):
+        return list(set(lst))
