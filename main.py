fd16c27d5770d75b48f66f66ead82e07abcd88dc
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:43:07 +0000

    equipment excuse experiment search plenty

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_set(array):
+        return set(array)
