5af9735cb547eda0cee17aea4108ec892866d062
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:39:37 +0000

    mistake impressed question Itself doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import os
+def remove_directory(path):
+        os.rmdir(path)
