7044a2b51d3d69c5197bc4b09856bc0271eb8bf6
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:30:21 +0000

    turninto poor win cost scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
