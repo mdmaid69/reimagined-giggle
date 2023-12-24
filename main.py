44cd6c6996cdbc1c957a1625023e4966eb3531a5
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:00:37 +0000

    well hide spend lose movement

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
