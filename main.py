09162f1b811e4272dfb20ac849ad895c78bd17b1
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:43:06 +0000

    lead over apparently part hot

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def find_max(lst):
+        return max(lst)
