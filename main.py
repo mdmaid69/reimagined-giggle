85e9b5b92c44fc6e008777cd18fb5f75bab00181
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:58:02 +0000

    rather collect slide generous replace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def square_number(x):
-        return x**2
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
