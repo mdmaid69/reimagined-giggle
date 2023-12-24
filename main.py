41571df9429a694197b452a636f4427c770ee57c
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:17:27 +0000

    way schedule news organize ring

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
