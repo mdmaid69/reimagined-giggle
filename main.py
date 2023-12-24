f729b9be3a952011ee8e7ca8bd54c030dd81d68b
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:56:34 +0000

    nothing immediately because cheap door

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import os
+print(os.getcwd())
