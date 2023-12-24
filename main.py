9a2ebf9301cd89458212984dac2edf5a6520b40d
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:30:43 +0000

    565end hurry final whisper luck

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
