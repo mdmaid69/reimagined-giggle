d5ddf9584a358ad856610c6347f2731e338d8c57
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:18:02 +0000

    occasion under Dd professional tidy

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def is_even(n):
-        return n % 2 == 0
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def subtract_numbers(x, y):
+        return x - y
