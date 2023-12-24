9024dce25d215588d11caa68503ca8edc05ec4d6
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:42:54 +0000

    refer trade chase run mix

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
