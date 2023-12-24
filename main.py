bf323b0bae5efcb79008b5f172ae224eaadc4077
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:37:23 +0000

    cruelty furniture even Nn authority

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
