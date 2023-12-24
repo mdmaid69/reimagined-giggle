1bfe2108546446e5dfeb5544f8f3235053ae3c41
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:49:06 +0000

    handle term usually habit person

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def square_number(x):
+        return x**2
