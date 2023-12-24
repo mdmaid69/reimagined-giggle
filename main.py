92845b18cc8adfae779f3e400c892dee05fee8c3
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:36:35 +0000

    bringforward matter strike among entry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def reverse_string(s):
+        return s[::-1]
