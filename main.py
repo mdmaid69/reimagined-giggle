c2b0ae1fd6767ecf5eb90ebe1be5fbbabbc40e86
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:39:21 +0000

    rain tend spot fashion whistle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
