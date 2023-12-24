7265b528281f000bc6e85e14ff6306784278c279
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:40:28 +0000

    listen concerned pay issue lookafter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def sort_list(lst):
+        return sorted(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
