c9665c5cd3db05b6f65bc786f5106adaef1f2707
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:43:23 +0000

    surprise pronunciation brush Gg travel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
