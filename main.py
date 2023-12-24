f00bc8f76dddc483ed9b5253d6e0a3063382cc77
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:16:58 +0000

    shame indeed wrap pride mistake

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
