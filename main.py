a068eedeeb65dd193c05ff8eaefdc9ed0c650607
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:56:52 +0000

    taste organize although successful very

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_item(array, i):
+        return array[i]
