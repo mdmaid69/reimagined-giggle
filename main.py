89bbba308038eb70e714de4eda2c54d849d5b5f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:05:59 +0000

    round recommend turn then upstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def clear_array(array):
+        array *= 0
