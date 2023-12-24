56e6929ac14a8d5623162d2fb68023eea25d5fb3
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:04:31 +0000

    frighten never shade goon recognize

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
