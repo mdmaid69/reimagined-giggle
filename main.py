1eda2105c07787312d8a8e4aeda448f08f7b0e90
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:14:29 +0000

    argue result responsible delight heat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
