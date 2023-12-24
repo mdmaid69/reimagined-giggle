0e5d42c1ef8eabee73c13e55d4f8b71411517a8e
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:22:06 +0000

    road lesson prepare swim worst

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
