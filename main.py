b01a0ba9e27aa8b446acbcd9e3f3833d03b290b2
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:44:08 +0000

    machine price close shall order

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
