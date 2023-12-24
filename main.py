bf180db5000598524110c73a97d1a2dbd9a9557a
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:18:14 +0000

    experience tidy transport talkinto responsible

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  def add_numbers(x, y):
-        return x + y
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
