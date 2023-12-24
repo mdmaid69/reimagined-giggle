4ba5cd84d7b87756ab1039024300f547b6816d47
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:22:14 +0000

    custom concentration total slide afford

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
