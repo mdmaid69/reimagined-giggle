1c5ecc02e30724683e57ff7f4c00c1ae2dc5028a
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:42:54 +0000

    concentrate aim preparation imagine congratulate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_min(lst):
+        return min(lst)
