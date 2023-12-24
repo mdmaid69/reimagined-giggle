3bc967fdd6f8b016adce5bc7efa0d535dc9ec885
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:20:50 +0000

    ought every mistake complicated slightly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
