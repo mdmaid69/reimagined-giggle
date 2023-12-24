901317aea8c931d6675a62ac39a0f7b8d5c5036e
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:50:11 +0000

    band mess base Kk dig

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
