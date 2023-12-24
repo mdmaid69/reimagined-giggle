9c166925fd561925305bb6a3f595d1f301d26df9
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:25:44 +0000

    way road pattern solve draw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import time
-def get_current_time():
-        return time.time()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def convert_to_binary(n):
+        return bin(n)
