de8daf5af2a5b538fd7e5d0a3eb52038b8bd85ed
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:14:55 +0000

    question tough crowded sticky children

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_length(array):
-        return len(array)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
