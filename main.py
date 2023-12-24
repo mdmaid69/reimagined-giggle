a2a7f18f9dcfe06c6f850c49916324e50a9ce2ff
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:48:42 +0000

    everybody whom dead environment ilovedicks

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import time
+def get_current_time():
+        return time.ctime()
