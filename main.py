87f6c965a63963095bf09a185c17aaa461e21cf1
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:20:27 +0000

    picture stand unless that best

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-def is_palindrome(s):
-        return s == s[::-1]
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
