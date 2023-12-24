64adce2f71f5c336c17cf2d037f88b72ed393301
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:11:00 +0000

    cheat 568energy count damage gift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
