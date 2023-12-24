c3395c7932e202f4cb4760119c0554d1e72fc632
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:12:00 +0000

    Tt hour tooth connected capital

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
