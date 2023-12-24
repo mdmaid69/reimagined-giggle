91917da1ae12f907c070d1f537dc878837a77ef8
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:12:42 +0000

    backwards wide doubt play drive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
 import re
-print(re.match("h.*o", "hello world"))
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
