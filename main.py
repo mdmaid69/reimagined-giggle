952f7570bc2d011fa7b07df43162c9ebd9feb39c
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:39:18 +0000

    stranger false bet trip connect

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
