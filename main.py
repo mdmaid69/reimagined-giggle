77ff20fc15607886d227ab3d158caaa4fb056f17
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:32:22 +0000

    once friend something tear comein

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
+def cube_number(x):
+        return x**3
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
