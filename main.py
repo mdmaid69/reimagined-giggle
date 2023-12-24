449f3351a4780981ad1003767195d9ed0272962a
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:29:24 +0000

    moment put nonsense mess turnoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
