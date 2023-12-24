20f0cc8ce24418ba6be90e082d6002ecdb3dc695
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:07:10 +0000

    benefit sensible how order sentence

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+for i in range(5):
+        print(i)
