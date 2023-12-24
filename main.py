20459f120871904d72ec0f0172b69f57d99aa5a6
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:52:52 +0000

    famous story least vanish fly

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import collections
-def create_stack():
-        return collections.deque()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
