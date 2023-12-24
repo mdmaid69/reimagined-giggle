9e3bb94ea6b72298ae5b90c35a6e0cdb8618839b
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:05:48 +0000

    anywhere hollow power each no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
