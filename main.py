464b2107b42483e59b53cd7f969a18938a5a9151
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:36:30 +0000

    rare method later survive once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
