4a512ab58951718a2a9294b2d4604632bc3d817f
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:00:27 +0000

    raise basically sticky drink all

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
