16a3f45f8282b77e505e238f4d326ba0150febf2
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:18:25 +0000

    share she hair coat suckme

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_as_int(array):
+        return int(array[0])
