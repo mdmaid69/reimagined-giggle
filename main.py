2d4f8d745d7b6def4b1b531f98393c0a877e1b6f
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:45:15 +0000

    maintain rely offer city far

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import sys
-  def get_python_version():
-        return sys.version
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
