ac83242498384680625be1f79640eac3c7199739
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:18:22 +0000

    object cause near seriously children

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
