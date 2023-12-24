d409240799bea983d7b63bce6ec81edb17384724
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:58:50 +0000

    guess accordingto breakdown Ss pump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
