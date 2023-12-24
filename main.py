2e620726989435cbdd22b6b8fa13132d264a7d85
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:09:14 +0000

    usual do lend Ff best

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_pressure(force, area):
+        return force / area
+def calculate_speed(distance, time):
+        return distance / time
