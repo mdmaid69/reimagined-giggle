9c5f843bae772fa800ce466a56224206fe073bc7
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:24:30 +0000

    owe survive not movement sort

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import time
+print(time.time())
+import tensorflow as tf
+print(tf.__version__)
