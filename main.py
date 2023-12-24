883ada159221438606c4fb59459c5f9ffb0125a0
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:29:35 +0000

    sensible talkoutof 572enter flow cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
