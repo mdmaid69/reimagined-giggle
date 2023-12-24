69f07fe1e2a5ef4c6b561856d3eec510ca060b53
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:19:19 +0000

    glad Tt shelter many wake

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+for i in range(10): print(i)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
