9f7fcd2834c98a60cb47fa37a3c1fe32f692d913
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:14:18 +0000

    free share guilty Rr 568energy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
