1c06f7ac350887cdcc697962bf8276f39521a5c2
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:47:02 +0000

    flow bin fine store desperate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
