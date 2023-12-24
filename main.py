dbc0c6ba03e20f4ca0c6df802bb52b4201c0a1f2
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:03:08 +0000

    blue surprised brother professional approve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
