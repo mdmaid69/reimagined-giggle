8a5fffaee03cbc30c72b999814abd04d5a90a31d
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:52:48 +0000

    weigh desperate practice confused burn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def remove_duplicates(lst):
-        return list(set(lst))
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import json
+print(json.dumps({"name": "John", "age": 30}))
