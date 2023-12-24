75b9de8c69d2e9493404709d3f7e1284797ea2cd
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:46:06 +0000

    example gooff food clear reach

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import logging
-def log_message(message):
-        logging.info(message)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
