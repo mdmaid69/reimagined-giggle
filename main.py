6acc851d59910a5ff54da386556a99dd21c50db8
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:13:10 +0000

    clean full flow fault bad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
