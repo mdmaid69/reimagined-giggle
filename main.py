6a707a4ba393034ebca127577583c05a48094fdd
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:25:27 +0000

    die note tend difference nigga

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def subtract_numbers(x, y):
+        return x - y
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
