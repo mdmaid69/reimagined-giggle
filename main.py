97f8259e2e33e943fcc8c7aa1b26fb33cbd3a2fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:12:56 +0000

    odd software trade everybody finger

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  def is_prime(n):
+def is_prime(n):
+        if n < 2: return False
         for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
+        if n % i == 0:
+                return False
         return True
+import sys
+print(sys.version)
