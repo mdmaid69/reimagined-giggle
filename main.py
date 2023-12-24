4971fdb70930d827899a3c2c8924ad0be1dc4295
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:55:44 +0000

    apply situation turn combine immediate

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
