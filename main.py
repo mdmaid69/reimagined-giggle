723e694383a79705f049889d25b50ae3fcb3f84d
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:36:56 +0000

    worse skirt tax slip afternoon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+def change_working_directory(path):
+        os.chdir(path)
