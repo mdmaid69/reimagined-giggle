5dd4010203d3f0bcc3952463fb9a83bb0fe15585
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:48:45 +0000

    ear fairly company off organize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
