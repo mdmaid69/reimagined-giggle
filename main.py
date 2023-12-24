49aedcaf730b8c8c838dced711889a3e8805b58a
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:57:53 +0000

    since fix stamp yes come

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
