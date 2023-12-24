8594240bb075f80e8dd1422c9e338ef7692ab039
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:09:45 +0000

    attention interested it light preparation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  def sort_list(lst):
+        return sorted(lst)
