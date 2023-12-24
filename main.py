0f88b05683af814319b83606ef5d334b25633ae4
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:35:07 +0000

    extremely oil runaway quiet maybe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
