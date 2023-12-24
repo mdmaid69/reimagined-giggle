e2a91953c2b4a94bb3c4fabb19c19fd488288717
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:12:10 +0000

    true joke workupto beneath nerve

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def is_palindrome(s):
-        return s == s[::-1]
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
