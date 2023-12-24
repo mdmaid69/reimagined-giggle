3affbfc1cccdb66c77aaff3bf39a461d2c3256d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:28:55 +0000

    regional flat behind border fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
