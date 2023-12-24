d19361c183ee19d82925942cd01278d819b1bce1
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:39:28 +0000

    temper decide the male bridge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def square_number(x):
-        return x**2
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
