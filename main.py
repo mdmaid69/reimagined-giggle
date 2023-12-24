2ff9208860fcf3a810fe98ccca5ca163353f6671
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:58:38 +0000

    separate eye awake broad smooth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
