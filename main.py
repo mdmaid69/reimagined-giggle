73703d00501b58f88872c16e0236f839331751f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:35:44 +0000

    make section well trap tell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import collections
+def create_stack():
+        return collections.deque()
