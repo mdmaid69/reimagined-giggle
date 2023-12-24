8f2587c6a5e1e80d479738bf0125b4a474db31e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:45:04 +0000

    sore them stranger 573entertainment Ll

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import sys
+def exit_program():
+        sys.exit()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
