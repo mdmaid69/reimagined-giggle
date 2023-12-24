d1f272273f8625fd810c4fb827841173174dce6d
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:56:39 +0000

    remember rain by clean during

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import array
+def get_array_as_repr(array):
+        return repr(array)
