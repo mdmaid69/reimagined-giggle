03b0276950e83eb9c7c6adef650ea30c67899d8a
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:41:44 +0000

    hope fair keep contact time

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
