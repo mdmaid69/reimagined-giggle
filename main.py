7ce36805dde6689115c7d3c686efb536307587ee
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:48:23 +0000

    poison chin mind hand geton

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
