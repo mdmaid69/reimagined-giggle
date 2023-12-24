2ceab8080c8aa5580ca480fd175356a9003bb237
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:20:47 +0000

    kind owe brush long secret

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  def convert_to_octal(n):
+        return oct(n)
