5343dbcb12fe4a13b8064b2e99fe16d9b9ca20d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:19:28 +0000

    Nn order holiday over lift

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
