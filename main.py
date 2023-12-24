a6afc901f84b8973a7842f50453e0d7a14fed334
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:27:10 +0000

    such ill roll ring branch

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+print(sum(range(10)))
+def find_min(numbers):
+        return min(numbers)
