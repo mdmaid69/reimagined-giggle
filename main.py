4a4c3ccc95622ed2db43cead33b445486a15e713
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:48:10 +0000

    spin point exciting close situation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_density(mass, volume):
+        return mass / volume
