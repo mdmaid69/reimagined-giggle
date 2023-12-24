b6316a392b727b48f1574f69d9648a35939449ce
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:27:00 +0000

    pin unlucky clean mood street

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
