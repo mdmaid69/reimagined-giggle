89d5d79162fc01afdea6fa9e4ec6216c776a462f
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:04:30 +0000

    terrible possibly file swim spring

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_power(work, time):
+        return work / time
