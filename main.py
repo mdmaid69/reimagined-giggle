49340634a4e0995f0924d20315d72849711ed90b
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:43:20 +0000

    sudden our dark public pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
