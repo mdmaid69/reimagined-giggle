548a3910ba06dac694974c4e39c9f9241e8d25fa
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:03:22 +0000

    Nn set reserve Ii plain

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def subtract_numbers(x, y):
+        return x - y
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
