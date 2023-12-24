8ab65a90fc9b1bc8545f2096085299fabe840d69
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:08:20 +0000

    and goout sort study clean

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
