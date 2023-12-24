2e4f53a7e658d7e7133db90f7deb829442843302
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:25:53 +0000

    warn march far downstairs meet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def calculate_pressure(force, area):
+        return force / area
