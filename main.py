12d3c77f5287659105dcfcd58aec9dada618ddd5
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:32:09 +0000

    aim sport yet attention all

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
