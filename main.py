101c8221a8d7b458c80c04eeeffd62fa22c04ce1
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:33:41 +0000

    turnon reason back long bringforward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
