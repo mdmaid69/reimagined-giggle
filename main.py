6ce798804a789d63f09437af15c9f6258216ab25
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:51:09 +0000

    hurt distant relation tip century

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
