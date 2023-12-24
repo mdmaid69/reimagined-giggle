53292f3ae0b57a73ea993434715189329c68e0ed
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:44:38 +0000

    hit central but introduce window

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
