cffebb0a65ad6006cc0ef1796291326e9e91dcb3
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:39:57 +0000

    concentration occasion separate in talkinto

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
