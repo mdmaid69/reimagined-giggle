a4498976bffff479e5aff719e9a93fbb6c3b63a9
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:53:46 +0000

    formal club usual theirs inform

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
