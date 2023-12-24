60e31e4cfac03fd5fca520a329fff2b68883be72
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:15:09 +0000

    ago effect holdout fix absolutely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
