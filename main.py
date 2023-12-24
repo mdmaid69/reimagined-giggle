b9671945e81479b42521f550b27038dca124bdee
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:32:01 +0000

    Ss recent top concerning land

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
