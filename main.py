9868879db516e9fd22b1a3ad64f5b6aaefd55034
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:10:42 +0000

    unkind relief answer boring transparent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
