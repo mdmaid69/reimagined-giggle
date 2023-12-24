6411731f513d02bfe9f17cca323e7042169ab26c
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:19:53 +0000

    pretty approval available suppose maybe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def remove_from_array(array, item):
+        array.remove(item)
