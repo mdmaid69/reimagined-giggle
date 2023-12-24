d62e09a833c2f5bf1396c90caf61e7a68052f252
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:08:24 +0000

    many Ff case effort fire

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+print("Hello, world!")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
