364b5d7d68450ff2449099400c21a0bf0f211a36
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:18:39 +0000

    shopping check exchange underneath use

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import getpass
-def get_username():
-        return getpass.getuser()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
