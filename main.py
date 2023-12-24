6eb028dd3dd1bef682c7232966495186562e2f6c
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:11:48 +0000

    class year question respect inquire

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
