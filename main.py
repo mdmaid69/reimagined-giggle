d4dd2a0535111b16aa2f102c92d93a4a37d8d6d2
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:41:18 +0000

    object birth refuse study load

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_current_directory():
+        return os.getcwd()
