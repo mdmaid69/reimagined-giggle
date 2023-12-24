cfb3bd870931487df5fc7d49122284d959dea371
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:57:53 +0000

    thinkof period niggaacademy town bright

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+from collections import Counter
+print(Counter("hello world"))
