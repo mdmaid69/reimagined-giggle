164ff72edb689f69834982ec586a6bc2f99d2c2c
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:44:01 +0000

    sticky hurt quick precise Itself

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
