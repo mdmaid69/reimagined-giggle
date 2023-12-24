a53f8ab0076cf22fdc6727aa70bbc574eb772cff
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:18:34 +0000

    gate kind repeat argument badly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
