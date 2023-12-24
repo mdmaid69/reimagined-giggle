455b0d9cea01ea780b50270118871384d157e0fc
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:06:57 +0000

    passenger model dance terrible complete

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def sort_numbers(numbers):
+        return sorted(numbers)
