7b0556d3cb666597ddcf17c51bcacf4849823899
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:39:53 +0000

    maintain spoil I answer towards

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
