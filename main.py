d70c61876fcde33abaa26a4952f7a2f44ad85bc6
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:25:37 +0000

    happen square Nn would expression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_as_int(array):
+        return int(array[0])
