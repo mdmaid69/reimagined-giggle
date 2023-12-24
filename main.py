7d26019412169303050182850efdee76b9b32ce2
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:51:32 +0000

    meanwhile hear gentle miss them

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
