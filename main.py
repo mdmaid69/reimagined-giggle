d4bf3b854f2cea1821092630ce92c1f8cb009345
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:01:48 +0000

    underneath bend answer 569enjoy shock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
