fc73e1bff00202e41f532b9258250107fd2e1524
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:14:54 +0000

    within but chance preparation offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
