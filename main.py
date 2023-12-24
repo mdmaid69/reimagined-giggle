95742dfb596115e959ac7255dd7de8c9658ec695
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:52:18 +0000

    refuse cause manner anybody form

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
