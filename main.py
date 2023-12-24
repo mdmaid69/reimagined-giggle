cbc8f04d7b4a013d641c0df1c1f8560531584f69
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:56:54 +0000

    from side favour leg shock

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
