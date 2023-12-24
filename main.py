d286fbdb7292d04051620ce302b243d4f2b2d9c7
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:28:23 +0000

    pride contents seriously approve yet

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
