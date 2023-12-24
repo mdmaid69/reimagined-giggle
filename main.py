30980c488be416be2b6fe8f4e12232258ae4687c
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:33:17 +0000

    fire criticism serve maintain whistle

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
