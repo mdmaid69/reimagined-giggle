4a04537068403bf276a019ba36c579895516bbb5
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:52:55 +0000

    fine giveback bin target lot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_acceleration(speed, time):
-        return speed / time
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
