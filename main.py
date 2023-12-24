3d3e0612f2a297417576943bbeb5df848a732877
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:57:49 +0000

    spin that bury chase relax

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def multiply_numbers(x, y):
+        return x * y
