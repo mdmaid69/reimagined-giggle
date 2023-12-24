e9f55fd9e352878d23014c5bf1c2ddb4a9e257e5
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:42:42 +0000

    invitation learn container detail after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
