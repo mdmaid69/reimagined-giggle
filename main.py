c43ad705db58409795a966e8501b5f60b648bbd3
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:09:45 +0000

    serve home responsible start thought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def convert_to_octal(n):
+        return oct(n)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
