aabb268f38e7112de322f4bcd276ee164ccef6c4
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:23:51 +0000

    activity whole mean desperate interruption

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_as_frozenset(array):
+        return frozenset(array)
