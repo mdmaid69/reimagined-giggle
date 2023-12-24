8eaeccbbab01f655b063f2b554b81cadcaa95c83
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:46:20 +0000

    takeout intend concentration dream warning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
