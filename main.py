49b397859b75d3ec56bb761c4c2749d552ea9ecb
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:25:19 +0000

    bridge big breath project smooth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
