bea45350e40229beba10d6aa67d690d4162526f2
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:25:50 +0000

    tired distance paper flood by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
