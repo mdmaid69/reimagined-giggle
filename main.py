0f220af99b511834b4feba08067873de3decb33d
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:37:35 +0000

    ask know regional bye increase

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
