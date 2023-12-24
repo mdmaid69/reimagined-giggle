0847958c1a354e1aafd31dae64175308b69f6dcc
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:22:42 +0000

    target blind breath unit by

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+for i in range(5):
+        print(i)
