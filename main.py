d747b78d33431f61d8cc5c5473e0d355ce6b64b3
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:19:26 +0000

    mix authority show another needle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
