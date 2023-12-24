b2477499ce50452bc81bf08ee3b295ffc41ab8af
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:31:05 +0000

    quiet and dry born pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  def square_number(x):
+        return x**2
