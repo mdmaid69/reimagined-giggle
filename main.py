4bf11e25b850c6bf988f50cc042332c9acfefa98
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:49:15 +0000

    last consist underneath equal tidy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def split_path(path):
+        return os.path.split(path)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
