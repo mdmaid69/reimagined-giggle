cbf107e7f11f330ea78904fe38c371c2e6681b47
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:54:04 +0000

    culture her activity flood creature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
