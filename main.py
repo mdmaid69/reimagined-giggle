8f1f7489b23d4d5f8f9ed1c95a4feb3c9b93948b
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:33:47 +0000

    sister also limit it shrink

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-print([x**2 for x in range(10)])
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
