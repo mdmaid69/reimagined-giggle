83fdcced89a3796f57f269dc407b153a374fa04b
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:23:19 +0000

    favour chat yes sensitive disturbing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
