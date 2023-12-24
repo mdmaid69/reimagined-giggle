eb83fdc772949ee986e969859ac4d7507dd13bb4
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:00:47 +0000

    cut closely man until forget

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
