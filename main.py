8df10b3d164ea9dda2da928617ff57257ce2dfc0
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:54:01 +0000

    calmly awake shelter Zz sensitive

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
