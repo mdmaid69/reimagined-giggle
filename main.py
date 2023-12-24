4f351ce59391e06c13d54a49ff9ac899f037306c
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:56:09 +0000

    herself ability mixture lookaround taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
