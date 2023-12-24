469be92c19aad93e39530cc6b8f0f2c2ac0b9cf4
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:41:21 +0000

    turn goodbye everywhere whistle by

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import numpy as np
+print(np.array([1, 2, 3]))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
