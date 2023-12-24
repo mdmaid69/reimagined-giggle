02eb08fa37bcee48a2b8ffffdebbea7510ead5f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:45:10 +0000

    store seem settle young convince

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-  def is_odd(n):
-        return n % 2 != 0
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import platform
+def get_os_info():
+        return platform.uname()
