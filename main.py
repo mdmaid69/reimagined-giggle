c17c073eb61f50dee05893e4b07505880e011dd4
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:37:15 +0000

    common wish history joke contrast

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+from collections import Counter
+print(Counter("hello world"))
