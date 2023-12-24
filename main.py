9eba6aca7a72be3a76c56c80b62c46d6ceddfe7b
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:57:13 +0000

    soft manage slice turn open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def get_list_from_array(array):
+        return array.tolist()
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_cosine(x):
+        return math.cos(x)
