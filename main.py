f64b6dde8423670ec9d9b77795eb77ba71a343ec
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:19:43 +0000

    wise mixture different replace either

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
