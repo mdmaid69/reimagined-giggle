f218df31f09ecb00a26ce16979c8b10d008a581b
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:20:08 +0000

    scale keep put design home

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import sys
-  def get_python_version():
-        return sys.version
+import os
+print(os.getcwd())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
