3e5d6fbedb868eef03b64016d56c64a237979dc2
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:48:36 +0000

    scrap level there mass within

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import random
+print(random.randint(0, 100))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
