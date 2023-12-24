944feb3d96033c6853da7ecbbb7c3b4e197deeb9
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:55:42 +0000

    road judge emotion translate plate

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
