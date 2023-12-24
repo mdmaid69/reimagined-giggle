7d1a88f79cac22adeeb0db3527cc3b0c4e72ac0e
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:52:16 +0000

    comedown tread explanation thin attract

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
+import numpy as np
+print(np.array([1, 2, 3]))
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
