80de1e4b591e8d12cc7866713312484c03c4e1ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:50:53 +0000

    absolutely tongue off waste interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  def convert_to_octal(n):
+        return oct(n)
