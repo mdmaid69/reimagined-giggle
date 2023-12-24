18e60ea65af9243a763d4b4c4b898e5760f7728c
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:01:10 +0000

    mistake centimetre fasten thorough try

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import time
+print(time.time())
+def calculate_pressure(force, area):
+        return force / area
