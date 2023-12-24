741b5889d49f6064bb0153b02496d3ea73e44f15
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:04:23 +0000

    Zz exercise giveaway education along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
