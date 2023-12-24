db510d5dfaa1875a07084545bd365b1c13dc9916
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:08:06 +0000

    famous fly might instruction intend

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
