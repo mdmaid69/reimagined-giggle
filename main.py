0a84d12d7f71f564f0d3bdaab64e295d897f50e8
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:32:23 +0000

    mind together steal motion suggestion

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def convert_to_binary(n):
-        return bin(n)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
