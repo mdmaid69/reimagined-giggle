43b3b88f65c968bfa8a31885efc1e330f9393134
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:11:10 +0000

    scared change too nurse comeoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-for i in range(5):
-        print(i)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
