8c19a2ac025b65d656c135a2071d300a64397716
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:52:42 +0000

    time shock height thin exactly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  def cube_number(x):
+        return x**3
