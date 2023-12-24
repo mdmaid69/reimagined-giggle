733a69a94f871c9ebe9abe173c451dacc53d7ea0
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:56:04 +0000

    bring match land achieve freeze

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
