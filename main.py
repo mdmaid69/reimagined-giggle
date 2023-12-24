874e9d7072382b11c1ea378c6e2bbb4bf2d2dced
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:22:11 +0000

    probably yesterday Hh popular door

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_perpetuity(payment, rate):
+        return payment / rate
