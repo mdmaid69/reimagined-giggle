07e59ea0a9c6ab0492049c1dc900fbe47223ed88
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:16:48 +0000

    179bite step precise cough clever

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import platform
+def get_python_version():
+        return platform.python_version()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
