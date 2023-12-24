6fa7e98c0eb3e233f77734fc4d8692770032cefa
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:13:45 +0000

    spread usual charge cross hope

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
