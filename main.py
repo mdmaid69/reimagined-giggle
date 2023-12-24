9a86f1d43932f5394d6f755806268ff9dff19fd5
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:00:50 +0000

    word among hole recently welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
