75cec220eee1ab5941855b2ecb7564dc367563ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:31:14 +0000

    go rob telephone neat back

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
