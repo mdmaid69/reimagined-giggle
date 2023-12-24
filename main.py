4b3acc2cba227de1d991e617102c4abda512c9e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:10:41 +0000

    close delight every shock hurry

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
 import array
-def get_array_item(array, i):
-        return array[i]
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
