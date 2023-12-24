a9a9a350d6484e6cf12ef2f1091db509d916af27
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:06:12 +0000

    scrape Hh pain hole content

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
