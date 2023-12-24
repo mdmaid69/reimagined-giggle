653ec1138f49ea70d366f255ad40452ef788acec
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:43:44 +0000

    passage basic great marry ahead

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_power(work, time):
-        return work / time
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
