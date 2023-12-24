9eba775e8d592f04e4faaddef2bd2b051a57add0
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:40:15 +0000

    Bb wash ear our promise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_acceleration(speed, time):
+        return speed / time
