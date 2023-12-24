8f2ad0be64ae72cc6768b3fa8c38d3a922bbf7db
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:57:15 +0000

    objection estimate clothes just false

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_acceleration(speed, time):
+        return speed / time
