07c5ce1d10eee30ba2deca0ae33ae4ae4132f834
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:32:22 +0000

    favourite behaviour fork key deserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import array
+def get_array_typecode(array):
+        return array.typecode
