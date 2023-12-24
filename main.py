7872e8d066425afcc41cf8d891eacd1080be88ae
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:00:52 +0000

    creature whistle Ww narrow interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
