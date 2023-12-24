bee9eed4b12eee9383069d3b4b460ab745e7736b
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:38:00 +0000

    watch Kk if deal pipe

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  def sort_list(lst):
+        return sorted(lst)
