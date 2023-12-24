7f9f23ae7e0b744181b3a2927f089a5a9d433704
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:17:38 +0000

    Bb confused combine finger blue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
