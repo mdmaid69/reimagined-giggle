4e82ffb71dab6dbfd07f2cb1b3132b7fc99dbfba
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:40:45 +0000

    extra defeat certain breakdown along

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
