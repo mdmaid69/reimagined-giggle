c2c5cb7145d7ade270c8f017f8a5c7d61e421d5c
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:33:05 +0000

    without spend lunch everywhere cope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_acceleration(speed, time):
-        return speed / time
+import collections
+def create_stack():
+        return collections.deque()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
