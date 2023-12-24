9c5c859d5832856cb3b70fc3a31ce43523f00576
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:22:46 +0000

    refer unkind clever book rare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
