d89b45726855140ef9393ad657cb120b090456c5
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:38:47 +0000

    pity preparation phone spoil remain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
