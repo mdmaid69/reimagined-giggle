0c136f3ef93d2513a1acc24927fb700973bd3bec
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:13:32 +0000

    hit comeover if distant put

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
