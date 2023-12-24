961b714308aaf979617d21948644751a4aeaab7a
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:29:02 +0000

    notice description gap office nature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
