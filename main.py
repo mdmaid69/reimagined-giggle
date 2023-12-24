21fa31e490de39951e6de7f24dc799c4694ab822
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:41:26 +0000

    dance usually wander lucky environment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+print(os.getcwd())
