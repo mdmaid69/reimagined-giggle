e6321af95e33f70b1939024ffa1dee3e7a584cbe
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:09:49 +0000

    explain seat unkind question mine

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
