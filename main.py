fc1646e092842988b702f8dcfa5f782377fcedab
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:27:04 +0000

    price cover mend save necessary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
