4c023833fe977702a8c7781d26843737034d784a
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:05:42 +0000

    centre practise translate tap surprised

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-print(sys.version)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def sort_numbers(numbers):
+        return sorted(numbers)
