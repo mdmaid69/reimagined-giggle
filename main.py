5b1742147cfed48ea58c357965ef5bdb037813b1
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:43:08 +0000

    burst who shout dick profit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def get_array_itemsize(array):
+        return array.itemsize
