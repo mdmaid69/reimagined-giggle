0b0c6e7db52a4ad097cd99c4099e56443737146e
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:59:48 +0000

    promise separate frighten forget exactly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
