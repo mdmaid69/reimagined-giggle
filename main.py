d89f4792edf1c07e07b1b330d9923b458b8bf2f9
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:32:56 +0000

    do crisis meaning tour bright

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def print_python_version():
+        print(sys.version)
