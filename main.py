3218e77fcd5097fafdd192147f0be5577de96afb
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:49:40 +0000

    upwards fashion bound could giveback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
