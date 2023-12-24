1db03cc80f0c3ae6226da49f8b80bbc8407ba934
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:21:17 +0000

    house innocent female finish react

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
