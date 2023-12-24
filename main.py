0d7697f6ce6b2f5ffa723827098713ad54368797
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:11:27 +0000

    trick date fence owe set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
