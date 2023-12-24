0b70c7b35da974dc085a417a2e82eebc3f98c420
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:17:30 +0000

    laugh whichever funny cope strike

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def reverse_list(lst):
+        return lst[::-1]
