d6435d419b5ed6a8996bf379d1e4d23ca9c3daa0
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:58:22 +0000

    question claim anybody remember deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import platform
 def get_os_info():
         return platform.uname()
