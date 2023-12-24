0b8e9cc072c0ea86f77466ca96571589e0bef413
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:45:46 +0000

    study runout even inform demand

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+text = "Hello, world!"
+print("Uppercase:", text.upper())
