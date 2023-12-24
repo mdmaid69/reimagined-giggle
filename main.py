22107fed75bf53184c56f0eb26db4134a380086f
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:49:55 +0000

    back class change actually tone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import logging
+def log_message(message):
+        logging.info(message)
