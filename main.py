c29a1487c4a81e059051f81908630bc73dcb7fb8
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:39:33 +0000

    increase raise leave cruel shock

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
 def calculate_power(work, time):
         return work / time
