8e0ec0f3ff65bd9b23b84c4f0fc6da5b9676b047
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:35:44 +0000

    exhibition taste hold tread improvement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
