6eb46f76432db9eb357a924c286fc27889dabba4
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:22:21 +0000

    reserve replace contents statement forgive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
