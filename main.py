ba0d74f4b8a15a2af31f773ce1dcd72d1553cacb
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:05:21 +0000

    goout common artificial closely please

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
