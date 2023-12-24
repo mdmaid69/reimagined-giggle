f45f81c2e1ca04a66f2e66210855bdce00251dab
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:31:22 +0000

    all survive return lie impossible

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def find_unique_words(sentence):
-        return set(sentence.split())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
