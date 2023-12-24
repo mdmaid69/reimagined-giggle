7d52669b5f5222c91ef027ba4b373212474a49d3
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:12:08 +0000

    equal possibly tone split determined

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def count_characters(sentence):
+        return len(sentence)
