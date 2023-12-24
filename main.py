36a298359fe20cafdfe6f5826a492e1916db58a3
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:52:52 +0000

    would well shock line peace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
