aac57ad7cd568c80f787df61fbdcd50fefb09728
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:05:13 +0000

    wild apart decision hospital keen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
