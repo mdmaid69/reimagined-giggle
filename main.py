74c3c717264a7146b555c6a8a14a082f2660ab59
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:50:53 +0000

    here unlucky meanwhile development common

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
