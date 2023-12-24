c6648fb314b73172b610af919c0c6cf47fc75127
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:44:48 +0000

    however complain OKorokay safe anywhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
