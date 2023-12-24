55025afdc4acd2651e79b29ad46527befe3f92a7
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:28:38 +0000

    start bet load present better

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
