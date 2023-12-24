30cc3ce8118f8331dade6f59808c1b40c0aa5079
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:05:20 +0000

    manner fast geton Itself though

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
