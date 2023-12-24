4556cb34e225e4908c4b272d685bcab349665664
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:00:19 +0000

    apparently wide along danger score

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def split_path(path):
+        return os.path.split(path)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
