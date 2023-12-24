5a22d6dede72ce20f03822ca09921dacb8856910
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:33:47 +0000

    wash prevent dick way run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
