7cf2d8b6dc8b0f126bd282fa8bca944b2319919d
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:44:40 +0000

    school house Ss usual offence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
