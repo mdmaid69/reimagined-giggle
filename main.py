a11e3b7e1b989e1fa176ead17871533070ad8d04
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:33:26 +0000

    mistake precise unite burn region

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
