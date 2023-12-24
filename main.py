982b0b3d9529e44433f80af3bb738b75b71d17fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:48:28 +0000

    related mess interesting range behave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
