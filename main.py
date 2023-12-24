58fc832f55fea012c7004115d6b32261a006d1e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:37:46 +0000

    frequent job meal still close

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
