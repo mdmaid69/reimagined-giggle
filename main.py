763549255ced6a07a481c5e1803251fd63b007d7
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:02:36 +0000

    nothing wave guess bad system

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import sys
+print(sys.version)
