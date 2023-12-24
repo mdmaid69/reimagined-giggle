3dbaa95bf2b297c7da920141cc5013afbb6f89c7
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:37:13 +0000

    unlucky about deliberate loss hello

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
