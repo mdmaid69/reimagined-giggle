df8f5c0ec3e1bb12fcf46965b6ad74483eb100d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:56:02 +0000

    practical below cover opponent breakfast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
