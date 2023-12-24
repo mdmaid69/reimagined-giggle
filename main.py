68701a58449da78c32b6a97de367f09fddf4ffe2
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:15:57 +0000

    top switch deep getout no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
