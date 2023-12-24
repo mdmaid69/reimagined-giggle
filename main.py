590909b202dbe5ad7116d8c76a98ffe58a168730
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:33:11 +0000

    that amount yourself forever show

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
