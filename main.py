8df94f453152ac02adc69ac670497f61bbd17382
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:15:45 +0000

    careful square proper coat thumb

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
