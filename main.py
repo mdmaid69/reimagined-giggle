7cea1d9191ac8d122bfea332e211b7ffe940629b
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:12:39 +0000

    show organization worth accept determined

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def greet(name):
+        print(f"Hello, {name}!")
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
