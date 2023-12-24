300ccc6e3a1ec00a88a1b7df6fc76de5ad587d86
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:14:50 +0000

    tear seem popular knock quit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
