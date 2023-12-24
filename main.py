bab29e92e02b769266dce4f1c60669dc695fdb10
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:06:07 +0000

    rough they when month idea

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_user_string():
+        return collections.UserString()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
