32456ae910da9a434dc5a1347c18c8b2435e3336
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:07:40 +0000

    precise person finally interest voice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
