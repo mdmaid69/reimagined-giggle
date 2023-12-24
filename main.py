992e1284c7b74bb93353763867f40ce26f286c6a
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:30:57 +0000

    why opponent tight problem suggestion

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_current_directory():
+        return os.getcwd()
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
