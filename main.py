81e930d1f99f54c3aa4718747a90007021a68321
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:44:19 +0000

    neither dull this inquire bear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_max(numbers):
+        return max(numbers)
