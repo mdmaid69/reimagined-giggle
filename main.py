a9827e0e21c27338c887ae625f5261db5592acc7
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:23:46 +0000

    goaway physical path anywayanyhow contain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def sort_list(lst):
+        return sorted(lst)
