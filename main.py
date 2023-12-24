3fa4a6f60ad3c63ec1bde5a60cea1d379c636560
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:12:45 +0000

    drop claim comeback burst region

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
