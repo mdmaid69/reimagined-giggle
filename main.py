49568924bfd2b690dbf32918685acd7b90c29517
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:47:58 +0000

    over so healthy flow safe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
