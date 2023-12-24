ee5780517e251d9a430889de2ab0f31d12754096
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:46:23 +0000

    getout drink with emotion manage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
