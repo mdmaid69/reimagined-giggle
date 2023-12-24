d651f15649dadc519e201f3940f8c2862323552c
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:10:18 +0000

    wind at it example people

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
