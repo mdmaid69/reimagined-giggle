429aed92e7f197d1856be00b27aa7013f0beb82c
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:23:42 +0000

    century argue stamp protection show

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
