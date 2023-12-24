05c1e4433758ee0a99572af41c34b1297a7d5bfc
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:29:02 +0000

    suppose drawing interview lend value

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
