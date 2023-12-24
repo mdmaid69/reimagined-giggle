9686751cfc98acf4dc6bec438b741b1315322ab3
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:46:42 +0000

    much any and slowly teach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
