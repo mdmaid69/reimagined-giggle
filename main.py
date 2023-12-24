b804d403c3540075e8f1a426e6cc985e027824de
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:51:24 +0000

    church store behave needle arrangement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
