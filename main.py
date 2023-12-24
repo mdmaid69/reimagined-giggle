04f6f985fcb8997155e0721277bb114a0aa81aab
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:49:46 +0000

    argument length suffer for below

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def remove_duplicates(lst):
-        return list(set(lst))
+import getpass
+def get_username():
+        return getpass.getuser()
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
