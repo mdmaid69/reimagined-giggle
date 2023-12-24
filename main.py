4809dfc256707eae24d1f02ff89ef446a3c34037
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:06:09 +0000

    injury callback either meeting arrange

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def find_min(lst):
+        return min(lst)
