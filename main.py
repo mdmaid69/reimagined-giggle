3134fc83d2ca8ff92bc9e141d8c064726ce0e68f
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:52:19 +0000

    slight responsible block payment discussion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
