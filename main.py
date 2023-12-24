130fbfc33fb42e34b9ac7b3715807fc1c1fc91b8
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:55:53 +0000

    involve authority name compare cook

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def find_min(lst):
+        return min(lst)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
