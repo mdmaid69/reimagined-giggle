70eab8624b6350f8a41ccd583e28767b0f610811
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:04:04 +0000

    importance mistake dry goback(to) emergency

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
