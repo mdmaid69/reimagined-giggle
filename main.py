8f1e30a2a06b9b06fc5e6d685ab510f6d8458109
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:03:38 +0000

    someone national every should warm

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
