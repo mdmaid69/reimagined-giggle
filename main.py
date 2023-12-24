eb3b5b329888915d7437efd0843366ce6e77659d
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:13:41 +0000

    way inform squash record talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+text = "Hello, world!"
+print("Uppercase:", text.upper())
