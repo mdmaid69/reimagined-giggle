06e13f689115e7aa42d7e6a2ec29339ec4040948
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:01:41 +0000

    test respect motion contact bottom

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  def subtract_numbers(x, y):
+        return x - y
