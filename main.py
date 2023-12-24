48ec87ae51acd5c2f3c7bccbb80bf536ca40fe48
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:01:12 +0000

    really clue young separate conduct

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_queue():
+        return collections.deque()
