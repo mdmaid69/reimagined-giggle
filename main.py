8647615b0187d7c039dc9825d4b1315d708b5bc3
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:04:19 +0000

    hold whisper realize anybody allow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
