7e4456aa099f4b89dd5dd25c282a000b991ea804
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:40:41 +0000

    motion fine crowd advise responsibility

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def sort_numbers(numbers):
-        return sorted(numbers)
+def add_numbers(x, y):
+        return x + y
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
