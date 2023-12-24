b06dd638e14dddd41f0208c5afcdc32aadb08f2b
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:57:15 +0000

    authority film reliable steal minute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_arc_cosine(x):
+        return math.acos(x)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
