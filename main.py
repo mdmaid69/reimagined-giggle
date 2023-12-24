763ca840ee4acdfb82de827b84284191fee4d926
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:59:14 +0000

    authority minor generally repeat brave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
