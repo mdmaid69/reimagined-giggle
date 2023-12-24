1570c9829ecc6765f13eaa87d8e89de38e2d78b1
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:58:55 +0000

    wherever 571enough pump damp nature

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_as_bool(array):
+        return bool(array)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
