1415a766582b7df7e0e47ede51cd1d4f397617c4
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:45:51 +0000

    cry goout real worst mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
