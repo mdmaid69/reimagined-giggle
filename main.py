ab55c00e489ab1c4be12b0f56bc56be593018eb3
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:51:34 +0000

    big after search main lend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
