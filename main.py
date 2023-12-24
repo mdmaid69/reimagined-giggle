4b1480e2f0f115147c83bfaafd8d88fa9c7a05a9
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:47:18 +0000

    constantly uncertain sky else sensible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
