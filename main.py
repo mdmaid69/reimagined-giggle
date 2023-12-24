1c6ce30de00ff0e48d01df3d71abe79ed8d24899
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:08:49 +0000

    promise attempt regularly clothes pride

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
