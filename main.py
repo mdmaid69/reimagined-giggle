d8f4de276aa3e3d5407cf25f08f60a4668dd462c
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:02:38 +0000

    television pollution favourite copy along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def split_path(path):
+        return os.path.split(path)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
