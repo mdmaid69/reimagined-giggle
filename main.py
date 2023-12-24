999826e1bb80f7e19865abd4af948ac0a610d5d2
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:22:01 +0000

    luck payment miss take influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
