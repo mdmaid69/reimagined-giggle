2c93bfa36ed62ce0828131b2e3ff57453eaecbc4
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:59:44 +0000

    game healthy clothes defend flavour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
