30014d571db9cc091f58eac60a99add7acd1c997
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:02:46 +0000

    land worst demonstrate small insist

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
