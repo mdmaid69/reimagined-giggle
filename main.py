6d339a424aa0e983d867710acc72d3dfc36bdf17
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:32:00 +0000

    problem talk communication wrap rain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import json
+def convert_to_json(data):
+        return json.dumps(data)
