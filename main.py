e8cec224af59cb9539dc83e80b5c65526f8a8ce9
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:29:49 +0000

    almost inquire opposite visit foot

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def convert_to_octal(n):
+        return oct(n)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
