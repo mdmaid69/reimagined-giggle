7a37c2b0b9ee35fc3a026f5d4c8de0d457492bf3
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:22:09 +0000

    question face closed black command

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
