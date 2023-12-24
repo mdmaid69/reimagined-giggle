54437813d02ec47947ff8a2567cc004b8ed5dc55
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:31:18 +0000

    condition interfere no separate base

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def get_bytes_from_array(array):
+        return array.tobytes()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
