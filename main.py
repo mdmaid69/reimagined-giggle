fae3c6f542cf10238be4ca00ccbad5fc3aca47f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:18:07 +0000

    frightened matter down both successful

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
