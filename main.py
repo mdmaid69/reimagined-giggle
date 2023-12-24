f6237da476fe72a3471ad61c1a569befc905241d
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:27:42 +0000

    fact trick jump very thinkover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
