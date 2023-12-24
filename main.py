d25574f606775faaf5edb27225dad3f36bc85141
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:14:01 +0000

    group shock cum of lookup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
