c352985211ef46b17b54f69a7ee89187aef6b10d
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:59:27 +0000

    breath loan home boat sick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
