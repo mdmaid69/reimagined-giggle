181bb81766e6708f6466d50505f7d83a2d150c21
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:12:42 +0000

    sharp fence other embarrassing area

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
