873356fe2d29afae99f013b1840b2d0b6a5e70ff
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:28:16 +0000

    tomorrow cheap natural image permission

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
