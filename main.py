6a7d97cec912478254a0e1ca357f5a666a4cfc9a
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:22:02 +0000

    already theirs doubt cruel lead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
