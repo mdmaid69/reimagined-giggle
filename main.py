5d0fdd4fd8fb6bef5ea7c9b45cefec47d6f9538d
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:00:14 +0000

    say start organization term total

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
   def reverse_list(lst):
         return lst[::-1]
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
