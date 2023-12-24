3cd4f0b07af2c904ebd14dcf92e1f70ebeadd5e6
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:12:17 +0000

    protest thumb coffee understand line

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
