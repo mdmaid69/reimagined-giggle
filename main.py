0e8cf079bfd8c8f384cb5643b4a21abaad5aa44d
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:37:39 +0000

    shine flavour cold switch colour

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
