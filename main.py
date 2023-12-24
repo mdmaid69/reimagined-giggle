23d8c87eb9d184b869d4fbf61dc0cccdf09ee11f
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:48:09 +0000

    petrol arrival mark perhaps tall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
