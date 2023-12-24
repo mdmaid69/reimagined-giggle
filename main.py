1976393cf66714bffd7c900504f1fe8f16bac830
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:08:14 +0000

    Kk include design blind close

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
