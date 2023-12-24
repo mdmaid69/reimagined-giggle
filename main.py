f18e1c263d93ec9708e574b3e33471d4e82ce837
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:18:09 +0000

    realize cry will size regularly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
