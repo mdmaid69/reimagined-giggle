abce7000f4f43e32407cc4683aa1242064c252ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:16:14 +0000

    while raise worth while murder

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
