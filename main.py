48bfa1451f84fd77ac402ccd2480a63769ff3683
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:29:59 +0000

    means trip time congratulate match

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
