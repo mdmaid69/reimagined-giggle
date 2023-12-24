1fd70e9b5d31a161340c57b9eb2a4d54c3af11d2
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:24:17 +0000

    still stamp slice comfortable near

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
