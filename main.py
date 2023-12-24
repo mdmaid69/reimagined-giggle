b94077806a06b54213072fbb38900683f00cdf0f
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:35:09 +0000

    interfere score pupil practical Uu

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
