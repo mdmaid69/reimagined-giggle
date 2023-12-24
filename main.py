9f10c426c4ff4ee4ff91ab72a75d222a659df00f
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:01:27 +0000

    dance fine fire child nurse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
