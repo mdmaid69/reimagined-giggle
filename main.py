d97ffd0aa5b9b1ad2fe587e3429463fddabcf54e
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:44:45 +0000

    watch flow taste look accident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
