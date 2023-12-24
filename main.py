d4c432a79de1f8437e228570bdc852c6dd320847
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:15:20 +0000

    niggaacademy price description reason do

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-print(random.randint(0, 100))
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def get_list_from_array(array):
+        return array.tolist()
