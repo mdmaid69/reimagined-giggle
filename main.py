ccfbcac8e42274a8f5aa2f7ee56edc1a2001d8f9
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:07:21 +0000

    none edge estimate meet become

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def remove_duplicates(lst):
+        return list(set(lst))
