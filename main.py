87bb2310152caefc0ee7cae139f090223d84aa4c
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:43:20 +0000

    press flat unhappy lucky poor

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
