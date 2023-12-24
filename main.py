da2b3eff82a2902a48c1aeb6ffb1dae678aa4cc6
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:49:42 +0000

    everything open print die fail

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
