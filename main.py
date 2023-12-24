35422963c4cd27755e1aa9159d7fb73bf3dc9b29
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:39:27 +0000

    thanks ourselves warm clearly welcome

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_set(array):
+        return set(array)
