0a4769ed45f5b0288f8257bb13268f9773277119
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:49:42 +0000

    dangerous temperature personal spite pot

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+def add_numbers(a, b):
+        return a + b
