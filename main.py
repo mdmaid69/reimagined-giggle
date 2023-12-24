3ff2b21ff88eff70ccd1b5c19ce6e89209c949f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:30:42 +0000

    value put 568energy high tune

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
