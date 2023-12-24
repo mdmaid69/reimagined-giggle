ec4b79576430d75c697f4181362f66a362e1b7ba
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:57:09 +0000

    beginning disapprove extraordinary autumn add

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
