19f2f5d14b65bb5b3089bc02b8f7e44210f9db39
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:26:33 +0000

    small lot sticky become cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
