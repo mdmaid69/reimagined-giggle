55425799695603ed4e04cb6d9bee2ff9877f1fc3
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:29:20 +0000

    comefrom dance separate prepare port

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
