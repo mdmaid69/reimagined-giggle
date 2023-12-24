570b146bc4ef9f3e272b0fafae1f5abdc014f041
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:42:29 +0000

    side clothes patience basis half

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
