fd9b73bef723b8ac0c4365bdef23fd6f476e5619
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:30:10 +0000

    stop drawing silent chase miss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
