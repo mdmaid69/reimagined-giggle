7d42a83b44a712a41aed00e06bc73f5d2adb4ac4
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:33:27 +0000

    tired fault sail medium score

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
