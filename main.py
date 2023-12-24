7c36105180ea3ce9225aea34cc3b2ec316d4151f
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:06:23 +0000

    lower influence war tongue home

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_area_circle(r):
+        return 3.14 * r**2
