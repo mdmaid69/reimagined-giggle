a846a94a51d6375121405074c89ec32aa2093eec
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:33:33 +0000

    pool fairly after interview not

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_work(force, distance):
+        return force * distance
