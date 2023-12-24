903d5a09ff160696f33976627496b20988cf178d
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:50:56 +0000

    tomorrow sister fair makeup issue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
