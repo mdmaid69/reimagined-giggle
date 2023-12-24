6eb6c2db7b1148737d51185c192265b293faa8ec
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:26:10 +0000

    Hh factory notice harm exhibition

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+for i in range(5):
+        print(i)
