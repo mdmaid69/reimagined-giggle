7d6bc3cfb4e24853bdc105e02dcd1778f96469a3
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:07:39 +0000

    lookup through priority although speech

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
