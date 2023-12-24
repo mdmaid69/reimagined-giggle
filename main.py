66286f06b59f8aa71feb735a872926467c0a1c78
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:49:22 +0000

    perhaps male emphasize however estimate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_area_rectangle(l, w):
+        return l * w
