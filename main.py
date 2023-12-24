d75d06ef85d320939310b765aaebb0bc08750889
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:59:35 +0000

    cost destroy conduct connected train

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
