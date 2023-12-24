5c95d9257f826a16e7af446f4ac01d1cba1b90fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:07:56 +0000

    yesterday environment finally system clear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import collections
+def create_user_dict():
+        return collections.UserDict()
+def sort_numbers(numbers):
+        return sorted(numbers)
