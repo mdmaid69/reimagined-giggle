0b44244af2259bf6ee25195e23bb621c91bfa4af
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:03:17 +0000

    closely moon turnup equipment butter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
