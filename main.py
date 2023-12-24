7c6a1272c90e4bf22bcd019bf9796b090a09e67a
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:06:57 +0000

    lookaround switch stage fear especially

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
