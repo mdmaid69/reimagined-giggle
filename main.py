249ad86ba237e339c80138970d864bb886accee2
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:10:59 +0000

    board except reply divide consider

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def reverse_list(lst):
-        return lst[::-1]
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
