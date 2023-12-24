955bdac1d9ca075b7a23b5c8364fed8b19311ef7
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:29:02 +0000

    science home visit chin smile

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
