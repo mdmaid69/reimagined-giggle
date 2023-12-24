612e89d760070562b453ae2d0206811fcbd525ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:19:36 +0000

    right worth feeling crime Mm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def is_odd(n):
+        return n % 2 != 0
