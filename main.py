2bf1d4c91a4b248f22d20a4618f7dbb4649be59b
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:24:04 +0000

    pleasure join unhappy turndown down

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
