917bfa4d33834c7ed502478dc48015d81371b7d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:52:00 +0000

    final attitude few cook runover

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def sort_list(lst):
+        return sorted(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
