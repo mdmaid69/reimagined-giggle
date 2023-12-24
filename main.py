1b0e0710b6b8fd52f37d6448e8a5c7ae8043b9c3
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:25:21 +0000

    male mail threaten square suppose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
