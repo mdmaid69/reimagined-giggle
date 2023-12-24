ee9983e665167488c234a6185bdceb31244498e0
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:32:50 +0000

    square children contrast deep reason

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_queue():
+        return collections.deque()
