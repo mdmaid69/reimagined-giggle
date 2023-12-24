6d3d48a48f5cfcb57d4f564da9815fad760e4d6e
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:26:36 +0000

    patient desperate expensive kill downstairs

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  def count_elements(lst):
-        return len(lst)
+def convert_to_hex(n):
+        return hex(n)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
