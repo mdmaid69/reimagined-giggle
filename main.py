b989a390b9b94b027987a20f7e0dc0038f40f4e5
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:34:13 +0000

    anywhere defend arrive good comefrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
