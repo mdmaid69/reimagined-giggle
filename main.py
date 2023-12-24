44986384626c3e9448e7fa0840ef9aedc57c03d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:09:42 +0000

    past congratulate type rent modern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
