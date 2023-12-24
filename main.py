c1fcdfd5d645989e097003a81ab66555f10ea6ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:08:01 +0000

    second subject tidy spill little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
