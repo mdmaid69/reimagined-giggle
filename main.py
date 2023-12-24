0746932cf7f5ae0cba2e821a104c6409898498f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:17:28 +0000

    Oo increase trap bad likely

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
