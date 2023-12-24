175c6f3b5b76cedeab4788a02864fdd77a49ba9c
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:59:39 +0000

    concentration pin still also stupid

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
