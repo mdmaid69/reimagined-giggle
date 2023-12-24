f232a4e10b2a7c17d2035a2cd07182847ba7d523
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:00:11 +0000

    result sticky suggestion breakinto Oo

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_sine(x):
+        return math.sin(x)
