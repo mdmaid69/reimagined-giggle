4c5b6e0f6797129c1fb2ad6ed50d3678a43be163
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:16:36 +0000

    bet patience Qq straight city

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
