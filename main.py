dcee9144885de1157e12b75a10ef97c2b69e9ee5
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:03:34 +0000

    opposite production principle interfere blow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
