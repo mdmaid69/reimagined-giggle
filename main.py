0dff897258e25a390ff825c31e69ccbc9b82ff48
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:11:29 +0000

    please sweet shut operate play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
