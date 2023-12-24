a83153197e9334568e0cb9950654d4bb028a5456
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:52:09 +0000

    age history level alone train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
