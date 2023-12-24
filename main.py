b30c9e8d9a27cbc3e70dee92e0944088f872a6c0
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:32:48 +0000

    bear accidentally force stir round

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
