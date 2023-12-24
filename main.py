480eb24f9f44a04dd3581a41714ccecdd2ce0465
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:57:50 +0000

    cope hate demand even goodbye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  def multiply_numbers(x, y):
+        return x * y
