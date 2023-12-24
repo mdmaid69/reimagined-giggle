ec415450f4780b82cd5511c77f92bd4f553396dc
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:41:16 +0000

    learn die area laugh know

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
