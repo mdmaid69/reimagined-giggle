8132f163623b6261fe96dea0e92bb3fbe01dabfe
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:52:11 +0000

    pinch serious proud skin minority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-name = "Python"
-print("Hello,", name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
