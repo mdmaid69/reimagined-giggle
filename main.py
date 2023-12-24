61e89540dc7ba976d345aed583f65a1901ab67f8
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:41:48 +0000

    564encourage cook backwards drawing sign

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import math
-print(math.pi)
-def calculate_power(work, time):
-        return work / time
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
