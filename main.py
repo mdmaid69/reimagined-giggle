8e0b03a76dd44b3ebe8cc9b8e83cbfb99bb3b895
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:25:58 +0000

    pollution spring everybody fairly exit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
