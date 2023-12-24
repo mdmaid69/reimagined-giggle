365a358547ab7ab2d70393a051b2944de40ae0b3
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:17:49 +0000

    visit movement know wind party

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import json
+def convert_to_json(data):
+        return json.dumps(data)
