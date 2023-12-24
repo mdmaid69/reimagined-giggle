121e359ec7e41f5822522694cde34a8e14f67a44
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:35:53 +0000

    Jj block exam takeoff stir

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
