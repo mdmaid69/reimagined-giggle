d0f33296eb522656e97e0061c231d45d5c5f65bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:21:50 +0000

    information anywhere pool through right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import datetime
+print(datetime.datetime.now())
