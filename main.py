c31a3ba00d1c1fcbea240b1b6a7150ad7cad19c4
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:50:54 +0000

    food watch takeaway plastic young

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
