012324a2c7e02b06df41625403b98fe16799a775
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:21:13 +0000

    cover equal treatment contents equal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_typecode(array):
+        return array.typecode
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
