8e2217f26473cbc82c2fb0fd88d3f4b3d9356cac
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:13:43 +0000

    moon us politics closed expensive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
