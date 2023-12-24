114ca1f69789ad889ec9410c381acc10ffb8bba5
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:48:08 +0000

    spill ashamed cover fix flow

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
