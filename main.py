5a0f59065d2d15362891001be366d9994fb6a411
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:10:01 +0000

    sweep would bury lift job

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-def find_max(numbers):
-        return max(numbers)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
