b3cb012b4dd44056b2689ce44a701383ce966fc8
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:19:30 +0000

    deliver course threaten but clear

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
