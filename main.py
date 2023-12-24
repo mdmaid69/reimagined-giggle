b968077085122b4a2b44a76294f8024bd44f7227
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:44:07 +0000

    sentence wake squash plastic wild

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
