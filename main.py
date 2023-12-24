cedf280c09b3b9e2630c6c6ceda585fa4ca03fdc
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:34:02 +0000

    share plan group pot evidence

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
