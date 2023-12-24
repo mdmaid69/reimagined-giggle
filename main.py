4449b08bc7bf79ccd77371bc54b96a41b332beca
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:06:49 +0000

    soft majority frequent communication blame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_force(mass, acceleration):
+        return mass * acceleration
