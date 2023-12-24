0a0fea4c2809b179316cf18d655ef0a24269f831
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:58:04 +0000

    evidence against danger flavour getaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
