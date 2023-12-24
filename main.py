6f2d96f01254cae1ec1b5883ac55efdf05f0adc8
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:59:59 +0000

    replace hot confident operate beside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
