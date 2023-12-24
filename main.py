a6ee432fad512cd7e2b175122c383ba2819f85a4
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:46:23 +0000

    I fall lock smell purpose

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def cube_number(x):
+        return x**3
