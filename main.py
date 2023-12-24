4594a25ca95279abe9c4df5e12a2e48016b6bf4e
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:12:13 +0000

    receive uniform turnback definite custom

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def get_array_itemsize(array):
+        return array.itemsize
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
