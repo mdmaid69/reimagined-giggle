1d778b66daaa9b68f4c75e86bb8f61a955c093d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:58:28 +0000

    wise church disturb length healthy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
