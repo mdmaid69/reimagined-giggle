640d2c1c4e6de9181b08a8ee3838ccf737887ee9
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:43:55 +0000

    never suffer mind near profit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
