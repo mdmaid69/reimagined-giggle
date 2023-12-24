6f5ad0bb926fe6c6ceed74bea21ff6362f30d56b
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:09:23 +0000

    impression protect talkinto Pp also

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_force(mass, acceleration):
+        return mass * acceleration
