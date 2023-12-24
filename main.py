12921bc25af753497262bca11f26f97b78806448
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:11:42 +0000

    572enter feature wear fill trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import sys
+  def get_python_version():
+        return sys.version
