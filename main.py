1c047009c1c53364d181dfedc499db47012f0764
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:42:31 +0000

    mean usually unkind unit soft

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-print(math.pi)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import sys
+def print_python_version():
+        print(sys.version)
