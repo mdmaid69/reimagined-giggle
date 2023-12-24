c59b56d13bbc1fc27764a70570a601309c21d7b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:33:39 +0000

    intelligent breakdown late block thick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_average(lst):
+        return sum(lst) / len(lst)
