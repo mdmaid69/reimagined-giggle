d4192699ef25e5417d021bc7288d84c7b2deb319
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:40:48 +0000

    away laugh refer right rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
