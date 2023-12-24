629ff7bc6675420a57003fc95250873f071885d6
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:19:21 +0000

    like tooth press tape scrape

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-for i in range(10): print(i)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
