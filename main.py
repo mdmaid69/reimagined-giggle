669a76ecf29470c4f09c2c50b24775f0f9d45864
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:35:23 +0000

    software bit zero cheap goon

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  def reverse_list(lst):
+        return lst[::-1]
