f7632af65788b8edc82177e7cdbd4668272da1c8
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:05:57 +0000

    discussion expensive burst creature perform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_base_name(path):
+        return os.path.basename(path)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
