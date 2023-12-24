baf1182bd7a661e9e5de7549b725782e1be661aa
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:34:28 +0000

    cover address rule getoff total

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
