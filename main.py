9eb205257f8db3111434f0a4f3c6c5cc246b48ba
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:22:18 +0000

    want present fence free reject

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
