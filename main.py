34c10ca1e481f745571f9598de717ef0cd570f93
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:48:38 +0000

    Ee here attractive more beginning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def delete_file(file_name):
+        os.remove(file_name)
+def is_palindrome(s):
+        return s == s[::-1]
