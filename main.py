caf59d00684d4702147e6cb80b4d2f3409545f84
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:43:03 +0000

    bored on well crisis future

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
