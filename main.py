ceb57be4466897c425ac16bfc73c744124db1fc2
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:30:23 +0000

    permission visit key keepup head

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_current_directory():
+        return os.getcwd()
