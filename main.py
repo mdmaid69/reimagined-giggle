02ce61ec82ef2293d9bf8a1e95e86d48168ed737
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:31:59 +0000

    meet sail sky bite case

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
