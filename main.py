baf852328bd973a2cb635acabfb432fa2377aa97
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:05:51 +0000

    less unpleasant each slice print

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import json
+print(json.dumps({"name": "John", "age": 30}))
