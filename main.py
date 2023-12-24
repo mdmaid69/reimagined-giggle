cb4784ffee60ab2c74d5011dcd3d195237ddae35
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:19:34 +0000

    music among claim balance mend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
