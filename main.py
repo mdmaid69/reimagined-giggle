5de870562902683b2ee32e18be5f254b00a50826
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:23:56 +0000

    lucky concern limit wipe hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
