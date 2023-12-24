9641130c75e3fa45c705279ae00d6f89de3cee6d
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:15:30 +0000

    reach stir seem vote cough

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-  def convert_to_binary(n):
-        return bin(n)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
