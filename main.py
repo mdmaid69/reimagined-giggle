07fe41ed3e6c433c063f04f8afe55374a4cc6fe4
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:19:41 +0000

    closed period value next whenever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
