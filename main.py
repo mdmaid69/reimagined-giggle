2741a5025eb204afe8edabe589d88b60d9ca1add
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:35:39 +0000

    dull outside for world now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
