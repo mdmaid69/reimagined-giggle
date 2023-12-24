a62e84b3a7e0bdadffea58bc36e458ad35b94177
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:26:15 +0000

    damp debt brush return complete

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
