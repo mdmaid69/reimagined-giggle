f5f5f29f1e4d8ac11d08054b1a00250f2c7613ba
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:29:56 +0000

    tough reason both me behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
