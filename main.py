3b981a0edfbeeeee07ee357fb6c40b1734523b4d
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:24:48 +0000

    view godown shoulder anywayanyhow project

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+print(sum(range(10)))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
