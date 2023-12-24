139aebe4cda22e1ff2a66aae4fd4bc197244fd3f
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:03:36 +0000

    sharp limit label rather exchange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
