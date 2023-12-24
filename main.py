02c3a9eb4f6a504fb12d22aa534ba55842693410
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:08:52 +0000

    judge blow flat suck cook

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import array
 def get_array_as_str(array):
         return str(array)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
