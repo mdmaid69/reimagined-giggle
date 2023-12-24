7bb7a8179172d7bd1434ad2bc1f4acb116a9c359
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:34:14 +0000

    tomorrow it sink anyone hope

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import datetime
-print(datetime.datetime.now())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
