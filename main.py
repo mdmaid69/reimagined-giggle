65f97e968616857cfac4158f5ceaebd9e4dcaa19
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:03:06 +0000

    finger breathe centre Vv thirsty

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
