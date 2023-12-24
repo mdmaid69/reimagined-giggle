3000d3a5b17cd02d26f613deb5df07c9af6a9754
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:31:52 +0000

    switch hold favourite children address

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-for i in range(10): print(i)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
