d0365fca5f48699c60df9c9045b85422a39ec8dc
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:56:25 +0000

    either giveaway them height match

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
