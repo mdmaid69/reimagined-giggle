5941e5b7fc5a79767fb91e3a3e29417fb72e382b
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:51:23 +0000

    criticize translate deaf search on

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
