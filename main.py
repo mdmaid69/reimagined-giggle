2b04e194c434033818d2c1c29692f09a7f782b0a
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:00:24 +0000

    trip move aim steep bath

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
