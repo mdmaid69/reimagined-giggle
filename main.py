56fdc400ed37c45cd1894830dd1df6bb9e56d9ce
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:14:47 +0000

    exam commit accident injury tooth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def add_numbers(a, b):
-        return a + b
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
