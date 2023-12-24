ecb2b3cd022114e66d7d2727562f8c1d74e3d601
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:01:46 +0000

    damage opponent shadow regularly bend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
