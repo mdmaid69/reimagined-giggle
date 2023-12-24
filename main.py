56be9d9de8ceac372a157a201a9de2250bfe3580
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:49:27 +0000

    emphasize boil completely in yet

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
