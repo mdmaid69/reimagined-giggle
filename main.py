26232638467e1375bc70bbce46a4f867a4f5fbb2
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:35:02 +0000

    characteristic key treat clever force

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import platform
-def get_os_info():
-        return platform.uname()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
