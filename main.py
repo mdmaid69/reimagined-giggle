c97a2d8fb6215b51d35072141a7242563747839c
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:18:53 +0000

    holdon pencil active children shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def get_array_length(array):
-        return len(array)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
