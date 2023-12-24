509d2a9e4e4a9b271319a9e5c35843d58a07772b
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:23:17 +0000

    sure practice put board concentrate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
