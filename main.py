f1b1b84a8a587eea991b3a9f29cea1bb8cf962c2
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:30:44 +0000

    possibility giveout ought tidy strange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
