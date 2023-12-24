51f7d0cc1dad12d7a688e95a9161961962ec4afa
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:28:20 +0000

    wise usual practical reply urgent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def sort_list(lst):
+        return sorted(lst)
