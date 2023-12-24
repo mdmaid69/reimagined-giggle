337fe9c80b7e6e51b84a3d6d55f2e8e3d1766e4a
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:11:52 +0000

    turnoff detail travel crime them

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
