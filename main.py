3e0b19d2819060eb2399a52fe831adf8b5648e25
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:18:05 +0000

    describe strength accidentally talkinto church

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
