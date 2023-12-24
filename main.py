94631c12fa064440e5c8beac5fbd49b2f393a1e2
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:03:45 +0000

    reduction comeoff danger turnup you

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+print(sum(range(10)))
