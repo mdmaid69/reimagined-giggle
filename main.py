10da6e0f65759ea8a7c0c5c0b7abb32d74b56529
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:53:33 +0000

    violence glass still basis news

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def append_to_array(array, item):
+        array.append(item)
