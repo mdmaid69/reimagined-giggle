ad2dc75c2e1f3eb64cc5c791d240f7e82f039d39
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:31:49 +0000

    odd decision block anything crowded

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
