5e8c82f3995ee2e74cdc3b484839ad1baab050a3
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:03:02 +0000

    behind on offer plain ticket

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  def square_number(x):
+        return x**2
