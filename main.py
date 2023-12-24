ba3663269380e08edc18b2193a2573e8060636cb
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:52:29 +0000

    quiet brush treatment control feeling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
