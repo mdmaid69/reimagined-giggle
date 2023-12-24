8c7b76fd2ebf553b9ec469c1cdd42a29402604cf
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:34:52 +0000

    of Cc play better sight

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_pressure(force, area):
+        return force / area
