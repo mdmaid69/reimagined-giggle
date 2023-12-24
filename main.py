e174be71b53ae44c7c7d261bababfd2b71123c5c
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:43:58 +0000

    over event thanks town spend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
