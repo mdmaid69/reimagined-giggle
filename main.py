2641aad892c875ceb662f714888e98313248a642
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:28:59 +0000

    accommodation afraid prize spring chase

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
