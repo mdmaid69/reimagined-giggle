216650ace61a41a73de58e41d5458204a2806f6f
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:33:57 +0000

    train mine history cruel do

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
