10ada91d4257828ced1eab878e9c659c80a50293
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:08:25 +0000

    wonder vote criticism for but

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_str(array):
-        return str(array)
+def multiply_numbers(x, y):
+        return x * y
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
