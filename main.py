730a94c341e543fbdff902d6b30affe65cc9fb1b
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:49:51 +0000

    start average could shall part

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
