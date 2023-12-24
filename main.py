832f6531672a16faeba64bd14ecb84bcd1d3daf8
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:24:40 +0000

    rubbish innocent paper character settle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
