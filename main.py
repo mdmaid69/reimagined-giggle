cae864ea52af7f5192b7150dabd8e61c384f7094
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:26:35 +0000

    tell century visit bush turnover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
