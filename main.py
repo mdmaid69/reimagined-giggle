cbede0fe8d835a46f34db00602471ca70c5727b5
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:40:46 +0000

    track it project fat fine

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
