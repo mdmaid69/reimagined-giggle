b2db7ca06404aedeb1064c2b576d6593aca56daa
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:24:19 +0000

    succeed prize deliberate better cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def square_number(x):
+        return x**2
