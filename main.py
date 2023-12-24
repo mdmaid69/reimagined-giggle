bbfa738ba47185d48809177dc33138a559929959
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:38:24 +0000

    truth lend opposite neither single

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
