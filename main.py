9b23f38de095310858e430baff07f3ae00e9e06f
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:53:54 +0000

    pass cut exam walk intend

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
