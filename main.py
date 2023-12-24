5819e05382b99c9a0adda29325fd7d6c85d3fd70
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:10:04 +0000

    any temperature doctor possibility protect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
