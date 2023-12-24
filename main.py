67d2dccb9df972f62683742eb5912a65a6bdaaa7
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:02:22 +0000

    ear black personal situation welcome

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
