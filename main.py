68714e5511fef0ece909b8a0034aca3999c33cc6
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:13:33 +0000

    frame politics share finish low

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def reverse_list(lst):
+        return lst[::-1]
