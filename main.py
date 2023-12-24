e772d872a00c77ea5f73bd4ea65d181bc19bcbcd
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:59:54 +0000

    clever sport brain help state

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
