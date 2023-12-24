a264faac229924cf3ad5199ba61c3dd946c32acc
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:12:55 +0000

    collect dry if Jj limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
