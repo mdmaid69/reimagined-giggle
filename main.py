a710110a1c1cb5d56dcb6acb7a822696a7d02f51
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:05:41 +0000

    question general excellent advise instead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import tensorflow as tf
+print(tf.__version__)
