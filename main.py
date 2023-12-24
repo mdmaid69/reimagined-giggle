4e1ccf9cacad83a6bc2bca3a5c197c3c2500ab89
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:57:19 +0000

    quick for public future sweet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
