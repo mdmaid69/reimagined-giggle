36767981fb3600b95f58d29130c1870f0d700344
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:34:10 +0000

    shut who sound finally follow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
