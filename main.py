6f10f9e4fb5e651a57382c673c3d82bbd6144e06
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:28:29 +0000

    point only guess job OKorokay

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
