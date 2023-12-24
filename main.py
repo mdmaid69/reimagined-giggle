739e3795fca2dbf6ae01d9eaaac3dd2aaac3e004
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:39:45 +0000

    straight Oo remark finger tense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import logging
+def log_message(message):
+        logging.info(message)
