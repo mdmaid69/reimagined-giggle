d6402c9abbd90cfd8eecf99f784b01163ea89ea9
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:53:48 +0000

    long mind worried fierce violence

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
