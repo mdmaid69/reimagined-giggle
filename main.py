d2eb0dcce6fdc643c2e48118b07bb6f9a0546fbf
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:02:44 +0000

    order insist behaviour probably shine

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
