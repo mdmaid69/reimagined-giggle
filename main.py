5cd65dfff371a0ef53f68672a07806c80235ea0f
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:09:43 +0000

    570enjoyable judge boat thick organization

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_str(array):
+        return str(array)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
