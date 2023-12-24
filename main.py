795d027b61d1dccfad00598650156e85c4cd8e7f
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:05:06 +0000

    noise wide where murder inside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_current_directory():
+        return os.getcwd()
