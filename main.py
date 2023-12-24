9e1c23558d663bbdcb497fbfc49587a5efa597a8
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:00:17 +0000

    impossible brother authority pay marry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
