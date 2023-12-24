ad74757ea5f71bdf2e5e3ac2431b866270bfde1b
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:53:27 +0000

    obtain shut tooth vision brother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def is_even(n):
+        return n % 2 == 0
