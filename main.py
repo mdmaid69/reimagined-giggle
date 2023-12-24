003be569313930f51a9420339a2debc7653eb9eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:29:25 +0000

    television handle near thumb stay

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
