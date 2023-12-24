230d0ab1db3857dc0f5bc0e37a5cddd8d276f22a
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:01:30 +0000

    expensive bored replace possibility this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_acceleration(speed, time):
+        return speed / time
