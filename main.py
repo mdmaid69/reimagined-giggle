1a1143e153b9fce4c067e85c938e8b0629a9a9f3
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:12:40 +0000

    key birth believe recommend fond

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import sys
+def exit_program():
+        sys.exit()
+def is_odd(n):
+        return n % 2 != 0
