e8aed5a8ef50d41588e9826eead7d1cb7db8d768
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:07:35 +0000

    reject high repeat upwards keen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_base_name(path):
+        return os.path.basename(path)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
