8b2682022b60954161cced4920500e07cc11be72
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:55:01 +0000

    past visit quit frightened study

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
