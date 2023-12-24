62efd35d0d31ac5e488548e219cb24208a9505d8
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:48:47 +0000

    cut suffer trip suit herself

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
