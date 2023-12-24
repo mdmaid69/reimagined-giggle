54582d9d0971f571970c4cdd8d5749fd3575c7bb
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:04:15 +0000

    same clue trade getout stage

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
