fbf6e0339149e4b4ac294311c8ab556a7ba67da9
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:37:46 +0000

    test right succeed lift maybe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  def reverse_list(lst):
+        return lst[::-1]
