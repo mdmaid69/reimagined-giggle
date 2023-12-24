e21f4106749828e4aa878d64556cfbf513f12f14
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:10:14 +0000

    nervous valuable entry colour as

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
