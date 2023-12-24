72d47754dcf00102da3065a13c3627769bbf2d01
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:27:35 +0000

    except all notice degree lend

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_volume(length, width, height):
+        return length * width * height
