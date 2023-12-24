d7eb235f0b0341db466bb302e79fb518901b2771
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:31:07 +0000

    soon body increase welcome where

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import os
-def remove_directory(path):
-        os.rmdir(path)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
