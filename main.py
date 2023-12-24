38cd5a0c062c53a5d67073f9c8035250b7568f13
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:13:07 +0000

    later public fond common free

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import sys
+print(sys.version)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
