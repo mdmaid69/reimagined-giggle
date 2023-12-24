04a84d01e5622566a1150e60006359b4fb84fd64
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:12:12 +0000

    peaceful yet embarrassed feel cruel

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
