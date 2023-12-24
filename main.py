1eceb1aa617b1b212d632143952a2acdb169c97a
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:48:28 +0000

    corner work capital total disturbing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
