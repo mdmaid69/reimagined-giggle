281749c88b70b6014d519cfe4662458837ae769b
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:29:09 +0000

    private save wind tax even

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-for i in range(5):
-        print(i)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
