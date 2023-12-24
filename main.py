0a46202a1f5c736868b33be4374676243d61b0aa
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:45:37 +0000

    height load finally generous bear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
