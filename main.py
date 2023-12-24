cf11908ec7d701b2a2de54caf1eb3487fb9f443e
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:56:57 +0000

    between string fast black future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
