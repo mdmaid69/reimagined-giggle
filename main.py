a51002c73c4f596073722806ad6e2f70db4e9de9
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:06:30 +0000

    standard count either culture who

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+for i in range(10): print(i)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
