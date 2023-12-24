98ba5441816451235693155d8150e335c78a161a
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:28:54 +0000

    spray play matter soil pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
