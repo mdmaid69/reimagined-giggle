7f6a1932c8d3ab8bd4191575e74539c5e26bf5b2
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:01:55 +0000

    nervous opposite translate him remember

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
