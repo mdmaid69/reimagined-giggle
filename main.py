0f105927c6e4913f40d7b8a267199532c8c5e097
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:32:30 +0000

    emotion treat importance tip alone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
