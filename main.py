8c1d2d81237ca684b871d732447f46018eddf1e6
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:52:23 +0000

    there godown hot unusual roll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_min(lst):
+        return min(lst)
