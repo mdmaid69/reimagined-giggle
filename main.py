31c2aba801b94833a0ca8850e1223e2bc2e96bdf
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:49:19 +0000

    drop cheap sharp edge food

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
   import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def calculate_mean(arr):
+        return np.mean(arr)
