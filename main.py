e48b02010d6b178cc9e76358367281bf0389e9f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:43:44 +0000

    desperate clean fix flood to

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def is_even(n):
-        return n % 2 == 0
+name = "Python"
+print("Hello,", name)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
