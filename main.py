709f30b43019845e9dcde225c907df94cce89c08
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:11:27 +0000

    shout dance oil chance less

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
