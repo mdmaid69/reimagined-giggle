828d9d3d24b695cdea857d825b9dfe2e9876c432
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:30:37 +0000

    major luck drink demonstrate water

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
