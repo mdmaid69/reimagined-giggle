868d88a45564496716ad3b580a476b7bfe93df7d
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:10:28 +0000

    afraid contain steam off interesting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_power(work, time):
+        return work / time
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
