b15f3c9ec4004f77432a353a88e2d54f77c52c12
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:43:25 +0000

    heavy traffic doctor sale responsible

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+for i in range(10): print(i)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
