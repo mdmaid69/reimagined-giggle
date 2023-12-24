6bf136cbf6cf2aac4f126dd6f37f3f918eac5f29
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:13:07 +0000

    male consider bringdown feeling sad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_max(numbers):
+        return max(numbers)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
