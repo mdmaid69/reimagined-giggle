03ff6c926780c5fe9d1cbbf4532d74736c7d2204
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:25:21 +0000

    considering press card welcome mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
