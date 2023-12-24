6acd2b84d2bb57a7db3c21fd120f703009512cbb
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:42:19 +0000

    motion task measure lunch wise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import sys
+def exit_program():
+        sys.exit()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
