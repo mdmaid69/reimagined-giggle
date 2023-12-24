8a03f8a9ab41d4c648c8700e8e1ac3364d9b3b50
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:21:28 +0000

    poison ours petrol protection ability

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
