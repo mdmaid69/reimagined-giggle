603199246a095f0240de1e8759236a5f4038ed8e
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:42:23 +0000

    furniture unlucky report me fire

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+print([x**2 for x in range(10)])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
