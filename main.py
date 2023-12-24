013f808a2cfa4f101f5026788e10c759ac36dd08
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:15:49 +0000

    collect deliver extraordinary keepout favourite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
