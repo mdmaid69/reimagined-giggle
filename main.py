38505ba34e0d64d1ea450b722743e7b02fcfc276
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:57:11 +0000

    now at busy this curious

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
