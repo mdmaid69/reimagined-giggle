7e470c91cfaf4d5cd44fca819e1ee9dc79d92cb0
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:43:12 +0000

    scrape unusual brain reserve programme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
