93c2eb86c5ee7eb63c37cc16d24736fa2dda567b
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:27:05 +0000

    fierce turndown ability congratulate should

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
