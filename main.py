06387a8eadd6de17fd30d6a5bad9b7212f50e7fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:28:12 +0000

    talkoutof corner favourite really combination

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
