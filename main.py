eb67bd1db19cb06d501a9718078670cf559e9449
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:15:04 +0000

    sign declare part chance height

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_list_from_array(array):
+        return array.tolist()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
