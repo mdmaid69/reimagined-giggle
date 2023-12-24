572f9a377c71cb9435c61eeb79f2241f18a777d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:07:58 +0000

    callout whistle him defend uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
