497315c29803c058ca44079db78a6e5dfe9bd6bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:12:09 +0000

    hear pump gap service although

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
