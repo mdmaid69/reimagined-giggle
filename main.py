3c76852f483b5cf216ae747bde3597affb1343e4
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:41:59 +0000

    child sail sudden trade best

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_average(lst):
+        return sum(lst) / len(lst)
