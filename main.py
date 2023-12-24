bc7079baa6e269648254429f3ff8862f0a72328f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:50:59 +0000

    spite deliberately pride song above

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
