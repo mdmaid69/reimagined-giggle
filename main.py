026ca61a187f84a9a468397fefec2f3d18440000
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:57:02 +0000

    brief copy gooff sad authority

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
