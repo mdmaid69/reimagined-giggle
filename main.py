e779028b4aac11e96dc3856d9b7b3f3745d97453
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:44:57 +0000

    by sore write news office

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_index(array, item):
+        return array.index(item)
