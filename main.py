90d75429cbcbbf167ac351a7ed23615c36bb8d7d
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:36:41 +0000

    command nowhere refuse treatment few

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
