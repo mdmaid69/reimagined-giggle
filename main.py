bc7e7fd6c86f29fc8fb42fc4445cad5334e3db47
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:14:15 +0000

    flood grateful admit whole direct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
