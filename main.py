0fabdcf7840611406f634eb4d6dc212563bb2017
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:49:10 +0000

    light side worth passenger major

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
