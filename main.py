767b619142dad9a9bb4fdbbceb0f87af7f2c5f05
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:02:59 +0000

    pencil step prevent by hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
