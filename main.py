449f1506943181ea4bcab455a6a3cf7d3a502326
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:30:31 +0000

    workon land summer store church

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def add_numbers(x, y):
-        return x + y
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_max(numbers):
+        return max(numbers)
