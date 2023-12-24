61da1127ae04884819f71b24ca272a9327453bb7
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:52:34 +0000

    upset convince bridge stay feed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import platform
+def get_python_version():
+        return platform.python_version()
