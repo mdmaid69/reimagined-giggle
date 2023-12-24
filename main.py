249692908089e9db6cbf6e3bd9a25119984edf37
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:20:08 +0000

    declaration minority wheel corner winter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
