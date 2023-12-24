9be2f1c1191fe29187aa0ac2693200b53e8f4863
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:54:20 +0000

    information cheek different honest obvious

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
