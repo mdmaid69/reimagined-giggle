bb3dc40c7719af484801a219aa831ecbc0e0cf92
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:26:07 +0000

    Oo possible tooth round collect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
