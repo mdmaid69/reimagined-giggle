1bc4261ce02ac8acad4e5897925502d4ae794118
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:56:10 +0000

    anywhere damage murder bringdown satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
