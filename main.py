d58326bd51d540277d3095accd045ef18e25c90e
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:07:36 +0000

    dream part number able train

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_float(array):
+        return float(array[0])
