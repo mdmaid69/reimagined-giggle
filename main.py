b344b17133602a22129c60c53d9707d497659f8d
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:28:00 +0000

    laugh name visit compare crazy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
