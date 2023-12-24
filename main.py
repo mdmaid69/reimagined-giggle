de3ec41631405af64599029807764411baf720a7
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:46:00 +0000

    relationship tip turnback Bb and

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import math
+print(math.pi)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
