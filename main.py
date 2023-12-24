53bd9b9b710bdb4e4c1340935ca2002543acbc53
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:57:06 +0000

    brush holdout your forgive female

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
