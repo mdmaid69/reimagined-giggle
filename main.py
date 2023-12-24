952fae70fdc4bc92d4fd2cb8d5a779c213a6d546
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:11:54 +0000

    excuse bill away shall perhaps

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import logging
+def log_message(message):
+        logging.info(message)
