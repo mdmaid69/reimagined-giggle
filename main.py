caf281c5275bdf47c7e17d8bff9aaf274197eaa6
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:42:03 +0000

    send themselves dare raw operate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_typecode(array):
+        return array.typecode
