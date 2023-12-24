ddc49f4695bd820ffd8f435f423392a00fbccf1b
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:27:06 +0000

    Ll leg otherwise narrow continue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
