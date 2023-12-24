12ec930447b7fc522643e9ec83734a0e46f3969e
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:14:03 +0000

    winter this war store bringforward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
