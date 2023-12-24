64a39a2cb891a5d649f033097c1a48c2d57df158
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:53:37 +0000

    wrong sting nervous treat appear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_current_directory():
+        return os.getcwd()
+text = "Hello, world!"
+print("Words:", len(text.split()))
