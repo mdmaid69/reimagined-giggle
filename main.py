7edde6e5943fb8d44ab50743041a9d4082c4d3af
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:24:12 +0000

    traffic confusing discussion genuine afford

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
