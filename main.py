a136894159c768f4f1acd044e40edfc44db036be
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:55:54 +0000

    wrap complicated wander sail accidentally

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
