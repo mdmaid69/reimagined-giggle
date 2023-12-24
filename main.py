50ee1d461296c173de985e094da6320cfeab3a0f
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:53:09 +0000

    favourite battery normal under comeround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
