fdc38cc2ead1af1e9e2260fa088434f3d7026b0f
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:10:24 +0000

    distance go suggestion suggestion price

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
