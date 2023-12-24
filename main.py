accc6c81ca854d1e244b5ecc1587224690cefe8d
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:11:09 +0000

    moral accidentally position adjective degree

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
