71b48ebc6d6f569c56c841c62f31a44857d2cfcc
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:48:48 +0000

    emotion equal represent branch print

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import time
-def get_current_time():
-        return time.ctime()
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
