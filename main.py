aefefbdd3d5ca73721f1f652dcd7b7fb3e9d7c9a
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:00:11 +0000

    action waste shade serve marry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
