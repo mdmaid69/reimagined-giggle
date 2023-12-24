ec9bd859803bd660bde6fe7d1884d9bf1e5fd6fe
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:58:53 +0000

    damage trade traffic embarrassed experience

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
