feb1786bc701b7e464d021ac77cba38549865607
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:09:56 +0000

    land determined now allow guess

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-print([x**2 for x in range(10)])
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
