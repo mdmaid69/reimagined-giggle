efc99c4f0abac5c74b32c3748dafff7fae3f5b28
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:40:05 +0000

    carry slow so year town

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
