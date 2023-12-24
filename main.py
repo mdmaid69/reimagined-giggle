e6c79686ba84bc09575d5b3ce1275d671f5953d5
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:35:31 +0000

    pour equal theirs breakinto everything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
