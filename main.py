2c2aea7af6671661ef44f16a912b376b1a13d178
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:00:17 +0000

    nearly steady Vv popular dish

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import random
-print(random.randint(0, 100))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def is_odd(n):
+        return n % 2 != 0
