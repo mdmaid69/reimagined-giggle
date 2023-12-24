03960c895d561a472d705153483c626e60a55fe8
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:03:15 +0000

    win lean story expression good

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
