7058cb4ce0c25bcf06284996830d9f3743eba2ae
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:52:36 +0000

    wonder upstairs talkoutof official opposite

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 n = 10
 print("Cube numbers:", [x**3 for x in range(n)])
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
