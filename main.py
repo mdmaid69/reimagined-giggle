f116d5b8c6d59781b34a307b445fbe90da362d71
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:22:40 +0000

    evening brother pass contain active

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import os
+def change_working_directory(path):
+        os.chdir(path)
