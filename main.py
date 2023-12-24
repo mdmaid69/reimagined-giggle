b5d503899a41bb0f84d16a624978113182ee5db2
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:24:10 +0000

    worry song whoever afraid miss

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
