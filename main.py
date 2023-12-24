4f0a2db520055a5696637e4f7651a477bec5088e
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:26:51 +0000

    message national all balance form

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
