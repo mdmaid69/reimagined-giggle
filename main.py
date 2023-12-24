4ffbadfe765dfd6a112f7c75d1e0db1a40106b05
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:03:38 +0000

    schedule range although everybody heart

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
