0887d189edaeb375a8018dd3f214010bf9d20785
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:52:45 +0000

    sport imagine lay forgive lot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
