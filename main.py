bd12d4b0c30a7e8305de1b35c853a02c9031e80e
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:50:21 +0000

    workon morning expect period pause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def convert_to_binary(n):
-        return bin(n)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
