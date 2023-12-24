c23a3e53c50a21a28e484985ed585d621104cb54
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:56:51 +0000

    circle disappear lucky black rest

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
