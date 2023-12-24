066097633737ab4b36a7c6d7cee9db0e256e7461
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:29:04 +0000

    pass poor bridge disagree born

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def square_number(x):
+        return x**2
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
