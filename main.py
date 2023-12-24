dffa007f2bb88f45fc6f623ae1f4ce23c4eb6d14
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:08:57 +0000

    lock love bright switch lucky

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import time
+print(time.time())
