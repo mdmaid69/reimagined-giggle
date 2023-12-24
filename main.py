f1afc96f43156c2716dca6258a29bae2453faa90
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:35:28 +0000

    callup schedule regional talkinto convince

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
