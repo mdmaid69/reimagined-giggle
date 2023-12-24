abe2c232b7f709b1001e7656bcbaf70f8a767d26
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:28:46 +0000

    load stamp allow her snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
