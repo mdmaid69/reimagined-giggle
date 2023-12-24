e5f3d0f54eb1f71ca6449c8e41c639a353f42d50
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:20:23 +0000

    share cry forget class face

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+for i in range(10): print(i)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
