823efd0f6d1bbf4b6efb5c1b5c69499403578c1d
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:35:25 +0000

    learn much soil content comeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def get_array_as_float(array):
+        return float(array[0])
