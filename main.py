fdc77fea87d23d402bfe6be409bf74d9455dfc3e
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:38:08 +0000

    dead complaint criticism word situation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
