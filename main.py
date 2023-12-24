9b33dd55384411d4a9327f9349b2df646a6da882
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:57:57 +0000

    fold copy exam within comeover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
