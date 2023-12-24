eedb3b53a02ea5043d212e3903f91326f1b3aae3
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:07:27 +0000

    rough believe earth drive regret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
