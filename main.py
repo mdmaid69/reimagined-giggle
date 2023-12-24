60136719d2bb3f142d9b7087dc62c1059672e98f
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:32:28 +0000

    port break plate jump worse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import json
+def convert_to_json(data):
+        return json.dumps(data)
