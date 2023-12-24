6e3a828774eee575a04c427e9c513af5bf728e27
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:16:36 +0000

    other television but appear admire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
