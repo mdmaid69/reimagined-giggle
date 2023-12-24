8efd0f54c82b603c349a48d40ee58e211afd42b5
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:36:47 +0000

    slip relationship main tie eager

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
