19e31a60eaab1d65c267cdeeaf8897339ad45747
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:23:32 +0000

    greeting worth getout plate right

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
