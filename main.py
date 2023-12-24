b7c76041bf00dac2ad356188ae540a505764450b
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:16:34 +0000

    limit valuable transparent that children

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def is_palindrome(s):
+        return s == s[::-1]
