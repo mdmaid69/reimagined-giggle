38eb0e90fb7e21fd8a44e86895ddf7212d6afdf2
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:45:45 +0000

    tomorrow their accidentally scare brother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import time
+print(time.time())
