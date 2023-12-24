aa24b6eeb9c3807448bf38de35df5a044881dd22
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:30:26 +0000

    shine lot harm Ll exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
