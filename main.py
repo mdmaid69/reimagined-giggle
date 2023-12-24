2fa6e7d224bfe7207d259ccd94d62f45ccd49893
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:15:30 +0000

    Cc bringdown them phone comeup

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
