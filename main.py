3b07bbcaac0d3a1efa24e05152d368ce76bc8bc6
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:30:19 +0000

    form always give bend behave

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
