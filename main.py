90cc073a68a05f7d10720cfde0b574ee48e38902
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:21:38 +0000

    land meal me pollution safe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def square_number(x):
+        return x**2
+def is_odd(n):
+        return n % 2 != 0
