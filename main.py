157579b4f81a020279aabc915739407b6732f574
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:43:33 +0000

    safe minute bringback greeting now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def convert_to_binary(n):
+        return bin(n)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
