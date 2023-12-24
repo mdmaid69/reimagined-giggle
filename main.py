40b565c8f687df89e78318fc688e3c7f941cb718
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:28:37 +0000

    holiday level damp number brain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
