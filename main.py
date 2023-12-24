1d7c4ac450dffc90f21b8d41828b273bedcb53ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:58:03 +0000

    ourselves mix follow strike temper

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def convert_to_json(data):
+        return json.dumps(data)
