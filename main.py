2ed3ee43180ff5fdb621c3f012e531e540e9f8f9
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:34:57 +0000

    fire greeting sing accident provide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def clear_array(array):
-        array *= 0
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def greet(name):
+        print(f"Hello, {name}!")
