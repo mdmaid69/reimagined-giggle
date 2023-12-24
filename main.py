48bdd274ce65fcda60cb1a8a4b3f5b62241fd26a
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:18:23 +0000

    turndown mad before rid light

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
