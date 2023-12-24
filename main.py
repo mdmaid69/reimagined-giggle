2ac772425f9fa0f6a5e03d30bdcc8785395cce53
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:24:40 +0000

    average curious which window discovery

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
