11e56ad767cbde7512f9d4eca82317c390791349
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:46:21 +0000

    correct properly vanish let make

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def reverse_array(array):
+        array.reverse()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
