7b6fd9a7299bd0b4fc04ab498c981263069bcf16
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:14:10 +0000

    rain answer age excuse meal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
