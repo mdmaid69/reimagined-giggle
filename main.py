3eb27a8b7d19309d0b0b0c110d5a6a5f47e7c087
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:04:23 +0000

    dinner march unfortunate suddenly pause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import os
-print(os.getcwd())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def add_numbers(a, b):
+        return a + b
