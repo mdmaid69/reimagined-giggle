21b11cf2b1e62c5da88b6b1166eef4b6aeea94d8
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:06:04 +0000

    autumn remove room worth comment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def count_elements(lst):
-        return len(lst)
+for i in range(5):
+        print(i)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
