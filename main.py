52e71bc78043913382c134cb71e56292787e70b0
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:56:38 +0000

    whether near injury party shrink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_as_set(array):
+        return set(array)
