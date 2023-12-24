dbd125add88904a17531453d67e6f5ea61cda835
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:33:06 +0000

    joke lookup develop dark shelter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
