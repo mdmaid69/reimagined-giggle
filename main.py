eb5962fca729fae8219fe9554c2eb4d2ab38a5cc
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:06:55 +0000

    happy sink makeinto slice slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_current_working_directory():
+        return os.getcwd()
