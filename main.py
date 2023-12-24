6bc12bfd08e562bffc0fd1927cbae7b1e868adc9
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:58:26 +0000

    also to thankyou never winter

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+print(sum(range(10)))
