c79dd747beb25b89cc3b7ec672ebc9ef6ac398ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:50:19 +0000

    grateful reason equal manage nonsense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
