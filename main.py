41f83bd4ac7c230efc53581faa183e8f91a897da
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:38:40 +0000

    callat typical Hh wipe whole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
