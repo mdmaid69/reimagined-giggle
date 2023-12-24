41185db91d87d7223a0c82b2695cd44b8bcc8ada
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:48:35 +0000

    always brown steal pure information

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
