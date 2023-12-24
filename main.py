32b245daea0cbb84926906bb5c3433eb0bce8b55
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:34:46 +0000

    yesterday music Mm interest ocean

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ def get_array_from_file(filename, typecode):
         with open(filename, "rb") as f:
         a.fromfile(f, os.path.getsize(filename) // a.itemsize)
         return a
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
