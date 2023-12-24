456cc1ecc891605eace2267a0bd44e7a0902ef3a
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:43:13 +0000

    concern limit aim them instruction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
