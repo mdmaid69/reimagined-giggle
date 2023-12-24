b841efdcd51c99bd49229ede31e6a1288e90bc8e
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:46:12 +0000

    ask maybe kind system upset

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def convert_to_binary(n):
+        return bin(n)
+def greet(name):
+        print(f"Hello, {name}!")
