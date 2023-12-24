87cf101138928016623ab5686e25820e02b1627a
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:25:54 +0000

    across anxious contribute steer grand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
