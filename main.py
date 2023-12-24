0d61bf14fd38828e62f48547c7d76ea742ec71ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:01:26 +0000

    sure attempt rain possible hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
