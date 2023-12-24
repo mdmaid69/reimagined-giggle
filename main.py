0bcbdf0c4d1bac8d73db4794bfd99cc5361273bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:27:46 +0000

    radio tradition write 566end pretty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
