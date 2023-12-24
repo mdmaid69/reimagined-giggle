443da5b1d781c2645a9a3e8c6d2cff30e556a1bb
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:50:39 +0000

    become wear introduction mean top

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
