2d6fa544be6f1b508abc00f3443c428866424f89
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:37:56 +0000

    turnaway careful lesson frame comeback

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
