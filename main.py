38693c9af2e42f8a0d307af90f8d4356c15713a4
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:37:44 +0000

    sleep rest nose settle advanced

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def sort_numbers(numbers):
+        return sorted(numbers)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
