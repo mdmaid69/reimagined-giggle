16bafe4f9a832c739e6259eb1139f5bee29ec148
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:54:32 +0000

    now correction chance personal exit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
