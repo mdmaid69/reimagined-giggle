f43dd378f9714f13350527bfdc4ccff6bc285976
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:40:21 +0000

    suck long success listen goout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
