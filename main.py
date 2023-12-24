a27fd08d20f561d6c41a4821fa974011b868956e
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:18:13 +0000

    cost seat comeup destroy closely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import datetime
+def get_current_date():
+        return datetime.date.today()
