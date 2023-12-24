69335e6baf2ad15d52f448b7592a5ed1bb418771
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:18:34 +0000

    university variety long off very

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_acceleration(speed, time):
-        return speed / time
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
