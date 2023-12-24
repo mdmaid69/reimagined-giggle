a55d810bd9715cde279716d04a93cbf7c71a9827
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:00:29 +0000

    runaway want behaviour in getout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def calculate_power(work, time):
+        return work / time
