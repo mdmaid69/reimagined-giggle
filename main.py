12933d54c6bc91706fb49806d7753c38b8bc1fc2
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:48:33 +0000

    alone build typical experiment shape

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import random
-print(random.randint(0, 100))
+import array
+def get_array_length(array):
+        return len(array)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
