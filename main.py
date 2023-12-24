c261bfba71f837f16f391ef0e87e0f3ba4bca007
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:34:23 +0000

    jump straight silent home curious

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
