4c2c7422e60b2db66e1567c8542f0e61a02824c1
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:04:03 +0000

    holdon listenfor surface fetch Bb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
