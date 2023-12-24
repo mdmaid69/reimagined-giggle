cc9acab336a88f945933e9f73b76d5429c130698
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:46:08 +0000

    woman fault power traffic receive

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
