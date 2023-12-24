79466b83f62321497c6444c50372f6120ee1da2e
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:11:44 +0000

    pot shape fight throughout confident

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def multiply_numbers(x, y):
+        return x * y
