38b48e0e27c84a4f6ec8b913344106d92b351f1a
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:52:56 +0000

    punish direction inquire small second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
