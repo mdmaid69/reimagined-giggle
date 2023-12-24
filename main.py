b0a1b44e120c1f5d86110557da911aa53371be16
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:44:09 +0000

    fold trust 570enjoyable around band

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_current_directory():
+        return os.getcwd()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
