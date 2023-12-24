edaa38f6771255c7701ffea2b11bb748248fc951
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:37:45 +0000

    nigga well speak fast without

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-def remove_duplicates(lst):
-        return list(set(lst))
