c75d96550b7b5a7e575981d106632f934b1e4cac
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:41:39 +0000

    sink busy any work partner

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
