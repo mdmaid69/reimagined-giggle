868e76c07fbb3d4194df93af813b73540ed89d4e
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:36:44 +0000

    plan secret platform present so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def convert_to_octal(n):
+        return oct(n)
