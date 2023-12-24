4edb9333927deeba9b2a5b4f120082bb68078039
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:09:56 +0000

    laugh shade my dead height

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import collections
+def create_queue():
+        return collections.deque()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
