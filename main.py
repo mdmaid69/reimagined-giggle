6c4f210c120f37425f9cfdfd34181c2766e193a1
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:51:07 +0000

    height seriously welcome tell prevent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
