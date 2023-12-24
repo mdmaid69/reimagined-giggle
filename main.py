3f74a8319f393381d1f59abd7ef81235707fbb76
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:51:31 +0000

    take advantage load wonder also

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def split_path(path):
+        return os.path.split(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
