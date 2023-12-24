141ce8f009442e62e145db442523e6d78d27a4d1
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:44:25 +0000

    behaviour hot influence process pocket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
