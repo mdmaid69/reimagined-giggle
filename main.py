8b0e2c039c8c7f1bde37192158c6b3ea5d0d1e4b
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:19:15 +0000

    protest sell snow free fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
