5f547e101b59f88bf29858c82070179057082a90
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:56:19 +0000

    speed joke contact 570enjoyable risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
