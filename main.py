adf66bb69cedf369c29f000c5d1f0dcf7dcd22ce
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:03:25 +0000

    Ss quality gentle though ashamed

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+print(sum(range(10)))
