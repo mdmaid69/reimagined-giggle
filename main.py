e66424600e9d1f3f863980270e590f1aadbfb346
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:41:17 +0000

    once during smell rush deaf

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
