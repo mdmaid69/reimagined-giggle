4b785234eb35fdaec8ad7b07fdb20d373b3858ce
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:36:38 +0000

    priority where control recognize completely

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print(sum(range(10)))
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
