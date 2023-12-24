a7aaa7b020ece5866602cefb501d80eb8f1bd007
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:13:09 +0000

    dream welcome declare wash clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def convert_to_binary(n):
-        return bin(n)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
