30efde52fe359de89103b3b1fe846db534e90ca7
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:21:17 +0000

    any callat receive 566end gate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
