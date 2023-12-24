4a99ade63b7eeaeac58d00347f1d2a8e97e59b76
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:24:36 +0000

    principle mean advanced sorry negative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
