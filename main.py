8d89de0013427b7a6267e788dd04de7112fe239f
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:09:16 +0000

    embarrassing future preparation reduce father

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import platform
+def get_python_version():
+        return platform.python_version()
