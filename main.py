680de5105a71230eb703afd4292e9ae05a15e1c8
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:45:46 +0000

    direction experiment wise at group

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
