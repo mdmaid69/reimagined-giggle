0e8753b90d6849758ef72f8ee3965b1ac5dcaf24
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:53:35 +0000

    no side break solve put

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_as_str(array):
+        return str(array)
