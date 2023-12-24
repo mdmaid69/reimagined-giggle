23d15e8b2ccbc7a55964caef11f79bf08ba649f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:53:49 +0000

    wild skill discussion balance drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
