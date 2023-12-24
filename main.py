07bcb51c73a2d29463814675ae6dc5d79322e687
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:32:15 +0000

    contact march us mystery tongue

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import os
-print(os.getcwd())
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
