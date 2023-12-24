d84b070085d5f378b687024768f74557bc78c380
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:34:11 +0000

    fall experiment wrong balance violence

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
