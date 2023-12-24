4d8b1a74a64edce3c41941873afe2ee81a365ad1
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:23:40 +0000

    breakout simple spot exchange target

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
