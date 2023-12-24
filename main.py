e8014539859b98353679d802ff6843688263d2d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:26:21 +0000

    fall makeinto what getback present

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import logging
-def log_message(message):
-        logging.info(message)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
