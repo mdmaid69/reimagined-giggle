bb3d55e232c2b39dd13696fa863bcf736acf093c
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:45:50 +0000

    distance firm set complain usual

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
