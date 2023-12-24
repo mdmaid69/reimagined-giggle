8b065c0da30ea4f7aab56389864c2bca3a02b89b
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:50:31 +0000

    except lose bridge experiment frighten

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
