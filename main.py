175f911202b5f1df3fc33ac9e4b03c1eee8b9560
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:05:46 +0000

    show young bringup sound thankyou

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
