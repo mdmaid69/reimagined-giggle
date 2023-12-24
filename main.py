1608f9b715987c959cdbd42bd774cf11dbfbf011
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:48:34 +0000

    exaggerate address same race fun

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import numpy as np
+print(np.array([1, 2, 3]))
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
