61d816f0035e707d57863ff6ecb5818b84f041db
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:18:13 +0000

    collect original own criticize lie

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
