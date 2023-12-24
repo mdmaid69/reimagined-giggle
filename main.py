42795cfa29462960ae4eb1e219dced357ee97126
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:41:59 +0000

    comeback concern fierce interfere noone

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
