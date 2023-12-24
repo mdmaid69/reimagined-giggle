ff240ccfabd2e19d004b5cdf6d2ad7ebfbece7eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:01:39 +0000

    cook hide breakin study Qq

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
