1cccca8a48b538aa00939f788f4fa80b8a504058
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:23:02 +0000

    branch score weather opposite guilty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
