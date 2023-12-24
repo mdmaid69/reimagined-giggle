e530b709d1844434b6d17be6acaa3994fcf61535
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:22:15 +0000

    telephone spring top worried break

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import platform
+def get_os_info():
+        return platform.uname()
