bf57e386049e6089323af2ac499241922e55cc52
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:36:18 +0000

    quickly fit light opportunity thought

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def add_numbers(x, y):
+        return x + y
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
