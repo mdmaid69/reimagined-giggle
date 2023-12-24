5d34c96a72b1c8ce792e43e2d49071f27a1b601b
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:13:38 +0000

    rub difficult call attention complete

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
