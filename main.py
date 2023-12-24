b5e9b61b13d8105b80fb57e3f91812e9a11cd5e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:59:36 +0000

    talkinto contact somewhere backwards delicate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_arc_tangent(x):
+        return math.atan(x)
