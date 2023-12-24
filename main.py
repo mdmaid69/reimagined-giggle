102a1ab88d9f9442f02414ac2326032d53914fad
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:36:29 +0000

    cry careful takeback zero stamp

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
