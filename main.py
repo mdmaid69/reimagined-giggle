5992fd92273e1ac95714ce75f6c3107fd8a80177
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:12:20 +0000

    take top gap offer goup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import datetime
+def get_current_date():
+        return datetime.date.today()
