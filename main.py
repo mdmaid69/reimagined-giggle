2ea9506c67dc28aba81375b640e68d0a65ac4f1b
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:56:26 +0000

    demand tap wash preparation hard

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import getpass
+def get_username():
+        return getpass.getuser()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
