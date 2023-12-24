83f23e96a6eb3941c7ebd1f8db6aef41b575c4c1
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:02:31 +0000

    ashamed around pain escape equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
