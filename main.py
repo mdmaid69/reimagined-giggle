77b5b490b9d066392d4f446a870aa59aeae1539e
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:42:59 +0000

    slowly expression extraordinary fact split

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
