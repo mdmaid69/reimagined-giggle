e56d2675c44cc9e450823630c216d84b2061a484
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:50:35 +0000

    speech deliberate to sense season

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
