3fbbf8cc6bcb3f303c1a41e12aa2859cc75ce56a
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:31:58 +0000

    agree agree fast stay runinto

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
