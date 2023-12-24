0cf9c07e333dff3d2e2b6207a400f503f10137b7
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:36:53 +0000

    hope first circle afternoon over

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
