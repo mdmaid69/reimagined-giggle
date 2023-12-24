7a5d5aea17345e3dfec409bb69dfab6f140008e4
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:42:35 +0000

    make work clever include promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
