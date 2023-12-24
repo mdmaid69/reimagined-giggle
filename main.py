22c0a933699e6e1aa9eca97a9e0ed4765e15fb98
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:42:30 +0000

    escape comeback level murder future

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
