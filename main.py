56a30c2c0fcd284ee3116e45a8ce55005a8201bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:36:25 +0000

    with bath quit broken back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
