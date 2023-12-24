6c38d650e27977665abafe0adfa2121c716ccaab
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:04:56 +0000

    bush contribute type ocean mine

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def find_min(numbers):
-        return min(numbers)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
