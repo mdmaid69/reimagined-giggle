a35d2f206f910e27ff35843ccda938e764f2c9a6
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:56:18 +0000

    miss motion stop reduction load

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_acceleration(speed, time):
+        return speed / time
