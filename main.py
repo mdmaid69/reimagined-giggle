53804bc8c5b75f459fd3f07deb4ced6c8000ba4e
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:30:25 +0000

    tired outside system everyone shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def square_number(x):
+        return x**2
