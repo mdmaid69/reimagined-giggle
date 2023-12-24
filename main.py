574ac304811349f521ee8ded6b95a52616647c43
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:26:18 +0000

    unlucky base rubbish sort direction

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def convert_to_octal(n):
-        return oct(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
