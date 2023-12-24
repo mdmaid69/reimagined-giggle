592ec76e0325b046824a089259b6820d8f39e26a
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:41:47 +0000

    569enjoy mean so during purpose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
