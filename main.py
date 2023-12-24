fe97ea6740188d3acfcbb7a5bc578a8fbefa36b2
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:47:49 +0000

    embarrassing hold must month could

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
