567c9b8e46bd9400716c9d8696ef54af91eeac83
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:03:28 +0000

    up fix imagination final steady

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
