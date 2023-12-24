5587911aa3af8e89d7856876370fd18be7b5d980
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:51:24 +0000

    recognize brother part extra goup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
