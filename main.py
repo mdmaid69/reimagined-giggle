8ad6119cbbf6372e7af614592f0c7975fef40457
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:09:29 +0000

    classroom degree bother so short

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_pressure(force, area):
-        return force / area
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import re
+print(re.match("h.*o", "hello world"))
