c1dd89c9746e903e4946b84e93f7966aaccfaf78
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:08:04 +0000

    to him odd detail swing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import datetime
-print(datetime.datetime.now())
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
