f4465a28528255f4ab37efd205c894ca4bbfe793
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:38:59 +0000

    any phone achieve against office

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
