fa80368dd117f5f985ddb6ce798e34c488203686
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:20:54 +0000

    afternoon group stupid music Zz

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
