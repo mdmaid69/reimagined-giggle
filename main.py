d95a97151f817612113bff130246c84b4d744c65
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:46:01 +0000

    bend who string mean fresh

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import time
-print(time.time())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
