25ba2f8b0be11b49f0ded978584396d551c8ec80
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:54:26 +0000

    weekend thing turnoff point muscle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def convert_to_octal(n):
+        return oct(n)
