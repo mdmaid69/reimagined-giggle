bba9fd95b6669bc33274f6af7530b46f5a42d645
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:00:42 +0000

    calloff study suddenly mark drink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
