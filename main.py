fac578217d13b055fce0f6a2473dcdcccbc0d65f
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:38:09 +0000

    sight mainly unite preparation face

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import time
-print(time.time())
+def find_max(lst):
+        return max(lst)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
