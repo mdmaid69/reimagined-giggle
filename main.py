469604ab5b4c5d95d6c10362881edc752bdec4f9
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:05:56 +0000

    emphasis purpose lift keep hurt

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def convert_to_binary(n):
+        return bin(n)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
