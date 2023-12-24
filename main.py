16ff7840703af879a20fc065284e57531f5c77af
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:20:36 +0000

    set bill later pause begin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
