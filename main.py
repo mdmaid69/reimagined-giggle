f8a504e37a60fcb7702f167bb10e6470fde97656
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:10:40 +0000

    unusual possession regard thin path

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import os
-print(os.getcwd())
-import re
-print(re.match("h.*o", "hello world"))
+def remove_duplicates(lst):
+        return list(set(lst))
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
