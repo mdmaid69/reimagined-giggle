9ed741a1e9dfa60e3060b0bad8481cad7eabb6de
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:13:16 +0000

    lonely across pure fast tone

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
