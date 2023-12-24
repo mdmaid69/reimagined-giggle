0b10ed4b27826cfc105f39e1717be6d7f7a2f257
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:50:33 +0000

    term giveout contact by always

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def clear_array(array):
+        array *= 0
