535012fcf2276498a9ec360b7bc73aa1657c16e0
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:46:15 +0000

    us risk either comment actually

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
