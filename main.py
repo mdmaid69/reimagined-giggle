67267a3bce2ef59d2c0e6511c0ebded7fd369117
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:55:07 +0000

    all watch mess summer further

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
