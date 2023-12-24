2c986a14746cce0d4a402fe2461aee56d1c6ba7b
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:34:18 +0000

    wall warm base broken mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def remove_from_array(array, item):
+        array.remove(item)
