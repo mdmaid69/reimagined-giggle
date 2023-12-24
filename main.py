166c64405d2910de05b433b0430c2c703880cf71
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:17:29 +0000

    care hardly for while direction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def append_to_array(array, item):
+        array.append(item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
