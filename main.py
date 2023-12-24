5dd5b2ffcdc748a9c2dfb8d11ffe89ab84b3eb2b
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:39:37 +0000

    mention suit praise marry little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import collections
+def create_stack():
+        return collections.deque()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
