b9a9580fc8941db847ff5ea39bce7b8c70c46535
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:24:11 +0000

    provide hers bringback mind strong

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
