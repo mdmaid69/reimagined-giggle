4cde798f4275183b78f38dd35e15bd18886d4619
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:37:18 +0000

    bend warning trust uncomfortable only

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
