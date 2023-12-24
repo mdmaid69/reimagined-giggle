3ab7cf77ad139c4794987cd6c72284fa2f16a47d
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:21:22 +0000

    dress case artificial excellent secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
