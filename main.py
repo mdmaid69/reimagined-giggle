44b2d2460528e40b68eaca11545f4d98b5891c5f
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:37:35 +0000

    high do recognize proof thinkthrough

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
