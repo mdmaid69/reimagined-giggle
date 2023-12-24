e648ee606f32597a2e00caf35fb9d08b86077182
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:02:04 +0000

    573entertainment emotion book talkinto police

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import logging
+def log_message(message):
+        logging.info(message)
