f56a016442ac2cd454d7e11eed114a3ba6498e84
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:42:02 +0000

    ours spot touch equal yours

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
