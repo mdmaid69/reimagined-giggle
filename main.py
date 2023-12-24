bd5c2fc1fc6ad4751293c358009446e5c08c61d1
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:09:09 +0000

    secret honest Ll arrival office

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
