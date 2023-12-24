5ef4bcc8c304eb34693c8baeb587a048f7474c50
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:50:02 +0000

    eager embarrassed chase sensitive tense

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-print([x**2 for x in range(10)])
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
