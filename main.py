3e4cdb788f0f0d1029a1b3a573f6830b49d60078
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:04:54 +0000

    century wealth aim apparently attention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
