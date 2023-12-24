779b1aad2122a5b76e28eeeea761edeff0d22a09
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:38:08 +0000

    march commit disturbing sort dinner

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
