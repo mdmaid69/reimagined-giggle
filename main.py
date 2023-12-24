425882622032ac19d50fbf8638c564585c3f87f8
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:38:00 +0000

    difference admit project legal official

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
