cdf11e7bd43477348466c701b797dc0f5157993b
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:13:17 +0000

    cross lower sleep part movement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
