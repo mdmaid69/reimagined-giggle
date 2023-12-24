b97b655b44e0cc34ace5c03b54b2ecfb61a7ae00
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:23:34 +0000

    free attention confidence hide search

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def square_number(x):
-        return x**2
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
