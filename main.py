7505a9266eb92310780d8d7cc8d143604b6fdfa0
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:47:13 +0000

    subject be future previous deliberate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_min(lst):
-        return min(lst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
