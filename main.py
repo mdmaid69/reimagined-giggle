78b7d36e81a46016fdafde77772f2128b2114242
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:43:04 +0000

    badly steer round move circumstance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
