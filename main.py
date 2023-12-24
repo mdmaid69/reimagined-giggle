5b8bd1f6f755f1c56538df96d3f69c91834510a6
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:44:02 +0000

    frightened need thirsty bringback adjective

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
