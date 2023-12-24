2803d5c6e826595a10329bcd3325fd551cb1aa59
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:33:53 +0000

    time cross nerve thing radio

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
