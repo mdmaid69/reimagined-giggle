2d252b84d58b47691b955cae5a61a391ebcf7b6b
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:43:43 +0000

    unhappy cheap it priority goout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_current_working_directory():
+        return os.getcwd()
