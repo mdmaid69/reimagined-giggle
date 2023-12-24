2c79d853979b6d1ff171b002fa026f5f184dd689
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:19:45 +0000

    hang quickly afternoon help lookaround

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_work(force, distance):
-        return force * distance
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
