681dc04bcc49f92536544b339422d0ebccc9421c
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:09:31 +0000

    movement shine exciting official beyond

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def extend_array(array, iterable):
+        array.extend(iterable)
