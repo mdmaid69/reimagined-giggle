20b9283182baecdb8bcdfdb181f64d5a3ac3a98e
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:00:46 +0000

    finish complain takeback suppose nerve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
