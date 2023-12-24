27951cd5e47ec637a9889b02d74e9c6a7e996f06
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:08:44 +0000

    target decide report correction same

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
