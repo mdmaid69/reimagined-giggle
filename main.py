72609cb8f23a714f919944f1cda61432d20cbc64
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:53:32 +0000

    smile muscle under honest prove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
