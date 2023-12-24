a86b2b9a6511cb523b97532cc9106412d63ef66d
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:29:21 +0000

    exchange relief whatever promise serve

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
