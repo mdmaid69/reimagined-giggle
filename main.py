e67652fdfd5145aee4f49c5801a0d559ebb1d377
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:01:56 +0000

    majority abroad congratulate bunch determined

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def convert_to_hex(n):
+        return hex(n)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
