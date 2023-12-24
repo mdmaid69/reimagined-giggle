35838a26b8b2d110cb23513e9800aef62eb7c479
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:46:48 +0000

    perform respect bringforward beyond as

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-print(math.pi)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def convert_to_hex(n):
+        return hex(n)
