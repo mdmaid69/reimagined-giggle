76154fa5d605edfc8bad8ad6b66fcec47034cadc
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:55:33 +0000

    pot tidy many swimming do

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_max(numbers):
+        return max(numbers)
