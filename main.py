129de0c6dbed0a99f4e5fbd5cb160921dedd1b5d
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:59:00 +0000

    meanwhile trade the direct instrument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
