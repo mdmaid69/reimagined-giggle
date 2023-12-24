7dcadd1d55f6b0ab0f2cf4f832e371b1641b6a27
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:57:40 +0000

    confuse phone disgusting later like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def sort_list(lst):
+        return sorted(lst)
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
