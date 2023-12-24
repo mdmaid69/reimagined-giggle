a76e18ee106cf60850dbafbaeb1c4fcec6947855
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:24:00 +0000

    move dear less willing negative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def reverse_array(array):
+        array.reverse()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
