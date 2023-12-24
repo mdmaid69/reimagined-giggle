27a4cc492df469857e623aa290cc5e4969495686
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:36:24 +0000

    plan about fork sister recent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
