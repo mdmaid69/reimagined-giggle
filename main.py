a84f18c31134498e30de6be80162f57bc836714c
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:30:07 +0000

    ground different figure month question

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
