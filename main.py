642ead733c35afc2bc8481f7144f69e0f4029b75
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:52:22 +0000

    community brain me tell discuss

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
