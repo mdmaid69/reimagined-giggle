e330a37dbc0df88425b08a5b133b948561260f86
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:45:34 +0000

    limit tool getoff coffee brown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def greet(name):
+        print(f"Hello, {name}!")
