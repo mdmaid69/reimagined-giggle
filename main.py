581a0abb0a84b79e2d7d734c2f92ac709249cc01
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:53:07 +0000

    action hurt confidence treat generally

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
