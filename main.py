818d3d75fe4e76761be91d9ea6f433ea46245bad
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:26:28 +0000

    quickly vote father quality awake

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
