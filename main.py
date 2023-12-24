4599e935009579fa7474ccba2a9b8c1231f5e2e7
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:15:35 +0000

    illness store pause clean attitude

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_pressure(force, area):
+        return force / area
