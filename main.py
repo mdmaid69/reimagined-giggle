aef0acd9ed26c2c2f685d9d1d8cecacb09e3061a
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:29:26 +0000

    coffee me trap prevent label

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
