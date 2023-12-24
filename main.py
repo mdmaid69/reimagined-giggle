784b1128fd1b3927e929902859c59ec2d8471f79
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:14:27 +0000

    later allow either crowded scale

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-for i in range(5):
-        print(i)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
