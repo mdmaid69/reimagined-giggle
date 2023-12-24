8b9bebe71482246d96b2adb243acb16b266891c5
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:35:24 +0000

    detail comefrom though cinema sky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def get_array_index(array, item):
-        return array.index(item)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
