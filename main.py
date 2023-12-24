bd01beb0d3199001cf0dd1cb558235cc56e19c8a
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:49:51 +0000

    judge straight front offend everything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
