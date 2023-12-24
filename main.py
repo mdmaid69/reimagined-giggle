b96cca7f52f29471b4071a456baca604f4561d43
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:18:44 +0000

    giveout address bag attitude roll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_work(force, distance):
+        return force * distance
