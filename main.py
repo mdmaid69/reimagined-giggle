be08b09fbe616bd3064773cb08947c133081ddb6
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:22:27 +0000

    turnoff practice pause break solve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  def is_even(n):
-        return n % 2 == 0
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
