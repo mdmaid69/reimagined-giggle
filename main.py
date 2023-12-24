ff4da52686d959596ec22879866fcfdb4684690c
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:23:54 +0000

    spin old country contents promise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  def reverse_list(lst):
-        return lst[::-1]
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
