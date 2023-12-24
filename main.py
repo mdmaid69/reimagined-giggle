7130a5040c394331e81d0e4b7a5cdb219e1c1853
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:42:08 +0000

    burn soil mixture treatment remain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def reverse_list(lst):
+        return lst[::-1]
