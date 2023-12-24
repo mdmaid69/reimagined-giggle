cdb22deadbf4a747589037987c51ebd78532fccb
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:08:36 +0000

    spring else success pattern emphasis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
