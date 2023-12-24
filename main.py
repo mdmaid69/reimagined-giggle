a3636ffe5b7889c26483b6cac05958c9bec5a6dc
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:11:27 +0000

    university whole else our average

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_area_circle(r):
+        return 3.14 * r**2
