c087262dc46d4dfdc9a4d4bc37326e0075691147
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:26:43 +0000

    approve coffee to shine bye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
