7592a2f8c041194c88031002797e5c039d7cab4b
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:25:22 +0000

    hate successful stranger shame song

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def sort_list(lst):
+        return sorted(lst)
