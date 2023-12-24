ecaa7496413130431c8a4f2426a33cd59b8f633d
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:26:41 +0000

    church cook health actual until

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import platform
+def get_python_version():
+        return platform.python_version()
