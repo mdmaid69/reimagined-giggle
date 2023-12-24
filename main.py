cf61c4239a076d9f3ce5929da65ed94527ea9b1e
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:51:11 +0000

    satisfactory almost cause reasonable bound

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
