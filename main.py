d0db82e689e3d4667fc1755ce1e84b2f2c049ded
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:10:55 +0000

    straight need beat thanks lock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
