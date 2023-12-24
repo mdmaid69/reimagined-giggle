d4ff1d9cc3f5f998e4de0857e70f44f01fb52cac
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:56:44 +0000

    she study funny quantity getaway

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print("Hello, world!")
