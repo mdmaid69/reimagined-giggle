1659347324f38a87ae0a1d151589aac79fc8fe76
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:54:21 +0000

    ride guide ignore book noise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_area_rectangle(l, w):
+        return l * w
