1164954e35e9a17010caaf3e8e12d9f7a22cc58c
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:15:20 +0000

    close behind play roof entry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
