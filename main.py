68a94ae7258b28db887d124b5e8018895a12be2c
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:04:22 +0000

    now either bright confused city

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
