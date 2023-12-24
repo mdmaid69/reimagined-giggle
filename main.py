fb86c981d734b002fe16d2cbccdb08b7ae19828d
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:57:40 +0000

    let match no pretty part

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+print("Hello, world!")
