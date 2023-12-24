45a6fb6cc5448af2ebfe33d3d3bbe644d884cae7
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:48:47 +0000

    doctor wonder luck cool advice

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
