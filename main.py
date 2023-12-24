6d95f4bb6e861e6b58c04adc449c7ce70c94f44e
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:55:27 +0000

    callout treat sense week not

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def multiply_numbers(x, y):
+        return x * y
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
