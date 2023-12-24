4d2950e5ddac13ee6de94660ff2759630cd18815
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:25:28 +0000

    memory make ago pinch work

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_area_rectangle(l, w):
-        return l * w
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_work(force, distance):
+        return force * distance
