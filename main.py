01c2804f7b4db1a8ff69e5cb263625993fc6faeb
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:24:48 +0000

    lock vote work its quit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
