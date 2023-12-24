e6c0fb7ca2504014dfe0857af71da4ef99a05dfb
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:53:32 +0000

    sentence easy intend issue art

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
