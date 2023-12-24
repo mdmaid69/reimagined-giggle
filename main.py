1c3ccd346e4eeca0fa54fa8a6205498b55bdd217
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:38:15 +0000

    point identify boring plenty very

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import collections
+def create_user_list():
+        return collections.UserList()
+def sort_list(lst):
+        return sorted(lst)
