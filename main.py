d198d5b5de44c545eec5dc6196cbf5eb660f7ff6
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:38:19 +0000

    trip zero material lookaround breakfast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
