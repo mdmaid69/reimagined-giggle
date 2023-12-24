ec3071d2fef136b57b67bc4873bb8bf974cda026
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:42:28 +0000

    possible travel appeal expensive succeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
