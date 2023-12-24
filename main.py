5c191c3bacb08e0572c0b60976ad0decd1f21a8f
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:13:52 +0000

    how system yesterday closed fat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def get_list_from_array(array):
+        return array.tolist()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
