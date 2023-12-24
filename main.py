a6c22eb0045c7577f8f46b67325ff35d325ae305
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:44:38 +0000

    round rare treatment often apparently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
