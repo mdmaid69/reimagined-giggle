f5eaeeb798766dadff005b4e8279e4861bfc2f1e
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:59:28 +0000

    tooth tonight praise way Nn

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
