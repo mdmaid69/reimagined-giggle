53469996411321cd0b769a051cb4a5c5709f4761
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:03:39 +0000

    suck continue wind introduce board

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
