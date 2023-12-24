6a699bb9eb4b5b4f263e7e1aea5a3f1f2ad2d00c
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:48:13 +0000

    keepoff entry thought back brain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
