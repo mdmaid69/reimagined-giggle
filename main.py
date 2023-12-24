169c0a4dce6aee9d041136696ced094f78f569a1
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:26:53 +0000

    sorry breathe thick like deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
