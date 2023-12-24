631e5759a85793451902fe7c09953288b4c0e696
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:40:41 +0000

    disagree neck injury lot naturally

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def is_odd(n):
+        return n % 2 != 0
+print([x**2 for x in range(10)])
