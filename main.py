118adabac915ca694d387521fb9c05ad272d4f91
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:44:41 +0000

    previous condition commit there favour

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
