9aa1edb6b4a2c07f485365fbe15514e26f623572
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:50:38 +0000

    unlikely second treat band reach

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
