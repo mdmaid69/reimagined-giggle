5fcb09203663891cd6f09b549a8ee9b0205c2b80
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:00:11 +0000

    order quarter muscle fall unlikely

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
