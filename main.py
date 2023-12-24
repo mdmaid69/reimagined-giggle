5142a0b32fe0f6d06f681b57ff705112525a9daa
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:38:19 +0000

    listen another black down sometimes

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-print("Hello, world!")
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
