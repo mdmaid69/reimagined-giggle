5b9b8962cb61668a5b3f108093f4ceee98442986
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:26:08 +0000

    talkoutof unkind cry anybody send

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
