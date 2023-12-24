d6ef90b119fbdb292b18d71240d63a53bda1e79f
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:57:12 +0000

    describe recent physical read against

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
