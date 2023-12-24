8bdbcfcf2562fa5158d472fa660998d75dc1c6dc
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:04:19 +0000

    turnback trick brush release head

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
