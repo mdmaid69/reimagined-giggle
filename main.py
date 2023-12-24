59c572be792a96feba9c07bf0a91abfe9877801d
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:40:40 +0000

    Ss good abroad share recently

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_area_rectangle(l, w):
-        return l * w
+  def convert_to_hex(n):
+        return hex(n)
+def multiply_numbers(x, y):
+        return x * y
