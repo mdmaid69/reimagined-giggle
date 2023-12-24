24cee610cd04048c2da987a98df8d75bae426969
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:52:49 +0000

    tragedy kill disagree morning scale

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
