cc5bd480273615f7c19e3189660e8d828b35d5c3
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:23:46 +0000

    neck drop Oo come once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
