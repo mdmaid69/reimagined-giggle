a69e5b0f5742e1484dfb0dc0575f5334f32a7a4e
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:15:59 +0000

    fast smile compare cut advise

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_power(work, time):
-        return work / time
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
