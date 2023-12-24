afd7fc3f0445a53965bce19e0ee8959c62011234
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:42:22 +0000

    evening opponent doubt permission murder

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
