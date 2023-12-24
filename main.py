5898bb43da2b36572fb448a8e26a5368076509ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:57:53 +0000

    second butter left flavour pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import random
+def roll_die():
+        return random.randint(1, 6)
