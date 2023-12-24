f3af355a659d674f22f63cac89410cad89df3b72
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:34:48 +0000

    track actual anxious plenty bit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
