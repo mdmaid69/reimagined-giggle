01e05e22795bbd1d62d05cb94af50ab003657053
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:30:55 +0000

    under excuse pen impressive wind

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_user_list():
+        return collections.UserList()
