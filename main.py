e0783dad79ab0cb046b0642c0b3e77191b455129
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:24:08 +0000

    instrument wear modern become wrong

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  def convert_to_hex(n):
-        return hex(n)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
