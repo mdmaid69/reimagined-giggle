534013cc6b6b90cf3a0144de91605c4e0be8d84f
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:16:34 +0000

    independent figure violent ready dig

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
