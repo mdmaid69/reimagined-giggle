d9f78320931317f31c3f4b90bd52b8950204586b
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:53:05 +0000

    so official needle silent quick

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
