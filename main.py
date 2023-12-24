ca3346dbc09f209988298a94ad4f13f0ac7573d6
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:49:35 +0000

    upwards imagination pipe stare Jj

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
