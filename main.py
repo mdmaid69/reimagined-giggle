2cba5e6a8ed8e93348d2a4e14f9d47bd38b16c11
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:59:49 +0000

    have deserve 569enjoy sense expensive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
