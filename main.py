9c003ebc1d841890ed455fca234fd0c037dc4a4a
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:07:09 +0000

    breakout good exercise contribute shoulder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
