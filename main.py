9ab4150beb39a36e6c5239d24278d8657d3ae72c
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:32:13 +0000

    from tape getup 179bite Yy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
