deecaab326b660293c28fa1e87c8bb5540650618
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:26:19 +0000

    sight trick 571enough combination attempt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def count_characters(sentence):
+        return len(sentence)
