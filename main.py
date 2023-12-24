11e11288505524b5946e4c52722c4afae068af0e
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:37:36 +0000

    profit nonsense smile achieve any

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
