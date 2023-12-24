e5acb779839bac37296276440b700181a4f4c608
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:34:11 +0000

    seriously final aim degree cheque

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
