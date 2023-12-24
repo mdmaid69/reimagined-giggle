c23e67ffac21e0fe7ecb9a4ff0f2d18b56a383a5
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:47:35 +0000

    mystery downstairs matter second upstairs

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def sort_numbers(numbers):
+        return sorted(numbers)
