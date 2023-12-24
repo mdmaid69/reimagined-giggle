fab8d4a7ef51c2db4d48921a01933329da18004b
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:22:55 +0000

    fine beneath rough pattern rough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import datetime
+print(datetime.datetime.now())
