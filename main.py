17fa85ad8c8eb0ff5a10353b46572c047837645f
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:01:07 +0000

    stir admire beside art lot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
