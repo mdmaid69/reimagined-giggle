38dc321c56285362bd11c29653b4a4f2ec36e5c1
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:29:18 +0000

    sweet flight use improve case

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
