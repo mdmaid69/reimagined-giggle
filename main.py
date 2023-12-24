21a9beeaaf83bb27c87140c3eb44513cb779fba1
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:10:51 +0000

    cost impossible show shout summer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
