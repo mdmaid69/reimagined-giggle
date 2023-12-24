67776870edcb6683a64e5df97239ebba1fc7f9a2
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:55:45 +0000

    book carry win agree spare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
