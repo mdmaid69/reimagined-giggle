3c9d7ad5713a46ca2cdca9e96f2e7d3652d638a8
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:14:32 +0000

    talkoutof nervous danger intelligent rest

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
