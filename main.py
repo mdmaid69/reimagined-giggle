7af76a2bd8485b05e96a2ae04348698f838bee7e
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:28:18 +0000

    murder plain injury takeaway regional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
