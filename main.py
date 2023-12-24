04745b16960316846f0c0f3b3377d1a95c625ea3
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:09:19 +0000

    payment above principle heavy disappear

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
