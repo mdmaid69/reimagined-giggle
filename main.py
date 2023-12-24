ae35dd5f7bd0e850bf38026ae2488b2ca7bbe9de
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:44:59 +0000

    Ll save throughout telephone choose

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def count_characters(sentence):
+        return len(sentence)
+for i in range(10): print(i)
