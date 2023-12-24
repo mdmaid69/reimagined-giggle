fd45de244bc896ed3fdf667f42e7866b05fd6f73
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:18:58 +0000

    excuse draw especially strict surprised

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
