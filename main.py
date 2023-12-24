68d1dad329e757736ad12bf3142bf09286c2a690
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:15:00 +0000

    away mark news valuable lot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
