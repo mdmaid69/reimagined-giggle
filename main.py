082da115e0c8550c058fabf784f273ba20493c38
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:08:39 +0000

    nearly head cry by circumstance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
