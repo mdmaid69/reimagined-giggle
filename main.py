1adf404a46706116b8229b21c092001d3fd4dea5
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:54:58 +0000

    translate detail rest main various

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
