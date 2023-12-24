61392a68f190ee09aca1f1b62b27a3000e331242
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:15:10 +0000

    occur feeling head confusing regular

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
