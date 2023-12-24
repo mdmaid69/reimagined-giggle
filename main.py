5383f8ccddfadfb79a1eb597a3af98f12f55da9a
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:53:45 +0000

    correct separate never pocket wash

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
