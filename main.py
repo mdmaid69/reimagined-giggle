3eae825c3c842904d03e2c11c5b51496077cdcce
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:18:45 +0000

    tip muscle precise illness interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  def subtract_numbers(x, y):
+        return x - y
