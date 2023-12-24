38b46803b4133d1f779d904b27fae8f091395cc3
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:46:37 +0000

    lonely steady hope display its

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
