faed174577da7459fcdfcbdb2cba02b068620f41
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:46:47 +0000

    process smell pencil respect how

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
