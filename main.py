b922cbbe0d289e1a7ce0768cde1a0163476ad61e
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:57:59 +0000

    waste half complete new furniture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def add_numbers(a, b):
+        return a + b
