aa58a8e99868d4f91b69ec20f8ef71ad6ca41ad7
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:00:12 +0000

    single win doctor plain seat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def reverse_list(lst):
+        return lst[::-1]
