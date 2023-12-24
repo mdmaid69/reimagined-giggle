5995986acd0d3327f9288c2b61559f21c40156e2
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:34:56 +0000

    result exit takeover table false

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def convert_to_octal(n):
-        return oct(n)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
