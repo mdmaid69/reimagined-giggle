8d287dd213d60d698a5d546c91fa0df4d612ce1a
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:10:09 +0000

    unlucky brief classroom succeed eat

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
