76720adecfa1d9af0c414f86ddcd41aaf9976398
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:15:01 +0000

    tip hollow he never swing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
