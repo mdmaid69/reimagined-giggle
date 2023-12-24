2f69c45d661c9d1f18e39383faae53f7a321fc72
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:19:58 +0000

    guilty here interview keep walk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
