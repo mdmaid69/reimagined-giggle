ad0e3c7efa0a5e96491df557fbf2725ec9ae9213
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:32:39 +0000

    our run cross feel cut

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
