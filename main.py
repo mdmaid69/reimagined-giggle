58f1c3ce78b9d48cdd32da067c73e8493bcf9b41
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:51:34 +0000

    write fashion exciting uncomfortable skill

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
