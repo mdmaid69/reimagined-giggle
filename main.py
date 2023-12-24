7eacf28f35a66ae94c5e2e99c02361754a42b638
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:22:16 +0000

    plenty children knock reject bill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import array
+def get_array_as_str(array):
+        return str(array)
