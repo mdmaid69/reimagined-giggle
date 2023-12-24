14d84fb70adea0829a8a933ba9a5a152c21cf75d
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:41:51 +0000

    miss settle describe couple long

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
