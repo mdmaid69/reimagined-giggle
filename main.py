17279f405ae39e1dd2942680ba18c00e60a5cf28
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:33:55 +0000

    deep attempt seat tie bite

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import datetime
+print(datetime.datetime.now())
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
