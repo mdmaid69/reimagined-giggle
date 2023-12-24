8ca204369a41aedcc9f45afc6315cbef9979655e
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:01:08 +0000

    final worry crazy description judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
