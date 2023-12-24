3214eb489d6d9c4270817a85ea89c4bda31d63d3
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:27:14 +0000

    pretty rare deep where aim

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def cube_number(x):
-        return x**3
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+print(sys.version)
