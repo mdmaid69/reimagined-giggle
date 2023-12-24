c4eb36b14714e8635abdfcc619585eb562e9e804
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:30:06 +0000

    basis first spring marry combination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
