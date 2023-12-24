e6cf161430bd18b2f6054c11e10b1204dfc7d573
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:47:16 +0000

    thumb comein state little knock

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def square_number(x):
-        return x**2
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_acceleration(speed, time):
+        return speed / time
