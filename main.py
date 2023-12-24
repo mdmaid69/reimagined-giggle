ab51aeeee04f0af0903197141bc7470ee4bfec4e
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:03:58 +0000

    main term wash prepare process

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
