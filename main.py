d8f97ce07c97d2bc847c9d6420585c0af2d26616
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:24:58 +0000

    opposite search reserve safe performance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
