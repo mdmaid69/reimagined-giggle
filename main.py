3807fb3aef4e11555bcf289e7898c0e5b515cf89
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:34:25 +0000

    read entry even entry come

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
