38f6b29a7eebc7d3ec111338a2d25b8c6d502b38
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:50:31 +0000

    finger wish no negative young

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
