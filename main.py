a2d1b521f2db5a54feea7fcfcb822aad47085566
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:45:13 +0000

    length scrape organize cook shock

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def find_min(numbers):
-        return min(numbers)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
