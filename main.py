a6bad87911306d185c021e2c2f6d793fef3e19eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:29:48 +0000

    fear recommend activity past clear

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def count_characters(sentence):
+        return len(sentence)
+  def is_odd(n):
+        return n % 2 != 0
