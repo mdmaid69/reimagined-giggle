d419cb97ebcbac00f69e61bc32e243f9e59df397
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:11:26 +0000

    ilovedicks head floor Kk sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
