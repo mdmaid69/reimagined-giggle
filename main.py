964c83de52976e49c8f1470aa2c2a68e3d5ac938
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:03:31 +0000

    centimetre book bank behaviour fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
