95a54af0c62389df7613ac1139f055af23e373a3
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:33:44 +0000

    payment cope inside behave mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
