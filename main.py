c25671c6d8022939429f038a1cbe76a1969ddf00
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:52:16 +0000

    false call treat chance threaten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print(sum(range(10)))
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
