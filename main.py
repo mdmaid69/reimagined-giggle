9f729c54dc82125af914267b4c93688cc01709a2
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:11:22 +0000

    no choose sign walk perfect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+import sys
+def print_python_version():
+        print(sys.version)
