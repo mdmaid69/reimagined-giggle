ea6215e5212db8c6d5404d46f0b65dcde234dfcf
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:32:10 +0000

    lead Kk rescue branch spill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
