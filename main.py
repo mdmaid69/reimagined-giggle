ab7223911df0d818c4e3ab9b80a7608fc4658ce1
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:05:20 +0000

    dry different respect market paper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_acceleration(speed, time):
-        return speed / time
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import sys
+def print_python_version():
+        return sys.version
