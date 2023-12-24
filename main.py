c757fe4138ef2a83494b7f172bb0a6a9d4771101
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:07:15 +0000

    worry second dream film announce

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_exponential(x):
+        return math.exp(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
