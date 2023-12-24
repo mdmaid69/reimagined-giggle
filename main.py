bed70f119fa9bc5ba2357af91443f0b2fba0ab2d
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:42:48 +0000

    grand call crash chat fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
