8ad92e543005239835559a09c687ebdad9542bae
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:13:45 +0000

    mess future cover strike pattern

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
