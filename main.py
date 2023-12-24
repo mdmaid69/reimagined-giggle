71572628905ac382ec9ccaafd33cf5bbec444b2d
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:41:14 +0000

    flow thought become surprising throughout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
