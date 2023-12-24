cba9dc5384ee0908926217dfcaa643507cb87c54
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:16:19 +0000

    goout immediate support pocket arrange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
