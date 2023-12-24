c95645ac5d479e1dc1f5c4af5b21dfc66601a387
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:29:13 +0000

    lower land profit not giveback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
