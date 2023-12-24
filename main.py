2869e13d24741b5037c8e7a5106796e71ed5c02d
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:50:46 +0000

    interested above runout rule cold

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
-text = "Hello, world!"
-print("Characters:", len(text))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
