7b527e820f866bef41fe4a1b5a4291d4dcf942e2
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:07:34 +0000

    want account forward nervous own

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
