37161cc758771843da82f912d3e1dcea7818d946
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:05:23 +0000

    solve style niggaacademy woman free

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
