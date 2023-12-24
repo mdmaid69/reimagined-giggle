c2fe7722cf77ee04dedb3ac9e6df1865667f8f20
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:19:06 +0000

    uncomfortable sorry aim thinkthrough children

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def convert_to_binary(n):
+        return bin(n)
