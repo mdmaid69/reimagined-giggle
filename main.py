205cf4e377181de0c1a19550c55b4d5419db53df
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:01:37 +0000

    rub could disgusting pen persuade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
