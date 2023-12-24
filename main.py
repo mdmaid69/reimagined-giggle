be40e20b4b4bc0645a7df4eabf7bf92d2b832fc9
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:38:47 +0000

    let very interruption sign tradition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Uppercase:", text.upper())
