6e9e0f20708fa873f4166158b59856ece02c6313
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:53:28 +0000

    bottom blame everywhere criticism work

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
