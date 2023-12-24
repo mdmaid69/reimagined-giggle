a6d9402b9b7ce47d730b429b8e45ed3349fc3e3f
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:08:48 +0000

    along cover freedom pipe work

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
