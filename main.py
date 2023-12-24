5e7d57dc438b9ccd7bbe0a6295299d510291a9f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:59:49 +0000

    place underneath appear passenger exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
