37bf21c90372f0f1727fc3b055c1cb6983d1cf21
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:15:14 +0000

    spot foreign behind fill town

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+for i in range(10): print(i)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def convert_array_to_bytes(array):
+        return array.tobytes()
