8c9d996e6269a3bec2efdde401ec4fd7a762269b
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:38:05 +0000

    account alone commit properly fall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
