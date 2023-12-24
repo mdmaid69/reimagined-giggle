b74be6bdb435396ffd9bc4bb647474f60808d9c2
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:16:45 +0000

    cheek question knock aim judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def convert_to_hex(n):
+        return hex(n)
