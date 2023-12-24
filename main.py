2198870f3e06b93df0bcb293272fab9e4b0c2235
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:29:49 +0000

    party team mark sleep world

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
