3ae30df4b7901461154f6821515aa633dc751585
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:36:32 +0000

    yet switch lose belong up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def remove_duplicates(lst):
+        return list(set(lst))
