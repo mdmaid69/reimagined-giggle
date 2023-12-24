66c78388834a6cf4d9efc702d5841e115594c3a6
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:47:50 +0000

    though all our dress shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
