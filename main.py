de9d71f525d752c7fc694dbe7ff9fdf8d957604b
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:03:44 +0000

    available blow determined mess another

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
