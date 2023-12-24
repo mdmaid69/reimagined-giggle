2434edf1eadaaf0a9c3083a31352236bbcae2d4e
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:08:01 +0000

    march little imagination smile Dd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
