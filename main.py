75f5cbc1fb0073cd3af8e918699594b4c249c3c4
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:13:39 +0000

    whistle split method window cause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import math
+print(math.pi)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
