fac356d3667c89d09be66747563d675f55b09bb0
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:54:41 +0000

    interfere point emphasize upstairs crash

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
