bfd3733049cf57c04c28b94884bdd90761de7ecb
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:14:57 +0000

    design form book spin Ll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
