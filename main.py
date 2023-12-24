7811cfe2c5161a1f7b56f8eb995d891f4ba252c5
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:48:54 +0000

    talkoutof downstairs custom mean admit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def subtract_numbers(x, y):
+        return x - y
