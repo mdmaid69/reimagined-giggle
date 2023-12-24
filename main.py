a2df46dd1e824aad4454a73ae386eb99ba6e7572
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:51:48 +0000

    world characteristic ticket lead lesson

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
