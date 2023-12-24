0e94f124c8d72d16fa6c70a95ed46732dd97c580
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:01:34 +0000

    blame nigga lend view Oo

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_acceleration(speed, time):
+        return speed / time
