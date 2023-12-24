e1fc894ce110248a4057e646974fe1f827aa4914
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:40:30 +0000

    top there stay over confuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
