dba22053662e49355ceaa3c3e7bca455fd8f6314
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:39:19 +0000

    section wonder step whistle plain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_perpetuity(payment, rate):
+        return payment / rate
