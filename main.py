77ffb8eb79382ff31aca94be9c03f44fa64e706f
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:28:27 +0000

    taste open ought look spot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import getpass
-def get_username():
-        return getpass.getuser()
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
