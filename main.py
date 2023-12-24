94d8035cd5cf8f541ed70f91cf30d979ae850f17
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:11:06 +0000

    score country idea sticky slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import sys
+def exit_program():
+        sys.exit()
