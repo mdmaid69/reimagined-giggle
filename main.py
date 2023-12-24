41bfe15248489b7b0f14f441ad9a068916253f40
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:01:37 +0000

    holdout always luck sensible half

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
