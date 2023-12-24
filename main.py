587b74a31ede49e1a75eb3ba9c1e81a90d0d75a8
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:55:41 +0000

    period equipment guess guilty disgusting

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def reverse_list(lst):
+        return lst[::-1]
