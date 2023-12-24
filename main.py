ad007765ff5841b99356997d186c1c2971215620
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:38:33 +0000

    which brain pay order row

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_pressure(force, area):
+        return force / area
