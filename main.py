8e66abb6adf80a94b8011ef00ca2705f9a5a34cf
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:40:03 +0000

    pencil private attention false coat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
