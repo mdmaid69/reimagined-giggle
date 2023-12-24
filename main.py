821ee34047bef3cb36a2226c9e06a2612a4f7cb1
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:17:50 +0000

    impossible performance thanks return takeover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
