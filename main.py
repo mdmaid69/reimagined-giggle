fa349acb236650a78a6055ba4fa0eb7323b85578
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:36:34 +0000

    scrap injury any train failure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_density(mass, volume):
+        return mass / volume
