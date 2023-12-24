e4c681da8202431a475a762d386c30966829074d
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:23:14 +0000

    clue call Qq but together

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
