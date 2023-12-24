5a837284822d95e6c3219db4ba7d6d9bc0665caf
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:59:56 +0000

    round check content Zz war

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def is_even(n):
-        return n % 2 == 0
-  def calculate_area_rectangle(l, w):
-        return l * w
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
