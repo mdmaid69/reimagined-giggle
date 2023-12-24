fb5138de49a12eade587252471578940a9d4f56b
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:21:53 +0000

    table move offend runinto violent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
