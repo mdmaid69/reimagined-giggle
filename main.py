235bff272061a853d72dd9b0f4ce2c477196b497
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:58:05 +0000

    trip trade break move issue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
