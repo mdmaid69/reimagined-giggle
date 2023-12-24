0b5bfa01ed2446c602db1dc086252f47fd83a10a
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:55:24 +0000

    university usual freedom everybody anxious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def add_numbers(x, y):
-        return x + y
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def divide_numbers(x, y):
+        return x / y
