e7fc2505b8a1dec3c1a22398b5d6dac6a49f6888
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:20:46 +0000

    refuse during neat brush train

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
