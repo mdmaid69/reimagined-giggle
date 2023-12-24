74366302909f0bb68c9b8fab5d08ac976280618a
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:53:38 +0000

    rubbish direct workout battery autumn

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
