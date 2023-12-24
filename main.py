9319660283a1f045503b40dc94a30f2d7fef0d56
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:42:46 +0000

    unit no share favour impatient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import time
+def get_current_time():
+        return time.time()
+import array
+def get_array_as_repr(array):
+        return repr(array)
