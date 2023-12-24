dc62c583449fe214fbac52acb8995e1332dcd16e
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:09:23 +0000

    unite cool pleasure bunch arrive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
