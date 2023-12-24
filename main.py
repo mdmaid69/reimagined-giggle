1b26e423a61691c96908ccf930302d1815f20ccb
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:27:00 +0000

    extremely straight new gentle post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
