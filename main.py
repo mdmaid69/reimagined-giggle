d67308d4aa7bb2a84f853f50c5f0bda56d590cb1
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:01:28 +0000

    winter naturally separate good tend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def count_characters(sentence):
+        return len(sentence)
