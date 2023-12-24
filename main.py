c9e9e5b50d1d361f80f40d7db989415c66f80958
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:58:26 +0000

    fly appeal shall burn objection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import numpy as np
+print(np.array([1, 2, 3]))
