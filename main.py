ba796d0eaeb50b8f3c113093485891ee906c5337
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:44:24 +0000

    sometimes original morning trip my

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
