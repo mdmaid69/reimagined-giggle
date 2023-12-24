6106ec7711d6deaa3baad5dcda4e5646f43f41bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:59:55 +0000

    runinto advanced wash respect confidence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def calculate_density(mass, volume):
+        return mass / volume
