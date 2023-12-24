42ab60ebbe3480876ac709c7c64c66bd995a4810
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:59:13 +0000

    earth half chin onto trap

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def is_even(n):
+        return n % 2 == 0
