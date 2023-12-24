a97aac02c2ba08f46b72fab0a99814b9fa9a1aff
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:39:30 +0000

    join pupil loan admit chief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
