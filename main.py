8a7af0e850132d450ba0a61193572575726cb303
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:16:04 +0000

    nose tired straight upset conduct

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import collections
+def create_stack():
+        return collections.deque()
+print(sum(range(10)))
