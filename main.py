0bcfb43cf580a91a924ec86c8a361ff22e83b4ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:16:04 +0000

    will cold create base concerned

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
