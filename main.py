c3554e10760ef9d983b3e9e145c4e5eecf71106f
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:05:49 +0000

    thinkover influence nowhere spoil result

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_typecode(array):
-        return array.typecode
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
