5bdf1d603647891365885e12ac5b27c4cac9399a
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:24:46 +0000

    coat approve sound habit stranger

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_typecode(array):
+        return array.typecode
