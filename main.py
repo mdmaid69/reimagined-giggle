b3579a28e62de8e1a6f64b0db9089c354b99fcab
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:13:38 +0000

    hold bottom holiday back once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def count_elements(lst):
-        return len(lst)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
