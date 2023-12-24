b5bce4d74174ea475178e4b0599ba2225538c28b
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:04:38 +0000

    border coffee their shout will

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
