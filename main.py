c7819edbe11cf0bffe9d91f94a4fa278f7a61e3e
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:55:46 +0000

    feel hurry quality arrive search

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
