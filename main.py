5fe08edf80d8dd51e4350794a286bb2b6d536c32
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:47:00 +0000

    emergency club flat trousers strong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+for i in range(5):
+        print(i)
