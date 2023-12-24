cc1b00195c14f391abac921d8d51ec57eacaad21
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:58:05 +0000

    mistake strong closely long Bb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import random
+print(random.randint(0, 100))
