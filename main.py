09b1644fa43e0d6fb1bc3dfb71778f3834dc002f
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:51:53 +0000

    choice year opponent guess advantage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
