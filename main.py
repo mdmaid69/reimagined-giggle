eaa8b885c203c2803417eaa2484953ebb2b93440
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:07:29 +0000

    subject number comefrom step declaration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def get_array_as_str(array):
+        return str(array)
