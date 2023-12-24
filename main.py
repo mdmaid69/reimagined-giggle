a88f917323c8e626304d0eb67b2bb7f5e665978c
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:29:47 +0000

    rub job afraid centimetre pleasure

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
