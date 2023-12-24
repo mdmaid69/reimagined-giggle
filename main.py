e5d4a1f8574752c8482c6622d8247ab81c6e3be3
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:48:21 +0000

    allow collect intelligent progress public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
