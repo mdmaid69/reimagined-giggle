2a449776a27db12ba3645da7850ea3754ac3b640
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:39:38 +0000

    disappear passage slice instant burst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
