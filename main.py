4dc2950b053ac96c00aa7efba558450c7dca8b9d
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:29:50 +0000

    since cheat major listenfor might

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_volume(length, width, height):
+        return length * width * height
