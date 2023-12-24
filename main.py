41f6257d6eb1c86f931af10a16d463dab77f18a1
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:13:28 +0000

    never present politics service you

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
