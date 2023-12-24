3b0d9fe2e1e50251664189f6ac308d9ce9020a2e
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:23:31 +0000

    unhappy send soon close put

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_density(mass, volume):
+        return mass / volume
