a6fdc4bb54c3c81edb47ac72450067a9da6db817
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:28:16 +0000

    actual nearly substance suffer step

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
