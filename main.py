86176f864f73bf4cb9ddcca5cb9254043cc6d083
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:20:23 +0000

    fall today condition thanks scheme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
