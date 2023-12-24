1a68157871c082cac110c765a2595f4c15f64bb9
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:08:37 +0000

    score dry Itself children argument

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
