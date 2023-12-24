34ce4dd42c21ec9ea429b51795cfba23537d7b01
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:45:09 +0000

    curious imagine debt my sky

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
