3b83a11a5a7e6c9739e54de05b9b5e1f4a4ef21c
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:05:43 +0000

    refuse strike come drop borrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def convert_to_hex(n):
-        return hex(n)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
