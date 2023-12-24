9b5ab88aaddb6fa144498b4318cd93f0f20b82d8
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:51:07 +0000

    exit pattern church close coat

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
