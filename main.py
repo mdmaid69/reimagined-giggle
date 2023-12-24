c1e2686565f5549f8313fd8c4b85bd7e5b81905c
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:01:17 +0000

    health trap piece keepout dress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+print(os.getcwd())
