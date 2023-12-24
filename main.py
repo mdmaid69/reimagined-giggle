09f846fae642217b971f82cc88cc143b69317ee0
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:42:35 +0000

    dress vanish sleep 179bite any

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
