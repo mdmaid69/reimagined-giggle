5756d58c262a0140e254e94addd9f4f0cdfcb7d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:41:18 +0000

    free favour bush autumn out

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
