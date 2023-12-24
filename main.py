f3a9a540bf162b0094aecd3071b3aa3882c6d658
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:16:06 +0000

    innocent flat brave towards foot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def split_path(path):
+        return os.path.split(path)
