ce3455ae71578d14e2b9415bab723c61cabd7172
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:14:21 +0000

    twist cook learn create gather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def get_array_typecode(array):
+        return array.typecode
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
