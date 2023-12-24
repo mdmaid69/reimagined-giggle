9082ec6d2e95e026a0f30d057ca26e76806e5181
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:03:10 +0000

    benefit store party move care

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
