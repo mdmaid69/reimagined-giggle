b1dd5bdb19846fa483170b0d9284c173c774f39c
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:07:58 +0000

    laugh gradually near problem round

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def reverse_array(array):
-        array.reverse()
+def extend_array(array, iterable):
+        array.extend(iterable)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
