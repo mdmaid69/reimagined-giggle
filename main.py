78b19b2ef4caa8a0068eee188c88e4095e52a1de
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:03:07 +0000

    belong call ashamed combine part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
