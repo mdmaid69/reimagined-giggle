d9244bfdbfbc5f42f475fb65c269fe8e54934c51
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:02:09 +0000

    if pin her owe view

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
