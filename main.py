1d7152001d8ff7fbef6cf726bb74a9e1a85fcf83
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:47:06 +0000

    there whistle smile corner suggest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def append_to_array(array, item):
+        array.append(item)
+def remove_duplicates(lst):
+        return list(set(lst))
