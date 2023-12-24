6de9003193720217052c74a9d8addeb055f8410b
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:38:23 +0000

    strength hurry arrangement rate write

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
