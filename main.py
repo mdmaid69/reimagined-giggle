7054e9463fdab83bdee2cd632bffd9554a48218c
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:10:19 +0000

    worse history hardly everyone everything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def get_string_from_array(array):
+        return array.tobytes()
