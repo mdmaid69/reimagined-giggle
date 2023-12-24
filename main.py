c35744c3d69302b823e332de71bb2e1de453f38b
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:30:59 +0000

    meeting hate dare raise read

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def cube_number(x):
+        return x**3
