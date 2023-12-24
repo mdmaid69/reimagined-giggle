189fe5b64c5ea9aaeaeb96b00f21ad204848ba19
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:46:42 +0000

    bringback shade create pencil convenient

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
