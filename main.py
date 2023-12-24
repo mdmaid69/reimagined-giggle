bf743cbccc4d20b2351751c019e716d32e5f8e37
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:02:30 +0000

    concerning could dead kind reserve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_acceleration(speed, time):
+        return speed / time
+import datetime
+print(datetime.datetime.now())
