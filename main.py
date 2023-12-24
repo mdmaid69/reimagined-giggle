a40b6b7a0214de0613bf53e2a648bba2c3fde147
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:44:10 +0000

    answer consist related underneath thanks

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
