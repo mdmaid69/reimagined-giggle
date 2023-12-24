e61e8de8eb6f2ea9224a4a0469fc7482289a5af4
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:39:41 +0000

    rest early offer towards material

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
