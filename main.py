20dc59395edbf84156fadfe0d684c1bfc065be4c
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:46:40 +0000

    warn aim interesting target draw

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def square_number(x):
+        return x**2
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
