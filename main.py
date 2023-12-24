80ac2068712a9f9c7f51ad370f4962c364f042c9
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:09:49 +0000

    sick expect same cover push

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  def reverse_list(lst):
+        return lst[::-1]
