e83893f0c1fee98f70c7a9f4c7991df57c505ac0
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:15:30 +0000

    occasion piece either home along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
