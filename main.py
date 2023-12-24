d9fa79ecc9ecb895c8da13cc2e14f8ff328c55f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:49:27 +0000

    estimate seem far Itself forever

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(numbers):
-        return max(numbers)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
