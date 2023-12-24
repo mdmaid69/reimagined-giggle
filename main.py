f6d0cf842019d399cff9eec99771fc86eaacd046
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:25:50 +0000

    hour kind actual provide along

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def is_odd(n):
+        return n % 2 != 0
