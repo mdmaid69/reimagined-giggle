dd55b9faf3b6ff63bcc63654a14524e56ee49862
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:56:26 +0000

    forward along stop get moon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import datetime
+def get_today_date():
+        return datetime.date.today()
