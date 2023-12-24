04f6c549c41334217229ab6335a81ac461ddd44c
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:41:38 +0000

    borrow crime path uponon pleasure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_force(mass, acceleration):
+        return mass * acceleration
