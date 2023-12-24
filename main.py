10227a505a972670b7d5277572f0b162fa93e0a6
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:51:53 +0000

    comfortable pay grateful exciting dry

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
