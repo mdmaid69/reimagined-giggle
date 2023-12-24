13c283678b9e7973424de404cf0e77d42fedc9d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:50:54 +0000

    all somehow board coat themselves

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
