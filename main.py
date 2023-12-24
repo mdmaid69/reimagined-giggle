3555f6e4f8844afff6ff77f4532e83d8b7bf6fe3
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:39:36 +0000

    properly circle left reduction no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
