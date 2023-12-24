b6349c4ebcaeffa765eedf77fd067f799a3b0089
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:37:16 +0000

    sky rent personal develop content

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_current_working_directory():
+        return os.getcwd()
