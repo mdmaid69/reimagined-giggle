23fa2d1defe7e07c0bf9a14d57455314ee8bdd60
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:00:13 +0000

    control use simple adjective along

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
