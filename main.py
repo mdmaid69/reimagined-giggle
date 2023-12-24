4ee6b2af0b17910ae53e717f137ddc4e4e6157fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:32:14 +0000

    hit group fine uponon each

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def get_array_as_str(array):
+        return str(array)
+def calculate_work(force, distance):
+        return force * distance
