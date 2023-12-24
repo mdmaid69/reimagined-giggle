59dd15e822aa93d5f3bfa584dafeb172b3d7ab52
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:34:46 +0000

    past kill who scheme past

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
