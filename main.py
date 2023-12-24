68c141aca54668304513d8098bb1c53970a48610
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:11:36 +0000

    cheap deaf communicate his aim

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def count_characters(sentence):
+        return len(sentence)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
