ed0f8fc7faa6d0e222199bfff127cacf566d7c54
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:41:28 +0000

    far nerve tired love strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
