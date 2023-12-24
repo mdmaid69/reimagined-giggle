8ec255f84196a771b54110882bcc39075dbe95d7
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:27:39 +0000

    steam conduct account excuse road

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
