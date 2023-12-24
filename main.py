38c6f6b1cf662042ec58510c41ac8ff3df855ec3
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:40:13 +0000

    force nervous favourite prepare bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
