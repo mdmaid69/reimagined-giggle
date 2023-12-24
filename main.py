50e5fd1e7eea491b549d4377d611bd8edb8c064a
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:55:21 +0000

    left contact compare clean owe

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
 numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+print("Sum:", sum(numbers))
