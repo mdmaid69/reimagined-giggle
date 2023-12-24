c59a8d61180110c04f98ff5f2aa489cd3dc9eb90
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:46:29 +0000

    shock ask big explain guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def multiply_numbers(x, y):
+        return x * y
