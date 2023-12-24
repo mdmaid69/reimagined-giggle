61d86361f80938cde2fe5fce1e3e250ba67e9d65
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:21:48 +0000

    neither quit total attention strong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
