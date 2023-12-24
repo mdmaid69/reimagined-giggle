9ee55cc8047d89964132ccc982ea0dbd260429f2
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:09:54 +0000

    better decide put protest age

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+print([x**2 for x in range(10)])
