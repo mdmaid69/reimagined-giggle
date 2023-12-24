dd6dc81e1badbb320ccc1535c94135f144e4d67c
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:30:29 +0000

    second cheek correction actual thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
