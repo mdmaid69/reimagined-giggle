be876b4ca407be27638aa0a75011a43416f900f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:18:23 +0000

    ilovedicks extremely in careful absolutely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
