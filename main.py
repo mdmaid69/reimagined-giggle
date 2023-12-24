636a1799049ebf19f8f4433a4936bcd8ae14e2ea
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:51:50 +0000

    within rough likely turndown innocent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_floor(x):
+        return math.floor(x)
