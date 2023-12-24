847faf72ca212f5b84c8176c0c53b06c5b127e2c
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:31:39 +0000

    guess mine pleasure forever shine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def cube_number(x):
-        return x**3
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
