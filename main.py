b52bed6a2519f1f5f1c634823a891b886f1104bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:30:11 +0000

    very takeback flow towards 179bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
