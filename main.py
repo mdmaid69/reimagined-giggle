2237ddb4dadc2c83e94507823224195f6a3fe5d6
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:06:25 +0000

    usual active miss style attract

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def add_numbers(x, y):
+        return x + y
