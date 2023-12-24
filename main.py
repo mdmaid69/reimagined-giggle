fed58647f4b3f7932c69e6f3b389d20f05bb9a46
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:53:44 +0000

    holdout mine chief dull poison

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
