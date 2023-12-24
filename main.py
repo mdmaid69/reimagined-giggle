2486a573e98dc0eecf6ecafe0542bdee69f0e070
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:39:04 +0000

    section help spite price either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def count_elements(lst):
+        return len(lst)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
