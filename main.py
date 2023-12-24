3794e8c539a28f3e21d44ba552ffb1b5f1cb653a
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:44:32 +0000

    somehow damage nice reject shine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def append_to_array(array, item):
+        array.append(item)
