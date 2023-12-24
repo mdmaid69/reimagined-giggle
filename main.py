461de6724877903c22f7d71bbc17887faf7a51cc
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:22:39 +0000

    order dinner hard show pause

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def find_unique_words(sentence):
+        return set(sentence.split())
