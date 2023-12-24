65025de2820847fceaf7609a3dec1536bbf8f98e
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:57:34 +0000

    usual below wipe guide together

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
