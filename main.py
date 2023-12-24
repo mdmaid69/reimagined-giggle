443cf55049ae609ee4c5b94a9157f1bf0589b1ee
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:48:23 +0000

    fence cost all contents distance

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import time
+def get_current_time():
+        return time.time()
