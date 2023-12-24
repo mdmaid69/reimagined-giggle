2d24cdaa57a0d117d96f1078c03e0501333f95c9
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:57:44 +0000

    average watch unusual it hold

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
