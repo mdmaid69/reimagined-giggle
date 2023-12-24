4313a0bc5365325c1a2faf982f851d86af837e6a
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:51:13 +0000

    spill like position cough disagree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
