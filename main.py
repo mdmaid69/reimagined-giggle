986b998a30f3b45cd09127ea2088a0271aaa4538
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:13:09 +0000

    fit bread snow coat steady

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import array
+def reverse_array(array):
+        array.reverse()
