f10ad24d4e8e336666b68daf7d8cc53ca41aff61
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:55:19 +0000

    description holiday noise settle replace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
