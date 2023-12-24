2fdd560495a1c1cf645db1d9a4dd24f00f605d2c
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:01:12 +0000

    best hour nowhere anything bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
