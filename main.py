da3627bfc9dffd026abf297ecc6ab2d0eff76468
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:01:54 +0000

    lay discover best callback bet

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import time
-print(time.time())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import tensorflow as tf
+print(tf.__version__)
