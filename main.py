7b5a4077c48592a7da47ab3a199b571212051329
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:17:31 +0000

    low include birth genuine ahead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
