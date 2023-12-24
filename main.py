800aa8544ec8a7705f751082699dc0161efa3dfc
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:54:15 +0000

    full life corner pen cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  def subtract_numbers(x, y):
+        return x - y
