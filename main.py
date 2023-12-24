07d696564ecf9a7654e7c51a862a23c091731caf
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:55:32 +0000

    passage room only swing do

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
