33862c2a512ee85dd9b3128a08661932bf8c39a3
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:28:09 +0000

    begin expression there crash only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
