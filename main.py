ab71fd0e3aa04959619fae7a4216bd02cc1c3263
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:37:15 +0000

    rest together makeup divide callup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
