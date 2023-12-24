736989316f4ad012d5ca6b4afbc7a9d33c392027
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:51:55 +0000

    difficult talkinto doctor turninto degree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
