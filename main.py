012ef94606d0d196c4f8a07a6ccea1fdb0df573a
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:18:50 +0000

    turninto unfortunate float brief fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
