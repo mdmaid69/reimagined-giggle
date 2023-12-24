edcb671c35cba610193b68ff23484fff20468436
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:07:44 +0000

    empty trick poison hang Mm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
