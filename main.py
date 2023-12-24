9bab7532987c98a3997daa4c392cbf40ae6394f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:45:24 +0000

    every morning address dry man

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def square_number(x):
+        return x**2
+  def convert_to_hex(n):
+        return hex(n)
