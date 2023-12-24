7de81d046fcf25c2c2c679594970280033f4d000
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:19:02 +0000

    plain pass sore shall book

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def remove_duplicates(lst):
+        return list(set(lst))
