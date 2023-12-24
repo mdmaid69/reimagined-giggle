54c98d880c7a790d488918b6dac13f60082feec7
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:42:05 +0000

    effect cook Ee agree deliver

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
