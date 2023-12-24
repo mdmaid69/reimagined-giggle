715766820693d376825ddbdec1bd8123df4ad905
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:57:30 +0000

    level lie insist lonely frightened

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+def convert_to_binary(n):
+        return bin(n)
