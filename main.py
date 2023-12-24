a1e78191caed6c41aa381e1f759f3a69e5ce41cf
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:12:38 +0000

    summer phone influence trust slip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
