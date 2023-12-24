d96f3658954797d02366156916b916635ef46573
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:07:43 +0000

    someone proud second spend swimming

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
