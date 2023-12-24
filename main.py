01a1572d6275c5ad7ffd6bb93d60de5420fd5266
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:48:00 +0000

    settle impression reply remain bet

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  def reverse_list(lst):
-        return lst[::-1]
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import logging
+def log_message(message):
+        logging.info(message)
