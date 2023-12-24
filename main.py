0f151248b4b6dae5cefaae0a2ce1f09f390e6bd3
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:06:48 +0000

    advanced free term spring stretch

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
