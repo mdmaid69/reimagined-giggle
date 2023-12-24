8b41aeeab785c7937d5aa68b63c0c342cfdd711e
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:51:13 +0000

    bad no little clue fact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
