b648df0d5ef5a0901d205805854c63dc9fcef01e
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:09:47 +0000

    snow man patience sale none

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def find_max(lst):
-        return max(lst)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
