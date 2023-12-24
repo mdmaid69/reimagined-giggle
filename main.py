783fc985bc54bd35240dee6a71e090406f9b309d
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:00:24 +0000

    reduce pleasant dick pay belief

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
