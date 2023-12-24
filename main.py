7b7d750a3d2aeb760912bf557f2f60e7d8bf4268
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:51:30 +0000

    calm pocket bag 566end speak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
