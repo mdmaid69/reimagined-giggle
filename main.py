c86f4f4d6cbd7f06f07a974cc96740de9a46bd4d
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:32:03 +0000

    string rise clue silent bringback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+print("Hello, world!")
