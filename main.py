e47f60fc654bd669651b001491fb09136e582d32
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:36:46 +0000

    false roof cry mix surprised

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def multiply_numbers(x, y):
-        return x * y
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
