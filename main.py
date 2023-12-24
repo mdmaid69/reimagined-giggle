4cafb45f0f2e2b637dacc3c9464553ec5f03fc84
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:16:46 +0000

    cope offer most season cover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
