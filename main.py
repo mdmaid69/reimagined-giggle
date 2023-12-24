6576210a6443985b0789e569eaacf6cdce093ad9
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:17:22 +0000

    environment sad me brief twist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
