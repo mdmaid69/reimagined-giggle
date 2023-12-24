693e1c8f067dc59969d3c302a3ffb4311d9fc25e
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:50:00 +0000

    offer survive level small official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_current_directory():
+        return os.getcwd()
