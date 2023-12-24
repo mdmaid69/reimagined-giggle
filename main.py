15d08a646a5d88bff750e941e7983bf7032d49be
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:14:53 +0000

    mine respect measure door shopping

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
