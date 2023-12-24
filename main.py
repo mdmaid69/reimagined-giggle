53ee5e717a20344938026b0bfbb0f140b930228b
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:18:58 +0000

    closely stay movement congratulations favour

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-for i in range(10): print(i)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
