32dce9922aecfe41971dd450c92b675d5709359f
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:17:02 +0000

    drive flavour letter talkoutof freeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import datetime
-print(datetime.datetime.now())
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
