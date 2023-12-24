d0a3a43418044f88846208d84bceae78c074b434
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:46:12 +0000

    track correction crime welcome whatever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
