d402b33a99cdecc36eb7d281a11fd1e3abc333ea
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:56:03 +0000

    roof 568energy improve information neat

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
