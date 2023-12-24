1217e89d52d748734feaed1b73f2c82d95afb0dc
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:37:12 +0000

    fear share modern runaway Pp

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
