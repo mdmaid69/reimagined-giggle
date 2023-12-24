3870a0420ea82444af459aa374c2b13989b97d49
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:08:47 +0000

    then previous train nurse total

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+print(os.getcwd())
