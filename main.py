7fd82dc38d94126b19516635e430e8197a1672a5
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:18:43 +0000

    joke union niggaacademy interested drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import os
-print(os.getcwd())
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_tangent(x):
+        return math.tan(x)
