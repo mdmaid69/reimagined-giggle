6a800f2f0500a1c0b5e4087d19759773e937241a
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:07:33 +0000

    worth commit moral dick balance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def remove_duplicates(lst):
+        return list(set(lst))
