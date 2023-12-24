924434d4dba32f1d84955daa6d8da623e2636501
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:28:18 +0000

    judge steam twist effect emphasis

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+for i in range(10): print(i)
