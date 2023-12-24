a281963adb8ad6fc533ea933f922e7e1089211c1
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:39:55 +0000

    confuse wash justice delight judge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def remove_duplicates(lst):
+        return list(set(lst))
