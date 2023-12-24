91aa16ad649795df901a0107c7310815123f9d1f
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:56:40 +0000

    friend damp late pleasant try

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_min(numbers):
+        return min(numbers)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
