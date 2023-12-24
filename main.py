541557f1a756d341b1a5324d4e068f881287ce8e
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:55:30 +0000

    fact thinkof success escape back

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  def cube_number(x):
-        return x**3
+def sort_numbers(numbers):
+        return sorted(numbers)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
