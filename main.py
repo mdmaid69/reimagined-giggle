1f15ce4c9ab5a88cc8833ec38e1761fd4f16f5fc
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:26:07 +0000

    combine school police petrol only

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+from collections import Counter
+print(Counter("hello world"))
+import os
+def change_working_directory(path):
+        os.chdir(path)
