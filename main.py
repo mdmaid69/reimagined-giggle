b8977cd2ca37a56af177bd935387d1916f7d336e
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:53:53 +0000

    drive produce region lesson paper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
