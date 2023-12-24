5576e51a396ef9c3fdd87f088ec7beb623928735
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:54:28 +0000

    medium this now connected away

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
