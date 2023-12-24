9bee2f818d820e034ddcb8d14e4b193ccf40301e
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:05:50 +0000

    underneath since service action upstairs

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_max(lst):
-        return max(lst)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def sort_numbers(numbers):
+        return sorted(numbers)
