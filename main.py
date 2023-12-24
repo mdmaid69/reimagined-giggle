bc5df5de194ddb150b277d9e2a3f70e1d7e410c0
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:37:18 +0000

    sleep and plenty ago throughout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def sort_numbers(numbers):
+        return sorted(numbers)
