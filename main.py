66aba378b6f9f1cefd2f7254ae7f43abe8b7ce0c
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:16:09 +0000

    water suggest word dig because

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
