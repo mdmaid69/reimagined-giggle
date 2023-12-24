6077e7b3d8ff6b798fdf386baad0683aca487f41
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:25:16 +0000

    glass supply pause can phone

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_acceleration(speed, time):
+        return speed / time
