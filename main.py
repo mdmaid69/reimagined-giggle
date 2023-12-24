d7f8d8308783328f951e4fac0e4d653f74c2f7e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:28:30 +0000

    nose shopping mess commit usual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
