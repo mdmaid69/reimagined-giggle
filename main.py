e91925679f59c8105062fc0ea5d2ab8f4931a41f
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:04:06 +0000

    spend pay cover apply food

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
