2f60712009b20109e6b17cca3a3ca2c2b5901949
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:40:32 +0000

    central slide we rather wise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import re
+print(re.match("h.*o", "hello world"))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
