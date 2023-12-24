6f12991a4ccd276d0e72554d6b55035dc388bb39
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:31:12 +0000

    increase statement reach conclusion pretend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_work(force, distance):
+        return force * distance
