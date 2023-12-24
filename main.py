bf71d2f9b9e30d0c91025f91d61a2792b9b2538d
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:24:52 +0000

    tall passenger straight sweep twist

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
