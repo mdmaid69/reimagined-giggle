09b9df514cfc34c47641ff6c16d9d98720e5ae4f
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:21:06 +0000

    unit feature uponon steer famous

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
