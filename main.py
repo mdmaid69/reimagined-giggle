5b3c8dd5e99248a37a41971997bdc6e2a78df20a
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:25:43 +0000

    gooff plain promise private behave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def convert_to_hex(n):
+        return hex(n)
