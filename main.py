0b38afe902327eb7da0d3b4bb0446568d5837865
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:26:38 +0000

    freeze Uu past many shoulder

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
