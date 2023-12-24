7edff21ed007511438291fd9ddf2ba8502084f1d
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:40:40 +0000

    tip fit giveout lost relax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+text = "Hello, world!"
+print("Uppercase:", text.upper())
