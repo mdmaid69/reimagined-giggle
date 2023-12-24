c484da414b4d3ada14de61da79463dcc712dda85
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:26:11 +0000

    but believe afternoon form comfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
