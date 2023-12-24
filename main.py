1022127c1bf5820885ef245ce9234579ea8f30ba
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:33:05 +0000

    protest to lonely dig bin

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
