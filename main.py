a9deb006a084ba3d9fb75e5c82667df64325980d
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:33:04 +0000

    mark so order size after

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import re
-print(re.match("h.*o", "hello world"))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
