ed6677451daa605f86c31ad09a1f11337df0befd
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:00:30 +0000

    spring pay generally desperate list

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_power(work, time):
+        return work / time
