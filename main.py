7f67182a6a3c68c56b260863e50bc5d004a2d9cc
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:30:43 +0000

    reliable right juice Pp whenever

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import sys
+print(sys.version)
+def convert_to_hex(n):
+        return hex(n)
