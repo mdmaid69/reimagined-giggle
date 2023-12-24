c807fd6d9e381bf74165041ef4f7fbb9bdbd49cf
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:49:17 +0000

    special crowded rank school disgusting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def square_number(x):
-        return x**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
