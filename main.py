88ada56d1747f538f9276e278c9c8a9587e38b31
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:17:35 +0000

    across early demand force emphasize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_itemsize(array):
+        return array.itemsize
