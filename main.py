50892e2c67ab247fdded606b241fa3e9e124bbcd
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:13:55 +0000

    unless bit leg rush judge

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+text = "Hello, world!"
+print("Words:", len(text.split()))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
