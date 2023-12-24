00466cdf4016cdd4e6da09788a74dadd21ac2496
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:25:15 +0000

    lock building ill connected nervous

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
