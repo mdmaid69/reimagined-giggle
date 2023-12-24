7a04eaada8407f6d40fc0df7b082a83cf7f2f5da
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:04:32 +0000

    than shut circumstance month sound

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+print(math.pi)
+def greet(name):
+        print(f"Hello, {name}!")
