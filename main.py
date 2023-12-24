b70149961e3e742d523783d104a92ce9cdd33612
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:17:03 +0000

    how guide allow print interesting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
