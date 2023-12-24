590be27402acb717ded81bb9864081ce6bd0fec4
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:47:27 +0000

    serve taste snow gradually little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+def print_python_version():
+        print(sys.version)
