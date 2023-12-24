7700afb5cabcc264a988ad74b2e5fd1c463fbf50
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:23:39 +0000

    nurse soak practical excuse into

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(x, y):
-        return x + y
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_power(work, time):
+        return work / time
+def is_even(n):
+        return n % 2 == 0
