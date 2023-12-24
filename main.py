b7cbf08ad5ed94e14ef5b0504da03c46ba905c39
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:03:38 +0000

    holdon confuse threaten trust snow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def reverse_list(lst):
+        return lst[::-1]
