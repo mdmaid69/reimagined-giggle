bda6ef18ce713d1c89cdd5fd0e4cc6aad45b0747
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:31:09 +0000

    reach ball flood all pass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
