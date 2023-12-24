0b5e419b2f3335d182ba28dadae546061e0c3111
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:27:33 +0000

    open flow tragedy inform club

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
