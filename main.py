324fe76e7298440e5e56422aa9644c4797fb279b
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:13:55 +0000

    hard best house makeinto comeback

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
