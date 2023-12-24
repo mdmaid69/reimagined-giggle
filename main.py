afa65886ebeab497db13fde1747920f11b9fe16b
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:26:41 +0000

    keepout creature regular gap cheque

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
