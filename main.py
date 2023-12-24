a4cf091f4e818ce3b9fd3ce7f534ca1edc4d1b45
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:05:47 +0000

    lookafter off means through climb

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
