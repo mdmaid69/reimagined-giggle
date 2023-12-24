36af3a3de984ad8839ffe1851ce4f5c37e66dc38
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:29:03 +0000

    safe picture unfortunate either use

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
