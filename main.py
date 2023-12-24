b41ae73146cf236852b9d8017fb8f698012b642e
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:49:45 +0000

    last sink sink Jj related

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-text = "Hello, world!"
-print("Characters:", len(text))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
