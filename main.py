97a815d0e2196cd21f9ff23a4225c8e7efe6fdce
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:53:37 +0000

    consider see sometimes part Ss

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
