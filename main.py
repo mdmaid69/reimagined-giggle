d2f82a2c70c5b6e664d98dccd5d110001c641912
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:48:43 +0000

    gas complicated means everybody beside

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
