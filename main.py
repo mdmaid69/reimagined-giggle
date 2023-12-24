0cf8e65aceed959f18feddc41ff5bd81f4de65ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:29:09 +0000

    order sport comedown bar task

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def sort_list(lst):
+        return sorted(lst)
+def count_words(sentence):
+        return len(sentence.split())
