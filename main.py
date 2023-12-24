75ce7df5377cc5fa11fce4ebbe51bfa370428c49
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:22:11 +0000

    achievement very mean intend attractive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
