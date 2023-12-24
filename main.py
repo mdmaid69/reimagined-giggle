b31f14d2f66a9b8fb97d74c6d1e9543be981b4c9
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:33:53 +0000

    567enemy through why flow own

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-for i in range(10): print(i)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def count_characters(sentence):
+        return len(sentence)
