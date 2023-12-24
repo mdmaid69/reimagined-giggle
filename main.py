e2d94fb6c4df5fdb4a77cc2b66552e1b73bee2aa
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:15:43 +0000

    kill arrival contrast last use

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_power(work, time):
+        return work / time
