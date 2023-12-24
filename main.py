4b0ae63a55326715022191ca3a4296e1f40eb834
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:14:01 +0000

    health build walk joke switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
