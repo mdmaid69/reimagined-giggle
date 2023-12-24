6b5014eda76cd9524f9c17a17ef483b3b6c3cdfb
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:35:44 +0000

    rain forgive firm tap plant

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
