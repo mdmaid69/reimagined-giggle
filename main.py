f4ae364dd2662d5f7001e2b4c98ff5bd0ea37695
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:56:42 +0000

    pay way conclusion before dull

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def convert_to_hex(n):
+        return hex(n)
