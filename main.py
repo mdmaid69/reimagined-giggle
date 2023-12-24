047a410a0f46a7955cccd60562cad80e3d05aff9
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:04:20 +0000

    most whistle expect organization involve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def sort_list(lst):
+        return sorted(lst)
+def find_min(numbers):
+        return min(numbers)
