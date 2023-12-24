86ed816c712739639e0efc9cfe8f0bfce787b0ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:41:06 +0000

    spill square to swing check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+x = 10
+y = 20
+print("Sum:", x + y)
