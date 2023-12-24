0f2630c7e2f62a2588bf719d35ca54eb98045b82
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:46:15 +0000

    tomorrow learn down write breakin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import sys
-def exit_program():
-        sys.exit()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
