c0418735f36c78f0307fd365115c9ca851a7dcc0
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:38:07 +0000

    race copy loan wind increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
