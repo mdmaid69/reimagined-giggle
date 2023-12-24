14c20684d8967096a4a0729afa0e60e4ce5a5184
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:02:03 +0000

    laugh Bb stir comeup state

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
