57750a9fc25f97cfbdf048cc703ca89bec6d3831
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:28:13 +0000

    praise blind culture decide swimming

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
