df9d1d0e5b49ba07ab430b0ae10c00ed220ef804
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:47:55 +0000

    progress taste show equipment hurry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def count_words(sentence):
-        return len(sentence.split())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
