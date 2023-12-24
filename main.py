2430872b6dec0de0fac694fb9fabce8ef7940de5
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:06:54 +0000

    score preparation air keepfrom stop

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
