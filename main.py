c4894f7709d06683cd0a262fabe6eebbf60aa250
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:12:27 +0000

    load escape show thing letter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
