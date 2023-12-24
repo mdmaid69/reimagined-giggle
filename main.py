bfc606a3c9e40a96adac18dc1e576413929d2d2c
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:29:23 +0000

    everybody awake comefrom that wide

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
