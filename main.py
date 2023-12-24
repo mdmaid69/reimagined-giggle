c6ea5eb21a2bea2748decf570d1f8d025c6faca1
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:37:27 +0000

    freedom cover right mark pay

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
