bde5e56d43cda9910824f494022f6980ab83f6d8
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:27:27 +0000

    principle equipment try pour exit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
