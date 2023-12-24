be0d827f3ff874a80e8df649d09c8a75cca8971d
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:28:53 +0000

    border on black become idea

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def is_even(n):
-        return n % 2 == 0
-name = "Python"
-print("Hello,", name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
