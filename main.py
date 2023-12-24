429cf2b1210f4cea5960c04dea215cca4e5f69f9
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:27:59 +0000

    eye at there coffee foreign

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
