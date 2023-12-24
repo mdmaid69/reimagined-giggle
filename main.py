1763a68f0b51631b375f43cc1d0f08ec9c049a83
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:45:29 +0000

    obtain man breakthrough usual oil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
