3e5bb89e1ec170b6c1b29a8bf78b8222f82bfb2b
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:18:20 +0000

    still sea hello sail stiff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
