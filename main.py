dcdf855301b44eb6a5b8b1ed38eb9ba7b11b8b5a
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:23:32 +0000

    flight head present spare space

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
