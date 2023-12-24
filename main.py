b8c7da7e90a45441d38dda79c94c9c2685dab672
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:36:34 +0000

    proud free impatient fun remark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_density(mass, volume):
-        return mass / volume
+import sys
+def exit_program():
+        sys.exit()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
