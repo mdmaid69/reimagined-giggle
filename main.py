677ba68964155bcc93c3d8c7d361f52ae17a78aa
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:31:28 +0000

    mind study confidence keepup will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def square_number(x):
+        return x**2
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
