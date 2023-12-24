bc934de4e4ad7abcf4b36dda05369735c76e447d
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:33:51 +0000

    joke club show exchange scared

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
