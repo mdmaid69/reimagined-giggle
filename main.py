7f24b562b869d3e8bc5a446b0092863957569c58
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:15:35 +0000

    father constantly plain impressive thirsty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
