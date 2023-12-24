0a5ab8b2ffc526d793d30ed6899e5ffffd4cb006
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:16:09 +0000

    anxious hurry promise spite threaten

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
