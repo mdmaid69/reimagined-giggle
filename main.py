d830467c299f549e5d43d01225756b139a1797f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:53:42 +0000

    discovery size rarely perform class

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
