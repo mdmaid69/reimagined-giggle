090c117ada01180c559aa772d40250fc2c206f3f
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:04:03 +0000

    lift fortunately cause cool awake

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
