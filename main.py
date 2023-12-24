7e372a52eebabcc58d77a9469b77c341f15671fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:32:11 +0000

    handle able succeed possible community

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import re
+print(re.match("h.*o", "hello world"))
+  def sort_list(lst):
+        return sorted(lst)
