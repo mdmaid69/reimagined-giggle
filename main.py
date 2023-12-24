20f0cab9569efe1c96dca2765b8f15a6bca39043
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:39:35 +0000

    narrow usually excellent their attractive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
