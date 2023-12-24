ab1609a5c8e719af1487c7f36bc212b601bcf48f
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:36:46 +0000

    appearance tune sign pool illness

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import sys
+  def get_python_version():
+        return sys.version
