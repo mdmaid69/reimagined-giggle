30477159ade8df80a2878bc5cbd4efe9a76db260
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:17:46 +0000

    Bb few contact approval material

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import os
-print(os.getcwd())
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
