5b93d00f6a230b4066b69f3dd880ecab8151fd25
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:25:09 +0000

    shock fall copy rest edge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import array
-def get_array_item(array, i):
-        return array[i]
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
