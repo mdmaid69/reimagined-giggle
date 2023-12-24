22322dd182f106f3c41df7e27ae7e88f91dee94b
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:00:06 +0000

    against ocean himself row callup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+print(math.pi)
