bfbff657f2912269f6575d8d1a1a75be503d9334
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:16:53 +0000

    zero me ever cost affect

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def find_min(lst):
+        return min(lst)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
