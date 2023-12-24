0b94ed9682c165fe72fe54cd39b0b788be49c26f
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:53:28 +0000

    pump soil false bunch female

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
