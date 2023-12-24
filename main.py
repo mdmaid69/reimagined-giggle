d10ded0643dfa803a78dfa82ee27545f45986e9b
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:06:09 +0000

    after any Cc board scared

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
