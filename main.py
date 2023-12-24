7622e2059495c24cf438535e2e945bdc2516c778
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:07:23 +0000

    spite tie edge except drop

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def count_elements(lst):
+        return len(lst)
