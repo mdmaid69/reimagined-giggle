971eebee03a70ec0f717e179ceaca26556d6684a
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:18:18 +0000

    569enjoy official problem of insist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
