b418eaccbc07bc7532184646a4113c14ea9dd7c9
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:45:06 +0000

    maybe train show slight buy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
