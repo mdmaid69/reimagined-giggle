e44a2c97251854d06c34c062314db8ed37a969a8
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:20:28 +0000

    show wire total wave build

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_power(work, time):
-        return work / time
+import sys
+def exit_program():
+        sys.exit()
+def find_max(numbers):
+        return max(numbers)
