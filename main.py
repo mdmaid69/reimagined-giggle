55c5a657193a3780f7bd4839881be3d4d909df3f
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:03:54 +0000

    concerned sorry dig cost thank

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def subtract_numbers(x, y):
-        return x - y
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
