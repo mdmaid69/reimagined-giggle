d562333e8bbd9b539c8dfe6cacdd2cee736c4a13
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:27:10 +0000

    wealth scale strict workupto area

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
