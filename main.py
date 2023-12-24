f6761ab359bf5e44500917d97e9db6e4777a7582
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:21:34 +0000

    formal hurt pay change thank

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def subtract_numbers(x, y):
+        return x - y
