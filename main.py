acc0f95550149b51eb0b2a3d5e604544712e6933
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:59:28 +0000

    afraid I feed workout yours

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
