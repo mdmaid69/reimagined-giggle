7fb7d09f22580708ed9c77cc269f02c67218e0ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:27:02 +0000

    whether completely great hurry work

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import time
+print(time.time())
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
