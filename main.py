915cea0432657324cae39b842f793286744e763a
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:16:29 +0000

    perfect cry talkback everybody base

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_ceiling(x):
+        return math.ceil(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
