83571e1d1d6e7552106ba81d1d907303652d03b4
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:17:14 +0000

    entrance Dd take guess expect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import collections
+def create_user_list():
+        return collections.UserList()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
