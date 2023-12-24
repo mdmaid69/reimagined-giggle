5254ec4e17f2615fc9a85db586ab79a5ef2a1b9e
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:09:36 +0000

    problem trousers beside with exhibition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import sys
+print(sys.version)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
