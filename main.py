a0d9c93ae5b4acb3c8a8e5c67b96a317bcaa0df6
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:08:59 +0000

    emphasize silent cover squeeze failure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import platform
-def get_os_info():
-        return platform.uname()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_typecode(array):
+        return array.typecode
