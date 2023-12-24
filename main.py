3e203206bebd0a93b5f7dea22af12dd16954b42a
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:12:47 +0000

    breakin coat old trap write

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+print(sum(range(10)))
