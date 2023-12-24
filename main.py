8100a83d213062e021938da6910aa1eaa954ef4d
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:05:30 +0000

    after stranger on able only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import platform
-def get_os_info():
-        return platform.uname()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
