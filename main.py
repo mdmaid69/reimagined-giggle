151562e3d86d7010668e01f887f7c0dc85dd8688
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:56:43 +0000

    patient risk start defeat contact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
