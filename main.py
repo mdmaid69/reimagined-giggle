79e84041771f577f6a6de2bad3730dc3d994e7d7
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:45:07 +0000

    problem guilty thorough show near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
