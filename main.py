fb598179495eb710601495e1abc8bf1b9cf9f2cf
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:32:22 +0000

    fun whoever whatever murder test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def add_numbers(x, y):
-        return x + y
+import time
+def get_current_time():
+        return time.ctime()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
