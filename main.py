7e8226d7df1534f6ef7696faa25dcf00ce4a2689
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:12:34 +0000

    rob snow act mess first

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
