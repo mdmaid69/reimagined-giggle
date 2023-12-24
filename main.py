9c65fef3dcab37c36b5b8e3ba36203d3688c7de5
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:49:47 +0000

    thinkof along angry cause subject

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(lst):
-        return min(lst)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
