f184385984b6d8887d32ead49b84fe082e965751
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:59:06 +0000

    spend someone order under necessary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_user_list():
+        return collections.UserList()
