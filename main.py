0a4c4c716ac083eab07669b13084ce8b037f3cee
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:25:24 +0000

    move turnout question soil vision

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
