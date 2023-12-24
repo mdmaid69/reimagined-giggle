928ea6b9dc1ca6b03c9ddfed81b3f75905e83ce9
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:21:46 +0000

    card choice turnaway urgent suit

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
