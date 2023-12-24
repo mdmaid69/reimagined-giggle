2f20cce2533503d244ebce59d3056fef01101692
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:51:45 +0000

    right bother rule follow willing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
