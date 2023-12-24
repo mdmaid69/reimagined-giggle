cc77bb94d15d8201ba8f61a44229cac839de34d6
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:03:50 +0000

    strict year goback(to) interview offend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
