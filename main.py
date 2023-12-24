8d15e9b7d4920b0dd82769cdfd0802794048c2eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:42:12 +0000

    bang surprise board put valuable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_current_directory():
+        return os.getcwd()
