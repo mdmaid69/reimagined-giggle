bbf0b392bba80bec38fdacbafc2605617f0f0491
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:34:23 +0000

    block pull arm plan block

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
