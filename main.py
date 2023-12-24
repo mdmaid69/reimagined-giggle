fa339a98a1244f4f7747d57b89f2bc1acb9b8d05
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:09:50 +0000

    trade mix Cc relative fashion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
