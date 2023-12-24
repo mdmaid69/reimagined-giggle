f617d74161c40d318b853cd56a21d1c86bff8528
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:29:38 +0000

    check lay opposite substance maximum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def convert_to_octal(n):
+        return oct(n)
