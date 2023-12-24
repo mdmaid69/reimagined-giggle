45bc259fa24e8ca514f8b0e1ac49a3343d3c0cca
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:25:38 +0000

    shock mother some meet zero

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
