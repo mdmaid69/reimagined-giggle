7ef92913b31a23c9eba78f7b83330911e37f2db3
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:42:09 +0000

    various lock consult use plastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
