01cfe79f2de467fce1cede9b1bb2bf78cd02e282
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:24:53 +0000

    class flow before hollow boil

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_list_from_array(array):
+        return array.tolist()
