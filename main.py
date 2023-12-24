3195275c564669c72e309a4433b0a25d977fe571
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:11:08 +0000

    pocket will early wall opponent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
