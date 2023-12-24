8b8da71ee167fc5886d6633fbf488d559e5114a2
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:12:22 +0000

    be gap dream uniform congratulate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_force(mass, acceleration):
+        return mass * acceleration
