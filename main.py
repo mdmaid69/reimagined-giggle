2b205c6d779f1a825a5d6216008b589ac715c503
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:25:23 +0000

    score protest dead speak trial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
