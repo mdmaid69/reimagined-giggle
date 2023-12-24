9dfb62c0258119036cfe4ba1f8d893995a9729ae
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:02:48 +0000

    birth worried Cc top claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def split_path(path):
+        return os.path.split(path)
