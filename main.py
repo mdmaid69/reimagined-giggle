e62827ae34eace2739c6d44fd40fe6e5d7b4f620
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:13:22 +0000

    steal body close wait machine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
   def get_file_size(file_name):
-        return os.stat(file_name).st_size
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+        return os.path.getsize(file_name)
