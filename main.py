3ccebfb84e02d89c0546442454c3d1b4c651ed98
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:16:08 +0000

    difference yes concentrate clean flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
