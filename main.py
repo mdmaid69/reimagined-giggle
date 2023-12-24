c7c41b9a2d2da800acbc6a9ae3d76fb2669edff3
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:33:03 +0000

    heat bush always juice down

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
