45359ca38c05379f82ba4983a2ffc5fce4c22d91
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:40:27 +0000

    mess holiday suddenly might slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
