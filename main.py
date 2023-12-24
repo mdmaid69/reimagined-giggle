fe05e1d0ed9c80a415c63182b2ec2f6d44a37e0c
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:47:33 +0000

    phone even exchange release match

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
