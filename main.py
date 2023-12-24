8a1da3ba26179edf3b00b71c46296b47609ff377
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:03:15 +0000

    it lookafter by laugh gate

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+name = "Python"
+print("Hello,", name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
