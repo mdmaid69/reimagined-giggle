abfec5b60d2c3225a3401573249aebbd33f53e3a
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:16:46 +0000

    sound authority would characteristic convinced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import sys
-  def get_python_version():
-        return sys.version
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
