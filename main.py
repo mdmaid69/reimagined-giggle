cc1b32cd3356a5faf34cef96105e7a7c5b215d03
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:27:46 +0000

    rush achievement tragedy remark company

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
