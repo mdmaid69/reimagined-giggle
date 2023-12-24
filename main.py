b72fa0b2eb91e388c491235853f2f844544380fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:26:44 +0000

    trip put possibility reduction similar

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_speed(distance, time):
+        return distance / time
