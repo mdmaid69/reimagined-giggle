f4b22f65a95f9bbd7f7e041fd2a42f01731bb631
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:55:26 +0000

    nice which clearly poison one

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_str(array):
+        return str(array)
