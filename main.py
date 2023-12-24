57ae6064f00aa3bd6961f496eae9d881235da8e4
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:03:36 +0000

    rise safe fair ours command

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_power(work, time):
+        return work / time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
