5ae4df1ca87e5fb6ede3693b1bf10abf04b8eb09
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:24:11 +0000

    understand guilty pay reaction actual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
 import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
