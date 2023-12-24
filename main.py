a735cc83eb511d1479beb068503e0631d5473dd9
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:16:39 +0000

    conduct fly like pay deliberate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
