167818b5a6ad1469fa422e946ce2b93bf02e170b
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:01:11 +0000

    meaning note before motion urgent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_volume(length, width, height):
+        return length * width * height
