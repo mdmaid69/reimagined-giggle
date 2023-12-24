8934e5779da245923e9e15ddefac627e5dc7f026
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:27:40 +0000

    important once punish pride until

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
