4961900736e6e2a6174d733a7150c46b88620f3a
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:54:05 +0000

    convinced leg ring noise bridge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
