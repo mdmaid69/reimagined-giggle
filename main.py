ca3b22738e4bcf7a04391fbdc88d97670d98d46e
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:34:28 +0000

    tongue inquire wake likely holdon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_density(mass, volume):
+        return mass / volume
