c149c2178709a8fa5cce465264cf55d92fdff2fe
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:59:33 +0000

    praise average no instrument gather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import platform
+def get_os_info():
+        return platform.uname()
+def find_min(numbers):
+        return min(numbers)
