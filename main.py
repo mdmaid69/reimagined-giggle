ec72f8473a46bb9711b8d68b9556d6090af378fd
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:37:59 +0000

    inquire justice convincing move grand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
