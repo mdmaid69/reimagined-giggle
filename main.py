3f04191afe5b77b6d02f8e049d4c1a0a7bf3331b
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:16:11 +0000

    day far wonder earth that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
