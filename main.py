2e24d4f7bd38ed4d1fcbbe30a86a5c8ad5c01f42
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:18:26 +0000

    whoever rely stamp dry accidentally

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_area_circle(r):
+        return 3.14 * r**2
