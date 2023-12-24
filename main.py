cb234b9f2cd2d0ea8a3ddbca61af33bd3e6704d8
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:14:46 +0000

    better clearly voice comein inquire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
