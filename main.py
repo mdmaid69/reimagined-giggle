e6fcdf53bfd756ebd2082cb7002207ed00fb027a
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:50:00 +0000

    skin now curious fly tread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
