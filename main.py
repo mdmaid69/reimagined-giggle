f89abb35cd8721db0ebc7d90cd154b3c56d778f8
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:32:55 +0000

    slowly trip final limit fair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
