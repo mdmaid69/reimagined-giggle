d389a629db96ee5304210c8d06b49d103fea88a2
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:01:21 +0000

    hole only together Pp judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
