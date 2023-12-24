e76c1cc1a1db555845ef9c0e258aee8465595371
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:44:02 +0000

    big teach skill related improvement

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import sys
-def exit_program():
-        sys.exit()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
