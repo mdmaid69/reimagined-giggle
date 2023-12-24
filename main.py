4706bbe0979afdf7e66f8e5e00c20cdcee24462d
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:11:00 +0000

    test estimate about soil eager

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def sort_list(lst):
+        return sorted(lst)
