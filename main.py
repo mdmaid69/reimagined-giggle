9eea2f3c514d8fbd9b771a2cafe48619f222bdf2
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:31:01 +0000

    follow comein luck receive sentence

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
