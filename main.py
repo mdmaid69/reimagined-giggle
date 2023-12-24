4838665fd18e9ff3332fcf96903876afc539cb3d
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:19:11 +0000

    simple persuade report call until

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
