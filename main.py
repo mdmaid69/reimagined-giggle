6f0a7a0cdd7357d1b8acec27c08891e8f48e6dc4
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:04:58 +0000

    talkback debt journey very tax

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
