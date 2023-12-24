b0e1772edcd2e82cc488ca2730dcba526d19a748
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:19:28 +0000

    whom artificial design win note

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
