7321f9e1286335a926a9d1cf76b60a8933011050
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:20:11 +0000

    wish project temperature ride personal

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
