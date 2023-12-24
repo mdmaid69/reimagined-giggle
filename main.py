c21eec231b15b73cafcda5dcdc4e84b9d3068960
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:45:38 +0000

    similar mystery interfere display lot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
