cb2a827be1ac2d871dac65bc75a43e257988a8b5
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:37:42 +0000

    car find careful theirs polite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def find_max(numbers):
-        return max(numbers)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
