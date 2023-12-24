9c38a7abe47ba5775bdda894de24513254324d8b
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:48:05 +0000

    few book length uncertain put

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+print(sys.version)
