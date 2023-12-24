b4ddb36d9d5ad26d62d9c842de6d9b2e866d9073
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:06:29 +0000

    old lead fortunate often modern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
