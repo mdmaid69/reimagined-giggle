ddf6bf42503570617ca5b4263327d7468b059468
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:20:21 +0000

    decision club holiday need back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
