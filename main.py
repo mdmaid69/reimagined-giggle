0393e097e1b08007b3a46cd4cebd212a0bb0cd85
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:12:41 +0000

    container sing fast sort yet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
