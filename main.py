5a7d1dfd2fdc039bd908104bab58b9a20ca3e48f
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:41:50 +0000

    march breakfast grateful forget nor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
