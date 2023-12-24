993f29394023e2a239389e0a2970f6dcad291f2e
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:45:43 +0000

    dare receive address increase trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
