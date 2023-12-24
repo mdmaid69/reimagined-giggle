cc976d1a23c36c11a5fe86f24580b317376ab886
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:54:05 +0000

    file hole lot patience character

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def count_elements(lst):
+        return len(lst)
