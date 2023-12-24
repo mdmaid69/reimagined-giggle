2408256f092b50cfd09df9648d7024e3af390de4
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:14:26 +0000

    day catch find what free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import time
-def get_current_time():
-        return time.time()
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
