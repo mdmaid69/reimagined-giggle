05c1e1f64d94d0a57a381541a72a2babe9d172f8
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:24:33 +0000

    ashamed we theirs not goaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
