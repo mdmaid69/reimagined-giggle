69ae5a811576cb5e8ac3f9d33ff918edc6a4b15f
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:12:28 +0000

    clear hurt exact sweet official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def find_min(lst):
+        return min(lst)
