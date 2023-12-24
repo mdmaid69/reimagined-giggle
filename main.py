acdfcd19e3406f2e840de61cedaf465ebfac7261
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:04:45 +0000

    friend mean supply ocean Gg

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
