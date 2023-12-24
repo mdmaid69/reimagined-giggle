30058db91c621f851a15b23a5ab51a62b2dc83ce
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:05:52 +0000

    wash rubbish learn squeeze arrangement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def sort_list(lst):
+        return sorted(lst)
