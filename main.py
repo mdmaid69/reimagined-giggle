c77e26c5e6884218fd67b21d95e3140c21628b13
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:04:19 +0000

    dream pleasant soil relation minimum

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
