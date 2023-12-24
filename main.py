187737980d47e62f5b8b6266d87a6d5e2956fe48
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:10:43 +0000

    confident else rush break lead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def add_numbers(x, y):
-        return x + y
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
