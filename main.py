9d0f695e295efd6f0fa6767baaf261fb25d01efc
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:30:49 +0000

    recover odd not spin himself

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-text = "Hello, world!"
-print("Characters:", len(text))
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
