a89fe1f40055a4e8b4ebcc60038c71e8213c9e74
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:55:50 +0000

    Qq complaint cover exact face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
