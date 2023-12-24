fb5658d341609413f09fb277d6d94ba8d030656a
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:16:12 +0000

    busy number debt nature carry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
