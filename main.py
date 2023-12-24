49976dc6089d168bfdc50d3c33f47be87ec176a1
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:15:35 +0000

    shock lunch tense rarely more

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
