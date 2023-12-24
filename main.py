0267700885cf74690c17c242edbe9e861df11704
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:35:57 +0000

    entry reach upstairs brother shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def reverse_array(array):
-        array.reverse()
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
