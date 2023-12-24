638ced3257006106cdd597dbae01d22d0e3fefae
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:18:13 +0000

    combination plenty bite cry land

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def convert_to_hex(n):
+        return hex(n)
+def calculate_speed(distance, time):
+        return distance / time
