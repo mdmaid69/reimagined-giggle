dcba3da15c3be646d774cb86cae01b03b45f6222
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:39:14 +0000

    deal near to exciting range

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sys
+def print_python_version():
+        return sys.version
+import array
+def get_array_as_bool(array):
+        return bool(array)
