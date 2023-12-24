49c95498180108c87a21a9ecadf0195a15171e45
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:27:35 +0000

    sweep nor include fashion breath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
