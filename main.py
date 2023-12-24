c3bfcb46cc408906f424d1276d27fb072693d3db
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:12:44 +0000

    disappear question 564encourage fashion indeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
