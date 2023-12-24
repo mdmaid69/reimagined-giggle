ce6f344daf7bafaf71481b37a5dd6ae297d4857e
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:25:55 +0000

    private getup drive myself turnoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
