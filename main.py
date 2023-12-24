48f779af120b6e117db676d6ab61ddae5e7ffafa
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:38:24 +0000

    gate spring never agree every

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
