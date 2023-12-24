183b340e3118ac6d29c656d0300fa3048e698703
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:40:11 +0000

    left majority question waste demand

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_max(lst):
+        return max(lst)
