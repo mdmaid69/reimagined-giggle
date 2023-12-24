9608c5eff649c197fb796624cefd88b5ed8c8d34
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:21:52 +0000

    lock protest clean alive gradually

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import sys
+def exit_program():
+        sys.exit()
