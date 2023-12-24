69d8601077974ae2659174cf2f3ca8ca91941b4a
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:48:46 +0000

    such laugh pool dark muscle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_array_as_str(array):
-        return str(array)
+def convert_array_to_string(array):
+        return array.tostring()
