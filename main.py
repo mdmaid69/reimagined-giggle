7bdaba9644ab75b56195dc0cb1db616207f6b649
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:38:44 +0000

    declaration container breathe surface hot

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
