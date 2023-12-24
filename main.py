35fe6bffc72443bb7b189aa9d968cf9057b3cf68
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:21:20 +0000

    shut way measure prove tune

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import sys
+  def get_python_version():
+        return sys.version
