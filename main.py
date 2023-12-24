b5bf455709b6cf3da7c2a1cf3b533bcd2f9540bd
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:54:03 +0000

    scale laugh degree into phone

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import numpy as np
+print(np.array([1, 2, 3]))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
