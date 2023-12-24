580690dad15fb36c5e7b79bb3184e73cdc5cf8fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:38:47 +0000

    friend reply thin settle culture

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_user_dict():
-        return collections.UserDict()
+name = "Python"
+print("Hello,", name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
