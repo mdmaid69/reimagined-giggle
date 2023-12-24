5839d3ca61eeedbb02ca7f7e9a3e3412fcd7911c
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:05:18 +0000

    long tidy soil Bb lost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
