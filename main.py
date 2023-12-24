0d982ad8d373bfd07a3cc4b92f51c250302cf22f
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:06:31 +0000

    runinto must shelter crash moon

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
