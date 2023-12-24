e4328c8ef455676ccb507a49ad517ced3ca18c4f
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:49:20 +0000

    piece herself stay ashamed produce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def reverse_list(lst):
+        return lst[::-1]
