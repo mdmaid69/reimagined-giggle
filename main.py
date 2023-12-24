b4ffb5aaf942996abb6e627c9f59221542e601a5
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:15:29 +0000

    pure future handle cheat last

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
