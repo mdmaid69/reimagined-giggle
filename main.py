65f82af0c17abf23908d9b97a95e5a260d16a911
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:09:40 +0000

    union match accidental scene soon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def find_min(numbers):
+        return min(numbers)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
