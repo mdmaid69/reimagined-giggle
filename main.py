c7069e786c1829600447b8f379202adf31a207ff
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:10:27 +0000

    justice brief nothing seem smell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_area_rectangle(l, w):
+        return l * w
