67a7943dc65ac402c032c98e89c2f76d61b4bbc6
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:55:01 +0000

    pool since towards send copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_volume(length, width, height):
+        return length * width * height
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
