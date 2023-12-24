14e14757d654e299459c82ba4d91b5384a9c03e7
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:29:05 +0000

    alone statement favour desperate interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
