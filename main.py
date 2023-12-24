3de8483ff8a36d5c79a8d0e228e8ba1ab9b88864
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:52:22 +0000

    but gap slightly ever minor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
