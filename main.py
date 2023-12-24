94ef2ef46ccc0d19ce3c8992e3d085372142d2e7
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:39:52 +0000

    shall furniture passenger top pleased

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
