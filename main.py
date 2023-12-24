e63e2434b54a84f6c6d4c4298abf0df09b834d5c
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:56:13 +0000

    runaway getout last start office

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def remove_duplicates(lst):
+        return list(set(lst))
