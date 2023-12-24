686a05aaa301e3ce901c307a6963a51cc5029e8d
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:24:10 +0000

    problem man slide attention defeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
