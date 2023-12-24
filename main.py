a48aa52c2916a6df3947cad161fbacc97c58de5f
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:09:47 +0000

    consist operation listen dance pass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import tensorflow as tf
+print(tf.__version__)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
