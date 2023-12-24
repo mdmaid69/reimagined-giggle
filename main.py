1997b8673a5c82b85bd4d1e802a539401e03b65b
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:36:26 +0000

    bringforward claim degree wait crazy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
