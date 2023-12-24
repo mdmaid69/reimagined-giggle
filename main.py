32a512fbd711d94bf5d20f8d4ace7ecb2e9057f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:55:11 +0000

    565end clothes well spin progress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
