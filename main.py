6326a1adac1f99d67fb9581c15b184d0bc4bae3e
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:36:15 +0000

    silent height offer calm thorough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
