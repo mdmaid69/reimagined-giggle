b1641f1f05919cd70882329c9ca844dffa188ed7
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:57:42 +0000

    finger patient perfect modern coffee

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
