a9529a16076770c5de03e839d0d3d00f4f2ebdb8
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:44:02 +0000

    danger building bang this address

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
