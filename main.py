acd1605ccccef334d1d914b589ef112fd846654f
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:05:23 +0000

    bottom Ll spring second threaten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
