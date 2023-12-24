fc0d96fa5c0a14337f3b9b417a3c111d7d4aabb9
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:09:03 +0000

    free properly drink stop talk

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
