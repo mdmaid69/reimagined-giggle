c23ab1e1809c4b06c8e741c008140cca5f8c6d7d
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:09:01 +0000

    your measure easy considering lift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
