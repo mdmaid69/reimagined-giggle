e04ff5c1d3cec6c442858e79257ab6bb1a319efb
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:10:05 +0000

    comfort forgive send turnup experience

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
