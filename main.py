e64ca312fd752a651d8f754dae72142389ce0669
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:17:22 +0000

    crowded light smile cry nearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
