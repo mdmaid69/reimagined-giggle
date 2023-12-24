cb87aacfcf657e7a104105baa5bf534af33d848b
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:03:51 +0000

    goaway operate honest equal Cc

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
