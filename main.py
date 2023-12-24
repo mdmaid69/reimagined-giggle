495d1739d995d4cca3020b580cc94626957760aa
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:22:23 +0000

    or not fit decide matter

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
