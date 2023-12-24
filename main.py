5ce8fc92bbb6331cb4a6c66d8729407fb33c7ab8
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:18:29 +0000

    letter total callat church workupto

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def greet(name):
-        print(f"Hello, {name}!")
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
