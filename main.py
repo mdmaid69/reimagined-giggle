d10de9153b27c09eec137aadd5bfc0f315fbd337
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:02:11 +0000

    decision Yy Vv 565end 179bite

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def count_words(sentence):
+        return len(sentence.split())
