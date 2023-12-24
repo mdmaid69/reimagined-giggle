b7bb402ffa16d5f2a29489e046bbf4d8cc062751
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:52:18 +0000

    couple traffic cheap children comment

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
