a54244b25b6bd8f78b9e0101159401cf4b671bb1
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:58:49 +0000

    active read tip consult autumn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
