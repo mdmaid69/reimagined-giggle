47156094b83d55d7a8863ca691eef75d0c406b67
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:32:32 +0000

    walk below crowd their judge

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def remove_duplicates(lst):
-        return list(set(lst))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
