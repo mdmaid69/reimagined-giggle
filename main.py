0e9de96b2bab9379811f31aa1b284a0d8bb337d2
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:42:50 +0000

    introduction priority exam bill prevent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  def square_number(x):
-        return x**2
+for i in range(5):
+        print(i)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
