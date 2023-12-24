1c387d010a4ab66f71534a00b9b62346efc04509
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:35:32 +0000

    lookback(on) fierce thinkover drawing underneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
