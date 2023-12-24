3a423cf1ec1dec17b361b5d4ced1d8b5b89c3b79
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:39:43 +0000

    fence heat workupto communication ago

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
