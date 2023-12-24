31342f78ff5dd8e7c9c80c6b19b6e6b680e1c617
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:35:34 +0000

    chat your tense wide hollow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
