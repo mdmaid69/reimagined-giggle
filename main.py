08b1602d38c933bb88fadc249ffda725bdde3320
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:31:47 +0000

    dig in comefrom national suggestion

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def count_elements(lst):
+        return len(lst)
