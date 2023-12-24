2b639faa57ccc2660b3bb0fb6bf7c355f5d089b3
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:12:36 +0000

    shrink hurt separate before sister

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def convert_to_binary(n):
+        return bin(n)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
