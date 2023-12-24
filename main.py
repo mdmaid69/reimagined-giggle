c29a9bf73f6545a653951e921cfa867b8f235211
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:28:45 +0000

    steam spoil similar judge least

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
