1042ab87b770b055de7041245ae05166d01941f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:19:14 +0000

    uncomfortable interesting advanced cough dress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
