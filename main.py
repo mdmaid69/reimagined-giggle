2534e0b620b6df96933eb0ce31d08bb40c60d76d
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:34:33 +0000

    someone to cut off save

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
