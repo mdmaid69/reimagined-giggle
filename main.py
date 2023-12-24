ce2db2c95bcc0fa1803e8a3863c3bc51ed5f3ce4
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:54:27 +0000

    rich already still breakthrough kick

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import time
+print(time.time())
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
