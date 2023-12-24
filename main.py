5e32fcc1825b298cc041c3f7621890a804bef2e4
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:23:54 +0000

    confused else cut trust quick

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  def is_even(n):
+        return n % 2 == 0
