860d24897ab3f4398297a02b2529ce3b47b8cba2
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:56:41 +0000

    relative wheel common anxious breakup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import time
-print(time.time())
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
