ba2d96bab2bf1ae03920c566c1e4199d33e8f38a
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:31:27 +0000

    mass convince minimum immediate active

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
