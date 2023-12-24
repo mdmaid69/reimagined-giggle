a4d2d1704dd148779d93c31b8f192dad81e210f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:53:34 +0000

    defend whisper equal entrance address

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
