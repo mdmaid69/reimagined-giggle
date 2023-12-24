ccf0f9e8a553813d442ea8ee044b09035a0d05e4
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:32:21 +0000

    scrape actually damp bus second

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def convert_to_binary(n):
+        return bin(n)
