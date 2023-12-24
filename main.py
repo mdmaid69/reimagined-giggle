8995b37bc9bd05caf61b8a031a2bf394c0d7dc54
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:47:57 +0000

    worried agree for open keepfrom

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def cube_number(x):
-        return x**3
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
