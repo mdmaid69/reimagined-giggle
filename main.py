dc7d8109112033bc0519edc3e4d6168fbeef2385
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:03:20 +0000

    quiet forget force note limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def remove_duplicates(lst):
+        return list(set(lst))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
