aad158756f5653eac4c678f5788213b4dbb0067a
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:35:48 +0000

    wear office relax general with

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def add_numbers(x, y):
+        return x + y
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
