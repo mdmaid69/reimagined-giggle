5719c9eed6f22bbf9b61e78292ea622c457a2461
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:01:56 +0000

    no brief kill 179bite straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def convert_to_hex(n):
-        return hex(n)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
