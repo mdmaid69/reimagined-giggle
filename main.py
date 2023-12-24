cbb4a882ec9af1d2e8eb2f8002d6aca177d32abc
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:50:24 +0000

    wave leg design buy guess

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def convert_to_binary(n):
-        return bin(n)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
