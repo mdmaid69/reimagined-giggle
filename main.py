94c77a6605faba7a2289a70a5961af1ca25add5e
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:20:28 +0000

    except computer stone increase order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import platform
+def get_os_info():
+        return platform.uname()
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
