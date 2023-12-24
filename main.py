b775dde2161557de177e11aadb0d9c3c095354ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:21:03 +0000

    use blow middle thinkof lesson

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+print([x**2 for x in range(10)])
