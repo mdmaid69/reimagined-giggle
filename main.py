dffba5ef9f30aa8a3d761b8f1022c054e9a0bd94
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:48:56 +0000

    object quiet grade load relief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
