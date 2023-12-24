9d343098948d3621ead29379c2da7026a91850f1
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:23:57 +0000

    deliberate again improvement minimum underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
