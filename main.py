49ac8b1329528adc2bc9b47d10035f165edfa5b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:02:52 +0000

    improvement address godown floor behaviour

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
