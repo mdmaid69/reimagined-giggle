9fc92271e287ad715f974e6fba329bb2cce1aa45
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:57:20 +0000

    crowded over uncertain major then

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
