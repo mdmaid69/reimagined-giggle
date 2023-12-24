ce2df8bf0de49c03fd3611d4a42a6025ad8c29de
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:52:21 +0000

    couple around send second squash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
