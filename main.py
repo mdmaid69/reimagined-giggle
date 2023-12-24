eef2a74bae588b3ac0b979a74eeabcae54f7448a
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:15:42 +0000

    disapprove error complicated wall surprised

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
