1d69099ba9e4e7548896bd6ba38bd25ae0b6615e
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:42:37 +0000

    rain room state behave gate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
