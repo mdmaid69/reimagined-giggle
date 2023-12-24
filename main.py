959c03b4f1fc26d82e51c9cbc22011b92cc5211c
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:05:36 +0000

    weather back keen either culture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
