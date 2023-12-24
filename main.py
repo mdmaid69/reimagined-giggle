0878c31fed1af88cf2cb99977e3df14b8c3435a4
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:14:21 +0000

    attractive further beneath search move

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import numpy as np
+print(np.array([1, 2, 3]))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
