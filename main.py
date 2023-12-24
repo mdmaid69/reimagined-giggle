be07df80b80fd93579ba06ffe04206f7337120ce
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:51:31 +0000

    question afford Hh fashion shape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
