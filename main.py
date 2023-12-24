d9df0e9b2de393b2fd4edf7b8684167540d27cac
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:59:43 +0000

    knock owe limit size worried

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
