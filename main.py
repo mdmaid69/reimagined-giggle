aa0b1a3e42fa540beb4a93c3874e9c9f21d74ced
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:59:23 +0000

    hurt shadow fit call guilty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
