5ecc0bcbc0de2efa93b1d133df822b7d84a149ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:59:58 +0000

    effort spread total reserve goodbye

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
