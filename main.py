49a96a1e33962ead1a6c5ce1649e27630222bd03
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:49:20 +0000

    takeoff dress go typical reaction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
