8cc2ba5a48bdca063791b543ec93376bdfb13235
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:13:26 +0000

    go idea anywhere comefrom centre

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import collections
+def create_stack():
+        return collections.deque()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
