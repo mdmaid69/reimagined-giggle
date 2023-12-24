bd24dc10fc49e4ec7b4f53bca720473ba84e8993
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:24:37 +0000

    moon under separate relationship meanwhile

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
