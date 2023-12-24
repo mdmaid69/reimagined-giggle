28f69ec6dc8e49ff0be3badc6f7943d2a12acb83
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:56:01 +0000

    style moon frighten thanks shadow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
