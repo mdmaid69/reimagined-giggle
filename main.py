fdf782502c0a4fe2e4a777a4bf8cb97f05e46d38
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:39:26 +0000

    before below value pick actual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
