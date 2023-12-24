b0f6a3cded91608a0775417253d2a2beef8d8cf5
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:26:52 +0000

    suck back also next thumb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_as_bytearray(array):
+        return bytearray(array)
