c1e851e2cdfc4919bb55da71f134cb1d8aa4af49
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:41:46 +0000

    introduction arrive twist apologizeapologise noise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
 import math
-print(math.pi)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_error_function(x):
+        return math.erf(x)
