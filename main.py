cedaee3998ba89dd580efd0e49d5bf7920089bf1
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:44:05 +0000

    passenger address Qq doctor Ww

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
