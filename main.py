aaecc8735f20993c1acf35abc24645318ec21eb4
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:27:09 +0000

    everywhere nearly left my wall

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
