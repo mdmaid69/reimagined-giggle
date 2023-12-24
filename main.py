0a7a43ed6a4b227dac8556a6cc653e5039157613
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:17:07 +0000

    forever payment modern dance race

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
