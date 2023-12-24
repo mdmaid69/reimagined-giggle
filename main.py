6289c2b69586a6f3f76103ef3f251fa86a5a6426
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:40:45 +0000

    successful dream roll have right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
