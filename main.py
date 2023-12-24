59763da0e6692630a1bf342687af2e2c83c61336
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:55:11 +0000

    trip grateful beneath suit face

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import time
+def get_current_time():
+        return time.ctime()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
