62c97fa5fbb79c932e3892f0a308d64d5ddefc31
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:04:06 +0000

    that disgusting swing stage something

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
