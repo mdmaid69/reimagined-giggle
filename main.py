2efc696e73ea3ddb21236899f06163b8b9aedbbe
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:06:00 +0000

    fact anywhere desperate detail practice

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_as_repr(array):
+        return repr(array)
