6a139af8c0b7ecf092c29fabb132ef3c6a94372b
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:03:48 +0000

    giveup simple accidental figure worry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_area_rectangle(l, w):
+        return l * w
