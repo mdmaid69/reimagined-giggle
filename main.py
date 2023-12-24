591a3bafa567b8021b92a354b5968ea5c34e08b3
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:40:24 +0000

    own tall base somewhere ocean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
