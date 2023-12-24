55ed03f6f50ccea140fa35b412e122c8d9c2aefd
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:00:54 +0000

    success now any play ever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
