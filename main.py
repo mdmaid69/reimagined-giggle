c984e331f01c8df67fcf6a4f3dafdd6d67889261
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:24:56 +0000

    declaration answer collection although practical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
