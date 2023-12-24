39aaf129bdfbabf703e987770137467624737549
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:21:08 +0000

    thinkover bury stamp dig sure

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_string_from_array(array):
+        return array.tobytes()
