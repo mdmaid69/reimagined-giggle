99a45b32f3c5f45fa5a53e07ffd5c58d19c95bdb
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:29:10 +0000

    straight cry callat unkind 179bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
