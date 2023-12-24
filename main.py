247bb14627802dbb98d73e7a80d51c4131fd2e3c
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:32:39 +0000

    plan bother regard exchange famous

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
