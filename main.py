15ee0458e87d6dfb8f7159ae61a73df1d1472038
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:17:49 +0000

    butter train software wherever along

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import platform
+def get_python_version():
+        return platform.python_version()
+import os
+def remove_directory(path):
+        os.rmdir(path)
