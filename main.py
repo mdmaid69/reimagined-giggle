d8320a6fed6a39e315cc045c0c25d2c131381ce1
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:23:24 +0000

    instead entry open quickly bother

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
