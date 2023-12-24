a1614206f999e5992a65c6d9386866ded4ec565b
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:37:09 +0000

    aware certainly easy sting plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_user_list():
-        return collections.UserList()
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
