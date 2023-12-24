7e4b150ae1afacb9b14f699e519e6e2e51f4e053
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:32:36 +0000

    probably pinch bus demonstrate survive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
