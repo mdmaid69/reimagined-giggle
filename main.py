de6289c2881fa10c915776360614623ce6a63644
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:41:37 +0000

    innocent again protect separate before

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
 import array
-def get_array_length(array):
-        return len(array)
+def extend_array(array, iterable):
+        array.extend(iterable)
