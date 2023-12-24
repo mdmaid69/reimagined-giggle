3245856bb2bfe162cf46810b41c85ad61cc30ab8
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:23:30 +0000

    notice gather error large criticism

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
