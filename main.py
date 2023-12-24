41955d7a76a6f313216e030178777f121d7816bd
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:04:35 +0000

    sentence home wall spread display

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-def find_max(numbers):
-        return max(numbers)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
