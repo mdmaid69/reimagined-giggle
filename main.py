b95aa3096ed3dc22c5dea0df391e4e411e1ddf16
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:08:46 +0000

    flavour rise goback(to) dry reliable

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
