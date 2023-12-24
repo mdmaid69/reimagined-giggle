b3e71b2aa1225b9792639e2db4af32fa81b242be
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:42:18 +0000

    success feeling load closed mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import numpy as np
+print(np.array([1, 2, 3]))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
