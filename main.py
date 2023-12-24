174b68205c78513ec792da40aa507555e6d2310d
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:08:43 +0000

    language yours speech getoff short

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
