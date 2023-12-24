c65ee595dad87f6e21f705eafb9a2eccc7e00c9a
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:45:32 +0000

    dick sister before abroad congratulate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+for i in range(5):
+        print(i)
+def calculate_density(mass, volume):
+        return mass / volume
