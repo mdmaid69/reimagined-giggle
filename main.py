132083cade1bdf9c32bd7475c5f9a8b993a1650b
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:26:29 +0000

    thought wind hope offer birthday

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
