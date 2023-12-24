f7e769f222410f221794ff06dccac9914c3ddc72
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:18:10 +0000

    sting tired wake declaration environment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
