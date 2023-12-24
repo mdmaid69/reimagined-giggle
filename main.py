686deac348aada54690713e15e7e408d8feeebad
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:28:33 +0000

    please opportunity brown name present

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
