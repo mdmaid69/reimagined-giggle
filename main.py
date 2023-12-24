589e1541eb39b0d3f76a5587bffd76465f355a06
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:52:09 +0000

    equal design winter attitude interview

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-print(random.randint(0, 100))
   import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
         plt.show()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
