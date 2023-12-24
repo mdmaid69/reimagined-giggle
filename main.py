7da07ab6565febed6ea13806561f14396c02fed8
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:00:16 +0000

    main make about unfortunate as

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_power(work, time):
+        return work / time
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
