7108f4e8a683902b3245c30ef279db8eb4e0332e
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:01:01 +0000

    stamp but start string who

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_power(work, time):
-        return work / time
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def remove_duplicates(lst):
+        return list(set(lst))
