5dc2fc8ea3985adf630d729d239c19d4180dda98
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:11:59 +0000

    of slip debt punishment indeed

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  def subtract_numbers(x, y):
+        return x - y
