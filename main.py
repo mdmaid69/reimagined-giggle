e03b7c42e74e44dad316dc928debd60f5c2bd1de
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:44:47 +0000

    shake thanks unfair completely proud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
