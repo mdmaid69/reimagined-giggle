083822d64f52b96e6538b6686b52d91ecddf9b3d
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:36:50 +0000

    ago reject calm appeal sensitive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import os
+def change_working_directory(path):
+        os.chdir(path)
