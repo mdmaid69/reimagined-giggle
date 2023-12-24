6c87129fe62819e8b0b48278b4b09b90b77d89e0
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:31:51 +0000

    success spot steam closely light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
