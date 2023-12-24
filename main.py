8c72f6a23a903c9d51c9701a82e38c46fc17dbec
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:36:34 +0000

    figure base blame fortunately machine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
