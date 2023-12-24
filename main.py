cefdb85a3d54300db7b656534749ca3bcf4e94aa
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:28:51 +0000

    limit keepup wait division law

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def count_elements(lst):
+        return len(lst)
