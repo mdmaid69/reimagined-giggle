70d2e607d70785e558297f85de509a85b670ed08
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:53:32 +0000

    sense schedule must motion sort

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
