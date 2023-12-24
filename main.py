c0187d4396efa0f57c1def596ce2ace5591e1480
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:18:16 +0000

    exchange tidy wise storm squeeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
