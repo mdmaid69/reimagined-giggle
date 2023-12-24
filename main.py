3d9bb1bafaa5fc7315a03baffa31ff238bed9355
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:01:35 +0000

    trick sticky tap know light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
