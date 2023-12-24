9c8a3555ad3c26d4682ea9c7f4ad4719458bed07
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:50:40 +0000

    above rank type breakfast vote

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  def convert_to_hex(n):
-        return hex(n)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
