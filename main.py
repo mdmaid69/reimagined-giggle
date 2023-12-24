04cdf40ac0b916d7b04d6975c600f6506040b2f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:22:53 +0000

    treat stamp major throw shelter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_work(force, distance):
-        return force * distance
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
