ac64f3939c22f0d2453af4bde049e739d35f645e
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:15:53 +0000

    busy thing material cry shut

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+print([x**2 for x in range(10)])
