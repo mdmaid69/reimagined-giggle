0548f809f872b63fc49a10454b4e583328da9102
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:06:49 +0000

    getoff coffee appeal across brush

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
