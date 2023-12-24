2c908ede0885297b10c336d45eaed9387e86686c
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:08:47 +0000

    exactly ask present shine if

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import tensorflow as tf
-print(tf.__version__)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
