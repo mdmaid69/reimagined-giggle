5659aaaf028096aa1de5666ff61c375bed67802d
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:29:05 +0000

    sleep estimate lock Gg mark

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def count_elements(lst):
+        return len(lst)
