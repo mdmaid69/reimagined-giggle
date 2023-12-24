7b3c90f7387321c8c0f9d67c6cbc1d1baa5a1044
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:09:00 +0000

    doctor wheel important pin mend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
