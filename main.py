eab5be50368a09f4dd60a409888f8c4616aafe19
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:23:05 +0000

    woman help record notice holdon

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
