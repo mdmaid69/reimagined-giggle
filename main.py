30df5d2f1f0977250964481cbb434a65074046fc
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:05:38 +0000

    mood sting every busy black

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
