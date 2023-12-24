69b7ff6f4ef42efa56d571c99bcfe691fabecc11
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:40:16 +0000

    confidence hardly us sea children

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
