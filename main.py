465b03928104fef3b209af0a18c373d63ea85d24
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:01:31 +0000

    keepoff juice ear show previous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
