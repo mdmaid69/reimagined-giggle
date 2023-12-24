2737cf5b3e8ecd9bcbe2ed0c02392f0eb8201151
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:14:36 +0000

    front post black target way

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
