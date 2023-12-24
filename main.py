52945ad691db696a5cae651f0115bab14da4d724
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:14:37 +0000

    strict post Yy yesterday consist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
