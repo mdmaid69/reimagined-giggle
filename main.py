2cdb7151e78a6001a03d92a35f3fea9a6b67ef13
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:19:29 +0000

    before second deserve they bath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
 import array
-def clear_array(array):
-        array *= 0
+def get_array_as_str(array):
+        return str(array)
