95425aec2dd2a23a765de8f2090c7c833cfa575c
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:45:10 +0000

    discovery conduct exploration drink him

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
