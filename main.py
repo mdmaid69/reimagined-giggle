09a0540351fb05be819f4c31c58d936aac4e68f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:48:35 +0000

    anyone hold demand careless terrible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
