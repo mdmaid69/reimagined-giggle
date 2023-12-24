fed69f2c11ee06fae28b02056e9b19ac92f89bc2
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:15:24 +0000

    takeback brown describe heat cook

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
