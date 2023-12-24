1de6cc05bd37765ad446f4aee504c568a93bf2fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:32:29 +0000

    point rise busy heat path

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import collections
+def create_user_dict():
+        return collections.UserDict()
