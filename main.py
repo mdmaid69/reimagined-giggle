76b9f1a27f1093148c2182766de23877ca95c486
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:08:06 +0000

    swimming breakin block clear call

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
