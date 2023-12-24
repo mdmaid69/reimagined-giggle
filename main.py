b8ff7f1f02a8b62f0e088fec701583a4be57a484
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:49:36 +0000

    respect room telephone callout nothing

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
