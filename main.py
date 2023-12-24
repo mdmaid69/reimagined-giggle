c8518dfc03156907c8b8edc1f21d3fc535bd5335
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:52:29 +0000

    behind fault opinion announce custom

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_speed(distance, time):
+        return distance / time
