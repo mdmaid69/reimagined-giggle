6a514fbffd88965819875c0fee6d60d17c408001
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:21:29 +0000

    past team usually hard contrast

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def square_number(x):
+        return x**2
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
