bae1ebf10807e6e3f074c48d1889875fac443ca2
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:56:15 +0000

    male somebody roll Ww about

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_power(work, time):
-        return work / time
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
