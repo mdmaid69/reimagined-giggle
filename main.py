5f6688212c6c306cbf926cff0c207c28334d5c9b
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:27:18 +0000

    mix shade tradition possession ocean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
