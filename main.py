09ddf51f1bcfa9c1fc6bbd10299cce2bcfd19e88
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:44:22 +0000

    doctor up breathe mine draw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
