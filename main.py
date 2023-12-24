ad81de7987c6b25a855d636c99374503ea411986
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:58:11 +0000

    ocean sticky branch slice everywhere

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_area_rectangle(l, w):
-        return l * w
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def cube_number(x):
+        return x**3
