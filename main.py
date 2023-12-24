fb3155dad76cbf8e16ecefdaa71bbf5279af06b7
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:55:09 +0000

    popular lead news scare feeling

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
