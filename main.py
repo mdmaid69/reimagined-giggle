e30ec659a7f127b43f2916b12b40008a147425d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:33:09 +0000

    emergency welcome disturbing spring brief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  def reverse_list(lst):
+        return lst[::-1]
