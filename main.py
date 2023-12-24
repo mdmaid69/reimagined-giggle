1c846d796ffab1d08fefa36afb31c635dc72ec50
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:53:10 +0000

    relative excited careful control danger

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
