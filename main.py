2d4b2ed0d6c7e6d5ae41da9c846d94de3157ba64
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:02:44 +0000

    comefrom close smile reduction plate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def is_palindrome(s):
+        return s == s[::-1]
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
