f7112d178772e37f16fdf1b5702b7de0f7ecbc1b
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:48:44 +0000

    national drive fast perhaps survive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_as_str(array):
+        return str(array)
