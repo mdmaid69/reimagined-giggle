ae823fcc1f94c31c40eb258ff2874e3a41c15f5b
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:50:13 +0000

    active plate turnoff key in

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
