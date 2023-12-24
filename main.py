f71a5b0f8a2f0e12aa433e47a1b383b168350bbb
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:02:40 +0000

    time now forgive suddenly wait

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
