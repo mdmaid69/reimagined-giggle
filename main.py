ccf8269280b6b0fa22f855ad6279c5703bd8fb2f
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:42:16 +0000

    about design passenger should comeback

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
