35435061535b190fb6cd7ba52b91d39094a3ffb9
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:44:39 +0000

    risk hurry ocean buy decision

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
