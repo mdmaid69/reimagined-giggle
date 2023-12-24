229988a3470268be6a27380b44592aa51a78b8ae
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:09:27 +0000

    inform gradually race talkback regular

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
