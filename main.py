43909f91d59afd2dfdf4aa3841e11bb998ef2e3f
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:35:36 +0000

    win smooth threaten previous tread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
