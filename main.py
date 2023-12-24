6b687452d3411e5802a276d8435b72986a0fc17a
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:07:00 +0000

    steer wind pretty identify brother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def is_odd(n):
+        return n % 2 != 0
