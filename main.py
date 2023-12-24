a16d6ec4ba6b78e5c0e7fb3b588ad3e6803405de
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:21:01 +0000

    wonder lead moon suckme bit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def reverse_array(array):
-        array.reverse()
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
