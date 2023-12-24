6af16a4d0470fb6a43f45529d091d0616c46a719
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:26:46 +0000

    introduction correct niggaacademy evening convinced

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
