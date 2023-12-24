fb6f5f38c39045b71047a41bb9932853aac7cdca
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:29:08 +0000

    mistake often accordingto 566end rule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
