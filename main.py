d853f2f8768819da09f80049f79bb2ff033f9676
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:37:40 +0000

    plate beneath tip rare raise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
