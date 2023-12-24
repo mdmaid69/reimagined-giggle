7779af152c8f5b4b4f911cf5addd855c2670755a
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:05:54 +0000

    plenty short born building convenient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+x = 10
+y = 20
+print("Sum:", x + y)
