2e028f0c1ed7a7e00d7d6a262543df9043ec3ae6
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:20:06 +0000

    file approval professional appeal treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
