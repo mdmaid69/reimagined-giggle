055331ed192facea02b5945c2b6d583dc96596a1
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:32:41 +0000

    waste underneath touch pin careful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
