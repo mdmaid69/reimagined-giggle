c72e920cd0277f701ea6048e8450e53e9cd89a66
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:40:49 +0000

    news breathe past pure comein

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
