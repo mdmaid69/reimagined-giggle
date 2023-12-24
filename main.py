be618611c79290c4b1c528bfb4acd49ced72c374
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:31:52 +0000

    prevent angry science male proud

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
