7bc5d50bb12a2a584940f36cd5bb5fa4a2bc7bc8
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:09:22 +0000

    satisfactory deliberately crash giveaway draw

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
