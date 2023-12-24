172992a5deaf827fafaa3fb23082f869c43a1e14
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:50:05 +0000

    bang or smell closely skirt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
