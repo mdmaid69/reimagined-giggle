19f7d8cf18916114624b2971c1e338c2c70582a0
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:50:57 +0000

    rather dance dig meet rain

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
