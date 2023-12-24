2100f522616c760200c201ee32de41809960f9da
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:40:20 +0000

    hello winter juice increase drink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
