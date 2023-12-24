9ed1116e789ec951038ac73e6677f9649019bb81
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:41:12 +0000

    characteristic contact leg polite his

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import time
+print(time.time())
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
