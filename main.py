f29930d88d79d71f0146dfc6f5681b00e64cdb2f
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:51:26 +0000

    street other any uncertain production

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
