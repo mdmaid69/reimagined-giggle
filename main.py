ce08cc5596535b59824facec22a5b8513ec57a38
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:44:47 +0000

    software getout recent natural past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
