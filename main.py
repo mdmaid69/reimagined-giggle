c53e25e6fda61207573589d964beb24778ce69c0
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:54:22 +0000

    its nigga answer mean thumb

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import getpass
+def get_username():
+        return getpass.getuser()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
