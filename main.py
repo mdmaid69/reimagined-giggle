4073a7cd00193759997456caa9ffcf0e7a381f28
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:14:13 +0000

    content exact used pull event

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-print(random.randint(0, 100))
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
