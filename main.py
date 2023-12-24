1154e7ece4e1db9e19fcde3c1f749df3a692f039
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:20:10 +0000

    fortunately alive tense phone cold

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
