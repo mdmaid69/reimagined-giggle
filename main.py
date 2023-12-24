65e1a83ee72dc8f14a34b5552cae519384c8a955
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:46:32 +0000

    turnback noisy sensible power earn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 def calculate_mortgage(principal, rate, time):
         return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
