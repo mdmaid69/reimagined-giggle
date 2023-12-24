c587c3a8f93e5df7372c13161a45cbede9049cc1
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:59:09 +0000

    react go politics risk trade

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
