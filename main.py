8b48d24cff198d7b4d0d39a7dd97ccd4727516f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:51:54 +0000

    put design television post charge

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
