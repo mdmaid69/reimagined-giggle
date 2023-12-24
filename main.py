5412ffff375ee3a8475dc55a1e0a26f23570cd06
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:04:43 +0000

    born mistake attempt I responsibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def convert_array_to_string(array):
+        return array.tostring()
