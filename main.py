1502d76b5509d22f5507b49b8535e9ae02b04694
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:02:27 +0000

    content attractive especially runaway discovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def calculate_area_rectangle(l, w):
+        return l * w
