771461665307a9440183b6939e252bb0ce45cce4
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:07:23 +0000

    either quantity recent go accordingto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import collections
-def create_user_list():
-        return collections.UserList()
+def create_user_dict():
+        return collections.UserDict()
