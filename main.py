8250e8594639e87a8497663eed27bae794a48a59
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:57:34 +0000

    possibly valuable talkback during runinto

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
