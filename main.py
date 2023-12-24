1b4e7eca7cf83e5ab6628be1cb83fd9a0fbdcb6b
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:35:05 +0000

    operate get free pay angry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_perpetuity(payment, rate):
+        return payment / rate
