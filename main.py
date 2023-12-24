ee315a1287194f1d6f80fdf3f8333f248c4a42de
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:59:41 +0000

    load noise calmly gap act

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
