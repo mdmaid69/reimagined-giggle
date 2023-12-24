c4e5d3cb4333c4a5f53a3b22a54d03e6b8436f03
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:25:49 +0000

    abroad impressive cause everywhere to

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
