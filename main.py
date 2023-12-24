a7cdf29f22faf6c8b487f99d87d634ccb8111e35
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:38:59 +0000

    window business fork write you

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def reverse_array(array):
+        array.reverse()
