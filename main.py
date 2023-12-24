b2436128bb1db9110dbb9ecda369305447e150bb
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:41:33 +0000

    proof visit fill challenge to

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
