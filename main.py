d55e8e2d0c1571db141c3809caa80cc5ddba6654
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:07:58 +0000

    exit hand anywhere standard range

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
