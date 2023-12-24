2f36e41dac846800904eb2be2590ed568c328ba0
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:31:01 +0000

    you for ignore minor report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def find_min(lst):
+        return min(lst)
