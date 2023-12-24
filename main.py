cd481ab0d1f71abb420e64904af9ce0b906d20b5
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:54:24 +0000

    opponent feeling common worth noisy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
