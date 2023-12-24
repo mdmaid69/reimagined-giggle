23137b7e9b4ad53c95d356074a3745cbb33adf6c
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:20:08 +0000

    talk jump explain region level

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_power(work, time):
-        return work / time
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
