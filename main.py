1a1bb148b7a3e2df246063bf35e8ee3736dab2d2
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:11:03 +0000

    forget takeback oil dear on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
