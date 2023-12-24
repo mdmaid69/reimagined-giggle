2b83ff7d13c6bfbaf4663ee3f1acee9228fd773f
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:07:08 +0000

    square heart runout noone instead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import getpass
+def get_username():
+        return getpass.getuser()
