6734a85799392fdb0b87e7f1196c9ebcf0e33c09
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:34:47 +0000

    refuse book contrast thick realize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def find_max(numbers):
-        return max(numbers)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def sort_list(lst):
+        return sorted(lst)
