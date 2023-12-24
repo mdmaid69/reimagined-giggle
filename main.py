8317d2992cbe810ab38c2cad20c2e9615e4818b9
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:25:42 +0000

    occasional care better small suffer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
