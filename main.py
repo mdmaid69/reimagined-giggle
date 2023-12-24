1ce4725b03b411646e3dba610dc0830d2f83efc5
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:24:20 +0000

    free dig interrupt interest beat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_speed(distance, time):
+        return distance / time
