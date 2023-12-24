2e839639628de1023d1f1ad893a30fd4bfed7a0f
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:17:41 +0000

    seat ticket large considering contents

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
