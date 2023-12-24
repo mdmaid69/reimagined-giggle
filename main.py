9379f3a1d550db022d16edeb725a6e70b03187ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:47:40 +0000

    past lead accommodation exist load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
