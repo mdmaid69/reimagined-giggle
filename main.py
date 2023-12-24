cedf6268b8b04e904f4d06cb32ec661960135bd3
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:59:31 +0000

    temper 568energy getup nose balance

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_min(numbers):
+        return min(numbers)
