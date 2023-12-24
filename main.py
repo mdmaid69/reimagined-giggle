6db7204bc4d348330ec269cdb8331f9346d534d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:26:30 +0000

    crazy drink exact event arrange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def add_numbers(a, b):
+        return a + b
