3bf4f78b19463c7558d8f44a627bd847fa7d0dc4
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:45:22 +0000

    teach pin thing foot contact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
