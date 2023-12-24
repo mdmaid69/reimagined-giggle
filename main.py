59bc8c8f15185bd1168db42297317c6fd66001c4
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:48:27 +0000

    apart big classroom fierce high

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import numpy as np
+print(np.array([1, 2, 3]))
