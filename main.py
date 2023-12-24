797cb1858b099ed9e3d278d8884bcdeb8c9146d7
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:24:09 +0000

    sit them bite emotion human

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-for i in range(5):
-        print(i)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
