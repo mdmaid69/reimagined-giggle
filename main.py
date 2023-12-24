e63dec715406c02118a1bc334f339f887f8115f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:26:52 +0000

    justice blame stone total lookaround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def clear_array(array):
-        array *= 0
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
