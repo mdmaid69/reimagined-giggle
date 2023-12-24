25b8850e1c698f918c445e776065bfebc84391f8
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:24:42 +0000

    bunch 572enter describe influence door

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
