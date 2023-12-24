b140e7ab283648c43806bbb599c0a69ac6ee7ba5
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:23:58 +0000

    maintain bury second talk fresh

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
