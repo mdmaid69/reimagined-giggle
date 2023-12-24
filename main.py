c1a68f6b46726dab8adeb0f7269176839510b150
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:05:49 +0000

    impatient cruelty comment produce comfort

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
