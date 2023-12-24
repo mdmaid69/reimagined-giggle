10b31f89a0c4d98e485b324b1eb97c5068f48a5e
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:47:10 +0000

    wise separate steer crash relax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
