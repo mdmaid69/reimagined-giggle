5565e97a185d22e6e028d69fa1882b70e4cee1bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:02:15 +0000

    cover band remember say Ee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
