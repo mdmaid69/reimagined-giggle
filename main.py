84bb84c1db9e0e949564c2742cc01dd95347447f
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:58:58 +0000

    opposite Zz not term contrast

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+for i in range(5):
+        print(i)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
