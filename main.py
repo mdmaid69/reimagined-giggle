4aaeff427b6f9566980171760596ecd216566b0f
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:11:58 +0000

    life wet mean natural small

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
