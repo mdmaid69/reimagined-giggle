b9830cb7158a64b15b71ccd705e4be24c04a2762
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:17:09 +0000

    giveup continue score second back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import platform
+def get_python_version():
+        return platform.python_version()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
