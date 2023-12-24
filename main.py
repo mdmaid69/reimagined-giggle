cfdaa9223333db51d10c16b89004efb229e3a615
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:29:47 +0000

    favour measure somebody allow fence

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
