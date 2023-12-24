4598737f84c7d437c273642aed50868d0d7b360f
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:47:48 +0000

    whom necessary 573entertainment apologizeapologise storm

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
