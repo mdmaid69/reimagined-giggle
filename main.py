ef70976f320b83435d476a7b5a576de9022e3cd9
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:15:21 +0000

    phone add though original rub

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
