5298facc49b4101c0fdf925d80660a734efd4083
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:42:56 +0000

    standard Gg later platform cum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import sys
-print(sys.version)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import logging
+def log_message(message):
+        logging.info(message)
