fa77e8c9720b5bc11134018d4ae41f90315ac40c
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:20:50 +0000

    lead detail middle various wander

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
