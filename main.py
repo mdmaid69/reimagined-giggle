d7d36f7ae35735861b78d5b93e4075f42d594b4e
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:01:24 +0000

    muscle collect breakfast wish visit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
