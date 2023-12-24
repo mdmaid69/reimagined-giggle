69f720657b2b5867e5d2d263d2c4cdea5e690a07
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:30:18 +0000

    skirt uniform far fill environment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import sys
-def print_python_version():
-        return sys.version
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def find_min(lst):
+        return min(lst)
