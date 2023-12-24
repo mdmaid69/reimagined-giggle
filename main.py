cbb96ede6f3791b687c54e2b7ac061789a1d3e4f
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:49:36 +0000

    mass bringup while swim thinkover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
