64a770fd6e8f4ac4b76db9be6132fab3bbdfe08a
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:49:14 +0000

    holdout persuade Zz model thin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def square_number(x):
+        return x**2
