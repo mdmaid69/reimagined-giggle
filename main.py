bbfac37ac8a000e36d8a388f4d576f7ea54b4478
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:36:29 +0000

    harm favour area care one

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
