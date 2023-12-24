13cf2f12dc1aedcff13239e467ffa22be92339d6
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:15:28 +0000

    business open narrow discuss urgent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
