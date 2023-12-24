fcc805bee4b0da7dd3e23c0ca24e75d2cf445706
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:40:41 +0000

    home proud night slowly far

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def calculate_acceleration(speed, time):
-        return speed / time
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
