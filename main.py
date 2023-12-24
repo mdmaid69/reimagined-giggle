a1f92def1ef336baf3c005017965779aaae082f8
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:21:09 +0000

    really ring natural clothes goout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
