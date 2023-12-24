70d4527d3e3e8b845b8e0a89fb77b31fc14f7c80
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:20:06 +0000

    sky collection slightly either work

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
