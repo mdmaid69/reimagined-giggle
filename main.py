c0d3136f932f1d7c73c683820073efebe378990d
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:44:44 +0000

    naturally pool scare over total

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
