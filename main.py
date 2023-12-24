f2b0597440cb4082a7277687f3f72a7a969fd5ec
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:47:43 +0000

    upset rescue busy base improvement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
