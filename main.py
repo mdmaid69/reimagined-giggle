e2a40087175507ec25f10bd4a2b7b54ed2d6ec84
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:49:07 +0000

    divide above pronounce this effect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
