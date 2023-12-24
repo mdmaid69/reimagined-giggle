933a9e159f831c800d8958a10708ad78497eabbf
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:37:26 +0000

    pause 571enough no acceptable noone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import time
+def get_time_since_epoch():
+        return time.time()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
