07ac66573eff880a8ce51b4796dd1d3cf69208e6
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:23:29 +0000

    makeup mind go dance call

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
