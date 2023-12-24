6b6176a2c528eb352c75fbae71b48bd9bc134446
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:21:59 +0000

    ring off negative good cause

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
