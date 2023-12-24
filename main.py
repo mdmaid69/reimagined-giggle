7bc4a3f65f0f898bf0b36afecac5e5c93082475a
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:29:33 +0000

    art partner argument half throughout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
