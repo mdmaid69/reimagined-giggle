0540f42e3bf87e648897963203ea66556ed02ddd
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:49:40 +0000

    still violent hardly sport separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
