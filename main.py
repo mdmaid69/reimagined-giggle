02b0649df14f6ab95782c3b4826ff53059cce4c0
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:38:33 +0000

    pipe but regular set pass

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import time
-def get_time_since_epoch():
-        return time.time()
+  def count_elements(lst):
+        return len(lst)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
