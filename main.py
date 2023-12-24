ffe0398f2be929b828be521b0d9aa8cdf744a143
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:35:25 +0000

    gate breakthrough complete sweet game

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
