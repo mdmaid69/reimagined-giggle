83b7b82e5ac4ac1e526597c838ce3a510b7a8cc1
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:47:30 +0000

    trick character persuade impressive every

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+for i in range(5):
+        print(i)
