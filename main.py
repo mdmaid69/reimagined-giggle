4c5a0c5961a4a07ad1b776e9cd50262feb8856b7
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:13:47 +0000

    suggestion ill programme doubt sure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
