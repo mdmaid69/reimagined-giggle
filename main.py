3d947f51318ed89e4bddd61d62ff334351687497
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:53:01 +0000

    slice minority constantly light intend

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
