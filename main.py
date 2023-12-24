da4ab32a7f897b2794e0f12ce1132ccad0b710e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:50:02 +0000

    final destroy goout preparation minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
