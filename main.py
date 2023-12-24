488607b5aec04637a13c5b7c45ff3a0625063e64
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:13:57 +0000

    generous Nn reject stir birth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
