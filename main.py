f3a0ac7f10891c7f4c7b94e2e1f6a431aa6ed674
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:24:53 +0000

    away embarrassed custom birthday pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
