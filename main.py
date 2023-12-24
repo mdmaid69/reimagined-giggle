b531aaf2c365758cdc2a5d195ec0150cd841a1f8
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:05:28 +0000

    coffee Gg minute greeting takeover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
