2161dbd85d14dd24f5ba0e858e46f11d7c99b25e
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:38:49 +0000

    boil top point badly burn

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
