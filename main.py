be61769702a833688c2bf3937989a17a5e0f6ffc
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:53:05 +0000

    problem mood sleep basically mess

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_priority_queue():
-        return collections.deque()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
