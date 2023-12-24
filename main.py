2757a0a0584fff845a44b5d801345c7a2e3f126f
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:01:22 +0000

    Ll wrap accommodation turnback approve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
