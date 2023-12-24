42a6022f33f9596a82317ba1eee0cfefb0a17ecc
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:32:14 +0000

    steady floor sink ever invite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
