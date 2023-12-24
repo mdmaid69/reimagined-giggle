98d0431b13440bd12b5e351ba1c39753f2127ab2
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:03:32 +0000

    positive my trial whether worried

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
