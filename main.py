bb698f075c291351c8f78d350ab65380f1aed2a8
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:30:42 +0000

    top communicate laugh profit loose

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
