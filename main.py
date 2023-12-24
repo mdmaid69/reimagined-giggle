4873d3e8de3dcb3dda244c4d536d18375a27dd95
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:38:30 +0000

    commit support comeoff by valuable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
