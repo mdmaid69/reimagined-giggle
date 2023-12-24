489f172f8f4cba33c798248b2c5c27834c757a37
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:11:04 +0000

    single squeeze fit concern date

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
