7edd39b416f902ca9a936c5ba92f909fb4c6751a
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:43:50 +0000

    punishment prefer free money throw

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def calculate_area_circle(r):
+        return 3.14 * r**2
