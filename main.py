f88bd4059a715d6ab098c00ecf22c0df7bae0af7
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:46:53 +0000

    truth ability give cost contact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
