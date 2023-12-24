ac70cdf863400756d03b49a632b4dec78775b688
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:43:29 +0000

    condition clue stamp determined upwards

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-text = "Hello, world!"
-print("Characters:", len(text))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
