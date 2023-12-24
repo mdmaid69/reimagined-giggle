454a53cb447481690904c53376effc038cd1ee08
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:29:51 +0000

    pencil available holdon tall early

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
