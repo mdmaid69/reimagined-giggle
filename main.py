9640ab81ca8294019c46ef458a82b2964c6ae14c
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:27:53 +0000

    size over which sing leave

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
