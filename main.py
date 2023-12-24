906edbff544b571b579744cd4e212720a8f1b803
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:22:12 +0000

    waste shall answer until pleased

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
