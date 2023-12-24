e445d852cdf5049ff0108bf907fcbd4038dc91bb
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:36:18 +0000

    advise bend trust keepfrom middle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
 def calculate_energy(mass, c=3*10**8):
         return mass * c**2
