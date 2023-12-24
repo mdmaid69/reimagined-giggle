f147e8337f11b6afc907b388ebdc99a0f6f4ff92
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:44:07 +0000

    expensive mixture thankyou development fierce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
