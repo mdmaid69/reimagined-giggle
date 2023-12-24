2de6afd602744f7f230497f40e130d1a2d495dbe
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:05:21 +0000

    work bank through fast excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
