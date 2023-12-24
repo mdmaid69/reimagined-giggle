b74f04ddc5647c84ddef577bb27f02083e07db0e
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:06:29 +0000

    charge pool tidy store back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
