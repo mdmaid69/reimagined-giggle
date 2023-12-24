1e0ee012f903f4cc362d0f6ab244cd49e91ea4e8
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:20:40 +0000

    that find mention shout conversation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
