0c5302b0b27466bc10c0340d5afeabc7cfd8b00e
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:59:23 +0000

    phone finish tool along weigh

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
