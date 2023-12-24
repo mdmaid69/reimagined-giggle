6a265bbcd38260740f643d8b73c2433644ef928e
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:33:02 +0000

    swim science dare consider convinced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import math
+def calculate_floor(x):
+        return math.floor(x)
