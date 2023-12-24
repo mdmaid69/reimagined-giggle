5e8bd666e88293e5de8b22b217eee2c6b7a2dd79
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:17:26 +0000

    cheque jump prize dare pick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
