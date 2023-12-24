1ff218c70152da02e525834b9879ccc54e06ec89
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:26:55 +0000

    suppose teach through comeout lesson

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+for i in range(10): print(i)
+import platform
+def get_os_info():
+        return platform.uname()
