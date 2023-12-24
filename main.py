c5058decbea22029f5a5d6289fcf876b120fd1ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:51:45 +0000

    control basis custom drop sure

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
