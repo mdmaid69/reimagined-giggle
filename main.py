73a88cd904b0ad693042ebc9d9fecff2eb1e96b3
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:33:52 +0000

    track minimum development breakfast figure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def split_path(path):
+        return os.path.split(path)
