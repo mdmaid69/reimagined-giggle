5b127e135be1c7a1318ad83464341171eb5b80a2
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:58:56 +0000

    well blame sudden air kick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
