cf7e95eb6bae655271758391fca07fd542cbb345
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:26:16 +0000

    exact expect choose in watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
