eaeebec5374fb7eb96d46ba7dbbfa3f34ef1510e
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:42:17 +0000

    definite weather mad oil minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_current_directory():
+        return os.getcwd()
