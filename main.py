2ea5b1b729370a2982c472934e93e1d52c25ebd6
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:39:16 +0000

    century birthday lie takeaway wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
