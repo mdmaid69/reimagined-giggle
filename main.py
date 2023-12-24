da0e02072e894e31a956dabbc3ab654c0646c3c0
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:51:02 +0000

    fairly rush little lead explanation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
