6257c25681a1241fff0b610bc3271db3991f492b
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:12:05 +0000

    hate Ss song press building

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def sort_list(lst):
+        return sorted(lst)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
