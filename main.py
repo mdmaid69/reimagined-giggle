55f08b6054c37ca2b005a398e4628855c7ae85c1
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:34:02 +0000

    Jj regard sore beginning else

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_min(numbers):
-        return min(numbers)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
