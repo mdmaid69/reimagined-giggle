13e5e6f2b77072abd224eff12cce8944fd6786ea
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:02:15 +0000

    sensitive motion break otherwise make

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+for i in range(10): print(i)
+import os
+def remove_directory(path):
+        os.rmdir(path)
