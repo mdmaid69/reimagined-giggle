0210a1a30a1cceb8fa8c578eaf8a788586a20d03
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:11:46 +0000

    ring imagine burst pleased side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
