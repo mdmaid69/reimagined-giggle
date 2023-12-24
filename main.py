356901001420fab1c136b0a77ded1fda9dafd6f9
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:22:46 +0000

    occasional fence getaway empty punish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
