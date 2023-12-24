63d9bb54e089a480b36474ba1c6a41d891be9e1e
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:43:58 +0000

    regularly night calloff rush female

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
