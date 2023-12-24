71e1e3d80222d8f13443a1bc04da640d52967657
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:18:32 +0000

    convenient broad container eye comeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
