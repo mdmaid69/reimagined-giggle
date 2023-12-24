580046e5dcf53845de96698418358c2cce112912
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:52:32 +0000

    charge neither lie we precise

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_speed(distance, time):
-        return distance / time
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
