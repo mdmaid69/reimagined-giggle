3750f9cb244e4d61af95f7d318e97d218ed94c07
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:41:16 +0000

    form storm first badly play

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import datetime
+print(datetime.datetime.now())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
