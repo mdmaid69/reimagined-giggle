c5c1cb4d20dc3e5d2ff265f4694993669aa08d45
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:22:42 +0000

    rid steer rent pipe choose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  def convert_to_octal(n):
+        return oct(n)
