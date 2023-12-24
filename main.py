25aa46b2391f11d956cd9eaa0f599c74f40cfffc
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:20:50 +0000

    unit best treatment fit big

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
