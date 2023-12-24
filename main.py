72bffa1038f8933cfd8471ca345b0d97aea94631
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:33:50 +0000

    thank land regret he leg

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import datetime
+print(datetime.datetime.now())
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
