7c4eb5c0fd33158e3ac2b060abb6f1356f50812f
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:19:17 +0000

    awake interested steady house connect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
