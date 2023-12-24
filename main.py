3849d447937bc32219deab6411a02cff1e0fc694
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:35:09 +0000

    but pause future tool sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
