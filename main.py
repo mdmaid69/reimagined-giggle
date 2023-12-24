93b1eb0d4aeea7f55bac85626da67c6d3dd68fa6
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:53:56 +0000

    callup note quickly comein doctor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
