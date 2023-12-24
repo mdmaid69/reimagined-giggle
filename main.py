6db050753306c16f75baf88c53ff74f7a1fd4d81
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:43:28 +0000

    platform suddenly interrupt left cold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
