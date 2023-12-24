4913608c6dd36dd5b8b3ffa970205208e8092fb1
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:52:39 +0000

    formal unlucky health pause children

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
