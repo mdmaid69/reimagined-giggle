fa2c3234421bdd0e6bd611a76cfcb8ca774de848
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:41:29 +0000

    disappointed extremely early reject fashion

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def convert_to_octal(n):
+        return oct(n)
