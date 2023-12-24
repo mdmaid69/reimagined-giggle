d03acd79aa7ad5499433a374b1eb962f569221ff
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:48:01 +0000

    agreement offence confidence night slice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
