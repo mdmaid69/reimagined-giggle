ca2f331a70011a5668d3bae97ed1f7b26f528647
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:00:44 +0000

    suit duty well why sell

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+print([x**2 for x in range(10)])
