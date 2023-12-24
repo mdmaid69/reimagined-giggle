2ad09300d5e34ef1d4948fdeafe2e21443c7a5d1
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:45:13 +0000

    waste how pause crisis regularly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def find_max(numbers):
+        return max(numbers)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
