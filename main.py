a15263bc8ea9244886140eff7e5151d60e22cab1
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:09:19 +0000

    row I angry help giveup

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
