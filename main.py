f939669f0cbbc929985f3fecebc7cb6032ae2adf
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:57:00 +0000

    shrink different closely his height

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import tensorflow as tf
-print(tf.__version__)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import datetime
+print(datetime.datetime.now())
