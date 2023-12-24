5d24452f94c31fb85c7c1a42dd003295cf15afc4
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:54:46 +0000

    comedown scream play tour freeze

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_typecode(array):
-        return array.typecode
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
