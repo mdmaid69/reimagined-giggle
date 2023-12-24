c76f9a7e165fa007f66dce1adc16b7e811ac9056
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:17:49 +0000

    previous crash heavy depend bread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
