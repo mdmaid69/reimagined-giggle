51282014c9255dbe942555fecfe6c95399505555
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:45:36 +0000

    order hot giveup mysterious object

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def sort_list(lst):
+        return sorted(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
