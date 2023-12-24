d4e8b51a401a7ffe6893f675655514357df1a2ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:53:34 +0000

    swallow surprise flow total path

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
