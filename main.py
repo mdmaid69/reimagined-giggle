f7dc9395e77b38a689f3a59123ba1ebfe35a4834
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:35:29 +0000

    yourself clean speech explain task

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
