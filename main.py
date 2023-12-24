1d7fa98b9bda444fc1bddbf1ad516d7e5d3a321f
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:02:20 +0000

    lucky watch lucky expect tradition

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
