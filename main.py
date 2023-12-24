7eba146ae7cf03f4ec07484acae237b1d783a7c4
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:20:50 +0000

    squeeze getaway quarter future bunch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+text = "Hello, world!"
+print("Words:", len(text.split()))
+def reverse_string(s):
+        return s[::-1]
