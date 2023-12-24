0e8032cc0b9aff9df0228996e4e004783b00ab79
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:18:31 +0000

    danger cheek giveaway next introduce

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
