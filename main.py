6abd248df2fd5277c1edaa2d866cad7dee982386
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:37:54 +0000

    disappointed insist estimate unit hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
