f61e4f5935d07da0931c09e13002bd88bd120afa
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:17:28 +0000

    trap middle image load tongue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
