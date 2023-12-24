6cddeb6f99a0fdf93d8ae413a0f35e62be1530ce
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:08:09 +0000

    clear cross activity succeed announce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
