d8a6db30933a2a28bf09216b824c5439c71e66e8
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:35:44 +0000

    567enemy way nothing bring floor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
