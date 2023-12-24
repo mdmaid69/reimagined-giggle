dbb25a1ec1522e3f9eb160f25593a10d759af88d
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:00:21 +0000

    comment flight movement light ticket

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tensorflow as tf
-print(tf.__version__)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
