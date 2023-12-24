17857b7898fc9485d7c683c786e5d8039caf100f
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:18:08 +0000

    drop protect plain plain hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+def calculate_work(force, distance):
+        return force * distance
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
