9d2f38af0c4d2c28974b8222c548a10152ecc9ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:59:03 +0000

    actually everybody worth object shout

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import random
+print(random.randint(0, 100))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
