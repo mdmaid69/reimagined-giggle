8014fbfa973ea7b1e80b76c07c4820bff78f1b7e
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:26:08 +0000

    familiar speech male comeround listenfor

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-print(time.time())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
