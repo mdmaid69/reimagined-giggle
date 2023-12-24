9dd31f4f9590a949b276bc5c151f2ccd339b41a9
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:53:55 +0000

    quality challenge welcome later people

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_max(numbers):
+        return max(numbers)
+print([x**2 for x in range(10)])
