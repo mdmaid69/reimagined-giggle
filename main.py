1e6d379a24d3729387c506daa6701d40c90c4442
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:05:04 +0000

    decide attitude radio rank under

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
