03ec724533f6c883fbcaa1c12d494769cf227025
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:14:31 +0000

    influence achievement it interesting back

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
