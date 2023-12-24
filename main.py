6879fdf34386673e6a8a17d72f4131d226986b77
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:54:22 +0000

    negative stir excellent average imagine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
