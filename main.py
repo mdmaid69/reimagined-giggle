4d649349d3926b5c2308939b7fff00195a94b14c
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:44:41 +0000

    spring law sure Hh realize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
