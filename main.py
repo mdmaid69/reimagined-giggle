f5c277ce44bf5063863ddd0228f05c4c1c2969a8
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:48:50 +0000

    afterwards professional thick thanks real

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
