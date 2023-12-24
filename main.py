1c172617eb99e15f8be22ea07bbba0a85bb2014b
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:30:51 +0000

    sport touch attractive buy spill

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def divide_numbers(x, y):
-        return x / y
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
