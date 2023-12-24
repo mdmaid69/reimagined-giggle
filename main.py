cd4ad42ab6cf4213c7f62cb7023d46b3997c54d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:57:05 +0000

    beyond giveaway confused cry perfect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
