8425af5ab0b51b5d32806143a7d29fcc8ad9adc3
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:49:21 +0000

    demand swim form wish between

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
