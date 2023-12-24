175712d9b4c24c98e5d654318bcd9f4560948be6
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:44:05 +0000

    above hide rank wrap consider

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
