791af92a735ce9941cad610a31351e7b1f4d8237
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:34:30 +0000

    guilty copy front season present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
