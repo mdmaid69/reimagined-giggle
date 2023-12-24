ca3bc980b1c60d9ef05adfa9337c6521541628db
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:47:15 +0000

    mend inquire further pure permission

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import collections
+def create_counter():
+        return collections.Counter()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
