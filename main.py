81ccea9eae86ae51e66ded4538e799b459760078
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:25:51 +0000

    sign price murder side teach

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_as_list(array):
-        return list(array)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
