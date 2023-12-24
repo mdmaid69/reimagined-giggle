d9df1d45392ce284c691b0115c364b6e83c58f6d
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:30:32 +0000

    ahead soak software offend pure

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
