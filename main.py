0f3bdccf8f9438022d40d63fc8ca825a4ad4cc64
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:03:17 +0000

    practise experiment regard circumstance unfair

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def calculate_power(work, time):
-        return work / time
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
