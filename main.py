a034e31c97827847b3630ae808f044cb30e56934
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:06:32 +0000

    gooff without bend replace permission

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
