f321d4d7bc14707732c59af0b128bfa5936a207e
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:53:36 +0000

    late average him either solve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
