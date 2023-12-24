7851c5252400207d46c2a1f2ee8ad748df8db1bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:16:32 +0000

    talk funny tear winter afford

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
