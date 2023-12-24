0558b4103f9ea52cac45ef01b7076cadf0f503d8
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:44:44 +0000

    OKorokay bridge pattern left hear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
