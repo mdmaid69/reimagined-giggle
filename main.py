9a612920a00ef9616bfafa3ccb6c8b9bbbb0f503
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:44:00 +0000

    limit blow try none achieve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_index(array, item):
+        return array.index(item)
