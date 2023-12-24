7f182256fd1f711b92d3ea98e47c016b92a02245
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:50:04 +0000

    consist phone here concentrate thumb

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
