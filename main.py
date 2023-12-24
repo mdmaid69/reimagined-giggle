0488706ac78850a981538cdeffda609ed21c152d
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:50:39 +0000

    release clear allow talkoutof criticize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
