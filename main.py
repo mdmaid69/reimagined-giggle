ca7a29a69a01a2fd99e1615d5511d9fc32a7f0e0
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:26:31 +0000

    noone what bringback score lean

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
