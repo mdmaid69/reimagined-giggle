64045c20aef66cc4199f4bc3e182b3234eaf1745
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:37:00 +0000

    century scare high expression pain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
