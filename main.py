a1e80d208e65ad9ba72530a2227a9079f6b04d02
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:51:35 +0000

    familiar poor turnback satisfied development

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import sys
-print(sys.version)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
