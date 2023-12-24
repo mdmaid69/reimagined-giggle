7bace8cc28ec17d7950dd88dd71cdebcc569e798
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:04:29 +0000

    hold borrow regional protest quit

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
