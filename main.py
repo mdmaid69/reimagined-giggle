078ed8f42e7706b5286c3f8950d7fbdeb55bdf66
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:38:18 +0000

    line glass decide generally day

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
