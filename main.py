158e5c45cc0d3261d6ad971a321a86450d6d28f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:58:03 +0000

    giveout impossible work wipe disappointed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import array
+def get_array_as_set(array):
+        return set(array)
