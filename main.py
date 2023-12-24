9a044b9b37ecdaf7e5a9247ef8f0a488d37bd889
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:38:02 +0000

    climb same plain part loss

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
