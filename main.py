b70c18bb8477fdfd64a30f7db68ca762580d6123
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:16:09 +0000

    software trip shame ill compare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
