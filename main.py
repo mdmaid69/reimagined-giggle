e89a261c94baf36a03bcf974abb684a14a4725b2
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:49:42 +0000

    birthday brother right round plenty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
