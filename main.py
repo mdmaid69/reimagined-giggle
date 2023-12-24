ee09f645360aab00fae7c5d5b917e89c595b687c
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:50:35 +0000

    succeed exchange deliberate edge conclusion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
