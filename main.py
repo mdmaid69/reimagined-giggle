a53ec7f392be91f4a0a1c80b1e1ba4139ea61889
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:50:57 +0000

    star shake cruel clue friend

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
