dd882e25a6f03048547b0b461f4d4bbe772c5949
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:24:29 +0000

    share cheek computer takeoff inform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
