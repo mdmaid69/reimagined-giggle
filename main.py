fad25d12241aeb3648df8bb3e67fabed04a1eea0
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:48:13 +0000

    realize large quit lean belief

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
