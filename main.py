9b8930ada17e76d4b7bc653d303846ddd6a67079
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:27:49 +0000

    sister smart out line thorough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
