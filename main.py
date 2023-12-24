c5f5330664a5f22c0fb2a560f1cbc06c4f172314
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:18:10 +0000

    art callout ground black do

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import array
+def get_array_as_str(array):
+        return str(array)
+import math
+def calculate_floor(x):
+        return math.floor(x)
