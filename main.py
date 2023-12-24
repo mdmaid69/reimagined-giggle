1ecc5dfc93bdef70f26aa77959ebb63706e8a7cf
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:32:19 +0000

    arrange wonder hardly bar tape

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
