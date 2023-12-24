bf7f2220383e8eda9ce97b4015805d83a0f4d9f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:48:20 +0000

    positive software replace stick road

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import getpass
-def get_username():
-        return getpass.getuser()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import os
+def get_current_working_directory():
+        return os.getcwd()
