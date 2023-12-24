fd56834b3996bdb0a8f7a5a12a32beae19466482
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:53:55 +0000

    else low certain activity recently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
