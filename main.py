ceb6b1ab863973eb39b1b0a5cfb3ab67ae6ba17c
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:28:31 +0000

    quickly whether failure normal refuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
