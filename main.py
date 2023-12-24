5fa8ba0f27edcbc943880258cb970e3a5fa66957
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:51:49 +0000

    watch trust accidentally spot runout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
