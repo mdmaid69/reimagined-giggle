551214df1c67815cfe968a9afc598d020f2486dc
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:17:22 +0000

    human fat chat consider profit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_ceiling(x):
+        return math.ceil(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
