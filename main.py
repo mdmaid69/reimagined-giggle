94bb0270131a9eac97c43f171226568dc6cbd0ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:39:53 +0000

    past claim mix mention hollow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
