9822adf3c5f50f7a804da6a736dd9fb7a68f730b
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:46:19 +0000

    emphasize swing manage ride upwards

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
