7db626f528e316fcf570f08e65082f767e6bf818
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:12:04 +0000

    second only turnover impressive temper

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
