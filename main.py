fb181ecf3f6fe92693a17f2d21118a7a5f9999f8
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:50:07 +0000

    wonder treatment sick doubt lay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
