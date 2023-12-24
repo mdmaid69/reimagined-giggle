a3ccbb37664a26e726bcf8499f699163fad9d0dc
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:21:28 +0000

    opponent embarrassing tired Uu memory

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
