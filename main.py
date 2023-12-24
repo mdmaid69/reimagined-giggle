39f3e92d5c8b637d26f9c4f8a5e4688b676485e9
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:34:57 +0000

    from fork cut place approve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
