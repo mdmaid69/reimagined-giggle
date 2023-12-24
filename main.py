1db01765ff0ffc78a4ce856a860ad78909d5e18a
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:27:12 +0000

    general gentle number lookback(on) Gg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def sort_list(lst):
+        return sorted(lst)
