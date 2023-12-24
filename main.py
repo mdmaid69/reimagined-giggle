be091863aa8d6265fec5b8e213fabd7d8bdc9a44
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:47:41 +0000

    everybody sign shock pull odd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
