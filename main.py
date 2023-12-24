704ee99a3f49f987fb85d10f3d09d82b810f409f
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:49:50 +0000

    necessary our ill long clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_current_working_directory():
+  def get_current_directory():
         return os.getcwd()
