4aac6aff3afd2d31e92cc568e12ce0d0ae4c6693
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:47:12 +0000

    lead keepaway brief butter alone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 def remove_directory(path):
         os.rmdir(path)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
