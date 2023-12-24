e578ac68ec4d4a5db4ad049e4a8edd46a51ba35b
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:02:49 +0000

    able slightly cold unkind definite

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
