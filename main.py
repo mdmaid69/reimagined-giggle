2bdba39691fb4d4a0cffc0f9615fdf1608cfd0ba
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:35:31 +0000

    before organize neither influence plain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
