0f3ccfd657e85d4366fa25388a5f9b24e221e6a7
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:41:31 +0000

    search minimum class plain approval

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
