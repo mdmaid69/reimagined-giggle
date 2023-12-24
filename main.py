76b41e8c734a1ebe125522080524b45e04e1269c
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:11:23 +0000

    nowhere loss swimming burst suckme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
