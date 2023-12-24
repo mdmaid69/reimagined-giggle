750040e3606359570c6913ab15f2124f739db5f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:04:42 +0000

    trousers before pronunciation my finish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_typecode(array):
+        return array.typecode
