8ae151ab7f4ff742002560af2e43268638fd725c
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:17:37 +0000

    tread rich probably rob fat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def find_max(lst):
-        return max(lst)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
