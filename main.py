8f89c8ff6b19c6c369a19745423fe60936d7d697
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:17:51 +0000

    nature repair where public ill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
