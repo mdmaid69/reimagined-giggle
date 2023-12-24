8ffa633fc347117334edce4bedefb57acdf09a85
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:59:09 +0000

    thumb stick sound lead please

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def square_number(x):
+        return x**2
+def is_odd(n):
+        return n % 2 != 0
