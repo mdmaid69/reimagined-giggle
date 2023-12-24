b342b9833dda30a72d02438e04424a1e17ff4011
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:19:04 +0000

    pay before phone keepout solve

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
