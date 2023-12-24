bd6f66acc2e41af8fd562529ae33ec784c7c06d6
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:53:48 +0000

    exploration himself amount hate tip

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
