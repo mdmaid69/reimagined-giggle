12b99bcda9f1e715842329fbeffed6d82269176f
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:37:54 +0000

    serious hurt choice winter mind

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
