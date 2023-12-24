1696213a4d89bb8d8d4f9d68bbd5bdb480eb0c18
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:26:12 +0000

    provide drive lonely for grade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-for i in range(5):
-        print(i)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
