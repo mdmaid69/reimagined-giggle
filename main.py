179d99687c051ffae450d3f3c40d4043e8049dbe
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:22:09 +0000

    concerned naturally ring unable whoever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
