8e0f585b235782b5b602ba2831c6ccddc403ab94
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:54:42 +0000

    smoke impatient worth choice meaning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
