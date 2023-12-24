5b5a03855053eefe8ef972acee56fd465d5b2be1
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:01:45 +0000

    behaviour boat produce worried whole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import platform
+def get_os_info():
+        return platform.uname()
