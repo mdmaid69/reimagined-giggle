1f8ccfb8bf235e405f14eac4f1b3ea7aedd916c0
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:50:49 +0000

    special appear divide suffer generous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
