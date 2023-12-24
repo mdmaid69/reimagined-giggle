b4dbec88a2884af78dae968d064b722f06cd727b
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:43:25 +0000

    trap little freeze live tool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-def exit_program():
-        sys.exit()
+  def convert_to_binary(n):
+        return bin(n)
+import datetime
+def get_current_date():
+        return datetime.date.today()
