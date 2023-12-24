2ec664bc9101f83e2cc43bf2cf6a515eaf595f91
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:06:45 +0000

    reserve certainly satisfied answer fire

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
