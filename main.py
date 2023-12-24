d4f7d7c798771a15098170c12a144d8d3fdf2f97
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:56:12 +0000

    image offence perform lookaround pride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
