89b5e063aa9b1cd1e4af31f36575ae048ceb6cc7
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:08:07 +0000

    taste leave trouble sale reason

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import sys
-print(sys.version)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
