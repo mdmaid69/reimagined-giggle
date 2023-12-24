dce8ad38e5c1e49bbc8d7398b13b8fc8843410a1
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:43:12 +0000

    spray exactly debt Yy shoot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
