5800237ac1f1738fdadc37f5fe3d73140af8a227
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:48:02 +0000

    swing passage flow fashion entrance

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import numpy as np
-print(np.array([1, 2, 3]))
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
