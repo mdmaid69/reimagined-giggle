ebc96ce49c66e2173993c4073e2f17f3a9d89980
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:20:46 +0000

    pleasant heart pupil welcome along

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import random
+def roll_die():
+        return random.randint(1, 6)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
