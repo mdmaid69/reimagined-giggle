51eacd0319afb3c4c6afa7810cc6acd19db6ee55
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:27:44 +0000

    coat nigga new passenger cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
