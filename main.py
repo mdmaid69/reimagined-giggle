47f013187d5ee7ba496b59160b44dba988a58842
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:37:44 +0000

    improvement basis shape night lookafter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  def cube_number(x):
+        return x**3
