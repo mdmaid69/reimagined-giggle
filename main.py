bc34aeb2473834b09cfc6ce0998fa04541cbf315
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:14:49 +0000

    willing sudden better activity force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
   def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+        return os.path.getsize(file_name)
