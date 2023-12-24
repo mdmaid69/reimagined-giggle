fd9c420f6cf30c7803f71a76f9aa8965e32c0215
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:03:02 +0000

    before trial spill sale unless

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
