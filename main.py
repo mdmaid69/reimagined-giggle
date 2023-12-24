f4fd7e4998a774fc190db95e308b28d7b4e4036d
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:29:44 +0000

    brave crowded mouth brush Zz

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_repr(array):
+        return repr(array)
