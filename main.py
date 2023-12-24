b0656cd865c8d9e6062ff423dfff20edd4f8e54f
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:31:00 +0000

    train visit scare cold watch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
