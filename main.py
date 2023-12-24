2e21d1b922209e066bc1d792bc0603d9fd0a87f3
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:06:21 +0000

    please number problem past trade

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def sort_list(lst):
+        return sorted(lst)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
