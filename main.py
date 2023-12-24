48abf150043de35554c62042124a20583f3e786e
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:17:22 +0000

    emphasis month protest dry exact

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
