d88241a2a0a20c26be1989de8eb7782a9999211f
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:01:52 +0000

    air comeover determined people occasional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
