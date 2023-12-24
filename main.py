aa91217b3527a9e867f159461633343db71c8045
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:49:11 +0000

    help traffic able personal row

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
