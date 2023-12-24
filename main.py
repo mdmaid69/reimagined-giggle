4d1bc212bcb97bba768fb2f305d024a121590011
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:44:07 +0000

    constantly second cover equal poison

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
