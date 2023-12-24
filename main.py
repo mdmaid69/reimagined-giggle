c880d4c833b6169e0fb886f175b0b9de6195a5cc
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:12:24 +0000

    study without exhibition below heart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
