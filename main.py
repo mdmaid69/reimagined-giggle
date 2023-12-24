6d96bd490ec026a19fae094b903443299280d1af
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:43:50 +0000

    force teach mix field breakinto

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
