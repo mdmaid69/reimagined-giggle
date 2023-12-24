b9bb41fe7b31cfb15d09941a40e2ef450f63cda8
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:00:38 +0000

    exaggerate bet anywayanyhow love recognize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_error_function(x):
+        return math.erf(x)
