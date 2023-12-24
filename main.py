4b2974210276312bbef665320f4b4bacd489670c
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:01:42 +0000

    immediate mind round ready clothes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
