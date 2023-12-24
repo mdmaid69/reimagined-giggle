f88d3e4e257ec48c0ffc060409c2a16e497ab520
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:22:45 +0000

    spill eager goon general shadow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+for i in range(10): print(i)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
