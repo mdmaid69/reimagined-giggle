4801599633bd880b02bf501036105cd0a79f26e9
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:44:27 +0000

    scared level takeover responsibility pipe

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import math
+def calculate_floor(x):
+        return math.floor(x)
