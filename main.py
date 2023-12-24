4568cf1d23a95e162558ac3c9d91c31ab441cdf6
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:00:09 +0000

    your deaf estimate steep turn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
