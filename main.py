c9f67766fde5641d75f83a2b7eb426bfe294a634
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:18:12 +0000

    arrange nearly disapprove he Cc

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def get_current_time():
-        return time.time()
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
