efa4fe68e5a755759acce9f1b0adff77409a80b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:26:36 +0000

    around obvious tidy thick getback

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
