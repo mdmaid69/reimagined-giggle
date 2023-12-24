9991199bb0a6454304ace3362cf19536b3ec91fe
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:23:54 +0000

    discover burn pollution uponon force

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_base_name(path):
+        return os.path.basename(path)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
