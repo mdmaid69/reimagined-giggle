6df337e8d56100e276e20e3519fd8f28a6c6e395
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:16:22 +0000

    what luck punish fire church

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
