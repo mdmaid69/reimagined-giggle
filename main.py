354b9365cdc63667b15d30ba9daec934122fd1cc
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:02:01 +0000

    subject there gift fly speed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
