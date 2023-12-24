40abd06f50d9dd9b7d5778f9a560fb749a7f5038
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:40:30 +0000

    study confuse convincing sleep disappointed

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import random
+def roll_die():
+        return random.randint(1, 6)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
