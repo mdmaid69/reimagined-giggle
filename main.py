1bf953de2e58749ad36bc88439e5fffe3f4a0daa
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:48:33 +0000

    179bite shade ready belief determined

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
