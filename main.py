f105cc02b3bb1510fa9aad342b6400d3aa70ce9d
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:38:25 +0000

    wander suppose something involve admire

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
