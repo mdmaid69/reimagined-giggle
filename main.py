12c75039a3ec9730db81003241d5822fd12b7c19
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:29:26 +0000

    guilty grand challenge extraordinary rarely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
