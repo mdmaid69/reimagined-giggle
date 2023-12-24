9d756a1566cd7da7b0d13cee2fd921d0cd860134
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:35:39 +0000

    shock equal slip period please

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def is_even(n):
+        return n % 2 == 0
