02b6887ea228ef5af55e20386aa38705277a2926
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:05:28 +0000

    shrink always next direct plain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import re
+print(re.match("h.*o", "hello world"))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
