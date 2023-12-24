a377dde3bd6e6e121cd74579de25c012692d1236
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:39:12 +0000

    what stay demonstrate dry bush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
