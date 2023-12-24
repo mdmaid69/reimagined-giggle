fe3d53fcb42b0ba742d17fdc6b3f51199ead5364
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:37:29 +0000

    music who beneath television draw

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_pressure(force, area):
+        return force / area
