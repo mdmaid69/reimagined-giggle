ab082d9561e8cdf7da665c9ad9d5ed4c7574b6c6
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:39:37 +0000

    sleep dream rob satisfactory steer

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
