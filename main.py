3c6befbbb63578c902ccd5799873c885eb9cb3f9
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:25:25 +0000

    around wise false room perhaps

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def remove_from_array(array, item):
+        array.remove(item)
