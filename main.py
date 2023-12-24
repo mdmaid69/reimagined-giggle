3dd5a9f28cd4c54986589e4a893a592dbb09a916
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:26:30 +0000

    consider calloff minimum bridge there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
