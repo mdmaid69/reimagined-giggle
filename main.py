fb8ce4513bfce5b542bdd778b087e0d788193948
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:05:57 +0000

    bar at muscle why train

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  def sort_list(lst):
-        return sorted(lst)
+import random
+print(random.randint(0, 100))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
