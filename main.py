3b8c33e0ad5f4ee8f28dcb89c517b115ce0b67d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:29:55 +0000

    such breakup future true perhaps

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
