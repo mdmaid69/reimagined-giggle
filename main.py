b75a38ddf083f8c6d0d842d2bac9123940bdd00c
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:23:25 +0000

    table stage not loud hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
