a857a1f2db12af6d9818b85c41202aae38667a66
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:09:12 +0000

    shut ahead chance in section

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import numpy as np
+print(np.array([1, 2, 3]))
