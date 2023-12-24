bc397382a1052231205a4143680ba5244f99b589
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:24:35 +0000

    rough exam maybe mean butter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
