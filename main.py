06194996779a6616e8541a76045f488ccec6ca6a
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:57:38 +0000

    introduction division conduct modern foot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
