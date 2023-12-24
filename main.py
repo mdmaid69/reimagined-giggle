31dff1b2b7248d17df5f3ef32ec8e7346ad89e05
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:45:17 +0000

    opinion plan above however accident

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
