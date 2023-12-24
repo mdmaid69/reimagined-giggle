36afb0f321652f20138956467bed031ee6dcf7a8
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:50:11 +0000

    through march drive lock cool

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
