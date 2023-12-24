2dbda910e2212c3eec389d6d1cb4a12594efd30f
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:35:37 +0000

    least excited guilty friendly lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import tensorflow as tf
+print(tf.__version__)
