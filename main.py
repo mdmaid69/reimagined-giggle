8f498ed7a9109169421ac29a7467db4966691632
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:57:08 +0000

    post report walk want clean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
