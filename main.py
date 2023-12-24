5c7177645f1a8ff0f29c85aa258f3cf4add0a949
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:00:23 +0000

    another future usually everybody defend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
