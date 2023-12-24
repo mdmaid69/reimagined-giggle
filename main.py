48cef64beb6e782be90b2a2665e4a9fc05cdbdc1
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:30:46 +0000

    suggest majority somewhere pretty respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
