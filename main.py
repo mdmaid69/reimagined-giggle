d2951aacb88159a19c0caabddc76d2582101125f
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:09:53 +0000

    bet about your cause no

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def count_words(sentence):
+        return len(sentence.split())
