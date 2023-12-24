4744c612a71e159ef7cc909d278b0ab6aef38103
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:39:51 +0000

    suckme truth every ride beneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
