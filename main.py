bdae659846eac63c33f185f27becea961c3160de
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:02:23 +0000

    fit sale many before spite

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
