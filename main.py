905d24f090db28b5b8e303f15cb8c3affcadc8a9
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:04:36 +0000

    concentrate naturally glad whatever book

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
