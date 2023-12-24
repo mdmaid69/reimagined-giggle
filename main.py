3045e9f4f6513aea51ab421605ca6912746c944b
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:03:38 +0000

    time crash lunch sleep switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
