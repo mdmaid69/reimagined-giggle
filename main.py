af7f873ef23479c1dfee95bf6450ff978bf01c48
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:45:07 +0000

    pocket best crazy beautiful suck

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
