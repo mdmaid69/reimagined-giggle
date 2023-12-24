6214bdefd3ab627749672d7c027f78fedcd6aa65
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:35:40 +0000

    popular least human better getup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
