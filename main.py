bda40a846c927339dec12f8189a2801c65c7cf98
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:43:23 +0000

    stretch contain talk direct towards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
