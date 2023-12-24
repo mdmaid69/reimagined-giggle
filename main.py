569a962d7eb65846505c429b90d157930c16ea53
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:31:13 +0000

    eye prefer strike there skirt

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
