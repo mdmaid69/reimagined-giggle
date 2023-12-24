b79576ca73bda75695ffcff7c2b31b386392fedb
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:38:21 +0000

    union muscle yes private take

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
