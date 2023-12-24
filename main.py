9909dbf45e33079a3f7fc4b40c7c3b48bfcaf7e8
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:35:19 +0000

    brush violence standard ahead thinkover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
