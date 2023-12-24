83862433a755a30cb495b1fec250f11299e27237
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:47:33 +0000

    bringup justice brother blue ours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
