302d4eda6f6f64f39055ccf2bb534f20b7fd2be6
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:56:50 +0000

    mark tidy top thankyou interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
