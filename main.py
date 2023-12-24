b78dc3f145e5c00814e0bc321ba1c38de657574c
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:04:44 +0000

    arrangement knock child mark above

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
