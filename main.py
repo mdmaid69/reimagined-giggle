d73dcab1b2341f75d886b84e0051e83318819905
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:46:38 +0000

    price everywhere organize win care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def sort_list(lst):
+        return sorted(lst)
