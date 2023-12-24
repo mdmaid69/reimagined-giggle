9299c9c3e8e06686e091164c0fd5a07ad29c371b
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:47:13 +0000

    any attract see weekend there

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_floor(x):
+        return math.floor(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
