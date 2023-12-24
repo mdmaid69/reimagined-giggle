220c901e7a236626b04aaf6e9e4bda380f2d2786
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:50:53 +0000

    takeaway birthday position refuse keen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
