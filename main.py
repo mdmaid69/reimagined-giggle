d82cc0c9fb001c6fca25ee9d13ce45ed5121ee5b
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:09:30 +0000

    say fasten crisis point runinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
