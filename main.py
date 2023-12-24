d3904df5e0ebd9865e4ccaeaed00c68e80383581
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:45:51 +0000

    reasonable row freeze Ee interest

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
