2dab72479955ac038923dfbd4855d6779b30c5af
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:15:18 +0000

    benefit order evening small guard

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
