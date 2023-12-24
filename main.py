364f05f92fb2c40b54711def55afe14afb70562a
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:20:00 +0000

    Jj noise opposite basic appear

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def greet(name):
+        print(f"Hello, {name}!")
