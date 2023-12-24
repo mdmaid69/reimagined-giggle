581c467fe333dabf7db914ec23dc4ea1af5e9bb4
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:12:21 +0000

    road strong keepup promise board

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
