d1be13240b4bf380267fe72e528d95c0cc9b472d
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:09:21 +0000

    care basic dare out lookaround

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_area_rectangle(l, w):
+        return l * w
