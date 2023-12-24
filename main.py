5e86bf84ee865f56070390d6fd6d8ff02a428336
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:00:42 +0000

    grade study full immediately manage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
