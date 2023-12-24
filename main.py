2ab9de89f7ee141aabd26ba0d6b020b316e9b22c
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:23:22 +0000

    ahead wonder intend might drawing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
