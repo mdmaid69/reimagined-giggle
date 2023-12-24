4fe26e70c5f719e9829fefd549e6db1706ab5fdf
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:07:10 +0000

    decide interrupt regard basically runover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
