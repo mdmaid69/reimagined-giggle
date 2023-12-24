86a14dcecc06733527bc2bc28cda63168db53f06
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:39:00 +0000

    human among stage challenge pretty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
