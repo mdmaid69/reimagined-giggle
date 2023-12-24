53ba25598b7d47cae60963792ff582ae9cb3e325
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:58:11 +0000

    use lay glass long brain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
