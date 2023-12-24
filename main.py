05e335366f8e2635362f222ad4427a5d2587d57c
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:37:18 +0000

    night turn hair press prove

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def is_odd(n):
+        return n % 2 != 0
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
