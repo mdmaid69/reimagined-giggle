2bc93225879233ba1090fe4bc3b8db9df7d95185
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:49:56 +0000

    frame pipe laugh unpleasant height

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
