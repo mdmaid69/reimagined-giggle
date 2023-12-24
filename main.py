ea9684ac4eb2097dffb09ff38fd3048fa6d90af9
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:09:57 +0000

    steady appear recent activity break

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def remove_duplicates(lst):
+        return list(set(lst))
