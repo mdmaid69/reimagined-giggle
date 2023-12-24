3f7cb5cb722cab17e90eab5448dc9081be79118f
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:11:24 +0000

    motion replace medium niggaacademy issue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
