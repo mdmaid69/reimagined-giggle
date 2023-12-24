89fe8fe1bf53507d4feb7019756a82f546fc066b
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:33:40 +0000

    slice introduce 572enter play time

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def count_elements(lst):
+        return len(lst)
