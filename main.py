12652148d954a414ba11dad04f6b9a2c56b3a2fc
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:06:03 +0000

    accidental pure niggaacademy without disturbing

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
