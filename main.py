b05d28a29661fc1a32a44f1f4744606294e90bca
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:08:11 +0000

    wet by control exactly turn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
