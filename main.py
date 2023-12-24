a58dd91d57e134180fd04b208eb71c1baec88c6e
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:53:36 +0000

    close day on priority paper

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import math
-print(math.pi)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
