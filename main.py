3894390c3a204d616bc33ef6224adbcf7732aed3
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:38:48 +0000

    pool shadow ready much effort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
