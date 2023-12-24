6f0e011abd19b3a8fb37ab5996dcc2fb4383b9f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:15:16 +0000

    myself finish fun hope fall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
