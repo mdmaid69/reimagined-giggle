b72161b6d4be8b4e5a7e293a129b713cb8eddcdb
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:43:35 +0000

    vanish wash worst whisper guide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import collections
+def create_user_list():
+        return collections.UserList()
