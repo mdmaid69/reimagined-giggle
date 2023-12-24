94c094436e4cb10ce0f6f934f0551e773bf7d12f
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:42:14 +0000

    uponon now suppose road nigga

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
