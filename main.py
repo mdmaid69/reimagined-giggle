4da50ab8dd419aee95ab16fd22620eea59a71310
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:38:25 +0000

    lock minority depend rescue tune

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def multiply_numbers(x, y):
+        return x * y
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
