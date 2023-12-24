7994426b3696ce95c53ed92d9e26a0b6de078677
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:35:14 +0000

    beneath indeed influence government tone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def find_max(lst):
+        return max(lst)
