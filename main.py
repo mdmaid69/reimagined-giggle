b919dd964b5b97841a1e307aeaf628ba6ba774ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:54:36 +0000

    follow loss interruption mistake beneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def divide_numbers(x, y):
-        return x / y
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
