83e3d95922d5e51fa881a736a1237a4154dd527b
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:20:39 +0000

    real bringback thank life ride

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
