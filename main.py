986ffee621787bc94250a70fa387f8f65b897830
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:45:02 +0000

    her measure squash exist apparently

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
