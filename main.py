ddb578435196a81caeb9e92da8e9739ca86db4e6
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:45:21 +0000

    practice outside exhibition left cope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
