d9367a9b9b5c540d564efedcf15002a3ff19ba36
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:33:11 +0000

    crime only name disturb crime

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
