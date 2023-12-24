ba11f0673f576fc60f538eb350f3650b5904cd39
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:36:47 +0000

    basic send punish split bury

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_sine(x):
+        return math.sin(x)
