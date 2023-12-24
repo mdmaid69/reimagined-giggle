e0132b6a2b3fc439c94371df1e4b6b618e2addd9
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:07:10 +0000

    region boring aware open except

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
