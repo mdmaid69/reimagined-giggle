7f79a27914d933564f58cc8a838569061f6b65fd
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:59:34 +0000

    trade strict beside win add

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
