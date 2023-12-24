2c3cf1e26c706cfef4dd17d2d21ceec1730e91ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:10:53 +0000

    bother taste agree mean national

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
