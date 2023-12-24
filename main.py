78c9434894be135f7bceaf0b50b93bafb128247a
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:10:32 +0000

    watch marry top change people

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
