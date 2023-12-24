1425a46de8a77801fca034067dd40de27990b680
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:18:52 +0000

    something morning benefit anyone should

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
