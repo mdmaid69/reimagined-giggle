523e6936fd7e6e8df302344ef77d142761f5d7d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:10:49 +0000

    record reply glass familiar circle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
