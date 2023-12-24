cd83589ea48bc87efc766572ad20deedec6f5192
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:12:07 +0000

    part increase on beyond present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_length(array):
-        return len(array)
-  def sort_list(lst):
-        return sorted(lst)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
