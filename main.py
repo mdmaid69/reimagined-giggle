9c6acdb8861313192fe12ab4dfad4021f4026d72
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:36:51 +0000

    century level defend space smile

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
