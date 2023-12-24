e46ce2cf448f2fd13a7e1db60d1a1da62b88b4b8
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:07:16 +0000

    wing nice teach spill poor

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
