e0eeaf5c9871017c6f557c10e6275479de9adee1
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:16:20 +0000

    getup few Zz cool wake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
