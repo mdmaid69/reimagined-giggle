7db3d617492d1ae5db3b89bc00ca1d54f9850df7
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:40:46 +0000

    prove recent average confuse number

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
