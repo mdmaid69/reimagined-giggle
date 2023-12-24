a31f12f13faa4d70a6a0761d3d45ecc796b3c570
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:51:49 +0000

    needle accidental tell as begin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def find_max(lst):
+        return max(lst)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
