ad17608e2636e96419b0fa21c31d9acb7324a5fc
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:59:13 +0000

    smile passage ashamed sister breakthrough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
