b0b16369611d597ab1ec0d812a0d9abdc347f81f
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:25:38 +0000

    unable figure music 569enjoy sleep

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import time
+def get_current_time():
+        return time.ctime()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
