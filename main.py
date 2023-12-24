5d5202cb26e88e59455e32a3d375697c4e84bc91
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:52:09 +0000

    workout interview light slide fond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
