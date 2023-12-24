1d30c2a8dda8564fddb7dbacc053344f0d978cba
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:48:17 +0000

    godown term recommend rather upstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import getpass
+def get_username():
+        return getpass.getuser()
