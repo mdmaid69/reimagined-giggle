059b19877790c88f98614d8028936acb6976a37b
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:50:51 +0000

    opposite test exciting spite go

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
