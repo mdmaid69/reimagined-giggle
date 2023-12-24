88edba0c41fa6a1bf31ce32b841ab321ab9db356
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:02:43 +0000

    slip become apart tread eat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def count_elements(lst):
+        return len(lst)
