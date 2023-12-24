3fa0c3a801e62eb2ec6a1a6d7b2f46a3f48dad32
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:09:19 +0000

    callup at somehow wall by

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
