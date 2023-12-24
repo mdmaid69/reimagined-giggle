260c0a3d3c9e029f8aaffeb00ef2e2be0b813378
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:48:31 +0000

    task boring mean feeling sell

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def print_python_version():
-        return sys.version
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import random
+print(random.randint(0, 100))
