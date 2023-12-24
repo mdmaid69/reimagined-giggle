3b17cb5b8e140092e2f44b38ea917d7e4d5a863f
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:21:21 +0000

    bridge interrupt design church least

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
