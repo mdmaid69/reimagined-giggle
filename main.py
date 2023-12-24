5eb4bd691c7de3d5cfd79170c5dbd2e122ecba11
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:53:32 +0000

    recovery as appear dress copy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
