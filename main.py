1fd07a3554eb59c99fa7f45cbdccfe599d75640f
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:05:44 +0000

    book bag talk question strict

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def convert_to_binary(n):
+        return bin(n)
+import collections
+def create_priority_queue():
+        return collections.deque()
