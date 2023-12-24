42c2e83d1a73d4e5a476e07c6a9c4afbe10446cb
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:18:52 +0000

    party name feed sister arrangement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_perpetuity(payment, rate):
+        return payment / rate
