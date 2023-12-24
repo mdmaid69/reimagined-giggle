d39933a043382b53368fd00644bb81cd7ab2aafc
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:27:35 +0000

    not crash herself kind over

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
+  def reverse_list(lst):
+        return lst[::-1]
