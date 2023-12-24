00603ce6d53949d5710e2c12aa9a5e52845622eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:13:12 +0000

    actually takeout tax visit time

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import sys
-print(sys.version)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
