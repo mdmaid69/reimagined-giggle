1700d5d323f5b9fbe9164277aa443167c64fd364
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:59:50 +0000

    fortunate whatever bath home steep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
