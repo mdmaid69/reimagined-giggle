f4684be511213257504964168a529215e5f751dc
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:02:19 +0000

    block bored concentration run now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
