23fec5fae89f1545920ff6a0f0bd5a54848b9e1c
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:11:43 +0000

    unite plate control believe really

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
