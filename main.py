be8e71e366db489e702ced0bb01c9e13971638b4
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:31:19 +0000

    dull Ee charge average boring

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
