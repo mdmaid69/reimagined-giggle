a02bcd6733f5f953312d59deb87974deeee364ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:43:28 +0000

    arrive content middle couple sort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
