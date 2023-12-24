89754b0115ced322b6ea3f92c881249e6b2d109f
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:38:05 +0000

    priority callback violence wind clever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
