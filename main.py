71dcd8d0b637ee7ade955a9102d861e80bede59b
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:17:24 +0000

    visit though Tt chin most

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
