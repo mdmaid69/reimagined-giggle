dddffa589802033efdfce5e44d10aad51e858ac3
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:22:10 +0000

    offence straight return turnaway happy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
