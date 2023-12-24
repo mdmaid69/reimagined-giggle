e567bd82766ec2cbfc06fd83f5c0992ecf00deef
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:35:43 +0000

    risk badly man secret method

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
