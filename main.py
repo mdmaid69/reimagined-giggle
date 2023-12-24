bb4eccce91b9898c79f4432d06f91f426867ca0a
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:48:39 +0000

    people welcome among sometimes agreement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-for i in range(5):
-        print(i)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
