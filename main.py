66a5c81784aab0babe991556d3cc88e022aa7118
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:20:55 +0000

    estimate wave address temper cap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
