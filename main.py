7d74ed47aba8b73b63747e66af9f1d5c2c9474c8
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:12:40 +0000

    interesting forget no upset whenever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
