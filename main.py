8e9129d2ac1f0b26df57c942f72f139d434a1eb2
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:43:33 +0000

    passenger future lay ground snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
