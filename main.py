f0fb6e87f6f1276b7604b7a118838ab5cc2d89cb
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:55:08 +0000

    either break patience centimetre development

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import os
+print(os.getcwd())
