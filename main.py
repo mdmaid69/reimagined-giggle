b364b6bd3bf1001abf2120640ebe02f8816ca13f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:53:54 +0000

    rarely water feed everything badly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
