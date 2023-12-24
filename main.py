eaa52919b655256c9f3511b1670a7e0f1d376ac6
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:21:29 +0000

    kick to clean satisfactory train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
