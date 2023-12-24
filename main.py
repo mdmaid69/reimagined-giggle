fc5086c1e25b38652d9a74952bf6ccd0f0e9f7ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:20:36 +0000

    suppose warning people limit think

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
