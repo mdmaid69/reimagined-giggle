fd86045d3422cc815fce740eb6c1d7436f54856b
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:30:17 +0000

    cover duty organization length slide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def get_array_length(array):
-        return len(array)
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_sine(x):
+        return math.sin(x)
