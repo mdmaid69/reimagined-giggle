e38fd66232fc8c0c246c09ccc4d386c09c5e415b
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:10:48 +0000

    message favour division quantity home

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_palindrome(s):
+        return s == s[::-1]
