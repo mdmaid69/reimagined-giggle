2aff0ae8eb54666037a008fa8263860f3712e219
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:42:41 +0000

    fetch answer name television except

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
