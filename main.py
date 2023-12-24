ce56e3b417accc37d0d6b438cc164bb79327aa43
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:37:42 +0000

    visit call finally task motion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
