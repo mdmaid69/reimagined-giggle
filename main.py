f947af7dd9829fd24015bfbc40592d6b25fa3c4b
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:21:17 +0000

    explanation sight thirsty tense feature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
