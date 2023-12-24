9638b602b2019304681e56e451ef7001bcaab246
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:30:13 +0000

    that Ff negative hide region

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def sort_list(lst):
+        return sorted(lst)
+import math
+print(math.pi)
