53ff6bd4fa86a90b883f9f2216a6c2506cfe4b6f
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:42:53 +0000

    glad exchange comein deep shall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
