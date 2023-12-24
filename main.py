1512b248c255a3d9ba9fecc4c1ae0ac55b1ed189
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:32:08 +0000

    yourself front wipe section reduce

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def count_elements(lst):
+        return len(lst)
+import sys
+def print_python_version():
+        print(sys.version)
