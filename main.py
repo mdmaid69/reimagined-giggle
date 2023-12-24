e492cec277d4f7d2b824e2b02ac2d86510abb714
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:45:55 +0000

    body imagination direct little basic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
