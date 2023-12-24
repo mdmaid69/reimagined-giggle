0db37a750b5fecd08b500cc130ffd690be2c5fae
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:42:03 +0000

    this instead legal worst you

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_exponential(x):
+        return math.exp(x)
