c1620380d6b558894fefd4a25a296b6323ceb37c
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:09:58 +0000

    cheap paper display study whisper

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
