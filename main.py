594e5b60e971f6605f782b1d919f1190cc238f15
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:16:32 +0000

    moment aim ticket chance discuss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
