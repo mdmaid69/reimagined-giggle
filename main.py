8200dfa3d1f239121120c00c86ebbce85cb44fa7
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:50:13 +0000

    short hope song workon interest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
