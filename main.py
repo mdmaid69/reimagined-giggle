0952b2e708adc85c7e904f91a9fbba9bf51bd42b
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:32:59 +0000

    lucky thumb false talk flight

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
