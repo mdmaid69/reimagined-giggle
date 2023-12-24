79b915248e413ff543961c43410db6ac087f7394
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:17:20 +0000

    create move worth steal 568energy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
