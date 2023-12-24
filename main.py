ef212dd1005af0333ebfc55f3dc3b88d9661482c
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:15:55 +0000

    black aim least Tt play

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_pressure(force, area):
-        return force / area
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
