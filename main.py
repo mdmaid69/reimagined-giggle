442fa6ffe6371b3d0db61ef1c930d12b79261090
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:36:57 +0000

    murder feeling stamp cheek burn

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
