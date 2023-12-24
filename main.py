73cbb32a3228c46cc460f9800c73b8c15f1fc002
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:35:04 +0000

    by straight never thinkof snow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_volume(length, width, height):
+        return length * width * height
