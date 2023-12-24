7365ac7fa26be68b152ccac119457d8b2edd5a42
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:15:58 +0000

    maintain worry picture load tragedy

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
