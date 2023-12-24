c8756ac6b3060949fabbf14cc63ea16b62c52d26
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:15:26 +0000

    deaf giveout payment border fork

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import datetime
+def get_today_date():
+        return datetime.date.today()
