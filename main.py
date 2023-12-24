bba8e7d2ebd46999ad5e37e9812bf4a743149c77
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:10:45 +0000

    cover tune active proof turnover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
