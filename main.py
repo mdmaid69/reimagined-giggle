75714acb53c633cfe1363e89f5552f1093a5cf4b
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:08:22 +0000

    job busy trip plant reject

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_work(force, distance):
+        return force * distance
