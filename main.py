609e1015f43a6210eb5f961d878ca9946e97502a
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:10:34 +0000

    turn himself nor repair flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
