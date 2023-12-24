c3ca0cc705120c96c6df80dbce557507efd44789
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:40:32 +0000

    battery completely prefer choose switch

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
