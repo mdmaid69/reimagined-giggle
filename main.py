e728f7cbcdaad3d3e78a7ceadc628cf276d271b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:34:15 +0000

    under cause fasten Jj stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import array
+def get_array_item(array, i):
+        return array[i]
