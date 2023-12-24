b03f72f3ba9843a70f6bc41ad0734993298cc535
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:46:26 +0000

    method shallow feeling callback yes

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+x = 10
+y = 20
+print("Sum:", x + y)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
