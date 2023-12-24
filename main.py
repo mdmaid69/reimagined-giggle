ecec0cd2577de100a86244249ade99c6fc5de01f
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:25:53 +0000

    refuse pleasant very believe minor

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
