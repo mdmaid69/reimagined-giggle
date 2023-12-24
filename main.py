32ac845a754e53c20a4ff81b7925873d75774cc6
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:36:09 +0000

    middle break snow improve sight

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
