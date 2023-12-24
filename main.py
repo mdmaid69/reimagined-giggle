34d9f52242e85f8a2150266b6729d3a8d717d150
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:03:15 +0000

    safety affect talk beautiful thin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
