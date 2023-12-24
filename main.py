1b4c64906830609c797f31bae28cb8d4798b789d
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:16:40 +0000

    dream workon care low imagine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
