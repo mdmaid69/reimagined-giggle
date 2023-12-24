623b0f76cc65dc74495da5940e533b223db7e018
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:19:06 +0000

    passage pump early announce sale

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
