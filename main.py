2b2ce7840feb704f6e1111084045fa2a8ead60d6
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:16:31 +0000

    complaint sore need broken slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
