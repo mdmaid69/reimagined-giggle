bfc3392fc5a30dee6029a5839622799107bcfaa2
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:42:31 +0000

    high usual interesting male right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def remove_directory(path):
+        os.rmdir(path)
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_floor(x):
+        return math.floor(x)
