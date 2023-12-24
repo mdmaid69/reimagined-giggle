1ca24cfc41d92906420e5938c8f6798882d3f563
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:37:43 +0000

    reason off improve squash admire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
