ad0e484ca5202431741a72241b722ad09f2a6b24
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:46:30 +0000

    recommend possibility affect woman wash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def count_elements(lst):
+        return len(lst)
