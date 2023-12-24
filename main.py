4fd5507fafb90d1d90dff32342f917260ab680b4
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:48:06 +0000

    fire steam punishment swimming eat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
