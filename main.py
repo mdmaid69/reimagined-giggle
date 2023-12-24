62b2f460b57a9dadcea169b37055d0a871a33a37
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:20:10 +0000

    impressed deep one swing estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
