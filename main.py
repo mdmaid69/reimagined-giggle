394fbfc2c0efefe52de775a215921bafb16b365d
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:37:02 +0000

    quit estimate broken urgent priority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def get_array_length(array):
-        return len(array)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
