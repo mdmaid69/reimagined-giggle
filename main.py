fe89224badd78bedd0fffb2c870cdd6e0732f0f1
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:49:57 +0000

    suit switch bus run classroom

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_area_rectangle(l, w):
+        return l * w
