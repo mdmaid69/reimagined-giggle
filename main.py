d90102856dc93dc49a95a92a3f7c67709270adcc
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:06:02 +0000

    factory your conversation sign wind

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
