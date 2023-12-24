831d120a4e61de343164e56ead1d28085c1395e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:20:29 +0000

    bend store careless close knock

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
