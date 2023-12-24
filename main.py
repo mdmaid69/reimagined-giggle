d48e6270e36ced42f175bb28dc0f9aada5c619b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:00:41 +0000

    switch past brother soft area

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def append_to_array(array, item):
+        array.append(item)
