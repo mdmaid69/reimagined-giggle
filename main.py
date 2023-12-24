981ac655b3c8ae62a046df67d5e3bbf75659f9b2
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:27:11 +0000

    happy injury theirs until sure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
