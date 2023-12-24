470b6843bf6a88039635cdce6e74b8c9bc3b152d
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:10:12 +0000

    support tense store fill country

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def add_numbers(x, y):
-        return x + y
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
