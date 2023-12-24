d27d4d1fdb5e2ddcb7cc83493026c7f2429cf583
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:59:07 +0000

    pencil owe everyone label change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
