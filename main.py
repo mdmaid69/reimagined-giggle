c8036596211369804f5126767de74679167cb040
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:18:37 +0000

    represent class break damage comeround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def square_number(x):
+        return x**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
