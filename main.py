33363e73d80ff5daa9ba345a821743577ccdfb1e
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:36:50 +0000

    judge get receive mixture discover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
