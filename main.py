6659a630f969baa7bdba521fad2688a0a93e7a6e
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:02:29 +0000

    point tax arrive combination nice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_index(array, item):
+        return array.index(item)
