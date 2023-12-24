b22fdc6224e6d55cba4f77ab7aa6b23e57f838f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:38:50 +0000

    vote fail hit damage least

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
