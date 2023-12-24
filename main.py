969648b2cdd136fc5ad2bb1b4f758146c38e4e41
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:14:51 +0000

    eager pleasure neither mess boat

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def count_elements(lst):
+        return len(lst)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
