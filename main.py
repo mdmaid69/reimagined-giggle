0dede52454c82eee7a50102c6f307f89ae8d18fc
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:01:39 +0000

    weather hair check start copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_as_bytes(array):
+        return bytes(array)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
