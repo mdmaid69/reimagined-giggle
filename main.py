320d6ee5595365c1103c8fa3678597d64b477b11
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:35:54 +0000

    touch marry thank search bright

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_power(work, time):
-        return work / time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
