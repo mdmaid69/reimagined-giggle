1b72eacecfeba947cabe7e5cd025dff7fabefbfb
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:38:07 +0000

    worse describe nowhere little gap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def set_array_item(array, i, item):
+        array[i] = item
