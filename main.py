ba998cfd2f46515345ea8d8dab7d263a7bdf52bd
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:19:53 +0000

    pretty impressive climb activity calloff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
