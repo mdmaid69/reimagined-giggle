dc2b73851c5baa345d7792c8622fa7c018ddecff
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:55:53 +0000

    medium degree charge fly wall

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   def count_elements(lst):
         return len(lst)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
