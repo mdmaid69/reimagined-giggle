31ae859894f7a1162c709580539f679b22b7218b
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:29:28 +0000

    exploration bath suggest round giveback

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
