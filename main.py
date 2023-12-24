91c6fb6a400ef623b237df3e499a33c9d57aa664
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:08:56 +0000

    hope shut arrangement collection instead

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
