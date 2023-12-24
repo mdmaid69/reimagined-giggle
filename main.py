0c4387474655d7d3698f3e66a32f98b22d57ffc2
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:27:39 +0000

    machine unless fortunately mad memory

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
