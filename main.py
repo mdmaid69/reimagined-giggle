10f2eef0ad76dc22a9c434239638e5b5aae77082
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:11:38 +0000

    strike comeround temperature experiment past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
