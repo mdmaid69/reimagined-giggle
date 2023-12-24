adc964f78be78fa09c031b37742f07a6a6b8f18c
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:08:20 +0000

    strike company add worry comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
   def get_file_creation_time(file_name):
         return os.path.getctime(file_name)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
