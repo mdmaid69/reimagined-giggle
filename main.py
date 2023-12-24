4d267ca6493cdb0870c3b8d33528ba7f3ad51b3f
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:54:02 +0000

    could hard crowd pain proper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_acceleration(speed, time):
+        return speed / time
