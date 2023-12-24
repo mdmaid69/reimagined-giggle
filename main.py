519f183905456cdd052888788b431271155eaa0e
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:24:24 +0000

    improvement target afterwards history with

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
