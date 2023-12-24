5cf4413096b38953a33b3eec8778c7c2c7c24bc0
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:46:27 +0000

    away health official healthy history

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
