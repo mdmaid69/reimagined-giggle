99ffe950e096ab7b1e400078f74c90a314544b33
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:00:58 +0000

    his touch face also muscle

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
