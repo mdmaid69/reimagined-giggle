99f291a844913eac3d68244eab9d7eb89b64e325
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:57:21 +0000

    transparent punish hardly contrast window

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
