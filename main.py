3625b2f6f46fdf9c320008c8073afd15a07f2806
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:09:11 +0000

    begin takeout nor Uu drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
