51b268327bf2fd8c8095cccd1b4e51179b483a0d
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:15:19 +0000

    car crime trust pipe invite

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
