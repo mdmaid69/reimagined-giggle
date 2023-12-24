b0636c638ab46ad7f392aecb80c1f8d5a25ce312
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:36:18 +0000

    round choose benefit fence 179bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_floor(x):
+        return math.floor(x)
+import math
+print(math.pi)
