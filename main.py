8ae0b6d52c1eb1a9223297b80da75628092ba115
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:59:33 +0000

    unit meal regret 573entertainment spend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
