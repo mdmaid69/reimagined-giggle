645766b86ea34762ce88a69183be34d45233bcea
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:41:57 +0000

    most connect wash famous sing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_force(mass, acceleration):
+        return mass * acceleration
