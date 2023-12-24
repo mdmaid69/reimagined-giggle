7dae750f3a7435aa37154b58ee8b66ff26d5f29f
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:27:45 +0000

    idea regular again protect time

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
