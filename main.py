d644739a3009f723db98deab3078c3eedd1e38f1
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:41:34 +0000

    careful hour laugh believe sound

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
