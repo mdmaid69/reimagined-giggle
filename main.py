85ef3e59b744e4d685c33944646a20806bf1dcd6
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:08:28 +0000

    equal dark exchange breakthrough model

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+print(sum(range(10)))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
