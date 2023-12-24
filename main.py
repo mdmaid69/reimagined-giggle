5075dda6f3b8549cf1ae9c17adbd3eea3cbdb208
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:52:49 +0000

    remain block film couple quiet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
