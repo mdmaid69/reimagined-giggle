be5fcc5ebfddd80d406a3f78bac45578a693c683
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:54:47 +0000

    odd responsible alive exam tie

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import numpy as np
-print(np.array([1, 2, 3]))
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
