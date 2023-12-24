a408778ac9c7304b8f87b73eb487e4d408c4811a
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:29:28 +0000

    keepup detail health successful fight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
