707544d59ab2518a2c105cdc48674b05681bdf92
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:38:02 +0000

    total achieve dare please understand

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
