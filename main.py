0a62cee6d52b1184936f399759667c14e7f36a2e
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:15:43 +0000

    chance clear holiday middle rich

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
