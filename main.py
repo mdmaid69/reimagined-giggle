832abe2411901f7865c6ac425d125457e2445e4e
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:32:35 +0000

    upstairs partner child even use

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
