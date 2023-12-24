6126353be308b7b16accb68840b5a3e309d2d3bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:34:15 +0000

    walk apologizeapologise priority so really

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
