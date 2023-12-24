eeb7bddc8624d298f405884a59d35a632f5168b5
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:40:34 +0000

    glad production cross complete message

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
