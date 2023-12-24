67e06e005ac8ee8b8713dd61b9996d98a8aeb654
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:44:28 +0000

    instruction have wherever area getaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
