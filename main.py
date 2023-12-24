acb5a81fee2b509d5a8c2c71589a26400b7089e2
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:12:57 +0000

    heart escape tough bill colour

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def convert_to_hex(n):
+        return hex(n)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
