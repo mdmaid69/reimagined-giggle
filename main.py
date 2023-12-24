1ed5de9dec546b4c6d04d86d012d4ceab66c031e
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:00:58 +0000

    promise would bye that miss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
