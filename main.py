18aed98d15284d1a21f087be5eeef793b7009aeb
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:47:15 +0000

    deliberate convenient unkind tragedy share

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+print("Hello, world!")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
