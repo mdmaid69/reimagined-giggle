90aea0d26358dc762f50904cff7025ea2e390836
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:33:10 +0000

    worry press again fall combination

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
