e17bfe484e6dcbbae545066709ad132d3c028836
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:26:16 +0000

    soon block sentence Ee impression

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def convert_to_octal(n):
+        return oct(n)
