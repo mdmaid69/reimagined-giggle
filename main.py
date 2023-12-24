2a4385511c19fc3f780aa121c1e9720f94d851e0
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:36:44 +0000

    bringback that noone waste whoever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import logging
-def log_message(message):
-        logging.info(message)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
