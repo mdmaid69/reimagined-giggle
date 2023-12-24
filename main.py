a5b76c2f16fcfe64d2522dc4e5f4d8e9914dad58
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:13:47 +0000

    sudden difficulty accident relative stare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
