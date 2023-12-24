c0b985095e2584016f9ca2201646c47e69460f09
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:15:47 +0000

    hot water patience deliver cut

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
