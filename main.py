0dfbc4741653088a17225352d2df8bb3c5071b49
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:25:32 +0000

    cheat hour age nature emphasis

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+for i in range(10): print(i)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
