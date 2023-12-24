f75efd14a1775268f0dbc1ee2bb000c59a486faf
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:40:18 +0000

    tune record sale move machine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def reverse_array(array):
+        array.reverse()
