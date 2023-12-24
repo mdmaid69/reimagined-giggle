952c36b3139577292fc2484d7d702511c38c38f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:41:05 +0000

    like pride few look through

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
