24cb2d1a64c99627afa317df958ea70d21c1b640
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:05:00 +0000

    bread various special fire correction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_pressure(force, area):
+        return force / area
