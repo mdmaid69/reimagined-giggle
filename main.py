30148801a160e8d328691ea6644630e578ec699f
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:23:38 +0000

    race joke fashion many reach

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
