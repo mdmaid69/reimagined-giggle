dcc79ca0be6da8ecea605a511257e6754f4611b5
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:25:52 +0000

    succeed nurse base getaway giveup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
