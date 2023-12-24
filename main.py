994b6e82de8beeae8eae014552cc92d2c2d9dbf9
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:12:19 +0000

    people set smell every modern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
