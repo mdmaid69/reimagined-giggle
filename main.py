454755346a96f22b77e2b75c31d207ba47468696
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:20:54 +0000

    ilovedicks Dd willing important gentle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_typecode(array):
+        return array.typecode
