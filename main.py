f96a4129d2305ce4a0b88d031ae3b19c747b53f2
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:10:42 +0000

    world crowd telephone cover force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
