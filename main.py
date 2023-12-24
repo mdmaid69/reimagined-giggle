39d55fc519a8c5c3db86ff20c1b2c06ba82f3704
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:59:29 +0000

    safety neck we survive popular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def append_to_array(array, item):
+        array.append(item)
