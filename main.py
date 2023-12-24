3c588931f09a7492e8c2cd000b78809e625e35b8
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:22:28 +0000

    appeal grow direct comeback behind

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import random
-print(random.randint(0, 100))
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
