0e7ec41ed46e6f3de8b03e5bd1bb64a27a92a430
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:08:06 +0000

    record slowly goup pretty drive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_power(work, time):
+        return work / time
