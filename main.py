041f0c77d4904f80def64daa44c4da57ba66b154
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:43:28 +0000

    unpleasant sister conclusion spend famous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_array_item(array, i):
-        return array[i]
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def get_string_from_array(array):
+        return array.tobytes()
