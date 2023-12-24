62c5f4d19f26b3a06ea5927852f3fd05cdf2369a
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:08:33 +0000

    offence stretch offend wipe clever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_length(array):
+        return len(array)
