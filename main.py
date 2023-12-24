256ce7d321ea12c6a1a3b3c535cba556a67de4ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:22:50 +0000

    mess emergency dry mother lend

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
