118e074a92eb920d8440e8d53503e9b5e9fc2070
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:46:06 +0000

    difficult feature later calm spot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
