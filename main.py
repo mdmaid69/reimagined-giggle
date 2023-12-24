068976fe70aa957865f14b69d3cdff740f0ba890
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:23:39 +0000

    unkind crisis position separate narrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import json
+print(json.dumps({"name": "John", "age": 30}))
