a259c1b0a62bba51ea05e5c6c48d67e001da98d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:59:49 +0000

    on press opposite sure study

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import time
+def get_current_time():
+        return time.time()
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
