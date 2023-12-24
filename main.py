d4a4b9ae921807c35d3ce9f9b27fc2a3c527411b
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:53:37 +0000

    basis mend whole laugh defeat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
