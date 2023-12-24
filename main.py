3f0b1d93d7a5e36fa1d7e67c20e5be30755fd5ea
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:01:46 +0000

    preparation evening interest 572enter fine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
