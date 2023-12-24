5d944368a481fbc537b60b2d0698beb6ffc70e0d
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:59:14 +0000

    agreement mouth most famous sensitive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def reverse_string(s):
+        return s[::-1]
