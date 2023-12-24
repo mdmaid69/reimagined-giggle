14f4794779a30e3e1fb7e023646d6530ac91b78f
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:05:09 +0000

    thankyou Nn communicate ear goout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_current_directory():
+        return os.getcwd()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
