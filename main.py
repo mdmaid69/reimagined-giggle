02cb9bd907069ec55d0d4a05e16f31d9c8fb819e
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:31:18 +0000

    who notice hurry mind success

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def clear_array(array):
+        array *= 0
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
