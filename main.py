9056f56b6331f63943a57410b602b9dac5c62dd5
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:09:52 +0000

    sense talkback comeover good holdup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_density(mass, volume):
+        return mass / volume
