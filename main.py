34a3b12016bd529945545e4a45ee47b91578318a
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:55:08 +0000

    kind Nn relax exist club

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+for i in range(10): print(i)
