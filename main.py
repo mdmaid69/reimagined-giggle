cd608114d4358afda236657e43da392f06562ce9
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:46:12 +0000

    know string turnout sensitive payment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_power(work, time):
+        return work / time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
