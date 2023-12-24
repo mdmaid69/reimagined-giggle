63f4e6ecf80e01b2bbd838dc84c4d92efb5cf4f2
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:36:53 +0000

    considering offence considering married part

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
