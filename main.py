41d4b932d5e1f14369301a71deb4b3224e7f4d0f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:32:20 +0000

    beneath dinner advise wealth break

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
