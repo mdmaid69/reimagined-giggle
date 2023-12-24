e8e5dbbe34f5065af4a0542df2f3f4c8204902e8
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:06:40 +0000

    exchange concentration offence thing breakout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
