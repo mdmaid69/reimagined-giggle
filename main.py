a769f69346c7d87a2df0bd545a506aa1d1dc267b
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:20:50 +0000

    blow earth accident into holdon

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
