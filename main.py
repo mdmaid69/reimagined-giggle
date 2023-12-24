175527a8acfc80c7a093e504b7b75f15ec9cf070
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:49:52 +0000

    after exam welcome room survive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_speed(distance, time):
+        return distance / time
