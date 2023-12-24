6715fdc2797b5d858c170db41a5adf73b53600e5
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:27:15 +0000

    desperate lean somehow window flat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def find_min(numbers):
+        return min(numbers)
