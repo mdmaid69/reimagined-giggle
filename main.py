e6506bba6def3a4832edd25e9de0ad4eb1138164
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:24:12 +0000

    fashion shoot relief meeting confident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
