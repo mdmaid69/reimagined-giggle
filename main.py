824a0727c6ba89c9e2e95cecf4ede4521f6303f2
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:25:07 +0000

    cheap unit broad weather bring

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_item(array, i):
+        return array[i]
