40e97b79d7dca1cd2e8e2bed66c0e28a4673ae2c
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:36:23 +0000

    help matter smell between respect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
