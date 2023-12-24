30eaf26c85f5a3602d4ec583e98e405c90bd95e0
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:50:05 +0000

    print season concerned combine content

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def square_number(x):
-        return x**2
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
