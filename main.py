54668a9de1648a2b6a0af160c40d714c8a048d76
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:13:44 +0000

    ashamed worry seem wish collect

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def count_characters(sentence):
+        return len(sentence)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
