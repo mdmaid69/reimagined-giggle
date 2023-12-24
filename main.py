ec4f01473b5ccbe179cd9a5b17acaf0405ea4440
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:00:20 +0000

    head tonight sensible computer careless

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_acceleration(speed, time):
-        return speed / time
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
