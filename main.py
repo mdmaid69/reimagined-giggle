822368eed0447ec205e2406daf1a5ed21499397c
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:12:03 +0000

    wing blame material original lookup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
