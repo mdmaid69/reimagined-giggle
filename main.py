f38e1ca243edce4a3b9eafaa3f48162d5b377f0b
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:01:39 +0000

    all best argue work new

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
