5641731658d358bbc2288d82ee29bb38807df548
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:16:16 +0000

    neck respect Pp getout guard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import os
+def change_working_directory(path):
+        os.chdir(path)
