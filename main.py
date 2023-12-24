73b20671ddaf4b2bf9df6be7e7b9c3e3d566d45b
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:15:08 +0000

    something state criticize foreign next

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_gamma_function(x):
+        return math.gamma(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
