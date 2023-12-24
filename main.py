13583a0f0fe47dbd31066d95dc0091fe25087e8d
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:50:43 +0000

    quiet special safe shame object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
