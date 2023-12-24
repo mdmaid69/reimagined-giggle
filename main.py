73b833e4dbc38d54281d7584d1b14a9e83a0730f
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:17:32 +0000

    arrange far trick fly taste

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import tensorflow as tf
+print(tf.__version__)
