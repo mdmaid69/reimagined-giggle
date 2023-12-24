c9e85abca2217b9e7d1e15c10447e74531e38844
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:23:45 +0000

    worth no war approve perform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
