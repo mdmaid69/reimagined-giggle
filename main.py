92c618215f207fb20c481c2e29001cb3ceac333e
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:37:08 +0000

    flow shall separate size tell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def is_palindrome(s):
-        return s == s[::-1]
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
