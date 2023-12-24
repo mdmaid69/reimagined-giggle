c2a73e2df63cf8570e0d2c0fff06daaab558e1ae
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:40:40 +0000

    sore steam rush set actually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
