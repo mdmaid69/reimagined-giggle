5aa48be5904557aa00574319d5b3a31e5cb55b95
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:50:23 +0000

    future band immediately great mood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
