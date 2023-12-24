4ffa85b0084387391add7918e3cccf5be71161e5
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:18:16 +0000

    Dd exhibition temperature work add

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
