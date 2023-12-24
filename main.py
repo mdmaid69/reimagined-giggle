2b236ec2ec54a4cd940233d32ccc04efbc05612b
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:15:41 +0000

    decide yours speed comeout vision

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
