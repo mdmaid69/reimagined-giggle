c2d17966526487ceb1c570996b309ea22597357d
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:27:10 +0000

    doctor earn bite lie image

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
