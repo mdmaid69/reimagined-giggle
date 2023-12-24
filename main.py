6be08f8e7acc4f59a6468c97c4e6d042d5350149
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:43:57 +0000

    level brave medium head snow

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
