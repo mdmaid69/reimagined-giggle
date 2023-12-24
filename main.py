13f01babda829fd88031aa20c606274c9eb85fb2
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:51:13 +0000

    565end blame separate several he

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
