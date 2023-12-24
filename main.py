fea71604118f621e3e32d46491a687c3341306fa
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:57:02 +0000

    everywhere since careless plate expensive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
