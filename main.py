1b0af3e75f0e6dd8a5689e1b9a188c012eb37294
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:01:43 +0000

    arrive offer same giveaway food

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import sys
-def print_python_version():
-        print(sys.version)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  def convert_to_binary(n):
+        return bin(n)
