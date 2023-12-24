db52cdad78c2952e5fb689ff3ed37706a38dea21
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:33:25 +0000

    embarrassed nervous female heavy collect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
