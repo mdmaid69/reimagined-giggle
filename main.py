d2b9a19eb348ae9c2e36246b4480303680726434
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:41:59 +0000

    tone top pull comeround march

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def add_numbers(a, b):
-        return a + b
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
