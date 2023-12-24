bbb991b13169a33052f9781d2b3ce8d4710aaa7d
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:24:12 +0000

    judge difficult quit separate whistle

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
