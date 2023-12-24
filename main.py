25a41085ae73f7221da9347ab40106b0e1f4605a
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:53:46 +0000

    fall throughout speak stare compete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
