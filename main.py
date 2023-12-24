405b13c92c926a6c1363156006901ff76d8d1b90
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:51:44 +0000

    world in book sudden custom

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
