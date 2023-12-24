e68925c5cefc6eb8bcaf5c3c7e10ca912b8ee021
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:26:23 +0000

    swim protest sting afford road

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
