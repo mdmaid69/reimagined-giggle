d95f4d98af93fc52d4ff80e147270a54954f37c1
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:23:26 +0000

    last part post hang muscle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
