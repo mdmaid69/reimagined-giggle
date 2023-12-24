659bc49cdf97188e4b76e354de7606377bb4c3f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:09:38 +0000

    dream still crash born dull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
