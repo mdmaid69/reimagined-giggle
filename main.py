610744e916dc4ccb1f8892c8182ab9ca451fa253
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:21:09 +0000

    fat scream plenty minority unit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
