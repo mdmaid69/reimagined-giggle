07f6d814e22c65d2a4b1b98ed67e73f726d80314
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:38:42 +0000

    frightened course leave bill wind

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
