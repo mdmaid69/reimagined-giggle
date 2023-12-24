d3837e063faf44c579f445245e9fa98cc798c9ec
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:48:12 +0000

    now stiff mixture surprised rely

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_min(numbers):
-        return min(numbers)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import collections
+def create_user_list():
+        return collections.UserList()
