1f377b6ad7c115eee0053501203e85c9ef4cca2d
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:34:34 +0000

    type minute opponent separate shall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def convert_array_to_list(array):
+        return array.tolist()
