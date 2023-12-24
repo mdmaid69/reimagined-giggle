34637321e34e60021f53d4c288eb4a209f97be67
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:33:08 +0000

    callout test attract branch suit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
