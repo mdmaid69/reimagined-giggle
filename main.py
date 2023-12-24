e99ff604c3d5ef819da7df7d2c74a38e9365f293
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:43:04 +0000

    interview wire easy lookback(on) basically

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
