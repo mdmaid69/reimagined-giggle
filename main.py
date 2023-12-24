a613be4b5ae09440905a13448114b1d0d24291d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:54:36 +0000

    pause lonely downstairs rule file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
