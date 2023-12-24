fb8ebadaf482fa3070892788f55e18349c3aeb5d
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:12:03 +0000

    subject lookaround cap help Ss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
