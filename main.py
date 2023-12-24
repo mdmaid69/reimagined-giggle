4a7c2d9b341430a4289274410e7aebe5ed0d7724
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:29:46 +0000

    ride approval unfortunate especially keepup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
