bd08fefe75c6a22a7bc58bb7f22708263b208377
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:39:50 +0000

    quality point unfortunate return accept

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def sort_numbers(numbers):
-        return sorted(numbers)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
