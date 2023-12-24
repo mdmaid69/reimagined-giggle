c9f9a48975e7ab0c34e329fcf6481ca4ed954d5d
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:20:06 +0000

    within press top already near

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
