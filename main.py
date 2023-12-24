9d2b590a28828fba68a512c3102a927ce05563b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:44:02 +0000

    something address through journey early

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
