c0f3835837b6f53547615cfe7757e96ac4f7856c
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:22:07 +0000

    try direct strength frighten home

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
