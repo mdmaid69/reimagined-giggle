cfaf322f930dfd60632390665e43a4828753b01d
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:07:31 +0000

    body thought steep autumn 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+print(math.pi)
