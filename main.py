66e2ec24b082d26570d637283ecb41671ce34581
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:02:52 +0000

    shrink mess over future disagree

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
