d6905d46b7478d8d7d3d09eda6ce5b536137535c
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:28:26 +0000

    team repeat wind principle fond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
