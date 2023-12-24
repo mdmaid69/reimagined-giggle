9a0495c3a4b3925831f84f587697344a71a76814
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:55:24 +0000

    build swim complaint smoke unlikely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
