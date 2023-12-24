8fed0f8583ea5a2e21e730e9d4dde1efe8e31590
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:07:52 +0000

    factory laugh realize goback(to) as

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
