44ce7f53e32f8f2c89bf23b19b28c5462537dc66
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:20:56 +0000

    bus whom talk trap deal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
