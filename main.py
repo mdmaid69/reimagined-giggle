279c6dc240a2ab04a1cb90cf635ca90850adbee8
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:37:52 +0000

    uncertain hurt union evening cool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sys
+def print_python_version():
+        print(sys.version)
