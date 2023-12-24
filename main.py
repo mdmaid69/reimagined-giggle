5db2250024200e5bc3f7cc5a7d1bb7c9ec132448
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:27:15 +0000

    health wind excuse full forgive

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import random
+def roll_die():
+        return random.randint(1, 6)
