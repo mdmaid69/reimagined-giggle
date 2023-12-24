0efaf9eff8253e847de9a031ca65ceff083ae273
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:08:45 +0000

    succeed tape 573entertainment suppose prize

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import os
-print(os.getcwd())
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import tensorflow as tf
+print(tf.__version__)
