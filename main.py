b7d59bcbc51d3547679099210b48f2f423b3aaba
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:24:10 +0000

    poor and slowly pretty piece

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-from collections import Counter
-print(Counter("hello world"))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import sys
+def exit_program():
+        sys.exit()
