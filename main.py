f556c3b83ea47f5bc6cc4e43445cef9e669a3317
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:16:28 +0000

    through difficulty weekend work comeout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
