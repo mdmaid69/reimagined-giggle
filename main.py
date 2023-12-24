af07b4da114c002f0619d150109f74a172c91df6
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:35:35 +0000

    noisy keepaway other from comefrom

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
