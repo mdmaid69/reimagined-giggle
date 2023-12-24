c651268d81a52ea91dd6b7ff92a173d9ddba97d1
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:15:11 +0000

    town set pocket water difficulty

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+i = 0
+while i < 5:
+        print(i)
+        i += 1
