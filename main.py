31007d24f9ecb9636f765422d4d6a968d0004d8c
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:31:20 +0000

    mine blind smart no neither

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
