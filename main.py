52bb2d791d053b64837eb1324a9879bc6c7bff66
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:14:20 +0000

    bringforward edge respect admire visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
