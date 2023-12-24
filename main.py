4de9f1dc31c34733898b09d9958668170284cf18
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:39:21 +0000

    land cheque patience pretty callout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_item(array, i):
+        return array[i]
