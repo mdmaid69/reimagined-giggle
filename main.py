7571ee6383eef19bbdeb26735205dd1aaf14bfbc
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:52:12 +0000

    invite cruel ilovedicks I clue

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
