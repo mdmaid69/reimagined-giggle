68bd01ed1bba1fe5a87b20671255a165fa168ccc
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:56:18 +0000

    telephone main wear hope public

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_work(force, distance):
-        return force * distance
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def get_array_as_str(array):
+        return str(array)
