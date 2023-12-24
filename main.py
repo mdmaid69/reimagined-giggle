b21fea426cca8be9a08e0fb068a75693c156f390
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:39:42 +0000

    still create borrow wherever exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def add_numbers(x, y):
+        return x + y
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
