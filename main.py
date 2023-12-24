d89685bd57a027ac496963713e66b03f5593aab4
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:46:26 +0000

    little material left keepaway politics

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
