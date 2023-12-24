be96024d1fafef81b9a6b449cf0757137cbbdad9
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:54:18 +0000

    blind danger as chat check

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  def convert_to_binary(n):
-        return bin(n)
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
