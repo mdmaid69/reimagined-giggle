86e8fa10627932c5b66fc2df3cd8eea6be257aa8
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:14:44 +0000

    discovery in obvious heavy start

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_acceleration(speed, time):
+        return speed / time
