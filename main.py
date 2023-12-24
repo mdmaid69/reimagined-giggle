858bfeb6db425cfd9fad176ea9860247bdfb5647
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:05:01 +0000

    wake rich news operate hold

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import logging
-def log_message(message):
-        logging.info(message)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
