79bf7f404db041b3b6a7658e3a13f5c85cd05b36
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:16:26 +0000

    cost shelter few point period

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
