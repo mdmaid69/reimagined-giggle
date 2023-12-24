6904b4bcd2ae6f9cccb456f4d10fcaf054e9cda0
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:41:09 +0000

    fact runover scrap foreign wealth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
