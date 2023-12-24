42225c419e3179306b3fd318a69c03721dfa8fbf
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:43:03 +0000

    slight normal century not kick

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_max(lst):
-        return max(lst)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
