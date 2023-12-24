409de66ce0b621a93b914789e406649b2da9af2e
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:45:48 +0000

    hand cross balance tool shake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
