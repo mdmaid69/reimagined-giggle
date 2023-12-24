a523c738305e5d8bae54b25eb643c3301e2ac587
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:24:16 +0000

    about success excellent behaviour benefit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import os
-print(os.getcwd())
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
