03436be926c167d6b6088a2ef219b4cb8fca7b90
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:15:47 +0000

    dress impressive single rest complain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def convert_to_json(data):
+        return json.dumps(data)
