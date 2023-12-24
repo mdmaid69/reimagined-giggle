fa0dcf28633c7617e615ab1a47ed39314c4a7a0d
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:29:25 +0000

    term stare mean relationship platform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import os
+print(os.getcwd())
