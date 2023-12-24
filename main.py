a472e23dad47c74a68b6054bd54fa90f4e999909
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:15:21 +0000

    equipment interfere catch fold evidence

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
