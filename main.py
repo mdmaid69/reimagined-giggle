587e66ce8e23a6fac7aa356f7542a4fc602144e4
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:07:11 +0000

    motion cause no bet mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
