d1cade4a6a71f6ab26a823ca91a184da2a7c68b7
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:43:56 +0000

    hand recent breakdown store certain

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
