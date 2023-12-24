8bc6333d37a6d31e028004559bb5fb2457fefb3b
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:03:21 +0000

    connected nice dress will show

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
