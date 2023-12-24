5ce6beaf2f46032ed989614d4607b8dcf9cc08dc
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:32:25 +0000

    completely truth pause immediate priority

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import time
+print(time.time())
