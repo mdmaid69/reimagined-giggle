22aa846aa50df865faff4a3856ac1c77408930bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:46:04 +0000

    immediate absolutely brown better substance

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_str(array):
+        return str(array)
