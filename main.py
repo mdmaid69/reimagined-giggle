9c8540e432fc8d50536a0d68729c6430612fc24e
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:50:44 +0000

    anywayanyhow meal guide breakup certainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
