adaf35c55d5b72f749d3b6cabdf21f220ee1964d
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:55:06 +0000

    month introduce how serve swimming

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
