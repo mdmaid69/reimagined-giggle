446acb5c86bd1bdd7754cd3d441e3cf0785122d2
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:29:55 +0000

    confuse side keepaway would bad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
