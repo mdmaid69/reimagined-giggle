36187e786f1eec4c636283dfa7137d3f6066f4aa
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:50:01 +0000

    roll sweep effect official present

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
