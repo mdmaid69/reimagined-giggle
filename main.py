dc4fa579a5c1324a0ccd6cfe5dd7796c8b7109f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:57:57 +0000

    opposite bringdown character undo poor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
