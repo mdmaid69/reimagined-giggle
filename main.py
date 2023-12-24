4f89c2a1445b5009103c01a5d62b2355ec76993e
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:05:39 +0000

    calm improve night left underneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
