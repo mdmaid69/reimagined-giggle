7e3727202d89cff2392267d6ffeda826d47c1e7b
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:30:33 +0000

    university bet know bar start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
