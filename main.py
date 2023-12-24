ffea3e0b9cf889109e8238457397e2ff6debed0e
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:20:45 +0000

    dark easy breakup design card

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_power(work, time):
-        return work / time
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
