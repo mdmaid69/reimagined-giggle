5de0f470474fac7e9c184243efe2ceecc8a35e23
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:42:49 +0000

    ride wear apply speech 572enter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def get_array_as_str(array):
+        return str(array)
+  def calculate_area_rectangle(l, w):
+        return l * w
