d17d4ce677eebab46034faf53cba0b800dcdc1d3
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:47:50 +0000

    car giveout train treat listenfor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_power(work, time):
+        return work / time
