29e62e0cf6259d2120115b1b3c9eed9ffe8cc246
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:45:55 +0000

    exam his bar frequent trick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
