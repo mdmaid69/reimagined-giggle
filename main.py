c284012c28427dfd957d5c996b84b7d988d6549c
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:27:55 +0000

    race either leave pure follow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_acceleration(speed, time):
-        return speed / time
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
