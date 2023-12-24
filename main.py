5a15ebf1803bccf680fbcef0f96a047de805d58f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:13:48 +0000

    perhaps test expression other cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
