8b083cce12f9eb0d6f2292cbf500112019d32759
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:04:16 +0000

    combine remark collection image beginning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
