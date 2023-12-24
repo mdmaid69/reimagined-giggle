b9795356978303323d1e41aaa49b86a81ecfc265
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:31:18 +0000

    head mad wear press famous

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def subtract_numbers(x, y):
-        return x - y
+import collections
+def create_user_list():
+        return collections.UserList()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
