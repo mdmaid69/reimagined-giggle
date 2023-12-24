c1dc39657585b7c9a7baa48673956eb5adffc57e
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:11:17 +0000

    look shoot convincing sound part

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
