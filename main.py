cbc7224de3a218bb50015256a543a8b88f6236f9
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:43:21 +0000

    occasional around angry slide send

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def add_numbers(a, b):
+        return a + b
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
