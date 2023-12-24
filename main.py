b088e71cd56ff7c4e91e45452b1df50d819f1062
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:45:36 +0000

    connection change I admit capital

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def add_numbers(a, b):
+        return a + b
