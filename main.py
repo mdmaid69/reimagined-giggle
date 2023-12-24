6324238f23fef66e4f59241a33bba54c759578eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:57:38 +0000

    prize control boring mention drink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
