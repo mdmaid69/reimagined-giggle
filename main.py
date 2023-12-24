f2b56798463c3cd713e9abe5876361925e945113
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:56:10 +0000

    ahead direct true total follow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
