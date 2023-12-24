c515b98fa08c6195ca73fd6a3e6a05d4fc54b143
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:37:59 +0000

    block outside object music ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
