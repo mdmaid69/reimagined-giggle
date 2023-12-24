f3c43099bd456e16625d17459d47239d8c3af35b
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:12:33 +0000

    eye worried in generous either

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
