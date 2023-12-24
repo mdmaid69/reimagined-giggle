10e73764d247bcd81354feb7549f9a7d8c224579
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:03:26 +0000

    when Uu high stay remark

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
