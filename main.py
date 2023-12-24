296d8b16a119b72c09669e5887f7f8504b0e8e09
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:31:51 +0000

    welcome combine evening outside morning

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
