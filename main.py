aa962aac8c47cf01d8149323b1184fc8801e79db
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:35:27 +0000

    luck around win centimetre 572enter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
