f65bd05f0f412e440bf8a79f0fa48d71262353cd
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:14:05 +0000

    hold demonstrate home fear direction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
