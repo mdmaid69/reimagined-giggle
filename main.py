bd238e9775f1bcd33c6abf5ecc3860da7aec1d52
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:23:52 +0000

    region protest only suggest loss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
