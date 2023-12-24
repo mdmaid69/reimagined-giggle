c6a706171fa2fdd3161ec4f24c173eea7852f16a
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:12:30 +0000

    office peace us protection eye

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
