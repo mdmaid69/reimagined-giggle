e5ffcf3f2c0ca7f3a1149a14988d61cc80ba94e4
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:00:02 +0000

    giveup underneath press punish display

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def calculate_work(force, distance):
-        return force * distance
+def add_numbers(x, y):
+        return x + y
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
