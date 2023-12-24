cec9b9c3d69689294c6099f12b79eeb8c72e036c
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:52:53 +0000

    comeout cry deserve noisy why

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-print("Hello, world!")
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
