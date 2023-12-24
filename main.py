32b1f00693780ded3bd922f80f4acad989015270
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:35:41 +0000

    OKorokay stop instant off treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
