1ef1bc40bcc68a269b0d4dab4b6a58a6b9ae39ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:58:38 +0000

    argument bite someone rush understand

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
