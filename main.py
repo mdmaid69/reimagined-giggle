b86b88965d91aa2aef5f7cbd930a8007cd757553
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:39:24 +0000

    care everything steer even term

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
