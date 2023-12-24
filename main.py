bffe97119abbfee14b64b356f07bb1df44f584a4
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:29:26 +0000

    wherever dick hurry lunch contact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
