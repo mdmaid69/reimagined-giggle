ae569d592aa0ffabe5cf377d1a36380baa74d71a
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:13:10 +0000

    cruel in tooth past press

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
