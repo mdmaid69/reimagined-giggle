6a669bc24aea6a64e895474a21fd9f0b63a18513
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:51:54 +0000

    damage standard mind spray height

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
