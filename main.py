3ce0ded09affdd8da25bb7f10dbdc152871b1316
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:27:39 +0000

    afterwards worried fail brown defeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
