327f3ec5ca466021e96830cca3324881ebfd6887
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:19:35 +0000

    treat special schedule see worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
