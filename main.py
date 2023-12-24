fd7830c8f48d5ba4237022befd81daa3b006964a
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:09:06 +0000

    needle guide laugh nigga that

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
