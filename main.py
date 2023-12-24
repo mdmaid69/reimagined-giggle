556d804171c9a3806e263f96b515732a624237ce
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:40:18 +0000

    foreign child rule schedule drive

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-for i in range(10): print(i)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def add_numbers(x, y):
+        return x + y
