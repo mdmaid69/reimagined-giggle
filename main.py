e7b91a0da2b4234b9d6c4a577e0ef4336c7ac820
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:19:53 +0000

    suck black below increase himself

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def convert_to_octal(n):
+        return oct(n)
+for i in range(10): print(i)
