872b9591a9858ed6675f72396fc694ef5baae8cf
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:23:44 +0000

    this trousers active attach final

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_tangent(x):
+        return math.tan(x)
