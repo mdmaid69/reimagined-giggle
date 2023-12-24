bf64451c873c2857bc9600ca4d6ac75d6e5e516f
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:32:12 +0000

    original solve get funny point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
