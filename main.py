e08930cd93c9aaa0342053097db808c80347230b
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:14:21 +0000

    payment great equipment help room

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_min(lst):
-        return min(lst)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
