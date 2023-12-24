051b8467c1971b2089f116abd5dcd5133e0d3b0a
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:24:52 +0000

    otherwise bye OKorokay target still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
