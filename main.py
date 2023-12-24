21e6502b8ea2f5cc470b3840454684022f4d5e1b
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:08:02 +0000

    trust depend guilty form impatient

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
