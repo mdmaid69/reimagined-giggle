38a73a109abc925384c941aa38ff67db133ed7b4
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:55:05 +0000

    extra greeting further punishment 568energy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
