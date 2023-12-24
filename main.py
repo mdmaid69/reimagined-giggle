305b8d506ea0c6df148df0de59aedf75f0024c4d
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:42:54 +0000

    relax exchange government herself giveback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import sys
+print(sys.version)
