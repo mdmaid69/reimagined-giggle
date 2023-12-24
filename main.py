cad55570601e113dd32ab6c982a0565ed576f95e
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:30:24 +0000

    late past crash dream father

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import math
+def calculate_sine(x):
+        return math.sin(x)
