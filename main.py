b4d78302b710853c8cd5c2bdbe672a5800ba4d5a
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:17:26 +0000

    factory stranger pretty contents 568energy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def find_max(lst):
+        return max(lst)
