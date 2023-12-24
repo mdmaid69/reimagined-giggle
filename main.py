0192ae8394e2e16655f0d33dc5b85fa8c962c8c2
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:34:18 +0000

    fair limit practise factory comeover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def sort_list(lst):
+        return sorted(lst)
