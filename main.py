7e417b4ae9cb5eb97666afb5d1f07a20a79140a7
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:40:19 +0000

    decide desperate minute decide spare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def reverse_string(s):
-        return s[::-1]
+import array
+def get_array_as_list(array):
+        return list(array)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
