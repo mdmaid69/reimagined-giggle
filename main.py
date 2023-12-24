161677cec38c4aec14aa17b2f2b28ef2291310c9
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:36:39 +0000

    especially natural shade programme bush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
