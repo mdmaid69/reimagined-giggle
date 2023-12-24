f53490900f80ca7a0fb271e89d16e869e26af148
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:03:19 +0000

    discovery plenty answer bring large

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_repr(array):
+        return repr(array)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
