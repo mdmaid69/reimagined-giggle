2d272d518fe00a1a8da8e0302011c400a081454a
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:57:29 +0000

    machine minute identify serious drawing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
