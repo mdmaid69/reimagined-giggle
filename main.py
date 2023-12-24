1520fd5da85e7e9491c8dbb83906b6ab15c70846
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:45:32 +0000

    friendly minority tall hardly approve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
