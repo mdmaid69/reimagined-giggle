08a97221c3012a87e2b0e7dcdb9f213ed2a09c6c
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:23:49 +0000

    health badly introduction outside comeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
