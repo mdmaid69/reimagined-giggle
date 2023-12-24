e7cd3a72722bec406283d5be11fffd7c75b2d774
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:17:02 +0000

    check so accommodation stare steady

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
