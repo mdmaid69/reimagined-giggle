30712c1109d04ea752dbb33cbabf8dfae19220b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:36:32 +0000

    confuse either condition pool backwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
