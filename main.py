e6e3f3c86206f53823d9f2990ac34d91438fb5e4
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:42:46 +0000

    pot risk mail make lie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
