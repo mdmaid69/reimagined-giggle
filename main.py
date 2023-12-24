67ca1dbbe74498098aebc5ff0acd23a65a8d9598
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:23:37 +0000

    match pretty smooth precise rule

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import time
+print(time.time())
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
