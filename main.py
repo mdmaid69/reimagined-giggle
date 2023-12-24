299c10049d9fc8814f62090b3978d331a7b7c18e
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:02:27 +0000

    uniform geton that thinkof split

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
