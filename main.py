57106b9737883ae4d120fdb9d36213d2dcfaea03
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:21:23 +0000

    apparently event object honest sorry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
