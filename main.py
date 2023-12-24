0ab2f46121fca173d449b5b7820133d4595ee18c
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:59:16 +0000

    poor search protect strict hard

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def convert_to_binary(n):
-        return bin(n)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import platform
+def get_python_version():
+        return platform.python_version()
