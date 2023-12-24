fe77d0e762f3117ae570159c0655c46bff5a6591
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:46:38 +0000

    group slip unhappy movement church

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_sine(x):
+        return math.sin(x)
