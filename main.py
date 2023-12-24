454998a762309f4cca8f53d7bc95c00065461105
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:24:18 +0000

    please past test think watch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import sys
-print(sys.version)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
