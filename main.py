9b3106205b6f59ae3746e268bb68a9467185e3f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:04:19 +0000

    unkind prefer hope tax she

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
