06fb115ccb883301b43bea69ce93922d1eb29753
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:08:05 +0000

    takeaway best country take himself

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import sys
+print(sys.version)
