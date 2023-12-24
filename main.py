6c0d0bcff8fc44698889ca12939792864dd0d4b9
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:06:46 +0000

    mine project tongue satisfactory produce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_acceleration(speed, time):
-        return speed / time
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
