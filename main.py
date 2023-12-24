c01667f727a9262feca7a17fc568680108f42ff5
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:47:10 +0000

    scene hold our undo happen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
