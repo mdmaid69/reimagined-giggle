b522eb1e229c4721790aef2703686de6ef6677df
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:10:17 +0000

    stop arrangement Yy light precise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
