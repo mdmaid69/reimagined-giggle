f612aae69086f83860876995ee17c4aa08cfa3d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:56:13 +0000

    accidental strike cross own wish

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import numpy as np
+print(np.array([1, 2, 3]))
