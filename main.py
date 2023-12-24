33438f28f646b1b39cf6e6ba3cb6d40ed3b399c2
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:18:30 +0000

    tooth hold get bill file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
