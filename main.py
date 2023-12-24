7612d78cd47c2bd784448f6fa98e8aa49dd57702
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:36:15 +0000

    basically 179bite fall reject little

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
