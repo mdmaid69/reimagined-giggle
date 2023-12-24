f53e35f17a7ad0bd9b76cf0b3870f427b678a132
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:57:54 +0000

    equipment before tour hospital aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def divide_numbers(x, y):
+        return x / y
