9419063d188a59dfd9db55de303027dd866bc3ce
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:12:40 +0000

    beginning plenty thumb building whoever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
