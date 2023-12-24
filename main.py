483886eda86cb8ce703884a553f5c965d8f32faf
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:04:26 +0000

    nearly tragedy length another plastic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
