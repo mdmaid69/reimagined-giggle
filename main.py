ca9e01554095d35b7a6096abc263a4e2659d4d65
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:44:52 +0000

    pin ours cost reply image

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+print(sum(range(10)))
