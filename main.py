516631850fdbd9bbd2703af469cd581c81284cce
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:50:20 +0000

    head drop doctor drive luck

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
