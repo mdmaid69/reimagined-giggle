0f5c5d14310a38f539093288dfedbbb0f11af14b
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:57:59 +0000

    flood actual violent fly stare

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
