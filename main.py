d0d77932ce11006afbf7b820659ef8dbb3084dc1
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:36:05 +0000

    time put ball head song

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import os
-print(os.getcwd())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
