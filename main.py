8c092f80c272390887449f8c20cbb73e09c5456a
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:51:15 +0000

    set mark telephone children number

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def find_max(numbers):
+        return max(numbers)
