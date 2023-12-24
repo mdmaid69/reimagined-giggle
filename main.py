fd0204f928ccaaecd6ee2adef837004d43c52c36
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:35:43 +0000

    unlucky warm part language excuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
