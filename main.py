3c1da424be13079b0ea3bbb14c9ba920515ca240
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:59:32 +0000

    inside squeeze exercise 572enter bag

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def is_even(n):
-        return n % 2 == 0
+def reverse_list(lst):
+        return lst[::-1]
+import json
+print(json.dumps({"name": "John", "age": 30}))
