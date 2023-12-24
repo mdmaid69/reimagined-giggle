8ecd621dfdb1826ed3d05e3764834973df877f27
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:49:54 +0000

    win this rarely translate size

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_typecode(array):
+        return array.typecode
