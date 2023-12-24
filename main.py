0bcd4a70e17b2e7816659e165c9e9769dcd9daeb
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:27:13 +0000

    person born connection bury turnup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
