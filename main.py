2087fc70c16661ebbb79cd02a9e08dcd86bdd6eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:10:51 +0000

    hot clothes consist gap debt

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
