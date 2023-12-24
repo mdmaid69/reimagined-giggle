1f3e97a66cb1e03d00c66f8eb113536b23343488
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:17:29 +0000

    address delight steady play limit

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
