7d6e23dd551010211c83d75fe86059e40c70e9da
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:57:05 +0000

    niggaacademy 573entertainment giveup development everyone

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def reverse_list(lst):
+        return lst[::-1]
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
