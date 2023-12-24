087617263120944aec7a54535e5836efcc2d023a
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:48:02 +0000

    by dangerous public possession class

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
