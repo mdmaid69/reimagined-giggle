4bd119b657df470960617b73ba1c7337f577e363
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:28:57 +0000

    breakdown common beat embarrassed country

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
