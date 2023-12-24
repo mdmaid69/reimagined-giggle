934e1f19343c62b30e2726a3ca65041645d99960
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:42:39 +0000

    post often scream bush fast

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
