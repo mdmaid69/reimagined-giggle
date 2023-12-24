b0a519359cdc89a0d7345765385b5a69ecf3e824
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:41:55 +0000

    whistle pour shoulder several catch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
