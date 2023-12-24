4118c6e19a1c12cdd3ab99759740144c33737482
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:01:21 +0000

    nothing praise attempt 179bite injury

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
