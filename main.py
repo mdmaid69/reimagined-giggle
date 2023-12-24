ae65917093fb3f1b393710ce565e9e046f54306d
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:15:18 +0000

    car splash mood idea wall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def print_python_version():
+        return sys.version
