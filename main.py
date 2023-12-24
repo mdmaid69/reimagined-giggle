ea78b8414f34bcf31a72f60cf0ffa977f6601e68
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:50:23 +0000

    occasion feed giveup by fit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def sort_numbers(numbers):
+        return sorted(numbers)
