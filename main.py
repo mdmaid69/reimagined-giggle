a298234006e5c7842eaa9e565a93c32cb5912480
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:54:32 +0000

    ago final stage note unlikely

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def delete_file(file_name):
+        os.remove(file_name)
