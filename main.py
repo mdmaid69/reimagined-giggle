835545a6fc22a6e927d836f9d4e1d22b7fb3f3d1
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:43:10 +0000

    wave firm tour rain check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
