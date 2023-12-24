1a567caac9ea625bbc9407284935aab529cb0767
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:27:19 +0000

    appear very picture share matter

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
