923569a27e78f78eb2dbfeaa20cf29dce6b1a0f1
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:33:34 +0000

    apply division pretty herself rather

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
