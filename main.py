6e4f42f12a5765204554f5e7c563043992fb2632
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:38:08 +0000

    reliable full fire label far

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def find_min(numbers):
-        return min(numbers)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
