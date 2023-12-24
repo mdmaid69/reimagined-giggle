4980888f29e1319a46551427c39fc2c444c635e9
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:29:54 +0000

    any clear lower thing petrol

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
 import array
-def get_array_as_str(array):
-        return str(array)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
