5dbba4976f289c1eba30129752b6baa0e150a701
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:31:44 +0000

    person still legal demonstrate question

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def get_array_typecode(array):
-        return array.typecode
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
