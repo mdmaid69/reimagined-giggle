de4eb95dea3aef77d834301d4640055301b33ebc
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:53:41 +0000

    off increase people pretty pile

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
