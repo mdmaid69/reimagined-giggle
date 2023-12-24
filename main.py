583616f9388f25fa57dd3d239fb5953e9feac15c
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:59:40 +0000

    way base actual himself undo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_as_str(array):
+        return str(array)
