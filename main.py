4ea29f4d7fb8bb792391163488e6d9a9d0b21f75
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:56:48 +0000

    admit modern pleasure each flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def reverse_list(lst):
+        return lst[::-1]
