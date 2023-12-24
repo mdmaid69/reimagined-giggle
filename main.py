a855041db4d1014789bf2b29879cdd8c2ceaafe9
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:39:37 +0000

    extraordinary theirs world smooth comfort

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
