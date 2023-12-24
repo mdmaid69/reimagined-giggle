1cbd676cb289382f24e898555f79606c0df28d6f
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:07:41 +0000

    risk defeat exhibition away this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
