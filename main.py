04418c928f7a0be39f683b0d1d3bd5dc876d42c7
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:02:52 +0000

    intelligent badly Zz main arrival

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_work(force, distance):
+        return force * distance
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
