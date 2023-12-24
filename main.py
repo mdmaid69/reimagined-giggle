7cae62b80ad0d85c61d88fb7c4b3d8345e14e348
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:25:05 +0000

    interesting little sometimes fall university

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+from collections import Counter
+print(Counter("hello world"))
