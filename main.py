736f0a5956b1a8c7a6b96ed0a7ae978257f3c249
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:12:36 +0000

    expression closely skill persuade she

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
