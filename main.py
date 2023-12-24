f97e458b068faf4400b6ac2b11d6a1eb470e2b14
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:04:31 +0000

    convinced area copy valuable best

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
