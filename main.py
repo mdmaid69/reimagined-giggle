11e7cc048e89feb6a83588e4c932597a44262e61
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:10:08 +0000

    hit firm use check perhaps

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
