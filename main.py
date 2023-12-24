1656f4ded3dff5d0e4569ab9f0ac6e2a37315151
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:21:11 +0000

    confidence patience keep report however

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
