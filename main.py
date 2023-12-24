3cb31794320fc920af6234f7a0bd96906a6a95a2
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:16:34 +0000

    worry relationship scream under quickly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
