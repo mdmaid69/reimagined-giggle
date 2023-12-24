380d33ca63734f2fc9bd5444799163e81178fcc5
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:50:01 +0000

    generous remain precise though head

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def cube_number(x):
+        return x**3
