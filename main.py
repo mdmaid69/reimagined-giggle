e7311e44436810c97a6adefcb295c7d9307d08f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:59:43 +0000

    plan feeling difficulty century once

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
