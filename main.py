e5ba5979e608664de036979295a6627e4ef3ae1c
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:53:05 +0000

    turnout breakout plastic Vv jump

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def is_odd(n):
+        return n % 2 != 0
