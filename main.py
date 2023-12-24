c01a82aaa254437ae3330c02296d3bb81ff7f53b
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:43:11 +0000

    borrow goodbye our again method

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
