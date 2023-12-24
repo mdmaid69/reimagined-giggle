b47933f36824c9e6948df662026e72741cded3f2
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:27:47 +0000

    buy pipe range shop cold

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
