eae13dcb8767057e15ff4827d6432887c1cc2fcf
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:43:21 +0000

    treat lift word undo exhibition

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
