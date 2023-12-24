d67e29e809cde8bc6a3ccc7af7d8013a38328565
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:28:20 +0000

    excellent wing takeaway paper bringup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
