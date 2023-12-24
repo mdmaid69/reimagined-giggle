c6472ea140c09b65dfe141ce72f4f6b9cedd7761
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:35:37 +0000

    spring voice search past reserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
