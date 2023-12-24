b3e49b82ea21e6f9fa5e9fa8f88c9baba0ab2f75
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:23:40 +0000

    part mix land comeout flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
