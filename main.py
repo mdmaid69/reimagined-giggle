fc4914e51f207301374642e3cb9324fac37c07e8
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:43:06 +0000

    impression splash course professional anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
