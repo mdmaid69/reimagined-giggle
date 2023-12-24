25c01930f0e8f9f5ed1265ba148501d11918609c
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:50:56 +0000

    cut now actually material negative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import itertools
 def get_permutations(iterable):
         return list(itertools.permutations(iterable))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
