81b8a7dd63ad33ba7d95dc7ea50f71eeeefed009
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:00:37 +0000

    tape trick top government naturally

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-print([x**2 for x in range(10)])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
