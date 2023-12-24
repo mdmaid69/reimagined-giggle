c5e17221e3238c0f4d109dcef578c04c08121c0c
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:33:57 +0000

    operate soak where job shut

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
