03eb9779e8d901464150e59d4ce806e2f6438a76
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:25:23 +0000

    neck anybody pretty compete niggaacademy

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
