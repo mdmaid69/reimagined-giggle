3a892bfa741a3ae8a5c0592f41667f0cf28cb448
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:55:39 +0000

    mistake test custom expensive board

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
