be86ed2f605f645e657e5888c46d8434b4cf1e19
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:54:13 +0000

    criticism murder usually natural scream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
