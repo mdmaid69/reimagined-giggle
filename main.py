532916c61292b97e6fae6f1bef89d1bc95e7221d
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:49:22 +0000

    wash Tt half also depend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
