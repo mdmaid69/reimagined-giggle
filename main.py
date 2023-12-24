bf2e8585e64acb70ae809304978216fdd5446af0
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:05:25 +0000

    estimate consider job behaviour hold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
