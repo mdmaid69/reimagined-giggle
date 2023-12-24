d1fc170095d2d1f8d77aedbd09b83e359e0565e0
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:35:15 +0000

    detail dead only turn Ww

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_acceleration(speed, time):
+        return speed / time
