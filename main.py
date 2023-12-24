314abe6c90ffe945b03c7e45b632a1254a9174f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:56:11 +0000

    avoid still light blame lock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def is_odd(n):
+        return n % 2 != 0
