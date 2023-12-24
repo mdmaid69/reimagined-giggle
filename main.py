e6a35630839f2a45fe976c9bf5a73a5f357cda59
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:49:25 +0000

    soak add reduce copy defeat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def count_elements(lst):
-        return len(lst)
+def find_max(numbers):
+        return max(numbers)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
