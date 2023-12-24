9fdfd4cc960fbbf34a9c53da011857a13c755cf8
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:42:14 +0000

    guide area door reduction little

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
