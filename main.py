0f143dc66e782e80ac2b7334a79eb6df4a7c67db
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:02:10 +0000

    change experience race be family

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def append_to_array(array, item):
+        array.append(item)
