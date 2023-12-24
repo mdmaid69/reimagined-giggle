42878b5a61de3aae428c3e39cf190c552fd238be
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:19:18 +0000

    true talk after principle team

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
