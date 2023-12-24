1f26513d10599db8553fea6054b3346f7b28ed90
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:51:42 +0000

    mistake nervous school calloff glass

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_max(lst):
+        return max(lst)
+def is_odd(n):
+        return n % 2 != 0
