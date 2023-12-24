0e7fae056acb87e033c1300f5af7b240bf605b3e
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:34:22 +0000

    this cold view pride floor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
