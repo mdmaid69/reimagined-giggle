73c184494b8d66cffb55c5d17bed9978e55956bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:27:53 +0000

    not occasional unusual stamp common

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
