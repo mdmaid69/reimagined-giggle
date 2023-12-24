ed96f2faba8294348422f76c270b587e18f1b594
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:21:44 +0000

    immediately burn feel cope bury

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
