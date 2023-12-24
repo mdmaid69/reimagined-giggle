d558794815c54679b9df079fe899cbcfabcf73e7
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:59:16 +0000

    year be appeal extra please

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def reverse_string(s):
-        return s[::-1]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
