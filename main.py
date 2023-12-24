22ed7bcb7c41572246e911cfbca32c65f46ac2ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:04:11 +0000

    nerve type change 573entertainment wake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
