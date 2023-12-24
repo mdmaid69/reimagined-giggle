7001c897cf1e628f2d8eae4f19849089d673dd6d
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:43:56 +0000

    tradition back accept bunch share

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
