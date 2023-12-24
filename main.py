95a12dcd2752f0f3e214aed27bb1852c8df56508
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:01:16 +0000

    quantity tense second suppose language

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def split_path(path):
+        return os.path.split(path)
+def find_max(lst):
+        return max(lst)
