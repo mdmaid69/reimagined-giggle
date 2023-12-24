493cd3f2d36c83ed64a09243abbebb19d96143c1
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:03:09 +0000

    light university order lucky cap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
