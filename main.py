9d361671dcd51c366e8c863f636c598da3efd84b
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:43:08 +0000

    wipe deep ear criticize rob

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def get_array_length(array):
+        return len(array)
