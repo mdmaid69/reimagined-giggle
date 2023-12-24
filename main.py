e9bb312990f77295151fbe95e6ccf84278128d45
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:05:52 +0000

    shoulder stone clear nothing off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
