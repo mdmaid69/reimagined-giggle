819e2f0e66132f91b3678f25ae5bd7fee525a7f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:05:16 +0000

    rub genuine miss pronunciation get

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_acceleration(speed, time):
-        return speed / time
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import sys
+def exit_program():
+        sys.exit()
