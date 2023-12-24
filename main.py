af0a74e734f9a18d7232398fbca16989248eeac6
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:34:48 +0000

    building nose choose patient detail

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def is_odd(n):
-        return n % 2 != 0
-def calculate_work(force, distance):
-        return force * distance
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def count_characters(sentence):
+        return len(sentence)
