08c6990f858e23f99c74c75469423353777d1b60
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:53:17 +0000

    by begin goup mine key

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_acceleration(speed, time):
+        return speed / time
