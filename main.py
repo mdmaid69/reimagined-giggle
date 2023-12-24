b012ce8585393eb2a417356fe9d71ba5b352cc18
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:26:46 +0000

    repair leg himself use more

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
