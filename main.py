1e0fba55b8b559e1d8b36762008a76dbc26f9f15
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:19:16 +0000

    no later smile only freeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
