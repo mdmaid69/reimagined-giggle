f79ff84bf8927426d4618058bd18d8617f8c2a21
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:59:36 +0000

    healthy relative allow hide neither

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+print(math.pi)
