3ea9bfb32e47cb937dc71a93a05e4aa6cbcf1f9f
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:27:35 +0000

    limit result crash own leave

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
