59bd8177dfb3bc7cb9f74588d6a6ddc6cc76bb62
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:50:13 +0000

    vote road morning serious stay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
