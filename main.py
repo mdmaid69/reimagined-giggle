2faa8ea28dd4228663f5d69852442726b53e9b55
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:10:31 +0000

    boring tone nothing television exit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
