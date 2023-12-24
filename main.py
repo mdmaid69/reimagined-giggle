5b78f0d70693b20a361a4ec9ac6d014b0ba40081
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:46:37 +0000

    programme laugh independent shoot turnaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def is_even(n):
+        return n % 2 == 0
