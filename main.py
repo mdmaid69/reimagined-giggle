97ecb8cc83228c355ce8d9a8cd88b04f0832029d
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:27:27 +0000

    frighten low lend performance become

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+for i in range(10): print(i)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
