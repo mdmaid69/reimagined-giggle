18d5275ac25c806fb06fb6a826ca7bb5d5c606b4
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:01:49 +0000

    uncomfortable either forever avoid border

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
