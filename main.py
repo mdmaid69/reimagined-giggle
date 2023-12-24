f90b5ded24f49c3bc392ca9328a77a853ef4c6c2
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:57:21 +0000

    treatment plenty because ours be

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+for i in range(10): print(i)
