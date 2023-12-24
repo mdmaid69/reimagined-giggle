88ae9678cc30e4606c1adbb442c470d473e976f3
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:16:29 +0000

    slowly and holdout bang repeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
