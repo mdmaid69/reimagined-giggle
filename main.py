a05184a8688073a4f5a1cf3a599614ce0cd44dc4
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:44:13 +0000

    dress find concerning community communicate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
