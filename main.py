0395e157e7a8fb435904d90e54b9df7a7805d1ec
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:00:36 +0000

    operate probably lonely unite nonsense

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
