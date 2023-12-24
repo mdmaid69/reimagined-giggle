57992e378dff771e1d6786b87962c5beb1252129
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:45:37 +0000

    anywayanyhow Uu light miss exciting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
