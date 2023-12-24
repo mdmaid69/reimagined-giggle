00161d549e9a296922b0edea33d1a9b54a415f29
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:29:42 +0000

    during come right afraid colour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import os
+print(os.getcwd())
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
