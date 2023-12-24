560dfead07d2b6fe790e22e0b3b47f82613778f3
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:54:46 +0000

    as base butter able live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
