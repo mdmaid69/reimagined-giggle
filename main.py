44c1a848b07b69968b9363f338beb5e04cc309f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:21:30 +0000

    whistle noise cruel exit taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
