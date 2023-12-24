3532e5dd8b85a515eef7caee4e118027cc030c98
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:43:36 +0000

    interrupt have boring rush trap

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
