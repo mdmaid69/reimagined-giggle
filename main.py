c3f575fba07ad99706abf3d526fe304501f62a66
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:59:16 +0000

    minor understand confident mix enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
