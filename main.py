d7c8fd81d4f41dc4c0f208aea3072a28a1c645e2
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:38:39 +0000

    estimate stupid fork club violence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
