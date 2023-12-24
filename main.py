71c1acb2c528625c5aa264719254868cedc29b96
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:17:34 +0000

    true ilovedicks at spot deaf

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_power(work, time):
+        return work / time
+def find_max(lst):
+        return max(lst)
