c37cc102c1eedabfc76e9c27cf2c63e4dec8d5e9
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:43:33 +0000

    sudden behaviour family separate rub

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
