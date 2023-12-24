7186e19829daa0ca89cd4d94550e10e79d88f80b
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:01:52 +0000

    about behind our unable suck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def is_odd(n):
+        return n % 2 != 0
