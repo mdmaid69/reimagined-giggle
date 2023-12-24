ac5d24213dd3042eeac07cd7e580809e309be03e
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:27:39 +0000

    and Oo address stranger one

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
