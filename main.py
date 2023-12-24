13fe4cb3599b0c7ca4e37205228affa6c02d8913
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:38:37 +0000

    fond nearly fair scrape speech

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def get_array_length(array):
+        return len(array)
