ace8e936b4ad326a8bc88aacb2841e0404a0f25e
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:21:21 +0000

    anywayanyhow stir left perfect either

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
