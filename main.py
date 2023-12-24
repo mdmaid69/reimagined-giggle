4f257c01a29e45eed7699927af4d18b93a6e1668
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:20:57 +0000

    object sit spray mysterious practical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def add_numbers(x, y):
+        return x + y
