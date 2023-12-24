c37d5c83ac63843678d59d0462d8a658934d6372
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:54:09 +0000

    rush effect room some failure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
