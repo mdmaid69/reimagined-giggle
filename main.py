64af44c7a9f3fe3987050c20769f8065e95b9150
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:42:21 +0000

    lucky free instant extra advanced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
