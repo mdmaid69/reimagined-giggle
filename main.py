44af76512260586e41ac21bac4fc0c1bd3d60037
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:04:43 +0000

    voice bound nose friend yours

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+for i in range(10): print(i)
