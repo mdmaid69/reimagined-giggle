78f36fe43f7fb21357659f4b5f56a720efdb0694
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:31:25 +0000

    reserve lose whom recognize none

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_max(lst):
-        return max(lst)
+def calculate_area_rectangle(l, w):
+        return l * w
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
