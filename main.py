a6f06a63afd7370ae49456fbc73a4f399ced2c1f
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:20:04 +0000

    meet part custom trick impossible

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
