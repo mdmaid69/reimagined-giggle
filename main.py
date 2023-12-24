d7bdfed2109d936d39fa7431d93e369d6407b058
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:29:44 +0000

    exchange there snow ocean law

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
