aa216c1fd5eff73e045875acafd439baf31e5b19
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:53:28 +0000

    recently worth creature thing man

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def convert_array_to_list(array):
+        return array.tolist()
+  def sort_list(lst):
+        return sorted(lst)
