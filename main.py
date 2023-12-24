8d36304400810a0d12e261c05540c26b83e30846
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:14:45 +0000

    offer flat off hungry personal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
