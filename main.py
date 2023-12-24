b2de307b3ec0b09942975c7a4964438635495366
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:49:36 +0000

    run mad purpose contents left

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def remove_from_array(array, item):
+        array.remove(item)
