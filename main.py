82f3ab2edaf19473c07b21321c3ebfdeaf8907e2
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:57:50 +0000

    nearly wonder stiff sudden telephone

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
