3d0e879d51e28162f992fb372de917b5e5aecab6
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:12:23 +0000

    part set handle room formal

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
