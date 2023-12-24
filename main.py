4421b87c21a877bbe571dc7d744fe2b06e146b0e
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:18:33 +0000

    so fortunately duty thought water

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_exponential(x):
+        return math.exp(x)
