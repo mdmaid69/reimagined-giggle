bbae729395c9f3a26e17c5ca0c671a9b35b71d64
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:24:58 +0000

    heart every record meet first

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
