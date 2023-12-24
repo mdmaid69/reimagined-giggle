6f6c3c5e39c4b4bcb918954ebe5c73c1f7446bef
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:10:25 +0000

    coffee help march safety recent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
