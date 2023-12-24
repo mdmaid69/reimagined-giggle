d1389fff52de3d6a903dc6bb9f56e070f84c245e
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:28:48 +0000

    effect dear discover interrupt avoid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_as_float(array):
+        return float(array[0])
