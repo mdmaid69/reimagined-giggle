3253675efbff55ad79989306e19fa2d58d9015d7
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:25:07 +0000

    discuss machine complaint justice transparent

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import random
+def roll_die():
+        return random.randint(1, 6)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
