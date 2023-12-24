0362b7bd60c2fcc538148775cc4f5aee0f4f1023
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:02:54 +0000

    announce performance drop side lesson

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
