5bad1203a6605c83104c37d284b2d9c17d48a7f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:55:09 +0000

    prepare region arrive loan battery

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
