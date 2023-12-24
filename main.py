f8e878f9e65488a641453046a4bde520130713ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:15:39 +0000

    heart national talk wash pretty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
