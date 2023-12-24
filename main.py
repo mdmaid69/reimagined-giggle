27766e005f5a217a7328aa3f142f0d38cc7c4c41
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:01:25 +0000

    hope television round disapprove light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def get_array_as_list(array):
-        return list(array)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
