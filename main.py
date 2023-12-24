6dced6929e62677012b5bb225f6d783ed4d80b5e
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:47:03 +0000

    dry winter spite lean cold

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_list_from_array(array):
+        return array.tolist()
