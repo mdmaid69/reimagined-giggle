0f1f29926034556d752017e32ef3234c9be90c40
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:42:01 +0000

    cut never proof amount check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
