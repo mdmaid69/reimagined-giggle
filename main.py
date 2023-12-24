c8bb786bf1aab55cde5a7d932a8198f95430e397
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:39:35 +0000

    Jj worried check control everybody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
