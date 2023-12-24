06e4bce4911005b12352d7ba3a73c6cfda923fdc
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:55:01 +0000

    different guide goodbye upset fear

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
