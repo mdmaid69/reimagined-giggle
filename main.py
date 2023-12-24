29962e5a37d85e6c81445a696fc32447500584e4
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:08:12 +0000

    tap bear wrong broken under

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
