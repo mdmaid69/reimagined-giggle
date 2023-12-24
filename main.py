ce7ebb8d3e9da9d492c468254f069547b5be25a9
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:21:36 +0000

    strike beyond black desperate imagine

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def is_palindrome(s):
+        return s == s[::-1]
