ac6d34d334a9c8b9cf3ad5bfd8702686749ba94a
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:22:07 +0000

    before already wrap birthday means

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
