8574761a270caf09b68707441d6956800eb59303
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:00:05 +0000

    disappear relationship art flat record

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def square_number(x):
+        return x**2
+import math
+def calculate_exponential(x):
+        return math.exp(x)
