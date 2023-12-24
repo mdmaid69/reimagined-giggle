c2a5f14d354a488e0049466bdefb245bc8149e26
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:55:22 +0000

    juice shelter remark unusual Gg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_union(list1, list2):
+        return set(list1) | set(list2)
