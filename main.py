9b5c03c5912969d6623ce8db704b2d61936f7adf
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:53:21 +0000

    glad thankyou reason gather excellent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_volume(length, width, height):
+        return length * width * height
