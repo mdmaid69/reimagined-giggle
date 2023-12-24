8d1cffcd6c1cee361474ad48763429289647cbc1
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:20:06 +0000

    allow rest every connect last

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def calculate_power(work, time):
+        return work / time
