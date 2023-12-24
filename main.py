0e3f1ed4f3436b1a5a561fbbd4000fa853249636
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:14:03 +0000

    today hole nurse gate beside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
