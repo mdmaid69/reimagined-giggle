c9d142957d9af3576a94c094bb3745aeba1794d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:24:13 +0000

    urgent above reject story thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
