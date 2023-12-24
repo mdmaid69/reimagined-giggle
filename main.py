5ad5674474d459fc0e24e61d4d8fdd9890367168
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:58:28 +0000

    own comeover summer type tired

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def reverse_list(lst):
+        return lst[::-1]
