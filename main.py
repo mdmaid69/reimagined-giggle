9b5d08e9275e353d8a6b42eb9026b53052226eb9
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:05:23 +0000

    display through top light lean

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import random
+print(random.randint(0, 100))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
