744e4f6911d2d116896bc8b4c4e9b272ed5f8972
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:22:39 +0000

    stupid close absolutely national trip

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
