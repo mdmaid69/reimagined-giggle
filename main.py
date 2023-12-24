a945deb4b2341dafbf5ad9f954c5fde9df0e5a54
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:34:49 +0000

    backwards glad breakup part sort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  def sort_list(lst):
-        return sorted(lst)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
