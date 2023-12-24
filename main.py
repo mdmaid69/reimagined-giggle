7042b93e681d9365a7c6bfefaf00a7603cd7562b
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:28:35 +0000

    use wheel body even receive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def exit_program():
+        sys.exit()
