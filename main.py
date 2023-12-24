3e4c115eeea5d9f7678bb846eebdb806b098246c
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:50:57 +0000

    free address fire original would

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_max(numbers):
-        return max(numbers)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
