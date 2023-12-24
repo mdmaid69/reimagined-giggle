1843d4d513b7157cea2acc4c0f7e33135e8e38b2
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:25:22 +0000

    along introduce probably their steer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
