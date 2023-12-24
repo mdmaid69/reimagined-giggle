11a8ae7b4d153bfce20bdf85a0ee7e960a9f2deb
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:58:13 +0000

    eat doubt break union still

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
