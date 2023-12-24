6c57f8cba831840f2a23ef335df11085935ca4e8
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:19:18 +0000

    hard side shoulder happy appeal

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
