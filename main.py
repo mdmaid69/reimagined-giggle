9f415bc88ec12a3e407641c641569d6b61b57d5e
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:23:46 +0000

    store basically workupto experience mass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
