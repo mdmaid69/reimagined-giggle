6ce64d3765ea6e18c56318dd1fd5a6ee253cf8e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:05:02 +0000

    win cost kind everybody top

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_as_str(array):
+        return str(array)
