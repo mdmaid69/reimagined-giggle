570fcfd79acba01c8209dd2f12f662350b29878b
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:21:05 +0000

    worst emphasis pencil run sometimes

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
