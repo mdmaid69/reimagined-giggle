d5780b6fd2bf7a7a8ee10bba7c0506c97ae75955
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:11:57 +0000

    pride stretch open false quality

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
