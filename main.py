4c742b2309beb040f0bf3390d95932f5c673e965
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:28:22 +0000

    trade drive turndown upstairs about

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_str(array):
+        return str(array)
