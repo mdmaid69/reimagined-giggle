88dddfe552a2a189be4dfdabf87de7089dfc3f18
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:57:40 +0000

    account here regular sight cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
