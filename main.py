3d3921ae4e6782a75170a300b15069ff655e0cab
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:09:23 +0000

    chin adjective claim near punishment

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
