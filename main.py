43b408bce839cc091df89ca6ed2748b0093e0650
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:01:12 +0000

    contents crisis favour tonight holdup

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
