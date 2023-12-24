6b679bcaa4fa5ef27e583cad2a82a3fd03b2c3c5
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:16:10 +0000

    past future warning on slight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
