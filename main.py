657469150ecfff07a6de5d587ffbdd6d0c2c7d57
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:23:55 +0000

    luck loud approval draw flight

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def is_odd(n):
+        return n % 2 != 0
