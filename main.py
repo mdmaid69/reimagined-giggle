8831d2c62fad5ded3bbdbc4fcf6d83bab5bd18c8
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:02:38 +0000

    as crowded conduct class disappear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
