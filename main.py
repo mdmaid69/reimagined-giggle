3b6d464e8ac67e24563645094ceb2f55550931fa
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:48:34 +0000

    all present expression little owe

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import time
+def get_time_since_epoch():
+        return time.time()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
