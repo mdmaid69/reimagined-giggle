8f140f64205ae5a6f39d82e759273da07ad45d0e
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:04:25 +0000

    equal admit trade cross under

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  def sort_list(lst):
+        return sorted(lst)
