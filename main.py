3d7e3cea5928011d896f146a88527ca55bb4c1e7
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:30:05 +0000

    until answer fight continue their

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
