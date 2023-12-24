6b1d3f6007f5be7bf8e1569bf443c0b2881e4010
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:29:12 +0000

    stand move figure solve loss

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import math
+print(math.pi)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
