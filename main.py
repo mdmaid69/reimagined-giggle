4bc030e540299648f3485a453d396df35cb412e6
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:37:30 +0000

    noise refuse lookup reduction empty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
