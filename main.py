73fd714f5e031bfae5e69c53e0a26b502c5f25a3
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:42:19 +0000

    empty sort accidental serious write

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import sys
-print(sys.version)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
