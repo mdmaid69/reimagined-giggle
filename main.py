b29e0310e96dca18110117a2c40bfe780ebab3ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:33:30 +0000

    anybody common dry piece soak

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
