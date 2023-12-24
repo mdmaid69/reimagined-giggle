2169c8e998736f5a143ff8938585b16cd8ea2c79
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:07:25 +0000

    but turnon mean tense getoff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
