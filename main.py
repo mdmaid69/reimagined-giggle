ec09ef0c213a31a7f62e0a250523abb0ce5cf527
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:32:59 +0000

    frame travel rank limit behind

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
