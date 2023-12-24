2a4a61046e48894a91dc8fa8bb99b156ef57a3b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:41:09 +0000

    treat waste intelligent even sense

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+from collections import Counter
+print(Counter("hello world"))
