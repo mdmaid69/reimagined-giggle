581929b667a5edf4dd11284d81dd8ba86eedad5c
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:37:01 +0000

    obvious recently television love in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
