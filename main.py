0671998509fd8f50bed5e05ccbd531a2493736cf
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:08:41 +0000

    hold Ll clothes part want

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_density(mass, volume):
+        return mass / volume
