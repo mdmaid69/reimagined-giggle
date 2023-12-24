7c8a6b6cb8937a7d8856af5338ff416ce478cc9a
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:55:40 +0000

    there place trip regard spite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def reverse_array(array):
+        array.reverse()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
