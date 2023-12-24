a8cb00566544880a845b4b76c941859b7979262f
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:07:20 +0000

    quiet benefit ability available instant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
