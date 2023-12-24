760598ceaed41c79cb2119254e29f92ab18811ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:25:07 +0000

    shout poor change keepaway mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+name = "Python"
+print("Hello,", name)
