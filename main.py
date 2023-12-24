59ac72cebfbbb2a544755ac2b877dcc79e0ce7b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:38:56 +0000

    point glad legal crowded wide

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import re
+print(re.match("h.*o", "hello world"))
