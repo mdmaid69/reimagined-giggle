28795b8d46c108db240997fb5abf72da983909e6
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:32:23 +0000

    callup education serve suckme exhibition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
