a4ede0abc4dbaf35040710cdee074af7abb10646
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:29:14 +0000

    another marry we basic boring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
