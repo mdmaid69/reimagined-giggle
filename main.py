b8f3933c3369a6c0195fcbbf9665e7c7aa7e43d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:16:01 +0000

    scream best part children fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
