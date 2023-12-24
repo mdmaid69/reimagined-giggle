324bac8f9189113f4f060a0b86fd3d03d066c28e
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:34:23 +0000

    abroad rush wide food know

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
