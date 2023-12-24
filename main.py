6dbac12d3beb9a232b2b55d8cd066a40a0ab7b2c
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:36:21 +0000

    actually will deserve clothes cough

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def remove_duplicates(lst):
-        return list(set(lst))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
