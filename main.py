15499438f74db5009071dabc01fc851aaec1e45c
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:49:00 +0000

    concerned sharp convince sing later

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
