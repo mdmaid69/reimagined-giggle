7d915ea8a4da06d1e07c5cd4335598abd7346799
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:59:14 +0000

    finish stiff turnback minimum after

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+print(json.dumps({"name": "John", "age": 30}))
