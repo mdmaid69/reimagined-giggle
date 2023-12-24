c953fdfafd4ce14b5ce7ea8f074bc664d2ae39b3
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:09:35 +0000

    display minimum want smoke wait

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
