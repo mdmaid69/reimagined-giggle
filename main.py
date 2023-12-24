12bb9c7c4fb4bee23143b1d75f417fe2f90ae65d
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:52:45 +0000

    fold lie guilty shop drive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
