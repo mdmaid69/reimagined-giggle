b555ca4e9fa7a5bf9df2319eca9bff8b8bfe3a21
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:03:12 +0000

    bunch face remain press usual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_list(array):
+        return list(array)
+  def count_elements(lst):
+        return len(lst)
