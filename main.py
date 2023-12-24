335de8446302c565874ff301e446f4e3b131375d
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:13:24 +0000

    unusual slow care dangerous cheque

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
