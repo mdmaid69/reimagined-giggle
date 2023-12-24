a612135dceec18294f1fca06ef4f56715957a544
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:55:40 +0000

    tax meeting occasion suddenly stand

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def split_path(path):
+        return os.path.split(path)
