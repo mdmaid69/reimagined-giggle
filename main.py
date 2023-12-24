c787ae591099828176d82fa51f75bdc958bdcbdd
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:54:03 +0000

    last test flavour separate rough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def is_palindrome(s):
-        return s == s[::-1]
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
