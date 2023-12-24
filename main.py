c5ebb493cb02b8c312c582f5510ffe2420277273
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:26:42 +0000

    stiff say where length carry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
