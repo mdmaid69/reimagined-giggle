ede5bb5ec8d327ffb79c76de815d4b62ce293c55
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:45:30 +0000

    turnover thick pour passage concentrate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
