46b8964ab18ac2c228d916bcb78d62166b3e41f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:57:30 +0000

    advantage pronounce lookup guide die

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_user_dict():
+        return collections.UserDict()
