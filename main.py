5012c779d53ab7d393f552406b94fc281960b3cf
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:14:49 +0000

    Ss could bear pupil takeaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
