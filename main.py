ef45bd1ebf6f82e0fe25bda5eb64ad8d757cd50a
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:32:45 +0000

    OKorokay bye responsible detail party

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_typecode(array):
+        return array.typecode
