bd75dade211d5fa73fe7d23ed1a6336324b3373b
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:08:51 +0000

    free lower giveback long comment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
