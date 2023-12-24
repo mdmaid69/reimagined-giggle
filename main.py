3fb4d58a949f3d00e309bb06153ac7923c64afdc
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:15:28 +0000

    area hair spill opposite also

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
