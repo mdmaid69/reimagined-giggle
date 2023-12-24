3a54f362a9db9458b21ab976f1e36681196ebfe1
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:55:21 +0000

    tough will favourite needle inquire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
