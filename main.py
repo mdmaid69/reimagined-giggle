3cd495496b09ae59a489ca04e0296e020b769f8f
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:41:15 +0000

    noone store shall port urgent

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
