fdb733f3a423b9359ed33935c206f49c223b0d76
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:37:01 +0000

    urgent bin rather flow slide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
