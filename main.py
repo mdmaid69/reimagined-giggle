12376f08cbd48462119f09b1664c4b3b1ef287a6
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:11:51 +0000

    size Yy tidy scale lesson

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  def convert_to_binary(n):
+        return bin(n)
