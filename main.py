6834f0b44f818b39ba7c364e476ebfe0c6316c1c
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:11:46 +0000

    embarrassed reject rather recently prefer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
