02687b0d382c572ce10702066e854e92948ceed8
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:12:16 +0000

    correct satisfactory block for everything

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import time
-print(time.time())
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
