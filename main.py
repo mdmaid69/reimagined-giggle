0d0f3b18e9f1a3e84417b0fa56b969977b97ec1c
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:31:41 +0000

    centre flood 564encourage bringdown target

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def find_max(lst):
-        return max(lst)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
