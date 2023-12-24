b89cc6cef11a43b2a502c853505805027e799433
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:34:08 +0000

    check inquire squeeze communicate pretty

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
