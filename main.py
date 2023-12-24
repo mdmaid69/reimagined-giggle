1373fc41ea468553b614a617700fd2a6d284ceca
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:04:34 +0000

    reply system tell forward poor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_floor(x):
+        return math.floor(x)
