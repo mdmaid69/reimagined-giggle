43d920acf1187639456c7bdc7917fced48e51288
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:42:10 +0000

    pen touch show takeover tone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-print(sum(range(10)))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
