17340aaa21efc3f65a1dabbe56a6613ed79305b7
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:43:44 +0000

    bang help burn authority holiday

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
