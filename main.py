cc1203fb2c267440b67d2c6252f3461511d52dbc
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:57:12 +0000

    thank band into pain consider

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
