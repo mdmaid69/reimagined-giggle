09bb0a440e6e052f7fbcf4b3f4d460b9fcb62c37
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:52:35 +0000

    profit high hurry ignore score

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-for i in range(10): print(i)
+def remove_from_array(array, item):
+        array.remove(item)
