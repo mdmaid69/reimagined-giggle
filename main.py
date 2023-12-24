68379bb8e0a033b29d57b207848a5b05aa81fc6b
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:56:55 +0000

    circle up exhibition road switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def reverse_list(lst):
+        return lst[::-1]
