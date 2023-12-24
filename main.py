f31b91110d0b539e922e2b4ee0f88182323906cc
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:24:07 +0000

    ground worry unlikely influence yes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
