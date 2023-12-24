a9e0d6b2965ef847b79369c10cd48ebf4639e733
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:58:15 +0000

    Ff jump where only meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
