20dfe098b3a99cc7ea371c417a2e550631cb8f87
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:31:39 +0000

    comein afternoon study goodbye maximum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
