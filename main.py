b50911b6b6db5cd4d0b2ff5c7eace976e7961360
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:12:32 +0000

    tradition goback(to) education apparently lie

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import time
+print(time.time())
+def sort_list(lst):
+        return sorted(lst)
