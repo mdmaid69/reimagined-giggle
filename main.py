2d5dbdf4c4ca2f1534efbfa6a0a3e5212635d56c
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:39:11 +0000

    until similar accordingto meet sight

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
