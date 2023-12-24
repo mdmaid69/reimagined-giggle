f4519a6f1f295e319f1938d8ba5d9ff1d258fbc9
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:35:15 +0000

    excuse return entry giveback manner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_current_directory():
+        return os.getcwd()
