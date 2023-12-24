d02f51202fb1d6e6282581de21b942c8614db9e7
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:24:50 +0000

    noone deliberately importance able 571enough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
