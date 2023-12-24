23ad8b5c5291844fa08792412cf63e5373ca124d
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:10:55 +0000

    hang Ss reply afternoon art

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
