19c1e89e3692c1f4170b1f033888c9cca771ee76
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:16:32 +0000

    damage stamp comeoff phone yesterday

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_hex(n):
+        return hex(n)
