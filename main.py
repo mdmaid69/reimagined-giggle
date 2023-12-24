ad7f9e4d05352054d1e8ab240edcc426721f6491
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:26:04 +0000

    sing police violence part he

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  def square_number(x):
+        return x**2
