27ebe859ad419b8d332fa74f31ce4f37b233d9fc
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:58:45 +0000

    unusual responsibility please remove Oo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+from collections import Counter
+print(Counter("hello world"))
