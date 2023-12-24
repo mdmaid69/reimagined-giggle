0e2d4f9f86688ec14ac4409dd3a54260c6033b98
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:14:34 +0000

    Ee word makeinto steer breakdown

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
