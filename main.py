dfc1b053179adb2b9bd638ccbb4a7b279053ddd9
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:23:14 +0000

    apply attractive bunch look paper

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_work(force, distance):
+        return force * distance
+import numpy as np
+print(np.array([1, 2, 3]))
