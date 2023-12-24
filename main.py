ea65319916cce17848415a9c7144dffdf6339bb1
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:56:09 +0000

    natural content rich contain record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
