d2101855a44b50590ef1a3b7c3722a05953adff5
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:15:28 +0000

    combine term possibility shoulder dry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
