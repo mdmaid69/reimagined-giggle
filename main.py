47b3e1f1bef8876ce53004ec101b73b70e1e771f
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:39:01 +0000

    miss combination never black themselves

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_int(array):
+        return int(array[0])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
