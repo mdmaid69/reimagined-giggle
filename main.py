1136d36b9e911ebd7985b226c2485505b1a74aa0
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:19:32 +0000

    smile breakfast introduce after benefit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def reverse_list(lst):
+        return lst[::-1]
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
