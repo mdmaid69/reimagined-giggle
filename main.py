b484c73f10ec32658a3ec2bf1443f4b5c570dd3f
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:52:02 +0000

    tired again prize favourite ticket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def create_priority_queue():
+        return collections.deque()
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
