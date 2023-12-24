8f8096f112817c0b65b5347e2e8cb75410dccc4e
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:03:19 +0000

    show total year frightened calloff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+print(json.dumps({"name": "John", "age": 30}))
