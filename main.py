709b8793ab6efab400a25aea9e8f5fdc411d89d3
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:30:07 +0000

    check unlucky anxious train vision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
