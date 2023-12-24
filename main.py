922561b25cd55f1c6ae8dac679a4c2e8434b3d78
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:51:35 +0000

    autumn my doctor keepaway old

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-print(os.getcwd())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
