520e3051d672ac987de8a1effa20fbfb1d69c366
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:42:08 +0000

    record future dick improvement grow

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import getpass
+def get_username():
+        return getpass.getuser()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
