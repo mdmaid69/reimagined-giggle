2960dc5c4884977b06cac5c2248a9c4cc86d1ffd
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:11:35 +0000

    country start weekend brain know

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
