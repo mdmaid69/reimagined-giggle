772c3e9cfeb9df534e9b5128c22032c1d841a1cd
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:03:47 +0000

    light unable fashion steer opinion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
