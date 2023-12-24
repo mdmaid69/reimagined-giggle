770462092d8df32e83d04feefae2a736ed7956de
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:23:10 +0000

    worry town spring produce listenfor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  def convert_to_binary(n):
+        return bin(n)
