32c976bebabf7d64caea33643b5db945b05ace3a
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:42:39 +0000

    powerful turn repair practise aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
