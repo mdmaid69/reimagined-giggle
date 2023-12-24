2b0b6bfb1453efb0b5ada885fc1fbb589e0d94c3
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:14:36 +0000

    before urgent foot basically left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
