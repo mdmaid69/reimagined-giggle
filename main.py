da9024093ee5efbd056cd817b1e9ee264cb0ae89
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:49:07 +0000

    boring part average keen land

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
