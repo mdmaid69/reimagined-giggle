a0c20910b747de0e6bb3a9c46cfc61a274fb325c
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:32:31 +0000

    owe forget push over gate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
