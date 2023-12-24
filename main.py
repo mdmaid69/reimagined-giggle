81505699a6dd7c703a0e5f7415500a128ab35d30
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:52:12 +0000

    attract minimum on past write

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
