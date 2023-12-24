117c20b156880ecf696c3fed44200573291c92e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:27:39 +0000

    waste be offend breakup influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
