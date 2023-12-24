6e001c43090318a0e0096b972cd9ade11afbf7fc
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:11:50 +0000

    crazy simple human separate lose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_current_directory():
+        return os.getcwd()
