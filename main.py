f1bf81ebdd42548e31faed92b456c15594a8f547
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:07:58 +0000

    leg free disturb lookaround brief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
