b44f02715dab5e48f0631b06044a7d8876c3228d
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:25:48 +0000

    boil pile pause colour congratulations

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_area_circle(r):
+        return 3.14 * r**2
