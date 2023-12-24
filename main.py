a53b154d13e4b77afc46021034105ef447668052
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:24:30 +0000

    punish question sale empty make

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import random
-def roll_die():
-        return random.randint(1, 6)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import numpy as np
+print(np.array([1, 2, 3]))
