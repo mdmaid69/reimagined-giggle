6a384d26dc14034de4a4bb05387c1ca3a7be6bd4
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:34:15 +0000

    train forever getback comedown boil

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def find_max(numbers):
+        return max(numbers)
+import array
+def get_array_typecode(array):
+        return array.typecode
