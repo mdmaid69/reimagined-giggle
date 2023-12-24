dbdb9a6f64dfbaa9d50e6f710f687e24269a8c8a
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:14:59 +0000

    demonstrate defeat save fence thing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
