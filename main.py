3a24a9decd030ee5934ef719db3afc2792e7ce42
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:58:35 +0000

    cheque mixture need sail past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
