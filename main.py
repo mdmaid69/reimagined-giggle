918b8568ef337fd293744a5e28e67d2763706d30
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:17:07 +0000

    repeat trade sentence march your

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
