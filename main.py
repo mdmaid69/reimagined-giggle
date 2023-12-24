a3dc5cefbd69a89b87ed1ed95f07fc4737ba97e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:38:41 +0000

    mixture would least sound reduction

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
