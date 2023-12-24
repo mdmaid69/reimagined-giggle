0a9558374580279733081c5e025f78b68dbd8573
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:51:14 +0000

    noone turnout accordingto cut phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
