3d7cb47679c6ae4917dc0532b9a84459875ee739
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:15:34 +0000

    pleased contrast lead sorry sudden

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
