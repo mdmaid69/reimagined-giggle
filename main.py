f1df590729f517ecebf958d6a569e2e932745c86
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:36:36 +0000

    office 179bite petrol objection sit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import collections
-def create_queue():
-        return collections.deque()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
