efc0877852931797d72c1c320f82d58b798efc9d
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:24:07 +0000

    jump persuade deaf truth contact

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def convert_to_octal(n):
-        return oct(n)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
