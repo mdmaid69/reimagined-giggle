22dacc2e285e8c36ade4f61f97b1081d0d4b99c2
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:01:45 +0000

    black sight pause plenty pass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
