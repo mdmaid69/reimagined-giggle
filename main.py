3fbb9e81d9fa3d0ca7d921f1095fc44fe135b235
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:19:45 +0000

    Oo keepoff control middle ashamed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
