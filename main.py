e8fc6ffa410a7d579b070baae7c4717ff80a801c
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:37:05 +0000

    exchange quiet condition under disturbing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
