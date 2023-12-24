c15ea3cc3e601d469e0e95afa31172d9451c5cd3
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:47:48 +0000

    big greeting display table drive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import collections
-def create_counter():
-        return collections.Counter()
-def find_max(lst):
-        return max(lst)
+def create_user_list():
+        return collections.UserList()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
