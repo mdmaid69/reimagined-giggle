7f86832d95cf96c361429ab58f3f20258734fa51
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:26:18 +0000

    giveback improve take shoot forward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_error_function(x):
+        return math.erf(x)
