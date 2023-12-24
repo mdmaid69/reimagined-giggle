6e414b1be217be2e2be9e2622d31a86ebba01877
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:16:23 +0000

    trousers actual unfortunate mystery comeup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
