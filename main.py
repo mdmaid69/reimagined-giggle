1b81c7cce15ee30cde250ed6499e9db05cc216f2
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:10:00 +0000

    sleep workupto foot order left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
