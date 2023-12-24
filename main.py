6aa86b8246953c4b5e283e8e5bafe675e71432c4
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:57:14 +0000

    drawing language knowledge off plenty

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+print([x**2 for x in range(10)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
