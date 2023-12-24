79f6b06ea9ce5134f9d0fdd40bf7b5e5a031de98
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:57:17 +0000

    destroy show safety move us

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
