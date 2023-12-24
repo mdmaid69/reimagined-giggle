178f8cb667111934d4d703bdbf55575c8a57f2d1
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:02:14 +0000

    talk act pick sure Zz

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
