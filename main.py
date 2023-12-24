db662231770ba79c7fbc505623ae229a13aee2db
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:13:55 +0000

    drive flow callat fire free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
