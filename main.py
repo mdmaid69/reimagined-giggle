ea5d07ea1cc33ea79b1ed6e6b2619f3d6ffc3694
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:08:00 +0000

    play last steep memory round

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-print([x**2 for x in range(10)])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
