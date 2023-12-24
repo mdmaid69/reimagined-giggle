c0f97fc93a5562aeb4fc24c12870ea158a7c7a8a
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:13:32 +0000

    play long state give second

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def is_even(n):
+        return n % 2 == 0
