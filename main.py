15dca8f8d56709a2ccdcc8c9b83bbfb217976f37
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:13:03 +0000

    whistle hope birthday pen thinkthrough

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
