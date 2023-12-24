cde321bd2d3ef7a61dd4280f9be467642f144016
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:26:01 +0000

    if week gift rarely work

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def convert_to_binary(n):
+        return bin(n)
