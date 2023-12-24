074cb27670578dca2fdafba64ae5cdd54508af57
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:31:49 +0000

    start medium wonder copy load

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-print(sum(range(10)))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
