48e0ebc24f10549f6b0bd1f328ee97ada1ee78b7
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:50:32 +0000

    yours unable past slowly understand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_density(mass, volume):
+        return mass / volume
