1245de28f0559c260597d661c82a4544feeb6d73
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:54:11 +0000

    art match takeover chief prize

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def find_union(list1, list2):
+        return set(list1) | set(list2)
