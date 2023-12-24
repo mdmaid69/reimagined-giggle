344aac7219e2f6849f760f2f65a416cbaf2c0d20
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:00:56 +0000

    trade burn dry thank independent

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
