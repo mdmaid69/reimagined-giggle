1ae894909cbc94dee82d0faa5be12b54164eeb59
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:25:28 +0000

    class excuse whoever future fierce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
