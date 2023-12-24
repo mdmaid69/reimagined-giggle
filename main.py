770f2adc74680cb78b38255594d51f4bee2a5628
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:23:22 +0000

    war ahead combine defend low

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
