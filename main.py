aade94327b197b67e0488ffce3377b934722bdcb
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:37:42 +0000

    bath damage interview big tooth

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
