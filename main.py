6ce28534693acb6ffab7cf2f103182fc1be3c9c2
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:54:27 +0000

    succeed water fast tight part

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import os
+print(os.getcwd())
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
