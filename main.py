3f76a401050ce62e796500418b7c806cc2caf37f
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:49:10 +0000

    ticket back against welcome trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+print(os.getcwd())
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
