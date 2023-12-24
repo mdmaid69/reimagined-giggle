eb596ae5b3e9a04f8f646d8d0cfb8f8c6059d2f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:07:24 +0000

    Oo example check though university

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
