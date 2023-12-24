3e054a9010128a84d4d2ee64b9540332d101b1ae
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:00:42 +0000

    power loan terrible female godown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def cube_number(x):
-        return x**3
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
