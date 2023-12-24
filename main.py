b7a80ae125266ae9d9cd23317c5db6d28f1c1cb5
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:53:35 +0000

    explanation sing furniture all full

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def get_array_as_bool(array):
+        return bool(array)
