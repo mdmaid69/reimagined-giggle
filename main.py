8fd121cf3e25942985f9d04fc19e3a7a1d6dc2dd
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:11:29 +0000

    bringdown sensitive worry pump risk

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
