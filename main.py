ddcb9aecc1fb860900221b5fb21971d7aabf7584
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:16:47 +0000

    major spring meal close different

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+from collections import Counter
+print(Counter("hello world"))
