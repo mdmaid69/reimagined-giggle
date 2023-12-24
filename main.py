22059813200e7d69f56a8ec595b2ce51a49ea89c
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:46:10 +0000

    view excuse because by over

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def is_even(n):
+        return n % 2 == 0
