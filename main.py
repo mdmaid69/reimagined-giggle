977b0100cdd471067a3e0e5524428bbf7bf585c9
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:04:11 +0000

    each give giveout refuse telephone

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def find_min(numbers):
+        return min(numbers)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
