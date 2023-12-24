6bc897f17235ef2bf6b944e8c63754084d84c86e
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:43:14 +0000

    view hit mouth feeling meal

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
