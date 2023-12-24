532ff4dcce69db2a5f8698df4b70b8574d6afae3
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:48:45 +0000

    woman since ring book professional

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-  def subtract_numbers(x, y):
-        return x - y
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
