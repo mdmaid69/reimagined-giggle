8d8696fe6fa6801a8aed3349b539ffcdc96ed5eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:12:28 +0000

    all phone closely cross circumstance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
