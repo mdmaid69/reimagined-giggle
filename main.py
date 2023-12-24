bbaa35bcdeb79926ea1145b2275ac79c2ef65ffc
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:22:02 +0000

    hit shop perfect limit comeback

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
