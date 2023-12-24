4790ff2f259f8641c1571320d366f67c7100d1af
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:40:40 +0000

    front chief present lunch approval

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
