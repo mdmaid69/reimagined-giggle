5cb02c950420d90cf9ed6604b405ef6c7ee169a6
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:12:38 +0000

    deep drop foot put understand

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_error_function(x):
+        return math.erf(x)
