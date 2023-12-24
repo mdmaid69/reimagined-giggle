d6473342d8b1376e89d0415b3a570bdb6788516f
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:43:57 +0000

    569enjoy forgive trouble Mm lower

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def sort_list(lst):
-        return sorted(lst)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
