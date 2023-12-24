07193fc62fd3a2751421aceddb9131cc1ba5d591
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:54:40 +0000

    offer moon improvement birthday stand

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def sort_list(lst):
+        return sorted(lst)
+import getpass
+def get_username():
+        return getpass.getuser()
