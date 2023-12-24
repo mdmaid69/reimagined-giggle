51b77485156173a05f86e35fbbb7e1c0085a07f2
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:09:07 +0000

    Pp bend contact boring send

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
