647d1b15cf6bc45ae7dd181ec2becda26c88bb46
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:41:09 +0000

    narrow around object strange age

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
