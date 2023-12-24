34f9eb73b61a261d16fa5a9423a4c34fd16c370d
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:22:57 +0000

    basis but choose basically behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
