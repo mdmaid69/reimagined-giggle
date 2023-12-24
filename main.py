97d88593277ce66296abea0fdf3e4f1d12d58ce9
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:05:55 +0000

    switch emotion runinto rob bend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
