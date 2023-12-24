1ddfef029f8778d267b8c2de3f0e5b9d8cb569b0
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:07:45 +0000

    569enjoy touch contrast keepup swallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def reverse_list(lst):
+        return lst[::-1]
