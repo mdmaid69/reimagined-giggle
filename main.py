e1ad49ac06daaff917ca6179b2e69ae49d9145c4
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:20:12 +0000

    bottom book disapprove forgive left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
