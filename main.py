b07fa2f3c8682c23d68313d1ef3c7cfa842d56b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:18:36 +0000

    mine often ought ill difference

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def sort_list(lst):
+        return sorted(lst)
