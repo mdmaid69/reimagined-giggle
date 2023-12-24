c4b175afad1ae3dea51e2b3ee0d029fdc8f4158e
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:25:49 +0000

    hardly benefit sensible right comeoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
