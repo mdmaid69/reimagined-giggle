03501cd4126ff51b0f3d5d4e5f3a99b1de63fac5
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:39:42 +0000

    among concentrate stiff interested block

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import sys
+  def get_python_version():
+        return sys.version
+import array
+def get_array_as_float(array):
+        return float(array[0])
