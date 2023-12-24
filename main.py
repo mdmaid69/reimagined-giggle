39321e988b88df1427bce1e26e202ffc78fa6801
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:05:45 +0000

    right announce suckme right radio

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
