5225db999b7a7384cc6fcd48029b07fcc7066eb7
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:10:40 +0000

    angry tense principle his tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
