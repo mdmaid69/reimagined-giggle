bc0ca738f0eed5363ff2ad59e21bbb294fd79183
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:38:43 +0000

    familiar size style freedom everybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
