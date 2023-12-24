58bdfc01cc533c9c20ce21cbbae30947dc3fe763
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:21:56 +0000

    desperate especially event scheme right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def clear_array(array):
+        array *= 0
