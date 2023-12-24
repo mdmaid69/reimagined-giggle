74bdf37bd5da5122fd8fca0d71c627801cd837c9
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:34:58 +0000

    572enter shoot plain week prepare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
