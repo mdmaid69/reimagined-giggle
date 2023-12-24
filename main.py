da9391f8c5a28f97662dfd4ced2e9ac290fdb8a4
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:41:48 +0000

    above lead concentration accommodation block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
