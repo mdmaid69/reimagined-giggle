853808bd43892515b62c468b31cd0d2f63a2fe9c
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:38:02 +0000

    rub hair few shout familiar

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
