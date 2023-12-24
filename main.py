e9c8cc177e701ff2c73b73ed63e4783adbaa57a0
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:25:20 +0000

    match nothing Kk fun post

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
