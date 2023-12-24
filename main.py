e99939e2335ebb304427c30698b569b35f5d07de
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:11:57 +0000

    572enter length Ss gift risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
