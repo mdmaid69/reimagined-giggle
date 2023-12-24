d70dd39b6e21cf9429ced5e1d980c68813b45d14
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:43:19 +0000

    work announce stay arrangement consult

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
