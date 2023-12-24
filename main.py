c1c6fa2a80857aa07a0d702808e6330a64054991
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:52:25 +0000

    advanced uncertain turnover various anyone

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+print(sum(range(10)))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
