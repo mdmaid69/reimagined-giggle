fb17d858e5d82c167df5494d6e50ea473a6e7b0a
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:41:45 +0000

    getaway train fortunate repeat steady

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
