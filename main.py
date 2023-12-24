c60c4b8507208c4b6d3bc18d7a2e962b258a8d34
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:50:30 +0000

    breakout crash science fortunate this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
