9c99f6b1cc38043611afa7ad0022bcc357f9f3ae
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:24:20 +0000

    Cc explain estimate task crazy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
