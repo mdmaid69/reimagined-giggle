1101e85158ef0dd122f8ade4dc5754786571b1d5
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:23:52 +0000

    urgent somebody Rr comedown rid

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  def reverse_list(lst):
+        return lst[::-1]
