980f17bef55c19d49b026e0fba740247ff2f7bc2
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:21:05 +0000

    collect welcome impossible loose practise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def find_max(lst):
-        return max(lst)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
