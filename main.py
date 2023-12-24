6eaae65351724a0a3460667c5020b72428edb226
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:56:04 +0000

    related pollution comment lookaround up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_palindrome(s):
+        return s == s[::-1]
