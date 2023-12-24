6a55217f5f42b30e5f9ac779cdf09a669f562832
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:31:44 +0000

    turninto shock teach sky bank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
