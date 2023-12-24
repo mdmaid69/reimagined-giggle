54d4fb3e29006f421807cfdf6bfc375ccbd41f64
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:08:14 +0000

    boat turnup crowd no attempt

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
