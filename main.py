786ee0825a484ef2352cecf9b85020cd49a23a15
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:16:01 +0000

    better slide wet serve unless

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
