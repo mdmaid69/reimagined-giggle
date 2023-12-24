d83aa1adfc079762c95ea1a924da304cdb2e33b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:52:44 +0000

    release still bringforward feed spoil

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_density(mass, volume):
+        return mass / volume
+def square_number(x):
+        return x**2
