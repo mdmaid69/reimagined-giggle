5ef84f6cf8e73976e1a934ad872f10f4d11bdeec
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:48:50 +0000

    admire best invitation side damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
