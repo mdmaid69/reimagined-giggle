3b33a50c3a2819e88912c64046507581a0d9bb38
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:30:03 +0000

    soon term laugh level ring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def square_number(x):
-        return x**2
+def find_max(lst):
+        return max(lst)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
