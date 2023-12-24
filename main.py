b83ab47f6a95e5159d1a98d25851a41d1c3a12a8
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:31:09 +0000

    look wind even world steal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
