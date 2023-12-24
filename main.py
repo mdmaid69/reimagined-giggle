026df819fd788f9cec730cd633d4ab23e1358aca
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:50:03 +0000

    Gg night herself throw before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def reverse_list(lst):
+        return lst[::-1]
