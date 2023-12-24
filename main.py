b9a2189b094dc8196a4935b5caa4639a1f133b3e
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:59:46 +0000

    modern emphasize base choice least

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
