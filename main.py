4fdc78b9355fc0add2d6bb36c6523504f2da2b31
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:54:59 +0000

    part storm other level whole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
