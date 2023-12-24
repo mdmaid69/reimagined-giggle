632f181444c6d79a5571a3156bc75fc6d4301efb
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:25:01 +0000

    fierce watch authority demonstrate label

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def reverse_list(lst):
+        return lst[::-1]
