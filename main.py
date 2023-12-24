6c2244df32fe0003156f15e1ada434534763f4b5
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:57:55 +0000

    giveback welcome judge stupid shame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
