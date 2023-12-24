06174725c923a1f591e6b7757d302ccc27230347
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:22:14 +0000

    terrible injury open prepare everything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
