8969a81f6195dca34fe77a33bfd09cbcb0107ef2
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:17:51 +0000

    hate sorry promise secret normal

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
