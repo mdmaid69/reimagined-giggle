b3429bd89fb23f70aa721cd4d3021ded2bfcb285
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:35:26 +0000

    pure left wipe judge arrange

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_work(force, distance):
+        return force * distance
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
