0fd7a96f45e0eba83e9ffa3f858c7ac46b42c5c4
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:14:14 +0000

    watch ought none plastic machine

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
