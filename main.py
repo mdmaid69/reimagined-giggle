baff3e1d1fcd81810f3048bbf125a084a9e5d33a
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:58:56 +0000

    meanwhile travel quantity side front

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def clear_array(array):
+        array *= 0
