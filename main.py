20b72102d385511d0fc286631e0ff8e1d1eb5358
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:39:28 +0000

    clean wise brown comeoff tread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_pressure(force, area):
+        return force / area
