2276e391fdb23382b6a0eaece35e3b0ac85ed33d
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:24:12 +0000

    success slice direct another leg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def remove_duplicates(lst):
+        return list(set(lst))
