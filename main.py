9267883248b3309bc1a2a161da6f35b4d171d0a1
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:13:50 +0000

    work remark disagree ride study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import collections
+def create_user_list():
+        return collections.UserList()
