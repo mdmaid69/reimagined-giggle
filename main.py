cf6d918cd06b0e9d7e507bc4cdc156b6fed1bc12
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:45:04 +0000

    blame rid high tragedy behave

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
