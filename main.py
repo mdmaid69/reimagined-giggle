e6d6ceeb42c5ca5fa57e361648885b1d047bc337
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:41:54 +0000

    majority much you blame nose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def exit_program():
-        sys.exit()
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import random
+def roll_die():
+        return random.randint(1, 6)
