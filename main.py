e39735b0bf1b6274374263f739debe27b7a75b5a
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:19:06 +0000

    concerned approve impressive Jj should

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
