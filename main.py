c86c6e05398ae7efd21a50474ba4bb122278f9e4
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:59:13 +0000

    getoff arrangement sign Vv direction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_repr(array):
+        return repr(array)
