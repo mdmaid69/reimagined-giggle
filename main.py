dc8aa28bd44a6833585f6c0546ac925cb1e9bbe0
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:04:03 +0000

    in line swimming noise height

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def is_odd(n):
+        return n % 2 != 0
