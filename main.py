4b5f2b02c42b7bab95cdfe1faf96125515814158
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:49:34 +0000

    together scene which immediate charge

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_length(array):
+        return len(array)
+  def count_elements(lst):
+        return len(lst)
