14637b36f7ca27bfd3271b81788b84bfc697835d
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:17:09 +0000

    attitude like breakinto fresh mean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
