9ea4f60dfc91791eba129486dff1a8f82df4ba1d
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:31:05 +0000

    indeed bring available Nn pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def is_even(n):
-        return n % 2 == 0
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import os
+def remove_directory(path):
+        os.rmdir(path)
