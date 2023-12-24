2a8c55f48306cccd3281c15e88d61a0bcd1f8848
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:26:37 +0000

    within will national crowd around

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import sys
+def exit_program():
+        sys.exit()
