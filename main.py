be080483c1b81a565dbbebc64a13dd8bdfac0c7f
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:37:36 +0000

    breakout cool cause dream usual

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
