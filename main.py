b35750e1655992dcdaf963169181d75fb1c35d7e
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:44:22 +0000

    maybe stupid order beyond instant

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
