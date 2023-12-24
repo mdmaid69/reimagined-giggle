41156ed4436ef477fd6d0cf22039caa1404efdd6
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:49:09 +0000

    carry length pile scrape below

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+name = "Python"
+print("Hello,", name)
