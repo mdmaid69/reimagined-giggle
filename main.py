99e6e1fb8c1b8e19aedec9db971e0144b2335ad7
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:17:53 +0000

    thank wander somewhere protest famous

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import sys
-def print_python_version():
-        print(sys.version)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
