6e2de2498dc430deaa2c94bdf96255fa8a4302a6
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:09:12 +0000

    juice rare concern swimming talkback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
