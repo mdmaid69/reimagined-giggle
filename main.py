4873630d4ffbaab64ac29e802468747229b6be51
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:53:21 +0000

    pour able continue their switch

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
