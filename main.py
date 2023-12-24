69fcb5bbf7d2d51fcd861bb503ce01bb29ba1b81
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:29:45 +0000

    market find call process consist

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
