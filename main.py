58dc1b14fd552a0dafffcb509455e240b617d34b
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:26:55 +0000

    together protest brown disapprove movement

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
