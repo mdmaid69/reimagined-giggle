1e3a6c472cac7582a7169a4359f1592f2f526253
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:04:25 +0000

    pure beautiful if surprise reasonable

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def add_numbers(a, b):
-        return a + b
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
