6ac9178a655c5542d1f94b4a46fe5420d1527ff4
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:14:49 +0000

    university cover invitation spring round

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
