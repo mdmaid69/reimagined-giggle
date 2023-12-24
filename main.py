0534298548848ac7d2971cd3e442e71cc01bf33a
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:48:27 +0000

    tradition damage sudden ago reduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
