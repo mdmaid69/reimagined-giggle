b37ea3d150adaff96b7c00805080f300906c359c
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:09:38 +0000

    difficulty importance slightly destroy criticize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def is_odd(n):
-        return n % 2 != 0
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
