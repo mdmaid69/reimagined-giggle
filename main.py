3f32da7e8ae8eab3657d78c7a0b60f684927eb74
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:40:39 +0000

    afford wrong complete exchange film

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
