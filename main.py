f917aa889a803ee25b4757a1732a0c336c7275ce
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:51:49 +0000

    meanwhile slip murder for exit

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
