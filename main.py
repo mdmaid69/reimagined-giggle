957e493cba4d103ccd145d6bfd3d75d7200f51ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:49:42 +0000

    bang Kk show base mother

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
