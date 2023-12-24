43de4a50e0bd7aaa0576c130c44661ea4ec532ff
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:08:01 +0000

    division warning back oil here

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  def convert_to_binary(n):
-        return bin(n)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
