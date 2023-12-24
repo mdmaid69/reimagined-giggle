8a78226d48e4cfa5edb1489a281801cbc5fa108f
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:45:10 +0000

    respect content circumstance clever high

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
