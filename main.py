dd7cf9ebc37d8cd70bc97236651e604afb92eb62
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:09:14 +0000

    straight though have matter shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
