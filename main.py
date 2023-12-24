ed8e4ee60b50116027c2df3e54e1c59306f7f7d5
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:08:43 +0000

    total phone they quick telephone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
