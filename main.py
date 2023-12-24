5d108ff3b75c5f95198bf7bc425ae2bfb9bd8880
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:44:21 +0000

    impressive knock clothes cheek male

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
