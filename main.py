67f665b0ae6510d190e4422c50a5f3f00bf4afe3
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:41:28 +0000

    work tap poor tonight note

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import collections
+def create_counter():
+        return collections.Counter()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
