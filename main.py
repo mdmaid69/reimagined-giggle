2aee4b462f85bbf21444a3c2ecde324e4100d910
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:02:52 +0000

    comeround pleasant spill lean spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
