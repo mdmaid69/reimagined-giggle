f20a039c7273ba34e8681a959fc2272c2ebf6840
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:53:51 +0000

    traffic band instead goup best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
