7ce81a0bdea868f2c290204405061bda6640e362
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:13:30 +0000

    nowhere wipe transport autumn share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
