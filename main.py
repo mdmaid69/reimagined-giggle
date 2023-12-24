780adfcca93081220c64d1459f0c1467660ef31d
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:30:55 +0000

    choice surprising drop crowd bath

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
