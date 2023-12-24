13f45ee85ed8037ab8ce740953e0ffb7f90b0d4c
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:22:00 +0000

    punishment object up small criticize

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
