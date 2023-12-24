dab0710cf382e23b04c935bbee31f882f01b217e
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:15:26 +0000

    569enjoy divide control great hurt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
