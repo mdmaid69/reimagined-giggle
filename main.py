3aed5705860eb89ede8f0997c5642b6babf98515
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:59:25 +0000

    quit floor you capital walk

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import math
+print(math.pi)
