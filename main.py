1b29e566e693cef57f347f9328dee1665504e7bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:17:34 +0000

    total bad he valuable rescue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
