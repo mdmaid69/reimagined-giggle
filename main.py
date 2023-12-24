c71c559876ce26c42e2d208a152f659289f5d222
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:07:06 +0000

    enthusiastic total earth measure else

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
