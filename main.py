9651c6d9d1eb3c0d14b7f338c58fe0bdff991d88
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:40:26 +0000

    angry hard shout funny turnaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
