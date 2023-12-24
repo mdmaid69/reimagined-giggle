e9522d6329f126c1018162cdcf45cb199e3437e2
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:29:04 +0000

    myself scene authority thirsty dull

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
