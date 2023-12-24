372d1dfbd012ebefabb3b7cb85d99c57b83c2b92
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:26:30 +0000

    nigga crash will crowded easy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+text = "Hello, world!"
+print("Words:", len(text.split()))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
