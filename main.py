d74c4dad64dd483f3a9a16577961b6d1c05d4189
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:50:21 +0000

    name breakdown inside share area

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
