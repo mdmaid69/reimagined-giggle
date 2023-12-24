5c71db9da93a109b85e40127fa3d793d3bfa4854
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:04:53 +0000

    announce point 572enter fail neither

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
