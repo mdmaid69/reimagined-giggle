2ac547c1e1176a2cfbf75627f7d4206bac3d0d7d
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:29:51 +0000

    emphasis move grateful action welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
