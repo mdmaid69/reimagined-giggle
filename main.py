86f37a1fd5d74338fd3242898d935857ac31b5d8
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:48:46 +0000

    hot write back speak path

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def divide_numbers(x, y):
+        return x / y
