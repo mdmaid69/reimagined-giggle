ae358721ae4e25067bb0784cfd6d7e0069749dd4
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:29:46 +0000

    wander progress snow bit gather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def remove_directory(path):
+        os.rmdir(path)
