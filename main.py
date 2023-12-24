5028ced41151fe92d74ff0ef4cc5727cc3272abd
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:22:59 +0000

    dinner might its edge there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
