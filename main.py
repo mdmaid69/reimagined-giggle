79967e0e4347d9163e004e06b32f1d66ae8e3a9c
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:00:20 +0000

    much declare face display simple

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
