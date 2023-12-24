3e6b0c6fff9c893e518e774fcf312dc7698d2ca7
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:41:50 +0000

    minority actual quit date fit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
