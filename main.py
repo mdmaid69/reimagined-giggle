b39d97c1ee2a43aa997b93f5084e0153281b2d61
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:40:51 +0000

    ability more feel account move

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
