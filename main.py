75410cd261c5bd8bed54393089f73a797ead8b24
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:04:24 +0000

    wonder survive base impossible mixture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
