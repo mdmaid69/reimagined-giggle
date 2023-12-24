9b6927eead86da4f6da840522a05bdd1d245d58f
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:43:50 +0000

    command do within collection turndown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def is_odd(n):
+        return n % 2 != 0
