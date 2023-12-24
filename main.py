8ea334ab0a88f77c90823ad674fccd23f1e33110
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:56:19 +0000

    generally plain secret lift spite

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
