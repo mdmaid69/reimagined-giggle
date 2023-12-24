60921005b3c34a33570490eb0ba67b1a18eff6da
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:01:48 +0000

    crowd strict wash open suggest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
