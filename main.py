fc395ebdccce3475f57f62775a996b3a7e524de9
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:05:38 +0000

    ours possibility clean comedown likely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
