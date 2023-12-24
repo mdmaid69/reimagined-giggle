927d3111698a6bd835bf8e767144a5266e6daeb4
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:42:41 +0000

    winter forgive march radio react

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_max(numbers):
-        return max(numbers)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
