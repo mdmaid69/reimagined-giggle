271fbfa1fea60f69f4010f81bd0cbb008724e7f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:47:48 +0000

    typical still negative advice design

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_as_set(array):
-        return set(array)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
