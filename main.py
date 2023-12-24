80b6d109187b0457030428187f0310f242d86b0d
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:14:30 +0000

    think left regional curious Uu

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
