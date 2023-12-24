cc8e20d97dde8e2a675cc6fe4e73e08034df7114
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:15:03 +0000

    happy past stranger operation above

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_user_string():
+        return collections.UserString()
