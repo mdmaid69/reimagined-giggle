8536d8a81c9bb5434807a0ceaaa2d6a74d501edc
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:47:42 +0000

    start description floor study terrible

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
