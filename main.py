dd6b22c34acfbc71c5a48fa3c083ce672003f5c0
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:41:38 +0000

    best relief wave job whenever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import datetime
+def get_today_date():
+        return datetime.date.today()
