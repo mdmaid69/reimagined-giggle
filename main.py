b4ab0b78ae2ce2f734a9cf3ace0070490aab1287
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:17:48 +0000

    confused wash quantity buy turnoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
