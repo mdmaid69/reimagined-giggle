70a1c840bbd0e0fe91dca892c43a467d58d46f88
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:20:31 +0000

    agreement if circumstance ride sweet

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+name = "Python"
+print("Hello,", name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
