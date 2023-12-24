ca1a106189c90eade429e187d19d801b62d765ee
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:04:17 +0000

    speech pure rule cover well

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
