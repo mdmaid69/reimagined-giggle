700bdaa85ecf9ffa8d653b271bf0cd666a238410
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:01:13 +0000

    manage do born brief heart

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+print("Hello, world!")
