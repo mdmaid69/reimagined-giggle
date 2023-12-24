f8cb7bc77117f10e4842fc0b1945d3b53e3ca84b
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:18:03 +0000

    tone mistake notice difficulty exact

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
