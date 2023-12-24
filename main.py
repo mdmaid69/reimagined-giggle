b22c4f65acee93222164b544d90c6134f8232ee2
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:37:43 +0000

    father sport switch conclusion belief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
