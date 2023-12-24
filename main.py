64eb779c1334b69c15b43774c78d4e7a52cb9305
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:34:01 +0000

    actually late at married sensitive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_bool(array):
+        return bool(array)
