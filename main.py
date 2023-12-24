e4ec4f1593a192c94989bfa23f8f2f7c8bb167df
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:03:20 +0000

    answer about doubt divide suggest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
