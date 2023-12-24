c78e43a002ec609aef3e76ce01cbe175caf7944f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:28:09 +0000

    march lie great though comedown

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def find_min(lst):
-        return min(lst)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
