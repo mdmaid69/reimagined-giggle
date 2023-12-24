9afeb7a44e4dcccbbe3d80f64846a40af025f971
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:21:31 +0000

    anyone circumstance doubt day either

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import sys
-def exit_program():
-        sys.exit()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
