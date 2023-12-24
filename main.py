e6d7094a9763409d1698bf13c05183bd2ef39fd2
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:16:49 +0000

    reject rank spin shopping yesterday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
