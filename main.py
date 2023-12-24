0903d360b3fa302635d55dc1a99eaea61a7aa317
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:12:36 +0000

    famous cinema may fit delight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
