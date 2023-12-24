ebfef1d56d29d9a8d9649eaf5583f08fcea293e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:06:01 +0000

    new related meet can uniform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def is_even(n):
+        return n % 2 == 0
+import array
+def set_array_item(array, i, item):
+        array[i] = item
