b8abb7b46363bc170abb78e98e72ada653caf1c8
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:21:47 +0000

    neat earth disapprove ilovedicks basis

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-print("Hello, world!")
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
