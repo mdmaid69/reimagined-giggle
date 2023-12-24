091401021d64cd8e2c9cd5dbf6300b75e40655b0
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:31:03 +0000

    yourself wind result minimum picture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
