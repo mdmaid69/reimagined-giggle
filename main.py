3fc46171799e494e1d0578c892564772581d38fd
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:23:38 +0000

    shrink let fat cut surface

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def remove_directory(path):
+        os.rmdir(path)
