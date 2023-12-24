ee214a5b057736f60e0012dc5b360ca8c493a6db
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:20:31 +0000

    upstairs target love sell group

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import random
-print(random.randint(0, 100))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
