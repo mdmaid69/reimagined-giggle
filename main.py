7167b2f6d77c5133b634f45382ec66e0b960a5c8
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:26:43 +0000

    both clear lift watch only

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
