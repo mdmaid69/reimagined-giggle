4e515d336729f8433a703dd1500c7585e3c4a902
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:18:39 +0000

    tongue stranger free only holiday

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
