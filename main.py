6f5e75c44f87a5ae00a151864a13e470890deefe
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:19:20 +0000

    wonder pride comein ourselves port

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
