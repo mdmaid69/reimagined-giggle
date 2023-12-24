796174da75609c2331a5e5e3105a6839ce5a3e56
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:53:07 +0000

    care especially position break comment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
