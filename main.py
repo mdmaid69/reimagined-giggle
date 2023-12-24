9138c3178091687b7b989dbb1497cb6ecc6bd380
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:44:37 +0000

    flow comeback stop artificial special

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def subtract_numbers(x, y):
+        return x - y
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
