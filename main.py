9e2453bffc3eae80cc81a98539ab7b804eb4575a
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:06:27 +0000

    sink broken earn recent glad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
