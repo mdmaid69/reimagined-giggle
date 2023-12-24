e73ff63704efec0a6c1d5e66408c479b64e5da59
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:29:47 +0000

    mistake blind platform die prevent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
