0e4e749da02a9c950e44fbe2170196c36efd2f51
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:56:17 +0000

    crime strength door come claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
