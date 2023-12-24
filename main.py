47188ac6511738a284f2d3900520d524974f6a6e
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:25:04 +0000

    ignore copy circle beneath effort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
