73c00444686ebdec75323926fe45c22b4dd23e44
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:13:17 +0000

    ring history own trap punish

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
