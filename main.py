3e6ce37a9793a685efd686e1d9aa56dd22372113
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:33:21 +0000

    relief getoff picture wide paper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
