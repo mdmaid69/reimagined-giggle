eb7071ac9cbca99925249b7dfeff81f06044d7c8
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:56:22 +0000

    change control success upstairs lean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
