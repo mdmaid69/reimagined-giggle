6ad22caa242b616c341bdc7f39ecb55ebc90882d
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:56:19 +0000

    into tip pause like right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import datetime
 def get_current_datetime():
         return datetime.datetime.now()
