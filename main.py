621b1f85208d332cb4eb2c7cd3a9add53494abb3
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:03:07 +0000

    anybody 569enjoy everybody suddenly hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_pressure(force, area):
+        return force / area
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
