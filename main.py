e8eff4204a14340cdefd0d01f1dc3fc18ebcd957
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:24:55 +0000

    rare own later bill law

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_list_from_array(array):
-        return array.tolist()
+print(sum(range(10)))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
