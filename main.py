10602fccd4d42d300e95dddad8a8796f275ea5c7
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:36:58 +0000

    ring swim turn nose damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
