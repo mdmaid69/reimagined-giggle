cde3837dffc8c0c90364d54e4e70378a9f916c68
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:37:49 +0000

    ordinary statement support praise explanation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
