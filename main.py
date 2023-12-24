73ecd4874d3549c4867f5b8af25ba13600a00ebc
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:38:42 +0000

    breathe movement lesson certainly performance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
