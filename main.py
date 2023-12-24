cd5693ae6882b124e3ab5612fbbe9874ad7a383d
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:22:55 +0000

    part persuade boat practise fast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
