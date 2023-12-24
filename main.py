0b802f79978b8ce40e40ce8e1a44c80e310d4631
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:32:07 +0000

    gather Tt present average result

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def count_elements(lst):
+        return len(lst)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
