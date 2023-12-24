073d950f11993bf6ca7307e7505944cb7efc6023
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:44:30 +0000

    emphasize case high visit our

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
