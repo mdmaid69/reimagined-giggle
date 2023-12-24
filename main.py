bd50cfdeea1d749cf25de2929a5dd7bde999a729
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:18:47 +0000

    refuse software board collection respect

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import math
+def calculate_sine(x):
+        return math.sin(x)
