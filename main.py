3330fd90d286043366a7964e2419370f668779d7
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:42:43 +0000

    swing swim guess approve equal

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import os
+def get_current_working_directory():
+        return os.getcwd()
