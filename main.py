6cf0871d643f104416422229f87363a06454c60a
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:27:09 +0000

    contribute succeed problem natural still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_work(force, distance):
-        return force * distance
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
