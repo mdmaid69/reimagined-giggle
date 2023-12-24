a7532e7d526958b408f91f2ec57e8131b1a3136d
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:58:02 +0000

    unless guide pair rescue run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
