6eb8c2713e8396d809b111a0cd3eab7bcc8690f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:15:07 +0000

    sort lonely always win fill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
