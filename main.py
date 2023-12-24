af060eb1efa67608edd9f9a5abf6662030cf25f1
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:25:11 +0000

    lay include waste comefrom trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
