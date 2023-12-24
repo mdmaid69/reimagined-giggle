33c0583be7ac7180d946926808c6736acaf6656f
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:39:58 +0000

    steer suppose ours estimate hello

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
