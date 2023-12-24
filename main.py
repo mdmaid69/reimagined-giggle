a25d2607d7ded9f7d2f3b758127faa1103c5d85d
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:18:58 +0000

    get expression worried public continue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
