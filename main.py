4c534903ebbf58d13ebed506c0a599bc56a7df14
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:55:31 +0000

    order remain discuss perfect still

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import sys
+  def get_python_version():
+        return sys.version
