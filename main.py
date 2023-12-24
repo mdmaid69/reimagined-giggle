7ddfcfe08a218d63ea0c7d8cf8afa625ed921bc8
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:23:33 +0000

    sweep spread telephone deliberate church

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
