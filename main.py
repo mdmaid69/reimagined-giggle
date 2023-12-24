0679503356ffbfb7e0f27b2e0f1ab5f1abb068c4
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:52:53 +0000

    Oo tough water police noise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
