66ca979b2a89d15866844de031b49876d15be27b
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:34:30 +0000

    ocean fail team cause cut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
