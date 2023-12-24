1b2b53fb2aa1ccba794dde4cb1b4f1c7c1a16125
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:36:16 +0000

    education central comeoff total win

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
