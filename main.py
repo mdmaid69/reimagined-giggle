525f1ba5a8d891a6d96f765aee944014915c18c5
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:29:20 +0000

    occur thumb performance both plastic

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import collections
 def count_elements(iterable):
         return collections.Counter(iterable)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
