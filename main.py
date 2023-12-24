63de857c23d5bf847236ef31844cf7a6ea254b59
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:45:30 +0000

    pollution maximum raise thinkof bang

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_max(numbers):
+        return max(numbers)
