a404b1a3e982b5d3b8b5a885988faf7ba8e61d67
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:39:51 +0000

    keepaway complain obvious spite stir

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
