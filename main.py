7bf10f1c8dec85ec54163138010c06f16a1790a4
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:55:47 +0000

    below powerful bear anybody cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
