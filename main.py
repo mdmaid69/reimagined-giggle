b2fb6be8bb429113f47bf91d1147d1ebd710cd2e
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:43:01 +0000

    breakin key wild add attach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import math
+def calculate_error_function(x):
+        return math.erf(x)
