e320a96746d6cd1c5e5bb8260c9cfbd85386098c
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:44:42 +0000

    pay stir trip both attitude

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def find_min(numbers):
+        return min(numbers)
