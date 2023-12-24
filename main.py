4f5ba315af31b72a22ee023c96be71d28b63a156
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:19:55 +0000

    game just time show you

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
