f85ff77062ba535d007a804adaa3f3bacbf8bfce
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:32:16 +0000

    suggest recovery movement yourself lend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
