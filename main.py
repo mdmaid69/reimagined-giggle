73d8e975d66401b100a9e02ef59b2035b44ffb55
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:28:15 +0000

    respect instrument anything luck ashamed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
