d4c9397a3faefc7257623ea0c60e680305480217
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:45:56 +0000

    connection worth behave herself embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import tensorflow as tf
-print(tf.__version__)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
