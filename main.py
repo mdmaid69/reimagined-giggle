ef14ddad777c70544107c42249157865dcd477b2
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:09:18 +0000

    sometimes meaning deep measure lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def cube_number(x):
+        return x**3
