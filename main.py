18870b7ee0c0be80a6ec69507449b14d6ae84ee2
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:09:03 +0000

    work 570enjoyable thin tense hungry

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
