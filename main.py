8fcdd8329f9f69bc2034f3ea29ddc4a1c4bfbcdd
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:47:09 +0000

    yes label vote correction them

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def sort_list(lst):
-        return sorted(lst)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
