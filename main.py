327b6b403416707ff6800e58c441f21940350fa5
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:03:06 +0000

    holdon enthusiastic again radio waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import os
+print(os.getcwd())
