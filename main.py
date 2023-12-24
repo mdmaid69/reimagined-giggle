bb527f637dcc906c56f6847b301170463d6b6687
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:28:21 +0000

    storm obvious reply waste second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_length(array):
+        return len(array)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
