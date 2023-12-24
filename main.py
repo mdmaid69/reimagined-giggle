7be1af352a4f4d9b2abd4a61a8aaabc88edf51e7
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:25:12 +0000

    succeed visit birth shut fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import json
+def convert_to_json(data):
+        return json.dumps(data)
