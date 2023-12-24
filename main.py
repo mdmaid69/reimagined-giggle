02a406efad4ba344c0f66e24fcdf51ab14c7b536
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:52:29 +0000

    high rent floor might trip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
