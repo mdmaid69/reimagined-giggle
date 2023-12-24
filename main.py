a8000e8eeaa0880606f9df4f069c39cd7445af66
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:52:23 +0000

    needle high tomorrow fight outside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
