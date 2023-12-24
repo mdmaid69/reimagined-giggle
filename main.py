647fd2653e972b02fa766ff9faa76608782a65bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:23:39 +0000

    road exaggerate thick hope hardly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
