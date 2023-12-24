17363da6eec59cf024bddce8649cf57eeb1d5cb9
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:05:30 +0000

    thinkof process bad introduction downstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
