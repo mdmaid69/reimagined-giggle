5a6c1f0a32a077885386a5a3a1cb94e52790b874
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:50:43 +0000

    unlucky system other guard rest

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+print([x**2 for x in range(10)])
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
