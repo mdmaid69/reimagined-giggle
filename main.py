39237f3a551ffceccdcd7f6ce871666aef3be4a3
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:11:29 +0000

    takeoff offence bang mainly ahead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_permutations(n, k):
+        return math.perm(n, k)
