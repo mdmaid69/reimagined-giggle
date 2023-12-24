7e2f10882441df2ee52875e97f022dd83e40c967
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:59:42 +0000

    taste escape rid stupid plain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
