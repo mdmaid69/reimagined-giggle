9a5f62d05a2e7d52bd1169df3136e1fd9bc66472
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:42:25 +0000

    survive excellent careful nearly beyond

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
