ef3bcff98952b3a92ec18b0272dd15fcaed8b420
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:39:20 +0000

    exchange runout getoff both obvious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_density(mass, volume):
-        return mass / volume
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
