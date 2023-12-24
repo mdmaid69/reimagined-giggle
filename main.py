877725bd0fe2cade4d66b6eb88c1550db7609742
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:31:53 +0000

    today equipment possible scene mother

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  def square_number(x):
+        return x**2
