064f6686b7955f412fae4d52438cbdbdfa6e446b
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:58:48 +0000

    we decision blue reduction cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
