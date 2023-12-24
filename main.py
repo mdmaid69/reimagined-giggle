a8253bd8a888739c3ec8987a890e0554a126c991
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:29:21 +0000

    indeed journey turnaway stop breakin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
