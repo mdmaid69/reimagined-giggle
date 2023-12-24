26c8390c0b0ca7337ede6dce011a20dd8b43da6b
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:19:47 +0000

    train preparation greeting tear wealth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_density(mass, volume):
+        return mass / volume
+  def convert_to_binary(n):
+        return bin(n)
