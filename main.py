dad2e1bc8ae7803893279c4763fee565936cff0b
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:09:13 +0000

    protest connected proof murder rubbish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
