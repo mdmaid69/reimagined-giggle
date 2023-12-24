04e8e7fbc1d34e54d38b71db079477bf607852f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:39:43 +0000

    tape home part collection type

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
