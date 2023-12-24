8f56636044f109bcb4525b6576e733c1590a5d72
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:58:23 +0000

    makeup set succeed back fierce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_min(lst):
+        return min(lst)
