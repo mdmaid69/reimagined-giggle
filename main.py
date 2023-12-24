8aefce6d975adeea8dcc6d69b1402fad0ff6a596
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:19:25 +0000

    treat half trade spoil fair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
