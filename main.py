e2130b353dcaa3c103e7d291d68731f8bb7cde26
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:41:38 +0000

    cold spoil unpleasant prefer church

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def sort_list(lst):
+        return sorted(lst)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
