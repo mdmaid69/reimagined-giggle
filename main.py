367fcd02589350621d5fb9d45c54a3233fb9fb75
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:41:04 +0000

    opposite one complicated mail unlikely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
