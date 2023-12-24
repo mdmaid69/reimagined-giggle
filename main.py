ef5ca20325b4bd426587c76cc0437b2c9e361ab7
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:23:04 +0000

    health deep fine less sit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
