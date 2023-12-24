3fe0365532662331dc634dacaa0f936404c66a40
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:44:19 +0000

    want wrap match country nice

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
