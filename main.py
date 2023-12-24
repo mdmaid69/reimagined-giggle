43038f13b198315e3b84f495bfabce4913cdd317
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:43:21 +0000

    determined usual Uu towards earn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import time
+print(time.time())
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
