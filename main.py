34d2e06c52e9d1789b1823169bd92c556a665d17
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:26:56 +0000

    way disagree calloff card afraid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_area_rectangle(l, w):
+        return l * w
