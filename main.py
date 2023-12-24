cb2ae2f115edd90e23889c89306e64bdfa070ec6
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:11:46 +0000

    brother spoil free blow duty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
 numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import array
-def reverse_array(array):
-        array.reverse()
+print("Sum:", sum(numbers))
