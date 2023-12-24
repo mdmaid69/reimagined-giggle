705ef7bd7eef5f300b816fb830950c4af78c752a
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:33:15 +0000

    birth tip sport dress relation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
