66b14780291fc194d16b32d67ff6981f68b3cd65
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:26:08 +0000

    human peaceful come level else

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-name = "Python"
-print("Hello,", name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
