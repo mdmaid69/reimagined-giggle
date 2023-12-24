3b5576bb4f7fae9e09b0df3c54cf17921edc73eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:43:27 +0000

    leave anywhere tough swing 565end

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import math
+print(math.pi)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
