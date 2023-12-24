dde2ca256acef6059d1fe95feb2c8a2c74f5e59e
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:02:57 +0000

    vote Qq negative fit account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import collections
+def create_stack():
+        return collections.deque()
