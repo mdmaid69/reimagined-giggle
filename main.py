8d3c85eec0c4c67af4361e9a3e58388821416e47
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:43:14 +0000

    he Nn unite cut schedule

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
