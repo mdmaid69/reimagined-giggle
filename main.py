a0c455c3117f1245a06f07ad1e1b785f2edff9b8
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:47:48 +0000

    turn regret occur fat depend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_int(array):
+        return int(array[0])
