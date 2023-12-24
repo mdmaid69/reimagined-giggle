8d4cf2d50468ba0ef53a4e2c214e0bf531b60bfd
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:54:05 +0000

    busy wing risk identify period

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
