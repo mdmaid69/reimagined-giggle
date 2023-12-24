c5e915500f96f0e980d7ca33b6f7e82e9fb9e68d
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:49:33 +0000

    especially stretch post uniform spend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
