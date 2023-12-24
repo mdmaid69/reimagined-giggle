3b1036e1a492f995885a95c1359d53b7bde67c93
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:24:42 +0000

    destroy science lead hungry start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import platform
-def get_os_info():
-        return platform.uname()
+def calculate_floor(x):
+        return math.floor(x)
