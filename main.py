d7e56855e909b43442b8db823a690f09421d05bd
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:43:00 +0000

    how callup rid draw 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
