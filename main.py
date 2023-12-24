61bd0c933b38de10eb5e7c5d76c0fc61086fb4fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:10:45 +0000

    frighten height gate tone phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
