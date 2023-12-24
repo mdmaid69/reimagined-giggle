437e6e09ba57d94ba773555585583d0a49efcf04
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:05:31 +0000

    explain unite book target undo

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
