eaf3396acdb0cbf0d78814993d0d6a5bf62ffda7
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:16:18 +0000

    favour worth surprise exhibition change

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
