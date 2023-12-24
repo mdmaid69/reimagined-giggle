9fb48ccffb4c00b0d06001dc8f3e92e845ed6691
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:19:50 +0000

    yesterday complete connected group moon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
