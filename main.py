934fe880b3e8b1fd46840ef23dd3a56082cd3e5a
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:57:59 +0000

    her manner replace gas proof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
