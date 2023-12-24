6e83d52e030babcc5cbbdf0f380b96d721509ff2
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:27:31 +0000

    content pause right own lesson

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
