baa959ba727189ecd91f1211357bcc13bfb50a61
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:21:40 +0000

    floor model petrol steep pinch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import sys
+def exit_program():
+        sys.exit()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
