9c7896b576ff97f3c3168a7d89a6e723f1099efb
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:04:01 +0000

    comedown which inform middle stay

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
