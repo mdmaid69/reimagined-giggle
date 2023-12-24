e754549c8b95934df5ddc12d2ec9129346a19596
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:16:43 +0000

    excited talkback below slice behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_as_int(array):
+        return int(array[0])
+import collections
+def create_priority_queue():
+        return collections.deque()
