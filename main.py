9cbde7ab6dc7d50da3a37970ac41cb551e2753d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:39:59 +0000

    comein through speech organization fact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+print(json.dumps({"name": "John", "age": 30}))
