8d08e8a60706260728e88a3e936f4f35ad2d11a3
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:45:01 +0000

    stranger rain wake recovery admire

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
