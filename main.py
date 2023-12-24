6ad1cb4c1e1f68347e5aef940d3a3cbe6501a8fe
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:00:59 +0000

    unlucky 565end extremely sure question

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
