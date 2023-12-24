4fb2ebb44c2883bb6eafc94461ac55ee4270ee51
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:41:28 +0000

    minimum send suppose once splash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
 def calculate_square_root(x):
         return math.sqrt(x)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
