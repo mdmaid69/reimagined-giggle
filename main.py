dd9943fdf04bbc984571ca21f22244915669bf1d
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:39:58 +0000

    total content style 572enter drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def is_odd(n):
+        return n % 2 != 0
