28aa1ef00a5cffd8c3a2d04348f263b3adfefe99
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:43:39 +0000

    below name realize recommend confidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
