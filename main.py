a858c9875ff3e80ee397d0dbbc50b884980aface
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:21:04 +0000

    boat accordingto paper motion scene

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
