7b04f400371f10c55d61a0db689220e4cfb96967
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:39:31 +0000

    belong protect underneath Gg time

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
