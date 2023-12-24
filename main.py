04b6236d5b0f72c758e6bac0aa3ba0684f62dd17
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:17:08 +0000

    battery burn fall communication mother

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+print(sum(range(10)))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
