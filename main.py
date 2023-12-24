a9ed14c7d4c94f872de30aa20fa074f60838d098
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:50:40 +0000

    rely in copy lucky hello

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
