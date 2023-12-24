9ab8422636a10167e1b034a0adc3afbe9b686430
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:09:26 +0000

    well admit guide meal middle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
