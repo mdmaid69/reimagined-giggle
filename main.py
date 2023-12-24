ac8bde5dbe5356a3261f856841814ded9a9f8b0a
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:15:12 +0000

    cross design operate light matter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
