9e73aa3ea1dfd1faa878b29b21e8ffe0b98d8920
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:22:01 +0000

    giveup Pp deliberate pattern far

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
