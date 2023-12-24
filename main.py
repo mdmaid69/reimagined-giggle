5846e1a2f2de7c58e8618192b0b4a42734ece7c4
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:15:36 +0000

    hurry too pencil invitation interesting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
