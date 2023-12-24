76adc372612a9e573844cb2a26315b1e7a94aaaf
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:00:19 +0000

    rank powerful thorough taste keepaway

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def is_odd(n):
-        return n % 2 != 0
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
