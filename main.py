08e1a567815be527d6e3fdcb300e418aa86fdc53
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:41:23 +0000

    health slight creature top sudden

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
