53d53536bf2c9f10d521d14db48ae2d2de6bf183
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:55:02 +0000

    introduce please steady instruction breakthrough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
