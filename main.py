500c011f0550973a539afa09cf3e9ba784b75006
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:56:07 +0000

    bend Cc clearly male today

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
