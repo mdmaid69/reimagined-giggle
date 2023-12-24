4b3e25b2c2179b9d45bf18d7c46e70b333add98a
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:26:19 +0000

    proud turnover couple lookafter mine

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
