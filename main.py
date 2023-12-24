6d90e9c44671f47c1b92f70eecdaac1cb6d004ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:11:28 +0000

    dangerous church contribute deserve season

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
