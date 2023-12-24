adead7701376e5eed0f1daeca8484ecae5286b75
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:46:18 +0000

    funny subject pronounce interrupt if

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def is_even(n):
+        return n % 2 == 0
+def calculate_volume(length, width, height):
+        return length * width * height
