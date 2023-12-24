75d0475b2cd16873667a7325debc1d5ef02558c5
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:04:12 +0000

    past spend difference completely go

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_max(numbers):
+        return max(numbers)
