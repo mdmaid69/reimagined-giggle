cc38c4007ee22e4025c1450771822525fda0852c
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:14:47 +0000

    benefit by rub discover quality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
