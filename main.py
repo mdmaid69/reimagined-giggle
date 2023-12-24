3bdec4882534cc2e0bc73533d8ea3efe53ef9647
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:27:41 +0000

    furniture nearly argument speed bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
