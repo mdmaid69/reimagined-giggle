d9d9f5f80a3df7fd5666da7067f60b6e3da94b38
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:43:05 +0000

    circle fashion common table report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def is_odd(n):
+        return n % 2 != 0
