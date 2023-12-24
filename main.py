f36c4ee6cf4d5fb8c26e8803dcb0d5e2c22aaedc
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:30:22 +0000

    act steal grateful exact where

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
