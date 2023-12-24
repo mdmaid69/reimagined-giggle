db85ae0c6b8bcc74332c27357b9135dba8b34ca7
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:58:58 +0000

    generally angry lookafter something everybody

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
