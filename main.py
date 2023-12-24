6876b25adc5d29653f45cd90e32f55aa461ec84b
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:47:08 +0000

    tape peace connection miss brown

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+  def reverse_list(lst):
+        return lst[::-1]
