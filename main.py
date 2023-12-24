1fc53c957866a2f6cdf05e029bf992f6f614d9a0
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:47:36 +0000

    570enjoyable nigga comedown total unpleasant

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
