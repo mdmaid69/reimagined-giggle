4c10a3e3d2029701160396ef56487fcfdb988143
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:52:13 +0000

    confuse impatient getout lot hide

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def append_to_array(array, item):
+        array.append(item)
