6eca10d853f986153a6c60a1d788c394279b1998
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:25:54 +0000

    truth group slip discuss pinch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
