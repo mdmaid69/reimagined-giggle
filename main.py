1b5d6f77e4d13fe2faac5372c2aac30196115c52
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:50:15 +0000

    season lead profit comefrom example

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
