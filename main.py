56e6c50f0ee7902c6d0812523bd53e7e27e267f3
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:40:29 +0000

    tax choice turnup year shelter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-def get_current_time():
-        return time.time()
-def calculate_speed(distance, time):
-        return distance / time
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
