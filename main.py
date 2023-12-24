0b163a9356fb19c7d71907f6fefca0fc4924159d
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:43:37 +0000

    keepfrom tense accident little ago

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
