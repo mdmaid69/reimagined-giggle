33a9de0b6195ba8d14be6c537d928401ff98d2df
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:40:21 +0000

    land likely rule that truth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
