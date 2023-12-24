00f25d9829b5893248dd84902c2d8807c385272f
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:33:46 +0000

    hold passage workon 570enjoyable later

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def find_min(lst):
-        return min(lst)
+def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
