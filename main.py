86dee65ace6a0bef0ab0cdc5059379ee6292886d
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:11:52 +0000

    Nn test season accidental clean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+for i in range(5):
+        print(i)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
