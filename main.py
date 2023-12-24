eaacda3e51a6fd1d79b756c2ea2bbe3d1575627d
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:00:55 +0000

    even head improvement experiment temper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
