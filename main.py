7c8ef96871d517cc806e5fdae78667aee89d384b
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:59:39 +0000

    children by respect soft minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
