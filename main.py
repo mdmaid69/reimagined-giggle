a8acd297c24e5e74fd2dd3d922eac6a561ac073f
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:00:45 +0000

    interfere sink pattern refer passage

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
