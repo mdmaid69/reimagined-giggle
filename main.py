890cd73d3f3c8ceabd66276925ead5ac388b25b4
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:56:28 +0000

    like community story hospital instrument

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_speed(distance, time):
+        return distance / time
