efd7d195b60ef0efce5d7d9c99f5df1451b7b6f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:37:15 +0000

    breakdown distance official speak greeting

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
