e8cad40e9f0f44368f1a269817be4ba702db6967
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:22:24 +0000

    hers write start necessary steer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def find_unique_words(sentence):
+        return set(sentence.split())
