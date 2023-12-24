49223d3fa2090c2d17bde81f99fcaf99963e99a9
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:06:52 +0000

    several attention there however reasonable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_current_directory():
+        return os.getcwd()
