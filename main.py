32dc8855a9da432af3ac496281ca149f1b896581
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:16:04 +0000

    certain herself quantity legal keepup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def cube_number(x):
+        return x**3
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
