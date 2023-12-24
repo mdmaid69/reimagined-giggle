aeb5becd6d9034ec0af58a49f22cf3e3d7759793
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:42:52 +0000

    compete runinto file round important

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  def calculate_area_rectangle(l, w):
+        return l * w
