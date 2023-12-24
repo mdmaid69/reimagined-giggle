ff9522933e6d64562c13188e0c660caefee79488
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:22:28 +0000

    crime problem slight familiar wander

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
