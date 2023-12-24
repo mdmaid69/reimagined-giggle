cc9852619302470e4ab3eff8b474c0424e1fa7dc
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:44:04 +0000

    goback(to) 565end goout dry snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def set_array_item(array, i, item):
+        array[i] = item
