670699f31de5079b349b87800c12506bf99756cb
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:51:12 +0000

    prove doubt bound success previous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
