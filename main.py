25f5a67d445080fc879bb9b5fa88daa1df91cc80
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:58:46 +0000

    strict it difference language thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+from collections import Counter
+print(Counter("hello world"))
