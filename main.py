8c825da2082a71ae270b8088be4ced91e2c9d6ed
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:09:23 +0000

    trust care shop proud estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
