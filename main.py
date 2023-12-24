76f85a24ee723f6c30f995a9203ace8aae4d0ba4
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:53:57 +0000

    about neat period theirs maintain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_area_circle(r):
+        return 3.14 * r**2
