0ad7021278ea5e4b1f8049e5c3f63e1a1d408a9d
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:37:16 +0000

    worse someone smile yet several

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
