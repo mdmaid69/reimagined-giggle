a8047f5660ddb8476d0da91f875ab9bfb04bce31
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:44:40 +0000

    win develop sort position last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
