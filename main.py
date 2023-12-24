a176e29eb92bf797831801f4d54cff66da3d0022
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:33:43 +0000

    acceptable spring getoff seat capital

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
