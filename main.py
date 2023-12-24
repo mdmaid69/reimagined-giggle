c37fd192b8d40c648b92ed60c12580709a9723b9
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:24:55 +0000

    female during scene away copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
