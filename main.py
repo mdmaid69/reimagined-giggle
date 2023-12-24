aa724af23bf56f58f067a779426e500ba18ab3e8
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:40:33 +0000

    praise treat only brief float

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
