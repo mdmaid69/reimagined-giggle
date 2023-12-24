6b788ce7990a5dde53d1acd41845c2df6f642094
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:07:08 +0000

    beside earn do risk possibly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def cube_number(x):
-        return x**3
+import os
+def remove_directory(path):
+        os.rmdir(path)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
