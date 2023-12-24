ee8b0057c92c634f458a715e437a2e580db8ae9c
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:50:59 +0000

    friend strict ability makeinto identify

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
