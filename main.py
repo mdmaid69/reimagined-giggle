0b07f601bfc809fdc08702d0671a6dce110ad03c
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:46:42 +0000

    depend favourite affect fine suppose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_set(array):
+        return set(array)
