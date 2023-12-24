bd172f6cebba1d861bd730c63b56050a53f10de0
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:03:57 +0000

    flat image Nn suck bottom

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def sort_list(lst):
+        return sorted(lst)
