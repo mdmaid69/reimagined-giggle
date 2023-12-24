1ee7cf3e83412f1511a952a07562512813d5e11e
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:36:42 +0000

    tonight interrupt area impossible could

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
