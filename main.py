f9ef3ec418f5782308a386f1c5504485d827ff86
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:03:57 +0000

    mouth university software visit safety

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
