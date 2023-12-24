6ac75f341a00e540ab4d403fa48dddfda56e360e
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:47:21 +0000

    comeout nor wind brain glad

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
