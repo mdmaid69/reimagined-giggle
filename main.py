49f141f997ed454d2b9d23e333a13ea063c2eba6
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:36:26 +0000

    people star belief into within

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def is_even(n):
+        return n % 2 == 0
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
