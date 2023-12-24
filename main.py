26382208cadd5c167a77f3991b349548c1a80462
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:27:03 +0000

    watch welcome mean identify wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
