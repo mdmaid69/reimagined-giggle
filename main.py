aed45e3ab3e476ec43de4a42b94fae875b2169c2
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:37:51 +0000

    pencil emphasis regular stamp block

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
