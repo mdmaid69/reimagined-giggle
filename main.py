f92537c14fa12f0a8e90152ccb07408c98e782cf
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:38:34 +0000

    flavour blame hollow talk provide

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-def convert_to_hex(n):
-        return hex(n)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
