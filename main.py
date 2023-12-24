92f0ef2370980ee78ecceb3ad9220b64d9d11611
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:34:33 +0000

    scare preparation giveaway close field

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_pressure(force, area):
-        return force / area
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
