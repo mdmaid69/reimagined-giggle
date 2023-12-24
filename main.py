f46c64aa9f0cc30e994f94604388392cffb07eb4
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:10:56 +0000

    recent small her culture list

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def square_number(x):
+        return x**2
