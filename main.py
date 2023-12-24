252b03f6072ada4343c68e2365f3642d2e58ab4b
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:00:59 +0000

    art temperature lock little switch

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+print(sum(range(10)))
