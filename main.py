23a960aad15047e5b3bc7bfc8857e6cdd61821f1
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:19:37 +0000

    wake brave office think watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
