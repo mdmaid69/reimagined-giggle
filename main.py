8312df66fc56c408f935978810a0e0a3f455d585
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:25:44 +0000

    aim quickly neat month such

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
