005eb38633b6680bfc07eb1adb1a64e899eee4eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:25:42 +0000

    prefer warm go seriously pure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
