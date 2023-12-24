86bf620eb56ba4b05a90865dca0259b155a98685
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:42:13 +0000

    worry excuse heart evidence offer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
