41d5b9677bb6dad78759376060495a6d63e00f77
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:01:01 +0000

    temper talkback anywayanyhow worth also

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
