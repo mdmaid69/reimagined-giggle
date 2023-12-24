a884ca836a323fe5a547f288442c6b3883306bf0
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:04:26 +0000

    correct cut borrow that holdup

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def get_array_typecode(array):
+        return array.typecode
