6bfa8630e65e6058f5fa422b84465970304a0628
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:33:37 +0000

    last inside part effort pain

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  def cube_number(x):
+        return x**3
