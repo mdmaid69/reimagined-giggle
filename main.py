c139ae9cfdf9e8815a16aa091fcb262b07de8358
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:41:09 +0000

    wild bill worry person 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
