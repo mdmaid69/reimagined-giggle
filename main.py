dd6442b8eb0ac4ab313d473aa2794d4fad0d202b
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:38:01 +0000

    content before understand sting term

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
