e165293d01f1f0f556f2a74d84ecfea51f4ef68e
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:06:10 +0000

    chat pile praise proof bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
