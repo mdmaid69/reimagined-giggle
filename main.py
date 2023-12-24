1b865a5008b4818296670c6cd48558b48b1cc13f
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:05:59 +0000

    direction sale bridge politics content

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
