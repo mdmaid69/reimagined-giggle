14ad40b6e78d4799a4576ad9e1c86bb914052ec3
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:56:53 +0000

    report book address clue confused

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
