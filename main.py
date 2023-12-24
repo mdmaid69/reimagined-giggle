96342b5f9b762ecbd2607642f7581d53746d0052
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:42:10 +0000

    discussion win develop born scene

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
