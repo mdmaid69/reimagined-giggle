f46bb824627a4b30d7620d5ade5a6d2c7e955b3c
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:27:41 +0000

    trip worst genuine start cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
