1a939c004c99aed93a99cd019ee4887561b65563
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:55:16 +0000

    block finally damage persuade upwards

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_pressure(force, area):
+        return force / area
