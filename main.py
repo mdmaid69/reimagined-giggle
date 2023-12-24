6fce10a788381b8090ba10975f08d1682fdf32f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:33:48 +0000

    completely advice free when complain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
