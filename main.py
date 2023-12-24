84ff34ada63f716ecc7842e7a0794f592b362a71
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:11:30 +0000

    can total connect throughout 565end

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
