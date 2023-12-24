f223f652a97d0f3671b1997e264e1bcf5f04ddda
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:46:23 +0000

    feed plenty mark brain division

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
