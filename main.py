7fa8dd188d39b42970891f5b5a1bd4dc3c0faa9e
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:55:27 +0000

    reason goback(to) certainly mind minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_stack():
+        return collections.deque()
