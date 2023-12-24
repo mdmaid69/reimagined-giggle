61ac9da95d8c515a742b16251000f5761784986c
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:17:50 +0000

    lonely operation cross yourself expression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
