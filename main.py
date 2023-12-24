f0e52aab1ea597dc75b1fc2b800bd882626789b5
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:59:11 +0000

    slowly nature mass tidy mad

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
