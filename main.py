2aeabba4955df8429eef4f03c535f78282c6dbc2
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:17:47 +0000

    bus interest square air simple

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_work(force, distance):
+        return force * distance
+def is_palindrome(s):
+        return s == s[::-1]
