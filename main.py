8ee953325e26c7e123fd6ee690cd0b3cbf2bb007
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:07:34 +0000

    protest rush various thorough side

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
