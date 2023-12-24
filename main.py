b09c0397f51a6438b2f8b4c0545316cae9cfd1d7
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:41:40 +0000

    what smile us personal odd

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
