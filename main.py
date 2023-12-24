739b736c4965711477479478ef66373f4aca9116
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:12:37 +0000

    exploration idea decision difference summer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
