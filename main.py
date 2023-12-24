de63ccc65ae45900e09509b040272d693e87d76f
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:14:11 +0000

    immediate natural impression fat embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
 def calculate_hypotenuse(a, b):
         return math.sqrt(a**2 + b**2)
-import sys
-print(sys.version)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
