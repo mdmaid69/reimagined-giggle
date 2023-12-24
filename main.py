3823fcc9d04edadc0465b4419c25d953df5c5186
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:49:14 +0000

    impressed regular cause godown recommend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
