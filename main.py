d381b49a25a800821ca36072a7c0c0dbe2c229eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:18:12 +0000

    delight workout slow prepare naturally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
