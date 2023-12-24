4db53627a88c9a53410a6203abf3216521fd6309
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:06:29 +0000

    impossible swallow empty big narrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
