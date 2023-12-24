e5b86969e6501a7b51c8b40d3c33d627e822f92a
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:05:43 +0000

    correct rough foreign pressure breakin

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
