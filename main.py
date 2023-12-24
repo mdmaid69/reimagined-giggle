233d4e76a60ae3e826e6a9105f4fb1bc7daba388
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:12:58 +0000

    violent concerning limit lookaround interest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
