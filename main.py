2e6d96336e4b2a81cc4f8aaf99f7bf2808022e1f
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:27:22 +0000

    clue spray spend waste marry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
