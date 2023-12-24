06a3586e04500b28247b951b41aed8660ef43552
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:30:35 +0000

    my culture direction less medium

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import collections
+def create_user_list():
+        return collections.UserList()
