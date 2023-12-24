df2fea90c4d50b18618bae80e2aec9cdf0f7b1d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:03:10 +0000

    evidence ago middle dry several

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+print("Hello, world!")
