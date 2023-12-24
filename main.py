8045261e22b26ca5ed3ba15f748b6fe5a20e48c5
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:22:00 +0000

    earth eat suddenly minority blind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
