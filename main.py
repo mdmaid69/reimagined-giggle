30eff13e18b63eb30b4739cde50b96dfafc78ed0
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:39:56 +0000

    border spite brown pity criticism

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_as_str(array):
+        return str(array)
