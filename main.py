58731f529e928fc27bfcfb972607b28c5a72da70
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:30:16 +0000

    bother identify receive muscle hour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import array
-def get_array_from_list(list, typecode):
+def convert_list_to_array(list, typecode):
         return array.array(typecode, list)
