0ba9a34c5020e465c54914f9981b080eda7b99e4
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:19:47 +0000

    expensive length behave error software

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
