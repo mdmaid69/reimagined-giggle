1ba38f16453127ea4c8c0eb62cdad595c6f8241e
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:19:41 +0000

    stiff fashion trade song imagine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
