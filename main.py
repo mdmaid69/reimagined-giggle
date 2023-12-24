70ace1754069629fa1aeb3b704ce40e90636356a
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:52:27 +0000

    command gather both upwards season

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def is_palindrome(s):
+        return s == s[::-1]
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
