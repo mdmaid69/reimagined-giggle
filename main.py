e48fa4a3313038118aec546df473f82c67e1bb8e
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:07:18 +0000

    only tip reasonable round under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import math
-print(math.pi)
+def calculate_exponential(x):
+        return math.exp(x)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
