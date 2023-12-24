2587917ad647b092f0142c6d2bc14b9a42d491a2
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:19:33 +0000

    difference whistle tidy happy go

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_stack():
-        return collections.deque()
-def calculate_pressure(force, area):
-        return force / area
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
