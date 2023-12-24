21bec349dac958113d893455d2b596e26f3e2eaf
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:58:13 +0000

    persuade prevent police convince suddenly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
