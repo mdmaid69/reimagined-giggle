e8ab5d92421b89a5656f8144785c5b221e235f07
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:49:45 +0000

    tax 564encourage breakin watch drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
