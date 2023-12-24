7937fb090763eeb2407982896ffd95abb53d133e
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:18:20 +0000

    seem hair hit used glass

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def convert_to_hex(n):
+        return hex(n)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
