3597c9e7ba3a73bcabd2b95e42d15b3eb527e3ec
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:47:36 +0000

    ago so order tidy beneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-print("Hello, world!")
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
