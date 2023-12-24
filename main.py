0eeed4c4b9e94a7eab9a5eebdc8321189443c176
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:40:03 +0000

    pattern sweep confuse again room

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_blocks(file_name):
+  def get_file_blocks_allocated(file_name):
         return os.stat(file_name).st_blocks
-def convert_to_binary(n):
-        return bin(n)
