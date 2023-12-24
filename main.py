1985a1b46d164c78a3c9580ffcb21906ec04ecf4
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:23:15 +0000

    swim suffer sleep remember unhappy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_pressure(force, area):
+        return force / area
+def calculate_acceleration(speed, time):
+        return speed / time
