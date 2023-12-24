6e733660b668f51aca33a98ca65b77279d6909c5
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:24:28 +0000

    day almost sale advise name

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
