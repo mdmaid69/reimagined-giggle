eeb99c3e57d9a1bf432d97015c0ea32a1f480530
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:09:58 +0000

    better variety rub cool possibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_floor(x):
+        return math.floor(x)
