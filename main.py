efaa387dbc3d1ec592bab2cf5569d8365f93e027
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:51:30 +0000

    embarrassing walk schedule reaction high

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
