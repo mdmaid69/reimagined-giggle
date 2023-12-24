d20d601ba51f62e50acfb88113317cbc5d552792
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:45:24 +0000

    nurse tidy crowded image roof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
