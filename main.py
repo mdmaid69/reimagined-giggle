8652775c69b16f3d16c376e3946ea433988579de
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:36:22 +0000

    plan interruption lie style 569enjoy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-print(sum(range(10)))
+def get_array_typecode(array):
+        return array.typecode
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
