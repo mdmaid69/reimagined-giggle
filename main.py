ed336104d854a3e0011c961c239ee7746671cdbc
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:39:01 +0000

    century important may stupid listenfor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
