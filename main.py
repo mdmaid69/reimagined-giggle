9936a52c93b9d6f9f4c131067a4dd24a38ddafc2
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:37:20 +0000

    platform amount scrap pot warn

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
