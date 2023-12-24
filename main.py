ffd3be43dbfad660fba28161a77fa5fdd9b84d74
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:28:00 +0000

    deaf battery file clean fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
