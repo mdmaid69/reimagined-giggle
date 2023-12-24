b79ea18db8ff5375d152c176a0d3b3d9e1b140ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:43:34 +0000

    so perfect development inform Ee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
