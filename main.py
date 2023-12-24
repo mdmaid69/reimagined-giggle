4d274f15631dc693007719558b56db28fdb77a5a
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:16:39 +0000

    together give shut goon effort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def remove_duplicates(lst):
+        return list(set(lst))
