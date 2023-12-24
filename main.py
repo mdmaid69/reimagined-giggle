a10e4c20b9d42bf662b385d64a8d966a17cc0b7d
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:29:55 +0000

    port other tear thumb sharp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def reverse_array(array):
+        array.reverse()
