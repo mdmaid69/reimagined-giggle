bee0ef0284561359e819be9634a7dcb9499f3ddf
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:48:17 +0000

    difference freedom contrast copy genuine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
+import os
+print(os.getcwd())
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
