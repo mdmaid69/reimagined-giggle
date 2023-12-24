935dc6bf61b67e27ba049fa7b40e290f9c033486
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:26:10 +0000

    hope detail all view giveaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import sys
+def print_python_version():
+        print(sys.version)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
