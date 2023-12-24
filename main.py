6af7d62d0b1cddccd78861a5022d78ea2dbae295
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:49:54 +0000

    grow we depend politics train

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_speed(distance, time):
-        return distance / time
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
