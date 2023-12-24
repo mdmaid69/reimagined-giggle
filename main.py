9f304149d0c039bf54f8b033aa11a2cf24cf7a86
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:09:57 +0000

    show count interview raise ability

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def get_array_as_set(array):
+        return set(array)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
