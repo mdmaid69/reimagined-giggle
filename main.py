299de60eaa31cf1b5d97c04f228cd3780d6feaaa
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:59:01 +0000

    bringback before authority turninto excuse

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-def convert_to_hex(n):
-        return hex(n)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
