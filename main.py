ed90cbd5d7c862cc76b3d28484e8b392344d8139
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:43:10 +0000

    sort better roof sail clearly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import json
+print(json.dumps({"name": "John", "age": 30}))
