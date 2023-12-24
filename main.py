214395173f706062dfa1ba5bfbf6051667812d94
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:55:36 +0000

    watch waste swimming more bad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
