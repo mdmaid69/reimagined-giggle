34ad66ef3d48e546d9092649a9ae2426d8f7ca41
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:37:40 +0000

    woman threaten never disappointed exactly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
