c90fba4a9650be8a79b38ac83f76c40741e7388b
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:45:10 +0000

    bound soil risk sad awake

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
