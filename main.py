5aa54c12f38f79bc842af76c68992a26362044dd
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:22:36 +0000

    do as tired unkind state

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
