45da893f8823ed6fc0e85d9f0ab554d7a947a1db
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:32:02 +0000

    live organize comfortable undo water

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
