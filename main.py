63d3751d3760411b9afea60a52ba860485ce08db
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:51:56 +0000

    prefer comedown whatever laugh control

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
