7027e01b602ebe0314fae35816c87a63eace7a63
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:30:46 +0000

    branch find later convinced needle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
