fce737434d018fd889a3fb9cb04ba06ca80efdb0
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:21:38 +0000

    ashamed across formal hungry complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
