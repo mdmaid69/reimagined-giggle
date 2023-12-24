e30e1d266176adccf6d0cc02651729692404b9b3
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:11:41 +0000

    back runaway account figure run

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
