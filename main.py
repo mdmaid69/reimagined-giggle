42dd93e145171875a59662d939a93a1928154eb6
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:05:50 +0000

    available fork admire owe explanation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
