4182638eb4bcd6d2da789bc9655c366cbfc7d2a5
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:11:52 +0000

    shopping unlikely beat base region

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_even(n):
-        return n % 2 == 0
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
