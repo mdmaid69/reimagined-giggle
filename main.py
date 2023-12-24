b7a0d3e006ce4c490299a4d4d69dde9d3e3e8618
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:19:58 +0000

    takeback rough arrival new people

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def greet(name):
+        print(f"Hello, {name}!")
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
