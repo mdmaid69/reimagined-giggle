8811f5e9b96324d6bb3e00dd78104dbc05f07073
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:16:11 +0000

    genuine pressure weekend production fit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def find_unique_words(sentence):
+        return set(sentence.split())
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
