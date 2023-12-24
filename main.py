6d4b4bee868ee5204ca3e0cc9b45a70e97df5dd8
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:01:51 +0000

    wealth sweep content break bear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
