fc7a3cb9b36c016b5687896a8989685c7001cbcb
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:15:41 +0000

    clean anything very patience doubt

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import datetime
+print(datetime.datetime.now())
