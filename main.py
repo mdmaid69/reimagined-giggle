dd2cdf2be0da878a6d8bdd8eb3256a597d797cdc
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:04:37 +0000

    politics development touch shelter draw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
