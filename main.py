7198debe422898e8944e8431cfd5f2ea90379865
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:53:45 +0000

    play turninto walk standard fresh

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def find_min(numbers):
+        return min(numbers)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
