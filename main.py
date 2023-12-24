5ed56fd4662dba9d6cf60021caecdc12c352bce6
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:04:34 +0000

    trap doctor beyond clean content

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def reverse_string(s):
+        return s[::-1]
