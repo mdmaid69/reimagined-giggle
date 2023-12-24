414957b284f2f0751b03a28bc14146d03d242e34
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:25:20 +0000

    shock thirsty right relationship full

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
