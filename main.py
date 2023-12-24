9bee38a0f2e90f87f8817d5c7db761551631e631
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:57:27 +0000

    belong shoulder dry runinto habit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
