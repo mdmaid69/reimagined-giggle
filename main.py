af8e977c9d1d4377a60f3485d664de3470e219f1
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:53:34 +0000

    paper mouth cinema cut movement

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask
-app = Flask(__name__)
+import math
+print(math.pi)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
 
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+{body}")
