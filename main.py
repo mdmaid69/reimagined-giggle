9054f429309629bede18a1e6cb5ad69df5b976f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:49:50 +0000

    unusual delicate authority feature little

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def divide_numbers(x, y):
+        return x / y
