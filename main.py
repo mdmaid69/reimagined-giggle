6241ad19e153c774f1e33d8bd45a7db6b2f7f247
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:17:14 +0000

    delicate this complete care sad

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import datetime
+def get_current_date():
+        return datetime.date.today()
