8f50bb897120880b9032677548bc6e30872bbd96
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:15:24 +0000

    second skin seriously cost still

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
