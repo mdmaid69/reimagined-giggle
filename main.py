6cf02ff7b722cfc040b80052062ed4a8b131d536
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:20:39 +0000

    forgive wheel loud punish common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
