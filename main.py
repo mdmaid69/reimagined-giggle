f67d9066d12f4b2f3d5109e9a620e78e3dade0ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:38:31 +0000

    point man over real settle

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import time
+def get_time_since_epoch():
+        return time.time()
