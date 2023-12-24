19ad023ce7fa1f2355f7fb002bedcb6dac29d932
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:47:07 +0000

    key main fix speak necessary

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_max(numbers):
+        return max(numbers)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
