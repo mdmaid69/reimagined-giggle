987f2532d0698830e1507bd7af87b8794dccd2bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:52:22 +0000

    hungry walk tap unpleasant some

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import datetime
-print(datetime.datetime.now())
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_typecode(array):
+        return array.typecode
