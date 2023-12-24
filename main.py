2699186c497cf1b7ffcca5e853eae34444f07d16
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:19:18 +0000

    model interesting breathe own neat

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import getpass
+def get_username():
+        return getpass.getuser()
