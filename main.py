daa40c77727b1a4f6fc928faadcd475f298ed2bd
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:12:12 +0000

    innocent light comfort shall below

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
