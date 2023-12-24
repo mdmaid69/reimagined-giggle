83c3ff6a80f36b2579527d63fc8480216b54c995
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:16:44 +0000

    advice factory sound negative callup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
