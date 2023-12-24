d9a3402836f242e52c5c1008f8e1bbef7943d801
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:17:12 +0000

    Ii traffic tap tip interrupt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
