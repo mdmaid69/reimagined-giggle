486c423db1ef8655740be377fba5801ed497eed2
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:09:47 +0000

    mystery 571enough mind naturally general

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import os
+def remove_directory(path):
+        os.rmdir(path)
