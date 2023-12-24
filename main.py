4a3896d1a0d0aeae5937ac2a3c27aeb283adf5d3
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:02:25 +0000

    past front content brush lot

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
