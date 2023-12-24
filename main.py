66420e9437846fe04332444ab868d50271c75a3e
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:11:16 +0000

    formal pupil zero sudden pity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
