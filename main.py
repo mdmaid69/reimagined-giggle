77fc8eaa30668e1bf78599f5b220a3fa0f626a29
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:24:16 +0000

    target stop wing cough smart

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
