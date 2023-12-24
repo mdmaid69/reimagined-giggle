b4a20afc8f3dcabdddbd889d6ad015f2029c2a20
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:21:21 +0000

    past discover begin dream immediate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
