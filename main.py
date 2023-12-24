8ef512c69c9fc90faf435e81b8888d07e1176e6e
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:15:06 +0000

    us until group practise along

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
