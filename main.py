1f0ed41c83dd66bde443085bfdaae7f69a4d8e7b
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:43:05 +0000

    long ask cook lie second

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
