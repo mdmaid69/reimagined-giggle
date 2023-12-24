19f68ba0c2d3c3853489ad7544e69b9fe4bac944
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:16:54 +0000

    reduce miss invitation describe pump

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def count_characters(sentence):
+        return len(sentence)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
