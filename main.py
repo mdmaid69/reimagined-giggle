971a84c4e52c039cf0aef8f9a47e39a4d8f41fe4
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:09:25 +0000

    communicate speech butter second the

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def count_characters(sentence):
+        return len(sentence)
