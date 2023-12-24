253c4f25986fe23b8b7b2545576d61c5268d2e7f
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:04:13 +0000

    brush wide truth leg improve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
