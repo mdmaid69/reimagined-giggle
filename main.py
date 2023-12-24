d92835de1b7d095fc0a4ec9129bbda3fcca4f390
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:20:20 +0000

    typical already separate declaration only

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
