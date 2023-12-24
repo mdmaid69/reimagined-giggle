aa9b0ae58b75f393f1feb3e8fdb33dcb83b79e92
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:14:09 +0000

    body cold within point lift

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import array
-def get_array_as_list(array):
-        return list(array)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
