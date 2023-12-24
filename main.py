7394bdb5346217a4b423583572a836c2830b5709
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:03:19 +0000

    smoke pinch disturb 565end takeback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-print([x**2 for x in range(10)])
+def calculate_logarithm(base, x):
+        return math.log(x, base)
