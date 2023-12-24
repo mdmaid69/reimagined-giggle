09a3a717d58eff2048fa512e553591688d9eb05a
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:36:22 +0000

    cum demand Rr charge please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
