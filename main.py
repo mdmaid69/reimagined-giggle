18ecb5d0052fd75f4dbdf3cfd40d1d73623c1354
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:00:43 +0000

    hope grand busy giveup friend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
