bc159cb2edef71514377d27861c9bcc05bc497a7
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:51:18 +0000

    Itself bye roll loss tough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
