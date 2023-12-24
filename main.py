75ce153c9c89e5da2fc7b70915bffef693999546
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:27:52 +0000

    act lie bend indeed lie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
