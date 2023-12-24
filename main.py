8bb666d69457bebf3bea196a5ae34e3c64fc427b
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:42:44 +0000

    position release refer mother dream

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
