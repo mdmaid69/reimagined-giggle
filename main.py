70872c7a433005ce6d20c547771371f4520ba66f
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:51:26 +0000

    offer warm if opposite conduct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
