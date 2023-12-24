ed315035119cd294e65530028df552796a13e867
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:42:10 +0000

    conversation see exploration suck shame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_list_from_array(array):
+        return array.tolist()
