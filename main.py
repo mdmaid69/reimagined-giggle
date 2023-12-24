6515b66eec6a4cf1b6bfae093b31587c67e7abfb
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:57:36 +0000

    summer pick beneath shine disagree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+import array
+def get_array_as_bool(array):
+        return bool(array)
