58fb9d83446d468325b7264ae44c1109be8d34b8
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:07:33 +0000

    again somewhere for indeed bank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
