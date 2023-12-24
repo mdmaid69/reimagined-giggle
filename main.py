8cd9c43ea6f359e52d42ca0fe65afa14946e3912
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:41:41 +0000

    rob bye proper age body

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
