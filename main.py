58b5fb1e6d617de42c932d1228d4a1f67cf09717
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:20:19 +0000

    firm exhibition hold study correct

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
