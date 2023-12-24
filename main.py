decb5c21852bf5c8080d96ed861fd84c30b68afb
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:52:50 +0000

    drink judge holdon up strike

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import numpy as np
+print(np.array([1, 2, 3]))
