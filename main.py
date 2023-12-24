eac770fe958cf794804b347cbbbebde225a14269
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:31:53 +0000

    state loan nice wild next

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
