803bb93cd32ef9f54f44d4e7df832aa0f034a87b
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:00:25 +0000

    round purpose scheme human shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
