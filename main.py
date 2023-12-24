4f6ad510e3f34569e3aa1f1feb3e28f3889e5f8c
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:15:22 +0000

    on sail male choose possibility

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
