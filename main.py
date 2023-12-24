f0663267a27efb36023eee8716bc20e146320807
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:15:12 +0000

    opposite race finally land giveaway

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_as_int(array):
+        return int(array[0])
