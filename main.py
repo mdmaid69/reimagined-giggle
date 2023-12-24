8902f9905c37ac58d64e53ee4d83d43dc752ce98
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:23:44 +0000

    suck hungry attempt refer thing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
