1f7d431466fafd0f4e94a869c73e8fe0e1634d6e
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:50:44 +0000

    right consider careful wish stick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
