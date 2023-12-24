c898975676aeb6ecd5838d8026f96641649f3bad
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:33:23 +0000

    punish secret never issue answer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_area_circle(r):
+        return 3.14 * r**2
