c10c9307b2eaf3f7c2a8495536a121801efb0a0e
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:28:04 +0000

    ourselves attach dream field especially

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
