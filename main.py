c5632de2dc3d3122c56b29b7b050349831949da0
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:53:38 +0000

    accidental possible takeaway light position

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
