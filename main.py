cf75750a09866bb4914222b10bb8942cb6be36b3
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:46:39 +0000

    report office brave problem forget

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def is_even(n):
-        return n % 2 == 0
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
