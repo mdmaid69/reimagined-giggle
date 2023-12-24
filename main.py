1c785e5b234a42cab2e7a671f8fb2c2e08ca1b80
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:33:36 +0000

    money agreement attractive practical whistle

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sys
+def print_python_version():
+        print(sys.version)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
