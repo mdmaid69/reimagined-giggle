c7fff9ab4a4d25b792c8154ca81c7800675bb31b
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:45:52 +0000

    punish from tap near stiff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
