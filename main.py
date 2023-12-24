ecf820322e5b381e0dd1a650c66cfb4285670706
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:00:15 +0000

    travel division sign shock day

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
