0b5feb08ebb7290edb840c31493559a32caacf74
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:13:24 +0000

    demonstrate fear trick tired breakthrough

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def remove_duplicates(lst):
+        return list(set(lst))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
