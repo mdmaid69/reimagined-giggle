11c3f0f2765006e0f260b8320b67f20bba325402
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:32:58 +0000

    mind creature breakin university past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
 import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  def subtract_numbers(x, y):
-        return x - y
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
