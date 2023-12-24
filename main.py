eb82ab1831eea768fd6ad6766c8e2e438b203ca2
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:15:18 +0000

    excuse several bread everywhere rough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def reverse_array(array):
+        array.reverse()
+def add_numbers(x, y):
+        return x + y
