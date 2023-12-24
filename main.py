921ed16e0e9f6ad47744da74042386d537d51631
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:06:41 +0000

    distance positive sight bar where

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
