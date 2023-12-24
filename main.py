0372549f3e39b3c35c72ed62d34670492093b4b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:53:15 +0000

    fork building protest loud lunch

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def remove_duplicates(lst):
+        return list(set(lst))
+  def add_numbers(x, y):
+        return x + y
