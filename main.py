a5b0df397f0cbc72a19ae72427c957a70dced5ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:33:49 +0000

    minor put handle when except

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
