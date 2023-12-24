d773791fcfdc37b52ed042c8eaba41ddd2d41336
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:31:06 +0000

    period case news school push

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
