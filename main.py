5570798e53eb382b2f10da859fe9695298869d61
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:38:17 +0000

    maximum cry beneath right debt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
