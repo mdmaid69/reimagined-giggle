052d22ca8c4061c5e658e130ff02030b25b890d3
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:06:43 +0000

    inquire confident concern downstairs air

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-for i in range(5):
-        print(i)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
