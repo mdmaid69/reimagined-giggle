c4131da087ea6228b56ce7b1d8fb6abb1bc92ab8
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:45:26 +0000

    care vanish blow night keepaway

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+for i in range(5):
+        print(i)
