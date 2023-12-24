13e3b6969392939aa3a9be12bb6b2d77c82806e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:14:53 +0000

    breakfast low brush full tie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
