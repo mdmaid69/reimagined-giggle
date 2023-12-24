f64aa3af3bc12e5d753858cc0e6299d936969282
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:18:58 +0000

    sudden suggest suckme crisis distant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
