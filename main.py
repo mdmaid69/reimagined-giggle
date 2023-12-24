fddc3dea6c9bd040b2e6b10b78bee29499120c1e
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:13:14 +0000

    clean make protect pull however

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
