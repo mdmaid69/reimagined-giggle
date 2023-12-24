f40dd858f0680f0723f049fef3e9b92b211cf9fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:21:19 +0000

    deep accept slice tidy sound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
