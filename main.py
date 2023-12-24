ea8e50ac591ed1eaa88273574877ecf5baf8dbdf
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:14:58 +0000

    real uncomfortable play firm heart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
