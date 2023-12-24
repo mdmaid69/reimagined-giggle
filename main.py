1eeea6c409e4062e4e7908e333244424a581d51e
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:45:59 +0000

    square shade damp exercise least

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
