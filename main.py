891e8aeaf1eddf5dd775389e5c71511e17982fbc
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:50:42 +0000

    basic that table storm plant

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
