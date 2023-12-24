cd3b0da6592dadb590dfee47c859cf896645c77b
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:18:17 +0000

    vanish proud shut do second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
