1841ccf809cd30e5892cb22133a5ad7580a81432
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:59:11 +0000

    love attitude bringdown slight material

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def cube_number(x):
+        return x**3
