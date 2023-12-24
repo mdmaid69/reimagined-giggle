0d087fb4226fbe9ce69bef76d717d927869ca6e6
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:47:04 +0000

    perhaps report hit somewhere urgent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
