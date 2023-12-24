bf956e27af9fa21d6c1ede31392a392841a0b574
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:29:33 +0000

    tour lookup like flow beyond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
