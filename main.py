d094ce2df1a887ad5783cf569b9274716448100d
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:24:40 +0000

    friendly emphasize fear sky stamp

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-print(math.pi)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
