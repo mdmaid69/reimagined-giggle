b689583e46e9c276f3cb7ba551d453bdcf28e146
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:30:52 +0000

    565end and funny touch cheque

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import random
+print(random.randint(0, 100))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
