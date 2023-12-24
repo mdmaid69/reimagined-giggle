057e1f1b768e3a4693a28fa685aa7884d8b46873
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:01:42 +0000

    false communication suffer question wind

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
