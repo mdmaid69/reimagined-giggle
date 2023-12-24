f8a9d077f24457a281f53f67ac7d053054099db9
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:24:11 +0000

    raise many stretch impatient Rr

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+name = "Python"
+print("Hello,", name)
