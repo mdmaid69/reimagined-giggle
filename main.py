5b982648bfddbde89d3ac4187352f3c6c8028630
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:15:47 +0000

    few intelligent emphasize brush giveback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
   def get_file_ctime_ns(file_name):
         return os.stat(file_name).st_ctime_ns
