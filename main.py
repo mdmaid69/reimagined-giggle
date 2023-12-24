6df0eaee80fee979ccf0314b1ebda39c53bcf028
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:57:09 +0000

    moral season squeeze develop chin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def multiply_numbers(x, y):
+        return x * y
