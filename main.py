c027df17a8ca132e75c6111019317615c8e2b76f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:42:50 +0000

    anyone stamp for shape cover

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def multiply_numbers(x, y):
+        return x * y
