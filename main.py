4a5d4e2c5d4de6c5a5af3c47b47a4ce5c768bc25
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:29:01 +0000

    book before gas burn storm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
