10a6dfd25bf744866e91fda769db6de5018dcd97
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:13:49 +0000

    remove wide question also try

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
