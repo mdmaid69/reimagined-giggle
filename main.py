3bd05fa0ffc4a214c1aedc48baac3b76879135cd
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:59:00 +0000

    broad lookback(on) send spray back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import array
+def get_list_from_array(array):
+        return array.tolist()
