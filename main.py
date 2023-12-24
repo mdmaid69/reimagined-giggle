73a8d4eee31e596300c30dfc6f2183a79c825f4a
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:56:06 +0000

    top what bringdown every careless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
