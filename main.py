7ecde6ef1f1f05f6db1d8daa37e8c7dbe5334818
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:43:00 +0000

    remark miss future walk either

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_string_from_array(array):
+        return array.tobytes()
