ab43742a3fea937b2cb029ea078e136146dccd52
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:42:51 +0000

    post true advise address noone

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
