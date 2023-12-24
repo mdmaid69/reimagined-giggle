bd6fbb5cce57bb5d0cb8d49257fccc31e8895ede
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:27:29 +0000

    light this spite opposite for

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def is_odd(n):
+        return n % 2 != 0
