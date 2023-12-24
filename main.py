e506b2a32bfb06ec24cea1be34cbe7e54650065d
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:09:34 +0000

    patient address happen turnaway neat

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_power(work, time):
+        return work / time
+def convert_to_binary(n):
+        return bin(n)
