6afc1742a3481eec310038bc08bcadf8f02ce232
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:00:44 +0000

    capital rent coat damage bother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_work(force, distance):
+        return force * distance
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
