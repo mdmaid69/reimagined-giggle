51c94429cb5e204dc477c669a26c2ae7531a7eb7
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:45:47 +0000

    form behave callout murder complicated

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
