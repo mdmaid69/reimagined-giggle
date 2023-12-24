6e57d4ae7f62bf8ee0c2681af763be48a2d9a18c
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:55:23 +0000

    568energy between where male break

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
