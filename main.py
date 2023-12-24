8275ca2cd4aa10b151edfb48e7bf6e609b2066d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:05:18 +0000

    holdout buy climb be Bb

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
