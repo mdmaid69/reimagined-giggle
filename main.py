dd17b9b9fca66a7fe9a65bd0fd0a228bb422618b
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:52:57 +0000

    temperature sensitive concerned relax male

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def convert_to_hex(n):
+        return hex(n)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
