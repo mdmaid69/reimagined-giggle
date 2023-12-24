b297f2686d444d9171b102c5f20ea9b59fa2361d
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:59:44 +0000

    opinion depend scared acceptable trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
