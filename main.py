383406d8f0e576338b5f8f0997fb0bcfdd0a61bb
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:02:48 +0000

    cover shoot delicate always share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
