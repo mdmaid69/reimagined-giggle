a6b6d643606174088883d5889d034084bf28ecc2
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:44:22 +0000

    guess so break it theirs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
