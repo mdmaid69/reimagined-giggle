1d69f92ede0d60ccf7906c5578ff98c8d58a76b0
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:29:12 +0000

    sea Vv professional short thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def get_time_since_epoch():
-        return time.time()
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_array_as_list(array):
+        return list(array)
