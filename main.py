84e488b6be747e727d9b64c6620cd3b1706084cc
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:40:21 +0000

    care body Kk giveback estimate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
