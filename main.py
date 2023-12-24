36df7b89a92c3ae0baa7027684fdf852bd95a8b8
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:54:45 +0000

    match just second marry excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
