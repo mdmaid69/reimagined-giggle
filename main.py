117c84cc5379759076d055d5a2d0022d0329ba4d
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:27:07 +0000

    now unlucky such among become

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_work(force, distance):
+        return force * distance
