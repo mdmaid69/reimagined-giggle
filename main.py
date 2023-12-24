38ac6b9f6b3ef42f946ebad044dda8b87e1b45b9
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:20:16 +0000

    morning angry closed up mind

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import datetime
+def get_current_date():
+        return datetime.date.today()
