14a20161155c909aa2cd84d253be267984770c90
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:39:01 +0000

    burn experiment fun mark recommend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
