85034546c5c57c1d3b27fd4220b31243ab5232ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:16:47 +0000

    Pp ourselves university hope glass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
