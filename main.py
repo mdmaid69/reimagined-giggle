fcf89d1376a59e1d9d0baf844e56e44e165d591c
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:59:23 +0000

    reserve tidy knowledge slide present

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_float(array):
+        return float(array[0])
