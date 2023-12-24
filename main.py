f6b1df8587826f209fe4c89814b884d003a2c345
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:12:53 +0000

    charge embarrassing for point substance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
