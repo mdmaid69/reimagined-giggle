226c7844c384531715cfe82bc829e0b5091851a6
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:28:40 +0000

    number open pronunciation prevent sister

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def count_words(sentence):
-        return len(sentence.split())
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
