cfec0a34d379dd74288d3e8285fc99b52819824c
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:58:18 +0000

    breathe used check short quantity

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
