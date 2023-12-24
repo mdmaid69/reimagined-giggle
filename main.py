b55296d9953fcfcb71ef8fc2090482b3b77cdf50
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:54:29 +0000

    workupto together relationship behind command

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
