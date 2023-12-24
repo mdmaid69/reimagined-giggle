0c075a5f862d92eaa68db55ba633dcde783dfc28
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:11:32 +0000

    breakup range touch thick tune

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
