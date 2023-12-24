2b28d764ff69661b04b0c3da1f953b3b49df1875
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:03:37 +0000

    pure error under reduce talkback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
