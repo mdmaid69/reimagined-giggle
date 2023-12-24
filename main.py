8e3018fa2e32ae78a9f8f25d5cf763205dc1d232
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:28:25 +0000

    bring knock correction slice comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import getpass
+def get_username():
+        return getpass.getuser()
