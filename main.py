6f66aa62d876351743260a19a0640fed83a63e90
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:30:30 +0000

    lookup job reach sight typical

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_list(array):
+        return list(array)
