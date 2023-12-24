80da1da3ccfed5d99c3f63dfb2949c26a3224ade
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:39:25 +0000

    from face hurry home instant

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
