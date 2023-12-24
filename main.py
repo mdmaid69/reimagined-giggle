91d35ac5839d57355cf00be058563f0b1fc5f14a
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:09:58 +0000

    since brief little accidentally where

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def get_array_itemsize(array):
+        return array.itemsize
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
