4e5423547afe006f17b7197b3cd71e0042536a96
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:58:30 +0000

    think world genuine in gradually

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import tensorflow as tf
+print(tf.__version__)
