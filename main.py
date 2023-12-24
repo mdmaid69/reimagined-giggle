a1d04792e342f0e8582a97ab79653d6d5b62970b
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:01:16 +0000

    snow black workupto uniform disturbing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
