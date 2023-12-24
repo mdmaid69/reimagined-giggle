add99f489edfa56262a9dae5e864c63b4711dd69
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:04:12 +0000

    comeup long cost offend partner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
