d647b9e84bc304f703854fe72596d34a20f25d41
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:21:28 +0000

    forever culture rid vision factory

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
