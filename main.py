3eb1ece6ac5ffec5085ee3bb84ba1dd35801beab
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:23:17 +0000

    show time marry future mother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def count_characters(sentence):
+        return len(sentence)
