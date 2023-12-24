5818d201a0074857ebec20ee88014ff024ce97b5
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:47:38 +0000

    sleep bringforward act suppose shoot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
