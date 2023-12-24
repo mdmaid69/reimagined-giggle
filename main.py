acf8ea22bb9383cb6dac8844e75ba0038c7e65e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:30:36 +0000

    tight crazy embarrassed interest count

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def is_even(n):
-        return n % 2 == 0
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
