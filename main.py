4dbecfe0e02b28af129b5e42bb2525a71cbaadb3
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:29:20 +0000

    odd certain quit whichever frightened

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def get_array_as_bytes(array):
+        return bytes(array)
