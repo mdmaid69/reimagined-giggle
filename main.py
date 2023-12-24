599d1313d7be175c1d82db2cd95d8eb20922d0f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:33:20 +0000

    suit care computer hit inform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
