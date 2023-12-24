3c4be3f06102777191cd3a64acc9525f11d72cd2
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:10:41 +0000

    hit back final eat brave

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
