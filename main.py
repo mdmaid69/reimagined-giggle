e3dcb8a220a97970cf5454bf82f8d23d1f511dad
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:26:13 +0000

    little ocean swimming attention rid

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
