3da133945418f8ed8100a30a5bc083fbe4364f41
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:33:55 +0000

    come exam will people chief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_square_root(x):
+        return math.sqrt(x)
