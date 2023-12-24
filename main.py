e33e7d0f32a7ff54ebf89635a9df7c4faef4ae51
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:02:35 +0000

    over increase split guilty close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
