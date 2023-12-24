2f3c1d674a282e87042fc5862b88734754112346
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:02:12 +0000

    connected dance thirsty conversation much

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
