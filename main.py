f6c8016625dad385b94f48cb79ecf9fcff7fe651
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:20:40 +0000

    dance spring clean extremely uponon

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
