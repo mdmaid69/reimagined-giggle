ac802697111a3d66b315f89856f1a8135f732e12
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:13:30 +0000

    cause punishment feeling perform invitation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def is_odd(n):
+        return n % 2 != 0
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
