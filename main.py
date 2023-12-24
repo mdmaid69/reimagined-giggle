01530d5b718f45264020761083c95193a3513fe6
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:29:43 +0000

    total straight calloff part punish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
