08a70f4244896f0851a47d398bc18b81050fd87a
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:34:59 +0000

    land port trouble cause defeat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
