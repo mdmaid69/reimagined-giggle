459651572615880c50206c4acbc36c37496d0613
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:09:04 +0000

    behave cruel new concerned declaration

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
