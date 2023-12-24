210f0ed75a4757789985bb034ffb7f4423957c00
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:56:24 +0000

    Tt serious strike before fortunately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
