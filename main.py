904ad7b000b4ff8ba6be20b38b9843bd29ff8263
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:51:44 +0000

    exit or challenge personal press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
