997a4ad26a535bb245c8912270f2f8ec26f2bd66
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:04:39 +0000

    callup physical needle demand drop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
