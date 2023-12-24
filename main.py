9aacafd0087f101346d366daa8431bd6e2145ce9
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:13:02 +0000

    if among call explanation present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_as_bytes(array):
+        return bytes(array)
+def reverse_string(s):
+        return s[::-1]
