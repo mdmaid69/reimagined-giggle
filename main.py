93c49f0676e83d3238e341426d59b52f3de59580
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:21:24 +0000

    ourselves cost communication answer sensitive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
