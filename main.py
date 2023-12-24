c0f7c28a6975125e269d29904710cfd1629e35f1
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:28:43 +0000

    plant calloff unhappy fierce plate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def split_path(path):
+        return os.path.split(path)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
