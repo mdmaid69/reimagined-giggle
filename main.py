7d4dacbffaf787e4c45616bee2adf1f3c77d9a5f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:08:22 +0000

    insist slowly us some impatient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_set(array):
+        return set(array)
