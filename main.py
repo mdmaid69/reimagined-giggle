3787373129baee6a419e1936926b3941983c184b
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:09:41 +0000

    model goup forward means splash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
