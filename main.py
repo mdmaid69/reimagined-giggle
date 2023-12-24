a8f3b455d478ce8bc873c28339ef362fdfa68c94
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:35:44 +0000

    stop difficulty noise emphasize prize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
