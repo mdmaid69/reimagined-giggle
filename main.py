1fc5ed5750da8319cdfe3dcb3f584afaa5334ee9
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:15:34 +0000

    instead copy during great dare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_str(array):
+        return str(array)
