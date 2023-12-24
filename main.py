41586184221dec7bd270a0763ebb9bf36811495b
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:45:00 +0000

    win born married room influence

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
