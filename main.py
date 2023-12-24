4ee67b23490f511b9afb224aaf7697a6772ea190
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:54:57 +0000

    news between state close distant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-name = "Python"
-print("Hello,", name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
