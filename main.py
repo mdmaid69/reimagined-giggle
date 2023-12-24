f7510baba7ac93bfc5031f79b6cc819c77ed1a2e
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:33:15 +0000

    tap match mind whistle union

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def remove_duplicates(lst):
+        return list(set(lst))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
