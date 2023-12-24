894045aae16e590b50c7515e7532589af788e84d
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:09:08 +0000

    proud dear him repeat crazy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
