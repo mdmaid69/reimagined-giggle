9012a6b9ae756bd93f0c2e9b3278b3ca41e74ed9
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:51:02 +0000

    petrol together relative reserve own

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import array
+def append_to_array(array, item):
+        array.append(item)
