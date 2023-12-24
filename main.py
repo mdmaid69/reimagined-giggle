28461c581508a6f5055672b2dc3810f824beaafa
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:14:53 +0000

    professional usually quick used happen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
