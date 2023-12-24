c443adfc6a29d0bba6ac9148a56cd699891e3132
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:29:49 +0000

    whichever sense strict shadow follow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  def is_even(n):
+        return n % 2 == 0
