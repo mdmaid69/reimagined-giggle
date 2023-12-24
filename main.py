e6ffa30798643e421d2ce8cd1b4033d72871db32
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:33:11 +0000

    Jj generally approval range notice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import tensorflow as tf
-print(tf.__version__)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
