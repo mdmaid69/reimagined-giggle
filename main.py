8f435c389d8938e6460f9796af969f52c7c5ce6a
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:12:20 +0000

    tour set beat concern cook

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def print_python_version():
-        return sys.version
-import datetime
-print(datetime.datetime.now())
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
