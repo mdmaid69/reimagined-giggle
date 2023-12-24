8c08cfa92f5b2cbbbeedcf6f86f036a126d8c41c
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:20:20 +0000

    situation transport day confused record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
