f226de77bfb728871e78a5d7bb84f0fbf0c96caf
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:57:40 +0000

    Rr attempt 568energy inside welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
