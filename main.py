a7c8d6e82683892ab8bd31c0430dbf262daffafb
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:39:59 +0000

    suffer climb push whistle play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
