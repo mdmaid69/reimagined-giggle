1359579934c29322938538e5237d480596f5e41a
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:02:51 +0000

    chat leave step convincing as

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
