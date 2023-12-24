3b8191a22c9276c40e69d3f21fd1c2a494a5e52f
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:10:28 +0000

    book song oil match exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
