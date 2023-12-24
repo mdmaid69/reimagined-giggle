ec484bf23a3f522549b328dafe20d1d4dab8bc3f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:14:30 +0000

    there stage situation compete sore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def reverse_list(lst):
+        return lst[::-1]
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
