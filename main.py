bd22a33397edb7fba744cf41893e65516c1ea6c5
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:44:19 +0000

    squash dream getoff comeoff active

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
