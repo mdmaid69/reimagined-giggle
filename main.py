56385a45b7ebe484b921c343a93871fd598e6ee5
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:12:09 +0000

    software regard sound no pair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_speed(distance, time):
+        return distance / time
