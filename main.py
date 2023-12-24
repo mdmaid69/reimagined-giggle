4165a4b2253e8ef9f806b73dcdcfdc78eac3e41c
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:34:57 +0000

    disapprove continue water towards plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
