dafe4cdb1adbcfae1290a69243bad01a90ed7faa
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:00:40 +0000

    separate takeout pretty weather mark

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
