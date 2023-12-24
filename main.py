5735a6272aa59470100d8bf04b930a492d252d14
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:47:56 +0000

    contact plan birth wet chat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
