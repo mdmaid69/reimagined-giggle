39ab6fe4773ef5d8fcac111ca71dbd4e42cb3207
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:20:26 +0000

    concern exaggerate whisper just interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
