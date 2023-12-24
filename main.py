6fb4b6c2e33a3d9c2177f80cf029f0e5077f908b
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:47:09 +0000

    account drink advice yourself doubt

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def count_characters(sentence):
-        return len(sentence)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
