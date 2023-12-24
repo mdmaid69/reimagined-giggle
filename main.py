36e9c6015bfa70af206404330ee5acdd0714fc47
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:18:28 +0000

    never giveup plate teach deliberate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_as_bool(array):
+        return bool(array)
