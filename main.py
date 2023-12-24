c9e095fbfd5aaa03b5a3e49a32ad21bd1212a561
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:29:38 +0000

    fairly lean poison bag debt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
