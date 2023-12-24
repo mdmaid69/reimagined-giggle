4ce5338dedd1f5eb1654a1ec63e404e30966cbce
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:40:32 +0000

    Yy peaceful feel trap suppose

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
