e811284c55c72b57886578669403b880c2dfceea
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:18:16 +0000

    tip like common condition muscle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import sys
+def print_python_version():
+        print(sys.version)
