759633bc33c9ea232be5f8ca6cf77e22a60b8aa2
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:40:54 +0000

    feature bus house slide air

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+name = "Python"
+print("Hello,", name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
