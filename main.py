2167c795f23fb9be9ab3f0318d38a9c9b61045e4
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:50:49 +0000

    central mouth quickly last love

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def convert_to_hex(n):
-        return hex(n)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
