2ebe129e6514be67b4d26274d9ab8bd13472efbd
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:24:32 +0000

    plant against attract somebody contrast

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_density(mass, volume):
+        return mass / volume
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
