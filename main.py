282937c0ea60cdf73ab2931f4ccd88cedc05b1f9
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:35:53 +0000

    importance cum unusual tax open

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_min(numbers):
-        return min(numbers)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+def change_working_directory(path):
+        os.chdir(path)
