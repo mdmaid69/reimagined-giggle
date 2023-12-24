8985d1f94535937733af25e1b5877263899c6cc2
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:18:36 +0000

    still Bb noise carry hole

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
