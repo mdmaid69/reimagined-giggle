bd239e65750a3e128699d808d129a7eff51196a1
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:10:23 +0000

    wipe correct legal ball jump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
