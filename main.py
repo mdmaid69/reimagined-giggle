eba7d5810b658127f2e637a891daa5e0e107e7bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:10:23 +0000

    clear play noise path rest

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
