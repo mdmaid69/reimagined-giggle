cd211a44409fe63640cce3b024dc7f3573368519
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:26:08 +0000

    centre transparent demonstrate blow day

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Words:", len(text.split()))
