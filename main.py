fa943410aaaf22ee6b195e0eeeeeb5ce15190c6f
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:43:55 +0000

    goodbye possibly strike trick his

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_queue():
+        return collections.deque()
