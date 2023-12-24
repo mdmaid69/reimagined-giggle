5a62b3f01825c87eb8b4367eb7e88414cdc78bee
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:09:37 +0000

    trust season bridge run late

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
