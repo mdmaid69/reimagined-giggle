136ca33f3beca6534f254c7e0274f7233516e37e
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:03:05 +0000

    mainly closed away interruption obvious

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_perpetuity(payment, rate):
+        return payment / rate
