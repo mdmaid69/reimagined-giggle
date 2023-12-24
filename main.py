d5743b5031a6e4e77d6750839a6afeec77e2c186
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:02:18 +0000

    hurt out moment treatment set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
