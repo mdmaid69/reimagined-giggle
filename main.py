df7ef46b812dfb86d51038174264ce2cf7ce8810
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:39:24 +0000

    can keepout nose quick conversation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
