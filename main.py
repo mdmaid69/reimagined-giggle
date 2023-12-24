b5fc5b2a09a50feb8cb131bc8435fa1131ceb943
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:57:25 +0000

    save line pause unless pair

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
