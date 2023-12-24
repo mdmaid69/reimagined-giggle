65cbe9994d8698ec7cba5f2525c2aaa6f7779a40
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:23:54 +0000

    operation pin sell for estimate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-for i in range(10): print(i)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
