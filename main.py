a9f5d955b99f5574f34c8e2ef92259dc0c2e1b09
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:35:26 +0000

    such defend smell intelligent enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
