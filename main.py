b19028eced85a7154e23f2fe357a545b0759e518
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:47:35 +0000

    impressive mysterious base estimate stay

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import time
+def get_current_time():
+        return time.time()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
