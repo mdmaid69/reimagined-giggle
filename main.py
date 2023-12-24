797251a0f8ba1cc8ce7f7c4df7dfb11a918b03ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:51:49 +0000

    pressure yes lesson taste tired

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
