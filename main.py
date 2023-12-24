1643936fa63b6c6b4d53b9cb989174b5f22b915d
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:34:22 +0000

    fair mark less cook buy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
