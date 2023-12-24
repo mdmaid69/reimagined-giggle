38f8770ad069d70e4bb73a1c68f2e4375f1423b1
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:51:20 +0000

    shrink Tt there may medium

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
