1efd5f8a08d5c1d597c6bfd6ca1273d530b0d358
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:42:47 +0000

    abroad wide pure dangerous improvement

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def divide_numbers(x, y):
+        return x / y
