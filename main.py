4877c96b00e9e31c6c08869d8ec5b170486c4b93
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:05:30 +0000

    poor increase central song past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
