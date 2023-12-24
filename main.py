c8107066557006fa4afd8dd09a140779a215704b
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:51:05 +0000

    explain active language section tear

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
