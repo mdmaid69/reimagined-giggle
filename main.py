eb07caa25903bf1686caf9fb0bebb3f109b42b65
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:26:58 +0000

    impression change lookback(on) eye recently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
