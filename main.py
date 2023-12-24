22c9af7df37e0cd8c546e11293b016ade3d5cfb5
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:26:27 +0000

    wind rubbish takeover present crash

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
