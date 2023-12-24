e4ef1b7a014b9ec3b83ed044b201b2dfb66f0275
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:56:02 +0000

    work mistake roll remove bright

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
