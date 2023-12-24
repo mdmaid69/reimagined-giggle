3f0c403059d9c9a5449f279f4ed26da7c52eb19d
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:48:10 +0000

    claim thankyou not confuse experiment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def get_array_as_list(array):
-        return list(array)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  def sort_list(lst):
+        return sorted(lst)
