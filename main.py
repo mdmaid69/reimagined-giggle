00ee953f32687e421adb6330209bb9c678ae0cd4
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:42:03 +0000

    joke position Ss prove unfortunate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_acceleration(speed, time):
+        return speed / time
