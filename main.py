808e8a275ef35a020e8b8096e78b8ccc1e523add
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:44:31 +0000

    pull however reject tonight other

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
