401b280316798d1f756a1fa73ad8a85fc723a1a2
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:28:59 +0000

    flood name lonely war acceptable

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def sort_list(lst):
+        return sorted(lst)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
