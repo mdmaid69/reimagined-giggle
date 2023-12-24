801077d1c937f691a21c07ecbe1640378c0fa227
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:04:23 +0000

    none ago cruelty skill refuse

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def get_array_as_str(array):
-        return str(array)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def sort_numbers(numbers):
+        return sorted(numbers)
