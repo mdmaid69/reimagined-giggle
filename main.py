0a7a741336baa583368c7f9ede7ed1bc59265fd0
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:40:18 +0000

    serious imagine border build big

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import platform
+def get_python_version():
+        return platform.python_version()
