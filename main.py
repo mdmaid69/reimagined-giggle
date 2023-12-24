79f61576210ea682f64782fb404c16145842a7ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:01:27 +0000

    blow naturally involve trade everything

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
