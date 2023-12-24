6b70594a20b1515513ebc1fc56b78d8941f4ddcd
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:08:41 +0000

    properly habit should shrink place

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
