b815817e0888eed4d82f29644db346423a51798d
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:58:48 +0000

    wander apologizeapologise shake battery complicated

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import os
-print(os.getcwd())
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
