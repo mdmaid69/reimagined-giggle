674185217d1a378073662a27a7212ae29d153c5c
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:09:17 +0000

    pride possible bang street getaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
