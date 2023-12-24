a1a3e811bb4003e813f33ddbc29cc73bcde449f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:58:14 +0000

    limit collect wild cheap deep

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def find_min(lst):
+        return min(lst)
