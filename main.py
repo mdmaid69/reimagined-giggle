7fc7302cbc76bf96b46d99a7a2621a815379a4e8
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:30:21 +0000

    week label scared hello damage

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 n = 10
 print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  def reverse_list(lst):
-        return lst[::-1]
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
