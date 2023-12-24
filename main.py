6fc33103144298d466d72f3c5e82f1b96a5b9e8a
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:36:18 +0000

    remember repair square one from

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-def get_current_time():
-        return time.ctime()
-import datetime
-print(datetime.datetime.now())
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import numpy as np
+print(np.array([1, 2, 3]))
