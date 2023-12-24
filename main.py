f2894816e3804c39a2c0f821f041c3fd73a1cfed
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:38:28 +0000

    human field estimate one afternoon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def find_min(numbers):
+        return min(numbers)
