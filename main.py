5f73a3ce48d99a1d639ba48bd22250076fcc19e6
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:57:52 +0000

    slip moral dress record everywhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
