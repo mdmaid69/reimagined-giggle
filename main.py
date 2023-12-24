912d4ddb3db6b393d68b1488ff9a0b1cd9db4ad8
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:50:27 +0000

    advise round fix pride piece

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
