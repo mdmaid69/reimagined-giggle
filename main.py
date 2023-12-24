5a6f15370dbf3241fb856b83962d6fbad832016a
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:28:42 +0000

    spray allow worst however clearly

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import re
+print(re.match("h.*o", "hello world"))
