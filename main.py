b4f7fed22cefb31f0421aae1be16107433edfafc
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:31:42 +0000

    callback disturb century autumn term

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
