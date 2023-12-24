e065129c97f4fc8b392fd0ef4d3d7655fb83fd41
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:22:40 +0000

    ill print feed judge appear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def append_to_array(array, item):
+        array.append(item)
+def divide_numbers(x, y):
+        return x / y
