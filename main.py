d7697335e2a2d265dd59bb4ac1c2e41378a66e53
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:33:05 +0000

    lookaround Oo teach minor mine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
