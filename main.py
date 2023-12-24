dd75027c555b598dd1ef855a018d71a971df0737
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:06:20 +0000

    water break air actually brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
