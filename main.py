9028be5f6a084efbfa66682a9b351ccd53cb2529
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:21:26 +0000

    term start sight eager I

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_str(array):
+        return str(array)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
