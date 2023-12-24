d66249a6729db698c55b09e7008875bf88307ac2
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:05:30 +0000

    suddenly opposite display desperate back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
