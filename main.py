b279ef5ce023a076e81eba3957606dd7dbe243e2
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:30:57 +0000

    usual find leg scene guard

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def convert_to_binary(n):
+        return bin(n)
