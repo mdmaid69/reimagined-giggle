de006461530b23c048205247d03ca6b4e2295850
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:22:48 +0000

    equipment record on pencil bringup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
