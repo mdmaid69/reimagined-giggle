dcb031e243f27b3a1639b638b910f7d6bb1dd8ea
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:58:00 +0000

    achieve always stupid form mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
