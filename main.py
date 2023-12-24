310e8bac8104c56fa731249686fbddd29921ebae
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:07:48 +0000

    side welcome share pool moral

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
