7385189b37d853118893502415e0070a2354acf8
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:14:47 +0000

    Kk nose comeup slip spot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
