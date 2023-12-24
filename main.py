546b49c84b05321831efea7b03c3605062ad29d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:52:05 +0000

    wish hurt get sort flow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
