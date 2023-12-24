ada6671c899ebc16a1c4ca881f3ca4aa6c87e65e
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:41:33 +0000

    right loan responsibility advice name

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def reverse_array(array):
+        array.reverse()
+def find_min(lst):
+        return min(lst)
