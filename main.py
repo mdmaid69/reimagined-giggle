be46ee1450fcf223f5c6a3d145ddfed11c447293
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:36:41 +0000

    back secret crowded long wash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
