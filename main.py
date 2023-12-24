c84712736d47ef9557ef714cc37a862842eda6af
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:00:30 +0000

    shade touch once claim basic

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
