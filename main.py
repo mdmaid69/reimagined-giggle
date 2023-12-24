51387517b53747f0e8f122c86b246f4e2ba1fb0b
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:16:55 +0000

    test minor label makeinto wrap

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
