388487a378220b87b04cba70ce4a018dfbe5497d
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:33:47 +0000

    still team now real 179bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys
-def print_python_version():
-        print(sys.version)
-import collections
-def create_user_list():
-        return collections.UserList()
+def exit_program():
+        sys.exit()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
