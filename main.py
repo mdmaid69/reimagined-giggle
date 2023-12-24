f2f6e7e2de9c7d6808edca33a563a21d46e43d86
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:24:27 +0000

    suckme replace interview visit consult

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import datetime
-print(datetime.datetime.now())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
