b09ece961240ebc0924631a940a5df6b1aba56d5
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:14:07 +0000

    turnoff think film choose rent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
