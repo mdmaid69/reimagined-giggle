ebc3c16a93fb757f16a006e21b92d4a153300bd2
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:17:09 +0000

    whichever bin great force suit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
