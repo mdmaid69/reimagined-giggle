60723a95a9ccd8461e5a441fb884d836559388d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:27:36 +0000

    either contact run course date

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
