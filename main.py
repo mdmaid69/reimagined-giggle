7a407a10955f6828238e5d067885678481011b5c
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:58:04 +0000

    improve estimate talkback bringback soak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def sort_list(lst):
+        return sorted(lst)
+import random
+def roll_die():
+        return random.randint(1, 6)
