211cbf5fc565460411c03bb24c49562359eea7d1
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:26:39 +0000

    excuse charge nurse use make

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
