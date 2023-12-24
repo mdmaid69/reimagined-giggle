23ecaa752e3b6077ca90c4a8faca71b7a5a930e4
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:24:32 +0000

    slide bit hungry morning rate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
