1aa452be863b26d3ef9b0cb1cc48e528292e74d1
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:13:44 +0000

    situation closed sail term since

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
