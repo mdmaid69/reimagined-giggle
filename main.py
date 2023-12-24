6710879ae3516183cb972640559949d152b8cc3b
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:27:17 +0000

    slice autumn ill Zz copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  def convert_to_binary(n):
-        return bin(n)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
