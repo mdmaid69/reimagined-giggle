f7a9323418498c75df6fcce23e6d8ffe9317bc62
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:09:21 +0000

    building room plate of tear

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
