096ab67104a45876d488f9785ce51667d9ba07ba
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:54:19 +0000

    snow concern service affect bringdown

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  def is_odd(n):
-        return n % 2 != 0
+for i in range(5):
+        print(i)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
