b61a531842935abf4d9a713b7f9ed0c16e740b9a
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:36:57 +0000

    century past bet turnaway surprised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
