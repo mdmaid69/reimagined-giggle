d1ebef7e185bfa0f46b92b27da15160a5d37c675
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:42:22 +0000

    who receive own illness to

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import random
-def generate_random_number(start, end):
+  import random
+  def generate_random_number(start, end):
         return random.randint(start, end)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
