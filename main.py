ce3ae0f5911103aa6af13073e925f8595ad5a4bd
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:55:52 +0000

    issue conversation loan impression right

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
