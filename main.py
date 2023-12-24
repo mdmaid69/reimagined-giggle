ef2a84068c46bdc936deed8dea2e49dcb95aa7fc
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:05:47 +0000

    importance appear match ring foreign

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def cube_number(x):
+        return x**3
