7cf18fb54042f2ac6b39a33b41bb5bb3bc4a0aaa
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:16:40 +0000

    disappear boat wing open ring

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("Hello, world!")
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
