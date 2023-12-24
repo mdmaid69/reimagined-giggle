1b28bb6d9c6ea0594a40d78f0b91c34c78a5a639
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:57:49 +0000

    meet steer city copy smell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def convert_to_hex(n):
+        return hex(n)
+x = 10
+y = 20
+print("Sum:", x + y)
