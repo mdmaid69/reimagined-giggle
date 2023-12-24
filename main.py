0b92a3ac31b9f5f402ce7dd9de38e23d8971c0cb
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:31:49 +0000

    emotion poor either thought under

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
