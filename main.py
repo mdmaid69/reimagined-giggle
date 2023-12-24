95d88806f92881a71e1d86369b941e865225f960
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:18:55 +0000

    round warn shallow foreign definite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+print("Difference:", set(list1) - set(list2))
+def reverse_list(lst):
+        return lst[::-1]
