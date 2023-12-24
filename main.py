8072f252f0335527ea40e91d95cd959741330715
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:23:06 +0000

    exhibition sorry tall serious expect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def subtract_numbers(x, y):
+        return x - y
