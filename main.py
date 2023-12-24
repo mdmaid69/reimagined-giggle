7ac6bd01cb90b5d15bd55d4060d263d4674b84bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:43:00 +0000

    calmly draw of success base

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
