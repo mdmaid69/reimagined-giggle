aac1c6817a4f509a353f3f862e5da2186b45c94a
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:46:28 +0000

    create will pour borrow which

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+def find_min(numbers):
+        return min(numbers)
