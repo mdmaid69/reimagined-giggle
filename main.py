439b3f0ce4d5ecace9fbbdd161a7bd117855c681
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:57:52 +0000

    means fail risk shopping plant

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import os
+def get_current_working_directory():
+        return os.getcwd()
