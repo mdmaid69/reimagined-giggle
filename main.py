7b7aa643f485a9946685ae4a992a19098a5af523
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:48:04 +0000

    discovery turndown complain strike adjective

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
