ebd4bfff958dfcdc5d44e39ea7030c141f8ed630
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:52:36 +0000

    identify doubt suit attitude hers

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
