ccc791b4cdaf0bf7580e915c7cc374ab987ef743
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:05:02 +0000

    science obtain pipe protest secret

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
