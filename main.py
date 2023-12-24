5c177cc02c33e0d434228be6a8503290fbd917b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:17:51 +0000

    each guess spend aware 564encourage

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
