604818f284b927565564fdd6c650a898c6cd2186
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:06:28 +0000

    family deaf except arrange real

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
