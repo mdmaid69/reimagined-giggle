3ab26d5b1a1d99715459c1c1ddcedd058c1a76b7
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:56:54 +0000

    show relationship miss or everyone

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
