f5cfd00c0448a3457dee35c89e2f7bbcad0e1bd6
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:03:05 +0000

    plan present fire legal female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
