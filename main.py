477772dd25fa14b380dacce35bda216988bb5b53
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:34:17 +0000

    since present Cc trip kill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def get_array_typecode(array):
-        return array.typecode
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
