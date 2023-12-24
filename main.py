ea26849ba959862bd6ec0054bd323d36514e27ec
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:05:18 +0000

    present straight pay country funny

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
