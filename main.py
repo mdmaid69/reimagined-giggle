a3e57da8c1ce0c25b6c03d3910548ca77df10803
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:48:34 +0000

    score cruelty runout future contact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def count_elements(lst):
+        return len(lst)
