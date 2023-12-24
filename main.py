446e71aa78a971e187f823e45b41573d33ee7568
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:51:26 +0000

    no office laugh mark estimate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def cube_number(x):
-        return x**3
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
