d8f15c43f3a169bc78f43dc52a2b17d3ee50fdf8
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:26:00 +0000

    unhappy list send necessary emphasize

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
