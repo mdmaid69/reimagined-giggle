9d50a270dca6c78f1ed135e059682d504ed07845
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:51:16 +0000

    remove study frighten practical track

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
