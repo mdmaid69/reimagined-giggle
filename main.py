6ca65e4a1c2436db144a12f140e6d2bade95fdfa
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:13:24 +0000

    hungry breathe march regular frighten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
