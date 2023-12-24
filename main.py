e93bace8569c3f2bba076edbff46dcb59c4aa454
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:59:41 +0000

    handle frighten custom her freedom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
