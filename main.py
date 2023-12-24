3ea320d2631fb301d338155670d07f0f422342ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:44:18 +0000

    swimming basic thinkthrough instruction storm

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
