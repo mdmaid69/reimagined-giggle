f827b75588291e6bf1b94af9a0b339d8fc005284
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:38:17 +0000

    pity port once study unable

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_area(radius):
-        return 3.14 * radius * radius
+print("Hello, world!")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
