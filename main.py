5d5164b14bc7560a8f2df0cf9bbbce6609dfbca0
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:07:35 +0000

    she himself shade result confidence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def count_characters(sentence):
+        return len(sentence)
