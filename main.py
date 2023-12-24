ea8119a6f37099d8383dd63ba9f810fde889990f
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:07:21 +0000

    cut bush upwards pool place

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
