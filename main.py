48c32ac44ae788f84f6baaae7b48cacb98a674bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:32:15 +0000

    entrance home charge journey almost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
