24267712ba6df1ea01e37e2a3aaa4845413fbc90
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:07:48 +0000

    scheme window work market peace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
