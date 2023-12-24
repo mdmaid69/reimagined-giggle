252dbdd5543201a2796aac959200e4014fbd9d41
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:01:04 +0000

    opposite appear coat sort answer

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
