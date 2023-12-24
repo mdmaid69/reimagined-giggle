7565bc05ed5f72b8abee46a2140f0d8cbe6d6130
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:36:22 +0000

    correction guard sink charge murder

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def count_words(sentence):
+        return len(sentence.split())
