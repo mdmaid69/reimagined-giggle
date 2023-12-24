e3ede9a045a0f6f8b80fbcd6b9d5d19b423ccaf1
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:43:08 +0000

    slip criticize young ear length

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
