942d68df72cdc93901efcbb0daf51787fbf70156
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:44:09 +0000

    container stare late demonstrate Yy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
