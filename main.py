ce9a7c6a48d5a8465e9427febd6ebc2d0cd594dd
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:32:48 +0000

    shopping factory why common government

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
