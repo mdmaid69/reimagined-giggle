7fd92d9cbe7c4e665326ed839448ec71425d5145
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:31:31 +0000

    week exercise offence insist memory

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_speed(distance, time):
+        return distance / time
+for i in range(10): print(i)
