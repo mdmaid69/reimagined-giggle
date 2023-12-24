225f339fc925b77f458a367ef9213345503a0b25
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:05:00 +0000

    friendly back fill it will

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def cube_number(x):
-        return x**3
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
