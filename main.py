87e821b494b20b912a15eebed00e683b734c5f5c
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:24:00 +0000

    climb disturb out contents television

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
