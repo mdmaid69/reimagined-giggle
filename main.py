a52494a35116dcc53810f9bcd7c6af0611eceebc
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:52:30 +0000

    mood majority period well low

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_power(work, time):
-        return work / time
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
