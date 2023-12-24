aeb9fcfdd3b0a81a3a5d43e77af5d2debe2c8c3f
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:22:08 +0000

    difficulty major like fact race

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
