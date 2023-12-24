b5446e279696f39c17714eeb29d9c75710732b75
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:17:32 +0000

    cruelty very wish means politics

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_pressure(force, area):
+        return force / area
