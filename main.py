f52fdc03994c6cb3f0b252d9e62df5b1f3c91d15
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:41:01 +0000

    crash turn explain serious boring

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  def is_even(n):
-        return n % 2 == 0
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_power(work, time):
+        return work / time
