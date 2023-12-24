9fdaac596f66a58cc66dfd7639e9704e6afb5dae
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:36:46 +0000

    low easy building design project

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def add_numbers(x, y):
+        return x + y
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
