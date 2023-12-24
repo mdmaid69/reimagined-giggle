49b5670a0a7dbb60b5c082512dcef4b97879b3e5
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:22:15 +0000

    better spin will mistake tough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
