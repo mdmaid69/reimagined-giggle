922649ffb65257403feaa21885252ebec1e3d68e
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:26:28 +0000

    flavour experiment use set crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
