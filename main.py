f313aa2ec45879204b2580e5f80b54a0eebe88af
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:46:28 +0000

    bright cold discuss book clever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def remove_directory(path):
+        os.rmdir(path)
