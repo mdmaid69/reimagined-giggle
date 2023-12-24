138ad06d99bc42fe400de0270ff7849ee0c96055
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:48:29 +0000

    cap drop sea week whom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sys
+def print_python_version():
+        return sys.version
