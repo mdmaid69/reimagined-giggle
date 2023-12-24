b9c5fd896680ba6f2116344eb5cfb50d5bac0c12
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:46:56 +0000

    wind blue broken it never

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
