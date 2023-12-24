6dc7d7a6a24aaf94bf0c8b1dd1816b84aafda90b
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:36:37 +0000

    thirsty switch burst intend lookup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
