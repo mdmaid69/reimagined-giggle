1574f99f6051a0b48ac0b20603d56e2420158743
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:19:07 +0000

    woman goaway sea stupid improve

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
