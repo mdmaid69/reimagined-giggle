fff5d9e0ec7d087fe03079e51d6caa8f940659f2
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:20:14 +0000

    land tear breakthrough godown far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
