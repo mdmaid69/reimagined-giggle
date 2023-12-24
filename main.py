0be975b8bea84162c7d77fc012ac3f497c3f9297
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:02:20 +0000

    glad charge single muscle teach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
