a5db2acfd2e5371124dd0fa121e8fed72d12f3e2
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:39:32 +0000

    567enemy film occur breakfast follow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def find_max(lst):
+        return max(lst)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
