a96fe9bc7c0bae60d51aad47faa21f9fb0fc0428
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:49:56 +0000

    bush anywhere sensible address disappear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
