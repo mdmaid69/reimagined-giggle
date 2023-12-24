3934f2cc091ceded23c755c7e2b745bbebe71a7f
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:16:43 +0000

    thinkof workout nurse right ability

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def find_min(numbers):
+        return min(numbers)
+  def sort_list(lst):
+        return sorted(lst)
