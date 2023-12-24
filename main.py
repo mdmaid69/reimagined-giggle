5e5ed061a31982a51de60ea39ff94c4dc005cbea
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:16:05 +0000

    illness damp move walk war

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
