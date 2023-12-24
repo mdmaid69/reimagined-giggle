ed45a3b16f5bd9b5e927181a2034ad2fcc81302f
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:18:22 +0000

    bath cry ear general stir

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
