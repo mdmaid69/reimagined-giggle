dabf5eb72b065d10b854b0fc218863c22209de84
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:16:22 +0000

    brain chief extremely nonsense emotion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def get_array_length(array):
+        return len(array)
