3fe699bf310b5ea8112cdc74d69b7140149772b0
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:41:44 +0000

    affect mess house whether slice

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
