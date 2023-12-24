088ab1bfa36de64c10dbfa6a019fca097ffb3f1a
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:44:21 +0000

    wish moral smile Nn medium

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
