abc79df9cd26c1bf7e63123a66491e69a69af273
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:32:33 +0000

    leg like splash might shadow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
