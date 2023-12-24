c4e63ac4f89486cfe774e87d2c6d301524209dbc
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:07:11 +0000

    fortunate boil team target rule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def get_array_length(array):
+        return len(array)
+import os
+def remove_directory(path):
+        os.rmdir(path)
