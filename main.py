2343d8192e2a03acb040a19414e0ea9cd9fee6d6
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:48:27 +0000

    stay yours flat meaning evening

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
