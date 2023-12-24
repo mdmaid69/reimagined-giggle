50cfdca1053ee1b1451f3de84344dc279a8b8616
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:52:07 +0000

    shape beside very offend emphasize

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
