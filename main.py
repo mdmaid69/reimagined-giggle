8699d150f52f09ed4f8f4b1f65ca6828d4784f2f
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:48:58 +0000

    contrast wish might cruel authority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import sys
+def exit_program():
+        sys.exit()
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def extend_array(array, iterable):
+        array.extend(iterable)
