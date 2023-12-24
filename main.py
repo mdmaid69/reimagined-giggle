43d998fc5ee4bc42aeeb7565dc4dd3971d639cf4
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:01:23 +0000

    ignore involve meal squash university

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
