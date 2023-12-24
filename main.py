6f49ae76c4b55ac22864bc31d1ce93a1c81e6762
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:58:48 +0000

    hide learn violence decide phone

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def clear_array(array):
+        array *= 0
+for i in range(5):
+        print(i)
