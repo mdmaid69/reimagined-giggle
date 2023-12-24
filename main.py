b03d246f22fa00df1ab9bf2af38b4604e4ed9bae
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:46:40 +0000

    during opposite roll wrong small

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
