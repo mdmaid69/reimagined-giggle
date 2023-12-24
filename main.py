6e2b3455611cd42f74be4f4d07ab597e8ef07f16
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:15:41 +0000

    appeal terrible worth busy anywhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
