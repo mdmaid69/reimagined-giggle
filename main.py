49f40b0621541f50ce96cd07745e0d94e0ec82c9
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:29:40 +0000

    platform contents imagine comeout floor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
