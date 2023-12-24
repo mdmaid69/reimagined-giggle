6bb5fe095325f8e65f3a1f7834a352cbdd73b771
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:38:01 +0000

    lunch very quarter some hard

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+print("Hello, world!")
