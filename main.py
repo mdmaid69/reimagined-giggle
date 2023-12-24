ffbf9756f527837d73ca9b8a3a968b63b9f7aea1
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:50:51 +0000

    interview declare smoke thought powerful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import array
+def get_array_item(array, i):
+        return array[i]
