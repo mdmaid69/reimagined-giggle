4ba28ed53f3fdaad381232dd13d98bbf75040467
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:20:54 +0000

    shrink somehow yourself want friendly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def reverse_string(s):
+        return s[::-1]
