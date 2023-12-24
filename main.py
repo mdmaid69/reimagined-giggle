ba89f13450121b33223fcd36626302a8222ec1e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:56:31 +0000

    worst knock news get advice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
