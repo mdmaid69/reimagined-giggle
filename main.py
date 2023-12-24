61a97a15a3fc8d58326427b37f59377a6eb7314d
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:16:50 +0000

    depend sure finally Jj answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
