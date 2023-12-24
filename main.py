7ac2c081c381e88eb03b3c70e754673da98cc9c5
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:34:31 +0000

    undo takeoff breath first fierce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
