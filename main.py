90171987858d27b4802926bf4ba616ebb6ba3924
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:19:25 +0000

    hurry waste fix by confidence

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def convert_to_hex(n):
-        return hex(n)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def convert_array_to_string(array):
+        return array.tostring()
