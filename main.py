28e661fbd8f382608919bfb16bb39baff12097ae
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:40:24 +0000

    store possibility summer lead sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
