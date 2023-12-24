80ea333a6817e5b152c2243e4e98a730fa5172e9
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:10:58 +0000

    shop law experiment place squash

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def subtract_numbers(x, y):
-        return x - y
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
