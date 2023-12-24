a5ee9a14e944d76c37046de23cc1cb65df4e4080
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:42:33 +0000

    emphasis stay no immediate deserve

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
