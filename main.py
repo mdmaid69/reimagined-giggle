97b342fdc04dc3f9e3cd9f903f0801b163efb0bd
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:09:58 +0000

    godown shoot police getoff runinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
