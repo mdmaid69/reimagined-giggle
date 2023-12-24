bca4c205f4f1055c23a7145f51ba22d0dff05b48
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:32:35 +0000

    wing whistle trial attempt get

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_set(array):
-        return set(array)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
