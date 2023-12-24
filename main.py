6f137885e35480bbbce398883f8aeb7325bfea2a
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:11:35 +0000

    market split describe announce want

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
