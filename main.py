ea1c90374f8b2da87618461d4be53c884cb16198
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:31:29 +0000

    fork indeed arm opposite brain

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  def convert_to_binary(n):
-        return bin(n)
+def reverse_list(lst):
+        return lst[::-1]
+import collections
+def create_user_list():
+        return collections.UserList()
