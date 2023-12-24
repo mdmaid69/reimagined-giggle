ce6b2a52f09f87092ae7ba9c7141d269f72b0005
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:32:08 +0000

    minority call apart vision laugh

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def get_array_as_str(array):
+        return str(array)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
