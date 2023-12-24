c71ccdf89ca4c7e366a44daf213dfcf937a4ef9e
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:07:47 +0000

    defend completely list intend collection

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-print([x**2 for x in range(10)])
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_acceleration(speed, time):
+        return speed / time
