c83d384c5d3f90892d4f559d6bbddaba48c5c1bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:31:26 +0000

    awake still exchange show fit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
