503976a385b988def9699640fd01796b3409c4be
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:50:04 +0000

    comefrom some takeout share successful

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import getpass
+def get_username():
+        return getpass.getuser()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
