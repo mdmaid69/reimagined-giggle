f1aa9d887f04242d0247e9b541ed94153ba93180
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:28:39 +0000

    light ourselves likely separate repeat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
