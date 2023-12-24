d3848d0a70eb88090292e1e6fa5461714105ebd6
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:03:47 +0000

    everyone mix once lot how

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
