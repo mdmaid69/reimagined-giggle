865863126e5fd8c8aff974ddee4b743763833fd7
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:08:16 +0000

    ought connected march rescue violence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+for i in range(5):
+        print(i)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
