c45a8ee2937e7d477c5d4765d8b75db141bdd4b0
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:50:02 +0000

    comeout conversation 571enough top spoil

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
