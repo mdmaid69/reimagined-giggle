0fdede0f9536b911ee5993dad94c3a0b09b647c7
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:14:04 +0000

    forgive school top smile mend

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
