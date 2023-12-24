c2df370bab5abdc7d3835b671433c68dbe86f77f
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:48:41 +0000

    importance study consist shade lonely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+for i in range(10): print(i)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
