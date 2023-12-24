006dd101cd86b4b53a8ba3eccdda56defc820d8c
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:03:38 +0000

    pain disturbing influence hard rise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def sort_numbers(numbers):
+        return sorted(numbers)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
