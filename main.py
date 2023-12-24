04fa5a571e55bef480939639ff6b516e360ff568
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:00:10 +0000

    567enemy Ll separate time tour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
