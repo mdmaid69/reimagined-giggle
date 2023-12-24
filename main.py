493e6d9c1ffce823fd179c9bb928bc5f6f22c74d
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:15:49 +0000

    closed neat motion inform top

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
