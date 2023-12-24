a07f28971c2a0dd6507630a11396f3c3b56c160a
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:49:24 +0000

    tooth back talkoutof judge extra

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def find_max(numbers):
+        return max(numbers)
