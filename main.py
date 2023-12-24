bb4b18c3b6ee6cf15f2548a42ab42ac3521ee328
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:06:00 +0000

    surface beneath sticky turnup 571enough

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
