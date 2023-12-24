cbb7a292745a503ed6028d34298578f0917004fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:01:03 +0000

    tonight sky turn unfair suffer

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import collections
-def create_queue():
-        return collections.deque()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
