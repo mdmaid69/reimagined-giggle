51e1c0bb669eb182d2db7c3314fdde703e1f1829
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:47:41 +0000

    defend fault possibility deserve go

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
