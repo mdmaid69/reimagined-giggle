cdbac61d40721cd73333eb3c022be4ccfd9b1193
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:15:43 +0000

    today fight total plan evidence

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
