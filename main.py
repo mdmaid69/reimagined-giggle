4bff0c60957080844e94b59813b2da70c5219396
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:33:15 +0000

    past stupid power calloff obtain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
