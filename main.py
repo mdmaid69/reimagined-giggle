51d0aa3d21a7b7d677d377feb12d6fcf78c2dbc6
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:17:23 +0000

    warm Gg future tidy close

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import time
+print(time.time())
