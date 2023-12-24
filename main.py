d5c9d81033fa56dd05a1a610950bdfaa1a8bb665
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:22:04 +0000

    university persuade whenever guess warn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
