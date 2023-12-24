eb18dfc42344c1196ed4e76e38468d481f02e54c
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:32:12 +0000

    whether style whichever geton pinch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
