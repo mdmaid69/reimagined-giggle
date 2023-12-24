2814230bd30e120ae136c9dc04ded0ce4b369d33
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:40:11 +0000

    improvement hate under keepaway unit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_acceleration(speed, time):
-        return speed / time
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_pressure(force, area):
+        return force / area
