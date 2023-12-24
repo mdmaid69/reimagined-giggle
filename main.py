4b36cd95dd75542c61aa746af493886f8ba5ed7c
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:36:32 +0000

    mind warning sure way lot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
