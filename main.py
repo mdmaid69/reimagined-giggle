45d1de8e5ed5431f4c9d4d9f974cb93ed95ce86e
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:43:06 +0000

    bother track advice impossible trust

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
