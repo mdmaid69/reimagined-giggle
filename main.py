68e47aa0d8fee54a48b7222b44f5f054fc3915f3
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:56:21 +0000

    doubt love admire ask music

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
