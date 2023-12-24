45ac3e26887752a36fcd04657f4f764d108a0fcd
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:10:47 +0000

    wind find sticky to spin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_repr(array):
+        return repr(array)
