dcbbf0b0e149cc5f20b25e86dbe8e7e3e309a3e2
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:45:26 +0000

    discussion fence urgent keep land

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def convert_to_octal(n):
+        return oct(n)
