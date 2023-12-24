7380f0ac1afc492e5e41bb6c92545cb18f01c676
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:55:43 +0000

    square outside base argument glad

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
 def find_min(numbers):
         return min(numbers)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
