3f73f762b7840ef78f664b950b21495fb5e9903d
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:09:37 +0000

    confidence table switch big combine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
