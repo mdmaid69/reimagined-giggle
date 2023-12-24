6f987398f5b81347d46b9db6f57fcae51d1d57da
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:57:18 +0000

    almost war properly patient track

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import tensorflow as tf
+print(tf.__version__)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
