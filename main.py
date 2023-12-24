b554c2c178fc3ec0af64dd82c69a58e7f291009e
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:52:16 +0000

    around attach wet cross forgive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_pressure(force, area):
-        return force / area
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
