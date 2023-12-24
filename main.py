521f2ce41231a1260e205970c076a8cdd4be7128
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:38:46 +0000

    respect believe minority statement tune

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def append_to_array(array, item):
-        array.append(item)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
