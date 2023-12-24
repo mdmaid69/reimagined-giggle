1924be4607355322b92b97d9b66221d7eef77883
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:55:23 +0000

    above catch allow seat opposite

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
