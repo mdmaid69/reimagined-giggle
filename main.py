a60ea6b958f32e93d8892b03c0047106993829d7
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:47:53 +0000

    error minor objection scream instrument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import sys
+print(sys.version)
