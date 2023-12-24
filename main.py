ac63ed7cff58e1770985485ad11968e65a3397a1
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:31:35 +0000

    everything sign rubbish basic square

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+print("Hello, world!")
