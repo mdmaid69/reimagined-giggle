3591716e77c7e7aefe3cb68f3972e27a6c8a7bfd
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:02:33 +0000

    report prize fold film float

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
