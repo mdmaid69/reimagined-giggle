1150565cbc18ef53816ab9ceb94603bf037067ba
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:18:20 +0000

    intelligent nature type slightly right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
