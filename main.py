20583f25961ba5929fe5e487fb60bf47483d1866
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:04:24 +0000

    old event float excuse bar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
