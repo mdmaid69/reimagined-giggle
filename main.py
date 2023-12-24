9751fda1e4182c4fe3ef6cbdd2140eec9ef3e32e
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:01:08 +0000

    backwards Ff warn gooff slowly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
