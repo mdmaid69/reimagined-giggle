8c8bf2c196d7ad7653a65200106aca7b23718d98
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:12:43 +0000

    threaten paper involve point system

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
