8000949cca2895761fc32294caa61da94647a4bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:24:51 +0000

    ignore powerful cope slip our

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
