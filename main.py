d57e0a34ee44776548fd3e4966ae9b34bf9d8a2e
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:28:43 +0000

    operation takeback maybe towards earn

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_density(mass, volume):
-        return mass / volume
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
