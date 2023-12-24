feff8c080f229d724712e5cf01dd0c482db13746
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:15:32 +0000

    avoid shock suggest any race

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
