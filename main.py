4d85772067e94610416ee11843febbf08afdeb92
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:35:21 +0000

    tidy move worse grow respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-print(os.getcwd())
+name = "Python"
+print("Hello,", name)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
