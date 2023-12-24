dbf40a72c6f4e520ea1bcf6ffcfda7415c6ee49d
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:05:52 +0000

    wind past comment offer road

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
