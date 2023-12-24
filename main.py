468dda9e83af89cea48702e7c5b31cf4b2c08c73
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:30:29 +0000

    unless other fasten emphasize town

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def get_list_from_array(array):
+        return array.tolist()
