3d2e716ae1e1ae49c62b64358ec96336a551cbc6
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:12:30 +0000

    wish any limit on invitation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
