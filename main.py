2869e9a84261db3196df2662549cc70c454f7ac3
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:15:26 +0000

    much outside tune feature and

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
