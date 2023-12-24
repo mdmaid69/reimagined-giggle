297c4cf82c64ecc8d817ca3868710be99d1854f8
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:00:38 +0000

    prefer trip character open find

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
