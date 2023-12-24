b4f112502ce3d533369f1d8e890c71e243395411
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:00:17 +0000

    history dull dinner sense early

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_work(force, distance):
+        return force * distance
+def calculate_area_circle(r):
+        return 3.14 * r**2
