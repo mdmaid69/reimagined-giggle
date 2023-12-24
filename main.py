445e2d89f8056f88fba8e0d3939e82c938c47ee2
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:27:50 +0000

    past happen crash for excuse

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def append_to_array(array, item):
+        array.append(item)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
