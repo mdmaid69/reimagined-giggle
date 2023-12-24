e196c4e13c5c739f88fa8acb466fac5eaa7332e4
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:26:53 +0000

    disappointed picture threaten Ss shock

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
