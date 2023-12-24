05f799f8492dcee4221ad5469a823ab676a86b77
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:47:34 +0000

    towards better turnover getback draw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def remove_duplicates(lst):
+        return list(set(lst))
+import datetime
+def get_current_date():
+        return datetime.date.today()
