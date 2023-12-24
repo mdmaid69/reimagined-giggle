0f9deec215f3b4e4509066a91e4956abe44363c1
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:42:24 +0000

    track song whichever try Oo

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
