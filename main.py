146a48c17b152ed29b3c9b29c69da73aa3369211
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:22:36 +0000

    makeup pressure prevent university without

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-for i in range(5):
-        print(i)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
