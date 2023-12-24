2252d7ce141b228e2bbaee6b3e7c49998213ba71
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:59:57 +0000

    pour 573entertainment word Tt peace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
