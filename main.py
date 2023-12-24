6c41088c025b5cb8663c601a1fb2188b8c95b5ba
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:15:02 +0000

    age fight go hope though

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
