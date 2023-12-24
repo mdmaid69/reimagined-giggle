c88d0de3203e89d5e4f830afafda7168d92e8ba1
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:12:24 +0000

    nonsense rush woman pressure behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_repr(array):
+        return repr(array)
