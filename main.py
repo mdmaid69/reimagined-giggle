ae6eee3e913af2749bcc223790dcacec0a2da022
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:17:25 +0000

    base cheque unless head print

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import array
-def clear_array(array):
-        array *= 0
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import os
+def change_working_directory(path):
+        os.chdir(path)
