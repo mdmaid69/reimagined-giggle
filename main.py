3349ad87a0e0b8359074ad7d1dbf7973296dbe6f
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:23:26 +0000

    force moral mother closed hold

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
