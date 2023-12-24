2160c7827589a1c8367d65d7734f558caf4b6b5a
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:29:13 +0000

    careless except anywayanyhow breakin theirs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
