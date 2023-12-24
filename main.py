2354b56830fb3a7ffb19b36d56b7e81a08184be3
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:46:46 +0000

    hurt consist politics over spite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
