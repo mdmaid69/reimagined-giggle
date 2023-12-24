23720ffc5e1f98192356e3093dcc33c8f0547bd4
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:22:28 +0000

    wander state aim independent put

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_power(work, time):
-        return work / time
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import numpy as np
+print(np.array([1, 2, 3]))
