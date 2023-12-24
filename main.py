b2aea2ef972f6a53b2196dcc25ac1423873a269c
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:43:59 +0000

    object suddenly under scare grand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
