ecebf4a248b24e212f3e58bf522b9733cb326e34
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:38:23 +0000

    involve unhappy recently body Ww

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
