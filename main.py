febc5852a85251c54263d1a643d040c90852d7e0
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:14:19 +0000

    aware innocent press night computer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def find_union(list1, list2):
+        return set(list1) | set(list2)
