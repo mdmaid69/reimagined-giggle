b12af8869676f2d5e5e98df40ebae9b1ffced68d
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:52:52 +0000

    dress simple range niggaacademy tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
