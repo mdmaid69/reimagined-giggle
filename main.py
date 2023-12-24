dd9f400e93a058fdfd532dbb446dc8ec6b79729e
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:57:00 +0000

    human visit rob period person

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
