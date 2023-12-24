f38ee27c8b14151e33778d2868da6e4fd73a8203
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:18:22 +0000

    answer back happen related yours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
