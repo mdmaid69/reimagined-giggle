efd5ad3b16fb26242f3559741f2a7ed09531c76c
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:37:31 +0000

    add trick beside join half

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
