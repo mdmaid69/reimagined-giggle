292a16e26dc6e254e66bd4d06d9c4fee033adc4f
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:25:48 +0000

    sea admire black careful thorough

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import array
+def get_array_as_int(array):
+        return int(array[0])
