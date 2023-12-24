34da882ee449c5902df958e837802b31408ab966
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:00:44 +0000

    continue rest jump he birth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def is_odd(n):
+        return n % 2 != 0
+import collections
+def create_stack():
+        return collections.deque()
