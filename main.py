0259046f5b5625c3e7cf48d00420526cc3f9caeb
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:18:47 +0000

    scared collection exchange test rest

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
