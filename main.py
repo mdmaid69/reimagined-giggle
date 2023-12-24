ef5f6d2555cfb28766e273b4a57341a49217a404
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:25:30 +0000

    talkoutof flavour father grade spot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
