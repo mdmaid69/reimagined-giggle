bee6020606d50d177cfa21a393a3e829b3080a3d
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:34:10 +0000

    arm path to whistle trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
