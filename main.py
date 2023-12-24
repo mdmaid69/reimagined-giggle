5f474a23b67fe4cd633f52de45ab3d5da200dd02
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:38:12 +0000

    gentle nonsense successful hit mistake

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def calculate_speed(distance, time):
-        return distance / time
+text = "Hello, world!"
+print("Words:", len(text.split()))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
