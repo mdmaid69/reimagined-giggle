cdc5dc7dcbb9ec7ea5276b0c556e9c0f2f311b8d
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:43:40 +0000

    comeup pupil consider environment tool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def get_array_as_int(array):
+        return int(array[0])
