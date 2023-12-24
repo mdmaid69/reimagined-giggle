beee2ddd825d0947ab1cd77f707749b38a39590a
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:13:35 +0000

    begin somewhere comment other along

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
