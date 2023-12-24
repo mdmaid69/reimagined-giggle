ad715d5a6df30007f283c8c89cec96c17242d7ea
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:26:21 +0000

    foreign go ask break anybody

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
