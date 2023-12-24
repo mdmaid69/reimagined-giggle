1278fa2be47399b9cb49602664e628f264ad7988
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:27:26 +0000

    speed platform snow out would

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import getpass
+def get_username():
+        return getpass.getuser()
