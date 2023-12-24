5191066e5545c73ea4bbb92cc341d8cd41073226
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:56:48 +0000

    wing rather kind body so

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_average(lst):
+        return sum(lst) / len(lst)
