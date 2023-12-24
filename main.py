0e611e50d31f248edf4b0cd0ede96fb78c2e2894
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:26:09 +0000

    formal decide would figure maintain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
