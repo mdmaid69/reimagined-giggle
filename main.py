8f04b080dbb3e48654458648ec49f4f503f6147a
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:19:34 +0000

    snow round wing accept crash

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def reverse_list(lst):
-        return lst[::-1]
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_acceleration(speed, time):
+        return speed / time
