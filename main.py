bb1cf8f5fb79ba214cd6317283749fe459588c8d
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:36:28 +0000

    circumstance task shelter fierce poison

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def convert_to_hex(n):
+        return hex(n)
