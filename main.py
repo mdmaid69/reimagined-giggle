da853bc97c7d4c7261829fa593126bf48176f903
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:51:08 +0000

    figure reply exist 566end wheel

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
