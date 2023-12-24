294772c7364065aafebb9434334d98b08421726e
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:42:30 +0000

    since off flow business leg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
