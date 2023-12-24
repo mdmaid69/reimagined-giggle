37a80422d09ec0ca843d81c70afdf84ddd85b7f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:14:16 +0000

    government considering describe love agree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
