43b8be4152116ef35695d8d5da3468d1da0ba2b7
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:13:11 +0000

    present bored plenty comeoff cut

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
