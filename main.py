d1dfa692484225a0a19f039021374646748558ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:23:56 +0000

    race second boil convincing remain

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
