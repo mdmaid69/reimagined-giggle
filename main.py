3c6cd52c5ce2b747972bd98737e1afed96ad2fd0
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:51:44 +0000

    estimate cruelty treatment pay friendly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
