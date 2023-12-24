1d4b3447ef1b6bdac80da2737d98c341d71a582a
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:35:26 +0000

    cross emphasis talk tear meal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
