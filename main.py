dea97c9d70bdc50b327c74c3150ecd8be142382a
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:36:56 +0000

    fence which copy lookup influence

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import getpass
+def get_username():
+        return getpass.getuser()
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
