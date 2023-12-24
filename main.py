5a1dc3bb5b70c880d0a1afa685fb0d60f29af93c
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:19:24 +0000

    fashion flood minor rain shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_area_rectangle(l, w):
+        return l * w
