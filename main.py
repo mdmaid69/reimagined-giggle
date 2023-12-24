ff344412993f5445ebaa39ec31368e693e115b67
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:37:38 +0000

    miss interesting country march myself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
