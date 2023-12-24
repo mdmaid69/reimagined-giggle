3bd96842ee77635f68f0e93289e6f427f1c676e7
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:44:32 +0000

    keep shame agree complete but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_acceleration(speed, time):
+        return speed / time
