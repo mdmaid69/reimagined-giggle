5361e1f67a5516053c564f03aeaf0e786e1de4d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:56:47 +0000

    sensible test before half possibly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
