ccaebdba7a249d154678a387cb6f1a868ba229e4
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:12:07 +0000

    consult breakup find needle ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
