f4fdd395c59bc771bfe08b32c95d65b6cbf40fdb
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:11:24 +0000

    female like brave otherwise wash

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
