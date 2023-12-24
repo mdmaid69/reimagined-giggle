8a459e22159e38c0c2c358eca58e641390d172b1
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:15:20 +0000

    office theirs achievement ear down

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
