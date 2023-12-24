8b40c6fb1885337b9b69c83386078fc5004e8335
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:46:47 +0000

    wheel bunch reduction be thinkover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
