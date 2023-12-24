cded65eb813ffa13672a9c8cc7c3948ec7f41265
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:27:12 +0000

    swimming discussion tear land quiet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+import numpy as np
+print(np.array([1, 2, 3]))
