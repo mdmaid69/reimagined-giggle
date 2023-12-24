92a521972eb987614c2234ccf9159ce212df2e88
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:41:24 +0000

    relax away perfect damp start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_user_dict():
+        return collections.UserDict()
