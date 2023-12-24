ecfbb57240d2e585498651b8e7cd779e6eb82582
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:36:43 +0000

    theirs poor behind look band

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def reverse_list(lst):
-        return lst[::-1]
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
