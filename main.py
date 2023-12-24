baedab6600b1fbf56d8415f5b6f8f0a703325ead
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:13:28 +0000

    Uu border himself flood love

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import json
+print(json.dumps({"name": "John", "age": 30}))
+print(sum(range(10)))
