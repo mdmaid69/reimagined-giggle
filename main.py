9cf97bfa3dd6af3db5a41282feb49082bff1d06c
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:18:01 +0000

    below past frequent stone settle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
