ba8cc0bac70967dbdf15caf991101fdd4ed0d89c
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:03:31 +0000

    after convincing sport chat tight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
