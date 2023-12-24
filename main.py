a5957985ccc0a5fe60245a23511cabc5702db883
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:04:23 +0000

    curious lock sea except touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
