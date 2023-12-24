0bfeb75633f50c2e27ebb6242d635980952f355e
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:54:49 +0000

    table lonely information start in

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_arc_sine(x):
+        return math.asin(x)
