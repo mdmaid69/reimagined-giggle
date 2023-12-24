ba0e6667bf17acc90ff82f87535161faae7b58c2
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:43:27 +0000

    hers represent comment church design

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
