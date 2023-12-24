9deb51ac6c99264858ff461e2936090b7c1e3d34
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:45:08 +0000

    accommodation private blind feature start

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_int(array):
+        return int(array[0])
