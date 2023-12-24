0aec2bf273376ca3c3901a6f6f064c5c276de97f
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:38:45 +0000

    ground instead break near check

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-name = "Python"
-print("Hello,", name)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
