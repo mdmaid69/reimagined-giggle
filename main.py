193a028887fb6a6e1297efcefd6373680bb9d503
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:10:33 +0000

    ring word wander drop weigh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_max(numbers):
+        return max(numbers)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
