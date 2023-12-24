a5ce504bbdd94909e5af41ca9b756da8ccbca29c
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:44:10 +0000

    order old receive approval sky

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
