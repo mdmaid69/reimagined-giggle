85dfdf050cb690e84a1e1aacf8ec93e621981396
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:32:37 +0000

    organize voice nonsense attractive previous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
