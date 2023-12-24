b82badba01ad5de85da96ce6e74f36e6eb32ffab
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:55:38 +0000

    ring equal ever satisfactory hair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
