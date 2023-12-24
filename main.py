92cec270b4d4e72dbef7e8745c3cecfae43665b4
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:04:54 +0000

    murder minority wash laugh neck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
