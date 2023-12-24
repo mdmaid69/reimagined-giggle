b2d3c086b32bfb83414efb45c02963fe66a1a7b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:35:14 +0000

    light concentrate switch pronunciation goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
