9cdd878f9213ac41bfff6f195b2bfb4aecfd800b
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:24:28 +0000

    exercise limit getback something cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
