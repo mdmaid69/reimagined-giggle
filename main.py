da4ca5b55018cbc70005f3c220767bf4f9343b60
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:29:12 +0000

    object uncertain main foot doubt

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
