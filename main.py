84fe621545bcef906294d066c69443579dae900f
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:59:36 +0000

    bar piece term steal fierce

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
