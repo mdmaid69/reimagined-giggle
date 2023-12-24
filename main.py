0c8e12f2243b311361f974d9f0f91c605464becd
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:07:09 +0000

    contrast giveup impressed concerning alive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def convert_to_octal(n):
+        return oct(n)
