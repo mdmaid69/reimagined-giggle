34cd0e41aabc2f633da273e3d3b5063e9a44e9f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:47:18 +0000

    mine ahead few Ff upstairs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def is_odd(n):
+        return n % 2 != 0
