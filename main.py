8c46bc163de1713340d22f1e43eb919116d7a199
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:14:19 +0000

    shout example big until classroom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_min(lst):
+        return min(lst)
