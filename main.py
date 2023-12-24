c2de429cc7688aa8d979af8c52dabc09a408dc0c
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:17:06 +0000

    sink impatient test track rub

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
