08edde30dfbacbd2075f84df7cccc7eee1ed39af
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:18:39 +0000

    nor neck bored concerned tragedy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
