8dc1b173c6c2e4341e217030fd2b8f52ec1991cb
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:09:51 +0000

    cool path expression guess callat

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
