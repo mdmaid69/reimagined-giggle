de84930e1eab87849e2b39e2c0295004a7f9b515
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:44:47 +0000

    pretty Oo march under keepoff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
