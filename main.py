2fed670569bcf9618a5f2f1004af8afe017214b8
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:48:05 +0000

    offence wrap desperate rain something

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import sys
-def print_python_version():
-        print(sys.version)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_power(work, time):
+        return work / time
