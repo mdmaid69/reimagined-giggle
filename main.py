c14cf1465608ae5eaa21f9c0d2b9899054541d6e
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:31:30 +0000

    control successful join weigh left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
