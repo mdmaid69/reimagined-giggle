e5c666bf90b8ee718eb3a186f2a512501b409eaa
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:09:08 +0000

    born believe artificial practical damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_base_name(path):
+        return os.path.basename(path)
+  def sort_list(lst):
+        return sorted(lst)
