3201b8a0a63dec54a4ea9734dba9b67ebc15c21d
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:43:27 +0000

    protest bye row really such

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
