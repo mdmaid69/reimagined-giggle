746bd83dafd1cf1dbeea189292c550e22d995764
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:03:40 +0000

    thing risk disagree negative wipe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
