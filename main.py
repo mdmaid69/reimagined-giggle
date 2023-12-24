a4b0cf809c18416f08cfa2ecb87922c799d4aaad
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:47:00 +0000

    care onto life top thirsty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
