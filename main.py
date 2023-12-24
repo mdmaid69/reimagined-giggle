8ae12136508264b4391a58f10933be611b44f335
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:01:19 +0000

    substance operate drop lay improvement

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_work(force, distance):
+        return force * distance
