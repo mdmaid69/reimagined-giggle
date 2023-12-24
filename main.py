f1aa2513fef78ac009df2a797746b47493a5991f
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:40:09 +0000

    no skirt difficulty design weak

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  def subtract_numbers(x, y):
-        return x - y
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
