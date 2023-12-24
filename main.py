db3a91d4a8c8464a24aaa7e4dcff2ebf6fd262e6
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:51:02 +0000

    condition touch lower Bb entry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import os
-print(os.getcwd())
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
