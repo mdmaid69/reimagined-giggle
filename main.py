cff402521171f4eeeb9e471a3011ca5c9e285a4d
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:03:27 +0000

    opposite bringdown sometimes damp up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
