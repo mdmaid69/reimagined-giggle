12eddac2af236eb3aedb7cb7701b3037349166c9
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:37:00 +0000

    delight explanation safety trick Uu

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
