b054e30798dade0f1040df10bd140a4e36edfca9
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:40:06 +0000

    accidental central getaway control interested

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def convert_to_octal(n):
+        return oct(n)
+import numpy as np
+print(np.array([1, 2, 3]))
