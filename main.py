6e7fd943a15f67b37cbb21bf324405cece48462b
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:14:42 +0000

    shout succeed belief spend approval

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
