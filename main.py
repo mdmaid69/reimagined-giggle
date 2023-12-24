28a1c5fcd2967ddab90d7800ffd1fe0662402eca
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:40:41 +0000

    clear sorry concern office strange

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
