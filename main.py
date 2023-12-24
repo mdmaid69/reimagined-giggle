fe7a2ac8945855f07369758afb3179ceeffbbbbf
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:54:18 +0000

    emphasis rush nervous crash accommodation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
