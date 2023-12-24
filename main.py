8dd875e3f576d08e276ffb1027c930ef3862ec40
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:58:38 +0000

    die stupid thanks window tired

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
