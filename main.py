3a59ac6da136fa788df61af422812adfa5fa61ea
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:31:24 +0000

    pattern protest Kk free borrow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
