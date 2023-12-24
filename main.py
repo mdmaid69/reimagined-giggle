78034b50652f3e56fd84e83cce91593961ab10ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:58:58 +0000

    rub uniform set beautiful sink

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_itemsize(array):
+        return array.itemsize
