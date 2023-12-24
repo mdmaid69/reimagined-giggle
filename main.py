2ba838e005a2b92fc79faa3b7e218fe903e970f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:19:30 +0000

    nonsense concern card squeeze land

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def add_numbers(a, b):
+        return a + b
