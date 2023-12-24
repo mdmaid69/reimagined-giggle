0ee008ff02f783c825f58565c079dff5713355c3
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:23:37 +0000

    food loss back safety report

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
