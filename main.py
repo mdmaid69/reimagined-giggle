8c0855720d370c2225dc5f784b1d6c4db37899d1
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:42:11 +0000

    talk about by part future

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
