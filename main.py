49c1caea5ce05245070c79a47477ea65a129ba63
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:56:28 +0000

    worry strong remember vote treatment

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def add_numbers(a, b):
+        return a + b
