65809aa5929b2e99af7c31082f630a5ac57b3619
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:21:27 +0000

    bridge grateful persuade object callup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_current_working_directory():
+        return os.getcwd()
