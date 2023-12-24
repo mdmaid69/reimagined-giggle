14e975baed32a223097535600ba5c4fa779cb5f6
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:23:14 +0000

    whatever who hit judge guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
