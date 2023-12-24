b2a8f498478c2274b8838d0185a2361aeb88f689
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:01:44 +0000

    either view government loud mind

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
