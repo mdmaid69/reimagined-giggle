8da39ce87c9f173e6ab16ec02d46ce73cfc57aeb
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:40:43 +0000

    back deserve case giveup modern

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
