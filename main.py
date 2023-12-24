8244ddd44613b9f3dcfa0984376b3d2788598ab7
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:50:14 +0000

    more approve round rain wise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
 import time
 def get_current_time():
         return time.time()
