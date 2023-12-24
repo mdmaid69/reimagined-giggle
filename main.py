2e2770d324865336044d9eee446448a6cdd55566
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:17:30 +0000

    succeed takeaway nice boring give

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_area_circle(r):
+        return 3.14 * r**2
