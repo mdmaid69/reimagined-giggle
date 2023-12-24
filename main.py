8cd7635e77310cdde7cec64454461e18347fb683
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:59:47 +0000

    herself big lot borrow ill

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+for i in range(10): print(i)
