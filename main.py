fae04ea5472c425f1c09529aa65d15b86e4532d8
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:59:00 +0000

    thankyou calloff school afford round

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-for i in range(5):
-        print(i)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def multiply_numbers(x, y):
+        return x * y
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
