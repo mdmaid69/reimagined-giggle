b3ec9f5e59369866879ceea22dbf217a75587344
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:31:30 +0000

    raw check top small point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
