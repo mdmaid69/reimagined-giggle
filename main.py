950af6f5d0623204c4ad306e177ed312763a7eb1
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:12:31 +0000

    nor down solve scheme permission

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
