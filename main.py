9138506431c52c4b3c4785760e98bf2f0190ac92
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:26:20 +0000

    old getoff sweet almost other

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def add_numbers(x, y):
-        return x + y
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
