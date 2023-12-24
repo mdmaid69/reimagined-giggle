d036ba67bf1be8043a03094e261ff27fde49b3f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:30:33 +0000

    prefer month burn public size

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
