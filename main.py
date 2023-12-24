63d9cb2d3b18c709a448dd90dcdb2c8a8fbd99bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:20:48 +0000

    once custom intelligent abroad return

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
