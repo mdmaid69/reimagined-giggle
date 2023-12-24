514d262e99ed3ed1cab2fe57d7383a2487b0c639
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:20:13 +0000

    switch frightened relief mine report

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
