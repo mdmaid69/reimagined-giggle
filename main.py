bef8930d23c474f6d270e8527fc6513fc5349da8
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:08:24 +0000

    smell force dangerous after minimum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
