2afa866d8fd3f688a67fee958400bd29218ca157
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:41:34 +0000

    score offence target preparation sensible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
