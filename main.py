76c2cdb4f3f96a6bae61e6ba0a9f7854b394997c
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:48:52 +0000

    instant up cost awake match

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import collections
+def create_queue():
+        return collections.deque()
