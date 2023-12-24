e47404071dbcfd846495ae74945b681bc6e210db
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:47:55 +0000

    comeout unfair equal runover definite

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_speed(distance, time):
+        return distance / time
