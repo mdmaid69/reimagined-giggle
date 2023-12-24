e46295b32bd25126911d978fb5a8e4b457fc944c
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:16:37 +0000

    getup picture right test dress

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
