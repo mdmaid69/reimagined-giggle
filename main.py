5b0903083cc4f6092517ea1c425d049161b0cbe1
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:14:37 +0000

    match use book now ashamed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
