6b44d5c10c99b7c10f284d3fbd713dd87d1c6449
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:17:43 +0000

    every tie one year meal

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
