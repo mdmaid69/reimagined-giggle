ca67f9e6d5cfe7e6524d92912383e52d116863bb
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:24:00 +0000

    either cap movement plain extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
