8c4ce4d724afc8a946d3f747a53ab7cbd5b69328
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:27:32 +0000

    mainly reason fit underneath separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
