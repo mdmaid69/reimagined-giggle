0508261cc307130bb4249d3472112771e2504acf
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:05:14 +0000

    shout progress spite equal class

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
