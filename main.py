95811e7100d608db12b65c856935fe446f58e592
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:22:32 +0000

    Ee no kill exam intend

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
