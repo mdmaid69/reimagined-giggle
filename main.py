ef82fe1810b074d76cef3a392d05e9f5d4c571b9
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:44:39 +0000

    sweep on our breakinto fork

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import platform
-def get_os_info():
-        return platform.uname()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import os
+def get_current_working_directory():
+        return os.getcwd()
