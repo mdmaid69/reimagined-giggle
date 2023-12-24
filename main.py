91e67c1638bbe4d2e2e49acaac3dd539e8e7afc6
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:54:29 +0000

    wander once comeup callat throw

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import os
-print(os.getcwd())
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
