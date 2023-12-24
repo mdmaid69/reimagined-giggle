3e80e2bb8793db3f83bedd666ff1ae912782bfc2
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:01:06 +0000

    threaten miss drop Kk polite

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
