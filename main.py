898eb7b6b098f14a456a3497a6f5e60500bb2168
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:33:05 +0000

    far bet best formal ahead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def square_number(x):
+        return x**2
