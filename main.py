ff0d99b2121a702cb93210bfddbba10c80e84552
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:56:15 +0000

    joke shallow comeround getoff tight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
