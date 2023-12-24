96553f8006ba4e9bd0d9fb40c9da5f158122d9d3
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:35:13 +0000

    squash achieve label everybody block

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
