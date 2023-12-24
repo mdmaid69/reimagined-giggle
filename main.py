38447b435fa596f9b12517098eaa6c8156734031
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:21:29 +0000

    remain store image tip anything

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
