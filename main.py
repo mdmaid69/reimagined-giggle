347313886f703edeab22108e1c669759521e2701
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:56:10 +0000

    crime great pressure thanks eye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
