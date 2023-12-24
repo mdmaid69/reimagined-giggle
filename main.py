97c76382d078ef96b818bb249fdd443347592b11
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:00:10 +0000

    delicate right hollow obtain getoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
