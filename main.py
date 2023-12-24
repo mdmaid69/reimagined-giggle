2f8917051a1194367881492ad1b4a499ee21c878
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:04:12 +0000

    translate urgent high uncertain room

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
