9eec2efa18f38d1f5879f15817eab09d6fd18694
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:52:32 +0000

    different book advice tense provide

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
