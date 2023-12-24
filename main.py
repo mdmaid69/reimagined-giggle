87ae868213097c9dda591783c088e8c7aa40f93a
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:05:01 +0000

    comeback middle giveback corner hang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
