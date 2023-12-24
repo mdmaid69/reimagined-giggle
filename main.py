8dc1038b6843e912016db8ace19c339d17c7717b
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:32:44 +0000

    sky winter getout long zero

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
