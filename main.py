cbdc050ff5af8b979353e2a9772b04e4050b3c61
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:07:52 +0000

    land moment waste hurry party

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def get_array_as_repr(array):
+        return repr(array)
