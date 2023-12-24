bbd7a0714449f3fd22b0b623f99f0f94b9f6c32d
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:51:19 +0000

    claim police reject area plain

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def find_max(numbers):
-        return max(numbers)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
