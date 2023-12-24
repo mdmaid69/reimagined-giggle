2b39f348f998b3375b69ba9273dafc1455da73fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:35:17 +0000

    no include visit increase float

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import sys
-def exit_program():
-        sys.exit()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
