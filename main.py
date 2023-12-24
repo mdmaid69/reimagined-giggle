679bd8021a7bb6cff0b7d4eac08d29f6242d7634
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:39:31 +0000

    pipe weak market shape discovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
