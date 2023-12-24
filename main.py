1e13fe42eb453c0206c372a735a9475274710e1a
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:34:06 +0000

    love soon wild church free

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import re
+print(re.match("h.*o", "hello world"))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
