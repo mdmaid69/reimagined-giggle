0b08d0e7245ffb7a326a398e4903e85a56eec555
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:49:35 +0000

    memory join Tt shut join

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-name = "Python"
-print("Hello,", name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
