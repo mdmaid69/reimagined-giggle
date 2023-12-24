71447412554c56255200532b34a13481e2e12ec1
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:22:51 +0000

    mystery smart must hardly work

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
