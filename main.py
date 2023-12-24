9f05908c3af325a8d42b347b42d9c88af0e50547
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:42:24 +0000

    mean length guard where slow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
+  def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
