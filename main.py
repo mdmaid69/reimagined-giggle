18f6806a8178f5a048af5bfe0e7678e8b06559b0
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:34:53 +0000

    towards aim 567enemy flood previous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_list(array):
+        return list(array)
