fabcb11d6a0778add43550d71ce924941b8bc894
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:17:26 +0000

    zero market high personal land

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
