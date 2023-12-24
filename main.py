2c28bff8796727d700ffec1fa19cd656fdd66278
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:55:40 +0000

    determined favourite wall debt separate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
