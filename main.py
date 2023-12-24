737ddb721a4ddcc6eb3d1409fb56e716dd720dc1
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:36:03 +0000

    getaway than damage distant sensible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
