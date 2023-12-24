5177997a3e5ce925b4c6b828ca8743fc3e2ca2ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:38:21 +0000

    main character gooff music bridge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
