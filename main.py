0a594cd401f2efe39d609960f3308c8409dfde7f
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:17:27 +0000

    settle bring upwards equal modern

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
