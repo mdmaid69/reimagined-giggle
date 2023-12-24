9266aa7c193246fd585952659d22406d3adad9f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:49:48 +0000

    fault speech to attention market

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
