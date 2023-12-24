acbb57f71de36cc0fa91a9421f583c3dfbefd16d
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:46:07 +0000

    bury also common accidental zero

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
