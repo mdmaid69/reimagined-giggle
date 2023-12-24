c3fb177867d2f90f544e7cd7829d1d8cd256c2d8
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:56:46 +0000

    possession today class but matter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
