b094276eed4ccb25525d530b26ec735d9e2b6993
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:59:03 +0000

    experience afford roof muscle circle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
