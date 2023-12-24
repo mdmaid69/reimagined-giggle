7edb7252af35fd910393b7596daa9d7ddc7c9249
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:41:01 +0000

    cap roll top alone bottom

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def count_words(sentence):
+        return len(sentence.split())
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
