844ee2562b1fcddd8d612dc7ddb7f12c3be79178
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:59:54 +0000

    just breakfast band order again

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def convert_to_octal(n):
+        return oct(n)
