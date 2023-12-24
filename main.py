df02409f535d166a86ce47a416021b97c23ac369
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:19:14 +0000

    themselves generous ours recognize spend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
