d98d1fdbd551a0959892538156dab027eea16ddb
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:24:32 +0000

    main car speed eye ours

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
