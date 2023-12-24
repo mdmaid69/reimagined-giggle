98bf1e7803f502aaa3e01555b7d05de5ba540009
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:48:03 +0000

    light complain guess occur climb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def count_elements(lst):
-        return len(lst)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
