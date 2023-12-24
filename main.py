0e0cc9674013e0463f76e306ba7ba6c538e414cb
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:58:56 +0000

    nonsense development receive pretend hand

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
