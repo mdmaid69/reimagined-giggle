cfe9e3bdfbb1be1b9abb80ffdcc745a7301baee0
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:59:14 +0000

    sail instruction bang deliberate tragedy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
