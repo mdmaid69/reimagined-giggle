1d832309bc8b998955ffa4e7b7cca7e7ef110ea0
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:03:52 +0000

    unite large door shop press

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
