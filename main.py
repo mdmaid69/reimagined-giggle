9f3bad6e515c3218ff172b01136c4bafe2eee9d8
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:10:02 +0000

    separate interview beneath breakfast maximum

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
