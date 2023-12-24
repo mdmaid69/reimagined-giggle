ac4f9f65c42fd7d95b8ee59cad6fe9dda73812b5
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:00:06 +0000

    delicate hurt hard breakin account

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
