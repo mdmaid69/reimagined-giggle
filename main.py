ec974159edae5f06daf0dbb9daf85ed7fbc87ede
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:09:16 +0000

    relationship behind Pp mouth consult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
