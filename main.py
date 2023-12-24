ef7348c29c8c2a8b52ae8bcc450a90078caabc38
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:56:11 +0000

    sort speak frighten clean letter

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+for i in range(5):
+        print(i)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
