bc31ed38242e29a49b5241cbb7c618ae6cf374e2
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:56:30 +0000

    follow clever meanwhile pupil whether

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
