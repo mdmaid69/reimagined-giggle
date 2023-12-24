e9071d39870dbb38dc7f80af51d22f3f2058cf6d
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:28:29 +0000

    raise cruel achieve equal top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
