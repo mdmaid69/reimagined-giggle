02b4c0d58597dd90511b70a7ed550b0e9f3f572b
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:30:17 +0000

    police fit worth win offend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
