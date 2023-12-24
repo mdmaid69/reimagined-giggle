130f22142c0f84e1d24f32251a8adfdde6dd5ffa
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:18:18 +0000

    swim ring adjective pull father

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
