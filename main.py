e872b46033e6694ea8f08cbdfedeac05cc401b2b
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:26:26 +0000

    message talk calmly hollow seem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
