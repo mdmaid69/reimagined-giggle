6d797adec5703bdf6bbe00742e7c0882c5551b2b
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:26:20 +0000

    inquire single story aim emergency

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
