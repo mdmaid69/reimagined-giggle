f83af97cbd6bad91f2e48047342ab0c11d2b3d15
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:43:03 +0000

    cheque cruelty personal quantity brush

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
