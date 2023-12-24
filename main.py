19cfbd7eb572613cca9efc875899907f585a8dd3
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:13:37 +0000

    under swallow opposite Gg actual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
