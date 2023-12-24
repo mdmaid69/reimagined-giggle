a017ae9d9af8a66c8d8bf73093197eb71d2a98e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:08:22 +0000

    full agreement minimum contact spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import os
-def get_current_working_directory():
-        return os.getcwd()
+def remove_directory(path):
+        os.rmdir(path)
