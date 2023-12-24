bab59d4dad3c776f367453af1a70454127f38d09
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:45:41 +0000

    container trip by juice improve

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
