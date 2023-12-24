dd408903a3f852f5bcdb22671e843e631a1dbe4f
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:00:44 +0000

    tour relation furniture abroad trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
