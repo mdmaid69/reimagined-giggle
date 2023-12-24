e2c1afdc738428892b763d8c01f3492536269655
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:42:34 +0000

    no type turnback cut art

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
