9ba35a49c439d49392eb0ee1d9d76a2a29244070
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:59:48 +0000

    fight our sense herself exchange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
