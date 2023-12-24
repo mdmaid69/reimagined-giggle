86174641706c1cf7aa374731970817e7aca2900f
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:19:10 +0000

    responsible upwards centimetre art surprise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
