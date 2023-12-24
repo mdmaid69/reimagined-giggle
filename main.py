3be7f85371a9f5e70b7eb84f22d8a86148624272
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:14:02 +0000

    possible game bet care wall

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
