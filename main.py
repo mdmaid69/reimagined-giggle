5efab520155b1db93b291f522ecca0169c313261
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:26:28 +0000

    mind opponent high sail score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import random
-def roll_die():
-        return random.randint(1, 6)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
