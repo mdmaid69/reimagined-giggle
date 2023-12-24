1c30e647ce67dc6d1f313615c3a7bae77d75d124
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:31:25 +0000

    lookaround press report basically might

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
