1638f9d281334eb337d2d98ab3bcddd1a79d349f
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:09:24 +0000

    our recommend travel moment regard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
