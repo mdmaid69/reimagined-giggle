0ef9d3422173c82a65509c868a6f27dcd58e1ad3
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:11:12 +0000

    lower block practise schedule rubbish

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_item(array, i):
+        return array[i]
