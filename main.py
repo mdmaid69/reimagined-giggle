9a3c88e59e0ae6970f6f3dfd524e9ba09fe24e45
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:53:21 +0000

    570enjoyable grade such to cost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
