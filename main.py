a2ab28973fd60d1b91e9e12c251f9c9c3c739d7d
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:09:33 +0000

    direct except better Cc meanwhile

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_power(work, time):
+        return work / time
