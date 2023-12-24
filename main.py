52b3b80673082f02d31906792e5bb19b64586ed9
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:03:21 +0000

    reject turn practice refuse effect

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
