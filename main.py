38bebc48dd5c97fbe75998e58d059b902c31ea99
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:15:15 +0000

    571enough scared ours bound mysterious

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_circle(r):
+        return 3.14 * r**2
