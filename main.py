b695e78d9b3cdd2b21a164ebd3dedf27f1972bdd
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:42:02 +0000

    put quality force pick number

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
