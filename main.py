55251419021c1db9a9aa9a0fe21a845fb672f2d7
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:47:32 +0000

    absolutely range mystery he fall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_power(work, time):
-        return work / time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_acceleration(speed, time):
+        return speed / time
