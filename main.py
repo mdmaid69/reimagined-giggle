6df67b5eb006f5a07ad26eb2f995689f4b89fb04
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:25:37 +0000

    suit however stand embarrassing behind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def find_min(lst):
-        return min(lst)
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
