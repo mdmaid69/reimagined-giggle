58062593b8ab46a374b2eaf25cf2aa0137e39e02
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:13:01 +0000

    probably approval Oo getoff own

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  def add_numbers(x, y):
-        return x + y
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
