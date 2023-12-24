02c30ec366a7fa188b1b18e83b867eaf3a5917a1
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:33:10 +0000

    cough because this turndown recently

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
