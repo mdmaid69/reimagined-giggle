ad6f44701743511c5f4d361488b0e708345cfbac
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:05:05 +0000

    mind platform present flood both

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+print(math.pi)
