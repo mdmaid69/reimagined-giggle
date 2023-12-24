0da40a9d510620626befd4fb16313926f69a6599
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:53:44 +0000

    turnout flat complaint steep help

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def add_numbers(x, y):
+        return x + y
