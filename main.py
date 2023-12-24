b8d81b6f7aef7a61305ac25311613d170328e0ba
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:13:17 +0000

    rare wish spoil minute store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+print(math.pi)
