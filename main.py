ce39b024ef6fad32ce68b34832c92f53442c646e
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:51:44 +0000

    load steal male plenty point

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def is_odd(n):
-        return n % 2 != 0
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
