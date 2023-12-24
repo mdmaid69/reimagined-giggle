ac8c5ac7511488a17491ee8124ccb2937baf4be5
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:40:23 +0000

    tongue calloff win authority whom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
