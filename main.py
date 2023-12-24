d7d0ccdee03405e48efe444e3fce1bc580aa31a9
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:52:27 +0000

    trip number joke thumb down

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
