5670e2729646ad8cbe9c5f14f81cea3eb4bcfefd
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:20:39 +0000

    back meaning book narrow turnon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-def exit_program():
-        sys.exit()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
