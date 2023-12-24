76c242c4fd846412206e050e166954716c03a6bd
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:02:49 +0000

    murder somewhere break flavour shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
