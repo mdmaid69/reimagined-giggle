792ada7a26c59c20397ea9e20072eccd027186a3
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:13:20 +0000

    most away distant importance rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def print_python_version():
+        return sys.version
