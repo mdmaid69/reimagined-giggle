16c8cfcc2a9a64a08daa10d39cebf4f9995e2680
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:11:57 +0000

    everyone neat theirs secret someone

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def count_characters(sentence):
+        return len(sentence)
+def calculate_pressure(force, area):
+        return force / area
