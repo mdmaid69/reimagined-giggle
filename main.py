53e387c242eb6aada15c6c97c84e07bbdb0e0b35
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:04:35 +0000

    risk reason holiday clue grand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
