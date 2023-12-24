4b351ef2d6f7a7c0ab5c9aad1b369b29faa6242c
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:06:48 +0000

    goon convincing price takeover present

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  def count_elements(lst):
-        return len(lst)
+  def square_number(x):
+        return x**2
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
