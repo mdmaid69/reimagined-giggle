393dace3fb088bb08f3d12acf4cd5e4a521e7418
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:40:06 +0000

    quality talkinto else chance gooff

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import json
+def convert_to_json(data):
+        return json.dumps(data)
