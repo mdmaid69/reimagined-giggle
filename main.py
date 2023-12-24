5360bfe573907aaf33b835776bc956b080bccf54
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:10:10 +0000

    means somehow like create considering

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+print(json.dumps({"name": "John", "age": 30}))
