c857ea7538a76f8e2934dfc3cbbc5a0025d0ccf4
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:23:18 +0000

    car round woman duty cut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
