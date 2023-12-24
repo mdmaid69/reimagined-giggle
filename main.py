ccfcb50bcf01596afcf076b5682d474235fdc4ff
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:31:12 +0000

    trousers tap stone match will

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
