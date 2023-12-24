b9fe638f4058d8b63ccd4d8d21865009a29c50fa
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:36:30 +0000

    several constantly lookback(on) wave heavy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
