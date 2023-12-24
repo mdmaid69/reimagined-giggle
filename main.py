4bb85bd119cdc0a9a94e410cf6854e77a84979d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:20:18 +0000

    coat speak over platform honest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
