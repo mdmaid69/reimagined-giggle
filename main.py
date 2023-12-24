cb9b497b1a159443b951cd2f0aa77bc2a7bf5cd2
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:11:55 +0000

    first lesson guess thankyou right

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def is_even(n):
-        return n % 2 == 0
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
