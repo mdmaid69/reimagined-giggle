476f5512097f83e8439bea2b0b4f1b982788161a
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:00:51 +0000

    left bang tear bunch organization

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
