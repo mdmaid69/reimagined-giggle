5e630aba51e89937662f124e04154f06408ccc1b
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:48:00 +0000

    murder as difficulty rare our

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import collections
-def create_queue():
-        return collections.deque()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
