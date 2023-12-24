5dd82d985dd96b41cf6683e726b9c7c9ae6b02f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:28:38 +0000

    while side praise cough yet

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def count_words(sentence):
+        return len(sentence.split())
