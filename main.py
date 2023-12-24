f97199fc05ccbcec75494a1ebcab65eb073598c2
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:19:28 +0000

    Pp custom announce respect break

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
