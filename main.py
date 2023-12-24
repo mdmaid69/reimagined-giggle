349ba89d7b2424702530508b8e362f55eb0b60c0
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:38:45 +0000

    usual offer effort everywhere attract

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
