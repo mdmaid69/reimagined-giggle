119fc8723660997e9d16a1f7a4fbf50a07b0ff7e
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:22:18 +0000

    hate cheap last interfere consist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def add_numbers(x, y):
-        return x + y
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
