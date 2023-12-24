3dda54fdc99075bf8e9e0138bcdff31dde880f4b
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:09:12 +0000

    lift seriously tight of message

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
