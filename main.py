4c6350ee689a7b761bba490a28b8208585666370
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:46:05 +0000

    bet holdup injury search voice

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def convert_to_octal(n):
+        return oct(n)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
