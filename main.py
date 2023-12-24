e52fd6b4bf5392c052447d02d154f31361e7f3fd
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:40:05 +0000

    forgive also group pollution sink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def multiply_numbers(x, y):
+        return x * y
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
