8a61256324f9f36852918f8103b7d977d5ce7507
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:04:12 +0000

    scream total include car confused

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def reverse_list(lst):
-        return lst[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
