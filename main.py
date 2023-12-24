68fbedfba75525da53d425e5743abfe12e057a33
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:20:13 +0000

    Nn pull neither confident round

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
