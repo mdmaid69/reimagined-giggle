1be464fd2eafeccfaa9125eeab626d5c7aadbed1
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:07:45 +0000

    give when 569enjoy recovery slide

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
