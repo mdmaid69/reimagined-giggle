86bd9a47960ceb11d9275966380684aadc3df4d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:40:06 +0000

    comeover dead active behind address

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_as_list(array):
+        return list(array)
