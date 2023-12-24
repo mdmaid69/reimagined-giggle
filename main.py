cb68e8210b563778274607658e96636dbf626d59
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:56:48 +0000

    should finger port convincing move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import json
 def read_from_json(json_string):
         return json.loads(json_string)
