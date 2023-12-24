4540c4b0cea2856761f0723782fbed9670ab3f1a
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:12:19 +0000

    only count accordingto bridge risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_exponential(x):
+        return math.exp(x)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
