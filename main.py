a4d67f138551f0a9d1fc14af12023e376b6238ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:44:11 +0000

    be exploration satisfactory distance personal

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+for i in range(10): print(i)
