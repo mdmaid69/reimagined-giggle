cefc800778926c98892495902cd73147b4f0cf99
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:16:47 +0000

    hurry table cheat afraid common

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
