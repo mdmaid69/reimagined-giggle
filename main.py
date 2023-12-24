6566b934a2207cd133d1a0c54d4f04d5bdcbe6b5
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:55:17 +0000

    furniture ring turnoff get suckme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+print([x**2 for x in range(10)])
