98e0fe05db538a1b360b194a7950f92077aef794
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:17:32 +0000

    target run confidence control offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+def find_min(lst):
+        return min(lst)
