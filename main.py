0723085571efa86f91170ea9ccbb0badb8ee8b8c
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:43:19 +0000

    sad react remark search step

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def sort_list(lst):
+        return sorted(lst)
