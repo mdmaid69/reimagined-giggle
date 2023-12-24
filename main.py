e714efbcbb27cc0a4fd8f72dfddd5bff1821355c
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:28:40 +0000

    kill smart embarrassed sign service

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
