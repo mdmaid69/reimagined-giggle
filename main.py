2ea54ec65f43048f2b75317fc72529b6a91aeccb
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:15:49 +0000

    replace snow impressive shout our

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_itemsize(array):
+        return array.itemsize
