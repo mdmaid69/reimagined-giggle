35a342dd08ae7faeae980abb0bf73bf0e4bdd574
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:56:34 +0000

    legal repeat company ordinary urgent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
