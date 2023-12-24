77bbd72e758eb5baca8a50894deb0604419bf634
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:35:01 +0000

    amount tall regularly plastic concentration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
