c0557b7a89eb48af42db067f7a2752e742885266
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:19:59 +0000

    tax start square he interest

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def find_min(numbers):
+        return min(numbers)
+def count_words(sentence):
+        return len(sentence.split())
