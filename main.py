64f0a891a06242b66440e4eca32c683da22b4bbf
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:56:36 +0000

    inquire address have completely comedown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
