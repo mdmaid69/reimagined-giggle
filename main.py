38a8cd49bf68e2c8f158e9adb92b9f69eca82a20
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:10:57 +0000

    circumstance bye spoil order window

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+for i in range(5):
+        print(i)
