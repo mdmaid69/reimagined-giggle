d795709de40e23e92266ce0955263868bec97946
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:49:01 +0000

    true cross attempt best record

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+for i in range(10): print(i)
