8075d2a289223f0f201ad9c25bb8ca5b4fae76e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:24:04 +0000

    by anything gift what event

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-x = 10
-y = 20
-print("Sum:", x + y)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
