c7201bef6fef8df2a735b07a7f64217e6baccffb
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:04:01 +0000

    scream punishment variety nervous need

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def convert_to_binary(n):
-        return bin(n)
+  def square_number(x):
+        return x**2
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
