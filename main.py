7d2f36c4b69218a64d565ce517a6ab46b61eab7d
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:36:25 +0000

    giveback wide Ll rescue argument

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
