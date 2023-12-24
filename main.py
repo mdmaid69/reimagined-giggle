425414ee897a01a0d354072039ee1e17eb218e5c
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:17:31 +0000

    me argue goback(to) order leg

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import sys
+print(sys.version)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
