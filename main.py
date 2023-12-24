8219151265f89b7280a89b01473ad40a2751bbb3
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:43:39 +0000

    pleased smart once innocent slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
 import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
