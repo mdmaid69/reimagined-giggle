3e95be421d27f960d31e57f4ad797357adcfdf70
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:46:55 +0000

    identify sink replace exercise squash

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
