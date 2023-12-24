8c54af0eae87fa169f27caceb0b14198cf3539d7
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:34:13 +0000

    reserve big nature empty talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+print("Average:", sum(numbers) / len(numbers))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
