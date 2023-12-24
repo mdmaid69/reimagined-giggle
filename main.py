4266ed4249b4fa3d2f5a2b6d40efbe3bb8b4fa76
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:27:23 +0000

    dick Uu object away other

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def find_max(lst):
+        return max(lst)
