42dfe1cb972d3506101e228e0d6dd1e570b9e698
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:06:34 +0000

    hardly punish sale flow behind

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
