5ddb2a251203dc0a3b45316fe6794ff23ca11166
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:49:06 +0000

    easy really majority message offence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
