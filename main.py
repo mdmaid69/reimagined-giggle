3554c25e02ef041ca4ab34f95b3b5082d1c6b1de
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:40:18 +0000

    afterwards measure job spring hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
