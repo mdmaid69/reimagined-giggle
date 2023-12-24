6bb0c953b664be50385b4d3c7776ad91eb391a7c
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:44:40 +0000

    centre Cc light recently consult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
