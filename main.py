1aa0793b87ae82e1aa7c2ab7e87ee28259f102a6
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:43:02 +0000

    principle problem address left dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_power(work, time):
+        return work / time
+import time
+def get_current_time():
+        return time.ctime()
