6a1a04c12f5be5147ed6f43d8256229df4ebafb7
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:43:06 +0000

    lookafter murder government keepup law

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
