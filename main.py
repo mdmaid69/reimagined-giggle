254f506cd84c829de3aa70720d6ffcf7e913065c
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:26:28 +0000

    confused send my sting environment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import collections
+def create_stack():
+        return collections.deque()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
