12a98293bc20cbc631fc91d8269243658ec64371
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:18:46 +0000

    weekend Cc favour entry often

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
