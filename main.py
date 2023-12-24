822d00936f9864a2f74bbab9e0826ccef4c10fed
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:49:15 +0000

    571enough discussion bring separate tour

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
