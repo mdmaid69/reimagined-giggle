f74ad5e179954d7ddc5111d3485e0b49ea37d278
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:01:38 +0000

    corner follow gift explain service

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import time
-print(time.time())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
