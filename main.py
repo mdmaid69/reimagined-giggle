f93fd0acb4e0e1de5cce76de279f17aa25415d89
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:49:18 +0000

    admire tradition whenever birthday invite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
