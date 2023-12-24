d721c8b6d4a2e1840b845e678c2f6250327b6ee2
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:03:09 +0000

    late Ww zero coat protection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
