408f215cdb4de2f021d0a379a07fc9b65f4ec9c3
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:23:47 +0000

    slice intelligent keen serious inquire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
