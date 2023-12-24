8f90d1a0411fe26b8afab832a06206fa78882674
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:38:36 +0000

    score black control let action

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def find_max(numbers):
+        return max(numbers)
 def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
