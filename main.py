9ae2d5fd385dcc434a0a1735c1de5a34eca2a563
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:06:19 +0000

    strength fact here before command

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import sys
-print(sys.version)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
