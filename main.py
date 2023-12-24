d7819cc523a2d4f4fa6ec6529bf45d8348270340
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:34:48 +0000

    fresh introduce day trouble path

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+print(sum(range(10)))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
