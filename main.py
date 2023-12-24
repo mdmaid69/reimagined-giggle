8731fcad0e5bb913fe1a0289f86a3f42f9b7f932
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:38:30 +0000

    study discovery general callup cross

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
