e9427a0c4ef1919557bf44a3e2ab4ff832cd3524
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:05:51 +0000

    pin pain announce head think

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_work(force, distance):
+        return force * distance
