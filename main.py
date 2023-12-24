c3778d77a1f5eb587f89c501f8d0b7527a533461
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:52:10 +0000

    settle reason pattern accordingto somebody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
