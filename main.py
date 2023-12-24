e367db7b6b2856a259e6fbed81dc932bd27ddf3a
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:30:56 +0000

    complete how range OKorokay mysterious

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
