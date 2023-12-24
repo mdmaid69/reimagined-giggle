cc2b3c9fa4f8d33e5c7eb5c002abb11b5c4925bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:06:15 +0000

    meet run cut pour real

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def calculate_power(work, time):
+        return work / time
