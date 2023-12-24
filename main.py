0af9647c5355e459b134e2c355672508e863d64c
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:21:38 +0000

    hope scene matter find pretty

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def is_palindrome(s):
-        return s == s[::-1]
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
