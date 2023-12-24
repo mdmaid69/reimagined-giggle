41cb5c5c590409fe8d6070b14cb0964b145f5dce
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:43:46 +0000

    apparently your downstairs 566end teach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
