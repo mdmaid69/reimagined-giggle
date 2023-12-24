5101f56762a1d93517360f6410463bfaa3a79299
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:12:04 +0000

    float mark lend spot only

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  def remove_duplicates(lst):
+        return list(set(lst))
