fe838db86d153dd2832f985a4fc5b08b6bbef074
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:14:12 +0000

    consult mix cinema attention negative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
