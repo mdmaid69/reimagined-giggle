4c14d0c01a5ee2ea0739692efa57659ba951aa05
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:23:03 +0000

    workupto whatever brown shelter stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def split_path(path):
+        return os.path.split(path)
