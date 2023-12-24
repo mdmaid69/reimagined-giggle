2e7cc8e5027adeab28e776b134efc62f1776095a
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:23:13 +0000

    absolutely regular Ss properly phone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_work(force, distance):
+        return force * distance
