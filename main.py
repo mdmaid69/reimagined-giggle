640df44151486884c5bd1d0e44dba6844b5490af
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:19:02 +0000

    standard on taste bang murder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
