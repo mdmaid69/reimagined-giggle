b5b4e1d73b3c28834479bd099b9b16772613e185
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:40:22 +0000

    right both callback estimate cheat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
