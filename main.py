415bb58064f2993a592fe7b5b6b2dd3c4ff10bca
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:20:54 +0000

    practise Qq lay limit odd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+print("Hello, world!")
