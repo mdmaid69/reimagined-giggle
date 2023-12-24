0817b6e8ce2182945f68afcb464b84dff6896da0
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:20:08 +0000

    drawing chase minute offer check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
