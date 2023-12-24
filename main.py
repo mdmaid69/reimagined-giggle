52ef6bf72999b8bf91811949f89fc907920cc1c7
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:39:27 +0000

    skill stare politics afterwards petrol

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
