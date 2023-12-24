c34a32d31d155e66023f6d71e33cb9a137f1cf60
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:59:03 +0000

    narrow hour true off statement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
