3420209618dd52cc49ac10a83f8ef5136a003a4f
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:15:55 +0000

    scream term discover band gap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
