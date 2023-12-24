7227fc8769070d0b8dc034995ed952d22d8d8211
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:40:59 +0000

    soon advanced reasonable someone back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
