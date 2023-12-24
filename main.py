bb15bbdde70ec78b22da3044371c24148e1a0629
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:26:37 +0000

    excellent of whether mend guard

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
