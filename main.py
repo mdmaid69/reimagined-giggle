291ce1296ef6247e30fc8d180799d5928575d427
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:22:13 +0000

    smoke worse want pot kill

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-def get_time_since_epoch():
-        return time.time()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
