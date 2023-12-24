db678a87440a2c7a89dd9cf69b246c91891c3946
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:13:03 +0000

    squash hers far rush still

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  def count_elements(lst):
-        return len(lst)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
