5e171c43cc65ff83cc3ed8ecf5d4639eb2cf549b
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:50:52 +0000

    computer challenge physical spray language

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
