11eb44b202b1bb9b38fff81e40795794004e04ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:15:28 +0000

    drop minor enthusiastic poor paper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+def get_current_working_directory():
+        return os.getcwd()
