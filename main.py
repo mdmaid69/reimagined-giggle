aa7114c91580d4eea09e0ca7acde5d5ac45e1dc4
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:50:28 +0000

    spoil section punish limit indeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
