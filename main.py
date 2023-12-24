994dc3807bd2941b8c74ce73ae54d7f84362efc5
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:38:17 +0000

    Kk handle common steady whom

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_speed(distance, time):
+        return distance / time
