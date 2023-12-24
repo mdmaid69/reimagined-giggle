610da30d971762381e18342e686243c98ec78675
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:17:07 +0000

    ours repeat trust busy back

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def greet(name):
+        print(f"Hello, {name}!")
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
