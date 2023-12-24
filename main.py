477f2ad2de0f07c78f7a98c72a4fe82147373b3c
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:08:50 +0000

    shake promise suck season perform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_str(array):
+        return str(array)
