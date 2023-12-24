7bd78ca1f558bed604c7edc42846b8090ed43571
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:48:01 +0000

    treatment present with wherever comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-from collections import Counter
-print(Counter("hello world"))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
