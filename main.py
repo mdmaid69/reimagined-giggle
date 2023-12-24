4cfcbe9f6c234101e930e3865ba9fa74ec904a67
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:31:43 +0000

    spot priority takeout develop club

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
