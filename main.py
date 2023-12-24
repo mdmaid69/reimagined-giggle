cae0b01a4a9f5d769d699f19bd8a72c7f6144220
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:03:50 +0000

    effort clean convinced can likely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
