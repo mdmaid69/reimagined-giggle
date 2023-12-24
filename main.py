b80a932eb6b57db9fd277113537b01723b17a6e4
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:18:30 +0000

    concern Oo sharp list border

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def sort_numbers(numbers):
+        return sorted(numbers)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
