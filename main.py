05edcace0c57a51785fb60aff71eeb003bb81d47
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:26:27 +0000

    left lead belief interview sport

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
