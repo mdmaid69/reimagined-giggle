0da5516c467e0e4b462bc2cdab8f37094f5767c9
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:52:15 +0000

    report common approval easy cheap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
 import array
-def append_to_array(array, item):
-        array.append(item)
+def reverse_array(array):
+        array.reverse()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
