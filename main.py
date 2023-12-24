c67de1a33743d2c4a69a5785fbc3970d4ed70d74
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:19:22 +0000

    takeaway dick secret shopping importance

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-import time
-print(time.time())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
