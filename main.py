c54c6824b44c6a57cb1646db79327389e186fb0e
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:06:52 +0000

    callback mean unite single action

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
