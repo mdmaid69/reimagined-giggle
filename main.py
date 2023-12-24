4050e64b96e9d949f7820d44018632c37b26331e
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:32:53 +0000

    trap run sight effect bother

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
