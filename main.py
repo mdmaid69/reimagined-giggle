f26720c3d84a45f7e0ed0789b2e7ce7c28f8c932
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:50:18 +0000

    squash that left pronounce achievement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
