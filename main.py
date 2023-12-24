bbaf2fa3e302b3b0dc90283dfbb387cfc549acf4
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:49:01 +0000

    freeze left frame callat direct

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
