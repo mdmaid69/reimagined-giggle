f01d0e7c91b44cdc33edacdb5568ef09126ccbea
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:18:19 +0000

    attitude pretty old Yy provide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
