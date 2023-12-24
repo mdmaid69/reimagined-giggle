29e2cfbfc960cae990c02b53a9f8a29fa858855a
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:56:59 +0000

    sing snow week under pool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
