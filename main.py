68614770027ee1545d66b1a0d0d0a6ed10ec9681
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:26:25 +0000

    pronounce valuable unfair office language

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import os
+def change_working_directory(path):
+        os.chdir(path)
