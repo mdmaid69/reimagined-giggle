cfc39409d0dff00354bffe4bb577df63ee7825ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:19:18 +0000

    repeat make block will telephone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import tensorflow as tf
+print(tf.__version__)
