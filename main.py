383c15d193194b0b7af223981bbdb3f8ae559e2f
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:32:08 +0000

    he while cost steady lookup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_current_directory():
+        return os.getcwd()
+import array
+def get_array_as_int(array):
+        return int(array[0])
