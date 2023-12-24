8d9fef57afd7affeae30ab43181624040511d6d6
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:26:50 +0000

    rubbish backwards Bb under taste

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-print([x**2 for x in range(10)])
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
