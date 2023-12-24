53b3188875a8d02b54329091b3c366d167c88e63
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:41:54 +0000

    separate will hold direction goaway

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
