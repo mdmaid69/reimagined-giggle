87293e458df587586384e06254d7497f6b0b8868
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:27:57 +0000

    bridge strong always risk its

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
