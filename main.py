487bd2785dede89a4b3ef685a70fd9c9afd8f6ba
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:05:21 +0000

    sweet century scale rescue nigga

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
