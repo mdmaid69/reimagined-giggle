c0299827902a5321ff591556ddae7163a0dc377d
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:58:47 +0000

    fun immediate calmly responsibility lay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
