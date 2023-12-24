821fa4bb6d902e921d7b927f5c6556103bdb1847
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:41:11 +0000

    sting discuss note silent honest

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-from collections import Counter
-print(Counter("hello world"))
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
