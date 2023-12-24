a0459381fe4ad87d3dbd47f6c6669631e2ee57e0
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:14:40 +0000

    sweet empty Ww new workon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
   import os
   def get_file_size(file_name):
-        return os.path.getsize(file_name)
+        return os.stat(file_name).st_size
