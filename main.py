71406f3301b2eed6be7933ed545e684f0d5eefa7
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:28:44 +0000

    unable custom blow slide read

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def reverse_array(array):
+        array.reverse()
