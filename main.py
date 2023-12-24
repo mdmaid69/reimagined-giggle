79b1cc6858edbe6395d91e777edc3fdcb7360ff1
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:44:48 +0000

    nearly against pen boat substance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
