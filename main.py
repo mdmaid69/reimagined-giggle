0ae0ae0ea25d43a54fed46c596b7ebaa55f87071
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:27:21 +0000

    change second rush sting either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def reverse_list(lst):
+        return lst[::-1]
