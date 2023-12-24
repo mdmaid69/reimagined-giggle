fb31b2f1482be52654e859e543879cbc92f679ed
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:43:19 +0000

    trip often reduction worried other

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
