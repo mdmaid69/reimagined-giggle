1148b43e1e0da9f97eb2500548b564da4331c2fe
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:51:11 +0000

    history trip strong matter bringdown

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import datetime
-print(datetime.datetime.now())
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
