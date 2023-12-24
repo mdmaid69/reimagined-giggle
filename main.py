0d516845ecb22ae3d1fb09665ed091e64fe5f578
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:55:51 +0000

    entrance young country underneath charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
