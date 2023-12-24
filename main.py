0c1b97e1e3770a677cdbf34cb43846f75ae9067b
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:56:05 +0000

    early butter familiar surprised consult

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def multiply_numbers(x, y):
+        return x * y
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
