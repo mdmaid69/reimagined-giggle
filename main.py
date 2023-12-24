059b7aa3a60f49eb00f4cb278ee21f9ac9580693
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:39:24 +0000

    cap neat earn message doubt

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import platform
+def get_os_info():
+        return platform.uname()
