f6f028eb2ab9339bea2edcb8e3a535d73607ce79
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:28:03 +0000

    weather declare explain he here

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import random
+def roll_die():
+        return random.randint(1, 6)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
