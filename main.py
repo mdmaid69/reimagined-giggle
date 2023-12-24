5dba03e773c6c5b5defdbaae737c4dddd4f1bd37
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:21:22 +0000

    callback whenever favour usually bin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
