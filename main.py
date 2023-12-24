76d659dbecf162701a8d4c0f66efc3325948cc75
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:36:04 +0000

    fun mine impressed example attempt

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def find_max(numbers):
+        return max(numbers)
