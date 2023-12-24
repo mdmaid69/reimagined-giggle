eb872d7aa2143060cc77c09fc71f0951d6e6340a
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:50:04 +0000

    pronounce rest though turnaway skirt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
