d84126921130c63997ac7fcffb9da468640ce35d
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:04:35 +0000

    by divide tap comeback space

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
