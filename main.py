0a25919da9c8b4c7efdec8e4038d3196ddd23c9f
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:23:05 +0000

    principle below never threaten face

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
   def get_current_working_directory():
         return os.getcwd()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
