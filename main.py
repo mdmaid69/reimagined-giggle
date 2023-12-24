92b185b724c3368d57393fda0dc2937d3854340c
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:42:19 +0000

    myself transport freedom because journey

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_current_working_directory():
+        return os.getcwd()
