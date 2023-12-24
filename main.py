42365e07278a50a3334e19a04f53a29e267c5873
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:18:02 +0000

    announce dish heat equal buy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_floor(x):
+        return math.floor(x)
