d209b53225e4457aa353ff7c323045249919cddf
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:56:05 +0000

    mind throw join noone safety

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_work(force, distance):
+        return force * distance
