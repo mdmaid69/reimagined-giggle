7bc4276916ade1deba66298977acf605a3175000
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:34:12 +0000

    light hand represent collect similar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
