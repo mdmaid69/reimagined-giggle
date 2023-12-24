332fc5282f5bb39970fb9bb70590848fcb1384ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:41:16 +0000

    age success judge bringback further

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import array
+def get_list_from_array(array):
+        return array.tolist()
