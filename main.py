32ffd7f063762423afa9e701adca605a4ef5de56
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:33:07 +0000

    period throughout turnup whichever past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import logging
+def log_message(message):
+        logging.info(message)
