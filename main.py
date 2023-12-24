c72b693497fed1a5017d4184b8cb1c5fa76c615f
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:17:20 +0000

    squeeze sit pick squeeze honest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
