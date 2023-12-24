7c91c8075e7e8d51739ca6b0143094dd58ed295d
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:45:14 +0000

    sore in callat further sign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import platform
+def get_os_info():
+        return platform.uname()
