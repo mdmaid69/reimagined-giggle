3734b215f124ef6488f2d4a86f899759f5406eb2
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:41:34 +0000

    official this say tone track

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
