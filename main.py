e64a567fa6eebb7526c34a74e71bfa5c4fe93245
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:12:12 +0000

    murder note interview bill surprised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def remove_from_array(array, item):
+        array.remove(item)
