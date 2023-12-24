34ef911491cda8d4197a6727237b16458e6c1b56
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:23:32 +0000

    cinema turndown mood uponon unite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import sys
+def exit_program():
+        sys.exit()
