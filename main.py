2009b4f8b6f6393f1f498169d8cac71cead1695a
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:38:01 +0000

    spite experiment surprised uncertain choose

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
