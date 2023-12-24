8c54c2b1a22a9e65994d38528e1bbe1517108c83
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:32:56 +0000

    side coat lift corner woman

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
