940596d4d2f2f0255e11bde436b75c427d186887
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:50:15 +0000

    path favourite building unkind Mm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
