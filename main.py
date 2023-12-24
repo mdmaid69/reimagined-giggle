ada85b3622607a675474d89ab100b4a46cfff736
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:50:39 +0000

    sleep reject delicate cum forget

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_density(mass, volume):
+        return mass / volume
