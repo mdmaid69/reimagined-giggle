778e6eefb433c788a417ba2fa0ac6f7be2462782
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:42:11 +0000

    goout present interfere switch pay

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-print(sum(range(10)))
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_power(work, time):
+        return work / time
