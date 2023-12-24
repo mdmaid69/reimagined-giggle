a04198a9e7fe4ce9555a63f7fec9fbc080631105
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:19:48 +0000

    takeout oil path second willing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
