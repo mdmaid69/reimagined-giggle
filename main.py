cc155161523a2c2823f4d5e339eb8f8bf0cba9b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:58:49 +0000

    record how flight meanwhile wander

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
