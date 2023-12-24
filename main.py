7788ebdd6fab670f5b4c969849f77ce55fdc8e7e
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:27:28 +0000

    spare sharp statement brief danger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import collections
+def create_stack():
+        return collections.deque()
