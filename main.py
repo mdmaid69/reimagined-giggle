de35c73e499ab905b2bd89836f0f12854431fcb2
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:42:33 +0000

    flood border cook patience part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
 import sys
-def print_python_version():
-        print(sys.version)
+def exit_program():
+        sys.exit()
