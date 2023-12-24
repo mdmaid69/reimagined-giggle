69c3b851bfe5ae4aac3c3b8d7b26ea82e5edc6fe
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:54:17 +0000

    plant uniform tread flat file

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import numpy as np
+print(np.array([1, 2, 3]))
+def convert_to_hex(n):
+        return hex(n)
