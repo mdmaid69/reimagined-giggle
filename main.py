b43f11255c3d4f28f4bfc86900c98f43601a31b4
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:08:51 +0000

    tear tight shade borrow song

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def square_number(x):
+        return x**2
