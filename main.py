519502f9add21c77098ec98bdacf61358b6a087c
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:37:00 +0000

    further crazy wish active early

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def get_array_length(array):
+        return len(array)
