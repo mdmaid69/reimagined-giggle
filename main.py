144c149041947fc2cb950da54685c11546ecb7d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:49:46 +0000

    Itself quickly getback chief noisy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_int(array):
+        return int(array[0])
