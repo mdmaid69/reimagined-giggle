2a88c87ddcf976dddbc5eb14fd2ad5f9dda6291c
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:39:33 +0000

    disturbing quality now either naturally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
