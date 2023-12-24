f960592a3c97c2f0f9f58a6aa77c3e99ff9f5c1a
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:57:28 +0000

    spray hair 567enemy congratulate modern

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
