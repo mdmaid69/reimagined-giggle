afe301d88dd0304919635e62ce78a15de30a6c17
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:04:08 +0000

    minimum aware ours trip bag

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
