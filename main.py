1e5b9b85daa39e7d2b2e47e1cba98ca00b52ccc1
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:08:12 +0000

    large step emphasis dry wing

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def square_number(x):
-        return x**2
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
