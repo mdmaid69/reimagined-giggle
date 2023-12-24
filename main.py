0e70a9d0683b6eeca9ff32c297e404ccc45bbf96
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:24:29 +0000

    little play separate bridge shame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def multiply_numbers(x, y):
+        return x * y
