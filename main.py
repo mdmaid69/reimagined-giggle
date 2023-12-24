fec16b14719254e60ef01fa9f276a26a46ed6173
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:54:33 +0000

    classroom cut hold argument me

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def count_elements(lst):
-        return len(lst)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import tensorflow as tf
+print(tf.__version__)
+def reverse_list(lst):
+        return lst[::-1]
