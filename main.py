426977b502e4e72a493ebe9fec5db5d6df418755
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:15:02 +0000

    claim deliberately choice fence love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_list(array):
+        return list(array)
