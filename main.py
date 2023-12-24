45f4b21f1acf9d083802e4a9967f516cea1725ce
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:55:05 +0000

    in stand wind beneath justice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
