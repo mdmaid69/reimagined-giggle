101c548ca95b9f597bd960018e58e24da31f10b3
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:03:53 +0000

    bend office attractive rarely note

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
