af4a0f96aae40b5e7985cd2d95dcfda39ff6b127
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:49:58 +0000

    buy pleased 570enjoyable happy street

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
