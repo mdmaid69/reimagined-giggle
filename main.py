947710ac66321a495243570e74eec561be4bc9eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:44:43 +0000

    gentle risk whenever organize relation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
