04096e62dac288b26c7f0550f01e0e6f521165cf
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:52:48 +0000

    obvious combination motion train tonight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
