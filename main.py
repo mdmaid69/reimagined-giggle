8f5713514122c683a04530f8c37be5138e76da32
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:28:12 +0000

    choice calmly air possibility ours

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
