15cb3d70ce86df533037cc2b07dee474b0e00e8f
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:14:44 +0000

    foot rely hurry inform suppose

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
