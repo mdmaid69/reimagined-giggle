d2c096e0618a94cf3904b9c9f4dd91734ac9fa1f
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:39:15 +0000

    year especially mood teach divide

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import random
+def roll_die():
+        return random.randint(1, 6)
