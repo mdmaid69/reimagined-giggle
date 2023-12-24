4fdbf1648d1c57a630df6ead705290e716d6f203
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:19:39 +0000

    community serious uncertain send swallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
