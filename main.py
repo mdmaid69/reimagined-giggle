8cd63fd6ea44e18d15a5ab176086f11a63702f62
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:50:55 +0000

    will determined runaway immediate forward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_max(numbers):
+        return max(numbers)
