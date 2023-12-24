df54a9eebafef2820641d7837e2608eef95175b4
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:25:18 +0000

    opposite remain today tend quality

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
