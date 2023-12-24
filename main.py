b20d2b9dbb6d095b556d035bc917877cfe715f5b
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:33:17 +0000

    thinkthrough central rescue conduct false

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
