2cb49295c1390f57275b4fad89560caa73c190e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:57:24 +0000

    high above cruel traffic stupid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
