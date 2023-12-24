c22b404c328de4f9704128404e99c581300072db
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:22:26 +0000

    whole escape trust takeout goaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
