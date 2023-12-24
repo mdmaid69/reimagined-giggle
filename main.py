1c86a9e4675c1977f614cca340c1c16147516c0b
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:44:06 +0000

    plan all roof like shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
