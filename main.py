795615b1312b0903f3a9455685b9862207b99aa7
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:04:13 +0000

    operate bringback suggest official plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
