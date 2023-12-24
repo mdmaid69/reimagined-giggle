01ead0ba94597e342b936f3ad28d720b51b2d323
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:40:42 +0000

    mention then pump anything comfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
