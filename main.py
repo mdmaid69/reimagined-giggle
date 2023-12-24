32bc404f46cc026ec39f293132bbb11db6bc78fe
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:20:20 +0000

    science male cheat debt Kk

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
