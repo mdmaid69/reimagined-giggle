248a015c45fb3d39e5211b837a3c6de1b4e0feba
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:57:35 +0000

    visit visit Ff song theirs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
