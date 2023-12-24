760cca27a325dc2e93850a5a34b25560c2e0cf78
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:01:08 +0000

    container software remark joke notice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
