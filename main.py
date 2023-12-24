8df98517b8ed151f93229293cbb9579a8cc217fd
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:26:39 +0000

    best away active push gap

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-print([x**2 for x in range(10)])
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
