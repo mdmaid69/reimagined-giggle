3da98cb470aa5a31ebe3ba733faca8bf1222479d
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:40:12 +0000

    weekend occur book hair explanation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
