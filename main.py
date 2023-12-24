0eb8c86b09fa4e9c49613a672c74ed0a8ac2b192
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:13:33 +0000

    light state joke make value

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
