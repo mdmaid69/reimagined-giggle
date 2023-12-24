4a8be4009acf3cd987c69accc14c11a0cec7f189
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:42:35 +0000

    job plan relation neck rain

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
