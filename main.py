5c163ee77ab01cd7d93cc4f6496798c8eafeaf73
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:19:22 +0000

    attractive port rich juice decision

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
