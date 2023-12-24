32c68de355294cd7a5c86afe264828ad4bf8937a
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:27:19 +0000

    search reserve paper lock sea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
