ae5118792900fee6286903229530988c353a5866
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:19:56 +0000

    and runover turnover drop either

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_acceleration(speed, time):
-        return speed / time
+def count_characters(sentence):
+        return len(sentence)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
