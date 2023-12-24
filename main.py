a0394dd128c321abbb948cd47ff6046c8b4c4e1f
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:09:20 +0000

    law keepfrom bad sea interesting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
