e8420a99274cbac7ad82e3777c0ebf3e46a8b1a6
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:38:20 +0000

    apply run murder practise genuine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+for i in range(5):
+        print(i)
