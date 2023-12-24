3f0b61e59d35841a1c7b43ba0217c88252702611
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:58:14 +0000

    record beside town doubt it

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
