c48e34602b82583335755e9875cbd4437c0f164e
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:01:33 +0000

    could hurry content open question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
