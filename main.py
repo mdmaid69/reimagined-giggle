4d0a80521a076397bc51141170177f3c53348dd4
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:05:28 +0000

    actually round distance taste another

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
