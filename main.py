53bce07357abf0e508d352f975caa31614e75121
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:41:54 +0000

    imagination strong rescue cover view

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
