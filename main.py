415dcb2837abd4d22e79861ce86e88e91e5d40cd
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:55:41 +0000

    lay delight before print organization

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
