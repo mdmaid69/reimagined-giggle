d0452c6b9e6b935a063a72d4901e6c45f8417cb3
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:16:18 +0000

    dear walk giveaway battery influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
