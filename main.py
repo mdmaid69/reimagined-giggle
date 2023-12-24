fe3ea0cf84d3e5b3dd3b5d318e68ec235221ff65
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:11:12 +0000

    content describe memory bored cruel

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def find_min(lst):
+        return min(lst)
