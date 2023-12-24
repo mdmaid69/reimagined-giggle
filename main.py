c40437694b9730e66ca5891689c647849eca1a88
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:58:43 +0000

    dance fold turnover hit acceptable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  def square_number(x):
+        return x**2
