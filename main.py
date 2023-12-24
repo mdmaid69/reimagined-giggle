a6ab850f4a72ab795886377bafd1e8e2369577cc
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:56:09 +0000

    takeout suddenly closed some demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
