9e6d38210e61ef91665264d0360a1107ad90bb34
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:29:12 +0000

    which Mm task home hollow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
