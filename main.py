c43fc04cee5bb56142c4eabf329e0e70449cfee5
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:14:29 +0000

    bear up lookafter lesson approve

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
