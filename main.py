b2d9f42cbf331755e6f85bef18f8558ffc52f0cc
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:55:56 +0000

    pride experience full nice Itself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
