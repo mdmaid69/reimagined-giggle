95eac83712ba2025cbbc89c511b70bc7e800ff47
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:17:13 +0000

    road lonely healthy fat both

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def find_max(numbers):
+        return max(numbers)
