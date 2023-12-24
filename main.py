a23985ec2d63cbafe2283746e12745217bc0f4f1
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:53:24 +0000

    easy boat strong environment difficult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
