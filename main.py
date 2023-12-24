b641a02bfe3676a7e55a8770ca8bdfa45fad7cae
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:06:55 +0000

    learn under crowd tragedy free

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
