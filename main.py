bc6df4bd841348245ccd87b7d60dc3885f504599
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:24:47 +0000

    influence tape power fast regret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
