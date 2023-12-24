3d8aae8d8761a600f79c66a00fa3c738e46253c3
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:02:30 +0000

    bang image empty kick Ll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
