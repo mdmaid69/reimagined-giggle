7b12e36d7041eb62699f7736fa89cb6161828127
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:12:27 +0000

    vanish far tough likely quiet

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def sort_numbers(numbers):
+        return sorted(numbers)
