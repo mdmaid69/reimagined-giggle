629748680946d6dc408ba1f849c6b6eaf0268cc1
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:51:56 +0000

    wander project open rely yourself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
