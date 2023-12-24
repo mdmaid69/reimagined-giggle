3c77692c900f8965cbd19ae2a4eec335186a5d95
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:13:20 +0000

    matter takeover deserve feature ever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
