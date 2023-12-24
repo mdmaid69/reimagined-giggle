3dd44c671d27292b9141881c22ecd0c893b1dfef
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:02:21 +0000

    centimetre myself contrast present temperature

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  def is_odd(n):
+        return n % 2 != 0
