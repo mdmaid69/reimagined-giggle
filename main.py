38b6d3de373846d9345f2f5e7eb13d7a7a345e1c
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:40:24 +0000

    arrival all meanwhile personal try

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
