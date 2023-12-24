e0a6ae0981643fcb6192956ab643d6239d3ba2eb
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:07:06 +0000

    exchange hang judge perhaps clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
