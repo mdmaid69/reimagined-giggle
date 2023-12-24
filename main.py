17402b3c797f437ab23066d77efb6b9fa8c4b5e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:02:31 +0000

    more round sometimes wish disturb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import collections
+def create_queue():
+        return collections.deque()
