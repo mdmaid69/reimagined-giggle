8429f18d03cfe7ece408c5f886944c0f49f87f7c
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:32:14 +0000

    couple surprising content contents bill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
