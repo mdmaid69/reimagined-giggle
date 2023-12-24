3d398a1487d977e71183cacd0632989a2d32b916
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:12:44 +0000

    bite house nice payment cope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
