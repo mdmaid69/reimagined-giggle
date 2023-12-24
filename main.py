6e8ccae2b76f0ccf68dd2e3cbc946f9125a2778c
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:22:52 +0000

    ashamed boring obvious bringdown into

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
