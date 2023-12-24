6594f4a13989f716b0014e372dde1cc401e31df5
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:23:00 +0000

    long sharp us defend each

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  def convert_to_hex(n):
+        return hex(n)
