5132b5427f397ae35dbabea0055c3bc5a9c8374a
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:37:12 +0000

    peaceful bill ashamed excited and

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def is_even(n):
-        return n % 2 == 0
+def is_odd(n):
+        return n % 2 != 0
+def calculate_acceleration(speed, time):
+        return speed / time
