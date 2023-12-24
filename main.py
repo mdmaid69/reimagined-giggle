76b52561e47785483b6ec2a8683a0a4915203298
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:37:13 +0000

    Ll experiment declaration children whistle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
