c6d58bb3bb1a732eb222037e594aba9dd7f894bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:33:57 +0000

    getoff round supply intend Hh

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
