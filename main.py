04dfaa68297870205d1be8b6cdecf2a0965b397a
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:55:24 +0000

    accidentally start be bright basically

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
