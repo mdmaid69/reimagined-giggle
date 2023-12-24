b127664554b82e73cec384f0bbe0d0cac5168e5e
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:23:36 +0000

    teach able raise grateful scream

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
