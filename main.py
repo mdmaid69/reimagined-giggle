a1c372addf38065151f8122501562762c12e906e
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:00:03 +0000

    heavy meeting cheek intend wheel

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
