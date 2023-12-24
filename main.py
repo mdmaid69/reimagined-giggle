f9737f0bfbe73078ddcd44ee8f43f2f96444a0d6
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:39:15 +0000

    ask soil exact desperate let

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
