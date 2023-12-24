cb4d08023d17874f922cf87267e6ce1360288416
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:20:41 +0000

    sticky return gap among mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def count_elements(lst):
+        return len(lst)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
