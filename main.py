2450509d640dc472fc517c99904465001087249e
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:16:19 +0000

    571enough score expensive high destroy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def reverse_array(array):
-        array.reverse()
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
