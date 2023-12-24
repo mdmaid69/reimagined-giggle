b603566dac68d4306b0d767cc41c8feea1c45ef4
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:31:15 +0000

    holdup 573entertainment squash weigh fun

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-print("Hello, world!")
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  def sort_list(lst):
+        return sorted(lst)
