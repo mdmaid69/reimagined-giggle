7a010cda57d7ccb7a8d3567d7edbb6cb34105ac8
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:02:52 +0000

    except comein conclusion pump market

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
