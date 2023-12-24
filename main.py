accd536609444642b0a914b5af91989e424ceb7e
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:59:14 +0000

    ride themselves high town without

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_area_circle(r):
+        return 3.14 * r**2
