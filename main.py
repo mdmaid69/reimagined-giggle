bdfc221e336cdb21b5fa01a02670d03381c56e62
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:19:17 +0000

    cap official visit left high

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
