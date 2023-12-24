869f918101d1e244eb57b7f668ebe9e72feb2ca7
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:50:08 +0000

    small frightened abroad hope health

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
