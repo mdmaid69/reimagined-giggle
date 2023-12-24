cec5ea94fa20a3f42c85e700a35dc758dd9dcc0d
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:07:14 +0000

    belong cope upset break achieve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
