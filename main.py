9d8eec81e22973f7b7ad3367ff42c4c9c1dafd56
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:54:56 +0000

    quickly illness smart cruelty Ww

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
