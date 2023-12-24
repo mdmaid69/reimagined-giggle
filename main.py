4fba4b84fefafe81c2cef6a959f6a7ca8044a971
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:45:08 +0000

    charge obvious ring pipe equipment

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import time
-print(time.time())
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def count_elements(lst):
+        return len(lst)
+  def is_odd(n):
+        return n % 2 != 0
