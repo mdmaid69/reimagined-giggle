0a80df9330b88e40c36dab8f97b465fc7cf5eef1
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:47:34 +0000

    split so break healthy tear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
