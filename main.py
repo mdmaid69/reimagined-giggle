b5f2d87c3421e18926b8934e73e3782355fe6795
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:15:11 +0000

    as receive mark programme fault

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
