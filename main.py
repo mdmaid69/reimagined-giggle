a845ede3711256c84838f4da4044fb2adf4c550d
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:18:39 +0000

    full gift bound card war

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
