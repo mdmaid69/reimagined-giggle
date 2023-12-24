20099f0f715ddeb4fc67c6e62236523495aeb440
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:19:32 +0000

    table fair laugh earn connection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def add_numbers(a, b):
-        return a + b
+import datetime
+print(datetime.datetime.now())
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
