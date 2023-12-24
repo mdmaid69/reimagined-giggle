cfab03f6f9368e4d683ed92dade27edd34080cb7
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:57:25 +0000

    sail giveback maximum aware middle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
