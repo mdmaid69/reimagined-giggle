34168d5eb52c0fe1057eddbd31d9555eb18f53f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:52:56 +0000

    within whatever among firm wind

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
