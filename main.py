6c29e0c947e6cc1e25a23d605eb52560eb1ddf2a
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:58:50 +0000

    switch divide guess difficulty single

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-for i in range(5):
-        print(i)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
