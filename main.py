b1cb7db9213335aaf673dd2526daba30a54049c3
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:31:14 +0000

    find reasonable keen rank practical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
