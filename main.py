e8a30e33e2ab1c6624e0726c5e59945c51ba82f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:49:58 +0000

    course tool class century stare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-name = "Python"
-print("Hello,", name)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
