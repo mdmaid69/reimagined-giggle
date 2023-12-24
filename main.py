d335aae9c97c988a42fad9158b9018bea0b684df
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:56:36 +0000

    science run song obvious Yy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
