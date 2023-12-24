89c95a2605349041f5d696040f50d3aa950a7a82
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:53:15 +0000

    speak record aim play young

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_length(array):
+        return len(array)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
