6b9cf7d8f2e56af24da7714dfa78373428c5d09a
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:59:29 +0000

    mine odd straight bag after

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
