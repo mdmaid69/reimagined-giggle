ae91dfa55cc651a69e8b95cf85bad096516e790d
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:27:21 +0000

    extraordinary free middle check punish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
