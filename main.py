0067ee08c7b7e702273cd8d703418f7716bee582
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:13:23 +0000

    by unfortunate thumb begin perform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
