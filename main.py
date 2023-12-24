7863b3f7bbd00a561763f4bb82d491e8aa41faeb
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:22:27 +0000

    cinema hurry why evidence exam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
