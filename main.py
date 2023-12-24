2df6f9b94f1b4ded18d0426e5a22ee5155270bba
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:16:32 +0000

    before combine value progress suppose

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_str(array):
-        return str(array)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
