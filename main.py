7b70460d7ff9197f88bf740ad142ec0ac50d1462
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:07:23 +0000

    vote thank such in start

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
