4d207878358e6b0f8254ed722fbe02b9483f5002
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:34:39 +0000

    introduction follow talk 571enough kind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_mode(file_name):
+  def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
