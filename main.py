b21d7dad828dd81630b612e0b491dd5c042d5f7d
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:28:52 +0000

    embarrassed dick summer value contrast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
