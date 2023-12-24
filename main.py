73c665fda69701af1ea6dd4261725727cae32a71
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:34:48 +0000

    shall date turninto interrupt getout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def convert_to_octal(n):
+        return oct(n)
