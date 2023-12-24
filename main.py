8fb21ef6af376ae6a3e495c55cec96429490f5cd
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:09:47 +0000

    environment clue pinch will plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
