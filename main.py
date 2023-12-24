9b128f986b4d88068ff230dc8c2336b5c80f2844
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:29:10 +0000

    travel pocket gooff then cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
