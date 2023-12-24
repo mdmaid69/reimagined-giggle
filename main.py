9d3923a27f5decc4250ba58a7e3ebe37018914c4
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:53:07 +0000

    ticket sound love but punish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_current_working_directory():
+        return os.getcwd()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
