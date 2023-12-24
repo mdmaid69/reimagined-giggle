08698c02eadc1cd059e821865e05e92b64534434
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:56:38 +0000

    influence picture declaration rather broken

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import collections
-def create_queue():
-        return collections.deque()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
