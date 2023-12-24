b85ffead85a2bf24f45bf031ab1577c1788e371e
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:18:16 +0000

    takeback fond sit neat think

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
