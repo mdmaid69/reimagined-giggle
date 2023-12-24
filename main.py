74c9031fa6d15be42992129e0749ed071d1bb76d
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:37:06 +0000

    cut tense bar view smooth

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
