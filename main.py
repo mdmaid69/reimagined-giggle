33318e567f3f214e62eaba0e33bc4084272bf643
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:19:00 +0000

    challenge worry office afternoon fall

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-import queue
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
 
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+{body}")
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
