2bd6e0d9f256034d03cba2686736294d5edef31e
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:07:52 +0000

    occur tongue giveback trip mysterious

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
