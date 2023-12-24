cbd22e08972e554819686d7745100c177d645fd7
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:02:13 +0000

    concerned injury smell absolutely laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
