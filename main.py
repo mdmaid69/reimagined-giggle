9926f1ed8325b48f9a671b1567fa978d41c423bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:22:21 +0000

    ear enthusiastic hit attitude warn

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
