168fab08edf08dd131bf7e03b3ceb89c7fd2a098
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:39:22 +0000

    criticize pull least congratulate finger

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def remove_duplicates(lst):
+        return list(set(lst))
