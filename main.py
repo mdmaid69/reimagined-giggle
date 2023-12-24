ea536ead21bc39b1cb60af4b6bd1338e42591e2d
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:13:04 +0000

    forget getaway argue characteristic his

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def reverse_string(s):
-        return s[::-1]
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
 import math
 def calculate_gamma_function(x):
         return math.gamma(x)
