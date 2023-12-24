93187cd23cab1376810068e5ed024adeac379e7d
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:16:31 +0000

    under in practise wind simple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
