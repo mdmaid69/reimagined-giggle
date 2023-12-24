87e5230a005fe42c18d35733d9c4a3600b1829b9
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:19:08 +0000

    shelter situation system country improve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
