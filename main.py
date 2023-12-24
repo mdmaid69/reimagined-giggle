ff900fcc36ef387ffc9df7814bd91e852b668dab
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:43:44 +0000

    couple slice influence safe main

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
