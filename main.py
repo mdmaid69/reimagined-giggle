7a84636edfbba8b114d59dfd374a5f4c52f0726c
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:03:37 +0000

    please sweep century sure breakthrough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
