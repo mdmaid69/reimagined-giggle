47ccaa5b8971f88ecb83d94a601096c5cc5b7ea3
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:32:06 +0000

    path absolutely could uncomfortable necessary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
