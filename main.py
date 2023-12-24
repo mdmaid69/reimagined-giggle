ca08840dd949c635790e01c41d8f3a6997a9160b
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:03:08 +0000

    godown popular past copy boil

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def add_numbers(a, b):
-        return a + b
-def find_min(lst):
-        return min(lst)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
