453c7440984a8441bf41bf3d4901644b76706222
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:40:13 +0000

    giveout private cry Ee minimum

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
