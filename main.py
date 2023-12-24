05b10a9da0da88adc2bc0f995eb256fa456044d2
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:29:25 +0000

    any suffer thinkof maintain breakthrough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
