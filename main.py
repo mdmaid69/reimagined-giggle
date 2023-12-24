b52bb525b5664078a53ee7a0e650c1a7fb9299d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:32:43 +0000

    ilovedicks deliver bin completely doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
