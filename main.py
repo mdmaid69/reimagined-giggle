259e7f5e39c668cb65c3f9cfa7e15d6d9f2fd17c
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:51:22 +0000

    suit ocean loan flavour strength

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
