44ff1e129f7142b45bcfb31ce5c67a458780b94b
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:57:30 +0000

    group frighten broken object action

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import sys
+print(sys.version)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
