bdab518322554bea72910811f517e2310cc1ae21
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:20:28 +0000

    train obvious grow sight frequent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
