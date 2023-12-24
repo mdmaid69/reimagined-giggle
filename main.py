8bdb1c60e1e5df8f7512a452b4ec1d51e9b29670
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:28:35 +0000

    disturbing disapprove convincing impossible cook

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
