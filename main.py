339edf623ed6ed6d5fa7f3aa82943642999f5639
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:36:05 +0000

    scheme embarrassed arrival strict turndown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_force(mass, acceleration):
+        return mass * acceleration
