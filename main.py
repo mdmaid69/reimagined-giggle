268117ed31d11d594502060433cd2a6ff6414f82
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:27:03 +0000

    family makeinto by recently suddenly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def square_number(x):
-        return x**2
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
