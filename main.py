d63e2c7b74c4413c0dffd7f3a17ecd2adfdd825c
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:16:55 +0000

    care worst thank measure anybody

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def calculate_power(work, time):
+        return work / time
+  def remove_duplicates(lst):
+        return list(set(lst))
