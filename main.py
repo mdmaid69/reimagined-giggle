013af92cfbe6d23a750e5158842e15fee13942be
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:48:44 +0000

    steady respect taste fast loose

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
