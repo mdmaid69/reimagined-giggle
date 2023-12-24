17e31b7e2320397e74c79776cd0f3c23c392ea42
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:21:58 +0000

    takeoff reduction way turnoff callback

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_current_working_directory():
+        return os.getcwd()
+print("Hello, world!")
