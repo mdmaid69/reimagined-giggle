e17fe95e9951289c615edcd0d726e46f7f048786
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:00:02 +0000

    breakup house design Pp past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
