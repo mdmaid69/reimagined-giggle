215711b545ba63e5a3fd5c64851a2c10bc403262
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:31:29 +0000

    regret anything certain natural destroy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
