913eee9ea46523043527270375b237cdebc75b14
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:55:33 +0000

    cruelty unlikely area question slightly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
