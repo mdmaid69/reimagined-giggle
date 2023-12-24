0926baf793e87caefbefc3b00bef4a9e1727f85b
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:29:46 +0000

    discovery hit could basically answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_speed(distance, time):
-        return distance / time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
