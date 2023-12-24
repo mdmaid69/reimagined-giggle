b7b8134d5b4746cf13db9eda5af9953452d986b2
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:57:17 +0000

    blame transport dig support image

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
