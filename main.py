d79e8783aae61bf17e11e99ca442059a1aa0b102
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:52:33 +0000

    form contact decide contribute stone

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def convert_to_binary(n):
-        return bin(n)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
