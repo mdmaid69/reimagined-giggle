ffe4ec6849c031089364740cf5f49e36174d1e50
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:48:15 +0000

    reduce depend appearance method extra

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
