c6cd0c31edfbaecbfbae3d04cb63d681a216a610
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:29:36 +0000

    new lock figure skirt willing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
