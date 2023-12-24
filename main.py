778be720c04ca68f04e7214dc529402d69dfce2c
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:32:45 +0000

    worst keepfrom floor reply unite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
