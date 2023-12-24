58636d25776b3cf2156fdb75de2b00924a573b37
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:16:00 +0000

    world body extremely obvious certainly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sys
-  def get_python_version():
-        return sys.version
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
