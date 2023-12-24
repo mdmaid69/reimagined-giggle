e5229657f54dd4d7622467ac160f72b20dcf2451
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:31:45 +0000

    between pot search general cum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
