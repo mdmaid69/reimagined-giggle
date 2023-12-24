dd8a95c414b50efeccef2e7c4682be32beb90906
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:43:30 +0000

    sore unhappy space advanced scared

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
