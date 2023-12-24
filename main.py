17cce1b4bb0a489a21b14f0dde84a22e304539b7
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:28:08 +0000

    although demand callat shop sleep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import os
-print(os.getcwd())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
