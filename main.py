34c5d36f5ec6052a47c8e5dcb42bee729932e840
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:46:18 +0000

    bad rest obtain ring sort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def multiply_numbers(x, y):
+        return x * y
