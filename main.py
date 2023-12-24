cc1dd991866de4f112c38163505b8c84feda69b8
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:16:56 +0000

    crazy slow slowly reasonable wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_speed(distance, time):
+        return distance / time
+import os
+def get_current_working_directory():
+        return os.getcwd()
