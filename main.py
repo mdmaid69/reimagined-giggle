66131de49953b3b05d3eb68bfa88995299670704
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:48:25 +0000

    table know meet nerve stir

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
