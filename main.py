752d4d4822649ab3f6cab5e1a634253ce6949234
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:15:17 +0000

    thing boat major pool extremely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
