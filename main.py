373053e0883dad95d0160b8c955f2de33ac50333
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:01:09 +0000

    record ride people such willing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
