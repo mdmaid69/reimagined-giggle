0d4765f752447aeb5da73e5359daa7b0307826e6
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:19:58 +0000

    protest rely theirs recommend tune

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+for i in range(5):
+        print(i)
