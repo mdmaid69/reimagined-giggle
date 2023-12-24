7094638e23d58923e7f8bdec0c17ad045750f8e9
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:38:04 +0000

    side doctor certain project stage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
