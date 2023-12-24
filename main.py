2d5ead53cf9e093d9b34cce121bfc0451c34face
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:38:53 +0000

    black poison downstairs advice contents

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_exponential(x):
+        return math.exp(x)
