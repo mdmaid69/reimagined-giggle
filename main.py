e1383fa94a7c369a5515f5c2a8bf0a951f5c8efa
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:32:35 +0000

    university soon crazy skin lift

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
