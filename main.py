2d69a8ea578f574583a29ae90d595588e505b9c5
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:27:39 +0000

    unhappy gentle kill window point

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-print(sum(range(10)))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def reverse_list(lst):
+        return lst[::-1]
