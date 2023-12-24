af2c5f7b4360c574cb21bb55bb3c794cf4cbabd9
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:37:34 +0000

    recovery scared sit getoff peaceful

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def find_max(numbers):
+        return max(numbers)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
