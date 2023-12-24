111719fc22cbdc169876a029847cea879626a75c
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:04:54 +0000

    fond improve lead whistle nature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
