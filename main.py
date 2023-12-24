c2194084e47d9793275db128ca048a1e0cdd1895
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:59:32 +0000

    influence Itself pupil mouth drink

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_unique_words(sentence):
+        return set(sentence.split())
