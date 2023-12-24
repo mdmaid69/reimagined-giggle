8ed45ddb8b3f1214259d87a5dc41b3d894212870
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:46:57 +0000

    laugh come honest announce star

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
