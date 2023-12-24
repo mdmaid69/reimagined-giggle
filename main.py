71792382a41f1a9bdff2339f8c44cb594e13d8b0
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:52:42 +0000

    cinema block crash cope wait

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import time
+def get_current_time():
+        return time.time()
