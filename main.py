8f026a6b35e3d614facfc5dec55a827463c73dd3
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:01:06 +0000

    desperate scream short slightly old

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
