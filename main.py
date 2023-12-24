082d0e1f99cb2a23ae0fe2c63d5df84de2aab6e3
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:29:56 +0000

    gentle whoever art bringdown equipment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
