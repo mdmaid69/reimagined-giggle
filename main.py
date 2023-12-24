0287e71cc2632375c3300ffb52283f3b23207a55
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:51:28 +0000

    apart 570enjoyable deliver getback convince

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
