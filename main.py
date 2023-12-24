b116059869bda7cbec3a1e59dc504617d834140d
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:51:01 +0000

    comment friend rarely important undo

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
