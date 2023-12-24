31ae204211db9c173227beddc5e3803e9452aceb
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:33:42 +0000

    goodbye long dance violent sleep

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  def remove_duplicates(lst):
+        return list(set(lst))
