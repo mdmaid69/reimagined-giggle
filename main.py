4fc584e53048eb6358da754205562c4b09af64c0
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:58:29 +0000

    touch dull congratulations health glass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
