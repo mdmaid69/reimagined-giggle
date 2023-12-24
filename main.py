66582656b877678eaee41d32aee2820d4c89ad3c
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:27:17 +0000

    achieve ours behind contribute error

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import sys
+def exit_program():
+        sys.exit()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
