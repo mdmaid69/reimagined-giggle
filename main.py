a7ba30f23aeda1169ad662654bb4a6aea5e1b93a
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:43:27 +0000

    send fat lean through left

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
