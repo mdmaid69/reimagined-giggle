4cc2bcff886a8539bf4db16ac12a76b90c76e2a1
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:45:02 +0000

    shrink getaway but offend check

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_current_directory():
+        return os.getcwd()
