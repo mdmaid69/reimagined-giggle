96a486cda96d722182a5e0cd4059747bd95302a7
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:44:43 +0000

    acceptable break programme fortunately evening

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
