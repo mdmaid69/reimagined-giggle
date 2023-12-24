06adbf7a3c8a7c89eb3051309faff603768be5e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:42:37 +0000

    welcome load family manage receive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_work(force, distance):
+        return force * distance
