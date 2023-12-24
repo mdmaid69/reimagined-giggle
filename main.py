2508ea852be4352397f3b8266d7a0a619cb29272
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:59:09 +0000

    tap snow chance dance their

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
