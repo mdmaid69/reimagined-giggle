0b4c698cbff4f4e9c51e5f312472d9b8b61fe9c9
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:34:17 +0000

    something persuade ashamed be her

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
