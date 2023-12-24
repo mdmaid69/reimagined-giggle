3163288b28da6b3a2b4aa0d29192262648737a7f
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:05:52 +0000

    bear raw breakout news fond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
