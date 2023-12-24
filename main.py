df0dfa4e755c07edd4bb76353381985bcae8a738
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:52:28 +0000

    Yy upset street hospital deaf

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
