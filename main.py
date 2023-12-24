b23615ce02b9bafd74d73bddcbf9c2dbead92c3e
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:27:53 +0000

    wish only active rank pain

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
