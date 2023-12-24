bc8bc71738b86d3cf3ff2527aa289e2cee2ae127
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:16:22 +0000

    else herself proper sight produce

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
