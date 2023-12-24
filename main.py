eb92fd32fe230ff3051d0db606100a91c33a5504
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:26:28 +0000

    perhaps standard lookup thinkthrough container

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
