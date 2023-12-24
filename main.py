b296446922825737d0745e94bca5799fcb0f371f
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:14:16 +0000

    congratulate fit innocent indeed justice

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
