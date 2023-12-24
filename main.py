44e2fbe83f71679d49eb3b2d057b3b8881771d40
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:03:17 +0000

    kind violent doctor content OKorokay

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
