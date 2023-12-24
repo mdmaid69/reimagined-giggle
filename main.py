bbe095814f5f9a17d199e72396a9255897538c9f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:16:01 +0000

    objection equipment cheque tell ignore

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
