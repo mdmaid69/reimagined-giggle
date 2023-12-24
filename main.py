4a8d07d128d7deaeb3b2930e00e7068a6b804c13
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:08:22 +0000

    perfect avoid limit sleep everyone

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
