79a9a394626e47b09f60bc68672bd76cb5cee4fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:23:21 +0000

    too believe Rr substance scream

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import sys
+print(sys.version)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
