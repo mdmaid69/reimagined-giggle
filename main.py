404d6b40f777843fab5a495c1c140b8765c5a51d
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:04:16 +0000

    respect as brown extra cook

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def convert_to_hex(n):
-        return hex(n)
+import sys
+def exit_program():
+        sys.exit()
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
