7019d627a49ff6575109e43db7765e7c09cb28fe
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:37:10 +0000

    underneath adjective peace beautiful behave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
