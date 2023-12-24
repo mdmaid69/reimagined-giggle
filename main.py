775e06e6146ac0c7e200d7f4e44d26b18c52ecfc
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:41:55 +0000

    national emphasis honest public century

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
