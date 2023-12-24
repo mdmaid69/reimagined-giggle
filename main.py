50a73a1f8ce947d7bd2ff78632e3f85097188edc
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:59:40 +0000

    winter programme better image proof

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def reverse_list(lst):
-        return lst[::-1]
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
