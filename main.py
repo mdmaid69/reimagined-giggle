d760dc3959d57cd78d70d343bd6e7dbe9fd64159
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:32:54 +0000

    prevent price petrol affect minute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
