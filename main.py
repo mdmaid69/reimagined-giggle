cb261f7500e4e891aed71b3b8e188c379fb48f0e
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:16:35 +0000

    put greeting major as such

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
