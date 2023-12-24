7575b8cd231639014be7c28fc78f27200aeb8c2d
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:36:49 +0000

    comedown character typical thinkover great

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
