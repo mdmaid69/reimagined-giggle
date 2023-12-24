20be5260860bf4f4501b20a9b8bd2909b139c736
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:53:46 +0000

    dead ill keep street opponent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
