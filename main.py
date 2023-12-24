0c32b3036835f45ceb57a382cc80720c9268c95d
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:31:35 +0000

    like responsible number ask innocent

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def sort_list(lst):
+        return sorted(lst)
