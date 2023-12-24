234fcf6781577f75ee7f4bf04aabf6b51b2443e5
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:32:18 +0000

    worry shake object mine any

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_work(force, distance):
+        return force * distance
+def find_union(list1, list2):
+        return set(list1) | set(list2)
