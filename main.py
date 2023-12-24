1ee539e9abfe49039840e988252ad763dc68ffe7
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:32:51 +0000

    however body remove try expect

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+for i in range(10): print(i)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
