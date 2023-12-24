5e4aa8619b08e2c063d3dd75d01c351bb1dfc1b2
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:07:03 +0000

    dance heart tear over count

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
