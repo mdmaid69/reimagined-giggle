3b74358ead19e2b126a662085d9ec9dbd9a41426
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:55:38 +0000

    raw shine nearly popular breakdown

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def convert_to_octal(n):
+        return oct(n)
