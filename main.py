5a14c89aad021db376ce6602306eb5714271a35c
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:34:41 +0000

    transport takeout practise score rubbish

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import numpy as np
+print(np.array([1, 2, 3]))
