dcb2cfa2f6f84a2dd10ed519a8509ece26e8ee25
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:17:09 +0000

    formal recent message it birthday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def subtract_numbers(x, y):
+        return x - y
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
