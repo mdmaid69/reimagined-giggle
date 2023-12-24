a9ccfb25dc16dfec3ba8948f7c7160838f2c0403
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:21:51 +0000

    cap scene criticism upstairs surprising

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import sys
-def exit_program():
-        sys.exit()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import json
+print(json.dumps({"name": "John", "age": 30}))
