dc62ffd66a704fc78613b880d058185fa19e4e81
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:29:46 +0000

    circle spill hole expect guide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def get_array_as_int(array):
+        return int(array[0])
