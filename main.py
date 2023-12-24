d22c4b57fd49570724ccd78cf2e3fba31715e9da
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:50:16 +0000

    connected conduct remain laugh yesterday

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def divide_numbers(x, y):
+        return x / y
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
