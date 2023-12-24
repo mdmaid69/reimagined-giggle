27719609c894272c5ef5effdc29f8e76f42cb69e
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:05:14 +0000

    appeal smart broad where pretty

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
