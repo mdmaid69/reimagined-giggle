9a2ffe9bb861fbc2d10ac1cfaf9ff06bf08bc058
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:07:35 +0000

    duty happy time cruelty upset

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_uid(file_name):
+  def get_file_owner(file_name):
         return os.stat(file_name).st_uid
