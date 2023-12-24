dedb9c44545a04f4845e6ff2e5306a7f78ff1624
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:34:22 +0000

    interview yet quiet combine dance

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def set_array_item(array, i, item):
+        array[i] = item
