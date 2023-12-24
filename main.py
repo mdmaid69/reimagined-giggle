52f4a262c11d34bf0d6b9db4f62024958a2e38bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:46:43 +0000

    question Jj think excuse excellent

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_index(array, item):
+        return array.index(item)
