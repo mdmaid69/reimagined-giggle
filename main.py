c6ace89bf4a9fcf70a2414395c9e6604a4a62b2d
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:34:21 +0000

    calm turninto responsible regularly may

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
