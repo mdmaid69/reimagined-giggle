45e8600047b732548ba9124c1e9128a5a924f04e
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:15:04 +0000

    slowly dear over stare disappear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
