5909375db068d2e967b9550fa00a282f2ce0d266
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:11:13 +0000

    angry run ready goup part

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
