22302e2f202a15f95c2a444091c0dd48567516dc
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:46:39 +0000

    turn reasonable crash proof question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_power(work, time):
-        return work / time
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
