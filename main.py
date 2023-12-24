705dd9865e1218ac182cfedb6888ac4bc323fa15
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:56:21 +0000

    shallow performance organization measure that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
