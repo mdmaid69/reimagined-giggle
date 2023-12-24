c0b3487b7bd7094e76cc2e2f938cd71bc4e55282
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:55:14 +0000

    rich speed sink fit second

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_work(force, distance):
+        return force * distance
