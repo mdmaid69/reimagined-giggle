bd878eb47c987d7d8d601560d35f4d027fae398f
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:54:48 +0000

    spray creature information because lot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_pressure(force, area):
+        return force / area
