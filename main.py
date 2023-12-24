0266f268667f3ae0fe61487381195a97164e3cbd
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:20:54 +0000

    sound ever path underneath turnover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
