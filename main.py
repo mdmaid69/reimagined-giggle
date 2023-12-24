9a71e105157b8b2185840cc71532f14e4af6f5a4
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:51:11 +0000

    boat clear pupil sensitive pretend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
