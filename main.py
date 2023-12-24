8b840426a94e55273733d05495d4352579674bd6
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:13:18 +0000

    like anything simple building short

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import platform
+def get_python_version():
+        return platform.python_version()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
