df3f1c1fd5c7e94cca7d827a7140b1bf437a53bb
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:56:53 +0000

    goaway question date journey breakout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def convert_to_binary(n):
+        return bin(n)
