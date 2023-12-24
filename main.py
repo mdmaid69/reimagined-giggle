8fd70c5ac369d0a786b37b41ae0e5193116147d8
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:44:12 +0000

    short discovery exploration female build

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
