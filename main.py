01995c1245109105dcdc896cf09deb0a3d5738d8
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:21:02 +0000

    same rough release disturb belong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_octal(n):
-        return oct(n)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import random
+def roll_die():
+        return random.randint(1, 6)
