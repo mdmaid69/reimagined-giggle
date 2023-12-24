aa6b17af750af67064a2877f75ffcb65251301d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:23:37 +0000

    scene strict combination national pressure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
