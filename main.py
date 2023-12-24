f9a35d5e955bc094449d6888e93e8eff48a5ad95
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:49:14 +0000

    correct disappear speed lose put

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
