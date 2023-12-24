13f34d7f2927463142ab5bf7a617deb780e2755c
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:17:45 +0000

    behind used division separate public

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
