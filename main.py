6e2ca50eb471e2fe16e1ebf74979f96d6e57695e
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:24:27 +0000

    much miss charge breakinto happen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
