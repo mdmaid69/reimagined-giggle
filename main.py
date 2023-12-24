013a080000bf302ac9840cec203e49e2c57c9167
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:25:24 +0000

    row breathe figure wave power

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
