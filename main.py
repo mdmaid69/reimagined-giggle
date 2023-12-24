b588c5b77881d5da37fddc09d920d3312bf99546
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:06:31 +0000

    send spill crash along noone

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_power(work, time):
+        return work / time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
