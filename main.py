799f0d12a1558f6821662099b010e47feee961a2
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:53:18 +0000

    rate regard concerned rich bored

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
