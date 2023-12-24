aea2f01836a81994d3737bce3e3a2cce81572517
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:57:53 +0000

    too small cut edge special

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 def calculate_area_rectangle(l, w):
         return l * w
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
