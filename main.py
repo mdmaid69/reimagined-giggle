c83ab8f6ad94f02d4aab3acd736a7109a7e695c2
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:10:30 +0000

    Uu die clean in hungry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
