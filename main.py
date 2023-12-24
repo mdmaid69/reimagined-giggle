4af40ff198eeb42c15ddb6c79567cba440091370
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:50:47 +0000

    off material yes hardly debt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
