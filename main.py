5037dced2a0b1f5c96cd18af80c3a966a1ad0a03
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:34:11 +0000

    wake keepaway ticket home connect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
