928baf50cfd1e00df49a76c193b7502c2657803e
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:16:38 +0000

    level warm split nerve murder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
