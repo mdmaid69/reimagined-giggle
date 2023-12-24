b759f48e1e2f8253d871cd6f8c4161c2699e87cd
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:26:55 +0000

    join model Cc typical copy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
