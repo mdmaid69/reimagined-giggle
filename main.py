d10d791f3dba4b11b792b34c047fa50940c257bd
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:53:06 +0000

    belief quantity conversation lose proof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
