44d8fc7cbb17c2b5610b7a84491cc77e5437cc64
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:27:04 +0000

    thinkover however disgusting hate discovery

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
