184ca2f972ec7eb8d8963665f1a7920ced0e8027
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:29:32 +0000

    ordinary impressed politics instrument stretch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def find_max(numbers):
+        return max(numbers)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
