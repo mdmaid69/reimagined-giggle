86a34fda6b627886858defa0628db633d79b171c
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:44:34 +0000

    physical ground right count bother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import getpass
+def get_username():
+        return getpass.getuser()
