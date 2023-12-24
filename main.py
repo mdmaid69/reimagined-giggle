88ce83c7b1a0472365520e5e386e6e8cffd1f7be
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:49:23 +0000

    light no condition well door

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
