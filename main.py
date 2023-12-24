6b65d86f8794bc0bad6c3f2bb3f83fe411e0ae2a
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:33:53 +0000

    zero thinkthrough anyone rid middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import time
+def get_current_time():
+        return time.time()
+def find_max(numbers):
+        return max(numbers)
