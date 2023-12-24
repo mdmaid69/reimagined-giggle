7b3affe46281e3abfc11de7d586b87ab6dc34fec
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:26:59 +0000

    loss against Zz centimetre home

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import getpass
+def get_username():
+        return getpass.getuser()
