a594f94018649a5d4e00487a4ad3fbe9c4dfa8e4
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:57:42 +0000

    turnback meaning sore rain shrink

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-print("Hello, world!")
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
