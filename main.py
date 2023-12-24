a1d5fddc8988e831e739765d5d84c10bf8cea8e7
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:16:02 +0000

    really light watch loss tape

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
