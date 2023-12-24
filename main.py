e88f7b795a1fbdc8655532571474be45f16d9907
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:54:16 +0000

    hers custom tap rely Nn

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
+def convert_to_binary(n):
+        return bin(n)
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
