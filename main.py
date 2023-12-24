4c149bd76dac25a00038e57c407f849f587b3cae
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:16:49 +0000

    detail likely note really square

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-from collections import Counter
-print(Counter("hello world"))
+  def count_elements(lst):
+        return len(lst)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
