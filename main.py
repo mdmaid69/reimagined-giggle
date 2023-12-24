63c59f4c12969ce2731ec3baabc85188a4be104b
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:37:38 +0000

    soil onto earth nothing disappear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
