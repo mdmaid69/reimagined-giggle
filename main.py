64202019e7ea6516c86266635bad4ec6a40003bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:56:17 +0000

    ever stop sing behaviour area

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+for i in range(10): print(i)
+def calculate_area_circle(r):
+        return 3.14 * r**2
