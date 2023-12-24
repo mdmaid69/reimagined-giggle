0a39d3b9258cca352c709c3b5c9f7fa4660ada81
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:44:48 +0000

    offer takeback appear past deaf

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def multiply_numbers(x, y):
+        return x * y
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
