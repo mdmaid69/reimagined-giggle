f137b2ca939899f01d6a587c20fe6445ba39f9bb
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:10:10 +0000

    so equal issue Vv cross

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
