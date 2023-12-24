17d066f2014ea7e278027d66b8904dd5b031be21
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:06:13 +0000

    dance steam shout satisfactory spread

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
