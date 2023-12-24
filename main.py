fae5ac132bb78c973d0ea2fa9d327652f5870f6e
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:53:11 +0000

    concerning suggestion expect moon gate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
