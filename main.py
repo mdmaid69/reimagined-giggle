57bc145b5ff82d42ea4de6ec79e41d51b0eabe01
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:36:34 +0000

    gift accident repeat challenge somewhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import os
+print(os.getcwd())
+import collections
+def create_queue():
+        return collections.deque()
