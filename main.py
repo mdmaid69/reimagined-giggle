b3471cea6584c82f8567de163f2d6e8703c71ffe
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:28:20 +0000

    wonder deep wheel mark later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
