0e879bf074925c684f16bbe8baae2b4297cb5762
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:54:28 +0000

    oil keepfrom slight deliberately dark

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_itemsize(array):
+        return array.itemsize
