723c60d4f3f6b14617f16c20682db39310a5f92c
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:33:54 +0000

    shut minute previous right scared

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
