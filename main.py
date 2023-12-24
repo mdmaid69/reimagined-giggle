0378367cae9ddd0b745548eec0768a0a08b3bd21
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:35:01 +0000

    past geton yet anyone share

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
