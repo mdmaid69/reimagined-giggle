19b0e81dbd1db432a70005c67df1b76b7115b7b4
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:42:10 +0000

    recent but plant sky bye

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import time
+def get_current_time():
+        return time.time()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
