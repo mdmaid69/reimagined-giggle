11507e42b7eb9a3e1ddac1242d77d29d077c362e
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:58:28 +0000

    snow answer handle seem moon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def get_array_length(array):
+        return len(array)
