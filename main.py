3f62535e9cabb362adb8a7d9e5c10dff2cdf7ac9
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:33:36 +0000

    present neither patience turnon hair

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_acceleration(speed, time):
+        return speed / time
+def reverse_string(s):
+        return s[::-1]
