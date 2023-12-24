a8fb45b13fc1991e3e92f621ae05baa6de1801aa
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:05:10 +0000

    basic tip obvious wind long

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
