4b07d1e5cad96444434bea39788a1e6db8d12f02
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:34:21 +0000

    giveback who bush should change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
