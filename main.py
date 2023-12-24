48531b2eae61920a3276f8cc24bc858477288683
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:28:56 +0000

    realize reject when discover world

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_work(force, distance):
+        return force * distance
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
