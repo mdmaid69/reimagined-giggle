68e6e594208653a26b975d9ed151f493f5e4d774
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:41:40 +0000

    tap want none load present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
