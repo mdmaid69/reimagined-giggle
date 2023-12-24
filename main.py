3e2c1c7ff015834b3bb9d5114bfa0729b979f2b0
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:45:28 +0000

    rule male runinto Vv forward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
