e5e71e65adc713478da777cc3e22ad233a2f88fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:26:23 +0000

    straight respect anyone beautiful innocent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
