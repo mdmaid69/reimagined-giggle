2ef0389b24c800e8d4616486cc68937e03a28924
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:34:06 +0000

    Oo excuse strength mail transport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
