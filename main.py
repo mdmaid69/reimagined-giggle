8b995656ea696a4441688bf2a9ee7da19690dff9
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:02:27 +0000

    real report mine congratulations rule

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
