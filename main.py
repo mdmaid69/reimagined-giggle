c93ae0d153350c60dd1ec791741514d309c1f057
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:41:02 +0000

    position mean talkback prevent forgive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
