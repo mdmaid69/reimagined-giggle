8b9a6cf79413e478057c5a83ee16048301ea6743
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:52:56 +0000

    nigga large discussion impossible narrow

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+print([x**2 for x in range(10)])
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
