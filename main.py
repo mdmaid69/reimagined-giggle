77281a62cb76ee9d091cdf01a29998f31e78c19e
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:56:13 +0000

    let suddenly cost count in

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def reverse_list(lst):
+        return lst[::-1]
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
