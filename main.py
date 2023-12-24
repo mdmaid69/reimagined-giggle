e57a7299d6671af05f775d09aca7d2ccf4698a66
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:09:14 +0000

    ever beat expression ago light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_density(mass, volume):
+        return mass / volume
