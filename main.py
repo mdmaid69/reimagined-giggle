85518421d481d6ed3266e3027d87c598d1f19ff4
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:31:27 +0000

    we principle forever fit dark

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
