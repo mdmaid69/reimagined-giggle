9bb1cf65d8cf46d358f1af43c47812a0adb52aad
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:09:16 +0000

    giveup her interview quit television

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_speed(distance, time):
-        return distance / time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import platform
+def get_python_version():
+        return platform.python_version()
