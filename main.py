dd13218659371bf6e13904e4599e5b7260f5fa45
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:54:14 +0000

    chat path write building lookafter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def square_number(x):
+        return x**2
