95483dd2a58a1705ab06302144a47e458f77c3a5
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:00:53 +0000

    physical apart pure skirt moral

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
