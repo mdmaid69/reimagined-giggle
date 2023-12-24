68478cbfd4aec9e24c5fb1e88c01d3f593e4dc8e
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:58:09 +0000

    raw argument praise protect appearance

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
