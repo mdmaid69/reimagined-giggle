4f79212abb3e8bb65325ddd49450410aa974badb
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:11:25 +0000

    education valuable scrape bye path

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
