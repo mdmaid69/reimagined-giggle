ea9a86f546eea1fca817f098a76eeac36413f2ff
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:07:15 +0000

    handle pay storm spin objection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def calculate_work(force, distance):
-        return force * distance
+def check_if_array_contains_item(array, item):
+        return item in array
