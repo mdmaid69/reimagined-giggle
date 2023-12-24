5d2eacbcf23a9e77ee81057af1391ced5a214baf
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:06:42 +0000

    exercise behaviour production recent grateful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_current_working_directory():
+        return os.getcwd()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
