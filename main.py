43de09830ee0ed20ae46ae50067bff0c479ac16a
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:53:35 +0000

    very prepare heat valuable prize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
