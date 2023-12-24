7ef6d6aa9ca8194efc6d8f7ba902990773aad701
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:00:05 +0000

    butter sky curious workout underneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def split_path(path):
+        return os.path.split(path)
