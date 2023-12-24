e6708ce0e0eb525bc1b9a1a32fa16175d479f894
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:01:16 +0000

    danger reliable hard weigh relationship

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import collections
+def create_user_list():
+        return collections.UserList()
+import collections
+def create_queue():
+        return collections.deque()
