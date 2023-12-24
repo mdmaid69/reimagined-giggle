4350c51db75bd5f174efe3f4baf7291e2f203ecb
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:28:40 +0000

    influence dig hold if hardly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
