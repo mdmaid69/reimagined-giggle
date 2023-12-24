816b1a4c46cda617e6a593d73314821a7f7aee49
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:01:48 +0000

    kick far money murder last

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-print("Hello, world!")
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
