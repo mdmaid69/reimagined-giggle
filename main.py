6ea9f1eec922f37f06dd04a9183867b60dca1ecc
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:56:01 +0000

    obvious method full every opportunity

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import sys
-def print_python_version():
-        print(sys.version)
+def is_odd(n):
+        return n % 2 != 0
+def calculate_area_circle(r):
+        return 3.14 * r**2
