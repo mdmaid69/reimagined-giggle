ca575281bc4d4b40853d360b104144cdc3aee0d1
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:21:34 +0000

    talkinto inside persuade healthy breakfast

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
