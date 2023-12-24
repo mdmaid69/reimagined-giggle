bdb39f2e5115ad890dbd2cbf9c0c7a07193e5748
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:24:15 +0000

    it between reduce dream detail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_itemsize(array):
+        return array.itemsize
