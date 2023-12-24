730869d1e2cc2e35b08994e7bcc1f7f34ea73e69
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:15:51 +0000

    basically sometimes anybody lookup sight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_acceleration(speed, time):
+        return speed / time
