e5e3bc056368fbf596495a48d1410f25b5778cc0
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:45:57 +0000

    bin transparent by advice uncertain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def clear_array(array):
+        array *= 0
+def reverse_string(s):
+        return s[::-1]
