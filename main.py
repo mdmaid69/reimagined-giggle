b79a4c7a187173e19cfbe1f2a12fecdba76511ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:11:07 +0000

    demand section stretch receive noise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import sys
-print(sys.version)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+for i in range(5):
+        print(i)
