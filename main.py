ae675c072864b886f80e43eddbdecaa6aa61034d
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:58:54 +0000

    level block shine soon force

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_length(array):
+        return len(array)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
