32f290c2b459a6c42d97aafae843cc7d4db42e89
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:18:16 +0000

    behave further match beyond famous

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
