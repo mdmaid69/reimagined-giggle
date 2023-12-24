c06f8f7a10495f5100c0fe7b0d3dbc0b62bbd6ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:06:42 +0000

    switch cut brush accidentally although

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+def convert_to_binary(n):
+        return bin(n)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
