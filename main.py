c6868a72acf000c010cb0c79169e8c4851874a71
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:22:58 +0000

    even fly ordinary mass ahead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
 import matplotlib.pyplot as plt
 plt.plot([1, 2, 3], [4, 5, 6])
 plt.show()
