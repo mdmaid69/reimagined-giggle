0a972a27c886c39cac288b61bc2beb58c5c5c23f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:39:56 +0000

    better position smell float responsible

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
