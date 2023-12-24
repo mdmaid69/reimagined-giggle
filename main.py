9c2a44cfdca16281ac1597ec47d02b061bd603c5
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:30:45 +0000

    finish it certainly emergency anybody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
