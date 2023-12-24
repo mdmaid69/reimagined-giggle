34b39598d71817072b2909f890c1fd2b26794f6c
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:32:34 +0000

    pity Pp whichever holdon mouth

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
