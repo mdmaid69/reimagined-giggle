a3f59adf488e67fae6bc7fd4566e100ce556c9f2
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:36:17 +0000

    score punishment let nervous patience

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def reverse_array(array):
-        array.reverse()
+  def convert_to_hex(n):
+        return hex(n)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
