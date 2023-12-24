f7dd1fe1394f68f6e57be8ae8c26dc298f4436a4
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:33:46 +0000

    case desperate punishment life example

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_work(force, distance):
+        return force * distance
