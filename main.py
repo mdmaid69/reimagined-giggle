443f4237eb15374ad0562d8da18e3d7dc9abf99e
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:32:54 +0000

    floor tense empty now tell

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
