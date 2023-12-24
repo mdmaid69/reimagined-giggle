4942d9a5d06c8b3cad7dac63f9133d16bc97821a
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:04:08 +0000

    least strength separate base scrap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
