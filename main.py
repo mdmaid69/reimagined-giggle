e6ad0a1bf21792153ebf3bf094155cd32070dd8a
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:35:29 +0000

    have no whenever proof exhibition

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_list(lst):
-        return lst[::-1]
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
