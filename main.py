758b85425f81dd774d9c0b4705b670d4f9389716
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:42:12 +0000

    turnover workupto string pair skill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
