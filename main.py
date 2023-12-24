ebcc6902623b21f3a449cbc39770d554d8e80c47
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:57:05 +0000

    just frightened score welcome such

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+print(math.pi)
+import array
+def get_array_as_int(array):
+        return int(array[0])
