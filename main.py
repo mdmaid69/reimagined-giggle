b201d45637a6fbd9b201e4c4269060b6dd8d4b22
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:23:16 +0000

    Bb dangerous genuine imagine care

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
