5f4793a5470efff6b95e342365c0c6443491b8d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:17:50 +0000

    above trip produce holdup whether

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
