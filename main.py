ace2a7d53032ab6ba1df4b94d93386e72edd8e77
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:23:09 +0000

    best contribute holdout important unfair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
