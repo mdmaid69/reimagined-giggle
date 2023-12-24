8758023fe7a6337df4d21c999b78819a3e3f53f5
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:49:55 +0000

    Gg ourselves obtain black head

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def remove_duplicates(lst):
+        return list(set(lst))
