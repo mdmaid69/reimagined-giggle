4c76c416c715727f6fd5f912611e7837cffd488f
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:51:03 +0000

    walk regret marry available hole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
