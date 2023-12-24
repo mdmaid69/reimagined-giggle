a144863eadc8629caeae14ba8f49cc259cc6a166
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:29:14 +0000

    plate estimate runinto well number

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
