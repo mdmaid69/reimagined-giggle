93962ab460564907b046fa5f5b0e13c9458766ea
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:45:10 +0000

    Bb content include fasten protect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
