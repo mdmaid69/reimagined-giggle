a15163c7a51b1fb2118230aadea4964e25d2991a
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:34:35 +0000

    land down perhaps summer under

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_max(numbers):
-        return max(numbers)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def convert_to_binary(n):
+        return bin(n)
