4389f2cefbe7b83319fee5f6a3e10fe29e4ba549
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:30:52 +0000

    wipe prepare tread squeeze its

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_max(lst):
-        return max(lst)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
 import time
-print(time.time())
+def get_current_time():
+        return time.ctime()
