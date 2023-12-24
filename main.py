7c8f80959de1ccf50060c471354553b977b15749
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:39:39 +0000

    sign degree cook bar OKorokay

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
