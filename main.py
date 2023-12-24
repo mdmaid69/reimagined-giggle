1e8903cbe2e625d2c8b223ff4c4988beeb603821
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:39:29 +0000

    pick complain pick Mm she

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
