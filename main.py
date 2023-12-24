0d054c977143f4f2da26da04210154fa28ebb89d
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:49:37 +0000

    basis scream increase wave advanced

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def sort_numbers(numbers):
+        return sorted(numbers)
