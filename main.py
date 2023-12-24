17b923cd3675e8869f1c4b8fb95fef4d8d74ad99
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:00:50 +0000

    skin free division treatment owe

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
