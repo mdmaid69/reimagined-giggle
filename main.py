b1ae50bb6c780255a20685294405719e18ae1178
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:34:51 +0000

    without separate willing get club

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import os
+print(os.getcwd())
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
