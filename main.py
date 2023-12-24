bf972798dbd0506c1d1dd46ead5d2010418e825d
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:14:38 +0000

    practise pupil post last continue

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
