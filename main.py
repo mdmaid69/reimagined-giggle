63753bc6e3e0c4a48e93bb998e35110645b8620f
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:22:52 +0000

    ask tough drive 565end visit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
