d5f0a1483c180cafb44800edd2b0c479cea53f2f
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:52:05 +0000

    hear check apparently wrong share

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
