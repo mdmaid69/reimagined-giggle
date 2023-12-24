5f44ba9b095c21a900f31f9bf98cc93d438ce220
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:16:18 +0000

    independent business exist cause news

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-from collections import Counter
-print(Counter("hello world"))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def add_numbers(x, y):
+        return x + y
