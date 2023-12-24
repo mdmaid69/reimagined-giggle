6ddda554921b23471f1235dbecdde0ba9395bee8
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:32:43 +0000

    sister murder receive great spread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+x = 10
+y = 20
+print("Sum:", x + y)
