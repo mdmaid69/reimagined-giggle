ecaeeefe2f4b7d750907d4ac96279ef95a11e012
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:56:31 +0000

    list professional contain match art

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
