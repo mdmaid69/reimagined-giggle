bb8e34dbe6d7bd9f50b317f344ae267c8e6b03ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:41:17 +0000

    aim judge another about cover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
