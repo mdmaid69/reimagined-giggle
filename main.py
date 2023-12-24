522c9c0ebe24f3956b2a052222d724a08fc3a81e
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:40:45 +0000

    your him insist rate slight

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def convert_to_octal(n):
+        return oct(n)
