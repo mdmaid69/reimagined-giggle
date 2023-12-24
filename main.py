0676ae0b3a6ef7a5dcaa680aa379b8e64e57192e
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:22:14 +0000

    broad instrument neck number small

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  def is_odd(n):
+        return n % 2 != 0
