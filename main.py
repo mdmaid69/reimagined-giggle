497cc3dbbb9cbfd8b907078cb3cfefe6f1e717b0
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:43:00 +0000

    wild relationship music drop direction

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
