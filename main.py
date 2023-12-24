17d187ede8b0e4c81d5183164eff41cf0b061150
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:22:02 +0000

    only influence fly unkind spite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
