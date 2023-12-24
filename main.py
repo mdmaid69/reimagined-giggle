1b4d11f5f0340d0003c0a118cbb1da9322297417
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:23:21 +0000

    trick cope week backwards city

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def sort_numbers(numbers):
+        return sorted(numbers)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
