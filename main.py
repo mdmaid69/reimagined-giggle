03d8964f1a4de228245216dfaec7b519cbb4e129
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:34:06 +0000

    luck tune down believe reaction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
