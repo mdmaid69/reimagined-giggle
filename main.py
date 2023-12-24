b1c227a5cf3136c77bc9222aa59a630ffea56b56
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:21:46 +0000

    silent bringdown careless fear clever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
