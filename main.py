c4f0a62c8d766cae80c971671bc8526660c2266e
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:15:28 +0000

    left run somehow part family

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+print(math.pi)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
