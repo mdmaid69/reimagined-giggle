d53d485aa79486419fdeda98f8fcd1fb428402fe
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:12:26 +0000

    process point back wrap spot

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
