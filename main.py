b1da58c6bb21d64d1c7b21079bb5117db1303f6f
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:45:47 +0000

    fun basic couple roll whistle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
