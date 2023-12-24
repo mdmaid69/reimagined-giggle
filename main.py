11847ac951b244897700fb9d96776a1961069c6c
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:43:21 +0000

    whatever small niggaacademy separate class

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
