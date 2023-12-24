ff0ec0ae21b7bd4101af57bf76a502695d34b231
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:17:45 +0000

    entry strike creature mistake generous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
