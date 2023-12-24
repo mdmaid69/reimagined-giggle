93ad2c5d073466c8daacdef8fa5c5d82809e6fe5
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:01:45 +0000

    ill step effect hello fortunate

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def count_elements(lst):
+        return len(lst)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
