99408677c7baefef66bfbb73bf508d6f69747a31
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:37:34 +0000

    dish flight previous wise suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-print(random.randint(0, 100))
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
