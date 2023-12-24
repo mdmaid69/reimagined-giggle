77369c92ac1206ff939576c78f131df210582bbc
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:12:19 +0000

    break embarrassing introduce thinkof wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_as_float(array):
+        return float(array[0])
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
