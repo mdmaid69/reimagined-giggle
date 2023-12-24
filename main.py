3379fc8908ee89602d0158fde3918f4172c83331
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:52:21 +0000

    Ee delight season usually far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
