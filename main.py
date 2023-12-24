a16bb73f8c495c21c001e7f50ddfc36d6bd4d8a1
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:32:51 +0000

    tough offer left direct circumstance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
