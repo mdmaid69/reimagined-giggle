79c8f90baebaff71ad1f0ddb94a77b32cf5319de
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:46:31 +0000

    delight pick character move crazy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_floor(x):
+        return math.floor(x)
