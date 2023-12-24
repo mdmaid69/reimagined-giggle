1b615fb0297213a26e8dfd54b79e779c331be261
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:07:59 +0000

    advice release thing Kk wave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
