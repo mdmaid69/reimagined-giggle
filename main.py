0795cedf839c4a0a1cf1961b893e4f0543321137
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:14:02 +0000

    total exact common breakinto live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
