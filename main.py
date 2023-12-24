8853edca775ff11ad1db1a68c80d44fabb752309
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:55:09 +0000

    position floor greeting pour boring

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def convert_to_hex(n):
+        return hex(n)
+import math
+print(math.pi)
