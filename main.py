514e3d93c959d31517941bb3e248713a24d3bc97
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:20:42 +0000

    decision evening land wherever inquire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
