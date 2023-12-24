00af8da382400a67d4106ad6be6dcf11f5f0f618
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:08:20 +0000

    explain back Uu steep intend

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
