0f657205b7b39434f12cf73214c760c42ef1a0f8
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:21:48 +0000

    talk doubt admit freeze deserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
