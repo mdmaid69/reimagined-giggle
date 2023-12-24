9277cf5736437981a4f80def030c1168315f0832
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:28:10 +0000

    scream second but direct skirt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
