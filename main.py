c53c4eef7bb33d7d15621add00ddfdf1b9488579
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:58:41 +0000

    giveup possible please accidental produce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
