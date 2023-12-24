a971a3b1000bc9f118a56d4f1ecd2d8e1e9c509a
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:36:11 +0000

    backwards exaggerate everyone spite practice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_factorial(n):
+        return math.factorial(n)
