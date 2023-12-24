cbe907d6e911edc621b93deed9086617b524ac64
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:14:20 +0000

    away judge mark reason old

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
   import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
