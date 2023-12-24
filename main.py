17e603fe3b970a4b744326c7f63d23780597bb48
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:34:24 +0000

    close disagree fresh 570enjoyable community

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_density(mass, volume):
-        return mass / volume
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
