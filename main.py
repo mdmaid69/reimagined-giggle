061fd6c4a1c71241406800efaf5aaf4f214e6ea8
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:25:36 +0000

    loss another us matter opportunity

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import time
-def get_time_since_epoch():
-        return time.time()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import os
+print(os.getcwd())
