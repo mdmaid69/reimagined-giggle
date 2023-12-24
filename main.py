91d69f084f6bbb600a954fe348f715d1b696895b
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:31:06 +0000

    pocket jump compete mixture mention

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def convert_to_hex(n):
+        return hex(n)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
