b9a7fb2c13b92c84f74435425870a75563c80bf7
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:33:48 +0000

    pull afraid negative straight contrast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_work(force, distance):
-        return force * distance
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import os
+def remove_directory(path):
+        os.rmdir(path)
