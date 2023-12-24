3a1128c7019c6cefe2a22d760873a20092f226b0
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:49:46 +0000

    find turn week Vv take

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
