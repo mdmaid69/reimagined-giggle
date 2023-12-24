66bba5683db7150fdf92c5a07d1b48542bdcdbb7
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:01:20 +0000

    disappear convincing empty size connection

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
