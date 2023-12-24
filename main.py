55c7bbbccc89d22308ff2b6426e9190bdc83365e
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:08:34 +0000

    visit medium pot joke tragedy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
