66d86a21c34005368620d975de7118ca12880e7c
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:29:01 +0000

    drink ring rush trap abroad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
