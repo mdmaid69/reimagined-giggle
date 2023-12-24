6b0d48126f44b8a4270c6d8aea7ae37022af517f
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:54:37 +0000

    beneath fashion polite person cross

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
