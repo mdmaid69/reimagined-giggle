bdd74ee79a3e4311c8aef88c71b851c7d5bda114
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:05:17 +0000

    message building answer fasten complete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
