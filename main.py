0ce3a481dd9d4f025ba07e65c6cc958d8b87a487
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:16:20 +0000

    scared fine short issue sweep

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-for i in range(10): print(i)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
