3295ec49d6161a730fe411ec90046e8f73a4e06a
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:26:54 +0000

    keepfrom office owe loss busy

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_slice(array, i, j):
+        return array[i:j]
