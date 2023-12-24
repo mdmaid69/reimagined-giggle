c34858a968f6647ce715c962d93664d23bdb6c03
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:55:25 +0000

    shock organization memory actually very

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
