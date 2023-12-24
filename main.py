95a4e87fc55132e2e6dc62e3786bcd1fca701cb7
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:46:27 +0000

    game this release chief usually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
