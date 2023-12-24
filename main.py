9a39a9dd41c162792d6b4fb62b80ef46d8a77b06
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:15:00 +0000

    carry divide exercise extra suckme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+def calculate_force(mass, acceleration):
+        return mass * acceleration
