7a945cd511e2067b195cb8455765eadcada9f5a3
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:16:05 +0000

    mysterious sentence point waste clean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
 def calculate_return_on_assets(net_income, total_assets):
         return net_income / total_assets
