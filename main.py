d56a0faba39c221e0e7619e62cf5196f252caa62
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:44:58 +0000

    middle only area put study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import os
+def get_current_working_directory():
+        return os.getcwd()
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_slice(array, i, j):
+        return array[i:j]
