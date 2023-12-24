7636531949e2ba078c4d4276d0b96a478c503a18
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:25:41 +0000

    takeaway possibly several repeat soak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+def get_current_time():
+        return time.time()
