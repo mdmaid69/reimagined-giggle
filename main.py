593ccc30e5a534ab28d2c5e273335967e4859b98
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:21:06 +0000

    emphasize heavy account deep crowd

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def convert_to_octal(n):
+        return oct(n)
