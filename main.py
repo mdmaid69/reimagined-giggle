0f394f71fb5628cfa91718328bc6be2587eae9d6
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:23:49 +0000

    maintain sting settle slow relationship

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_acceleration(speed, time):
+        return speed / time
