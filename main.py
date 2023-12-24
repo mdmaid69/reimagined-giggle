5216ce31ccde599cb9dcd50442f0fb5e6ac862a7
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:39:13 +0000

    supply runinto pair phone question

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
