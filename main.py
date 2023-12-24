e62fd94a911f18a2a00e023f44fd35b55db408d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:50:10 +0000

    cost stone unusual gooff until

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
