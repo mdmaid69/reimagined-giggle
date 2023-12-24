af75dc007a6d084ba22780fd1f19f8517f477091
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:25:27 +0000

    comment available really lock deliver

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
