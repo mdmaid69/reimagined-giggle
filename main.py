86b378ae759ec410d39758e6ad671c5f76b65a51
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:05:24 +0000

    hold again combine side 566end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
