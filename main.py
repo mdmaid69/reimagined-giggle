9fe1fbc9fa12c28be92bb4923b26766d05c9300c
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:17:55 +0000

    ours frequent interrupt hang for

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
