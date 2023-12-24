ef894cdbc46d5deabe85d3ac795d0d8912726b93
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:57:57 +0000

    chief sort speed special breakthrough

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-print([x**2 for x in range(10)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def split_path(path):
+        return os.path.split(path)
