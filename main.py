28d88788471b82e2168bb70a8f2150f82ef4d00f
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:59:23 +0000

    easy full 573entertainment anywayanyhow natural

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def reverse_string(s):
-        return s[::-1]
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
