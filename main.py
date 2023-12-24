97d6a627cdba74fee727f1c544b8f06f2d50ca68
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:36:01 +0000

    exchange opposite afterwards pain used

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
