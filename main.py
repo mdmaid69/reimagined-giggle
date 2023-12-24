19aa8ee4058afcbfbeefd236544ec674d056bcf8
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:53:26 +0000

    general clean allow fold fold

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
