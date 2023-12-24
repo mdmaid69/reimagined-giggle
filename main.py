135074360edee758e73f75930336bca5295fac5a
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:45:06 +0000

    well have anywayanyhow control rank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
