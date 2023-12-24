bb2bd2fa1eb4a06914b6ad4bfe411e1f675989b2
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:21:56 +0000

    pretty reply disappear talk achievement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def greet(name):
+        print(f"Hello, {name}!")
