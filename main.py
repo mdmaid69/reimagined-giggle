9c6568597bc891764f34b0b3c4be52fb1719c754
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:14:36 +0000

    normal debt brain error surprising

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import collections
+def create_user_list():
+        return collections.UserList()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
