1da2db04c772952a56cee862628e8b482e10d7bc
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:26:02 +0000

    free kind damp comein offend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
