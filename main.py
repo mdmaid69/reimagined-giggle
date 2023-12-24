3d9fa63660b84e7b68217b37a23adfbb1cb119b6
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:50:11 +0000

    pool no seem fairly Ff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
