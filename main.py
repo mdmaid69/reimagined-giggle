e428dbd03d276754602bad5b449ea76cffd2227a
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:31:51 +0000

    unfair spare water treat uncomfortable

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
