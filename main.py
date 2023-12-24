443bcc084e20af5bc3eb998ad6ab5e355df0cf73
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:23:03 +0000

    branch fly whistle uncomfortable bang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def get_array_as_list(array):
+        return list(array)
+  def is_even(n):
+        return n % 2 == 0
