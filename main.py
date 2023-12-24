f5f27b94433a683c491651131207fa2ceb69681e
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:40:05 +0000

    workout brother good reply tell

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
