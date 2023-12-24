366cdf3f4e7b5ee0243c9333b962435b46fd0efe
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:31:17 +0000

    skirt day table herself same

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
