6bc600b907bb9ecf83d705a510073efba0907fef
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:43:48 +0000

    cry worth demand bar nowhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
 import http.client
 conn = http.client.HTTPSConnection("www.python.org")
 conn.request("GET", "/")
