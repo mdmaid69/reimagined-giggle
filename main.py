64e7d46db3c1805539481f5d523cb20809953b97
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:34:23 +0000

    owe matter sleep congratulations crisis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
