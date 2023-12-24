4f6ac919eb3d4b1de2fb017bcfa9d0d6c3343b26
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:09:34 +0000

    mend forgive case basically runinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
