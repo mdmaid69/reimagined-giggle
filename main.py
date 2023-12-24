e42e4e92b12adfbe65ab66fc1c791b14b20fb3ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:39:20 +0000

    comefrom regard physical Ww display

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
