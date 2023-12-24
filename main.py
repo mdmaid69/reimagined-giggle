df716bb201fbfa92b5c4d3da4d18ac0decafef0d
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:12:05 +0000

    translate minor square week guess

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+print([x**2 for x in range(10)])
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
