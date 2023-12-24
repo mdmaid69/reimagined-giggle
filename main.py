8893d49854e834d9fecff9aace0d50bd94bdfcd9
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:40:22 +0000

    open independent protection recognize grade

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
