e166ebbfc0fc8c8be029c5ebc7bb3ba980333d60
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:10:55 +0000

    help bet pay steady phone

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
