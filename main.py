3b16f3d2dc7c50a337cb7a15ba72dd8063637d6e
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:53:36 +0000

    travel interruption onto clearly comeover

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
