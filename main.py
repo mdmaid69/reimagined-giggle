db295a2170d9f6f251ea272243f2ff32ab0443ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:42:35 +0000

    tape window survive beside exchange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
