86dbe21ef5537cc1f598e731e9deb208006c33f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:10:08 +0000

    purpose fill responsible punish project

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
