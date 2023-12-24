6071d1307f737a61aacfe2474bd094ac3e1debd4
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:17:09 +0000

    satisfied punish defeat dream praise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import platform
+def get_python_version():
+        return platform.python_version()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
