441f1581fa11a3a60234468a5ded15c01145ac3b
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:54:41 +0000

    fear bye any reach night

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def remove_duplicates(lst):
+        return list(set(lst))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
