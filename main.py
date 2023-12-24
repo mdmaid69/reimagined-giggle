2b13b14705115c48413e5ac0baeb00679d4c94c9
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:01:03 +0000

    under intend disturb protest maybe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
