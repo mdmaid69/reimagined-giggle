601e1675db6ac73ac837cf0403fb11495bc99ec0
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:00:52 +0000

    add steal improvement drop so

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
