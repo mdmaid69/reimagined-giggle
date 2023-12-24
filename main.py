40ec590523cbb9c7f6c486a1ffc580f1bbf0f0a1
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:03:46 +0000

    turn aim embarrassed whistle figure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
