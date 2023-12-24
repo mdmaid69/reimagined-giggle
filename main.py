075dc0b12aca1c43d6aefc2ed31ef4a8da7fbfd6
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:10:48 +0000

    indeed sport male problem rather

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
