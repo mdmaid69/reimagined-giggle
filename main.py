d2564f78e165b24988b8e26fe5f9a8e9a8ac098b
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:50:15 +0000

    among while smile manage yourself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
