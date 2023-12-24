5ba1281b789802fdd6a0a1a5eb9ceef1b605ddfa
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:59:17 +0000

    thought fresh bath gap describe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
