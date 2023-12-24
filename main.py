9177fc8593734f5a02f59139dfe3c4c9b8a490d8
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:19:10 +0000

    ahead immediate rough pay kind

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  def sort_list(lst):
-        return sorted(lst)
+def find_min(numbers):
+        return min(numbers)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
