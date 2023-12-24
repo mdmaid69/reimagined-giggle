590053a6fccf046d1d336d064f1e008a8587fd9f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:20:10 +0000

    bit emphasis face month influence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
