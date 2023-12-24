a06120fb9a30f4fcdc7462a99709dee4cc242737
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:35:58 +0000

    steep crazy organize seem possibly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import collections
-def create_priority_queue():
-        return collections.deque()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
