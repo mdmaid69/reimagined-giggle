3fd4466907b4b90c795fa6b11075120b18a5faf4
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:31:20 +0000

    deserve my used couple fault

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
