ff9b0306ec87c5f28b6072fe82afa006b1cfa696
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:02:59 +0000

    black content possession phone convincing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def exit_program():
+        sys.exit()
