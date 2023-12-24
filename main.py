a1492d058813467615bc17df4de8a539e9bef938
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:32:02 +0000

    attitude comment habit breakdown charge

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-for i in range(10): print(i)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
