48547e489270cddb707a33d1778a61fa6704d6d3
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:06:08 +0000

    force prefer open even interesting

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-for i in range(10): print(i)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+i = 0
+while i < 5:
+        print(i)
+        i += 1
