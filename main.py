0c7ca92476b85c8a67ce9f6a10cb5c6e5a4110b9
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:18:50 +0000

    win breath guess comfort birthday

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  def square_number(x):
-        return x**2
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
