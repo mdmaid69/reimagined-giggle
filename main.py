9bf296def9d120fa5a84b6b9e52f702344f32460
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:39:44 +0000

    hospital air force back case

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
