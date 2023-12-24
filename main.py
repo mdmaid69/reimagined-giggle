dc12c0da5a668a141107d712203298da56e9cb8b
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:38:20 +0000

    please able soft sticky underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
