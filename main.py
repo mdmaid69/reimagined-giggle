f67372a58387d0dbd81a94e97536729b1ab3aa3e
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:32:29 +0000

    share stop laugh naturally stone

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
