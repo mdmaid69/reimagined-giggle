1fd7054dbb6451eced2b2997867bbfff273acf92
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:42:38 +0000

    slowly nose even on gap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
