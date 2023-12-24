519aaf437251eb380bb2c79384ed1bb0172fef17
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:45:05 +0000

    rob paper bother illness decision

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-print(sys.version)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
