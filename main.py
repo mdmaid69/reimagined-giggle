4d6534c06e3b8624f08e25930003f71ee0c9a5e5
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:14:41 +0000

    about precise fire song journey

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
