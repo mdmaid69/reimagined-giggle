e2947adcd15b6581af3af63764d153b793176e1c
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:45:14 +0000

    plastic family change surprised might

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def split_path(path):
+        return os.path.split(path)
+import random
+print(random.randint(0, 100))
