40934485a9f562402fa16ba2e79f710fec139a55
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:39:28 +0000

    accordingto burst call capital hers

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import math
+def calculate_tangent(x):
+        return math.tan(x)
