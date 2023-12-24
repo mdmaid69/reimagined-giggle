c3fa8e0b12d0ba46d2503cb56eaadc7235f6b5ae
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:54:52 +0000

    search wet tooth any smell

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
