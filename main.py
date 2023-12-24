7e61f3f7757b85253ad81376a477342ff2743d7a
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:28:23 +0000

    heart anything cope disappointed plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def multiply_numbers(x, y):
+        return x * y
