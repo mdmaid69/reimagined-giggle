be8bf5f032121f4317096831f8d7636a7f301adb
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:59:35 +0000

    quarter love leg breakinto friendly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_acceleration(speed, time):
+        return speed / time
