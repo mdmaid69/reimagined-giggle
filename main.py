912fba93c643dd2f63f6f966f264dba6b062c4a3
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:14:19 +0000

    anything listenfor sound temperature occur

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import time
-def get_time_since_epoch():
-        return time.time()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
 def count_characters(sentence):
         return len(sentence)
