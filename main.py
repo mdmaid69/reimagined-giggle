5d612afbbc65554b795958eb3bb28b1cb7e233c4
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:08:16 +0000

    long variety bear part sting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
