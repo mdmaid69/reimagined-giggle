fd0d3aec0e4217598bd092492b4ff2a43db569ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:12:43 +0000

    television taste but bringup off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
