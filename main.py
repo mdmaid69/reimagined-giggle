78f2b1f08f5a1d8e993c6724b122f007b6c49360
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:02:51 +0000

    swallow pick beyond wish design

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import math
+print(math.pi)
