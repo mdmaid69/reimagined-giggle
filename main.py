598ef16e9938f1d04c30f3b7df866d65d917bd3f
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:41:34 +0000

    war black road buy worth

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def sort_list(lst):
+        return sorted(lst)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
