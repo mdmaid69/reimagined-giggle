6f69336a06e6f04cf44ebb621d51ae0449e2ab18
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:28:17 +0000

    under pool produce nature place

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_power(work, time):
+        return work / time
