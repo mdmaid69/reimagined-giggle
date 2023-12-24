026d39d342e939245b82f8c8785d7976e37480cd
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:12:03 +0000

    foot book start short average

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import math
-print(math.pi)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
