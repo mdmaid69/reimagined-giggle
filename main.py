f51baad4deaef8c58febc4d2471323400e21c1b8
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:23:48 +0000

    onto alive note claim genuine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
