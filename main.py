0a6d975dc41f62eb4c9dd9d9ffe3640ba1373c9b
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:36:19 +0000

    well small tip amount serve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
