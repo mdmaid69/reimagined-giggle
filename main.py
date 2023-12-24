c9f6f9d4333ca226b4cab0a1c70bae7662aec98c
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:31:46 +0000

    mood come contact gradually move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
