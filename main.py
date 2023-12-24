f108f20624834a747a6dfd6a819c9ecfd9cea032
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:03:41 +0000

    boring crime consist clue let

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+def get_current_time():
+        return time.ctime()
