8a547be266e517b49e124e55ff62c95d68173d6a
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:21:34 +0000

    minimum combination temper back eye

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import random
+print(random.randint(0, 100))
