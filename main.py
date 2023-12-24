bfb18d4e378e67d31819b0b7996f3528b29ba6d1
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:53:25 +0000

    event neat class alive bringup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
