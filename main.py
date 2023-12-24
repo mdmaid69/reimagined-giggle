1a3242020a185d99147d2e157b64971312ae7f19
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:29:33 +0000

    cheek point complete own unfair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import math
+def calculate_floor(x):
+        return math.floor(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
