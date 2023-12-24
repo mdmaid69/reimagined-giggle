e56aa7b1659c5ea1fc1f6973f6ab903460fdf3ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:53:19 +0000

    hear nervous splash as completely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
