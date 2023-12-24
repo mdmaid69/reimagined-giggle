c8e3fd9776112d32a5501254a1f32bd9f08fa677
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:05:21 +0000

    plan related group tip stamp

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
