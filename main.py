cc47b346a67678d73eca377e5518bbb5bb4e2300
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:22:50 +0000

    wash he satisfied alive sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
