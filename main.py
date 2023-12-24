5dd0e21903f1df8f3bedf242b5da33e335c9a3e7
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:06:47 +0000

    comment like design proof slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
