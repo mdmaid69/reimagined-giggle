b8bacf84efea5f0d6136fff5bf9cf5557487c9ed
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:17:57 +0000

    slip disappointed sorry strike quick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
