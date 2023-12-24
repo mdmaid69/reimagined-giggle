c1ab434fee9263cfce7b1bcc03c201037228cf63
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:55:51 +0000

    each harm quit cry bunch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
