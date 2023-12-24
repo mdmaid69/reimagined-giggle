e414c8e865996450141babb0d8c701641046e2b8
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:11:21 +0000

    furniture learn respect breakdown Zz

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def add_numbers(x, y):
-        return x + y
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
