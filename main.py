d1cea69cfacbd9053ef2f5120070cc8212324ea8
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:51:10 +0000

    second people sail work gap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
