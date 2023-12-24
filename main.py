8fd35a3f7719fbe1418fe2f10356c91b008ca037
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:31:28 +0000

    dig rest float contain discovery

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
