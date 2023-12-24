7db570e510c797d6f80253bc53d8a077135f45cb
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:36:48 +0000

    fond square proud goup lot

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_density(mass, volume):
-        return mass / volume
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def find_min(lst):
+        return min(lst)
