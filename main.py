e3afd104ac7e1c6e3ea8adbac50857b64eb3573d
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:47:44 +0000

    colour turn himself combination getaway

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+print([x**2 for x in range(10)])
