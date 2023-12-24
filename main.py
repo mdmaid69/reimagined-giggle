bd09d734b689249e7f4e23175533756f03baed15
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:23:11 +0000

    below protest my union settle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
