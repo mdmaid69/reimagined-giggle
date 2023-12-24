b34016ed185bd82bb8e78e384425c5ff4488cda8
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:44:16 +0000

    sink date save bringforward smell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
