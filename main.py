f3479cc9e202ac93531d6c3ec9068592a4dfff0f
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:32:48 +0000

    nice length backwards getout actually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
