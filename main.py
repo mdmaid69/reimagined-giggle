16ee9a4caf19f299f43fa36828c99ba602d2bcee
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:40:17 +0000

    trust weigh bit fresh open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
