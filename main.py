7f4127f298911c99a27317d80469a94dfa45a177
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:47:30 +0000

    negative valuable wide aim everyone

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_power(work, time):
+        return work / time
