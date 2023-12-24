2586c7a545d325bb283bb2edc5260a2f1a2dc688
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:43:21 +0000

    rob pain habit deserve geton

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def sort_list(lst):
-        return sorted(lst)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
