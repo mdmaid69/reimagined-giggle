cc9e3270b875e3975b81f532468eeec011135ccf
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:26:04 +0000

    much impossible brush hers pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
