907556db5942a8bc628b952dfb18453039b11ee6
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:22:09 +0000

    runinto produce hear runaway exciting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
