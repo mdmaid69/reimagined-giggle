b79bd90aa04674f65c4e725f0497175d47d069ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:05:04 +0000

    share lesson pile strength relative

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
