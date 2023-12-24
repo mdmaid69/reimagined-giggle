65de05eb17456596a1ddaaf909c5666c246fae5b
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:10:59 +0000

    explain transport actually doctor operation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
