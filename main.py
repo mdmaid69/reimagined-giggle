1e9effffb4b390b485aa7c0d085ebc4a987a6912
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:04:12 +0000

    plain certainly coffee improve sport

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
