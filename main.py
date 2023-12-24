12d6648632ba1bfeff5bfe22466fe716b5514a2f
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:48:58 +0000

    tread holiday heavy arrive depend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
