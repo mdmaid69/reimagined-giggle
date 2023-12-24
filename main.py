2060c28a1ebfe241bb1c7dd894fa81e3172b0996
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:32:04 +0000

    wish lost touch mark measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
