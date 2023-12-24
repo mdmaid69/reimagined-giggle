0494f91eba83e7a2ab3ea4a22e5a9306ed61f5ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:47:08 +0000

    shout system come work day

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
