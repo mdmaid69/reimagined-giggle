09655a081499934ce2706870a7a687a9e02f28c2
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:36:28 +0000

    drawing murder still woman squash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
