be017c4e32cbceee2deaccfd2718d25fb9fe85fa
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:32:44 +0000

    hers in cap then flat

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-print(sum(range(10)))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_int(array):
+        return int(array[0])
