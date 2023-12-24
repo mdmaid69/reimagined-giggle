367e760fedb11a722324ca3728744464f20cbcb5
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:22:42 +0000

    payment shock silent scared space

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def find_min(lst):
+        return min(lst)
