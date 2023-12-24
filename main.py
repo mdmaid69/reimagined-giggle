becb30e6f1d60391e00fd3830e16482648bcf23a
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:30:38 +0000

    benefit bush string great wind

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
