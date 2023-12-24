f2f87d28217cbde5ba7746d759f08133a3dabf05
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:36:32 +0000

    pile check healthy scream permission

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
