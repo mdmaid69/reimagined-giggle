7946f11d024829efbcc59c189de983b4d81aa31d
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:57:18 +0000

    hour getup necessary breakup last

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def get_file_size(filename):
+        return os.path.getsize(filename)
