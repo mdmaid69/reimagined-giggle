b91c0830265694b63226cd140695775a3049531e
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:41:19 +0000

    taste goon unit interest pass

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
