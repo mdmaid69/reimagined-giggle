b191fff22c0b07a3b11fa412de6d23517951f584
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:44:39 +0000

    snow laugh burn pity on

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
