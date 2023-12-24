f3bb9ea67312302ab794ce0b3fd249c45f64f092
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:46:15 +0000

    mix pollution sing below getback

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def is_palindrome(s):
+        return s == s[::-1]
