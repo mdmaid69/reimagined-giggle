3f19fdde1c02f67a77dc4961bd7ac16e62fb348c
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:04:47 +0000

    fill turn cool interrupt zero

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_int(array):
+        return int(array[0])
