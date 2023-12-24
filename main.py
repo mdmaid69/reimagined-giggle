9ef68b869d1daa13b5c25c6671e1e54f125637f8
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:18:19 +0000

    now value wall 570enjoyable in

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
