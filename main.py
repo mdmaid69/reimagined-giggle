71350196895b492ce1369c6451783d6c79d67c18
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:05:06 +0000

    piece tough takeaway right remove

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import logging
-def log_message(message):
-        logging.info(message)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
