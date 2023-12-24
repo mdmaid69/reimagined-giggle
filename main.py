3f52e1b2f50839ee9ff0226c5ca35dce27be2b49
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:58:34 +0000

    throw good protest performance call

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
