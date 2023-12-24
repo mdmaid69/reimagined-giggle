c7e38c7053cb048e277075f7e4e6b481ff51580f
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:46:33 +0000

    apologizeapologise official building yet anyone

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
