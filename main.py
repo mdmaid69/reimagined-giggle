ad7fc2b3f734c0b98deb9e22363981243e5bbba9
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:51:51 +0000

    thumb traffic mistake prize picture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_as_int(array):
+        return int(array[0])
