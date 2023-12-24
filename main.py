ccd4ce0456b0d874aff3eab28b3e844b147d1a36
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:40:26 +0000

    put broken underneath call mean

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 def calculate_pressure(force, area):
         return force / area
