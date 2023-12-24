d0e606dc4f12d7296d641872b6b5293d209a246d
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:40:16 +0000

    original flavour Pp development understand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_acceleration(speed, time):
+        return speed / time
