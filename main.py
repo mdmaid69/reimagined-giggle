05f289261de424e6637c6270ba96fbcfb0a4bc0f
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:48:09 +0000

    just poison ago variety lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
