fef37e00105537a30c5da4a4e4371c73f4162220
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:38:26 +0000

    sweep effort surface criticism else

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def find_max(numbers):
+        return max(numbers)
