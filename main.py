248267a79f131e67bfe2e345eff5d913f4025f70
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:54:47 +0000

    fun announce situation comeoff shame

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
