e6ab93bd9c9f9a5fe51eaf8ff3d38dc076db5a8f
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:12:06 +0000

    best weigh interrupt race emphasis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_average(lst):
+        return sum(lst) / len(lst)
