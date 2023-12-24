dbd968d2306cf8d307afaff2c73e9ede014817d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:49:43 +0000

    turnback drop drop wild command

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
