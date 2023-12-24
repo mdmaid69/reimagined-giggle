d47aaca156388baa48a21f73646e4ce5b1755dcf
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:27:41 +0000

    relation content settle warm confident

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
