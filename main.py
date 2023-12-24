01f1b434c85c69403b546d5c6cdb0ab4a14e3f7e
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:36:09 +0000

    reduce heavy release protection art

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_error_function(x):
+        return math.erf(x)
