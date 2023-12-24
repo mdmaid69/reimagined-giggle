ba58a09af1c4fe8bb1dcf9ec90f59485718ef044
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:44:38 +0000

    last introduce bus string sore

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+print([x**2 for x in range(10)])
