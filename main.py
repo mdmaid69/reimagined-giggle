d69ac2302009d2fc7562bc8a8529c56cfa52aab9
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:08:58 +0000

    represent reject better live direct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def convert_to_binary(n):
+        return bin(n)
