b5c6307227df062c3888f4df9e88a5f78888ed9c
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:41:40 +0000

    law march visit lock pair

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import datetime
-def get_today_date():
-        return datetime.date.today()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
