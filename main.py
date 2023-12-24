1eb0093724c2c7ec78ecf1d29b81651d1026b478
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:07:13 +0000

    television blame favourite light probably

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
