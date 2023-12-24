eefcbe13b1e527753da37f0b15a1fd4cb8903857
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:18:51 +0000

    thin rest quarter down workout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
