945d029545ac86ab109f490bc393613aa02bdd71
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:33:57 +0000

    secret priority neither worried moral

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
