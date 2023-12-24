5b0c68d3dd635b7b75acf9dfb0883a89a643c80e
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:45:08 +0000

    trousers negative equipment music deaf

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
