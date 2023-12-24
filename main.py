270432204ae6cdc5590b38653523ccff374b917e
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:00:46 +0000

    neck bite gift mistake ground

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def convert_to_binary(n):
+        return bin(n)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
