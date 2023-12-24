8807de8e86df9bc15776a3f73bb78d4f49fb7c2b
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:01:29 +0000

    upwards 573entertainment opportunity difficult close

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+for i in range(5):
+        print(i)
