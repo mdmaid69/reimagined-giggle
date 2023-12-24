d5c2fe3bed06e6cb6e655ec3bcb98818da748bda
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:58:10 +0000

    private evening afraid wire under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def get_array_as_set(array):
-        return set(array)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
