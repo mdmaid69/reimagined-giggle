3793233580222e7fb93d6c377dc2c0d590986d43
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:42:36 +0000

    nice law information later pleasure

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import array
-def get_array_as_list(array):
-        return list(array)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
