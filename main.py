0d955b333506011c8394aacd3e5b5aa02dd9373d
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:10:51 +0000

    quarter print border constantly once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
