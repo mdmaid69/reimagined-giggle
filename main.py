f6a504faca7147a18e2624799627d7ca5f03f1e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:39:37 +0000

    cause admire than under careless

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
