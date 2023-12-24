4b708e1735695ad4b2e305cf0b0c9cc12bed63d3
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:13:51 +0000

    straight debt never than switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
