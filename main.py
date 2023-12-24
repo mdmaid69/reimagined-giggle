733d3a9f21e282b7359017c06b588d6bed35a112
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:50:32 +0000

    thought lock flight justice extraordinary

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
