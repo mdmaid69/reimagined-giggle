946b77dcff870cbc52d5628ed47f80a0f7a174ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:52:20 +0000

    drink mad official arrive light

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
