c86443b4f536bb96a033615356a0166ee68fe454
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:32:27 +0000

    everywhere our weigh prevent height

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_base_name(path):
+        return os.path.basename(path)
