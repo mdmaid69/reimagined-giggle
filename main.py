a86dd3ab71965c6dabf08a78c18a656f4f8849a4
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:07:47 +0000

    repeat considering label object central

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
