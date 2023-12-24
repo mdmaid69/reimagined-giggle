d4c991554d31f92df3d9c5d694ef2447aa161ca1
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:35:27 +0000

    complicated stamp callat few talkoutof

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
