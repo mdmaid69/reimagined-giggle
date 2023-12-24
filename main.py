8fa21bceea87fa2498e9c4b3c021406fe3dd672b
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:20:18 +0000

    thin either early even expensive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
