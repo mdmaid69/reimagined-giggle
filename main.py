bc6310f6d53a6678445f76d30addfca173906bec
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:21:58 +0000

    depend either light display paper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
