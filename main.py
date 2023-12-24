4a183bdcaafc2f631db63aee8043116ad2285711
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:11:59 +0000

    peace not notice bored active

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
+import time
+print(time.time())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
