39461db11a541b822417cfc3c6ad2506e510be25
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:18:33 +0000

    everywhere divide mixture plain bringforward

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
