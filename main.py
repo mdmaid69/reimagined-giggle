70cc97a04ae00070afdfa4f0429c9f5e990438c5
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:58:58 +0000

    play follow strike far get

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
