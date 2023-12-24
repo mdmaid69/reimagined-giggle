5ab813189097b705d63a998e65ad6b16a027403e
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:07:02 +0000

    trade holdup who practical bringup

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_area_rectangle(l, w):
+        return l * w
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
