1d57068ec5fb9c23251457709f1bdd1ecf9871a5
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:38:14 +0000

    rough embarrassing fast backwards dinner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
