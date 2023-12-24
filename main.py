71ba4ea55ff4d917f695668ffa4451f5959e366e
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:13:47 +0000

    it pen once later unlucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
