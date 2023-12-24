071fbea97f4b7dfc8c6b978419834ba197c9ba81
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:46:14 +0000

    place without help far afraid

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_min(numbers):
+        return min(numbers)
+from collections import Counter
+print(Counter("hello world"))
