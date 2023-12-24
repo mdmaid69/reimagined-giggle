6d53d5c94f009198cc99e16c0be184f4ac0ed228
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:37:51 +0000

    shadow otherwise shall makeinto material

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
