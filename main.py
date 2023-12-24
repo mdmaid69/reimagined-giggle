148ea61aca6a9883c8b9697414dc8a3a0420fcf9
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:28:27 +0000

    feed hand treat except raise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
