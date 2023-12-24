04e5c05ea5bacb3b5ae7802efa5d907031214e99
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:12:30 +0000

    allow equal several proof godown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
