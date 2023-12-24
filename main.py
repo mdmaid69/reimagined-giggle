b67eac56622c7e78cde5a85d1a2486df5b288ad5
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:41:55 +0000

    ago welcome turn go comeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
