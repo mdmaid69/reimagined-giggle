c5209cafcb40c8fd5273117f8f4561c83df943f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:39:14 +0000

    rub right trick identify over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
