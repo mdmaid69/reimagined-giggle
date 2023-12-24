1ba0fd41aab92c0d83ad8fce148f57325604d028
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:27:39 +0000

    contrast fortunately talk move certain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
