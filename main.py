056a4b513852ea22770c5d43a508e9d143aa50ce
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:26:53 +0000

    bound busy post waste success

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
