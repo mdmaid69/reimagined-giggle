ec8a1cd7adf8bdf9f3b12aa15c736f6cf25fb7ff
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:59:02 +0000

    patience chin content rather thing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+from collections import Counter
+print(Counter("hello world"))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
