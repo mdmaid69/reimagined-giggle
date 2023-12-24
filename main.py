820ef37dd215e8075a968ff315ce53d4493bc2ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:13:03 +0000

    minute need upset aim hold

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def is_palindrome(s):
+        return s == s[::-1]
