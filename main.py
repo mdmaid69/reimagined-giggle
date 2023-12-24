71228190f02606aee157304ddc82d58a8d1860fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:32:27 +0000

    press type instrument opinion instrument

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
