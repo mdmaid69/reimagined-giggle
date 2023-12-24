eeace4618f605f3842bd704c85281c64039ef06f
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:34:03 +0000

    occur start cum takeback concern

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-for i in range(10): print(i)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
