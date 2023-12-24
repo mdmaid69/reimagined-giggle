ab17da00607a528c2c95af2fc44173196b410a6a
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:53:17 +0000

    content hear relationship chance muscle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
