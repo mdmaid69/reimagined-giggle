55f496806a121482cf35aba8cad9d41f17a7eee6
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:29:30 +0000

    practise straight breakup makeinto bright

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
