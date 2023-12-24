9a7e3b364461da489ce1fae11968b08ece731fb1
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:21:14 +0000

    especially new anything blame flood

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
