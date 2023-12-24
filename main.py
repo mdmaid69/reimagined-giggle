a6b765eb119e4cb8c70e145d8724fa925c90a684
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:20:07 +0000

    conversation convince aim store fall

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
