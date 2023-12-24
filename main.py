be1ee4c11d8a81c075d019f976488e5316f68931
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:28:57 +0000

    whoever punishment secret sorry right

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
