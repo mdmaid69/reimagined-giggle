582d43fb5b7b9add2d7e21f4f912b0bf241431f3
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:56:34 +0000

    confident hardly them voice unite

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
