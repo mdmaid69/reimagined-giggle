57035de9f7505b584abeb7e65e066f301c694f96
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:38:24 +0000

    spray born table follow lead

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
