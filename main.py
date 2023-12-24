80e2c0728273839b21fae0e3638414799a242410
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:03:28 +0000

    choice harm confuse emergency fight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
