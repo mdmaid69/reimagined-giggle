0a9e0f8b2d79f59e6947b7f895ff38126deb0f17
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:35:09 +0000

    decide sleep travel talkback breakdown

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
