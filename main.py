8b77251511e6c0351b6bfe734f40c5ce6c2572b8
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:16:14 +0000

    remain which thanks appeal drive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
