d16d7736122c4e4b157d3a2eda645803b86c5dad
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:27:49 +0000

    agreement pipe tax extremely organize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_priority_queue():
-        return collections.deque()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
