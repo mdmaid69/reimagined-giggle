726989741c7d46ebb07f1c70a6b6bec69c312705
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:14:34 +0000

    trick time determined stay hand

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import platform
+def get_os_info():
+        return platform.uname()
