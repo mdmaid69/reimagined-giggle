cb034c72a1122fd91896aa6c98cd7ef952e69a29
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:30:02 +0000

    brain standard admit mark permission

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
