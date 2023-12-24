dcd65398e4b8ca385a08d0ba2df019b0d58c3fba
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:26:39 +0000

    prevent experience ask plain should

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def find_max(numbers):
+        return max(numbers)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
