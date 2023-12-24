2a4f4cf5932c240fcb810d13f1640fb14ad03c2b
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:23:57 +0000

    spoil cross suggestion concentration but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
