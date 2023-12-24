f43c363b7cabfd98d6380059eac9eb5482a072e5
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:04:10 +0000

    attempt experience hair regular shop

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def count_elements(lst):
-        return len(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
