0e97b96e1d6bfd4605ff3fecdabe53ac7e035c42
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:40:37 +0000

    innocent score space relax touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_area_rectangle(l, w):
+        return l * w
