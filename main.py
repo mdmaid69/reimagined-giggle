c942e90acfcc042c0aed1c3fd477f0f999895189
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:52:16 +0000

    upset place the it holdup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import os
+def get_current_working_directory():
+        return os.getcwd()
