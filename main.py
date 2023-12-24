1fb80d34441a5926fec6697476dd511dc8fde8ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:26:50 +0000

    weak form side find sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def clear_array(array):
+        array *= 0
