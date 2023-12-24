33ee3cf73943aa10c193cc83062f02b027c6ca42
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:33:40 +0000

    badly downstairs frighten commit pronounce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def get_array_as_str(array):
-        return str(array)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def split_path(path):
+        return os.path.split(path)
