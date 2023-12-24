515b96a3eebc9cf12bf5d0d51c56f78e932f49e4
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:57:25 +0000

    weekend nurse bottom important general

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
 def calculate_area_rectangle(l, w):
         return l * w
