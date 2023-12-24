595b7627e4f51e9bf22d9c43a0ff6eeebad3bc6d
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:33:32 +0000

    aware contact classroom plain slow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
