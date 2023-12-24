5b5d6019891a00e1a0ac75a0601ee41459936181
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:35:04 +0000

    happy production care people phone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def convert_to_octal(n):
+        return oct(n)
+def find_unique_words(sentence):
+        return set(sentence.split())
