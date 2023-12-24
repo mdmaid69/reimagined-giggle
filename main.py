018744aa898e7599f2eba6b4cb38135ab41d250d
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:41:55 +0000

    rely cry swim put worth

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def is_odd(n):
+        return n % 2 != 0
