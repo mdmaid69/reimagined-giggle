0455bb4b19631919615f03c84a33e5fefff5445e
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:09:49 +0000

    project line score handle team

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
