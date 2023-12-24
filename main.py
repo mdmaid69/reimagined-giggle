e2ebe87755ae01d8540969dc58baaeb4af44621f
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:41:23 +0000

    rare market eye spin transport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
