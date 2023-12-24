968ddfd3a30f63bd9d550149f532cd9ee9f02313
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:21:13 +0000

    judge few shade collection succeed

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
