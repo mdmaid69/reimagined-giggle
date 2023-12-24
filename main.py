4ad60334c8e225be4e0fcdb115de180da1c218d6
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:56:47 +0000

    total break waste instead do

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
