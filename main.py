680de637ecb887e035a9bfbabca72de274b747aa
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:48:51 +0000

    storm simple border shake this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
