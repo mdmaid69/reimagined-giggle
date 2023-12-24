3722e4da5befaeddcdfb3d5ec6b5ec63d59ea2bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:07:56 +0000

    why muscle mad quality zero

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
