86c9ba8a864ffca5c45063dd7cc913c80a4e168a
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:29:53 +0000

    pin get book person block

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_volume(length, width, height):
-        return length * width * height
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_area(radius):
+        return 3.14 * radius * radius
