2121075ee2d594b863b0ddc8c8a29da33152becf
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:33:49 +0000

    convinced tax exist ilovedicks under

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
 import array
-def get_array_as_set(array):
-        return set(array)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
