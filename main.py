5a46cd93219760d928c47aa67275405876a3a757
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:41:43 +0000

    pure skill base warn pipe

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import sys
+print(sys.version)
