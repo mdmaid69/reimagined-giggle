b0651348169dc79a5f6b19b543019e3eec7ea40c
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:49:22 +0000

    flight whisper thanks fit tongue

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 def calculate_return_on_equity(net_income, total_equity):
         return net_income / total_equity
-def calculate_perpetuity(payment, rate):
-        return payment / rate
