620e20ad5a2bc587047f6ab647744a7b1f0fbaeb
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:24:15 +0000

    no rubbish table hold buy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
