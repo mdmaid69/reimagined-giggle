c25ff9de913cfc6068ab0e2e5d94fb0ad9927594
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:05:41 +0000

    each bury immediately one exciting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
