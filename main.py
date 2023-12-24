4fb7ab09bd61a95d5982101823614664b7b22cf5
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:49:45 +0000

    giveup large perform claim world

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_unique_words(sentence):
-        return set(sentence.split())
+def find_min(lst):
+        return min(lst)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
