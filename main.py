ea1d3c6e11c83d2bc73f9325bedcc737889164d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:45:14 +0000

    flood next connect pin convincing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
