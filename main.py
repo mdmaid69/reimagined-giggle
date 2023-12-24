b9fc7d1233123d19cd0f2a4dedefa4ba62a682dc
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:29:07 +0000

    breakinto system eat which wrong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
