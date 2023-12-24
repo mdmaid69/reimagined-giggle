0c7dc98df8adce3f69d0a56ac6eb957c31b9515b
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:59:25 +0000

    Ll though exist meeting handle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
