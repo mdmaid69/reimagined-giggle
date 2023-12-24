3acb4e28adc9db8a30f7414ff55a833573777779
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:32:16 +0000

    satisfactory minimum spring cause through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
