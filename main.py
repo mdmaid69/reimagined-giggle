a90afd104623f645891f8fcd60ed7fe6efdb1090
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:34:18 +0000

    provide discussion zero score depend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
