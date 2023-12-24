51e93c6b904e84331aa13be9f193181d100ea963
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:09:53 +0000

    develop lookback(on) send group dream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_min(numbers):
+        return min(numbers)
