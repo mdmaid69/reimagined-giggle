fa02c39e2110b11a9bf3468c1451acf0a06a98c6
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:19:39 +0000

    fun march awake care wise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
