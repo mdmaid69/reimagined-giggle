fcfd7ce11813121dee9e088aeb4501354bdfd8a5
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:51:15 +0000

    drink ordinary public refuse call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def get_array_as_list(array):
+        return list(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
