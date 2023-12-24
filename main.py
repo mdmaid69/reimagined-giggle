a8ab16eb4f2a1c275cffdf66c5c91f11a957f44f
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:51:04 +0000

    present balance search tonight prefer

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import re
-print(re.match("h.*o", "hello world"))
-import os
-print(os.getcwd())
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
