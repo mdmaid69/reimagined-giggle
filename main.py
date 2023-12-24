4cb3955e10a90435b9eab09d62ae297b80f40df5
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:16:34 +0000

    rarely takeover entry confuse effect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def get_array_typecode(array):
+        return array.typecode
