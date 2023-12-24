404d6e8f83973a1763604890bf4a57337fc9fa92
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:11:56 +0000

    rare stand kind contrast protest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import os
-print(os.getcwd())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_volume(length, width, height):
+        return length * width * height
