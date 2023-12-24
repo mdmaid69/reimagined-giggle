bf2fd2fbb1029dfa9597c5d442df9c39e34e873a
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:48:09 +0000

    age weather error fix exercise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_queue():
+        return collections.deque()
