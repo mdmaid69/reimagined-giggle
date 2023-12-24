8685fa38d8e687f160dd93ea68aa488430ea9ed0
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:21:31 +0000

    control artificial punishment storm noone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def square_number(x):
-        return x**2
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
