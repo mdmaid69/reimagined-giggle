4bdbc5876d7883ef752f748dd83f65d0a10fa77a
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:32:26 +0000

    bringback Jj hang breakout under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_queue():
-        return collections.deque()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
