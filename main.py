1a71631181fb752daa1708538bc9886dc7b4633a
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:29:16 +0000

    people gate stamp miss stiff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def get_array_as_list(array):
+        return list(array)
+  def square_number(x):
+        return x**2
