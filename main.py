445d7a2ca6214a5b5efc26f487c04158543a9045
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:51:08 +0000

    explanation minimum autumn back responsible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
