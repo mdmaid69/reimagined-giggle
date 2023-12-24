fce14d759c88600bd4345cfb7015707e49f67dab
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:24:16 +0000

    carry total history slightly lucky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def multiply_numbers(x, y):
-        return x * y
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
