103c377ce84ede9f69971a604a60ff9c8a8e4539
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:34:22 +0000

    against neither stare unlucky evidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
