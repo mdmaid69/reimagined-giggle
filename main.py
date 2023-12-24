8b475f189127b20715939df4c31e858f7b353ab9
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:39:45 +0000

    picture pot nature soak total

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def cube_number(x):
-        return x**3
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
