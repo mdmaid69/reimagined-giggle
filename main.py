5f3c295e58844ff57897422b18076098ec26b873
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:18:55 +0000

    entry rich total record serious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import time
+def get_current_time():
+        return time.time()
