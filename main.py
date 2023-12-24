7a0ae42821c611047c2eb7a434b2a9b3a12e0263
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:36:36 +0000

    bus land stiff plan beautiful

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
