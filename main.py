06694d9cc60b865b46d4d0d224a730f2fb7c6dd7
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:10:32 +0000

    Cc able lesson knock earn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_string_from_array(array):
+        return array.tobytes()
