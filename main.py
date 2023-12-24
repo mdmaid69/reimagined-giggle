508b7ac4748e40c355b03c6cd627d3a2a368a988
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:13:37 +0000

    purpose description bottom the gift

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def is_odd(n):
+        return n % 2 != 0
