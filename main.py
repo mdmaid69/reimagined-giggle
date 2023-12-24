bd054fd6aa64f5aa3c6bc5178308604c546e4dbc
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:30:35 +0000

    opposite round mother clever stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
