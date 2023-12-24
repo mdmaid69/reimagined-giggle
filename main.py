94b78686268e5a89e0fd3ad2dd6ceb7ffc817e89
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:39:30 +0000

    still talkback earth bath fierce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  def cube_number(x):
-        return x**3
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
