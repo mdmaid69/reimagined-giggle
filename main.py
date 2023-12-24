39fc21283f71780af4922bede424709ab5061d14
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:09:45 +0000

    repeat meal comein swim process

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
