9c0c17d518b432fc5df6d0afd9ed1258ff5daf0d
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:13:43 +0000

    same personal step near mind

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def find_max(lst):
-        return max(lst)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def get_array_as_float(array):
+        return float(array[0])
