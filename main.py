37f702216d22db5c096657f6dd2873a528914b6b
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:35:57 +0000

    air recent plan also software

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def count_elements(lst):
+        return len(lst)
