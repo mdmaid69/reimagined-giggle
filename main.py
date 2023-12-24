73de0e33373e35986446d17b119ff3bd2061529d
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:39:10 +0000

    right fashion personal conduct crazy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-def get_current_time():
-        return time.time()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
