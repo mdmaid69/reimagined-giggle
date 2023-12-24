612fa8875b743f4579f525c086fbe7bc5b7f7e61
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:19:18 +0000

    tie instrument satisfied brown manage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  def sort_list(lst):
+        return sorted(lst)
