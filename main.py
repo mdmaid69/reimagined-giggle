b351e58e1674f267b6ace03d12bc8ae191251a6c
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:47:17 +0000

    draw object rob almost swimming

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
