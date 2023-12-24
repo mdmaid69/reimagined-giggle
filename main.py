172ca09a9a8282392f788f5323f3a34d1e468203
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:18:46 +0000

    interested letter shame workon precise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
