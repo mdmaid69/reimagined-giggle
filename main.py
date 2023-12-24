8bdc1b9d1858fbbe938a29f21022390e095d1d9d
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:04:02 +0000

    hole tidy public report trust

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_area_rectangle(l, w):
+        return l * w
