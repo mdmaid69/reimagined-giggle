f21286fc525a9f6b08d8a9c9d74c5de5b347c702
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:01:23 +0000

    off success down suit production

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
