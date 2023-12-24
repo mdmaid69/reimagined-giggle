93265725c9016203fc1f511c70121c6d912ab96b
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:55:21 +0000

    feed idea would moon just

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def greet(name):
+        print(f"Hello, {name}!")
+def subtract_numbers(x, y):
+        return x - y
