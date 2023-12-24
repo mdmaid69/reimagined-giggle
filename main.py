a62d1d917b48da74e6efdb1505ab78026b940296
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:41:38 +0000

    label part brown keepup check

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
