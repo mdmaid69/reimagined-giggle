299152185fe1cf5d19508c0af8ca6cfe82acc930
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:43:21 +0000

    birthday inform love fill medium

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
