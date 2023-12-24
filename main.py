f0cf68b3119efe1351b9e961a5284196e1091d5c
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:39:58 +0000

    discuss rest argument bag careful

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
