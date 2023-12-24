5ed1bf0ce5e231f2c1f97292fb7ad92bc6bf2f97
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:09:24 +0000

    criticize another smile wrong time

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
