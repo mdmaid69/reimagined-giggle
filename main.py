fbb9630a4f6cab79a24297fdf1a4363ccbb94a12
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:23:08 +0000

    present giveout you plenty violence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-def calculate_acceleration(speed, time):
-        return speed / time
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
