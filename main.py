bff01be9fa38c719458e86f24990509aa70b4d76
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:54:44 +0000

    even ours last along impression

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import re
+print(re.match("h.*o", "hello world"))
