a62aae259f5c1eeec30839e12b41ecf1d83ef90b
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:27:54 +0000

    oil opinion test past off

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
