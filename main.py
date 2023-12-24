f348c9ac73dec1b20623ef37e05076a6b620e638
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:31:06 +0000

    voice mark pressure music myself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
