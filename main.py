5ebb6ca27a0e611c95f0b8790b5c78cc5f722102
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:56:38 +0000

    rest copy unable kind flow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
