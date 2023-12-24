61d38702eaefa2a6ad1c7b1e72e249ce242d238f
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:16:30 +0000

    prevent seat sign really ought

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def divide_numbers(x, y):
-        return x / y
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
