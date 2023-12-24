d7be7b8b86d5ea8f530209c3b7af4c8bafa1fedd
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:32:41 +0000

    command identify equipment sign software

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-print(time.time())
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+print("Hello, world!")
