7d76646c7a494e94d295fdd8b604d9c7fb65dd56
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:48:33 +0000

    speed help concern deliberate Ee

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
