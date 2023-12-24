6e85c0dd75d1f0898f5a43ce1450c0340c009832
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:02:07 +0000

    come office make stiff calm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
