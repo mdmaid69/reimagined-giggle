60e982f7624008e386067da3431a945dd55a1740
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:32:24 +0000

    deep last difference prefer material

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
