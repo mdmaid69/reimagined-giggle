eabdf9ffedb101869fcd71854dea50cab5e5d4c8
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:03:42 +0000

    heart sharp Yy boring boring

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+print("Hello, world!")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
