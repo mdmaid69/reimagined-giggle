bfbbe09268d36721aa8516c78bb8ab6686a2713d
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:34:00 +0000

    opportunity new wet yesterday count

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
