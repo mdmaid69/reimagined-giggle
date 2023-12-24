4d6d34127ea40838a2f80b322cf8191020b17c7b
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:19:57 +0000

    different club concern afford gather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
