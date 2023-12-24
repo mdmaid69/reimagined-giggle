67173aef25ae8e3c4d0a81e59036e4c3ebb347e7
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:09:06 +0000

    gather anxious share behind advice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def calculate_force(mass, acceleration):
         return mass * acceleration
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
