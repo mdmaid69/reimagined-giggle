d8b0c0ede8c10c8e19357d9802644574b96c3e88
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:56:44 +0000

    recover table tip criticism introduce

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
