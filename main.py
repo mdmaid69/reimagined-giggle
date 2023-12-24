5aba44cf49e24595641fef9966d6f3e5bb0f37ce
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:43:52 +0000

    abroad fail black none cold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
