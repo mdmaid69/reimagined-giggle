a89bb140c778b601438c9698041eef4f741bba33
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:52:51 +0000

    song discuss entrance formal skill

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import tensorflow as tf
-print(tf.__version__)
-for i in range(5):
-        print(i)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_as_bool(array):
+        return bool(array)
