72e5da994a8a2b06b7ff35451a8ef30b4cf4a286
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:52:51 +0000

    OKorokay past condition ring 179bite

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
