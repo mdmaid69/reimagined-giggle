5d07e7a95817ddf3e4302056534b3406d7126841
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:30:15 +0000

    under available child change name

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def find_unique_words(sentence):
-        return set(sentence.split())
+def multiply_numbers(x, y):
+        return x * y
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
