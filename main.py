d5afca827a22ef857105e91301977baafd889f08
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:58:16 +0000

    goodbye down accordingto yet rob

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
