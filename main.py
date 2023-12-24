23927dd807a5cb77e64227d84ba7f57226d85060
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:33:55 +0000

    truth someone clear confidence run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_user_list():
+        return collections.UserList()
