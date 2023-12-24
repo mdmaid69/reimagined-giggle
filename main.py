188ee3b7b75625fe6d477a297d3227c995689940
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:36:01 +0000

    simple pull better rescue rate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
   def is_even(n):
         return n % 2 == 0
