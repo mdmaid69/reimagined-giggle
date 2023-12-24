a050ecc992d12e74d9249a7b5a68f5a170244421
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:47:12 +0000

    pronunciation friend market Zz over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
