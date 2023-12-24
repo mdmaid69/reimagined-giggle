edcb7ad6319e014915dd2dcebddc9b237abe3e15
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:16:46 +0000

    challenge generous grand breakin noone

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
