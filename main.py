3b25129acc979cfbc900f993fb31bda57577f7a0
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:08:56 +0000

    wheel gate rush deep guilty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
