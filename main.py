c23f58a5268cfa8d112336738ab9d9f7e3fc46a0
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:26:35 +0000

    wash star wall limit follow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
