038bf3956105d0a8fba86981861bc241594e308a
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:17:13 +0000

    steep ill question luck community

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def reverse_list(lst):
+        return lst[::-1]
