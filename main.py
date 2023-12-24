ab243c2d30033e308602b60e4dc83bb471d7e48e
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:44:55 +0000

    division use finish phone basis

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def find_max(lst):
-        return max(lst)
+for i in range(5):
+        print(i)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
