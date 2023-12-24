ba0c6d46a71f8e5b91bf56803fcefbe33cbb2ba4
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:47:51 +0000

    doubt damage 570enjoyable criticize exit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+for i in range(5):
+        print(i)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
