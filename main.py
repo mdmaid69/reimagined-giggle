f569a4fb533f3df11e3c84d7bdfc55aecf2cf804
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:40:34 +0000

    provide let even tread life

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+i = 0
+while i < 5:
+        print(i)
+        i += 1
