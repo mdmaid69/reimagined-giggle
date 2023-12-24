af6dd19849b87077368fec33d6dcf689fdb5ef71
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:48:54 +0000

    waste peace pleasure famous comeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
