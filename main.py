8359308ef6f5ad316bc9c50d6cbde68c161ae6b2
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:23:31 +0000

    free this cost consult goback(to)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import tensorflow as tf
+print(tf.__version__)
