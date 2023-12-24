880384376beb1692f9182e510886a4b692b01932
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:12:03 +0000

    concern inquire summer offer exist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
