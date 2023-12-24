1271c55ad2afd50108b52b9b786e9f53e809681f
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:59:38 +0000

    warm situation they hollow cruel

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
