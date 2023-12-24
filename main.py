ba409cf99026da36d9c7c292e35266bdcd3f8558
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:19:25 +0000

    support desperate promise priority warning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
