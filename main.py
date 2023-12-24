c5c78e979671fb8a14a74682c056f5bec665e233
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:53:27 +0000

    rush silent similar bring trick

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import time
-def get_current_time():
-        return time.ctime()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
