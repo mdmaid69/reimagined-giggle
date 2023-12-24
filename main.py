4a19094b75df6dddb33cead26e27e91f68ece986
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:45:55 +0000

    ground continue swim organize dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
