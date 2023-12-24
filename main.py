4fb360ce06eaaf010f8f6dd998678f81595ad8d0
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:22:24 +0000

    practical frightened culture interest offer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
