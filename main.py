ebe1640cd0e3b0598cbcca11fd29dce13a75a8e5
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:52:56 +0000

    degree swim unit recovery afternoon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
