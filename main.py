6a894de05738bd108ab41c3c1d0ac719078e4b37
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:53:46 +0000

    like second push badly aware

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
 import math
-print(math.pi)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_ceiling(x):
+        return math.ceil(x)
