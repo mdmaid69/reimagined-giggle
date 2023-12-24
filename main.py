bf4a0e7d43676f531af5f6418287b462573f4071
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:41:29 +0000

    interested gift throw brave tradition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
