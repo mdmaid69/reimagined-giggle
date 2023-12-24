8aab79abb3d5cc833dbf1793ea8b323ec587d372
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:41:20 +0000

    although dress below measure last

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
