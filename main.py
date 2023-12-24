cfe74e8a332e5cd3aadb8f814e5967401e75fe00
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:25:11 +0000

    soft rest something boring adjective

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
