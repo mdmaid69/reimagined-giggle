b46334f48ff8a265e450ee2934e5840f832c649c
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:31:03 +0000

    word eager storm either catch

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def is_even(n):
+        return n % 2 == 0
