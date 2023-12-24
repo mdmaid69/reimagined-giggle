feb3e4cb4fe3422f1ceec9f42239661b9ab41da7
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:01:22 +0000

    that bill pen expression sky

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
