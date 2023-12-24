6427b135f8e45fd135af3193b67f464ec5335c0e
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:39:55 +0000

    mark stupid lock wish emergency

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_max(lst):
+        return max(lst)
