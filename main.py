f04b180903acc25ca9053480fd8f1da54c0462e2
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:28:54 +0000

    hardly compare kill understand believe

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
