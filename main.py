dc180244d32c0b440b63cb80b90a1d823963b6bb
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:46:18 +0000

    him figure cum exit pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
