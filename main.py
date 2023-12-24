c0c87c07ffac1f3c460d6b532cbfbe21afdb049d
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:23:01 +0000

    push runover trial project fight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
