e3c80742b2dae4aca3e0b6f372419248bbcc1a36
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:06:49 +0000

    word morning scare last ocean

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def cube_number(x):
-        return x**3
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import os
+def remove_directory(path):
+        os.rmdir(path)
