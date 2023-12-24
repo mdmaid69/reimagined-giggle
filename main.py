d5adc042e27aa46334243e5cc653a9e2f206d3c9
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:51:58 +0000

    wander guilty getoff gentle wing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
