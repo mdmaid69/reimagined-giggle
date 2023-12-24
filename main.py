900a2738121cec9b66a2eb97ac5a6729fbcb402d
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:40:01 +0000

    attempt interview land reason row

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  def multiply_numbers(x, y):
+        return x * y
