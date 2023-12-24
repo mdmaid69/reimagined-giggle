9348ce6482a5932f731aa80d16b84db6de8707bb
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:32:39 +0000

    face characteristic influence sentence method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
