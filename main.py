c348e29249bb0b6dc0adc27973b81950ee5ef46d
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:03:36 +0000

    circle accidental open object back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def convert_to_json(data):
+        return json.dumps(data)
