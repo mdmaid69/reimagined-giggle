ae0f262ff96804ab89b29fc498690e4a8247438a
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:26:53 +0000

    float still sort let film

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import collections
-def create_stack():
+def create_priority_queue():
         return collections.deque()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
