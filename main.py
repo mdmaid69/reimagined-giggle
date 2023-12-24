1544a0a1c873299542247f604de725a033847415
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:08:19 +0000

    wing both another comein size

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def reverse_string(s):
+        return s[::-1]
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
