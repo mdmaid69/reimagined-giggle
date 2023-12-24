481bd491b572b4664569a8cda2e700786d48d351
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:52:22 +0000

    body task you difference unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
