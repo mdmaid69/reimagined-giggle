138f9130b02cfc9a22981f1712e30d6c0b23ab9c
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:38:23 +0000

    brush trousers confuse join pair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
