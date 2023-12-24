3281037e1d04e0d927ea325d233761c7d6f1838a
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:50:00 +0000

    lonely innocent right herself corner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def reverse_string(s):
+        return s[::-1]
