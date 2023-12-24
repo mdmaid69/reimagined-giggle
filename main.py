34816fd6e578bbddd7af362d433e8315f3996978
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:46:31 +0000

    block trousers deal she bother

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def count_characters(sentence):
+        return len(sentence)
