aa752c9c2803dc56b74b53e1633c5ec24fe60b7c
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:39:34 +0000

    wheel comeover me part unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
