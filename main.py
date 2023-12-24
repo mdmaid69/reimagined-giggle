b95c2032b35add02b7d3e6aef1e6f304a0d6e041
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:28:37 +0000

    fight beautiful loose you creature

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
