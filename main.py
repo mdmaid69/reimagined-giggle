9b4476b4eb332fb8320272f9c5c74955a99d3515
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:38:29 +0000

    pity instant almost Ww first

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import time
-def get_current_time():
-        return time.time()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
