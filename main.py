e91fee4941e9593cc42bd56a0992019e92af362c
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:49:30 +0000

    the later scale rush approve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def square_number(x):
+        return x**2
