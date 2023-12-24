6a67c0cf90856e72892bfb17611c7ff0cb86d415
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:28:45 +0000

    divide rate desperate recognize know

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
