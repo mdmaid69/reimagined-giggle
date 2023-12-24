b0bfbbceb36d1624178d77f33b63b5fdf23a28f2
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:25:27 +0000

    claim strict wonder replace whistle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
