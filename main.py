352c97577e534c8e84b01c7dffa0cdba691b4c9b
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:08:43 +0000

    reason splash bang refuse tread

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
