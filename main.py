3af0aaed17c891287837e539099eda93598ffb8d
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:06:01 +0000

    notice behave show share run

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def greet(name):
+        print(f"Hello, {name}!")
+import collections
+def create_priority_queue():
+        return collections.deque()
