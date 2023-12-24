e337cebb623b9db85d38dc2872da73088196e8f9
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:44:56 +0000

    exact holdout neither deliberately fight

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
