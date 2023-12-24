894bae0643a9440c81c66dfd7048de9fcfececcf
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:43:23 +0000

    book excited meet exchange each

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_speed(distance, time):
+        return distance / time
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
