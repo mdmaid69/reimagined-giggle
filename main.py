72475e3d22e63da9f6dd73a56f82710352561ffb
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:58:32 +0000

    proof 569enjoy book church precise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
