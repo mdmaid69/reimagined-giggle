b358da839d028e49a1d388ceca8b31f99367c39e
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:36:47 +0000

    doubt frequent gradually peaceful couple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
