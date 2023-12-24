37a6d5ef6b593976ee04592a443c2d54b6c4b4c1
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:02:25 +0000

    once distant time raise direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
