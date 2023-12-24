2267f887e34157af43cf54399fa8ff451c32ae45
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:58:38 +0000

    community beside laugh equal target

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
