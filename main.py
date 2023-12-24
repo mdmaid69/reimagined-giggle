318b99f4939af892ba30b61dc0fb873988d62bd2
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:10:15 +0000

    bang detail phone waste climb

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+print(sum(range(10)))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
