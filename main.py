2347e4cdb760940da94f0113357dc8ff2df49d1f
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:34:07 +0000

    embarrassed recovery flight field land

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
