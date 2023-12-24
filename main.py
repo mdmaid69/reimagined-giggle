baf17e99aff21b38c9351eafef791d89007a3960
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:55:50 +0000

    control listenfor takeoff tear find

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def is_odd(n):
-        return n % 2 != 0
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
