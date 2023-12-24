8ef78107dfd1389d7eb77dd11e7e208a38b583b2
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:18:28 +0000

    first walk break offence step

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
