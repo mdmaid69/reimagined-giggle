79950c1ec19619a3137a2b1e82e2f8aeb1d2c2ec
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:29:51 +0000

    too consider fall obvious authority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def count_words(sentence):
+        return len(sentence.split())
