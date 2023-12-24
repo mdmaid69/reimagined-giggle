b0b44fd83289b4474a2b94452026fa80221c6bdc
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:43:33 +0000

    project refer prevent direction machine

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
