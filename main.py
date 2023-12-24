bda024a7ceb3bdda1b22cae131347a493cbc7b15
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:09:30 +0000

    hers fit after same print

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
