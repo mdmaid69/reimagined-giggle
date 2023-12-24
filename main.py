cd6b6f2a1aa25a6a95229a228410cd0792a930bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:39:24 +0000

    street outside study above body

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_item(array, i):
-        return array[i]
+def is_odd(n):
+        return n % 2 != 0
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
