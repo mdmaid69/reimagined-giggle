3997f1b89c3df2ed01d43ee4600d51058322e840
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:14:08 +0000

    crash length suppose kind lunch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import datetime
-print(datetime.datetime.now())
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
