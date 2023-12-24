d3d712d6257d430aecd78703a3ac2a3f58ee7014
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:31:35 +0000

    value no wet not scare

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
