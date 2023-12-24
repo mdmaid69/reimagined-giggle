0a403a660fc2fc43eeb656350503806599e6751c
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:17:05 +0000

    among pile score need side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
