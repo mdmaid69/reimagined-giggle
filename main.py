f2bf352c244321b73bab5cc2a06fab31986eb1fa
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:54:33 +0000

    slightly ignore middle perform enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
