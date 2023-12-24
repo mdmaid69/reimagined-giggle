140109fe4806ae4f0eb94f1da1df009b89c239f3
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:31:32 +0000

    sit modern sensible delight movement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
