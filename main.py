2ab3e9b5cf6a45c844d5ba8cd87d42150d7012f9
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:17:03 +0000

    talkoutof second delicate form defend

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
