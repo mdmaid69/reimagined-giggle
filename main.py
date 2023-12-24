7dc651d6d89858b16b01c2776f66d6d4df765e7e
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:56:23 +0000

    light cinema comment dress Bb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import time
+print(time.time())
