4bf460970cf0817af36c28fe79bc1fc1bbe7e1b5
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:31:07 +0000

    book anybody boring convinced closely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
