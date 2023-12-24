55540a40f2dd27851c3169d2f5a00e4885bbfc71
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:31:16 +0000

    set run real person promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
