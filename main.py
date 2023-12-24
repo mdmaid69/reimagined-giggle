2df7df4ed94459b45a3348e292494c5fd7a59dec
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:46:16 +0000

    common arrangement unlucky run arrival

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def add_numbers(x, y):
+        return x + y
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
