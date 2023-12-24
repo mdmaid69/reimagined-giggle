a85935c2a221d40c8fa7f28b32bcaefa38a217bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:00:20 +0000

    alone schedule connection this keepoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
