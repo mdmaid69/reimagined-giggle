0f12fc5a63fe7788484e5ecaca336a10b131789a
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:50:06 +0000

    copy undo brown balance none

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
