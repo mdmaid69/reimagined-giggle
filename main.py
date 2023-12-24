72b3a5e4cebeeed99a21caae7fba8946b66c8058
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:42:14 +0000

    urgent advanced crash Qq string

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
