0642ad8b144258cf8f0e5186d2d20539715192d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:46:47 +0000

    note protect glad doubt increase

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
