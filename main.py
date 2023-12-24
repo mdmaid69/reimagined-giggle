c75437f70d51a97df71417c0613933bd9b6a680d
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:25:38 +0000

    565end Mm film abroad sail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
