b3248574f26b388c010a4b352597b33b44a96f06
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:23:45 +0000

    cry spring giveup recover fight

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import sys
+def exit_program():
+        sys.exit()
