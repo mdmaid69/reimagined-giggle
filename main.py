2774420a6614cc7cd62a92924b0cfe9cd98a8707
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:22:28 +0000

    band private wear dance suggestion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
