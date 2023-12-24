869c2b25ed3ec7ebfe248dc0ef0032c8d9772b54
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:45:54 +0000

    file prepare chief stage invitation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_factorial(n):
+        return math.factorial(n)
