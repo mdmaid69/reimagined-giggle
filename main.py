dd91d8f31f9cbee33f4bf09ae2c5c70a76b2f792
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:18:43 +0000

    extra quality wise entrance share

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-print("Hello, world!")
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
