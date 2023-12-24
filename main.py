e655f56fe80d759cf81780beac33b75516ef6acf
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:06:12 +0000

    able split change attempt type

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
