726cfa043e6ec08ad58d40d8776952b232401599
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:42:32 +0000

    mix cook under news different

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
