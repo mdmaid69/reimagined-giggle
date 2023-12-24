baf1015c282cfcfae2cc2d42a9ae1b6f28367e32
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:01:24 +0000

    behind correction priority only backwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
