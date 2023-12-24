5f8390bbe32f9c2f3dcd4862039e234b87f0073c
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:56:00 +0000

    play air forgive turnoff about

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
