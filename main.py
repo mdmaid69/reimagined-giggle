8a55aff856505442b7e20770bef4f76598fc70ce
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:31:18 +0000

    beneath 179bite date expect handle

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
