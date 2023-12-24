2e80bafb6bcbe5520b0f65581c2fd998858da6f3
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:52:39 +0000

    problem belief regard light floor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import collections
+def create_user_dict():
+        return collections.UserDict()
