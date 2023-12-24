eddfc56881e042e42b39758128755e7d307b0c78
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:39:38 +0000

    present pronunciation decision well butter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
