a4432f8866db8e0ddc26c605329ef8cdcb321d64
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:44:08 +0000

    pride land than accidentally emphasize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
