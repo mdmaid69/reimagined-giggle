81c9880d5463fc7b0348a1f6e01a1d47cab52728
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:29:07 +0000

    material unlikely physical keep remain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
