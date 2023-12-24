4b5d1333722c7541c7706ed2de421af15ed59bb3
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:45:18 +0000

    ready community talk example long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
