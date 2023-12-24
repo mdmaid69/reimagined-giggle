46ccc5a3f271943c31daca6cf1d8bb63c0a13ba1
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:53:10 +0000

    independent word estimate careful bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
