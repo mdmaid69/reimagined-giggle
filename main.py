6c00aa50f97f937213ee776b818d00b724194262
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:04:49 +0000

    total advantage scale try trade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def multiply_numbers(x, y):
+        return x * y
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
