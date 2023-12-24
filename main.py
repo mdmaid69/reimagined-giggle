e11a519a344d93ec4be399a31a791b62494688fe
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:30:39 +0000

    touch touch design organization area

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
