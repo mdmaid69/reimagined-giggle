bdd6e21c7525ff46eeacc79255e68f0a19a8eb6e
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:57:21 +0000

    accidental market unusual standard share

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_density(mass, volume):
+        return mass / volume
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
