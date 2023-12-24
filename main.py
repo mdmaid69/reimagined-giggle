e996dda310228af2d57bae0f67a8a3c48ea1740b
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:50:30 +0000

    circumstance do short plant admire

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  def add_numbers(x, y):
+        return x + y
