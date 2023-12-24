4536241b16b0771da4867088dca7887bf35ec6f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:14:06 +0000

    action shop passenger Cc 179bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
