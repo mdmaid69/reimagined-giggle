f229cbb9cae0faa7b5851b05ab0132a5556ac288
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:34:50 +0000

    567enemy satisfied turndown land ignore

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def convert_to_octal(n):
-        return oct(n)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def subtract_numbers(x, y):
+        return x - y
