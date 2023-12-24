fc6d0c677f4a396e3a74182aca705b7daf810de9
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:53:23 +0000

    small except follow describe lend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
