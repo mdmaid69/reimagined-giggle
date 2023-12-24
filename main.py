f862dfc9788d7a2398b1c6f3a8d345c95bbeed40
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:25:10 +0000

    abroad warning old play general

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-for i in range(5):
-        print(i)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
