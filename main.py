7b14b7e25524e18fb512e9bbe11acdd038d33e8f
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:30:11 +0000

    miss repair from its reasonable

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def reverse_array(array):
+        array.reverse()
