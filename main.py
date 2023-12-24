ec6e2a3473f94aa6c229bcee106b2f9ad161d54c
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:36:20 +0000

    everybody inquire production community every

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import numpy as np
-print(np.array([1, 2, 3]))
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
