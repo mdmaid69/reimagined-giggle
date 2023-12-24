ee62928482308a60ca9558f1c1713be5f588aff6
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:56:11 +0000

    brain extraordinary influence busy behind

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
