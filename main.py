23fe1ce6b88b6c47b68b1516195d318995a3c5be
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:18:27 +0000

    argue building furniture nose border

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
