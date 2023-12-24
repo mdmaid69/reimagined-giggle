4821b03e6c878c03d76448f813444aa9f6142eab
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:31:22 +0000

    improve share Ff dare calloff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
