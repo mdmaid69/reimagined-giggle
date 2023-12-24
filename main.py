fdec20083d6a653c49dfacc0ee1bc0176affed39
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:36:05 +0000

    crowd century church grateful lesson

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-for i in range(10): print(i)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
