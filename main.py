33931c2dbf5a98dbc9591bf9bdf86f3fe8054085
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:41:57 +0000

    nearly why coat task sometimes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+def calculate_power(work, time):
+        return work / time
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
