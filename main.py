8fef0391034c179d4f148f511c8b4728f5454d2c
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:11:08 +0000

    lot fly cough well section

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
