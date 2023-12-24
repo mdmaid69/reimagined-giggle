c468ed72913bf75b12231f4b872203c49fbf5818
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:15:32 +0000

    wall strong visit wash side

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
