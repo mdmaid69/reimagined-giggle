7819afba37cd73abcbb52915a8e05d62b27ef341
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:47:33 +0000

    deep praise prove go piece

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
