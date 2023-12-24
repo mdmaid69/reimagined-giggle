72aaea0b3e9f1a72e519b6526f025d5eb6b1a41e
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:01:50 +0000

    cook stretch squeeze see birthday

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def get_array_length(array):
+        return len(array)
