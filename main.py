e47b56e77ae6a044029890424848c9ec6ff5b301
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:20:02 +0000

    comeup build chat example fresh

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
