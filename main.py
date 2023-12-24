4a66f5ffa0c3fbf03e4144acae5eeba3904dc7cb
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:25:14 +0000

    comeoff chance middle lesson 179bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
