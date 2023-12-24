f1df311e8926d987885c045dd1a762a06a808e41
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:17:14 +0000

    suckme foreign if official risk

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
