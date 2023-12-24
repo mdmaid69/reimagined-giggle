aec536a243f30512bffd05f327412b018dbd7992
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:21:33 +0000

    break exactly whatever memory anything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
