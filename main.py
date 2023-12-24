2055c5e4bb427e8c2b23fe55f1c79747f64fd4b1
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:16:49 +0000

    knowledge but pay bite mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
