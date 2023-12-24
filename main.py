9b5cb6c0606b9fdaa338cd5c8e90a71bc6ce8bba
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:51:02 +0000

    switch under conversation why crazy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import logging
+def log_message(message):
+        logging.info(message)
