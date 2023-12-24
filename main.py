80886e68ea8aab8302ea3b588ff57d4c1f2ce338
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:48:55 +0000

    experience well question set keepout

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import socket
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import queue
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
