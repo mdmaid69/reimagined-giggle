63689c1b18135885eb913206549cd0f8f5bdab72
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:49:24 +0000

    store peaceful across hardly noise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
