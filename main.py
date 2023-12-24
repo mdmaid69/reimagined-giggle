38b526cccd8c47ba7acdc9f1cda0b774531833a5
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:22:10 +0000

    bury rent breakfast stiff usually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_max(numbers):
-        return max(numbers)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
