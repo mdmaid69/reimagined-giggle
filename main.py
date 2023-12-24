4165f4b499270eb086c94ec15ec0030fc0c9b342
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:58:01 +0000

    OKorokay congratulate bar beginning ocean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+from collections import Counter
+print(Counter("hello world"))
