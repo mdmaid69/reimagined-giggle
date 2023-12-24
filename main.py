f9c999544105b76370346c01e552a560917c78ae
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:57:33 +0000

    near further wide contribute name

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
