89d7e0ee1a82b15fbe8c6833cfec8998eeba106b
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:54:38 +0000

    room stage shade spread nurse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
