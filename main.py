13624c0a1328eb17d3ade69227f1280c4c1253a8
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:18:21 +0000

    bury theirs game scrape fence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
