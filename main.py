47781bf6b2d2e2c9050f74111e0b7400147ab492
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:11:09 +0000

    really born complain both plenty

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
