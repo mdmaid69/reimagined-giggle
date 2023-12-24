db2029c8819957e6d8f0278720bb59717baa0903
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:43:57 +0000

    storm uncertain least finger under

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import json
+print(json.dumps({"name": "John", "age": 30}))
