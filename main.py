e70c8d0d75921590b7aa668ba2bb8fc6a0f01013
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:35:59 +0000

    trip black big chin switch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def count_elements(lst):
-        return len(lst)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
