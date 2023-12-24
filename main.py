fb3f41f7764e5a0fe4690ca954635c7f79845454
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:06:37 +0000

    support further science getaway set

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+print(sum(range(10)))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def delete_file(file_name):
+        os.remove(file_name)
