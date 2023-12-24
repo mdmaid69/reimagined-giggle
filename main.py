a96d5a7dc31344bd25626e0f3f8808f83206bfe7
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:19:16 +0000

    accident well by science punishment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
