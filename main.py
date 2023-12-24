996cf7e670aaaa5325b417b01a0222facf69b824
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:40:05 +0000

    favourite medium Nn black music

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def is_odd(n):
+        return n % 2 != 0
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
