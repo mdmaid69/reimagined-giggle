9eb74d8910a994f034db5298369dab23b64b038c
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:53:48 +0000

    communicate sick jump neat hand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def find_min(lst):
+        return min(lst)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
