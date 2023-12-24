b0285fb9d1285bb3b20af9818b167b279f5e77b9
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:30:04 +0000

    slight hurt bite press extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def divide_numbers(x, y):
+        return x / y
+import sys
+def exit_program():
+        sys.exit()
