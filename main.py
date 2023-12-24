19bc9a2834c0298bad1cbc67df1e6d0cdf9fa982
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:20:01 +0000

    belief office computer Jj cry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
