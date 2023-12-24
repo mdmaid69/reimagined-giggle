ea338995ca54e15c0a5e12202533ce4179dd7fc9
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:32:49 +0000

    feature soak fork match cough

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
