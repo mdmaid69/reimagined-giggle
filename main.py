1e0df97fa60c33b3b6c74c239b2e58f7218cb458
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:26:49 +0000

    experience how perform music grand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
