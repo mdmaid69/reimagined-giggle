48bd32cf7f7c4e9ffd61ef661062bda7253f156c
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:20:33 +0000

    love protection release recover excellent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
