f0d944e305c3f2de87735e846a923441d5d26ee3
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:22:16 +0000

    fat sweet nowhere takeback basis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
