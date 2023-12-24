ca333e3a2039021ca6b80a841a8b0f0f8460a868
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:37:17 +0000

    climb collection basic inside something

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
