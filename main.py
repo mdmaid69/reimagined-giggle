fe50682abe12a827745966ad5d3f447116fbd81a
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:11:19 +0000

    street neither appeal lonely godown

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
