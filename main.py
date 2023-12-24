76328cf2fb02b79039f79608e4b134c78616a8cf
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:15:26 +0000

    emphasis bus deserve save only

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
