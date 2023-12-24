b63b12e765db4efa1836ae2628c1c15354c33f22
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:13:16 +0000

    gap curious nature strict minute

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
