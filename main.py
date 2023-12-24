4490097ea3bd0e042779d13cc5ef62f2698472a1
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:43:16 +0000

    lookaround minimum arrange whom share

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
