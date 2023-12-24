0e8ba4a9d2a48fa34e294f425dd0ea1a1b46dc47
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:20:20 +0000

    language voice tired top land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import sys
+def exit_program():
+        sys.exit()
