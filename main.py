246caaa3e0478d2ce6427450826ca7bdfe561179
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:50:50 +0000

    suffer turndown able square ours

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
