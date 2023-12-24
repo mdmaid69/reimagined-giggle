1ca0af49fb31e313e873ad370330357e73934f72
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:19:51 +0000

    comfortable pocket describe coffee polite

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-for i in range(10): print(i)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
