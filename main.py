93482ac5d152d4840daa24aaf20cc12e3b3c68ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:43:04 +0000

    hurry whenever surface nearly pass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
