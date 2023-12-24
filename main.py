709ad0ff7600e72f38528385272af9d5b4df33bb
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:14:29 +0000

    sudden Nn dig sign recent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import sys
-print(sys.version)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
