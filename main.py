04e743cf257656e190fc43b6c6211b80617a01ec
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:44:40 +0000

    apply somebody development string alive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
