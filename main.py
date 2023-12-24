f63e62104e0835aced5e1e8bff5f4f5c28fabaa4
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:17:57 +0000

    thanks report expect tip but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
