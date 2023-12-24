f35bb594313d58d19de2a0a1308a789bb3d3b838
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:16:32 +0000

    until workupto half wealth either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
 import array
 def set_array_item(array, i, item):
         array[i] = item
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
