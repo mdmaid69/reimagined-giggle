9417aa84d9bcf8cfea9a58295910ad8a83600d94
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:23:35 +0000

    whenever smile split miss dare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
