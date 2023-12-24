015e2ac76784d5da362b70e33ef5d440c967b4ed
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:31:08 +0000

    down healthy while fold general

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  def subtract_numbers(x, y):
-        return x - y
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
