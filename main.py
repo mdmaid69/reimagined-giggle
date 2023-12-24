7146418b6a971cd966fe8057101250f1623c15a6
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:34:15 +0000

    spread behave detail thorough tradition

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-for i in range(10): print(i)
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
