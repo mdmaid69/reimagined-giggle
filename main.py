fd41b7d0ad20418d3a54bf20daf1913015a67385
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:38:56 +0000

    willing type breakdown turnup run

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-name = "Python"
-print("Hello,", name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
