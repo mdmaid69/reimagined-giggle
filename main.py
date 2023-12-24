f97cf198290a28b6fb9452c4efa33d186cc8a276
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:46:08 +0000

    turninto behind result different feel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
