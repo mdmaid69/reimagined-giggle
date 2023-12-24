9e3f6ff6973c2e63eee9288a68e60461dc0daf07
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:45:00 +0000

    you trap Uu degree tense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def convert_to_binary(n):
+        return bin(n)
