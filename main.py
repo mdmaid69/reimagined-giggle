415cdd0358b345fab7ecbd8cb65739518ae2d5ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:42:22 +0000

    habit attitude dull content practice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
