a603a9fd8939af5a70b1f3cb20a07d1ed67fd9e2
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:38:26 +0000

    greeting somebody priority callup dress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
