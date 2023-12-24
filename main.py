38837df4b29ad384918f836f1e96a47dfe419b1a
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:27:45 +0000

    giveout temperature yours difficulty quickly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
