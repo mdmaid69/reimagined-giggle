d58477df8914788bf84c6c946e9c952247edbc3d
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:23:01 +0000

    workupto fill manner report man

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
