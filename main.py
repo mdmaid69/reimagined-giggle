8e4009891b60895226f7193aa4962772fe1c51d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:41:14 +0000

    tap base mind put bye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
