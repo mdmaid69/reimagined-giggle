e115d359d53168e80f12d992eba95dab601100b9
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:26:21 +0000

    fierce wake dry trade range

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def count_characters(sentence):
-        return len(sentence)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
