ffddb1f005bdd1424ca13d2e03938fb11564a9fd
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:16:37 +0000

    correction put problem window unit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+for i in range(5):
+        print(i)
