25b55afdda6ce31572235249dc9fee3b237a1b3b
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:32:50 +0000

    track shopping draw hot difference

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  def count_elements(lst):
+        return len(lst)
