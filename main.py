b23513609cf6334d6e59fbe3271f7bc3650f226d
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:04:23 +0000

    sharp limit table sick perhaps

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
