ad64e89dd73a3ea36fba99db54534156b2bdd120
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:20:43 +0000

    complaint confident forward thought fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
