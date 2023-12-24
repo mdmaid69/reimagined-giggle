d6952897da5a19a68576e2d96dcf8be3b66ff17c
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:57:10 +0000

    couple platform ring too dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
