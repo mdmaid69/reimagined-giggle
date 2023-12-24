20072060b95c3761ed99cc416e2b6d95e7f3a3fb
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:08:36 +0000

    call so sharp wish report

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
