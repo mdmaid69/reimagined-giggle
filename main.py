bdad607c750a2f82f549e37427d445e18e1c6cd6
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:40:53 +0000

    Oo grade point bill themselves

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def clear_array(array):
-        array *= 0
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
