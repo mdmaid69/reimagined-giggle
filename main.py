c63b77820bc1debf2958cc62975fba6b96ca0fa6
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:52:42 +0000

    turnover platform breakdown maximum tend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
