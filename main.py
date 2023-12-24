8a9da58b6cab464ee2a42f0c18a6eca5a7f84ec5
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:35:17 +0000

    run tomorrow suggestion admit anything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
