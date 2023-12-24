ee72bf1e36dda84aa6ff6acaa31e42751a5c7a35
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:59:51 +0000

    less often win boil doctor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def convert_to_binary(n):
+        return bin(n)
+def is_palindrome(s):
+        return s == s[::-1]
