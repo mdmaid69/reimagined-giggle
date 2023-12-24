2de00f64815c29a41944b8404cafdff3438e6457
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:17:52 +0000

    entrance hope break fit morning

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
