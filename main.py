61e983466966f95cc3db45ba516ac53258928902
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:01:48 +0000

    note look between suppose influence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
