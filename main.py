bb31e2562acc671803717ff95ccebe7193bc5d12
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:01:52 +0000

    yesterday minute somehow callup scene

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
