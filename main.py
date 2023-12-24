7d61e8e779aacf5c07f724d35de2e916ed0cd109
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:54:29 +0000

    breakin there space public space

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
