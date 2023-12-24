6bbb708ee5fb6320aea11c8e4e9c001d8f2ac498
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:52:11 +0000

    fail show if tough trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
