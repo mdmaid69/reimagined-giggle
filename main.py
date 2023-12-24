ac8f620b30031e977baf7032ea6bf9768ca8e9b8
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:59:52 +0000

    bank patient turnoff small provide

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def get_array_as_float(array):
+        return float(array[0])
