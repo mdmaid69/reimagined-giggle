efdec22a6649e69f03332ad973253f8672bff4c1
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:05:46 +0000

    lock smell get grade story

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def multiply_numbers(x, y):
+        return x * y
+  def is_even(n):
+        return n % 2 == 0
