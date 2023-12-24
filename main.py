4cd35e5af41836f24649dbcb30c48cce15256098
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:04:57 +0000

    rent whistle ours mass year

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
