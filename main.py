b6c30f952909961b0c21b6403aed06be5f238cc1
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:17:27 +0000

    one explain pleasant knock art

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
