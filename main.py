85569d7083c35afd773d650a3d403837f1c3ff54
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:08:53 +0000

    identify emotion once long increase

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
