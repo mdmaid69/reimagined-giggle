0d44f6eb2e9fca1a7df40e4a5cb00f905be47918
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:28:33 +0000

    nice bury provide that total

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
