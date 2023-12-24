0c5ca5325cb6197660f8ecb27db8736fc7a32067
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:08:22 +0000

    guard feed price narrow hope

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+print(os.getcwd())
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
