a9e6ee078519b2abefc177a32c88e0707306221b
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:38:56 +0000

    style Ee wave common marry

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
