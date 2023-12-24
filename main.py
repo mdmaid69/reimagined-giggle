db9b98083539ebf94c36b6446d6343ea7aac37f9
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:23:02 +0000

    Itself level name bang gather

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
