3e953968782fabd95a01cad6b02f4380485edb29
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:30:21 +0000

    bored trip concentrate come love

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def count_words(sentence):
-        return len(sentence.split())
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
