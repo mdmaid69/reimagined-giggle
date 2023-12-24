59443c62b29e8851dfc05791b99284fbda699f5c
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:10:27 +0000

    have care idea shut exaggerate

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import array
-def get_array_length(array):
-        return len(array)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
