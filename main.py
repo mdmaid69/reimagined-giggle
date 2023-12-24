3c0138a295e547dbfaa722bc23dad51d16fecf45
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:14:51 +0000

    shame try comeover middle wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
