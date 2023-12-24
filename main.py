bb09a414c68807379a331a6371549a22588ea020
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:11:55 +0000

    turnback amount cook unlucky discovery

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
