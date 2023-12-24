a9766015f0239c57c33c3f967c328b56a2b27a12
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:59:09 +0000

    count fit feeling air rare

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def is_odd(n):
+        return n % 2 != 0
