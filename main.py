508c1372c4c52c084fba6df2949035c1021acbb2
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:31:37 +0000

    use takeout lower stand judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
