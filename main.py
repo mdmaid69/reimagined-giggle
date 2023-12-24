4dfaa676c068d195cb3a36ee98ccfc34867aa54c
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:20:38 +0000

    plenty in more hurry pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+def print_python_version():
+        print(sys.version)
