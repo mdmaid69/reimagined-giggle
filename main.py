c4f0164048f8ea66c388038fdc7aa1ca9c16a7a4
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:40:28 +0000

    traffic bear mess good walk

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
