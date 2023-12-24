7d8ad4e48513b46bffa8bbdba7e1e7ed80ed65fe
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:46:08 +0000

    here rescue use whistle offence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
