67146a06f679ee3e7c9a3a5fb6136ab14e16d5ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:04:32 +0000

    cheat walk mean through up

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_power(work, time):
-        return work / time
+import array
+def append_to_array(array, item):
+        array.append(item)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
