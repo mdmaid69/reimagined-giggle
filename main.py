011c70d56ad0aa80d66a743b6db5807eaacfaa1a
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:22:44 +0000

    than judge attract slight doubt

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def find_min(lst):
-        return min(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def multiply_numbers(x, y):
+        return x * y
