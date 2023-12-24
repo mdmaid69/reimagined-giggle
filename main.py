c73860864ca019e9a800326067f09f7bd09a7350
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:13:34 +0000

    ask left 566end free free

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
