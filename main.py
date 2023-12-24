e6f261c16d610e45d3a36bf09339ccc358d476a2
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:21:51 +0000

    touch rubbish sail left nose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
