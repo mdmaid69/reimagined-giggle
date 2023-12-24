bbee89bc11da96be60b7734f9d01054842868b1f
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:22:05 +0000

    pause over ball measure weigh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
