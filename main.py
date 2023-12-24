5879c83aff354928b1676f65022f451f90d11bcd
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:03:55 +0000

    towards power sore partner become

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
