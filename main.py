aa1d9ab5bc1783353b88b96fd3562839aba88307
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:52:27 +0000

    hungry dish takeover centre usually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
