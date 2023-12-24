f86d73ac47aa5c5c7663262a32c911f43624bd3e
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:44:59 +0000

    shadow problem Qq weekend early

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
