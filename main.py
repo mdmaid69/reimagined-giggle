bc3b2692d99ca6afb76489b7545c063894237aa9
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:09:45 +0000

    spill within lend comment dick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
