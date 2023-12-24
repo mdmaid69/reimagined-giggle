c11ac0be09e07e1f94869ef1edbae5cca3f8d2b9
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:38:25 +0000

    drive slowly throw definite list

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def find_max(lst):
+        return max(lst)
