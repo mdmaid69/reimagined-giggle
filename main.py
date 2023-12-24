66ef23a04b6b08c0dac3b4682bb48ac06c6c9116
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:48:09 +0000

    path slice destroy climb brave

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def is_odd(n):
+        return n % 2 != 0
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
