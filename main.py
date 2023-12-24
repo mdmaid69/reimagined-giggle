bbece991968d38d750860fa25ff2f90f8b7e05c3
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:29:22 +0000

    door flow breakdown limit contents

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sys
+def exit_program():
+        sys.exit()
