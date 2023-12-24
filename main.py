e20289804d6ea43c264dadcafb3e0fe0167b4f51
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:09:20 +0000

    key little watch support month

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
