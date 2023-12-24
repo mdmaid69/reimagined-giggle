cb04db3985487d267b363870213c6ac2238a3ea1
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:56:17 +0000

    round discussion minority giveback thinkthrough

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
