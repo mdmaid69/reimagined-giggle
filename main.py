2c83f8fa63eeca316b9b87cf7cd9cde3a89e0610
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:54:42 +0000

    control happy apparently slightly nature

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
