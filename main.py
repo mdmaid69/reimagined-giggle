fdbf65589951747e18466af05ea3316cb06f3cd8
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:37:51 +0000

    study match cheat interrupt fill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
