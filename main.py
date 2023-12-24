e45af8de43b639f03a272cefbfb1fe52c0314dde
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:56:08 +0000

    but ourselves range dear release

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_circle_area(radius):
+        return math.pi * radius**2
