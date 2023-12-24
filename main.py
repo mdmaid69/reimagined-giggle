75bc35a16498e1656ffae14576545d5b93d2555b
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:37:44 +0000

    answer smell each leg recommend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
