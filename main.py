f10f1077e7fe1f7e914cd7c1fd872d2b3c19160c
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:42:31 +0000

    protest scream cause nervous decide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def is_odd(n):
+        return n % 2 != 0
