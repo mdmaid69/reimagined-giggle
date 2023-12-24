e5322ec638472f34643b9e668bb57566e8b7050a
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:17:51 +0000

    as family wander design stupid

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+print(sum(range(10)))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
