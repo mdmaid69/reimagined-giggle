25b75c94920b9e61bedeb2404a8c42653750ca1b
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:45:22 +0000

    interruption heat public fall immediately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_as_int(array):
+        return int(array[0])
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
