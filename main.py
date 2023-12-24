879e985c12ded144744ef54cdb95ad4436496e84
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:09:43 +0000

    shut meet add state part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def count_characters(sentence):
+        return len(sentence)
