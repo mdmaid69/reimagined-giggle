e4b8a4e0660d6380417e997ec67c46b9899d014e
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:50:11 +0000

    through count surprise sight exciting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
