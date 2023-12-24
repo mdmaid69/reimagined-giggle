aa403d70da2edc1907f80161a97424e72f276514
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:41:21 +0000

    capital accidentally talkinto shop drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_sign(x):
+        return math.copysign(1, x)
