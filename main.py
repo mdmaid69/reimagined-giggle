6c57f943fc634a07b77dbfdc5d20d2362bd8ae0d
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:34:04 +0000

    interesting attractive fear considering people

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
