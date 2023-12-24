0be3b2b4fee390a78ea87b74a410a5b4b390c6a3
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:06:01 +0000

    trousers him scale secret rare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def clear_array(array):
-        array *= 0
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
