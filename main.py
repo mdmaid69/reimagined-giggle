d8639fdff3696cca1c5a672ec5b33ca07b49cf14
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:21:40 +0000

    noone note breath he safe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_area_rectangle(l, w):
+        return l * w
