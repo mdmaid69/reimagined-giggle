03823656c9756c2b641f1afef76038a46af742ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:20:58 +0000

    play check mixture target patience

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
