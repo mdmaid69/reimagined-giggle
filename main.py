04e2de940a210e4465125fdf8b7dd5f95d562209
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:02:24 +0000

    visit dream on avoid new

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
