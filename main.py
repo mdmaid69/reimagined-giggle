91546e30dcc2eb75b477a3fec74a1b28cae3dd1b
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:49:10 +0000

    before stir label wide meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
