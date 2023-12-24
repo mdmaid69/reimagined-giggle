2034726d57c2b757a987de45ff2bfc9ed7b43adb
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:10:05 +0000

    Itself range whatever cry in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
