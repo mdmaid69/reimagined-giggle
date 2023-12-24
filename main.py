ad4a8d91738959839fed914ada4976e8a43c6c40
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:51:05 +0000

    fence board discussion to radio

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def split_path(path):
+        return os.path.split(path)
