ad3d8ae5ecab4e440c00cc3bf685ef1b4254eec5
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:47:40 +0000

    mad demonstrate work Qq mad

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
