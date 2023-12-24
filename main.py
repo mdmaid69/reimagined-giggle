2e64075b138a5cf49f83bc9c4cbd527a0a53b546
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:19:50 +0000

    arm accept pipe refer wing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def remove_duplicates(lst):
+        return list(set(lst))
