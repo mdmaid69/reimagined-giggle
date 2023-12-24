eeb41a8fdb9b8d48e0a27981c4ef68f0e1214b5a
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:49:42 +0000

    winter tidy use whole reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_factorial(n):
+        return math.factorial(n)
