fae117d8fac6c3e75baecea7fd6cfb30d917fbb4
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:47:24 +0000

    get press lay might line

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
