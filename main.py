7b832cb6506d40ee0d8e8e3f9ebd3f8f5de3576c
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:08:37 +0000

    board uncomfortable both far matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
