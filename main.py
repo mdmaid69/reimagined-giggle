f63b0317b03744816184c62ba8b1f3f9816306ea
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:50:42 +0000

    argue sing class yourself issue

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def convert_to_binary(n):
-        return bin(n)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
