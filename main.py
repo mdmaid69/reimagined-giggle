243b612751dcf0623903a9edb29f2700cdce0816
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:45:33 +0000

    hurt whom rush determined friendly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_list_from_array(array):
+        return array.tolist()
