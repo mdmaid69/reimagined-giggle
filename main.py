1e369ec1dff3fbce8cb867bbade9ecc72d3e511e
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:53:11 +0000

    nor now blame sentence mainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_sine(x):
-        return math.sin(x)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
