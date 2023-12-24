a181735cba8c3660daa6e3e19623f37d41c423f0
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:19:33 +0000

    disapprove hope want machine borrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
