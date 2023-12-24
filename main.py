106942a42c3f43125d22fa474cdfdc274d736d14
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:05:48 +0000

    ride search blame phone stranger

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def find_max(lst):
+        return max(lst)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
