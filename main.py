61b986ffe6a8e41897906b2d5357e78982ceced3
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:53:08 +0000

    organize he popular into quiet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_work(force, distance):
+        return force * distance
