bb3d6c10ba7defff0c32e5a8b878c096bdb5c471
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:30:07 +0000

    male correct interrupt comein 568energy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import getpass
+def get_username():
+        return getpass.getuser()
