76ce88dadf6e03b3a6c55241cae79cffd9f32a9c
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:52:57 +0000

    murder themselves shoulder load measure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import random
+print(random.randint(0, 100))
