3328a734082350d7188d9facf1a15d9e29bb7691
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:20:50 +0000

    plastic bringup soil wall runover

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def square_number(x):
+        return x**2
