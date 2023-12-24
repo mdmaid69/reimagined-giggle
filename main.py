e67ac61c52aac16f49c48911124252c383e190af
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:47:19 +0000

    sink live impression cheap responsible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import sys
-print(sys.version)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
