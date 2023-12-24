77c2b06000bd6a9d9856ed6297390eae28025f5d
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:20:18 +0000

    repeat weigh off spend worse

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
 import datetime
-def get_current_date():
-        return datetime.date.today()
+def get_current_datetime():
+        return datetime.datetime.now()
+def find_max(lst):
+        return max(lst)
