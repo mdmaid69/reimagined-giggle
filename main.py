f690996c101e59b8b3c275d6c03ed74ac579ea52
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:27:08 +0000

    run empty least either first

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
