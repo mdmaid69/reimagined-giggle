29026667abffb9e262d07b7986a22dd0b709bc19
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:49:22 +0000

    forward ticket note and deliver

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import getpass
-def get_username():
-        return getpass.getuser()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
