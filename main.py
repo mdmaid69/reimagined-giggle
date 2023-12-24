1eb238f9f8e7eb6555da9aa20789b6fc77aa2e0f
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:25:42 +0000

    cry hang measure other smart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def add_numbers(x, y):
-        return x + y
+def get_array_as_float(array):
+        return float(array[0])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
