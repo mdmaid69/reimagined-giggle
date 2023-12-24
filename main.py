7ba28f87d50b0dcda76a1253efad4a4f10138737
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:00:34 +0000

    hot form programme badly material

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_current_directory():
+        return os.getcwd()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
