7a262a58f812a4145afb1a1dd7fb4faf409179f7
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:43:31 +0000

    573entertainment carry right suit weigh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def find_min(lst):
+        return min(lst)
+import array
+def get_array_as_str(array):
+        return str(array)
