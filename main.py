197f4bd8c0bc1178910c63ea126c565b9144a146
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:37:04 +0000

    risk short score keepup admire

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
