ec6a8acc99d3a01535a185ed11b97d8b5b2d385d
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:17:22 +0000

    talkoutof argument wide whether large

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+x = 10
+y = 20
+print("Sum:", x + y)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
