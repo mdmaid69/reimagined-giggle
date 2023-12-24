68f0f55ef02f385f9c6dff65797f109ae26bf353
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:16:47 +0000

    accident glass boil defend unite

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
