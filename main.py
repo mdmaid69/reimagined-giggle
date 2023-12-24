3a93a458537a6b4c25f3157a978f545e30f3f6cd
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:35:17 +0000

    style sticky soft move correct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import time
+def get_time_since_epoch():
+        return time.time()
