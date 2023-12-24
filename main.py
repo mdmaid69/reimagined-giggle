9366a3571305304dafa1e3dc75f3768f45edd2e4
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:32:50 +0000

    paper mad brother emotion leave

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
