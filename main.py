493b98553b073fb3186c2c73433e51b1251c7098
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:40:57 +0000

    century want getup space blow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
