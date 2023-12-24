26b0a8a6eb685e258c04cdd79e11f57da605b8ef
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:49:27 +0000

    great nervous close crisis protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
