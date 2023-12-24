02a179ac74cce376a4fefe911735f4f45ce66a3e
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:49:56 +0000

    space together tough formal winter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
