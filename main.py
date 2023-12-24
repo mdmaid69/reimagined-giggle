f6f6f8553e3d24e4e0b4ecf90e20a50ba8b92383
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:46:57 +0000

    shadow gather great famous arrangement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_bool(array):
+        return bool(array)
