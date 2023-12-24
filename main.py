8481f6d431bf481c5bf2b45f087e95c41e0e266d
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:37:20 +0000

    throughout desperate fasten several stupid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
