d5f049ccb020dbaffc368ea8ce157b1b642f61f2
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:58:59 +0000

    juice place show suit autumn

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
