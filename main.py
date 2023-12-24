c445e188e7944b287cbb8bc2d4d3575e25379c0b
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:06:06 +0000

    giveaway bread fit ready machine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
