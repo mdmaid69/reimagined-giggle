b71aa8896ba708af644a94b8c71960c829c4e489
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:04:55 +0000

    lift tax check build today

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import time
-def get_current_time():
-        return time.ctime()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
