798713782632c070cdc9d6b3ca7f5ea82d3f63f4
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:09:55 +0000

    both room conclusion responsibility keepout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+for i in range(10): print(i)
