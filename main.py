0b7954c4753af4ce9c03b7a304c2362f7210e63d
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:59:38 +0000

    advantage public grow include which

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_power(work, time):
-        return work / time
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
