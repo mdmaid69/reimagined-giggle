d705ab8014a2a57a4b36aee476efdb256644e84f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:17:52 +0000

    whichever near correct objection shoulder

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_as_bool(array):
+        return bool(array)
