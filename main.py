328993e45ebb76a057c72683fee386f7dc4ce476
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:30:46 +0000

    shoot temperature strike trip lead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
