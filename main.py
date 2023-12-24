4a577a9e05b55a86834825de205a1f6fafb963bb
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:08:37 +0000

    unable considering switch reasonable yes

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def multiply_numbers(x, y):
+        return x * y
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
