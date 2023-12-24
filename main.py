3d5d71bea01dec04703a68d008291dcb53853f00
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:50:34 +0000

    band copy certain clear check

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
