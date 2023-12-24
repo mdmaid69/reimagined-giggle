4a353afc7c933b6f6ee17a2047330454d54edfe3
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:38:22 +0000

    build century come concerning rank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
