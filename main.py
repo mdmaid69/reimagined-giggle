c97d22e873aae2054e226d1cb2c06301c753eb55
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:02:46 +0000

    giveaway rule definite obvious gooff

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
