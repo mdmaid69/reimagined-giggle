8899f363cb06b7d4461a6438f99977131c7b0274
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:50:07 +0000

    just sometimes interesting set sing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
