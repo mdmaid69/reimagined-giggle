3465c5a1c1134be544f42355726ec1bf5d584203
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:48:32 +0000

    reject turnout them rain organize

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import tensorflow as tf
+print(tf.__version__)
+  def calculate_area_rectangle(l, w):
+        return l * w
