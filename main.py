50086a220d7e4b6b5751a02e744b355e4b44f403
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:55:14 +0000

    intend awake divide shade phone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def reverse_string(s):
+        return s[::-1]
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
