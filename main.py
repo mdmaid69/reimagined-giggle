f3da6e6b761dffd0b88583def80be53cfad8cfbb
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:42:14 +0000

    praise better properly stick ready

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  def is_even(n):
+        return n % 2 == 0
