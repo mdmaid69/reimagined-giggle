8055926b5a76de3d2215f8c274f16284e1e4fb09
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:12:28 +0000

    healthy bunch mine gas picture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def subtract_numbers(x, y):
+        return x - y
+import os
+def remove_directory(path):
+        os.rmdir(path)
