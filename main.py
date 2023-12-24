ba48bf07cf9562ce7677d260c7bcd26d10c9ff3f
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:34:58 +0000

    wall immediately pity challenge gentle

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def find_unique_words(sentence):
+        return set(sentence.split())
