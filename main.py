75dd16a00706b2342f0992779f2abae357a8b297
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:49:08 +0000

    estimate trick meal he name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
