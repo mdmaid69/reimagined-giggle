cca3172a7addf0e006517860f40f6cd9c5a55ce6
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:10:16 +0000

    contents goback(to) contact will reply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+def convert_to_hex(n):
+        return hex(n)
