6cdb268402a4d0c8fa3d3d4c5fcb0683b13f57e8
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:07:18 +0000

    professional side fork crash die

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
