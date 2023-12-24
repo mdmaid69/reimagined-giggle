d7c4e34dc119be0e7ab9955ab41338ba53853726
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:43:17 +0000

    hot factory translate lookaround chin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
