e5a62c0c63ad2083dfc5ef76f8d9ce5961c4a3a2
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:24:34 +0000

    brain admit small organization print

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
