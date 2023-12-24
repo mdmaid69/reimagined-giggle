53c98564eb4225885e1671e0a4574fe2de8c8f60
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:04:13 +0000

    keepoff half story community simple

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_floor(x):
+        return math.floor(x)
