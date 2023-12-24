40de9aaffb273a0ea92e5cc638f2461a7ee9129e
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:48:36 +0000

    owe hide that happy wind

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import random
+print(random.randint(0, 100))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
