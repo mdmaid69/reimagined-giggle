236e36793abbb70e84f73f95c50dc7d7d1b97f0d
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:37:42 +0000

    tip deal dance wide ever

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
