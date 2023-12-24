8e5d7e155bac901684bb55c4e81a53cb9abcbff7
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:38:47 +0000

    unusual they free care very

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
