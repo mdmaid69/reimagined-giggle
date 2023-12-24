67a15cd6c9663e2593d7dea90d27eea3b53d33ec
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:58:43 +0000

    sure tape another die this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
