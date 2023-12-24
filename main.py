8afd9eaeceb7815f83244d3d27daecf98bdeaa8f
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:09:23 +0000

    absolutely chase by you watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
