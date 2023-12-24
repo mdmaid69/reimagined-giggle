3ad627f5b7f3bda8b96d5a2c1585a684659eea86
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:04:42 +0000

    confused survive especially eat spray

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def sort_numbers(numbers):
-        return sorted(numbers)
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
