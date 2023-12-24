41aaed3e031ab85800efe5e0d5fedc9b19a5c724
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:43:52 +0000

    use open progress week centre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def count_elements(lst):
-        return len(lst)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
