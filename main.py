d7de504f59b9bc032d6e8efed85d89f6bc746f2e
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:35:11 +0000

    fork film mistake disappear wipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
