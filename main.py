c27b9620141e769c69d09386ae2b972cdf94960f
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:49:48 +0000

    store permission exist drink yourself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import platform
+def get_python_version():
+        return platform.python_version()
