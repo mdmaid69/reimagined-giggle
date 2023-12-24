30a3153ff960412b92c0c08c517c8ee8e99ed717
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:35:09 +0000

    name criticize dead emphasis part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
