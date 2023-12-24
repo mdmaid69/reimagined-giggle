4215c1ee99dc311e1423a2944a9a04151d0416e9
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:06:59 +0000

    relation limit eye go estimate

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
