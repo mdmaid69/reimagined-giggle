6db3e81703a9f05aa243476bc6a48f3285d3ff8e
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:35:59 +0000

    exciting part human pain step

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def count_elements(lst):
-        return len(lst)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sys
+def exit_program():
+        sys.exit()
