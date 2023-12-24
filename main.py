eea8831ff0086627085c9967bc47947574967b54
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:39:00 +0000

    set precise comment suppose waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_length(array):
+        return len(array)
