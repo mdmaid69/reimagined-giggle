ccfa8b1e0e6286206c650cc7855105b02c82610f
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:20:01 +0000

    invitation favourite rid risk go

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
