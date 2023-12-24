8c4946400040aaf5b7d5e90181ff0e492e6c8115
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:22:59 +0000

    payment accordingto steady dance clue

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_as_float(array):
+        return float(array[0])
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
