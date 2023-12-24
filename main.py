4662d3d8c1922840c94c77b4a32aace9fdd08b8f
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:04:58 +0000

    pen trip himself storm experience

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
