c55f12639b0bb294c9dba05cac09e6b81ac25ac3
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:17:11 +0000

    violent below most noise morning

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_floor(x):
+        return math.floor(x)
