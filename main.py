fe76f95d4f033d33a44f0776e851b96131168231
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:34:34 +0000

    yes pile face talk proper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
