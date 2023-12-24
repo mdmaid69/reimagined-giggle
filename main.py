a7d22fbf1e19cf0bcf2e578e06e8601bc63d4a69
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:58:05 +0000

    pipe tip proof ability damp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def change_working_directory(path):
+        os.chdir(path)
