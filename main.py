74088b3f1246763f52b748bd4a676794c30e8c30
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:48:45 +0000

    another future completely speak speed

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
