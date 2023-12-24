361b55a84ee3f38fa38be14d62af8e425e0883ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:12:37 +0000

    key substance else bye consider

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
 import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def create_ordered_dict():
+        return collections.OrderedDict()
