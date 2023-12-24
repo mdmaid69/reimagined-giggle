0c15836f9d3216b4f295ca22bf41fee3f06ebaaf
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:44:31 +0000

    danger rescue over and expensive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
