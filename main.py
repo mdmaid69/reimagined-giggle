3a268b397034b6d87b9153a84015f031fde035af
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:22:55 +0000

    reasonable fire advanced road introduce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+print(math.pi)
