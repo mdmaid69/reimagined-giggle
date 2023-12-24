2acfa4efef2504ce0f876b059e0e64b52ccf0172
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:41:03 +0000

    view minor inside chin fond

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def square_number(x):
+        return x**2
