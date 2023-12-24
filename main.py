4e68199787a45d6364c62d6717ce38ba189403b8
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:43:19 +0000

    568energy standard other traffic majority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
