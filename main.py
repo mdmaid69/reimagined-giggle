a44b9fc84651a99cca8a9ff47da02418e8b3a7e2
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:45:46 +0000

    respect failure shop leg noone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
