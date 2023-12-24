1dcae23c7545f9600a2cc8dc9048d92d4d2bf5e7
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:45:12 +0000

    team direct card fire difference

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import platform
-def get_os_info():
-        return platform.uname()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
