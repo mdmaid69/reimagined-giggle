5df62e19178b9d97339b99a066712274accf7ed7
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:15:16 +0000

    question talk ground stare suggest

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
