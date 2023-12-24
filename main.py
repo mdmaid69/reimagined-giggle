0e093a1c1e504189aba63158256879ce261b9ccd
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:23:46 +0000

    lock sticky agreement fail another

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
