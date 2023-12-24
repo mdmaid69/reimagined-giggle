603405ca246d13b01c23044771ff63db22f3b69a
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:30:12 +0000

    dig stare would continue radio

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_list(array):
+        return list(array)
