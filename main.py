d001a4553ecab7f7f0f5b35bbdcfe6192f2c42da
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:30:53 +0000

    lend mood sting comeout afraid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
