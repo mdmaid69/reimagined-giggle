3202bf68551ff92c734d5ed38e3ad376977ab317
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:47:24 +0000

    lend for wealth limit fortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import getpass
+def get_username():
+        return getpass.getuser()
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
