f7a530b96688efa8c0ad5fc030cac799a48c4c1e
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:01:51 +0000

    through 565end form loose shoot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
