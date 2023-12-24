6de5b6fbad598ee181b5ac08e10043f3f1e84c9d
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:41:18 +0000

    backwards female drive bound surprised

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def is_odd(n):
+        return n % 2 != 0
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
