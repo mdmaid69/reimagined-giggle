1ed8bd3cdf587930dae531c0060c6d8fec3bf13d
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:46:47 +0000

    phone keepfrom debt tell prove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_repr(array):
+        return repr(array)
