d15e85eda00b1b12b158ff05332804f1faca0e98
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:51:28 +0000

    exchange pinch neck patience difficult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
