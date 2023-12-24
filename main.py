7808a9e30d62be960737fb35869f16f27d68bb7f
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:24:46 +0000

    identify guess mood wide study

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
