c4856755fc7ea4d7e424e83a0725b4e57a609da0
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:28:02 +0000

    art scrape other bag street

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+for i in range(10): print(i)
