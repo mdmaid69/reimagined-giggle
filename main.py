763b1da0fbf483e3af87c4d392ee45b20d100fcf
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:07:53 +0000

    female expect holdon determined 179bite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
