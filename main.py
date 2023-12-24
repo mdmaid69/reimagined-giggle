53a51eaace775957387c4bf4bfb82ba591bdd7ab
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:33:15 +0000

    away birthday disgusting beneath sink

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_area(radius):
+        return 3.14 * radius * radius
