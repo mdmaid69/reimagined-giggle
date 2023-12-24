5939fa5fedfba2005b30cf24be25dfdc11cf4ec5
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:57:37 +0000

    connected in ilovedicks impatient lift

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
