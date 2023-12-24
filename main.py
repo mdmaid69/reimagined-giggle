86673e8ac47bfe6cb91a8f0ab377a05698eb8a50
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:35:19 +0000

    study hello case age square

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_repr(array):
+        return repr(array)
