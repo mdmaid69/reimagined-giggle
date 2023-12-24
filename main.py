a8645a3042d77e1fdf205382a74a67fc898f4e23
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:34:54 +0000

    immediate straight school less flight

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def convert_to_octal(n):
+        return oct(n)
