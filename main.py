ff28404696570d20a2bd71e79bf54bf1d31ebcdb
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:12:03 +0000

    create open wise fall frequent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_bool(array):
+        return bool(array)
