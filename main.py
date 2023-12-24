e13f0faaefc11b194cf5dae5419e1dace71c9f3b
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:39:27 +0000

    turnover circle continue patience noisy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def split_path(path):
+        return os.path.split(path)
