7e0d067ec249fb572507632ec7588da4f643af48
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:58:02 +0000

    pool free catch unpleasant turninto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
