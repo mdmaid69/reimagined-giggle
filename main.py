aeb7e576f4c0e8eeaf2fe45a6b73c736ac36846c
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:31:01 +0000

    inside pride search react protect

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
