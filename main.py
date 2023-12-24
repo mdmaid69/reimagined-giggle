163adaa9e16b0b2636bb716483da2784e064044e
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:34:01 +0000

    any somehow lead act prove

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
