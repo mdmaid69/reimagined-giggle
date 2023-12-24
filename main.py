9386af462dd899050398d97224b88043e4ced4e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:19:54 +0000

    shame breakin cost lost comeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
