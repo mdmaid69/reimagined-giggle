e1c6ac9257465fb554c74638ed9d11b9330edc64
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:40:48 +0000

    OKorokay study wind container runinto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
