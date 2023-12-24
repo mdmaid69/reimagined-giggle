89f7e68664074afa2973bd52cdab238a2a526ac5
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:03:53 +0000

    whenever serve lookaround beat proper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def convert_to_hex(n):
-        return hex(n)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
