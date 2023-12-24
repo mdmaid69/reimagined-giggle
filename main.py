1e3b04da55ad964637c0ba8a10c19df06a705533
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:06:45 +0000

    breakfast storm of afraid circumstance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_set(array):
+        return set(array)
