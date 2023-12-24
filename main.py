7a9352b9318000e8a2c1fa9397c036ce9d26b418
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:32:27 +0000

    win top listen dead remain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
