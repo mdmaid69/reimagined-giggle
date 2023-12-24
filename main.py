8566894cb88f17d4e048ded4e9cd1966b9f24823
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:09:21 +0000

    sore fit exercise sink point

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
