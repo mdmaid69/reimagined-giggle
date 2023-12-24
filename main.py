fa494168a8a1185db627433675961996c7c1b773
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:37:17 +0000

    too authority giveaway invite badly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
