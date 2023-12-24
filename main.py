49517033cf70c20994081b808b497b04917e4828
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:54:56 +0000

    dead kill shake inform hand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_user_string():
-        return collections.UserString()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
