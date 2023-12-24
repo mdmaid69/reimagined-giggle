90c460ff3150324b309b20309a7d55961fd344d1
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:48:39 +0000

    fact small blow deliver by

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
