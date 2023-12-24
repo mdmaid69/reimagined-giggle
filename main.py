5c658189d58419258f99dac98cd1142287e6b4a1
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:54:51 +0000

    unfair hide risk part 179bite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
