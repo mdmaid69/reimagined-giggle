00d990f9aa16af64edbb2bf795160e12b6728e6f
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:31:47 +0000

    good miss people damage firm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
