4570d0b2fab6cc33217d4e7a47ffb4c00c4f9d52
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:06:14 +0000

    introduction fierce quality pair it

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
