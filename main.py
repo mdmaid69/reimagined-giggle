47865f027511882febe0d19a3aef7c97e0ec02da
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:54:21 +0000

    match content onto cheat freeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_min(numbers):
-        return min(numbers)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
