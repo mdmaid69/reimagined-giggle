9897f34b5953490f0bdaf6fa8bf0fafeb6f1dad6
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:56:17 +0000

    patient start bottom list use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_density(mass, volume):
+        return mass / volume
