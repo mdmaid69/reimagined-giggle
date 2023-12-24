662c5af1226d8d1baaa9383916bf8ee9cbe76f58
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:10:10 +0000

    behaviour suppose cinema great someone

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
