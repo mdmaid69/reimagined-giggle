450673e4c1067ae9153e7c6d8333d94a0a72d5e7
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:32:00 +0000

    guess expression different inform soil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
