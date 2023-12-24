34996b589f8bdbd424955f93080ddb0068497e82
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:33:11 +0000

    take flow prize mark destroy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def find_max(lst):
-        return max(lst)
+def get_array_as_bool(array):
+        return bool(array)
