0f2006a66a4f1140cb47a50975f8f19e39cbfcd5
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:15:09 +0000

    style corner Ee hurry once

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  def add_numbers(x, y):
+        return x + y
