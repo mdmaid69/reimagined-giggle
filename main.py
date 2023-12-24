208469d01fcb4db3788ba964de350eeb22247747
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:11:51 +0000

    book tear unlucky bar matter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_palindrome(s):
+        return s == s[::-1]
