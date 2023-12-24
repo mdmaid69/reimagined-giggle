19c258917dd7ae7e40884ae25b7ea44b29b64a40
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:53:08 +0000

    method crime past touch takeoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
