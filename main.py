9ec0556592681c07a5ed9e620381a43583b74d34
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:11:20 +0000

    film translate pain rescue always

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
