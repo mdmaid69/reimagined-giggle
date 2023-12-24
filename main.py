7efba52bbe450c06db6d51f0e36d6db921336e85
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:41:19 +0000

    art nothing apply OKorokay common

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_set(array):
+        return set(array)
