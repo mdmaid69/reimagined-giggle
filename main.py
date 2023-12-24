3c8e0ac230541ff07ba41aca82c28a0c42995754
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:58:36 +0000

    scared easy pay fill until

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
