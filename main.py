10f70232c5ea0077dad31abaf970a8aa7bd0d5d8
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:40:59 +0000

    advantage thinkof fence chat active

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def get_array_as_int(array):
+        return int(array[0])
