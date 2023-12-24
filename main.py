ebef8cf16938c33b68e903a482b5ecd5759f27d2
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:43:48 +0000

    fall borrow tired hers genuine

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_max(lst):
-        return max(lst)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_sine(x):
+        return math.sin(x)
