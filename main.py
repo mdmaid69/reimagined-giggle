3f0c76d1750b47dd64f9c833c0e557fed8cc28c6
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:07:31 +0000

    late fail speak tight then

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def multiply_numbers(x, y):
+        return x * y
