bffd8354f3d339b78a7bc7e6d510a5a44e46ac95
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:31:54 +0000

    same report standard punish skin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
