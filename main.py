77d0388c39fe6f52d0192727ab607fe9bf1d333e
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:56:23 +0000

    exaggerate quantity different generally demonstrate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
