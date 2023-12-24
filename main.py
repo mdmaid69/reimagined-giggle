5c61741f22cc4fb74c76f12c8bc1dc9b61c82547
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:22:05 +0000

    run behaviour reasonable full understand

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
