b7e71bfe7906c076ca3697d6110d6300673b24fc
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:47:30 +0000

    instant actually match pinch crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
