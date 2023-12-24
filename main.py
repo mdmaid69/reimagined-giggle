0a5e6adcc9ea4d16666223307dcfee4f6ba19ebb
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:34:47 +0000

    welcome identify blame shake frighten

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def split_path(path):
+        return os.path.split(path)
