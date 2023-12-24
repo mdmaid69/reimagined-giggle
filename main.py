e3d4746f6e6cac4f9babb88e0b8ba6c707042863
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:16:45 +0000

    would case suit congratulations limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
