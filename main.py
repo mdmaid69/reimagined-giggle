fb62277540881c31cdc65af07e3a2f7ba5d98e48
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:15:17 +0000

    light wrap suit ball subject

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
