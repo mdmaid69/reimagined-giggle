1a4a4899b4605bbef78c9a47c73b36d1dfd936c4
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:22:34 +0000

    bottom stamp judge pretty plan

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
