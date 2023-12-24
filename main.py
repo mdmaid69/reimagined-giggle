c03621c49e12ed16a2b1394c7728a1887e030668
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:04:12 +0000

    active would stiff remain list

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
