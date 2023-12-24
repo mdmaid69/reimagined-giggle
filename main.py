ba7e2d71948fb0b7a83256524abafcbca5c5b67c
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:07:48 +0000

    set talkback marry mainly sick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import tensorflow as tf
+print(tf.__version__)
