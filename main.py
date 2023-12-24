f78f6765bc9b44b704ee2b3c44354ad7a5132734
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:36:57 +0000

    danger action surprising how out

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
