9cf4897620a9d6e076c6bbd5056879e2ba4522ad
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:09:05 +0000

    bang towards gather year their

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
