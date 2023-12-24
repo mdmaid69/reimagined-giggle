2b35ff7443ba85a08055d781d26e82e5c0ba0160
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:54:27 +0000

    sing yet people stamp uponon

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def count_elements(lst):
+        return len(lst)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
