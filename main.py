18c86baeb547711e45f151ee1cdcee7812f96312
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:06:25 +0000

    on Pp section moon mention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import collections
-def create_user_dict():
-        return collections.UserDict()
+print([x**2 for x in range(10)])
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
