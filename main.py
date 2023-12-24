c3a5f5444e984103c13be04485412578447aff6a
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:16:28 +0000

    science sorry ought slice rather

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
