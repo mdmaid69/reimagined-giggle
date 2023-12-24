1814abfc444206651bb8a5a774f51f57700f4c02
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:19:49 +0000

    contribute story warn leave smoke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
