8f4435f877d59e1a83fdba5ddfd2a265628ba7c0
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:20:12 +0000

    subject fond straight clear happen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def sort_list(lst):
+        return sorted(lst)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_index(array, item):
+        return array.index(item)
