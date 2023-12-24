3a995cf4adb7547584ddfcb24c1be33a1a18a02a
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:10:38 +0000

    pronounce along broad callout check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
