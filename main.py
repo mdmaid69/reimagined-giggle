0c01a24ef57cd9c8069b68868ac9cf030fd266d6
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:16:32 +0000

    its sea sit memory equipment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import random
+print(random.randint(0, 100))
