045565fd900738f29cb2e7f26a4af44a80c4c4a1
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:17:37 +0000

    use kind bear accept consist

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+print(sum(range(10)))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
