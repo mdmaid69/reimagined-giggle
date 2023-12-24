b94d8ebfa4337f38d166e8297a9ed8b21ca487cf
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:50:42 +0000

    future suit before silent spin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
