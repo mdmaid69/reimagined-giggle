52f3d53bd62b674dd7e78684bb1e659c28122ee3
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:18:35 +0000

    could improve long land total

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_exponential(x):
+        return math.exp(x)
