2c935d6ab2fb3b8781a5841e05de5b85dad352b0
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:19:02 +0000

    go matter temper beginning shelter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import os
-print(os.getcwd())
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
