1130ebc557baeecd6795cf9ebbe5c62cbe90a9c0
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:43:37 +0000

    walk matter light total help

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
