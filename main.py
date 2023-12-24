a5f2c6591544699fa19039c95a712497b6b78696
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:20:17 +0000

    after correction mark platform honest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
