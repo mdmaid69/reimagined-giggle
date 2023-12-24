8e4569c55173c6e11cefefb7d3a4197d7598b7bf
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:48:53 +0000

    sort join you quickly profit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
