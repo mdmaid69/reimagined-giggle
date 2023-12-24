86dc56f132850149f551563481f3cf3d315f5b7c
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:16:56 +0000

    thank intelligent peaceful gate thinkover

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
