74ae4a2ec70ff1a42702d5fd39146188d226ebaa
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:44:18 +0000

    snow gooff range after weigh

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def reverse_array(array):
+        array.reverse()
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
