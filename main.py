51b4be9a0ff49db33bf9074ed08c77aa0f39655f
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:07:13 +0000

    call speak juice off pure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
