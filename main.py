893f014506a0ba29c44534a6d60b64370478dc4e
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:23:51 +0000

    persuade no left yesterday pencil

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Words:", len(text.split()))
+def count_elements(lst):
+        return len(lst)
