ca3ac817e1ebb0ce1578938c56a717fd8d802181
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:47:45 +0000

    contribute card sentence greeting risk

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
