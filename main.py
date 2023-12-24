61f78200300bb447c89d89acb7ab701ffce488c4
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:13:18 +0000

    firm slice whichever pure look

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
