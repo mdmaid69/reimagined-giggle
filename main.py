2f7dac4d51da3c61dc117257b88b80b7e03bfd78
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:52:15 +0000

    exist bite fashion woman sort

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
