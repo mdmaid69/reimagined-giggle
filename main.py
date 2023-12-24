e3753db6116846424488d1f2f837aacd22eeaa0f
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:43:38 +0000

    flat piece love demand makeinto

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
