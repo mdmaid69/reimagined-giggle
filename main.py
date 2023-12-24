d712bdc8765e0abcb7193722ff42d1ab75fb0dbd
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:07:19 +0000

    spend with great progress gentle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def count_elements(lst):
-        return len(lst)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import os
+def get_current_working_directory():
+        return os.getcwd()
