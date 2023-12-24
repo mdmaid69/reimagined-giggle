5bf61b9d3e042679245c62b202c9e15646e40cdf
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:00:28 +0000

    reject any various mail payment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
