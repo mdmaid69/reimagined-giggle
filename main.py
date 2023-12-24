44c396c36ab0c1e83398d296bd62be9d479d63df
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:00:57 +0000

    about immediately manner priority account

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
