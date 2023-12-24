f88b90cbdfb99ec3113987e91a5cc41c84df5e96
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:03:07 +0000

    example charge discovery brain once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
