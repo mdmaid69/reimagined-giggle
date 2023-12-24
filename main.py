56bb461218ea30f415a7ef3d25d419eb6288019d
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:56:21 +0000

    thanks trip bar imagination Jj

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
