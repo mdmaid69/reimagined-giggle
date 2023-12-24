c6c9485e2cdb906298bc607ba405bce2485d1ff4
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:03:28 +0000

    fairly somehow gradually for connected

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
