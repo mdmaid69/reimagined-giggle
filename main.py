c136302da8a4177dce411fb7bf2fa250fbfff119
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:34:48 +0000

    recently community set awake yet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import getpass
+def get_username():
+        return getpass.getuser()
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
