a0747f1c5b889a38e9adc3df38d23524d36100c8
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:47:52 +0000

    favour right welcome frighten autumn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
