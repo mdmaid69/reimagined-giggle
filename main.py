3275792e1e9fa03e71fa5fbab76847ce894f92cf
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:53:49 +0000

    run opposite wrong load off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
