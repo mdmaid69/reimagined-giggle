a035c8e074d5a5447119f8d062204557eea05d6f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:48:38 +0000

    love tune none material comedown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
