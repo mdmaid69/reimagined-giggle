06b02ad57cc28d3062978c4e21ed88b398354442
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:20:40 +0000

    herself any themselves stupid interrupt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
