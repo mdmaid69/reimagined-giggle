232d51d0907b11c856831f0888a0966d517363e6
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:16:42 +0000

    school record trick next obtain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
