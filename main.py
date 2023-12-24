c31514ff1c5deb31569a6a3028964a59c9fa6bed
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:27:08 +0000

    pressure laugh young tidy method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
