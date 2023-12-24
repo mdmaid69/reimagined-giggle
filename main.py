f4a66157966934f16e8311d5ec0699b164a4cf03
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:39:19 +0000

    receive watch rank reply alive

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
