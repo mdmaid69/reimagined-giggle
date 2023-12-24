2a6f778510063033948f1dedbaa40063d21db5b4
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:02:54 +0000

    cut talk already year hungry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
 import math
 def calculate_product_of_sequence(start, stop, step):
         return math.prod(range(start, stop, step))
