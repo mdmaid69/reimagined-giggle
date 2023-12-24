f376bf2866d9333199fc70b5957a3539cf18330b
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:49:13 +0000

    company tend skill pupil stupid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
