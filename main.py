8c496fba02bbc7c422115265cb7c5cdeb7299eb5
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:31:19 +0000

    under even nose no accept

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_str(array):
+        return str(array)
