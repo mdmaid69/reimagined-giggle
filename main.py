90fbd84ca26335d5e7108869818727c652503244
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:17:28 +0000

    pump quality pronounce shop plain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
