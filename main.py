9b8978cd9a1bb1695ce145988bf0d1fd8130a6f1
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:32:52 +0000

    port supply satisfied phone cook

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_string_from_array(array):
+        return array.tobytes()
