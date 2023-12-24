5f3578373d014f553dff93f03db119ad67b0c389
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:24:57 +0000

    smile there wipe trick happen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
