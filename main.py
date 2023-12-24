3c5e09aef94bb61bcaa72f7d4134790260903eb7
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:36:20 +0000

    swallow getout work uponon here

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_pressure(force, area):
+        return force / area
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
