556b723e31ec016ffa91e2cb47800ad2656f5feb
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:15:01 +0000

    concentration guard chat inside offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
