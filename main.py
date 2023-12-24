e54c587a9738b57d40380152ce3a08242605bcc1
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:00:40 +0000

    excuse duty count moon mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
