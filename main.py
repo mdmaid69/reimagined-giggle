b25be4855b00f9dec3b57d6a16a5d29c01751278
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:26:03 +0000

    preparation loan bringforward brain past

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def append_to_array(array, item):
+        array.append(item)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
