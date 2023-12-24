cd46bba1e4a997f19c9dbf5b10d7ac58472829c3
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:45:48 +0000

    quit side discussion may print

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
