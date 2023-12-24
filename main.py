3fe453af2514b7751894e098e17e85dc5186a382
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:40:16 +0000

    translate rank television somebody hide

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
