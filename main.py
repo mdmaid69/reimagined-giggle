3a11c7b9928c5b808289985e06c751c015740485
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:59:23 +0000

    wash steady moon little actual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def create_ordered_dict():
+        return collections.OrderedDict()
