5d685b8fe0a39dc65cee80c3dd0e3f2fc22473d5
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:53:27 +0000

    private healthy cause tall announce

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
