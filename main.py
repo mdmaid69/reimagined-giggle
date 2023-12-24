7c05316c5137dbac6bdeca7b5c2c938b351bcebd
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:49:29 +0000

    occasion yesterday man sister shout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def count_words(sentence):
+        return len(sentence.split())
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
