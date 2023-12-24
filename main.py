34e1628b2af609998691011fa9a30c71a80f5080
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:01:58 +0000

    laugh turnup opportunity tour makeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_binary(n):
+        return bin(n)
