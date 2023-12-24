99b7b8a99a250173f24085f033606df05186aa60
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:41:47 +0000

    wave win recovery swallow accordingto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
