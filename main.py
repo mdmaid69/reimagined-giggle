2fc9a5f11ad96d6afab92d2596395b5e86792db9
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:52:23 +0000

    high summer imagination about culture

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
