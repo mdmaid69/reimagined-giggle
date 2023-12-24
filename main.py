d97043f33f7415c968fba6420313fc029521a8df
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:15:26 +0000

    grade nowhere floor spring pupil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import time
+print(time.time())
