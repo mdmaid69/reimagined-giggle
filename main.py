67982eb1f95f57101a371c9ce61d17bd8b0fbd68
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:35:28 +0000

    turnback other drop programme share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
