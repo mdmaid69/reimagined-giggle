010ce8fbb75044b9c3e1f9a32406c5ccd47c229a
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:42:45 +0000

    apply otherwise creature tend regret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def reverse_array(array):
-        array.reverse()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
