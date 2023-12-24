19a8ba3404ef8d47186db9275ad396eaa708e7c3
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:43:41 +0000

    Rr cinema dead ready convinced

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def is_even(n):
+        return n % 2 == 0
