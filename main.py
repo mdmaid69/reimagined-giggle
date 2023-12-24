0a8818f87ec37ecb48576fc09b993c2576697b4d
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:11:38 +0000

    announce protest make smile emphasis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_work(force, distance):
+        return force * distance
