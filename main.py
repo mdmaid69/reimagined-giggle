f64352ff9dc64740d1dd97386a9dd18b0c4d4e6f
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:04:28 +0000

    near dear autumn follow centre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
