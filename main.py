7a8958df5e7204e382b456d081c13add9e21b390
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:59:12 +0000

    fortunately main shall yesterday abroad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
