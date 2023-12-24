b273a119deaa2d19b169a8413d24787a214821ae
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:59:11 +0000

    work keep official criticism pool

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-def is_odd(n):
-        return n % 2 != 0
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import collections
+def create_user_string():
+        return collections.UserString()
