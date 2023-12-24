bb3e2ee4c9a0579bd92887537a78a3d4203fa3a0
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:04:10 +0000

    produce press remember dry afternoon

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
 import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+print([x**2 for x in range(10)])
