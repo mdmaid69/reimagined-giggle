14cf50f70e01f5cb3dd439b8a1ef420d1eabb757
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:05:12 +0000

    actually where during not seem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_itemsize(array):
+        return array.itemsize
