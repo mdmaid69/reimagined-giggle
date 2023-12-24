ee316381f56f80e2fea151c917142f3e0f2980af
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:16:13 +0000

    hit file race address trap

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def append_to_array(array, item):
+        array.append(item)
