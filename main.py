e232d4b238d15705ef006c1c41b858d61648ff2d
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:43:32 +0000

    suppose nothing decision brush doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
