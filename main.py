090d51afeaca3b99710b73436f0251c021ab4b39
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:29:03 +0000

    principle mark late train music

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+print([x**2 for x in range(10)])
