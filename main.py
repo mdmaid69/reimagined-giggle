0a4b1d9238b0c98385911a73d7328a3fa2507919
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:57:58 +0000

    die order style welcome explain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+for i in range(5):
+        print(i)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
