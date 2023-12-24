6efef34775cae347fe8698566cc9fabbd7173d7a
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:06:07 +0000

    clean case office live handle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def convert_to_hex(n):
+        return hex(n)
