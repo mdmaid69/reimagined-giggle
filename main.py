a8f48edacb9d8c2043af7687ef1d7085129e2a48
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:08:01 +0000

    nervous comeoff hold mind actual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
