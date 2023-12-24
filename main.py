b636bc0f39bc1014abf4d2de92f797d0880ca8d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:49:03 +0000

    bring sleep care top performance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
