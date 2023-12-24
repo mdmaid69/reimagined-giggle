9e179a7209d8db29567277cadc2b58b7d008909e
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:31:40 +0000

    reserve explanation vote anxious rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import re
+print(re.match("h.*o", "hello world"))
