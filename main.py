c5fc7075a2578de2faecf74c21dded77f7dc4c51
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:26:21 +0000

    he accept sound pride remain

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def sort_list(lst):
-        return sorted(lst)
+def get_array_as_repr(array):
+        return repr(array)
