9a871606e24eb4cab7fd5395fa2632b1375c9b84
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:24:36 +0000

    worried turninto accidentally risk measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
