65672f93b0cec566792f1d233008d31de50555e2
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:06:49 +0000

    trust collect nearly unfair wave

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_min(numbers):
-        return min(numbers)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
