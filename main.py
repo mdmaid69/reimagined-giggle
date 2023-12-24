ec604a5b667248af135ac29e02664b401b10cdba
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:59:58 +0000

    embarrassed space comeoff personal visit

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_index(array, item):
+        return array.index(item)
