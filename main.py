154ed55d414ee493adb653091521c3f441e1e72c
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:18:03 +0000

    temper key union weigh sure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
