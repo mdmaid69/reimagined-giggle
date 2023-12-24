0167e7bc8228148cbdda952a623bb69cb12fae1e
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:09:29 +0000

    567enemy problem grateful stiff pretty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
