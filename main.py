55ed6216c32c3d9b0f7040adfe44fb67c20e714d
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:48:41 +0000

    choose shut needle hope trick

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def convert_to_octal(n):
+        return oct(n)
