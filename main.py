27e5a1a176759cfca1ee6bf015fc317977e24c4a
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:15:16 +0000

    after ring hear apart correct

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
