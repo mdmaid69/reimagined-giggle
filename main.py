1bcefbd4b1ad8e8f1046e570913e8fb1787ac9b7
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:39:46 +0000

    lunch reasonable communicate buy one

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def sort_list(lst):
+        return sorted(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
