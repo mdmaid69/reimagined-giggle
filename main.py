7a35bbda37e47fda33174e9851d8513d6a87f6ac
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:56:33 +0000

    black result influence pollution score

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def split_path(path):
+        return os.path.split(path)
