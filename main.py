9431900041b1d004443ce8a8733c6e4b85169f0d
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:47:42 +0000

    poison flavour fix point precise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
