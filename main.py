936b632e95de86590fb658867fca57bf9b1c32c2
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:53:59 +0000

    brown fashion improvement describe write

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def remove_from_array(array, item):
+        array.remove(item)
