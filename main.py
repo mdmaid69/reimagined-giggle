6e059fb4f8d9ca40966df631da9ddc1c4f16176a
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:27:51 +0000

    line burst handle public lonely

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
