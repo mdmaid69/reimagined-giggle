5d56dfd7b2d47fe9a8350df24195da7dbd4aa70a
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:38:22 +0000

    path serve front middle careless

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
