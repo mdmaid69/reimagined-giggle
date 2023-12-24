8c682bc69330bbfabd89264c6d23e4531533c273
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:24:39 +0000

    bear holiday entry understand recently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+x = 10
+y = 20
+print("Sum:", x + y)
+import math
+def calculate_sine(x):
+        return math.sin(x)
