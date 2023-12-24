061474199cea254a36e469c8491832537f76e53f
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:14:01 +0000

    combine pass wonder rubbish wonder

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
