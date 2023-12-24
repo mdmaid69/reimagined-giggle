a41ec860787e8e157dee17f97750cc429dbf2573
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:19:18 +0000

    holdon somebody test spoil use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
