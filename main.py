4e51d2f0bd790f6552d1a459c621c2bdbad25e32
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:13:09 +0000

    owe attract certain charge where

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_speed(distance, time):
+        return distance / time
