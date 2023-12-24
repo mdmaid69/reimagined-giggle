028ee15531bb592667cb32c6a14bc92ef03175ff
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:18:15 +0000

    stick recover harm disappear watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def change_working_directory(path):
+        os.chdir(path)
