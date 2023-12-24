0b9ad8487c6da47053035fe5fe9016b8822d21b5
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:49:19 +0000

    safety no plan grow birthday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
