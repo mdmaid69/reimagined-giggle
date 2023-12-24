47db41a4d6bf6b5164de59cab20ffef444d7c75f
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:15:41 +0000

    fortunate understand unusual possibility really

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  def add_numbers(x, y):
-        return x + y
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
