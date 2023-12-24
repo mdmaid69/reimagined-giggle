e3585797f38dabdded904866b41a4d3dcab541d4
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:33:34 +0000

    trousers people fear march common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_typecode(array):
+        return array.typecode
