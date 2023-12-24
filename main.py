184e63502a1527e50c1c5df499f0698b72f0f4d5
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:05:43 +0000

    mark noisy can callout onto

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
