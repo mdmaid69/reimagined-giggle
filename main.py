21b6f47f1022e15c4ef6207b5c29b009b9ef5fbd
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:15:10 +0000

    right difficult word fairly discovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-print("Hello, world!")
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import array
+def get_array_as_list(array):
+        return list(array)
