ddd1efb0c24151b45879a187dc2c2ba573ea94a6
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:00:57 +0000

    rush Nn press surprise skin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
