6d29dedebd6a0191356c01f4680ff4143d70bb37
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:54:26 +0000

    necessary healthy read quiet practical

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
