0ddd06ddc573179fd234ee217ed5a149fdb0a1be
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:02:00 +0000

    private wear behave backwards classroom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
