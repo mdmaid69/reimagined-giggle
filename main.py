89a906f9269560f0e19a7ce65ebbb341fc4585a4
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:32:14 +0000

    accidental size danger children apologizeapologise

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
