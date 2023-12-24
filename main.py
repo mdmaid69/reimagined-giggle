7aae23a486a9493345b16e5004b90509455af75f
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:45:34 +0000

    expression train bush label spring

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import time
-def get_current_time():
-        return time.time()
+def calculate_power(work, time):
+        return work / time
+def calculate_perpetuity(payment, rate):
+        return payment / rate
