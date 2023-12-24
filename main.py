3ccc0efa04a92b80cc70efbe6b78cbad3199f8dd
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:49:16 +0000

    cheat wish alone money reliable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_exponential(x):
+        return math.exp(x)
