0cf95025ba51a440252e618b10adf148993ee2dd
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:32:20 +0000

    form comedown doubt ear alive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
