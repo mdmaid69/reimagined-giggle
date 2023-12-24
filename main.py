4f1d5923992ef94c2560850a3ecb62a2f856130c
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:56:55 +0000

    statement reliable bother exercise ill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import datetime
+def get_current_date():
+        return datetime.date.today()
