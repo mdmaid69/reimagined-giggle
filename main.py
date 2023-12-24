336dc63732ba3b63b4e8fbd6f61e5eeb57653fb4
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:13:48 +0000

    spring brown bringup promise discovery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_itemsize(array):
+        return array.itemsize
