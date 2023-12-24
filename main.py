050e25c66d9c8f56bf29dcd6d27e96b81c30c8a5
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:34:55 +0000

    same deaf split event except

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
