0e21d1e454f56e60366e6ecd299974d169c2c5cc
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:54:54 +0000

    almost band frame Ll fight

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import os
+def change_working_directory(path):
+        os.chdir(path)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
