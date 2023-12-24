42d4939e52abee349d49ce95ff9a7ee062f835fa
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:26:04 +0000

    pencil there brush everything cum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_as_bytes(array):
+        return bytes(array)
+from collections import Counter
+print(Counter("hello world"))
