eadb5a16670c8c0886c794f2f3458aa829bca7cb
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:44:21 +0000

    band vote top breathe quality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
