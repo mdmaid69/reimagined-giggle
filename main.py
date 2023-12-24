5b20f6ea0683e765e187a5a0618f9bd3b292a4e4
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:25:51 +0000

    benefit that suffer letter language

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_floor(x):
+        return math.floor(x)
