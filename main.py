8fe63f7bbca0c4504d3cc2ab59bd3749af72b287
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:15:49 +0000

    letter attitude go but thanks

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print("Hello, world!")
-def count_elements(lst):
-        return len(lst)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
