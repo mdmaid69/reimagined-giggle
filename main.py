76a51cc47386277844ef7e94043baa388353761b
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:01:14 +0000

    turnup early onto personal bus

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_exponential(x):
+        return math.exp(x)
