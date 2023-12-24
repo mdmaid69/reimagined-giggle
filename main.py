fd2b97b8637e141f62c85990668580098dd04ccb
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:48:00 +0000

    reduce yet gate bridge sweet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+print(sys.version)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
