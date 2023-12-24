4d4c27a2921659e7951f40e498e16707f682ea9c
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:16:10 +0000

    backwards space mouth almost awake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import platform
+def get_os_info():
+        return platform.uname()
