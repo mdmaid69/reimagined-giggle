e4226d334340530f465d7972a3f74f8843926d91
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:55:59 +0000

    fence touch either when safety

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def calculate_density(mass, volume):
+        return mass / volume
