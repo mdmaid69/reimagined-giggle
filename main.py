d26d08db29ea3acca8a5788d8ecbfac8a8e4ae3a
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:28:57 +0000

    compare Ee test area takeaway

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
