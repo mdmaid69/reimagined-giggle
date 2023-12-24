4a450e2185583bccc8a2af0a0d2e0741b145dd8e
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:08:21 +0000

    hit trick lesson standard trouble

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
