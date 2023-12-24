d2845b5fdbacba7637073f7e8fac5448b65f7ba8
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:28:33 +0000

    delicate person possession great limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
