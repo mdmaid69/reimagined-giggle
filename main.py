9b2efd5bd723bc74738361e58df70c38e58f7d02
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:57:57 +0000

    meal content behave ignore sick

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
