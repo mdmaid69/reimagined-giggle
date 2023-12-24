0e6ac7de9cd1b2d71e39d102c25639b0a28853da
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:44:17 +0000

    attempt offence mention wind over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_work(force, distance):
-        return force * distance
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_speed(distance, time):
+        return distance / time
