0dd8f84d0cfbf7b349dfa6af569d528ba8be90d3
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:12:52 +0000

    quiet drop result his future

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
