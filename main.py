bb16abe28f8b664bcf9bb3ce38f145a63be51801
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:18:49 +0000

    fire excited disagree foreign understand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
