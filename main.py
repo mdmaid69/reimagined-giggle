f192f0d91a1c4707334b50f07a0cd94d0f1f9ae7
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:55:47 +0000

    hand combination match getoff sweet

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import array
-def get_array_from_string(string, typecode):
+def convert_string_to_array(string, typecode):
         a = array.array(typecode)
         a.fromstring(string)
         return a
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
