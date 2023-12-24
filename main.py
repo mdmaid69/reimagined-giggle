ce7858b78bb1e49b2ced6441f37e4845ffc70e69
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:11:21 +0000

    pocket scene purpose around trust

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
