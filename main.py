b89f428a1d1e9be4d4c20fe9bbe05ba2a0761df5
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:41:10 +0000

    level disturbing goback(to) nonsense opponent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def clear_array(array):
+        array *= 0
