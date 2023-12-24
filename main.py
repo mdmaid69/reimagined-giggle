29c3bd42649fe31c29744b2d5e2d61b90ad2ec1a
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:59:37 +0000

    grand clear stranger promise absolutely

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_index(array, item):
+        return array.index(item)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
