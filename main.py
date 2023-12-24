eb509bab7b64a0be32a16ccc2abb23987a38e9ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:33:21 +0000

    shut agreement much born 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
