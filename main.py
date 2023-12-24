c70c2524e155f80a228a2e2813668d915ac99d0d
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:51:47 +0000

    fierce thumb talkoutof land ever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
