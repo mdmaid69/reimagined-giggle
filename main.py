68481d26f49d2cfea2e90d05d8048d9d55c270ea
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:20:35 +0000

    shelter distance sound embarrassed entry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def get_array_item(array, i):
+        return array[i]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
