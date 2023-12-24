227981302fc7d134f1553cdeb798eceef5b4c36e
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:31:55 +0000

    translate by recover check violence

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
