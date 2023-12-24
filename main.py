c10d00da255700a6a3dd0564c10cc68d2be7bace
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:12:59 +0000

    shame remember lunch onto trade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
