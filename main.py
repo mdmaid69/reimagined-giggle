59da5b5f7420f2b196821d4b6380b04f30a7bf92
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:53:57 +0000

    drop punishment down board constantly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+from collections import Counter
+print(Counter("hello world"))
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
