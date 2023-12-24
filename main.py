7e1c23dd62f5d8de67e48cf13905262ebf903652
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:21:49 +0000

    lock pain wait so last

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def convert_to_octal(n):
+        return oct(n)
+print(sum(range(10)))
