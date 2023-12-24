12fb6739af12afbae6b58eda8260c5f186f94263
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:55:21 +0000

    immediately worried lay keepaway store

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
