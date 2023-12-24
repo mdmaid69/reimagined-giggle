5196c20dc9054aed871cd04bc08f0b721ddc572e
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:20:55 +0000

    stiff rare wind backwards apparently

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
