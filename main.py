1ef876cad4b254675bd57e4dfed968980f20762e
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:33:24 +0000

    true different exploration different should

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
