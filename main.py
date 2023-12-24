ba6cbd1cfe8db33c17c72d6fc992b413ec5777e5
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:01:58 +0000

    Oo square flood borrow club

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+def print_python_version():
+        print(sys.version)
+  def square_number(x):
+        return x**2
