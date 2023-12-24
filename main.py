aac2bb220ba5c5f6de1042e13507b1d490b7d132
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:14:06 +0000

    supply out getup right wrap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import tensorflow as tf
+print(tf.__version__)
+import os
+def remove_directory(path):
+        os.rmdir(path)
