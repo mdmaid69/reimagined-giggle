29294268841de029dc2bf0f13c38c5481ec05a29
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:37:48 +0000

    course correction favourite its stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  def is_even(n):
+        return n % 2 == 0
