32d7c8a5ec51de7435db5a3a56570889decd727f
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:46:23 +0000

    light goback(to) squash sad difficult

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def get_array_as_bool(array):
+        return bool(array)
+  def remove_duplicates(lst):
+        return list(set(lst))
