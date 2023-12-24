310d8d1aab4c6bf29b043d29c34d8a0986fa8fb6
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:32:02 +0000

    trip reason cross help produce

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import json
+def convert_to_json(data):
+        return json.dumps(data)
