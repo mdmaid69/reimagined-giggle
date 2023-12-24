4f85e749e952a5a9e51407b749587341041d49f9
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:28:41 +0000

    your passenger print sail indeed

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def clear_array(array):
+        array *= 0
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
