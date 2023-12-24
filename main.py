f8007b3f4049874ae53fb6d3b59cc36b1d93efa9
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:36:55 +0000

    unpleasant rather shake like soon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
