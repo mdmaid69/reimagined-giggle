ef2d8ef8e3346a8aec6af67991fc96758660b251
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:07:44 +0000

    class rent light art depend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
