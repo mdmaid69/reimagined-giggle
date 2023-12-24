6f1b3f2db5ebf1fcac8047b33190a6876b82cda2
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:58:37 +0000

    tap equal value floor section

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def reverse_array(array):
+        array.reverse()
