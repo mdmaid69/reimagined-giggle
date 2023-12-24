384f948c5c219df140846b5f35a805f5bf273df0
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:52:51 +0000

    special process brave run bear

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
