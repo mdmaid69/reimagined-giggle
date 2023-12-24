c1908adf4c2a2aa5c37a6e830cdcd5db7c627387
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:49:09 +0000

    meal afford used company bag

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def split_path(path):
+        return os.path.split(path)
