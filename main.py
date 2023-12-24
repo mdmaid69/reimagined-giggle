413b2ca60f1c0bac2c67dc01290cd067d1464b78
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:41:09 +0000

    beautiful Kk because narrow history

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-from collections import Counter
-print(Counter("hello world"))
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
