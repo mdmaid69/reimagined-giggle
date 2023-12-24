052fc0ea97d3e45f0d65bc37350013754392e843
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:34:08 +0000

    book backwards temper even estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
