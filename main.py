fe64c6963f1bbe172c691cc8c27857fac5673c90
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:04:10 +0000

    recovery born murder everywhere positive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def count_characters(sentence):
+        return len(sentence)
