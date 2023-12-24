36bd124030d93dc12a235d4f4878e8b8ea9a705c
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:50:42 +0000

    score type him public extraordinary

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def find_unique_words(sentence):
+        return set(sentence.split())
+def find_min(lst):
+        return min(lst)
