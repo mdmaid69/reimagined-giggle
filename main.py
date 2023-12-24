abdc132e32f9c2370bdbc929db69ac6b9cd284bd
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:38:48 +0000

    leave bread cheque stir closed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
