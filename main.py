46037e2c29053c59251580e2f4e6bb53416ed12b
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:45:56 +0000

    custom arrive container apply stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+print(json.dumps({"name": "John", "age": 30}))
