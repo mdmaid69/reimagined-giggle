f5e316dc979dcd8885a641efd8fe9decfc63e1b3
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:22:30 +0000

    smell block pleasant wait foreign

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
