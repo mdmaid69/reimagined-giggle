4c6a323ed2957a95f9e11e0e21a94d97a049bb34
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:31:32 +0000

    comefrom hear bottom uponon invite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
