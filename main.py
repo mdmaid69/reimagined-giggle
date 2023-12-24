fcd20117c1f411697ab3fc962f61b46f384b471b
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:04:31 +0000

    heavy block present word achievement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
