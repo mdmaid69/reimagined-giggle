fb0dec6f46bd849b9257bc2c107bd39b43758abd
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:02:25 +0000

    minimum clear disturbing drink basic

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
