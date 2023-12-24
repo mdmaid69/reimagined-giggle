2823d4ad0cf5e1c8d79306c31dfe4b1a05c29966
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:31:41 +0000

    between university classroom circle Ii

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
