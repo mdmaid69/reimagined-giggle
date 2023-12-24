3d8e351a834c08a69278a627bdb1a63500532cfb
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:07:18 +0000

    worst worry strong lower callat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def get_list_from_array(array):
+        return array.tolist()
