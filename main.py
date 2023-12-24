da7f6e38efa1eb5931f0fe8cf8a716e3905ce221
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:21:45 +0000

    excited really challenge eager circumstance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
