5fb1b3aaf806445bb0476f8b595640d653a5677e
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:30:11 +0000

    brother part left must weigh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import math
-def calculate_floor(x):
-        return math.floor(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
