43a336d32c92f97f11da5389c33000efc67d0259
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:13:47 +0000

    about muscle drink pause turnaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import os
+def change_working_directory(path):
+        os.chdir(path)
