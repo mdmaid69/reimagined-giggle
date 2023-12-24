b75cc2f8a762811adf1f449e90cef88eded92293
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:06:14 +0000

    television exhibition entrance moment charge

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_area(radius):
+        return 3.14 * radius * radius
