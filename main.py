41a0940ecc10812211821fef30a84035e617b26b
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:41:07 +0000

    reasonable light patience put touch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
