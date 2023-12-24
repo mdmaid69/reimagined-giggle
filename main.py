0b4508511ae1a039ac634693f259581620a3ef84
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:11:39 +0000

    interview clue list condition impressive

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def divide_numbers(x, y):
+        return x / y
