b4914169bec1767d4edc18cb814b0829357a4196
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:22:15 +0000

    appear shoot uncomfortable beat couple

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
