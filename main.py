f5ee4331d80b4f8496f7ab975abeb4aaddd2ea03
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:07:01 +0000

    fetch deliver boring future town

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
