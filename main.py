0197c34f563c5292e0e41bd6e741cd55cfd6d5e2
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:57:01 +0000

    steam describe border shock phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import sys
-def exit_program():
-        sys.exit()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import json
+print(json.dumps({"name": "John", "age": 30}))
