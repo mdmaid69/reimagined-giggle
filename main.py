03d3a2bcf13132c5ddaa0774a8f671da59bfb80e
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:49:18 +0000

    ourselves warning 569enjoy like general

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
