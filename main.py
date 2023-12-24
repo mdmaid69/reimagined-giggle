118343b61523e0eb9ae2541b97d3d5e9d0d4dc29
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:48:25 +0000

    sound compare break inform absolutely

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+print(sum(range(10)))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
