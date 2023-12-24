260e788624306a8b586f60b314e52ae60a130d0e
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:56:22 +0000

    to makeup takeaway seem bury

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
