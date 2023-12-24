6bcc36162eeedd382e4864a6672fbb11ef5806d6
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:08:21 +0000

    produce remember bet steam switch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
