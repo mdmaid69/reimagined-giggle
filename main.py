3e03af6e06b73c8fe6d843cae52b27f7a1b2e011
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:30:16 +0000

    gas thin breakin thinkof beat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
