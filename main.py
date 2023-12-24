ab3f8cd42a84eecbb8272572efb1a4879b1410e1
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:44:14 +0000

    flow benefit much official action

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+def remove_directory(path):
+        os.rmdir(path)
