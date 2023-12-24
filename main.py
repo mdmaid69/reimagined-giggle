3e7a8dcc119cdccdad390ca2ff788c970f7e32a4
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:03:07 +0000

    get square 565end child final

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
