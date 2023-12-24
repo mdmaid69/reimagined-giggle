1d423ea0102e419f376a8c6bbcc2f5d01a9684b4
mdmaid69 <robot@alexsyw.me>
2023-12-24 10:53:54 +0000

    dig drop contrast away exit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  def sort_list(lst):
-        return sorted(lst)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import time
+print(time.time())
