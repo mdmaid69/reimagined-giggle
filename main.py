1bf75e13254e59b5ab0a0505f0c4599bcb2b3e01
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:46:15 +0000

    rob list whole greeting willing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
