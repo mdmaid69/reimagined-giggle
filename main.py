0aca6d7e7986328cee9fc48a600bfe831ef40374
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:41:31 +0000

    keepout workon sound circumstance meaning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_min(numbers):
+        return min(numbers)
+import sys
+def exit_program():
+        sys.exit()
