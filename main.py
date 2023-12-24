84e8882cb972252c3c75657685ca334fdae0ad7c
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:36:58 +0000

    by on bet meanwhile careless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_length(array):
+        return len(array)
