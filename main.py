14b760adb48f14145b637817dccb2ab492513c23
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:29:19 +0000

    takeoff exam or Pp feel

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
