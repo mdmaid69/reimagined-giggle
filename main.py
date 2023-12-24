8b0a5c4eac25d0b7d604465466ff8ba5a8ed6fce
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:23:16 +0000

    use cry at word dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
