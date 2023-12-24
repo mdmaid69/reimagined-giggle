1681d0be995bd8927e68edf3f294b508a36c7855
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:17:33 +0000

    sky close wipe paper report

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import datetime
-print(datetime.datetime.now())
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
