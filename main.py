059feeec1c415d8f86463fef48ab2e84584e2182
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:28:10 +0000

    break little free pipe habit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_array_typecode(array):
-        return array.typecode
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
