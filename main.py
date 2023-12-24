19d678dc1b6f47abac67627c3c46b4dda0f7eda0
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:49:40 +0000

    need weak thin hope town

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
