b79365a1d48de95007a5140e20e445942c85b9ae
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:51:09 +0000

    somewhere during die tidy let

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
