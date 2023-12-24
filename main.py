b239bcc4b535b70981dd354dab4ff7b3b3d8e222
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:19:06 +0000

    sing pronounce exit suffer surprising

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import os
+def remove_directory(path):
+        os.rmdir(path)
