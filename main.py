d25090e682d9a84f2d90fe0d74e2f145b165403f
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:41:20 +0000

    else adjective passage autumn entrance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+print("Cube numbers:", [x**3 for x in range(n)])
