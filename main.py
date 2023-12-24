261851025ccff40b323bfeddbe7a35a0b79b5780
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:33:28 +0000

    bread public even innocent bin

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+print(sum(range(10)))
