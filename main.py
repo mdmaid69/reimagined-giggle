d652e1c430248b611d8aa098b2b706190f126c7d
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:22:11 +0000

    produce free total smoke invitation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
