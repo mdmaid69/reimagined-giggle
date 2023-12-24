878c70f8d428fdb0e3a50c59bc3cda06f7638320
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:18:33 +0000

    reply anxious impression easy aware

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def convert_to_octal(n):
-        return oct(n)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def square_number(x):
+        return x**2
