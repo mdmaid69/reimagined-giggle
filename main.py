9b6b60f7d7558d2f72dd1b73a5ebc499580141c7
mdmaid69 <robot@alexsyw.me>
2023-12-24 09:06:45 +0000

    middle key rise then wander

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def append_to_array(array, item):
+        array.append(item)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
