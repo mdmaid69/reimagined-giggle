3818a106d99bcbc96df1aedf16f0a53a9f379f75
mdmaid69 <robot@alexsyw.me>
2023-12-24 13:32:28 +0000

    matter dress point us search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
