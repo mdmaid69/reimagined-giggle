7c151596818b4a2eb7149823f5cc5f5d9de58113
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:04:56 +0000

    Itself comeout test breakinto strike

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
