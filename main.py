4031716d87404f4eb7cf1c29214e1c02016ba4ca
mdmaid69 <robot@alexsyw.me>
2023-12-24 01:53:31 +0000

    used wall coffee support allow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_min(numbers):
+        return min(numbers)
