aaef72700fdd1725ba22af1311ed8e52d9f6d37d
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:54:01 +0000

    put principle pool meeting sing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
