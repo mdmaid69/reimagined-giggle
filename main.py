73b7b69f6bab4bbca8afffc4734b2934ac19c43c
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:57:55 +0000

    remove colour nose imagination Itself

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def append_to_array(array, item):
+        array.append(item)
