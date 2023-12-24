706495feea83838754cf67ac4feeac1c9af52ab7
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:17:55 +0000

    nurse interview rely hand watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
