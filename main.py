67153b445ef3a2fe3d5afc663e765769b6e91d28
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:13:29 +0000

    spin temper build loss tell

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
