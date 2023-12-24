58359625a0ada971150eb2369369ecc8d4ee33f3
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:15:56 +0000

    cost scare fall separate her

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
