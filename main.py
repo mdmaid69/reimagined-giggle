0c952812c2fba01808792be6791aa6727ef77553
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:27:51 +0000

    secret bunch whenever skirt spread

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def find_min(lst):
-        return min(lst)
+def calculate_arc_sine(x):
+        return math.asin(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
