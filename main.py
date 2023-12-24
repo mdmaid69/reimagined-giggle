bb5a8260fc077d59c7a9d9a24c235faba818aea2
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:22:44 +0000

    disturbing other public fast confuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
