15a680af44a0a35f031ac7e64e4ec9aad45aecaf
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:13:51 +0000

    union tie damage Qq act

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  def calculate_area_rectangle(l, w):
+        return l * w
