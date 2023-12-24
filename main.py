5a5a0675db1264410923cfc8ca98b1e3065798d2
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:32:46 +0000

    circumstance strange build sweep form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import os
+def get_current_working_directory():
+        return os.getcwd()
