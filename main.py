e3b5f42bb07bd5b91c7d885696826365e850036e
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:04:46 +0000

    after giveup mark size worth

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
