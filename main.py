6dd162a821c916711ecc82657c9b6bfa9693a142
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:00:05 +0000

    right progress correction once breakdown

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
