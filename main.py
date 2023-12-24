c9b91c26bb094da7e7982dd72e3c1a2e6010f55f
mdmaid69 <robot@alexsyw.me>
2023-12-24 08:56:44 +0000

    silent read smoke freeze famous

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
