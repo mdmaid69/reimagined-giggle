cda90dd2a816ffc5e5cde6ad3e4ff686925676fd
mdmaid69 <robot@alexsyw.me>
2023-12-24 02:32:49 +0000

    string lend final comment sport

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+print(os.getcwd())
