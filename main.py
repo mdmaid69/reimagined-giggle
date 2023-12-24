a42efdef898be58c9cde864728b0a3e070d47e92
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:54:22 +0000

    across Hh flat best clearly

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_density(mass, volume):
-        return mass / volume
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def cube_number(x):
+        return x**3
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
