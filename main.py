6bc1db21570ab9f662780b4cd8f3b347d2369b54
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:11:37 +0000

    reasonable little stop true begin

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def reverse_array(array):
+        array.reverse()
+  def reverse_list(lst):
+        return lst[::-1]
