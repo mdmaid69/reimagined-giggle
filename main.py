5b36e3227fb8c57e4d54ad532d19b5854a45535d
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:51:52 +0000

    physical demonstrate admit nigga forever

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def count_words(sentence):
+        return len(sentence.split())
+text = "Hello, world!"
+print("Characters:", len(text))
