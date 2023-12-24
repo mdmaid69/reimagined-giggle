ba134dcb3c7f4ded3ef081970bae4465130e36d9
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:30:55 +0000

    cough concentration head comfortable care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
