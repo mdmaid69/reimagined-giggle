5c668f2e677a598401c5ffff99bf960ed0ce2a80
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:46:02 +0000

    morning crash film suggestion ready

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def cube_number(x):
-        return x**3
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
