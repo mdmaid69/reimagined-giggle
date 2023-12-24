9b03e9ef1f3c824ddbe7387d77eb38febcc2b40a
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:18:54 +0000

    classroom patience tongue sense loud

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
