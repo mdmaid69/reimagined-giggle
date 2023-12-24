25c96f6edf9857653f06ff7db1a2dabb19c0bdd0
mdmaid69 <robot@alexsyw.me>
2023-12-24 03:35:25 +0000

    564encourage along male full only

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
