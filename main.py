4d2092c8afcf0975a3d0236fe7ffc6c45d0b9028
mdmaid69 <robot@alexsyw.me>
2023-12-24 22:39:13 +0000

    sell neck need speak scared

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def create_stack():
+        return collections.deque()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
