9b08ffc988608f98d1213b6df698fa3572768bc2
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:44:57 +0000

    chat history backwards turn police

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import re
+print(re.match("h.*o", "hello world"))
+def find_max(lst):
+        return max(lst)
