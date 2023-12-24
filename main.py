5f104c4e1f0adbe6c7abfd2a66b8509424b20bcf
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:20:26 +0000

    lonely cause out takeover second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
