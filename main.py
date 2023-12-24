74b9f4e509db3b2aa5004ea2894c6a5384f99cf4
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:34:43 +0000

    advice computer television discuss takeout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
