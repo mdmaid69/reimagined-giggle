8e808e0d3e0a4bdd86f2a73c8adbfc2c2c4c0862
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:53:19 +0000

    eye close score extremely either

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
