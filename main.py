a581f9b499652572d86cb1bb7f8740c234a254bb
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:59:07 +0000

    dare maintain repeat part detail

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_power(work, time):
+        return work / time
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
