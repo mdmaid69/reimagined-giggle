960061fca339056a366d668d9bd2ec7923220c12
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:00:08 +0000

    your ticket means must risk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
