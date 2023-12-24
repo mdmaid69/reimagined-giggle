a3b84e8ea7c86be557f8e73085a311fab7ca9c58
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:42:02 +0000

    winter account discuss car advantage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
