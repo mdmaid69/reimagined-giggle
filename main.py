d91b243b458319d29ae41fb5637b8deebf7e68bb
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:14:34 +0000

    successful for part appeal holdout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
