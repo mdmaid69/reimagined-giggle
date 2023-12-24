6eedef2064935283a7f35bea23ff11f6ac5d0803
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:54:01 +0000

    agree shout basically far press

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-print([x**2 for x in range(10)])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
