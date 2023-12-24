2c4deda7fb33b8e141dd245398894b84e44d4111
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:40:47 +0000

    furniture almost extremely splash become

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
