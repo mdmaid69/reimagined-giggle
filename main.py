672303ba4628205cf950df1c8a0b7bf5aab74844
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:45:45 +0000

    reply grand study middle off

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-print([x**2 for x in range(10)])
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
