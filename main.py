08fbf734e3018d4abdebefcf56d64ec2d0b75ed6
mdmaid69 <robot@alexsyw.me>
2023-12-24 21:48:02 +0000

    failure and come whistle explanation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import math
-print(math.pi)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
