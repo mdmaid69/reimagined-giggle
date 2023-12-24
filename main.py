6495c89a67e07c747314f45eeabbce2bfa1e488a
mdmaid69 <robot@alexsyw.me>
2023-12-24 20:41:12 +0000

    cry before condition hair whoever

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_cosine(x):
+        return math.cos(x)
