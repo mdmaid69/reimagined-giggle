ea064e7a21f1ddae7b3e40ea2b18025479c9c2a3
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:47:40 +0000

    professional artificial error brave answer

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
