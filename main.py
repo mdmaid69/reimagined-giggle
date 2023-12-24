8e64ac2c1726a3adb2606d0130ca30daa42502a9
mdmaid69 <robot@alexsyw.me>
2023-12-24 23:01:21 +0000

    language target translate holdout explain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def is_odd(n):
+        return n % 2 != 0
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
