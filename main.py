ef800d78bac11cd766a5fed99a2fa6b10125c51a
mdmaid69 <robot@alexsyw.me>
2023-12-24 04:05:56 +0000

    yet impressive responsibility measure bridge

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
