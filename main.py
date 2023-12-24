6a3ba8d34f108348fc9c8ca08354b1d25049b9aa
mdmaid69 <robot@alexsyw.me>
2023-12-24 15:44:33 +0000

    crash quick beside charge everything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def print_python_version():
-        return sys.version
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
