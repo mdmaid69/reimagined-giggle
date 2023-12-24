f2c8bb6835dccf8359cd3ec2050ca8dcba4cb384
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:24:23 +0000

    clean objection surface legal schedule

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def sort_list(lst):
+        return sorted(lst)
