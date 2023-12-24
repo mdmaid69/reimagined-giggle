560421f6253cb4d16fa188b6550e22bcf4387b69
mdmaid69 <robot@alexsyw.me>
2023-12-24 18:32:13 +0000

    thin rob roof snow window

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
