77628121f6a70e549a4857ccbbeca225c1e3a774
mdmaid69 <robot@alexsyw.me>
2023-12-24 16:28:31 +0000

    pot giveback delicate fun pattern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
