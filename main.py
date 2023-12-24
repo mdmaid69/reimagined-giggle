3d865df911a7614bb0a97291e6b8b68f07afc05d
mdmaid69 <robot@alexsyw.me>
2023-12-24 06:22:06 +0000

    space near light burst yet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
