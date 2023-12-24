61bd9f1b6d14a7f6c158beb34d46902d2f7c2d40
mdmaid69 <robot@alexsyw.me>
2023-12-24 00:09:38 +0000

    burst live inform afterwards custom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
