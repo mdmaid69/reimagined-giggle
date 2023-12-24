2893d7575eea1b221b69dc156f4324f8be19d85e
mdmaid69 <robot@alexsyw.me>
2023-12-24 12:22:53 +0000

    from loss flat runover silent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
