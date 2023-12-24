51d67ecb72c36ea48f1ba6dec8f40043ade426bd
mdmaid69 <robot@alexsyw.me>
2023-12-24 14:18:59 +0000

    win tear my ours unit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_pressure(force, area):
-        return force / area
+def find_min(lst):
+        return min(lst)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
