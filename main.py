ecacea975481b82dcc5449248920a07ba1bcd43d
mdmaid69 <robot@alexsyw.me>
2023-12-24 19:06:43 +0000

    mess dinner simple apart squeeze

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
