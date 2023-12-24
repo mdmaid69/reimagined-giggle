b33ef88c8ea906009a72706e142d146c610dd889
mdmaid69 <robot@alexsyw.me>
2023-12-24 05:23:22 +0000

    only turnon afterwards boat final

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_current_directory():
+        return os.getcwd()
