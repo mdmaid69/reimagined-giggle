50f3f9a32715707bef57c13466f5e151a8f14174
mdmaid69 <robot@alexsyw.me>
2023-12-24 17:42:51 +0000

    book extremely risk 567enemy let

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def cube_number(x):
-        return x**3
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
