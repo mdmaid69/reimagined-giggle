9b9f2503f17eb3a37060003f18465c22039047c9
mdmaid69 <robot@alexsyw.me>
2023-12-24 07:26:27 +0000

    person wrap pretty public manner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def clear_array(array):
+        array *= 0
