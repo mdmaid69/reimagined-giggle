f1a6337279c17ee3e7f4c70cff8dea4fdde51471
mdmaid69 <robot@alexsyw.me>
2023-12-24 11:22:09 +0000

    needle public among Pp direct

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def square_number(x):
-        return x**2
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
