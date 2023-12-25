ea71c66897024d6d58d6c56c4565381538523373
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:21:44 +0000

    choose sign break challenge anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
