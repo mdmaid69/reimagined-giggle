462e9f279e5169fcd003d84f80e740ec3539a556
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:44:40 +0000

    street whenever mail match tough

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
