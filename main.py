c99a678d8d23d0189d1c61e1a180d754868fcf1e
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:21:40 +0000

    stiff accidental likely store miss

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import datetime
+print(datetime.datetime.now())
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
