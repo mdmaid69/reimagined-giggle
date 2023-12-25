95fea7f8fd39b4862148684b3e808757952f2908
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:11:27 +0000

    concerned head listen upset movement

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+print(sum(range(10)))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
