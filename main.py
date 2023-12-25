dd52a4b591a51243726fe5bfbf1dd0585566495f
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:02:45 +0000

    Pp interruption firm takeover jump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_list(array):
-        return list(array)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
