1cf598bcce5188816fbf770b32ac3220b6eeee6a
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:45:26 +0000

    copy usually theirs comfortable shall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import time
-print(time.time())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
