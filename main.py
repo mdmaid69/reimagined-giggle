40dc98d8c96abf6ad3856eee4b6d41ec80a14216
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:39:38 +0000

    exam poor pupil exam open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
