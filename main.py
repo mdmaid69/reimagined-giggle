a24a18ae1a78c8e2a12680d08aaef6fa1bcdaf91
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:41:48 +0000

    in strange factory event passenger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
