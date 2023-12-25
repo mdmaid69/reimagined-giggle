63c4bd0ca5ec87210980b9609d0977fce0b53971
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:50:13 +0000

    congratulate somebody float suffer past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_palindrome(s):
+        return s == s[::-1]
