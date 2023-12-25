aaed4f0fb6949864eaacbf416d078c351b9be3ad
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:54:46 +0000

    demand duty main badly children

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
