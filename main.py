ba20c96b2c10ab4b557a1a76e916531cfc8d49f9
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:20:20 +0000

    important care surprised do age

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
