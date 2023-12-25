56bad695027db5da3a285c7fbe551886187fe3e5
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:17:41 +0000

    explanation now level long keep

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def find_union(list1, list2):
+        return set(list1) | set(list2)
