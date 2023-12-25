a5ec2771c36111a3afdbe10f447144d60f1ff705
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:40:11 +0000

    fair exaggerate any wheel transport

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
