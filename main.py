aed8115a3fdd0bb9963982046b970e75fe301f9d
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:01:54 +0000

    bath meet slip Gg breakdown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
