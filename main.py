219ec1ca70aca76deee9e8d65a44c4377c5b5850
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:54:49 +0000

    pride plenty next embarrassed body

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
