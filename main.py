07f748d742ae1662b4e7369857a0987ec34cef17
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:32:08 +0000

    mind have by possession pool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
