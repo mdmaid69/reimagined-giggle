85f95ad4fa871ff2610f20cdd09617e12c36fc11
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:58:13 +0000

    space waste regular short getoff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import time
-print(time.time())
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
