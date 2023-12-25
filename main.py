b4af0b3b5044c45d5d22b5564204f9cbf72e93fb
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:48:01 +0000

    stage mail wave keen with

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import re
-print(re.match("h.*o", "hello world"))
+def find_max(lst):
+        return max(lst)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
