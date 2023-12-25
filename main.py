57cb1f819cf7d5be4f20cd648769aeafe189e0c2
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:49:26 +0000

    depend circumstance strong choice under

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_as_repr(array):
+        return repr(array)
