3ab69ae5d6b4bb6372b385cda3aed0ce01c255cc
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:57:14 +0000

    pretty Nn sell plenty contribute

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
