2d409e17c11f1218494f872e89ed52002797888e
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:33:37 +0000

    worry choose runaway radio swing

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
