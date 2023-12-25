afe48cea510955c57c41709ceb4950983e378567
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:29:16 +0000

    turnup any niggaacademy work reasonable

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import sys
+def exit_program():
+        sys.exit()
