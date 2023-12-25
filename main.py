affdb361665114715854a96c5c390ae3c3c885d4
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:19:44 +0000

    obvious from process callup walk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_perpetuity(payment, rate):
+        return payment / rate
