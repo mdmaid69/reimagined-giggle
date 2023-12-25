5ff53484e70360fe7cb97fc203d9b28c27cc2311
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:52:57 +0000

    figure side practise wipe improvement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def reverse_list(lst):
-        return lst[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
