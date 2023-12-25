d5e7fbf9c2d583ae2cfea4eb4f4ed229b034086b
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:39:35 +0000

    pity recommend react nothing first

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def count_characters(sentence):
-        return len(sentence)
+import os
+print(os.getcwd())
+def calculate_pressure(force, area):
+        return force / area
