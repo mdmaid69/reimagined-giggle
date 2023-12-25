32870390ca9d5ef92238d678d108c3568c2395ba
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:26:52 +0000

    whom show mark sometimes law

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_min(numbers):
+        return min(numbers)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
