5220eb7f7b1c23ab2ea88c9360339a54dc03d172
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:29:26 +0000

    list fork object limit tap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
