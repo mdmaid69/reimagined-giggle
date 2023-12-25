63f5e308db0d9daffe9822f3cf178f5ba2e2a625
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:42:19 +0000

    succeed get once little spot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
