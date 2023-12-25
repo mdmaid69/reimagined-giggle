6cc48490f401c6a3d91ecf45e380d09a51fc0d5a
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:20:15 +0000

    I seem right suit Yy

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
