7adb265953d1d651f61947a45dfeb5f7f98486c0
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:59:23 +0000

    Bb keepaway I machine you

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
