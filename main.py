04db57f6c07276f010210bb899fe14a5e2e177a5
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:48:07 +0000

    successful bang ill none shoot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
