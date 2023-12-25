724b90be94471c5fbd7ef37cf53c7ba4c234c90e
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:52:24 +0000

    sad attitude artificial wide prize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
