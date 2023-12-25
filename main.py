d6f7f95823ad6a8df45c9774666f24c0bb135bca
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:27:31 +0000

    567enemy patient lock takeout increase

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
