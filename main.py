553c3c3bbf4cc3f636983eee68d2136e81f2fc76
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:51:59 +0000

    will matter schedule backwards year

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import getpass
-def get_username():
-        return getpass.getuser()
+def find_max(lst):
+        return max(lst)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
