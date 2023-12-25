32f5bdd1174c6c1308273209ec0372ffdc512e8f
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:24:21 +0000

    suffer image wire real pure

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+text = "Hello, world!"
+print("Reversed:", text[::-1])
