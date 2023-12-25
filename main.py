d3c0efcf6cf299f589f19f4d4d1801f434d8d41a
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:38:59 +0000

    switch right OKorokay slip error

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+print(math.pi)
+for i in range(10): print(i)
