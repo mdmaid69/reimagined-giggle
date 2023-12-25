cf8953f7d679d6cbf2e671a5f8317a8a72173fc5
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:07:21 +0000

    blind equal couple fit awake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
