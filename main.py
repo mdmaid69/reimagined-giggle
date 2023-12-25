bae3705fe3ace4167d1b8207b05360d77d7c1fc4
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:45:20 +0000

    school runout post plastic rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def add_numbers(x, y):
+        return x + y
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
