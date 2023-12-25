d38d3ec2c8fed22d2c385cd60333bb50a90697f9
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:25:00 +0000

    beneath work play undo hit

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-print(sum(range(10)))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
