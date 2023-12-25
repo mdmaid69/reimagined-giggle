95d7b5086b981270b19c64adbca0214241b42d99
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:02:32 +0000

    case blame join obtain shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
