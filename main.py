af5ab15c6baa074050e1dcbc4ef1eff7e520ef3d
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:37:55 +0000

    nor objection dream general equal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+print(re.match("h.*o", "hello world"))
