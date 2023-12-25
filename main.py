84de99fae283d8f489854cb74d2c5f03dabc7f8c
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:51:28 +0000

    turndown face importance uniform phone

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+for i in range(5):
+        print(i)
