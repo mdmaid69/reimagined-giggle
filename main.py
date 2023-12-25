9df4a09b65d4718e643cb74b05a1b10b93531cf2
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:40:50 +0000

    minimum dead steal scream direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-import array
-def get_array_typecode(array):
-        return array.typecode
