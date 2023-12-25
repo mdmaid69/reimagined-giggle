d65c7c830dc89d8e889f9e3ad58109427485cf47
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:11:57 +0000

    crash clothes entrance measure true

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
