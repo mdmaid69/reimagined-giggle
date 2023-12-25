265ae161ad3e3546c75144f8b38c952f664b6a38
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:40:08 +0000

    closely whoever complaint chin door

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
