db4d5c68671c6a8734b904372af2d21233fc72fb
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:01:50 +0000

    funny suit today discovery health

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import random
+print(random.randint(0, 100))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
