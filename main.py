a6e1e8e73e3838077e38d73006af41534bf2460a
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:57:21 +0000

    hold serious just cut union

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def find_min(numbers):
+        return min(numbers)
