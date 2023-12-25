8f40917ea03a5143d521e6dfd5c33c5f59761919
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:06:09 +0000

    your extremely there close name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
