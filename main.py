e26700264ffefa24af0082a38aa649557f998425
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:28:35 +0000

    soft should wild pretty pleased

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
