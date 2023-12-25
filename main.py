4f568dcabbbc1ade890a9aefd367c3627fc05711
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:17:44 +0000

    play good giveout Rr neck

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import os
+def remove_directory(path):
+        os.rmdir(path)
