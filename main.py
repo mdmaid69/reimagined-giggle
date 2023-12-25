804b5884163fdc5280c92e2037b2a920d667934c
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:13:05 +0000

    pool hang greeting position then

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_area_rectangle(l, w):
+        return l * w
