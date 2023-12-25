779415ddea96e8330ef40df1cea6dc4fc8064bed
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:18:52 +0000

    type store easy production ours

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_area_circle(r):
+        return 3.14 * r**2
