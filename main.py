23560f3cea18a471d996b963f62666810995d6a2
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:24:45 +0000

    grateful emphasis port rise them

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
