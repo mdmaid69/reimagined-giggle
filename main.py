7d6b0e2638276f68121f9f9aa0d81cdb6901d1c8
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:46:44 +0000

    amount increase immediate shake do

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
