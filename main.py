1a0ffe306f871d411de0e1181b34ff2b9c4c11b7
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:24:28 +0000

    difficulty surprise normal rubbish wealth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
