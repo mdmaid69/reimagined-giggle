fe5533542598d1a6f3f9c29a726fdbe7e6f2dbe4
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:35:32 +0000

    sweet length safety excuse wave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
