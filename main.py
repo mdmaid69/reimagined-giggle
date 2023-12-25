8828c795113e1c46a96012e91b92a2a2fd1a71bf
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:51:38 +0000

    accident claim herself yet yesterday

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
