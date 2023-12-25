b3f25d4428d0792f1448a2a1e5046b8788786683
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:30:05 +0000

    square Ss thank dinner famous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def square_number(x):
+        return x**2
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
