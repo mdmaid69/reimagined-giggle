483e8c670b8d06c6cb103f9296441a3f01697042
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:12:52 +0000

    method make thin brain why

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_user_string():
-        return collections.UserString()
+import array
+def reverse_array(array):
+        array.reverse()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
