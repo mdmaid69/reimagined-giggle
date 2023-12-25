9d0452442af41c0fb67ca121b0564947760951d6
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:53:18 +0000

    herself plan workout naturally Qq

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
