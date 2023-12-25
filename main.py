e3dacf81edb2313b27994087c511af7eddfabd36
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:11:09 +0000

    basically punish knowledge total moon

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
