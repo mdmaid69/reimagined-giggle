6e3b0c16d6cf458a88320abd9d332cd0185bd1a8
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:21:39 +0000

    give statement wheel clever post

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def find_min(numbers):
+        return min(numbers)
