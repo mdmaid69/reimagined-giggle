d35ad6151c14fdeba140dbe2501a89bf6b048ce8
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:38:05 +0000

    laugh uniform excuse circle trouble

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
