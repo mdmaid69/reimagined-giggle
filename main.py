b28a0f665f3e7ac06f44774ec5c56925ba3fc436
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:14:26 +0000

    imagination everybody match scene equipment

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  def is_odd(n):
-        return n % 2 != 0
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
