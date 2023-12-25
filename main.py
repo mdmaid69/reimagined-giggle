f89366990611876c39bdd12c0fa0c2e67ac03a57
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:00:28 +0000

    maintain mend view mean scared

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
