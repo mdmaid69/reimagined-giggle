251e38f40b3126b79e404ebf22be16cd56f83ae8
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:33:44 +0000

    set obvious cost force probably

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_work(force, distance):
+        return force * distance
