1a6ede557f6350485e9864d58e3bf8b142804307
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:39:15 +0000

    development niggaacademy together knowledge under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  def add_numbers(x, y):
+        return x + y
