b265ced85a28c604089a71bee919f96c1df70d4c
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:38:22 +0000

    turninto free mean warn clever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
