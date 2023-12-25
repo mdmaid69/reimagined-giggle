0da681c673aea31d94edf9b609ee22b7d9ef6f37
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:32:34 +0000

    television yesterday measure set mysterious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
