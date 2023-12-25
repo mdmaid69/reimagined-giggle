2150a6bba7d35d3f8410699505d1d2f136d7181c
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:06:45 +0000

    brave record reserve bring obvious

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
