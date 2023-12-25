08008115ce05185fd2ac00b09fa980066f93c5cf
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:26:26 +0000

    exchange heavy pleased noise tongue

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_power(work, time):
+        return work / time
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
