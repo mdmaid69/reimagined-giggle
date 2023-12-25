2a3c30d72085c6feb5d97a1a3528c02940f2904a
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:48:09 +0000

    doctor side card unpleasant enthusiastic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
 import sklearn.datasets
 print(sklearn.datasets.load_iris())
