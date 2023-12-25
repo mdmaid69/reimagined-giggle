2d6db1c28ad90ded97fb653af15186aeeaf8402e
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:53:04 +0000

    interview solve shrink now beneath

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
