a09922bdf7c2df0c2aef92bc9b54eb4b84b78695
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:37:21 +0000

    reserve water unpleasant trick scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import numpy as np
+print(np.array([1, 2, 3]))
