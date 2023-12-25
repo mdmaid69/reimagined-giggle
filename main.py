79f7125df81cd3d846d8aa4dd8ffeba8d68ef7be
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:18:14 +0000

    accommodation modern concern damage few

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def find_union(list1, list2):
+        return set(list1) | set(list2)
