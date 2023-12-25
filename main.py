920fb6d85eaa93cebf8ab2f9aaa1e6e2b0c68895
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:54:18 +0000

    be male doubt bunch return

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
