a4404ff7e1af95449e81b81c32684e2317b157ce
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:28:41 +0000

    chin firm takeout formal connect

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import time
+print(time.time())
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
