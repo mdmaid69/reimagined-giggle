2e393d6dc05ba73e5b7cb59681b4e592a5147dd7
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:29:18 +0000

    where school seem coat loose

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
