1170a6649729893fedb09550b55a0cd54b052f95
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:47:31 +0000

    star question view promise floor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
