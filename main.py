3ccba7f11d2954dac026c09ea5e06015135ee8f0
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:09:40 +0000

    single Ff cold estimate apart

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
