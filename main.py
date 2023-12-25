b6633352fbe8aeda6f80e25fb7916a49ff0ad735
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:14:23 +0000

    harm ourselves goon where schedule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
