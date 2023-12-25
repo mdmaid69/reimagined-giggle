082a975d99ef94a4aad649912d1796d37b1d656f
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:45:54 +0000

    report concentration chance house finally

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
