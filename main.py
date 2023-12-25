0e335683108104e79998966d7cd1d519d5f05b1e
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:09:39 +0000

    favourite crash silent middle substance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
