24beda8090554350e0404eaa218c551c5d25c2f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:53:42 +0000

    strike voice keepaway worried may

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  def remove_duplicates(lst):
+        return list(set(lst))
