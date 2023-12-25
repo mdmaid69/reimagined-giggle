c8a484f3a495de79d56ad02ef171f8d1f869fffb
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:47:46 +0000

    feature travel cap spite mess

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
