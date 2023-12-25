64faa56e9c30366c4e6fa7161a1bee37d29f878b
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:19:01 +0000

    hit sad trap where original

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
