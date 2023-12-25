06463c4f3b706a9e202b261481551b3aa047bfe6
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:03:07 +0000

    comeover Bb shut minimum plenty

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def is_palindrome(s):
+        return s == s[::-1]
