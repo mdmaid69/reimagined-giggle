de206cf71710a8f0d65d918e73d8965241e6d11b
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:34:58 +0000

    dare people fall spring attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  def subtract_numbers(x, y):
+        return x - y
