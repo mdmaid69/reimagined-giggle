60300f1ad16147d8c8fdff70fa702f928586d8fb
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:30:21 +0000

    their judge total approval perhaps

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
