82c610e07a1030c009046b013ae5687dcdca8d99
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:14:30 +0000

    head shadow exchange trouble you

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
