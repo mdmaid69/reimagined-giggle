6454047aa5d57145c3eee56ccdac74dc93d7af04
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:25:25 +0000

    raw bottom name murder above

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def convert_to_hex(n):
-        return hex(n)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
