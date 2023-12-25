255776f7ece05480753251394b859ea05bd67ac1
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:39:56 +0000

    confident most quickly smoke actually

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import datetime
+print(datetime.datetime.now())
