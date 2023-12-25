048bafd44f600789d4200180be445c277dbbcb4c
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:39:21 +0000

    replace trouble suck to crowd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
