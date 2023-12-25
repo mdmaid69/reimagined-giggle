ae63b92adce2c704ce839fa3dcc37bfa46e3f941
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:45:46 +0000

    married guess read workout division

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-name = "Python"
-print("Hello,", name)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
