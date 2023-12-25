dd9c9a66313487453d2af475a254b9f2bd547b90
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:53:54 +0000

    minute commit to treat seem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+def get_current_time():
+        return time.ctime()
