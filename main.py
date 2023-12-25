3f22163b6b3d1efb1796403c162f0d9f94f2e975
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:55:30 +0000

    even regard turnoff catch taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
