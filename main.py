d5458827427d020c630562be1d81acd7c24af14a
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:43:00 +0000

    mark calm collection possibly gather

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
 import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def square_number(x):
+        return x**2
