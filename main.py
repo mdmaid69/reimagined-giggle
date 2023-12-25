52181941086ac2a101fe85e4c5cc804c5badecd0
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:56:00 +0000

    related mark argue manage flat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_acceleration(speed, time):
-        return speed / time
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
