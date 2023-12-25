4997f7fc655d031d398ea92c9a43dcbc348d7680
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:49:10 +0000

    do dance similar boring careful

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
