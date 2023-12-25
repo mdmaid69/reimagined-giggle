681597c25281793b75a82acd6c95258db0b02e96
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:36:27 +0000

    dream affect woman disgusting present

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print("Hello, world!")
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
