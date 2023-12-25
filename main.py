c3aaba5f5912a7228402a779ec2bbde8d9afab75
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:58:09 +0000

    short obtain possibility oil active

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
