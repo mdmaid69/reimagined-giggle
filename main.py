9e5f555e430e055446d686cd2e41a3d3cf9ac1f0
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:53:47 +0000

    oil respect ride watch avoid

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_directory_name(path):
+        return os.path.dirname(path)
