ebeb3c71b95a6b855233a74cdd1fb62919f442ff
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:43:28 +0000

    grade wealth luck protest explanation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
