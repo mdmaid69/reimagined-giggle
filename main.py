eb9f7e0734c632036c7c49494aa49c80058c399c
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:03:37 +0000

    injury sink Bb go sometimes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
