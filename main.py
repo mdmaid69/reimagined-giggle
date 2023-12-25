ad3f2710dc358d75254a296fdc616a5a41bb7099
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:06:25 +0000

    Mm inform convenient unit regularly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
