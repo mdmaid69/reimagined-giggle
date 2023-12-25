104a5dde5a27f3878c868b0285c4da4aa5fa96fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:25:20 +0000

    impossible furniture strike might place

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
