e348c5c99d9ce04e667b43aeafd1289c8bf99e97
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:28:29 +0000

    tap emphasize comment tonight suffer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
