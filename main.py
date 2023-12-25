721e81ef9e205c598635003b2d3e43246e1c65bb
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:42:31 +0000

    refuse unpleasant splash cool with

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
