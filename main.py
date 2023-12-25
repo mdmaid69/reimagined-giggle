8f9773ce3fcdc41bfc4ff0f2713f33dcf9301ed0
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:09:52 +0000

    react any ride false big

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
