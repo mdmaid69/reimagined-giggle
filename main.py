db2f75080cbf51642a86c0811b057fff1662e490
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:01:09 +0000

    lift past listen quick ticket

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import sys
+print(sys.version)
