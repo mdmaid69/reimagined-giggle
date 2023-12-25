9e715c4207dc71f54c9b97c93ff8b31f6fc7911f
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:32:50 +0000

    combine try abroad Kk split

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
