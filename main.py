754eefed9f66c1088c99f2777b9128c313f6a236
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:50:19 +0000

    busy life wave exhibition last

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+print(os.getcwd())
+  def calculate_area_rectangle(l, w):
+        return l * w
