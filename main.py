a29fea633ad78da11ed47de21444e2528acd003e
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:13:30 +0000

    neither feature unfortunate grand suffer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def count_elements(lst):
+        return len(lst)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
