d5db2e9f3fb990e074762150255df56fb562e305
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:56:44 +0000

    ball immediately agreement promise blue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
