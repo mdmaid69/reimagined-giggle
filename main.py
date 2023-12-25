71c8c8f00f96c99f12e1dc1814d921c5a2d7d927
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:53:06 +0000

    defend brush breathe mother regard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_current_directory():
+        return os.getcwd()
