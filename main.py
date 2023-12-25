42df8d065cfbf692f630dbaa8dbd2c2726f6cc97
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:48:51 +0000

    method lower accidentally pure seat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
