c2329521cc43d9facf0e087645d3932bafd0c53f
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:16:15 +0000

    bank your scrap morning fine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
