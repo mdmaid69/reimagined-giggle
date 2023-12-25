41f4ada7f518c15454a605f4771adc18936f8cf5
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:09:08 +0000

    shape turn affect spot consider

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
