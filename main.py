e2bf4c8b1e5a1dc3a09e4ae9e54268ba4afcd672
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:50:01 +0000

    when dead mad turnaway crash

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
