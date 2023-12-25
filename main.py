eed0f1a96a0d72d4080a8dbcd38b60e52cf04cc6
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:58:25 +0000

    steady string either snow invite

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
