1c316976e32c6e87937fb37edde06497c7a8fe49
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:28:04 +0000

    visit cope preparation disturb Ll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import getpass
-def get_username():
-        return getpass.getuser()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
