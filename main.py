911c0a49aa5ab4cf3da8136513757463aca30414
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:54:50 +0000

    spare calmly night butter happen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_typecode(array):
+        return array.typecode
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
