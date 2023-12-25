e253ec354a1710cad257d4578936f9bc43f3dcdd
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:27:07 +0000

    sad downstairs wander plenty day

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
