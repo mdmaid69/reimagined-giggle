61e3fcf83d03c0f31707df3e6c60d4a27c24157d
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:20:05 +0000

    strict achievement lot motion trust

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
