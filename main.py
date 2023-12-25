f145af2c0bd5ccd5b449ddc619b1e843feb1666b
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:20:09 +0000

    speech brother opinion land official

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
