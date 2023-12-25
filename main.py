222fcf0f2c7022fd88dbfcdd42f4b17b32a9b5d0
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:27:09 +0000

    present pronunciation sharp instant skirt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
