fe0da65b210a792d4144cde481d747c683ee8fc2
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:34:18 +0000

    minor flood dear by stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
