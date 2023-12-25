679e14a5a06abe94153e90ea3d4e339a54572c11
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:00:17 +0000

    near shout coffee weather help

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-def remove_duplicates(lst):
-        return list(set(lst))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
