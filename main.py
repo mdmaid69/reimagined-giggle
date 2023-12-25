befb2cd95db9f13e5810ecbc0decbbfef9dc55a4
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:11:53 +0000

    aware church protection to any

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
