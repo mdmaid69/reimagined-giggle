253afce23fdd2566c66a7a067e5c24fd9d983789
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:19:55 +0000

    separate square noisy laugh possession

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
