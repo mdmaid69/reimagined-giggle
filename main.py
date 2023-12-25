9450058cf5660578757ea1003fc2f57afe038200
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:09:49 +0000

    whichever skill watch nor doubt

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def reverse_list(lst):
-        return lst[::-1]
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
