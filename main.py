4db8d96ba8cd34a93567bfb44096314fef5cbc77
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:00:30 +0000

    office so copy rid bin

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-print([x**2 for x in range(10)])
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
