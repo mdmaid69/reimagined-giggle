fad20a0831533c50ac21d8b9e42e666f149df7a2
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:07:17 +0000

    anywhere hand catch personal emphasize

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_acceleration(speed, time):
-        return speed / time
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
