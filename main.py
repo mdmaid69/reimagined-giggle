4a7de2d47d2738e53ebcd9ee93c5d6b106fe918a
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:47:38 +0000

    genuine progress reserve connection now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
