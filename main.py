1e52259ae48cbcaa87a8fb48f9c85d51c5bfbbbc
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:58:58 +0000

    notice work person switch light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
