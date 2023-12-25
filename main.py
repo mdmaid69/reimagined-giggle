98119432c9696bc39f85ff118314338429dc530e
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:07:38 +0000

    confidence container pile brush communication

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  def add_numbers(x, y):
-        return x + y
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
