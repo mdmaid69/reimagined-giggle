fbdc3c9c700bceb25a7aa368838f0d11dcad789b
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:55:25 +0000

    warn steep throughout break appearance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sys
+def print_python_version():
+        return sys.version
+i = 0
+while i < 5:
+        print(i)
+        i += 1
