43327d7a450d1f857def40ed7dc9f19da83cb27d
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:48:21 +0000

    capital let blame actual however

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
