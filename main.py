8a2b70c68fbc520d8fe5be7b771ebf17498c593d
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:44:05 +0000

    absolutely market disagree crisis science

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
