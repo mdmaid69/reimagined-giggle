690ac98890593d46aa67af37f4d86060af2160a1
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:49:37 +0000

    criticism his sore approve live

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def fibonacci(n):
+def fibonacci(n):
         return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def is_even(n):
+        return n % 2 == 0
