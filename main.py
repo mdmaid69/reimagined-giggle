4cb979e834cb688c22a3e80ca7a9a054397a1208
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:20:01 +0000

    least broad reduce really rate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def multiply_numbers(x, y):
-        return x * y
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
