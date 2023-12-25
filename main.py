7811431e29cf6e8c2edab37d47ab81ca13780054
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:40:07 +0000

    complete place put round feed

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  def cube_number(x):
+        return x**3
