7e535eca55c3b5ab4a921d05a0aa179cb50a6a66
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:20:15 +0000

    stand region forward narrow community

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
