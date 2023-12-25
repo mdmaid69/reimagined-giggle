d859b718f35d0454558cdbd4d71e86eb4f99c43c
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:19:19 +0000

    wire almost advice able mess

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def get_string_from_array(array):
+        return array.tobytes()
