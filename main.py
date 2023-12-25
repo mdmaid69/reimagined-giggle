c57fb245d68c3e123e4fe77d8aaec4f6bf5cd94d
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:25:33 +0000

    transport noone taste breath cause

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
