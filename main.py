e38d59247053f7341a3f92137bba86fe12308e2a
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:51:41 +0000

    guess thumb reserve major mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import os
+print(os.getcwd())
