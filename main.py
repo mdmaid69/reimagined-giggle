e46147b8c37eecbdc3eedaf5d420639ef090ac2f
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:58:54 +0000

    kill operate well will man

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def find_min(lst):
+        return min(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
