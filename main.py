3721b1dbf2f55a557220a64339c213dd20d06213
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:19:45 +0000

    in official habit block introduction

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def calculate_pressure(force, area):
-        return force / area
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
