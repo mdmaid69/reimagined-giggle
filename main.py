2d53c44099430e1529691f517468a27ecc03ff2c
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:53:34 +0000

    similar accident pretend calm gap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
