263c3dbe6320453651c71bd076a8ae9f2ddac0a4
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:08:29 +0000

    wander study journey payment play

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+print(sum(range(10)))
