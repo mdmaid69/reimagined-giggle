4ee4bd283b51b1eeaf3cbe3f0378df80146b01d0
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:32:52 +0000

    centimetre gate warn height female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
