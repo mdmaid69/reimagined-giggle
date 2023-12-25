2189f3a671b3d9f031aff9c1ca9273eb129f99b7
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:33:06 +0000

    heavy refuse touch force until

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
