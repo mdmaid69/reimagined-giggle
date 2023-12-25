d0cb7c91daeb5197a330a19dc8aab809e896f881
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:00:34 +0000

    hope disgusting age wealth letter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
