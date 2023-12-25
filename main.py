5e97621fb4a27e89c400912a0510d8327ffe7863
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:38:04 +0000

    difference thin own feel spend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import os
-print(os.getcwd())
+import array
+def get_array_as_repr(array):
+        return repr(array)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
