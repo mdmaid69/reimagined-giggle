d5d2105b1b948c547afbf72024a85bae770262e6
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:16:55 +0000

    prove national handle usually winter

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def count_words(sentence):
+        return len(sentence.split())
