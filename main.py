95db3ec58439e7b583b36d0d8b72104d34f18b55
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:05:04 +0000

    clear project match contrast spite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  def calculate_area_rectangle(l, w):
-        return l * w
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
