e7ec982b0eb92a1977b18bad22027cdb34cc681b
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:54:47 +0000

    spot what see failure towards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
