6edde9590d6ec249cfeb8527f9eb716b0c42c41e
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:19:06 +0000

    safety breakin trip thorough language

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
