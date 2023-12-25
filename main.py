6478a7be6791e12fce9fca8551b9a9b911299110
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:39:11 +0000

    let permission cause confidence wonder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
