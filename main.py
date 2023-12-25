d12f4938407a0febe22cc2bce4f335fba094cadc
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:16:21 +0000

    name Itself injury short land

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  def count_elements(lst):
-        return len(lst)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
