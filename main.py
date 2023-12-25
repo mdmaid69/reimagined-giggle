b18eaefacf29e385473457b90f31c5103f058675
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:37:34 +0000

    track correct this workout top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
