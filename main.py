98c409d52b42a59dfe30a48afe9c3f460819b1d7
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:08:47 +0000

    return operation habit star Cc

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
