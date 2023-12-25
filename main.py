75c3d60c37963219d327f431a1c94355f0fd2b04
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:28:05 +0000

    mail flow blow total recommend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def is_even(n):
+        return n % 2 == 0
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
