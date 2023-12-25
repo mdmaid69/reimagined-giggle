23b1cdec1f87d193630346262b0e6bea82f31861
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:51:27 +0000

    brush even rain answer mend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def get_string_from_array(array):
+        return array.tobytes()
+import random
+print(random.randint(0, 100))
