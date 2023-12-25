6d9498e2c56fcc7224ed28aaf859b60e4d476d91
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:52:58 +0000

    finger extremely hang fire Ss

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
