eb4ae5a463afd7de2d820ba5c7d6ece3136f4029
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:00:48 +0000

    interruption before comeoff regret confident

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
