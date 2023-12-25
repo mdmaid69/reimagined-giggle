cd609662cc1ab3980b78015295d477d5662aef58
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:10:57 +0000

    could surprise summer paper occasion

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
