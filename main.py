dad33008fd1122aaf685bba36fd91207ed30ec06
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:12:53 +0000

    he contact process shock consider

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
