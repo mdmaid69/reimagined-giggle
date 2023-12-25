384a4920f9fbd91f6a3869d7a02895a39621aa90
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:47:36 +0000

    wear fear rest various apply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def find_max(lst):
+        return max(lst)
