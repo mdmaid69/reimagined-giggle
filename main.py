e8874786b9b443bc071896884d8bce6b26799118
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:41:16 +0000

    young no unable like pencil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
