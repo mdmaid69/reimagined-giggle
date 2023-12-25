1df619fe843e1503bc3dbd556f0cce8fd197aed3
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:44:28 +0000

    breakout tomorrow crowded mail school

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
