d2a8dfd0658f5764a92945449715863f0f385dd7
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:49:18 +0000

    flow wave subject interested begin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
