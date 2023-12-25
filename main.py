f8c926c2b2839537357b7a26c71ac895f894060c
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:19:18 +0000

    pencil mine event measure ticket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
