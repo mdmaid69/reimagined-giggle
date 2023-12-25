c0404e74dc053d78d1762f26b3379d73d9e5fcad
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:13:59 +0000

    kick area convinced through wash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
