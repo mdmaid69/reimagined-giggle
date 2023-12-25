85f70c836314facfc4379cde5117cca154e8b6e1
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:48:54 +0000

    amount discovery return space dear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
