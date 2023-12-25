5c0f0b0df447062e0b75e64294f2e9afb0811e06
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:00:21 +0000

    zero estimate 565end free sweep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
