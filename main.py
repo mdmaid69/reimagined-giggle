d50cf6fdef251c67a604d15490bee606fc713fb7
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:53:11 +0000

    noisy round spring failure bank

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
