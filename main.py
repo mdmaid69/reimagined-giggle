b2e5f1e9c0168ebc0156b4fd85bb0baef1d6e8d8
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:36:36 +0000

    week loose trade rule hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
