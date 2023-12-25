18a7ca2ed2573348bb7a72f10d765dd8e33ed268
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:54:57 +0000

    secret crash guess refer at

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
