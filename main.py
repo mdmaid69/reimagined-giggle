15b327ae042869774a1fabf45df9c790a269611a
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:43:01 +0000

    edge instead look medium disgusting

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_max(numbers):
+        return max(numbers)
