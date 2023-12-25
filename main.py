1eb31a18bdd6a3a90cf06e39475c26d24a7b65c8
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:00:32 +0000

    shoulder market properly live himself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
