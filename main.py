cdaaa925162512b3fa8800e647e5a26e06f62c75
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:34:42 +0000

    blue goback(to) fortunately list beside

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
