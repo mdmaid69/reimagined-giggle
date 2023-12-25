e3fd9d4e26da19e8a98627ef1765081f80c07065
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:26:10 +0000

    side 570enjoyable will offence exit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import tensorflow as tf
-print(tf.__version__)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
