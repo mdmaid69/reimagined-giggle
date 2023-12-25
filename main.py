9892d48c963ffa211ceb5f9d5800526eec21a4c1
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:18:04 +0000

    answer decision classroom sweet trust

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
