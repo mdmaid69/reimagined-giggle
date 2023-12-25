38fedcdefd79c7d300d92e6328085949fdb384f8
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:08:54 +0000

    clearly feel must interest seriously

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
