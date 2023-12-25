2cc2b17a654c262ac5cb0825d7b66d7a4427e555
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:16:16 +0000

    smile deep effect kind switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
