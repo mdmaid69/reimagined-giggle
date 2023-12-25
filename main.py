c0441ba653bec45666ae61558751b53d747abce2
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:22:18 +0000

    desperate down attractive night pass

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
