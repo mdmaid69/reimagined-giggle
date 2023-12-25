fdfb56d89e63d98dd21c420d4fcf6a77cd153e9e
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:24:12 +0000

    blind the try pause Kk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
