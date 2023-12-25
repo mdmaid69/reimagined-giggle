a753c58f68132833123e9d370a5f007d91f8baae
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:41:38 +0000

    nothing hour judge find world

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
