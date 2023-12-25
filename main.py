e84c459b3fa17d2fe71e52b2eeae76cc72121dbd
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:47:42 +0000

    appearance precise usually job fun

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
