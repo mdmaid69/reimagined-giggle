e2c467c49f9e47691983de8c8f779b50c3dfd478
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:05:00 +0000

    satisfied lunch shout gate breakfast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
