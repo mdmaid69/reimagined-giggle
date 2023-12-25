ad30cf37ef51e618ecc5f93add97884c555f110e
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:04:48 +0000

    finish motion small pull progress

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def divide_numbers(x, y):
+        return x / y
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
