65be3d3db915da45265222195fee1c643ee27a61
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:55:20 +0000

    loud area intelligent phone authority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
