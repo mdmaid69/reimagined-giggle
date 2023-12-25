fc4ea35e5e85d74c5cdf38cda815cb86c5dd8814
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:05:02 +0000

    spread interfere there again extremely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def convert_to_octal(n):
-        return oct(n)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
