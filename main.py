b738658e0ffee3ac97b7d89dc68071d591c416f8
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:16:44 +0000

    oil female confused supply charge

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
