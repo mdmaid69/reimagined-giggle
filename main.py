d84a8669d1dd0c06a976645423cf723126b94930
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:04:05 +0000

    visit fun or funny check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
