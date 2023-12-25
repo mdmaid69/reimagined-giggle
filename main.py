490fa61486fb880b4ea1af02c7519bc0cef2fffb
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:37:12 +0000

    comeround into shoot past sort

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
