fdf8c6f539ce7489a0b80569bc3d969ae9322378
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:28:19 +0000

    convinced nothing too good low

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
