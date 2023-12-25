dfd09747fd1b35a6c55fbe457ff4ab36706b9a98
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:34:34 +0000

    juice important top 179bite spill

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
