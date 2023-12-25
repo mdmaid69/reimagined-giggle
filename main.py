ec54a990402ff9ba2a466722145e856e5b87eaeb
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:15:27 +0000

    voice pot system work excuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import platform
-def get_os_info():
-        return platform.uname()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
