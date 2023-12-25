8b791f8a5f3a6ffdddb628ba95bc2a0399b50969
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:09:37 +0000

    interview like owe declare equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
