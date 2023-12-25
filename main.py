6ab99bf5122b94fa0589ceaef0590eb37eea7110
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:17:51 +0000

    benefit progress near present cinema

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  def convert_to_binary(n):
-        return bin(n)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
