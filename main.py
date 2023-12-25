3bb7b2255ea469c611d70db5a76738c79121c121
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:51:54 +0000

    face myself settle bang flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
