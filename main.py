acfcf1d6c0f8f74b7af26a01578fa7880dde4a78
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:14:03 +0000

    dream famous break extremely state

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def convert_to_binary(n):
+        return bin(n)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
