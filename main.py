f0f25bb52500877358dcd4453bc37097466a7277
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:17:04 +0000

    in spring change detail play

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
