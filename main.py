ed8c9f7b8a9643797c25de646ba3c693fc68460d
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:23:07 +0000

    extremely correct change beat somehow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def multiply_numbers(x, y):
-        return x * y
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_area(radius):
+        return 3.14 * radius * radius
