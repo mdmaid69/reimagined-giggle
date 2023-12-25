1756e17d32c5aaa073b77654c284de25b0f347ea
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:15:59 +0000

    as show band class blame

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_floor(x):
+        return math.floor(x)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
