11c1f99a67af398060a00b67dd05321d14620330
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:49:59 +0000

    view reach drink house butter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
