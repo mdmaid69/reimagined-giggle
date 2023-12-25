6f88fad8f1e19f43795d35a9fda416376f6629fe
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:31:37 +0000

    forgive concerned entry safety sensitive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
