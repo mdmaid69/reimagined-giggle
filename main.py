ff9bc800bc6827655fd0d884ef3015410753ff12
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:13:26 +0000

    568energy gather breakfast drive bet

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
