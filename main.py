3416b73cc326c41fffe3c271c0bb23a2e4e1c291
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:53:37 +0000

    criticism present just fit length

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
