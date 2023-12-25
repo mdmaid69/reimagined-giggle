10ab82d650586c57f29a6cc2370ced6d23204914
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:37:04 +0000

    rule centre congratulations impatient love

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+name = "Python"
+print("Hello,", name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
