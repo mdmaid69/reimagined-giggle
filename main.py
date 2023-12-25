c5bed9867bdb399b2f078f3a6c6a850ece67638d
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:36:54 +0000

    somebody breakinto community just point

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
