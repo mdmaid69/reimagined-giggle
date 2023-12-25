55bed355252f26fd4c246a7b162cdf31bf7f04df
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:37:44 +0000

    guess Jj frighten clear conclusion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
