5b8b0db31e31627abf2abecc77bfd7e9eecae132
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:53:33 +0000

    ourselves period do store track

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
