8f7382d3c4e48fd07f40c33c17a74427fef2e077
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:52:10 +0000

    talkback experiment serious well turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_float(array):
+        return float(array[0])
