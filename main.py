1f9322f16f26f581c7775b4e27353a9540da8e13
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:34:00 +0000

    taste few decide land operation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
