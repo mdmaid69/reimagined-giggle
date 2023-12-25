6f6bf8fcf9bc81641393bc39b9d145f9c0e1d673
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:11:04 +0000

    hospital flat raw keepup along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def multiply_numbers(x, y):
+        return x * y
