dbcb94ebae8a4426bce82a86392957b1254502af
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:34:23 +0000

    so water repair care soon

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 def is_odd(n):
         return n % 2 != 0
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
