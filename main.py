606987b99fe95deef19a69e59f6ae3f44309d4a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:21:04 +0000

    affect lean damage test hospital

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
