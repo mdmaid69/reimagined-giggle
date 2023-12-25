d1c134e729682fe3da8a48c7c53095beb2439bf9
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:55:16 +0000

    pupil neck delight wall spring

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def clear_array(array):
+        array *= 0
