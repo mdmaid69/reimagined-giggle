704d7892098eb76fd2e9e11de4156d4e9e6c64d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:38:41 +0000

    stay politics escape chase crowd

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
