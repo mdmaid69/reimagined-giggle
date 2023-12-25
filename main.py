7546db92583e8f1a6559c086a67ffb38b8e7b114
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:25:52 +0000

    thankyou lend precise hear healthy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_pressure(force, area):
+        return force / area
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
