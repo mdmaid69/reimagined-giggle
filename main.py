565d914c4f9506d88ffe5ceb790e83f795eaa80b
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:23:36 +0000

    nose row least dangerous risk

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
