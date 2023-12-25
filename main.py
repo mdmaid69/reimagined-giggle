ed48e75eca442c7be0cd1359ce2a9691689d29e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:57:19 +0000

    catch soon certain physical brief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
