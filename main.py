48279b0a11f19b08c4e75154d3c4aaf3280ccca0
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:12:40 +0000

    accommodation bored anyone discovery fall

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
