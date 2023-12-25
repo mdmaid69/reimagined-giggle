ad483ec32552a381712f6011b8f961029c95e6df
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:14:17 +0000

    whenever test include face fear

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
