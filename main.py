b154d1b158fb6749eec0631bb202b05f5e384dec
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:33:38 +0000

    suit confidence comeback strict few

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def remove_duplicates(lst):
+        return list(set(lst))
