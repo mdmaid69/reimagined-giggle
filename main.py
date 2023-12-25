929e46d49a1f261c72f92893bee81f4b435058aa
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:05:30 +0000

    squeeze sink adjective challenge slightly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  def reverse_list(lst):
-        return lst[::-1]
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
