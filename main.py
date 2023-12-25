e7e2a5d73745b91c7b6422ade247240fc13148db
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:32:43 +0000

    trap attach minority shout extra

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
