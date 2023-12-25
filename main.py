4a7f0f67cb4014dfcd6b341e7a914bd6b6c8301f
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:59:20 +0000

    mind occasional clear behaviour tall

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
