c60138bfc9d0c922f84e48a8bb6c97c19b6b8687
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:45:57 +0000

    friend behaviour pump occasion scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
