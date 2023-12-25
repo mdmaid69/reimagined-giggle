7ee58ce51443a9338ee4d4bdf3336ca66d043530
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:36:02 +0000

    fire decide object call 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
