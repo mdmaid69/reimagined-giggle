f66e67afd58baa255b143c6ec2debf6e8c88ca01
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:02:12 +0000

    course thin quantity behind keepup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
