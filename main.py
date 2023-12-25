d841c08d2913b7fe454d36298f697be4d0dc3145
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:12:41 +0000

    introduce account treat breathe spray

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
