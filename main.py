bfb4e090f7dfc06594faaa44781f6ecc2262161f
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:30:27 +0000

    fail state careless threaten joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
