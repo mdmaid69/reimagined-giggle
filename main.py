bf19d4fb2e6442aaa086706d36187ba149bc0859
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:46:05 +0000

    interruption steep allow wall mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
