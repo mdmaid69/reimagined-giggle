ef639b8d10f9819eb35ccf5bed25e265473e2cfc
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:50:01 +0000

    last tip also true would

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
 def calculate_speed(distance, time):
         return distance / time
