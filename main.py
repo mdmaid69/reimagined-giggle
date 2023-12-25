a782af920e6b1af2d051b0f8fe7af51be24f73e7
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:05:31 +0000

    turn bank length spare glass

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
