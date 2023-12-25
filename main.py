6e1868ac38e440adaeac87782e21e184e29a5ada
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:36:20 +0000

    tradition sail breakinto giveup rid

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_work(force, distance):
-        return force * distance
-def find_max(numbers):
-        return max(numbers)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
