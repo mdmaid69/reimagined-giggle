3a0235cddd29060d08ac19592bdd196aa479c01d
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:50:41 +0000

    frighten throw whole feeling image

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_user_list():
+        return collections.UserList()
