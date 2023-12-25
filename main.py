ad50bdea049db833edd2a7f9e69444a874f9b07b
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:32:41 +0000

    comeback offend safety apart makeinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
