7c2cd1b97b35f9ae4cf76420f45fca6544505b42
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:14:38 +0000

    arrange turnoff detail account set

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_work(force, distance):
-        return force * distance
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
