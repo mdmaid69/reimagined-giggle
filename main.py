33b73b7170a38ee4c4abf537a6eb677566fad928
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:24:08 +0000

    freedom since stamp team thought

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
