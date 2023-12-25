da7e63413c1f468e4e38f758e39e37e6e3912d30
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:08:56 +0000

    later environment sweet wash winter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_device(file_name):
+  def get_file_dev(file_name):
         return os.stat(file_name).st_dev
