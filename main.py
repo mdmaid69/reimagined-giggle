d898756ab104567da2424e56564d53d233fd13ee
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:55:29 +0000

    shape bank murder quality suck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def find_unique_words(sentence):
+        return set(sentence.split())
