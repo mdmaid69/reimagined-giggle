055dd29ed23030abc3cd869d9c0f4fa58b8dac26
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:13:21 +0000

    serve glass sort probably protect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-print(math.pi)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
