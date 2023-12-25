41f638c4122a61714ad35d54e1d23087f8dd851e
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:49:38 +0000

    mark behind exercise ignore near

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-print(sum(range(10)))
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
