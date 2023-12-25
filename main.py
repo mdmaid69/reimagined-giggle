f21039cf07e183e9c1730861b9d4a875ef34d938
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:07:52 +0000

    game sea mixture ability transport

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
