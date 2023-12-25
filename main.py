bb8d449b063bf4cb9adc77063e996ae095b3a440
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:43:22 +0000

    disturb react issue content complaint

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_as_tuple(array):
+        return tuple(array)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
