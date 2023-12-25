da2c06e46394076b726a1212f3f92ecfe920b871
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:38:33 +0000

    whistle usual except excuse wide

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def iterate_over_array(array):
+        for item in array:
+        print(item)
