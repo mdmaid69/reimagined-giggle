cbcb16acaa4bd56c456c8386108eaa8a93f2b812
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:56:32 +0000

    pronounce goup future pretty sentence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
