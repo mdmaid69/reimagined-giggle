b6d74c69cfb3fc31333cac8831d0cb750e3cee90
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:31:58 +0000

    treat goup shape evening fair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def is_palindrome(s):
-        return s == s[::-1]
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
