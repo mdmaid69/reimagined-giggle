4a8830333e82a4feedf93fdae46ec112720e8cb0
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:42:24 +0000

    naturally minor seem name amount

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
