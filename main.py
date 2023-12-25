36942fc739ba129c82aa2d65caf9eba92a64a673
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:20:53 +0000

    minority rush characteristic total promise

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+  def convert_to_hex(n):
+        return hex(n)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
