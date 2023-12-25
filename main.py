2bf8eed2b0aef4cf4db88f8402f49d63aa281c45
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:16:15 +0000

    understand ring join group convince

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
