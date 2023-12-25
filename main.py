2a988cb8ecce574b1b6a731549a7163c49d680b8
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:03:10 +0000

    sometimes damp blame plenty callout

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_min(lst):
-        return min(lst)
-import time
-def get_current_time():
-        return time.ctime()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
