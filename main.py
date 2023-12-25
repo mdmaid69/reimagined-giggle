9849eb510201e58595577ccb77bd3b3b2732df03
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:45:39 +0000

    hand maybe far surprised television

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
