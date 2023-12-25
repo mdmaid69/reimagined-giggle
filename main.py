385bfb37352a2bb8d19b5c593900001d5bce31ef
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:23:59 +0000

    community bang sport warm scale

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def is_odd(n):
-        return n % 2 != 0
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
