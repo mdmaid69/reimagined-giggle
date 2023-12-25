77028d99a8e45a08e061bbf88ff3f1fa60713925
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:47:33 +0000

    about section confused this passenger

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
