c600d06683185d7a7976c79252e9fa722876f8c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:35:33 +0000

    hold borrow rush mind bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
