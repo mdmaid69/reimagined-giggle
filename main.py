ac84fdcd4b94a8a31952a1b0ce86065eb93e9bb6
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:28:47 +0000

    past minute ask condition success

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def count_elements(lst):
+        return len(lst)
