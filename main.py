3418b4e1d87a874b0868c83ae785165a116ce907
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:51:34 +0000

    suit godown thanks recent borrow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+print([x**2 for x in range(10)])
