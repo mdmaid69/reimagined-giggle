9a9cb9a5021b194d27affdd0aaed2d2055a9a3d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:35:58 +0000

    recent exchange point substance hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
