295ab539392589d8a1e65ae15cc0c6a4604e0cca
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:04:04 +0000

    collection afterwards check rely cheat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+def calculate_area_rectangle(l, w):
+        return l * w
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def iterate_over_array(array):
+        for item in array:
+        print(item)
