6527fc1b4882b36d75f844038396292809fe5342
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:40:58 +0000

    treatment rather scream point dinner

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_tangent(x):
+        return math.tan(x)
