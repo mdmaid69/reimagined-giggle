c5796c52062986fe73ba1d4e387b3a65b3232e6a
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:13:44 +0000

    factory any used alone always

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
