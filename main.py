bf2d006cf832c6231d434aa06b0d14e0f74ea417
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:39:58 +0000

    patience succeed sure getout door

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
