d83aad22b2467e2922f514281d8db15404f0810a
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:00:19 +0000

    compete taste post right early

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def is_even(n):
+        return n % 2 == 0
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
