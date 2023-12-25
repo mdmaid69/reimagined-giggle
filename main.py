110aaff7b063ea216aa21dae5ed53e8cdb1e9abc
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:26:50 +0000

    leave wonder likely hers pronounce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
