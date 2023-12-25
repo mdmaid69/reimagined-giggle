e8664dd8aeffc14f717afa5c820e1630026ae920
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:29:48 +0000

    schedule each steal relationship door

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
