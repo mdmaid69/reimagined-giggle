2e3a977d40c7fed923a4c3eaf7d1310a31f08bbd
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:53:25 +0000

    train tie about born on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
