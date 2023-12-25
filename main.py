b7da9126b496535e20a10d7a8188bd87e2fddae3
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:44:08 +0000

    advice play underneath theirs pleasure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import sys
-def print_python_version():
-        print(sys.version)
+def get_array_as_int(array):
+        return int(array[0])
