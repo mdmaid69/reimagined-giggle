afaf9cdd34edcc63fe42da01c97c731ec1cf8bda
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:01:27 +0000

    divide breath keep again point

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
