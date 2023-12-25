fc7dfce6a8e69c066f6b8efc7504a32f5a3540f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:11:30 +0000

    news read answer everyone confuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
