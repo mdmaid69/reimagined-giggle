a5a8b4899276cdbbb415ea5368a58d1c93cd28e4
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:31:34 +0000

    announce zero concentrate talkback scale

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
