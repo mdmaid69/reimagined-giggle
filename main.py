c7347b13d0c6f9329a4ccb49af0f07ed88b276ad
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:47:16 +0000

    reply bunch damp be uncomfortable

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
