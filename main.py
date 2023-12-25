3a78de7bca4e121461af4befe12f5c2d90feccc3
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:07:33 +0000

    demonstrate size disgusting lookup careful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
