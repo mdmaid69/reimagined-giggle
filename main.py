c5f74c46f0300544ce73b19d301c725d83bb7f0e
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:20:02 +0000

    fly narrow autumn far stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
