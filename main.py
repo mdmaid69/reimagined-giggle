12a188b3d7ce8fea2686d15b0abee3d590ba8d2f
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:42:25 +0000

    through talkoutof make forever grand

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-i = 0
-while i < 5:
+  def get_current_working_directory():
+        return os.getcwd()
+for i in range(5):
         print(i)
-        i += 1
