c8c6ac7fd854aaa66bdbe8dc3208c3338dd96e75
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:58:44 +0000

    apply stretch row grateful ought

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def add_numbers(a, b):
-        return a + b
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
