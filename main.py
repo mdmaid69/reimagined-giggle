17a7a9a74c64d1fc57eff73291b4e944480653e8
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:35:45 +0000

    excuse extra belief gentle lot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def remove_duplicates(lst):
+        return list(set(lst))
