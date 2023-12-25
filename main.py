4a933aac3538e27538021be3eab5c5fc9a4789b8
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:39:44 +0000

    introduce bag earth object accidentally

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
