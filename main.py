a1cc949ef62cd2ac16f254531af63c407c549fa4
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:03:57 +0000

    quarter treat reduction question hide

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
