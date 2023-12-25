1f866f7ea6345cc0b1eb800ee95e4691ca9de21e
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:09:58 +0000

    class pen uniform down offer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_area_circle(r):
+        return 3.14 * r**2
