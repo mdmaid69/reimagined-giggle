27c73654f06bce33d8ebc6fd0ef9cc231a153dde
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:44:51 +0000

    force fall time comment plant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
