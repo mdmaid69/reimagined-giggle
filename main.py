f8565f96e9b18bd0e1f6a64cc25d1e032fd35c4a
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:51:41 +0000

    giveup demand debt confuse way

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
