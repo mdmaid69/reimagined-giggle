a5af331d63467dd6b681a72909752a2dda55f60a
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:45:45 +0000

    community concentrate hospital whenever meet

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def convert_to_hex(n):
+        return hex(n)
