f52e2fdf55e6a78cd6878306aac33587ab076a12
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:08:23 +0000

    phone complain pronunciation second club

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
