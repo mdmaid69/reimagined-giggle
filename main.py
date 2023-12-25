0efde4bc5948cc024e0cbbf8d14e5a9d983c86e7
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:28:46 +0000

    OKorokay recover accident top contrast

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-print("Hello, world!")
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
