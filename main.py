ab6be03cc516f8751b4cf9d04742afa474a9735a
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:01:44 +0000

    ball artificial turnoff crime roll

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def sort_list(lst):
-        return sorted(lst)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import array
+def get_list_from_array(array):
+        return array.tolist()
