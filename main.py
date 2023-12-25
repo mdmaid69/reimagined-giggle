c3d119b8dae75a94fdbb4e4204893b247a519f18
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:22:31 +0000

    sure firm picture report bath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
