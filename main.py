894960079247d86012ce84c9b2496472205024fe
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:21:08 +0000

    immediately determined holiday vision stranger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
