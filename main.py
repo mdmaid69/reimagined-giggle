9a4fb5c2b55285438077bf41b7f4d170869f1bbb
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:57:47 +0000

    breathe preparation alone far sit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
