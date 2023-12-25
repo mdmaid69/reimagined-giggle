5da67647486677a44a8f215db64a3328e486fc8f
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:33:21 +0000

    friend lookafter powerful sweet workon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
