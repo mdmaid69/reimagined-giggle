0e0f97f95c8a19a68e4ea082b11e9b8b2744c415
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:00:35 +0000

    call abroad clearly matter cheque

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+text = "Hello, world!"
+print("Uppercase:", text.upper())
