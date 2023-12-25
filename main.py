c736af3682ad83fcf9fb0968b05d37add40bdf71
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:27:10 +0000

    produce pick sink change book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
