feaaf1227d98177ddf9ff9baf95a64689eb4b28d
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:26:35 +0000

    production depend season noone smile

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
