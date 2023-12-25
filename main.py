b84e1712984aec10f11f4e0369b3a243d3a6edbc
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:03:48 +0000

    interruption related recover confuse century

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
 import array
-def get_array_typecode(array):
-        return array.typecode
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def get_array_length(array):
+        return len(array)
