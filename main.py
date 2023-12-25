fb437387f0f531ee806443e4bafc9d18794ee7d5
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:58:11 +0000

    tidy tear sentence bored design

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  def cube_number(x):
-        return x**3
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
