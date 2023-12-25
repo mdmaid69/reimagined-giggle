03ab6d577f344f08c9e559859e36049cfa3429db
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:42:45 +0000

    contact pretty delight talkback get

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
