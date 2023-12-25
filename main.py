09a51c5b72486ea440d60c9786be52af005c2c91
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:06:01 +0000

    none contact closed blue sweep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
