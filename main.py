036d8ec70514ae19e6a0f3b40d06bf2f261716f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:40:44 +0000

    payment getoff aware discussion forward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def count_characters(sentence):
+        return len(sentence)
