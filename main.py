e84c9350a1096dbfb3ca9922e7cba48b3ffca41d
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:22:01 +0000

    cause brother event hand place

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import array
+def set_array_item(array, i, item):
+        array[i] = item
