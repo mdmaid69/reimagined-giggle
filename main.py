ebbac203323131d79c58b483fcf7fe98dfeec633
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:13:51 +0000

    however picture lower scale image

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
