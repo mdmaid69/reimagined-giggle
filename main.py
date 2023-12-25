665a5129f76de1081a5e25c0265badba19ccb8f6
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:45:10 +0000

    centre soil win keep embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
