3b45a955d19f072ed99de59b8589ec75a5518354
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:23:05 +0000

    exploration home zero offer train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
