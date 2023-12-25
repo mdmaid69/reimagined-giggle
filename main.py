91e046bf4828c115835d80747166ed49fc28ff78
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:56:42 +0000

    prevent wall payment ashamed earth

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def find_max(lst):
-        return max(lst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_pressure(force, area):
+        return force / area
