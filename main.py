80f60b769a461d63dfa476de8e18a70955102978
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:55:19 +0000

    tear cost subject contrast set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  def sort_list(lst):
+        return sorted(lst)
