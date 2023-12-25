29fa748fe52f731bf073bf6fbd1850a413f0db33
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:24:06 +0000

    represent me reply object always

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def clear_array(array):
+        array *= 0
