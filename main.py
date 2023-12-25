4dfd490f4920fbda3e8e0b8b42dbb05b3a0d7e08
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:44:19 +0000

    callback blame hurt level sweet

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def get_array_as_frozenset(array):
+        return frozenset(array)
