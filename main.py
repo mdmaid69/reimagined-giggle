f29d9a4b6f565fdc8095da98bf5845a5e093652f
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:46:45 +0000

    press difficulty turninto tap where

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+name = "Python"
+print("Hello,", name)
