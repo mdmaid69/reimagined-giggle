ca03d5fda67b83ac7364ca21ecac2c09670ca079
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:35:48 +0000

    criticism season advise fall drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
