6c4a2f45e2eb7440cfdb21fe416990c53bd10bb0
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:43:17 +0000

    scale half touch hide base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
