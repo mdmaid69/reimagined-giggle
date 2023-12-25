4b279cb63d989af715159ca1eae0288582fbf076
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:20:58 +0000

    suck dress nonsense score destroy

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-for i in range(10): print(i)
+  def split_path(path):
+        return os.path.split(path)
