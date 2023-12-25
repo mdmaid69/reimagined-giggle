1e316482037c59a89e975e2e188fdb97978e90a1
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:33:53 +0000

    disappear shadow colour trick share

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
