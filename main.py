c01462fb0a8d93f5ec57a389c13db2d1bf6d48fb
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:06:14 +0000

    yes always steam talkback discovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
