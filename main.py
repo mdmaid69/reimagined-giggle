d3ec91ee6b3e531657e62db2f71b73e1802490fb
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:30:45 +0000

    attractive onto cause takeback long

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import array
-def get_array_as_set(array):
-        return set(array)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
