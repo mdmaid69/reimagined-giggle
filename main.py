54225789a2672fc3ac9e3524cc85de2b70b9b711
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:45:30 +0000

    bottom idea ear fall hurry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
