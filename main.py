674ee518188c69591dbd272a392218efe8f8f120
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:05:21 +0000

    estimate hear expression hate form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def change_working_directory(path):
+        os.chdir(path)
