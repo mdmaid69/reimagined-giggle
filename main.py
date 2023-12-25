b2085bf8b58b1d9ea36be594c009db480d6c8d2e
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:38:16 +0000

    height kind regard tap else

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
