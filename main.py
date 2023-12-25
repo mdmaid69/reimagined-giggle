e0ca7aa3e38a22fa102b6c369c84857ae6a418b7
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:36:22 +0000

    information comment storm standard Rr

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+print(time.time())
