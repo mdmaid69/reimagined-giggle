ed3a663e96e4cdea890f92f8b704f1a99b413c77
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:45:57 +0000

    curious fault well handle strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
