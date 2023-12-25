c1400446f7517823af26aa344729507d023ec909
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:34:02 +0000

    ashamed country whistle church grand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
