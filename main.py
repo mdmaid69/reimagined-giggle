1217d9815b64caaecbb066fc8278dd771aab0986
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:37:00 +0000

    avoid guess embarrassing word tend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def append_to_array(array, item):
+        array.append(item)
