46bfab331d8e4922baf25bddf4cf0075fff45550
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:16:15 +0000

    suffer back stare cum prize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
