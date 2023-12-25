43bf9ca7336499cb15fba225ed22169b9c0a326b
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:04:30 +0000

    body collection creature area by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
