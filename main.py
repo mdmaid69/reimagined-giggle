a58a494917553c1ec06a3f552988a4f6540d33ea
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:05:49 +0000

    keepaway giveback report callout trade

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+name = "Python"
+print("Hello,", name)
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
