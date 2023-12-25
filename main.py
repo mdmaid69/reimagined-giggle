071a6695d83d1da8aaa3d62f56839812b867086f
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:15:31 +0000

    consist lookafter children independent believe

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
