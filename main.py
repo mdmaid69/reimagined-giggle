a38b70e29445632e29ddfa1aff8ad8d2fb27e48e
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:56:08 +0000

    generous or apparently bunch level

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
