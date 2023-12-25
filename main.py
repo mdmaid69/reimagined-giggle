513aade9695933fe7c63000bc84a22a016fa4441
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:23:47 +0000

    terrible usual wall ask early

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
