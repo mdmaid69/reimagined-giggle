485615319864a587ca707f2f3bbfb1eea146a288
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:12:13 +0000

    follow Itself very gooff art

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_min(lst):
-        return min(lst)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
