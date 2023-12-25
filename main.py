5e735415e841a51f7681e685f42c6550bd08c49b
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:11:18 +0000

    total brother avoid trap label

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def subtract_numbers(x, y):
+        return x - y
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
