1ab8b1a5516ba2beed9b4c15ad9b40d3f4c95d71
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:42:53 +0000

    weigh forever clever shallow somehow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
