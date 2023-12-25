c0a161e20377c9bde3758f2de443e91e1754162b
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:09:34 +0000

    story accident fire importance fair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
