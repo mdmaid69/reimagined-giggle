c30941b7a4099cee0e398a1a77724aaa87c484fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:01:11 +0000

    load apparently rub peace passenger

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def greet(name):
+        print(f"Hello, {name}!")
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
