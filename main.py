e06120a744c1a626d4d80e3ff6700ce69f68a09f
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:59:38 +0000

    day question ignore sign unite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
