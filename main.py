8e5bbbe4513c268fcbc89a0680212245bc6f9f65
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:18:00 +0000

    kill sentence poor workupto deliberate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
