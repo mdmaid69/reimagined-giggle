d43cbbe9201be10277ca752c2b83e7c56c971d75
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:45:20 +0000

    account fast terrible town embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
