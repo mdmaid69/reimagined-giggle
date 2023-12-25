fc77a8bf8d567b86d54a27b44da15d49c6765505
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:24:54 +0000

    pen praise pollution offer present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
