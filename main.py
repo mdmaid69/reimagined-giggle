e6f66098c8af558966a199ce6aaf500112234b52
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:08:40 +0000

    test her thanks wealth crisis

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
