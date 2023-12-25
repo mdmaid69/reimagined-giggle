48b933d1ebbfad032e6d66ca5357e9bee61317d5
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:40:09 +0000

    glass contribute Oo opposite takeaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import time
+def get_current_time():
+        return time.time()
