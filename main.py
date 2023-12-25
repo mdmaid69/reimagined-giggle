5fe2e884208ca128b8ca0be328b2d22e7e2b9073
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:31:24 +0000

    lookaround cut provide key people

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
