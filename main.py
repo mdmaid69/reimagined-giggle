5ff35e69f8ab0d1b0402392b20e59975bbc98bc9
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:02:21 +0000

    against undo away instant sorry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
