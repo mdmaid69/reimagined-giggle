403277f96dd332aea9b13beb23afb7d00d8b73a9
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:59:22 +0000

    find empty force calmly letter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
