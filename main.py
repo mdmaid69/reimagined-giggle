f63d540373dd600747eaef793da3ab123c911a0b
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:15:17 +0000

    occur exchange tell reduce consult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def sort_list(lst):
-        return sorted(lst)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
