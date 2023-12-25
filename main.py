f0dfac81f97b6b71f8b33cd196d6dd1908e711fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:00:40 +0000

    miss order label match certainly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_pressure(force, area):
+        return force / area
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
