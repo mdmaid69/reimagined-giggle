157f80408c67ef55121e60094f8ae3359683a1a6
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:10:14 +0000

    stranger voice natural line smile

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import time
-print(time.time())
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
