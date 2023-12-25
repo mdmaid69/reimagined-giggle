1310868dd4c135cce6a039b031777e539d1f4433
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:18:43 +0000

    common disapprove teach noise damage

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_bytes_from_array(array):
+        return array.tobytes()
