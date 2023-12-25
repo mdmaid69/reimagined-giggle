d685fb8afc9cf9497fafc91a2b5e23c35fd2fc60
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:18:01 +0000

    shop wing water lookaround date

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
