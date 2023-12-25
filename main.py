49a474360ebe5cf2378ae8d607469d1071ae2c0c
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:22:00 +0000

    supply music concern connect past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
