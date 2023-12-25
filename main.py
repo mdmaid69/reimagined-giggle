efaf9bf89580c88e33102549ae9b1bfa7dab2638
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:41:25 +0000

    goout turnoff length represent Ff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import datetime
+print(datetime.datetime.now())
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
