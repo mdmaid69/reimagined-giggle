889bd576e0c1d80ea104f3d37fbfc460b166997b
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:48:48 +0000

    often height claim dead price

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
