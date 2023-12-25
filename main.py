5259fdfda6d058718cdc25aa24387507eb61795a
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:43:54 +0000

    whistle shout punish modern bringback

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import tensorflow as tf
+print(tf.__version__)
