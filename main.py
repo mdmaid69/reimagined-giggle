b6d5e329e6e13c099552f35d296a9ef2b5653a19
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:40:53 +0000

    once play colour mean brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
