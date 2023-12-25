e28ad72ea8261c1c55278213586dce2c2d1abf63
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:54:45 +0000

    wire doubt intelligent square swim

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_permissions(file_name):
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
