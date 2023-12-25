e0c4d6d9207f361735b8ac12a923b10e3005d85d
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:24:31 +0000

    recent flight left free own

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
