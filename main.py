cbd923d8c00fd0f4bb4ee0dbec4ae8f661e152ff
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:52:49 +0000

    standard obtain really call meet

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def is_even(n):
+        return n % 2 == 0
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
