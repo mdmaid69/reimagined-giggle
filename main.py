bfcbb8f6acb9bfab8e946fd6c93b35bf8fecc25d
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:56:36 +0000

    satisfactory recent cheat wrong tough

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
