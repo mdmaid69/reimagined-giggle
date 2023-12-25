8ba757a5584890ef62fd4c71aaa1cbcfc637063b
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:31:14 +0000

    blame separate outside it support

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_volume(length, width, height):
+        return length * width * height
