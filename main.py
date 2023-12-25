138c27844320a0809192bd31afd4fd751a1c74dd
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:22:38 +0000

    tooth own free speak reliable

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_density(mass, volume):
-        return mass / volume
+def convert_array_to_unicode(array):
+        return array.tounicode()
