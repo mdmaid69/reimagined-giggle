ec94330eba74681617ea484d8d04c3f1ad5998c8
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:26:23 +0000

    for estimate week just much

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
