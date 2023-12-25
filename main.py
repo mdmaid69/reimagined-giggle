9df9f3939b027fb2aa9d25b5ec6dd1ca66d6cab5
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:59:55 +0000

    actually demand hurt invitation steam

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
