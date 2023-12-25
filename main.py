6fdbc63de43b57141be278d1c9f70cf6a5945a81
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:43:56 +0000

    entrance really neither argument yesterday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
