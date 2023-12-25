9e99ab2e2f4cd3abb4a23256e5afc84701b4925e
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:07:59 +0000

    mark degree factory love save

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
