5060f3ab3b5022838ea3bf226b962e6148a4f777
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:36:59 +0000

    week divide excellent usual burn

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
