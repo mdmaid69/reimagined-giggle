e86aec7e3beea19efb1b54f87c3e5373464af10b
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:18:25 +0000

    trust talk storm university usually

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
