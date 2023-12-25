d60f571fa10e8a7325aacbb6c7d5f6ea2bf3b3ee
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:59:33 +0000

    takeover half all judge term

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_speed(distance, time):
-        return distance / time
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
