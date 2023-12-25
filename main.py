1fa75fd6f9db93a77ec28603b8516e61b7368c51
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:31:33 +0000

    impatient invitation fasten several design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_list(array):
-        return list(array)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
