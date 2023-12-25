4eb03d694d3a2fefcf68c4a3b46e76eca7d27e1d
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:31:55 +0000

    black tongue print manner slow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
