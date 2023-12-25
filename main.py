36735a8c289578b2a4d42c5cc1bec649a64d2c90
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:27:19 +0000

    process long hair please cost

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def convert_array_to_list(array):
+        return array.tolist()
