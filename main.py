67feb630c748637ed21f56b753bd1f58047fd475
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:10:48 +0000

    reason tidy pump complain arm

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
