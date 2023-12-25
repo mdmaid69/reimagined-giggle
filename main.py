3a4d01ce46b48b68ab95b9b548e2d64d0d20cf4c
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:55:39 +0000

    dance pollution unlikely yet order

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import datetime
+print(datetime.datetime.now())
