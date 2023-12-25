3a858952b74a11705e4953fe4ffa782a10ffddf5
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:29:49 +0000

    probably why careful crowded guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
