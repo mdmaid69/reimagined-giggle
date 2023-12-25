d7ada639e00e16b99a61bc64fbc0d9396644306e
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:25:55 +0000

    pretend offence such coffee material

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def get_array_as_list(array):
-        return list(array)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
