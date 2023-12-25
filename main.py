817a3b6bbc0b630101fb320942a5a801a7d8be04
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:14:56 +0000

    pattern skill cheat let issue

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def find_max(numbers):
-        return max(numbers)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
