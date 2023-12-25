baad17cc905be69cd55fd53dab7b7b1087ceb60c
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:06:29 +0000

    marry thinkover office deaf breakinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_base_name(path):
+        return os.path.basename(path)
