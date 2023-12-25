4f276bf52d37767a3f4d55352aace2cd9851cd25
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:50:31 +0000

    main everywhere bin door heavy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
