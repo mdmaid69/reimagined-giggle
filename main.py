1f02b9e70ab7bbf4e56619a1e901843f2eb57f19
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:24:20 +0000

    conclusion giveout sort shrink turn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
