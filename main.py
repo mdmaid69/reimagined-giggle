6c96b8dcdfac122d9336e8eb1b9e69a4b705ab48
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:26:19 +0000

    ordinary clever surprise complete explain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import collections
+def create_stack():
+        return collections.deque()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
