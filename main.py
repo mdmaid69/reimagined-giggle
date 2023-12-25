4454cef163d6222d3fb6f66a1d134dd34fb67a05
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:33:45 +0000

    sing brush nigga expensive pocket

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_current_directory():
-        return os.getcwd()
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
