6d5009ffa0e76506155b825cb6c18c396f12af27
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:20:03 +0000

    suit jump rely bright 573entertainment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
