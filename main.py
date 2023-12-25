531faff66b17b69a7225114aa596576f8566c68a
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:27:01 +0000

    stretch everywhere declaration lookafter either

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def add_numbers(x, y):
-        return x + y
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+text = "Hello, world!"
+print("Reversed:", text[::-1])
