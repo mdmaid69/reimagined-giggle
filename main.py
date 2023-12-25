2815c845bb50b1a4e94699a3a1eb4c62cb4c2a62
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:43:49 +0000

    press relax away environment boring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
