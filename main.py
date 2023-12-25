480f7f8a118dacc2e175c3a26755eac8cb3f3819
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:05:25 +0000

    eye half callback apologizeapologise uncomfortable

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
