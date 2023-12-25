581ec09772b81e9249e64a0107fddb0aa2e0c711
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:37:13 +0000

    war mess anywayanyhow guess set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_as_set(array):
-        return set(array)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
