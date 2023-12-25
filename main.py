fcc2ea60d9b1eaf5bd1e197a63555c6e5a062d9e
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:34:12 +0000

    correct far exciting downstairs by

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def remove_duplicates(lst):
+        return list(set(lst))
