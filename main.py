5dff646f3fa5b0cc700eb4c32c2979559711c2cf
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:54:02 +0000

    letter then road available chat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
