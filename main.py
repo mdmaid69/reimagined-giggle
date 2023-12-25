1638981472e226cb5d0793cf76a12afe40f2401e
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:58:52 +0000

    Nn someone rub building use

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
