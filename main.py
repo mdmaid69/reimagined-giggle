de5ec61eeb16dbbe196f1f9ffd822ee71892ed22
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:39:35 +0000

    fair high question important turnaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
