292291266cfc70252a9371d8359cdca9cf6edba7
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:38:12 +0000

    tap mess interrupt Uu suck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
