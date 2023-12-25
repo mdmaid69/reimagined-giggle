b01071fe1473f9010214be5f9d727c76d3ef3049
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:00:47 +0000

    upwards worth possession command new

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
