76f480303dd9e2d8e438644e4c159fb0d980e748
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:52:16 +0000

    together inquire earth Rr sharp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_item_count(array, item):
+        return array.count(item)
