fac1d01c740b407236205a62c07ce27a1ed7cdc2
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:08:00 +0000

    mess you approve another phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
