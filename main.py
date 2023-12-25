b5cd5f4d2f938cad7a178c9a2e58fd66810f7651
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:46:12 +0000

    voice unkind demonstrate exaggerate dream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import sys
+print(sys.version)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
