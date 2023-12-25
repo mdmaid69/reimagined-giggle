eafe531c1fe1b99c85889daca9aeb8b60bb83a3f
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:59:07 +0000

    worry whistle lookup win breathe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
