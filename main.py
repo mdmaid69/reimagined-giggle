c9e1f0ac76aa02bf62944649df858502232f7f67
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:52:01 +0000

    certain bringforward positive pattern operation

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def reverse_list(lst):
-        return lst[::-1]
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
