e8b83385a0d78ad9d3a2b3e1cce75ce8ef675c89
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:14:27 +0000

    pot miss owe suit run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
