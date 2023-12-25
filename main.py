0600aee3fb3b6b7b944f2fd2448361977fadca41
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:04:52 +0000

    give well rubbish offend wind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_bool(array):
+        return bool(array)
