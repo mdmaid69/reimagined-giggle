c7ab7490be6fd6cf80b83d53d5921763d8e6502b
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:36:21 +0000

    rent maintain here judge boat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_pressure(force, area):
+        return force / area
