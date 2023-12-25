f1a09692c7ee61fd198626f2d4b98d6aa1d115b3
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:31:20 +0000

    so violence sudden understand manage

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
