8a1b8ff13eea43a0b61c18f08ae1a071eef65d02
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:51:23 +0000

    somebody meanwhile child tip another

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+x = 10
+y = 20
+print("Sum:", x + y)
