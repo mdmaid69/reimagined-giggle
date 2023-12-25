bfe57cdf7ecf34ecc6a10a59f22d0466c8ea0e76
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:43:49 +0000

    actual waste owe pretend sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
