b883ec7e1d1fad9646813653270daff49ac5854d
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:50:09 +0000

    shrink national busy through waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
