a9dd70e093a1eb5b62a011e8943a068ce54a966f
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:10:34 +0000

    when aim smart limit hour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def is_odd(n):
+        return n % 2 != 0
   import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def convert_dict_to_json(d):
+        return json.dumps(d)
