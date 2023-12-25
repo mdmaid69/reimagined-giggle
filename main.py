aa18f7c116318fe62d9c8c4ffc1635f8cfabcba5
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:54:40 +0000

    safe say pattern disappointed right

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
