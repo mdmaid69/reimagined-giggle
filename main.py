9c8d7189834818fd9e67fe6f1833de780d27c480
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:18:42 +0000

    forward least name repeat to

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def is_even(n):
-        return n % 2 == 0
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_acceleration(speed, time):
+        return speed / time
