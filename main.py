785b6efe72ea0a154f1b5749704668237be6e408
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:22:58 +0000

    kick female sticky hollow off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
