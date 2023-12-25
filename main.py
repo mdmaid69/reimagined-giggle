5c0c40bd047734ceb8f100ee9f2c12a270fa8aa8
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:06:54 +0000

    ourselves burn trip throw during

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
