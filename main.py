a884456b7a971216dfc022694c05e2353ae720a9
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:39:24 +0000

    detail shelter regional middle finally

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
