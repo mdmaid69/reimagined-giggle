42b6d7aef70d1b82f8902fd46466eee3c6580fe4
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:09:12 +0000

    coffee find constantly season mix

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import numpy as np
+print(np.array([1, 2, 3]))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
