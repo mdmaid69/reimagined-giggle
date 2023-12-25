4f5b8e95f526bf6a97f2061e10d1d1b4e8893897
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:50:56 +0000

    Bb warn body instant abroad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_stack():
+        return collections.deque()
