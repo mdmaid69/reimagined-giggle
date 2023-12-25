93832ac255391bdf4f5df2e89aa1eed06a310edd
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:09:29 +0000

    noone anywayanyhow us approval joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
