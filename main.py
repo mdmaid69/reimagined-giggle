93e251367a4548c9b7b0d372ca40a848d151efd1
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:03:58 +0000

    long arrive remain split Tt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def subtract_numbers(x, y):
+        return x - y
