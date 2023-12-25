261cb21e99d540f3bbd56363858f13a8097f7ee0
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:57:54 +0000

    geton pencil otherwise see ignore

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  def calculate_area_circle(r):
+        return 3.14 * r**2
