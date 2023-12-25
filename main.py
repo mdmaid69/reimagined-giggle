1a6c46aea0e3bd1bc224b1c26b1fc50d7b4a77d6
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:21:54 +0000

    information clear boil slowly attempt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def is_even(n):
+        return n % 2 == 0
