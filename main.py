570906ddbc4a9f0bb2acf88886cfb1dc9ce87062
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:38:44 +0000

    pleasant mistake bill strike convenient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
