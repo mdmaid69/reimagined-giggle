8ad1d8dd6bd0c2ad6a97950143e890e778d77072
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:27:08 +0000

    show trust land fast shock

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
