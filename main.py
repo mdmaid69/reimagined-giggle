478cedad64792b62ade5aa5776ff9d9e79332c99
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:04:45 +0000

    platform expression degree freeze every

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
