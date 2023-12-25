fb10bbeb93f930cf34a08f00ce151ecd7bfad7ed
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:23:05 +0000

    hour comeup 572enter march through

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
