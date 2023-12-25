f17de0f6a6edfd2fcc6e8096a10bbf788a1f7700
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:48:08 +0000

    relationship stir child takeback immediate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
