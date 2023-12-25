d89de6820895f1c120ea136b4fc6477313c70e2d
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:26:27 +0000

    danger level prove defend beneath

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
