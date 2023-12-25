68d3add0fd8397052a19656f993ac5211d750fb8
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:44:02 +0000

    water hair community education Ee

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def is_palindrome(s):
+        return s == s[::-1]
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_sine(x):
+        return math.sin(x)
