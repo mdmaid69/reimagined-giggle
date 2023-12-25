70a2dc45574c8c80b73b95d6f31fa6f501213043
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:26:41 +0000

    matter 567enemy to century rough

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+print(math.pi)
