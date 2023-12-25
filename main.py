50bee48437d72de686d4f3ebd238c8653c3e6b39
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:15:59 +0000

    by copy middle modern half

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_density(mass, volume):
+        return mass / volume
