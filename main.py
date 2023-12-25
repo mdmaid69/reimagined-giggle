ad3b9e0c3caeaa7b66d60acd67213fd32e5cfc9b
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:33:26 +0000

    or common separate tough figure

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_pressure(force, area):
-        return force / area
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import os
+print(os.getcwd())
