bef63a88aa8f08aca574b9e8311f64cd09abbd37
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:36:48 +0000

    Ff better join up centre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
