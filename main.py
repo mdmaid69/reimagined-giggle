b7218408679fd42f24b720ccc3760df9c0bff139
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:47:30 +0000

    scrap getout rent giveup band

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import os
-print(os.getcwd())
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
