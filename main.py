e22f3408172980716f615807f35f5c3cb348ec17
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:12:46 +0000

    for general mystery shout accident

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
