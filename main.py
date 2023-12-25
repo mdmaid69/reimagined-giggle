43ebcbf2413226a6556414fac5d394bea34791e5
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:17:08 +0000

    into operation reserve summer available

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
