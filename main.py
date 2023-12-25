0115bbdb4657aa7faf7b043f0d49d5dcea982c12
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:40:43 +0000

    stage cause move impatient correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_exponential(x):
+        return math.exp(x)
