5f9d65d9eda63c9c7c42ebef6969dbcb5da27281
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:14:57 +0000

    earn violent operate creature crazy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
