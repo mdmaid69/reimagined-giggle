6605c6caebe98afeabd421b5586ed1b124dd0b10
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:16:12 +0000

    scheme near handle main alive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
