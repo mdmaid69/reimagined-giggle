5c27b1b5778222d6d830cb77412c63350406ca1e
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:20:16 +0000

    hour violence bag late raise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_itemsize(array):
+        return array.itemsize
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
