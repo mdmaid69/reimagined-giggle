80a9b32f7c08b1ebb1baf9032c0125b5800548f9
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:33:53 +0000

    average Cc describe business sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import sys
-  def get_python_version():
-        return sys.version
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
