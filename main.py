777588dae459bd983f72b9bf12bb3cd150793bf3
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:36:39 +0000

    sort wander feeling mind scared

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def reverse_list(lst):
+        return lst[::-1]
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
