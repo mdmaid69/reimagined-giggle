4ef746c655c2aa71dc4c4fc53a493c3ea928e088
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:14:17 +0000

    command if hard meanwhile wheel

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import collections
+def create_stack():
+        return collections.deque()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
