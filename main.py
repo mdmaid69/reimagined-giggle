60d735067048e853aabf3b28babbf3c582011970
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:56:29 +0000

    tear soak secret must thing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_min(lst):
-        return min(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
