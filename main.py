8d174f414086aed8ebd868b75f85b161686ca6ff
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:20:17 +0000

    children promise health who love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
