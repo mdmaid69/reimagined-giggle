5fc8daa853d712f20d1c799b03b944f7b4766bbb
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:29:35 +0000

    chance concern equal themselves favourite

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_area_circle(r):
+        return 3.14 * r**2
