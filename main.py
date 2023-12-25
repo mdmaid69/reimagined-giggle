7d86a8fa8a199018a7e9468bc770f81361a1a10a
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:02:02 +0000

    impressive organization left pen break

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_volume(length, width, height):
-        return length * width * height
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
