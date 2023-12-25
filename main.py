0abb675371e96daa2a25617e0e22a5aceded18d5
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:48:38 +0000

    watch perfect valuable comfortable minute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
