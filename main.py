ae558b4a9bf2e42637f05cd11fe2ba639daab873
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:27:31 +0000

    legal human attempt consult pleased

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
