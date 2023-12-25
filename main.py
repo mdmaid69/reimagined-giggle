38c410fe785ec43b326b44f8c5036fc66f73e2aa
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:57:01 +0000

    strength throw the watch distance

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
