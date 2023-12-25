32c2e2e4d948854b27f79139bd750e9eb63fb33e
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:51:13 +0000

    once transport record hard odd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
