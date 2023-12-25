bca9625e25de178ef68c7c9d9619aec5af06e09d
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:03:28 +0000

    constantly disappointed increase deal event

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_typecode(array):
+        return array.typecode
