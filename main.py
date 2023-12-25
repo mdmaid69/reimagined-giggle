1440e774f3cadc51175eef65f4b7ac8f1e247bc3
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:18:28 +0000

    pause minimum minute but repeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
