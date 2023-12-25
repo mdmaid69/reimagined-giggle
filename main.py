ce0222af7f27f2243de73a395264e68cd74148f1
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:13:07 +0000

    add capital create damp model

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
