721ba23629bef8feaa37e624ad8439416dd51902
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:59:52 +0000

    sign like always information excuse

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
