9bae2c720f9ca9a7deccbcf01f2faeb36ec4f730
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:38:39 +0000

    trap communicate light knock shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
