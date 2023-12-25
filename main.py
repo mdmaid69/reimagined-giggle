9846ace28994575deb3f53780cf1715eb742bad2
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:33:04 +0000

    bye intelligent false terrible unlucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
