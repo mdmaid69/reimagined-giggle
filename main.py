e9cce60d132f1b384a334c2e780f3e91424b55b9
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:15:55 +0000

    lucky create air opinion come

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
