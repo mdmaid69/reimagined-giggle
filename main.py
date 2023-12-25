02f301dbc999e3ede918fbe6829d555cdef0a58b
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:11:59 +0000

    naturally memory report tough material

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
