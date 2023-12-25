661a7a2de01cdeeae5c11065fac9c69ff26d8547
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:14:02 +0000

    teach cinema side ignore unkind

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import datetime
+def get_today_date():
+        return datetime.date.today()
