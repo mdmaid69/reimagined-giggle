cf0538d468aa57bdd2e62c9c4cce1905dbbe358c
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:14:17 +0000

    relax single side shout track

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def reverse_array(array):
+        array.reverse()
+  def count_elements(lst):
+        return len(lst)
