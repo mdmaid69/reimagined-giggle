787d7d4204ea0757238e7e16a32b0ff015e27512
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:13:33 +0000

    still branch discuss secret remember

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
