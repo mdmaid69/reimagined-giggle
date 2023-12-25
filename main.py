da4ec517ecafa406efcf10dee450e7c168c1a019
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:45:20 +0000

    tradition slowly replace year outside

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def is_palindrome(s):
+        return s == s[::-1]
