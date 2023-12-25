240878657916afbfca6b2e24790aea7adaae5a99
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:29:08 +0000

    tap to various takeout ourselves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  def is_odd(n):
-        return n % 2 != 0
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
