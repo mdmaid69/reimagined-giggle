5d6feb46f68fbb470cd75cb0f91c7446d7aa2c5a
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:08:35 +0000

    sale arrival waste surprising contrast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
