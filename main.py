d0557faa64ffdb329794e25d65b1ee3d7edd6b4e
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:30:21 +0000

    post wave behind bring central

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
