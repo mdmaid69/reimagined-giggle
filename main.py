02b7385660aa3651ffe8e34de23c534f7b1c8ed5
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:37:55 +0000

    past comeover unfortunate with Hh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def subtract_numbers(x, y):
+        return x - y
