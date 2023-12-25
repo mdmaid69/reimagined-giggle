cf0dac17f617d0d7479812a69d707d9831c74b90
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:34:10 +0000

    interfere present light measure second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
