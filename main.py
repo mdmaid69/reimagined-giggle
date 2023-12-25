a883484a372a3b87ed6f6b2e7796c297586f8300
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:38:24 +0000

    example dance soft because miss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
