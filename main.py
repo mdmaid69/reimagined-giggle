aecf63cd5760182de0e6c7725ad62db2513796e2
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:52:11 +0000

    born manner dark suffer discover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
