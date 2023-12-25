83cb4eeff5ad12932e9959e98b898f64e27e02b8
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:11:57 +0000

    important there turnaway exact shock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
