c85e45dc4843c3b403f198ee9a4866ccc5c3232e
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:04:41 +0000

    rubbish slow acceptable half pressure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import tensorflow as tf
-print(tf.__version__)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
