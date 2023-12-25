b514eaa2b13ab1363175c388ea107a6df9062d32
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:30:35 +0000

    everybody Cc whichever steady sense

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+from collections import Counter
+print(Counter("hello world"))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
