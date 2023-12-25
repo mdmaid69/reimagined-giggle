e7a266b5620e71f41d34965dea0b4e8087f211be
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:00:46 +0000

    list enthusiastic tomorrow private ride

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
 def calculate_neper_number_to_power_x(x):
         return math.exp(x)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
