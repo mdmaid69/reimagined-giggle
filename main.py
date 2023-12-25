c6fdfdac63e13d3ff030669503e97eb2ef66757d
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:36:50 +0000

    cause file left gate mine

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def reverse_list(lst):
+        return lst[::-1]
