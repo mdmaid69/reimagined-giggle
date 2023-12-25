2f889ff48b6795c4f2533478c03e2604eaacdccc
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:43:31 +0000

    post production copy later choice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
