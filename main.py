f5a133d358e2831f3f453a245f99a1c631a17bdd
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:27:03 +0000

    false either drive drink beneath

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
