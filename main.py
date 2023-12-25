ad482d8459643e90baf8310f79c9486eb68a8218
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:30:32 +0000

    moon train eager on tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def sort_list(lst):
+        return sorted(lst)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
