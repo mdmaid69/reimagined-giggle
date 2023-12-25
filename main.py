e17f35f541c38eb398324df166f9f23c55cc7fc3
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:58:09 +0000

    fold even again shallow battery

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+text = "Hello, world!"
+print("Uppercase:", text.upper())
