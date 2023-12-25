e47189f09c3f11c7ac3ac44f268806c64a1db9f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:23:53 +0000

    house ball story lean lay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import tensorflow as tf
+print(tf.__version__)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
