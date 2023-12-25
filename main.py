6fb104bba71bbfb0487c945fb51047074c5f59ba
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:57:53 +0000

    eager yesterday programme shop study

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
