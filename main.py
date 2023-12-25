aa3bf5a633767d5f6f6a416ea8f789e4070a1247
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:11:13 +0000

    trick organization payment his afterwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
