f9a344cb2a9aec62fe36ecd5a295252c7326d219
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:33:25 +0000

    Gg mess no accordingto skill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_volume(length, width, height):
+        return length * width * height
