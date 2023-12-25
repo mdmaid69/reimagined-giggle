f1d98c83d3aa764385a550a086a37119d0742798
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:23:19 +0000

    shape emotion runinto board politics

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
