2d6bc053d3074660ef555c7b36ca0fe6514c92fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:26:29 +0000

    maximum race drive degree sell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
