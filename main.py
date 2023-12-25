6b7ebb39bec384a16b2add294fdbf427f684ca36
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:08:31 +0000

    back scare safety pool spare

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
