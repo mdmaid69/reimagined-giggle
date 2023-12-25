acfa9bcf2550a123b8ecd6a49e68387eb90e303c
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:31:20 +0000

    deliberately turnoff reasonable giveup bunch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
