04529ab7c3c15e17beeb84ddfe0b61698a4fd6e7
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:16:02 +0000

    Vv loose deliver sea lead

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
