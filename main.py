5dca5f0e349b592c4ae12d97bbbae15a6a1bb1d6
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:32:47 +0000

    beside scheme genuine murder poison

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def remove_directory(path):
+        os.rmdir(path)
