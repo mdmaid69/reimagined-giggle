fd220ef029da4acc56907655d11ec2d1969de731
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:53:25 +0000

    breakinto boring dry excited label

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
