62c1f90eb7563297f93e7ea105d993ada9543b9a
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:28:10 +0000

    correct hide equal whom somewhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_max(numbers):
+        return max(numbers)
