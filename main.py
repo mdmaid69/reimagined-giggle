46bbe62960ba3efeb113c3b350014fcb255de219
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:06:48 +0000

    polite excuse pencil wealth juice

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
