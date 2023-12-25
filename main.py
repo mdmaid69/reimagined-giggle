ed534a0fcc623101a26b50dadfb05d095d7c6445
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:44:06 +0000

    education bad embarrassing try goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+print(math.pi)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
