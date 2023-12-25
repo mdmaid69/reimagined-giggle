ab4c62e2db921f90741c4fa46322b88c05de9596
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:44:38 +0000

    independent direct control smile win

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
