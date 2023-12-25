2541c4e2d3b9c9418fb83e819e8c07ad3abf51ac
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:51:19 +0000

    central guess mad holdup rush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import getpass
+def get_username():
+        return getpass.getuser()
+def find_max(numbers):
+        return max(numbers)
