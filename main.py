5b6bbfa1b8becc9565f91c08d7c3fc3b7defa06e
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:57:40 +0000

    language prize beginning great concerning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
