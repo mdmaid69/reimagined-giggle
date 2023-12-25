a10d5bbf6bef65530374395065c86e35afa5956c
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:56:11 +0000

    proper brief reduction sore original

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def calculate_area_rectangle(l, w):
+        return l * w
