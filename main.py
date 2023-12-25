a6a81e9dfc7081da7fe90996037b18737a251ead
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:59:22 +0000

    base check section scream mainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
