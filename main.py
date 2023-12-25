1d24541007e7226f55c05d3338c006997b46d616
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:52:58 +0000

    rush land platform surface contact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
