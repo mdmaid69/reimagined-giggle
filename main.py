362eeee981809f4bd9f74afec06677c5b8391a75
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:33:11 +0000

    congratulate Cc class date use

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
