c05f78a199047f3cf61608c8eab3b9f909fe0adf
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:37:48 +0000

    indeed breakinto much upstairs meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
