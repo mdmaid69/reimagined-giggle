488aca5fd15d6347e92b462f9a2250c9058e1489
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:20:00 +0000

    attitude change formal start bringup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
