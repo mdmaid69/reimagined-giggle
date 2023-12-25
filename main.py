2f9233688e1cb8d438a340911ca238d77be9c6ca
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:05:39 +0000

    switch way load none paper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import array
+def get_array_item(array, i):
+        return array[i]
