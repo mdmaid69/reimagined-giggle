1205875e3df4df2b4227fa1630cf1dc9c46105c0
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:04:33 +0000

    favourite bound action regret play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def reverse_list(lst):
+        return lst[::-1]
