1e3c45af4a6bffede09844e38929946ed62f1c83
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:26:25 +0000

    plastic section discovery brave think

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
