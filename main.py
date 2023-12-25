b6c35dfdc31ceddf156c2e14df52705f29f962f9
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:43:00 +0000

    close declaration rush week into

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
