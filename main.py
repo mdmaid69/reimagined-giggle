e1a574de95d3dd808201f157fef308b984f0cab1
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:05:17 +0000

    live whichever cap underneath mail

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import os
-print(os.getcwd())
+def square_number(x):
+        return x**2
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
