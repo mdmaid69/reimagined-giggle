c598b0a5fdb6d74a5741152292f18d126d6943ab
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:01:00 +0000

    talkback getback gas repair software

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
