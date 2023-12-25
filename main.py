7a37655e86b26e5d4d1e0090fdda67444de5770f
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:54:58 +0000

    entry increase underneath even bang

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
