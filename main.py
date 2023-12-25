fe4bcd798dc0138f2df5e004931eaf253754a61a
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:44:05 +0000

    cheque minimum skin block lift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
