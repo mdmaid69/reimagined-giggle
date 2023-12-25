c3f355ef81bfbd4aca546b4e7b875519139bd223
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:23:14 +0000

    law warn already fine importance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
