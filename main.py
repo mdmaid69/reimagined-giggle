e2cf7fa005e263b9be5803e739d8b2c3cdb3b93e
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:58:48 +0000

    concerned convince damage along off

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  def is_odd(n):
-        return n % 2 != 0
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
