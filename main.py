017026cbfebe5df34a8e6107a20343f5e9a46637
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:42:11 +0000

    continue most file attractive argue

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def remove_duplicates(lst):
+        return list(set(lst))
