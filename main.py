6a39bc161519351b12892225b4ccce6ccdce1f8c
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:24:53 +0000

    whistle behind own report blow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
