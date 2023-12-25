12559cc06662ef61df0d3e3ccf2ef29917e0aadd
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:26:21 +0000

    defend turnaway peace expect male

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def sort_list(lst):
+        return sorted(lst)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
