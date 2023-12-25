34f8228eec9163891504140c82312debfac6b9e6
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:36:30 +0000

    luck crash fasten recommend study

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+print([x**2 for x in range(10)])
