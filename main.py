42057b9260c14d4e9131801ecaeb69580119f1de
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:41:34 +0000

    clean wrap personal violence underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+import os
+print(os.getcwd())
