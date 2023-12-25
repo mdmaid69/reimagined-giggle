c141e0b5415c2e1e2a100c1c2bda4e9ad529477f
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:04:18 +0000

    defend affect mix poison attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def get_array_as_tuple(array):
+        return tuple(array)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
