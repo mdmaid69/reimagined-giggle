51d618719cf5072eafb3b29806f1ced9a48f93d2
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:54:48 +0000

    manage on tap connection battery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def count_elements(lst):
+        return len(lst)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
