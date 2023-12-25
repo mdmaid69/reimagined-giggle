696a8f3f67f976580c2dd7bac64a177555f20d63
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:43:36 +0000

    no ordinary project plastic around

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import os
+def get_current_working_directory():
+        return os.getcwd()
