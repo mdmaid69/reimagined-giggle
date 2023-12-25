ca2360002be61cd8fdba6729c5ad43f2965c8d99
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:07:35 +0000

    laugh strong ourselves drive shout

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
