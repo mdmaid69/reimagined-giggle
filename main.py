47887c19f9035a67ed4886a18c337460e8d7654a
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:31:17 +0000

    turnon exploration achieve may persuade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
