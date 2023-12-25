b83286e78ce1fbbef6419d87246f22a322aa7f44
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:55:52 +0000

    course offer main operation education

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import os
 def get_file_modification_time(filename):
         return os.path.getmtime(filename)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
