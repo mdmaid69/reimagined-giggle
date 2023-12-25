7febcda561799eb27a75f3cdb45bb3b8d0722956
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:38:07 +0000

    geton respect prove attempt objection

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import sys
-def exit_program():
-        sys.exit()
+import collections
+def create_priority_queue():
+        return collections.deque()
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
