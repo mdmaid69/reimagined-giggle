274d9b3ab5131910e644c8e410496eaab9405e73
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:41:16 +0000

    straight take guide difficult murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
