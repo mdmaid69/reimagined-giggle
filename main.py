6cc06a0bb95476d5ffcbe1a8181bde3614e7081b
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:02:58 +0000

    her height ball wise finger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_base_name(path):
+        return os.path.basename(path)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
