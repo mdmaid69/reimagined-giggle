20cf2be00f0b5bc7c142dab9dd8b0072369cc924
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:45:08 +0000

    another himself transport smile down

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+def print_python_version():
+        print(sys.version)
