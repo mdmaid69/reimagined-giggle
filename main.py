ea5414890f3b35b509bcfc46ed4860c336a448ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:53:07 +0000

    neat truth pollution authority basis

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_area_circle(r):
+        return 3.14 * r**2
