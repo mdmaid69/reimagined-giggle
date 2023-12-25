10f0cadc96daceff49b37fe6f5358f9f4a4ed133
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:48:08 +0000

    pen ball true centimetre spare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def append_to_array(array, item):
+        array.append(item)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
