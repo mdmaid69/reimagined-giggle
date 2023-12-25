fd27cd9ec650ff2cedbd6039d1432895793251dc
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:00:44 +0000

    blue cook influence fold introduction

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
