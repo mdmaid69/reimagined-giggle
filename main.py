805b990805fd48f1f7c760e656277c6f35553da6
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:59:05 +0000

    relative expensive game last escape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import platform
+def get_python_version():
+        return platform.python_version()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
