d7754c8bd418978557765b3e11f8253d67098ad2
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:54:17 +0000

    couple watch production runinto goodbye

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
