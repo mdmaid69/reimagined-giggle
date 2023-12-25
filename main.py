1dafcd7fda1d81679c24dfd13d8412fec6f38b49
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:07:56 +0000

    limit amount later address object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
