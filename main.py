2d108eebf4bb7e662903b4a0595389d0d91e4bda
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:50:43 +0000

    seem keepup take risk sister

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_user_list():
-        return collections.UserList()
+for i in range(5):
+        print(i)
+import array
+def get_array_as_str(array):
+        return str(array)
