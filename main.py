ce8a674892b716d39501678f32606004fd522a92
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:21:58 +0000

    justice whole fault straight common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_queue():
+        return collections.deque()
