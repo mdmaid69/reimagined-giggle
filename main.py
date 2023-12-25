1c70cc5136711a652259066fc86725f8bd123697
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:21:24 +0000

    Itself wipe naturally material after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
