39c4496bfbeb8b142da0ff6a71f768f59d61c6e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:24:32 +0000

    constantly seriously pot quiet cope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
