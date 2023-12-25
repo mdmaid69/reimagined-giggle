20175c0353a3526e7e235e2f9a1486ba110e43af
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:08:29 +0000

    butter mess storm protect laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
