337e5d8e09277e6f32c241cde1ddabe60053e123
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:12:26 +0000

    cook brown convinced love achieve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
