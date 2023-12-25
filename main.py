20c4530f8ad636c6f5911658ecc22e231c67a280
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:01:28 +0000

    reasonable brief violent crash with

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def sort_list(lst):
+        return sorted(lst)
