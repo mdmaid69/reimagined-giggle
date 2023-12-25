7fbc109bb4cb6ecd15405b0eebc995638a1efc0f
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:48:34 +0000

    brief great confuse passenger bet

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
