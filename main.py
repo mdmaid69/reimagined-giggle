c575f8453ff43ca42e836126ab8ddf02328b828a
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:18:30 +0000

    secret below save tip wait

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import array
+def append_to_array(array, item):
+        array.append(item)
