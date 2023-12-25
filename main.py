dc485e376c12145a6f821164e315a42b21d5af80
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:53:05 +0000

    below full rough people sleep

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def reverse_array(array):
-        array.reverse()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
