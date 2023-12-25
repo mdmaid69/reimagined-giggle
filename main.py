dab72222f281a3c4cc4fc1a719f7498425cb2e0c
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:28:50 +0000

    always repeat along manage hold

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
