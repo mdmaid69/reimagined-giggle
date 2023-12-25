1594434d626b9782b8d905659ad7512b3da8631e
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:16:56 +0000

    over favour backwards surprised ride

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
