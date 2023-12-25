c27f576865c9455b4f4a6a0b11222bef4b210d7e
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:29:56 +0000

    wave full lookback(on) suggest to

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
