a6db4fa4c175052086c3ba193c5aa81541c92506
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:52:03 +0000

    improvement respect watch pass woman

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
