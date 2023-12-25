02f29eaa7ab34e83f158b99028c24b0bbcf17e40
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:23:50 +0000

    but big cut raise giveout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
