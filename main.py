9b54103bacd653cd1a24805aba1b0d7b0ed28fcc
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:18:20 +0000

    stone giveout still relax need

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def delete_file(file_name):
+        os.remove(file_name)
