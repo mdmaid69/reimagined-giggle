394bf71af467b511ed0002a2e46c27571afbb3aa
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:46:44 +0000

    further influence keep criticize centimetre

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
