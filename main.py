7cc2e439d4848d10ac18f7ad97b3eddd0c5556e8
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:06:20 +0000

    sight favour closed dance tread

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
