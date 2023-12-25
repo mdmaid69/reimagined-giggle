fb0653e97e7375805f73efaadbc3165a78557cff
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:09:29 +0000

    fair clean nervous step slip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
