6e3fb997f60f13c71701586d543e2cee41fd2e73
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:02:38 +0000

    some world branch goon cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
