7db27a276a8e6fee01f3d4f3b3b48c09b0ada8a7
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:22:51 +0000

    container tooth support sink many

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
