e284c5c58b6bb2a6a73868bab57b0446894558fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:33:24 +0000

    179bite always worth sting skirt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
