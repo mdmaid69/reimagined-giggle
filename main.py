6ec42fcd00936ce4ba95d89e6b579f9813c06c0c
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:51:46 +0000

    majority solve history tune stop

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
