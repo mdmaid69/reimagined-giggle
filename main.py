80dc5644b2c9acc525da685310cb4c4cc568862f
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:58:17 +0000

    physical hello yesterday Vv this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
