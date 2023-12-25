b1f21d9af36802ee9a47e40ba16893c88f06c599
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:51:59 +0000

    write demand thinkof develop remember

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
