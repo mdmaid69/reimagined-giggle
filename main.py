1d1bcb2e9184b0db1383dc029f90bb947bcab4ef
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:36:22 +0000

    572enter immediately point before spot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
