8711255b888ad769f10581fde099f1b86758a569
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:01:05 +0000

    blow duty sting exam several

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+from collections import Counter
+print(Counter("hello world"))
