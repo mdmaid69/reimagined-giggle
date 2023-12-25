5cf4c25ff7bee204b531723ae8edde04738baab0
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:45:52 +0000

    block somehow snow board callback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
