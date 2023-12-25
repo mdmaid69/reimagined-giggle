8e39204f6ed68b5f7d3f7edc253476ad3095aeb0
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:01:24 +0000

    expression swallow afternoon still stone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
