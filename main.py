baa31772fdfd35f412866f756ed15e25e0d64787
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:39:45 +0000

    standard priority zero persuade young

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def cube_number(x):
+        return x**3
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
