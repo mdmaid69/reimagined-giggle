85d2170d5efc23e00a5b3df7da6dc9f9c686eff4
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:54:32 +0000

    yes in exploration difference reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
