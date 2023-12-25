3d44cd618b59a8771610ad1b80ab1440881b88d0
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:13:49 +0000

    copy new dark journey translate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
