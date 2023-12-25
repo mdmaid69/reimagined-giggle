11c329ed4db0e321c2ac5d4466bbfcc79a2bfb55
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:55:55 +0000

    appear actually shrink measure quick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
