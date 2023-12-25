6924c8c3fa774e1f19b2016acaa6ef3aedf2be26
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:03:37 +0000

    impressive neither value sad dare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
