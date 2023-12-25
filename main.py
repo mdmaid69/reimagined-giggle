ba2bdbbb513d4efdfa991d04ea2fb49d23d9554c
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:15:23 +0000

    forgive sit star car help

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
