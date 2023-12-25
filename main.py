7ee6459555378a7dc0184234f3ef852248d9edfa
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:20:49 +0000

    practise wall copy compare soak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_item(array, i):
+        return array[i]
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
