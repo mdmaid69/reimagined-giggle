8af695c72556c1f2afef7be2304ec965caa5f609
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:59:51 +0000

    waste belief office rare little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
