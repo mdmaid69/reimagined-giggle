61bcd76e095e458222e5035601ab523d67cef85b
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:55:56 +0000

    print admit catch part disgusting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-print(sum(range(10)))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+print(time.time())
