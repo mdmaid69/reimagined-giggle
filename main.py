c3117b58978b85e75dd573559689ac5603d92adf
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:16:14 +0000

    the road advice available last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def count_elements(lst):
+        return len(lst)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
