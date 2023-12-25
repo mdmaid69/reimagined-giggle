e64372d17d80bc7cb95da60a30f35b934202a62b
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:13:11 +0000

    phone question through say underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
