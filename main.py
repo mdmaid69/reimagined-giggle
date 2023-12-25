6e697415fe4c6f55b78cc841b85dac9b5712a3a9
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:53:25 +0000

    fold wander dish freedom open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
