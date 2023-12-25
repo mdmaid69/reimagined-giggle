620be1724ab847c177ad58c97fb250867f17842c
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:24:39 +0000

    unit quick run impressive opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
