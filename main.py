d49ad0de2570521446685a215851366489d27899
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:48:04 +0000

    win progress team scare manner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import os
+def remove_directory(path):
+        os.rmdir(path)
