2e6b3728668575d5a76b480a4d955b3b620549ba
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:41:08 +0000

    push tear excuse secret invite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
