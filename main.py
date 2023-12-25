fef755a0cd6bb21cd4f09733b577f3aa4703a6ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:01:33 +0000

    snow row steep behave least

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
