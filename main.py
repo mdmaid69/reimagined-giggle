8c1b1289037b960419f188fd5904ac506c816812
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:12:57 +0000

    everybody show Cc name exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
