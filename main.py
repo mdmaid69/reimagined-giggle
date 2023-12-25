48bebfa31cf1831a9432f5a99953f6ac4b57dccd
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:28:08 +0000

    turn listenfor whistle bush too

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import os
+print(os.getcwd())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
