68d9b5b10191b682fff2dcc675da776dcd6fd245
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:22:57 +0000

    guide content turn admire theirs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
