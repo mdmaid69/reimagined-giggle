bffb3e3d93b804624ded67451e9b678ec6b0e0a3
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:14:19 +0000

    card limit go spring still

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
