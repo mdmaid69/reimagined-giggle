2db71d3cad54897a8a8d72a8f798b3a511169d45
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:13:17 +0000

    weekend state offer have worst

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
