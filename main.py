cc8006700b80a184db935a9768501410a3744707
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:28:22 +0000

    neck live boring marry Cc

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
