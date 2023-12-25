bd02f6a3c200ad9fa139df6f0ef5698f447351c6
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:03:09 +0000

    walk unable 564encourage no bored

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import time
+def get_time_since_epoch():
+        return time.time()
