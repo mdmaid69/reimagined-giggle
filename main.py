97b839063dabd42b20733c6a55477d4f57386862
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:23:36 +0000

    sink bend brain blow shop

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+print("Hello, world!")
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
