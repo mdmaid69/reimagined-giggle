281b3c1211d86b56964e7533267b0e1294e91d8a
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:40:05 +0000

    kind true clever kind comeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def get_array_as_list(array):
+        return list(array)
