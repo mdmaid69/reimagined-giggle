c264dad0d9d164c58fe256981830498d857590e9
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:52:56 +0000

    deal so money mistake come

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
