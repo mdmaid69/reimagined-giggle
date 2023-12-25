f99b66e60a5bf70b6fc5f88fee4bdd24754f3a84
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:35:27 +0000

    bother evidence open violence mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def is_even(n):
-        return n % 2 == 0
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import math
+def calculate_floor(x):
+        return math.floor(x)
