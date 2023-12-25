dc22f9f79f681037f07b4c1eeac83470b3eb3fe1
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:29:29 +0000

    popular mystery search appearance guide

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
