3b7df37f32e8d1648091b5c3fe53bb50587e10c5
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:10:59 +0000

    573entertainment different pleased world about

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_user_list():
+        return collections.UserList()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
