5040934a97bfadebdccac5052c2ce49c012a325e
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:48:02 +0000

    impatient exhibition upwards present design

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
