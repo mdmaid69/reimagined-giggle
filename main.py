8dc1ef039ae4cbf42f9f3b0ea2a8b12f2ea88240
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:28:30 +0000

    free claim office raise object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_user_dict():
+        return collections.UserDict()
