5dd291ed30797bce71952b5e2c76f1bfd2635dd9
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:27:23 +0000

    music supply today refer trial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def get_array_as_tuple(array):
+        return tuple(array)
+def count_elements(lst):
+        return len(lst)
