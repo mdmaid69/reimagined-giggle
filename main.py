978367127d7fb2664484f95fefb7cb4b9b83a1c2
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:20:45 +0000

    sensible godown reject anywayanyhow display

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
