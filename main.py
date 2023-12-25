a13d3362e933cfb19c3b90b88895c4cd0bbb7551
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:12:46 +0000

    help turnup for survive picture

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
