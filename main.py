29f4489c3b879c84c4253716e76534e8b08b45c5
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:14:23 +0000

    together do exploration appear instead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def cube_number(x):
-        return x**3
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
