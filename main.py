c28d5d081e39bff87c2895d10560f41c1d3d0813
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:49:45 +0000

    lunch total breakdown image giveup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
