cf4a6de1ab245f6715b6a7967c5000b390dfb0b3
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:44:59 +0000

    say love typical kind chin

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
