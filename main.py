d0fa457603ca1f7ab6e90e557df5886fbaba0aab
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:40:58 +0000

    detail justice settle drop comedown

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def add_numbers(x, y):
-        return x + y
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
