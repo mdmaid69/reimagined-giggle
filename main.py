ff3ffe44103302afdec8f8064af35cde8aa26437
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:08:08 +0000

    smell plain conclusion sail light

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-print(re.match("h.*o", "hello world"))
-import collections
-def create_user_string():
-        return collections.UserString()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
