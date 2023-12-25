14cd40f69566f53b5117b409850b078acdeec7e2
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:34:18 +0000

    fire special behaviour nervous negative

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import numpy as np
+print(np.array([1, 2, 3]))
