e76e1f70748163da263af9a5d5543373b87988ce
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:02:58 +0000

    mainly sell church trade patience

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def square_number(x):
+        return x**2
