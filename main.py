413fdae95c7eaf8bba372a3678918d624f7f3c2f
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:26:58 +0000

    cope coffee smell phone model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
