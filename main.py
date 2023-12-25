15bb28ee4ab58de3d83e7d5c41bab09235aa3ab7
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:12:54 +0000

    sport each control talk home

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
