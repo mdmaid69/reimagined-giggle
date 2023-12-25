e7f274f43bb9ba6feae2be01a25460dab74863f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:37:20 +0000

    light complain party certainly comeup

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
