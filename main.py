04110502d72d4c66fb30e232ef86271fb735c5b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:38:22 +0000

    discover gradually rank preparation please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
