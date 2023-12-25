114c263303fbf14dfac49f2c59dcc8fe440fd3b0
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:32:03 +0000

    violent suck wing steal bad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def get_current_date():
+        return datetime.date.today()
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
