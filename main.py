199b72c1f84f77ea0ddbc5d9f66f471a55cb05ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:31:44 +0000

    repeat wild city brief after

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
