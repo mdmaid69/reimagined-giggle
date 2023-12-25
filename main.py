46fad6ba604a5d67bf2dbae8aeca105360cb5df1
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:55:10 +0000

    drink regularly everywhere either drop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sklearn.datasets
 print(sklearn.datasets.load_iris())
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
