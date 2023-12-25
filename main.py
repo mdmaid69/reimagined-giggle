23279d612894d34d169c0a979bbaaa4aa35304c9
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:46:18 +0000

    crazy apply not dear extremely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def is_palindrome(s):
+        return s == s[::-1]
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
