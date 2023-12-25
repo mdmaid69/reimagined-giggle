56f42cc12dc2affe064bc566aa14208b32e45d60
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:23:57 +0000

    urgent fear pen characteristic house

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
