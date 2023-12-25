335d578ac213a5ff5c3826ab9fb3d0612a3321dd
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:40:07 +0000

    detail shadow stamp public spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
