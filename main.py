b4ab2cb50eaf1fa670530c02861691c1a2fd3f28
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:52:07 +0000

    pressure his prevent across produce

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print([x**2 for x in range(10)])
