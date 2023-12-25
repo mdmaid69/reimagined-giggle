e0be9c2684730edebe5c6a5dac6fa054d338f54d
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:28:11 +0000

    taste declaration speech kind adjective

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
