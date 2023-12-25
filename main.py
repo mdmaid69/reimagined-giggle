0dde618decb600bfdc2b594aea4847649e41f9f9
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:42:22 +0000

    lower nor catch recommend far

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
