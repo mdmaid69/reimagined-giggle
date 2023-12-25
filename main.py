4e41c52c6bef25dd341d0e19e46f8ead9d5e2628
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:06:04 +0000

    return voice influence mean sensible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_hex(n):
+        return hex(n)
