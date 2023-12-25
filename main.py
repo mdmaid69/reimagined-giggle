3a0e33558fd1e48c1d6f8c73c14bbe14f69e73b9
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:41:04 +0000

    regularly deep cut prize entrance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import array
+def get_array_as_set(array):
+        return set(array)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
