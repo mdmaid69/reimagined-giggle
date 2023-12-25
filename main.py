9d4b798cfb9214537b1c91dd5c61100b3f0eb36a
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:19:24 +0000

    godown television muscle always sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
