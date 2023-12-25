5dcc79ce9dd4df4a9a86d260e1683927778be9ef
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:59:18 +0000

    pause connected seem explain exchange

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
