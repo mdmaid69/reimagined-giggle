bdb9dc19a4a40303125c28fe24a4e11619e83164
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:41:48 +0000

    someone subject patience mistake tour

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
