94215332fa79ad6c552d3fd7bf4c402da8fe5397
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:21:45 +0000

    Pp personal whoever invitation anxious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
