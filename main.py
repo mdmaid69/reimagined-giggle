f30a0d240115409a28d468b2e82adfc3e230463b
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:35:41 +0000

    smooth foreign below breakinto neck

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
