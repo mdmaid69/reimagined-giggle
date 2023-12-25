c64102838b5ff255f07bee9e7c00ad49e216cadd
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:31:56 +0000

    injury road refer break only

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
