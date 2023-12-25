e86f569a12d285628e753c70b6a913f662c71d12
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:13:34 +0000

    flat whether rough raise family

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
