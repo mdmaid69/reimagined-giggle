7046396a1052caf4c4b79ed0eb9ba04f41ed6b20
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:04:26 +0000

    wind control pair over factory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
