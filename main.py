3ef1d7c9ccaa78934ed0260319a84fa176da4dd3
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:03:26 +0000

    shine progress form refuse test

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
