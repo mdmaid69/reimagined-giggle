4f84eccfd22abd0ea54d109721651af8ff65fb21
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:19:47 +0000

    family turnup miss exam trip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
