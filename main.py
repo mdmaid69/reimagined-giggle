5bde511038f1512db9abdcbbfc8409677440032a
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:50:33 +0000

    memory tool ready long affect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
