291aa2ca36374fd0ad8b277b6ac4a1c7892f78ca
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:23:34 +0000

    communication problem season improvement attractive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def reverse_list(lst):
+        return lst[::-1]
