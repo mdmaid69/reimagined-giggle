4978336884bef5e46582b7c0a74858ac5a6cf568
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:03:20 +0000

    give imagination definite ear anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
