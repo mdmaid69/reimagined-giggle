60ddf94f66c45ccae91785e789eb56d78be5f09d
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:36:24 +0000

    yours substance platform temperature advanced

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
