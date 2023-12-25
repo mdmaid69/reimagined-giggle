1056b9158a2388e70a57bfe79d6dd14ef7db901f
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:23:13 +0000

    tired circumstance punishment out material

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_current_working_directory():
+        return os.getcwd()
