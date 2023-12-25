2ded131f41c77a30f61a6b8334d1828800338dec
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:10:07 +0000

    charge get summer best rush

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import sys
-def print_python_version():
-        print(sys.version)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
