df123d596cf82fa09d304eb586c7eba2b1b5fd33
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:35:46 +0000

    free defend no extremely immediate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
