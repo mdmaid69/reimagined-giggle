966c83932a31e9d16992e703ee21b76735995205
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:45:38 +0000

    die lesson previous kind blow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_density(mass, volume):
+        return mass / volume
