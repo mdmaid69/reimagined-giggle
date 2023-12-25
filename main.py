bed9d34e5a37d0c08e6400765b3197861015dc47
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:46:32 +0000

    from ready port somewhere slow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
