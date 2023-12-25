0867b049028350685c52b5302db365011d78857f
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:20:29 +0000

    we smell present question but

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def cube_number(x):
-        return x**3
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+for i in range(5):
+        print(i)
