287ce7c37ae4bcd1ae628e2a8b34cf43e4363235
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:58:52 +0000

    pencil marry connection basically brown

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-print(sum(range(10)))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
