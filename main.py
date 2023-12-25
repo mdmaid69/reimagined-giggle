8cd129725cd92006d6c7698e6e76d46c897cd646
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:38:00 +0000

    afternoon city split excuse fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
