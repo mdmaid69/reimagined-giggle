ec5025d0ea09b4ead37aa0459cd82a1107b48e1c
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:25:04 +0000

    each so circle already study

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
