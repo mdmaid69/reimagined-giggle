f614f4d3f87b5d007670385922192e08f6f38ccb
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:07:16 +0000

    570enjoyable sure authority either neck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
