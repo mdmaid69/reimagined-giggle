c3af9beb015c01e3e250e4361a49fbe40ba990db
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:42:11 +0000

    own depend nigga workon shade

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_density(mass, volume):
+        return mass / volume
