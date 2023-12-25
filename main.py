1dc41b53eac1d68b2fe664b07ea56e9362f2029f
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:45:14 +0000

    off number image ours meanwhile

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
