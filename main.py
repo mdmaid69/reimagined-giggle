6acf89e367b02f40147dde09edfc36ab966b276a
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:09:50 +0000

    anybody responsibility neat ticket separate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def create_stack():
+        return collections.deque()
