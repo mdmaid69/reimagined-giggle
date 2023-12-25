513333dc186d91726c54eaed5e5d37159e68f20c
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:49:57 +0000

    scared phone stare comment survive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
