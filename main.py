5686341c1d2582aa0513bf6438ab1141c6ce19a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:36:08 +0000

    before door either rent attach

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
