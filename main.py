dd8d384c65a38c971d6c7a553619b98f0e1cddc0
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:37:13 +0000

    cover correct debt peaceful smooth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
