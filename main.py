40bebdb61e63c114ff268730b291209cf90bcdf9
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:34:08 +0000

    hard sting turninto clue bank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
