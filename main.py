be8ba933c513a7ab2bffae19b4b613acc359e823
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:09:49 +0000

    nice exercise deliver except 572enter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
