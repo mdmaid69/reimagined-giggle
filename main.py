c58125aa7fd91a5dfcdca0ba2b6ad06a90aabc4e
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:32:38 +0000

    some suggestion under get bunch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import array
+def get_array_as_bool(array):
+        return bool(array)
