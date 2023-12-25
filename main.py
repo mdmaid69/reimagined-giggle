86abfa83ae06996d9a1209678969286a180f80bb
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:24:45 +0000

    during offer exit half amount

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from collections import Counter
-print(Counter("hello world"))
-  def reverse_list(lst):
-        return lst[::-1]
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
