0f2ddb41f6f3e0875ec1da3ce9f31142a5c47fd9
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:03:26 +0000

    bet takeover dear another tough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
