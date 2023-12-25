44e994b443d356ba497af5930d2186421adb09d4
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:58:15 +0000

    angry escape violence reduction wise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_acceleration(speed, time):
+        return speed / time
