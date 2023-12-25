584395d5f94dca02fe6aad92d511de5c7ad69284
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:13:26 +0000

    pronounce repair beginning translate little

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
