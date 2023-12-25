8a6a260aabff00c2fb99c6cbfb13a5bc00c5f8e3
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:51:36 +0000

    bath relationship surface bored thankyou

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-print([x**2 for x in range(10)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
