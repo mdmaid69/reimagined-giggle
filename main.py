372837c04f7c4d68c65a3710382ece720c00d9b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:17:14 +0000

    store correction any whisper swimming

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
