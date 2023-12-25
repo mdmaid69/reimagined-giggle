5c992aaee216f6bf28e1ae15e5c117b55f47e496
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:22:48 +0000

    settle care complete anybody bye

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
