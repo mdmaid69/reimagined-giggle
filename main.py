529577b1f0308230e912ced26483d644f37d66bf
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:55:39 +0000

    makeinto seat side properly surprising

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def convert_to_hex(n):
-        return hex(n)
+  import sys
+  def get_python_version():
+        return sys.version
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
