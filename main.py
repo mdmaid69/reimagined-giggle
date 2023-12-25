f73d63ee5fec76c67e5dc4d06ac69bb9e77a98ac
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:08:36 +0000

    excuse rush my emphasis change

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-for i in range(10): print(i)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
