10edccc7ae0661697b2e7e5d092a73027aa89443
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:17:24 +0000

    pronunciation rush this contents Ee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
