556e26270d35928430a045972bed4c78ca516cea
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:37:52 +0000

    temper wide surprise point surprised

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
