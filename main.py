3e5a4fc10139001d80558a3c145eb56ef1d9f89d
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:06:35 +0000

    exhibition remove believe partner sad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
