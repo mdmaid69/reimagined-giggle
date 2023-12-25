90190d6a7a83882ad949fc3f6b6f09e316c3a4e3
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:17:05 +0000

    new whisper debt genuine dick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
