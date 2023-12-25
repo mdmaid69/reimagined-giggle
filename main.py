1060f976e80686821193fb5f153dbe061f12ee4c
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:36:38 +0000

    fat high makeup harm curious

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_power(work, time):
+        return work / time
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
