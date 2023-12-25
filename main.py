2fcb305f7da3b4d5d167a8a356275170a119602e
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:51:13 +0000

    excuse insist Qq Itself opportunity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_power(work, time):
-        return work / time
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
