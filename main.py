76147430b9ab2c0b1cac2e5c30f35d5396fcad99
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:35:28 +0000

    concentration repair difficult show effort

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
