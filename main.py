cfce04a3d2f543fab15804bd4922a6e8cdd81c05
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:51:48 +0000

    achievement advantage everything himself congratulate

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
