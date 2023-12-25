75f5be3902b7084246562867829fd85420cda14e
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:33:14 +0000

    make crime cause damp shame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
