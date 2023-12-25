7f2f5bf5b7521f3331c7e562050725474f21cbca
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:26:53 +0000

    destroy responsibility own picture out

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
+  def remove_duplicates(lst):
         return list(set(lst))
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
