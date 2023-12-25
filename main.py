cdda9b8e63c9ee1ca5c9f96200db9a6f3c5fd163
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:39:41 +0000

    takeout kind press juice join

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def square_number(x):
+        return x**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
