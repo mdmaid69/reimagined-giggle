337e7b44393c15cfbc82769f2a0a4e2e53b4f463
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:38:28 +0000

    prepare mad discuss personal male

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
