efc0929ebd46e9500d77433dfc91063c4cd36a69
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:43:13 +0000

    principle flood patient prove bringback

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
