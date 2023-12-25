7e2656e0a771739b7cd83e79130daee4591643a2
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:28:40 +0000

    needle turninto idea offend doubt

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
