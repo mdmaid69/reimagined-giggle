cfcfd72145434f3f565fa4e482479dcb7bf86ace
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:31:28 +0000

    coat could whatever cost light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
