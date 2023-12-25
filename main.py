08c88e031ac3233291ea4261630484597334cf81
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:58:36 +0000

    explain up downstairs fold offend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
