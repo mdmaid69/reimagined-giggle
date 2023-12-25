4599a23433ac44e23a492f0233fd81b849a0ad57
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:31:05 +0000

    temperature pick stupid stop bush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def find_unique_words(sentence):
+        return set(sentence.split())
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
