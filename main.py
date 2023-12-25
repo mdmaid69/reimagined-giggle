9b2137c82f5ae6f0178d039d95d034c06cfbdf6a
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:58:45 +0000

    wall illness lock love 568energy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import sys
-def exit_program():
-        sys.exit()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
