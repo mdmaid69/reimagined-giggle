f0e73f54d3b19dda28fba5c3b8a7467fc668531d
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:00:52 +0000

    shame hour afford smile represent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import logging
-def log_message(message):
-        logging.info(message)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
