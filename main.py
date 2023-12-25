f4d3fb11d99bfaca0fd17e7190c278d8189a0a30
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:12:13 +0000

    track genuine share gentle Qq

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
