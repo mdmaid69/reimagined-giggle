50ddd7b93972ec596d0d6f1ddbe8a0f108643363
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:01:27 +0000

    command cheat dig on project

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+def count_words(sentence):
+        return len(sentence.split())
