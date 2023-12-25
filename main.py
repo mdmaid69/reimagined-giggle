bc10e7a1bc89b9fb03a0acf52b92435de0f80daf
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:23:46 +0000

    reaction simple breakup every wander

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-for i in range(5):
-        print(i)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  def square_number(x):
+        return x**2
