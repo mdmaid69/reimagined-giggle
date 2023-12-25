d30ca87b3b3acdd0afb8dd3a7d87097760f8b37a
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:03:47 +0000

    use treat design minute keepaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
