4a6519bebe880297ace77c82e6c4698d1210af3b
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:12:05 +0000

    lost suppose exactly perform tread

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
