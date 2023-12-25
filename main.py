1e7f01fd88446a97ad203e719e34ab0a7ea3b98e
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:54:50 +0000

    568energy split condition niggaacademy at

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_density(mass, volume):
+        return mass / volume
