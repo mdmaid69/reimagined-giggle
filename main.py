e782280c282b88ef4a44f071ec4bcd89bbbea5be
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:51:19 +0000

    business laugh turnoff lock say

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def is_even(n):
+        return n % 2 == 0
