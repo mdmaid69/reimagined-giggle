68dac0786e273c2ecc05906dfa451b37137d0118
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:49:12 +0000

    theirs violence ignore how type

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
