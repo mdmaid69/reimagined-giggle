b50ab970df3b2dbe0e133f47de017b5734313ba2
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:28:39 +0000

    free apparently main Ee getaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
