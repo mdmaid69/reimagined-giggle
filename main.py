8be11389c67dcff3cd37db737a2b68e0cf74a89f
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:00:33 +0000

    something instant quit Pp consult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def greet(name):
+        print(f"Hello, {name}!")
