55fe38a0a45aa67688b57cf6d32ea69bacf885c1
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:15:40 +0000

    except fill wet goon suggest

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def is_even(n):
+        return n % 2 == 0
+  def count_elements(lst):
+        return len(lst)
