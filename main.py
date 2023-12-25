0a7dea84e4b6e313db10353cc408c0c27dd3f586
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:55:40 +0000

    godown cover charge quarter task

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def square_number(x):
+        return x**2
+import array
+def get_array_length(array):
+        return len(array)
