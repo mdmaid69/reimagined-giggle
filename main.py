2a3367d64328c3f2b04d2530a53917ef24457124
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:32:45 +0000

    before eye trip regular noise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
