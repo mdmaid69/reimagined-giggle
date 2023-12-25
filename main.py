85fa9124e1cfb7d8084c49e7540505332d82a07c
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:11:48 +0000

    strange slowly handle keen upset

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def convert_array_to_list(array):
+        return array.tolist()
