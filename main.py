ba8efb1abe9d7beceb14d1a3d4b48ead9d090081
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:01:38 +0000

    embarrassed fortunate grow circle insist

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def append_to_array(array, item):
+        array.append(item)
