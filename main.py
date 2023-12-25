80c733f21897c841bb4ccfb427682f01fbd3e6d8
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:45:48 +0000

    high sea imagination surface life

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
