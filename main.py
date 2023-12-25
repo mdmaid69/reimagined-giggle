06367f896585a9c762ad4e92d2247aa3b6f428a8
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:47:18 +0000

    freedom necessary bang change free

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
