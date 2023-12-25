bf807a3c8300bda12f5dac7cc0c5b413b1b526d5
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:13:37 +0000

    disgusting love statement prepare pair

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
