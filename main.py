512025b05fc222bd7499efd932b368f106768c03
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:13:34 +0000

    excellent morning earth least Hh

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def remove_duplicates(lst):
+        return list(set(lst))
