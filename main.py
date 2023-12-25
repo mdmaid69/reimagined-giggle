d58b713c7a8a50a6848f46244c7cef63c0841b0b
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:24:44 +0000

    borrow soft damp force relax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
