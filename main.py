cf7ca14fcb785c1d73ecd69fa68d27f39a93a980
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:51:21 +0000

    have one claim such characteristic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
