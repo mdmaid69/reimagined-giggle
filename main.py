af04163341d1acdf12e4b26bd805ebb76d856474
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:25:23 +0000

    because race cover tie plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_bool(array):
+        return bool(array)
+def find_min(lst):
+        return min(lst)
