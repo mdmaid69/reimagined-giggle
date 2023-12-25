70869a45915de071b4032ac6dcfcaecd49169209
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:49:38 +0000

    famous board any who godown

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
