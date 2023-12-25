2551f804d910efa4cb04e60cd879ccaaf37b7e90
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:23:01 +0000

    city travel rent catch gate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def reverse_list(lst):
+        return lst[::-1]
