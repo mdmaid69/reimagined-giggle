1017fbc9311c235dc29a7f6bf84c7078b3954873
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:43:54 +0000

    especially grand sense whether single

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
