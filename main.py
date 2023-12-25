37c3c72151b3c2fff774c7904fedb4989abc268d
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:30:11 +0000

    breakfast believe beautiful whoever fortunately

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
