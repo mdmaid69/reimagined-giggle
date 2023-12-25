d051e5a29504f203a75052c2ee7044313846f667
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:37:36 +0000

    plan concerned subject hang pot

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
