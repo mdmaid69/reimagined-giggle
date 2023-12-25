d931edde186c8dd5a8175eeb5880e29ed7829208
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:28:32 +0000

    badly reserve fall unlikely game

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import collections
+def create_queue():
+        return collections.deque()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
