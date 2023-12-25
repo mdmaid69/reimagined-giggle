62fe3f8d5591db7b1a70767f7d5b10672c31dbab
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:03:35 +0000

    bottom patience crash value lonely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
