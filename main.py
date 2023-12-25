3d3f428fe5b4f3327f3ecae11422d9747c24b991
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:19:17 +0000

    warm second doubt new night

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
