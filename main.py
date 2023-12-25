7ab656e06f9690ab35f057413edba9e9c25c792e
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:24:39 +0000

    town proper along love whichever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
