1359ae91704622973713a4ab8f7d8cdafcaf3f06
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:29:11 +0000

    mystery smell pick chin fashion

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+for i in range(10): print(i)
