2eb4ce249ae53fa3d3987c2a6177efbdb0234891
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:39:36 +0000

    interesting thirsty pay thin bend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
