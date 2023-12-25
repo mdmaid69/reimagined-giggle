c27fedd91f077004a314c1a11d071fb99f716207
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:52:49 +0000

    shut properly load public soon

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def subtract_numbers(x, y):
+        return x - y
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
