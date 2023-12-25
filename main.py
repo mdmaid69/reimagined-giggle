e36d4b1ed304049b701c876379027d5b21d73777
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:47:40 +0000

    immediate teach anything unless work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
