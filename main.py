cff24ed6b62c9ebd82e21401a156c7887465a168
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:31:12 +0000

    illness angry find claim remember

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
