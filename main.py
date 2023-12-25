24813c733d28eb65ff320976ae7ebeb8e2aee55b
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:04:27 +0000

    important rush train personal once

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def find_min(lst):
+        return min(lst)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
