546ab5910f06953b8850d626e2512646bcb3c81b
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:38:36 +0000

    offer takeback face basically hair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
