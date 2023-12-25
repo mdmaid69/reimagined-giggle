48f409bba7d453c3d8d52b2f43b1166bcaed8190
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:19:43 +0000

    rubbish cut evening dick square

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
