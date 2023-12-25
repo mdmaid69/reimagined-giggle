bfe28c31239f1089cf9212c7a84259f6fca916f0
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:33:36 +0000

    sort centre right sell concern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
