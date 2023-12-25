530cfd295b399233bdff1d1c5e1aabe3ceda4626
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:34:50 +0000

    all offend angry mail explanation

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
