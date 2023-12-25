3752f28d941f89215e1135aa4ba31bd91a281a72
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:39:43 +0000

    eye connected risk now decide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
