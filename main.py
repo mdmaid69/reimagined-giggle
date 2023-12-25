d23b4b0b625f1b360f3bc4ee852fb5a8eebcbf0e
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:41:15 +0000

    stone die platform service fat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
