46b2bb06b8eb33d01989e60257818d11fa140f33
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:59:04 +0000

    pull trial urgent object comment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
