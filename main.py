7f657a957bb1a03e10f5b34d94c6d6f96f458536
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:49:49 +0000

    stiff strength smoke formal fit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
