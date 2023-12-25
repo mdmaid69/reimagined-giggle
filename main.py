1a2d1af67dc7b91073436c0d2f2889706eb2d3ba
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:13:23 +0000

    side bite snow run this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
