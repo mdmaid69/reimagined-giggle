f88083097a8c48131215adf17e003dda857ac4a1
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:40:24 +0000

    eye cook shut giveaway comment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
