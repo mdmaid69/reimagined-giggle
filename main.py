9565fb06d11344d225a54f1d54c1ba8d9d2a90a7
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:23:51 +0000

    almost recovery represent comedown odd

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
