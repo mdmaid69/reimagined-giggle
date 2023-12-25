18243784a1a82689cc83f3b67d5d8730485a99b6
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:01:44 +0000

    loss tidy her mind pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import time
+def get_current_time():
+        return time.time()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
