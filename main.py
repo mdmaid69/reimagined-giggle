9a3d488647d9ebb71c4df2f33d78bb6701458b19
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:30:52 +0000

    book likely easy pay shine

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
