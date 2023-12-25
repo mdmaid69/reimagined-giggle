ac7e7b846d434bb1a27e5c18afc9f3adffb79f30
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:47:02 +0000

    sleep confident fly winter female

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
