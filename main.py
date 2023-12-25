ddcccd9cdbc747dca7f970a409e439e2271d31d5
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:21:36 +0000

    Dd occur substance fight unlucky

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
