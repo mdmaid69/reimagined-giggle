595dd2beb869c2fbbf8c7c4808ab7a2b080e60e8
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:58:07 +0000

    load allow connected dance unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import collections
+def create_priority_queue():
+        return collections.deque()
+import array
+def get_list_from_array(array):
+        return array.tolist()
