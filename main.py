cb6b06b744fe2b52369865462bafad1ccccfc411
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:26:00 +0000

    cry talk meanwhile they own

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
