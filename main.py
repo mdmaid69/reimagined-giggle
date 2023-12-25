55b9561d46fa52917b5ba4993f30c05e18bc56a3
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:02:19 +0000

    kind pinch offer bother science

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_average(lst):
+        return sum(lst) / len(lst)
