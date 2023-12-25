8e14f181930a90d4827e98011d94f3a20b8037a3
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:22:40 +0000

    interfere border sound want exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import sys
+def exit_program():
+        sys.exit()
