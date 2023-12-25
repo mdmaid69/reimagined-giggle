8fca02db9ad8eb0b24a0f647cfe2654060441b2c
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:17:45 +0000

    clothes walk closed study approval

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
