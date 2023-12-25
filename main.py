ed571d0fabbd00218df6ab1196d4a12936a3f8bb
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:17:10 +0000

    woman hold holdup your ring

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
