4fd20c99e2c3edf3e6c3146ef7157be0c4c9d6fb
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:02:59 +0000

    mean bath run rest confident

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
