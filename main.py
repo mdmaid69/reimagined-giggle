4e0528ed4dd01f959b28b8c6efab9564088a3807
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:08:40 +0000

    set tie sticky takeback slowly

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
