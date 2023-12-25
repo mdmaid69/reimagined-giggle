fc9ec93e579c3c6c6d5da303a8eea05e70e2fc70
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:25:04 +0000

    lost cost first total summer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
