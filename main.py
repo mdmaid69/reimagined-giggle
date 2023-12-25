389cc048e18e208dab7c6417cc56841650439a62
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:58:29 +0000

    stay cause them organize suggest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import platform
+def get_os_info():
+        return platform.uname()
