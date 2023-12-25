ae02364aa2cb48cd25d291d83d33e26482f81ea1
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:59:44 +0000

    mistake wish gap work limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Characters:", len(text))
