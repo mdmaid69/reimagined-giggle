95984a84d67823e66adf7deabd3b7bba42eec190
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:37:12 +0000

    takeoff extra leg smooth contribute

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
