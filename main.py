8721f27a7d07e3959692cdb4d183f3a122227d6e
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:31:24 +0000

    comfortable already unusual use attention

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
