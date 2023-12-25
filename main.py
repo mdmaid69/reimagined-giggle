a11fbde7cc5f5dbfeaa2a05f6ba0aa897075d5c9
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:43:15 +0000

    perform strict thin often complaint

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
