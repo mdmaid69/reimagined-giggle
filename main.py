a0c2303945ecb2945a842a9a7e8815c986d8bcfa
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:10:29 +0000

    fall burn wind better waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
