154c185fb6b4616d045b35f6f03ad2827b94250e
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:59:00 +0000

    play avoid instrument pretend pencil

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
