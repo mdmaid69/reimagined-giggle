cdf212b6615e767298fd801b2a3fde92c810089b
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:07:35 +0000

    plate perfect ride gift desperate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
