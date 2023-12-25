bda5cda2ed362fef830c6f212a959eaac05e39ca
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:32:15 +0000

    call total protest bound interested

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_list_from_array(array):
+        return array.tolist()
