3f2fce739566dae6b4d6d1ffea23fcdb75471b13
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:19:19 +0000

    famous plate deal dress sore

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
