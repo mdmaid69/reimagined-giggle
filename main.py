3a9251688685750fc2c95eecf928b2ac36df3d70
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:42:50 +0000

    force point plenty period concentrate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
