ab6462d558ee7814778d4e64ed310e1ae3cb9e2f
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:28:16 +0000

    left glad opponent means whenever

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
