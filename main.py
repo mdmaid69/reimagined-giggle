280b4dfb58c78c0ae599de56d7b0770fa2183bed
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:59:22 +0000

    thin ask control poison safe

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def insert_into_array(array, i, item):
+        array.insert(i, item)
