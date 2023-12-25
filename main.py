22037675b9162282e732ce61dc5ee373b7d36d40
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:28:59 +0000

    shoot creature them noone impressed

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_area_rectangle(l, w):
+        return l * w
+def count_elements(lst):
+        return len(lst)
