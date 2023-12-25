221a3c07be49d7ee2912c2e8c372019df917a503
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:36:24 +0000

    in dress later inform leave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
