f0d4c20ecec8cb22653d33aac0476fda35d12378
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:24:26 +0000

    pleasant unlucky prize difficulty firm

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-for i in range(10): print(i)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_list_from_array(array):
+        return array.tolist()
