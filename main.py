a073faae7da0c54ecc60e7ddd5336d4cd99690d2
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:34:40 +0000

    seriously speech calloff worth once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
