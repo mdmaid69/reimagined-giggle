a0de9fc9d531c993671315db5ac07a31046ee6e8
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:53:41 +0000

    familiar amount occasion copy correct

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
