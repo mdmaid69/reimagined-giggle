bcb0acb2839e7d300593571d62532bfecbd32c01
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:28:39 +0000

    black dream aim pronunciation marry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
