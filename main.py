999d9a4ceff389cf5d41bd768b526b5bf4614532
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:36:56 +0000

    feature performance inform experience history

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-for i in range(5):
-        print(i)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def count_words(sentence):
+        return len(sentence.split())
