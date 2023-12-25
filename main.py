8186650adf8885aca4324f6887662dc865f50b96
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:07:07 +0000

    connected switch seriously many week

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import sys
+print(sys.version)
