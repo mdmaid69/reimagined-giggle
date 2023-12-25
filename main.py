e45a89b803dbe9bf69f11bfcddc34d70f5fe138d
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:01:38 +0000

    street away after combination matter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
