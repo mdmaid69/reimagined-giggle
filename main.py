510a04cddd3f64c2a93902795aa1a88cea9f5c48
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:05:56 +0000

    break mention busy male case

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
