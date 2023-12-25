c8e323da8a239c9ad2bab33a64c0b196d89d684e
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:06:34 +0000

    waste tip other admit eager

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
