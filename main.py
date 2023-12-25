4180f7132c26fdd2bc7186348e373c125f329a2b
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:26:47 +0000

    free indeed mood ear test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_density(mass, volume):
+        return mass / volume
