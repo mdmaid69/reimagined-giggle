f9eb44dcea5ef0055786b37dc9a5774ad353aea3
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:08:00 +0000

    agreement help his pleasure moral

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
