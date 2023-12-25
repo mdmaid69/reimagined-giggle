241c42836a9c4a927842ee5f44300ecf25c8f2f3
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:38:37 +0000

    long room swim true listenfor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_octal(n):
+        return oct(n)
