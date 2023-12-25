40bc1989df98ed22947de03f86babe71b7ab55bc
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:31:06 +0000

    burn impression secret habit flow

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
