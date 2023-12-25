a1030c581b2968398a660fbaaa6ee805f3a72541
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:05:24 +0000

    discovery else rent close embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
