1e2a001417483b2c64cd9f588df48642e35fa387
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:43:21 +0000

    church telephone small whom release

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
