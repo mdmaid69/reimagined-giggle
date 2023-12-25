6e357575a2ee968604f539dcc27601ffcd5414c6
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:30:18 +0000

    polite certain extraordinary because raise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import sys
-def print_python_version():
-        return sys.version
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
