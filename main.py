483030efcf1bbd2778ad334617b89501ea22292d
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:59:24 +0000

    wonder while no wait wild

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
