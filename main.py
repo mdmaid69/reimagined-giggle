10356e889118679150ca15e9a480febe407d09a5
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:46:54 +0000

    swimming by for go clever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_repr(array):
+        return repr(array)
 import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+print(list(itertools.permutations([1, 2, 3])))
