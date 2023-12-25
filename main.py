4580dffc229fc98cfac04abc75a82610dc91d43d
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:28:04 +0000

    interfere rank storm easy dangerous

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def add_numbers(x, y):
+        return x + y
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
