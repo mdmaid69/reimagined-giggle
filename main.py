c109ecbff5795fa718c57f643f32e0efa6a652f6
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:46:03 +0000

    face care thorough rub main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
