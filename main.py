952b300a8e68ce3a9f8887466260f74faf25010e
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:46:55 +0000

    promise impressed high adjective satisfied

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
