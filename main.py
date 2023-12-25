91f45845c496ac6a42a0b9fcd831c19456d3c921
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:11:57 +0000

    present guess alive hungry study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
