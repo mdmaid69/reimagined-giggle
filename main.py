762c75edf0adac7c1bcb830abb3a4fa0431d22db
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:36:41 +0000

    action advantage private claim so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  def convert_to_octal(n):
-        return oct(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
