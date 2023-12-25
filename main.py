298fc61f3a0225c20553dbb54dbfa6907edc8edc
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:53:42 +0000

    favourite considering go keepaway spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
