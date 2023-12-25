01a8173596d683017ec5c73474485430540cb3a1
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:08:21 +0000

    unhappy regard blind pipe convenient

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-print(sum(range(10)))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
