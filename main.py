36dd0e0b0efcdf85c503e8f0e217cf6ae9bc3f43
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:19:08 +0000

    ability match walk copy exam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
