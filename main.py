3c7100b1483598c42d10334c592d9358b697fcb1
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:11:17 +0000

    well glass average comedown one

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_min(lst):
+        return min(lst)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def get_array_as_bytearray(array):
+        return bytearray(array)
