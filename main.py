389a6a1c2937fef7fafdd0b193a15f45ca05ec9d
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:33:47 +0000

    him noone hardly responsible nature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  def is_odd(n):
-        return n % 2 != 0
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
