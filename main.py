b07faf568739258f47c772d3a1d6c70dfc4e8867
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:04:32 +0000

    buy complicated last tradition opposite

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_work(force, distance):
-        return force * distance
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
