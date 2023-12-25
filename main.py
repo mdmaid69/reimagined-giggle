0b92c986ab57a20136b6f7e165244d2bb66f2f0f
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:00:56 +0000

    single responsibility name strong personal

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
