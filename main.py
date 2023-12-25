9b9bc835b70fbbbc5dc85380117246c039305881
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:12:42 +0000

    clean teach limit crash beautiful

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
