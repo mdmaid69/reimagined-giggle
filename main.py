4748eba61106ca43a8ea9a9df1726fd3401b26c7
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:02:02 +0000

    mass little simple declaration comeback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
