46a9b733c1bd0dab18937c8fdb920b71fba821f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:19:18 +0000

    regular godown familiar broken healthy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks_allocated(file_name):
         return os.stat(file_name).st_blocks
-def calculate_acceleration(speed, time):
-        return speed / time
