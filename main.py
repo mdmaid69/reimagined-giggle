a9432afad71c43146d32333b3c874095efd9fb03
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:20:06 +0000

    country throw front comfortable development

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
