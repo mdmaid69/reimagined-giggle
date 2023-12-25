dda2c8f0b15ee7141a485e88723f879775447a59
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:30:39 +0000

    square tax trust trust hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
