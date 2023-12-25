6a275773809cf6dc0d8a46030f0547441b7f7c4f
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:45:01 +0000

    believe thankyou here address both

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
