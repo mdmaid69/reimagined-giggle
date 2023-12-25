a1aa233009c66d1247b32484f1de20bc27a0e858
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:20:48 +0000

    market develop refuse near wear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
