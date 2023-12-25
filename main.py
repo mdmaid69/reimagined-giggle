e29dcfd0eb3301ae3ccc6829516fe032f7a06718
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:58:02 +0000

    maintain mean beside exactly Ww

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
