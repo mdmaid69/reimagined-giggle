54a66fb23d174ee26549d1cfbdd694635a5bde99
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:06:04 +0000

    interrupt die advanced talkoutof change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
