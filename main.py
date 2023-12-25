d61c7c1510018d5b7b1a5a3d58971d4537ada937
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:13:42 +0000

    check before Zz wind anybody

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_force(mass, acceleration):
+        return mass * acceleration
