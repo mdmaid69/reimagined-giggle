b9fdcc8c83423101a8cc0b3806bf50a8bc970d9e
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:35:18 +0000

    no before block question extra

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
