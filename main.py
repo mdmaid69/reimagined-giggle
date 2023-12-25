9a2c49f54b93b717a76f7199059169fa4cb62dee
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:47:38 +0000

    very earth reach boat harm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def is_palindrome(s):
+        return s == s[::-1]
