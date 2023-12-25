bca51f56b94c634ca513fbec6422fc8d07726001
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:58:26 +0000

    build load dance platform relative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def find_min(lst):
+        return min(lst)
