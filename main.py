45fd1709a9231ccb0caf8f4c4ce263e71355733d
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:58:36 +0000

    type ago influence represent tidy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
