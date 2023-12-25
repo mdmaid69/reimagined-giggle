35b618cbf1380caf2b020ec74e7ca09bb03b9cb7
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:04:04 +0000

    detail correct shopping range do

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
