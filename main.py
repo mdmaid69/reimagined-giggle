f88933d517b0aa4f42b03d7ec7c2123607401c07
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:44:43 +0000

    before explain plenty key split

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import re
-print(re.match("h.*o", "hello world"))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import time
+print(time.time())
