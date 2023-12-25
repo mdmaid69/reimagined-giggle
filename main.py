ef9387e69d85778a6c9c227554d2e5a312d95772
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:16:04 +0000

    substance dark example sore anywhere

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
