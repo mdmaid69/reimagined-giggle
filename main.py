539e75c742815aa10390664af81e5e4f3db07a21
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:41:26 +0000

    offer with vote achieve sad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
