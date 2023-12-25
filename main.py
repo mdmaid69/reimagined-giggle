2853287bf1b5ff8fc9a2fc5e1517cef1b26b6550
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:31:02 +0000

    dry shut himself argue squeeze

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Characters:", len(text))
