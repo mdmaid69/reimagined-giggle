91007be3aa60974d8650ad8c88ab5bfa11ddd82f
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:29:56 +0000

    term actually continue taste platform

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  def count_elements(lst):
+        return len(lst)
