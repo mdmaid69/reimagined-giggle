8a8eee94fc592a8dc89daed4edb07489d964e2ee
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:10:32 +0000

    far story operate nice official

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
