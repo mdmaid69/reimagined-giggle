b1b07ac0fa9aa995119bea2646a5cb7f623e3025
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:30:50 +0000

    Yy church cook flood difference

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
