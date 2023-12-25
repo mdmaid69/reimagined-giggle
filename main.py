41bd0aabf1d7884dd7477ec0cd98b3e81d2b0bf5
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:13:00 +0000

    machine future sick natural air

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
