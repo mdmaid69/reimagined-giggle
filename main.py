3aeeefeddf22571edfdd945505cb40a34a4aeeee
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:48:57 +0000

    lead set example flow wave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
