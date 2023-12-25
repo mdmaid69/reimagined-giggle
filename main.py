3426110e1becbd2ed9966642ca16b83e321b0e6f
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:24:43 +0000

    inform really solve object high

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_list_from_array(array):
+        return array.tolist()
