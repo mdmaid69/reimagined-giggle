fc5df743bdde42fb603ae27460fb5efc640f139d
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:33:52 +0000

    interesting seem fortunate description Yy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
