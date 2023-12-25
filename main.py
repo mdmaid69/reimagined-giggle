b2154e5e6d93f3a53f069238ab83c0b88339bea5
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:27:41 +0000

    transparent off favour development warn

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
