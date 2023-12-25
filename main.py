7d4869450eff577de44dcacbdc55470c74fb403f
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:49:54 +0000

    plenty waste mess music hate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_speed(distance, time):
+        return distance / time
