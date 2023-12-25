b99c93dd8034b923869b402bc25c293431de2a4f
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:47:35 +0000

    tax slide run call whoever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
