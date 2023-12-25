0fd3f61acae47732a9a81f8cd83c1d658c3770e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:24:31 +0000

    cut smooth board attractive occur

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
