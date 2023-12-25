bddfb438a102ea0bb276275da4299cb464db744f
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:18:19 +0000

    frighten arrive turnover wrong there

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
