0089304c2a8a9e66a68e291b50d5e1bc57929256
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:20:57 +0000

    bath everybody invitation goup imagine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
