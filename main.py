9450029621288085f6915eeba1c9d856ab2348c8
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:05:41 +0000

    wise extra spring furniture recovery

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_factorial(n):
+        return math.factorial(n)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
