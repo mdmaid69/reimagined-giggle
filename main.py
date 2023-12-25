4dcb453a4866f59da3e37097feeee81eee942fb2
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:42:24 +0000

    sound over nose ticket relationship

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
