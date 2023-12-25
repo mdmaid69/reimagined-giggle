c33263196c41948f2c6c2ab1cbb48c98e01ccbcf
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:58:48 +0000

    excuse steady material ought glass

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
