77538a7ca80051a9fb5d0ab78a7b7bbe49c51c7d
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:53:03 +0000

    track off risk kind under

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
