083d425095c1f8178f8e6e6aa3845a1ed75b53ae
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:44:42 +0000

    more second Yy benefit excuse

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import os
+def get_current_working_directory():
+        return os.getcwd()
