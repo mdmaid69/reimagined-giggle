61fcd54e837f4d76505f05e13f9f4795eb0c8a15
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:55:59 +0000

    ours wrong cheap pain mouth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_repr(array):
+        return repr(array)
