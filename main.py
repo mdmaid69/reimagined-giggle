937ae11708cf4a1d7e6eb04fd1c4650f98780efb
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:00:22 +0000

    change blind mood come justice

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
