3944b1b457f57e79dca0347c34fe02bc5f3079b4
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:55:30 +0000

    buy among well please together

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
