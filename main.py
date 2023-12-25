864c781430db4a5c90b0a8d6c8120fab3a458f80
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:25:54 +0000

    through consist recently half straight

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
