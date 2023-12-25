f87220b75f5fcbe0a4dc54b046d6e087a9b7ba8b
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:22:56 +0000

    above care generous sight point

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
