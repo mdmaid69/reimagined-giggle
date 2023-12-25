4a14d4112e7d2ba139c923f4eb31b2acba0962d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:35:40 +0000

    reliable force until class left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def count_words(sentence):
+        return len(sentence.split())
