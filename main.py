362d93de0e6282ffe1c842d4bbdaa45518d569ce
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:13:11 +0000

    list compare pot fall rise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
