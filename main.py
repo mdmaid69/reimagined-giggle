64894fd30d67d772e2482e2cac3968cf181ce214
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:36:46 +0000

    opposite lay modern born label

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
