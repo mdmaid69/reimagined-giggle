34b5f0e606b4c29726c653d2ceab6f517b489e77
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:57:15 +0000

    succeed only slip wash soak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+def reverse_list(lst):
+        return lst[::-1]
