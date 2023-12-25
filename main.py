ad0897813052ec3aea672152c731e9b15c0f1bd7
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:58:19 +0000

    burn balance example breathe right

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
