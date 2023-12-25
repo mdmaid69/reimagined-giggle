4ef2a54c9591887d505b0fa392fbdef16b8af1fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:20:29 +0000

    very hear thought care joke

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def add_numbers(x, y):
-        return x + y
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+name = "Python"
+print("Hello,", name)
