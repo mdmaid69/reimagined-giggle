742e240281d8761aed7360fcb0e4c72a96b51992
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:00:49 +0000

    confused hang frightened combine straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
