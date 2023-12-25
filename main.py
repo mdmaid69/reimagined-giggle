f906ef83ab6fc6e22496c60dfe96cfd4ddb6cb95
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:17:49 +0000

    jump telephone gentle success responsible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
