d48cfa4f7c30459adcf83591718fe1ccce2c08d4
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:11:42 +0000

    wrong bag experiment spend hole

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def convert_to_hex(n):
+        return hex(n)
