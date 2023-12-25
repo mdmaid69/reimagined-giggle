e29af443ba866abec493895f977f8c446bc4bd38
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:32:34 +0000

    distant wind complain except fear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def sort_list(lst):
+        return sorted(lst)
