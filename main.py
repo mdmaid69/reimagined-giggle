90dfab829b9b3d92454f5db9f25cd494dda01f75
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:39:48 +0000

    first fight along injury moment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
