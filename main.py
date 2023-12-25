dead4c3b1d877baa9ba7bfda452720177dadddf8
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:35:21 +0000

    occur whenever taste negative back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
