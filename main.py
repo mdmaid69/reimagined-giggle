ddd729330e69eba356ca630be4c9c0d363f632d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:27:39 +0000

    callout want telephone closed accident

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
