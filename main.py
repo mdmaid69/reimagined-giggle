9840f2db5c92947a40143ba12fcaeb7cb6a2d818
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:51:02 +0000

    knock thick heart direction feeling

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+from collections import Counter
+print(Counter("hello world"))
