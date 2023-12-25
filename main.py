eeac78df107d3d2e9954f887dc1256b448913aa4
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:31:03 +0000

    nearly slow getup burn fence

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def find_min(lst):
+        return min(lst)
+import sys
+def print_python_version():
+        return sys.version
