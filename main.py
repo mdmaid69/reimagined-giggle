4104475a4b9f7709687939396224b6c54cd23d27
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:25:58 +0000

    boil spoil spare scale hurt

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+name = "Python"
+print("Hello,", name)
