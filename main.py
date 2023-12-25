befe0c20ee12dbe3bccb4dd88600040dd5a74c63
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:00:51 +0000

    mean enthusiastic Nn realize wear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
