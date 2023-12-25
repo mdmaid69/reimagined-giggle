4c5d3554e832bc3b8a7c6b950cd5d30a4836db52
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:08:00 +0000

    free board waste free memory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
 import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
