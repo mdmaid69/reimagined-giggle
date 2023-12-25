07f843065bd6bbc1a4cb748d32c47966881dbf3b
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:13:28 +0000

    maximum search best pass considering

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_density(mass, volume):
+        return mass / volume
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
