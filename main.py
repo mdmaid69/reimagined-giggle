c61dbed6c8a599d07bafc94851524bbcff892c2e
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:12:56 +0000

    morning might luck negative risk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
