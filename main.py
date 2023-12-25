7cc3edf6813d87b7d4c00fcf1a17483a4c6cd485
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:57:35 +0000

    look tap regularly fear bush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
