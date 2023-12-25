c78407aeeb1001a7dc1d49e645ee994a8fe5ba26
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:01:08 +0000

    back frequent protection usually section

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
