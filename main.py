ac34afa8c31d36407f07f60230eb2c4f3146a1ea
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:03:16 +0000

    goback(to) negative keepaway position contact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
