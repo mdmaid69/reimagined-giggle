b863a2a3215951115e318169cafcbcc662804fe9
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:27:12 +0000

    nor close block war ring

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
