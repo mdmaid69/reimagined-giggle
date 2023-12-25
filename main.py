01316edde3564b98582f3d16f92b2a9df3291b03
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:10:00 +0000

    slip wet coffee recent authority

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+for i in range(5):
+        print(i)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
