23c09969f0da4c6f55f556b13b70d41b1fa5aefe
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:13:13 +0000

    search calloff limit greeting sharp

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import numpy as np
+print(np.array([1, 2, 3]))
