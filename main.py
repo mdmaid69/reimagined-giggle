95955c161e82009c3443ffaec87dac5e3f0728dc
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:21:39 +0000

    turn thankyou hot straight 568energy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
