d0e687093219d1fd0e5cbb305a77c371d577fcca
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:54:49 +0000

    line certain except depend must

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
