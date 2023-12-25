188b0728d2b2d7b296762717accf34ad130b05a2
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:20:24 +0000

    each scrap please health stone

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def convert_to_octal(n):
+        return oct(n)
