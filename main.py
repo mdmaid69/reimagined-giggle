37ea751f8170b73085ec4dacdb1e416dbad2b551
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:56:10 +0000

    keen well interview improvement glass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
