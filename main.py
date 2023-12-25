38622c4a1c490cb051b1e8f4b834195da30f7aa1
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:23:15 +0000

    thanks check mess keepoff classroom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_repr(array):
+        return repr(array)
