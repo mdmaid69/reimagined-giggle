6cab882be2722a388d834b6dbafdc7999385e93b
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:19:50 +0000

    pupil it solve under contact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  def is_odd(n):
-        return n % 2 != 0
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
