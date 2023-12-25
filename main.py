8c4323fa24485d84c13cb06f8a44102fa45a157f
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:57:16 +0000

    pleasant sleep large friendly noisy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
