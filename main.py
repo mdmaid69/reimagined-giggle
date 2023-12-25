413b5c09c96229e5b6435e47f9f85e013bf82bc6
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:42:41 +0000

    morning actually bang go mysterious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
