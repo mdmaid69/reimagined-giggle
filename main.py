9a02f906f8e971fc0474fdb885f2b5089eead530
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:19:22 +0000

    interview post word variety pot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_acceleration(speed, time):
+        return speed / time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
