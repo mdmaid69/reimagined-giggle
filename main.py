7a946a323f60a55317b4c7d524477b67262576f4
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:16:50 +0000

    tidy introduce worth stick behaviour

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def convert_to_hex(n):
+        return hex(n)
+  def subtract_numbers(x, y):
+        return x - y
