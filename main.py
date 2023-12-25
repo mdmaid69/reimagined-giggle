92dd44e5f5fdf5df67b21c7fbc662935a06e6d7a
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:41:00 +0000

    sick twist hate Oo look

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-from collections import Counter
-print(Counter("hello world"))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def is_even(n):
+        return n % 2 == 0
