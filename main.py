55b2109a5650fbe36f5ce9da1476e0cc9598e320
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:45:42 +0000

    can land relationship bye bet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def is_odd(n):
+        return n % 2 != 0
