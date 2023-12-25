4108c998a200dfc707891585759353d629c4ff77
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:13:25 +0000

    565end emphasis repair hang medium

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_counter():
-        return collections.Counter()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
