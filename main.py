46bdab19260b386b119e493a1e6b55d95653d298
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:31:50 +0000

    cross back fire seat suggest

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+from collections import Counter
+print(Counter("hello world"))
