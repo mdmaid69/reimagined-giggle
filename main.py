84285ec946c518523dc77972eb5db115b29a14f3
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:31:39 +0000

    main stranger suppose shop shout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_binary(n):
+def convert_to_binary(n):
         return bin(n)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
