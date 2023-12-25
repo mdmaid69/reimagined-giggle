05bf89ab341600d3b4059a9eeacc2f5bc5f48314
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:55:31 +0000

    consider impressive runaway cap makeinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
