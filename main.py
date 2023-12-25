e10ca6b2ef5f10564705aea178a42bdaa9d9cafc
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:07:50 +0000

    rare interview late guess sudden

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import time
-def get_current_time():
-        return time.ctime()
-import sys
-def exit_program():
-        sys.exit()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
