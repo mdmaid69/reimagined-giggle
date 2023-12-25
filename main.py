6c4e8efb9bdc8afd08fd51edd2bd394f46177df3
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:51:30 +0000

    plastic Cc chase exam inside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print("Square numbers:", [x**2 for x in range(n)])
