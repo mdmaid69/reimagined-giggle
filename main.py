ee6c024f7a8d09da330ba033d62953f3e9b095a3
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:15:58 +0000

    advise dance train scream succeed

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print(sum(range(10)))
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
