9185bdc131e6fb225ea7ec2559d789d57f18db02
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:02:53 +0000

    stupid drive now suddenly heat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
