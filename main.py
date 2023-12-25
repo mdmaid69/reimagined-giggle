0b1ecd1e0cf1ad3565c08c8951d9f8329c6cd07b
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:22:32 +0000

    spin block sort search left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
