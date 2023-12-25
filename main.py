7330c757c56f12917faae447b7d0b3d9afac0cf8
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:36:17 +0000

    music dangerous pump Ss quantity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
