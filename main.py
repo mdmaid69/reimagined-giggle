1aff354fa0e6172b4e24d7047e3d016e5bfab4eb
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:11:58 +0000

    getup stretch priority nervous interview

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
