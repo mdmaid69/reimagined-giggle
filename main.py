2b1c91270624834bdfc79720f569e306caf480f9
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:32:32 +0000

    movement block pain Ss ocean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import time
+print(time.time())
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
