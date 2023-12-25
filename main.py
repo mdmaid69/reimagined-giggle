3b50c4445e18c8f089a5d4a0e6f68d34f53e79ca
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:53:42 +0000

    right join class along likely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
