dc1410752a4730e64a711922a92a6e64ca8c036d
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:55:38 +0000

    my school wealth project size

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
