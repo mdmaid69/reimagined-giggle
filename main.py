939f93d9f5d198aed9e00a52681a8474b70101c5
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:49:45 +0000

    reliable aim begin gift different

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
