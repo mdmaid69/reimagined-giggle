fa6b7440adffbe8eb15aa61d7290baa02614983c
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:55:01 +0000

    move hide over regional dangerous

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-name = "Python"
-print("Hello,", name)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
