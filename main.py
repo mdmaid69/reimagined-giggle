2d7a769c4ca6b1b9e32f0761ca8dfde6549e6f07
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:36:07 +0000

    live add estimate string protest

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-for i in range(10): print(i)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
