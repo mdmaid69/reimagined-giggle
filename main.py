ff580c4d6e389aeb505519c3eaa7341205739d8b
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:58:51 +0000

    free turn acceptable nerve hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
