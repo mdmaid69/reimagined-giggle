48964c328321e07f0e8526998f96c9317a627d34
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:02:43 +0000

    miss wall total power powerful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
