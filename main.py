54e24aaa52e75fd6d492c56223423aedece9bf22
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:02:11 +0000

    male noone project tooth coat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
