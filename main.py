32963e4ba7bc7285bfbf1586ef2a80ce02058323
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:10:58 +0000

    once appearance until respect hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
