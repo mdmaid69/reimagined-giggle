36577427d2340acfa874daaf8a85d83284fec85e
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:38:31 +0000

    oil pipe pass deliberate story

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
