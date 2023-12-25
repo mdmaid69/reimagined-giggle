0fc8057d76a91ca6e02ee55d72a4ff1f7e3932dd
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:15:15 +0000

    too class skin quickly unlucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
