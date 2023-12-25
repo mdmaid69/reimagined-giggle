d3e7067189e4a8e4c6377250ef1c0aaed85058e4
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:41:45 +0000

    introduction bringback bill convince male

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_string_from_array(array):
+        return array.tobytes()
