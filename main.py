fbe50534f68883e17df91f82cd166c82e3b90406
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:26:28 +0000

    pinch praise comeback advanced exam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import collections
+def create_queue():
+        return collections.deque()
