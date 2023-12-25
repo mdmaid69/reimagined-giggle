20084b4e8dd78cbd0468128bf54c02a1563a5282
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:46:54 +0000

    common clear exploration trust software

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
