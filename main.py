797579ab2af2ce4d0a26e6cab1ac58aa127597f1
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:35:42 +0000

    intelligent answer custom shoulder downstairs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
