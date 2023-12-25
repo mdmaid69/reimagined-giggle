a5eccd0feefd6a4960f78fb2171ecce0570bd482
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:14:06 +0000

    personal various present complete return

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
