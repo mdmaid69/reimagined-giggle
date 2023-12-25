04c84e906df16b4f58d70ec6272e5b14310da1a4
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:13:11 +0000

    plenty collect match complain old

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
