93f1f94059f6729d7101d8ba1f8e290d547823b4
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:24:05 +0000

    wear comefrom suggest plan distant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
