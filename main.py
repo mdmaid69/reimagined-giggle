bfcdfc5b2545071e08eab743f87b59b9fefcdde5
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:41:44 +0000

    intend rare chin poor yourself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
