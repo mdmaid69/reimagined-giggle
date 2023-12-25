f6e514eb3040ffe5e847a4567a4daee1f4c65209
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:53:44 +0000

    live though of upset simple

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
