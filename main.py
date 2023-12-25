fd1ec66cf7d982e8c413d0ce0289244a6a603872
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:36:47 +0000

    ordinary habit Kk attract sleep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
