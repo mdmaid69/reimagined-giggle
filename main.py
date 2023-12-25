9f1ecbb2d73a0193af6cb0ad2650601ae032a38f
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:55:59 +0000

    emotion reason bad compare standard

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+for i in range(5):
+        print(i)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
