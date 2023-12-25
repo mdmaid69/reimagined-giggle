4cfeef62aad645b6ac5cf12e16331660b0a9e420
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:52:41 +0000

    accordingto mine land hope square

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def reverse_list(lst):
+        return lst[::-1]
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
