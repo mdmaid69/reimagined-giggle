3ff51cef96249719bd1f73ecc0f3c3bfaa0afb39
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:06:35 +0000

    leave force know miss reduction

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_area_rectangle(l, w):
-        return l * w
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
