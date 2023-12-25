f14d9802bae635c86f9a2d39b403edc34f88ae34
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:30:06 +0000

    polite frame suppose wet order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
