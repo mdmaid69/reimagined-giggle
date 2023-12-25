65f6ee70c9e11965524db2b9352b9491bbbbd3bd
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:47:39 +0000

    top wrong interrupt argue spend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
