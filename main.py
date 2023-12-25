1c8d60a45db7b71aca79ddb9fbb58e82689f863b
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:15:46 +0000

    free else independent container rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import sys
+  def get_python_version():
+        return sys.version
