1b94be931957d75e6bfeb6fda1875154fec11655
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:18:12 +0000

    share ticket land place shop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
