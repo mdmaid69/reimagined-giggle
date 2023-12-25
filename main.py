b0f4a78f8d80891547c8e1f2fa3eb8ccdf36369e
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:22:46 +0000

    frightened round you fall secret

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
