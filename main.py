b575d68765e5c3b10f8589f7f4040cd16c25bdd6
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:23:55 +0000

    length comment comeround wise worst

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_area_circle(r):
+        return 3.14 * r**2
