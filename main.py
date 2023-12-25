2466851f213821928e0ac5625761af68636e2328
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:09:05 +0000

    behind difference chance table dull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
