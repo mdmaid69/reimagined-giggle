6ece7e7a5df5856c6dc7f9701981897290d2dee9
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:13:17 +0000

    certain excellent constantly please card

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
