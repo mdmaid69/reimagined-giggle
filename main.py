d78fe26ae323e9aa43adb039c40b12b1f94fbe4d
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:32:18 +0000

    belong transport answer 567enemy trip

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
