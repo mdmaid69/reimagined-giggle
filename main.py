57545a4214852acc38f37cbebac98cc93c405054
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:03:55 +0000

    everyone mine anywhere keepfrom scared

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def convert_to_octal(n):
-        return oct(n)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
