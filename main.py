53fdb1974b61515ecfb8f1e34f6d9957b074d2fc
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:29:52 +0000

    middle approval shelter probably nearly

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
