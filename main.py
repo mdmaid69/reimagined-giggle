96f3fc628eb7ad2d55dcf4604f4187603d343db5
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:29:23 +0000

    afford poison breakin rest do

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import array
+def get_string_from_array(array):
+        return array.tobytes()
