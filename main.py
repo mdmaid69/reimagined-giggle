fa739b002520fdd9fe6b1e7e2737f6ac1bef3273
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:37:43 +0000

    use protest accidental city suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
