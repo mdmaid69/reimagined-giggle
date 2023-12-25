3c51973874e40d894543671c2dc5240cdb493956
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:52:20 +0000

    sister suit forget test major

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
