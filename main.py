41949c89c768ed7b1dac5d90959bc847431b2ad4
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:30:14 +0000

    correct container still excited deliberate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def convert_array_to_string(array):
+        return array.tostring()
