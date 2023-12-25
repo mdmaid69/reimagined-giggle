8bc1fdc53cfe24d9a3020a56ffe3a549f70cac0b
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:42:55 +0000

    tear welcome turninto difficulty film

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
