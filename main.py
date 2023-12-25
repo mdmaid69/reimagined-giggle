46c6b7fd66459756fc74a944c82760740dfc4453
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:00:52 +0000

    greeting eye wind wing music

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_hex(n):
+        return hex(n)
