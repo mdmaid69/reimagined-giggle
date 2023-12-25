109560db3d1f6f3d8370d718141c641b85a517ba
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:02:20 +0000

    put now now arrangement convenient

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_sine(x):
+        return math.sin(x)
