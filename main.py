a3d353840bb0513c11771a3d561ce76d6b739251
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:37:43 +0000

    past waste left term suffer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def find_max(lst):
-        return max(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def sort_numbers(numbers):
+        return sorted(numbers)
