5781b44e87343cb7d4161a665dc3b8b789b48740
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:43:27 +0000

    else sign steam development money

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def add_numbers(a, b):
-        return a + b
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
