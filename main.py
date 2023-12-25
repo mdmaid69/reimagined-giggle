bec567a74cb7ddd5e984ea5fc54971fd49095e50
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:43:43 +0000

    achievement science noise part Ll

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
