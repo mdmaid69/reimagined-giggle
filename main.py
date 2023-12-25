f69d71e9415b5a007227c68d534cc25cea8f165f
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:32:16 +0000

    bend turndown how tense violence

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
