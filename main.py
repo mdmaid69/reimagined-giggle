a9fa57ed65ad04ee8b486569ff5d3eb14cec8ae2
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:16:35 +0000

    clever reserve press care ours

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
