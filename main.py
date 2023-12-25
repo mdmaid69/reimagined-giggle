cb3348354bb077837118fba597261bb9c4446969
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:37:18 +0000

    pollution begin imagine age coat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
