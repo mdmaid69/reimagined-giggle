28a11667a94fd25bd2c8009f9fd9b3985f7afbc6
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:40:01 +0000

    port clean harm tear certain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
