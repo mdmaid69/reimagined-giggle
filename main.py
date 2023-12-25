81b472016da6f3bc896b8462504309942416fb66
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:13:24 +0000

    negative ever move human uncertain

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def reverse_list(lst):
+        return lst[::-1]
