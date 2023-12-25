3f3eac742e8c56cd7c47fb3b5f63bda1454185f3
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:05:24 +0000

    ball emergency cut tone stick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
