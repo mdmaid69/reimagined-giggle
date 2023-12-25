dc3564793c60ffa540b135a2b5aa3264e45876ff
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:18:44 +0000

    kill real impatient square nature

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
