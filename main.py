8574e71083473170c10c5fb62f7bc7418150fbb0
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:02:12 +0000

    spite fairly loss address plenty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def cube_number(x):
+        return x**3
