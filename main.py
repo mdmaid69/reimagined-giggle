9fc142087bac06803bf8749722f662f083249fe0
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:32:35 +0000

    satisfied paper date rise circumstance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
