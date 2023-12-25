7d6d149f2063d024310213574a080c5074605b25
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:16:11 +0000

    undo pencil damp wealth invite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import numpy as np
+print(np.array([1, 2, 3]))
