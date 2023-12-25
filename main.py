cfbb5bae0a77d19f3b850c7da7999089ccabe162
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:58:48 +0000

    everyone more argument breakup hungry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def split_path(path):
+        return os.path.split(path)
