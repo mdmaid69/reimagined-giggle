f890b6c8fb3e66e0ebc4fde78fa2ac06c7d8a140
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:59:06 +0000

    film miss shop promise miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
