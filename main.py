8d6261ae6cb4d3880439b80fb935f69c749bd31a
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:44:16 +0000

    willing use town neat bottom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import array
+def get_array_length(array):
+        return len(array)
