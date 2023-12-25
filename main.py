d79947f5e0e8733ee254936ef289d53b0c2083c1
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:28:42 +0000

    quantity turn any short scene

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
