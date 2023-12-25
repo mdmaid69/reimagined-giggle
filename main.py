5c4f2eb878e02197733d8aedb0f81909f6e59099
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:21:21 +0000

    slightly position voice old divide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
