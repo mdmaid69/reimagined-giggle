3a43653f5a9fdb2ea3799d5931a93b3eef3f64a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:56:40 +0000

    dish protection act gradually visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
