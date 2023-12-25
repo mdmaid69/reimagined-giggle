deb4e6e3715f6bdfa221dd6a39c0f1e05eb0a106
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:31:49 +0000

    best entry small expect bye

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def greet(name):
+        print(f"Hello, {name}!")
+def calculate_density(mass, volume):
+        return mass / volume
