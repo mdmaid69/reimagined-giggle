eb29a6bc4c068cc36bf832ba76888ac688e1c588
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:35:44 +0000

    store breakin rise prize top

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
