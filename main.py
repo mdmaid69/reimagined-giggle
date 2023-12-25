a88cdd6f5f3049c64d50f18ddc6ce0d35108b301
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:38:07 +0000

    add fierce join blame know

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def is_even(n):
+        return n % 2 == 0
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
