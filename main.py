67309360209a4ca40d95828656c3ca2a603b93fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:33:48 +0000

    embarrassed Rr single stare development

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-for i in range(5):
-        print(i)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
