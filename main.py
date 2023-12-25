07d0fcbafe3966c6adecb3a7cd70b85985bf7498
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:48:47 +0000

    whenever fence delicate description trip

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-name = "Python"
-print("Hello,", name)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
