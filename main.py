76f1e7fbb2eb9e238d5c6d608a2eaec1f13a4cfe
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:36:57 +0000

    convenient far game responsibility effort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
