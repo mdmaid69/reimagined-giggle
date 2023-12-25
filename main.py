18bc433049e3b5d48dbb7f164fdd52e9f742a247
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:24:51 +0000

    hospital bath behave mark within

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
