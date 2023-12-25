b892d3c53f5f83d2a1ac117fbdf6395c233b236a
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:19:09 +0000

    size protest public keep birthday

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def cube_number(x):
+        return x**3
