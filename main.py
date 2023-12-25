5639229cc199ab60552198b828fa8c558e182afb
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:54:26 +0000

    deliberate detail song aim vision

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
