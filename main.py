2e8aa77db0afd450526edd7d289bfb17a2e5e52e
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:26:56 +0000

    identify their owe much plant

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
