74759ea33ca2e938fd1b6f2d700e50dd1c72c565
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:38:59 +0000

    Bb release quick port building

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
