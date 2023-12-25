d721b383bc298f7a65acf0ef43ed677c6689851e
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:33:49 +0000

    Dd nice spare they offence

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
