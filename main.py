357c225c18117f352f19beb656dc47447e655ef4
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:37:09 +0000

    splash emphasize wonder clothes refuse

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+for i in range(5):
+        print(i)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
