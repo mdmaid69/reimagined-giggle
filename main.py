b523347b381417aa5bc20b8400b06f81967287da
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:59:47 +0000

    convincing improve must best everywhere

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def reverse_list(lst):
+        return lst[::-1]
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
