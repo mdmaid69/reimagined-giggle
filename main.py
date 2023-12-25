efa0bf0cab7a5d97e879d05a3af10d9dc819f741
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:58:30 +0000

    sound sound available runover telephone

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
