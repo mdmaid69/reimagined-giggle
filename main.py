6f143b398d6426ef3cf8f284403b97c16ecafe41
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:23:01 +0000

    offence ours enthusiastic still excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import numpy as np
+print(np.array([1, 2, 3]))
