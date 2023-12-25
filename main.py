8b211f33f670eb7b7bdab32fdf4d98e2525b2bc5
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:19:15 +0000

    comeup noise division preparation freeze

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
