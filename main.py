fcbbca6fdb11ebcefa6b99be95002e94d33b4508
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:33:26 +0000

    under arm stop argument fall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_power(work, time):
-        return work / time
+  def get_current_working_directory():
+        return os.getcwd()
+import sys
+def exit_program():
+        sys.exit()
