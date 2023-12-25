245f80777c0f4470af74207070c73e484680a74e
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:38:12 +0000

    nose style dish scrape passage

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def clear_array(array):
-        array *= 0
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
