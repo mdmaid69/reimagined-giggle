c0674d3d3b3da2fb23e2e8b8f5b43b53ead2ff03
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:42:59 +0000

    destroy bored remember defend cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import time
+print(time.time())
