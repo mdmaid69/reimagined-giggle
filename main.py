1bd56d3766ba64e3a47540cdb47b56adf82d84ea
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:13:54 +0000

    569enjoy save cross ticket ticket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
