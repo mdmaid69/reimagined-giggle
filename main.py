30386a26c0b5bde643d5a18162e5503e581235d7
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:03:29 +0000

    free foreign any 572enter turn

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
