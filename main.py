72518316f13a87451cd3fd738fc971ce5f1dac3e
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:38:43 +0000

    godown offer goout expression conclusion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import collections
+def create_queue():
+        return collections.deque()
+def is_palindrome(s):
+        return s == s[::-1]
