0f1c407bdfcfddde2875f26d1c95d828a3a4cd6f
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:19:46 +0000

    put call refuse emphasize team

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
