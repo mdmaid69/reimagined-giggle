784f10335de6dc2f1d09a8ff711f0b400e43feab
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:10:16 +0000

    whom violence possibly level ever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_length(array):
+        return len(array)
