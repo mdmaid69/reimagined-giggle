afec378aa4756ab5f1b95891a87467505caf4453
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:53:34 +0000

    ever personal bottom practice change

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def split_path(path):
+        return os.path.split(path)
