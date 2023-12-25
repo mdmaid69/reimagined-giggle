b772cc2fd2cf83f66474c0088eeb3fb4c75ce97a
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:03:56 +0000

    react justice vanish loud round

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def reverse_array(array):
+        array.reverse()
+def is_odd(n):
+        return n % 2 != 0
