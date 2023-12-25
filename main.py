6cb49bbab2fa9a1b55859c9d9f8237055863ec3b
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:09:35 +0000

    criticism goout lonely send children

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
