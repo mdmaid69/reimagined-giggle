4b779668e6065ed4db3f2bd79f3252bc8ee09bf1
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:03:18 +0000

    dish takeaway still deal way

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import collections
+def create_counter():
+        return collections.Counter()
