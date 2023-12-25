b3575fc99399c4947752efdd26313997ce897d22
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:21:50 +0000

    neither much but because mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import collections
-def create_stack():
-        return collections.deque()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
