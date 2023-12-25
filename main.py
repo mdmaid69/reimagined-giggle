ef9a176501e23b09d675203f5a193aaf7febd9bc
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:15:28 +0000

    band back common apparently cause

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
