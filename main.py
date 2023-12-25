67e85e6d7d97e489b7080d53d77b588853b5e904
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:42:25 +0000

    callout skill depend shoot why

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_nlink(file_name):
+  def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
+import platform
+def get_python_version():
+        return platform.python_version()
