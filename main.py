b3f8dd5fea156b09d924db5524aeb31619d78179
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:15:02 +0000

    boring teach smile bill difficult

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def reverse_string(s):
+        return s[::-1]
