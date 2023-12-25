0128ab4ca56151dfb577c8feae39752c350249bc
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:52:24 +0000

    rain uncomfortable consider chance schedule

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
