9e45429f014823bbbe649c39a93b01e62e66d732
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:09:02 +0000

    may slide forgive bringup keepoff

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_as_repr(array):
+        return repr(array)
