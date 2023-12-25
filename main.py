460430928c8939ac89a20020f449220e58868254
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:44:34 +0000

    smile nearly passenger unhappy laugh

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
