3d1807d06276b11448df15f94400cca6fd6f7629
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:41:49 +0000

    characteristic wish shallow last control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_float(array):
-        return float(array[0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
