f21bf5b438f9488080872cbfdbe59a69a6cd5274
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:07:07 +0000

    file thumb offend charge have

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-print([x**2 for x in range(10)])
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
