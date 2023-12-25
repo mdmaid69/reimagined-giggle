73d9130e3bd6851f27886098517da222846bf024
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:14:28 +0000

    makeup instant health store below

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
