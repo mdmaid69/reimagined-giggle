7f7c31d3232131ed561dfb2d81c738b20097316c
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:58:34 +0000

    doubt willing let right priority

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  def count_elements(lst):
-        return len(lst)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
