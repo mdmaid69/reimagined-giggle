208c64de1cc33b83330b41e03abcb79b7ae02165
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:44:04 +0000

    figure themselves nerve you yet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_as_set(array):
+        return set(array)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
