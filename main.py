fe4c8724893efac0ce131aece637ba3d8fa922ed
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:47:53 +0000

    quiet car evening change raw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def count_words(sentence):
+        return len(sentence.split())
