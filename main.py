4af63f9c704d37da4a21525d0ca808bef9c6e0e1
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:34:26 +0000

    classroom connect settle successful passage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_density(mass, volume):
-        return mass / volume
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
