cad680cb1c0976d47a6e5fc596ff52c77087d3ca
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:11:26 +0000

    smooth information mind certainly culture

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def clear_array(array):
-        array *= 0
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
