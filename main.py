531c5591f3528608ce8d052ebf87404e99bc27e4
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:41:41 +0000

    deliberately mean petrol goon guide

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_power(work, time):
+        return work / time
