833668db8164545da678dc87b47fc6beafe32c4f
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:07:46 +0000

    guard offend closely peaceful violent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
