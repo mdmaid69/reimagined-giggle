3f86cbee6eb80a11e0cf5e553a48a1288e6ffa3b
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:58:48 +0000

    afford neither sister education direct

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
