7b277fdfd3b64ba93e00c5c44db37d631c2eb75a
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:44:55 +0000

    although post abroad goodbye near

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
