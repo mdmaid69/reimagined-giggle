7e8209319cafe879339b8ee4bd9dbdce06dbec03
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:49:52 +0000

    patience spoil remain mistake connect

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
