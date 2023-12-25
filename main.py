4b2e385a29fca8acbef8ec0716474c9e344af4c2
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:52:14 +0000

    complain sale sleep touch comment

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def convert_to_binary(n):
-        return bin(n)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
