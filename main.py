494a21b1bf09066fa5c5f2aee08eba1db6cd3b66
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:51:04 +0000

    as achieve debt slight brief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_itemsize(array):
+        return array.itemsize
