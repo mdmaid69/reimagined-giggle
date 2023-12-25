efbe9d68a0bccf5b4932cf166ac195c19c8fa14a
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:03:00 +0000

    run discuss compare do save

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
