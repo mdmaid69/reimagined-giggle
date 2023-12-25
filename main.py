c1526bfa45551b8905418a494ce8738491b09ff2
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:18:04 +0000

    tap hospital our compare destroy

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
