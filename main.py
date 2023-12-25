091fa0eadbc6a6cf336090a49f753117da284faf
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:55:32 +0000

    who laugh listenfor easy crash

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
