f45ba325ed4206ea0c83db8b9d8b7d0799829d44
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:33:54 +0000

    protest also base eager greeting

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
