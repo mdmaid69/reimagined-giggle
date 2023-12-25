a1271723a9e81705c0ab832ee773551fbd2a149e
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:55:07 +0000

    herself suggest fight spread order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_array_to_string(array):
+        return array.tostring()
