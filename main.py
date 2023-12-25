cf68320370ca7784099da432ee72ca0e2b6ee119
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:43:22 +0000

    first get know community bin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
