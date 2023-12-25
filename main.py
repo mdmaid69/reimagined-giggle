0b8a9b619461a2b613bab3448057a97bad806d1e
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:03:39 +0000

    impressive base alive welcome point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
