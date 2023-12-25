1bb4926021684a6e815aa21c79e619e19196997c
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:32:25 +0000

    destroy accidentally yourself practice now

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
