91afd6c9b79547f1013194c58c95121c62ff987b
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:43:11 +0000

    crisis previous pinch plant agree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
