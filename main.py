e97075db85e8fe6b8a6606f30a67ae081377d92a
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:42:00 +0000

    ball disturb worth steep music

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import sys
-def exit_program():
-        sys.exit()
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
