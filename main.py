b3588d1baa8b66d501d2d9330f301d3d69567e4f
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:20:45 +0000

    Hh material danger convincing eye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
 numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+print("Squared:", [n**2 for n in numbers])
