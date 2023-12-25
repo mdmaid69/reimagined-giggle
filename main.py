4ff12506aca265c13c83e60756ee85a0b0ae4299
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:00:28 +0000

    calloff soak world position coffee

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
