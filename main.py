edebe01cb7e56598cc6a8ffb4ccd757301dc599f
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:56:36 +0000

    weather for bringforward home secret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
