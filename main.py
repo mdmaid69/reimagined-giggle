96df654ad2d6763328b3e726f9d617dff8bf0eeb
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:34:35 +0000

    piece cheek cheap trouble justice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
