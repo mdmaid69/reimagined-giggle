3647ed62df52481461828aaf1bd48dcd6f482de1
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:12:20 +0000

    precise shut race right exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
