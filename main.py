bdfaca38b1602224c68e17014b91b20fc8d0edbe
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:25:07 +0000

    ignore amount like just open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
