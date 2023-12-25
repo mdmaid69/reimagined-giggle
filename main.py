8e30118f440e2baf800ddd2df04b7d19bd26b6ff
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:12:59 +0000

    dead desperate lower slightly principle

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
