077c418c67d7795c9c05b6670df512fdae3ed77d
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:40:38 +0000

    row name zero square act

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
