c5db0a0aa4b886a7b8276d2cb17100151127a38b
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:18:37 +0000

    apart scene winter slice generous

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
