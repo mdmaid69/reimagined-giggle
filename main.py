58041ed4bf485039663e79a565b017e3a06bc2a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:13:28 +0000

    or best contribute close Dd

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
