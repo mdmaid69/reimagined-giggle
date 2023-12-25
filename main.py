c1655ef147aa5b09f76a4a3efd511dce767db420
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:04:31 +0000

    minority wipe provide giveback personal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def cube_number(x):
+        return x**3
