3779ee0889ab5496a6cc7331dc5222e1ef18f1ff
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:30:19 +0000

    rest height please return wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def convert_array_to_list(array):
+        return array.tolist()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
