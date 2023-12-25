ef58069a7470807f0141479faffbac4ffab0de35
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:09:39 +0000

    surprise trip join maximum pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
