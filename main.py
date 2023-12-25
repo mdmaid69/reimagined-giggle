65e5f6e82fa82bcb117f057dda2ad907d5c71c3e
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:46:31 +0000

    door quiet ride wind habit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
