1099f45db1691646579b739fdec8bedaa20b7cd7
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:41:56 +0000

    amount laugh buy combination unhappy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
