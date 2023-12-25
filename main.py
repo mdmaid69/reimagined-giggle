505933fcb34131220e3db8989da66ea45ab6a8cc
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:44:39 +0000

    dress bridge live door again

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-name = "Python"
-print("Hello,", name)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
