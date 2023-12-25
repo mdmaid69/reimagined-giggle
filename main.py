fba2eb5ad2722a787bcb891bd22c699b92d416c5
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:00:49 +0000

    human vision listenfor past splash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
