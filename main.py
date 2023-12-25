0b3895e16e45e33026b5bc81afbf89f8628715d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:07:10 +0000

    eager equal impressed walk cover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import os
-print(os.getcwd())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
