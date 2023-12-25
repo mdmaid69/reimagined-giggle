32330e1f3a127f5fa5a8ebbf45520b5acbb28236
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:34:16 +0000

    grand behave call teach listenfor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
