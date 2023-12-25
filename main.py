6e20ce1cda3d1b3fda0df70ed45e1f2303bbf99c
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:37:14 +0000

    unlucky Ee thinkof brush container

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_as_list(array):
+        return list(array)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
