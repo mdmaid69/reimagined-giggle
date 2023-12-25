0b4d82173cfee5b9fcc7a21460e1d9bd0d97d73d
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:48:00 +0000

    gate 572enter total as frame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
