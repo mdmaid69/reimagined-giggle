f8df0349fb4b77f4fc3148acb93d23cc4d346c0e
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:16:23 +0000

    dream accommodation standard nature church

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import time
+def get_current_time():
+        return time.ctime()
