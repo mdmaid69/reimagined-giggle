bd3bd237531fc963d9d90660c526e28ce603dac2
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:38:01 +0000

    spite promise open Tt shadow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
