36e9c0a4acae718111fcee353494a68a91373dd4
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:29:21 +0000

    strike swing spoil shopping keepaway

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+print(os.getcwd())
