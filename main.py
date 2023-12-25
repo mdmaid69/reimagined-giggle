3334572431af8da3449acb9f026ed57dc30cf195
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:15:54 +0000

    healthy project with round after

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
