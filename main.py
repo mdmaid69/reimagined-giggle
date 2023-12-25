af565c3f345aba9deda3f51ba06a311c9076d518
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:51:04 +0000

    dig store steal fill giveback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_as_int(array):
+        return int(array[0])
