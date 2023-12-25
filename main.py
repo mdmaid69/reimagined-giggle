d42087874bbd8bb3b4f3f04d3f1f7fa076863273
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:26:58 +0000

    play rain preparation track journey

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def count_words(sentence):
+        return len(sentence.split())
+import math
+print(math.pi)
