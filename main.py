81356b715a79f818fb6d5504bea9c6d92d34163b
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:38:10 +0000

    base lower holdout positive exist

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  def reverse_list(lst):
-        return lst[::-1]
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
