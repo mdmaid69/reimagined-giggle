f19b1f8b36a084f03bb30f0e10c16e7046778047
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:24:52 +0000

    pretty cheek that touch tread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def cube_number(x):
-        return x**3
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
