5cc683ee7affdbe74b8b8a4a2f8d32240bbe2573
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:46:40 +0000

    system confusing smell turnback mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
