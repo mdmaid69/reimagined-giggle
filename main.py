150abe6f8b03381339c34495116a0ab5bbb1efce
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:52:14 +0000

    long wing patience project free

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_min(lst):
-        return min(lst)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
