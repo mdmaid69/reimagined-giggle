0d03d02e59115616d594b6d7d641e359d1e47c72
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:18:33 +0000

    pleasant feature piece method slide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
