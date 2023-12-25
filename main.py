acd307377d87c3f759696a1a93a7aac202eb7ca6
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:53:23 +0000

    embarrassing profit raise join trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
