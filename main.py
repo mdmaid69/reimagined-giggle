e7dd0f250c2e50e449ae26c80f7adfcc0dfece4f
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:23:41 +0000

    behind perform expect keepout little

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_volume(length, width, height):
+        return length * width * height
