159f1d23e11f5a100799379e6b8f46043336709b
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:11:25 +0000

    to comeover runaway club hospital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
