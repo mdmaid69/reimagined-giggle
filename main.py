b113593ca8eb262dba85fb8c594871591c22472f
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:58:07 +0000

    straight hurt smooth software present

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-print(time.time())
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_acceleration(speed, time):
+        return speed / time
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
