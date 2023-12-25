576c10a06fd68db3d95672c1d7c9bf2644d3cc1b
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:10:57 +0000

    round sort goon man character

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def convert_to_hex(n):
-        return hex(n)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
