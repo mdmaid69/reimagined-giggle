a1639e45b1dbcfb6430ca126eb00ce08a2693b1d
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:01:03 +0000

    class goaway visit giveaway wave

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
