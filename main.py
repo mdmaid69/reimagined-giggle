6dfda359bbdd792e8e6a2d54ca827ad88d71872a
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:06:36 +0000

    hungry achievement back throughout tough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_current_directory():
+        return os.getcwd()
