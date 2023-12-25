316354b418935cc45a08de8ac0dfde04922e3abc
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:09:21 +0000

    cry branch half deserve blue

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
