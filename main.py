89a257aab5338f790eff69ca404957c04fafe27a
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:37:12 +0000

    wind date even unless big

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
