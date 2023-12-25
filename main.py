586ff7b1c0768d83bac76c2c81c337aa4c66fd60
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:00:18 +0000

    play sing high over history

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import numpy as np
+print(np.array([1, 2, 3]))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
