f7da0d5e1e934e6d0701bac80d1d820772fee661
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:50:30 +0000

    play brown protest return goodbye

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_speed(distance, time):
-        return distance / time
-import getpass
-def get_username():
-        return getpass.getuser()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
