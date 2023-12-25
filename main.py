af818b1f0c02141d043a865e1b4e510ad7380b18
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:59:38 +0000

    emphasis work takeoff long love

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+text = "Hello, world!"
+print("Words:", len(text.split()))
+def find_max(lst):
+        return max(lst)
