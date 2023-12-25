c569e18f4f8e0b59974a34e888522d14dcdcb062
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:48:32 +0000

    mixture murder must blow innocent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def is_palindrome(s):
+        return s == s[::-1]
+for i in range(5):
+        print(i)
