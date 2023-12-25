1e13158c812bd1fa65801fa94b742d3c7bdbde06
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:40:02 +0000

    Gg raw worth scrape game

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
