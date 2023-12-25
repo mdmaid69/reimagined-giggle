d95c07ee11e2e221743ad4a6338f4601380aea88
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:40:12 +0000

    shut pollution prove plan what

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
