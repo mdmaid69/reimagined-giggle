7fdc11cfbde9e32415bac8c394362f6155235800
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:22:19 +0000

    pride copy stand never married

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
