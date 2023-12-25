7eafa236040b2e770e0d4809e993a9edfc30e36e
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:59:34 +0000

    muscle battery available suckme trap

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  def is_even(n):
-        return n % 2 == 0
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import os
+print(os.getcwd())
