2cbaab4e63e5bfa38a661c75e4edafa4f087d461
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:38:17 +0000

    both value stand grade card

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
