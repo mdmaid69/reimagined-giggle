754dd062caf83d4f75ff0de5eb401b3e8e41d24f
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:09:29 +0000

    star surprising lower which centre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
