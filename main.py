db6b35c79e4bc9459b52f3e23db71057b6f8c1d8
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:42:34 +0000

    professional deep against phone spare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import time
+print(time.time())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
