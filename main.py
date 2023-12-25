20371f2ecdc9937ccc06439ae1aa54ec4913a3ac
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:08:52 +0000

    form complain goup equipment building

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import random
-def roll_die():
-        return random.randint(1, 6)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
