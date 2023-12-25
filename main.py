73d236d02ddf2d6b0221452ed50c29be2cbd65cb
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:53:29 +0000

    along ahead comment ride wave

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def count_elements(lst):
+        return len(lst)
