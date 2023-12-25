444603e30f56328e750d1e6540950efd53ec0ba9
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:35:04 +0000

    bunch entry order duty born

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
