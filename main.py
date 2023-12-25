ad4cac24f69f47c1ce56eb31d795419bd8ac26ea
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:28:45 +0000

    brush hope whichever introduction question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
