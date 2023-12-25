08ee220fb8979e32288d811218a694320570573d
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:13:13 +0000

    experience 564encourage hello soil past

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
