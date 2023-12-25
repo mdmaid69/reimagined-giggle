7ef5b5662d4863ed1ee1db8188a3a4b5d6642a60
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:23:53 +0000

    whether correct takeoff born some

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
