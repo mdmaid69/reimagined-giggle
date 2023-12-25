cba971d4620edcfc1daf829729b263310a2bec05
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:43:25 +0000

    never shrink row paper maintain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def is_even(n):
-        return n % 2 == 0
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_item(array, i):
+        return array[i]
