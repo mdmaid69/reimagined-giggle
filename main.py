644db60732ee3e0855c1893c31d807dadfe6cc0e
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:29:50 +0000

    table lookaround coffee underneath name

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
