7177d1be78440519562b0b947ca1d81657f2210c
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:16:39 +0000

    permission theirs or practice dance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def is_palindrome(s):
+        return s == s[::-1]
