692509ea4206804703024302f097cb94033d443a
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:46:28 +0000

    lock recovery action confidence communication

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
