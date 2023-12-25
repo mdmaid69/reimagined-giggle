2fd9096db771133c546a8f354fd3f6a2b2b562f0
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:05:33 +0000

    lot ought doubt prize motion

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
