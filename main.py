4570d1ce9fb38b78ebf531fc3de058b0df2de5f6
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:38:40 +0000

    certain goup concerning Kk fail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import tensorflow as tf
+print(tf.__version__)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
