19656719b365a479a0d2c44ec9e3e35661908122
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:39:12 +0000

    rush confuse name sport opposite

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_floor(x):
+        return math.floor(x)
