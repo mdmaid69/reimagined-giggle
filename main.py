0063740186869e19e88489d467ac55841cb82a79
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:59:43 +0000

    public gift corner family regular

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_odd(n):
+        return n % 2 != 0
