9b0d8f2f75d19bd5d179cbd72ca0dc79645a78d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:05:41 +0000

    deep rule holdout used measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
