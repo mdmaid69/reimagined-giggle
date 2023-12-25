24a31b4b42d69338227987f9bd80d856c3102434
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:28:59 +0000

    defend quiet period bridge long

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_as_set(array):
+        return set(array)
