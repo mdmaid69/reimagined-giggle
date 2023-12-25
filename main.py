bfb13a3955f72636c83c90f9871775b892ef8a73
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:34:12 +0000

    criticism getoff disappear modern ever

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def print_python_version():
-        print(sys.version)
+import os
+print(os.getcwd())
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
