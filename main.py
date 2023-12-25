a8fb3a5e95f66ba15b10bad4eebd66a7754e0daa
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:26:46 +0000

    length 565end stir goup care

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
