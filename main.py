1d4770728bc0c54b524a0e4172cb868d09bc8a73
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:19:50 +0000

    fortunate height somewhere breakout confused

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def reverse_list(lst):
-        return lst[::-1]
+import os
+def change_working_directory(path):
+        os.chdir(path)
+for i in range(10): print(i)
