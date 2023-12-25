5948bf0cb6f5c2af2149c9e3fd07070183dc0728
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:02:08 +0000

    friendly drawing style workout Gg

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def reverse_list(lst):
+        return lst[::-1]
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+print(math.pi)
