7e3f0f1bd1414762dab9ab47e92055fef9a8e414
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:37:22 +0000

    waste appear rain yesterday 571enough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
