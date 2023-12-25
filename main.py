3388dbb7a5352b2aabd063560709cda1c59dfe44
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:30:00 +0000

    turndown bet approve force want

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
