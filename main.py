39615bad88bd348680cc6850595913ac6e65b791
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:47:58 +0000

    hate claim only autumn flood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
