175cf3a3bb3c7c4f5179b5bdba314cec0d990c95
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:35:25 +0000

    visit taste second opinion thinkof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-x = 10
-y = 20
-print("Sum:", x + y)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
