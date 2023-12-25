a38bfadb556e5ae5f1314077807799e144559df8
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:00:44 +0000

    mine getback somewhere common count

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_density(mass, volume):
+        return mass / volume
