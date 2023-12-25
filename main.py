7ed73a8b94086f79d2cddfbae384b613a7f77f31
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:51:27 +0000

    issue everybody convince treat keep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
