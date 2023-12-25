0d4d0e6f8fc9f7e58d508e250256a34d92b9cdf5
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:58:06 +0000

    interesting bread crash hard callout

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+for i in range(10): print(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
