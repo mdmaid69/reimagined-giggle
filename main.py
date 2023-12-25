32d825f734c6f30722f69af71ebbb981b272836e
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:54:13 +0000

    correct innocent plan everything task

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import platform
+def get_os_info():
+        return platform.uname()
