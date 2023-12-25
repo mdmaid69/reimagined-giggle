56ea6a4d45cabbbb2809096d163e8763d492ec4a
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:50:38 +0000

    part on reject sudden accidentally

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import collections
+def create_user_string():
+        return collections.UserString()
