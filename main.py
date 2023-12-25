c37ceb85dacb8ca96b8f9460d7cc9c29e2d38601
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:36:19 +0000

    day shoulder disgusting rank OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
