170e5e544a1b68b8ace57c3db0fdad63ae42dd2b
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:12:41 +0000

    raise arrive voice rather usual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
