5def26f7c791cdb267d47353b4f2d1f254c05546
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:54:28 +0000

    marry pair properly worst find

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
