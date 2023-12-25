0b292144ad0e984bd54b06a1a4dbb4dbf2776ab2
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:58:14 +0000

    add doctor present gradually tragedy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def check_if_array_contains_item(array, item):
+        return item in array
