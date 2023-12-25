78ff7588bdd09f8ca2ae0da01046c23c08cfd77f
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:50:53 +0000

    somehow equal challenge claim underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
