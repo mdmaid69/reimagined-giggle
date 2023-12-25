840892ad03eeb5eef7bc32d762117d94e9b90a34
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:07:04 +0000

    dangerous walk but callat tune

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
