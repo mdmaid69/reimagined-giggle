082eadeae96605ec26d9e45f26b22fbb416c9f64
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:55:20 +0000

    tired dish however us relief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def is_even(n):
-        return n % 2 == 0
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
