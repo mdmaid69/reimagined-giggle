d3f8a6f2be1707b93156d7595a0573dc6459c3d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:24:19 +0000

    declare sail play represent like

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
