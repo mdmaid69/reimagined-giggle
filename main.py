eeff9c9e152426edb1d6b6fab6fb043bf64724a3
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:24:49 +0000

    deliver amount sail score makeinto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
