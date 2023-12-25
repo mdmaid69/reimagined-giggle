49cba075fc72f39af5a63dba6cf795cd202278b9
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:40:20 +0000

    room live head traffic top

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
