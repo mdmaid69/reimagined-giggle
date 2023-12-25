4d6fcc2534ed7263173947bea9af28172c322b80
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:34:33 +0000

    talk he keen conversation race

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
