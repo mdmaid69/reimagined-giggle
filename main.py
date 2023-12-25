6dddf39865cfb35ec434832ac4b4dc1c3739c4d1
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:54:07 +0000

    ahead piece some back reject

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
