cd37d79037a1821d0940efd23635b40f68eacdb1
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:48:25 +0000

    stone exam up them tune

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import collections
-def create_user_list():
-        return collections.UserList()
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
