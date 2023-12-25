937f7ec280384aa44d257dfc9f1130fb825b51ff
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:26:39 +0000

    prepare tone unusual confusing same

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
