2946f34dca1a72925420f892a06ac3942159c6d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:43:04 +0000

    way leave bill freeze lock

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
