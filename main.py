9f263ed79f57bcf02dd8647191c8bf013532d9d0
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:05:49 +0000

    lower fact 564encourage influence with

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
