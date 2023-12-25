eaf56dbbd7e7620482454ee9278957635913298c
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:09:02 +0000

    statement dance everywhere hold moon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
