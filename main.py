22a07e561f0a49dcab93748e7b800d5daf99699c
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:33:25 +0000

    description bringforward however satisfied would

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def is_odd(n):
+        return n % 2 != 0
+print(sum(range(10)))
