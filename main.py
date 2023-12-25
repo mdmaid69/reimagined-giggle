797046aefdcbdfb8d38ba53ecc27a0267097524b
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:25:41 +0000

    medium keepoff onto loan appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sys
-def print_python_version():
-        return sys.version
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
