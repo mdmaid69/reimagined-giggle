738d77420859b8b98cbc2dcb90846d394f32d860
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:39:51 +0000

    argument Tt debt other mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
