779647c3ffff21625ade74d2369f09d359cf8c4e
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:10:50 +0000

    concern expect sure science film

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_octal(n):
+        return oct(n)
