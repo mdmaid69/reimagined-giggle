0ca6d25a9a94849afb338ed898de6022ef2093c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:56:27 +0000

    borrow street goodbye everywhere ready

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
