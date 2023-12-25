41e3ed153a9509bce7c87c9261269a9b77d31139
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:23:09 +0000

    difficult react study holdup characteristic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
