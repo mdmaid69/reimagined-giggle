aac9432b7806c04a740a52b24fe82095f2f2ab72
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:17:31 +0000

    preparation behaviour takeaway common fortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
