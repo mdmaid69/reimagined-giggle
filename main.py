d32ee50b7ecc932691581e5551c9e98d6574d641
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:52:27 +0000

    stay swim curious true exchange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def is_even(n):
-        return n % 2 == 0
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
