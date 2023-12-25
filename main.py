56475070bb700bd91af22f701bba44e4626e1f16
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:16:40 +0000

    hang circle gradually upwards long

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
