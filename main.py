45bd80756f598b453fdb8422105afed6ab7ffe0f
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:50:35 +0000

    project in sorry scream fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import sys
+def exit_program():
+        sys.exit()
