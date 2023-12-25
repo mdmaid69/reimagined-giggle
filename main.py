0a604ba12ee72a9715cb706b60607b2f857a42cc
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:20:23 +0000

    mood slow supply high account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
