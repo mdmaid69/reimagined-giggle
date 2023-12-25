e4689d85f59d6d9697868aff943e8cd4afd2bc3e
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:16:02 +0000

    begin Vv form back yourself

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import numpy as np
-print(np.array([1, 2, 3]))
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
