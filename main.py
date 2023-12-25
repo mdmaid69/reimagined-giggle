1f4bc57f369d6999771a0c0beaea3f8f39066bd1
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:50:09 +0000

    stupid pin grand otherwise rate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
