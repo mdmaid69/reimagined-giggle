f03868cbc854fe2a66dd20e59085a3db13e099de
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:53:13 +0000

    figure high hello below grateful

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
