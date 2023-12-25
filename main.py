12fecc45a540ee7273d8e2402189c6723d46d57c
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:20:31 +0000

    blame dance relation you noisy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import tensorflow as tf
+print(tf.__version__)
