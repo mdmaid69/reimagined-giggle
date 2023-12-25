fa6cca7a1f5a363ca738ec452aee0ced1d6971b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:19:04 +0000

    put private behaviour city lean

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def find_max(numbers):
-        return max(numbers)
+import tensorflow as tf
+print(tf.__version__)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
