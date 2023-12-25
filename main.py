efca05e44c79f0403001b8373ef52c7138b98a61
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:07:12 +0000

    accept consider able love thankyou

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_pressure(force, area):
+        return force / area
+  def convert_to_binary(n):
+        return bin(n)
