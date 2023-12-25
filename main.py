2d4394043542e443cb5412d5c9f0ccc5a931757a
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:48:26 +0000

    pain event murder however blame

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def count_elements(lst):
-        return len(lst)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
