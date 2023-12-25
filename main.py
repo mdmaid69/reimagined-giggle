29700f6b8b2d11e79628e75c9748c0ea72d700b5
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:42:47 +0000

    ours cover come manage instrument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
   def create_directory(dir_name):
         os.makedirs(dir_name, exist_ok=True)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
