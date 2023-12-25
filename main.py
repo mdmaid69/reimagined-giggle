7047f02483e193d1b0073da09ebb8566611814fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:41:36 +0000

    yes heart pass scream production

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-print(os.getcwd())
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
