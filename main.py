92b0b3267251159b75b6a18411d928ad048959ef
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:25:28 +0000

    inside direct mistake kind glass

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+print([x**2 for x in range(10)])
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
