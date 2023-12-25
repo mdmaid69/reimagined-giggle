d6d73e4ea3fa1b9f221ab498acae60182608bb78
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:24:50 +0000

    basis best high responsibility hide

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def is_odd(n):
-        return n % 2 != 0
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
