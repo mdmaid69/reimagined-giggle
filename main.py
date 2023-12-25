e52b652050527be4f273ad42a11216733335aca7
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:31:03 +0000

    change comment mention occasional centre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
