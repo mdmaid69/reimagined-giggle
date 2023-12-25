58a579b7de92987d1eaf6a9bc96fb4a38fa4db9d
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:30:39 +0000

    slight favour rush belief opponent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def count_characters(sentence):
+        return len(sentence)
