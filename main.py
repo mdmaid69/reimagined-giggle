72a23bfb7f309a28ce16e42aea0a4f3b21da9ad2
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:09:48 +0000

    damage run cheek sister best

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  def convert_to_hex(n):
+        return hex(n)
