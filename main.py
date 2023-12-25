ffb89f2413b7c684bd5b05e36bac706707ceb3d2
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:11:13 +0000

    prefer bye edge interview immediately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
