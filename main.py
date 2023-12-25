3af9eba0e2c531809818f2d838fb6576f7c0b692
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:01:04 +0000

    Gg brush proof gather closed

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
