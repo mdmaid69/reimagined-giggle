cd8973736cb8038f313c2fa3122811b8fbba42f6
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:59:19 +0000

    waste achievement characteristic stick near

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
