0250d387a6e46a5259dcafe2f825c3f7f522f118
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:54:56 +0000

    refuse afternoon skin divide scene

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
