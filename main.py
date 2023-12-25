3bd737340af2d60cf00fee635713d7f01e506a97
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:56:23 +0000

    battery act impressive stir yours

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
