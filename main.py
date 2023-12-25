c855459e0826a560dce1a3b6452bc6fb74939a5f
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:41:39 +0000

    second slice spite callback claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
