be02ee24f977178d6ef0c4021524a4a6d1c2bee2
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:46:29 +0000

    worried expensive rest shut nurse

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
