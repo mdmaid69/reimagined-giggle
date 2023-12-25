b5f0e505328c596704966753d847d191c7f2fe98
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:07:36 +0000

    awake Gg swing regret use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
