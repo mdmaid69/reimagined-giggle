ee544775cb4a19cecaa7e60f83267abd4f638eee
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:54:45 +0000

    copy himself effort interview guard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
