703c403da21b89d686669749043dbebfc8e90d2a
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:41:44 +0000

    hope flat song or complete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+def remove_directory(path):
+        os.rmdir(path)
