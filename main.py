e069c8900b04cc2c5d280ef86b40ab0dcf8c27f9
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:34:37 +0000

    frighten turnout contact only harm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def convert_to_octal(n):
+        return oct(n)
