182051a86fee3c0d306af2ca9dcfd5021f1fce20
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:42:41 +0000

    Uu whoever exit apply experience

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
