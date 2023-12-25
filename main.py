3e3d670cd4f3c9c7a033a391232ee07ae84f1977
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:40:58 +0000

    uncomfortable bend hollow further cum

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
