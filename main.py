b944e1e279f9f75122139f4654928d8db5f9b03a
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:22:36 +0000

    fast typical power try so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  def reverse_list(lst):
+        return lst[::-1]
