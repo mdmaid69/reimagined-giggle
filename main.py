08367b6908950efcd16b61fd9547db62b3614240
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:35:52 +0000

    regional switch about do mail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
