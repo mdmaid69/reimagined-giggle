79d36d5581a3cdf2007b96c3da331922907f4d66
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:56:31 +0000

    square off arrival information ear

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
