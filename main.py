05ce80c62a20add4cb9c5cb1ea36acef1c02628f
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:52:59 +0000

    Itself Ss authority hit Nn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
