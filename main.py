40ffda275deb97e5386e13577dcd99a7c3b09c95
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:13:02 +0000

    bored file narrow impatient cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
