7f6c63ef9e607b60cbad457acdeae8be921d598c
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:14:40 +0000

    week should tooth alone cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
