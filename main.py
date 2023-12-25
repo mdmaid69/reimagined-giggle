0148471077746f5e087afaa2ce52748b4641751f
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:46:19 +0000

    translate second branch immediately history

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def calculate_area_rectangle(l, w):
+        return l * w
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
