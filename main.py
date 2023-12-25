87981add41325cfff5ee636c4dde0a7f44d3b2e1
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:18:59 +0000

    live whistle formal naturally it

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
