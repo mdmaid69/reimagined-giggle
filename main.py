b0362f905689a8a344805431b4d91643aa55e5b0
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:41:43 +0000

    564encourage admit compare head comedown

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_power(work, time):
-        return work / time
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
