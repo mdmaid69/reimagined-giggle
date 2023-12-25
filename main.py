d5b22216b86f649449056687394accc15d66d1a5
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:48:05 +0000

    close difference broken if married

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_arc_sine(x):
+        return math.asin(x)
