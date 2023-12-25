df7d455c7781ff104b696e4afc1f30dff4e45cb6
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:51:08 +0000

    rise stamp sorry fat used

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_stack():
+        return collections.deque()
