ee5a2bd6fc1566a85942ea87f6bfdd39751b0821
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:28:00 +0000

    basically lookback(on) spare thirsty poison

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
