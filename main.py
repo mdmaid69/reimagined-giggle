1c0e47d5b856155c89b10ff84b87bb3d873dcd68
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:16:58 +0000

    use him side brown lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
