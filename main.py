b9ef3ac16d8bd0485a8b50ef1741ab81eea87ee0
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:30:52 +0000

    science popular factory impression mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+x = 10
+y = 20
+print("Sum:", x + y)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
