6636793775fc95ef1b2b732e071776d1fa83de4c
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:37:32 +0000

    bother complain breakinto wise butter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  def add_numbers(x, y):
-        return x + y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
