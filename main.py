63c6cd8243cc7ee5960083f2a70e93dd0b7d4a4f
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:10:37 +0000

    wise frame when skin gap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
