c0e72bf63c965c0c04f28c1f848ea9b35154d391
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:36:51 +0000

    shopping accident criticism form dead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def multiply_numbers(x, y):
-        return x * y
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
