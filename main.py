2e76b34cb2457b28f008dc0e796caf1e3c583677
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:04:33 +0000

    turnup false wrong blame belong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
