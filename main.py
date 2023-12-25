4061ffb9d99eb713f5bf0472c8440c9daed08d24
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:51:26 +0000

    guide sight glad loss scrape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
