eefb9b5b1b2bc0b00de00d8fd0b3db30f3797678
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:23:57 +0000

    work by unable occasion pain

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
