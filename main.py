0e7f9dfc2b42218d1055b69a1b1a574ee01ad149
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:37:47 +0000

    break apply human nothing claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
