8e92d2391a4aac28b305cc3d42077883454ac380
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:43:25 +0000

    callout female cross weather travel

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
