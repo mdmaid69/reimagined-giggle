937c846def7ec98717f0282bf86e0ef0bc1678d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:33:44 +0000

    close drink who shadow manage

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_typecode(array):
+        return array.typecode
