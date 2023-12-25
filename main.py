9b50ea144b9e46bbe07e6ce68c9e67966f055f76
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:09:56 +0000

    patience Kk target its speak

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
