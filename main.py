16c632eb4d6685dbb0871b092ef016f9d144e04d
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:48:12 +0000

    pollution shape today crowd print

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
