9e7c626f78d18562fbe21ff6ecbddbcda632e103
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:13:19 +0000

    sleep hurry invitation mind unpleasant

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
