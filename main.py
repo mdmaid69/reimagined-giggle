c944e1461660cbfd54f542cc34c43ac0e90aec60
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:31:03 +0000

    hers trust team fear basically

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
