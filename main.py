c5d0369fd28283535a717c6114c4a4e6242bd774
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:41:18 +0000

    everybody exam swim pipe aware

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
