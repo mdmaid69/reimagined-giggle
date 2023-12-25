ce862d2641f9c02d87c42911545a7879df4e0150
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:14:53 +0000

    tread holdout sharp tense crime

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import datetime
-print(datetime.datetime.now())
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
