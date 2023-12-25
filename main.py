fcb17c429baa4c2611f90299f33c0db9da37e854
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:04:29 +0000

    memory once creature then afford

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def remove_duplicates(lst):
+        return list(set(lst))
