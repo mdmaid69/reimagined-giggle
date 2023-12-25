e90dda863b06d13ee2578571012371fd9d48a7c6
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:08:36 +0000

    priority material communication moral suit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
