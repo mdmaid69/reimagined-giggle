258aba1e6c40eb238723371ffccef8837ae8a4a2
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:41:21 +0000

    climb argument disagree apparently sign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
