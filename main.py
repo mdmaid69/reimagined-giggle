a5cb55b402cc73b333b92b6bac3206cebaeb70a1
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:00:24 +0000

    unhappy copy fit pair stand

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def count_characters(sentence):
+        return len(sentence)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
