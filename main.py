09afeb54a19f95af63664b3c4f3f44f2e6de42a6
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:48:51 +0000

    listenfor depend push expect Ss

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
