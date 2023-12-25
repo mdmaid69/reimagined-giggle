c702875a32332251907e90b9238186bdfd201123
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:25:38 +0000

    cost onto goout neither order

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+text = "Hello, world!"
+print("Words:", len(text.split()))
