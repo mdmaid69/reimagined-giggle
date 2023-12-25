e0633be8d69f5596444367a1c860eb874c7d97e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:21:06 +0000

    love couple name awake service

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
