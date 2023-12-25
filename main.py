337f5c10e567640f680d2b7bd35a2ec7a168a73b
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:36:58 +0000

    false little sticky explain whatever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import sys
-  def get_python_version():
-        return sys.version
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
