db71030e0afcb0fed15c76e2553be941176951b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:09:43 +0000

    maximum coffee belong slow out

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import getpass
+def get_username():
+        return getpass.getuser()
