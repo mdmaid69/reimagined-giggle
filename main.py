a99e66854a74e652ad9077cece56f60c6b68b67f
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:03:59 +0000

    listenfor object same step reliable

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_as_repr(array):
+        return repr(array)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
