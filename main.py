427971c7132f1ce9e771db06f9d2682fff4b727b
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:30:41 +0000

    ignore schedule unusual sky influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
