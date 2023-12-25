3dd657b849c73422df3760b3e0a84ebbe4deb93c
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:42:05 +0000

    business theirs invite game gather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
