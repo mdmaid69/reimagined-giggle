4b0f4ef1de7aef1a8c4dcd43a4bb5baf075368a6
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:03:07 +0000

    weather performance spill unkind Ff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
