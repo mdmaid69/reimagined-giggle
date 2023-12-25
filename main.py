700780f48d0e8f8e2bb4870f505e934a905a3263
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:13:56 +0000

    cruel home though design broken

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
