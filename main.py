8f5925e4ff35a19dc146d74f999b721a14003bcf
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:30:46 +0000

    loose actual recover advice sing

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def add_numbers(x, y):
+        return x + y
