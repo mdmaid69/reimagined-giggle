014f2eb35a7fb5cb143f6af2684ccb3a763f056b
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:17:22 +0000

    behind thirsty sale ours shop

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
