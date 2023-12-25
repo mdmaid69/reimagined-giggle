4b99af9f8ff567eaec2037e5674b0a0016e5fc5a
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:30:19 +0000

    child tap top 569enjoy excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
