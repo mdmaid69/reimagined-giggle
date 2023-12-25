e67a5526d6e11ac17b2941d2e45c4dfde0008e5c
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:17:27 +0000

    necessary apologizeapologise suit treat no

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
