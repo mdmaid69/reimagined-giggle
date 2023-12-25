d1e26f3baefd1e3baddf7b68c404c7a7ca9b5727
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:43:12 +0000

    arrangement contrast create fair about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def square_number(x):
+        return x**2
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
