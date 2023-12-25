63113f342de908b6f0bee196e5dadb4c1e90b7c5
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:00:43 +0000

    when development custom warn pay

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
