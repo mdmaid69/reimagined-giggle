e3e0fda54eb5b1e1242c0341823c2f5825b11da2
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:29:12 +0000

    special walk travel flood eager

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-for i in range(10): print(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
