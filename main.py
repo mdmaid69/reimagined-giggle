3207f75f614fc762cc782e178f1471baa1f72d39
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:11:46 +0000

    empty related lost thinkthrough hang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
