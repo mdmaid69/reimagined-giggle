9245eeaa7a5a8ae3581d48bb22186b1a1ac6b6fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:09:44 +0000

    keep born shape black sleep

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-def add_numbers(x, y):
-        return x + y
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
