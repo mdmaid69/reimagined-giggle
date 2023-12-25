19bd214f1a5cd06dc12b1fff88855d23f6a81bb0
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:27:43 +0000

    welcome afternoon mess powerful lose

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def convert_to_hex(n):
+        return hex(n)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
