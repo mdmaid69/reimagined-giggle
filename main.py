379e18e58f3c00e2ea2562a6f5c890522a431f58
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:55:52 +0000

    finish concerning jump manage search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
