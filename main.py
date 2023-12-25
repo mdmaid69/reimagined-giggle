80d463a25cc81ecf84163505561936145cc0595f
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:40:01 +0000

    tax proud repair discovery mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
