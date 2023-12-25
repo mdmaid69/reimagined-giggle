78c9a992d4bf37a39b4c9ca1d4f178514fd53586
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:25:55 +0000

    disappointed defend usually strength dark

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def get_current_working_directory():
+        return os.getcwd()
