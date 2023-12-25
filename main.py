7d4483d870dd656109fb312e955ee31aedcc72a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:08:16 +0000

    preparation left trouble hold jump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
