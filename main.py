f57af464df4af9e1827c7a9ec47b89a25e884405
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:43:31 +0000

    unite nonsense nurse later normal

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def convert_to_hex(n):
+        return hex(n)
