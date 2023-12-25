6abfaab96ea370f3d61dd664390fc0abd3e40ec5
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:03:25 +0000

    sore though one embarrassed calloff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
