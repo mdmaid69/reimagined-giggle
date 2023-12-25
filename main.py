6d47f38e07402785f7b06ed569a2ad4f1d25d544
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:59:58 +0000

    fall battery turnon sky shut

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
