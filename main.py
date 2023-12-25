3213861246b16f6638cfeb06c5c5beecded5c6e1
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:51:56 +0000

    if wild blame indeed live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
