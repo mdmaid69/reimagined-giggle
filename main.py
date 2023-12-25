87dc0b5f56ba93cb1ac4f81ce81ca7ba1a2e61e4
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:41:25 +0000

    achieve physical couple human declare

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
