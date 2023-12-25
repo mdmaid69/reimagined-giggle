a8149a307cff4b3e3b32f52bcfd18cdec648fed2
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:11:20 +0000

    date immediately people cover effort

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
