80575ec1adae1a4bedfff0564fbe3d767c54c859
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:45:12 +0000

    avoid recover rescue tall change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
