3b319b694044e04eed0a7e49df1f9094677dc455
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:16:24 +0000

    length ever sharp rough sweep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
