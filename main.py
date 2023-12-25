427864ed648291c866cb250af58375ae15f73155
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:10:39 +0000

    the sit afraid sure book

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
