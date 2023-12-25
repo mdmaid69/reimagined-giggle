60139ac5004ec44741e0d2f4fb18c86d4487555c
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:22:11 +0000

    imagination however style together quickly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def count_characters(sentence):
+        return len(sentence)
