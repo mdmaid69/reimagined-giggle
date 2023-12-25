c6445bc7f892b2f16f35b9cb79e1d1d178a83948
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:16:42 +0000

    side funny organize straight lookafter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
