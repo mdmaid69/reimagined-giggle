15d2916c6b5dc6d8a72c9e22d05e0a57cfcba73c
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:13:46 +0000

    breakinto necessary run maintain no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
