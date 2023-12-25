1ba84ab501cf1cc773cb13f622b236376d0ff092
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:33:37 +0000

    be finish keepup holdout could

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
