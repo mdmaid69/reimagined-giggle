e0d5985b348c1af41baceabd8c39a18bf371a316
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:28:48 +0000

    slice possession easy human your

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
