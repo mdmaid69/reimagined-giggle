124ec51a2610ae1a1d2d231df956ea020d61234f
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:15:13 +0000

    male dance feature hide neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
