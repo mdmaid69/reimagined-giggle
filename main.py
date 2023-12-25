122561898d81cdb7af0f3d222073c14676a4469f
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:36:00 +0000

    forget wave division keepoff wet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
