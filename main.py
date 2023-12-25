77df1e44a96db616aa618cfdc094fe0094df406c
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:26:13 +0000

    instruction impatient than relation pure

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import sys
+print(sys.version)
