a639ed4a444eaef737b9756a79b9e5f891a7f9f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:51:50 +0000

    strength receive lesson seat basis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
