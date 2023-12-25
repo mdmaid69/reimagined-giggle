88499694387a6d66f0775a8583cb44d63b318f86
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:24:18 +0000

    aim 573entertainment book build favourite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
