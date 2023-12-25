7e11e7866e47ce063f8ee42ac30a39f690c00945
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:54:54 +0000

    example use empty trust offend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
