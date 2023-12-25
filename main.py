38088a7652e2243558c4d4db13d80ffb02469482
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:39:46 +0000

    history criticize admire contents flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
