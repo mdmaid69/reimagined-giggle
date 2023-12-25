b4525491754dd965600e33555afca0a190c9058e
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:01:39 +0000

    church break goout class gift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
