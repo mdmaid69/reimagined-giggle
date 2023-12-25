8ac65ed70bd532db351e25483e723fa39e9ee290
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:19:54 +0000

    crash embarrassing authority quick address

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def subtract_numbers(x, y):
-        return x - y
+def square_number(x):
+        return x**2
+def calculate_area_rectangle(l, w):
+        return l * w
