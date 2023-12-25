15823b10249eaec49391522b3d127698413b5462
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:18:02 +0000

    improvement activity young discover mystery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
