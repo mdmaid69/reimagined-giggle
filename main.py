922797bf3f5d82e81ca948344f6e02ea4b0f32c9
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:04:41 +0000

    mixture fear young decide against

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
