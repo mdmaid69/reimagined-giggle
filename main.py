d1187cfa48f4dd35a0bdf35a448efc4d1f9f28dc
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:45:03 +0000

    climb until physical tax Mm

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_unique_words(sentence):
+        return set(sentence.split())
