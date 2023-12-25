5001e0a043f39c748c0a73a970f31019cfeb2ecf
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:27:22 +0000

    edge food defeat television conversation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
