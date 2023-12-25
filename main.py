0784c53394cd209d184bbcbe24bc65022e1499b9
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:05:46 +0000

    mean who tune rest influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
