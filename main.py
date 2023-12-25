283ee5af6c2d1cae44abcd3e768b7477a0cee7d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:09:42 +0000

    character trade fail reduction move

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
