5c66f26dca0a484e1af578c46c3ff425367f6632
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:11:34 +0000

    full approve town drop turndown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
