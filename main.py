59b1e714dce68ef9a6c9f7c9cbbdfa4f8854ad35
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:26:09 +0000

    spray me future main weekend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
