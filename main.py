957abac8388498bd8123a4564cbfffdd7c8ceda5
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:08:07 +0000

    know although leg straight live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
