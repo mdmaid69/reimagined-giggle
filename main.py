644b8d99151530fe40394b37aaa5d9d2585f10d2
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:20:08 +0000

    Bb fix win hurt limit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-def find_unique_words(sentence):
-        return set(sentence.split())
+def greet(name):
+        print(f"Hello, {name}!")
