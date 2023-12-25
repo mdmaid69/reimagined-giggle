68dda753b6169642e233e6476b08add67d67c10f
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:38:54 +0000

    light move charge family tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
