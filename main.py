94749e18f836a35212b602fd8799d9a39d59d03a
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:34:39 +0000

    damage surface suddenly stop situation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
