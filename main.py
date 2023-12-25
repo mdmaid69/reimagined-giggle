03c5bd5a05abf8c37a5c04ec12a5acbabe068e71
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:37:06 +0000

    uniform cost direct worried comeup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+name = "Python"
+print("Hello,", name)
+import array
+def get_array_as_set(array):
+        return set(array)
