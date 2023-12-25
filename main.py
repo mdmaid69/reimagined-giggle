a3d735a3a05cb5717dfacac707ca4a0c9d7cf3fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:39:25 +0000

    wind finish stand stretch runout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import re
+print(re.match("h.*o", "hello world"))
