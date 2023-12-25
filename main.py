4f73d5a978ff5cb747c41e1a53350af0896ab9dc
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:36:47 +0000

    accidental line him seat rent

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
