b8d1aae1722a9a37293debad3acb52b953a4bc84
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:54:29 +0000

    fit rain line contact judge

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
