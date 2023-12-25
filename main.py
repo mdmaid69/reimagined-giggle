5edcbc5d8d3be62942ebacdf0bde684985972795
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:59:30 +0000

    information responsible Ww firm check

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import os
+def remove_directory(path):
+        os.rmdir(path)
