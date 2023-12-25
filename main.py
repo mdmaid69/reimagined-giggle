8391481fa2b1b5eac4b44f9aaa8938e7f7176ab2
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:56:55 +0000

    possible see greeting above charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
