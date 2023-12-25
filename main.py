551fd5e2f17791f4bac3355829e38485d447ce06
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:56:30 +0000

    Rr play somebody talkback occur

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def count_characters(sentence):
+        return len(sentence)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
