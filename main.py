c2026220ba197ff10c21ccddea5fb154d36e2dab
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:47:57 +0000

    niggaacademy ask announce permission comment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
