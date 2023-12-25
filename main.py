c603ff4cc6b3982238ace241a6aa838191134a93
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:34:03 +0000

    dance correct pin bar protest

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_unique_words(sentence):
+        return set(sentence.split())
