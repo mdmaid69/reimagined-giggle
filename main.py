c5866c462ea3b2256ee0564897460801b6b73c26
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:34:36 +0000

    hardly it principle window luck

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
