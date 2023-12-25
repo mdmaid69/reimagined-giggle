2546368e988a9e80749a560d06d62b2828d1c390
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:22:43 +0000

    most suddenly central reaction machine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
