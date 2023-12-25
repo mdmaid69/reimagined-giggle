1b5011efef191ec781c900c8e1bfa36bf44dbe65
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:35:47 +0000

    knowledge on Jj whether satisfied

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
