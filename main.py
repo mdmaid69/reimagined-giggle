628d3e35a7a5d2561502a52f62672fcc7fc7253a
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:12:51 +0000

    criticism meet copy suck equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
