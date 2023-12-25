b28ccb316df4c4c240a35d947ee1ed0bbdfdc500
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:04:13 +0000

    introduce shop special open early

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+name = "Python"
+print("Hello,", name)
