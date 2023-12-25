48b5967760ad7ff08a85cd2881a822e68270c90d
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:07:23 +0000

    skin decision step fierce container

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
