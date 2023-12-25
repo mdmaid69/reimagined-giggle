dc7ca7b71064742899b9dadfeb018b08b246abaf
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:07:35 +0000

    behind beside male run avoid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
