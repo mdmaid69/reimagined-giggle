9cba80f94f2c7146af77e894e4f35ac47e1ed9e8
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:17:47 +0000

    dress sea dull programme wash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
