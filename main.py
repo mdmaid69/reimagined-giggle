b0a725e85e65358c418ee220e775e85ec6ee0dcc
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:45:06 +0000

    sore trap brother guess moon

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
