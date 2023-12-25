43263cd55d48bdd3ab3cbe27c2be949ca2d6c45a
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:19:02 +0000

    buy tap typical excuse meal

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def find_max(numbers):
+        return max(numbers)
