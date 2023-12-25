e2215c8e5565654d043241f12b74e19f80e2dbbb
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:28:51 +0000

    suggestion season getback trust wish

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def is_even(n):
+        return n % 2 == 0
