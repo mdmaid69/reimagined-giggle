4ba88fede50774fda7b3b83357a4d4fe07b5d59b
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:46:09 +0000

    dear brown tonight offence there

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
