8e0d98dca55b12bd4a4f68ddc81fc04f773cd91b
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:18:31 +0000

    wash advanced limit brave exciting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
