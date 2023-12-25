0460933306e4994b34f9aaf93c0c3d5d8d8a367a
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:39:35 +0000

    amount frame sell kill wake

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
