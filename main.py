55ab1d8357fb726e93c1381a377a51611f859f3c
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:34:09 +0000

    about agreement eye pause mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
