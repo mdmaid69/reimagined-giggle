5138ed34d3263b5c0ab9811d7d2378aff9e79107
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:14:44 +0000

    grateful which dish die meanwhile

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-for i in range(10): print(i)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
