a783c618fd5f83ab6a4aa6fb1b226f87443eb057
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:59:33 +0000

    badly run form sore entrance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
