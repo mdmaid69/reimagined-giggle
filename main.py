ac38d1e1a823fd884198f406c55621dfb8442b5b
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:37:59 +0000

    important water alone repair usual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
