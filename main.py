3bca57037963ca82ea1b60dca45602d8c48eca14
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:03:34 +0000

    rely problem dream trap hit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
 import array
-def get_array_item(array, i):
-        return array[i]
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
