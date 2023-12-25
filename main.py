96a99e9aff7f4fb1dca376a88c5f6bef78153c17
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:36:38 +0000

    shrink appeal complete violent stamp

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def calculate_pressure(force, area):
-        return force / area
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def convert_to_binary(n):
+        return bin(n)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
