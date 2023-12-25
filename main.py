574280033f241e6b6d7305da8877fbcb53394d3c
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:00:19 +0000

    Gg unable decide thumb divide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
