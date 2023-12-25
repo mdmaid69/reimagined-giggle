a56cd01329ff97aae3cfbd2b764d2c20afabdde1
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:41:59 +0000

    position advise while tall key

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
