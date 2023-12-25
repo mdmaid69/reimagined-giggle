15333840276fc0709670a3f1b096c7adcc9ea98b
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:38:33 +0000

    way difficult unfortunate distance further

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
