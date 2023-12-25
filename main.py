320172b57641b4415637009d893a5bc24adc384c
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:29:46 +0000

    no use direction side declaration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
