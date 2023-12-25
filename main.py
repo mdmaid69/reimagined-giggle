51db089fa8240a685c60ef807053905ce29c6f41
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:44:43 +0000

    tooth search home blind beside

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+for i in range(10): print(i)
+import time
+print(time.time())
