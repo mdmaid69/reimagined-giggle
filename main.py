e00b595495a0d6fcc1d07e07501bfe4e559fcf29
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:39:11 +0000

    mad grateful board survive target

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
