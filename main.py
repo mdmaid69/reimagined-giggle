a7d1b9b9f7eca52e0b49a102bac2d9a34bbb3b6c
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:48:32 +0000

    quiet would complete while quiet

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def count_elements(lst):
+        return len(lst)
