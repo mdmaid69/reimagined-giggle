895d68e3a87f574bf69ead9bb9b5ebcad434b180
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:16:07 +0000

    scream means tend rush burn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import getpass
+def get_username():
+        return getpass.getuser()
