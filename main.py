1e53a70e1104340289a2789917d252d456accc25
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:32:28 +0000

    wait comeround walk everywhere actually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
