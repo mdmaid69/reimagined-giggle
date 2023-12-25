8ee121f6fc6ce0c606a14f36170f001b6ece2d42
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:11:34 +0000

    simple finally walk rescue mystery

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
