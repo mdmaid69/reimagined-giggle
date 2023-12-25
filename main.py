323678cce63851f0509142e2426d562d50d30a08
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:43:12 +0000

    tonight less spring mine announce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+def change_working_directory(path):
+        os.chdir(path)
