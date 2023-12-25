50be2cb1151d9e77bdf4321219fa7c5ea043e766
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:32:14 +0000

    entrance finish may trial luck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
