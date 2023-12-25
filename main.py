c2eaa7d9af167274b703303f4f3678c0b4fdbf51
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:36:03 +0000

    right occasional honest minimum town

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def is_even(n):
-        return n % 2 == 0
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def sort_numbers(numbers):
+        return sorted(numbers)
