4c798bb0b31d0c220e186b16fdee5423b955a005
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:35:08 +0000

    cruelty both stand nose healthy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def get_today_date():
+        return datetime.date.today()
