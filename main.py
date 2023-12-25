1db35c85aba63dcda365536f555cd9a4435cc52c
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:44:00 +0000

    authority rob game somehow weekend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def square_number(x):
+        return x**2
