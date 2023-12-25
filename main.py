4679dade74e01b5c57dbc1e0802dc6f33eefcb80
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:16:10 +0000

    frequent comeoff cost aware arm

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
