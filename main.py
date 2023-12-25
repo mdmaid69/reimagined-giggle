86250b3b5d902ab2ab3f31efdf6b20d84b41d43f
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:59:36 +0000

    crash mean happy crowded brief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sys
+def exit_program():
+        sys.exit()
