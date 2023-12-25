74582d1af009e2cda7d50153a013785dac737d3a
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:21:06 +0000

    compare 572enter exciting task list

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
