f59a7049d2a74c50584354656239dd95ae46632a
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:44:38 +0000

    slice apply with hide himself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
