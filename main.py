754086f4a296514fd4fb6a21187a40bcf45e20b6
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:21:29 +0000

    lie show gradually minimum active

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_length(array):
+        return len(array)
