e068bfada44c715418e279198f9de0a66d01171c
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:48:49 +0000

    climb disturb view number final

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_power(work, time):
+        return work / time
