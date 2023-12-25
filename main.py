b3bd76e4267e12b06949f437d3d99be4501ab057
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:15:37 +0000

    bother left respect help poison

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
