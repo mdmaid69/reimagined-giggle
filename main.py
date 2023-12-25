a51b4c7565b44c7fd59052c6c5e2d3ef988aa1b9
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:16:00 +0000

    curious round lonely school discussion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
