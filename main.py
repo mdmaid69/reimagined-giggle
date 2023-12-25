f76d598ed53c99d3f584a0ebdcb1d89574c3d388
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:59:18 +0000

    callout eye hurry bad message

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
 def factorial(n):
         if n == 0:
         return 1
