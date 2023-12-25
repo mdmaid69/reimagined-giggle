75971fdfcbbf5ae7eb082ea7616863ea84c9e66a
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:57:50 +0000

    warm apply slice market good

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
