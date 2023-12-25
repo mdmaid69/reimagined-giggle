438ea81166aa469b0c218d466560cb8f6ab09001
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:26:35 +0000

    view nervous middle separate about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def convert_to_octal(n):
+        return oct(n)
