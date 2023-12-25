dd479a6454169cb202fc5680e3ad4f4e8902ea26
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:57:43 +0000

    important play family tax emotion

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
