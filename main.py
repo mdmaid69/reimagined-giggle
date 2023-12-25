25194703d90a56688a6a56ebb8ccc9306564fb71
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:04:31 +0000

    flow heat goup 565end cool

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def find_min(lst):
-        return min(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
