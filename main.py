90ae1c1fa7bebd4ed769700bfd6e3bb32a0427b7
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:31:22 +0000

    off foot what hide pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_blocks_allocated(file_name):
+  def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
+import array
+def get_array_as_set(array):
+        return set(array)
