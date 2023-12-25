4e09513f241eabc895aff7710f90629307ca8387
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:53:50 +0000

    uncertain confidence act finally fear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
