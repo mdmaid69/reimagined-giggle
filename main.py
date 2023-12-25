f87e2ebbe8a00cc1f5f79bd3e8238c41381f3bc3
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:15:57 +0000

    respect hungry typical move worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
