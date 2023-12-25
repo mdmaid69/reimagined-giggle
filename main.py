cedc72e09e9ef15529ed965f5aadd843a4719c21
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:40:37 +0000

    view quickly she matter home

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
