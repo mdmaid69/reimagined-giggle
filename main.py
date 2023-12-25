65b02c56720c59789a5cdcc40a134c09c4e8fa64
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:41:27 +0000

    understand dangerous refer takeover previous

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import datetime
-print(datetime.datetime.now())
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
