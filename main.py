fa6319aa0042e629f6acbbc1463f4336a305bf56
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:07:31 +0000

    opposite familiar exercise tomorrow scare

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
