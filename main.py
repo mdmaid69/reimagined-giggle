3086251d928065d16170d0e18f5e7354edafadf9
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:46:30 +0000

    tap test affect border several

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import collections
+def create_counter():
+        return collections.Counter()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
