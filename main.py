bc0a6f223c076cc9e053b0ab64e364fa3695da8f
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:39:29 +0000

    print breakup soft notice line

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
