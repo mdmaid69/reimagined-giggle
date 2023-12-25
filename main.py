4f6eda03473fe1e607f8317a42de5ab7e3186502
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:07:37 +0000

    cap greeting quiet afternoon sorry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import time
+print(time.time())
