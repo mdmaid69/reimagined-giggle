aff1e91428b6dbab078df763762786554f618a97
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:53:45 +0000

    somehow Oo pipe wire stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+def print_python_version():
+        return sys.version
