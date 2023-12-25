1536895f9767e2ee11e28166460a8f5236fec333
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:21:49 +0000

    pleasure obvious weekend constantly threaten

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
