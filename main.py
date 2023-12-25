382b6f93487ffbfbcdfd1bc5465885b3dfd6eb36
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:33:53 +0000

    race hit meaning breakout suit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
