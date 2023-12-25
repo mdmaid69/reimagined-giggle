3ffc0aaacfbc11841d164196b2a867b767449962
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:34:48 +0000

    pattern pocket father lookafter age

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
