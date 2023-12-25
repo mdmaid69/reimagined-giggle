b2c8d5376d68f1237523bdbd698bf8adf0ab1a70
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:08:51 +0000

    fierce cry turn imagination very

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
