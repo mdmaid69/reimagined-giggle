cea897b23cdd660a02a1d350810670c6ca81a328
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:27:20 +0000

    jump mistake runout measure hello

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
