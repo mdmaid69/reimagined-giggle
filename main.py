e87502b33be05b2267c908064296b39a5e721d50
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:12:19 +0000

    chat exactly reliable criticize mainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
