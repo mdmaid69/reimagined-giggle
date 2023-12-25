2a97e835bf73f1c1ed5c7680c03b54ec843a4180
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:48:28 +0000

    themselves bear getup emotion cross

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def reverse_list(lst):
+        return lst[::-1]
