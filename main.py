53adc1b918da20df41f29a3f13be4f83b95bd366
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:21:35 +0000

    visit comeout tear mess suck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import sys
+print(sys.version)
