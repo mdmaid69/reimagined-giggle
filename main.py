2cf6865730ae15dd2393fc8f587515cfbc2ab363
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:13:49 +0000

    inform welcome motion visit tidy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
