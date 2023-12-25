511aeddafb466a1f97cc59ffbe41923645272c8e
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:24:25 +0000

    boat ignore on hers movement

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_item(array, i):
-        return array[i]
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
