2c7a8d3c4efe1588347c5fb3bcb1c0cefd7c7af8
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:31:31 +0000

    terrible chance hungry quantity wild

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
