ac43fbb534826f90b9969f68c7b6e767171ee48e
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:24:30 +0000

    extraordinary shall there common close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def remove_duplicates(lst):
+        return list(set(lst))
