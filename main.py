3068ead485485be8413bbc9f1175f2a811083d71
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:18:48 +0000

    stop experience substance ashamed convincing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
