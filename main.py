47a39218a1a043c7467c4a82a3022c37bf5ab90a
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:56:26 +0000

    figure dance provide expensive long

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
