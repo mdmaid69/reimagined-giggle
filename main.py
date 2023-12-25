cce28ce7742cb52f72e2e1f35a538c7cf3b0586e
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:48:01 +0000

    sting visit pile chance typical

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def sort_list(lst):
+        return sorted(lst)
