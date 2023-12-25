e475fd8b49008fedd0ec4184b1ae23b5c07ce103
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:09:18 +0000

    respect advantage freedom organization drink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
