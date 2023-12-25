ae233a45f9304c2487205ccc628a31e4eb95d0b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:36:36 +0000

    no advice talkinto their lean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
