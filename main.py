2dbf888aaa92cfbe4d8ee4c5bdf3ab577e5e18af
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:31:37 +0000

    song party exam reduce same

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
