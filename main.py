2d575085020f2c9f71fed3c07371b82b80d30732
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:40:03 +0000

    search practical report perhaps market

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+name = "Python"
+print("Hello,", name)
