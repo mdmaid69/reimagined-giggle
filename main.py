48a162d10a349975a28209a58f01d8ad5d3252e8
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:25:29 +0000

    expect sound ride joke speak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def add_numbers(x, y):
-        return x + y
+def find_min(lst):
+        return min(lst)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
