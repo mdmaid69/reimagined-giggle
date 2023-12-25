8a35c66c5411c2849d218c701cf5358a4dde7f3a
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:50:20 +0000

    tongue guide stick frighten boil

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import getpass
+def get_username():
+        return getpass.getuser()
