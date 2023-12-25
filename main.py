c024774540960f7b119677b5c92753be5fd2ed41
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:21:23 +0000

    error science deserve smile wherever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
