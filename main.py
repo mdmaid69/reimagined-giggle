b538f81d31a5eb0f91ebecd7f39f0a8b0f37d1a2
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:14:19 +0000

    guess satisfactory general black usually

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
