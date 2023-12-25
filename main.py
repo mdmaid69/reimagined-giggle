2aeca88f4f048bb48e85d214d268b14282412a6c
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:47:32 +0000

    build recent deep swimming part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
