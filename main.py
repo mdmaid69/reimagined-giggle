ad1ec050371e470358eb5175985b9b7aeb75011b
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:54:27 +0000

    character makeinto alive seriously centre

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
