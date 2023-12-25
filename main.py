34bb93956080cc06e981a7048329c1a609effd6e
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:25:52 +0000

    yours very respect badly legal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import sys
+def print_python_version():
+        print(sys.version)
