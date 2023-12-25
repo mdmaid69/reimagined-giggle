ee50f0decbfa180e697825a778a4054603256a28
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:22:26 +0000

    heart anybody tonight border see

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def is_palindrome(s):
+        return s == s[::-1]
