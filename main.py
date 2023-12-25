8266030727897a47beeba139001de23e2d0f498d
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:18:45 +0000

    defeat usual common exaggerate thorough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
