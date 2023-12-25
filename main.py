bdbd2dedb60c3a54ae9f28d614a9e16daa05c2c9
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:44:42 +0000

    sort sort damage uniform blow

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def calculate_density(mass, volume):
+        return mass / volume
