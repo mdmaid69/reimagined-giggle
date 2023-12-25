5dc824dd07e8a502851c43e11e28671434ced0f4
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:35:23 +0000

    project Pp learn so set

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_str(array):
+        return str(array)
