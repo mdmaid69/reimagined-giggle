d02581e7ae302c59b4569d1172473b6c258409d6
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:36:43 +0000

    process weak great ask tap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
