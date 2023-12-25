7e8444c3c8cc4ced423e897e37e58c7f9a439e88
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:21:17 +0000

    remember neither light television father

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
