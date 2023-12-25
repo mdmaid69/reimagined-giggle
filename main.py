6b2d81fdc020145e0e8d97d15c769ea982e9b7da
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:52:25 +0000

    claim couple often reject pile

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_power(work, time):
+        return work / time
