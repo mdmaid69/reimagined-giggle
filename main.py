c11bf6c128ff8137df87fbbef5810ba61bc06818
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:53:13 +0000

    call battery body towards hour

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
