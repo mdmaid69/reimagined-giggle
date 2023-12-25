59425249a00522a99be0fa504714502481860a4e
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:04:39 +0000

    Hh official run dry charge

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def convert_array_to_unicode(array):
+        return array.tounicode()
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
