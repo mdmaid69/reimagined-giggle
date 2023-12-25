a1422ab9e4a70557c1bd2154f70e093bb5f373e2
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:06:01 +0000

    comfortable pay his about difference

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
