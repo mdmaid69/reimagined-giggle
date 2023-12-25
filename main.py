bbe95ca66e0e21bfd4c11a7936154c828cef0985
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:03:20 +0000

    takeaway yourself attempt take price

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def add_numbers(a, b):
+        return a + b
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
