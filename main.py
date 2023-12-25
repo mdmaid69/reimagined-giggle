69a51c19f0a850eccbd757ddf0de43b38beff2c5
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:44:17 +0000

    spill lucky motion feel collect

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
