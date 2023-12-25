59fb1e4fd18fbb293ac1a30d283b41ce2f20e52d
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:26:31 +0000

    impressed achieve view rest memory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import sys
+print(sys.version)
