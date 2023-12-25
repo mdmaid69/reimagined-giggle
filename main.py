5b720d270c8197be7d376af3434cec49e04a198a
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:50:52 +0000

    scare into present radio tie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def remove_duplicates(lst):
+        return list(set(lst))
+import getpass
+def get_username():
+        return getpass.getuser()
