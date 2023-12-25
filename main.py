bc12e42c6b90a21e80a5e7253c8f3ef5fc91718d
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:17:09 +0000

    wire news himself plenty class

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import re
+print(re.match("h.*o", "hello world"))
