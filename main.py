1cb1a7fffe1c15a6e02dfd0f755c0fe862294e68
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:36:29 +0000

    risk afternoon considering be reaction

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
