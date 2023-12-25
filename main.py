18db161e3934f668389076ee317a499c7d835e24
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:06:45 +0000

    satisfactory story centimetre brush than

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_power(work, time):
-        return work / time
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
