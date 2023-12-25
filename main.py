a5a41062e5daf605a0c6116a7cdeb6169906347d
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:37:48 +0000

    loose principle respect second pair

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
