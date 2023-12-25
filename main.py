e0e60cbfb4e69eb4da6c6c6730bd19499931b11c
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:53:40 +0000

    stretch welcome mood blind experience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import os
+def remove_directory(path):
+        os.rmdir(path)
