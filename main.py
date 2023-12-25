c86d1b3b420eadd088dc05798fa64a6c97ac3024
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:30:11 +0000

    confuse stranger string Nn culture

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def cube_number(x):
+        return x**3
