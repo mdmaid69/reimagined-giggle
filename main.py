1256b07a28b965c56a5085974fad065ddd44276c
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:44:42 +0000

    bring contain trust point interfere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def remove_directory(path):
+        os.rmdir(path)
