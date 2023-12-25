49a2b2e1b6349fb4d39b9cab4dd747fadaad4af4
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:32:29 +0000

    dream acceptable terrible 569enjoy stiff

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
