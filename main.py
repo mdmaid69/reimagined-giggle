3274d4cbfa01c2ff40a5ce4fa1f83cfdfe846ae3
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:01:28 +0000

    cruelty get completely tool show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
