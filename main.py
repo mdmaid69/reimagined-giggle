1ac957dcff4bc28bd7fd9f426c5929ca21f7cf42
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:11:46 +0000

    push all declare basically desperate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import tensorflow as tf
-print(tf.__version__)
+name = "Python"
+print("Hello,", name)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
