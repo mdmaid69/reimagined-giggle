d4df0bb297910dee744b778544bf768b07fa9d05
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:03:28 +0000

    price convenient friend 565end smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
