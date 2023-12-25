fe863f86e1760c896c91b4a66694d9a85810ed08
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:03:07 +0000

    sort tend phone 564encourage scream

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
 def calculate_area_circle(r):
         return 3.14 * r**2
