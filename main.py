c7c0db999374a25f1986fdd06c1eb5a0afe267f3
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:39:38 +0000

    break warn indeed himself lead

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-def calculate_pressure(force, area):
-        return force / area
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def append_to_array(array, item):
+        array.append(item)
