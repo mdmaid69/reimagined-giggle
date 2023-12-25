a4b4ed1a3d3b78f2ced06b6bf24a783b7fd6566b
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:35:49 +0000

    search ride breakinto loud fetch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
