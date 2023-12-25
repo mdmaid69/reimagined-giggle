cc3d083535f09cff349dc5d9715d0c52620c98c0
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:37:54 +0000

    imagine snow successful valuable remain

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
