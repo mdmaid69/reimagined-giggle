948d88c34e8960b73161a1596003bfd6fef42243
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:54:30 +0000

    tour university interesting glad cap

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
