267beb7ab261da3901f642e21dc9776c119f39ba
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:30:16 +0000

    present block container point length

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def find_max(lst):
+        return max(lst)
