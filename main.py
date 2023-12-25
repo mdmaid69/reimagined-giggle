79b4de8980dbd610b353ffdaf091776ffbf36a6b
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:19:22 +0000

    already past angry catch gift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_typecode(array):
+        return array.typecode
