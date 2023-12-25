dbd02173cb30bb07490d0fe5bc6d168dd1ca809e
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:17:17 +0000

    undo plan fly cruelty watch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
