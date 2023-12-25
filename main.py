23fd9eeb00df4aced988fdbb928d82f95a8367c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:14:39 +0000

    arm Tt around interview admire

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
