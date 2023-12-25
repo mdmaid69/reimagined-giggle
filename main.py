f83156692a401f59c052e9a6e012da5961099b21
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:20:52 +0000

    glad relief now recent pause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
