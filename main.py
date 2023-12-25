803a1358fe74d452cd59dbf3cd73fa9e4d0b848e
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:21:35 +0000

    stop desperate whichever full his

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def get_array_as_bool(array):
+        return bool(array)
