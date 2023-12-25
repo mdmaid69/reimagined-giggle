2ef888bfb0981563ba046384f8960eaddf91163e
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:20:51 +0000

    borrow die each tidy or

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def remove_duplicates(lst):
-        return list(set(lst))
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
