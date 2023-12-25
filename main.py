96952c469f94dab1f2772da1ec49192953bb598f
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:49:37 +0000

    everybody preparation so undo pride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
