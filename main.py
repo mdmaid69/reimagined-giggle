4ed2225fe7545a4fbd3edb4fb754fba47cf2e8a6
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:20:26 +0000

    steer skill belief train report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
