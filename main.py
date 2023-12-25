e7a3f863e572c78ddd430e7ac8cbef49e768265c
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:46:15 +0000

    new nearly public intelligent production

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
