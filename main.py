869fb5378afc0d2d521a203991a6444424dfffc1
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:43:34 +0000

    smell rob bring manner machine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
