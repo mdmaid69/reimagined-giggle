20d67a4bdc0d2e3714b9f2f6250416782a7ba4c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:58:18 +0000

    arrange race recovery close about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
