31900b31e2cd3cc0294eb5f62a89226394a2792f
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:15:34 +0000

    polite watch habit one reject

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
