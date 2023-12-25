5fac480c506f38a49e3eb0922b67aa8e126f4dda
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:42:28 +0000

    muscle little everybody view equal

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
