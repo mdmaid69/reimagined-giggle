53f5c43c733f5f485fce1be52ab0310e36e55c2b
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:03:55 +0000

    soil poor but clearly turndown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
