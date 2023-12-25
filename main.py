bcb7b3a9ee72e8eb320f6bb4e7aa90e7f3a09302
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:18:38 +0000

    busy clean block fortunate healthy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
