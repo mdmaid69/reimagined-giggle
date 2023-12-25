5a1f34850e90bf174d749e66faf032fe3951cefc
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:30:52 +0000

    wide public acceptable occasion 179bite

--- main.py
+++ main.py
@@ -3,6 +3,5 @@ def get_array_from_string(string, typecode):
         a = array.array(typecode)
         a.fromstring(string)
         return a
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
