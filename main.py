382614e6bc1114009288e0276d736be2b37b2a40
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:39:19 +0000

    slip conduct edge must deliver

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def square_number(x):
-        return x**2
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  def cube_number(x):
+        return x**3
