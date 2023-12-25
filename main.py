202a649174b84ed06d7a98bb52117bfd184024c6
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:50:48 +0000

    properly rich temper follow come

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
