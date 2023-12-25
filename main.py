0a9ec5779fbc8a44339b119971d9e4e66db29498
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:32:22 +0000

    getaway bread measure reliable themselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
