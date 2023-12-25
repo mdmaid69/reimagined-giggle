fa43a45891c101371be733243d0fc6d479f0c769
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:12:52 +0000

    need afternoon win glass next

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
