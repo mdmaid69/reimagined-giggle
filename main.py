c1224fdbe09fdb621d0d8afde5cbabc3f9c32a0b
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:04:01 +0000

    pour present married advantage floor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import time
-def get_current_time():
-        return time.ctime()
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_set(array):
+        return set(array)
