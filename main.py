67f03804745d8b3cb7f74fa9551dfae1fedec534
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:45:32 +0000

    noone pronounce woman wear worried

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_current_working_directory():
+        return os.getcwd()
