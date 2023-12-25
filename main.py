f6843b1e3399d7e38d90315fe5a6292611dfce4d
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:22:30 +0000

    better rubbish combination mark never

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import platform
+def get_python_version():
+        return platform.python_version()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
