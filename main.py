cc8359ea108ba7fad8fb99ce1a81131460222afd
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:48:47 +0000

    score weak narrow edge right

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def add_numbers(x, y):
-        return x + y
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_list_from_array(array):
+        return array.tolist()
