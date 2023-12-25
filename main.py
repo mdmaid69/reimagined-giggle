ff5412fc230e0da27925dc47b88d6dea8d9bdecd
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:11:40 +0000

    meanwhile trip attempt interest report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
