5fa7406b93d23e5264155034211133ae35ae04f6
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:42:38 +0000

    date act takeoff pronunciation club

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
