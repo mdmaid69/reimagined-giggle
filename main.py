c311936e2f170ebf6b79348cd8f5c101f59c6b68
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:37:24 +0000

    later bring scared detail surprised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def reverse_array(array):
-        array.reverse()
+def get_list_from_array(array):
+        return array.tolist()
