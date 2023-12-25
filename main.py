5f93ee5f838c37b12d75cb8795ffd7b64eb3efc4
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:22:06 +0000

    muscle argue we idea instrument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import sys
-def print_python_version():
-        print(sys.version)
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
