7d3821008e940bccdfabd5fe6f105a7b7c033b75
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:21:35 +0000

    Tt thanks together to Itself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
 import array
-def get_list_from_array(array):
-        return array.tolist()
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
