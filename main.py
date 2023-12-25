13ff428e8e8f66dbf643c2e59162c1f040798dfe
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:15:33 +0000

    any Vv club weather closely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
