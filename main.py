81f3acc67e870605e96755f46c78c85460ea5a18
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:15:19 +0000

    goaway behave basis flat keep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
