19617054c62234b2642dabc6f7906fda76675e2f
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:51:49 +0000

    collection maintain control everybody shut

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
