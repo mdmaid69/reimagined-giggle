5854b103a5a6ea0d307ac8ca7e1bcfcfb7432a60
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:44:43 +0000

    fetch clear figure oil boil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
