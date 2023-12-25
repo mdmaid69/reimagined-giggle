8282093c6cbd3723e1e905a766a1ad385e775ded
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:42:48 +0000

    cum sharp possible man pen

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
