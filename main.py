bbb731fd6d1c80c8a2e168ef4bd86cd8afc3944f
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:51:08 +0000

    anything scare ground ourselves makeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
