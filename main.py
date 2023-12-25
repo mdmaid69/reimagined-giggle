41f559ca5590a9e4f3c1af77177b58983ed7eaca
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:37:38 +0000

    edge something waste goout beyond

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
