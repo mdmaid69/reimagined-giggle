aa50391130d40c23e38ddd377a0d8f434b48e6a5
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:16:13 +0000

    sign pattern workupto Itself Cc

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
