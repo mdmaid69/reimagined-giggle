ec9e260f31c3f9614d8bb23b1f177a42cb26109b
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:20:06 +0000

    cost recent inform pity skill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import getpass
+def get_username():
+        return getpass.getuser()
