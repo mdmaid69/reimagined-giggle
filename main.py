aaa0710d31f212ecb71dfeab499b776833b50eac
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:14:24 +0000

    estimate concerned view street picture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
