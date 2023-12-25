2750e65590ce303a13ca1b832d9648ca125bbe15
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:06:17 +0000

    question very famous such can

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
