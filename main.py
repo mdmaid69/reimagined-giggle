a5c126bd90f274182393516d1f620f117733572f
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:52:54 +0000

    chief tap further everyone upset

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def cube_number(x):
+        return x**3
