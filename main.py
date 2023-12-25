c71f15df652b01361cd5f66d4da7a2371628cd89
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:05:59 +0000

    this part insist thinkthrough tall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
