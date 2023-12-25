9498449cc8908680da753caccecb907db78e3a48
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:21:54 +0000

    piece private tongue breath confuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
   def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_acceleration(speed, time):
-        return speed / time
+        return os.stat(file_name).st_size
