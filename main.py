c273f1b390afdd869280a06e7b73ec9e601a167d
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:24:55 +0000

    deliberately imagination lonely whether edge

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
