1f5156af483d89910d3856a5aacd9ab91b8fd674
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:46:39 +0000

    shame heat complain breakthrough strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
