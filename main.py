8788e421a249d0616633c4d77e7f870704d192f4
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:23:34 +0000

    frame there whisper grade ours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
