0f045084859e24419d58ad595bc72f56f6b13bb6
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:46:44 +0000

    boring broad round course shoulder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
