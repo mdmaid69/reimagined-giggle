03ede40db7a790c9dd635278ce9650c7a52a281f
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:56:55 +0000

    recovery little content damage grand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def get_array_item(array, i):
+        return array[i]
