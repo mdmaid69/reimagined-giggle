09a179504f9581a1e43263b8ce61d34b985f53df
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:02:35 +0000

    breakthrough reaction cost clear inside

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
