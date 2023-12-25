0776a4e855ce54f459732da95bd7c3fe26cac0a3
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:10:37 +0000

    manner pretend tone complete whom

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
