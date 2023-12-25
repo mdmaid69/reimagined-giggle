80c344beb99844ca3d928d2ded1676ef0e781e54
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:50:42 +0000

    impressed I roof pretend definite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
