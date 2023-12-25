78b2e76da9831c27d526c693ed1b30c856e17a07
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:35:12 +0000

    damage weak gas food takeover

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def count_words(sentence):
+        return len(sentence.split())
+text = "Hello, world!"
+print("Characters:", len(text))
