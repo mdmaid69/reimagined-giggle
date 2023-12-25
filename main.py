868e10dcbfd34a47d889139d9b71104a44693fe2
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:28:15 +0000

    within truth him rough proud

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
