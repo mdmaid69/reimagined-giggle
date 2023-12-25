c579f2f8f3e8a27eaa4bd1c16935e2cc6c1d2927
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:15:02 +0000

    conclusion generally real intend afford

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_stack():
+        return collections.deque()
