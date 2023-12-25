cad9a8bd063898e4a95ad888b2ce497b99e06115
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:18:55 +0000

    concern voice Ll zero breakin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def add_numbers(x, y):
+        return x + y
