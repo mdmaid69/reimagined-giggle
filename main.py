0644f6395fbe2e3c7e083699b561b5d54dabe3b8
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:00:15 +0000

    open rob pronounce treatment surprised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
