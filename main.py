fc86c89dce7274512aefee90f8c2769838be789a
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:24:44 +0000

    tidy sometimes continue demand near

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
