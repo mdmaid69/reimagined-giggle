371e243089314c602c09b962dd0cc9d24ac73636
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:05:46 +0000

    disturbing spin fear group hour

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
