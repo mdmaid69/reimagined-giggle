cf41891eb764acef680cb1f113efa692dd720cee
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:17:29 +0000

    read neither reply public lock

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
