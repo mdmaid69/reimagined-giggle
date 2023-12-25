94e083ed640b62fb93a8371051dbb663170f220f
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:00:45 +0000

    secret clearly regard scene limit

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
