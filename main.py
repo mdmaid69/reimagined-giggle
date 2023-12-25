a1f728aa6a59e2f253b0e40e84ac93df32616b35
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:19:23 +0000

    direction private suffer sometimes deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
