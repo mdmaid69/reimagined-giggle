97f5f2e3a3af8a1679dac98638da732d11dfa54c
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:46:19 +0000

    in in dry behaviour call

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
