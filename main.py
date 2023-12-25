c16a8af89d226bea801d775b11be2d1cf7fd814a
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:21:45 +0000

    progress pour window science promise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
