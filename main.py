fe26b9f86f975fd05c008b611d47d224bed99d2f
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:21:10 +0000

    special thankyou recovery rely visit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def is_even(n):
-        return n % 2 == 0
+import time
+def get_current_time():
+        return time.ctime()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
