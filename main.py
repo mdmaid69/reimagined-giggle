25163503c3c3140261da95af1cc22c547e8fa734
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:10:22 +0000

    obtain cry large formal swallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
