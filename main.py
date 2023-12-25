158dda0490cdad8148b03bcfd763572dbc9eb5fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:16:04 +0000

    zero moment plastic free hate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
