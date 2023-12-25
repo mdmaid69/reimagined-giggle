22444609bdd8f4bd54e9aa3e43ea1871e707ded6
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:58:43 +0000

    run get dress complete where

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
