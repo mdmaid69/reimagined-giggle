5c7a3363cd4951c90a11d689df10f3b7e1d044cf
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:22:29 +0000

    late terrible neat shade connect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_as_list(array):
+        return list(array)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
