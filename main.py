38ad94faa0a74ba4ef64b31280235d15c507db24
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:41:16 +0000

    570enjoyable shadow feature thought exercise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_speed(distance, time):
+        return distance / time
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
