222a7d28ec6b5d531bec2e503fb1eb7a6713c95d
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:02:12 +0000

    protest close neither contents visit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
