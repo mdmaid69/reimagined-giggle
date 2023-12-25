659f59ff1bba596e5c4902da733982ee21496e7e
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:26:16 +0000

    nose along say sing snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
