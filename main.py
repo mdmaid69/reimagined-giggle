110be6fa6ea06310369810e5b6ded01c9d8b8c17
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:58:47 +0000

    gentle new noone discover you

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import tensorflow as tf
-print(tf.__version__)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
