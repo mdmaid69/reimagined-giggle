ca282ac6c6468947c4f4616aa24418a182260c93
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:18:37 +0000

    Tt destroy comment floor oil

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
