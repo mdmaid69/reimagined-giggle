a52c5567cdd48ee262cab168b9525d68084c240b
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:03:53 +0000

    history profit lookup pump idea

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
