bc6911f926d6c7acd67cd56619bb7a145d993128
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:49:09 +0000

    sick object winter split tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
