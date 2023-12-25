4866ef20ae20345f3609155f12acc9b89fc88d27
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:32:58 +0000

    tap sign arrive press now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def calculate_pressure(force, area):
+        return force / area
