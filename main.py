82fba14b11a38982634d0fc88897e1c3567a5cc4
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:08:09 +0000

    rather as ordinary spring direct

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
 import math
-def calculate_floor(x):
-        return math.floor(x)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
