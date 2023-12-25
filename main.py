9b2747c35b834b1d41556bcb5ca0533db2186853
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:37:10 +0000

    exist community splash pot track

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import getpass
+def get_username():
+        return getpass.getuser()
