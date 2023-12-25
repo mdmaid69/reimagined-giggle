a4f8a44b7630e86ab5e16ac8a8762990b0ce94c5
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:56:56 +0000

    answer right legal opposite centre

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
