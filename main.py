b7293687c22d06cfabee466d7be2c26f79c9bb0c
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:04:05 +0000

    peace off warm bit help

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
