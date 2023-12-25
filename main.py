a76daf5227042eded3672732a06ac389b22a4678
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:18:02 +0000

    on decision image snow statement

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_power(work, time):
+        return work / time
