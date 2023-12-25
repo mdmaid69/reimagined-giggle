67c6fd5f6dea7505d78e36943059479f3b03a7ef
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:46:42 +0000

    bring genuine get land themselves

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-text = "Hello, world!"
-print("Characters:", len(text))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
