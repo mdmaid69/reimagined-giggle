aeb2c5a4cdb126d2fe144f2d064e4e912f768f86
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:06:35 +0000

    rich principle hit apply order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def sort_list(lst):
-        return sorted(lst)
+print(sum(range(10)))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
