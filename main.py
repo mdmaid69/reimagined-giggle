3b07bbaf145899b72111bed23ddef38ab993636e
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:14:57 +0000

    this key difference none kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
