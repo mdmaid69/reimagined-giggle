b57922e2768b82fb92a2192d7ca4d2e06f4ec648
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:47:29 +0000

    unfair perform loud own keepoff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-print([x**2 for x in range(10)])
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
