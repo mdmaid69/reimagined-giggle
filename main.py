5a3bf9e2149e72b812cec19c4b4144665706dce7
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:34:35 +0000

    another unlikely business than giveout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
