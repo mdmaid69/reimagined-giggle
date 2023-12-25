70602e5bfb29698a2df0923740744af4d62751bd
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:57:51 +0000

    children balance water top ask

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
