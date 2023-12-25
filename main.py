40a2abec65bd75e3c757a7dcee14b2f5695559fe
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:02:27 +0000

    giveup reach no sting after

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def convert_to_binary(n):
+        return bin(n)
