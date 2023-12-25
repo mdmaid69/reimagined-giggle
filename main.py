f9cad3539069bd66b8e4cddffe46484045e3f1f3
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:57:22 +0000

    there medium black workupto alive

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def count_characters(sentence):
-        return len(sentence)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
