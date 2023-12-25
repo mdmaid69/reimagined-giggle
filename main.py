7df8a1af7b7283193f7ae7bec7a46da94cf5c1c9
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:33:10 +0000

    workon bye thought work pour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
