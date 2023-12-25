94d8c526534835d0ab0afd357c6c953f82a96835
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:35:54 +0000

    unusual relax so slight snow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
