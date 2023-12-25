7987bbd13b38c0711de8b3d0577a1e25ed842479
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:03:20 +0000

    union thick human best scream

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import math
 def calculate_cube_root(x):
         return math.pow(x, 1/3)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
