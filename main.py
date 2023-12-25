3257ed00b636a0edfd1b885bb2193706720a5ed0
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:33:57 +0000

    effect geton always shut description

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_power(work, time):
-        return work / time
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
