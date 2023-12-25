ed60982b5ce9d458e15227226093d3de0a8c8261
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:15:17 +0000

    hurt war today habit show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def clear_array(array):
+        array *= 0
