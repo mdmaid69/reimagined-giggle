4ee7995f53699c7118ba39ada14c9764f5a11c3d
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:59:27 +0000

    height excited steep tear petrol

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
