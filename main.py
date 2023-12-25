a7df958084ff586d074a8c8f3f55eafe1091fc2e
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:47:51 +0000

    home interview same man minute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_area(radius):
+        return 3.14 * radius * radius
