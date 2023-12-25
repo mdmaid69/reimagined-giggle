dd0646abbcdb666f531bf71d9b2d582d067d7e40
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:15:15 +0000

    confuse mainly thinkthrough taste material

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def sort_list(lst):
+        return sorted(lst)
