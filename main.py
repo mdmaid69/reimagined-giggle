3e6a9c5d7a8594bbc6f7c0d124adf082594ca5c3
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:28:12 +0000

    shall out acceptable store intend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_max(numbers):
-        return max(numbers)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
