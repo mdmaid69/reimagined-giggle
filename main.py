e9736107bc34cc97c01742a5dfc414518a86d065
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:52:08 +0000

    base with tip straight hold

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def find_max(lst):
+        return max(lst)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
