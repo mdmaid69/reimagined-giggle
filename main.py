a32c2103b22785d3e8227c9f41c3bf10d22c10ca
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:14:40 +0000

    fit peaceful rain defeat suggestion

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
