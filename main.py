b32777d0db104af8d796aa27b0265619d7258b87
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:14:59 +0000

    still tool live future we

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
