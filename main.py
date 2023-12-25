855f35ea5fa64fa7805f015c865e942d153cfb69
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:14:13 +0000

    fair turn Cc finger fall

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-print([x**2 for x in range(10)])
+import tensorflow as tf
+print(tf.__version__)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
