fee139c0eed7322ace239a9ff80991a2bf821a16
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:46:23 +0000

    dinner glad otherwise unusual ought

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import getpass
+def get_username():
+        return getpass.getuser()
