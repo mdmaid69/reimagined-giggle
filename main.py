eee008c24b1949b315ba602c1230987284421db7
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:54:43 +0000

    disturbing stay hit freedom worry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
