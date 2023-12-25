99f82c7877f437d920b859f4b9df4369ea755371
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:42:46 +0000

    free trade cry firm live

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
