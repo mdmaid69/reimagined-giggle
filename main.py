c4a2b7bb2643a9d20f86f3843817487121d62eb2
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:40:46 +0000

    zero temper dinner confusing surprised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_length(array):
+        return len(array)
