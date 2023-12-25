3ebb393a5c7eba717c186ec548d3e81b0e201722
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:04:54 +0000

    strict combine aim eat Tt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import platform
+def get_os_info():
+        return platform.uname()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
