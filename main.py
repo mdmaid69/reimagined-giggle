76ca3ec6171561dc2de5315703d68e9044b10111
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:09:44 +0000

    pair serve rush produce spot

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
