bdc3a0fe14ef678321620edaddb7b3c90f593430
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:14:46 +0000

    concerning dress fashion powerful brain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
