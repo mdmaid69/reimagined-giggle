4c53b64faf541ca1d3acd3d05304c90bd0d07b12
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:56:38 +0000

    cook callup sad beneath rob

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
