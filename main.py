67cb2392e0939463d446259ef5de3d3e1e2ffb47
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:46:57 +0000

    once container post prevent doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def is_even(n):
+        return n % 2 == 0
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
