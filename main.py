0d6b1b534b2b15bbd4557221fbd16147253e50a2
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:10:35 +0000

    object accident fly underneath adjective

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-print(re.match("h.*o", "hello world"))
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
