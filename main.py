2ff4b705218a7494054f2ac91bb599c5cc417b12
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:00:18 +0000

    better die along label computer

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import array
+def get_array_item(array, i):
+        return array[i]
