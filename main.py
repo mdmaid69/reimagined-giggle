77a59626ad7b8df2c67aaf942f81907f5fa91b80
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:46:04 +0000

    issue chat pair failure bear

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def find_max(numbers):
+        return max(numbers)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
