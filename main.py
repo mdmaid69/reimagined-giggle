bb08469be49ab850aa04a5df25a01e7271c0b1f3
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:24:37 +0000

    broad let scale relax not

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
