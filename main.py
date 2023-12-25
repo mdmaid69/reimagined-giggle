a12f92caff4b0b5520560f4167e37625e523d501
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:10:42 +0000

    else behind Qq meaning rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
