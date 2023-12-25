8bad180ea3e70396b78f06c11af4765b0b55f844
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:37:07 +0000

    cook combine fear rain stretch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-print(sum(range(10)))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
