96d2c2d156d474dc47abbf2fa368778a9eab00ac
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:17:19 +0000

    solve more complain environment lift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+x = 10
+y = 20
+print("Sum:", x + y)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
