e4053c7c01b2215738878623ac93483bfe8ef53e
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:10:33 +0000

    anybody right after taste offer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-print([x**2 for x in range(10)])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
