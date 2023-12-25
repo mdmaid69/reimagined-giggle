e4a927b2560931eacb4927d5f7aea3c7429d15a4
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:20:10 +0000

    attempt handle consist comedown Mm

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
