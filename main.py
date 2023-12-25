84d25e562eda24d313d2ab26b0f679b366d56b08
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:34:36 +0000

    radio nigga surprise rain congratulations

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
