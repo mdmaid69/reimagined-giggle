e25a410d3f09d1cc0710e510e0011a98aa4c3d68
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:27:39 +0000

    put acceptable yes kind Qq

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import array
+def get_array_index(array, item):
+        return array.index(item)
