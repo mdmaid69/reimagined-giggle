5d057ce07ac5f7812df5bfcd24ab466f9859ef75
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:33:38 +0000

    still passenger attempt operation press

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
