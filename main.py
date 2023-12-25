81aade495b82ec1626af85f72d9ba5daa347fc7b
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:00:29 +0000

    rather under fire nowhere control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
