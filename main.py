f1306d46f261aa599b9ba83f19c585a6dff8cec5
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:25:33 +0000

    official about hurry again content

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import tensorflow as tf
+print(tf.__version__)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
