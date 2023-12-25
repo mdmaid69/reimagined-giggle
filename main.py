8d7019fac4030cfc23a279d58cdaba5978259d51
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:31:40 +0000

    desperate position frighten whistle well

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
