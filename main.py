44cf9ccbd703488ab1fd6709d4fbfdf4b6ea6fcd
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:16:27 +0000

    real direction communication interested bunch

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import tensorflow as tf
-print(tf.__version__)
+def get_array_as_list(array):
+        return list(array)
