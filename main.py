eb57a19c27bacc1654873c7560b6dfe04b98fb13
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:09:19 +0000

    miss back common term to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
