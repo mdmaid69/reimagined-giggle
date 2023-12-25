38fb455761715cf422c75df8e5314f59befbc6a2
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:52:04 +0000

    detail alone block well turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
