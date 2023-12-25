e48e556e29348cef34d1c546bc5367eab9eea984
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:09:56 +0000

    considering change pressure welcome desperate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def find_min(numbers):
-        return min(numbers)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
