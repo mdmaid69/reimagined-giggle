750041a017eafc0d49ed22cb17a09f54e0bed62d
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:53:08 +0000

    fear fashion agree successful hope

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+print("Square numbers:", [x**2 for x in range(n)])
+text = "Hello, world!"
+print("Uppercase:", text.upper())
