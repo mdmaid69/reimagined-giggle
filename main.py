8e2b4463dc879cbf364ae96d2fa735f27f6ceb6e
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:42:49 +0000

    refer wild concern cause minority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def reverse_array(array):
+        array.reverse()
