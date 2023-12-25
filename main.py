0f3e17e00b6f4e18e978d32fc34d6db92f02ae2c
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:36:58 +0000

    suit sight doctor Zz flavour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
