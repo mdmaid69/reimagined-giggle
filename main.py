a393324f7a23883cf601a1998d5b3a69900344fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:50:50 +0000

    responsibility friendly home reserve comeover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
