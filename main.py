d46955862ff4b5695415f294eee893bef7d3b662
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:26:48 +0000

    involve shout fact back warning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
