ebc0c494f46383d290493c6b198370433d66e343
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:39:59 +0000

    holdout influence his objection lift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_array_typecode(array):
+        return array.typecode
