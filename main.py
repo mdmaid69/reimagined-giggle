4676f8131132c760b311dabaf4f214a51433feb7
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:02:38 +0000

    unite by angry reaction stir

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
