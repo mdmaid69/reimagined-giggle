8a5c096f65b94409f1b43f25dbf79500b06a23d4
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:13:43 +0000

    quiet influence open contact fierce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
