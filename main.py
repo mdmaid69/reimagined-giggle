d3b9f0ede0887dfeb7f4d7c6791541c6d29ee8fb
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:31:56 +0000

    name exciting scrap welcome practice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def split_path(path):
+        return os.path.split(path)
