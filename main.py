c462254f7084c1e8b0160826befcb593c130fcc6
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:20:06 +0000

    else flavour plate very normal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_min(numbers):
-        return min(numbers)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_unique_words(sentence):
+        return set(sentence.split())
