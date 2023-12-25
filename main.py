c1ec061049c32cbaa3bed2e5e28374c9f2c962a6
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:56:16 +0000

    call fast ashamed argue common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_current_directory():
+        return os.getcwd()
