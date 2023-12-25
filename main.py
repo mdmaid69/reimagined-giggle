2680e0852796b281b7bb05020ddf4da2bd4fbe20
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:47:14 +0000

    fun human create fly sting

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def add_numbers(x, y):
+        return x + y
