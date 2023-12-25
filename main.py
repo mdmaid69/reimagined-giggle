a6253c7ff78ae6bcd70915625f5b6ac77e835d11
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:27:00 +0000

    polite result butter post exit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-for i in range(5):
-        print(i)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
