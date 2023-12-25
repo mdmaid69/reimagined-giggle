4a86e211c47771ac990223fd5ba67503e4c09c11
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:55:47 +0000

    exchange callout rough row declaration

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
