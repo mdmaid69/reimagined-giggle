827e09b8f9ae676037dd5be5a05843d5061559c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:10:03 +0000

    attention far sink plan rise

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
