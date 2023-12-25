a924799d438807b86fc94199a89c159fe7a65429
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:32:52 +0000

    phone slightly home rather scared

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import datetime
-print(datetime.datetime.now())
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
