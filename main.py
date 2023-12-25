652ae03eff126d2086ff614dc2f5430fa6c50a35
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:48:14 +0000

    later pencil Rr meal call

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def sort_numbers(numbers):
+        return sorted(numbers)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
