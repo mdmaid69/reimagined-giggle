7dff828d6acedb82a69a5f0373c6ab79434d3098
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:24:16 +0000

    occur print say fair friendly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
