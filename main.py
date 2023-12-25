f6f2abd21bb4103cf9097b4d16da804b0c4e38c7
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:53:19 +0000

    maximum cope likely watch distance

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
