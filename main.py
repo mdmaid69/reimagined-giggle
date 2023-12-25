0c034fcdc96c9223ad63db1e03048112b3e16fa2
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:13:15 +0000

    violent calmly hollow valuable geton

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
