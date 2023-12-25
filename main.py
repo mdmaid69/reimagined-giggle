ea733e7f4da9e18656e9233cab07643a9c21f741
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:03:03 +0000

    change argument pile or unable

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_speed(distance, time):
+        return distance / time
