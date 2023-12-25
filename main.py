fb2ce3f3b157d3c40b9fc3cea2b6220397c97d7a
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:52:19 +0000

    unpleasant payment goodbye difficult possession

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
