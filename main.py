63c4985b9d6a47d1e9a1b311ef0be99db1d9801d
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:58:38 +0000

    smile somewhere report design ticket

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
