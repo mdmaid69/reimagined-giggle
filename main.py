fbe767b66a29682061ac4cc2d31fd048423a1dc3
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:21:46 +0000

    talkback serve brown obtain surprising

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
