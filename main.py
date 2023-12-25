269b018df991e5afbd6b713a0fa59fbe8e742813
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:54:43 +0000

    plastic take press catch connection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
