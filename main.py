3218b1d12b07165c6d9b704d8eb4e9376e99aea0
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:01:25 +0000

    dig without tune exactly mark

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
