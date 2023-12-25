39981eeaafd72a537c22e2d320abdcc2c625f547
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:34:12 +0000

    565end space clever none build

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
