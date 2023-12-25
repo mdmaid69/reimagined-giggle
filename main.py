26ac0034917cef5d1b961cc39c967ffe48dfe233
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:53:21 +0000

    high breakfast escape favour union

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
