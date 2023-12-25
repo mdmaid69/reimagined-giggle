307cbc1395cb9fec5e4f4910af9e6312e90b8307
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:10:04 +0000

    intend coffee business Dd knowledge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
