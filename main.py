62deca509efe667c2eb8159db266e27ef6bab3d8
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:02:07 +0000

    section butter pin tap complete

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+print("Hello, world!")
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
