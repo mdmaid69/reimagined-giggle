7554ef2abffd22bacf3ef04ffc34148ec617e287
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:33:28 +0000

    entry fact fight discuss help

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import os
-print(os.getcwd())
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
