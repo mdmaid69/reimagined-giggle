072b768b0b59a61ebe60528d53868834ce1a51d1
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:55:37 +0000

    rest myself bound guess should

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
