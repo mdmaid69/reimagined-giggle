3147bc4ddfa7f4b8a3759f013c8e1a4e0f9fec8a
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:27:13 +0000

    entrance professional afford total love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
