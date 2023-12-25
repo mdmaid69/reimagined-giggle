10b7efc55a064d575819c142a083886d04c1b5e8
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:54:58 +0000

    sensible declare past there mark

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
