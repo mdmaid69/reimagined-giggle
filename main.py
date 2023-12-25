7fee9824fa70fc4fc255315abcc6a84da04df24e
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:57:26 +0000

    lookaround market lunch hospital colour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
