8addf50e32225889d3380f55dad0cf66ba50a43e
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:04:39 +0000

    guide welcome swallow forever warning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
