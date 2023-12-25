85b3faf717d4bfdb6bde3e2c93e65d1f070f54ce
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:48:42 +0000

    close behaviour trip bottom capital

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_work(force, distance):
+        return force * distance
