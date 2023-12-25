ba1196490192846836a2a19b199cf8bcd79beb26
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:15:54 +0000

    life blow keepoff goon fly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
