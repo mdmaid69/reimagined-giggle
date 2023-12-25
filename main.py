131da2c8501a28a8ec4d470d3452ddc85781a46e
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:03:41 +0000

    will purpose lunch false praise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import time
-print(time.time())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
