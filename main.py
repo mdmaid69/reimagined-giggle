83bbc0ec34be0ad7dbae48caf1bdab5a1b2dca82
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:20:22 +0000

    name poor produce worth pronounce

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_error_function(x):
+        return math.erf(x)
