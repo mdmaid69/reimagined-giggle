3f5dc92bd5bbcd1c9b06ffab886e460a045d33fb
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:41:01 +0000

    unless survive roll smell reject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
