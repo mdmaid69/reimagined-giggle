d5a688bd901794087ffcdc82833f93f3af48385c
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:49:18 +0000

    temper desperate stage love excuse

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def add_numbers(a, b):
+        return a + b
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
