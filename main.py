30ba861957af217abf0ecb7eafd6131b0adcc48d
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:30:21 +0000

    anxious delight in influence letter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
