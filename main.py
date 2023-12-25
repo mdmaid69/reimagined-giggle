c5eebbb6c1aca81b8893577055de98fe7365bafd
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:58:24 +0000

    discovery cinema closed hard one

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
