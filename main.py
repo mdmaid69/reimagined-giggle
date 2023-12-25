881b827446e37495f2703b03bb9bd62f601b173a
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:05:30 +0000

    petrol convinced there steam explain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
