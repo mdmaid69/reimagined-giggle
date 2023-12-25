26870af9917d26f60d4fe63c80889b60ac40eb11
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:10:14 +0000

    avoid 567enemy matter abroad conclusion

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def convert_to_octal(n):
+        return oct(n)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
