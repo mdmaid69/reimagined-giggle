fbf59fbdcaa5820ec63e835b94a176b2d1e91e35
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:57:14 +0000

    holiday sister grade rain bridge

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
