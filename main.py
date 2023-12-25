fa48c7f6cc9ec63a982fdf4353c7422e77278784
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:31:27 +0000

    idea lean slight around cut

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
