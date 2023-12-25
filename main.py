7b6bdd7e46bc9ebacdb1e436501ce2ef12e966c5
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:58:13 +0000

    another bang sticky bottom hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
