1efaf05241aae08a84f2469ea6cffd9240dd4627
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:06:27 +0000

    hide dry beyond easy us

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
