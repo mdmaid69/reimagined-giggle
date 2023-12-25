294e5b3921045138c33bd8e0a6a460a07e21fbf5
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:11:00 +0000

    claim actual region basic some

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
