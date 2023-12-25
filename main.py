5a2b6c66ea3c590aa2b72a05f8df4264e5d6fb97
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:24:29 +0000

    takeback war wild cheap print

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
