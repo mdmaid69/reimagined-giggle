90a2069feb9495f8a256402d08eb09e515015cad
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:24:39 +0000

    smell unlucky yet soak tear

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
