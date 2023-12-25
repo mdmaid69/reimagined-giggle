5a81de0cb115abeb9bb1d44ce4c6e7dd9f4f2651
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:19:52 +0000

    medium anybody week off waste

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_unique_words(sentence):
+        return set(sentence.split())
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
