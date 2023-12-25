b3734664b99ec8f7dd86c2a32fe71673f49fc34c
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:28:20 +0000

    none listenfor past feel moment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def get_current_time():
-        return time.time()
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
