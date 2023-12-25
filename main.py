f8613f5f6b8eb5c37af0266f2a0d0d81e541d558
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:00:55 +0000

    noone shock condition characteristic exercise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def calculate_area_circle(r):
+        return 3.14 * r**2
