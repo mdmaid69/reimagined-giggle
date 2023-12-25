b0f7eef29b07053e2ea9bd90a396af46e1e274bc
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:11:36 +0000

    edge main arrange as hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import collections
-def create_user_string():
-        return collections.UserString()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
