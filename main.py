88e7460f78b89aabb14135382e46ad42e1f82c5a
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:08:21 +0000

    dance term rescue drawing debt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import array
+def get_array_as_set(array):
+        return set(array)
+import re
+print(re.match("h.*o", "hello world"))
