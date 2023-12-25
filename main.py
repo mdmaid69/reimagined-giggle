6737d22e963d30873582654bfde72fb606bf2235
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:48:23 +0000

    contact shut somehow rarely tradition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
