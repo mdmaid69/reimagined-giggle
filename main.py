fa492e7240ea1d44c9fd4a20f76350afc5901906
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:53:44 +0000

    throw risk present wash disgusting

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import random
-print(random.randint(0, 100))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
