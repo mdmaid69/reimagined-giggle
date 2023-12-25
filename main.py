501c453e611e50da9a1d0f40594eb5f2db44e295
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:54:15 +0000

    please giveback cut but start

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
