2ce950e2511a7d131df3ac7d1489e051fcbaeb6b
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:24:15 +0000

    occur slice mood cry range

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
