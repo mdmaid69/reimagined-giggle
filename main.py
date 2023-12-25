a4feca16ed654e4c05150d137a8de4058569c2cb
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:05:50 +0000

    object chase lie speed organization

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import platform
+def get_python_version():
+        return platform.python_version()
