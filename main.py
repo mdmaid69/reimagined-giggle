a1e98b02af90e484ea1fb90c7f0b4a856032458f
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:53:26 +0000

    material safety town approval border

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
