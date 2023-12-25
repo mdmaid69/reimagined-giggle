2cd88977779bbe10a741a81f542fb48f87cf1a41
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:29:02 +0000

    bang harm inquire recommend push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def print_python_version():
-        print(sys.version)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
