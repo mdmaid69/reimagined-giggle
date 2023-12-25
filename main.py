2e79b46ab328939e9fefdfd02c519586c9cc6fb2
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:06:59 +0000

    holdup pattern risk whenever phone

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print(sum(range(10)))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
