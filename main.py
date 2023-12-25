4019f6df695dbaa7876af2996f00d55c59699b3b
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:32:43 +0000

    bath shop disappointed strange guard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
