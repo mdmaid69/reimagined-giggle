d5a09969793ec0e8723c4b1dd07a23fd74c9c5c2
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:05:58 +0000

    by guard workon cross extra

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
