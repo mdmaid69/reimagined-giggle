fa74955459a675f72dd76316bf903a0a4fdbd7cc
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:03:41 +0000

    taste calm arrive getout strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
