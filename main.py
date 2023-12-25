70682f35cfc0ca9abe5b64866d535df757d58c9e
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:55:51 +0000

    disturb spring switch nature tip

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def is_even(n):
+        return n % 2 == 0
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
