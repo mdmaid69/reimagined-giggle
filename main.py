6168d3d4f9984273aead4e8f4df78247b4fd2e30
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:14:30 +0000

    clean male free cheek impressive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
