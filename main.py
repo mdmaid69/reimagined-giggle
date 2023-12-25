5e50a9dcc8c3738e12d4c70d80423528e974fd74
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:15:44 +0000

    fairly original secret brown film

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def subtract_numbers(x, y):
+        return x - y
