8b6fadebd96b4009e8af139bb575b456e8c263b4
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:32:34 +0000

    experiment letter splash pot gradually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
