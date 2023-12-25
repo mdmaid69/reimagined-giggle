50a231b187ecb5619671c516c6fa4a5a7d2bdc79
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:32:05 +0000

    slow real during eager science

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
