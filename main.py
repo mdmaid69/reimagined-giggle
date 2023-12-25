8c1eb9ce621b6195ca943fd9f0c6b8e3eb37bdc5
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:50:37 +0000

    touch bottom provide definite exact

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
