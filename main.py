199fe8097e288f035e2c24232047446a93dcb76d
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:00:37 +0000

    seriously giveout trust news unfair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
