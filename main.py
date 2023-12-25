3df609b569e1fe71562a3f7132567b3bba6c5be2
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:42:07 +0000

    woman equal whistle force attempt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def count_elements(lst):
-        return len(lst)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
