f3cf8e7f2afe4a3b77d3e1a93598eb1d6dee97b3
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:21:36 +0000

    maximum view pause equipment touch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def convert_to_hex(n):
+        return hex(n)
