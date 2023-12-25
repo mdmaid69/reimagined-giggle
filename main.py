92a53e24b15c6af3ec4e01461eed6e5b875ea203
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:26:28 +0000

    clearly basically level heart convinced

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
