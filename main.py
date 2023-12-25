f9eb1c164724511a351af6226b438088af296490
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:01:22 +0000

    tape sale ring no thinkof

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
