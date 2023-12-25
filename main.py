9b4497b468eb4a8df9f9025a825b0077fd8cdeb4
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:07:06 +0000

    570enjoyable charge clever cruel case

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
