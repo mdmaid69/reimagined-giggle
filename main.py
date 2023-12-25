4d8cb794299b482dfa1077db644acc49b26f19db
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:09:51 +0000

    proof conclusion how protest round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def print_python_version():
-        print(sys.version)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
