3c12b2622b785dd4fe056ff0b3176e3c38996a43
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:02:25 +0000

    practice force hurry lend feeling

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import time
+def get_time_since_epoch():
+        return time.time()
