331135b582a661f72353d435526ff01dbb5203f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:54:14 +0000

    fear emphasis relax breakfast frequent

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def add_numbers(a, b):
+        return a + b
