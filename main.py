39e70a16c2544237398cbec319f0c3ca563e20f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:50:18 +0000

    giveup lead point hit kind

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
