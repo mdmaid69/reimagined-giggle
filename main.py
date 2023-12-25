6cd8197aed3a1cbf3b234d1ce6d1283dbe9b9cf8
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:29:13 +0000

    issue love runinto consult OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
