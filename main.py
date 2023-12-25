a43cf12b1d6a8c5ed39812fb7ebd1cc9c2c7a218
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:07:31 +0000

    start sing strange picture contain

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
