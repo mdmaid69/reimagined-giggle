30440d66c2c7173e001616d0988d0d0b127b0b48
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:02:22 +0000

    choose clothes matter hour unusual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
