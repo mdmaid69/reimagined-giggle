ad7810702743f385486cc6d2c12930fd554d55d7
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:59:29 +0000

    rubbish report school lucky study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
