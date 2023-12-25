11083adde0495a6298a0ae5b17150788c4160578
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:05:25 +0000

    border relax up explain you

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
