f0898dd26af7c51ef9537126c38e6451d6454b82
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:07:11 +0000

    noise dance mess negative trousers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import getpass
+def get_username():
+        return getpass.getuser()
