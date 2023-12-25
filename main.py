8dace24af525f61ac895aa1bea265440cbbf385e
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:39:50 +0000

    string club fortunately scale on

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def get_array_item(array, i):
+        return array[i]
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
