708bd9cc1c673e8024ff271bff9af8b3e7b2667b
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:21:31 +0000

    deliberately mess 572enter border mean

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
