e65573e27b119d2cb73366ff534dbd82d0aca432
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:01:00 +0000

    various book behave sister wake

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import csv
-def load_csv(filename):
+import json
+def load_json(filename):
         with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+        return json.load(f)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
