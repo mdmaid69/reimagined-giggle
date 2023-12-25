de7acdf1d6a0576e15796daa51871e280fb5c4fb
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:52:12 +0000

    turnout inquire already emergency approve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
