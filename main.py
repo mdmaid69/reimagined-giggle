d5a290736debd03d1ce057301d2be0d56fb77a27
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:27:33 +0000

    burn hold next surprise copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_current_directory():
+        return os.getcwd()
