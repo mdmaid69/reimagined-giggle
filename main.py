61717fd54132e7dbceeab0bde897d0ba8dc22807
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:48:59 +0000

    length dream born up upset

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
