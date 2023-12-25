c0a40ed0e66b473823dd658b7bb8b665bc3a6ce4
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:55:05 +0000

    interested pile average lost development

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  def convert_to_octal(n):
+        return oct(n)
