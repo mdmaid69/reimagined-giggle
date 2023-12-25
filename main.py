21c5221efbf7bdd51309388473fdd5b2e780d59c
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:48:52 +0000

    wild good responsible born pity

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
