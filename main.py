ae938375906c4eefa09a64bd9e1d543061b8bf6a
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:48:26 +0000

    holdup immediate operation frequent thanks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
