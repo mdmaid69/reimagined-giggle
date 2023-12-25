18afb417f719161e57be32aa4dfe69164eaf12a5
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:32:20 +0000

    wave remain see deserve scrape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_current_directory():
+        return os.getcwd()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
