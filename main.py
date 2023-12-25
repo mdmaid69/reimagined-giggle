a5459e871ebdcfbcc4c3674ac63b5921bfc55b61
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:58:59 +0000

    exhibition rest comefrom near smart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
