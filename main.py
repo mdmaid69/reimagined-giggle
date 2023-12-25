b608b8e24a8db05f5b5779ee7541eae14d919093
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:40:16 +0000

    total pollution deserve else comeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
