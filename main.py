2e72bf094b522992fff511bd32903f9c6b2e2058
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:45:44 +0000

    talkback happen last tone comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def is_odd(n):
-        return n % 2 != 0
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
