6a772f75c07e32bfc3e6209e3e7ad94aece53f44
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:08:23 +0000

    demand prove awake wipe doubt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
 import array
-def get_array_as_set(array):
-        return set(array)
-import re
-print(re.match("h.*o", "hello world"))
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
