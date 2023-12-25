d1634d023859f816c88bfefbbcce1df644835fe5
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:01:30 +0000

    awake beat story little arrange

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+print(os.getcwd())
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
