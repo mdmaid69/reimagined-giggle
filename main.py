4842ed05e2a36c0235cf7446b31b2439b74b8d91
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:52:58 +0000

    demonstrate talk use bye weigh

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_power(work, time):
+        return work / time
