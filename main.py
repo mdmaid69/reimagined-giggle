2e88f84cb43f837150c4531014958f82583ff6e2
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:09:21 +0000

    judge interruption splash mention earn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
