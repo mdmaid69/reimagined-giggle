0552676a3368113c9119c7f4f108c02d9d1cfe35
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:42:25 +0000

    priority meanwhile grade sing fine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def convert_to_hex(n):
+        return hex(n)
