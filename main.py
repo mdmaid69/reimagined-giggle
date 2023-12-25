bcba71ad2b5b8c470b163143de6045da16df5d35
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:54:51 +0000

    breathe speed borrow contain surprising

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def convert_array_to_list(array):
+        return array.tolist()
