3bf50df6b3f35e536a979071e77b652cc4df9224
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:08:09 +0000

    now hang fond lock side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
