25dfc5281948b9efd9a02e2b00d4cbba47a1318f
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:18:17 +0000

    still money exit learn treatment

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import sys
+print(sys.version)
+  def is_even(n):
+        return n % 2 == 0
