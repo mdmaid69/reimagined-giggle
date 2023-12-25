b3f63e867dc4637169ecc3ff433fa671443ee1e5
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:53:01 +0000

    taste injury place rubbish apparently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def clear_array(array):
+        array *= 0
+def remove_duplicates(lst):
+        return list(set(lst))
