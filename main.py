0bab3e5eddecb2cadb5bcce5ce0c7cda6adf1a43
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:55:20 +0000

    contain will evening drive exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
