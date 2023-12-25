c934b857741c65794700e867e7f51f14e5957ba4
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:06:42 +0000

    rid different beat swimming quantity

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
