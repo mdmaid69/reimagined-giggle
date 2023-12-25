37190ca25514696dedd0ec871d3dab26de7f1ed8
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:02:10 +0000

    road dangerous out contribute failure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def add_numbers(x, y):
+        return x + y
