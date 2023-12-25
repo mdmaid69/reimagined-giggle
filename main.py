de6e10ba41dd8a02ca28cabc49be62c240df0c74
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:14:45 +0000

    defeat combine suit look someone

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
