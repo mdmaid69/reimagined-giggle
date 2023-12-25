48d081668b6331b1e5d9d96eab1ab3e7b60c6ac6
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:29:44 +0000

    remember mail willing sweep building

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  def is_odd(n):
+        return n % 2 != 0
