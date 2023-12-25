09677349317bfdb3cfd1746f0f70e15fdcb2e9e9
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:41:55 +0000

    long size yes ring between

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def convert_to_hex(n):
+        return hex(n)
