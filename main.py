46cb1b3e8dc7863056d7a5dca1e116aa872d1d86
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:55:12 +0000

    neat hit choose rest climb

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
