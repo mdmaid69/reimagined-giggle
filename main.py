a4c380c4831a5edafb2edd43163c30f699e7538a
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:19:00 +0000

    stupid danger promise underneath male

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
