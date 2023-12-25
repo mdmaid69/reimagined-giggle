55750651d6a3f7f33b1e1abdb901e33f6b9c17c6
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:38:47 +0000

    meanwhile least prize concentration chase

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def get_array_as_list(array):
-        return list(array)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
