c3fb959df40cee2637dea0b1c78ef63b7a5ca7b1
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:37:51 +0000

    afterwards machine even embarrassing aware

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
