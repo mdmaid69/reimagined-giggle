5f78e2042d546fc3d1dfd887f9d57830209970c1
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:55:55 +0000

    pause gift funny lookback(on) deaf

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
