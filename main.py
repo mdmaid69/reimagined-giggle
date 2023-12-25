1307995d1fcd1042f53e3a387a9857e97ec085de
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:47:44 +0000

    duty treat exchange nothing smile

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-print(sum(range(10)))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
