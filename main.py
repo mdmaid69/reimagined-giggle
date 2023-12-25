16fe02341c246e9e6f2fb25cac531fedf1025ef6
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:05:04 +0000

    visit long refuse house use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
