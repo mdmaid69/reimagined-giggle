915e792c3744996410668957d2f9233454b1fdd8
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:54:34 +0000

    means rate cover difference cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import tensorflow as tf
+print(tf.__version__)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
