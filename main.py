1fe7b1546080d696d2b002b525df86a54d4dd280
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:44:35 +0000

    wall on pile handle final

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
