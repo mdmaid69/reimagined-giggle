c8e2aeaf4134a71742439b49a384692c3ab7fe43
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:37:09 +0000

    fear still wire club rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
