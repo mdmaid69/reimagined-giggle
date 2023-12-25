cb27e800ebd64991e49732721c4568158b0abbcb
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:35:22 +0000

    major cheek there burst Gg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
