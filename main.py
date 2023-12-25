9783723ceac5f7cd1cb05ce4ffb196d22eab9e5c
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:02:49 +0000

    why thought basis rest dead

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def convert_to_hex(n):
+        return hex(n)
