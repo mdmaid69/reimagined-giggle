005db55bab17616164d25b3e609822164eb516f8
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:10:46 +0000

    mine manner who sure model

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_area_rectangle(l, w):
+        return l * w
