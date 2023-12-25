400e929242a57b562260515939c078666e620db1
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:16:28 +0000

    base communication tough satisfied shout

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
