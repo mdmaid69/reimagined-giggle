b17ed8cf215e5a620a1a40ee8ab36a90382e4712
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:35:36 +0000

    promise achieve some rough develop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  def square_number(x):
+        return x**2
