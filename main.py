76c196f55038b02b45dd0d0c785c05ab640cfd73
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:09:15 +0000

    except even event hide whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
+def clear_array(array):
+        array *= 0
+import array
 def insert_into_array(array, i, item):
         array.insert(i, item)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
