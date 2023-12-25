795ac43975fd30b3cd81cefd76b2d0860b24627c
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:06:47 +0000

    dead emotion cool correct work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_min(lst):
-        return min(lst)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import time
+print(time.time())
