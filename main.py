614808447ce5c3b92f170f79760130538d5e3611
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:32:59 +0000

    pretty pattern blue correction inform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_as_list(array):
+        return list(array)
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_error_function(x):
+        return math.erf(x)
