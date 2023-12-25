1f6b549b2d991e66b35830ffc15c1790c0a3b867
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:37:02 +0000

    lucky rush reduction perhaps dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
