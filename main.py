909de56ad476d7f420f2298118f2140bc3144996
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:46:59 +0000

    face bringup push pleasure him

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def convert_to_hex(n):
+        return hex(n)
