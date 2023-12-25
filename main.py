30ec70b7760d377e17a53e54a77a9f79210a2c25
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:24:22 +0000

    Hh run sharp bit treat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def sort_list(lst):
+        return sorted(lst)
+import os
+def get_current_working_directory():
+        return os.getcwd()
