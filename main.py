64c6298e3c098ef6655f9c2850519ad163c3e532
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:43:37 +0000

    sense stamp generous comeout unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
