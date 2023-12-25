ecdcc0ca4b92de53b95df7b188c584a8dda971d4
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:05:04 +0000

    probably its warn slowly intelligent

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
