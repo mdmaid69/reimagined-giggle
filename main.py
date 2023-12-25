75d9a766176e388f5bfd847c822ace931dd8e972
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:30:43 +0000

    clean callat Vv either please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
