fd72ee9d0235e4e5f52eb41a85200af9631015a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:47:47 +0000

    know bet holdon hurt though

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
