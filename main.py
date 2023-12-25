ba17922a5eea8026804f40f5a694edfbe9dd8da5
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:17:01 +0000

    fast must division ground slow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
 import array
-def get_array_as_set(array):
-        return set(array)
+def convert_array_to_bytes(array):
+        return array.tobytes()
