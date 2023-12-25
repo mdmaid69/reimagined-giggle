b00edff2646272df8ebf77c2f697782a26e94507
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:27:55 +0000

    lead cinema change intend juice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
