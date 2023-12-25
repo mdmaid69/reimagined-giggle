7c2c784b7ef16290e447110a03ef656f4718b158
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:33:28 +0000

    serious whom event time degree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
