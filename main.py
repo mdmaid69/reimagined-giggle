ad3bfcaf645c25ff08b39fb0f91d8b9a576713d0
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:08:19 +0000

    difference waste genuine base threaten

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
