ddc3c924f9c6fb0c074cfc062b702a076a04ae0a
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:27:44 +0000

    main 571enough sign unhappy suppose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
