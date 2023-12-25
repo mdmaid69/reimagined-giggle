84ad79fa3cdeb794bf550ab13b1268a71d3d1e5d
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:58:27 +0000

    Ll clue danger form cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
