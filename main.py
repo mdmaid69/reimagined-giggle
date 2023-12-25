58430b68bba2179e4ded630ab44eb4a8caf21121
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:36:29 +0000

    future bear shrink this bread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import sys
+def print_python_version():
+        print(sys.version)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
