84702b75b29bc1e9120f11b44831c26f2ff8ca85
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:19:40 +0000

    mention train boring man payment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def square_number(x):
+        return x**2
