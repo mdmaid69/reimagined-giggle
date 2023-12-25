4c9fe11914536977102bd4f6ba6074a39817cd47
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:59:50 +0000

    breakin mend experience likely behaviour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import os
-print(os.getcwd())
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
