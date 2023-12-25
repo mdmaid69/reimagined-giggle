da4d27c5faddd07a3a0fcc1aa0e422f454673b01
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:19:51 +0000

    turnon during church cruel ticket

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_density(mass, volume):
+        return mass / volume
