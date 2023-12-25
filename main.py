49b751bebf0632381713acb473d6ba4597ce4ef0
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:46:50 +0000

    holiday fork juice replace mainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
