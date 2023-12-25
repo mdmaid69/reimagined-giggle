8eb7051e01f2fe0e10c9753ce4c9f6354e427fde
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:07:26 +0000

    explain weekend sad clue guess

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
