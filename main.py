e5142ce55c4b74f803d6502f5fa65861ef8325f0
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:50:56 +0000

    claim summer properly hard estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
