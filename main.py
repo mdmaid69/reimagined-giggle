052135bb039b8ff3e3cfb5abda4594ad09172e59
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:22:32 +0000

    division keepoff chin march think

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
