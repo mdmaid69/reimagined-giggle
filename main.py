c99ec94234d3c59514ab0657f7609f7c342e52d4
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:38:50 +0000

    573entertainment need whistle tap manner

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+for i in range(10): print(i)
