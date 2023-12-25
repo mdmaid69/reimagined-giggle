7540bf6dd5ba4a7d55fa6f569b4916ac1c92021e
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:20:33 +0000

    nerve rubbish dress bag hope

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def convert_to_octal(n):
+        return oct(n)
