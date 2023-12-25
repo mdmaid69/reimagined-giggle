b462d8cc38c1befbc85de1b4a2aaf09d61564546
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:09:47 +0000

    burn skirt strong grand sweep

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import csv
 def save_csv(data, filename):
         with open(filename, "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerows(data)
+import sys
+def exit_program():
+        sys.exit()
