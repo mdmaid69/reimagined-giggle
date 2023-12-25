cdb48ece903eb1a1193e45043550d54673696ff8
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:22:40 +0000

    distant meanwhile deliberately bringdown hurt

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def reverse_list(lst):
+        return lst[::-1]
+def greet(name):
+        print(f"Hello, {name}!")
