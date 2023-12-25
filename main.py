7efcb63e5034c5f38268db1cf0150e90fd63c34f
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:49:01 +0000

    deep total the entrance force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def change_working_directory(path):
+        os.chdir(path)
