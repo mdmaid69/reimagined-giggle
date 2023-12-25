94ff73b4cb8989cc56697f49a109683b7f1d2220
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:11:06 +0000

    my off independent backwards special

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
