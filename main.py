ddc001e1134e900be7b1768c06113ba4814914c7
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:33:34 +0000

    let convenient hide dress afternoon

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
