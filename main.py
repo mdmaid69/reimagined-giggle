d6f50e3fe3f5d09478f0b4484f06e0a3414d9417
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:09:54 +0000

    brain both solve somewhere drink

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
