e46c1ee9c5b33fa5c38c85496c8d16334964cf59
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:11:40 +0000

    prefer satisfactory muscle ourselves wish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
