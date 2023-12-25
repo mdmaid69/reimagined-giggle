2911e8dfd48d21b603564b83c7c4a80aa8fe2d8a
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:56:56 +0000

    shake tear soon country band

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
