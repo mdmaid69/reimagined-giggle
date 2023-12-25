207f640ae8aea52aecd8576ba006455ff6024dd1
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:09:54 +0000

    final remark movement design force

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
