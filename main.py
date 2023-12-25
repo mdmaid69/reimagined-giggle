e2a7f7169138d5024250f10f5a0929dcd3cf42ca
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:04:22 +0000

    fun cool first serious comeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
