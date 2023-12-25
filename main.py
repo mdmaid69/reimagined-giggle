cccb7fc7f370e46b9d66b743fc3cdb1d6014dd06
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:35:02 +0000

    dig effect of hope power

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def count_elements(lst):
+        return len(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
