9c397e03a13e7173cb4f79f9976f80be708a1a69
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:53:15 +0000

    summer breakfast go pen backwards

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
