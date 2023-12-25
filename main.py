bd7aa179a1ce41e7d66a24a4ec4a0d04b4649cd5
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:51:58 +0000

    confidence actually involve until miss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
