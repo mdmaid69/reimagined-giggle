ca9f3c587daaa645b8fac697312d6052c8df01d4
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:55:50 +0000

    strange project swim dream possession

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
