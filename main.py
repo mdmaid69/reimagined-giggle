97763d72fd07985ff05c096428541ee2ef8d57fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:36:23 +0000

    mistake want people convenient want

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
