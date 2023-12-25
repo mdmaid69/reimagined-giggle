9d4a3bc63fa0f6fc3c041929614ce1ecf17ac052
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:02:29 +0000

    accept though shock pin accept

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
