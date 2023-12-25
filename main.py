c8918b60325d7272a8535a43310d59985828f171
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:45:01 +0000

    niggaacademy kind attach bush that

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-name = "Python"
-print("Hello,", name)
-def calculate_density(mass, volume):
-        return mass / volume
+  def reverse_list(lst):
+        return lst[::-1]
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
