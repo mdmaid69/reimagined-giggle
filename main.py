5035e4954c9b46d7993164734e0ca8c485b14717
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:44:10 +0000

    mine petrol offence steam side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def sort_list(lst):
+        return sorted(lst)
