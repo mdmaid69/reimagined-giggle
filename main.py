9e89ac137a15088defc53034e29e1a68be5b7274
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:08:54 +0000

    room light pay empty maybe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
