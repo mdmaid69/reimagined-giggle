30aef1966d5fedeedd4a29a5c11fc9313808d47c
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:03:00 +0000

    bringdown profit can check Ss

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def square_number(x):
-        return x**2
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_work(force, distance):
+        return force * distance
