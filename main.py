2736b8960e02a948f29b26c01883139aacc2b34d
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:45:43 +0000

    plate other disturb it intend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
