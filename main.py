ff64e0453eebf135c3b41e3038db1e3a7f4c6f1b
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:19:46 +0000

    separate priority your sky bridge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-for i in range(5):
-        print(i)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_force(mass, acceleration):
+        return mass * acceleration
