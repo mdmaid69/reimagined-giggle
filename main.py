4045ed8d92dc3ccf0fec08105dd1d1ad4bfd2017
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:01:10 +0000

    seat bringup ready wall settle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import re
+print(re.match("h.*o", "hello world"))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
