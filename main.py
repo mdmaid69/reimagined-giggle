ca85b5af718ca9a9bcc61ebac9ae68f603df05d0
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:18:39 +0000

    play process floor influence small

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def divide_numbers(x, y):
+        return x / y
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import collections
-def create_user_dict():
-        return collections.UserDict()
+print("Square numbers:", [x**2 for x in range(n)])
