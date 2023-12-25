23afebea9625a094c40f0e64d60c309fc8559010
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:46:57 +0000

    attach along dream hold hide

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def convert_to_binary(n):
+        return bin(n)
