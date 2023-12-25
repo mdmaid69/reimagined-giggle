bc7820f7067ed1d9f362f17ff4ecea4b836590b1
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:24:58 +0000

    change surprise hurry catch ignore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def convert_to_hex(n):
+        return hex(n)
