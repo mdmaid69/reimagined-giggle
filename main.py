e4982bc4af5d43b361d3aea58af40418e5c33528
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:02:07 +0000

    tense action godown breathe last

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def count_words(sentence):
+        return len(sentence.split())
