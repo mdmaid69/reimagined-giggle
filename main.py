edd846a9878ce998708aed59c45b5827974bd731
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:40:44 +0000

    birthday stiff sad software sometimes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
