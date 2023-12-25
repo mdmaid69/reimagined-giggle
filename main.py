7a17ef8672b3abbdf4fb45363e3d4d413fab3668
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:31:45 +0000

    distant alive 566end respect brother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
