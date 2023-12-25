0c2a13dfd16f244ab074e9caf978151c38d7fb8d
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:02:32 +0000

    to nerve yesterday relationship turnup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def get_array_as_list(array):
+        return list(array)
