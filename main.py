01be5a8a9921cf7c4239c6388c85b5cdad628d96
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:36:10 +0000

    scream force remember now in

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_length(array):
-        return len(array)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def multiply_numbers(x, y):
+        return x * y
