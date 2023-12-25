ed836e18abf5bf0a6c8b8bf7bf724d6113eafe82
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:46:14 +0000

    word address any makeup range

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
