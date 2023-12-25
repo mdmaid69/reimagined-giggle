675f45d8465675240de1537d1edd6444dbec2c17
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:01:09 +0000

    today extra talk handle accommodation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import time
+print(time.time())
