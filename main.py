a8bc78cb7bb754dc239d643e773cc02d66c1e62d
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:44:59 +0000

    belief turnout morning press special

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_area(radius):
+        return 3.14 * radius * radius
