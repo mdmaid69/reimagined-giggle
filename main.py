9eefc04502ff6c64315bd6aacf373e6553a7e176
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:42:43 +0000

    report crazy paper serious demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
