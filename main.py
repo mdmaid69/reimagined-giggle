fab547a1b02776660fdd48fda26f0a43caadedbb
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:02:32 +0000

    organization freedom tune whistle file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_work(force, distance):
+        return force * distance
