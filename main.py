dd04288189dae26666cd3dea21291a7a8d2b3f9a
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:05:35 +0000

    debt until shut trust height

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
