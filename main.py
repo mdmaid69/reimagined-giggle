ebef4fa69593306d76f7971058b49f2ac49d3151
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:58:36 +0000

    temper scheme help fold dress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
