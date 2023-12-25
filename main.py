002c96fa1ac0775ed5f0d4b7c8d87ee98d5f0c2a
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:05:25 +0000

    laugh brush rescue alive outside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
