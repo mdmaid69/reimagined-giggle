782b5e34f05db7f2b09529592efd5002216f6c58
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:56:08 +0000

    style attractive convenient any change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
