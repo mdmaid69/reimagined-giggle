8321b6380fea5e6e1a02bf76b50a77f10dd4c818
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:29:41 +0000

    look deliberate care downstairs weigh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
