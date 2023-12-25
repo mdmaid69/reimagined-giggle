3767686ec44c9ec63e489910dbd92b5fe821305b
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:32:42 +0000

    broad zero pupil matter permission

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
