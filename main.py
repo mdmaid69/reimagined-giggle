2622c62d5cc1f8ff212d7f25591a4364af33dd7e
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:31:10 +0000

    smart name angry might touch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import math
-print(math.pi)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
