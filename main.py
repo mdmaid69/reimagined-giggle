d2fe8d14886552689fa7815d9eb9745cbed47f28
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:07:34 +0000

    distance foreign ground Nn no

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
