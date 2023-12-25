5159bf06f1ceca501fef4ec696c3f9cbe91a1ac6
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:48:23 +0000

    problem nonsense Nn later Zz

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-print("Hello, world!")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
