8aa373905c2f748c0e3421d01a9cda4093f0770e
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:11:26 +0000

    Tt slightly pair conversation low

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
