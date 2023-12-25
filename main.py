4ec17fa87dc45d7e2619379b426663b523b3951c
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:23:19 +0000

    collect choose surprised tax nose

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def reverse_string(s):
-        return s[::-1]
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
