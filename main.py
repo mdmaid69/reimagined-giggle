8eb6f0e3d05efe3360c31fe3e835b881723faa85
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:45:47 +0000

    sense survive everybody guide sore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
