5c7e7c5d674916256cb26f08ff040a893f2b0e8f
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:58:55 +0000

    worst church boring interested too

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def square_number(x):
-        return x**2
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_exponential(x):
+        return math.exp(x)
