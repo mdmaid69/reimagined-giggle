198e426baf852991dd4dd458edc1783c538d0a01
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:25:45 +0000

    payment way ball 567enemy uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_speed(distance, time):
+        return distance / time
