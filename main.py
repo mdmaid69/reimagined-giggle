b1f93c3e1b22dd47148f525864bb6454e6f2d9a5
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:22:52 +0000

    excellent hurry test achieve important

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
