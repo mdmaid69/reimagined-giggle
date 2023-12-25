8c6c86f6e811bd1420edcc8c6b1433f6a62bd20f
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:24:56 +0000

    soak load thanks official point

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_array_as_str(array):
+        return str(array)
