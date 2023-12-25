cdf32b43056acae35107981d25239ca0df7363f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:59:47 +0000

    only moment trap prevent trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_user_string():
-        return collections.UserString()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
