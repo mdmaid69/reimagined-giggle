5d693b4d099c2dc97b6a48e46071c304c2038192
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:40:14 +0000

    lonely exaggerate beneath stick pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
