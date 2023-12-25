4d0a48307bbbf81cb781d9f258b5a7070b846ab4
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:58:17 +0000

    here connection pleasure language image

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_user_list():
+        return collections.UserList()
