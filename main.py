65edaa28dbaa5f16e4d8312f76930c83ec19ce91
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:47:00 +0000

    off bank workout century lookafter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
