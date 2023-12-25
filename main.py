8976473d2565fbdb027be335ce6a58c34eb49cde
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:30:09 +0000

    world season flow nervous powerful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def count_elements(lst):
+        return len(lst)
