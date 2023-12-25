a9c677752f323282dca53142fb71d67cf7085655
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:50:46 +0000

    second question left group their

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import sys
+def exit_program():
+        sys.exit()
