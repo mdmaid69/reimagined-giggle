899424cb627260013cbff61457588fd70358e099
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:14:09 +0000

    male system less lock busy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
