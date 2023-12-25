aeca4114ec2c22b4e312b4c1d0cb2f84ac7a562a
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:32:18 +0000

    lay worse black bottom impression

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import tensorflow as tf
+print(tf.__version__)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
