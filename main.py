b53550d6dc9262d93e6da92e8d750c50bb2d53b5
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:58:14 +0000

    appeal conversation pure small reserve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_max(lst):
-        return max(lst)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
