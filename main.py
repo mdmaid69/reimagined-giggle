36d4db30e59a2a58f75b9551b01cbf6a904688aa
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:33:50 +0000

    walk visit motion shoot birthday

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
