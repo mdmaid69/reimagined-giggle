705f3c5a351fc8fbc8ebeb2f80f0d540bffe447f
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:38:29 +0000

    wonder likely right pleasant crazy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
