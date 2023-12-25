0ba3463b85b2c60fffd56c9b85ad4b702ac4f1b1
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:53:48 +0000

    disapprove indeed nigga chat stick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
