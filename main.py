c5346fa10510af0ae66020f1d026bead276bf0e6
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:52:04 +0000

    affect sting trade deliberate matter

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def delete_file(file_name):
+        os.remove(file_name)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
