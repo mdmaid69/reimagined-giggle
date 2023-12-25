ce2fd3918b9bc93a30c8f480bd512ec0bca2ba70
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:52:27 +0000

    about sweep firm benefit left

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def convert_to_octal(n):
+        return oct(n)
