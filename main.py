5676bedb79e71e047657f17642bfa5ab9ab3e3a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:00:14 +0000

    back shape point Tt brush

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+i = 0
+while i < 5:
+        print(i)
+        i += 1
