49526f6ce15fca729bb19713bd85f8f321b02935
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:07:25 +0000

    drive scared traffic crime kill

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
