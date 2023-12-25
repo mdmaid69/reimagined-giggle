503c3056054c157856dd3b9bf8924ede255df551
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:49:17 +0000

    lock stupid mention change grand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
