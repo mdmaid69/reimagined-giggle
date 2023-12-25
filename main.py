c2d725a23c192da8e7572f72d73bb4126be58ca8
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:16:06 +0000

    extremely sensible stand exchange understand

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
