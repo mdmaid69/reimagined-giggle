c29c8b2d7bb14c2b937df94a2b238681eeaa8985
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:36:48 +0000

    wear frame stage hello please

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
