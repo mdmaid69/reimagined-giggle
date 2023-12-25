5b907f0f6d430664f181937aa240e1584a9f501f
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:54:37 +0000

    present willing anxious reject less

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_item(array, i):
+        return array[i]
