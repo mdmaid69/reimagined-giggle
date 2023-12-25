6315efda360c72f3de2f15e235513ce4a9b3b873
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:20:52 +0000

    wind sharp machine weekend guide

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
