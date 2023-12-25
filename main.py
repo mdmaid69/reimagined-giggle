11e641ddd77734a1bfdcda3177b64ab8fbd09255
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:48:38 +0000

    wash clever bag Qq wander

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def is_odd(n):
+        return n % 2 != 0
