6d200cf2ed1b508cc3648aa143537b3e8e008050
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:24:00 +0000

    learn aim mail moon fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def subtract_numbers(x, y):
+        return x - y
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
