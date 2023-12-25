ba911c9e5f53435ec0fef3be091616ad925b59e5
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:20:06 +0000

    delicate that present hand traffic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import json
+print(json.dumps({"name": "John", "age": 30}))
