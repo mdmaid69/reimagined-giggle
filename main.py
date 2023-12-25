77231bddf49a3d2e06b7fa8dfcd063aa39cde2f9
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:00:47 +0000

    stare marry split Rr ride

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  def remove_duplicates(lst):
+        return list(set(lst))
