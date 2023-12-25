45da3f9c42f3152022b9eceb33c9fd99aa62c197
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:02:30 +0000

    necessary address least confident smooth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def is_odd(n):
+        return n % 2 != 0
