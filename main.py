066dfe5ec32f66c1b1af3270aa70740baf3b173c
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:45:49 +0000

    base neat healthy mind possible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
