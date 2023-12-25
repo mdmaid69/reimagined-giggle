ded9b05103cd1235869a48f27ca92ae891aca434
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:57:22 +0000

    argument minor modern stretch formal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
