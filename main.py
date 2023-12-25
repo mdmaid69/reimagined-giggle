8a5933d697092ba7602bfa7da7fde879a19c9a72
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:27:11 +0000

    software flat secret wind real

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
