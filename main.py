2524fc51b5ce8a4151154c6bda708af0c827d6e5
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:04:09 +0000

    regional judge open otherwise along

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
