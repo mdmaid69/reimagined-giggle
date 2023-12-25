0727e0e8d45aaa2322470f371e97b8b796e0ccc1
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:27:29 +0000

    rough male practise defeat light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
+  def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
