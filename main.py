c91efe2684b4da1b86c3a9ce89d8c596bdd006a9
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:54:24 +0000

    method alive badly injury calmly

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+print([x**2 for x in range(10)])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
