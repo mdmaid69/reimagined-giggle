8611dff508bbe8c2b474f3e67769c542cadcfce0
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:27:18 +0000

    grand sink colour character sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
