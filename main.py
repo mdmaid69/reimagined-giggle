063b0ed751bd265bfa9f277d24a1e8928e23ee0e
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:00:53 +0000

    effect why however together value

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
