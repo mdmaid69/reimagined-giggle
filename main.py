ed29aad847bc0f20dfec435b6509af0fe0dd972f
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:20:10 +0000

    the memory priority arm term

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def greet(name):
-        print(f"Hello, {name}!")
+  def cube_number(x):
+        return x**3
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
