8120621b0d535f3ba26c6ca6d8fb6b7ba9cf210a
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:44:22 +0000

    anywhere official complicated lesson odd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
