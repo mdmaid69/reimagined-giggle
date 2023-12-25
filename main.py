e1c4a700f84138a8174c6876338f2d8fdb4a362a
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:20:08 +0000

    arrange as runout I just

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def is_odd(n):
+        return n % 2 != 0
