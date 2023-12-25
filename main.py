31bf57f7419bad2ae9334dfa61199bcab7612c45
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:06:43 +0000

    stranger narrow bad onto low

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def append_to_array(array, item):
+        array.append(item)
+def calculate_acceleration(speed, time):
+        return speed / time
