a0b8cb61c631d79ba07d4a73b21633e81cbbaef9
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:45:18 +0000

    practice knock consist build factory

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
