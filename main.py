e6fd3ad30449dc182406a9e5ab676a702fec9080
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:01:24 +0000

    569enjoy never spin row crowd

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import time
-def get_current_time():
-        return time.ctime()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
