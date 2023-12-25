23019068ca966d5cd9f850f264dd1436d7765231
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:53:00 +0000

    combination teach dress computer action

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import tensorflow as tf
+print(tf.__version__)
