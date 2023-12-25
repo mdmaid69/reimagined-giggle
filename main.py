768b8e6a3be1ac103488f7aa90f6be1519cf31ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:35:56 +0000

    illness finally charge mood range

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import time
+def get_current_time():
+        return time.ctime()
