829ee79d2aeceabd2496aeb7472dc989da373527
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:31:52 +0000

    recent fill plenty natural exactly

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
