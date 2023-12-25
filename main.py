f42f4f82ae61596334c4b24ff6e64c4f9799fcde
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:29:55 +0000

    fear unfair scene appear each

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def add_numbers(a, b):
+        return a + b
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
