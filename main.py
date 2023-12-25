37de6b792831c314720f28cb0e99b15ea445c340
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:13:33 +0000

    forget face trip pain shrink

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
