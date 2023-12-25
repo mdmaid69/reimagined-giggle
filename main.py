94fdb615e3e4f7bc6e0b81bf3d3f489ebce1f2d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:04:51 +0000

    from extra mistake keep knock

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import collections
-def create_counter():
-        return collections.Counter()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
