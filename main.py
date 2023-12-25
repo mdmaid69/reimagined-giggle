b4ae7359c4e275ce8a74ec6263df0cd0726b7338
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:46:29 +0000

    sink deaf rubbish if lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_power(work, time):
-        return work / time
+def get_list_from_array(array):
+        return array.tolist()
