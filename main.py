1b7765bed882381e0d6431abcba709e170829595
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:13:50 +0000

    tap force sing convince judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def remove_duplicates(lst):
+        return list(set(lst))
+import platform
+def get_python_version():
+        return platform.python_version()
