6b32c8c5a74124c78ee262bb6321d5bf1ef27861
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:30:31 +0000

    squeeze sticky which dull ground

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
