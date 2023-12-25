c42ef357597b65f5eb16825e44c36395a81feff1
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:29:04 +0000

    not support set delicate hold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
