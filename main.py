278087793a5f7685fa405904ebd48a85fda1e6ac
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:21:10 +0000

    cost thankyou will business oil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_index(array, item):
+        return array.index(item)
