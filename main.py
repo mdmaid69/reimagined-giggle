8d10b0294d57fe72dbbd293dee3c00df385dfae6
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:52:48 +0000

    total tongue attractive lie complain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
