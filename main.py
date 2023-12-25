4b478a96289e69b9a627497d45ae5869900fea4b
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:59:27 +0000

    church neat wild distance fork

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
