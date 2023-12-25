045d9cf66bea3da446825c7e562e62ca66d09138
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:08:16 +0000

    our question lost straight phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+def count_words(sentence):
+        return len(sentence.split())
