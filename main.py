a3ed51a13dc52674a44f863310ded0709c4ae069
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:40:38 +0000

    calloff stage sticky callback tour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
