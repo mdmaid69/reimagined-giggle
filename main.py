ca94c9976dba7c066ca6e231dad4371bc3f3e454
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:46:19 +0000

    rest turnaway private list settle

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
