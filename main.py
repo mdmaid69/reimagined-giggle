d6ca849477df26559bef689c4c47d00343849078
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:12:47 +0000

    hand violent possibly male fence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
