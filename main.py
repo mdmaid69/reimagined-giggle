5fb6166914e22d87161a0fcfb7086602449183e7
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:14:09 +0000

    cross port shop suck to

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def divide_numbers(x, y):
+        return x / y
