7d805bd20892791e1a2e16fcc763b1857b656f3d
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:42:15 +0000

    scheme flat someone away room

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
