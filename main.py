1bedaf81aef5d42e5302264dacad3f177dd2339d
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:02:13 +0000

    translate card provide press fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
