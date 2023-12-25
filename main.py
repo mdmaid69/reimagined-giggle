11bb6eb3354827f3f358a26a0d1344c7ac7b7d56
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:31:13 +0000

    too young contribute unless Tt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_int(array):
+        return int(array[0])
