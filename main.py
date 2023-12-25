412e396a858b11ff6f054329aea92343953c0026
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:25:53 +0000

    grade public nice rain dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
