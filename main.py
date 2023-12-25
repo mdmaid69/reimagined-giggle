d366f769f78d18ee21896987ef7f2f9b412e8bb3
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:58:17 +0000

    crash clothes stupid attitude worse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
