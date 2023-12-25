bfeb0505f2fbf301fb0d67690ce4369c996c1b00
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:25:03 +0000

    dance brush exact past free

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
 import array
-def get_array_length(array):
-        return len(array)
-import math
-print(math.pi)
+def get_array_as_list(array):
+        return list(array)
