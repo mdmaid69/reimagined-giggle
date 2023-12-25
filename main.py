5e258524892e330a17f208367c2bf6884caa744b
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:55:38 +0000

    organize grade exploration exaggerate attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
+def calculate_pressure(force, area):
+        return force / area
