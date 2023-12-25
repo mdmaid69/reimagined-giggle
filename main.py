1d27fd9009da9e8c77f59fa4b1445152599f6db5
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:33:23 +0000

    connect appeal shut her bringback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
