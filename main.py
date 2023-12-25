975148292dec609e871c1420c636f59b330620d0
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:34:04 +0000

    pot gift danger spring temper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
