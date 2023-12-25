f9d90f069ef6fb81f0028b246537af72641b804e
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:42:11 +0000

    bush lower like base little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def exit_program():
-        sys.exit()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def calculate_pressure(force, area):
+        return force / area
