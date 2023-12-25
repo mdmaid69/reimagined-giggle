9b24f1eb0f9804f6530038f67c45499e7ce71b58
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:28:47 +0000

    blow arrive team memory film

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
