66bbf2d81d8d572b3137758e43347c5095dada62
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:50:00 +0000

    team drop remain season word

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
