95c879fd1168ab9e7b1b12b0a9ed98b2a6faa0c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:44:43 +0000

    edge unable flood offer step

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
