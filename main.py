fc4909f62b7d1dbe512c4d9163472213e61b32f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:24:07 +0000

    please tap keepoff sensible why

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
