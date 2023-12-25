057a257d4f10b07fec699bc68361904ca9cb8f26
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:25:27 +0000

    break once damage part formal

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def find_max(numbers):
+        return max(numbers)
