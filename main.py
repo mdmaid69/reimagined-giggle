66d66984a3dc06ed89228da32ccce80e0f7de919
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:28:33 +0000

    uncertain cruel someone late start

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def square_number(x):
+        return x**2
+def sort_numbers(numbers):
+        return sorted(numbers)
