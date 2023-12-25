72164cf43bd4fdc83c7e3a3bf089d662f49a495d
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:59:37 +0000

    secret onto more surprise string

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def count_words(sentence):
+        return len(sentence.split())
