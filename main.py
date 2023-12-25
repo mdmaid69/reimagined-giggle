e1148ea5e34f6df9b5687af5248417bcd65d85c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:26:57 +0000

    concentrate base big deliberate learn

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def clear_array(array):
-        array *= 0
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
