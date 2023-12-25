0e4dd11cc85e71152a662ab5a76028446bdbd486
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:21:07 +0000

    wish burn clean run outside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
