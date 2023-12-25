2efb7419a59627fa62eaeeef920dd40418e991e4
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:16:09 +0000

    cough embarrassed back joke place

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
 def calculate_perimeter_rectangle(l, w):
         return 2 * (l + w)
