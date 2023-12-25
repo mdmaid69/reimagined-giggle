4b2a10cb6a64c15418009d45f533b44d5f7a2711
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:11:13 +0000

    priority see through climb church

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
