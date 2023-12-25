f46f8457234a3fd7d0be6b110b0f2e32abae86c1
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:27:12 +0000

    possibility damage ordinary strike responsible

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_density(mass, volume):
+        return mass / volume
+def is_palindrome(s):
+        return s == s[::-1]
