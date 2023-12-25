3640db2d7a581c2260aa45d29edaef4e6ec54958
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:37:15 +0000

    respect good race pause yet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def multiply_numbers(x, y):
-        return x * y
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
