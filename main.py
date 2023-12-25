504c5b67a6f9225b3c2b96e75acd8e4a326524f4
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:27:32 +0000

    so shoot Cc handle post

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
