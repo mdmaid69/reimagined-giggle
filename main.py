e79cbe0cba88fb7bcc779cb9ff77c916f73d6565
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:22:22 +0000

    public board home pen mark

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
