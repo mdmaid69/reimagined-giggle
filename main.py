8dff5aae6757cbe5b0996264287c12778b0f0160
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:48:15 +0000

    programme know see figure either

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import random
+def roll_die():
+        return random.randint(1, 6)
