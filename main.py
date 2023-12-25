018faec8a2bb8c38bb3fc2150d0d60bab1547107
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:28:09 +0000

    shoulder neither advice trick force

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
