84f41efbe63998e302853e8e756690b3a4f5565e
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:27:30 +0000

    him nurse slow public have

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
