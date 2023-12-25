17ecea2bb66336332a1783aecad074128496d9a4
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:21:41 +0000

    passage process lead trial roll

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_max(numbers):
+        return max(numbers)
