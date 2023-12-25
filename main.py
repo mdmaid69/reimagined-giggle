3fa5de358fb9035ceed33d00e2a83eb8f427edc0
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:41:24 +0000

    dear rough hardly Oo form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import collections
+def create_user_list():
+        return collections.UserList()
