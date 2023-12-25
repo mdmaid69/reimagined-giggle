53b3dc525c796c29d8d037dd1fe5d1541c3c31de
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:57:46 +0000

    neither cinema whistle drink owe

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
