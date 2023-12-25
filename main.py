d3e5614be2ca69efe81bd8a6189b34d4123ecad0
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:15:24 +0000

    nose insist swing neither block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
