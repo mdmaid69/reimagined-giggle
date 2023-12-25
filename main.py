9eb07ba47ff4fd026ac6ca692781a3843d6e912e
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:28:24 +0000

    cheat example high finish defend

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
