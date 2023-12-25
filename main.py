44791dd6e0e528b3940b39d37f3a17271bb14d26
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:24:41 +0000

    pot repair across software themselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
