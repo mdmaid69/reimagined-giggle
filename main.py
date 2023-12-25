a54943bb9056f21a74f4035ba4662efb151727a4
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:12:38 +0000

    cool wide similar wave legal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
