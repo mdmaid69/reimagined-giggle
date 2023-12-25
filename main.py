0702ec5c791509759ab50409230aea2c65c46473
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:07:14 +0000

    wing clear react turn sweet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def sort_numbers(numbers):
+        return sorted(numbers)
