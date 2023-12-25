0fc6c0d8abb7c1e7131ef5850d8a373e56883bbc
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:34:06 +0000

    side boil guess if period

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
