4418d66990c69a413ac0f554eedf798bd6386b2a
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:57:08 +0000

    rescue fork normal empty true

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def convert_to_binary(n):
+        return bin(n)
