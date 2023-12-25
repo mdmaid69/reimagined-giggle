3e4a820536a2cadc7ff6887b6d7ce481346c6c15
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:07:39 +0000

    pity refer name even before

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def multiply_numbers(x, y):
+        return x * y
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
