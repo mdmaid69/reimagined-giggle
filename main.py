b5b76cce445ba9e0b5b829ea6b7f605f179d064f
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:18:54 +0000

    improve fail heat recover takeover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
