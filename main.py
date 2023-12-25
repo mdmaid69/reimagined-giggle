9bdf02076b4fbba2d7042e47aada4cfd73fc2972
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:17:58 +0000

    lesson pool mystery lucky warm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
