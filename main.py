9fa207c314ffa1727aa69c271a4d7e26128270ea
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:28:03 +0000

    on list pain deliver dream

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
