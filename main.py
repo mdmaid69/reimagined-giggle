0916e71aef6c1c67d670036cf156f1e196b81f74
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:33:10 +0000

    fresh failure breakthrough married tall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
