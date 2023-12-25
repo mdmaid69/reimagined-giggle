62a3a1283ca94fa9ccb71f8b00c9692e2b48cbdc
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:16:58 +0000

    collection careful keepup ear shrink

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
