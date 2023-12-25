faa6da6ecdc6355df1082d09a7ade817d8e55ce4
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:57:23 +0000

    draw cinema swallow calloff legal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import numpy as np
+print(np.array([1, 2, 3]))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
