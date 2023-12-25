f303287fcbdbc6244be64a7202f0782da3ac130c
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:04:29 +0000

    band relationship sell Kk fence

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
