00be5e7cb982e0dbf37436383a1e712b71076fab
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:33:14 +0000

    clear shut service crime natural

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_hex(n):
-        return hex(n)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
