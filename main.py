da3d25bc55ea4605af38745f839f4b26da93a57b
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:25:58 +0000

    freeze fall mine event attract

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
