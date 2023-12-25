15bde242c7636f76b9dc0b4b9a61824393ae72cc
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:06:42 +0000

    close authority mother reduce project

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+def divide_numbers(x, y):
+        return x / y
