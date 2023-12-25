f8bf49ad56c06f7cf0fd53886dd2ea72904f4979
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:26:21 +0000

    father heavy male perhaps plan

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import csv
 def read_csv_file(filename):
         with open(filename, "r") as f:
         reader = csv.reader(f)
         return list(reader)
+def is_odd(n):
+        return n % 2 != 0
