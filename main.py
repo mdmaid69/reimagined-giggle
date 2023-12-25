8f932237b0aa4b87d2c3deb310780ba6b64ecb66
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:06:11 +0000

    accommodation keepfrom create trouble promise

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
