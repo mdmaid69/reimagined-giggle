845dd0b91c1ebf997f19c44f40bea9cf3727aa02
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:06:23 +0000

    suppose 179bite large wealth react

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
