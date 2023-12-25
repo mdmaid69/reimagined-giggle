d2a8f02b0e6f386d1ed0a87e49a181bb1618f4be
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:10:44 +0000

    statement earn bang justice brown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
