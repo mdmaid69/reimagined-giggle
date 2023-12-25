b4f04ad236a6e887cd95ce92bd3fa6c74b7b7323
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:17:06 +0000

    undo basic cheque actually top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
