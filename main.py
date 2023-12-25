1a96a413a0a77b1ab433fb1561a8d99f59220420
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:55:26 +0000

    holdon uniform outside run sensible

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_density(mass, volume):
+        return mass / volume
