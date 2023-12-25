3823413635faf8c8997c46a9218fd5e698b31884
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:15:43 +0000

    account disturbing field smoke war

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
