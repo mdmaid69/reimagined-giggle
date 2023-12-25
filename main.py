0cdfc17ce0fcde64f4609ce02d60b62eb179c088
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:46:41 +0000

    even touch project division vanish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
