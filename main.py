6c2134fa54e0a4da3841e5d9d52e1db7ba16160d
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:28:23 +0000

    begin influence calm influence exit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def add_numbers(x, y):
+        return x + y
