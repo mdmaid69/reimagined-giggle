6a707a284ce34a7bfb44ad7667fbdb90d7e69e19
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:14:44 +0000

    Ss row twist attention list

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def split_path(path):
+        return os.path.split(path)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
