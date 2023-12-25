79e7ac9dfcb11e512e8e1024eac9e8e076ab328c
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:01:16 +0000

    gooff news view opponent correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def find_unique_words(sentence):
+        return set(sentence.split())
