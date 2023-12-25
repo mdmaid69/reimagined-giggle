2817c8a919245f2fe7fc7d1fe2a992a4367dca8f
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:42:03 +0000

    absolutely excuse concentrate amount holdout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
