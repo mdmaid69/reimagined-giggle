e876fc4ff404622d7fd0256fc1344781f3ded8e6
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:39:05 +0000

    meet married further favourite back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-name = "Python"
-print("Hello,", name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
