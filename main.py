bdc029eb10e7ea4d56b4ddcc0c41e4580a5d2c13
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:27:47 +0000

    protection promise cry system reliable

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
