ff9598a37395e33f1bd94168bce0df8d5b355576
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:48:59 +0000

    mixture detail lead common street

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
