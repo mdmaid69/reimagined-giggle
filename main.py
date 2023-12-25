a12633dba1fab5d61457e51b1569106a5cd79c2c
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:29:18 +0000

    Qq where frequent crisis lookafter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-  def is_odd(n):
-        return n % 2 != 0
+def is_palindrome(s):
+        return s == s[::-1]
