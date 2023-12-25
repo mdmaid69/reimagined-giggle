543f9d0ed01e94c3fae5b09ef7e0bfe47daf4738
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:52:51 +0000

    stay occasion sky object regard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
 import re
 def replace_all_occurrences(pattern, replacement, string):
         return re.sub(pattern, replacement, string)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
