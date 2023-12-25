743356392b331d001d85f0138d74374c26814aca
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:08:50 +0000

    country attach commit over towards

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import random
+print(random.randint(0, 100))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
