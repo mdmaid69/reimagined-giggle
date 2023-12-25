00a40c5a9a530da01f51e9fcfb11abac3b67abe8
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:38:41 +0000

    apologizeapologise degree sister trust final

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_stack():
-        return collections.deque()
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def find_max(numbers):
+        return max(numbers)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
