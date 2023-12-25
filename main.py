a26ce7cdc80a6059bb99d23203be5363cb4d5aa4
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:25:52 +0000

    transparent continue under share grand

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
