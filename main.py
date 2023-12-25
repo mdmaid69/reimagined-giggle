d65d4ebe94c7336f071c7f55b45203ded9e6e658
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:15:27 +0000

    OKorokay contrast trap worth have

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
