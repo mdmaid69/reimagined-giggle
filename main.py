3354d487fe546b624b38794a58d80e62597d7fd9
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:22:10 +0000

    cut autumn among steam forgive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  def remove_duplicates(lst):
+        return list(set(lst))
