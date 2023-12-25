d3413231486332119f5fd739aca6e469285ba9be
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:38:53 +0000

    city country payment common hurry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
