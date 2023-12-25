3b324c5327348ba6f2530e7d86a29f7b82f5b68f
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:35:59 +0000

    valuable over mark sky strict

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def square_number(x):
+        return x**2
