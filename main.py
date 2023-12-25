1317e47b4f25eca37c8d3d6802e447275fe44548
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:28:03 +0000

    bush phone basic certainly pass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_speed(distance, time):
+        return distance / time
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
