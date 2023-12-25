c464704f7dde060b5a5894ddb349b21f860c871e
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:07:52 +0000

    from along learn brain idea

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
