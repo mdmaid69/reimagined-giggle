dddfebcdaf07e0533705c75f1f61fb9b62f1bb88
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:02:18 +0000

    slow method half bet earth

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_area_rectangle(l, w):
+        return l * w
