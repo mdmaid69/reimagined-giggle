504afdac9a19ce1ca55789582a1fb5cbf59e2d2d
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:20:48 +0000

    provide compete black at mistake

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def is_palindrome(s):
+        return s == s[::-1]
