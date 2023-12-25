fad0828db123f76764f78fea92ae26a7e9c7c632
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:44:09 +0000

    basically scare thumb leg next

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
