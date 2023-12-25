ebdebd37b1ace5a8da147127a44f5182ba196b3d
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:43:51 +0000

    opposite attractive definite stupid odd

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
