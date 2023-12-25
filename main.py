70af85c6abd3d135076a40a86ed93445a143f6ef
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:59:12 +0000

    strong smooth accidentally hold wherever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
