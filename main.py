9004a82b8bad01aa8a0f87f2934a0c2c58ca0f80
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:16:29 +0000

    turnon taste unpleasant sale wind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
