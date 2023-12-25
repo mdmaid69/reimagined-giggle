d00f53c8db6d0f53775cd7d2639e3a99563a9330
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:34:00 +0000

    already more callup start tragedy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+print(sum(range(10)))
