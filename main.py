bb40254ee8bb5405feab91bfd79b0037d992d382
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:58:10 +0000

    present contact lie best basic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
