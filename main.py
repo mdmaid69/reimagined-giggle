d0a5c92c49ab13a010721ff0aa393e2ca78fac7e
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:02:36 +0000

    arrival possession bin tune anybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_power(work, time):
+        return work / time
