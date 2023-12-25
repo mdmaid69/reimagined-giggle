1f3a8310eb7ab328bfafde3f6ac88ba22d9e1102
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:25:06 +0000

    clearly sick communicate desperate muscle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
