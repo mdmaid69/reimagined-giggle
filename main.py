c680e5a503af833eb320ae407dca92d3ea4ada56
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:47:36 +0000

    emotion unhappy group colour day

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
