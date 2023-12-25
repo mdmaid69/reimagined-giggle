71ab7c80f3cc2d87a0a718729d905384aac0ce64
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:28:57 +0000

    curious winter way tend call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
