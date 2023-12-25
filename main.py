e4f2a911dc174d06efecfff36a8f9a555af1586c
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:29:25 +0000

    contrast wet comefrom nearly future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
