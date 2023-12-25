047679c75b4e8c3c48d172c41ebf9127a24a82f7
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:27:06 +0000

    disturbing voice sound squash underneath

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
