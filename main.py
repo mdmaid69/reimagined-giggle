80de32ed154a00d7c4768d654ab22d3c26df8393
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:28:33 +0000

    sail him balance godown hole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
