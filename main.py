00c98e7a3bfa91b59412db0769d4024f43927099
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:39:15 +0000

    art mouth comment for danger

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+for i in range(10): print(i)
+def reverse_string(s):
+        return s[::-1]
