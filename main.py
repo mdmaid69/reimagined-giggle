d85005cf9386419ad0046ec252247be39c4ab64e
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:01:35 +0000

    tend draw Nn pen bath

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_speed(distance, time):
+        return distance / time
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
