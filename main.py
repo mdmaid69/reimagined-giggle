c1283ee3501f634ba30e15edeb8b7cfc1d23470f
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:47:46 +0000

    difference crash sport talkback stop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
