ac222ed802140623c0d16f541fca1e28bba83a62
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:40:31 +0000

    feel add range since impatient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
