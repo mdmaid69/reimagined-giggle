2b10fa02d7e2411d89c0fe874951c6c4c0df3362
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:42:57 +0000

    direct fight run turnup trust

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
