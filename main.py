3ee6be7d7cc6e0f88205ca909489e58b8c4ee4fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:40:43 +0000

    calm report offer unusual steady

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
