cf88ffe966b56ae711f2f1e00ef58b34a75b4dd0
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:07:48 +0000

    movement black nonsense patience wave

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import sys
+def print_python_version():
+        print(sys.version)
