99609b143b4563a7701e1f0fd625b3c43b7f7fd7
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:37:06 +0000

    long lower Pp tear she

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
