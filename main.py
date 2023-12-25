ebb992900cc653038b71f09806cb9ca760dab2fc
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:09:11 +0000

    blame spot disturbing stamp record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import json
+print(json.dumps({"name": "John", "age": 30}))
