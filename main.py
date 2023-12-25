a0467f8ae0df9ab04676cfa972f13643fd7711c5
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:41:32 +0000

    its emotion match work success

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
