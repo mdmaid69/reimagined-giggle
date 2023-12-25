cf7b83643d60f307c295312e632d433a1ab6b7a5
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:53:23 +0000

    listen sure inquire depend out

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_acceleration(speed, time):
+        return speed / time
+def count_characters(sentence):
+        return len(sentence)
