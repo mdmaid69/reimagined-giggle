69bbe5751b2b166c42d1bd115f1c18c5ffc27e44
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:56:26 +0000

    usually that afternoon crash large

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+for i in range(10): print(i)
