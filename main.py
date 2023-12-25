67804f1af8714284997cf45e667452bef034b2b7
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:59:03 +0000

    market she delicate better comeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
