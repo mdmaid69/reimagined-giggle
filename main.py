4b7f3cbb625b2413ae6cc4dba973686f97d6a570
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:09:27 +0000

    stretch criticism take unite first

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def clear_array(array):
-        array *= 0
+def sort_numbers(numbers):
+        return sorted(numbers)
+import sys
+def exit_program():
+        sys.exit()
