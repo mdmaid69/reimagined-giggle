a2f56166cec0fe25c337217c50b93f2dd3a1df3b
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:48:22 +0000

    direct generous good my waste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+for i in range(5):
+        print(i)
