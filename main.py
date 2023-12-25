511cb4bd0bd79dfbc9acf064aabb0fea64f8685c
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:22:21 +0000

    equal awake cool dangerous shopping

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
