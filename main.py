3c363d894272d4a7d5be78d39b152f57cb9bbf00
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:37:34 +0000

    thorough poor home yet busy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
