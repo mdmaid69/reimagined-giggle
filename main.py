b97b975dc9f7589f0c55a58218761af3d93656f8
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:55:00 +0000

    say stir push field love

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import platform
+def get_python_version():
+        return platform.python_version()
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def check_if_array_contains_item(array, item):
+        return item in array
