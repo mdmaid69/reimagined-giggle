9656bb35e22cdb84196dc2d121d6c4d07498f5f8
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:28:29 +0000

    sign bin may niggaacademy prefer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
