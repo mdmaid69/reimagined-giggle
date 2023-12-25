e876c0b9779f39b023057f551207672c40d72545
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:08:16 +0000

    rough anxious prefer sweep whoever

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+text = "Hello, world!"
+print("Characters:", len(text))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
