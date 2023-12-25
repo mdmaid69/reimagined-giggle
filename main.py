ff2489a4800fc74941363cdcfdd20dba5041e932
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:59:02 +0000

    follow ordinary stare few Ss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_max(numbers):
+        return max(numbers)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
