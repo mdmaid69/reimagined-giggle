9db570c7397494a3f228c744bf7748a9d9d2e8c3
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:23:22 +0000

    yesterday line patience improve soon

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
