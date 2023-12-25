cf5309d75cc9b99e7e54d837a5ec04738f73a78f
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:56:12 +0000

    arrival content fault realize scheme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
