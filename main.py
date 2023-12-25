d73a90f09cf0b82e5ce0455ccb47efffb670ea2c
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:18:43 +0000

    dress single modern pause drink

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
