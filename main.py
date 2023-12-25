2767fb59105a572b261566567e51300ffb0f423f
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:33:28 +0000

    rent subject behind safe free

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-def count_characters(sentence):
-        return len(sentence)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
