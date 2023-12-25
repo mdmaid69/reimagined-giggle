49130340c068648bb2aa7a943b28826efd2b0148
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:27:51 +0000

    ground why hope into ocean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
