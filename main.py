ea22fc6e84e412311823c7bd6e9734d1478139ce
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:37:04 +0000

    lean best brother after healthy

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def find_min(lst):
+        return min(lst)
