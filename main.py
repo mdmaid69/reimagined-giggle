48cbed223574ca7d85e602a9d7d867fc842ccced
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:31:56 +0000

    sit leave team leave figure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def is_palindrome(s):
+        return s == s[::-1]
