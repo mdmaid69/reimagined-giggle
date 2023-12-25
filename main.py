d74eb346599550ba0b20fda8584f9a83523ec45a
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:22:13 +0000

    Dd nervous rarely themselves reject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
