db1c738ef19c87d82fa2c797f75ec26beee3e992
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:43:47 +0000

    impression feed greeting operation wide

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
 import re
-print(re.match("h.*o", "hello world"))
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
