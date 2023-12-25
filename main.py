4ebefbe6122f34201da6fee60de6e2b3e9899b8c
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:44:03 +0000

    hold crowd achievement morning dick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
