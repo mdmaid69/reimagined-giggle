3ec1edc1b60a9cd1351ef734369e05d8a9295908
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:58:23 +0000

    smooth schedule so learn happen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import tensorflow as tf
+print(tf.__version__)
