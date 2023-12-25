d00adfbea0d77668f229384252ac666fa38fa00f
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:23:27 +0000

    contribute plan flat learn apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(x, y):
-        return x + y
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
