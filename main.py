1565b3ea525d92824aa4e644465c2a858bc5abfe
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:58:59 +0000

    afraid dare fill bunch excellent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def reverse_string(s):
-        return s[::-1]
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
