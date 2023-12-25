0099ffea147f0777cf8776f73e39e28ac6118214
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:55:33 +0000

    drop means justice compare 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
