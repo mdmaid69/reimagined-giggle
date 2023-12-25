8038aae099b23bae1422ead46254882a7fcce562
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:13:55 +0000

    debt head arrival remain survive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def square_number(x):
+        return x**2
