2f138ba2f312504e8ae6a5c64241d591aaf648a8
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:44:15 +0000

    score estimate service dangerous wait

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
