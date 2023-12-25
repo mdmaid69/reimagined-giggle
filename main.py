1d5468e93b68d2f33c68b1c23c150116b1647b8b
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:57:51 +0000

    because improvement zero in earth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
