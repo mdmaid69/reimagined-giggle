d72dc33ec4e31abe455246a6435fad9230ea0a20
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:10:30 +0000

    destroy clean Oo town fence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
