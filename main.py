249b0bce22f6334d28bc8cf902f97e7384cfdbd6
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:13:01 +0000

    anyone string breakfast mine fit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
