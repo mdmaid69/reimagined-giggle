00a8a3840c488a8a966a33f62e152d71dd31c4e4
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:38:14 +0000

    impossible think behave in but

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
