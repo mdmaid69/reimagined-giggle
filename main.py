c8265a9b338e996168ad0e2d309d363432688076
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:06:40 +0000

    hold breakup unlikely one depend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
