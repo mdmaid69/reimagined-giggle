b6172031588b9def13f55cc07e9aa1d196530be0
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:18:47 +0000

    phone clearly choice open manage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
