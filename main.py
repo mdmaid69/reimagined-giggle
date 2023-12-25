e4df3b1cabf33d96d830c10538ea429d75aa23d7
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:35:45 +0000

    part damage platform comeover convincing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
