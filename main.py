3a32e645cfe664ff463b71288838a87136ddb320
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:37:24 +0000

    ago content deliberately previous tip

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
