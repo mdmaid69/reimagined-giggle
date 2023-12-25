bd3de6f51fb673ae21f875b48fc38c633a253df1
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:01:53 +0000

    importance dick 570enjoyable away seat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def square_number(x):
+        return x**2
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
