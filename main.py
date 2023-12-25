2061f501dcd09a96fb8a76b5ef0f56ee7637aba6
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:57:53 +0000

    wake treat home hope and

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import os
+print(os.getcwd())
