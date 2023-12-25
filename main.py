8737f51f53f9d524e6f2a533046d88bcd4a39ef1
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:32:12 +0000

    couple remark ready mind intelligent

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import unittest
 
 class TestStringMethods(unittest.TestCase):
         def test_upper(self):
         self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
