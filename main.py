ae3632ec4875236db31d49e70fd363a0966b976f
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:24:50 +0000

    pin artificial acceptable comeup simple

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import math
-print(math.pi)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
