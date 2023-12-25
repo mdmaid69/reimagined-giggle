6c5126d38a343755cc13cf6082f5c310c971dcb6
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:58:34 +0000

    worst along room possession bringforward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_density(mass, volume):
+        return mass / volume
