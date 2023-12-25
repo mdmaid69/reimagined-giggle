136aa69a3d10b866a016c55554e7aa27713caf91
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:18:57 +0000

    guide match copy live praise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sys
-print(sys.version)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def calculate_work(force, distance):
+        return force * distance
