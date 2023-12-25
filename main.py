ffb596f5e0ede6a23373b889d6aa963d48fae28d
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:09:38 +0000

    steady first drawing sensible back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def subtract_numbers(x, y):
+        return x - y
