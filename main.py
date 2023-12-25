1ded80f10d679e35a67645c92b6831d58ad8eaae
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:09:45 +0000

    otherwise explain proper wander uponon

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
