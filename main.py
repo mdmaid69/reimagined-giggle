a595e1f12cd8c4f04dc29b326122d6d970352146
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:42:36 +0000

    memory report reply so move

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
