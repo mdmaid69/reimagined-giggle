42fdcf4913f8833d34cc51921d7922607f30b6f9
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:48:38 +0000

    comeover lower joke address surprise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
