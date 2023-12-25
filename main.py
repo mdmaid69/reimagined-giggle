80ded292c7fec244de777a3dff7f5363961b278c
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:20:21 +0000

    tongue minority properly fit foot

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def convert_array_to_unicode(array):
+        return array.tounicode()
