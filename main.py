46f07c926b98865936d0b8231213f7d9627da889
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:30:24 +0000

    catch what flow explanation scheme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import sys
+def print_python_version():
+        return sys.version
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
