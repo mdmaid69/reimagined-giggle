2afec92d72727fbe26d881721f1cfd926f8c38ef
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:28:34 +0000

    remove bored anxious herself in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  def count_elements(lst):
+        return len(lst)
