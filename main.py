6bb643a2758a14144d0f77f0158297b91a83cb81
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:56:28 +0000

    hang thought empty excited explanation

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+print("Hello, world!")
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
