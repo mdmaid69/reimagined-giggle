d73ca5d228ccc717d76700d046b188005c59ce1e
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:30:02 +0000

    think awake fact relative chance

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
