684abec27d429822c6c7ab0d56a34f2a379099e9
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:17:33 +0000

    receive room everyone outside official

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def reverse_array(array):
+        array.reverse()
