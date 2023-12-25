89c346b494b212e48698601e01bb65d0f6240e57
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:39:11 +0000

    Pp rain law arm challenge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
