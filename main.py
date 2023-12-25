18d9d6387d7363e8f704f68afc9bebc8d91fc7e6
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:58:20 +0000

    thorough exactly light ours flight

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
