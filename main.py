3ae6032939039ce6ab0242af2d659788658030cc
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:02:16 +0000

    split later drop roll building

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
