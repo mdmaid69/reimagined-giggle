44198fe44a3fb58efdcb5242c5ea5f7171b41df9
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:09:05 +0000

    workon shrink season goaway surprised

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def sort_numbers(numbers):
+        return sorted(numbers)
+def subtract_numbers(x, y):
+        return x - y
