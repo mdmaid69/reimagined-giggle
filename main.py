e7f56071614d9d1821dac8a42049c122f9e4b4b1
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:38:45 +0000

    throw definite he giveout prepare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def reverse_list(lst):
+        return lst[::-1]
