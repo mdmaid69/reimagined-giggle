53b76225fcc101b8c0ce5354247168edec2298e3
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:12:52 +0000

    broad challenge day one goback(to)

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
