c08bcb1dd21f3b176c0ef9945247863423e93da1
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:53:07 +0000

    afraid quality tense effort valuable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
