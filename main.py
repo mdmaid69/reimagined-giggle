d8bbb8683fac0aa7d96b0617549afca9267a83b7
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:35:09 +0000

    aware tall ahead fashion radio

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_odd(n):
-        return n % 2 != 0
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
