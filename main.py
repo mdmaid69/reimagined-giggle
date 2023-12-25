ee3c9d41a4556a55dcc14a0804be560faa9ee4fe
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:39:35 +0000

    rule modern comeover question style

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import sys
-def print_python_version():
-        return sys.version
+import re
+print(re.match("h.*o", "hello world"))
+  def cube_number(x):
+        return x**3
