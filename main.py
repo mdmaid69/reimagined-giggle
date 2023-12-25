6d965e6d9c99bf3002219574e0de068c777f112a
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:02:08 +0000

    measure heart plate home church

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import platform
-def get_os_info():
-        return platform.uname()
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
