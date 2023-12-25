21e3142b58c6c45a8e9e3af1b202e46c120cb92b
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:20:29 +0000

    introduction welcome contribute front movement

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
