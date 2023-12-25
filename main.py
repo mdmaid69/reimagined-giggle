bd6b2ac41aec6873200bcac2f696123fb366621b
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:54:31 +0000

    downstairs exact sight ours wet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
