c64c7205721bc3650769f9a69e7624438525f32d
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:06:06 +0000

    minimum hear side purpose sometimes

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
