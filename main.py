8b5443d8344acff99594e77e2f38bb8d6f20fcae
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:29:57 +0000

    eat progress interested either in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
