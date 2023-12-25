e22573b61deca587f68f789fae5755504ea8207f
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:26:41 +0000

    female except doubt disagree off

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import logging
+def log_message(message):
+        logging.info(message)
