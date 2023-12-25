f7d09725d127a61e7228a629f3d6c138ea788f33
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:13:50 +0000

    field calloff accidental listen union

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
