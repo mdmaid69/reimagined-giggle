3333f27afb0c651f83d75c94fddac736e16ec172
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:37:01 +0000

    heavy cook properly introduction fault

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import re
+print(re.match("h.*o", "hello world"))
+def find_min(lst):
+        return min(lst)
