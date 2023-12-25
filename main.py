52191d58b1d3793ed06fc9b065ac25c9e0c020e8
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:04:16 +0000

    rain below film pause grand

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def reverse_string(s):
+        return s[::-1]
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def reverse_array(array):
+        array.reverse()
