8d57c632c9488f3a39d2bb794fd347baea3f6016
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:19:52 +0000

    brave government your smile thick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  def reverse_list(lst):
+        return lst[::-1]
