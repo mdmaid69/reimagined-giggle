d6ddb4238c223f1c6c73b94fe2daf16b2f32683e
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:36:06 +0000

    sea joke talkoutof sink squash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def count_words(sentence):
+        return len(sentence.split())
+import os
+def remove_directory(path):
+        os.rmdir(path)
