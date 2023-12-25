cb0e948c17c301c648c9311c93349409e5ec29e5
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:04:46 +0000

    team lonely prove upstairs field

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
