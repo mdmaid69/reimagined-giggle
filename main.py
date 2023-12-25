d55a278cf3e170603d6493e7cbca62f3af67f303
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:15:46 +0000

    cut card size turnoff song

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def square_number(x):
-        return x**2
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
