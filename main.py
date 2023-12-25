81c90d1e99a5f9611c30ef8c508ac49e4ae66f2b
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:29:45 +0000

    now by sort once mess

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def add_numbers(x, y):
+        return x + y
