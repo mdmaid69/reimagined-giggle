b5b5433e65abd71f99c7228a22aa1b106698021d
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:59:41 +0000

    yet rubbish female dig listen

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
