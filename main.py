2038310d13ee060327ace0304fbb20351952aa2b
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:29:12 +0000

    flow I benefit hold mood

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
