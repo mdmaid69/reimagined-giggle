720b100046b834411469ce9b3951d29c571cb844
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:23:22 +0000

    stamp anywhere any unlucky honest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
