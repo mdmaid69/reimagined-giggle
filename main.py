6f8e320aa4ff833e4a10e6a897eb58d0aeaf7192
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:12:53 +0000

    watch scrape almost sweep themselves

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def square_number(x):
-        return x**2
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
