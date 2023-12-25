7585ca5de164133f1f66f8be330b510a25f98409
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:29:15 +0000

    classroom feature platform mean thank

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_sine(x):
+        return math.sin(x)
