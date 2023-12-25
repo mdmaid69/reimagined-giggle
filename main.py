acce2b2381acab20dcf3cce3120d01268e73306e
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:15:30 +0000

    song dance treat though contrast

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import unittest
 class TestStringMethods(unittest.TestCase):
         def test_upper(self):
         self.assertEqual("foo".upper(), "FOO")
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import sys
+print(sys.version)
