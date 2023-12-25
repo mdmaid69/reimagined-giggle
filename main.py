7de8f7ceb759b44e94d6845bba071c38d322033f
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:25:39 +0000

    waste pretty centre service service

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_max(numbers):
-        return max(numbers)
+import getpass
+def get_username():
+        return getpass.getuser()
+import sys
+def print_python_version():
+        return sys.version
