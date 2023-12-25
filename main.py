8624d33e6957ed74183e778af756a6b5904b769b
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:58:51 +0000

    occur duty people bang love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import array
+def set_array_item(array, i, item):
+        array[i] = item
