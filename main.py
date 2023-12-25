5a5a06f0979698402a4b0faf594a32f1a34558b4
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:56:18 +0000

    breakout be evening body completely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def clear_array(array):
-        array *= 0
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
