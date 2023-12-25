53a2b401add3a1370f8f6c3a6cc749e11d5d24b3
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:01:54 +0000

    rank middle kind many offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_density(mass, volume):
+        return mass / volume
