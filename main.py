d6de907637db91c2b11a8df66470b494f5e7b029
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:07:12 +0000

    almost centimetre lesson pause especially

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def add_numbers(a, b):
+        return a + b
