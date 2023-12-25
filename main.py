56e3a0e28bf6e80c2cc1f2c954f360cd82bea33d
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:17:33 +0000

    disappear Bb joke keepaway woman

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def get_array_as_float(array):
+        return float(array[0])
