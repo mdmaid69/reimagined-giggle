ee6631c3290d20b01b176fe237acad43266047b5
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:41:34 +0000

    lose concerned bread prefer characteristic

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print([x**2 for x in range(10)])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_exponential(x):
+        return math.exp(x)
