b473ffef7c6ee2a6a99f59952eed15198017df0f
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:46:54 +0000

    custom frame crash which position

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_current_working_directory():
+        return os.getcwd()
+text = "Hello, world!"
+print("Characters:", len(text))
