efec38b30ddfefb9295948091390b95b714fa87a
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:01:00 +0000

    room odd kick share return

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def is_even(n):
+        return n % 2 == 0
