4f98d06c03a7f182c38686986c25f434758e010c
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:52:09 +0000

    equipment bridge shame game company

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
