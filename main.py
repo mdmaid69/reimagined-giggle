01408c570afb59990474d340ed9b9b4eb177f7b8
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:38:43 +0000

    freedom nothing stop laugh face

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
