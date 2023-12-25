01d9bff45afa7120b03c7634c03b8a39feaa2b09
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:47:08 +0000

    land cost opportunity right score

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
