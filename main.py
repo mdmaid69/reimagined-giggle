67318f06c5ab95235bea62df9bcd2892fc82d9b6
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:20:17 +0000

    remain except pattern relief Oo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
