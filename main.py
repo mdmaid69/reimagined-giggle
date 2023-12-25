ae6d1344084d430797af15e68a29c0626772457c
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:21:51 +0000

    past mystery serve development that

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_density(mass, volume):
-        return mass / volume
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
