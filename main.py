69590b34fee164f53d78485b06f221867a67eccd
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:14:07 +0000

    average there place dry hang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
