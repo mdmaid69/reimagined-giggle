330a9fc6b0711973aa0ac9636f19744beeb0bd43
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:35:40 +0000

    estimate become trap late correction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
