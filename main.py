c6a22065b67f6025d4b16eae54d8c5191cf1cc21
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:59:24 +0000

    success not invitation get talkback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
