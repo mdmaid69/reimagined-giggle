c14757441fd8886ce41eecdb86c671609f0db3e9
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:48:51 +0000

    height today more nose interest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_current_directory():
+        return os.getcwd()
