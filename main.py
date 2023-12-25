f9c58e5d777749c3ed8a9ff728b511bfca2fda66
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:30:47 +0000

    meanwhile crash loud fierce wealth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
