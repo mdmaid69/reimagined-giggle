36a3506c5a1daac347f41569b0cb5d724584715d
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:16:52 +0000

    sure she before right so

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  def subtract_numbers(x, y):
-        return x - y
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
