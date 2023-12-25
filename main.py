4652ede25929d5e527af1f02e74452afd0772d90
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:42:50 +0000

    black home test out centimetre

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
