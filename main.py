5893f380bb1e9c48d69e245cd3c421e6c9233d4b
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:31:08 +0000

    none sometimes definite agree succeed

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
