0bdc883001782edc45ab6b2fc49aa5083e9aa9db
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:32:09 +0000

    summer round help abroad collection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
