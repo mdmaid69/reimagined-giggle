3233d80b499c1c4245b23f6cdae3afd7927b316d
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:42:48 +0000

    matter because moment soft somewhere

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_power(work, time):
+        return work / time
