b448c1185adde83c938b1691940531e1bcb5ceb4
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:13:05 +0000

    factory cover who practice pattern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_array_to_list(array):
+        return array.tolist()
