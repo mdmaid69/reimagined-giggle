b158fa7ef869dd3b4ba9015060d75904380b2e71
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:45:12 +0000

    attitude serve damage typical try

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import sys
 def add_to_python_path(path):
         sys.path.append(path)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
