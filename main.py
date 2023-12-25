6e1a3d9d9634d12e27df57e71e57100b344c1e3f
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:38:18 +0000

    interesting common match seem world

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
