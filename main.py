0154fd4dd568ea172b1a2cc00235a8433c68c62f
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:25:43 +0000

    appearance deserve world worse afternoon

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import sys
+def exit_program():
+        sys.exit()
