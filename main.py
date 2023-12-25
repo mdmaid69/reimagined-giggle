16e9d065455ded030435413a59fa16521574579b
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:25:13 +0000

    command from basically programme choice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
