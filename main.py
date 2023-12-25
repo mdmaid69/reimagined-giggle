ad50509e305f3fcac211b5ac57bd94692a027f74
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:33:48 +0000

    class level new steep evidence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def convert_to_hex(n):
+        return hex(n)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
