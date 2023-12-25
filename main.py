337af3781c43ff3bb682d337182e47f3295cf9ab
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:57:35 +0000

    use touch matter difficulty free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
