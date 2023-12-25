d8b15476dc0a36c9e123340bb54ed8e21b4fc530
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:44:06 +0000

    dear only clue any money

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
 import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def get_string_from_array(array):
+        return array.tobytes()
