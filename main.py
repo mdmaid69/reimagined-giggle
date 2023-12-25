13980d19422ed875a8c3284a7d7f3b49f9f5f3cd
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:02:28 +0000

    steam bill sure greeting nerve

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import collections
-def create_user_list():
-        return collections.UserList()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
