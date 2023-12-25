a9e89b15c0ce66ec73feb1383e2d2bc0a83bbba2
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:50:34 +0000

    block advantage split tell recently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
