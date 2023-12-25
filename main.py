0bdef4d632f821fae01db8af29cd44f5f1cc2a17
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:09:12 +0000

    sister build probably moment crowd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
