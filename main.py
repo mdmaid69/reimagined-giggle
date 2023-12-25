05873e943c69b3902541980285580fe75a1d9992
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:13:13 +0000

    I mend people police laugh

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_bytes_from_array(array):
+        return array.tobytes()
