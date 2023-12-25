688e4507f9d9ab3cc3f0fe2142aa19853edf6e0c
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:20:45 +0000

    total future extra tired sad

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import getpass
+def get_username():
+        return getpass.getuser()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
