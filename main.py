7e18f936766246a6adb3cc1f18555fb48948edc4
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:03:41 +0000

    chance major objection land break

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import math
-print(math.pi)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
