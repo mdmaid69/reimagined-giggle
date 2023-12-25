b37e5bf62a1d9cb07e64df138ee85ce4da2b2abf
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:15:31 +0000

    agree control reply weekend bury

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
