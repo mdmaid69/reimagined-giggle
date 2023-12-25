eceb16007b36c45d397b34b40f60ad79ece0446b
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:19:26 +0000

    bar out deep estimate point

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
