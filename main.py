b51af9ea89228427659035352e5c880f270e0130
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:31:18 +0000

    maybe wire contact paper report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
