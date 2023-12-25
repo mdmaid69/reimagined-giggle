42cd4d47c1404851a7265fa38d0260015402c5ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:53:27 +0000

    Bb equal worth fun healthy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_power(work, time):
-        return work / time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def find_min(lst):
+        return min(lst)
