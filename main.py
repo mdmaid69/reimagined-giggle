5bfdb2963397ae38d998bc18b54658c06f741404
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:49:32 +0000

    mother carry pride acceptable relative

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_pressure(force, area):
-        return force / area
+def get_array_length(array):
+        return len(array)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
