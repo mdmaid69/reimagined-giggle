3f01e519ab7b74746d3dce898fcc1841d86fe37b
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:23:15 +0000

    pollution dare sensible present shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def calculate_density(mass, volume):
+        return mass / volume
