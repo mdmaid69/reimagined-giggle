29b9751b77ab186b9bd41606e27d9f79376fec5b
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:20:17 +0000

    564encourage fierce software getout since

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
