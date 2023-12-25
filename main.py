ade7fddff76a0ecf51f20c2c4017c3d1e80a8e86
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:39:17 +0000

    never decision bound legal television

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_exponential(x):
+        return math.exp(x)
