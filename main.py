94d81f8f6fb089dce295c6362d8adaa83d1a6c9a
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:46:43 +0000

    happen complete hour test disappear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
