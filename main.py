a70c0b3a3cc0fc5abf9066bcd22c89c3dacecfe6
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:13:55 +0000

    practice follow aim speed frame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
