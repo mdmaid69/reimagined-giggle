b1bdad45368c40530a97a1f1d22b05074266dc4f
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:40:00 +0000

    dark recovery out cold relax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
