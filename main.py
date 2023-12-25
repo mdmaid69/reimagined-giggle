6ed3af5c8c78b476e8f37c74edce4ac6ebad189f
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:15:05 +0000

    somebody Ww fight wave murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
