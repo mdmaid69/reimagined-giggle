90c797059fff5f25f10d89d618aa29bc7f2d4000
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:16:57 +0000

    taste desperate year delicate win

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def calculate_speed(distance, time):
-        return distance / time
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
