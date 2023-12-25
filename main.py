2c4ffd412ceef08cab467b4e3c4b345554f2b011
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:20:07 +0000

    shout last deliberately substance pass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
