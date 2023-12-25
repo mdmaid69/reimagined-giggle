e672b8b4fd7bbe7435df0a3de21d6adf38c2ca77
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:53:25 +0000

    gate other shout calmly happy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-import numpy as np
-print(np.array([1, 2, 3]))
+def clear_array(array):
+        array *= 0
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
