99793c6b504a6664d6580fbb776ce1a7b736664a
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:12:41 +0000

    trial sorry guilty nigga honest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
