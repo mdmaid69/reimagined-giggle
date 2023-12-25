4f97ebd892772eb2e14370a2976b4d07259c877d
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:12:22 +0000

    formal wash gap indeed use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
