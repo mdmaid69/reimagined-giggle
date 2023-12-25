bf0d8559fc570b6b5fc25ed807789cb87585cb00
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:23:44 +0000

    earth listen fit slip speed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
