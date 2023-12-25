c970e48a856361d1b880e798eb258234dd6ac64e
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:43:43 +0000

    pride quit print vanish thin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def multiply_numbers(x, y):
+        return x * y
