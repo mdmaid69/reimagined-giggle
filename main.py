3db2ee42f7190453bb63b2f9d5ffdbd863da16da
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:34:50 +0000

    sure lend combination nigga because

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
