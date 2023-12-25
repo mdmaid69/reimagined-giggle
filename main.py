ae192b516f1d579cd7c4be4932606f6d6c5e12ce
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:21:14 +0000

    guess rank increase minimum play

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
 n = 10
 print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
