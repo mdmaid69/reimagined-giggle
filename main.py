836a9686398740433fe24fe29ef223136f11470a
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:32:45 +0000

    community limit demand suckme challenge

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def add_numbers(x, y):
+        return x + y
