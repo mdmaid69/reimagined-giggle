ab87ff459fe3abb65f421f1047f4f44f11cb8da0
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:40:08 +0000

    chance car lesson consult mood

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
