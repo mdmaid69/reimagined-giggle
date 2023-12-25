76665c260f990b8ad803337f41c3fc4472abaa12
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:26:12 +0000

    hole indeed length concerning brother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def square_number(x):
+        return x**2
