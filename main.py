0dcd6270790ef1d3ab4b50ffd96f8672a5fcc4f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:27:44 +0000

    birthday away fold mark lock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import sys
-def print_python_version():
-        return sys.version
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
