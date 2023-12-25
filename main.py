152d897d07d8b8371ab309a3b2179138dbd7b8b3
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:10:18 +0000

    tongue care over means declaration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def convert_to_hex(n):
+        return hex(n)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
