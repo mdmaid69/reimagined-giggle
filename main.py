c5e73cd0510f9a5ffc07fdae35d1bf44e7ef1861
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:47:21 +0000

    surprise lonely closely habit all

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import time
-print(time.time())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
