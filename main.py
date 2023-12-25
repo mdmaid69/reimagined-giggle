95a6099ef7bd39614cdbe6aa0a359da3a39391c8
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:01:38 +0000

    involve cry laugh union inquire

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
