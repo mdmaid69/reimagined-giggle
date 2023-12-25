a6fe2234ef1f0f18a739911b98bf8a889ebc6ec7
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:46:40 +0000

    pretend report common keepup die

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
