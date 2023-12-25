2a9625f502b73d30530c37f046a66d151b20828f
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:07:46 +0000

    sound appearance transparent answer strike

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+from collections import Counter
+print(Counter("hello world"))
