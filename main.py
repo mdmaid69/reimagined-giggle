bac85bcabf6ce3007257556ef915edee9ae0b540
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:43:13 +0000

    upwards fire breakout have load

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import collections
+def create_user_list():
+        return collections.UserList()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
