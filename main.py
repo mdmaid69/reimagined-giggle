c8f5a5faad9fa81dd2e522c3d20fba75f99370b6
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:02:45 +0000

    normal heavy community regional slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def find_max(numbers):
-        return max(numbers)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
