4eae573e42264aac63d669db5ac3f4c7b4f246ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:53:17 +0000

    star sort whistle experience dress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
