c64d32822035c7b1a67c50c64b2b0ab43693565c
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:27:07 +0000

    whole determined straight spot best

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
