9f0164ddf2a50587b0fce1da4e93af350fea4a8b
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:38:25 +0000

    border term now improve buy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def count_words(sentence):
+        return len(sentence.split())
