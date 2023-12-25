6230c1371ff3f66598f0d808e3a75d888acc7d91
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:19:48 +0000

    statement deep somehow married music

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
