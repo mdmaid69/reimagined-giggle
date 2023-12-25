80a945de4957f2f1ccdac2f3683d98dd58633022
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:40:53 +0000

    mend naturally argument tall experiment

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
