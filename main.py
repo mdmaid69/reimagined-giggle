3e1d7970c6077592d1792a3e462f2bbf3d535359
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:17:54 +0000

    unable substance chase claim 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def find_min(lst):
+        return min(lst)
