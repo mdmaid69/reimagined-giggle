15ab9750165b53ee41f44d62f21e833a5637e540
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:39:37 +0000

    broad awake fire impressive belief

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
