45649b4b8ecf5ad3b13c87299d1d941295483f17
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:27:55 +0000

    bar maybe meal job plant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
