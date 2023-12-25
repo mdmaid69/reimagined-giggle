cb377f28b258e871c6cdf6c0fc56bc1abc68da47
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:59:16 +0000

    interest genuine release stare settle

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
