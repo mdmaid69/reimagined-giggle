361efd64c8f82bb924a0dd63d415cab2f12d6307
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:04:30 +0000

    have cause could see uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
