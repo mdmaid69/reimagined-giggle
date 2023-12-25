788d559e4e96200ba1eafe3e6795f11e5d86b7ab
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:31:46 +0000

    none insist polite comefrom cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
