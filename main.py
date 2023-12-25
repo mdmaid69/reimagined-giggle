38ad5bc74d53b453b84686e1a92b01d502d294e7
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:32:14 +0000

    pronunciation subject rush suggestion meet

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
