e9744c63bf3507c6005b36b49e4c6906450271c9
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:24:16 +0000

    community tough tall escape scream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
