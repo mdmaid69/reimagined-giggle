07d4c292c498e4f56794e4c82a358d9114be5be8
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:32:33 +0000

    Ss trip emergency activity shall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import time
+def get_time_since_epoch():
+        return time.time()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
