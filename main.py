2e75b6e417df4f844b93321fbc818b3b96fd4546
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:46:11 +0000

    busy chief constantly means study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
