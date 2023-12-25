f94efc7566a1fd68e1bb7db5c6ede00280d8299b
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:59:36 +0000

    cheat transparent afterwards whistle soon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
