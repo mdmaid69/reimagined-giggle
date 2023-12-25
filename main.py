b5d5c48bfa77270ee68089aea37cc31ce53b1b24
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:00:13 +0000

    beyond dry awake professional minimum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def count_characters(sentence):
+        return len(sentence)
