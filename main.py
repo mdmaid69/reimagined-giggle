bdd82cfb0ebcd615d0f4927f11778b2ca6195067
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:08:11 +0000

    hot base keepup damage middle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
