4f58ebd6ee23ead0856e75a101a8cbe7eacfbbb5
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:05:19 +0000

    her exaggerate hospital limit break

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
