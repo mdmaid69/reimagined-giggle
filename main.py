4dbc66518af683f324c710025549f524e92fbe35
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:37:59 +0000

    crash freeze walk once choose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
