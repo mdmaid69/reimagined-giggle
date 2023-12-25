8865e4302c5774e5aa97fd8dee65a3c5248abdf0
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:03:49 +0000

    fact stranger lean term comfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import sys
+  def get_python_version():
+        return sys.version
