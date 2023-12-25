06f4262836b06c0c6bb60ae6b130dcd7d08e7cff
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:02:52 +0000

    row get child draw authority

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def multiply_numbers(x, y):
-        return x * y
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+print("Hello, world!")
