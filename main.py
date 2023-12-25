d04db4a10908347c4c9c0913bf21515a1e055725
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:35:08 +0000

    relief right brain estimate final

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_square_root(x):
+        return math.sqrt(x)
