8fe49c4f74339ae5ec383aff97467f52a73e0c36
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:27:43 +0000

    start love regional approval disapprove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
