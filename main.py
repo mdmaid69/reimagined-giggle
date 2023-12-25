ef41424430a30d5a1cd7ed33c79f694c07008f54
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:08:00 +0000

    roof call description guard absolutely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import datetime
+def get_current_date():
+        return datetime.date.today()
