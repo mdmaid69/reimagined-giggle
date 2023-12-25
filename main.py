f8fffe0e89f6d20701691ef935ea72a30fd4e8ba
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:28:35 +0000

    yourself buy minute rise wheel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import numpy as np
+print(np.array([1, 2, 3]))
