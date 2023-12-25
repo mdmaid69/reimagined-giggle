8f2bbfb7a1c351f1a24b021a7a66963a07c4eb12
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:21:19 +0000

    hour soon blame sensible space

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
