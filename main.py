71a19a37858cd172a2cc82883586dc216f48ca0e
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:34:06 +0000

    surprised please both risk influence

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
