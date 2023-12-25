c5a2202fcbfdb2dd0f18c636c8adef247b409acd
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:55:30 +0000

    order pour spill Zz fair

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def calculate_force(mass, acceleration):
         return mass * acceleration
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
