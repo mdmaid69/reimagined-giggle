85cb111eb4ea5c9cfd045ebdb8bca83f703b784e
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:56:40 +0000

    doctor tight care form last

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
