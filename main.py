294c4d229dc20b39c4820832528900ca09dbd45f
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:41:17 +0000

    glad head sleep runout unit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def calculate_power(work, time):
-        return work / time
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_work(force, distance):
+        return force * distance
