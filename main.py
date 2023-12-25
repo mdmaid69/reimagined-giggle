fba8992f0a5ddbd275b08d66de70be499f8f53dd
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:01:51 +0000

    goback(to) base clue wet centre

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
