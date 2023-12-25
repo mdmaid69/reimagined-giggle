f782ac2e28f23804be67251b869c30cc531c9324
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:13:02 +0000

    terrible take typical lend difference

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def add_numbers(x, y):
-        return x + y
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
