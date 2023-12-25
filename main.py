50bf9b8acde68cfc29a61a9d66749a56c046f56f
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:39:21 +0000

    Itself track match spill healthy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
