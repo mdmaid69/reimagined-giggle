84700df63c4cfb22e38483f96ff5204eaaf926b1
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:06:52 +0000

    free natural behind choose suppose

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
