bf3843bc4e6d6ec9c224453ec6ad4f1218593a18
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:02:23 +0000

    Ss bringback pattern weigh ability

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
