7aa0f085059e90492821ba14db66ca3dd7c0b2b6
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:48:45 +0000

    side remove dry whichever greeting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
