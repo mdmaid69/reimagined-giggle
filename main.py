ce936d48186fb3cdd3e8958a4a31e593dc7ff511
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:49:56 +0000

    peace partner desperate habit bar

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
