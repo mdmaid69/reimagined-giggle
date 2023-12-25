f66491dbbd7791fb1b0e2d40a0c7be7428d46959
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:35:08 +0000

    muscle eat honest total die

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_as_float(array):
+        return float(array[0])
