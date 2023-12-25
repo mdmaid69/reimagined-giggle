15dfbdd6e62f9db15c65aad09bfa4a8be8548f1d
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:35:14 +0000

    sort ball slow you modern

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
