c21189cb8fe79c66cfea9cdb40a118e4d21e2ff6
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:53:15 +0000

    offence side continue bored appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def add_numbers(x, y):
+        return x + y
