42d40c28c6e5895e02cade951a689dc7ac20b048
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:21:17 +0000

    complicated worth towards alone birth

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import json
+print(json.dumps({"name": "John", "age": 30}))
