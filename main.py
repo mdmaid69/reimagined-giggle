000c200bf6edded8132d93dcdadf6e6dc41d68ed
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:59:30 +0000

    loan blame repair nowhere wild

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
