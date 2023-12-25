f9b2901e11d13d7cc1939dfefd8b5d036dc71212
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:29:39 +0000

    development possession after valuable apart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_speed(distance, time):
+        return distance / time
