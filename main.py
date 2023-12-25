ced03f01eaec58273f697ae070dd77a190d3c817
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:21:09 +0000

    prevent guide careless own performance

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
