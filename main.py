af4fe86a4bdf07d522b53cd533078e27c477c0da
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:13:38 +0000

    secret stretch try loan I

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
