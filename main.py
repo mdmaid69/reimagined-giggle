85ef27ad459922d1c97ae643f9d94d372b2260ee
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:40:17 +0000

    left notice colour as black

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
