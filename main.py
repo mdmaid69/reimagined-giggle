c6af39b40a0c46413c55b5680d0032135873ceb7
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:22:41 +0000

    wake 566end congratulate extremely home

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import platform
+def get_os_info():
+        return platform.uname()
