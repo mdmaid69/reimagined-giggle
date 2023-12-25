21d8c8628bb7220b2229246ef7ae4fe649b4e742
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:46:56 +0000

    present breakdown tear rough brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
