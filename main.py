07e0a3d9f35c0bc10e8f56a7d204258602011760
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:08:44 +0000

    actually left burn computer day

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import sys
+def print_python_version():
+        print(sys.version)
