66912f50f2bde6b70789b93d360e1ed4da89ed94
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:58:46 +0000

    572enter late Mm damp emphasize

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  def is_odd(n):
+        return n % 2 != 0
