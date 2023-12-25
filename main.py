e4a896a074a104d3ba091002aa582eabf7a09928
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:57:56 +0000

    curious Jj ticket trousers support

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def square_number(x):
+        return x**2
