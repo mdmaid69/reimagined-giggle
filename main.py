6d82110ea3804a93cd8bd445e394a27be8db722f
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:46:17 +0000

    against train form runaway control

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+name = "Python"
+print("Hello,", name)
+def reverse_string(s):
+        return s[::-1]
