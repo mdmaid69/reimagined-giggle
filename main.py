c80dd64f308b1cf3571a311cd07d94c77f0e70f4
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:36:10 +0000

    wander argue confusing split little

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
