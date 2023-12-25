603b3b1d30c819aae07a4746cabb074f076089a4
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:56:14 +0000

    treat but 573entertainment declaration scrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
