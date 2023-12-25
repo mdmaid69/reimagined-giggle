5865e9ff50b1ea07865b4e27b545383254a95fed
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:33:05 +0000

    environment care sleep bar stir

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
