d5fd4e215146f0f67e69b1e2bfc86640e9b4498b
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:22:33 +0000

    judge visit herself success compare

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+print(sum(range(10)))
