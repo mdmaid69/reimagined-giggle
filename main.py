549a2ab0e53c08bbc12e708822acb084c92dd045
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:09:06 +0000

    common laugh task Mm breakthrough

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def is_even(n):
+        return n % 2 == 0
+import random
+print(random.randint(0, 100))
