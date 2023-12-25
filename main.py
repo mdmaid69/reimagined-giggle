8cfe077f9ad68cc9832815dc3fe2b2aec0a20150
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:01:17 +0000

    secret fortunate thumb lend rubbish

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def set_array_item(array, i, item):
+        array[i] = item
