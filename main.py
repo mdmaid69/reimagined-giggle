decd812734e4261e576126087da036878d51bfa6
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:50:08 +0000

    shop feed takeout occur powerful

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import re
-print(re.match("h.*o", "hello world"))
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
