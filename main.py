dc21703b3bace4252507c4ecd594e8d01c8395de
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:15:25 +0000

    chase welcome bad tonight attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import time
+def get_current_time():
+        return time.ctime()
