0d50a12d52f7ae5b27c12cfccb886dcae0b03966
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:39:01 +0000

    avoid spray niggaacademy silent hair

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import time
-print(time.time())
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
