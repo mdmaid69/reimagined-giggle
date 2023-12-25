c420a52d5a9acbead82f200400b055cacbed9f1e
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:59:59 +0000

    dry rarely off turn start

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
