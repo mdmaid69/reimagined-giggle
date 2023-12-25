118b293e16efb65e9fe825ecb67fec6b9c72976c
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:50:29 +0000

    material chance seriously upwards it

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
