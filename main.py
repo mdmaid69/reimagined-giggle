4ebacd7cafa7b3146d35ba4eaff11b75c47f80e5
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:17:52 +0000

    class influence bit holiday bar

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import sys
-def exit_program():
-        sys.exit()
-  def convert_to_octal(n):
-        return oct(n)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
