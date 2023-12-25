69ed04e99dee804f8d7a5cdf30dd12538c59484a
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:02:37 +0000

    world progress store distant obtain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
