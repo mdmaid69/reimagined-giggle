a60c432564594da4b9d0abe8aef2b7c059cc9ae1
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:35:50 +0000

    press block quick occasional send

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import array
+def get_array_as_list(array):
+        return list(array)
