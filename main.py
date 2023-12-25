36526df0bf6bcaa473657fb73f9d9073d469bcae
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:34:16 +0000

    catch explanation rain jump nowhere

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
