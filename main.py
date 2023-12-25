de399102a07e93916340880fedf7363248cffe4c
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:42:31 +0000

    plan familiar teach comein where

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
