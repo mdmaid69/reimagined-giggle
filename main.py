0664a9c804154f33b508fe8d3ad778d809115566
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:15:55 +0000

    land confused know Ww evening

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
