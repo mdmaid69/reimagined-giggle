72c2ac2de39b56f117f5ed665b4a8256ed5767fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:34:03 +0000

    value part personal later imagination

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
