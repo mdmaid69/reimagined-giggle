8ba649c3d71e33b4d1073fdaba5d7284a8fc7373
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:15:14 +0000

    comeover interview niggaacademy fear description

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
