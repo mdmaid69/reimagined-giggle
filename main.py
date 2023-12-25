4a613a2836197c725ee6fddfcd20951936b3532e
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:22:20 +0000

    proof friend hope nose tongue

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
