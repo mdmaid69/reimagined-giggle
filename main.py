9156304f209f016b27f18bec1de3a5112eff12cc
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:15:33 +0000

    example after clear slowly area

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
