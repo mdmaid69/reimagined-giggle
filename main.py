243f374889da821d02c09aab6ceab366f05d9fa0
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:19:53 +0000

    balance advise impressed pleased breathe

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
