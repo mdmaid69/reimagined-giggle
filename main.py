b928e7c27486338956b6004a52cbd15cd0f8daa7
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:23:07 +0000

    actually something represent nigga type

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def convert_array_to_list(array):
+        return array.tolist()
