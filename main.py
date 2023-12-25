7ba4897144424e5070cded2411bec8e041accc67
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:14:28 +0000

    dare stamp runover laugh none

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def split_path(path):
+        return os.path.split(path)
