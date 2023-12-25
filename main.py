51d1bfc3ac74a1b16a007a1dda1bec5b187cff38
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:20:59 +0000

    clear runinto middle 567enemy profit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
