8e5b59038a45873317754c6f0556af9190a27b82
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:33:16 +0000

    reaction stop off prevent scared

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
