252ca1258cb3217f6ae2286e18a44e625399cfd2
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:15:06 +0000

    through guard consider shame where

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
