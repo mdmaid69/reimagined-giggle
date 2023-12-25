246be7e216775e33466c48a827e74464fbe40926
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:04:06 +0000

    somewhere blame treat straight lookaround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
