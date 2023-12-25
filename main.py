788c6c53bfd81d2e827a1712168a8facfeae0ff9
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:14:51 +0000

    today niggaacademy kind answer belief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
