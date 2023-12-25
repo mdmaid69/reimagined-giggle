45a44aaebabc8e7f832613dcf9c44bec5c5a90d6
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:37:57 +0000

    consult buy pity deliver perhaps

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
