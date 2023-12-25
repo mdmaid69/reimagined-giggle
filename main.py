645039e93690047b90b3041b3a97a673a2603ac6
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:10:51 +0000

    mind difference figure pronounce mixture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  def sort_list(lst):
+        return sorted(lst)
