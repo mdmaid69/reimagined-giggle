3bb6bde7849bc08f39b7c49f82de245a910ad6b5
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:43:13 +0000

    Gg trick beginning empty preparation

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
