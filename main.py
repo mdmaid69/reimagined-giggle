48ff03302bc11539cff6143b7aa892d2ee85a4fc
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:03:39 +0000

    turnoff thumb very trip organize

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def append_to_array(array, item):
+        array.append(item)
+import math
+print(math.pi)
