7267234ceadc191a656243c1d07cdc11e28836d7
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:21:40 +0000

    top service them collect standard

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def multiply_numbers(x, y):
-        return x * y
+def subtract_numbers(x, y):
+        return x - y
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
