7c09baf94680e354467b135193a48d24403e1cc7
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:53:21 +0000

    danger crime thank first proud

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
