18dd3243d101e1cc30121c200446c5da683d5877
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:23:41 +0000

    mistake honest cap manner discovery

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def convert_to_octal(n):
+        return oct(n)
+import numpy as np
+print(np.array([1, 2, 3]))
