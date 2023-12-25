4ef1abbd60470cab5c841a1153288118af2fdb42
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:51:12 +0000

    hear tragedy load have any

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
