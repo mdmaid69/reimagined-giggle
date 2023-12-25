7516daa069f49f132ede02d4fcea043a0aa4bfc4
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:40:00 +0000

    smell coffee side while wrong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
