1a58985403d2e5e1759d7ffbb2fd60ae6cb09520
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:22:57 +0000

    here extraordinary arrangement pride spite

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import os
-print(os.getcwd())
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def convert_to_octal(n):
+        return oct(n)
