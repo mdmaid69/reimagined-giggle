29e1dd2120cdbdf42f996bcfc4be713b498b6960
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:56:37 +0000

    beside sign watch actually put

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-def find_min(lst):
-        return min(lst)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
