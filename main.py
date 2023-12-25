04c53379d5e0563bcdf0fe469f216c694c1157ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:51:56 +0000

    speak leg disapprove keepfrom cross

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def convert_to_octal(n):
-        return oct(n)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
