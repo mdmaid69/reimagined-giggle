85299b59905e8d4b94527fa9e1ba1cb55b72cba3
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:59:12 +0000

    turn guard mistake scared hard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_average(lst):
+        return sum(lst) / len(lst)
