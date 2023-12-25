d071328bac373773fb1cce39693782e0ddc52faf
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:04:10 +0000

    shall interrupt either break choose

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
