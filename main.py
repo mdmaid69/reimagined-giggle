b8192729d49af5188e55b2a47aeaef2210b9f991
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:26:06 +0000

    downstairs car rich Itself advice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
