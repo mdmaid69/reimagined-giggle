567fe4b5b3f4f0fdc89c975e63bfd99b605a0c55
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:33:37 +0000

    opinion probably care chase send

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
