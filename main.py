e099238ac971e61005487b5463d4f6dfb901b488
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:04:52 +0000

    most further noise offence well

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
