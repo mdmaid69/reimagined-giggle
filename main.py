67b9522c493cb8477a85d6e4cecaeae0f2109346
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:24:51 +0000

    runinto principle else state responsibility

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
