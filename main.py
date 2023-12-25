5044ab8684c35abf2aa935dbef8c9fce898007e4
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:55:59 +0000

    trap point how power duty

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-for i in range(10): print(i)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
