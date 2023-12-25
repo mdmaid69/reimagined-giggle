7a622c2b59c01638efe6644119bd46d9bea9b946
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:48:03 +0000

    release comeback agreement shoulder understand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
