bfb7c6973e0dd0f396a3f35424be7d765d593995
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:05:10 +0000

    safety half comeover question trial

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def convert_to_hex(n):
+        return hex(n)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
