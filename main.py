87fc0bbcd5044a1cfd09add698e866a6d33fa720
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:15:00 +0000

    half public pretty send base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_buffer_info(array):
+        return array.buffer_info()
