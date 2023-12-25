c8b358f73d9017d78a7ff30d490044c144723120
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:43:41 +0000

    than callback peace left whether

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
