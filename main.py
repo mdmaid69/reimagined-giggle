ffb3015d5543caf05ea4a1e189ad0955eeab7f58
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:32:16 +0000

    detail decide scream real opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def calculate_acceleration(speed, time):
+        return speed / time
+i = 0
+while i < 5:
+        print(i)
+        i += 1
