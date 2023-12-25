8e61cf4f628230137641af6a88cd5ef6e9a01858
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:36:29 +0000

    age Dd pen 572enter time

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
