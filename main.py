e932ca7429345743fc7bb2f0b248b2f9f0dc1548
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:56:13 +0000

    behave way receive wake minor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def reverse_string(s):
+        return s[::-1]
