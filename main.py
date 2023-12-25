d07ff08bf5a0b3df352d9b5c77d8907a017742ae
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:45:18 +0000

    create idea like risk care

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
