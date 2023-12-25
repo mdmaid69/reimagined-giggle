f1cce30b39b89cda1b51db664147ee530205a694
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:03:22 +0000

    path separate maintain good furniture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
