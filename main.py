342619df773c689b662635c0ca86f969b414039d
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:54:16 +0000

    bridge behaviour count urgent minute

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
