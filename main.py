f47da7d9a01b46268955999d6a66f91b9f95bb71
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:33:11 +0000

    shoulder attractive gap pen promise

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
