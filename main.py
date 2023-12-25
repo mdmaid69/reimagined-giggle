0c0649292a969fe351f0f76341888a086ce9b652
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:27:33 +0000

    wide mouth tough cause experiment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import random
+print(random.randint(0, 100))
