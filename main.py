5169d28cb40cccad6184743d02544f5c733f9b1d
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:56:24 +0000

    complicated lucky by proper tall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
