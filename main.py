ea0a13738d0ffa728d0a90bd9169e96cf430515b
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:11:53 +0000

    us depend rob unfair difficult

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
