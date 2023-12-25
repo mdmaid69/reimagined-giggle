c6455897c750130a3fd995f635cf00e509f31023
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:28:27 +0000

    roof send gradually software happen

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
