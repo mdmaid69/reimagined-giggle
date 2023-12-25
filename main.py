1f57e8a02c23cf0b991cb729581a29dbf667866d
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:50:39 +0000

    clever tight crowded somebody case

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
