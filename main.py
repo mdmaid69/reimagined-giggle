6006dab393251a5e2115a9b718776792cb3fc03a
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:42:49 +0000

    ball somewhere aim themselves platform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
