56a0a4a3f6c170d0906ca51d1b033ecba22fcca8
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:56:20 +0000

    brown maximum cause check purpose

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
