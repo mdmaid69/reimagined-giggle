0cf16d99556c0c371e7196068387dfd01b7742e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:03:19 +0000

    reduce little its after her

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-print("Hello, world!")
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
