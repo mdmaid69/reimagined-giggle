04592aef110725875f33bed44947c97694e43e4c
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:48:54 +0000

    slightly still study before cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
