23131402d1ffe48b94dec4be08b2da30ae3db73a
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:04:12 +0000

    believe order face worth obtain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_power(work, time):
-        return work / time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
