38c7975d79b181b8ab4518aef6a22a35291aa5be
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:25:05 +0000

    spot Cc dream forgive gradually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
   import os
   def get_parent_directory(dir_name):
         return os.path.dirname(dir_name)
+def count_characters(sentence):
+        return len(sentence)
