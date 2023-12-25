5014791ea8ecb19af4ee59f248a2869834cbab5f
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:20:33 +0000

    joke wash deaf backwards dry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def cube_number(x):
-        return x**3
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
