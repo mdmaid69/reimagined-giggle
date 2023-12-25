003e41016a04f824bc43a7a46785cb90c7d2ed77
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:35:49 +0000

    here through offence operation agree

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-print(re.match("h.*o", "hello world"))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def subtract_numbers(x, y):
+        return x - y
