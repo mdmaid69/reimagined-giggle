3930f34211308fc8b6bada21c5e2259b99d8a3a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:13:13 +0000

    emphasis land priority boil wheel

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_set(array):
-        return set(array)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
