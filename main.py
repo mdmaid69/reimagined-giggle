eae33162cc559938df4bb2ee9ff65a747a0d0821
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:38:01 +0000

    joke by square star image

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
