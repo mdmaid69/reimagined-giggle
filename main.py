099f49a7f50096ef940ed6ca6fad91fcc2a2c35a
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:41:47 +0000

    except by measure reason 570enjoyable

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
 n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
