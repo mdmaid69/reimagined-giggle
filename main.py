0dc112804ee039e726694b3e588a3ca50a9ef90e
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:29:33 +0000

    car tragedy sea walk steady

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def square_number(x):
-        return x**2
+print("Hello, world!")
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
