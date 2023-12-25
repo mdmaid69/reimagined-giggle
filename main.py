9e4ad0e9936cb12787d5e29ad868108f0517c730
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:39:33 +0000

    comefrom noone mark bus business

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
