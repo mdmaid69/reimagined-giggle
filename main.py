7eb9d15dfbcd70e45ec49e6ba277abab50f9c775
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:20:24 +0000

    practice guide greeting rain frame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
