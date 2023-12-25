1a26bf71ff3c670c67893a3c2003f304d3bd7c06
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:14:50 +0000

    exactly wrong everyone Qq commit

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
