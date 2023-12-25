117cc5a113cae9be515b82213759fd1b130f9759
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:50:38 +0000

    shopping fly fond delicate wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
