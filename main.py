b733e93dc8f2ea3310911aae4593ce38aeb49436
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:59:42 +0000

    already backwards wish deliberate satisfied

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
