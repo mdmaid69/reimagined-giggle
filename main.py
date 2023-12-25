e1dfd93bc100e2897d79122d8d0253fbf3d62ed9
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:38:41 +0000

    clue pass wonder welcome run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
