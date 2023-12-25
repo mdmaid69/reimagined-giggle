1e14c9ef8df52fb7eeb4fca34d1202afd5671f5e
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:56:46 +0000

    phone fond taste gap sorry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
