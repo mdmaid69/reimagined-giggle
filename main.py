55b1f761ab42c07f0dd2dbf084ce25dad8c56e49
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:52:48 +0000

    point note delicate no will

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import sys
+print(sys.version)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
