82d5cf5a97dc858a7e1cde3dce386975f2ca2718
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:06:40 +0000

    impossible relax pipe bother rob

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_density(mass, volume):
+        return mass / volume
