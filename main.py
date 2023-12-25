c2ed70f10ada7653123e66877aaae7c804288086
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:37:57 +0000

    ability disturbing whoever deserve copy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import collections
-def create_counter():
-        return collections.Counter()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
