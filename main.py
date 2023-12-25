c4166ccc6c4d2dad4d61dd8bb6f7ba99db201ea9
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:49:04 +0000

    journey justice please 565end something

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
