30bf021901d95272648445572ddab1466c400311
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:49:31 +0000

    thumb load manage invite calm

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def divide_numbers(x, y):
+        return x / y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
