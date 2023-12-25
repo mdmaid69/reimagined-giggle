fe3b10825bb20e9f0f26a0f3c516b2f994167682
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:29:50 +0000

    careless dull seat weather path

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def count_characters(sentence):
+        return len(sentence)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
