08d97fb3996a9c4cfd9723dce0e6d98582e1ccae
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:23:46 +0000

    path possibility condition before hello

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
