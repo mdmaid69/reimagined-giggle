f6f0d43c4366fa511119db2dbd8f1fa86f7ee156
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:57:24 +0000

    unusual advice programme equipment system

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
