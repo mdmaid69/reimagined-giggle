c3b20a03358d1607c74b4b957b9924c9ca237fc7
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:36:17 +0000

    thorough final variety future load

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
