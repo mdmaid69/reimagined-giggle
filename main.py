e389c87e0817c5c3fee2fdb05985d523d9640a0b
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:38:29 +0000

    pleasure original fear building claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import time
+def get_current_time():
+        return time.time()
