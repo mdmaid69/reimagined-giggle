35d8b84b45ce4a3766d65148a9ea2b422de8c405
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:35:56 +0000

    reason brown table runout Ee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
