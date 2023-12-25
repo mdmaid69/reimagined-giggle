f8e9b11373a4f978343eebd31b8648e85ef1957d
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:24:14 +0000

    tape stick pay guard excuse

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import logging
+def log_message(message):
+        logging.info(message)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
