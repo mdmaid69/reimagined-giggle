060eb0597d3927708378325b5b33d647d1ec2fef
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:31:55 +0000

    Hh equal trap turndown solve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_work(force, distance):
+        return force * distance
