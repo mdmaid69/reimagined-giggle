8a1cf78713d14f24addd5c79977973d80743a3b1
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:02:34 +0000

    into check share off load

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
