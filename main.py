452fa0213b89d4660fb1385de724711af184f335
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:23:57 +0000

    embarrassed will nervous expression firm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
