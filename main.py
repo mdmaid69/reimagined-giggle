c079d9a6600392e5df455e9b0562ce5599e61478
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:24:05 +0000

    permission book whether touch split

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def find_max(lst):
+        return max(lst)
