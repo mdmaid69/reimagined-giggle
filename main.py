0dc0bc5d78e3c1f73e4831c9f2ecb25f75b256f4
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:27:39 +0000

    computer scare excellent their and

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
