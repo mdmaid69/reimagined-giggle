a3ecf430e3789f0c5b812907546489f4da6632c6
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:53:54 +0000

    ago high tall call describe

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
