dbaec00eca26b9036a39c3d9f419e9af0f3d9677
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:17:52 +0000

    chance trust light room regular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
