2071dd7013f60b0791dacc08711f3543d87d895c
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:49:26 +0000

    taste traffic sell free evening

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def remove_duplicates(lst):
+        return list(set(lst))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
