527c3de476d04fa3156556db981970be129ef789
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:51:14 +0000

    round instruction door trouble pity

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
