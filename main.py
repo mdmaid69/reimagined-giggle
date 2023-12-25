d226416f099a24922e5e9598f2dceb56bffe8915
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:52:26 +0000

    provide beginning worried meet Ll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
