5377106531c6c995651c7c28d3ff70f554f05f84
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:11:28 +0000

    easy suggestion city bye throughout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
