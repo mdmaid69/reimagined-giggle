815cedc45ae90ceecc3cd40467ffac260ea1b173
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:37:10 +0000

    it turn far lookup hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
