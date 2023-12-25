80d7f585ff319323b5e4d63ddb3c6520bb27b6ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:01:35 +0000

    hollow physical maximum through excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
