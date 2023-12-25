e74366ab749094c760d35b919407e38b5679b467
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:53:17 +0000

    raise slip lucky majority large

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
