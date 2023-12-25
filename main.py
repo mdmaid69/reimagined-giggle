4b68e778bbdf8b33e103c5c3fc2d38c44bad9d7b
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:05:21 +0000

    formal frame interfere clothes forget

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
