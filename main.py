d57b7d124d37195f6ac9b97ee28abc8bae4af88e
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:47:50 +0000

    offence late lock equipment another

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_queue():
+        return collections.deque()
