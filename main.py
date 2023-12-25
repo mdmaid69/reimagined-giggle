33766e6bbbace78062b261ca2754751516317f50
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:06:07 +0000

    in regional trade argue stupid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
