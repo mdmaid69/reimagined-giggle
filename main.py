54ab588e842e0ab1a254056cd3efe5c6604eb78a
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:03:45 +0000

    568energy regional busy black spend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
