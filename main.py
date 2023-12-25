cf62425be8e0aeaacd82fecd186d622319a852d4
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:32:09 +0000

    it without pot sleep bank

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
