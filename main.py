f36049998ba4088f1b81b9743e2d58eac2d77b50
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:38:22 +0000

    order hurt light wild boat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
