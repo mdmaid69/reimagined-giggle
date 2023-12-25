e0ca50798f262597cb558ba8877ce58786afca66
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:34:18 +0000

    about wake shut large climb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  def multiply_numbers(x, y):
+        return x * y
