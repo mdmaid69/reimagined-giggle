fcf8ce8d03e7474fae00ee0edb687a38492ba0e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:05:36 +0000

    brush rich touch quit punishment

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
