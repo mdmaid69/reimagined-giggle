da619a969916981dcb0e11fd4e8960137378ae96
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:30:51 +0000

    thank treat interview without transport

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
