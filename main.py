0bfa9938bcaa2eb6989ebdf6e6039bae8ee61008
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:45:19 +0000

    can opponent little still anywhere

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+for i in range(5):
+        print(i)
+  def sort_list(lst):
+        return sorted(lst)
