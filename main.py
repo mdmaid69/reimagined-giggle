df7e683d344146e0f80b604592bfcee33ebd8463
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:51:45 +0000

    sure everywhere arrange contents straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
