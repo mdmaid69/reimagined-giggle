5dc46185376d762f42a8b0e21faf6e3c7a6ca1b1
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:22:00 +0000

    system too arrangement hit preparation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def convert_to_binary(n):
-        return bin(n)
+  def cube_number(x):
+        return x**3
+  def calculate_area_circle(r):
+        return 3.14 * r**2
