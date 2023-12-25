e695d704a303073fad8b060d876f802f306c8e38
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:33:07 +0000

    thanks properly sleep few sort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def add_numbers(x, y):
+        return x + y
