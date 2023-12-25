aab2c4db990dd8426ed5487d8afc91e56f794f58
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:57:41 +0000

    proud against should exchange at

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def divide_numbers(x, y):
+        return x / y
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
