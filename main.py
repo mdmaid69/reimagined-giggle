356b4ff40bfec84312a6fc14d3f478edcbd2432a
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:08:41 +0000

    married present forgive comeout trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
