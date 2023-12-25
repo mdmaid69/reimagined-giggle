e1ba6274a6b69122310debb88846eb0bd22371dc
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:21:10 +0000

    quality protect turnback computer dick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def find_max(lst):
-        return max(lst)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
