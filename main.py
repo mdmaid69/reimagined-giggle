5ad9522ad06be34382b31941f6414bcb51a09a6c
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:29:12 +0000

    stop conversation sea tight charge

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import math
+print(math.pi)
+import array
+def get_array_length(array):
+        return len(array)
