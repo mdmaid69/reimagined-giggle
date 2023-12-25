ea73edce9aaac78499f04b31ebebde818d191b04
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:45:36 +0000

    thanks either eye future both

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def convert_to_octal(n):
+        return oct(n)
