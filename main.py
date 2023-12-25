14e04c747cfb3ecd5921792a9e96e7659b2d5087
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:05:05 +0000

    possibility pour arrive goodbye collection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_union(list1, list2):
+        return set(list1) | set(list2)
