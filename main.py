bea99a5d261b4f4454d7db9e09551426b586b6fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:36:12 +0000

    group unless little listen general

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import collections
+def create_counter():
+        return collections.Counter()
