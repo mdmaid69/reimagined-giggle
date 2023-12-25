e465499524009f7411b2fd8dddef9f8305755a39
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:19:16 +0000

    mother Jj exact whoever year

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sys
+def exit_program():
+        sys.exit()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
