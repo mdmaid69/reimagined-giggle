df38b7525dcf7997f0cc9f51581e761366edcce7
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:05:18 +0000

    high force equal game wake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
