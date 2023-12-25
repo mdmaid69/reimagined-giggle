c9a6ecf8fb72546d1b089be9daef85d2887be43d
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:07:07 +0000

    their roof much narrow bringforward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
