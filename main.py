c33d820049e1f144843b8a205589a08cc869502d
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:36:47 +0000

    hospital his wise smart complain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def subtract_numbers(x, y):
+        return x - y
