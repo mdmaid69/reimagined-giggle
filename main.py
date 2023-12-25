d3dc34d538362e533e0017fa60fab4b76cc13a89
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:41:37 +0000

    there away wish nothing profit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
