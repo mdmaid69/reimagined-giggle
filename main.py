5fcd37f8dcaee9f6013f53e73aaa8121a5b471b0
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:19:54 +0000

    act straight kick arrival speed

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import numpy as np
+print(np.array([1, 2, 3]))
+  def is_odd(n):
+        return n % 2 != 0
