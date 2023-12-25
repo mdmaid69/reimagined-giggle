2ece7c7e86925f28bb91940d035d96c79031e8fe
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:53:28 +0000

    own accidentally smoke party slightly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
