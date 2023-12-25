2d24ae1fbad8750e5704a3d7c9d73139cc91894b
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:57:13 +0000

    increase crash board somehow through

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-print(math.pi)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
