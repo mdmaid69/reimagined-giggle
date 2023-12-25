45e276181cbadb3f044c4fdf2fa57435186aacd4
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:04:36 +0000

    by healthy surprising deep prevent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
