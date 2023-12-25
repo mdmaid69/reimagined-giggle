88cbde5e0c64fa246dc5f779f2e6bd788b13a907
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:38:36 +0000

    live continue can man frighten

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import collections
+def create_user_list():
+        return collections.UserList()
