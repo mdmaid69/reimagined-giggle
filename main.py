c82759a7643f0540d97381fad4a59f50afca3033
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:45:37 +0000

    knowledge declare mistake area proper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
