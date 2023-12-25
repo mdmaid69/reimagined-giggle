39711ff0af2ef7dfe508cbe68aa5d002b857dbd9
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:49:47 +0000

    no react evening learn aim

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def remove_duplicates(lst):
+        return list(set(lst))
