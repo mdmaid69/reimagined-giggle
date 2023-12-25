0ad1b2c5edf53531f4d340c159b76789603fcfed
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:12:24 +0000

    brown man object middle confident

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def find_max(lst):
+        return max(lst)
+  def cube_number(x):
+        return x**3
