26034dafb4a7095a95a9325d3cf49d2dd8932c72
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:27:04 +0000

    gradually tell exchange record deserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
