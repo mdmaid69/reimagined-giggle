bd2efa0501f8424b67dccdbe3b41102999351564
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:08:29 +0000

    slice suit hear rid worth

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
