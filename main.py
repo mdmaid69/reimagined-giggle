2731744ee077f69d0efd1758581559c7f5d88469
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:47:44 +0000

    callback spill like church from

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_density(mass, volume):
+        return mass / volume
