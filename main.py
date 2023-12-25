fcdeb20c1695c5fb1cb523c151ff93b2630a029d
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:18:09 +0000

    damage bottom term appeal finally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
