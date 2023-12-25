0258f813fc04b89839c262eaad4d4bd861189da5
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:37:08 +0000

    attract Ff leg before miss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
