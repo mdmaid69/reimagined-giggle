fcc16a6f56deccf2976dd117264917e2c97862fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:10:01 +0000

    lend makeup direction invite become

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def reverse_list(lst):
+        return lst[::-1]
