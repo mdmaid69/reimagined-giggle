898c4c39d4bd4eb53a9cee02faa01fcea09d1948
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:46:10 +0000

    Qq injury talkback thinkof light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
