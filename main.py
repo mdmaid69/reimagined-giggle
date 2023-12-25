97d47e4a51cb75b313b88abbb1c6f4fa2152e4ed
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:37:58 +0000

    negative sure average very wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-print(math.pi)
+def calculate_factorial(n):
+        return math.factorial(n)
