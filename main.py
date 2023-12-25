10c8eb1e086e0ad91c17b5cd8318c1453ab6d1a2
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:56:53 +0000

    easy politics room goout upwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_work(force, distance):
+        return force * distance
