757e5b9b82518d8482e3ea02ec48ae305dad5bb6
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:05:45 +0000

    travel know frame minority related

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
