e613507c97c51575bd2627b15c6a8de94753ae01
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:53:44 +0000

    hurt point sting soon positive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
