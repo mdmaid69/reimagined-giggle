febcffbed03cf7f21f7a2f9868dfb7b3f1256b22
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:24:25 +0000

    female prepare list married operate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import platform
-def get_python_version():
-        return platform.python_version()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
