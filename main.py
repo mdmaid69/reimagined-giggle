5d34b68c7a61a7ab0690af903071436889b3d9d0
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:54:22 +0000

    be track about stone hollow

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
