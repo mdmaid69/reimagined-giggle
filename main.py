d3ff7728ea76e0a47656f6f4558a495921a25187
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:11:50 +0000

    image lookaround wing along pull

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
