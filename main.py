0eb61d727629ea072ae5a7f9fc8995b150d7557b
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:45:36 +0000

    this die challenge drive though

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+print(re.match("h.*o", "hello world"))
