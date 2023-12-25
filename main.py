1e0d99b0532990f70f42897e928241c6780ce982
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:03:49 +0000

    wave protection lie colour weekend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def find_min(lst):
+        return min(lst)
