4f79c347ddfd3c274f5285533f23b67d53dd831d
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:45:53 +0000

    wrap by school phone against

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
