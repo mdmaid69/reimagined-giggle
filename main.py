afd2303cd1d5420cc02597190843c840d989cc50
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:21:17 +0000

    after more mass getback mark

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def square_number(x):
-        return x**2
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
