b5f52f3448c0cf43ca79f4b93bd700865f210376
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:11:13 +0000

    but peace opinion responsibility straight

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
