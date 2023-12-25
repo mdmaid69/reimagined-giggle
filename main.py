3749257cc525126b9ccf42340195b60ce12b9b2b
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:30:44 +0000

    goback(to) afterwards justice poor service

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
