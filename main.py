dc2190ba935847db95e09bfdcfd989142f4fb3d0
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:13:04 +0000

    least slightly rare fasten relative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
