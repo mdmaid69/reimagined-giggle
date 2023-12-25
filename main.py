291560cd08db2f889276f1897d2bb3e3717526a7
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:48:03 +0000

    equipment wise fast extraordinary relax

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def square_number(x):
-        return x**2
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
