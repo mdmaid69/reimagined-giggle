325e62d63fb495e7a0e26c11ad216afb3446057b
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:15:30 +0000

    scrap before serious experiment down

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def sort_numbers(numbers):
+        return sorted(numbers)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
