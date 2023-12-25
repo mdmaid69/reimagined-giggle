e245404c1f1a5507f3926c6579cffa1639a92f3b
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:15:57 +0000

    subject blue home rush unable

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
