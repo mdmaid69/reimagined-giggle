3a001c6be3705eb30feb009dfc4a6b7bc1b38e29
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:42:58 +0000

    entrance perfect each Yy choice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
