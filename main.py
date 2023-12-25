656c669b2c5bd2deb58ca22fa0ff0d97129bec84
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:32:50 +0000

    wander concern address appear expect

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
