94cc48247f0c491bd7aa172a17d696d330b291e4
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:39:08 +0000

    laugh Ee union feel fire

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
