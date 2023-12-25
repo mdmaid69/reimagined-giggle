44906f9f52a480f08259abdb6689993c5969759a
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:03:17 +0000

    loan model only characteristic statement

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
