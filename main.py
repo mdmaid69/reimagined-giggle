1e438c977c8cc7a2348a822720e932ab76053e03
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:06:21 +0000

    pleasure post payment listenfor exit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
