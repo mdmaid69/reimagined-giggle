f19332edd8343d0437e9cb969e589b481667eb33
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:45:04 +0000

    better takeback sensible complete black

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_index(array, item):
+        return array.index(item)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
