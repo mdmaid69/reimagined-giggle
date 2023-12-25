b626a569ea7704207e9f844cb8aae2b11aa18523
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:01:03 +0000

    hard occur terrible punish confusing

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
