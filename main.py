93c2aaf92442f5e9da0d62b19926f4ca90b58c48
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:06:35 +0000

    thinkof complaint visit 564encourage sweep

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def sort_numbers(numbers):
+        return sorted(numbers)
