5ed197c7c1a7100f946a01932da4f0ffee3fefc8
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:31:47 +0000

    bye loss band need weather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
