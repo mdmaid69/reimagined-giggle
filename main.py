60203c17bbc122620bc66f48b470d24534c40789
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:55:56 +0000

    comeback gooff down giveup empty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import platform
+def get_os_info():
+        return platform.uname()
