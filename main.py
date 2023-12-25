3af42879f5d4d0ab5f26974d486ea7285f6719d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:02:27 +0000

    unfair cinema somehow tip punishment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
