17a7d293a920d03cfc78cde90d26bd68a957ddd4
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:14:30 +0000

    guess hurt moment method post

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_str(array):
+        return str(array)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
