430c7d2c5f673828d3d29f94bc257aedeaadb4b3
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:09:56 +0000

    job question pair doubt grateful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def convert_to_hex(n):
+        return hex(n)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
