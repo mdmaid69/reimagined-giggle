dcfa6084aec8331a3d46e44a9177de5930d020a7
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:46:27 +0000

    fairly shake age reduce obvious

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   def multiply_numbers(x, y):
         return x * y
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
