40579274ff549733daf14a7d5bfb5dd452f497b9
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:42:16 +0000

    shade country imagine voice lay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
