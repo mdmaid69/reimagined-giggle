3b5f6a5902a242e283d6261be15e20df9adbb362
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:46:46 +0000

    thinkof belong Bb reliable person

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
