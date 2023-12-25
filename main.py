a782d57c64633a5fcd50f2fd2c6604a24047f5f6
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:45:27 +0000

    life guess simple theirs train

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
