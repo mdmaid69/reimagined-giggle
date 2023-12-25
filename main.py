982897a578dccb41c90b04e84f51e831093ddd4e
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:03:36 +0000

    calm bank activity demand near

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
