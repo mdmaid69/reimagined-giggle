45f45a9d7780ec99f2fe2fdd79b1fb35aedac1f6
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:52:09 +0000

    ashamed fond careful entrance it

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
