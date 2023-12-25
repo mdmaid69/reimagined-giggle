24d8b481a0bc3cb76e95f10feba45313a5463f83
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:46:39 +0000

    everyone commit pupil pronounce convince

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
