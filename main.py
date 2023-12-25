9cea5df29c233e6e154850a51e56c2fe6614c0ed
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:18:38 +0000

    save suck me collection bank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import array
+def clear_array(array):
+        array *= 0
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
