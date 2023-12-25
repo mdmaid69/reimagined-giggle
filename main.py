27a4a5e416c4bd1b4107320050de64e8bf20c52e
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:04:24 +0000

    break rubbish callback example appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def get_array_item(array, i):
-        return array[i]
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
