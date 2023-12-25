7809898898d2facb48d7c266e69b0cbc2baf077b
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:02:46 +0000

    past lend towards crash street

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-import array
-def get_array_length(array):
-        return len(array)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
