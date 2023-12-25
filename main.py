cde90fc265d5b0b6224e171e2755116687036e77
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:35:12 +0000

    exhibition thought telephone present serve

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
