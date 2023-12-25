c9692c599c970fdfd27fa6d07c8aedb04b258c6e
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:03:56 +0000

    gooff insist flat surface properly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
