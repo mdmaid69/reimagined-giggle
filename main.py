354de171aeca73aba95c3eb59e5c7aed6d576c36
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:00:25 +0000

    century prize issue pronounce movement

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
