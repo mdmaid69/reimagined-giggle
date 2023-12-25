074d94b76aa020024639c38cfa54fe1de7237817
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:46:32 +0000

    through unable success expect blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
