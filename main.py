5698fc68be2817a7d7c89c10c431539d8d08f05f
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:03:11 +0000

    heart bang word children lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
