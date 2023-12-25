5a160206dfe023d1f31ea08ed75363110bd2b1a3
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:54:50 +0000

    pattern nothing still correction black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
