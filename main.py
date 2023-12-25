4c56923cf2979f4091ab17f16e89aa02ee849269
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:16:19 +0000

    lock water design her company

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  def square_number(x):
+        return x**2
