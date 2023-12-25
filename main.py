d726b2d076132d6ad01e1fa48537ceed64318aec
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:44:13 +0000

    all remove window 566end moral

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def is_even(n):
+        return n % 2 == 0
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
