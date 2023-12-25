14cd7e1c20f2d045ddfe50ca571b2bb8a7bec485
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:43:37 +0000

    interest bang transparent instrument touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def exit_program():
+        sys.exit()
