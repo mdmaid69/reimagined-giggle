3331b1b13ce73843f48a041ddf2188b93e808037
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:10:47 +0000

    afford stand neat illness unfair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def reverse_list(lst):
+        return lst[::-1]
