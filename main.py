215083645c898547f7956f50f4bb4c87187f48aa
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:53:31 +0000

    influence Yy calloff claim hurry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
