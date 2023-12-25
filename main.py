6ce72c7739ea8bb1100723c5b0cafdfb9133e476
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:35:03 +0000

    left become meal bite translate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def delete_file(file_name):
+        os.remove(file_name)
