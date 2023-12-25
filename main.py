23557fcc22c7b940b0ca0ed1e3c0058b0cbb43e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:39:17 +0000

    small chat boring air under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
