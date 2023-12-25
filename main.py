6ca77e48be68d41632d03997821897191f7628c1
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:03:47 +0000

    wide safe recently appeal listen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
