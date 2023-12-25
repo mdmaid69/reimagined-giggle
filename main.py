e1dcde7f652a010e2847da7bbd7d54597473fc22
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:42:50 +0000

    amount also rid suffer little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
