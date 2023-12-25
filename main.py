8e729433ff475ed524f7a27605e6132d5207d5dc
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:19:50 +0000

    search square achievement important roll

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def append_to_array(array, item):
+        array.append(item)
