55a23bb9b33c55af011e3aeb2258d131427d53f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:38:26 +0000

    ear band squeeze wing thorough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
