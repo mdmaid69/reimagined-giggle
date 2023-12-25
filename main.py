df52fbbcae31711d1ebe9ad7150f1b189e24ea60
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:09:14 +0000

    crime since count average peaceful

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
