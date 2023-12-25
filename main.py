beb47ffb76c84c4e3400a92d76efbb8476abf9ea
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:22:23 +0000

    tonight appeal fortunate real battery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
