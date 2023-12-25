26ef5a35cee331cdcd366024aaace0c344e08014
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:11:50 +0000

    number achievement full naturally Jj

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def append_to_array(array, item):
+        array.append(item)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
