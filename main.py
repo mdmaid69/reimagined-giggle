c13bb254760d07d95921ffaeeb23bc2fbff57384
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:06:43 +0000

    second cause lucky sorry closed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def is_palindrome(s):
+        return s == s[::-1]
+import tensorflow as tf
+print(tf.__version__)
