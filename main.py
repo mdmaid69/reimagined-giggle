9f41dbe099da898ad6682c82d455577d913497f0
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:04:27 +0000

    embarrassed worried generally comeoff matter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def add_numbers(x, y):
+        return x + y
