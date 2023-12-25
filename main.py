768a0e2b2de8f2f022f9264e3aab8a0e87f4888b
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:52:48 +0000

    different afterwards smooth government power

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-print(math.pi)
-import collections
-def create_stack():
-        return collections.deque()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def split_path(path):
+        return os.path.split(path)
