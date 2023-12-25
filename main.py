d8b0054806553ae9ed71a5c2c217a3bca201c6d7
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:48:26 +0000

    comeback union education face certain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
