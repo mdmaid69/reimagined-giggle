fba7cbdc9ef835affa723e6935e4a27a8c8a97ee
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:52:11 +0000

    clever language marry just start

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
