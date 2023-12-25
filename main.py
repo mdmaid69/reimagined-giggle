3fdc9c25c1bd2748782b92fcdf15d7e60226a50e
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:45:24 +0000

    church Bb profit hit afraid

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def reverse_list(lst):
-        return lst[::-1]
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
