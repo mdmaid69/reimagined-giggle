cd91e0940fe8928485f1a2fa8095e4f6c86b8efc
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:24:48 +0000

    hello skirt perhaps like point

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
