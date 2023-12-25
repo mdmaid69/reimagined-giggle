57e1185883fb40bc60be2c4b373a6e51c19cda14
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:59:05 +0000

    wise means ashamed less risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_user_list():
-        return collections.UserList()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
