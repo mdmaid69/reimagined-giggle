d88775123c9916b9ab3f7cd688f4c2054547b23a
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:57:09 +0000

    run drive trade 569enjoy legal

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def count_elements(lst):
+        return len(lst)
