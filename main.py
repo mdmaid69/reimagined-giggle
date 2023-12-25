60f39455f8196c5d1a37db364dd97e840aab104c
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:22:04 +0000

    sure 570enjoyable hair guess principle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
