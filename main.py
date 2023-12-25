c9d74da47e9a48316c957c1f3f9d4af9329e72ce
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:21:44 +0000

    last pass valuable turnover who

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
