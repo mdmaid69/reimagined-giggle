2d51a41b0a21dd94d5e3e4d99122cc7ffe264593
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:21:37 +0000

    so loose damage juice purpose

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
