75aefb3df03e57fc3ed52354e92b770cc86d4dca
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:36:55 +0000

    platform bright niggaacademy Nn edge

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
