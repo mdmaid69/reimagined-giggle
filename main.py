3429701cade3a867854cc8585efa625ce339b181
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:27:06 +0000

    disturbing well pour concerning point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import time
+def get_current_time():
+        return time.time()
