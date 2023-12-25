34b34cc54c9f92d942c63184f6ed3deccd2ab0a7
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:29:55 +0000

    fill priority recommend upwards wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
