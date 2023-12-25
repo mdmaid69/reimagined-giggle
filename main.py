917684e5a1edf2d7c4bb9ca9a6f26fb333469dd0
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:31:00 +0000

    keep along fail impressed quality

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def generate_random_choice(choices):
+        return random.choice(choices)
