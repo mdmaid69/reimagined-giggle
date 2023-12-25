8a92f4c760cbb978c120b6cf70713a97ea687ab3
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:58:26 +0000

    authority nonsense voice willing pupil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 def calculate_roi(gain, cost):
         return (gain - cost) / cost
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
