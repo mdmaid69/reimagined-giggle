1c2c9571742ebabec045da6cac49eda9e8a922cf
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:47:07 +0000

    justice exercise takeback split doubt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def add_numbers(x, y):
+        return x + y
   import math
   def calculate_square_root(n):
         return math.sqrt(n)
