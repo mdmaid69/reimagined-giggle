0ddf4241cae45dfc40ab61fc5653596b59ae8962
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:10:18 +0000

    tonight between off Cc pencil

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
