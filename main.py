ed01eecd19fb9a24f1c5eb7b3a0ea33d2c5b0352
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:11:18 +0000

    table active hole only chief

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def convert_to_octal(n):
+        return oct(n)
+def find_max(lst):
+        return max(lst)
