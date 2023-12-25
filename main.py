fb7f130724178ca98af9fc0772859238fcb0b2e9
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:01:16 +0000

    rather blame be undo in

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import sys
+def print_python_version():
+        return sys.version
