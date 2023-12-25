40e44d2c97166714d57ba34229fc75d3d9769191
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:11:36 +0000

    hurry clever confuse house needle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def find_min(lst):
+        return min(lst)
