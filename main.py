360c6ff449fd0ca91014344d96ced45f2e3a3c3f
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:20:46 +0000

    spin unable unusual be outside

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
