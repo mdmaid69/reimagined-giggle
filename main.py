7ac3b43601441896b7c65592a3a960673f35f8e2
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:03:10 +0000

    detail grateful trial project chief

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
