4c15701df29c1e882fc4270c971217009f1bf612
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:28:54 +0000

    him waste maybe them hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
