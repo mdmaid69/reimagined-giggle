f091ce09d890e17d28dfc08ba77b6cbe1b25744a
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:06:12 +0000

    lookaround meal own part runover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
