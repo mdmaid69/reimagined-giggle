b5933e2c355b4ac58c67d1166d518ff4c2938498
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:01:36 +0000

    allow wrong disgusting gift alive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
