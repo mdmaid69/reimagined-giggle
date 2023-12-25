47adca152f399917bedf27484890c7a08f15821d
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:52:20 +0000

    firm suggest sudden fix Hh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
