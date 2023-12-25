0729f7fee19a845e01cf8b3f4c7c411bc74f87e6
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:34:14 +0000

    rise loud theirs connection bear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
