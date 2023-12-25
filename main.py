368ace6dea8874fdca83ad3e52014c3069371c29
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:22:34 +0000

    wide team which mine different

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
