1c705edf819b2f2a6f12941ac13bc6d06b09cd7a
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:41:39 +0000

    pocket taste apparently influence reasonable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import os
+def get_current_working_directory():
+        return os.getcwd()
