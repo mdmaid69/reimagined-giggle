2f21d9fcafa8c5d95fea500bed9de908bdf1d6d6
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:49:52 +0000

    spare future your so embarrassing

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
