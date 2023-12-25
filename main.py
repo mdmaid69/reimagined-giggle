b5e73b8953c3155e4ce3a42b3d32b06d64724353
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:15:40 +0000

    answer related cost Jj preparation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
