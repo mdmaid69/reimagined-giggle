77c6d5e48a7ba4bb509e1864a8fac5f099812478
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:16:00 +0000

    inform breakinto disturbing neither health

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  def multiply_numbers(x, y):
-        return x * y
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import numpy as np
+print(np.array([1, 2, 3]))
