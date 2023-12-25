41a939374fa83fac64ddd940f896b2169030f09d
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:33:15 +0000

    journey cost design heart better

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import tensorflow as tf
-print(tf.__version__)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
