90304063ec231b33e8786c1c0952e909ba1b82ba
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:26:58 +0000

    eager minute wipe chat genuine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
