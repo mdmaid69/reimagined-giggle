e9b70f4ebde705f055aa2a9f2ab427dd405f8aef
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:21:48 +0000

    sense fair usually talk grow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
