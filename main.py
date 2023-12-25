3b24575e4161d57032671cafe0945611fb1a1d52
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:52:40 +0000

    although some tap operation activity

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import array
+def get_array_length(array):
+        return len(array)
