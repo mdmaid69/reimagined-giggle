ab530c3a5034c344edbb47d8531e9a9a433e5c55
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:51:09 +0000

    interview broad job post shallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
