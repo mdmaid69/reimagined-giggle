582289a0add96d77c4b2e60a9c278749536326ce
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:53:11 +0000

    push probably getoff several brush

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def multiply_numbers(x, y):
+        return x * y
