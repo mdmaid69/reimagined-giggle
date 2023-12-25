bab13611f9faff56337e037cfe6fb4e0f4394615
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:26:02 +0000

    discovery fear famous under angry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-print(random.randint(0, 100))
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
