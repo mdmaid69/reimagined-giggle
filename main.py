1d1f1fa5216db4bed1e0bf13d31fea67664e392b
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:22:40 +0000

    confuse post prize closed means

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
