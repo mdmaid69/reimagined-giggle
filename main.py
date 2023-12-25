c484bd82798c129e08f96df8373b2f31e0dfb54f
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:16:01 +0000

    finally couple belief busy turnback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
