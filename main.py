a9e6ec5972eedc612fccbf0d4f9f9050ab970122
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:16:15 +0000

    match care fear increase storm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_power(work, time):
-        return work / time
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import os
+def change_working_directory(path):
+        os.chdir(path)
