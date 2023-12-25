4637754152a06dd2e194a9607de7e7c3294e88f7
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:24:24 +0000

    keepoff confused object hour set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import sys
+def exit_program():
+        sys.exit()
+import os
+def get_current_working_directory():
+        return os.getcwd()
