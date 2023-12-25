5c178d028a4a0b366407d7d9fc9806a40142b8d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:15:15 +0000

    light sky consult explain replace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import logging
-def log_message(message):
-        logging.info(message)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
