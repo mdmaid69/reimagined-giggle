a155ab94dbe637c5ac34b2b11946ff3633b9cbf9
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:14:38 +0000

    book back stupid might little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
 import array
-def get_array_as_str(array):
-        return str(array)
+def clear_array(array):
+        array *= 0
+import tensorflow as tf
+print(tf.__version__)
