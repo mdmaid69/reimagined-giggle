d4cfc7c1015f65b6572b7455e4294dbacbe566bf
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:11:23 +0000

    ill wake sensible cheek Ww

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+name = "Python"
+print("Hello,", name)
