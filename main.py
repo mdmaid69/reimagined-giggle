e51f3371164ef86408509cd87a21ffa5dd9e074f
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:52:27 +0000

    around boil statement past think

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_power(work, time):
-        return work / time
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
