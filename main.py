3497877e166f4e7c5d2beead1d05363e00878994
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:25:33 +0000

    reliable deaf sleep convenient foreign

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
