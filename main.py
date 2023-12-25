40f838d9e270f60e069ac8852e29d0b590ce0e97
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:29:31 +0000

    thankyou clothes body runover scream

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
