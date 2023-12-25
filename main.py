5722c2f85ea21d599cc50b28efbe84e477681f39
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:55:18 +0000

    swimming method because their glass

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import collections
-def create_queue():
-        return collections.deque()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
