5471984a33bea60858559e9ad3ebcd10a58483d4
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:13:09 +0000

    afternoon beside desperate expect pay

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_acceleration(speed, time):
+        return speed / time
