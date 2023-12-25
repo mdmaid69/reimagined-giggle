a930362e7b30fb266e8c135e2e52cbd1a8a2764d
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:58:38 +0000

    takeback impressed eat downstairs situation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def cube_number(x):
+        return x**3
