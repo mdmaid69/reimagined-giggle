5b01d77782041cd940e7859d07e9187c739b9cf1
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:03:11 +0000

    herself disapprove other section plastic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
