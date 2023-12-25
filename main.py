80a88c79d1655e8274b551b126e5772411b3d385
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:39:04 +0000

    from breakin promise kind minute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
