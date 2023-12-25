1c3667c05422d49b3532f9be86c501d314050896
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:54:59 +0000

    send try expect fresh estimate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+text = "Hello, world!"
+print("Characters:", len(text))
