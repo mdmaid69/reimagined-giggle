be3f13789fc8312cd737ce4360591da3ff528966
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:29:29 +0000

    female connect season never talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
