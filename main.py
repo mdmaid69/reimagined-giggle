c2d631a995f1a1ec360b6ea465071ea536611bd0
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:16:04 +0000

    occur vision praise rain uncertain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
