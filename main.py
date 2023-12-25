c331088bbf76ccfa3834d986d42f3a251ef4284e
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:51:13 +0000

    damage foreign past against different

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
