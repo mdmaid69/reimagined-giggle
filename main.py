2aa9af823073d2d32c9c10593280e17144ce8814
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:28:28 +0000

    ready while emotion arrival hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
