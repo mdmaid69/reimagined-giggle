08dbce0be3682939c6a208f9a3747e2df0f9bcc1
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:49:04 +0000

    yes dear bringdown plenty our

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
