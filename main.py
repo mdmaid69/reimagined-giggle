328ec629e1442fad32f1f9e1326007f98b71cbff
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:15:48 +0000

    weigh crash between nurse expect

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
