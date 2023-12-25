5de556530db05495ec4c6e62c40509dec08860e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:14:59 +0000

    moon keen pipe risk disappear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def get_array_as_float(array):
+        return float(array[0])
