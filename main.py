a933aab03c233fdd27cf4c32c52d50e109a4ca13
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:50:50 +0000

    bringback realize mysterious relative right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
