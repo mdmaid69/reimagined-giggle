ad7765a68417d98beee6dfc5c029969f3c2bd608
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:24:39 +0000

    figure official peaceful top copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
