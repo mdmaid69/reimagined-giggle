ebbc7ed27f2d0d40f8980086b79f1eef32d738e9
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:40:12 +0000

    free bin break murder seem

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
