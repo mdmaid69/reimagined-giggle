1f146380e4ad583202a69298ad34623335ea6765
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:51:27 +0000

    what independent inform have period

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def reverse_list(lst):
-        return lst[::-1]
+def reverse_string(s):
+        return s[::-1]
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
