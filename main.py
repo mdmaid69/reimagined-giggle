d36839492c93fe85e38387f9dd9e07c0b4905ece
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:45:49 +0000

    secret watch Rr section close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
