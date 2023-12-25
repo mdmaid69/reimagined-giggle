6a3eff18eaa290e431e230ba9e393fdca5ff324a
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:58:52 +0000

    related hang offer pattern case

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
