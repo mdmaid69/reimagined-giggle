ed270fe7f0434b6dabae765d231458caa781673f
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:58:23 +0000

    show step future country dear

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
