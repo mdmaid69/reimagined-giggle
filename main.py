c9dfe5883d6f8933a8664743d97a36485a3075b9
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:29:08 +0000

    direct healthy benefit while question

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
 import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
