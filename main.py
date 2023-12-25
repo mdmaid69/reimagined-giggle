b08979776a41849471a2fa66037aaaf0a937f434
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:01:05 +0000

    spite interested control door replace

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def sort_list(lst):
+        return sorted(lst)
