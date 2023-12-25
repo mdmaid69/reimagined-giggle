afd1a421e8b70bf5292242814a89ab374407a46a
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:09:29 +0000

    will spring live detail uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_priority_queue():
+        return collections.deque()
