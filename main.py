ea11cb7b7f8f9c7861663bdbb1d097cc110c8a89
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:01:12 +0000

    zero their best recovery backwards

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+print(datetime.datetime.now())
