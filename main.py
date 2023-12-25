2dadae66e6c60db3e69556bcb2e3da00b4603e00
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:04:31 +0000

    congratulations against government name stop

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import socket
+def add_numbers(a, b):
+        return a + b
+import unittest
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
