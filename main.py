79b32bd8f316b047082ee07092683ad753a1dc37
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:47:09 +0000

    OKorokay exploration very past clean

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-name = "Python"
-print("Hello,", name)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
