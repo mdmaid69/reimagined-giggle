239adce3855d749dbd9e57012d6f8582572df9a4
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:11:48 +0000

    middle excellent smell finish recovery

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-name = "Python"
-print("Hello,", name)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def convert_to_hex(n):
+        return hex(n)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
