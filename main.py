c14c0b5de4657948845d4165a619d099057e8134
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:20:00 +0000

    instrument herself wall admit ordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
