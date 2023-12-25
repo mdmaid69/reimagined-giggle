5ae4c6fa41bf21a404bbb5ea0603a1b2b3305d71
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:42:33 +0000

    brother uncomfortable programme various exactly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
