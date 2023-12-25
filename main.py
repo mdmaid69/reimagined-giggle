2f4aee9d1992db52c73d276cf242feb79b725f8f
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:08:29 +0000

    turnoff hope narrow shrink usually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import time
-print(time.time())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
