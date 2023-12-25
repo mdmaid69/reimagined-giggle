7e78f7b09992915f83fa473bb80baeb0f16e3c02
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:16:29 +0000

    development mine firm sea level

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_cosine(x):
+        return math.cos(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
