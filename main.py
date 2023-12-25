f8821d9199aa48df9a1e88879649cd9bf9711348
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:28:08 +0000

    sea book place else anxious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
