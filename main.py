8281403fe3fb441873382936b13da69fd58182e7
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:05:30 +0000

    such free comeoff positive maximum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def square_number(x):
+        return x**2
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
