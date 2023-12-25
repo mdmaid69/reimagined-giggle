0c98806ece82656cba780d5f8194697b3b75a6e3
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:11:02 +0000

    convenient spray another anyone study

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import getpass
+def get_username():
+        return getpass.getuser()
