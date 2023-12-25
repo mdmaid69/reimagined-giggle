9430b9637937c88f20845cd8dde8f3ddefe08d1f
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:34:24 +0000

    Pp play date uncomfortable figure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def square_number(x):
+        return x**2
