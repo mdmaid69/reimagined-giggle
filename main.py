84322bbd093655fc39627a76da00083a487e93c3
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:13:55 +0000

    confuse attempt maximum reply term

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import sys
+def print_python_version():
+        return sys.version
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
