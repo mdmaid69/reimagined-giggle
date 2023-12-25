338c945d598840ec4e5541492924d52029d02a4b
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:03:53 +0000

    song gap fault sorry culture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_index(array, item):
+        return array.index(item)
