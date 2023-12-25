cf88a3de8b7930d67256f928177fc6d1d43cacd0
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:35:07 +0000

    learn instead hand business path

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def find_max(numbers):
+        return max(numbers)
