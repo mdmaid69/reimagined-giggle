2c2da8c0bd3f2b5ed0567d39d43f9f97302b57f6
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:10:54 +0000

    put method anxious skill accept

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_current_working_directory():
+        return os.getcwd()
