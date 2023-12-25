08f2be7cfb0f3b8402d7ca00e001542404fb1219
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:46:30 +0000

    study spray cool risk old

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
