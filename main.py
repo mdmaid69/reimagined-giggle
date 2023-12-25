4a167640609a03ee65e0aa8f18d6c44435213c90
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:19:23 +0000

    really open runover walk this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
