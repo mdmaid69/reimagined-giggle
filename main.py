46b79dc385d4d372483db61a41f6352163b1113e
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:35:37 +0000

    relax tooth board modern character

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_set(array):
-        return set(array)
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
