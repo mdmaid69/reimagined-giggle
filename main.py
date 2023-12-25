3f2ab657c384fcdc15b0dc8c1ee034e061d612bd
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:29:20 +0000

    feeling people clean shake put

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
