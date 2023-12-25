938011df399e81a5478c398c991350c8d9d26ac6
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:05:03 +0000

    stiff divide area hello sleep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
