b5f193fee620986dba11ed5b0791ff8c93c035e4
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:03:40 +0000

    ago argument early fun mother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_as_str(array):
+        return str(array)
