c0eb3019a9f4eaa10af6e361c8feb87ff654c955
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:48:15 +0000

    family naturally cry very spoil

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
