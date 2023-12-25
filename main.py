cb6837aa56e905389d9fe1bfd6bdd2297951b70e
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:55:02 +0000

    sudden dress gate along hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import time
+print(time.time())
