8f064f1ca31eb469c07cf7fc03a6310edba28f4a
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:50:28 +0000

    but smooth unkind comment shall

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_speed(distance, time):
+        return distance / time
+import getpass
+def get_username():
+        return getpass.getuser()
