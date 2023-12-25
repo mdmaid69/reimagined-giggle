427d6c49cc6da9ae667492f25c3273581a03fac7
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:31:55 +0000

    chat drop not class date

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def split_path(path):
+        return os.path.split(path)
+def count_elements(lst):
+        return len(lst)
