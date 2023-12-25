b851903e4b557ab6ffdd67b4569b7501d58c1753
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:40:23 +0000

    wash thank somehow slow shall

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
