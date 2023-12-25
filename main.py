2a5538b10bddefcd7beab4fb7f2fee441f9d4799
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:53:02 +0000

    do announce fit profit welcome

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
