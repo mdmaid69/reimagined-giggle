4a6094ed56b0316902ee279a95f84c81a13ad73a
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:17:25 +0000

    delicate history opportunity occasion steady

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
