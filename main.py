321c1504e6e576a873f9c0fbd4c31e52823fcebc
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:59:58 +0000

    welcome brush terrible consider bath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import getpass
+def get_username():
+        return getpass.getuser()
