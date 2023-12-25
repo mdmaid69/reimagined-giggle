74ee461a7eacacc956a332000cc54fd4281e7f01
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:57:09 +0000

    satisfactory while today seriously protect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
