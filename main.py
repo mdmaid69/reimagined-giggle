05f455e2f3afcf4937493f88f7667c428f052539
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:16:18 +0000

    cheap pull room care tidy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_acceleration(speed, time):
+        return speed / time
