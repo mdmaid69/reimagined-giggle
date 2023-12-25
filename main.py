e5fffa62b80c267397f62f04500a6ab31517f574
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:54:16 +0000

    beyond shake base live crash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
