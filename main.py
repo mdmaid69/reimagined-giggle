c0177aef171d02e560b34c8b88711ba82815fa0e
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:03:45 +0000

    lookback(on) content Itself disgusting too

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
