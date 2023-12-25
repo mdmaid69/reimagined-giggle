03b36b1062d4a86c6b2f624bc8bccd26566c5c8e
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:11:56 +0000

    ours peace practice goout position

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
