d053cb246bf20e81c23723e7cf6add904f33f37d
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:45:14 +0000

    tradition previous as respect transparent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import os
+def get_current_working_directory():
+        return os.getcwd()
