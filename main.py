e6524d72f40bbf26d9b6854b20d40af99bde9cb5
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:55:12 +0000

    successful occasional able ocean label

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def add_numbers(x, y):
-        return x + y
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import os
+def get_current_working_directory():
+        return os.getcwd()
