87acd85c2719bc70f6100d6a1f7e85689b096169
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:21:19 +0000

    notice bunch shop announce below

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
