28c1b43c10ac4730bdf5f3801b24a26ecc35f97b
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:48:56 +0000

    prefer pity type here godown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def subtract_numbers(x, y):
+        return x - y
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
