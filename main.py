b7a8d755ccb91fbabc658a16a383db6b2804f9c1
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:02:20 +0000

    hers row proof forward fairly

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
