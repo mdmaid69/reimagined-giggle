1b2d3858c12adb0ec38e54ffb27f7791c389caa8
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:13:32 +0000

    book juice print disappointed experiment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+def exit_program():
+        sys.exit()
