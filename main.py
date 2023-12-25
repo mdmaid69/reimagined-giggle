45fd267de25c72ad2c8dc82e1ecd60fafe5da104
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:04:37 +0000

    store seriously opposite modern excuse

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
