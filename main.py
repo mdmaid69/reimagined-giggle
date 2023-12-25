9559d52f95315b49cdc35ad8d943cb9e4d76dc8f
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:32:50 +0000

    rent improvement forever join laugh

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import datetime
+print(datetime.datetime.now())
