5b8555e4916933918bab055b51e7d34121b9ca75
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:23:20 +0000

    seem waste introduction wide 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
