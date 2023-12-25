94a0dbd38f0ef50fe64e12aa4833ceb7993c7c7f
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:27:02 +0000

    summer waste situation down only

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def cube_number(x):
-        return x**3
-for i in range(10): print(i)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
