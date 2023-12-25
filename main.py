fb3ac4162f2f4f70892cf6162b91606f3dd109d1
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:10:47 +0000

    tear beginning total everyone breakinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
