022e15a438e68d8e42c5bc3f499471fef3563be8
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:49:06 +0000

    hot unfortunate oil whenever whole

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import sys
+def print_python_version():
+        print(sys.version)
