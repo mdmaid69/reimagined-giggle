45435d3e82b46eb3375a3d419e0ab40488bc3748
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:07:23 +0000

    match follow negative genuine take

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def is_even(n):
+        return n % 2 == 0
