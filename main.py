f60022e430246c1275d0c3359b1b3e385e0e6b32
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:41:46 +0000

    tongue nonsense embarrassing out survive

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+print(os.getcwd())
