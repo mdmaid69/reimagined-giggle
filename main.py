5e51c017bc30193c1eee2d6933690d93abbfb1b6
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:00:14 +0000

    complete around far inside exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
