be2c545ecb5ae9fe3fbe6384e19464adeeb02daf
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:14:50 +0000

    coffee ever plan face estimate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def add_numbers(x, y):
+        return x + y
