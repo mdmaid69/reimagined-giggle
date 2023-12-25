a9abca08485f5590d1afbcdbb919715d05acb9cc
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:23:03 +0000

    sticky funny arrival block future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
