75d9ead6890b48ca24d59dbd5a665959b864fef1
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:57:24 +0000

    advise possession appeal pleasure trip

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_list_from_array(array):
+        return array.tolist()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
