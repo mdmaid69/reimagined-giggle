2d5524e4c3e1568a616a25e74ad00836f3fdbead
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:05:02 +0000

    tired cause mix ignore very

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
