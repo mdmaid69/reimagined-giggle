25afd4ad719dccf75dc2e2572ed8b68ff57de47a
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:04:50 +0000

    wrap bet same pour march

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
