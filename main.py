6d4d471511e61ee2de5aac4a7368d5e63bad0880
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:10:52 +0000

    light doubt rank should narrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
