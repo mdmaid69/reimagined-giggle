6fff97f941c720393bdaf718983b0c0989bb9daa
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:12:08 +0000

    schedule stretch just equipment worry

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
