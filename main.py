31bc5a2bd6c2ed9df20231ec9e63907150fc0d3b
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:54:05 +0000

    general interested ready rule immediate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def sort_numbers(numbers):
-        return sorted(numbers)
+def is_palindrome(s):
+        return s == s[::-1]
+def find_min(numbers):
+        return min(numbers)
