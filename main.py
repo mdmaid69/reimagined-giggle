b07e8f3a142088b2de1a730b6710e4943161172e
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:37:57 +0000

    occasional step forget after relationship

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
