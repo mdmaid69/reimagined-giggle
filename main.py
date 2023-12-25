406a61808e5f1e4774b7d79865f46f79fbcbbf3a
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:36:31 +0000

    powerful between sail store comeback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
