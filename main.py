df20b23143fe564d4afb7c982a977970f1d6f4b8
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:38:04 +0000

    chin patience mistake tidy nonsense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
