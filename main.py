c4b2bffa6d5748c9810da1748b7cdc764212b002
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:50:39 +0000

    line badly Hh hold rub

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import collections
+def create_user_dict():
+        return collections.UserDict()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
