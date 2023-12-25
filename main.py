7e1c2529d687dd3c08ec1395b163a1026261c7d8
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:51:16 +0000

    hers get just area slice

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
