3681be1bcb09016c92f3578af40e0ef12219f7a6
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:49:49 +0000

    crowd fast touch ordinary stop

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-print(sys.version)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
