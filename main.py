1b99b68574174a5d60d9c21616e23f540ac9584d
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:31:35 +0000

    try giveup bush recover born

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def remove_duplicates(lst):
+        return list(set(lst))
