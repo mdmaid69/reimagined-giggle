7ecd0b2b6e98e9b78cd7f04b4211104f3582941e
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:41:44 +0000

    confused plenty decide keep opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
