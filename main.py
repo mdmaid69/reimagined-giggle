58fe80d55f12b275e4a4f781375e0b7d90c37c28
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:12:19 +0000

    wheel unable description but season

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def append_to_array(array, item):
+        array.append(item)
