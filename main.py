7a44cfe9c0f2d1734230a3a1be49cecc55017c99
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:05:14 +0000

    noisy split curious interest reason

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
