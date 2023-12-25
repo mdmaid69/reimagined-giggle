3170bbc9f353c63c4c4875920a2ab185de907066
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:14:05 +0000

    can anything pride suit involve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def exit_program():
-        sys.exit()
+def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_length(array):
+        return len(array)
