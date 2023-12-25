c7de408b8164b6bbdeca1b3312031863251f9462
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:24:31 +0000

    shall herself join possession strike

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_binary(n):
-        return bin(n)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
