e74e9902ca1db0a0e95f057b95e19bf94c08eeac
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:34:52 +0000

    immediate bear shop front crisis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import datetime
+print(datetime.datetime.now())
