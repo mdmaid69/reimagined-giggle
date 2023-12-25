5a58da7afee307c8378dfecae64e6abf085457af
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:14:18 +0000

    trip hold beautiful count water

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import collections
-def create_user_list():
-        return collections.UserList()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import json
+print(json.dumps({"name": "John", "age": 30}))
