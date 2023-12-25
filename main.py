7df4ce2d7ad292fc38248c183db2b9b1ac2a66fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:56:12 +0000

    pot ashamed fetch tonight Oo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def is_even(n):
+        return n % 2 == 0
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
