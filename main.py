92f1be3fa46fa7ff85ad88e42b92948e20951409
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:21:57 +0000

    common sign memory interview cold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
