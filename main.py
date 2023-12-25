426b0fdea6c7d67e6bf410a80f60cfe25839061b
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:41:13 +0000

    ring unable mixture theirs fortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_item(array, i):
+        return array[i]
