50a821e56d9ddde5f2c092af604cc16480ac67ae
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:49:14 +0000

    that politics without return show

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def get_array_as_str(array):
+        return str(array)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
