6e78d79810ec451658492b49a1bebf7c3bae1237
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:10:44 +0000

    space doubt holdon achieve accept

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import sys
-  def get_python_version():
-        return sys.version
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
