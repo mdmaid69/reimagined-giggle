d05cffde65326c4cac7e544a67f30fd202870300
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:59:08 +0000

    free time listen between motion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def subtract_numbers(x, y):
+        return x - y
