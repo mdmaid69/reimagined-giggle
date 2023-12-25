0e28c1c03769fd609a3a105c6fac3673be4f1761
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:46:35 +0000

    pronunciation again nerve pain female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def convert_to_octal(n):
+        return oct(n)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
