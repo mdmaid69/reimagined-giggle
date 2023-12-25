4bf81d1fb643c5f9ebeccb0658f7071d18633407
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:08:25 +0000

    you important same holiday delight

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
