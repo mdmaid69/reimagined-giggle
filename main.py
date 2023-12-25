e201982c186f03d46f89a74f57622f7b85751094
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:33:47 +0000

    Ss little mean weak expression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
