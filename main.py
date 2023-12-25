0571d95e3999f61c0331ca0ac563370063294eeb
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:02:09 +0000

    less teach pour immediately exciting

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_sine(x):
+        return math.sin(x)
