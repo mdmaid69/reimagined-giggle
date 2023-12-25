a7fbd20dbb18b061f87ebac1e605cd5adf44c778
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:44:18 +0000

    bright sight police disapprove someone

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
