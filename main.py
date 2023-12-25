581c98c435e63852009fac8fee83dca0ecdc6a26
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:17:28 +0000

    slightly event material old value

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
