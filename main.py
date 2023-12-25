122aeff0adda942dce8d1c92e4decc2c21b0d105
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:10:50 +0000

    dare trap similar high extremely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
