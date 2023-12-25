faaa013e154c1bd2854fe2127b8fa8d15c49b4e8
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:47:34 +0000

    still flow themselves shopping lookaround

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
