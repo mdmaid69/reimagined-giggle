19801705cadf5984305dc66a3a211842779c68d6
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:36:39 +0000

    person live offer passage himself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
