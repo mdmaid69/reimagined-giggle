42c8fab92bcd542120519a924a3a43a8e3f59e4e
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:43:57 +0000

    can smart dish contrast moon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import datetime
+def get_today_date():
+        return datetime.date.today()
