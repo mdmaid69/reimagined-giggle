8bd69a2e0121b3c731adc84d2a98518e57c00865
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:25:36 +0000

    town injury describe draw add

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
