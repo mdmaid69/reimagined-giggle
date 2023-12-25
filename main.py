3dad30803bdc969385578062856e6d6b1d9a84b8
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:39:35 +0000

    condition believe suckme type dress

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-def exit_program():
-        sys.exit()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
