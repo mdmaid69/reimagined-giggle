afbd62d1d3c13cf411cc6ff5224758f3123aacc9
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:05:55 +0000

    news pinch ring put silent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
