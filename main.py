b6ecef118e6b0c9705ad1109f02963fd8b99f7b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:10:31 +0000

    circle fall bury runout after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
