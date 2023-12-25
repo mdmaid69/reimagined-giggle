8b7c729d38ebee52b71c77e7ec46115f5572b336
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:10:02 +0000

    into purpose smile frighten reason

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def reverse_array(array):
-        array.reverse()
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
