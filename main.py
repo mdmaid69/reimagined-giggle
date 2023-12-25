e1815c035f26142b0bfff80057e38473becba0b1
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:28:38 +0000

    debt recent takeoff track debt

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_power(work, time):
-        return work / time
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
