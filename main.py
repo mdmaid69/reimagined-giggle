bf1206b04c09df05df044519e136e9e06e5a6759
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:05:29 +0000

    see hang funny success clear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_volume(length, width, height):
-        return length * width * height
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
