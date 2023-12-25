fb9eb67eea6ae0d161f0cbda715990ce4df99f71
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:50:55 +0000

    trade temper crash instead fear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
