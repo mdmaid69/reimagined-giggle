4706f41e7485e6edc3181a27937ad1c337a877e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:47:38 +0000

    anywhere final arrive boil chat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
