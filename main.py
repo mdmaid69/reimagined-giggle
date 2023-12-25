0f4a7f2f0ee8c3eecaea774626db9686ffbbc580
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:15:35 +0000

    thing get freeze past wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def convert_array_to_list(array):
+        return array.tolist()
