bae5c3de84f3bdc0627eb0c4cf0c7ba7edc473ca
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:37:11 +0000

    polite straight increase convenient hospital

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
