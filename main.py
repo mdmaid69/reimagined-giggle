71621ad59c89e10f38d6c1d39b3378ea59b34a06
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:10:51 +0000

    quarter slip spoil memory convince

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
