5975c9d62657c54b22556666daff4604a514a6dd
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:53:45 +0000

    star promise sorry in side

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+def calculate_work(force, distance):
+        return force * distance
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
