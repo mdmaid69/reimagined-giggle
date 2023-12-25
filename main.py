14aa0931a861da3d3e6aaa54d4bcb28efd508404
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:33:24 +0000

    advise run own gooff hold

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
