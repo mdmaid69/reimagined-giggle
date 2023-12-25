12989acc12fcb5e4d7ca773951e7ab6fb4884159
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:57:10 +0000

    every plant soft Gg spread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_area_rectangle(l, w):
-        return l * w
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
