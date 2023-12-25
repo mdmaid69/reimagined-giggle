4824bf51ed8e4a88df8ae97573a22e0041e03671
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:04:39 +0000

    point lunch respect exaggerate meal

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
