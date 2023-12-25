e9dbfcc7ed08f43ceae3cb8d760a46e58e8853b6
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:08:50 +0000

    protect movement activity angry suggest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
