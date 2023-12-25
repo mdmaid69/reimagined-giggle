aa08e5fc1a513c76f01ac34d64e9feb0308fc670
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:13:00 +0000

    squash journey only against century

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def add_numbers(x, y):
+        return x + y
