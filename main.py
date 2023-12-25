a757c68796840dba8154df60e379f20a2da0686e
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:23:16 +0000

    club talk attempt block minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
