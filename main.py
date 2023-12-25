c9fbaecd3b1c15ad26943389eb999b9eda8ed135
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:13:40 +0000

    continue Pp admit complete wave

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
