8b6e87bddb0c6cdebcded7b90c1eaf740f639e0c
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:40:09 +0000

    destroy minority appeal comfortable punishment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
