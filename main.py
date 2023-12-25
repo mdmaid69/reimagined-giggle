ae217cc621efd2476b2743168a81a6ea6e852e96
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:35:22 +0000

    doubt know start sound either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
 import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_itemsize(array):
+        return array.itemsize
