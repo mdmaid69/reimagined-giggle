45bbc16f319acbba026b7debf2a819bafd89e9d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:15:44 +0000

    narrow over collection kill respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
