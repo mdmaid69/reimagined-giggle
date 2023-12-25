2426c94f4e68b0f34dbfaa9eeae565d84e6b0408
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:33:48 +0000

    connected print snow relax cook

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
