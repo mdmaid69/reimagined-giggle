7cd0099446a2a46f20a4c82731cf58c77fc4ff71
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:48:11 +0000

    hang instruction weekend eat comeover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_area(radius):
+        return 3.14 * radius * radius
