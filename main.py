fc85d404110478432b76de787a11a3a6d18a5b44
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:24:03 +0000

    emphasis character tone wait force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
