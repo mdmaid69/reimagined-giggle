40a6533927100ebe284d1cd31fa7977fd9b687a3
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:45:34 +0000

    bad side attitude lose stir

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_area_circle(r):
+        return 3.14 * r**2
