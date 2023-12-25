31a4404950f867592bca6256c6034f8d5f93b354
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:59:19 +0000

    forever content confidence sore shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
