d9e3c30390a817764f897d7d746b7356e48ce974
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:57:27 +0000

    chase drink proper spring sight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
