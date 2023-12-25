5fe08963175fc4e24737adaca9a7eec98b0bb8c9
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:33:44 +0000

    drive machine mess receive broad

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def square_number(x):
-        return x**2
+import tensorflow as tf
+print(tf.__version__)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
