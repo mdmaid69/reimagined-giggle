0600bbd92d757d61a27bdadb1a481148cc9b334a
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:38:51 +0000

    pay bath kick sudden unhappy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
