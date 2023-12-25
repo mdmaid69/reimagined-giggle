146865fed34ae6d45ea644e5ebda0c84b0535895
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:57:22 +0000

    pleased answer tune discussion storm

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+print(math.pi)
