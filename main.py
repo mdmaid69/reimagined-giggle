00fe87d72e09a21f5e1dd0a861be796a7672f9db
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:55:47 +0000

    minority wait office near sea

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
