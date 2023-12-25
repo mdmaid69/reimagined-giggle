9bbc71f308ec6c316379cde2ff5a97a48b16e832
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:48:48 +0000

    hand maybe public meet door

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
