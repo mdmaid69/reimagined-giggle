9a345efd088197969684c8ecf0b0e5a0bbf955d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:07:07 +0000

    Cc broad guess demand model

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
