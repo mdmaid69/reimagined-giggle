58a6bd3cc3e9e564fc7de3569029749004a36ab6
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:47:22 +0000

    row dress bin waste ticket

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
