15a07e41ccbdbe61fcaf0de3326fec1613accf20
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:51:58 +0000

    programme drive spot in press

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
