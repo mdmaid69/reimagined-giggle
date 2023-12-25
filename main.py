02d1cfb2e5a2ca07e1c4f71ea513ee4dd5dfdfcc
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:01:56 +0000

    trap vision narrow throughout evidence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
