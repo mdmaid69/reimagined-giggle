10688451a5508f1685fb024a93fcae886af1025c
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:26:13 +0000

    anywayanyhow 571enough whole anywhere name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
