2d50e0f48b3671d3832e6aedf16fe9db69fc82de
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:58:12 +0000

    completely independent frightened in settle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
