304cf3b9789b1ae620d10294c5141ae9eb4b065b
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:47:09 +0000

    voice responsibility war heat brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
