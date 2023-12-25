c0ab9e24c13fe2c68988824f2436e096730093df
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:52:53 +0000

    claim transparent personal occasional statement

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print("Powers of 2:", [2**x for x in range(n)])
