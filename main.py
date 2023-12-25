535ce02f61876f46c285e8e3c9173ade56ba2cc8
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:16:36 +0000

    object rate vanish him more

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+i = 0
+while i < 5:
+        print(i)
+        i += 1
