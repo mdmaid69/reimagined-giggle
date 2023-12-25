4d1127497ac9cb6488a360be2c78aafc6c0a0153
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:48:48 +0000

    warning by nonsense hurry damp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
