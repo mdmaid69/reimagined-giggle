fb57c48875460c048421a43921e26e74c074f0c3
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:56:05 +0000

    tidy steep connection contrast address

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
