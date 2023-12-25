ce9ead25ccfadce2044a1eaa886adf544d2233a3
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:22:54 +0000

    central spin somehow duty impossible

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
