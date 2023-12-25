95b2470f77c6799c2375171c543970e0f40a292c
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:11:30 +0000

    cruelty angry pollution day tape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
