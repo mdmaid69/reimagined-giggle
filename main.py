8cda3da00c8c2eebf7d7e80d42f35b18ab5daa79
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:07:13 +0000

    future lesson worth try introduce

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def split_path(path):
+        return os.path.split(path)
