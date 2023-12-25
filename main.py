751d8508046701a24fd1c8cbb65092228ba777dd
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:47:01 +0000

    scared run colour valuable matter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def get_array_as_str(array):
-        return str(array)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def find_max(lst):
+        return max(lst)
