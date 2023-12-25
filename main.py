a6c4e00fd86f8390fa04c9ab6b8b135cd287c543
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:10:12 +0000

    track bury pool politics convenient

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def count_words(sentence):
+        return len(sentence.split())
