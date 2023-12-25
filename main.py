e2e375e688af4c7f98453fc33caf74f66fb5f662
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:26:05 +0000

    understand performance quality ring pencil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
