0ea2b45f8f2a7253186a9a8e8b6dc39a42277995
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:14:36 +0000

    happy favour they debt swimming

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
