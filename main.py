7983a4a0bfbcd10e4d9fd63754ceb02cb2dbcd66
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:27:45 +0000

    young this second deep escape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
