f57d4c4f758ff3f83b03b7d657ff3ea169febf3d
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:25:43 +0000

    police back available point upset

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
