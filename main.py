abe113e1fce5fa8c193da9a205157a595e49d432
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:55:56 +0000

    thirsty relief swing harm memory

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
