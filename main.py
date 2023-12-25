b45bd56f9f5eff6ad31c261651bbf17fe42525b6
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:40:55 +0000

    project occasional comeback place arrive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def sort_list(lst):
+        return sorted(lst)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
