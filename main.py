4c5f29b968a4fa8ad1aae63d69922a4959ca7590
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:15:08 +0000

    basically insist plastic wind train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
