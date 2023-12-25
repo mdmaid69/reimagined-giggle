a219d94f6266cb139945f5e34eea1e40aa61dad0
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:43:43 +0000

    suck regard difference strange level

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_acceleration(speed, time):
+        return speed / time
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
