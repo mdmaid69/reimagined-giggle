3aa9c66a2dbcba10f9edd75985ce67d44164ec1b
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:06:05 +0000

    means base emotion whoever another

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
