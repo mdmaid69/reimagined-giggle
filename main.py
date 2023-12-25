5b8ae89a774f72a7a0fd8aecdfb13f4d145529fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:51:31 +0000

    slow cost protection hope section

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
