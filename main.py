d7c646981d8e729cf0a16822200c36037abbe40e
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:03:35 +0000

    pot complicated suck content congratulate

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def cube_number(x):
-        return x**3
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+for i in range(10): print(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
