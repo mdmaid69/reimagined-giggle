2ae1365ecad04a25550b10dcfc7955bb4f6dd1c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:15:07 +0000

    communication any brief likely conclusion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def get_list_from_array(array):
+        return array.tolist()
