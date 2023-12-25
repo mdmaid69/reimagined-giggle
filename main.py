1b88e9281346d1776053e008dd6a848e2047cd19
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:43:13 +0000

    pollution like cry check action

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("Hello, world!")
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
