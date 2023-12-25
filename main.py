c30844bdf21035250fddfb082ce80ce01b959547
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:52:31 +0000

    at note sign cope gather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def get_array_as_int(array):
+        return int(array[0])
