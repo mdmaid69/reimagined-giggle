e3720bb4b9be0e241ede49806d2fa25d271eac01
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:27:58 +0000

    impressed price notice message either

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
