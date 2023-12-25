75e86e6fc97330a0f7a504303f3028aa3e3f317c
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:16:33 +0000

    firm fashion march ring base

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
