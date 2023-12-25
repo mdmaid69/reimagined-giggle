adb2c810279418a7bc67a656169502419d648554
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:12:17 +0000

    accommodation foreign vision single thankyou

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  def convert_to_octal(n):
+        return oct(n)
