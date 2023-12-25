8210ee6e85d795c1256aa28c343b4c51f782efc0
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:51:17 +0000

    reason decide scheme turn understand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
