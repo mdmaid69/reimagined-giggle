88c2d1ce56e2906b361fe041ca1d55e6a6d824b4
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:25:22 +0000

    space Gg body worried mix

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-print(sum(range(10)))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_max(lst):
+        return max(lst)
