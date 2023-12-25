caa3ebe453e97b502f95ba65142968fd2c733ac1
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:18:57 +0000

    minimum bright steal freedom minor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
