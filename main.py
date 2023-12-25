cfe6e4094cd1d3a786f1735c6f71bea7b7a04305
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:26:27 +0000

    guide exhibition behave problem mystery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
