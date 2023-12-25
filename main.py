31c65e38710981f5c1bbdf7bd1df1fb83612aed4
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:39:42 +0000

    lift sail let impossible receive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
