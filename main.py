71007f2ee6e1b135e31e300b7569368751730fc9
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:51:21 +0000

    education hear exam study considering

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+for i in range(5):
+        print(i)
