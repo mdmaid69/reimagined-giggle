2119e58d4b359a43649b7a1fdc720bcfd9f11347
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:27:09 +0000

    rub away Bb any object

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
 import array
-def reverse_array(array):
-        array.reverse()
+def check_if_array_contains_item(array, item):
+        return item in array
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
