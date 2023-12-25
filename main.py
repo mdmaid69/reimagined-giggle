027fd0cc4235633b6c514efe025ad30f83194c49
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:58:40 +0000

    callat mail modern wish spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
