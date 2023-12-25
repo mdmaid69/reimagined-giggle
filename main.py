833aed065425c2806caaa977ad05f8b33f4b64ff
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:30:58 +0000

    history miss communicate hand another

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
