35d4c67c40f839332b32a93403cdaeccc718d26c
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:36:14 +0000

    real free pile unable bye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import collections
+def create_priority_queue():
+        return collections.deque()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
