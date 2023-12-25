9a1fc7e3d80b5674e993d815f9bb8bdc0de71aa3
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:35:42 +0000

    usual ear pretty reach stay

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def calculate_area_rectangle(l, w):
+        return l * w
