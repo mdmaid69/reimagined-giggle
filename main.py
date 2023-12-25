e102f8f20932f010c4264d17ecd40ed03126ff09
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:36:59 +0000

    produce emotion comment someone safe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import collections
-def create_queue():
-        return collections.deque()
+def get_array_as_float(array):
+        return float(array[0])
