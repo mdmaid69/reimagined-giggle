ccc9998bfc9924ea3721d7b81f05be5da36e44b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:00:43 +0000

    boring separate connect Kk half

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
