881ff6920ea67e62667577e5ee880638a76e9239
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:20:56 +0000

    reply little birth quantity turnoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
