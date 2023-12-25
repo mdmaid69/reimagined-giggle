4018d4b02fa0c8e7e739fa46c40eca69bcb2afe0
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:13:11 +0000

    concerned interruption wise concern object

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
