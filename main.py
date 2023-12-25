ceb17724f498cda2e969c6776624068875489a87
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:20:40 +0000

    nowhere sell science question else

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def is_even(n):
+        return n % 2 == 0
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
