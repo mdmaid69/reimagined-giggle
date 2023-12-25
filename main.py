89cb5bc7ee9572a1e2ddf9dfad2bbfebcec2822f
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:23:48 +0000

    unlikely count promise switch communication

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
