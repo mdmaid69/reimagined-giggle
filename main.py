b5d638a455f926ebd3c556b881518c9399119847
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:08:02 +0000

    benefit stop special travel now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def reverse_string(s):
-        return s[::-1]
+def get_array_as_bytearray(array):
+        return bytearray(array)
