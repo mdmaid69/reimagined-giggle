9c85ac67d0cef289f7d1138275cb1ac9e4c94be3
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:21:08 +0000

    tense speed fly increase laugh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
