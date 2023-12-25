828823def29a253e6e297bff37850a150bb9936b
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:06:30 +0000

    aim calm face water certain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+i = 0
+while i < 5:
+        print(i)
+        i += 1
