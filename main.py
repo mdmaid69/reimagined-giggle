a58f030f22122a784490e49022291176ec721744
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:41:32 +0000

    fine practical off organization also

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
