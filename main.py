ec74f6fcff2160608d460903e53870b9f73fefeb
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:40:39 +0000

    tooth real thankyou company uncertain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
