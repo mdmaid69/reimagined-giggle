8151d0796e080224aeec955526097669051ffeb4
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:26:08 +0000

    right different bear large copy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
