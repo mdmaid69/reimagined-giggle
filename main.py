52bd6c244e81983a3eb6ccdf3d8fcabda0e53cba
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:27:10 +0000

    lookafter Ww state suggest why

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_as_set(array):
+        return set(array)
