9ee89adf398a5a4b062015e9d92175465f849ea4
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:56:58 +0000

    bite tip 564encourage operation teach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+name = "Python"
+print("Hello,", name)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import time
-print(time.time())
+def convert_array_to_unicode(array):
+        return array.tounicode()
