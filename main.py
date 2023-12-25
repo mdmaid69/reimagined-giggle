2546c8e86261a25514e20e2c7caf18b8a9dcc2b7
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:52:53 +0000

    be century worry mark Zz

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
