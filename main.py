b3a62005167ef61b33e3a906233fb2bd49437853
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:04:16 +0000

    pretty pair usual advise gas

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def square_number(x):
+        return x**2
