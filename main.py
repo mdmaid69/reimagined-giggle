0dace522da97a87f5e4bd25201ddd5cac4a5506c
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:51:01 +0000

    anything but hungry character turn

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
