27a5ce8b7bf297a3cbb39b1d7c7f0172397922c7
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:48:45 +0000

    copy alone comeround like his

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+name = "Python"
+print("Hello,", name)
