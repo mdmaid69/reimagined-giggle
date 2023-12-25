57d1745cf2f739720ebf362d88a1f9df5a321354
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:59:01 +0000

    we feeling education boring let

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
