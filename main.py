200bb2514aeb59c06e85fbffdec70598f1c5c1f7
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:50:53 +0000

    dance burn 566end equal tune

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
