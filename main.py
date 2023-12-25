f57f9574a29f754a06977fb8f90b8159a1b32a43
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:01:21 +0000

    repair fortunately shout carry refer

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_volume(length, width, height):
+        return length * width * height
