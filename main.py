650867f73dadc64f7e6f501854eb2764febffcfb
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:43:10 +0000

    fresh group turnover man lose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
