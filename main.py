f201bab3f54b3ce7f752c2cf35b9727e6f30a11b
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:54:18 +0000

    breakup hide manage key scene

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def square_number(x):
+        return x**2
+def calculate_force(mass, acceleration):
+        return mass * acceleration
