07955aee86ed5a227e017d455c94864f8d87584d
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:31:11 +0000

    remove custom bet furniture keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def add_numbers(x, y):
-        return x + y
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
