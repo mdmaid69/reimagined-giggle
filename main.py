1107e50843dcf9903a8971f010735ac186ae20fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:52:51 +0000

    possibly pronounce noone talkoutof dry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+print([x**2 for x in range(10)])
