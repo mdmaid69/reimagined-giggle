7efc23bd544a34c8f310bc2513c031ecf9356b60
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:05:56 +0000

    joke direct beside obvious big

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
