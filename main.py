fb020e0077ec9ed0e70b4bf5fdb0d79e1e2783f3
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:37:38 +0000

    bang wave goaway length prize

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Hello, world!")
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
