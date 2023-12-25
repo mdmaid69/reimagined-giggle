f7661dca4658ed5c66f46226cb7486325822d714
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:03:33 +0000

    wish confusing identify us geton

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
