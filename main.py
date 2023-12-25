81109c7d467b84dfed6b66812b1a836447a6ae73
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:36:50 +0000

    needle major slowly beat its

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import time
+def get_current_time():
+        return time.time()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
