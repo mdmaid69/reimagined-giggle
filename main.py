237ed9e23d5ec04d5cc687622e250cea80151676
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:42:46 +0000

    join drop fine cap use

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import array
+def append_to_array(array, item):
+        array.append(item)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
