5d2167954fc5eefbfe514aba89b7e998ae07f427
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:15:56 +0000

    anxious tend congratulate Zz subject

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
