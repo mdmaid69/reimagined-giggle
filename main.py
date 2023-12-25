3e910fef30e4cc39dea2a2865c51150cd1419dd7
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:05:22 +0000

    over write off difficulty this

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
