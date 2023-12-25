ecc8d668c66445abd5d3a73fdcadbb4244925c1a
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:46:54 +0000

    commit both still relief organize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_ino(file_name):
+  def get_file_inode(file_name):
         return os.stat(file_name).st_ino
