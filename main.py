1838879c628db83b71f17a39e8bb4d199b932586
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:34:37 +0000

    through allow suffer plan demand

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def add_numbers(a, b):
-        return a + b
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
