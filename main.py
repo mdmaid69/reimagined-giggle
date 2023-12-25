e238f7da64b132bce1cdd2b4be22005e1f88497f
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:41:43 +0000

    shade instrument talkinto runout bend

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
