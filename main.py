fdb04e0dac3b3a7b7a7c03e562a4bb8b7b3174fe
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:59:57 +0000

    considering expensive transport thing land

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import datetime
+print(datetime.datetime.now())
