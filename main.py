1df74a3b835c9462b6ce7a59e3aa0ab8bfea3311
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:30:22 +0000

    similar sting free lie sensible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
