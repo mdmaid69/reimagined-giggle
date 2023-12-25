e71fe9e82d9ed3b96544472038771addcbc0ca61
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:25:28 +0000

    takeoff strike praise Pp obvious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
