756e6a6bacdf649753162cd2a62c0d874f1bd324
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:47:38 +0000

    sing no send copy declare

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
