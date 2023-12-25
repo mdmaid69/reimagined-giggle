dbeeb6e9b71916ce7f56dd7ad2249c4a0fc4e68f
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:13:45 +0000

    collect lookafter somebody worth stop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
