24483615fdbb782e78c1ebaf1c94f2a758f99186
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:45:29 +0000

    throw bother me about flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
