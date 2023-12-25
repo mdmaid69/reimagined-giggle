fad74bcd5145d3bd96fdd3bf30267729a94ceed7
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:41:41 +0000

    traffic rush dance catch trap

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
