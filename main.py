9cadc68c50d133cf11d546400465eae3215aaf98
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:51:54 +0000

    convince relation confuse forgive offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
