e7783492518eeb2d43d75b003624a2f99a158b4c
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:13:08 +0000

    act Dd borrow treat point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
