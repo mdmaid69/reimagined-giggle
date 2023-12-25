edbd0228727a28e52b013ff30a25bf697643cf5b
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:43:57 +0000

    learn up them so from

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
 import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
