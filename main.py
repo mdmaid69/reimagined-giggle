b21147d8231978fe1da4862af79db8f8ac32005d
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:12:10 +0000

    acceptable children exciting finger guide

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
