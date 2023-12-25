5b0aa407ee182dcd2b6ce053d61e7e7743520011
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:58:53 +0000

    home address speech silent centre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def square_number(x):
+        return x**2
