dc6138975488c49826eb5ebbd4f5d285a692c1aa
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:15:49 +0000

    draw damage possibly birthday mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
