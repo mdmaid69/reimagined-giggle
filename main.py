09a1cf5c7432b396992ac79e4251eaeab8ce5cbb
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:26:46 +0000

    consider match study quit way

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import logging
+def log_message(message):
+        logging.info(message)
