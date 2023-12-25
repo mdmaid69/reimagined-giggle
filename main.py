222dacb288baea143cc7422eca78aa15578e3542
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:29:27 +0000

    different herself neck safety soil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
