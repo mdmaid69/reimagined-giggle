7f6ac6a0a0ef58c366a5c5f154f7c7398b807f57
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:01:43 +0000

    bottom mean turnout border expect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sys
+def exit_program():
+        sys.exit()
+  def remove_duplicates(lst):
+        return list(set(lst))
