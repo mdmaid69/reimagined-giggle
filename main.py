f19738e706b8cc02d17b41a955e8febe363db77e
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:02:15 +0000

    fetch wide back climb wonder

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def remove_duplicates(lst):
+        return list(set(lst))
