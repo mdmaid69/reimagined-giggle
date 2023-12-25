cdfd06b209c71d47207bca4f11640941d1232b6a
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:53:22 +0000

    part arrange broken protect short

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
+for i in range(5):
         print(i)
-        i += 1
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
