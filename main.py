5d396e9e52fea0a8daa9dbd8ba89acf24c9fb2b1
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:12:56 +0000

    rain pour tread heavy themselves

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
