39cdcba75316806914520a5d5572bc8916a90f01
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:28:44 +0000

    spin branch news both extremely

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def count_words(sentence):
-        return len(sentence.split())
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
         plt.show()
