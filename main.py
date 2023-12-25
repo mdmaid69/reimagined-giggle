e1f65673090a92b300b21323a2e37c166271eb27
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:30:23 +0000

    beyond again whole performance broad

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
