45671ee954c89bfa500f1b3bcadf72e0bc162153
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:30:25 +0000

    be scale announce book immediate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
