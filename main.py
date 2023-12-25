9399dd9b054698372043784d844e07518c3a3851
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:48:26 +0000

    himself matter dry arrive sort

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def sort_list(lst):
+        return sorted(lst)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def pop_from_array(array, i=-1):
+        return array.pop(i)
