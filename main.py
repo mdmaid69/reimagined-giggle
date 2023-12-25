68092c4b13d13c6a58ac4527734e3c20c05bc1d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:38:37 +0000

    classroom gentle because command wherever

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-for i in range(10): print(i)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def remove_directory(path):
+        os.rmdir(path)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
