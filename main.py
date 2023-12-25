068ebf47b210eee0f3038184c2b50fcae0f1ab99
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:53:27 +0000

    eat great each trip almost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
