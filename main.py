5b4fb7a9e83764d4cda130d91ad7dacd2a6b5834
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:06:56 +0000

    our experiment impression spot song

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+print(sys.version)
