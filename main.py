52a2275f7e3082e1328f65352aa2bf927ae79048
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:26:45 +0000

    Oo juice suggestion promise head

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
