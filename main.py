d633c7c27af3381c16e9e5d50d5883a372a95dd2
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:50:23 +0000

    appeal funny not goout fire

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
