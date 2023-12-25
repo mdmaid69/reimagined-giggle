b3c23f809f2bb10d08d7fb3cdd92b935668d08e2
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:11:00 +0000

    sit listen upwards Uu correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def remove_duplicates(lst):
+        return list(set(lst))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
