de8c2cae0faef3469588414d8453af85abc10584
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:00:34 +0000

    declaration increase thick wing dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
