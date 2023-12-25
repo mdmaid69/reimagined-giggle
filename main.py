2ca9a2d23635a8d9b6e45abbdfc767c733bcdc77
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:11:54 +0000

    party during wash plan beat

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
