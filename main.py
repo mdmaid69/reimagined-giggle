65009e19a70874811a6eb0333bc4ed62e7b26ead
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:28:14 +0000

    direction lot acceptable world thinkof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_counter():
+        return collections.Counter()
