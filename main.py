71963f0f3bb651006200a98bd8944a1805525875
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:48:34 +0000

    defeat fetch shoulder callat anything

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
