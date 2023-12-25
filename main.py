0dc3b6deca12c6312b6164b515c5ce5d8a397522
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:12:11 +0000

    except point size piece lesson

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-from collections import Counter
-print(Counter("hello world"))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
