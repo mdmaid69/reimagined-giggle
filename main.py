bc7e86b9bcec135259b61ea074c3288d8fe21228
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:15:20 +0000

    interview Ff bother government walk

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
