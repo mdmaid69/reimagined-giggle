f040d244b9a0291e1af39877477190d4bcf633a8
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:36:27 +0000

    firm have gradually keep wave

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def reverse_list(lst):
-        return lst[::-1]
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
