035a9bad4cbc988f199acde14df936ea328452fc
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:12:12 +0000

    cinema perhaps guide out type

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def add_numbers(x, y):
+        return x + y
+import re
+print(re.match("h.*o", "hello world"))
