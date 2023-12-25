6f258014c483fa626f3c11a146f4f403fee97241
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:07:04 +0000

    sell instead fold cry decide

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  def add_numbers(x, y):
-        return x + y
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
