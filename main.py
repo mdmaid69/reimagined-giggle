bef4e436a6dc13771dc237ef07eba741013340e4
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:11:47 +0000

    constantly brave walk 572enter goout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
