3db892865e121dae2498f46f5c09e50ac54974a6
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:49:48 +0000

    admire clothes dull happy loud

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def convert_to_hex(n):
-        return hex(n)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
