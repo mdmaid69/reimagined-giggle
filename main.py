a7958122b7ff5a2a57773d515aedfdfbf181c594
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:50:13 +0000

    able reply smile wash row

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
