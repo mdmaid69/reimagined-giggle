9e08f807e982977de683b5bd2dc0b2024379831d
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:37:53 +0000

    confusing drawing pleasant spread properly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_array_item(array, i):
+        return array[i]
