c6e821a8e3cd81a60b7f367aa3853f07b2307cef
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:53:48 +0000

    press crowded pure claim runaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
