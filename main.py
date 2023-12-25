fbf5fdb71f650b390b894fa23990e22cb1031430
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:46:49 +0000

    surprise police vote do bringback

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
