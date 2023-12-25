4732dbeb9501ca64b7d9570289dd4072bb0b6d7c
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:02:41 +0000

    sting climb shopping sticky gift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def split_string(pattern, string):
+        return re.split(pattern, string)
