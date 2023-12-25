b9e744e77d375925a1e5b99fb7f75cdaea951e48
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:07:25 +0000

    open grow before funny dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_tangent(x):
+        return math.tan(x)
