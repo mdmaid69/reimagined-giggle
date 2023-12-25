1e46c5a3d3da4da4802476301c4b9b4b5bf5f5db
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:57:11 +0000

    trap everything sign tread short

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
