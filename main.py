0a5b042f2448e38e839d707c4b60cfbd2ff91b7d
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:33:53 +0000

    tight stay badly when fall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
