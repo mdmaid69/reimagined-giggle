addc73a2b74ca8ca70bf318b819d8797ee57116a
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:10:24 +0000

    freeze sharp get shallow guard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import logging
-def log_message(message):
-        logging.info(message)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
