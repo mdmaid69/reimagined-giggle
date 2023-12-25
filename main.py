d27fe008d63998cb9f20cb412170118e0dbbadd5
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:01:46 +0000

    Itself under majority organize possibly

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import random
+print(random.randint(0, 100))
