42128f0194c1ee945b4459a451c7eb2240dba528
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:25:08 +0000

    downstairs tongue thick family Rr

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
