27caa57f012171f3fa326ddad4248133862aa61b
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:53:05 +0000

    concerning then gentle introduce lot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import array
+def get_array_typecode(array):
+        return array.typecode
