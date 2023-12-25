9eda26d7a5045f3fe175383056841142c0e975ac
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:48:57 +0000

    between tone programme Qq form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_user_list():
+        return collections.UserList()
