7d55613d94c67dac0fe24e0c24973b5eabe37b3b
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:56:24 +0000

    lose touch borrow order arrange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def convert_to_binary(n):
+        return bin(n)
