15fafdaadf2da3bfe99368d37fc7666d167e503c
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:35:04 +0000

    bite most repeat birth sick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
