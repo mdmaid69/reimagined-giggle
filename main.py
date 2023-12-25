127d0843d75bc4d7de6303fce58c99ff6955abf9
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:45:00 +0000

    giveaway married distant knock experiment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
