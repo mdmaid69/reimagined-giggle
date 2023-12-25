7ca1fea0dc316a128c662da9cd57131885d64967
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:12:01 +0000

    Zz same again possession jump

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
