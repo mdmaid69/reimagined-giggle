b466e3c020ce4b4727e0698bd6906e73765011c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:25:46 +0000

    OKorokay draw long lunch sorry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def is_even(n):
+        return n % 2 == 0
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
