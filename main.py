22051bb5f5dbf626cc1c965daf755bd823254e41
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:14:05 +0000

    better wheel last capital except

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
