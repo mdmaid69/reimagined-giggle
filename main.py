17d3895ad95e32cffc4bd084d61b48a3916e7faf
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:24:51 +0000

    level smell state suck usually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
