2d01689856dc5f306c94e21ba111a7241cac53c6
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:23:38 +0000

    sport band tall attach tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
