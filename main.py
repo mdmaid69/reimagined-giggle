5632936fa74e9d635d7581000f6f242eca57c232
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:26:54 +0000

    limit mind test behind less

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
