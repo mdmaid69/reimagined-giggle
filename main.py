32dbde7c0d9001b4fddb24d8f18cc81e7ce9d396
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:29:46 +0000

    advise rubbish stand climb most

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_as_list(array):
+        return list(array)
