b618070fe9331e477cdb3f8249e713774cdf16f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:23:49 +0000

    uncertain plain able complain environment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
