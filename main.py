fac98f1830554a9fc70f9062568eecdd796707f4
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:38:49 +0000

    central hollow who entry news

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def square_number(x):
+        return x**2
