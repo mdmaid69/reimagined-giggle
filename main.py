c67c49f7212e7e96d65307c687116ed0fa103362
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:50:47 +0000

    swim square immediately target risk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
