159faf94f748b45861892224b4808744301ddbc4
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:41:30 +0000

    ear stay improve conversation everybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
