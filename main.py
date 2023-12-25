da72818f60b5306bed6075beb43500e3bf97e4c7
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:46:49 +0000

    too afterwards prepare lean passenger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
