1d962e13289b96879d9183a4ef13f0d3a38bab4c
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:08:01 +0000

    advice breath roof pull wonder

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+print(os.getcwd())
+import os
+print(os.getcwd())
