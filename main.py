ac6f74a845a368c65f82458596a74170ca9a4cfc
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:03:43 +0000

    circle she base file cap

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_pressure(force, area):
-        return force / area
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
