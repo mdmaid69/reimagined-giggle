aebf590853d0b3652ddc169c0e75f616536ee817
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:40:04 +0000

    recover again thankyou ashamed regret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
