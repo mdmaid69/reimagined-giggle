05cfddac6302e723cea67416606bb2063139fd6b
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:47:58 +0000

    handle sign who second impossible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  def sort_list(lst):
-        return sorted(lst)
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def get_array_as_str(array):
+        return str(array)
