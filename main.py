05a4121921314939611747441a720f79237304b0
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:29:22 +0000

    platform deep freeze disappointed fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
