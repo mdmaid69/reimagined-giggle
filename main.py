a049db36279412a93402c39ab9fce88f9eee009a
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:25:49 +0000

    Jj ordinary agreement breathe possibility

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import sys
-def exit_program():
-        sys.exit()
+import collections
+def create_stack():
+        return collections.deque()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
