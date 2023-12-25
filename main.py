d5e4487b0ccaeaec47b127443052746d2139ed51
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:31:14 +0000

    tight again show satisfactory ashamed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
