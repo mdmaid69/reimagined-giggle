3a656c039c384f8c5159651a61c1d77b0b48028c
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:30:53 +0000

    formal confused visit discuss present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
