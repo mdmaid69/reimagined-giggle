156a2ad2401482f3ae19ddede822f4741b4a99b0
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:12:38 +0000

    trick Uu valuable disagree too

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
