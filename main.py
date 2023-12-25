6d6c25d3a201b7c5cfc8f98626084b57cdfcd5c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:53:19 +0000

    different scrap top shopping bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
