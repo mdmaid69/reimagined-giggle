90d6706b76a9dc3b38595f4a46ce0f7681d9f5e2
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:37:23 +0000

    willing heavy book water willing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
