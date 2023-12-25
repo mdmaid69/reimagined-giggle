2204ebce3bacfdc2f15bb1b521a6b44b92104c8b
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:39:20 +0000

    float bury owe family onto

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_speed(distance, time):
-        return distance / time
+  def is_even(n):
+        return n % 2 == 0
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
