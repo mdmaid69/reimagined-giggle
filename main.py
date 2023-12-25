088398a53deb485e035d85d982b0c557b40a7c56
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:59:17 +0000

    hello ring flight interview now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
