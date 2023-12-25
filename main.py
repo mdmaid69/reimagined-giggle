22ec03040bd4c5d3a670a14eafb26362284c5072
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:45:21 +0000

    discovery recently container post discovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
