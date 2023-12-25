17752c1f464e9cee2bd4f91ad2295ff9af0adaa7
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:01:32 +0000

    mess because thin wing holdon

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-print(sum(range(10)))
+def remove_duplicates(lst):
+        return list(set(lst))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
