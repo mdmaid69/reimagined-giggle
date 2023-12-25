f7721714e8de8eaa7505e6afa5acc6b04cdbe227
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:58:32 +0000

    hungry other plant turnup stop

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def convert_to_hex(n):
+        return hex(n)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
