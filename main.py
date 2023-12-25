7ef3052dd95394ebacd245259a26176e6e4cc98f
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:56:19 +0000

    persuade unpleasant second any forgive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
