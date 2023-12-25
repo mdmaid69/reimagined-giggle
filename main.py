3b47fbeee4761ca19bef52a484101bf4c7bec94f
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:38:19 +0000

    point period thank patience typical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def sort_list(lst):
+        return sorted(lst)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
