fc54eaae43f465e4222ebde956055f6978746b0d
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:30:02 +0000

    trial keepup spoil disgusting chase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
