ea0ec44005162c3e4991a432bcef1a3f79e18909
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:02:32 +0000

    false heavy perform especially still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def is_odd(n):
-        return n % 2 != 0
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import os
+def change_working_directory(path):
+        os.chdir(path)
