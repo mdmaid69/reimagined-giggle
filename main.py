781a0761a4d015905689c1605e50cf82a3580cd7
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:07:07 +0000

    start style change alone imagination

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
