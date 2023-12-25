c67e5da41c8325c6f577b09e386c2062b8557022
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:10:42 +0000

    protest upwards male anywhere water

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import collections
-def create_stack():
-        return collections.deque()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_string_from_array(array):
+        return array.tobytes()
