45d729fd5fce3a1867b89995164ad84587141896
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:13:41 +0000

    forever excuse birthday bottom keepup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
