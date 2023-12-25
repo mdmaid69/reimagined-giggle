cc8810ecbaa4d28b0aacd32c8149fd9f31f75f0b
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:34:06 +0000

    conduct swallow dare claim but

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_work(force, distance):
+        return force * distance
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
