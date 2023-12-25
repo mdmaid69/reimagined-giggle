24d69ae4a8e88201688496757af32a7b8043805d
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:25:28 +0000

    sweet burst bank live head

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
