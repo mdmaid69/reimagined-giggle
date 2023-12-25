e09d88a0b086e8229a63dd7e162049708bdcbfaf
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:10:06 +0000

    imagination pile cost still tear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
