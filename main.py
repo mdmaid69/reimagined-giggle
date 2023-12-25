38dadcf9db24446c548da1392cd8206c6889b61c
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:01:36 +0000

    money stare man original mention

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-for i in range(5):
-        print(i)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
