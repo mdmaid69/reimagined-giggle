d0b0e05e387443ec219dbc3fc6c182da663d81f3
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:52:27 +0000

    hungry criticism bath easy protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
