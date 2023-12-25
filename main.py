12c07328b8eb81160d8ad953bd91fa9b0e44fdc9
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:58:46 +0000

    pour split same than rent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
