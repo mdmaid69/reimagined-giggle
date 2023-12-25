fb050f0b8f346715a210d14dc57664b1e9ab4e99
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:38:15 +0000

    respect middle practical important step

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+for i in range(10): print(i)
