bf633db77539641433323128e2c36b330ef03420
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:51:30 +0000

    carry worry house as wash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
