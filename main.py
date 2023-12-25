ceaf3b42bd357827629e5eb0f0e631f22069ec0d
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:05:56 +0000

    quantity mend rid anybody quick

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
