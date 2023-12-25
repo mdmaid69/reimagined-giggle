7b59086087aa8a2f57005c9ccb782bcaacf06b78
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:38:39 +0000

    hello bath talk claim fail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
