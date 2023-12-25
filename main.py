1af9539d7fac9a95bb1c56aa1c73ee890f3b07fb
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:10:39 +0000

    aim press 572enter speech comeover

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  def convert_to_hex(n):
-        return hex(n)
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
