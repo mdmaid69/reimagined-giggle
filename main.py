4b439eca0019ed901a1a845b76d0058bd22347e9
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:53:15 +0000

    back runout exchange absolutely tight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def convert_to_hex(n):
+        return hex(n)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
