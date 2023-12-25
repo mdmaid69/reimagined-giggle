f98633550d2e686e1d26c711123806baa4fa800f
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:25:08 +0000

    goup separate plain cough warn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def multiply_numbers(x, y):
+        return x * y
