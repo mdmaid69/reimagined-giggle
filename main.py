b49a31a99ad1a2721839d3c4f0e9e98999a01f98
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:27:19 +0000

    single lookafter depend heat error

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
