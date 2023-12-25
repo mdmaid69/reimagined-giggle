82071e917bd392a6b3260df4a170db941697cb82
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:04:43 +0000

    plenty respect though soak transport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
