21d71dbc0bf08fad4734c7aa74a332bf5814fc84
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:13:34 +0000

    rule table attractive wide effort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
