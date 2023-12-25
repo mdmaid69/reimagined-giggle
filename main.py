cdd46571c75c8f64d780b722a90a0276e1e73ed6
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:15:01 +0000

    responsible show Ll actual occur

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
