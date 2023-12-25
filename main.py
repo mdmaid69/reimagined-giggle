8ea3cee523e49cffa658f68b325928ac17f4a752
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:20:14 +0000

    children settle when branch convinced

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
