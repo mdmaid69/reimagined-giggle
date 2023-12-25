44075d53f9f28def61ef8b439666e8aab318d84e
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:53:55 +0000

    lead wash threaten address fear

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
