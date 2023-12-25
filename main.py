a4fabe055b00b958afdabf2c4bd77de6aedcae41
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:51:25 +0000

    drop trap sort news print

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  def convert_to_binary(n):
+        return bin(n)
