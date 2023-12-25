a3e14532d34bccd0b1263137b690a8b3befb41f0
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:15:56 +0000

    ahead positive natural difficulty term

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
