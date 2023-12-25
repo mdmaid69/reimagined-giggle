80a2af6cd8c1ddf370ec8e1d03d32366cf696f78
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:10:42 +0000

    callback feel make price accidentally

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import re
+print(re.match("h.*o", "hello world"))
