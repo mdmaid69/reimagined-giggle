f4e09dde9173e823055053cfc39f5ac402a11be5
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:26:18 +0000

    turninto fall disappear between perfect

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
