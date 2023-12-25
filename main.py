49f44f29394b9b4cd2853d3118fd3f81ca85f04c
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:37:05 +0000

    pressure shrink takeout before mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
