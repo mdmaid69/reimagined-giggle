de5203a93eacf10ff3e961c21693069d62cf87f1
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:10:33 +0000

    shine thick basic whenever centimetre

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def find_max(lst):
+        return max(lst)
+def multiply_numbers(x, y):
+        return x * y
