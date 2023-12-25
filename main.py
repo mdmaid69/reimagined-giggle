dd89e6b3907791068a275bdae00e13b1579def85
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:48:46 +0000

    willing minute allow ours ocean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
