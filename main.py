cb940c65c92b8d13effe85979b735ea744bafcf2
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:54:03 +0000

    generally morning market idea ticket

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
