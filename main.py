7771c4a225056d9ee59514ef06129c11a1d635a3
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:29:25 +0000

    health attach mistake possibility cinema

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
