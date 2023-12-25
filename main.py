3bfb91b135f66dd1a43e4a23de75f4625a52d89f
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:29:17 +0000

    sure train tall clue vision

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
