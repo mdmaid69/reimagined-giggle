ef59d846784c331b677b063b258c92058389acb9
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:22:21 +0000

    file finger meal doubt pass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import sys
+def print_python_version():
+        print(sys.version)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
