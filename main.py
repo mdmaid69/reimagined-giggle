9dc1b695fddbe585ccbccd4649aa9f70d92eb5ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:02:21 +0000

    difference tight appeal motion their

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
