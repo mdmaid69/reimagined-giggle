3f7ba8ce9b0d65cf90c2c76b2fa19d426e430444
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:45:31 +0000

    left mark ago programme glad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
