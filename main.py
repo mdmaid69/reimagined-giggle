cf4540be84c0c85855eaa660cbe2bfb7953edfa7
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:53:38 +0000

    light money boat still arrange

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def find_max(numbers):
+        return max(numbers)
