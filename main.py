5b225faabf02dec02ef9036c02d721c5f76775fc
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:06:18 +0000

    hole stranger amount completely laugh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def get_array_length(array):
+        return len(array)
