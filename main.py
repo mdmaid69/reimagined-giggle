3a78ad657b7d27d9f2fff2267ad810d3f3946e8b
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:02:21 +0000

    onto contrast not deaf lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  def convert_to_binary(n):
+        return bin(n)
