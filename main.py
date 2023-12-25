895d5bd0115e44e6cde10267427f71a6a4c55909
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:58:57 +0000

    comeover spray dance fix attempt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
