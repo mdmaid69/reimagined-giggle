5b7d6ecd5cc896480e2a61b20c81cc4c9af89c12
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:28:03 +0000

    pain course dark polite scheme

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
-import sys
-print(sys.version)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import os
+def change_working_directory(path):
+        os.chdir(path)
