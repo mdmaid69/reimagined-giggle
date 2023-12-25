7e2486abf09200e4d3cc9cb096e563aed86b4bb1
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:17:26 +0000

    gas distance birthday hope cruelty

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+print([x**2 for x in range(10)])
+import tensorflow as tf
+print(tf.__version__)
