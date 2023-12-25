cf7b2df52184a3c39ed80c5a8b15b3bd9f7680f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:30:00 +0000

    either music stop fork turn

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
