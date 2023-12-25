15e449ce415fdf557f5bd1be8eb917e7c0c92879
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:14:30 +0000

    divide exam active permission shall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_density(mass, volume):
+        return mass / volume
