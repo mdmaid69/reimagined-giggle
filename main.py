b12f66b9b367e01053a5c19fe24fe3419dc52307
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:54:54 +0000

    conduct soon ahead benefit upstairs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
