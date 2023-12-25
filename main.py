6d59766288dc9c91751a13d190a4e839f98e99b4
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:29:32 +0000

    cheat expression middle criticize joke

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
