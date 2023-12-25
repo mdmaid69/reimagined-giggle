07014097ef8988a3013dbb938595d0b9156d9d88
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:31:20 +0000

    consult accordingto sore drop degree

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def get_array_slice(array, i, j):
+        return array[i:j]
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
