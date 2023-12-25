47a11483b6fe16904572d58249512f578a416ddd
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:04:28 +0000

    fast shape like next patience

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
