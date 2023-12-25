807dce5fa9f017b6525bdc4bfda47f923191c3b4
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:47:18 +0000

    make anywayanyhow main battery rush

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
