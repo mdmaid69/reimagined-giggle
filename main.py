1d3e7ac969ac5a07f7158bfb056d8cc354267e61
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:57:56 +0000

    centre even talkback minority judge

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+import array
+def get_array_as_int(array):
+        return int(array[0])
