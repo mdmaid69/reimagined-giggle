6ca328786d06f11a9baa14daef1d29bee174231b
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:20:20 +0000

    gather state begin luck lost

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_min(numbers):
-        return min(numbers)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
