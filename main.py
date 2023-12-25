3dc1b281032c543269729fa232d12d18500963c9
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:43:39 +0000

    centimetre illness memory tragedy moon

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import time
-print(time.time())
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
