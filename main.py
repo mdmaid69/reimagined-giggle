b5c3c4a5c9f953674228bf803eeb410effec785c
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:09:54 +0000

    yesterday substance wrap rich breakfast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def clear_array(array):
+        array *= 0
