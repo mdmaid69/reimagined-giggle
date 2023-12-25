595df0ceee73c3d10d05628f64f0a216eb9d8b68
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:43:30 +0000

    sick memory match proper shadow

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def add_numbers(a, b):
+        return a + b
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
