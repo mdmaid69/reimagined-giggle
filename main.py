efed5ca74ed062a930acc6de944dd33fa3acd821
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:31:47 +0000

    mistake copy middle occur some

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
