ed9c579ff996057a2dce22edd52fe182192644fb
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:48:04 +0000

    closed pot through possession bin

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
