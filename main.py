a5c7a1d2583754cc827c402188e646f90497b581
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:30:10 +0000

    turnout early fear hit giveaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
