2822cfc9ff32a0b0d1a5404cead436545191d952
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:43:22 +0000

    solve judge swing tense protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def find_unique_words(sentence):
+        return set(sentence.split())
