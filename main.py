43760fa0801d54eb68f7f8668759bf163738be46
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:32:36 +0000

    system couple out sky really

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
