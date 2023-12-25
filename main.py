0096c1c3c215dd531ab06035f09fe33102ad5211
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:01:38 +0000

    ourselves mother watch city rid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import numpy as np
+print(np.array([1, 2, 3]))
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
