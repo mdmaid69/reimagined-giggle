c7fda1c9dea21daadf3ca17ef3dabd9be2acb0bf
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:55:28 +0000

    kill wish manner aim hurt

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def divide_numbers(x, y):
+        return x / y
