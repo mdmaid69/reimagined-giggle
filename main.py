f6ed67d770ca9e900e7a3bbda10e19716e84e1df
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:40:39 +0000

    trip offence scale turn thin

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+print(json.dumps({"name": "John", "age": 30}))
