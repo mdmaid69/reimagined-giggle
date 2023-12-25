d14faa74f5a1cd4635693c56e44ffd8b8e3eb501
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:38:10 +0000

    future equal education pull relax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import collections
+def create_stack():
+        return collections.deque()
