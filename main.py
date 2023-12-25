9e5ed9190ff593fd93c385d6651768ed84a62322
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:36:20 +0000

    appeal legal control them exam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
