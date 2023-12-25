dfd6bda8f340e856e2fcf4f2537492441a7c29cf
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:13:50 +0000

    rest murder offend intend poor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_work(force, distance):
+        return force * distance
