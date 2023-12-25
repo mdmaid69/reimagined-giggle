8adfbdb1389ccab9922eb1b2afce16e05c0ffb24
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:01:59 +0000

    pin plenty start wide although

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
