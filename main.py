8e7fc8e986e0d45415a40bf58cbba5d8ee2bddb4
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:15:50 +0000

    comfort shut wave getoff class

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
