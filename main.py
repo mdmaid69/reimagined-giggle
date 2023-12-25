35966e4618bd893a5f49a1f180d485165a9e2ed5
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:02:58 +0000

    name dead cost breakinto demand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
