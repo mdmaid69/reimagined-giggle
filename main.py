eabca32f26dcad5a23c139fd0e8d4f496f569f32
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:02:49 +0000

    address tense fashion narrow almost

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  def add_numbers(x, y):
-        return x + y
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  def convert_to_binary(n):
+        return bin(n)
