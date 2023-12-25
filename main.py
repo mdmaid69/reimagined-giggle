be02b58b74f939874c960bf7d51904784227cc93
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:37:21 +0000

    control characteristic house first admit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
