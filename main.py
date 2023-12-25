057f389d19b57d5cd656c7b091ec474696a0e2a8
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:21:25 +0000

    lesson me love goodbye freedom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
