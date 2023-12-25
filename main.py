0e8d62c3a228119e768a0b3aa8ac69ffc7e581a3
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:39:45 +0000

    object work though after butter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
