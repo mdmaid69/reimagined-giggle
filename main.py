02dd56c6b67a0e8621d171aaaa1ab68488b4c91e
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:50:09 +0000

    soil system afraid Ii record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import sys
+def print_python_version():
+        print(sys.version)
