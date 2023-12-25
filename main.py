980d557c01615d2bf63d7651b9e3bfe76b6e1214
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:42:27 +0000

    cry price shallow empty building

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import re
+print(re.match("h.*o", "hello world"))
