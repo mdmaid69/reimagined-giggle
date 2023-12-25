502c3abbf79377a96099dac179c58fb53f769dc8
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:56:39 +0000

    lock below importance dead judge

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
