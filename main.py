7af52df95e7158b453eb0fe530b41d12c309880f
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:06:35 +0000

    comefrom before bored achieve giveout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+text = "Hello, world!"
+print("Words:", len(text.split()))
