4471854ed103e0519a31c3470a8f13a7b230f3b7
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:54:40 +0000

    inquire aware school habit definite

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-for i in range(5):
-        print(i)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
