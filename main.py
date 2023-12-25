fc26c5a59ea43dba7d63e59edcfb0ae34091afd7
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:05:04 +0000

    sort course rest squeeze wear

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
