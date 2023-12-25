c3490eb1d8a1170841e5c200dc778997ead01dbe
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:56:15 +0000

    such style foot live game

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
