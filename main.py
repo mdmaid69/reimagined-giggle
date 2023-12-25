f3580d13e530c2166d38c4502c6f73a5ae130476
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:35:25 +0000

    want in towards order any

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_index(array, item):
-        return array.index(item)
+for i in range(10): print(i)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
