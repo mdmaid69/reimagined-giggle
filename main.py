658a4e76487cd06af770795c39505b608c962a1c
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:40:27 +0000

    fear little to 573entertainment thumb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
