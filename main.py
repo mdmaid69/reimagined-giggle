401ab44724b0f0d1ea3bcad03e02efcbe00ea699
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:42:23 +0000

    steep affect might obtain dream

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
