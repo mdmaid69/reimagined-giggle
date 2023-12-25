578ad426df395e78ba7ade9c173b70f8d7383ea0
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:43:33 +0000

    special track responsibility happen grow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
