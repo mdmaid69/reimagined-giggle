fd44752d931567e87a0dd3a7860e9ec4eb9f4fde
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:48:11 +0000

    branch cope spill correct store

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
