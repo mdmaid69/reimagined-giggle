5a23c20024cfac25cbbd67261496936303134b19
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:26:00 +0000

    table fence enthusiastic popular instant

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_work(force, distance):
-        return force * distance
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
