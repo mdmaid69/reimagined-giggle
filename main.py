9b3b3c9a665104d197e485aed5f1c4c7e65e88a9
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:11:01 +0000

    fall foot principle no forever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import platform
+def get_python_version():
+        return platform.python_version()
+import sys
+def exit_program():
+        sys.exit()
