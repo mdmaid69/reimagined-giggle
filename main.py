895364b62982e2431ad215e2ec2458fd5a7e9594
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:53:15 +0000

    lie complete point uncertain sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
