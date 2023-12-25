592235ce1305877dddeded6af1d307dde1cdd8a9
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:02:16 +0000

    argue search major image possession

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def add_numbers(x, y):
+        return x + y
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
