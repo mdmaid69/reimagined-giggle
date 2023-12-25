2ca95d9d4c530969df6675b5bd6680bdb3630bcc
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:50:56 +0000

    takeout change cold fill quiet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
