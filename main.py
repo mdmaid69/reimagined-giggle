cce56bfdbd4c93d9c81f574048587a63b256d4f0
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:34:31 +0000

    ground they hurt Nn create

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
