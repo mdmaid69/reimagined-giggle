b8d3484c8d42bc2751f00421938c4150fd492246
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:23:49 +0000

    under Pp blame address though

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
