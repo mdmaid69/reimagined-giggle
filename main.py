2a4be707a4190ff3718c697e42be4c7998daceb6
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:10:56 +0000

    play used confused breakinto letter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
