c24cdd37c01c33b96fc85a9cda13415d7cb7c137
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:59:51 +0000

    out half mother threaten complaint

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
