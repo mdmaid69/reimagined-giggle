2ebf072ef89cffb406a071320fabde56bd866910
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:56:20 +0000

    family flat treatment rubbish cruel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
