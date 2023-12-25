0ee51ff311cb6cfe85e3f5af688248d7281074f9
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:21:51 +0000

    public hide more dream breakfast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
