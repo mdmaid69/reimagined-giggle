b6ac6b739f62b2c8a76df11d9d0d9b9d661f6d07
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:53:32 +0000

    official town sudden tense once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
