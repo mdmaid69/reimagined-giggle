515af2fd3ebec08610b151809b69471509e2724e
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:37:31 +0000

    opposite you sentence arm around

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
