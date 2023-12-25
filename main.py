6a51cae549fe7b77609dc9296d8f179267b96ba7
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:07:40 +0000

    approval offer talkinto job part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
