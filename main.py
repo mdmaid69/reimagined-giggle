23e4a6e47f9397849240a67932467852f7827933
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:16:16 +0000

    threaten instead recent everyone same

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def split_path(path):
+        return os.path.split(path)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
