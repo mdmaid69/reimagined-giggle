8ebf1cc1a46f5e5273c3331eef2acedf125a476c
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:18:45 +0000

    total conclusion powerful improve maximum

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def find_max(lst):
+        return max(lst)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
