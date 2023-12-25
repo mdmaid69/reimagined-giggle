224fca32edf93e5d3a8523d6b4afb891e3eab4f6
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:02:38 +0000

    turnback trick aim repair communicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def convert_to_binary(n):
+        return bin(n)
