9f4819ce1691fac023ba8537ff9797dfbfbe1378
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:49:15 +0000

    pollution swallow combine Ww nature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import time
+print(time.time())
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
