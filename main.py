7124b0d1f844adce91d3d0110498b5ac243fa478
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:43:57 +0000

    male Uu mass language underneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
