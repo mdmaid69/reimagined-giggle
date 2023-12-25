4cb95fc56a2e103878558e51f449ae429206bda1
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:12:28 +0000

    nurse blue classroom comfortable mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def convert_to_binary(n):
+        return bin(n)
