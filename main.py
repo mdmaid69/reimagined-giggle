693ceee06502bd3160b5bfb834e234e68ab91f59
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:07:53 +0000

    unkind nowhere giveout as rid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def is_odd(n):
+        return n % 2 != 0
