697bdf09cdb743cc7d2745ba955d700c9f167ba7
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:30:24 +0000

    should you charge friendly rarely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+def convert_to_octal(n):
+        return oct(n)
