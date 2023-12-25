28002d1bcfff8ce3608e6811e2a1889667658001
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:18:31 +0000

    speak giveup Qq pocket callup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def reverse_list(lst):
+        return lst[::-1]
