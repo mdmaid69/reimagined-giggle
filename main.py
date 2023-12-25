20a01f734c300f0ffbe4a013c2859d4bd6b85f52
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:32:11 +0000

    build profit still change scheme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
