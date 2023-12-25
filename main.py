4e51cf1f8ce1b5c408ff1f192c3321b5bae477e5
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:58:59 +0000

    mark join popular fast else

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_bytes_from_array(array):
+        return array.tobytes()
