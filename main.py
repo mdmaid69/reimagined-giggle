4f3330478ed0e980d26c80db1d28aed340415e66
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:15:26 +0000

    sharp equipment history schedule estimate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import numpy as np
-print(np.array([1, 2, 3]))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
