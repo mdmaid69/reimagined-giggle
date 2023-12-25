13dd720ec6d678837e8002d80837366d444eaebc
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:28:37 +0000

    Rr police Ee sweep achieve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
