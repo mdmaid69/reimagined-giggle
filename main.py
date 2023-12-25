0fc4958b3ba651e7ef68b0b4984eb4147566f20f
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:34:41 +0000

    wave future towards direct steal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+print([x**2 for x in range(10)])
