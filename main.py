7fd0eccc6a1d5623590933cef6247282f80bdc5e
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:29:22 +0000

    could around clearly conduct guilty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
