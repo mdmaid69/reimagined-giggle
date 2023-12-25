ef29b6ce9147264a815815750934e707b49a290b
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:59:55 +0000

    hear fasten clean build woman

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
