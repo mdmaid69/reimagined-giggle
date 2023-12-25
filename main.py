13b1592b37c6ee604abba6f1eacf67442918f9d6
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:41:33 +0000

    letter before strike order great

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
