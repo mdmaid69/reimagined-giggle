ba4b1d5c03c577a7cc556928e467368ccc249e35
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:23:36 +0000

    correct sound straight male follow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
