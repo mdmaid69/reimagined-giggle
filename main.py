8ba5ebf6018f44c2161dbd700a77e2c309333f9e
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:09:01 +0000

    issue dear quit down range

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_acceleration(speed, time):
+        return speed / time
