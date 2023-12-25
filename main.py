d39eddebd7d7b10f9da1a33dcaf5ede0ef90669a
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:18:22 +0000

    while over its goon note

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
