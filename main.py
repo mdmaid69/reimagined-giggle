551c22cc2148febb6ed3806b19790eba287e9657
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:06:42 +0000

    exam change question form Kk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
