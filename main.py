a3ed9d347298a175facfc4ed694578181557819c
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:56:45 +0000

    mainly field reject freeze play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
