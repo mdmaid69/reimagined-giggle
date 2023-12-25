a6c45dfa67dec2c65232884733e3b8653767e7bc
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:08:26 +0000

    none line no neither clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def find_min(lst):
+        return min(lst)
