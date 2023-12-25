ae5271c39357379e14aa2edf8c3109f004475460
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:17:00 +0000

    contribute 566end hit will radio

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
