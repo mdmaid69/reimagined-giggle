02851550f2564c966ea4a0fe923ac52061015ac4
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:00:46 +0000

    content lower scrap pupil well

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def count_words(sentence):
+        return len(sentence.split())
