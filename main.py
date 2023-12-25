fe93ac0170b775d559918fc89a5edf5fce94c1db
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:38:20 +0000

    injury tongue swim poor switch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_bool(array):
+        return bool(array)
