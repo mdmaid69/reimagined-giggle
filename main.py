6c4c3d7f29a56e38e73e0935f6f00032c733d92b
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:04:09 +0000

    bright narrow relative under immediate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
