5399a1fc8a254ebaa5e13a1d375e00c65876dd5a
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:53:00 +0000

    operation university would manage breath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
