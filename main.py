52914b908de9a860a7560df9323840ed34fd80dd
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:40:34 +0000

    state play fat woman brother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
