f4f2de36b8b739ea6162bd3dbdcae00d3a9a4ad1
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:35:18 +0000

    goback(to) spring drawing now slice

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+for i in range(10): print(i)
