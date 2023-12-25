9eea41c40cdae1374b3f0ac6bb3f0b64d241dcca
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:45:34 +0000

    minimum thinkover runaway wide benefit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
