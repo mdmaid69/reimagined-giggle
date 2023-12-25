2cc3e0448d4b5bf0072e943243bcea7f203c0f92
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:02:29 +0000

    table still smile takeoff message

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
