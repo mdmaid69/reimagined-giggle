69523758b4b0c70df2a61e25bf41ed99fab226fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:03:11 +0000

    please bright settle road public

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def subtract_numbers(x, y):
+        return x - y
