b6b895ce7a233d2c73aaa232de4c04ee3fa0cf52
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:35:28 +0000

    collect judge takeout disagree swallow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
