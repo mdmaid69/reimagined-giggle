63e0d2aa414667ae729a7cce92a2703fd4e88685
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:29:23 +0000

    exploration remember point command quiet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-print(os.getcwd())
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
