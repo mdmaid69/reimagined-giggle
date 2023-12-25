bb8b938e3e742283d4e5996c5d7b310255f187ee
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:16:21 +0000

    steam level cross interview corner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  def square_number(x):
-        return x**2
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
