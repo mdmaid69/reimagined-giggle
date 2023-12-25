d5e2449b33f7c11aa60416fa7369d835733db9e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:32:22 +0000

    fast speak plan every tone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
