2eaa3a30de2ded5ec59c7489927eafe5a7ed2ae8
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:51:14 +0000

    coffee bank responsibility freeze draw

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
