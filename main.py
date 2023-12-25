b25f3b58b779091d55cff606d690c8078b416bef
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:30:22 +0000

    grand bringdown cope of appeal

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
