6825ecaabfd9d334b59dd7f1e18c603e12da4619
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:58:12 +0000

    care broken underneath strike ability

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
