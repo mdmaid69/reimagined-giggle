b38f4a8c4ae60bf03a8e6f2f93b4817a8c3a2b4a
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:15:41 +0000

    turninto sensible on hour kind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_power(work, time):
-        return work / time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
