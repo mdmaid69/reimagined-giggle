be21946d21914f77fdc1e6dba75545bf86ac3ed3
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:17:36 +0000

    minor pen tune pause progress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import os
+print(os.getcwd())
