b3e7d33d8e08a9e324e2a14c8209eb0f983004f3
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:14:28 +0000

    involve strong improve live marry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
