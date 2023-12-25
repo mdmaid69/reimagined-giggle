1d0d98b462a4cdbdc57fb67e865e5749ebdfbe87
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:33:44 +0000

    possession Vv comment nothing convince

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import time
+def get_current_time():
+        return time.ctime()
