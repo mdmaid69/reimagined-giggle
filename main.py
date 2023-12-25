50a3321ecafbfc4d145955ee1ec2bd462ca69466
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:25:37 +0000

    hard count fork delicate heavy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
