36e061ca3d466833cba99f77e7ec3a682f70de55
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:10:09 +0000

    affect hurt branch difficulty collection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
