1d1afdd472ec9912470dbe3b483b207ce13671a7
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:09:33 +0000

    least square practise cinema takeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import sys
-print(sys.version)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
