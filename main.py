7e6b457dcb177503a1d16735a3bb2563cf3ea8a8
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:27:21 +0000

    bury lonely reply separate petrol

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
