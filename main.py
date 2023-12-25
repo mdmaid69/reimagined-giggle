77c7c263ea02aed6199680da43407b7b681b2ee8
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:02:05 +0000

    front bin language 572enter pipe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-def calculate_density(mass, volume):
-        return mass / volume
+def count_characters(sentence):
+        return len(sentence)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
