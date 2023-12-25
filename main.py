d65aa720530d4b10497606ef0a32dac89d9e4d92
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:40:05 +0000

    pollution precise university typical look

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-name = "Python"
-print("Hello,", name)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
