d50697a449073b0bcfaaebeb7700a0b16d9a0a61
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:24:26 +0000

    attempt boil low carry case

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_average(lst):
+        return sum(lst) / len(lst)
