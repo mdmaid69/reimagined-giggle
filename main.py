f68ed40d739557915ce73f29a06c66467c75f958
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:12:33 +0000

    sing manage run sleep again

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
