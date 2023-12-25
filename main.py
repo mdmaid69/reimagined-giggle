8f9c78031f8c54dad781f59098b34f87ba1330d2
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:59:46 +0000

    charge back small treat lost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
