8b8771d3c0d6304f79b2ba3501ff324ecb876135
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:13:24 +0000

    together smile murder century mix

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
