6b3c9bb6abcbb939cb6ed93c9b06992510902253
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:34:23 +0000

    wrap on yes crash ring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
