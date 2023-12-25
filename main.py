60ae6a925905beaee5d9970b54b87b745f4a811b
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:34:31 +0000

    fit belief treat clean thinkof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
