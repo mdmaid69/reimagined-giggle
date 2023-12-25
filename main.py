ecf77563bc58d44adc1b4e07cf6412b414706c7e
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:29:39 +0000

    off autumn key contain wall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
