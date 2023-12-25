f176f260e3441e72e2965b38e1205c4e1ef5ab60
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:33:39 +0000

    wall deserve authority section fun

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_int(array):
+        return int(array[0])
