91af079c53fc1001abc6ed3f49719706597d25c1
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:50:07 +0000

    since report remember divide check

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_density(mass, volume):
+        return mass / volume
+import re
+print(re.match("h.*o", "hello world"))
