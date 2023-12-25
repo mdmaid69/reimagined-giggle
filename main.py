228806bbecc685b2c96e06680b3ae63c6e771774
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:13:51 +0000

    hot sure forget official regularly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import time
+def get_current_time():
+        return time.ctime()
+import sys
+print(sys.version)
