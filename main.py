ddac18fd57a1d4fbe483680e8085f0154b0d5c98
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:06:27 +0000

    human interview score schedule information

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
