93dec383f1ea59126588102c9235eda067799f96
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:02:33 +0000

    extraordinary crime storm question mass

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
