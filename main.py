0c5ad8cc399727f59710cae3e667cfb2f3a951cd
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:06:45 +0000

    capital imagine kick worse workout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
