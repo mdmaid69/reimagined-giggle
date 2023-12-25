07afb29a565d8de2c13a0d20969a93fe99fd48ef
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:50:41 +0000

    dance honest finger breakup certain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_density(mass, volume):
+        return mass / volume
