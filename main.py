5e1d99d2a359220c9f15963085101c1f633f6a23
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:34:11 +0000

    politics worst cinema sad be

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
