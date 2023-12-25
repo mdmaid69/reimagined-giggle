7819faba01870b93657a1e9ac0f1048245dcbdc7
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:17:40 +0000

    me professional computer couple dear

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
