0a960826a2574a836a68851c5d134a1f36f3be23
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:48:34 +0000

    spin rescue happen comeoff dare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
