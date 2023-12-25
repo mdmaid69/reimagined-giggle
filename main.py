6314778bb70d95a8f13e7c915d9c6e43b725a940
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:30:55 +0000

    hard commit extra blame breakin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_density(mass, volume):
+        return mass / volume
