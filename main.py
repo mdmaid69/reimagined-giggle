f0d588d371df2349ed6bf71baf77a11ffd6205c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:44:18 +0000

    result patience regret heavy nowhere

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def square_number(x):
-        return x**2
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
