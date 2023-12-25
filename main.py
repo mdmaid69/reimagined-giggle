9ded2c086b49e01b2adb462bf7a7c3365f42b3aa
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:13:10 +0000

    comeover climb adjective lean long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
