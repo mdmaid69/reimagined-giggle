04e44dfe2627f4a46336b9f8a5657089dbb130d8
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:21:43 +0000

    stay conversation dry together rank

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_average(lst):
+        return sum(lst) / len(lst)
