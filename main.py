9b76ee58340ff4f653f23015c134acb7d1f0b091
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:24:38 +0000

    crash suppose tidy payment tonight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+for i in range(5):
+        print(i)
+def calculate_area(radius):
+        return 3.14 * radius * radius
