445384014bec383a1afbd18c5ff912632cf97c3c
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:21:56 +0000

    laugh takeaway common increase doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
