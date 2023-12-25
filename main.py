529c2b29bcf141366e9446f053b302bcd4082346
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:45:03 +0000

    spot treatment reserve meaning brother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+print(math.pi)
