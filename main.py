38986bbe9de60c4e80f1e99ec9018eb635a44263
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:58:27 +0000

    story lead state maintain like

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
