3131c3df2b122803574577efa8f67afb158f9130
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:27:33 +0000

    traffic cheap typical exist correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
