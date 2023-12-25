81aa2361a3941ac06b5a3a13b996957e7801135e
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:32:21 +0000

    very ball already hurry entrance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
