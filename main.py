186d15d3d00a2949afe165c0d21e95fa12073c60
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:44:13 +0000

    during creature important affect character

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def calculate_perpetuity(payment, rate):
         return payment / rate
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
