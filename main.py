2899db12a2ff31beef68b8b57e7114b6fa4ffaaa
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:15:51 +0000

    brain push further mix passage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
