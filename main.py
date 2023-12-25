e4aedd378200e761526c2d3a08b33bf3176bd1d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:14:44 +0000

    complete address important police warning

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
