2291787b726a38f89d35d876b2ea4d1bcb0e94f6
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:04:53 +0000

    model makeinto switch responsible motion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
