008296a68b86b877c969518ffe5f4af134863c8d
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:57:23 +0000

    spray treat workupto lookafter entrance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
