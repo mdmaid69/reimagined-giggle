c0ab0f323c552b05ad6c13aff01759b903cd3045
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:09:25 +0000

    flow regular survive snow payment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def clear_array(array):
+        array *= 0
