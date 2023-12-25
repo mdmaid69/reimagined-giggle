d43eb08a34aa81307b37d929e611926452a6c69d
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:51:39 +0000

    fault ours tired learn buy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
