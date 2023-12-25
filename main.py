a0b5840b5c91b08c2e6683fca12ef18444efb501
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:17:17 +0000

    fit immediate thank free head

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
