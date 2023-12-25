ad098a4e8b23b24f0cd89d45a04469bb5ff8c909
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:25:20 +0000

    least score bottom national piece

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
