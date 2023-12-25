cea87207743a1e1158237009738c62a5f6f68494
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:25:58 +0000

    ticket see breakinto breakfast trick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+name = "Python"
+print("Hello,", name)
