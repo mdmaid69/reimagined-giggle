caeadafece07188d94a32445fc051af17ae671f3
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:31:34 +0000

    chat noisy avoid class danger

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_list(array):
+        return list(array)
+  def square_number(x):
+        return x**2
