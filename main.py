c9a37efcd2ce597b8c89c85b80a9a6e4837ce95e
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:08:04 +0000

    before prefer comment find prize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def delete_file(file_name):
+        os.remove(file_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
