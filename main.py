f51874bc6fc40718f84bc1b3153d820463101516
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:44:28 +0000

    567enemy brief needle father but

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import collections
+def create_counter():
+        return collections.Counter()
