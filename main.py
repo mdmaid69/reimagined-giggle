f1010b69930b8a7c58651f495ab34e4f1820cd11
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:03:45 +0000

    cold getup building yours absolutely

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def append_to_array(array, item):
+        array.append(item)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
