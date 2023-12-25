3a6390a1e08299974d88199b591aba85122a5bc8
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:59:52 +0000

    mainly responsible whistle name regard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
 import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def get_array_as_bytes(array):
+        return bytes(array)
