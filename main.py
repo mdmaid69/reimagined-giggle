f347379c49c33df740cf95415ddb3f3aedd9c939
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:38:06 +0000

    class might dry brave definite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def get_array_as_int(array):
+        return int(array[0])
