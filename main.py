0d104f6de316b4e237272aaaa755f9e06fc99121
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:32:54 +0000

    behind comefrom brother keepup shine

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def add_numbers(x, y):
-        return x + y
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
