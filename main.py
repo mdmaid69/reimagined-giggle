ec785ced9d9e54f4fbf81d0e220e2183f705c215
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:58:54 +0000

    mass to tonight flavour finally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def get_array_as_set(array):
+        return set(array)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
