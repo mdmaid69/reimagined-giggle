fb2b60d16b9787fac953e4debeed339bf1322754
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:59:14 +0000

    compete miss Kk mess trust

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
