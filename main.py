45d5d046f4db725475f29b4b2e53cc272f770fd4
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:31:36 +0000

    pay separate threaten worth about

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def sort_list(lst):
-        return sorted(lst)
+import datetime
+print(datetime.datetime.now())
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
