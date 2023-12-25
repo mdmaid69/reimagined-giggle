b9eea18ad0dec5ed97dacc56cfe4e36fc9ad08e5
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:29:17 +0000

    occasional pocket pipe bridge row

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
