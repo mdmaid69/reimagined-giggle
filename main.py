3a9a5e945d14a394f2ba04b1f707b0b83e89359e
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:18:20 +0000

    capital knowledge takeover about false

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def sort_list(lst):
+        return sorted(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
