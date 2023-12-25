65e841a2b3b0d22a29558f455674cb25cd74fc22
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:48:33 +0000

    tie national bottom cry permission

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
