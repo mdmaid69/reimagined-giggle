0a62ab29e3b4eef41a92504bf68f56c0419324c1
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:40:54 +0000

    size shopping force lookup minimum

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def convert_to_octal(n):
+        return oct(n)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
