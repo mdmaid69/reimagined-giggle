82b5a867afdae52c9ccf64d12a0691adbb691ece
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:36:15 +0000

    star instant which basic soon

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
