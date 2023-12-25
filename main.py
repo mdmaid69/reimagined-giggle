8e3b9156a2871d9c244f5a1060829793c515f3fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:54:35 +0000

    forward open pretty earn fasten

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-print(sum(range(10)))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
