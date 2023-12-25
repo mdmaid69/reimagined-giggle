295dae7fefe16f316541ba49d9c63bbe079b7128
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:50:16 +0000

    like trust grateful bring carry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
