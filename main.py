a9bd6623ed121aa983583c4d3f65b02e2da5d20b
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:28:31 +0000

    exactly split now hit preparation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
