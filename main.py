7ed690ebff3ad67568535067ff7951b39b0f395f
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:42:57 +0000

    level to demand classroom gate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
