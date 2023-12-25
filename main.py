bbbb3a10b51d8545e2f652c16bb937edd4892c65
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:18:29 +0000

    return address relation lose clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_power(work, time):
-        return work / time
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
