dc04857f2892ec1454128a9e5656b96c5c02c202
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:35:43 +0000

    getout pattern throw try love

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def find_min(lst):
+        return min(lst)
+  def count_elements(lst):
+        return len(lst)
