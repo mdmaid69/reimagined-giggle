6da9162f71646a48f04b24bbdf9cafd583659aab
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:15:24 +0000

    probably together tool professional patient

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def convert_to_octal(n):
-        return oct(n)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
