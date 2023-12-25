5cbd27883a6de4180a471b30d88243fb0c09e7a1
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:55:34 +0000

    dance declaration least moment behave

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_logarithm(base, x):
+        return math.log(x, base)
