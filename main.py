cb7f9ae79d82682bb24bb78fe6cf943ba5b52d2a
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:15:35 +0000

    arm myself goodbye rich row

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def multiply_numbers(x, y):
+        return x * y
