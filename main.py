476b0d422237a7926a0d36e7a05c822e489a25a7
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:13:39 +0000

    over matter where walk wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
