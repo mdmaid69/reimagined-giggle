f9655263ca372fc50ed826d30f0d85e661b4b196
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:56:34 +0000

    lookafter treat would advanced cheek

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
