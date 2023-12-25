92101f99a5eb1333da411fae5fc71b9ee90a3c0d
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:04:09 +0000

    ignore bringup process bottom light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
