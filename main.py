93063e04735b8653619b96160585f8a4c8058f34
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:46:30 +0000

    machine turn shrink cry ashamed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
