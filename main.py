083d2cc2f6192e0e4a48f11886e8de169c5f5eda
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:23:34 +0000

    angry invite remain post bang

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import time
-def get_current_time():
-        return time.time()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
