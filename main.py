675ef248a3b6401449ff926a8b183a1510507edc
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:41:06 +0000

    match Ll unite stranger geton

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
