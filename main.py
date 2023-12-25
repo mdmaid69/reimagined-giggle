73da1680effe80ec045545b2934fb30833f284d0
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:12:30 +0000

    spoil government content lock available

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
