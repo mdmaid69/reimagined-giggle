76d5b5ddce617599972b10e27a5bbdb521b66000
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:57:16 +0000

    battery thought almost neither holdout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
