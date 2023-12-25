0d81cf59cd9b713d1ccb2afdf020574de1ab0011
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:53:35 +0000

    transport extraordinary tragedy lunch copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
