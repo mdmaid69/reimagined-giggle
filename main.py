87a506a09e6b3f546840ecd9bac6f1dcdcc01067
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:45:39 +0000

    hungry gentle fat truth back

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
