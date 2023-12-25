b6c820b139d7f2b06385d112132e1c1bbce3446f
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:19:02 +0000

    finish over back they scheme

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
