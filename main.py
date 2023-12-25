7ee4d1c0199f278562a48258c00d750be8c8d264
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:25:04 +0000

    supply worry commit up beautiful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
