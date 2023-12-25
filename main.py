961ebbcb00bb090c924fb5d426f0c7894b96c313
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:47:08 +0000

    number fine guilty present through

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import sys
+print(sys.version)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
