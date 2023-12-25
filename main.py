79e10aca1b4da3ae4ef8d11616ea86ce5d1a95b8
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:07:35 +0000

    bright switch shine total estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
