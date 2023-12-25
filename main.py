d03d98165fa439e5d2cf6b4dfabdee501fd5269d
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:37:16 +0000

    spring minority generally tie wrong

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-print([x**2 for x in range(10)])
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
