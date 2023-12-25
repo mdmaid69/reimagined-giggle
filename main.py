4e454b4e4360635f543b22e0bb6d684a67cc283b
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:24:33 +0000

    mix arrive branch imagine cheap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
