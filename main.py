34e6487ff1c24b0afe9a00fc7de9bcb1657eed0f
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:36:31 +0000

    pressure slowly persuade hers bend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_speed(distance, time):
+        return distance / time
