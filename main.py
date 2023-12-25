d23e67371327eb40b1b5df4f8a2132bfda4a51b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:55:48 +0000

    bury film foreign division different

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
