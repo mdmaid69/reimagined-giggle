efdcefb3b6c0c39ae9e5161026b46e104fc49781
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:39:18 +0000

    pupil machine field look variety

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import datetime
+def get_current_date():
+        return datetime.date.today()
