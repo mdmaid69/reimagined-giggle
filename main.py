7797cddaafda25e7352669d33312df778b847898
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:41:02 +0000

    skin somebody pencil bunch separate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def is_even(n):
-        return n % 2 == 0
+import os
+print(os.getcwd())
+import random
+print(random.randint(0, 100))
