6ee3b8656a67ff88033388e5478095b02ecd6406
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:33:06 +0000

    fast breath reject represent herself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def reverse_list(lst):
+        return lst[::-1]
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_item(array, i):
+        return array[i]
