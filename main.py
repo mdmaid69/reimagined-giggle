35324f9dfc6ca17616ab28029b506d658bc425c6
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:26:08 +0000

    circle attention stare pretty peaceful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
