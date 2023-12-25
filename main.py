7105b78fc7a74c62f123faf567dda6d7a547208a
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:03:28 +0000

    train little someone considering greeting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_min(lst):
-        return min(lst)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
