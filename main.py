b98bad6cae8bffac35b3f661c607d2b7dc83d6df
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:11:53 +0000

    any discussion surprising properly feed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
