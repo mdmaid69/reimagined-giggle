f16cea350f7edb9ba8c7e430ae9e73d234be1746
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:32:36 +0000

    lost best line target wrong

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
