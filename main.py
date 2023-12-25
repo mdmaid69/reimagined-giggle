a651b0beac1dc2c2b280d32afa2a956b4e4dd22f
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:12:40 +0000

    people different front reasonable under

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
