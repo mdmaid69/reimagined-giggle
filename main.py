507512bb61385e7442254de54d2f6f4db3b9ca2d
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:32:07 +0000

    forget may undo together quantity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
