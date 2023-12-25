10f2b85fbb376de12e7e939ed7a36adce32747fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:08:39 +0000

    talkback mystery impressed this pronounce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
