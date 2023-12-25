f2b047a12d18be9a8e42f0379f811f3e85a409f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:09:27 +0000

    port meanwhile before trap long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+import array
+def append_to_array(array, item):
+        array.append(item)
