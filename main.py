4889fd1dd1eace1b45519aeddf29ebde41a4d98b
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:57:44 +0000

    subject nurse check much save

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
