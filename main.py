50371256b852f30f11849f8de839b2c6020782d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:36:13 +0000

    offend favour shrink unpleasant trade

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
