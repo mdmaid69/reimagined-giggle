2f28580755d65537e9d7624eb69d8fdb846d1451
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:03:24 +0000

    underneath hole rest tell goup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
