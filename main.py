e54650f128106ff07d5f9c5c493063235b27407f
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:27:56 +0000

    unfair top finger bet slide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_acceleration(speed, time):
+        return speed / time
