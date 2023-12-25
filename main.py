7c0ef71be099e28bc5567c3e5003fc12d59a2485
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:04:43 +0000

    wide film flat everything save

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
