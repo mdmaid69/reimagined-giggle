1e6a3c407ed1a228ab1f47777b52a6f1ba04d5cd
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:25:48 +0000

    first climb cook creature state

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
