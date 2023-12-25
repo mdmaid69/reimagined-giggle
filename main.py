6fdc9e36cc3ddbb594106b1b8b96e38d89ef2d01
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:03:49 +0000

    stand Uu check who uniform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
