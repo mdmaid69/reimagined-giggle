f042e5714322d67e209262ac467b5f4840a1427a
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:15:28 +0000

    mark accident movement spring comfort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
