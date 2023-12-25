45d35bb67b126f5c324194a64d9acc39c544e854
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:05:45 +0000

    fast clean suck that instant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_power(work, time):
-        return work / time
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
