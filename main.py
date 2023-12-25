cd10ec079043138fcf557c1cd090fdd9de19ce5e
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:17:51 +0000

    without compare behave achieve well

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
