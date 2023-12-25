5227ff381ca8191b3ca30599fb367c6e39efe225
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:35:22 +0000

    second unable help store age

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import time
+def get_current_time():
+        return time.time()
+def calculate_pressure(force, area):
+        return force / area
