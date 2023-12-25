1f88c44cb71976d5fa09f643b3fe2ecffbf3888d
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:27:14 +0000

    certain walk that getaway flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
