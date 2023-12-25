d398d7d7c3d84320f038c239c8421598e5ae6793
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:45:38 +0000

    land stir appeal reason probably

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_speed(distance, time):
+        return distance / time
