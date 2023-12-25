f697cc129bd831057fd0bd1c167472ea722c728e
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:11:02 +0000

    education rescue way container worth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
