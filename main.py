5bd2f9dc37e70568b4360df43c8c8b500863f615
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:38:37 +0000

    rain rob near pause whenever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
