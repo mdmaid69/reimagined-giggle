86d4e07041cf319b42729e50ae046f3df892c7af
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:23:53 +0000

    bit fortunately instruction past ground

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
