aad6df562ba200dd82dd545e5cfdda90420df3dd
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:40:43 +0000

    touch unite conduct nerve takeoff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
