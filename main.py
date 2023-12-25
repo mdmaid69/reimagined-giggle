d9049beda9b393efd34dac7c7857f9b3a39f74af
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:31:24 +0000

    mass tread fix stupid though

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
