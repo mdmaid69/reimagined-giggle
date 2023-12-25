20c822500d948cfac08fa8c618f34b11432b1374
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:13:54 +0000

    frightened include clear objection correct

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
