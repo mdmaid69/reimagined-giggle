3d8664e3067941dbfc0246e05d0171afe0202427
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:47:13 +0000

    pay see taste pour future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def convert_to_binary(n):
+        return bin(n)
