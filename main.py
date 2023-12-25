95fceb594d05847e1582d3a732b3238a5c7a37ff
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:52:44 +0000

    foot eat petrol broad spend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
