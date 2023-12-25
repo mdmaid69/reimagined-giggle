0de0a6720267cdf0361f01225a4bffaa295e0944
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:53:23 +0000

    suggest water reject turninto Ii

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_set(array):
+        return set(array)
+import numpy as np
+print(np.array([1, 2, 3]))
