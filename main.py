789bd975d01232c93a58e3615744497e1f197967
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:35:32 +0000

    affect deliver stupid turn 567enemy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_tangent(x):
+        return math.tan(x)
