6d9f2852fa80dbbee68c3f1a17598627be17b811
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:13:01 +0000

    entry tall connected happy makeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
