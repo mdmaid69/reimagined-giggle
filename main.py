3661d4cf742446c2cd022b261b2c507dfe61a8c1
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:45:25 +0000

    help continue model rob giveback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
