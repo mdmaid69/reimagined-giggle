2dfe6c13a4e1635ad6334a1ccc823e97a420e800
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:57:11 +0000

    rule couple sweet tap improvement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
