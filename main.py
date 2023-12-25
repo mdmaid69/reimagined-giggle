2b60e496709e06d814725f88b2add3fd6a69450b
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:00:36 +0000

    exam finally unlucky attention occasional

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  def reverse_list(lst):
+        return lst[::-1]
