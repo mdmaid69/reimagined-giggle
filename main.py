56441b7f1915fc2f156e3563bac58c352da245a1
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:01:45 +0000

    provide boring fond paper whichever

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
