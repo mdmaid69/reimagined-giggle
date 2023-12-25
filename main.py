7a26ea965692f0514240914420da5ad70103721d
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:04:16 +0000

    pain though extraordinary hard considering

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
