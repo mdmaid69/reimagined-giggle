f965cf8018abc60f978eb420ad8616d2d379badb
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:18:57 +0000

    agree 571enough finish usual past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_counter():
+        return collections.Counter()
