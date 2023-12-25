0a443922a200488cfed08aecc2e9d01825ef5467
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:20:21 +0000

    juice middle wipe law present

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def get_array_item(array, i):
-        return array[i]
+def count_words(sentence):
+        return len(sentence.split())
+def sort_list(lst):
+        return sorted(lst)
