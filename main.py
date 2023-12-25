5b501bc84aa0dde57e4aca797d87102f82bd3fbe
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:41:40 +0000

    towards provide stamp team convinced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+import os
+def remove_directory(path):
+        os.rmdir(path)
