2e86eb074cc08572d943db167071faf96ba59fa8
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:46:15 +0000

    foot race guard human exist

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
