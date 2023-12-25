fec5705943c994685fa3af1e366d0051545ab37f
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:03:39 +0000

    start defeat fond take exercise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_density(mass, volume):
+        return mass / volume
