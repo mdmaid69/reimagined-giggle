7755cb3e6338660edca3b9dbb118132db4935e2c
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:56:54 +0000

    minute workout beautiful nowhere clever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
