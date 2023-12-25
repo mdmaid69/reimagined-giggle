a6400f13302671363bae5b557be53b724ac156a1
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:05:55 +0000

    train ground operation poison tune

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import tensorflow as tf
+print(tf.__version__)
