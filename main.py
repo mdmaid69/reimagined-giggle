5f40aa349fa8e71601c58df91ef872895a85e9d7
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:30:46 +0000

    clothes fault race bringdown convinced

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def convert_to_octal(n):
-        return oct(n)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
