260671b026b980ff29f5996885fd429a3b2927db
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:42:23 +0000

    hospital beautiful thought reliable there

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import sys
+def exit_program():
+        sys.exit()
