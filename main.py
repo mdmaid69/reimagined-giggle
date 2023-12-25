dbb3286648e97f0eb6eed1817b636027bb1b22ab
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:36:41 +0000

    half chin still responsibility gap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
