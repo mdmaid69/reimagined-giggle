12def5d279da8ed3f86e008b6126f7c533403152
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:42:04 +0000

    remove related probably patience model

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
