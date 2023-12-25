ba90008f3601b6f739725721fd0b7d60d860402f
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:34:36 +0000

    although poison exit refer hurt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def count_elements(lst):
+        return len(lst)
+  def remove_duplicates(lst):
+        return list(set(lst))
