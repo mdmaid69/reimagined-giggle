115cda73f51493a5c1ba90c9776599b9b7fcf943
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:06:25 +0000

    occasional unfortunate reserve any cope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
