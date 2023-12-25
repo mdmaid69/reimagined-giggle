c4ebfb4d2ab514edce17477666e308deafe0d51a
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:10:58 +0000

    with someone roof unit mistake

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
