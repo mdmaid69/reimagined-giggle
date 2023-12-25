9591dcf059974e7ff770ad45a56e0ae62b3a3e20
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:00:36 +0000

    voice sport only strict rush

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
