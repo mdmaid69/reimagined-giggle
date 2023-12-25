d3cdd2f71770b66e1b73e956e69397f8a98b6f21
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:34:57 +0000

    impatient difference closely march necessary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
