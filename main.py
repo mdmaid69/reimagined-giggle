ea955a47c38a16a7c25499bad99912ed17fc9f9e
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:44:07 +0000

    equal pinch job forever wipe

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
