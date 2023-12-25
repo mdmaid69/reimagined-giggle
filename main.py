c79cddc952dbef79e3cd775fb834e6c1badab0eb
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:37:57 +0000

    yours confuse sleep check fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
