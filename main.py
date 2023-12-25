3909c014c040b0a2eefb2bb9d655f9ccc8e79e49
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:39:25 +0000

    invite deaf cheek takeoff container

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
