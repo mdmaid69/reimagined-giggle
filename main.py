c978926c4c68b7ff31d4b60a6bbca80dbb605a4b
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:53:09 +0000

    spare base still vote shoulder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def is_palindrome(s):
+        return s == s[::-1]
+import time
+def get_current_time():
+        return time.ctime()
