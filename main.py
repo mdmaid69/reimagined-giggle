11c74a1c80a0679787b8497ae7a5b0f70b167fbe
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:33:42 +0000

    company solve surprise in history

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
