dc8affa31ed8c735b47aa5689bb953f21536f0f7
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:26:59 +0000

    suggestion minute large for Rr

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
