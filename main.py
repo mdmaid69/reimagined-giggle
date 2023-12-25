dbcf95e218fc304676c73e341d939e4fb908814c
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:02:57 +0000

    journey crash please body characteristic

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
