9c692be6f8d6df097c5443e018b2b42a55e97bf2
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:02:36 +0000

    afraid place amount history 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
