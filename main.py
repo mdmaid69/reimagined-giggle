b9dd09ca1af536a060da5b2a1a5b781ff82ae5d8
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:37:24 +0000

    later pin promise doctor outside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
