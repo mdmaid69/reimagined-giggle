c94bdc1a9af6fc8a6986e927f3f3576e3f593ccf
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:59:25 +0000

    go thinkof less cover return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def reverse_array(array):
+        array.reverse()
