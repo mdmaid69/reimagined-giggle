dd06e3f45c0b4b7e2c7d4308cdff40481b5bdfb3
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:43:51 +0000

    frightened cheek character emphasis soon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def remove_from_array(array, item):
+        array.remove(item)
