0b5ebbdad93d6f61e37ee7026da9609810ae6a35
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:26:41 +0000

    imagination second born belief trap

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
