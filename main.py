48ca4e99bf5a70a075895ae488285541d850526e
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:48:58 +0000

    important very strike cut calloff

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
