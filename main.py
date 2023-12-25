0b04c6405c9072df8ea81fcd7b13949900c4e48f
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:15:23 +0000

    begin clothes lonely direct guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
