52deacadf0f31e5ba0940cffe46f72168f7b922d
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:46:25 +0000

    of cause bank realize slightly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
