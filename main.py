d08eeab52e03ccdfde0d91e914a449fe90d7d0e9
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:06:16 +0000

    worst official rest convince true

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
