e84357abcfcbaec7d56152e24820ee3a25de6249
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:50:42 +0000

    rise how trick workon unite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_string_from_array(array):
+        return array.tobytes()
