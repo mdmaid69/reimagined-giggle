0e6b70a8bd18f57cc1a21770b8d3fe7193d6fe67
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:43:50 +0000

    then complete improve write remain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
