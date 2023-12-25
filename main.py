48efb7799398ad3930e331453aa90aac3592455b
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:33:21 +0000

    compare pin note disappointed gooff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
