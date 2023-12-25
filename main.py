eacefe83ca2a8401e01e9e029508943492208275
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:06:22 +0000

    whisper instrument affect quarter fit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
