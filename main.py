aed24791314dadce961e0a9d07445ec6d3363e52
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:01:55 +0000

    start wrong collection pleased term

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
