5483e31a29d0f1a2562d3c32d5595b964609ebb8
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:52:06 +0000

    after continue bet nowhere public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
