aaaa3d26be9f17b37d9f580171ab8a422c9e1b09
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:12:59 +0000

    instead to story stop ever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
