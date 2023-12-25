3ad6fd248d9d210307818c09d60cf39765a4f6d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:17:28 +0000

    off silent public talkback type

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
