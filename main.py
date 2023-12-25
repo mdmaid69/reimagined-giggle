e0ac1a21de4631de33a9a93fd7359f7ee7623f8e
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:25:37 +0000

    welcome progress teach holdout space

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
