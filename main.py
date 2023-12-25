9b9f5d4ff6850c54dd9bae77d01942bdd48680f6
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:14:57 +0000

    into under raise unfortunate cheek

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
