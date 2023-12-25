8c4cf13f4d74c1b9ff3e33ff4ac276d251fb1e7f
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:18:30 +0000

    winter bring far base maybe

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
