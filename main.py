ecbbdce728bfed788d27250fe18295a61d96bec2
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:52:16 +0000

    treat problem woman accidental knock

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def set_array_item(array, i, item):
+        array[i] = item
