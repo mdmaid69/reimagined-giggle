e6d78deaa9c7635f935b2d5a37b75fb8521e2b1a
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:18:30 +0000

    free anywayanyhow little language emphasize

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
