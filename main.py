af39ada38bf3c62c837cb8ccabda43ab3c40a34d
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:29:58 +0000

    properly her both music free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def add_numbers(a, b):
+        return a + b
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
