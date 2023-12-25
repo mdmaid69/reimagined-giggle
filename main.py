d8b19e988b1e455d6cc68b746adcc778eca30329
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:21:35 +0000

    absolutely both by poison tool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
