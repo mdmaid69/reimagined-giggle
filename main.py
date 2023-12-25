0d7bb3d0d502bb1a3475460696338414fe11fdec
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:37:38 +0000

    lower beginning movement receive exhibition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
