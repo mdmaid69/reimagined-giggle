ab61a28d2d1fa74a8cac39811063fc1bd9c919fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:45:29 +0000

    instant direction grateful majority nigga

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
