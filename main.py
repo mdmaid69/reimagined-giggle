8e5cfa2f164623469d2fd83a4bdbe4cd319d4cb4
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:35:14 +0000

    allow earn 572enter wind room

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def find_max(numbers):
+        return max(numbers)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
