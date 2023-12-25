95fdba86fd43e76f25137f27c00ee3db109973bf
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:16:05 +0000

    communication foreign getoff should goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  def convert_to_hex(n):
-        return hex(n)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
