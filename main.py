8d8b42b2e564b70ed86c68e884c62e7a32a7edb7
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:52:31 +0000

    row tooth spoil behind meaning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  def multiply_numbers(x, y):
-        return x * y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
