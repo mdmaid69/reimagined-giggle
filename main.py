6755f3506cd16d3f807799d5fd60568fb63479b4
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:00:42 +0000

    destroy traffic not hear quarter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
