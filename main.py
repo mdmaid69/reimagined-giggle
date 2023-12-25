11b8cd93a90ca91715c22998490d521f0e8a41ea
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:24:09 +0000

    consist breakin television bright rush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+for i in range(5):
+        print(i)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
