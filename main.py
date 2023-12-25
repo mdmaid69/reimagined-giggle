7214c4ce50ee02fe51e35412459ad3b1a931cbec
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:06:33 +0000

    activity build before comfortable fence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  def convert_to_binary(n):
-        return bin(n)
+import os
+print(os.getcwd())
+import platform
+def get_python_version():
+        return platform.python_version()
