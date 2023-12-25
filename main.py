de4e5b25dad440c2c413daf9a4fd194413b12575
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:58:13 +0000

    relief journey injury pin he

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
