492f04903d8f28f913ef157a5184dc8622b3ef8a
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:15:15 +0000

    trust recovery fashion be meeting

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_square_root(x):
+        return math.sqrt(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
