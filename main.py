cf3c723cde0cd7461eaec48b5de53a5fc71a2315
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:23:57 +0000

    rarely speech party protect brush

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_floor(x):
+        return math.floor(x)
