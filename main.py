b5c76a953ff3bedd5ae6362097c9e485e251de8c
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:48:57 +0000

    perfect holiday raw pretend effort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_str(array):
-        return str(array)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
