f20ce75e677245529a2987dc0028101ab7519e18
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:19:24 +0000

    bury draw slice lower attitude

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def is_even(n):
+        return n % 2 == 0
