a6ba8bbc97eaf15c8b7843155505bc6d3ea57ecc
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:13:26 +0000

    gradually measure present rare post

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
