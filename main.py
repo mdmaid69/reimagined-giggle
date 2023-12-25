92d7c0bb660481d448ffd50062fbd507836d740f
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:11:59 +0000

    breakup discuss keepoff abroad obtain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import sys
+print(sys.version)
+def convert_to_octal(n):
+        return oct(n)
