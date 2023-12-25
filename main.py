109df23b3fbc0f4c2f6a76f4bf9f895b03290c93
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:04:12 +0000

    home murder upstairs respect cost

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import os
+def remove_directory(path):
+        os.rmdir(path)
