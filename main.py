46098fd5ebe6f168bff3c8ff10f2ac13686c5acc
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:47:32 +0000

    into thirsty type visit level

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
