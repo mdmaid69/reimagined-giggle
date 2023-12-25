58e0f83123f008f00533311ca0f598eb0aaa0516
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:22:30 +0000

    partner soft instant along instant

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
