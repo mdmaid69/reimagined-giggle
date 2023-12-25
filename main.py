4e33a01212e1162ef054e58153c8b112f21e3b51
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:41:29 +0000

    improvement matter cross workon talk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_string_from_array(array):
+        return array.tobytes()
