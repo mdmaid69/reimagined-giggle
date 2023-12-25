46ab685b7299d489468a3725725ad94fc46ad79c
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:44:55 +0000

    mainly bottom convenient weather acceptable

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_factorial(n):
+        return math.factorial(n)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
