f1575ce1c3b41a8308fdf39c7cbecb7a18359865
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:53:38 +0000

    invite show need frame light

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_int(array):
+        return int(array[0])
