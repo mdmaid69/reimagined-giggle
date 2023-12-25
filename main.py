8a5771baa2002a1cc5200eda6190f40c5db50f9b
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:13:03 +0000

    about fear early suckme method

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def reverse_list(lst):
+        return lst[::-1]
+import array
+def convert_array_to_list(array):
+        return array.tolist()
