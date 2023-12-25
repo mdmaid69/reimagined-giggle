4819954a68ab114e5c9f51cfa0dda8754e790b93
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:51:42 +0000

    workupto true hurry always 566end

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
