19fd9a3a690016ddc77ee3e17fa70ef8824bd3e9
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:31:53 +0000

    corner weigh room soft gather

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
