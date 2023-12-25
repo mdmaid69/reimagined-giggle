59ca348d81f95a1ca684abf5b6f7d70fb163d1ca
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:55:37 +0000

    fond law somehow story considering

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_speed(distance, time):
-        return distance / time
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
