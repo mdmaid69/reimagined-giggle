37510eb4fe4ac7f9a359a9ed471f25c1e937b272
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:44:45 +0000

    congratulate trust shadow glass force

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
