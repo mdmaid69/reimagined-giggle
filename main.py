f1b8e31e9445f654b3b460e222be0f357bb01ac8
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:20:01 +0000

    ourselves snow while attention comment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def cube_number(x):
+        return x**3
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
