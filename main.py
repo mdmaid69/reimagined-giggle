5d43b2b22475a71f8b1a9f59f7071a491f4b8376
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:05:10 +0000

    mention few trade involve teach

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
 import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
