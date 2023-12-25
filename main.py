085875c983698f01fdbd29a5fe6c305d680b7922
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:45:06 +0000

    no pencil history rest answer

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def get_array_as_bool(array):
+        return bool(array)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
