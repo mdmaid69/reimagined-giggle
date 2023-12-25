74199a54a8a8492e4202844e882b55dc3e2a403a
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:30:24 +0000

    brief music home clean cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
