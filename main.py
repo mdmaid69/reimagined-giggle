c7f4abca8609b521c6db86b8f1846c0b056cbca7
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:15:08 +0000

    trip out matter telephone follow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
