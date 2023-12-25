34dfb473c6ff81067c6cd0a9126c04d1a5d07889
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:23:06 +0000

    excited wide just after beyond

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def find_max(numbers):
-        return max(numbers)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
