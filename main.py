0dd1f340bf7b816add863965942c0a6739c45f6b
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:11:00 +0000

    copy sure continue 569enjoy strict

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
