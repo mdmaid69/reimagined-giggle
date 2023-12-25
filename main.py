65b6602a71fdcf01360fc0b5706656480e0ccbc4
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:53:37 +0000

    measure protest weather experience emphasize

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_exponential(x):
+        return math.exp(x)
