ed2f5b6a08913496eb0f4789ed7e8c7a484fec29
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:22:47 +0000

    suck wire law last become

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def cube_number(x):
+        return x**3
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
