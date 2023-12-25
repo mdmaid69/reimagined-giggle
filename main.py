6247c6ce05a36f41971c2e7d2d07026c58ed082e
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:54:25 +0000

    rush fond subject quality blue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
