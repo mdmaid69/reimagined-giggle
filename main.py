fbf953de7fe114b8de91f7f42bdd927c5323709f
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:23:46 +0000

    confused alone upwards mark far

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def count_words(sentence):
-        return len(sentence.split())
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
