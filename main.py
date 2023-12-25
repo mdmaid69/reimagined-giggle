0ae9a5b717b007b66857a538a9560fbbf0aff9a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:17:23 +0000

    pencil connected set possible safety

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
