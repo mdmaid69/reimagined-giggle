c5130ad04b2e0c8d32d9b46c0fbe2eb8946e6ca9
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:15:37 +0000

    himself curious neck off sticky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
