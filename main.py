442bb8d33d315783f8827ffb757d8585d2c0f41e
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:57:20 +0000

    settle dish 179bite its my

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
