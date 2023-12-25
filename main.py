007c8b2bf258122d7a5c5ed801fa63aad2f2942c
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:04:15 +0000

    club argue work successful design

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-name = "Python"
-print("Hello,", name)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
