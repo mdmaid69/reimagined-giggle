0c7c0fba73fd5ef755dfcee8a910d367e4da4b27
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:47:50 +0000

    takeoff explanation but couple phone

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
