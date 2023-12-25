65a8fd3cebea74d3754837e3add2eb4fa266cd32
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:50:47 +0000

    pool choice unless wrong area

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
