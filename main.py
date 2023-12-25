0a4e253fb07ccb751d34703c7a352fbdafc717b6
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:22:42 +0000

    sure giveback goback(to) pleasant back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
