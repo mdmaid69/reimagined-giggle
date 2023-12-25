8b54a652fee1158d1e22cbd0d0c06af7cc4fbc6b
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:54:12 +0000

    model straight draw comeup plenty

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import random
+print(random.randint(0, 100))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
