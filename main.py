6d5d5b8470bfe966643dc8c0ed865d8b27ffde56
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:40:28 +0000

    listenfor number something various chin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
