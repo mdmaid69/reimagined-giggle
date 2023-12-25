5e03aad1641e7ceca381f936822304779856d69e
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:42:32 +0000

    happen suppose herself one downstairs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import time
+print(time.time())
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
