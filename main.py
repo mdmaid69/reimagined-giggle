6fc8db20b55dcd68b63b805464e42691a4699c43
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:11:30 +0000

    suit soil favour task yesterday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def reverse_string(s):
+        return s[::-1]
