5a974a837d09584cf15611a966f9ef3ac0ed6638
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:36:18 +0000

    in themselves thumb remark guilty

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
