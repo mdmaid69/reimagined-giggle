db9a944a75ea151383f48ee8b4cf52c92c1eccd7
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:21:02 +0000

    past near letter couple holdout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
