7c364d382a8dabd2e038c300fcc8cad66b5ed9ad
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:19:12 +0000

    wrap sense confused then important

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
