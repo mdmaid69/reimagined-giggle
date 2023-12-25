7e07f6b5c11f704939a851b5cc6a969d3d40f0f9
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:31:34 +0000

    limit cost glad swim march

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def sort_list(lst):
+        return sorted(lst)
