4390f6204cc3d33c1674b3cc20a9a1d078857bfd
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:06:22 +0000

    fall organize flow workout Kk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_power(work, time):
+        return work / time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
