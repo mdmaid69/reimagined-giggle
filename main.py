7d7ec0f9f23c3ffcb975f482cda986bc5359af6e
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:03:16 +0000

    careful regard love hardly curious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def remove_duplicates(lst):
+        return list(set(lst))
