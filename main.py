c949072516071a451b37fe0973efbd82fa6c9c4f
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:25:52 +0000

    discovery edge notice government have

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def square_number(x):
+        return x**2
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
