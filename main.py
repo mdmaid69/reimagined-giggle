1ce85ed57b0e1b14fd1fd7804c3afbce4eae7b7f
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:11:55 +0000

    save find club unit order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
