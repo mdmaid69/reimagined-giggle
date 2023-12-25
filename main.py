641cc14cca475fc474110b7c0e3725c7608d8880
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:35:37 +0000

    achievement movement extra just stiff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
