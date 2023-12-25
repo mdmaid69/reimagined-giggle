5deb9f3753c13053bf2ba27be9ad628057a6c428
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:06:28 +0000

    kind herself mess independent grade

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
