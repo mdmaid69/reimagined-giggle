8f68f5ce015b4d3f6c47e0fe1fe9891ddc809549
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:55:37 +0000

    unable careless fair national flight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
