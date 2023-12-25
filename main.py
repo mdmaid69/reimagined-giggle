a0798f7a3d1b6fdaebfffc1b0f6923e491ee68bb
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:16:10 +0000

    sort stop know design report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
