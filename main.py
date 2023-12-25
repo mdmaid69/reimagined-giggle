25d7905f120725bc11a90252d990eee1bc8194da
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:05:26 +0000

    ask type Ll near loud

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
