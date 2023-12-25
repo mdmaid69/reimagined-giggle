9a0d09d75cca692f69d74bfb942dce735c912b7b
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:59:32 +0000

    clearly exact official something unit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
