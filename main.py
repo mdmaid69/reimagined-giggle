b1dcbe3233b4c09dc1e586bd1a37671bac2f161c
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:34:15 +0000

    embarrassing basis band anywayanyhow service

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
