a64174536d1e1f41d1d4c9cf13e2aeef0f90739e
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:28:35 +0000

    consult Ss type lift smile

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_power(work, time):
+        return work / time
