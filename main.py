961ddb0affb2979e53efbb40ba922f6dadf43cdf
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:31:55 +0000

    protest concern frequent treat injury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
