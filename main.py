28a204bbaf48de97b91c8416d7e93674db627867
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:54:29 +0000

    this Ii spread danger relative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
