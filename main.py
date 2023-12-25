d050c67f8c6a3a068ce540c2f16e2ec1ff3f7f22
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:32:31 +0000

    instant car rarely Kk progress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
