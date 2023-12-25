404810ba185017fac4486aa998119fd5599832d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:25:53 +0000

    best roof straight little modern

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
 numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+print("Squared:", [n**2 for n in numbers])
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
