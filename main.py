fe6f0aa0d1c9a37e3c7ee3d3c90106ab953998df
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:31:26 +0000

    miss attach chase sleep train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
