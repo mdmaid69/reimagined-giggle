15331a0edacdc6692e2e6bcef4c41c4085e28f11
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:41:04 +0000

    stay price circumstance group friend

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
