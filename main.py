01e60982d7612f818f23239ee702627cbd59aaef
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:30:45 +0000

    behave hello herself form breakinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
