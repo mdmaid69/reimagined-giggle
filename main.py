f34f0b3040036dd5528878598e69cf60f76045c8
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:00:12 +0000

    eager hope holdout stupid hot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
