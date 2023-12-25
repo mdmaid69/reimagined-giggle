0b8647b240e8aed7f32879d5079ed7e5d98757f1
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:34:58 +0000

    fat sure claim temper well

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def append_to_array(array, item):
+        array.append(item)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
