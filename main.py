6686911adc92b540d98404d0225db6ef74d938be
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:36:35 +0000

    passage past skirt herself turnback

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
