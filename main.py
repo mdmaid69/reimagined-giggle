67e619f9eb2cf7933e5e69accc7da82baff952f1
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:30:46 +0000

    heavy boat worried admit maintain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
