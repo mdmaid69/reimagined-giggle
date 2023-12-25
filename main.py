44cafe536c2878b1cfbd7681a4e60ae96c7008e9
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:05:44 +0000

    tight object can modern hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
 import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def print_python_version():
+        print(sys.version)
