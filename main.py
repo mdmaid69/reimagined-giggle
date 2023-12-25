3542d7c7a33e060e9d3aefaf278baebfb27ba5ff
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:02:50 +0000

    mean quit Ii early creature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
