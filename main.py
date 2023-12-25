7dcf217f152042f019cf42b72d2a48c35804b675
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:23:09 +0000

    ear black disturb worry mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
