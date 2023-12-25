0c7ac574dd195382beac429212077fa1874c9c2a
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:23:02 +0000

    wonder hurry go workon 565end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  def sort_list(lst):
+        return sorted(lst)
