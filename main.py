98c9fd6297bc2b0a41b33d43758400782d33afb4
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:25:06 +0000

    someone question 569enjoy hope key

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def sort_list(lst):
+        return sorted(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
