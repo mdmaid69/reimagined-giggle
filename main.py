3f7675aa0b3fb0ff8451c088395ee3f40e2fe7b4
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:07:11 +0000

    weekend cheat work port teach

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
