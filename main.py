255007c9a1277f687b55c66117e6b7f162b9985c
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:41:26 +0000

    impossible neck somewhere behind disagree

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
