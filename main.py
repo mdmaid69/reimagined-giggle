f1ee008261513a141994d266d8e19d443945b378
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:29:34 +0000

    scheme pinch confuse repeat worth

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
