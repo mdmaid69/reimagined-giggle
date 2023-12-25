fb5e9f8d927e3e6c6ad97849335c7a272a2987df
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:44:59 +0000

    wall fix likely within otherwise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
