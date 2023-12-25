a7b6e99cdd2b50aa85c4703d31c6062468caf1a8
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:09:56 +0000

    book wealth drink thing community

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
