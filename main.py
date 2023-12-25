5f457444825fdae704938df15e2f1180317d830a
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:27:08 +0000

    trade Ss unusual information forever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
