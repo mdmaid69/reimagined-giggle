cb5129bd496f62dfac37085130c0066829276394
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:30:00 +0000

    butter less spend moon light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
 import math
 def calculate_greatest_common_divisor(a, b):
         return math.gcd(a, b)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
