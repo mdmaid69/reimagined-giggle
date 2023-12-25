5c1008a12ee761eaa0a9c9e2bb876293da0a693d
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:13:19 +0000

    hour comeup eat mean anybody

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def remove_duplicates(lst):
+        return list(set(lst))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
