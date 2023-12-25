50b392df7a33dc14a48c1bf5a5ac3c73e2b04391
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:49:28 +0000

    early drop near wild figure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
