838d1f74e17968fb7b1e3386abd4f61f0b509e27
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:24:23 +0000

    whom grade yours condition guilty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
