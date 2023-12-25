c04400c9c39a10d563f201d2af24380834412ed5
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:35:23 +0000

    normal Mm both success still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def find_union(list1, list2):
+        return set(list1) | set(list2)
