30668545e8918e20238840c3967ac884e392f739
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:05:27 +0000

    spring spite record beginning wave

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
