6226b4053f3920b6fb0ded659ea8926144deefbf
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:47:15 +0000

    along awake floor bye brain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
