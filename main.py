61c9c01526a1fa30d56ec1d628058da302dd4081
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:37:51 +0000

    wide thanks mean tool different

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
