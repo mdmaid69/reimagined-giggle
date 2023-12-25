365b5a50626edd88c84cd20afca251147b6ee06c
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:14:07 +0000

    confident state body mine hot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-def calculate_pressure(force, area):
-        return force / area
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
