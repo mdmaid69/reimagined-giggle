0f1a1384a2f0e96df8abacac47167254aef9242d
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:14:55 +0000

    actual deal Nn necessary quickly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
