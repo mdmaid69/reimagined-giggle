3d8a92df65bb54e57bf3f1806c4d08c2e8894f58
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:26:56 +0000

    welcome doubt powerful spite stamp

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
