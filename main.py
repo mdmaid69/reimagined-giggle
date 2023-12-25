b4453ac9b67cd9ad405f8d59bbe825fc2d980963
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:00:42 +0000

    priority duty get mystery where

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
