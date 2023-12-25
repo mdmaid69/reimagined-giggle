83f8e19d35e03aaa007f70b637a9d296d351bc8c
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:40:49 +0000

    so terrible heart in city

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
