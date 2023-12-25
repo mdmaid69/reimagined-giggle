18e6945fc55b4fdb1149771f3395584952ce7d86
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:36:50 +0000

    tool common tread bath away

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def convert_to_binary(n):
+        return bin(n)
