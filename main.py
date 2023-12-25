84a5e2c0aa61d7ce7e11c511cfca4320e5cb9eaa
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:46:54 +0000

    summer long which snow excellent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
