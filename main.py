ad224523b9cacd7c4bc465a42f7698d981612f04
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:44:46 +0000

    beginning dick soil your terrible

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
