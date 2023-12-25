80101a8b188ee2b388208160217f5ca64e969c80
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:17:20 +0000

    active bath laugh face plain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def is_even(n):
-        return n % 2 == 0
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def get_array_as_bool(array):
+        return bool(array)
