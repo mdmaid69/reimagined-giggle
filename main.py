bb68c43b34b23906a6e6c44499150701f51b7177
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:01:04 +0000

    minimum male leave among squeeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
