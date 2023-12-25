e622322ab803dbc04dd0ab9442d4d0cbe62ccc9d
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:40:24 +0000

    discovery game freeze open repair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
