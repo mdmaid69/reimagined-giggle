fc9690734a305d75bedd5b8e6d803bbd26ecd972
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:39:56 +0000

    independent scheme male plain material

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
