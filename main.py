a52043a66e1d26057814e61aac8be10784b5d213
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:38:38 +0000

    year mend drive lock wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import collections
-def create_user_list():
-        return collections.UserList()
+def create_queue():
+        return collections.deque()
