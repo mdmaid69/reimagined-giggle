718ac9756048e13ff97baa39037f1f856436f493
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:54:28 +0000

    cope meeting apply giveback children

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_pressure(force, area):
-        return force / area
+  def multiply_numbers(x, y):
+        return x * y
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
