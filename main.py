623073e51ec0a866de02cb9e4fd3262895ef1fa1
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:52:51 +0000

    cheat nearly challenge port positive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
