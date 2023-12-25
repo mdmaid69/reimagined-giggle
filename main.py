eba934eba0fef2f6abcfe1cfc95742d09f6b7f52
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:02:57 +0000

    supply unlucky fit debt plan

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def square_number(x):
-        return x**2
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+from collections import Counter
+print(Counter("hello world"))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
