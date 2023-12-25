9b98f09fb2fb38dc09d0aa5bf1c9b18086cc4b10
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:46:47 +0000

    gentle pronunciation pleasant beside ought

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def subtract_numbers(x, y):
+        return x - y
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
