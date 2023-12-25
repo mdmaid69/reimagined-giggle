2f94900780ccc8b10dfe11c96cd353a05d491bde
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:20:58 +0000

    division charge unlucky his head

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def sort_numbers(numbers):
+        return sorted(numbers)
