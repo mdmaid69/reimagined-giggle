c6e9a8560c651fa1adb680e9c88a1995db9efea4
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:41:49 +0000

    discovery food invitation coat swing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
