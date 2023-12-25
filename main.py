05d890ef6bfaffe29630985e5428cab2bf2730f1
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:31:28 +0000

    571enough tell handle praise terrible

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+print([x**2 for x in range(10)])
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
