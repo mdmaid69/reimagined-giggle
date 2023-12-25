b5ab1b5ae91468f333ec251096f94327f1356a96
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:56:36 +0000

    vanish possibility ear aim reject

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def reverse_array(array):
+        array.reverse()
