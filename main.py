aa4f8755b2be3f53a8388c913336ae75852a27f0
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:59:04 +0000

    save slice way wherever fat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
