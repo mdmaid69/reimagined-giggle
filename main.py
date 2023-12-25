702fd8864a1e268425ee9f51b41594ee4a2b09d8
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:30:44 +0000

    development wind good platform bury

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
