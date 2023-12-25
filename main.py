366cfc962a2689427db6dd83c29ddec515861a3a
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:04:35 +0000

    not summer lookaround use draw

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
