fa42d1c996679acf23ee360f3897dfe911473773
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:45:51 +0000

    rob estimate language plenty need

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
