b3b8033dc200648a656003dd8a5e41cd2d34d405
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:06:00 +0000

    throughout wear schedule cruel offence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
