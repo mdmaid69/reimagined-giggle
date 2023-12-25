c6051f767ca0751babcaa42bb8c39cc3f47ba7cd
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:35:26 +0000

    relationship project close load odd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
