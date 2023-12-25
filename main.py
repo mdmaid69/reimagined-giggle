0e2d40cb0b60842707a81ec1383709929438c80c
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:33:07 +0000

    thank risk workupto Hh board

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
