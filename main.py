1baf1164897aca3a9f52ff0dc1da974bb1c1225f
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:08:35 +0000

    defeat sentence consult somehow middle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
