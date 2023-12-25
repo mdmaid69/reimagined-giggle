4051415fe4d9c8f8ea59c956a063f9fc3f470b7b
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:28:49 +0000

    sentence mix butter cheek instrument

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def remove_duplicates(lst):
-        return list(set(lst))
+name = "Python"
+print("Hello,", name)
+import datetime
+def get_current_date():
+        return datetime.date.today()
