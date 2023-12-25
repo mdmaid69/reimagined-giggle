ff33b3e1bfe731ba3959524ab0c2d3613fd4f160
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:41:21 +0000

    people deep emphasize complaint door

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+print(time.time())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
