ee3cd7758d0a23980b6554500c66e7c6e367213d
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:15:23 +0000

    swing hope interview able shape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import collections
+def create_counter():
+        return collections.Counter()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
