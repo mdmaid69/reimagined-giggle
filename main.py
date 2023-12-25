366745df12042a3b20798c996329f8f0f00bb9a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:40:57 +0000

    task fat blame well factory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
