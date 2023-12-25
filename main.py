3ecd4b02d78bebd6f545d32a3e30ede86a5d8b31
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:05:19 +0000

    hot march thin pool suddenly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
