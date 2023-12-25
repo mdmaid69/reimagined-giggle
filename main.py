b6919995e2951a15bc99fb5b8b562b28497b9f20
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:58:57 +0000

    getout Jj lean wire spread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
