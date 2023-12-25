fd13630732e5a7f58c6fd82d9b92e3e981c6508c
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:58:53 +0000

    expect possibly translate condition strong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_work(force, distance):
-        return force * distance
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
