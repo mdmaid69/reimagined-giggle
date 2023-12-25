dcdc9db4bfa104e627d7ff6c69b87785f186ed5d
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:44:11 +0000

    middle practice remove past change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
