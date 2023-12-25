44bacdf68a98718dcb6dd81be563fa74f50daa62
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:58:56 +0000

    identify already interview night just

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import collections
-def create_user_list():
-        return collections.UserList()
+import random
+print(random.randint(0, 100))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
