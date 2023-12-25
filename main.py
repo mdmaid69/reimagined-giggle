26428bfd43eee58fb096a380a88f5ec5a34db853
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:22:19 +0000

    survive tongue rule copy ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
