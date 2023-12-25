7ce2159513b32a6a0244613fcb16af5e63be2b3f
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:04:32 +0000

    careful bread swallow space strike

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
