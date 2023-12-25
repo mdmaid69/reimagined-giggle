255807daab320662a2e4d7fd6d80fffac35531fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:12:32 +0000

    clever board left breakdown original

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-print(json.dumps({"name": "John", "age": 30}))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def clear_array(array):
+        array *= 0
