231237a4ac20b2c659a9eefb8a3b0e8dd8a811e8
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:46:09 +0000

    exit partner nose emphasize clean

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def convert_array_to_string(array):
+        return array.tostring()
