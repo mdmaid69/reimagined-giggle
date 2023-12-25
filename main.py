8fc868b409db84a654fe0bf131ff20882e4791c8
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:48:05 +0000

    shout into thick because in

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+import random
+print(random.randint(0, 100))
 import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def get_current_time():
+        return time.time()
