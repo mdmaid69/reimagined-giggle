5f2f01d777b8a1cc491e1c74c8518ef96f775b21
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:05:55 +0000

    car nonsense cough majority spill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
