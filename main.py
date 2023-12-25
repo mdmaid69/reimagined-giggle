fcb7fc121b16a05cb4108423d3e3d9c41ec2cc27
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:33:51 +0000

    nurse cool makeup all majority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
