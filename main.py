0fb1230cc73e18cfd75904878185799d3a314e0d
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:51:21 +0000

    flat nose demand tip develop

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-for i in range(10): print(i)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
