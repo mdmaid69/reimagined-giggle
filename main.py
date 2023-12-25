a9386fdc8c917cb7f060d010f913c5ceacc83786
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:42:15 +0000

    alive uponon turnout love dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_item(array, i):
+        return array[i]
