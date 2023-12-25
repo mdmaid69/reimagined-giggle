659b97e4d895a65bad6aa6596f88f5820bd5f594
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:50:15 +0000

    572enter tap concerned special walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
