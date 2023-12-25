a5923b85895df208dff274670cb4fae592667dc1
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:10:22 +0000

    settle explanation call smooth level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def remove_duplicates(lst):
+        return list(set(lst))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
