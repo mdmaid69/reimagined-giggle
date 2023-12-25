b2b191e98fbc164f0216d1922857850137f84132
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:33:42 +0000

    reply prevent crowded attach official

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
