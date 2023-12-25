86fa7b0e10427722475648730eed7a47841841eb
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:37:02 +0000

    somehow dance authority none that

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
