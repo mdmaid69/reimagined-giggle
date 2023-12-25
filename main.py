6cd5b0e881882a63299cca14f795242941089fac
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:15:58 +0000

    kill manner part convincing picture

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
