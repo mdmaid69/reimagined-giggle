597a89df54cde5c90867216705a9a2832ff5a538
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:06:24 +0000

    doubt reject hang change calloff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
