bb0433038ac5ce288357aeec631e6f720e27be4d
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:05:53 +0000

    century shall keepup achieve funny

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
