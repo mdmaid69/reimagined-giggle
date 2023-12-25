731745798594328810c4522c6c01d7af32c7d8d5
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:14:55 +0000

    flavour takeback ticket book seriously

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
