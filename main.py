5e3ce665103b2fc2830237d639703fa1a38d0382
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:15:50 +0000

    try cold hot goon time

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
