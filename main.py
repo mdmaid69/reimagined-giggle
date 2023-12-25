1d681d9241c854aa41196ff0665fe4e0a9dc0eac
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:26:50 +0000

    begin physical getoff straight dig

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_density(mass, volume):
-        return mass / volume
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import collections
+def create_counter():
+        return collections.Counter()
