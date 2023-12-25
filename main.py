9951e91b0e891b27ef109f7518e7ca3eede367ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:41:10 +0000

    lie crowded material float sting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
