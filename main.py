eb6909b8434f2f16a60f2f2beac057c4a928adc1
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:05:06 +0000

    judge fast centre programme behaviour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_set(array):
+        return set(array)
