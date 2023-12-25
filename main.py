05401655655198b8d324f7818394041d9c58ba12
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:15:29 +0000

    instant want confidence owe old

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
