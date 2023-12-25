10189c42e4e7a34ee57fb355fb64e1e8ccc15c47
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:35:10 +0000

    fairly mess mean decide forward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_work(force, distance):
+        return force * distance
