493dcdb65b9188d7cf14ba898c78c56f055f7e9b
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:18:57 +0000

    trust compete contrast impressive family

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def add_numbers(x, y):
-        return x + y
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_power(work, time):
+        return work / time
