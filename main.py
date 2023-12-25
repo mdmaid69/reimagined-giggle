157e5ba2396a76139bbe797c111d6913009eb6e9
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:03:12 +0000

    stranger advise independent expect frighten

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+for i in range(10): print(i)
