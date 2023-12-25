0b349f6069329628123562c88ea47c97f83b4d68
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:09:25 +0000

    skill unfair software transparent will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
