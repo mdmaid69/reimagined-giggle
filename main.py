ec3e43163cc3ff1e56c7dee1e5880a63bd781cf2
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:56:53 +0000

    challenge impressive spread confidence minority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
