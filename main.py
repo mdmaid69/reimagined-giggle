ba9e7ccf44b440e6d9b0a404aed1c33fc22bc85c
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:11:41 +0000

    shoot slice allow choice accordingto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 def calculate_amortization(principal, rate, time):
         return (principal * rate) / (1 - (1 + rate)**-time)
-  def square_number(x):
-        return x**2
