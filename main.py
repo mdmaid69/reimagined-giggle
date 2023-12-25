fb8b92b4de20834a0acf443e2313c7efefb81e54
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:56:17 +0000

    shop listenfor fix occur handle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
