c1c0f22b79bea6ea287d23b1aa8d4f268c8b1d59
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:34:59 +0000

    young poison talkinto near call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
