3d5a131d48c4734f1f7cc0fb3f6d309f3b0f682e
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:50:43 +0000

    train disapprove travel general compete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
