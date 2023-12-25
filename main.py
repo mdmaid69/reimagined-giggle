8659fe5261fe78ebf96b57c52252f3e22782f9e6
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:56:04 +0000

    kick immediately school love cheek

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
