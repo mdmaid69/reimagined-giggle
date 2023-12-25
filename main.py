035f67340d3932eb819d194fc9d74a6a6e70aa7d
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:31:59 +0000

    noise winter spare contact possibility

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-for i in range(5):
-        print(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
