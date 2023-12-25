4625a9d0886215116d0b13cf2e10ea79d9a7c513
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:54:57 +0000

    limit beat excited beside artificial

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
