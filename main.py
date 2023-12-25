4e9a3ebf9c700bfed2cfec936e0be04beaf9978c
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:32:56 +0000

    spring single support female communicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import logging
-def log_message(message):
-        logging.info(message)
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
