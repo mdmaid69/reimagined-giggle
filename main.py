2c0b0f4efceb0c969b729209b20a3e41b0b12b09
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:46:23 +0000

    noise zero slide pollution translate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
