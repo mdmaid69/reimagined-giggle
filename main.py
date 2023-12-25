4942177694b6495c3e4ef56ece4c78ce25d3be9e
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:50:25 +0000

    protection 565end male receive rich

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+print([x**2 for x in range(10)])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
