63a90a257fbad7ba91509680cad94c820bea293e
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:30:55 +0000

    slowly behaviour put thinkthrough help

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
