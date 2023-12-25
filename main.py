7f1b68b24ee5e71c6c6f6b64977c55aa82673107
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:00:00 +0000

    uncertain thank standard brown drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
