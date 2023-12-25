eeff5c1d10c77359d68334b81a1ff0a24341d95a
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:03:01 +0000

    her agreement new past slightly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
