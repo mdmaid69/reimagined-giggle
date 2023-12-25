218fc274ceae2adc705fdb9dfbb37b66f41459fb
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:18:25 +0000

    cum lock interesting pass rub

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def sort_list(lst):
+        return sorted(lst)
