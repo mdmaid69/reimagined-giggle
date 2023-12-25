ac346f131454ddd61d708fcb4a5b8f0399f4e9bb
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:17:58 +0000

    mine label gentle upwards whom

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  def count_elements(lst):
-        return len(lst)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+for i in range(10): print(i)
