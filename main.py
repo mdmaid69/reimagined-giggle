7f1721192cbcd869b831df006cf276ca92a93bb2
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:21:42 +0000

    city innocent afraid star house

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-print([x**2 for x in range(10)])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
