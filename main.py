a9d79d5896302da1c6b2af084c05af7a62724b8d
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:07:33 +0000

    they invite finish dinner once

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+def print_python_version():
+        print(sys.version)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
