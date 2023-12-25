d86a94f5ccd9ac255d38f7113140a571636d7145
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:26:52 +0000

    wall extremely case reply regard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-  def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
