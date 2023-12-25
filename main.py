c5af7bdd2c3409a4f6f64e95741f647cd26b187b
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:09:15 +0000

    suck theirs borrow high rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
