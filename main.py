bbbd0c5e81cbbf538e92402b40a4703bd6d95046
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:16:24 +0000

    kind decision ilovedicks worth minute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
