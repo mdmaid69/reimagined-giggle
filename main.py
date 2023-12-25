6d8d606c2593277b8aad0d0b45301c6316e517cf
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:18:40 +0000

    pick almost in post myself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
