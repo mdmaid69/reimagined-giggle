b065e7d616e516ff296a369c62cfb5ccda064cf2
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:41:29 +0000

    sing collection accidental office bin

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import re
-print(re.match("h.*o", "hello world"))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
