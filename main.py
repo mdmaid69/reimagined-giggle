79110409d78a2dc8af35909a32275e52f2bf0350
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:48:01 +0000

    have pay even foreign convinced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_speed(distance, time):
+        return distance / time
