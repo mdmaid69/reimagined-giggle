ce0fb88ba39c25ed24d6d98b06b988ef495d4487
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:18:44 +0000

    keepoff takeout thanks market uniform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import datetime
-print(datetime.datetime.now())
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def reverse_string(s):
+        return s[::-1]
