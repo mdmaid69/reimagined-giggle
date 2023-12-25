bf16b01a3e115adfe974c8cb97533d4664fcc57d
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:46:46 +0000

    use friendly approval unusual tidy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+print("Squared:", [n**2 for n in numbers])
