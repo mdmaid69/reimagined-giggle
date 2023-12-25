ea13fd3f0a3902d6a52aa170943e2cc5ee796ac6
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:37:46 +0000

    565end 573entertainment there much air

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
