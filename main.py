4995b35d8c88a3eeadae49ed37ba987689e2ad61
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:02:01 +0000

    afterwards dick anywhere defeat explanation

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def divide_numbers(x, y):
+        return x / y
