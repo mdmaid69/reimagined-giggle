52003e6b068df6ed453421d0f4923a5f91ec62ae
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:00:24 +0000

    bite equal include dress Itself

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def change_working_directory(path):
-        os.chdir(path)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
