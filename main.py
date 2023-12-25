4cada2a389fca0d4f6e9956f02f2f98526d17b3e
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:05:53 +0000

    geton shame obtain excited ticket

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def reverse_string(s):
+        return s[::-1]
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
