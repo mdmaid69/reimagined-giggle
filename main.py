3af8233e6ba2eed7bddc8c87d9c95a278d0a9bde
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:38:02 +0000

    onto already announce church age

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
