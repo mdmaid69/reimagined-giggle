e1a08bc9ae448c20cca3476e290a7a5f398360b5
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:39:30 +0000

    freedom shade scrape settle blow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
