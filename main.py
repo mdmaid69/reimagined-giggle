cf8d7c102a307389fb3438ddc103f927883380e2
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:49:13 +0000

    far joke measure nearly reaction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  def calculate_area_rectangle(l, w):
+        return l * w
