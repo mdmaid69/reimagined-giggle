bcdbb10c0719ef778bc54871e44617a855f6ed56
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:57:53 +0000

    watch owe still hand shrink

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
