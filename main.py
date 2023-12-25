5aa7d107609ba86716c93a1c9190fdc3c5caf713
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:57:55 +0000

    limit tight breakin make calloff

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
