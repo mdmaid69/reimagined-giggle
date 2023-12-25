81d069d90179bcb4e6519f2caa782a856c05cd1e
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:20:27 +0000

    little change stick communicate weigh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_factorial(n):
+        return math.factorial(n)
