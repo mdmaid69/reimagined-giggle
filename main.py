c678735597d0461978fd2f691df96e62f6ae647b
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:26:07 +0000

    little century slow tonight confuse

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+i = 0
+while i < 5:
+        print(i)
+        i += 1
