607cee27de4fa1fad011a63147f64656c64d3736
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:25:45 +0000

    stranger apparently possibly increase above

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def count_elements(lst):
+        return len(lst)
