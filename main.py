135fa04bd9e0ee6d21b2c1135b5c770acef6c067
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:06:03 +0000

    bunch listen round switch joke

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
