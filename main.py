0df02546d31bb528eb6999aeb38ee1be2e77ac97
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:34:46 +0000

    life wave bad gift furniture

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+print([x**2 for x in range(10)])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
