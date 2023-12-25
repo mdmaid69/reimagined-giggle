37fc1d574e774bff2a953ca040b879f123551949
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:13:46 +0000

    lift better step hang mine

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
