c9d032c189f17c5b7fdab338ed938f713994cbe1
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:27:16 +0000

    large each raw before inquire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
