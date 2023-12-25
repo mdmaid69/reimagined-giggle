c5d730432bdaa66a2d0a1e6e335c1306a56319bc
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:44:19 +0000

    benefit string bet 568energy 179bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
