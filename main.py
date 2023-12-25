b91611ca63e553571883905dea3b959ee2ee8989
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:01:17 +0000

    typical apparently reason bread bored

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
