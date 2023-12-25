906341350e65d4c04d210cc253ff832eaf1d91c7
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:34:52 +0000

    secret offer fill defend considering

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
