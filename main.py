a53aacbd5a34a4673782a0e4dfd84a074c2f48a1
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:26:03 +0000

    treat balance delicate already turndown

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
