addb8f40cdb378446697e249e25636d2421ab6a3
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:45:11 +0000

    myself whom mean favour support

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
