848e720225d020633e1d484cb1c9dde9d9bf4e66
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:16:21 +0000

    offence tone play basically trust

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  def convert_to_hex(n):
+        return hex(n)
