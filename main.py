101378088eccc6679eba388172f79120648dfe1a
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:27:55 +0000

    wind most error nonsense service

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
 import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
