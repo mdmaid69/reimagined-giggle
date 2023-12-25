b67c26921e67ab7705810834906c95b56b70c369
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:51:41 +0000

    substance sometimes object talkoutof heart

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_perpetuity(payment, rate):
+        return payment / rate
