9c7a57512fa1a2148c34c6d16709dc861a4883dd
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:12:30 +0000

    fire on hungry repair bin

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_item(array, i):
+        return array[i]
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
