78af47e1fe4dda17b4e20cddb21043f3759dfb25
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:22:44 +0000

    trust tongue split plastic mark

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
