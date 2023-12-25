220e9f7223d9b44bf5812f31a2259c39c5b79069
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:20:50 +0000

    pollution tour meanwhile children clear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
