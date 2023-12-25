7b9d23685afd200d6719adec02a3b1e83a358956
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:10:05 +0000

    emphasize spare seriously nice turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
