c42eb3c1936f9828663b742c54ffeb338b5cc635
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:34:47 +0000

    ear add through progress term

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_characters(sentence):
+        return len(sentence)
