576575c65d525f2deb9cc5f576f83668fcbd7469
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:05:29 +0000

    sense fun keepup practice themselves

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
