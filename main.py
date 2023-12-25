4cde29e0828f5b04f62155289e6b648f92283f2a
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:34:29 +0000

    march split pronunciation ago minimum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
