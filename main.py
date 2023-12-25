e37762e399e55b37262bbe0c1bed06a2eac42bc9
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:19:20 +0000

    trip inside correct tap chat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import collections
+def create_user_list():
+        return collections.UserList()
