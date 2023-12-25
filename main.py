90058abe41769e20e781af4331274dc2325ef94b
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:32:40 +0000

    ilovedicks come form brush dream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
