caa94b724f1d658829a4e9fc0ff789b4c6d5704d
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:22:36 +0000

    after runinto breakdown miss gather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
