cba4c6a1b810d411e46b074fa3f23b44657bdf0f
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:07:26 +0000

    tap problem concerned market plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_logarithm_base_e(x):
+        return math.log(x)
