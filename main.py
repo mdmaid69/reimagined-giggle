853faf2deb09bd9bbb8a15f5da591990ce5f0230
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:54:56 +0000

    tape our prize split colour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
