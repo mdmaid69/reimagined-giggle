9bc5c7919108d0f036fa8f0c51129228c3e5a430
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:46:58 +0000

    close variety design crazy excited

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_itemsize(array):
+        return array.itemsize
