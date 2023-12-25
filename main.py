157b2aad98939fb9136eddb34104e00fa3b5ccd8
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:51:38 +0000

    course already congratulations heart emotion

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
