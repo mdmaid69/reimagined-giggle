5c47df3158236a2e3db53aad21dce8889a188e3f
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:49:00 +0000

    top lock force ability design

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
