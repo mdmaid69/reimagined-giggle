d599914ef9e81d800a23837eed8d8ccd589b3bea
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:54:29 +0000

    another force history goodbye authority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import collections
+def create_user_list():
+        return collections.UserList()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
