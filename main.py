f7c40b1a6b7640500fd802ed154e82c75cbd8ccc
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:30:24 +0000

    wait freeze among ability please

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_work(force, distance):
-        return force * distance
+import sys
+def exit_program():
+        sys.exit()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
