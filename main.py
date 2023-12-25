6fde0c0e75494258fc38400211392a5382d0a9ab
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:50:56 +0000

    quit juice spin principle wave

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
