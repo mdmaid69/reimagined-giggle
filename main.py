84d1b7b791a3d35696842bf6198d6e40674ba449
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:43:51 +0000

    arrangement demand respect delicate finish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
