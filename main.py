dd62f2fea4c3923c21b2a97f00153077315791d4
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:04:47 +0000

    judge pair silent deserve shake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
