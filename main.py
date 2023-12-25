5034c0bee0c5c80c05918fb6c267207de33bbc1e
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:06:54 +0000

    sign obtain getback feed getback

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def remove_duplicates(lst):
+        return list(set(lst))
