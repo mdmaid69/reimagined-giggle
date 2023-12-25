1886f80f311dffc299ca81779158f60fc7df6197
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:28:46 +0000

    bother feel exaggerate past refuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
