b91d644a3015540cd3b4318d8a5d08b5f6f88d42
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:03:30 +0000

    basis label strict love trick

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
