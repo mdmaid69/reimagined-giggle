5ca5c6cb8d05c2197a1ad89cc0c7b02970ed586f
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:53:38 +0000

    suit inform offend word total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_str(array):
-        return str(array)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
