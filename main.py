6f4fda2027d1b2a5906c6b5a9ca036e77bec907c
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:34:10 +0000

    event care advice could keepout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import platform
+def get_python_version():
+        return platform.python_version()
