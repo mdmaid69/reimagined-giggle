b75162624b05d3f48a4f7a196b701a25d894a2b1
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:17:53 +0000

    border full or protection hardly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
