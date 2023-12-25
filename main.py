4aa1e3cc1ab406ac1bd413409ee2e145c6e44a19
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:46:47 +0000

    Zz advice immediate memory pupil

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def calculate_area_rectangle(l, w):
+        return l * w
+def sort_numbers(numbers):
+        return sorted(numbers)
