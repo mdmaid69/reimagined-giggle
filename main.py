96fae375b62f1e25ebd1bf12e8d852b7c6d4f5ce
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:54:28 +0000

    minor quantity swimming about tape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
