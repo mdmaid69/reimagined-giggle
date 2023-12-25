e25585d96e0013520bf7bc44ac75386848d72fc8
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:47:33 +0000

    nature cruelty trust report heart

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
 def fibonacci(n):
         a, b = 0, 1
         while a < n:
         print(a, end=" ")
         a, b = b, a+b
+import array
+def get_array_as_set(array):
+        return set(array)
