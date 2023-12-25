18fa82bde9b71742286780ab99237aad0e41f169
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:27:02 +0000

    check pronunciation grateful let face

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def is_odd(n):
+        return n % 2 != 0
+def is_palindrome(s):
+        return s == s[::-1]
