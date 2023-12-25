2d930dc222e20ab349c2932d58c7e94a84cdc6ac
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:12:58 +0000

    consult somewhere set Ff impatient

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
