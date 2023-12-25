4474c4b22d234076c3060afaeec79fe66c916d6a
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:16:35 +0000

    sign slice criticize effort slowly

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_density(mass, volume):
+        return mass / volume
