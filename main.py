0489382e765b62470b406ff1293ca80f4c8075d8
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:25:27 +0000

    separate comein strange replace introduction

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
