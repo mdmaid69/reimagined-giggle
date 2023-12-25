7ddfbae2cf031e24b4c776ccbf8f3d54220833ee
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:46:49 +0000

    beneath crash responsible better letter

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
