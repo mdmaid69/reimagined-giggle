d82108024bb178d409c3320f0347c4701ad08cbb
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:48:15 +0000

    last dish capital swimming start

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
 n = 5
 result = 1
 for i in range(1, n + 1):
         result *= i
 print("Factorial:", result)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
