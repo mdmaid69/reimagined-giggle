8e85836e00ef0e4079c2c4f24247e3604cd81f47
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:41:44 +0000

    trick practical rank pattern difficult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import platform
+def get_python_version():
+        return platform.python_version()
