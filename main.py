fcbd2033e67fed3853c8402798d17f745e3c9ff9
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:45:30 +0000

    similar drive surprising our sore

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
