bc8bf6b7392f9614dc6c31dd098db67a3d633fd6
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:18:54 +0000

    survive moral unit swing train

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def is_palindrome(s):
+        return s == s[::-1]
