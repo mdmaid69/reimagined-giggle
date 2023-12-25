f112891296cbcd6634e2534303abc1cdda06ebbb
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:53:32 +0000

    however giveaway shoulder giveout computer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
