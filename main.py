815422a75dac2bfac72fa8cebdf65c9d0132fc98
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:42:26 +0000

    hers goback(to) settle both extremely

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def reverse_list(lst):
+        return lst[::-1]
