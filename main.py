e7b334b6847dd2923628e8e3b9304fcca267ada6
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:46:55 +0000

    design fetch spray book smell

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def remove_duplicates(lst):
+        return list(set(lst))
