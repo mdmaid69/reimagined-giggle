6494d7b4a4aeeea12f560bfd452bf0709db0d8f1
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:07:37 +0000

    city Itself represent rather roll

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def find_min(numbers):
+        return min(numbers)
