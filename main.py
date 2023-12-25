c13ad57622bbf6930cc662eaecb543e69af6f5ce
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:00:03 +0000

    before get collection insist perform

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  def square_number(x):
+        return x**2
