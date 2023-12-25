a94f80f5754753ad9339ffc9727874c7f9fd2c3c
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:51:35 +0000

    remark turnaway at cross wash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
