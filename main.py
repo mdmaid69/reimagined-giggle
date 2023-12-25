0a1e42030a599024af0514cb53ba9ef394fc1588
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:08:00 +0000

    wire interrupt ahead will show

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def convert_to_binary(n):
+        return bin(n)
