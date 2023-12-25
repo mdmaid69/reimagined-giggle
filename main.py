030f7bd1db55b247f2bba761b529dc47b3cf2344
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:18:51 +0000

    fault compare police at obtain

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
