a7fd9e3006d186984b71ef5a588ee6c7109c0af0
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:19:18 +0000

    remark spot Bb sing hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
