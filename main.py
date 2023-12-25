55b80eabe98e8a1ed28db53646166a90fe45857f
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:42:31 +0000

    skin agreement store along why

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
