d4a4308d5292574b2952f477093e880c0c9bccff
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:32:53 +0000

    any kill scared discuss discovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
