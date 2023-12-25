bac5f3661b966e82183c1125fc7e340f4d974bb8
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:21:27 +0000

    impressive arm next hello brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-from collections import Counter
-print(Counter("hello world"))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
