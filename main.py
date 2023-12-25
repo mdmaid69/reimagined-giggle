ea42dd154a5fb90e28c29816f5233a8daae92f5e
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:13:17 +0000

    less funny joke nose always

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
