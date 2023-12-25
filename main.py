9121108ee8c7a129aa8bcabe7304cec11de392a6
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:56:10 +0000

    goout temperature view reduction official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
