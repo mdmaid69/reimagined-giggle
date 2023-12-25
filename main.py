6a2b898987c2dfac1db2441718b836f6638a35f9
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:53:46 +0000

    justice score hope improve sing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
 def calculate_roi(gain, cost):
         return (gain - cost) / cost
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
