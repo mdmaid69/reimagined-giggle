941c76996ea642c4826b0a577f367e2c6565ace2
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:17:04 +0000

    chin autumn whistle finally choice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
