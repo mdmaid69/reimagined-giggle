e55cc78047491e18246536b360df45bfd382276f
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:49:08 +0000

    touch rid change delight head

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
