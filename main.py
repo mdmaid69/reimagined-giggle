445b951bb4802afa8c5f5391d0f8c6bd37e5a6cb
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:04:14 +0000

    rare left evidence complain table

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+for i in range(5):
+        print(i)
