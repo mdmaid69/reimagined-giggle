5e11d284eddc34847b16d0b6c3139a4bcecb44cd
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:42:15 +0000

    exit who disapprove voice petrol

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def remove_from_array(array, item):
+        array.remove(item)
