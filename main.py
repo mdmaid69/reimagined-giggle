8fb7e4b48271f0f26d1a003f888de22ecee77095
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:19:33 +0000

    sleep before decide impatient prove

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  def convert_to_hex(n):
+        return hex(n)
