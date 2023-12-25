2df54324c82fb36310d3895c6543a0b3e6c63468
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:07:15 +0000

    crazy ready book innocent careful

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_area_rectangle(l, w):
+        return l * w
