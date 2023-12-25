1b6904f87791e323de0ff5bcc6081792ff73613b
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:41:22 +0000

    zero smell allow shame road

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def add_numbers(x, y):
+        return x + y
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
