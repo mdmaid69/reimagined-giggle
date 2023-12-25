8fe92008d071f5054b1fb2b8189a86da57f5b379
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:30:45 +0000

    holiday interview straight guess bound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
