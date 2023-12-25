a2087500fefedb7e0d1b3b295be3f0506e4ebfd4
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:11:07 +0000

    outside exam absolutely keepaway prefer

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
