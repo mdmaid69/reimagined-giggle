6260602fff39efb1a4a68ce6e947d6a8f89e2df3
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:53:20 +0000

    team hospital brain wise thought

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
