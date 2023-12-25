bfcedb3006d6f750dcf9d9acabbdc3eae6ca5edf
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:45:31 +0000

    pen job funny single destroy

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def convert_to_hex(n):
+        return hex(n)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
