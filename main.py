1e1e5b8dd0a2713f414cb6f188e096073276512c
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:03:32 +0000

    pause holiday me deaf Gg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
