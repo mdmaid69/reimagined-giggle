f4210bb146ee8751dc92547c8efc2efde4f374fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:03:26 +0000

    contrast exploration train today outside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import datetime
-print(datetime.datetime.now())
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
