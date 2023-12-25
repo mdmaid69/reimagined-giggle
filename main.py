a87e9be2c377b661e4db6c32dab5dec45f9cee43
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:34:54 +0000

    glass disturbing turninto unfortunate imagine

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def reverse_array(array):
+        array.reverse()
