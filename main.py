c98df69431d33aa7351e07ef3c0dc0dd8bfbe888
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:56:33 +0000

    female bye flow radio favour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def reverse_array(array):
+        array.reverse()
