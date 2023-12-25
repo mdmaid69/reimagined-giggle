3f505e181b420128160dbc12d6139eeee68210ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:40:44 +0000

    calm such light minimum steal

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-print([x**2 for x in range(10)])
+import sys
+def print_python_version():
+        return sys.version
+def find_max(numbers):
+        return max(numbers)
