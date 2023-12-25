99908c461b96eaec4ff55a1e1ed6b80ccbf4f122
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:48:33 +0000

    hard importance nurse usual discovery

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
