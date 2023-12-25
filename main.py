5f0f8e5b43bb4eaafc9768da717e607a4738a909
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:36:20 +0000

    fresh season sharp negative centimetre

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
