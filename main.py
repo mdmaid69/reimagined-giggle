0c8bfcd6822469bc5ae1ce12e54297fb5c949b28
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:17:12 +0000

    improvement possession odd we wire

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
