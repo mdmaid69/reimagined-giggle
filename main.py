ef800c5d87ef5543536950cf0b93d1b8173cd296
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:18:22 +0000

    feel club as store temper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def is_even(n):
+        return n % 2 == 0
