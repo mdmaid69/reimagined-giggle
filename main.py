a3615d0af744cf964ab045e0e81ad2f56014de8f
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:07:12 +0000

    immediately pain gap keepoff steady

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
