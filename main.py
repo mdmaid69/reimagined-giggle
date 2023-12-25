29720fb873b38278f93510653f3a4615a616f378
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:36:02 +0000

    worse mysterious replace case sale

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_current_working_directory():
+        return os.getcwd()
