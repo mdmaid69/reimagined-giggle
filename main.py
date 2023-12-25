6a16a3c887d490abaafbfd7458ef51fefbac7d3b
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:34:50 +0000

    yes near breakout neat system

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
