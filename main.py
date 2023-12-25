5a61a5f4f70a65944133f546a48ce2e9085dd521
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:58:15 +0000

    Cc stage broken right reject

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import datetime
+print(datetime.datetime.now())
+import array
+def get_list_from_array(array):
+        return array.tolist()
