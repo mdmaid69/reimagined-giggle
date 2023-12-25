ebfed15870cc0d33a623d70d0293444dfd028515
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:23:22 +0000

    which development calmly so warning

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def convert_to_hex(n):
-        return hex(n)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
