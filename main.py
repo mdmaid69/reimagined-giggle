ed38c39099d5581d1cde6bbb46f8bd12c103ce08
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:48:30 +0000

    comeover clue worry port force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
