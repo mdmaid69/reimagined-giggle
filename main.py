fdb6f199afadb7a4c60e7566d8528fe0f2010e88
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:45:56 +0000

    anyone that rough result steer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
