770e438a79a8e1ce64b0921cdb1952a316cf1269
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:53:41 +0000

    not careless law previous exploration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
