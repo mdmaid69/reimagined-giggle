3a999f4f74fc0608d51f7c2fea965884d39992a5
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:20:35 +0000

    listenfor history that hide run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
