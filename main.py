b7f45cf840db88a3e194969a28af79885ecd2620
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:59:40 +0000

    invite guess opponent eager anybody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def create_counter():
+        return collections.Counter()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
