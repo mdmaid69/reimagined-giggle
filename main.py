2ba9ea1bedc468748a11189f6eb5e8056394503b
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:37:04 +0000

    contain win talk out degree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
