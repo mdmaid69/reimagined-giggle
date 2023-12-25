09a4b677d30cd33c9e8a6de2b81ccf52d0d38d40
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:44:55 +0000

    stone of suppose lower best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
