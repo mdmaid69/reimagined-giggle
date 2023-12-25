f5b4545daf96cf912a503d4e0938556a70cab977
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:33:55 +0000

    goup news mean service not

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_power(work, time):
+        return work / time
