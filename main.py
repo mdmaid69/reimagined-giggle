283fd03483a99304a2130207eaa4683112c22973
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:09:56 +0000

    nature offer getback hour speed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
