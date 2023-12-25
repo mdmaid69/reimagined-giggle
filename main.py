d18e632a289bcf98e272fc1ced9fab394f9bb6e1
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:44:44 +0000

    family event criticize attempt borrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import math
 def calculate_factorial(n):
         return math.factorial(n)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
