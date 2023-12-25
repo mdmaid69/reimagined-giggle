0bb28322129ccded39195e03b0be0323b79f869c
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:44:38 +0000

    film house block ocean drive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
