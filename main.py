eab7903063a9d1ed8100484338b62f73e59df953
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:50:24 +0000

    type trap should instant where

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
