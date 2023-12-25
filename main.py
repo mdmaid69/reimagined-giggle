72928d3829d545d93d7d81bfa1dc1802fadff3c5
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:33:56 +0000

    excuse seriously cheque street yours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_stack():
+        return collections.deque()
