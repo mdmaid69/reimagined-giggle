37d62c403e1fd2c11e45c59abec7959a80e91858
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:20:27 +0000

    dare talkinto patient press patient

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-print(math.pi)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
