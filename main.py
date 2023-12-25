18c58c9190c0729fa23d59f4a40966f42bf0368d
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:42:54 +0000

    mysterious date Mm forever attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
