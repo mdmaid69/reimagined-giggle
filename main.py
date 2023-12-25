c3cf4b598bf2da4cba071212a2cea5eec63d4bfe
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:38:23 +0000

    block emphasize debt shock prepare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+x = 10
+y = 20
+print("Sum:", x + y)
