35d98daf999892f76dcc7d93f5d50b2c9849ba28
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:03:25 +0000

    of sale blame tear organize

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
