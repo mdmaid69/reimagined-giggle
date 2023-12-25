699a71bdea32b59e32cff9bae51e7ce846db48db
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:58:14 +0000

    heart principle house concentration lock

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
