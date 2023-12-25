9fa29e1719c2a3fd928aa02a4f630b04b582b9e4
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:47:57 +0000

    criticism through wind combination colour

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_area_rectangle(l, w):
+        return l * w
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
