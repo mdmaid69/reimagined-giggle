5d83054e68c6e316be15350953f1ca6f78b5a8ed
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:08:04 +0000

    accidentally minimum ill immediate memory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def reverse_array(array):
+        array.reverse()
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
