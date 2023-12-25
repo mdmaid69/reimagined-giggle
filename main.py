daf12b33813b9aed5b53d947f6189428e679c96a
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:24:42 +0000

    possible move children shame centre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_acceleration(speed, time):
+        return speed / time
