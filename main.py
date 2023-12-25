fafb62df3b825004da9327afd6ff7d88f7f34935
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:47:55 +0000

    instant this perfect persuade skin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
