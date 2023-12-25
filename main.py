b069c9ad7a812871fd2d239b4b47d9416cf0542f
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:27:20 +0000

    human injury nature dry slight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
