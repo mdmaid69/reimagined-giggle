55293b7682d4499322bb8fb4df49ca91213bbca2
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:34:39 +0000

    damp dare OKorokay wide wave

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
