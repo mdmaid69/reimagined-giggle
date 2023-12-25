bac5b6ecbff1697b8445590a072238a5b425c998
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:52:47 +0000

    bit tired rain post pen

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import sys
+  def get_python_version():
+        return sys.version
