f54dbaf0fa96339b21416655be5142e0f5d810d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:09:21 +0000

    thinkof sensible worried authority coffee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
