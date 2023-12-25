2a5cd5cd03f73789118075f56e59c5a4344aef9c
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:09:40 +0000

    violence tour dress gas less

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import math
-print(math.pi)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
