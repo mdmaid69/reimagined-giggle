5e050fcd2d8ca2a3bde1b29c90e96c2ee79ec728
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:18:12 +0000

    coat sure succeed much tip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_circumference_circle(r):
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_circumference_circle(r):
         return 2 * 3.14 * r
