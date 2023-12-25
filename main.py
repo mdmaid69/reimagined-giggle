0773a5d7f03c247230a2382080feafeac1e701f4
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:54:54 +0000

    little most bite dig pleasure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import collections
-def create_user_string():
-        return collections.UserString()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_as_set(array):
+        return set(array)
