5238dcad3e4d0c57ffd21425bc85f1cfcf4c18d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:42:35 +0000

    police fetch female necessary hear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-print(sum(range(10)))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
