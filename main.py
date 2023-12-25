1b2fc86d0ee288b022dd9275f8c6fd56919cea2d
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:10:55 +0000

    solve level cheat secret offend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def convert_to_hex(n):
+        return hex(n)
