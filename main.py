faad84a3f3bf0c7da4ef972c6f2d2f46797110e2
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:02:36 +0000

    goback(to) preparation situation objection now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
