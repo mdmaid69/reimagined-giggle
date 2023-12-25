ba628d8ae2a93db6d8140e5df862c7d2eded6a18
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:46:04 +0000

    state extremely pleasant sure factory

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_density(mass, volume):
+        return mass / volume
