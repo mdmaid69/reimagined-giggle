7ba75ea4140f387438c10fee4eb9c466fac07f8e
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:36:39 +0000

    border challenge rid pot different

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
