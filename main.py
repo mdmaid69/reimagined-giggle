b67c11e521c9630ba441a369101f3d53284cb3e7
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:05:03 +0000

    guide refuse describe moment unlucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def count_words(sentence):
-        return len(sentence.split())
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
