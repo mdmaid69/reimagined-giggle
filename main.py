93320fa94264ee8fc337fccd16cfcb7c8b7cc52f
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:01:21 +0000

    plan bit tidy get thinkover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
