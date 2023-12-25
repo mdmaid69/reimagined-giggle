cad04474a851cd3f7b90cee81f4dba7446d7ab9d
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:12:03 +0000

    face popular this rather meaning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
