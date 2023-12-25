a5829c7802bb09bd51d958b2e7f6b90b57936015
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:59:35 +0000

    apologizeapologise care risk force view

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  def convert_to_octal(n):
+        return oct(n)
