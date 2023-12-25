3a37b7569bcabb67b6b971b11c18401b24d02b70
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:04:45 +0000

    list cinema best sound cost

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
