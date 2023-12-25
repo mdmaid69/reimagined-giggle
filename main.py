223efc58b130838c80eb7d347907bb126a8acaf0
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:17:44 +0000

    damage promise drive basic before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
