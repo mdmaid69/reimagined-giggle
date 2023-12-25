7c326b48c1d79276f4d57e664fd94c91ea79b1dc
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:25:05 +0000

    die by study priority beside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import time
+def get_current_time():
+        return time.time()
