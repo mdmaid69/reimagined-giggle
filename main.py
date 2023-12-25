15c74c8eb1d43d50710e0ed86519ac322d1090ea
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:07:03 +0000

    loss colour uniform in use

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-for i in range(10): print(i)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def sort_list(lst):
+        return sorted(lst)
