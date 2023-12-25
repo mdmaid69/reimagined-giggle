2ead14a89fd42e9654b5775af4dd1bbc64909c36
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:55:54 +0000

    model store grow question type

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
