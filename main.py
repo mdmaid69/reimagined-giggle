81f59b55caf1f3305504381df095ffebb454f8d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:02:51 +0000

    suit rise break sweet serve

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
