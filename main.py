3e9b05b03e8b528b778247c492a37085744a4e42
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:41:14 +0000

    gentle die further definite separate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
