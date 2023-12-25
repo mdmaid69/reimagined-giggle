2aa34539ff10a6e9c8a076f7075df71ed8d2243e
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:58:32 +0000

    letter owe turninto example coat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
 def calculate_bessel_function_of_first_kind(n, x):
         return math.jn(n, x)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def multiply_numbers(x, y):
+        return x * y
