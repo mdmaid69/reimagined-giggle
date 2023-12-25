1ec0fe03cdaf5cf8f17469e59219d33620e67726
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:46:08 +0000

    design perhaps generally include whom

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def calculate_area_circle(r):
+        return 3.14 * r**2
