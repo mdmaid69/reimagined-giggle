9b3efa09fbaebc331dfb80b29e11d7c7a2e2bb56
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:43:02 +0000

    to chief use model attach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_base_name(path):
+        return os.path.basename(path)
