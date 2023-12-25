69f9a40725672dd612d44369fe61ccb7e498e43d
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:32:15 +0000

    talkinto conclusion affect after difference

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+print(json.dumps({"name": "John", "age": 30}))
