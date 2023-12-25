3e6f254722a773e5b2c68bbf9e902b496faf3070
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:24:17 +0000

    recognize dry share join waste

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
