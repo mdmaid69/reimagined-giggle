ca80b32017651c7bdcde91da5a53b417347e58ad
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:50:57 +0000

    equal comein mail firm order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
