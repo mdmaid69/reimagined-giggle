97935f0dc9a3e60d243ecfe54264a654bb3a3a78
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:02:11 +0000

    they whistle around step play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def append_to_array(array, item):
+        array.append(item)
