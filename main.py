bf8554a023ebe4249451c9474dfed88c4353a95f
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:40:16 +0000

    571enough experiment commit exercise rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def square_number(x):
+        return x**2
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_arc_cosine(x):
+        return math.acos(x)
