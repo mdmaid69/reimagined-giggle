260ec28217238599cf4ec61646f0149c86972111
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:03:43 +0000

    successful clear check practice behaviour

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import time
-print(time.time())
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
