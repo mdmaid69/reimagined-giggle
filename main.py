5c7177e406a042e92bcb6a2703473e265fe19d35
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:59:00 +0000

    transparent discover tragedy healthy recognize

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
