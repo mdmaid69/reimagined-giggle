44333b1f8c778326f2e9e7a3782dc5cb626cac0c
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:49:09 +0000

    carry save possible connect mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_volume(length, width, height):
+        return length * width * height
