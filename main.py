3cc447f8848c76851241b7350bb41bfe61953a26
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:18:34 +0000

    importance offer callup transport lucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
