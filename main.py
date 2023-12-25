1a5f6de1338b41884e109ad558a50b291a850406
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:48:35 +0000

    above fast popular brush survive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def divide_numbers(x, y):
-        return x / y
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
