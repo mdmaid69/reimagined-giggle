7028acd404f409c11e687ebe05acf86c5d32a250
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:16:30 +0000

    comeover much cover after shock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import time
+print(time.time())
