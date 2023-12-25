b0c44d903af2e80f82516e8816e030a5f5458638
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:45:49 +0000

    nurse mysterious extraordinary race yesterday

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
