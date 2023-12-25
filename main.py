f2429f2e2998d200967548623f39b3dccef79625
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:30:25 +0000

    provide deserve left brother weekend

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
