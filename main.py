10add3d0f8f440ccc128fcf8b8d8c27749383c29
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:40:07 +0000

    creature watch cool trick look

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import os
-print(os.getcwd())
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
