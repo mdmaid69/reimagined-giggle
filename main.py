0d85096de2004e75b834f4f78b54209c55931f33
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:32:09 +0000

    past mix store equal pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import os
+print(os.getcwd())
