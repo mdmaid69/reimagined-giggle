63831392557841adbb460f9a9a45e3ccd98cd6a9
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:05:04 +0000

    mess poor almost correction band

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
