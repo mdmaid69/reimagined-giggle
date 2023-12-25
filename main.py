f555b4c0a8a9d1a381f24e0c8f283d2e276a0c65
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:38:22 +0000

    sting payment love suggestion wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
