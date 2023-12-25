3f5981801c7306609b40ab5c22f1107c2f96d589
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:16:18 +0000

    bring show side with limit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def add_numbers(x, y):
+        return x + y
