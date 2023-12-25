c8ba369ae79b9153cf80c19091e83d85d32f7831
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:11:45 +0000

    lock top as sure confidence

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  def convert_to_octal(n):
+        return oct(n)
