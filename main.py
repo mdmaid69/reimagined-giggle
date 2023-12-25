bf36df30046fcf0175042e0d51b60e227f36f4a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:08:21 +0000

    thanks convince where frame over

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def divide_numbers(x, y):
-        return x / y
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def find_max(lst):
+        return max(lst)
