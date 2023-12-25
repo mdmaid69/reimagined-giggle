d69dde17704f4a1d9812efb4d240d647ecb9efcb
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:13:11 +0000

    mad once play earth section

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
