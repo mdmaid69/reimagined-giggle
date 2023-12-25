4e5957b46b8b5390a486f595f18fd9c0f336fb62
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:02:38 +0000

    shadow right breakout wing splash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
