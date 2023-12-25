e307fdf7c2b766c74b097f86c6982601e8f05b35
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:55:16 +0000

    confuse understand name charge tax

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
