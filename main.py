1454593336020ba8bf1c89789f5a36a801102ed5
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:09:46 +0000

    lucky software conclusion duty little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def print_python_version():
+        print(sys.version)
