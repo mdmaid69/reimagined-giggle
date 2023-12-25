5031c292913a881c7476ad3b189d07083bb09c2e
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:48:13 +0000

    home circumstance dress beautiful onto

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
