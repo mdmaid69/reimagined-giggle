0ed66c237a2f6af07969824c7f7d89abaef16c6d
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:27:40 +0000

    married communicate reduction urgent ground

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-for i in range(10): print(i)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
