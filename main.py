d7745c044e0e613aab1247b3ba3eb07bbdc95fff
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:17:25 +0000

    ready object far account switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
