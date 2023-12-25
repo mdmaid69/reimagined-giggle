9bbddee671acc1019b56e2872b167cb87a61ed40
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:57:15 +0000

    exactly sharp everyone slightly guide

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_int(array):
+        return int(array[0])
