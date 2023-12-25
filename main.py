a6b544abddc0c91422f884487d1fdeb4c87988ca
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:43:47 +0000

    immediate protect need pretty disappear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
