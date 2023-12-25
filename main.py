ffcded3fa4c4358d76a88f41762c6646efa2c802
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:47:57 +0000

    straight destroy fall company goout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
