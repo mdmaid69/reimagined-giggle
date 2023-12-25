7da1a0713a3e88d849401f005315a3c72f8fdb47
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:21:04 +0000

    probably fault lead Tt reply

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_str(array):
+        return str(array)
