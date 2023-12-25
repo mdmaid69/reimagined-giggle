8da7829a96220b1c9323e9d703aaee767635c1db
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:08:28 +0000

    for suit marry fasten disturb

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
