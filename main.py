11f7c1d41545c9ce4ed0bc3790b9c4e81e3fbb5a
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:47:17 +0000

    mistake danger carry else 179bite

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def count_characters(sentence):
-        return len(sentence)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
