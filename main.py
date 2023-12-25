95a5ae92568042eb18ba9686540329b9e9c19203
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:37:52 +0000

    such brush luck loss door

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
