4f04c6397ab5b1ab102a75f2c93105efee6605aa
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:53:13 +0000

    rest gas pencil heart fix

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
