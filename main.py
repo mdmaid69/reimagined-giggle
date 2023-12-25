68acff423edec5afa60110381d9b7e6b1457b122
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:08:18 +0000

    rare measure direct model surprise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import platform
+def get_python_version():
+        return platform.python_version()
+import sys
+def exit_program():
+        sys.exit()
