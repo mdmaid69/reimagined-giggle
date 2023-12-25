e9d613c65bad30198c6e6bd953c9dd15ece6db07
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:30:50 +0000

    as still bringup set fail

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
