43d8ebe17ba2bd813079eb01ac94a816d9acf197
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:17:10 +0000

    giveup mess below excellent wonder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_set(array):
+        return set(array)
