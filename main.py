ad044fae33c95f718130584cd67019b20e9b5ba1
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:49:58 +0000

    cruelty part brush favour dish

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area(radius):
+        return 3.14 * radius * radius
