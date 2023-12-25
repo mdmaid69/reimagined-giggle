bd718dc805c8a198a2ba496ac4b3c5df9eda1528
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:50:35 +0000

    somewhere disappointed edge impressed responsible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
