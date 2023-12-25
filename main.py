081a906a388ff4ed18f8fa328c53529081e477c2
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:40:42 +0000

    value Hh bill shoulder shrink

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
