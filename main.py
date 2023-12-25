e70c2eb205e666c9988b333a9a73261fab94e91d
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:13:27 +0000

    convincing disapprove sorry public shame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
