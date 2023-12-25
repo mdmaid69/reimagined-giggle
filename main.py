8848b494c64f3e6ad231717e6e233eef7774315e
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:07:54 +0000

    put wrap onto surprise doubt

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def calculate_work(force, distance):
-        return force * distance
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_unique_words(sentence):
+        return set(sentence.split())
