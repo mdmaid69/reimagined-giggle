21c067401689da4ce1a102b3bcbbc496626915cc
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:14:37 +0000

    rush Tt world Tt shout

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
