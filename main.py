3d8a0e34df1c5c782bc0032617390507b83ac2a5
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:10:23 +0000

    mistake equal high smell ever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_directory_name(path):
+        return os.path.dirname(path)
