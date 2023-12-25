84b04c508c093c235df16cbff535bb550a5bd186
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:16:06 +0000

    workupto considering stretch narrow pencil

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
