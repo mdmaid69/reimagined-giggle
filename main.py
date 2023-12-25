d280ea3f4354838258508863cd299155ff48c22d
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:01:05 +0000

    quarter collect break breakup story

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
