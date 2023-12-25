b91e30021b7ad65fc2f5b3e3367bcd1573defdcd
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:39:27 +0000

    light doubt Nn squeeze print

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
