fd3605ccf9d895f181162c613c2a5f8ddd271449
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:11:31 +0000

    previous relief finally heart act

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  def sort_list(lst):
+        return sorted(lst)
