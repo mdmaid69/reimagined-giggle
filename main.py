e5338a1b5dd6988bdde8220386a3a2d19e7c4453
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:02:29 +0000

    Ee off Uu party high

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_as_set(array):
+        return set(array)
+import sys
+def print_python_version():
+        print(sys.version)
