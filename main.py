7afc304f1fafc5a83cad148eae36e40e5a04fa8a
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:11:17 +0000

    rest offend family intend nowhere

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  def is_odd(n):
-        return n % 2 != 0
+import sys
+def print_python_version():
+        return sys.version
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
