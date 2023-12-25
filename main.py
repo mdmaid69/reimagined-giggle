4fe3f1d3fdb961428b32557de35e54454cefbbf4
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:18:02 +0000

    excuse enthusiastic spring height ago

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  def count_elements(lst):
+        return len(lst)
