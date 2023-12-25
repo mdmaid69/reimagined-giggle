0485ac6410620d092f6bc8f204863baa67eb2e43
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:43:09 +0000

    general whatever escape hit genuine

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import sys
-def print_python_version():
-        print(sys.version)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
