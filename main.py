2756cc21838deca4b67ad4959353dad88971a798
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:59:37 +0000

    tap march valuable objection achievement

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_speed(distance, time):
+        return distance / time
