4e4cda14417681fe7ee2a2b0349aed0e1aaff986
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:27:40 +0000

    combination reaction save accordingto form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
