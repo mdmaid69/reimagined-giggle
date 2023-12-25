a815181ca2bc6191cc492ee7ee7709bf26523535
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:53:25 +0000

    professional hurry environment criticize entry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_power(work, time):
+        return work / time
