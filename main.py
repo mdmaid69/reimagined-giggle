b8df69f9a404ce3c8b295ef32fc8c95169a7650d
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:58:04 +0000

    market repair boil sometimes track

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
