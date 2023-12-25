2515b1e220d0691a2e1ef3729cf66f7e5ec77017
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:07:48 +0000

    believe test thick someone both

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
