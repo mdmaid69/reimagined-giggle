0a112f992799b164ef51ff26bfc13ed64f20f727
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:09:05 +0000

    judge comeoff free black new

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
