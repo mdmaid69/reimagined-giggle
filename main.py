a52160dc4dfcc86d008f6b011476040110f27720
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:17:38 +0000

    responsibility gooff survive drive high

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
