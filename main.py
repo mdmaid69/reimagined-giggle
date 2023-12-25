231bf966397488b0de43a42c509ae5cd0322564e
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:31:39 +0000

    makeinto moon beyond 566end path

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def square_number(x):
+        return x**2
