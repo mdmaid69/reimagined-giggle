4c70a10baabe73947ef20e7c54a4c0a747d47dae
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:51:43 +0000

    unlucky harm law edge roof

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
