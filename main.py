2ed2ff107c8804c799d7e90431a116269928a080
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:51:23 +0000

    freeze sensitive store treat song

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_min(lst):
+        return min(lst)
