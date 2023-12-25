fc2974debda26ce8714f875a4ea203f665c40312
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:12:16 +0000

    broken happen around splash wing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
