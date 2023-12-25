b7135173f4a82645a7961f9688fba52c14b50b7b
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:54:14 +0000

    stare surprising negative talkback pin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def square_number(x):
+        return x**2
