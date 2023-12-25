4cc501c88adc54604129a94985c0b15bfe82ad66
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:28:04 +0000

    suckme excuse relationship image second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
