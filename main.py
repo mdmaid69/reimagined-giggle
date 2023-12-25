4f2de340a3d9f671ac71d151db8314d79d20a8dc
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:31:16 +0000

    plan yours exploration thank describe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
