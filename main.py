8963c74a39eab283a84ef8c90f6e08ef44eaeb34
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:30:34 +0000

    build spill improve lead doubt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
