5e264749af6a774073ef162b3dbcb10a035b639c
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:41:43 +0000

    squash reason land rain upwards

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
