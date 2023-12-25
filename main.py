47d198e21a2ec9b9bac4e17b6831700c4da9a63e
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:20:52 +0000

    properly squash tune sink whisper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
