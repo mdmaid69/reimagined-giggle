48389908ab06b2b1cb6ac286ef0fe347cb363a1a
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:30:36 +0000

    ticket confusing suffer club base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_volume(length, width, height):
+        return length * width * height
