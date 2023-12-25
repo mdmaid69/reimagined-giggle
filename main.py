6cc0f2b317df60dc14d5f0e87779f4368d8d6c4f
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:05:54 +0000

    excuse doctor correction answer concerning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
