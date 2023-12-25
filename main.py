f69fb4be92f9e925f7e350255a81ee0fc840b2d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:04:18 +0000

    man pause breakup post hit

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def reverse_string(s):
-        return s[::-1]
-import array
-def reverse_array(array):
-        array.reverse()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
