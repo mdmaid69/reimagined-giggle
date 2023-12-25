90c964c49a1c2f61df71760de1d1a7ac29ce3d83
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:54:01 +0000

    slight dinner transport then date

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def find_min(numbers):
-        return min(numbers)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
