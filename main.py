e4b41f958cd61e943a524ecde92d17b25412977d
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:24:18 +0000

    method reach payment further surprising

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
