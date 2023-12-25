95809ae49161762f31df219e77ed9796fb7e99fe
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:26:43 +0000

    sing image create dish description

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def convert_to_hex(n):
-        return hex(n)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
