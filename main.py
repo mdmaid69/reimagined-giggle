b19d97972765a3bd278c890fe1078282ab697325
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:15:53 +0000

    charge ignore description fashion cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
