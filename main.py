5045588a37282fc72e13b2fad4d60bfee67f33f3
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:22:34 +0000

    full needle repeat boring crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
