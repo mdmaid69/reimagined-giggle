c5756d4979a377982b2118e05ef127d5ad24a9b6
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:46:10 +0000

    means lookafter mass down block

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  def cube_number(x):
+        return x**3
