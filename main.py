edd104e3f076254ff82a3f0d4460839e4090e0f1
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:57:34 +0000

    even breath cry reserve straight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
