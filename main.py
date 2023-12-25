034fca1ae3d942633bb79016f6a2b8f94031a160
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:54:40 +0000

    passage price bit print knowledge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
