70a8af4840c33f27d6bceefb0725738f13153ea9
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:55:14 +0000

    describe be tap fold issue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def convert_to_hex(n):
+        return hex(n)
