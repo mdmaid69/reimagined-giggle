b9dacf4c67b55f79e6e2b33f7ed62557da3f9a91
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:57:10 +0000

    survive breakinto flat label downstairs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
