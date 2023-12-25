b64919bf508c5895b076609fafa00b1850dea624
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:49:08 +0000

    something introduction getup sell experiment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
