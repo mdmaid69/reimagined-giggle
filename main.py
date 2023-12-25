1d93ed91bb1dbc6adcd7a11da7b85fa4ff2b8afd
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:28:29 +0000

    wrong same serve interested than

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
