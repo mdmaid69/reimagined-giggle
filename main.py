b202dc66d3168c5955853d269bb31311a439ac95
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:45:23 +0000

    pleasant lead now main underneath

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_index(array, item):
+        return array.index(item)
