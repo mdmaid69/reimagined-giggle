66a249566d52a68aa7934011a210a609956f350c
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:52:09 +0000

    sleep still convince lonely pronounce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
