d3ee879978fd0a8e21e5580d0457d5a7ca32663f
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:37:25 +0000

    undo spare tonight correct hate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_current_directory():
+        return os.getcwd()
+def remove_duplicates(lst):
+        return list(set(lst))
