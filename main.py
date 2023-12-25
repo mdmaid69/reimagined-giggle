5e1f4d60b11b581a3cbc8633318ed2b4b9b6fc1c
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:28:58 +0000

    breakthrough talkback drive runaway brave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_factorial(n):
+        return math.factorial(n)
+def find_max(lst):
+        return max(lst)
