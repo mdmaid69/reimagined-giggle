f06757ac68f67566312a903a5997b22d539be3c5
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:40:42 +0000

    turn explain other foreign live

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+print([x**2 for x in range(10)])
