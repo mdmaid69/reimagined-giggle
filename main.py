084a7abe794412584fbd1dce1f088d2b7f2e929c
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:12:05 +0000

    town story settle clue human

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
