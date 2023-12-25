688d4ca5e1bb188dc7c8dd04b02041037eeff732
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:00:22 +0000

    rich occur care centre trouble

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
