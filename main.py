a86eef9648b937b053b85780c2754545a186738d
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:16:00 +0000

    discover invitation book thumb reply

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def is_odd(n):
+        return n % 2 != 0
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
