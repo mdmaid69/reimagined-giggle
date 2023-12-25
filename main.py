2499a9a1c9bed6b0e9f000335ceca292293f842a
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:10:19 +0000

    let ahead still division entrance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
