01855a54e84f483f662747cbc8d7e048e98d81c9
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:51:46 +0000

    his herself cover it awake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
