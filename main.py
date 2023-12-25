099ac1ef29bc286aaa8df08958ec8a7e687a63bc
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:10:29 +0000

    responsibility declaration compete properly occasion

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
