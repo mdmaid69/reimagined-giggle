0907085943f7032e79f63f7b5c0f3f26aac274d8
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:29:28 +0000

    firm affect relation comeback important

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def set_array_item(array, i, item):
+        array[i] = item
