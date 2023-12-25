b41630b3905ef6a955627eddf45a7b52c87660b1
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:58:11 +0000

    opportunity flight custom full safe

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def convert_to_octal(n):
+        return oct(n)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
