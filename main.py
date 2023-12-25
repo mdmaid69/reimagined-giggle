863b1fe08216a578f07e3b6cf5deffa0046275a9
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:43:09 +0000

    flat catch 567enemy now sale

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import datetime
-print(datetime.datetime.now())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
