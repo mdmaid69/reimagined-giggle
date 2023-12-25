5b6431751f8816208607fcf37f7c01d9a2737a0a
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:00:09 +0000

    shout bath fat body inquire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
