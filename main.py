830af6dd5423778028ad91971862bef0c25c3d47
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:14:11 +0000

    Kk month possible must negative

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def calculate_area_circle(r):
+        return 3.14 * r**2
