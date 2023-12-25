5205b08e405fabe6f999954aa3c8b5120da75f79
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:11:01 +0000

    play money little pretty mistake

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
 import collections
-def create_user_list():
-        return collections.UserList()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
