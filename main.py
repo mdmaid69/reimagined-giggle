1ce6880d68a8b243c10ab99ef63734ef4bdac6a2
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:04:28 +0000

    steady famous better all cry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_power(work, time):
+        return work / time
