6f22498ffc1cd4f7b6869f59ff9ebcf904560f65
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:08:26 +0000

    work whether refer powerful club

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
