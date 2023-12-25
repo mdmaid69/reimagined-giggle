2781d60906ff8a80c9541faa6683528b13618d1a
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:17:25 +0000

    speak blame steep children description

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_current_working_directory():
+        return os.getcwd()
+def reverse_list(lst):
+        return lst[::-1]
