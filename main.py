b76cd8a77926ca0a7362a0e81421288a06e48afd
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:24:01 +0000

    example failure doctor there yours

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
