ce0db79f1b81c8c05ebb2319fe122420915d0e97
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:43:39 +0000

    top around balance tomorrow attract

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def divide_numbers(x, y):
+        return x / y
