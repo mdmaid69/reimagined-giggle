fd2fd5dd8d15e583e7dc7dffca0c97dc43ec7710
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:16:42 +0000

    ask in difficulty hurry glad

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
