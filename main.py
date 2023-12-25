2d809848f6f07f402032c247f3b174b2add1ddad
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:58:03 +0000

    path Vv border straight shock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
