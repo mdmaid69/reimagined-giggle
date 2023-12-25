2c92ad94ee2c2b95d041c604db411a9ae820c554
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:34:09 +0000

    music ready community until pain

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
