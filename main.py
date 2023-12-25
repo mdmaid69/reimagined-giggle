d2a4da628f5c9391dc25185c471d675727b4a191
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:05:49 +0000

    temper bank production shelter anywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def append_to_array(array, item):
+        array.append(item)
