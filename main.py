e4a30a1171a50d84ce3d75e273b2430da57c8ba8
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:49:08 +0000

    anything connected brush week sound

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_union(list1, list2):
+        return set(list1) | set(list2)
