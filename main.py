3c306783ebbb1b88397e96bff63e14b321070f90
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:01:53 +0000

    control bath for since shine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+for i in range(5):
+        print(i)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
