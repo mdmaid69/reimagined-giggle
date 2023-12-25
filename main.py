22b6f2f364928e577e6d3770032ab6f1527c31df
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:54:14 +0000

    half separate last bridge skill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
