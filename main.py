fe78c509aeda7fbb67a4f4d7ab9e1863c7622cc0
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:14:44 +0000

    number disturb suit plan probably

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
