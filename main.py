b9c4df8548048faf973b2408c5d7cba18baa73be
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:04:47 +0000

    like cover fear lost punish

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
