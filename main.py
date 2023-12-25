6248e9f9c0cbb2735e710914d51388e503dcc93a
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:27:43 +0000

    memory expect keepoff admire town

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
