0a825ffe6d336c69b98903b6a20205062c6d352d
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:44:26 +0000

    health towards soft threaten choice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
