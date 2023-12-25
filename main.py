278c1594aec1ee11553642fff37eb582d0fb3e4d
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:32:31 +0000

    afterwards never 573entertainment quit satisfied

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def reverse_list(lst):
-        return lst[::-1]
-  def convert_to_hex(n):
-        return hex(n)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
