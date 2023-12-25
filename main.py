537b9e052f0d7f13ec91b860472ec2c6d499f2a8
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:28:49 +0000

    unlucky deep lean telephone neck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def find_max(numbers):
-        return max(numbers)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
