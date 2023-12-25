5df656016d1a3e9fd8f17f1e48fa583d54cb9e8e
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:40:10 +0000

    chase sorry crash lend wheel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def get_array_as_set(array):
+        return set(array)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
