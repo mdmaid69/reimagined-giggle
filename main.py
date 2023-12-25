e94fbd9a6a5abf816ed4f340eac8b7ad9e140917
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:17:06 +0000

    about family score environment exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
