579ec1e410a182a3c142f0e74c8fdf35afebc627
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:26:43 +0000

    week runover project also towards

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
