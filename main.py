956e7f556bf57abae6da20e11fb52d69ce3a4508
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:55:48 +0000

    obvious day geton front away

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def convert_to_octal(n):
+        return oct(n)
+import sys
+print(sys.version)
