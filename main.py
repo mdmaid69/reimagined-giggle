327267713ab18545755aa6b19c16c3f8595dc39e
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:28:42 +0000

    otherwise pause process unusual disappear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
