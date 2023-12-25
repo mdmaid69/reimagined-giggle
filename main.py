447c20ff5f72c8f1bcb64c759119286e2e761f9e
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:46:12 +0000

    interview single send port nearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
