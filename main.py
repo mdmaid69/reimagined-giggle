a7607c7e120be1c8bfefe412271f9b56f8a04b70
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:33:09 +0000

    story create event impossible tune

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
