9e031d0855498396ed8006b79c0861ca81ca3275
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:45:25 +0000

    little spring invitation extremely experience

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
