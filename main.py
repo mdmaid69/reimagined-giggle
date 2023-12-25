f0637ee18b37199a19d77c20e9d88015486afc1e
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:42:11 +0000

    thinkof keepout sad boring win

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def cube_number(x):
-        return x**3
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
