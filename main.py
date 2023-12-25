84f0efd7507d2c793d99e27e59ae6a862f862bd0
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:28:14 +0000

    emergency exhibition Yy university Nn

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
