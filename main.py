dc1eed71d4688ad6ab41e31ed13148f14bd85b3c
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:20:20 +0000

    middle Ee slow board confused

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
