7700cfcfc90eb5ae1c65e53f6311d17c2822817a
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:51:36 +0000

    open secret unlikely smoke loud

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def remove_duplicates(lst):
+        return list(set(lst))
