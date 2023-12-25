7ac7b3c8458287553aec4b36d75ecfc39076520f
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:37:54 +0000

    late cover apologizeapologise bringup waste

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_acceleration(speed, time):
+        return speed / time
