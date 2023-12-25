b1c20ab5de906f90bf2ccec338fcc95f3a642da2
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:58:13 +0000

    roof in nonsense report path

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
