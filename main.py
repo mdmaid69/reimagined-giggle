b6ad48bd77163bd8687a8d4546355b5a0c1ab279
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:42:48 +0000

    environment figure situation prize thick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import datetime
+def get_current_date():
+        return datetime.date.today()
 import array
-def get_array_as_str(array):
-        return str(array)
+def convert_array_to_list(array):
+        return array.tolist()
