4bae2192c2baf2d5b7625c0113ac03e474ebef60
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:53:01 +0000

    each build mix feel their

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
