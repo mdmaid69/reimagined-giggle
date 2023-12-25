da44fdeb970f66e79a7992996ffd44c5c30795c3
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:00:46 +0000

    telephone close store bear wide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
