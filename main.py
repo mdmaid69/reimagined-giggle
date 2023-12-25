841a4fa0893cf84c6e1cd0f08acb6b689d346706
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:45:27 +0000

    lonely thinkover avoid bar suit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import random
+print(random.randint(0, 100))
