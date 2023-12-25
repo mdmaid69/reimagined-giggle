1a56c8c25c68e41ddbec12ff5d9e96852dba702a
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:06:10 +0000

    entrance scream emotion system tap

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
