00574f63adbf7128999ad6be30ed13a4c643f568
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:30:45 +0000

    near copy learn compete taste

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_area_rectangle(l, w):
+        return l * w
+def convert_to_binary(n):
+        return bin(n)
