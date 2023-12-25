576f8af5e129eebb9b43870ccf1c140e7feaa207
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:45:17 +0000

    positive piece quit less deaf

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import random
+print(random.randint(0, 100))
