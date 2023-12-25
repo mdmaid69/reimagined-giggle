fdc30614dd5aebe85a3d5436274450c733bc7218
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:16:32 +0000

    refer settle kind interrupt dear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def cube_number(x):
+        return x**3
