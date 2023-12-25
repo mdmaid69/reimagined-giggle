104192420fc5a95229852ac9a6de90915234843e
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:04:16 +0000

    both reduce present almost scrape

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_average(lst):
+        return sum(lst) / len(lst)
