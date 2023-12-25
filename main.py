089c47b1906c884bd671fdd166fe28673fe88d62
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:26:38 +0000

    work pay soil them normal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
