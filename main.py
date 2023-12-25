1e7149a5861c46d3a772c75aebacf4f0fcbb4274
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:49:56 +0000

    welcome majority wake sense put

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def find_unique_words(sentence):
+        return set(sentence.split())
