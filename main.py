c4ccd7d2dd5f6263b34a585af8f6735e56d7368e
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:53:46 +0000

    but raw face wonder opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
