a6f760ee7fc656d6b3bb9f5c26d03e65edf548eb
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:44:40 +0000

    like rare hate over sort

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
