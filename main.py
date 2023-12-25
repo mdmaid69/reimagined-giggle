cbfa5cb693de5f6c6322b6f6c688bee5190c1003
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:39:47 +0000

    stop after wealth high children

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def set_array_item(array, i, item):
+        array[i] = item
