0a4085a337c05a5237e727197b30564158414cad
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:55:47 +0000

    hit base shall gas clever

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+print([x**2 for x in range(10)])
