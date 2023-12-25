56b64528c092d4692033cc231be62d97bf447168
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:24:44 +0000

    569enjoy risk bear as plenty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+def exit_program():
+        sys.exit()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
