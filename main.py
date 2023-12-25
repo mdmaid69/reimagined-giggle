fd09c5c099ffaac534e286a7f6547b5e26f82e43
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:51:36 +0000

    pencil study soft separate order

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
