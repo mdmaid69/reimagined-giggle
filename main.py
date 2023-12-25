18b36fe69835e27c1f9b60c1144a4c4e26e831c1
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:52:01 +0000

    opportunity description later explanation fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
