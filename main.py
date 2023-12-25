497f0c3004ecc27b21489e53a47cbcc9bdde2ca4
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:19:32 +0000

    solve movement thought none married

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import collections
+def create_user_dict():
+        return collections.UserDict()
