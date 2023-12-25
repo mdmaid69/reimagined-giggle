66fc37f27f7fc80b660c35ae42c0a5e42b984bb9
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:38:17 +0000

    journey cope much only whole

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def reverse_list(lst):
+        return lst[::-1]
+import sys
+def exit_program():
+        sys.exit()
