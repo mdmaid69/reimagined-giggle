4581ae5b273c9a2104893b360ad843e93e700641
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:48:03 +0000

    cheek fear weak container offend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import collections
-def create_queue():
-        return collections.deque()
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
