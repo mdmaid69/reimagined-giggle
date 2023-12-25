ba3d047c5b664707c117ee376b2d2d368bf5b6d7
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:07:18 +0000

    less seem mark yourself rob

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
