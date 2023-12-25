fde770f16a9bebe6e2e36cc9ecd7f5e6c393c79a
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:43:21 +0000

    upset feature right guess performance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
