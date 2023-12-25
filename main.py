4aa84dd1196fe5ae195a56f6cdd07525967aa00e
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:46:11 +0000

    gift open lookback(on) heat clothes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
