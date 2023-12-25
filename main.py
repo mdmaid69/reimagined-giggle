ee3b3bb2b625a4b705e34e86040bc1a8c965aee3
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:37:47 +0000

    experiment wave comeround number sometimes

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
