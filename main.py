f272aff107701e3cd5d7f77f73cc6c76570c0a32
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:25:01 +0000

    advanced forever trade delight upwards

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
