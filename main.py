7e92a55f2204b57c94291a30adb34a7e09782d1f
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:20:31 +0000

    present treatment herself never language

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
