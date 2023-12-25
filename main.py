7cc5f7027ea39924a8a912cadd569c334fe6cb00
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:27:30 +0000

    base big earn statement rest

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
