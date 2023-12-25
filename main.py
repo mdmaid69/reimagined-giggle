81e0d7c29f01e3de8459442c6151df22d3bbe2d1
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:17:38 +0000

    stop mind ought mess confidence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+text = "Hello, world!"
+print("Words:", len(text.split()))
