3721ec1345c214b0744796bcdb657c94115acf35
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:11:14 +0000

    guess hide continue centimetre branch

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+print(sum(range(10)))
