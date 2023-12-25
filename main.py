72a3bc62194f27e61fc1a419db053ba860ab15fe
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:04:55 +0000

    usual dead off hide disapprove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
