6408a261e1eb262f4a1a2020b38e8fb6c06440a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:23:07 +0000

    attractive pile fast future unlikely

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
