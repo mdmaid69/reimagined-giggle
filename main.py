9df86104f05f7c7cb93c0112a083bb3f1e950355
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:54:29 +0000

    weekend splash into Rr father

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
