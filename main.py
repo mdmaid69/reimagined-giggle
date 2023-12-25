bcd31453adcd86e105f0bd53635d4065a52eca87
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:11:19 +0000

    supply telephone television holdon fork

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
