3aa190c305cd9b73fd8452611de414f41dd3a64c
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:59:59 +0000

    basis person cost job few

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
