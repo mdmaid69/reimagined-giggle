2dc375a6e090109e1f6a26ca9634756fbc14e765
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:35:27 +0000

    exercise trust equal sale loose

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
 def find_difference(list1, list2):
         return set(list1) - set(list2)
-text = "Hello, world!"
-print("Characters:", len(text))
