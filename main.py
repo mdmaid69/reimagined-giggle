751a0c1bf168d86cb85c5b1ef2ba506d5180ad62
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:30:29 +0000

    support press substance flow art

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
