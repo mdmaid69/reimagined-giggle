be1fda58fb4f776cce251490a220fde882d7b269
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:59:47 +0000

    old classroom Rr score centimetre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def sort_list(lst):
-        return sorted(lst)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def square_number(x):
+        return x**2
