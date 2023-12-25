92c3d2c8711df40a3a209e0566909ada3e86b629
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:31:39 +0000

    off distant print slice approval

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
