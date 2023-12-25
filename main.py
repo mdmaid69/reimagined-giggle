18592380bdba990d89d35236c8079771ef693766
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:38:16 +0000

    matter bringdown over cry accordingto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
