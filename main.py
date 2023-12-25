2a9efb65f9302eb587ca60d70c079549f502b22a
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:07:03 +0000

    spend bit manner news process

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-print(time.time())
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
