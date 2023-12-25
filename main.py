24cb5763b655be50e0afa10488894af2af486967
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:48:01 +0000

    mean again sticky pen consist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
