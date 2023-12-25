2abba37fe6f18c2531fb723e0309bc1d2c5cba16
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:38:04 +0000

    surface piece design accept 569enjoy

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def remove_duplicates(lst):
+        return list(set(lst))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
