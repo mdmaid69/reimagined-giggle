4034d9a4a804fe824c901786c83862e746028e82
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:46:39 +0000

    fortunate crash that professional pattern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
