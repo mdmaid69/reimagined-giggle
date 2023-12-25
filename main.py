e35eb8f2bd0aa3cd8d3eb483574b63a8879efef5
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:23:19 +0000

    nature forget breakout just undo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
