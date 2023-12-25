de0fad760046510b2d1b4f934f5891b55db91a06
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:31:11 +0000

    less contain card come show

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+print([x**2 for x in range(10)])
