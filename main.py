aec03f61fc78b62afed7eba73962e20a5b2f73a2
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:52:01 +0000

    fire keepoff represent consider kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
