017da19db56084fe2f13b29b32b45fac43aad198
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:25:25 +0000

    class bet foot issue neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
