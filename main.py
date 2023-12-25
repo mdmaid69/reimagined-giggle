90085d5b1a801f823ef8a83fe0a9e9f102809dcb
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:45:10 +0000

    along during immediate play Cc

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_speed(distance, time):
-        return distance / time
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
