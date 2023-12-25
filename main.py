7bf4f8646a8bfc019c6c2d0099a2b880566e6189
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:03:34 +0000

    sensible pen habit fight workupto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
