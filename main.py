08f18cc9ae5ea77da3f56fae662708ea7bcce3ce
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:40:45 +0000

    man possibly indeed cut week

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import sys
+print(sys.version)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
