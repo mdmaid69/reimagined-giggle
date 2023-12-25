ac24af13cb311d0cba571dd88d0264df51816ad2
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:23:42 +0000

    sit expect port deliver himself

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  def calculate_perimeter_rectangle(l, w):
+def calculate_perimeter_rectangle(l, w):
         return 2 * (l + w)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
