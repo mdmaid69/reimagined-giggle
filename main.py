950a60e74e91a73cd84897f10b84e5502b5e1c4e
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:23:57 +0000

    march scrape open statement between

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
