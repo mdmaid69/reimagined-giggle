48fb06f6e3d52c5e64e9b4a396922b54d68f03ee
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:37:00 +0000

    several together deliver brush give

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_volume(length, width, height):
-        return length * width * height
+  def split_path(path):
+        return os.path.split(path)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
