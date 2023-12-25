57f0c77a0359901a06cb1b1162e58956e15433ca
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:48:27 +0000

    holiday getoff print accordingto pinch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
