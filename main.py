27c41f52a23c22197dbe3d33a06418237767302f
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:00:18 +0000

    generally correct his we over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def sort_list(lst):
-        return sorted(lst)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_floor(x):
+        return math.floor(x)
