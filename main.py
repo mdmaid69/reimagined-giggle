53cd6817d7538883949fb6f27fc4383a7009fb5b
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:56:41 +0000

    burn silent correct present lower

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import re
+print(re.match("h.*o", "hello world"))
