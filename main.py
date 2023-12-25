2215492694351e5e444658d42891e5f42467766b
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:51:42 +0000

    fold reach weigh whoever quickly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
