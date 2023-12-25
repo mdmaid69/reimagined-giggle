b71e287fded375ff464a82cdd19076c0462a829f
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:51:35 +0000

    answer refer name deserve go

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
 text = "Hello, world!"
 print("Words:", len(text.split()))
