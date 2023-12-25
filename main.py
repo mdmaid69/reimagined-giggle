de74d794f0706794d87b551763150b6340bae864
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:49:34 +0000

    worth strange juice add movement

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def square_number(x):
-        return x**2
+def divide_numbers(x, y):
+        return x / y
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
