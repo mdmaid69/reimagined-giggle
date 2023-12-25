6b4c38363512a8515e958d4174e568389a6731f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:05:19 +0000

    except sensitive high target write

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import itertools
 def flatten(iterable):
         return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_as_bool(array):
+        return bool(array)
