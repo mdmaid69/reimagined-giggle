004088a3a1e1ce68aab728c522a451e398e9fdd5
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:56:01 +0000

    edge perhaps major culture proud

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
