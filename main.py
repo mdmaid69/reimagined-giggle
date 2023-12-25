b31340c95bd63ce38986c72539ed19438a64d50b
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:26:41 +0000

    wild left contrast push dead

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
