d73296f9ba870ab35fcab5becbc4e2880995e6eb
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:13:28 +0000

    best touch wonder sail achievement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
