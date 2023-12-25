90ac60a2b6bb292b7ce72f6bc76df7c4c3681103
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:35:57 +0000

    do need point fix calmly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_min(numbers):
-        return min(numbers)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
