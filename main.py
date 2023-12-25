5196054fcd374214643086ff00050e4da57a131a
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:22:36 +0000

    somehow side jump collect correction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
