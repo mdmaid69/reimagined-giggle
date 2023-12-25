de9fa11a8ec03fb857898a74de215fc06b727205
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:18:26 +0000

    advantage forever disagree escape round

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import sys
+def exit_program():
+        sys.exit()
