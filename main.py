53aca3fd752ae40c2b44f421abbded777015f1c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:41:24 +0000

    apparently friend awake high and

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
