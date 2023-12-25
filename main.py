6a6be3643c449a176adb963508d9307f7b53cdb2
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:56:45 +0000

    pause grow remember tear level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
