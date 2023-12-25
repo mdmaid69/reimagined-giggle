d14f2cf09e5bdd4b93b6f63cfc58b27fe0e00be9
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:16:14 +0000

    back delicate water perhaps dry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_max(lst):
+        return max(lst)
