b3cff64ed73aaa295e00ebfb40a469d690806e63
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:37:16 +0000

    peaceful nature attract position fetch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
