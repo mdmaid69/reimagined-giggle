46c648b51dad82aa11e5120590d5c788fc2b0152
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:18:29 +0000

    because destroy noone complicated train

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
