ac41f1b72184107b1c88a0da6e716b687cc246a4
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:50:29 +0000

    pronounce pronunciation thorough arrangement kill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
