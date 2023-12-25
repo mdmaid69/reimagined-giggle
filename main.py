0c5d98aec32599260b7251f240799ed4ceaefed8
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:03:20 +0000

    stiff backwards wing control interest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+for i in range(5):
+        print(i)
