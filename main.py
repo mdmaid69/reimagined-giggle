8ea3497969925e83b2efc67668e7f3b88319e2a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:44:46 +0000

    operate collect bored calloff correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
