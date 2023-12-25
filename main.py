bb24aee0c98d99c3feaaa0c30021e70c7f3820e5
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:48:19 +0000

    gift anywhere rise doubt able

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
