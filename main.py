ffb8775c90ad36945dfb5557aafc7d59f86fdf5c
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:50:03 +0000

    hurt who neither correct confused

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
