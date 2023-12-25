c4af07cca2615e12321321cb55f09fac62ee9f27
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:43:55 +0000

    movement state surface ours wipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
