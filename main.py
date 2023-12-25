479f48f8676505050f095a7c66edaec74aa17075
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:12:12 +0000

    real move benefit clean thumb

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import array
-def get_array_as_float(array):
-        return float(array[0])
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def find_max(numbers):
+        return max(numbers)
