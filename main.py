5f44de93321749ae4cbee87568d7913998c801fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:48:07 +0000

    support beside hospital impatient 179bite

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_set(array):
+        return set(array)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
