2b52b557e7a7e74513ecc43511dcad24d5edf222
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:14:57 +0000

    education mind 573entertainment fit put

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def calculate_area_rectangle(l, w):
+        return l * w
