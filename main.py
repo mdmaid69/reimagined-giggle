841b2b288d11f65d1f1aedace475fd8ed30e5679
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:11:08 +0000

    street match form butter fix

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def calculate_area_rectangle(l, w):
+        return l * w
