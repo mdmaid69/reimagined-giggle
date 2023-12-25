6d29ca8af33718b221b3c6d2643099c6492b8010
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:49:41 +0000

    suck switch wear imagine determined

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import os
+print(os.getcwd())
