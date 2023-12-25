fedd3f459b0c284c1726b51f10eb996ccf787941
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:45:33 +0000

    choice prevent accept flavour equal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import collections
+def create_priority_queue():
+        return collections.deque()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
