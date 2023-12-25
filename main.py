07de64979b2ea02d4af476b549a9f61c2201a550
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:33:11 +0000

    fetch prepare free something little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
