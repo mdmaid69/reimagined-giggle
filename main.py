4f824a6e8b71450c5e41cf1de3b8cf16fb4b77df
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:26:11 +0000

    circle properly responsible mine there

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   def is_prime(n):
         for i in range(2, int(n**0.5) + 1):
           if n % i == 0:
