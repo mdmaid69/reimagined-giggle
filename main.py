845555f92c871744f5796049e67ab024fc7e08a6
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:04:01 +0000

    brain basic warning improve set

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
