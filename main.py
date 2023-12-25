83260482e9d9fd2fbbf07e9ff54df547fd806693
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:16:36 +0000

    criticize injury since intend public

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def convert_to_hex(n):
+        return hex(n)
