56e61dd306befebb75fecf3767fe75f02e035580
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:22:38 +0000

    mine appearance provide everywhere wake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
