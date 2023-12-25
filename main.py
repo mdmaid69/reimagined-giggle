5937a9d88a966689dabede0de6848a235431035c
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:48:42 +0000

    surprising off arrangement little valuable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
