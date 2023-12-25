c482189d99b3035077223d30fa023ed5aa8c176d
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:00:02 +0000

    pattern always correction suckme position

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
