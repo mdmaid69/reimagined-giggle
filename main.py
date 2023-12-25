2e1f41cb03d03baa4177cef9f1a26e0290f03143
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:20:07 +0000

    squash Yy start 568energy taste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_volume(length, width, height):
+        return length * width * height
+import os
+def get_current_working_directory():
+        return os.getcwd()
