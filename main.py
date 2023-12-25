9edee32657a7bd1019bf3a6dc570e55ce06a5811
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:20:47 +0000

    advantage sort pure circumstance however

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def square_number(x):
-        return x**2
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
