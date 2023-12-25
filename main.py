8995d66c53f9f404e6fbfcd68d6861eb57ea0fd8
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:37:34 +0000

    car book rain tall under

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  def is_even(n):
+        return n % 2 == 0
