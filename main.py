b674b03e7e17111476eab0044776da3f3ddf0b3e
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:58:06 +0000

    giveback system mail air disappointed

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def get_array_as_float(array):
+        return float(array[0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
