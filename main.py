8a2664a8dd07ed87b5264dde5099ec30108f1317
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:01:17 +0000

    relation lookback(on) command empty shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
