9251ab605bed6029932258c8dbec23ac0466f2c2
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:23:55 +0000

    past ear central remember minority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
