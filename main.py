d8ecc3146a115e2c63b652dccffabce50fb53650
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:22:30 +0000

    getoff storm foot support difference

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
