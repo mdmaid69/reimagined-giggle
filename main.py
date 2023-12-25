7cb5dcae9ed7b3a01c0eb645e260ed5fff2a0f9f
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:00:31 +0000

    agree port brown entry shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
