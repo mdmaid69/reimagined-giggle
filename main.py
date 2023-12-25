05f0ed034e2bd6e610010cdefd9a6fc72f591587
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:26:43 +0000

    wave also unfortunate occur cook

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
