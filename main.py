0c97687d93ce45889f6ad9aab803ccdb94c89fa5
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:03:25 +0000

    description anywayanyhow flight wonder unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
