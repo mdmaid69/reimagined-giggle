3a5f03ea2de77cfe589b35e542cd1fa09e6e495a
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:18:01 +0000

    trousers uniform science prepare gradually

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
