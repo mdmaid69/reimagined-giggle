43e3bde306dfcd5496808d70429b36469e45b6f3
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:15:29 +0000

    summer height flat winter beside

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
