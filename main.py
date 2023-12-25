623c1089cdfaab04674ee30206fabf0e60fafff6
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:30:55 +0000

    peaceful remember tip ball workupto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
