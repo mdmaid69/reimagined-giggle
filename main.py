23aee22bda004ced36bc6f4aef6be7a6b4e0cebb
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:30:01 +0000

    wish Gg Pp chat become

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
