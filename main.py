5dfbe5202e40fc9b108027de0421382a5543adda
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:29:31 +0000

    necessary company makeup calm influence

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def sort_list(lst):
+        return sorted(lst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
