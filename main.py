be2ad14fc366b25d93f8c8dbf76ad4598d845fd1
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:00:16 +0000

    apply sensitive freeze card concern

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-for i in range(5):
-        print(i)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
