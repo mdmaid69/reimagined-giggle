7e1e1ba2703f1493170c8922978f383557a4b062
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:48:09 +0000

    dangerous disappointed any scale push

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def reverse_list(lst):
+        return lst[::-1]
