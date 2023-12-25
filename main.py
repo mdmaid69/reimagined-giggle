5fc3fb74e36bfb5ae2b5150db58876f73f6f3f94
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:09:41 +0000

    reasonable cold limit any spill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
