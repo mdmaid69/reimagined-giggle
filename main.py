9e44b4717d7889cb2ba61c249c73b1ae567f362e
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:40:00 +0000

    beautiful goaway mark damage slight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
