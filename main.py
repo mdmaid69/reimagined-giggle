7679e16207d3fda907a3b371c357b837f47fef4f
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:50:49 +0000

    divide people thinkthrough mysterious crisis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
