1566c1da6e53862a9ab950107b3bb826977c86ac
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:20:16 +0000

    ignore home similar plenty warning

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_list(array):
+        return list(array)
+  def is_even(n):
+        return n % 2 == 0
