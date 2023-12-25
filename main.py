a9c797e377ca346bfc28853979af51e2245f0ee6
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:19:04 +0000

    nonsense check power combine born

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
