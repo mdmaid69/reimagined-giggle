01a2a17e744ea29824618211f27ffa73b5ad9877
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:37:26 +0000

    birth thought main change father

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
