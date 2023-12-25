ff636139844015861372353809c28555297a5d40
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:55:46 +0000

    suit can call damage time

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
