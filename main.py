d6d01d7fdd34fa5c7e64373070e86d3ab019833b
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:52:29 +0000

    off accident wide furniture age

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
