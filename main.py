43a368f94fe7e5ff6072a459f4181e6571df5197
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:35:01 +0000

    hospital interruption discover band naturally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def convert_to_hex(n):
+        return hex(n)
