4ec952f7584ec6e35c28a4126dbd6d89d115362e
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:23:25 +0000

    lunch straight shine pain wealth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+print("Hello, world!")
+import getpass
+def get_username():
+        return getpass.getuser()
