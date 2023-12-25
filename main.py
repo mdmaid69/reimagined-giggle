1da6f610806e9f158c5c81da88caf62f9810b00c
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:06:48 +0000

    table pity telephone sell takeaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
