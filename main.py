3714184ef77d023c84120c7c2bbeb8769cd5a5df
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:38:23 +0000

    peaceful key raise circumstance finally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
