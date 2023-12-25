1822c0395083697dd584570f403872b550be8449
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:26:21 +0000

    environment scream only great over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def multiply_numbers(x, y):
-        return x * y
+def convert_to_octal(n):
+        return oct(n)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
