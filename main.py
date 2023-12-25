29799e14170cf1dd094ef902c3dfa28ee8ac6dbb
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:20:02 +0000

    disappointed crash word port correction

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def is_odd(n):
+        return n % 2 != 0
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
