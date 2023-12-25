af492de7b231fe5de218311a3a7e2c8f95eac3d1
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:05:49 +0000

    lunch joke sight pin hit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def clear_array(array):
+        array *= 0
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
