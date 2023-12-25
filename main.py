fc24c23e3587223f4a573ebee5698a3081834845
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:45:44 +0000

    ought pity national completely thankyou

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def append_to_array(array, item):
+        array.append(item)
+  def add_numbers(x, y):
+        return x + y
