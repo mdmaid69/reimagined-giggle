1430652fcb7f7fe1c729b2cb98af5a3ed67b023c
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:53:58 +0000

    long sink somehow pass fasten

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
