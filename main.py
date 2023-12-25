82159fae2ccc8d5465bf69f758e0ef7815a81a72
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:26:58 +0000

    tend hurry and complete familiar

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def count_elements(lst):
+        return len(lst)
