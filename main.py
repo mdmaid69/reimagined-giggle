f7a5bf0a2e3c90dab16e72c453c4d4b792429b65
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:31:26 +0000

    believe strange tense load this

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
