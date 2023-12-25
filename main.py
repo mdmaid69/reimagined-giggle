b2e9f312abb56bca4f37c44dd7f507cee00af5ee
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:53:40 +0000

    seem active high comeup suggest

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
