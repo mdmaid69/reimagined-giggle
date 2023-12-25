147af76e8b3870278fbe1cada2ca3f13c98e41c2
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:21:52 +0000

    pen mood glass hospital achieve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
