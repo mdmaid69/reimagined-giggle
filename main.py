1f1d296ae5db60e8e6c6f5d2c2b389a83bf18cb2
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:52:20 +0000

    protest experience guess detail closed

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
