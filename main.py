5682c4e029ac148f55dd051ba69ed35ea457006c
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:18:43 +0000

    indeed mix steep naturally search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import platform
+def get_os_info():
+        return platform.uname()
