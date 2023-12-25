5a6404ee7806ba0ab5364980b7da5979123ca47d
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:44:58 +0000

    anything calm 179bite display glad

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
   import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
         plt.show()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
