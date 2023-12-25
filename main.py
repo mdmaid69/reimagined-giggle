5ca6969638f353bc23216c677d21030cadc2a439
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:46:13 +0000

    transport open moral usually chance

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
