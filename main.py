2b6a43dd87df74784f827635dd8be5320a55b1e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:39:08 +0000

    by unpleasant still ever nice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def add_numbers(x, y):
-        return x + y
+print(math.pi)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
