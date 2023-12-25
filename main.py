b8a5c0d31aa6b582c296502a41cc5d9d826b7c7f
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:29:10 +0000

    Jj pupil mouth reasonable getoff

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
