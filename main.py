a4cf289928d8f76983a1791090c09ea2809cfb68
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:47:07 +0000

    spill brain holdup prove smile

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def clear_array(array):
-        array *= 0
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
