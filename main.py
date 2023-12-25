8860baee76bfd32d7ac750fd153a17920dcaf63c
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:11:36 +0000

    provide claim argument accidental marry

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
