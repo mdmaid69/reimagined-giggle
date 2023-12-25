d972a43f2eb45d5fc11e49131e07549dba3de392
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:36:06 +0000

    tragedy shoot turn rather answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
