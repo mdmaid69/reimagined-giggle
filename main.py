82f50ddb5a0c2a18f55e98079eb2bbd7ebe134af
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:18:22 +0000

    after much circumstance 565end impossible

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import getpass
+def get_username():
+        return getpass.getuser()
