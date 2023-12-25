f144ebe21ab4bc09ef73c1c5c298b34652529f0a
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:56:27 +0000

    turnup concentrate jump makeinto flood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
