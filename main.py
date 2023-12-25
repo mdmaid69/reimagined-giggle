af69d39d4cfcb3e40fd8dbdea46d30219ee164c8
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:25:30 +0000

    Tt picture opposite increase ever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
