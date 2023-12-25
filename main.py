e79a28c960b3fb6c474816591387b31626bc71ee
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:41:03 +0000

    behind coffee unhappy comfort speak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
