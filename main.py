bb6ef7d5f2d20c7448b790cca988a26050a130a4
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:21:52 +0000

    normal Kk gradually bottom record

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def change_working_directory(path):
+        os.chdir(path)
