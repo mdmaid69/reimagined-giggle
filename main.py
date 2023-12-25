80c37eb8a54119b8a420241d645107e4493e1a9b
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:13:01 +0000

    temperature top lead holiday failure

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def clear_array(array):
-        array *= 0
+for i in range(10): print(i)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
