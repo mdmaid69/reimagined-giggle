45d5b23259ddfe6b056a8be4548112568cab6bac
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:51:44 +0000

    beneath left win until month

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
