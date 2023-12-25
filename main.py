3e5fc90023cb8a98f0c27137508a7f851c567892
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:13:41 +0000

    mind creature write visit confuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_speed(distance, time):
+        return distance / time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
