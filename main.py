8d77dab12ab630a667008fc7b3f7ca07b7c797ae
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:18:55 +0000

    explain game responsibility separate mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+print(sys.version)
