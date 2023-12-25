c2e4120c9aa3326bf4a476e6d358e57afeda14e9
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:57:49 +0000

    person hold clearly nerve wait

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
