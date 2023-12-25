9553f52c8c58dd461bc27f5dc4871e6170b11a26
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:35:48 +0000

    complicated either wheel increase test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
