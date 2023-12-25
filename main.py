38f2bce1123bcedbc140ff9fb366c53ea508dd83
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:19:46 +0000

    word definite sort expression power

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
