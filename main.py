6ace28ae01b1eb62ff0059b32b61d97669758d17
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:51:19 +0000

    speech over could worth instruction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
