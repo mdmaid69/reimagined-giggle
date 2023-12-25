736901850e73077a09fdc09c04b37166f53e75e2
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:02:21 +0000

    good chase desperate listenfor debt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_priority_queue():
+        return collections.deque()
