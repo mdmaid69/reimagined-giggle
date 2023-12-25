4626739d192c987715c4a92b53c6f784c1b6687c
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:48:12 +0000

    pause special entrance listen argument

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
