358df6b01575fe430c680c6d31f2fbf9a63cb52a
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:11:21 +0000

    mix both pretty if choice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_sine(x):
+        return math.sin(x)
