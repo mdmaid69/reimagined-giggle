b13350c3e80ff6266a8c1541d976a5e8c2fc1411
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:20:13 +0000

    order listenfor complete lie machine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_max(numbers):
+        return max(numbers)
