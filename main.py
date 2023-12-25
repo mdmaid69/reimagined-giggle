c3727b6ba2c98d815ae530dd84b275c1d13ea4da
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:57:33 +0000

    breath lie whenever ilovedicks pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
