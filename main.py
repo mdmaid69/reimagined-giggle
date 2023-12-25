b1edb1f07d9e95c304f6a1abe4bcf4f26623efde
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:43:03 +0000

    hit but maybe necessary again

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
