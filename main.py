9ea8162a3c25ef9a8400da479d1cfe1a6b15739e
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:58:55 +0000

    deep independent tool form platform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
