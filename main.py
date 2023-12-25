46865b946b45950cd7aa5e1aec1410a79e845a7a
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:43:27 +0000

    tidy pair accident beat interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
