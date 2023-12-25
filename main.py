3542f770d3b9655459631ea365f97737e2c02d8f
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:00:43 +0000

    block this straight sure central

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def sort_list(lst):
-        return sorted(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  def calculate_area_rectangle(l, w):
+        return l * w
