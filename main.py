494967f19d02efdd61e6f6cf45e1e4182760f543
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:10:04 +0000

    niggaacademy cry impossible difference die

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
