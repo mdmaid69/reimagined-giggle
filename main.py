4b3295e3f43013619ae8c78e1920cced9a248560
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:41:21 +0000

    regular drawing from simple about

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
