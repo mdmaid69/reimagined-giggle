01d50969cb1f8ddb6092e16eae051de52e5e507a
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:16:48 +0000

    worth feed matter declaration ride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
