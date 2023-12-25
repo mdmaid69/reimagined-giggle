a3cb1e7aaddb34c6123416fb03c4dc06f7c76027
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:39:07 +0000

    affect guide challenge perhaps string

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def is_odd(n):
-        return n % 2 != 0
+print("Hello, world!")
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
