672013ab879b3d882528698506b1b485f07d1acf
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:17:12 +0000

    bit voice will 564encourage colour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def get_array_as_set(array):
-        return set(array)
+def convert_array_to_string(array):
+        return array.tostring()
+import tensorflow as tf
+print(tf.__version__)
