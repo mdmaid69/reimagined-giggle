39893172b42245793337391febdb6493e4be2e69
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:34:07 +0000

    charge tired power Cc tap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
