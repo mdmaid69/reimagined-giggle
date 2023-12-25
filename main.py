59395aca3fb78254951ddfb3fd24add464d35b11
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:32:19 +0000

    free subject round advanced guilty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
 def reverse_string(s):
         return s[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
