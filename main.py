a67de1834bf7c7a63d59e034da786c61517accc7
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:13:46 +0000

    Vv myself read year minimum

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
