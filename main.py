7721f72ab1c6b64637cc837ea49cbd53cee402aa
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:43:16 +0000

    injury black stay straight risk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
