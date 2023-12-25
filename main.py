95c448ccbd05153b487e806ad4b9e27ad0492fc1
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:27:27 +0000

    personal noone gift mother circle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
 import math
 def calculate_gamma_function(x):
         return math.gamma(x)
