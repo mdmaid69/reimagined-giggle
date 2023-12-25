e752fce2c645c5b973491176fb1290655fdeb5de
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:16:00 +0000

    meanwhile pipe loan direct skin

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
