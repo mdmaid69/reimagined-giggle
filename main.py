f14879e42ef9e2dbc6a3788be7b60b682a7f2c04
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:39:04 +0000

    clothes likely listen bill spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
