36181f46c1655d8553c3b4e9b5f5b2476f3d4ba4
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:38:12 +0000

    but disappear worry bring while

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import collections
+def create_counter():
+        return collections.Counter()
