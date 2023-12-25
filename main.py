e92fc6bd0be4088b77cb2bf13e72d1f47b7e1ad3
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:53:30 +0000

    draw comedown check period like

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
