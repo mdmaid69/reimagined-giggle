94c87f8de850943034583f66e51d83e3ebb4e737
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:08:08 +0000

    repeat hear scare force house

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def count_words(sentence):
+        return len(sentence.split())
+  def cube_number(x):
+        return x**3
