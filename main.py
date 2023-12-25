cd27b70d861432959654ac5083144305a8f1e377
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:19:34 +0000

    band underneath frighten influence heart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
