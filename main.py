6052d5ca500d7b895a5fa7d66b52b3587cb68271
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:42:21 +0000

    nurse last tie through suck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
