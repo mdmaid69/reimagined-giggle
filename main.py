60d7936e920e2c2b4765ee08d403c6605629a179
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:37:45 +0000

    involve lost feature worth cheek

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  def square_number(x):
+        return x**2
