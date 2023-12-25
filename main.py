fffccf5698ec93c48ad899d88200ad1ba93ae2ae
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:48:41 +0000

    force result finger pretty gas

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_cosine(x):
+        return math.cos(x)
