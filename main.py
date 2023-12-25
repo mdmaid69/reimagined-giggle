01d09087cfe3bb3d2f16d842c0de22debd3df107
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:08:53 +0000

    seriously boat modern various challenge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
