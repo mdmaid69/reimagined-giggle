d9ebe15ddd61627675523194e7d036147c125295
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:56:59 +0000

    cause their upwards moral speak

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import random
-print(random.randint(0, 100))
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
