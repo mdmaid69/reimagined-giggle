5f9f9a1682e25fa05c37b309ed2dc3e280b8134b
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:17:55 +0000

    delicate mass damp wait reply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
