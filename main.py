a73c0a905934ec8b66f41f3514671b8835173c55
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:56:44 +0000

    left born mend right spare

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
