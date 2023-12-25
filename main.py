e0d0167c93d5c7ab5135a9143f6fcd21f93905a9
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:02:18 +0000

    blame tough rob trade unusual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
