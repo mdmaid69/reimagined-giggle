fefce4d3efcb218cdc70936c78a241fd2ac3a83b
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:55:36 +0000

    night breakin forgive breathe fly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
