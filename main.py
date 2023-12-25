e75d8e29d29c59fd19746298cfca73e9d7af872d
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:27:51 +0000

    trust aware point gentle basically

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
