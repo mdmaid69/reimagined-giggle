32f096cd2b05a2b33e600a21bdf791a552d6b0ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:38:59 +0000

    nerve aim habit further offence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
