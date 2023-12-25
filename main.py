94a15e6aed43cf8a514ee3b0ac523530db769c7b
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:15:28 +0000

    degree address pass mind crisis

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def convert_to_binary(n):
+        return bin(n)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
