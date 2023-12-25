3152e826879eb9b8a3a3351daeaf23656df8826a
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:14:09 +0000

    weather behave Ee count Bb

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
