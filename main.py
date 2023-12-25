501372096eb82cd01e08584d86eb9fcfcfc56f7a
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:19:55 +0000

    calm shake centimetre equipment boat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
