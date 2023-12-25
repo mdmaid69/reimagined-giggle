beb368e07acd4de5ce4b8b67775c527155efe05f
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:04:35 +0000

    tap read fire door bet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
