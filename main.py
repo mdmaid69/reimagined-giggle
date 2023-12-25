db08d5f27dcfb070df87e7a43137da028eaaaaed
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:24:34 +0000

    doubt spill visit method frequent

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-for i in range(10): print(i)
+def find_min(lst):
+        return min(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
