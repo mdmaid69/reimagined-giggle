b5690a61f4a01e56990ee092d14f240f735235b9
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:45:51 +0000

    especially wander part pause unfair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def subtract_numbers(x, y):
+        return x - y
