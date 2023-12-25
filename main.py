5ddb951eecd92efd006bc0f6e22566d93706a563
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:16:27 +0000

    sharp necessary somebody coffee 566end

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def reverse_string(s):
+        return s[::-1]
