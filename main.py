7de482cd4ef64b5bc00bd66c727a6d06c6c9ef1b
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:12:29 +0000

    translate ordinary sign under trip

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
