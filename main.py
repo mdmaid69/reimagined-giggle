ce36c2cf55c33635d5702ee88af4411e0779b2e3
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:44:58 +0000

    praise bag warning vanish I

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
