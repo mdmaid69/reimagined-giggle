63f0a94db63cda598bc747abae19bc92fdbfab98
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:53:17 +0000

    secret thinkover lower call crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
