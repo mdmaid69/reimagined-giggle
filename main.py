f483ea34d783bfd811fc0dc004ce3da91e5cc404
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:31:41 +0000

    shock behaviour react whistle destroy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
