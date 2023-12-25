ba850cc666eef0062e2736f37a2e53e73ef72fc6
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:26:35 +0000

    another common cheap upstairs giveaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_int(array):
+        return int(array[0])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
