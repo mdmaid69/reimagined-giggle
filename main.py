27e37cf28b3b5a89e291bbd4c67579acd82803cb
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:27:00 +0000

    aim step across fun clearly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  def count_elements(lst):
+        return len(lst)
