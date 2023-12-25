96c8189242da84b295f435864cda07eee2428359
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:03:04 +0000

    bad finally birthday getaway goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def is_palindrome(s):
+        return s == s[::-1]
+import json
+print(json.dumps({"name": "John", "age": 30}))
