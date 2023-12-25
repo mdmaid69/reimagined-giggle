ab0a672b03a63ee846bb09e38a46ee55af7acf09
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:46:16 +0000

    frighten possession discovery tour temperature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import json
+print(json.dumps({"name": "John", "age": 30}))
