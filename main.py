40f2e80d4cc8552c9af14d29f7d1afc16fba597a
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:29:53 +0000

    design weak air little university

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
