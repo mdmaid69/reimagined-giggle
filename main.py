1677f0e88440dba39d65bd219711986f904cf3e7
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:43:27 +0000

    around middle suddenly begin interfere

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
