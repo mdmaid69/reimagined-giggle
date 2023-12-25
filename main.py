857300a822ebf8a57e6577130ced1a32841175ff
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:05:59 +0000

    different family shut actually reduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
