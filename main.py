3900ae8279bb9f39205dc4b7976981baf7440608
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:54:14 +0000

    several suit consist into run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def is_odd(n):
+        return n % 2 != 0
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
