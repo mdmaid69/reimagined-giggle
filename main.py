9b31efe2ea3c655c5459d4dbfe77447d3339aa8e
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:38:09 +0000

    nice callout message person phone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
