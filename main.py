e118a9a23f0d0b1564894ea20ffcb306cda5e238
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:16:16 +0000

    hand centimetre law workon gift

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def multiply_numbers(x, y):
-        return x * y
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import getpass
+def get_username():
+        return getpass.getuser()
