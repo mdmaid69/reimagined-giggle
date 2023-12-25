e69200eb22e63d85ab78a914245c4e96969a7bbc
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:32:12 +0000

    measure find ever warn able

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
