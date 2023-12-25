f3c13ac76c688d772ffc893b4ac75b2c46092896
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:38:50 +0000

    male hard opposite increase tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import math
+print(math.pi)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
