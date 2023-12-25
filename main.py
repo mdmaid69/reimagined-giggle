60286c2f70f37996979be40c38c6ea71db0905f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:32:57 +0000

    this night information shame search

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
