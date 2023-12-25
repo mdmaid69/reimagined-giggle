eef448c3444e537790ad6913dbecdd4b2927283d
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:14:03 +0000

    blame estimate news field show

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
