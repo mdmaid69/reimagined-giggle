825ed084e3c97de738be53ae03057c015ecc6f07
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:11:19 +0000

    Dd behaviour worry success callat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
