262454f5f3a556b7977567707203e1ea1cfb6408
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:27:55 +0000

    firm female bottom touch turn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_bool(array):
+        return bool(array)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
