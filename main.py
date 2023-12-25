e7e717575f9657a7fcc77d62e6b7c7a1d7b6f675
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:48:24 +0000

    relax through cruelty complete wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
