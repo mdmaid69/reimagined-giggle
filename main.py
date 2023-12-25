4130bb3a97091b69a726f65a492a0a5b5bd88810
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:25:08 +0000

    perform regional might geton wheel

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def split_path(path):
+        return os.path.split(path)
