8f126325b4962b39cf481c790c4b6073960e9e5c
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:25:58 +0000

    anxious skin force tense lend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
