e99fedeb86b98276255e7d7db7c08b1e9464c406
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:29:22 +0000

    dress train now tool getaway

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
