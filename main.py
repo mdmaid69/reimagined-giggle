36002dc0133d8a28d71777a28419fab8ffb3c7b4
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:34:17 +0000

    word surface no polite against

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
