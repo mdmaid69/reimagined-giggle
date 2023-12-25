7b858619f9c8c3d8669e63cf6a9b5290d330cd2f
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:47:25 +0000

    finish wealth Ll play flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def split_path(path):
+        return os.path.split(path)
