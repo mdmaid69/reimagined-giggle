654bee8153498e06ab41ef2fc0e13fe0efc76510
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:27:14 +0000

    place firm separate convinced tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
