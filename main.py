45014580e3381063010a8180d80b479e4ba9165f
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:10:06 +0000

    perform Tt backwards warn natural

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def check_if_array_contains_item(array, item):
+        return item in array
