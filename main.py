5cc999babe96770ea257e2e1be449aa4e9e6517f
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:16:22 +0000

    prepare runover world young consider

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
