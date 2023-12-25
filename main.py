de449d664881b099b67525e95dcaefc5e673d1d5
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:07:29 +0000

    down inquire ask cap into

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  def multiply_numbers(x, y):
-        return x * y
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_work(force, distance):
+        return force * distance
