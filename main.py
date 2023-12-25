25c29e37a8095ae0ea510684436ed372a5f93dd1
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:48:36 +0000

    lucky common radio voice shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_as_bool(array):
+        return bool(array)
