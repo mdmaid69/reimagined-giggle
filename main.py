a1ac6514395f7065e89fede32210e911a98a300d
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:04:37 +0000

    exaggerate deserve suckme squeeze occasional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def find_min(numbers):
+        return min(numbers)
