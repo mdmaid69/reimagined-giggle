5ab901a03d140aed3dc7d76650ef705dd4af07fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:21:41 +0000

    attitude accept holdout bringdown worth

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
