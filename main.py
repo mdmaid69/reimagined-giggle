5ed87c70384c1153ef0be8f3377eed7d18fed4dd
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:00:27 +0000

    label original stick judge bound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import array
+def get_array_index(array, item):
+        return array.index(item)
