22b112cee277e36f42333afd14d7a486bc8f576e
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:54:26 +0000

    moon score moment still discussion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_pressure(force, area):
+        return force / area
