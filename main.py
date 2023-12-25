49a119f75657235317fdb957a605ccb8b1d6d9cb
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:17:01 +0000

    recent computer content distance distant

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def reverse_list(lst):
+        return lst[::-1]
