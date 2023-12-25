158d778fc195a6893ef88d4995d8c07898db707a
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:12:47 +0000

    apologizeapologise telephone school notice search

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def count_characters(sentence):
+        return len(sentence)
+import re
+print(re.match("h.*o", "hello world"))
