29f79efd4e38805041623fe2422784320125fc09
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:39:18 +0000

    both takeout popular stand basic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
