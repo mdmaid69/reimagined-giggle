21babd0e9b7bee3f37a37e570d4de9ebc8a48155
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:21:10 +0000

    trouble by direct false care

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
