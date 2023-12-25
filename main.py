686d18553a49f236e7b2c96fbf7ee3b273640113
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:41:25 +0000

    positive wait machine new since

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
