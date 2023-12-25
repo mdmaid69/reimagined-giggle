b5e46add348081d56915a99f0c45b9b62cb53bae
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:02:34 +0000

    city comment suckme touch label

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_power(work, time):
+        return work / time
+def find_unique_words(sentence):
+        return set(sentence.split())
