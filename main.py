192a885599fe497c7ca85a3f5acd563d3ffed225
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:47:19 +0000

    wander single react runaway future

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def convert_to_binary(n):
-        return bin(n)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
