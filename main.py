85d5d2ae79c6b043ba3f3cd5e1b4aef393a54c2f
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:58:55 +0000

    truth workupto cross hide flow

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import array
-def get_array_from_file(filename, typecode):
+def get_array_from_string(string, typecode):
         a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        a.fromstring(string)
         return a
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
