5c75b6b588fee75acb9e26d7de24d763365ccf31
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:12:26 +0000

    change imagination fat seat laugh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-print(sum(range(10)))
+def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
