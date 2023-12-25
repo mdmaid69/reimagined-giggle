84d8158140c8681015c8060043aa04fd3f203e38
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:44:51 +0000

    pure apparently new ask group

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
