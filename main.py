fa245503fd4dd1bc3eda74aa056b0f8c8d969b72
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:23:11 +0000

    uncomfortable organization lower better upstairs

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-print("Hello, world!")
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
