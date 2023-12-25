988bfbb24ada7f766a8605abbe2130823bc2f556
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:57:22 +0000

    flat 566end pattern last on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+print(os.getcwd())
