4b1c39e0f4abb4db44b75ca30a147a6099d03bb9
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:11:14 +0000

    decide whoever decision aim expression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
