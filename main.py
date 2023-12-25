399f8d9f6f048ff9a514832124a1922494fc79c8
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:22:13 +0000

    wind everything rush spite whole

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import datetime
+print(datetime.datetime.now())
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
