54b0fdee7a5421458426b346c9385ffbb3cea6c0
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:42:14 +0000

    correct unusual pollution marry well

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def sort_list(lst):
+        return sorted(lst)
