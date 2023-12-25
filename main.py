7cf3b3b3633d2410d012cf105a7c5a0fe05126a2
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:40:52 +0000

    regularly basis glass treat remain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def get_array_item(array, i):
+        return array[i]
+def find_max(numbers):
+        return max(numbers)
