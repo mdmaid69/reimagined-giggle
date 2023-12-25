34fe56e75c900b9cb7565f3a416d00956eac8aef
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:52:54 +0000

    detail trousers branch judge press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
