8e4107847f7f4fd86310f97f899707ea5696d318
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:09:10 +0000

    until heart during human street

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
