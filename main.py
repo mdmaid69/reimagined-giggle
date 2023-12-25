1b6149763d93def4d6ee9c2d69ebd0950c58d46b
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:10:02 +0000

    morning pen short offer plan

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def square_number(x):
-        return x**2
+def sort_list(lst):
+        return sorted(lst)
+def convert_to_octal(n):
+        return oct(n)
