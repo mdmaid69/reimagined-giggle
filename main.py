3886fd987907ef2903e2e61a84902d4df0eee34c
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:44:40 +0000

    mean sore fill able off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
 def calculate_radians_to_degrees(radians):
         return math.degrees(radians)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
