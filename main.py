70013b7329d1820eeadee9ac3c0e04bad255bcb7
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:20:29 +0000

    in pleased flow they poor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
