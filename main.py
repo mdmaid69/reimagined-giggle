1f1ae7a21f2712130db586f679c29a7e6444aec7
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:30:16 +0000

    distant natural walk demonstrate colour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sys
+def print_python_version():
+        return sys.version
