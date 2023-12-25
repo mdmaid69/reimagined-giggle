7efd4dc1d07c882710662b69acc7612e21aca160
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:07:08 +0000

    amount will thinkof yet wheel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def remove_duplicates(lst):
+        return list(set(lst))
+import datetime
+def get_today_date():
+        return datetime.date.today()
