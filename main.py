38c805ecc48dc9f5b387af60e25973046116936f
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:39:00 +0000

    need radio human snow sticky

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
