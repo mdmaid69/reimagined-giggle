d625cb5aca8e62fa55956ffe56e16cfb512289ab
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:14:15 +0000

    project answer hair cover generous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  def cube_number(x):
+        return x**3
