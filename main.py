c3d1b20666eaa8d5ac50221bf1fc599ab0ec89f8
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:32:21 +0000

    share dance pump apply manage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import numpy as np
+print(np.array([1, 2, 3]))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
