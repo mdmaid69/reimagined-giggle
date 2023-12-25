5f5a898e497b35ae6327e849db2dfdc5b56a1d25
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:35:11 +0000

    early little collection hurry comment

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
