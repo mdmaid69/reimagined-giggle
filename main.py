cf4a5134fff386ac4becfef3318075d03b047d2b
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:44:53 +0000

    tight modern plan obvious even

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
