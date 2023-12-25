9c65b46f8184668c5aaad4a30187d8a66a731af1
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:23:02 +0000

    inform fit team represent tonight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def get_array_as_bool(array):
+        return bool(array)
