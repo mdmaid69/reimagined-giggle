43f6653d092753299e5bcbce7fb7cb9eaa24a78c
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:10:18 +0000

    Pp punish they principle bus

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def reverse_list(lst):
+        return lst[::-1]
+  def multiply_numbers(x, y):
+        return x * y
