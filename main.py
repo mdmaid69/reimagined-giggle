d7430fdad43400be6684ba536b239fe51ee2a931
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:42:08 +0000

    work narrow strange sometimes everyone

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def count_elements(lst):
+        return len(lst)
