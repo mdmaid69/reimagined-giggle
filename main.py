6672aa1f04c32940fc269b4a72e899c927635f9f
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:27:36 +0000

    detail wheel practice damage name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_array_index(array, item):
+        return array.index(item)
