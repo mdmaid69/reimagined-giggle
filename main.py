1424caa3bec2edd346637d60e3e3340bcad6fced
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:09:38 +0000

    list accordingto holiday point prepare

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
