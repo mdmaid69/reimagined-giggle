559c3c042cef0fa963f886aaee814a1d7b9fc730
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:33:48 +0000

    feel proud quick wind runaway

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
