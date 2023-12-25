09f0886902a2f1e4ab8babcb392d6de54a3556b1
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:47:48 +0000

    teach act your arrange ordinary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def split_path(path):
+        return os.path.split(path)
