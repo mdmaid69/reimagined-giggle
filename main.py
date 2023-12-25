34de2f827e1d79751b288e344b967d1f9d23fa94
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:01:41 +0000

    imagine error feel comeoff movement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
