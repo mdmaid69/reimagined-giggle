4a3602a55ef8550cdf2686124f6f5f2f5cfdd56e
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:29:59 +0000

    comein correct precise hard future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def sort_list(lst):
+        return sorted(lst)
