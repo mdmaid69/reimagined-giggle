e92ad086302de75303318c496027105be6f27cc1
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:55:33 +0000

    replace window sail spread drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sys
+def print_python_version():
+        return sys.version
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
