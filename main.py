3aafdf0729e6f15c6511f81a6b7d892e98bf99b6
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:08:50 +0000

    pause loan negative slight pin

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  def reverse_list(lst):
+        return lst[::-1]
