558a12a81bdbb441692a8cf9f68a5cf9d9890604
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:46:02 +0000

    pronunciation form careful silent hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def sort_list(lst):
+        return sorted(lst)
