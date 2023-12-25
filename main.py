34efd887f50cce865940751a55eb0b817ccd9b39
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:28:30 +0000

    soak dance plain delicate see

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_even(n):
+        return n % 2 == 0
