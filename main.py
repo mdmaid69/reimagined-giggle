802f5d0e007d19a9789f699b3ecfaf109406d574
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:00:33 +0000

    neck strange town eye burn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
