02da7cfb4ebfd85e7f7883d160bc8e73278bfb04
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:39:19 +0000

    worth afterwards realize crash ill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def split_path(path):
+        return os.path.split(path)
