b64ce861b81a1a2e06f9568b5bd9b13634680181
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:41:07 +0000

    live common father calmly off

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
