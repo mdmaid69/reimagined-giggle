32cfabea5114315d8abf245d23fdca8940df51db
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:52:28 +0000

    attempt operate base number sign

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
