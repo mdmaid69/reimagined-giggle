1a3c752a7322cf80851f34a671e12dfc9b26de2b
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:45:00 +0000

    dick pot study turn stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
