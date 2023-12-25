dcfd4285837ce66786881bfa89fc053a2ce191a8
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:05:16 +0000

    least interview service tape search

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def count_elements(lst):
+        return len(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
