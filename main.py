ebd262f0a8e6e9fe394b15c5fb550edf3980a501
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:29:59 +0000

    human answer tidy married responsible

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_bool(array):
+        return bool(array)
