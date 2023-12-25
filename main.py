caf2da94798fddd3a05f88888b3d27e66a35817a
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:58:07 +0000

    general along play taste over

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
