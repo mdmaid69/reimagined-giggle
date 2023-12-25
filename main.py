5adaa40346718a765a8ade9c1a47987da1c47eae
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:03:34 +0000

    dick habit simple careful steep

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def find_unique_words(sentence):
+        return set(sentence.split())
