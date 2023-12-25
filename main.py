b1f134b285ecfd2305b939ae66438873626ab49d
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:09:31 +0000

    all eye impressive nothing Qq

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
