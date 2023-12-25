27ad9764afca96be67f0c21df85bed931403ef61
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:34:34 +0000

    Ll nose recently profit attach

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
