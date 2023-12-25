6e3b1201b9d90c9fc0ec4bffdc0a1aa0c67b8fa4
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:09:03 +0000

    apply bet bet inquire keepaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
