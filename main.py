3dcd691baf0818f8d77cb3b9271ec349f9f29db7
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:29:06 +0000

    safe target pin downstairs space

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+from collections import Counter
+print(Counter("hello world"))
