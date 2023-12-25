599548d1121e6252c9f8c6b23c4ca6586b5ecd49
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:41:59 +0000

    in badly pollution ring become

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_perpetuity(payment, rate):
+        return payment / rate
