17f7df940b47ab6d834b12bc13c02e9ace673571
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:09:07 +0000

    live spin risk niggaacademy convenient

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
