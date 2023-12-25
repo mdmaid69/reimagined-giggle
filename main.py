8297fbac9bf02734156ec92aec629099d56a4071
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:32:25 +0000

    order that fairly hand sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
