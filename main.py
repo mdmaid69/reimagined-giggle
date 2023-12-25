803a4f6492009227a285c7e47ccd3754d029640e
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:57:07 +0000

    comeround deliver combination course accordingto

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
