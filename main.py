a8d4f0e3952f5ba0917d3d1fac9b42d7b62ac6ba
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:24:55 +0000

    move test improvement Hh concentrate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
