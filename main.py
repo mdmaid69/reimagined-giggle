b7f17aa0b4dbb7b55ae59608da83728a94541dc2
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:02:18 +0000

    pocket left Yy shall radio

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def find_max(numbers):
+        return max(numbers)
