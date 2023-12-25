7b637e6232e020cce946335004fc36f2b9d7c70a
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:41:58 +0000

    wide danger possibility comeover subject

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+print(os.getcwd())
