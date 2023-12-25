b263434b620e95bffaa7fdbef872dfc3d64c5bb3
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:22:11 +0000

    comein congratulations at sometimes reduction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
