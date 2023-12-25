5b23ae665b2d058dac3eef65ca72a8d77b0f6e73
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:21:50 +0000

    hear important description exhibition doctor

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_density(mass, volume):
-        return mass / volume
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_speed(distance, time):
+        return distance / time
