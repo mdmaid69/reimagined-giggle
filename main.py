f6eb5495615fbec1fcbdb3bf0bf62e09c3c9d249
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:03:04 +0000

    comeover bringdown heavy noisy rush

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+print(sum(range(10)))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
