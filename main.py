4c5049710f1bf145223ea83ef45f223364dea4ad
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:55:27 +0000

    language out unusual tired right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
