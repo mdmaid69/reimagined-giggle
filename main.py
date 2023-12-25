48b3f4afd1d4986fee7d7233e4346c08a7af4ab5
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:34:51 +0000

    task cry organize exam summer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import time
+def get_current_time():
+        return time.time()
