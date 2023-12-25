05df904ae21d7355c8e35353b11add7592929ce4
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:07:25 +0000

    main disapprove accidentally admit water

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_arc_sine(x):
+        return math.asin(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
