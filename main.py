892772c07e5e78a380ebc377c671093643677516
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:19:40 +0000

    flow attach birthday decision dull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_average(lst):
+        return sum(lst) / len(lst)
