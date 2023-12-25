7e16b1ae8f87ede461fb68e520727bfc3389306a
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:47:22 +0000

    company principle land where interested

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+print(sum(range(10)))
