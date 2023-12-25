829c0a72d4e9da5e070fc7a85fe770eb5b3b95d0
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:42:19 +0000

    free crash also temperature ours

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
