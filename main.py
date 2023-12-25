2cb11b5762f1491b3e307510bd5f807138c326fe
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:02:39 +0000

    slide matter odd bush male

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-name = "Python"
-print("Hello,", name)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
