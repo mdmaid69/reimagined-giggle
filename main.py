7ff17dad26ad3e34c96cdb28fb9f1470be7352f3
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:53:06 +0000

    present curious face risk impossible

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
