a79a21f85347830402cf3b61ca2590357e308b51
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:44:21 +0000

    takeback goback(to) perform perform work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bool(array):
+        return bool(array)
