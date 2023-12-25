d5988e6c0c6556215317f1277e71284336919571
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:33:22 +0000

    rush acceptable basic makeup lot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_work(force, distance):
+        return force * distance
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
