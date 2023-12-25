0d39d624f7d825f3267acf0b5e8912ea09869dac
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:02:38 +0000

    container very few at dear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
