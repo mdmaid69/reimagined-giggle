dbad6a6598d9decb046f4d6165e6e687f289baab
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:57:57 +0000

    light Ff mess sore factory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
