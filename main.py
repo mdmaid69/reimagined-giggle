ca6554bc77a366e47e3dc7807309ef017a6262bb
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:21:32 +0000

    wind opposite develop glass kill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
