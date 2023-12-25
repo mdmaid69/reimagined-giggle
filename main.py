c0799adc0bc63d1a67edda303f3d3c60c1196b69
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:32:55 +0000

    tape fast meanwhile provide organization

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+for i in range(5):
+        print(i)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
