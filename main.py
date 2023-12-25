bdb512e5fa8785a3eb54851d829c75076a77a75b
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:06:56 +0000

    division profit objection what quickly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
