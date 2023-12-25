6958b2c2559e6c12028deeb1309a648355674edf
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:17:10 +0000

    basically which ball drive there

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def add_numbers(a, b):
-        return a + b
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
