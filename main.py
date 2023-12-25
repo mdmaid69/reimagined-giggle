b7df033a7ce595f66e3bb53e6bfa487b576f510b
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:31:19 +0000

    plenty steer interview argument yet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
