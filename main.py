4d60a291dab352926920cc204a8c7cd293e6222f
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:11:46 +0000

    appeal sharp mistake sentence smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_as_str(array):
+        return str(array)
