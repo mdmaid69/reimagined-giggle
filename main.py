7083e7b6c3fbdd9a442d66afdae384b06d68f8c7
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:06:37 +0000

    yesterday quiet bottom genuine little

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import sys
-print(sys.version)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
