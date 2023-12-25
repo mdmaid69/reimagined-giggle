535d03bc25645c1bce2c466544c33019f708476a
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:39:56 +0000

    find win narrow short our

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_item(array, i):
+        return array[i]
