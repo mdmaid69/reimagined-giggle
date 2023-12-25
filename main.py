a55f5b0d952e170080b496e0ecdb6fcda5c3ccb2
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:35:01 +0000

    urgent visit once separate he

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
