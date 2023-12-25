ea956d46de30e8d2a28d033e9fd0eadb97b11d7f
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:02:31 +0000

    hope talk mention occasional to

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def sort_numbers(numbers):
+        return sorted(numbers)
