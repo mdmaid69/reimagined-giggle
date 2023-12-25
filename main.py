5b2150af19cfc842a9a96cf2ed2c1fac375add61
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:36:48 +0000

    project keepoff thorough lay healthy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import json
+print(json.dumps({"name": "John", "age": 30}))
