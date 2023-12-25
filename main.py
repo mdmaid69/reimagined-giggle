16d2414f621c54ef4d441427aa992809afcf88cf
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:46:27 +0000

    plan sink division measure create

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_power(work, time):
+        return work / time
