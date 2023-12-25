5e51e8aea4dfbcdc7a8f6eb12d1e8e4fc345e791
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:46:12 +0000

    delight already sleep feeling finish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-x = 10
-y = 20
-print("Sum:", x + y)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import datetime
+def get_today_date():
+        return datetime.date.today()
