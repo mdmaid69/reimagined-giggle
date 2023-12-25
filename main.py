29e3d682511150ac0fbed7b95099efc461122ba8
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:30:58 +0000

    reduce plenty unfortunate surprising risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
