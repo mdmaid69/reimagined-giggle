7b384e74d7d467da74b9afaea0f2ef64efe25706
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:53:51 +0000

    afford feeling human bottom lookaround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
