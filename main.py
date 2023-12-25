c7a2ecfca8022d4ba0c92a4658ae709e33acb49d
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:00:44 +0000

    exist fall getout mean everyone

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
