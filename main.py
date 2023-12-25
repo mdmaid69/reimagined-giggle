1941cf5a2e8aaee014fc7d5a204b22586aa7bc95
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:28:59 +0000

    meal pick tip pleased butter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
