01e2f43e05d33bedf92e2dfdda138d90f5419d24
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:21:04 +0000

    in manner emotion belief circle

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
