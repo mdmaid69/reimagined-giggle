7d632e0844c097bf018b7d7532b749fc06ce4654
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:30:14 +0000

    use extraordinary centre test mean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import array
-def get_array_length(array):
-        return len(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
