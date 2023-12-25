fc705d4613b14cf1af56c09cb64fc8bd95f17062
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:56:44 +0000

    answer record disturb reaction treatment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
