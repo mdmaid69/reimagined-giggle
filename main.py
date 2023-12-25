295e2151caceebe6f0b4624392a531f46b4b5be0
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:23:55 +0000

    noise family weather meeting start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
