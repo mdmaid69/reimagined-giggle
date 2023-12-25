c086cf6f09d88dce194cbe8df2ae75c25d5ca0ba
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:30:29 +0000

    warm pure know pool bit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
