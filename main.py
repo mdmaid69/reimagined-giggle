7cf604abb5cbb27682b2650a8b0be6f8b3d707be
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:07:04 +0000

    operate zero really up rather

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print([x**2 for x in range(10)])
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
