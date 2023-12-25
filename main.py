afa65b0f3d8c59f9e67a6f39e116ff90417d6890
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:46:03 +0000

    view confident sink behind supply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
