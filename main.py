248d3004662a9fd83fef2d4c90de42b0ed9ffc24
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:19:45 +0000

    read bringforward star play weigh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
