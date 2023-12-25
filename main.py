e4d63bccabe671267de1becd52bc186264109ebf
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:46:51 +0000

    fast squash second memory will

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def find_min(numbers):
+        return min(numbers)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
