3a634d6c100ab58a12fd11994e943cf6a4125017
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:56:30 +0000

    inside convinced put bath walk

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import time
+print(time.time())
