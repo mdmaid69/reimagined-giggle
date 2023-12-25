562952054a685f2c89aa1405ece2d6e0117655d0
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:48:32 +0000

    comment Rr remove down turnoff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_min(lst):
+        return min(lst)
