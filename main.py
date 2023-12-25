abcc3f7309c252fc2e3b2a91b1f32449702c1ff5
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:15:44 +0000

    sort score travel rule leg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
