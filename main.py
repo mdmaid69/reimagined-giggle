2178e77342311a0d28aa1ddb260b370e1157f404
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:45:22 +0000

    rest apparently order nearly among

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def is_palindrome(s):
-        return s == s[::-1]
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_area_circle(r):
+        return 3.14 * r**2
