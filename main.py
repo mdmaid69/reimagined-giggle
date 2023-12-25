9ff8bc24278f4fbb0f5ecd618cc0386b4f610566
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:21:37 +0000

    news risk lead news over

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
