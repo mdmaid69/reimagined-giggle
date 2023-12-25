6331db568fda3b298edc098c4c7b136a95212895
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:42:29 +0000

    between emergency scrap healthy size

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
