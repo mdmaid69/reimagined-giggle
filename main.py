157df68cd5a96682813f0e474112bd698f2831d6
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:48:05 +0000

    far free final mainly command

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
