82932cc248a474dffd9b20ab82dc16dcc1abf532
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:42:44 +0000

    especially hard exist sore 566end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_current_directory():
+        return os.getcwd()
