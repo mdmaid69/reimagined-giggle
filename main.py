43bcc8a8d5b999a81d1667b338b7dcc0f5d08611
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:17:19 +0000

    after excuse wire scrap pile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  def is_even(n):
+        return n % 2 == 0
