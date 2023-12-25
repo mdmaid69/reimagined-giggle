08e54b45c7169734862efd989dd8d42a8f5992d6
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:28:50 +0000

    several protest cum willing thorough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_floor(x):
+        return math.floor(x)
