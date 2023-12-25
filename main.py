e815366029522e7610fc475f9924684ac2045c5a
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:44:05 +0000

    so juice water tongue wise

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
