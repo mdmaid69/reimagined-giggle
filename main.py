d014b837201fc1bc273a48d7eb9d6ab030d44b5d
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:27:10 +0000

    find sorry Vv anywayanyhow trap

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
