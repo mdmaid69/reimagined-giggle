0016bcbfc613abcf53a012452e14efbd9d1857f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:42:23 +0000

    base science thinkof raise walk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+name = "Python"
+print("Hello,", name)
