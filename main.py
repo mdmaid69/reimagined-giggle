bc572a22ec5f0bd0450d6aa266f51ec04354ecdc
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:34:46 +0000

    another tradition roof anxious while

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
