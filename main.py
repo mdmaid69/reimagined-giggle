98c98964e88399c6c8b5a547c60237928e08de19
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:44:22 +0000

    callup scale popular discover odd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
