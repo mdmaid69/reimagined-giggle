18b9bb2567ed81d65fdbc704c892bcfaea1f2271
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:47:28 +0000

    tonight trap cruel skill hide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
