4998657dab90c5a82eecee5cde1792b95db08f60
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:47:07 +0000

    old lost getout introduction drop

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def reverse_list(lst):
+        return lst[::-1]
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
