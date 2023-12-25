dc790e073d6d4df414a7758f2a0c5deae0795507
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:38:25 +0000

    Rr door strike blow combination

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
 import heapq
 def push_to_heap(heap, item):
         heapq.heappush(heap, item)
+def calculate_speed(distance, time):
+        return distance / time
