7402670810244dfa3c9f211ec99bb78bf276eba1
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:28:36 +0000

    task arrive sky schedule insist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
