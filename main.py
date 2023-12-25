dbed6b942bd4ad7ac702e6607854a17085a6bf10
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:22:16 +0000

    under whoever wide pinch listenfor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
