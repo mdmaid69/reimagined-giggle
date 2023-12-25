ea816360a9edd69248f22e7ef22ea91c65498f95
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:12:09 +0000

    wear proud firm connected necessary

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
