591d774d6574b4f83cffaf4837c90e90b7e09d57
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:45:55 +0000

    trouble accept only production congratulations

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
