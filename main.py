4a71ae288f575eeeaada8942c8aba2e7b58f57b5
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:13:10 +0000

    rest somehow clever breakout born

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
