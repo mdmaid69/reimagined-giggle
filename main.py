55a417a63d911867b87ea03ab83826038fe09a12
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:23:49 +0000

    type characteristic deal country gather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
