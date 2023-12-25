c9d89465dd82d7350e567ce9c54439bab800430a
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:21:56 +0000

    imagine close woman listenfor search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
