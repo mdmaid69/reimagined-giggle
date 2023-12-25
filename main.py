0f9aa170d8e37d86b4c662ec9a13d1aa13566b6e
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:10:28 +0000

    scrap hate spare temper drive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_volume(length, width, height):
+        return length * width * height
