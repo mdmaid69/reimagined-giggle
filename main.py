0a1269163ac9dba56eaec7de1996c28202e52d80
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:47:55 +0000

    complaint noise ride precise compare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
