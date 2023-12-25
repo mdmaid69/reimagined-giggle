397b4f24e8c087bc402799fcc252928dbd518974
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:48:40 +0000

    shall come may wind cough

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
