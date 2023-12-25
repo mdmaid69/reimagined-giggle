df638a89c95586bf6ef672dad76376460ba6c2c5
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:34:43 +0000

    pronounce quit increase majority whenever

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
