4ca96834dd8516530cf3cd81563e5e7e34abd92d
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:33:20 +0000

    open fun hello different reject

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
