7234d001c2d076c57413c0cb452e618034a5d33a
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:10:49 +0000

    appearance runout whistle probably demand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
