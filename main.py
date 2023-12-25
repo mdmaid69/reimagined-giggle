9d9d24ffc913a0ff34221fce1e1d73afb3ec541b
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:22:39 +0000

    please plan pronunciation present bit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
