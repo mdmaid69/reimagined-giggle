76926328252229dac8ad3b1964c831ffa693f9bc
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:27:47 +0000

    operate cap workout tape spray

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def multiply_numbers(x, y):
+        return x * y
