9aaf801ccfe5a9dd138a19d2869cba3e9d40c438
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:55:26 +0000

    piece much description holdout draw

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+print(sum(range(10)))
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
