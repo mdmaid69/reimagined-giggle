3cc6054106ab8f1dbc5b03694bf759778f34f907
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:14:50 +0000

    talkback OKorokay mess left waste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
