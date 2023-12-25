a1e09af309aee78370be01b8b8d41bba1b0bf2b0
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:28:31 +0000

    grand row order pay present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_work(force, distance):
+        return force * distance
