ab8faaab48e11a54548b780f08b49d8ca7fdab1e
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:49:50 +0000

    much sister information gooff learn

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
