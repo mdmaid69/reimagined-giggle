2d3bd3e37cc3822b79148b6a35f3f3490b52b4e1
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:53:27 +0000

    hollow scale back nose contain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
