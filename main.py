33b97db39d78078a7e6fcf7997937dd6374de344
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:45:23 +0000

    oil fall container statement finish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
