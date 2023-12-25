12c7ff70754190337c04b38472510f74df754529
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:33:14 +0000

    try mend finish badly blame

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def reverse_string(s):
+        return s[::-1]
