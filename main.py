b43e4721bf799753d2801b561ce9d17cc4774780
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:00:30 +0000

    turnback war correct note exaggerate

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_density(mass, volume):
-        return mass / volume
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
