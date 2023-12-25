5ab487c73bffce8b2bdfb6e288ea7b032187d9fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:55:14 +0000

    sink class pattern thorough body

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import getpass
+def get_username():
+        return getpass.getuser()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
