cf89b49102cf223db39636ac2e2b5b8a335cbc5f
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:22:27 +0000

    action left already add claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
