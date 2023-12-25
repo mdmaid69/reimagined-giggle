2910cd38ef37e1dadca0be21e6fa1b1ee82bd93d
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:43:59 +0000

    project difficulty careful aim love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_item_count(array, item):
+        return array.count(item)
