79a673e2824a54f42790826d5e41113949cfd5bb
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:56:16 +0000

    sign Jj waste smell into

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
