492d597c4838855148e1c65bdf56073626081da0
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:13:56 +0000

    our combine spring sensitive band

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
