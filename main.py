d0a7e21bd410254b7793819f6d176442a9052d2e
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:45:57 +0000

    act shopping service brown sit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
