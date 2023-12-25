5cf2bac1bcad3da7960b0379c79b772b98048628
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:13:54 +0000

    behaviour preparation bit play arrival

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
