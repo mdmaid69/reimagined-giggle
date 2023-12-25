0990fbfe893058bc6b6ba8f3777c2b3cea598f8a
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:28:33 +0000

    strike something escape count drawing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
