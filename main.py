ec6d3e04762782102180fcb91af447bb6e0ee03d
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:35:35 +0000

    gooff runinto relation do minor

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
