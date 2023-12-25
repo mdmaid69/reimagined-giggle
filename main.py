752dfc632e9d9ed8a1d1a5fac1141241b1d56380
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:27:21 +0000

    straight connection must complete major

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
