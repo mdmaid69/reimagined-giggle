9899a39f02a76be2ee1eec8aba21648156c1ee1f
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:31:51 +0000

    friendly good again return table

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_speed(distance, time):
+        return distance / time
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
