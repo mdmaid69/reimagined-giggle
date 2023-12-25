4ab71f64409b5e4a2851de0582ab8c7e41f78bf8
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:48:27 +0000

    pleasure congratulations software tool announce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def reverse_array(array):
+        array.reverse()
