6d61e080692ba820ed6b8027db7589c7e402b1f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:47:39 +0000

    safety shock method pen course

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
