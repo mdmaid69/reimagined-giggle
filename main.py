fcfb51565af6562afcf09f3f87812fa19a649043
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:08:35 +0000

    Ee heart temper scared claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
