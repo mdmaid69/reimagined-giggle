08fb5ee47d9139439b31ee91e31fbebd75319029
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:29:10 +0000

    water free interview receive below

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+print(sum(range(10)))
+import datetime
+def get_today_date():
+        return datetime.date.today()
