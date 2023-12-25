460a26fbd64df133cc020bde1f6af4370affb2d6
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:36:03 +0000

    strange hold Tt story private

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def convert_to_octal(n):
-        return oct(n)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
