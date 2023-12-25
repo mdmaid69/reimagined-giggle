5116ec30474eca5061aac5612be9b9390e584824
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:41:35 +0000

    sound talk genuine familiar fond

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def convert_to_hex(n):
-        return hex(n)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
