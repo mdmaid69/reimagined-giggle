b22c094c6c0b4871b7fec629b36088285a1a8ee2
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:50:12 +0000

    match behaviour Mm pocket afterwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-name = "Python"
-print("Hello,", name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
