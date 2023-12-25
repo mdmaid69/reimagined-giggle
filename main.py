98c10ce922217ca356ff4f278a623e60bb08522f
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:17:09 +0000

    giveup we nature responsible this

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
