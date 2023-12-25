cba9f04be1f37ac322b07dcc8d5c2f28b02ae357
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:37:52 +0000

    Gg damp muscle shame fact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
