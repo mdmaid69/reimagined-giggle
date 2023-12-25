1c4ba81ebe99c204f92da1c909710e6591e08247
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:01:45 +0000

    union cum yourself concern television

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
