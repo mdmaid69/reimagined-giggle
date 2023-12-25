0ded6bf3548b4a83c87496bb1565f8d4e73282d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:15:58 +0000

    exhibition mad until extra information

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
