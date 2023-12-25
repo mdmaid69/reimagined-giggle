67b57d37168980fe635f0a667f5bf8f32c147af0
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:23:15 +0000

    573entertainment carry Ff world square

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
