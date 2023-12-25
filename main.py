8ee44b0fbd23720867c155f17985072f3f2fba93
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:06:48 +0000

    visit food announce ground blind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
