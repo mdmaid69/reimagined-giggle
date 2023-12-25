9618a998a5c57c0f108fee19b527da2fb50c6797
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:28:39 +0000

    instruction meal advise past break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
