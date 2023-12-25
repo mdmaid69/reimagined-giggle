3d584ec1a0042a7e139df52fe9fc2b68891533a4
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:35:55 +0000

    crime prefer without one underneath

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
