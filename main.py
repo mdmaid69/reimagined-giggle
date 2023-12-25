68499352bec3a502e0f48681e65e9e03c9b2f072
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:48:32 +0000

    often just show blame corner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def remove_duplicates(lst):
+        return list(set(lst))
