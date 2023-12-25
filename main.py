f80b2bcf0966f06dff4628385ab6eb28c446498c
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:46:58 +0000

    comeup date tap understand cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_pressure(force, area):
+        return force / area
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
