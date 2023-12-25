eed90dacd24c7f46153ffebf38dea95de00f2a27
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:25:27 +0000

    fault holiday into final university

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import getpass
+def get_username():
+        return getpass.getuser()
