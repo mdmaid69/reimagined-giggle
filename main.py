d42eb46bcfc730c43d534bfbb5df70f674cb7ac4
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:46:13 +0000

    rich some in lonely squash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
