b84241c888c9fdd28b5b45abd8a5a6f2391f1b30
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:46:21 +0000

    little judge happy jump especially

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_max(lst):
+        return max(lst)
+import array
+def get_list_from_array(array):
+        return array.tolist()
