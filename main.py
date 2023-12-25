cf6009ec1258beb69ef58a3b29cb89d4f7197760
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:55:47 +0000

    lost back takeout offence improvement

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import random
+print(random.randint(0, 100))
