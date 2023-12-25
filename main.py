bddae16cacd192933015193088ac9e55bea3b6c6
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:00:25 +0000

    Rr sad against turninto relation

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
