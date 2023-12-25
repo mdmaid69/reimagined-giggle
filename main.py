2fb4e56634f3eb77da6eea8ecc37af96b660806f
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:24:29 +0000

    imagine too claim tired poor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_power(work, time):
-        return work / time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
