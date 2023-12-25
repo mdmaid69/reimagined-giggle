fc9103c07e759d603445590af78f43b469ae5b4f
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:18:31 +0000

    clean letter goodbye squash branch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+text = "Hello, world!"
+print("Words:", len(text.split()))
