4512fc94025f01ff18238eddc64595db6fb2a058
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:11:30 +0000

    definite dear meanwhile fast glad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
