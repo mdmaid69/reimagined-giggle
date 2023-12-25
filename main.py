41e5e21e48074a425d78d5aa478a2834d856e907
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:56:55 +0000

    space happy trap tune frame

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def divide_numbers(x, y):
-        return x / y
-def find_min(lst):
-        return min(lst)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
