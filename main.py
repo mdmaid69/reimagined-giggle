96768b394750e44ca049bba9b2325f0b444736f8
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:42:26 +0000

    with opponent trial risk anxious

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
