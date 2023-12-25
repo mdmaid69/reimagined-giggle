65a62986910ddb9471c2dd2321bfe87d44d6ddcc
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:35:18 +0000

    city foreign smile actual protect

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
