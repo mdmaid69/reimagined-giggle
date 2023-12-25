b338a439e757f53296b05c4ca63f9ea9da68c4f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:31:06 +0000

    discover Yy callat brown reaction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def square_number(x):
+        return x**2
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
