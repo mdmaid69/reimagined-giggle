fb52a6668bcbb60af7179292f828699dd478a1f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:55:22 +0000

    successful treat collection disturbing shock

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import collections
-def create_user_string():
-        return collections.UserString()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
