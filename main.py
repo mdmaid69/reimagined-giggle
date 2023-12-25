c986ef7d6726cb1fc67277986efe21c601df6328
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:07:31 +0000

    offer yours turnout before spill

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
