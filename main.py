0752856a0d20a797aee84f8580d2ab2f3223e567
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:55:42 +0000

    crash race heart spring good

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
