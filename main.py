cde18ee45a2a36b0d3e9d254662f395e3f1ee44e
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:49:09 +0000

    remark handle move mouth declaration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_work(force, distance):
+        return force * distance
