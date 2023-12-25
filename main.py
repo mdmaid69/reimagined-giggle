483875b658a6dd9837b3ff9c742deaba4470efb0
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:42:06 +0000

    rescue surprising cause well hang

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ def load_csv(filename):
         with open(filename, "r") as f:
         reader = csv.reader(f)
         return list(reader)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
