c39330bc2d869e06f44430fde6a15d5e5fcf37d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:21:27 +0000

    date praise necessary behind borrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
