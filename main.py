1bddcc1606303c9e943721df697e14a452d58535
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:44:49 +0000

    advantage explain can frightened workupto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
