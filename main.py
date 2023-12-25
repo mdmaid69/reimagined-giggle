ab3dfd01232ea5f50d71c1e757c694ef9c99f3ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:11:39 +0000

    able there strike scrap high

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
