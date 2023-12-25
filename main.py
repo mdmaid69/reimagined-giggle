a97fb870929b7550d2f4e2daaa770fd0bd1868ef
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:54:10 +0000

    wake control left hardly stick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
