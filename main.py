2e80984d9d8fe23e3e4ff6d4d6d2ea726bd959d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:14:39 +0000

    lot accept weekend behaviour conversation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def add_numbers(x, y):
-        return x + y
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
