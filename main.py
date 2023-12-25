4b68938abb6ccb53c9aed810fb86abccaca356be
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:50:14 +0000

    pool wash splash hand off

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_as_list(array):
-        return list(array)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
