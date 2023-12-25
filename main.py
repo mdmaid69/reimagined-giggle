a0d622bacdcd50605976c6d2ebb93d958cebfef6
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:48:40 +0000

    breakthrough programme rise note train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import json
+print(json.dumps({"name": "John", "age": 30}))
