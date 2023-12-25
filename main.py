aa7bc44623d8e8cf281dabc5d2b5e93ae776e572
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:16:49 +0000

    workout comein comein admit production

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
