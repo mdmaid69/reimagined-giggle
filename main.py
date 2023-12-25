43b9f52d4b50f6a3107143e652f1846ce02fc64b
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:08:55 +0000

    their prevent decide acceptable tour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_list(array):
+        return list(array)
