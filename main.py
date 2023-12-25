da27cc419a316b10dbf381ca827fc4dab3af7a89
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:05:39 +0000

    minority comeup measure deal surprising

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def convert_to_octal(n):
+        return oct(n)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
