dac5723212d034231ed994f99e70e69e3646770a
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:49:03 +0000

    protest branch push keepfrom present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
