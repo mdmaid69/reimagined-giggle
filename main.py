465efabeefa9677707316fdb0ee78e87f5260776
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:50:21 +0000

    bag wheel sad rush crowded

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
