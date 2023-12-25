65e436931acc205aa597043a4207fdef101f66c1
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:07:25 +0000

    voice lesson more everyone think

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def get_array_typecode(array):
+        return array.typecode
