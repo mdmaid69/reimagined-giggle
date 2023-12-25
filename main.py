eaacd26afa8cedc2737c9c7dbf0bf1d3c3bb0ae7
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:57:38 +0000

    exchange calloff ahead along organization

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
