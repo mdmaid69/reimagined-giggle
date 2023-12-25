db972f51ffb922a21a4547c87de58d87dcf17279
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:00:09 +0000

    approval advice difference considering share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
