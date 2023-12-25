f86615fe0c27183bfbe868b274e487ab4134875b
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:20:24 +0000

    home ground stone service in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
