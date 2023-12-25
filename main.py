0542796ea945ccc0747bbd35ca915f063310611f
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:51:31 +0000

    display treat whether favourite scream

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
