f4c36ad874696dc4e0fcfe3852e604f86f1bdc38
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:43:23 +0000

    fault runout moment freeze heat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
