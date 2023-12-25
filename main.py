ad31447f9be0f9d0e97c3ce11beb43b87b78bb8a
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:54:24 +0000

    live turnaway load suddenly cross

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
