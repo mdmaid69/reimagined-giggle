36dcfcede523605da017c1af71ddbb63ca09a1e6
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:22:49 +0000

    dish exit Gg butter copy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_pressure(force, area):
+        return force / area
