7078386d0a73df97a84514c6e79431133ae1a09b
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:17:38 +0000

    behind possibility frightened disappear basis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  def convert_to_binary(n):
+        return bin(n)
