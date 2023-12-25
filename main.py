7f377eee875cad424b3f0d7e9e3e98fa0aae05c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:15:02 +0000

    plate disturbing patience perfect cheque

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+name = "Python"
+print("Hello,", name)
