effa89b25bb44b564038ce804ef4b4954cc66d53
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:10:19 +0000

    know expect worst nigga disapprove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import tensorflow as tf
+print(tf.__version__)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
