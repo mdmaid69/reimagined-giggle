b6a6411ce91144289a1195fe5d54f22277dab73f
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:40:15 +0000

    demand inform escape record system

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import array
+def clear_array(array):
+        array *= 0
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
