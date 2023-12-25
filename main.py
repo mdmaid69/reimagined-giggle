2dbc1657287a1aae135a566a9ffbe60e9c287b21
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:41:53 +0000

    nervous ride object this godown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
