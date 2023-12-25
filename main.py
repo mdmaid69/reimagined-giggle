523d9afe8876c9a7e6f4f94b7adb483f5af83faf
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:56:17 +0000

    responsible bringback forward band intend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
