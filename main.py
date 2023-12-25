a03cb249f768c2fc53e824bc2d15b46cc6f18ed9
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:26:43 +0000

    complicated leave reply keepoff draw

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def convert_to_binary(n):
+        return bin(n)
