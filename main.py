c8cb8b26731be41aaea5907e645ae353b5f4e5f4
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:20:33 +0000

    schedule total mend firm window

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_group(file_name):
+  def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
