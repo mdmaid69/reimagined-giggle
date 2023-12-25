6af258ac809a267a97c6698d09f98f48378b1bd3
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:02:05 +0000

    instrument short rather chin unable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+for i in range(5):
+        print(i)
