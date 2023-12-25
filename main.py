a5aaf76cd65d23fb28b152d55172b53496a605f0
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:48:39 +0000

    trip free state hear want

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
