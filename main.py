a6123f17802edb5fd5d18ea974862b1c6f8ba17f
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:11:18 +0000

    anybody minimum walk smile wheel

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-text = "Hello, world!"
-print("Characters:", len(text))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
