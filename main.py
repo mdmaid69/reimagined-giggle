7a928d715c6668b6569c783f7473d41b99244974
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:27:37 +0000

    may record walk opponent arrive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
