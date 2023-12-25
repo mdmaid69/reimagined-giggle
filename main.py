73aef879ce3e96c4ebad24b01e7fd20d0b452a4e
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:07:37 +0000

    cut modern cry stay slide

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
