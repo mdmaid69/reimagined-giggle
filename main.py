174e5afbe357a7780ac9652cff83134a1980936e
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:59:49 +0000

    means ocean group attempt protect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
