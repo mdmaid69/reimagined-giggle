09f9fac117f32a7e6c28c39ef380b6a0fc2f922e
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:04:12 +0000

    minute clue charge somebody go

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
