10a6c394e3c639492d6f158fc23909d346f74f18
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:49:46 +0000

    rest trap nervous false neither

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+name = "Python"
+print("Hello,", name)
