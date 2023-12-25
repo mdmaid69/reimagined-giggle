ab8033cb79d1415cf97de3180e80148a250989fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:04:45 +0000

    wing disgusting fail already pot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
