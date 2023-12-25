1ae3ce5d128a05994016dd3581a7d72a389e3cb3
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:49:16 +0000

    welcome apart brother talkback listen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
