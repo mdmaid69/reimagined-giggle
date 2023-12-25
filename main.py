d604eef1139b8c044ecb177ec408fa873fbcc302
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:09:25 +0000

    study let after collection pencil

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
