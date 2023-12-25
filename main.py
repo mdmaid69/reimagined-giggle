8333c070e87a79b554364ae2e0cc66eab7dac938
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:30:25 +0000

    mend uncertain defeat again tough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
