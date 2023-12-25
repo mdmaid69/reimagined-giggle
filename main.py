75627679a7012686d07420ed0ae1b251b1201dfd
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:12:35 +0000

    certain its disapprove realize shall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
