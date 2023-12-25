e7e584219895d21ff08fc1420c6e6f45f4cc11d8
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:03:15 +0000

    admire persuade organize advanced nerve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
