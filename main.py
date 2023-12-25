8b349e77c2ec043c557de1b76f9d56381a7fd571
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:18:04 +0000

    guide regard street noisy trade

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_power(work, time):
-        return work / time
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
