d56e3a628a28dca0b13885a79cb1141ce0fed63a
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:06:37 +0000

    could wide patience account ocean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
