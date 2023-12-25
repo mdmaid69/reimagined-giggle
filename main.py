a4d07837709a43c4647b787cc011773c9c057314
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:51:46 +0000

    destroy destroy church trip battery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
