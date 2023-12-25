cb66e7442f79551acba7cce3a0a02ba66f94fffc
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:27:04 +0000

    rid approval suit receive boat

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def is_odd(n):
-        return n % 2 != 0
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_speed(distance, time):
+        return distance / time
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
