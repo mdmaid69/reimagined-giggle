2468f9f06c689d991a4c6999e27bfa0f201f5e0b
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:48:40 +0000

    succeed trip exchange tooth chase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
