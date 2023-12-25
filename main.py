0e6311c7907f7e202199e3286207adfb410a650a
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:53:11 +0000

    bet pretend goodbye song weekend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
