b931e5db6f4465315e71326586405f6cb9a5f442
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:20:10 +0000

    pencil hers duty 565end road

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
