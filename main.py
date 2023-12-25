15e31487532057bb7838d7bc030b754743add22a
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:56:35 +0000

    temper profit operate neither succeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
