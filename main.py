f75ff85801a5bb5f6dae372b3e89ad76ac7ddb86
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:04:18 +0000

    fairly no natural shame distant

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def square_number(x):
-        return x**2
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
