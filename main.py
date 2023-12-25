60c4b94086c16e5657cb8657842d70c4bb7f7d9e
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:44:36 +0000

    doubt cry wash open father

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import sys
-print(sys.version)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
