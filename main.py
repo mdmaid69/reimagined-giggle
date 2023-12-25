fc395b02d0e0d3846d80e69b693682ee03d643df
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:31:17 +0000

    address level bath blame door

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def remove_duplicates(lst):
+        return list(set(lst))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
