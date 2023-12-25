767daa827088791dc1fb7c2471698f7f48cda9c8
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:39:15 +0000

    letter tidy science complain serve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
