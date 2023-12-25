9e87a5307f6ec7fc7d81730c55f024f2d6e02570
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:33:57 +0000

    distant so prevent hair event

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
