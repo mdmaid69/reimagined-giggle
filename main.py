3745c7166e44cf3b92033744293b5f54f359c969
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:57:27 +0000

    along thirsty squeeze absolutely emphasize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_work(force, distance):
+        return force * distance
