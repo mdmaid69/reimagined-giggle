220936a417606c208a33285c5bf7091dbf045dbe
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:21:46 +0000

    record fight concern broken smell

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
