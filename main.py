551e018146caf82c818b7688cd4491056a29b4c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:04:58 +0000

    search comeout violent turn address

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
 import itertools
 def get_combinations(iterable, r):
         return list(itertools.combinations(iterable, r))
