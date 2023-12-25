f31720f329be0d3102bc154131d5486da060c72d
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:45:26 +0000

    intelligent separate process satisfactory love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
