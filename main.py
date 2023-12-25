d88fa58f93315069a6edcd550830fab89e086d21
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:43:39 +0000

    chase gradually normal negative stretch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
