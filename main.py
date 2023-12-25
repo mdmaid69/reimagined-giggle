1ea361d11aaa552dbde0208aa779382422f9be05
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:10:02 +0000

    afternoon apologizeapologise sort approve stamp

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
