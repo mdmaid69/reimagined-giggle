bdfb4f467953a1357fcb0eeded07e070123603d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:09:08 +0000

    legal tend impression pollution shrink

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
