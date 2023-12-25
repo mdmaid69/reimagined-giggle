72ce10bb19feb596aaad0e20a9a393347bbf8f4d
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:41:12 +0000

    attempt keep different might suggestion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
