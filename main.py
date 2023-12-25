4f7cb61e98a23e60b54b39aa6d7719148154bda4
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:03:23 +0000

    listenfor precise fight smile platform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
