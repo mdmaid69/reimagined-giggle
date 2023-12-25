177d019a2d442a5b2ad98ca28ca6b4a8d7013395
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:14:53 +0000

    judge forget on also hospital

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
