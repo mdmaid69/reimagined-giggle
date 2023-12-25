507f91eaa301663b6d3a09bf107ed202893f27e3
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:28:53 +0000

    soak rate war bound wheel

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
