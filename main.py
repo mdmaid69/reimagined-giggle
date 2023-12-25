afa16ac272451f328996ae90e5b837e893327c8f
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:57:18 +0000

    friend slow flight do music

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
