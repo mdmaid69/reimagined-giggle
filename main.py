583574e2213ca350fd49a196f1eae30c50b72f74
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:41:28 +0000

    heavy fly exploration breakout conversation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def subtract_numbers(x, y):
+        return x - y
