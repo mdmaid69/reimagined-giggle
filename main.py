304f489145b26e40996a28a5be7e17cd50925cb6
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:24:18 +0000

    shop crowd punish perhaps already

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import datetime
+def get_today_date():
+        return datetime.date.today()
