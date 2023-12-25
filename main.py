07c96cbbe243c32c77557bae58374628f9dec40e
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:27:21 +0000

    well several strike runinto fair

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
