3eafe0f97f35d16be6574015f1f474e9c78d3166
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:16:39 +0000

    scrap hurry quick window stiff

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
 def calculate_pressure(force, area):
         return force / area
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
