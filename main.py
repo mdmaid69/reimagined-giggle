4300c21530285840b4e8ec6c8787be782879517b
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:58:06 +0000

    Kk shame face treatment steer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
