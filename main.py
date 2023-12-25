2db05a00ca649201b91814cc3cac4e076542d646
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:06:40 +0000

    scale she gradually him agreement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def sort_list(lst):
-        return sorted(lst)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
