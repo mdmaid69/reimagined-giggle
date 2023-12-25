db65885e0542b1622c8b23c04f29f14badab6e13
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:15:50 +0000

    smile damage party behind steer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_typecode(array):
-        return array.typecode
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
