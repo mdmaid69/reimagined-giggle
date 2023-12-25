41cc7b86699d662ae22f61fc952230d7fb389905
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:23:57 +0000

    instruction scale worst rate custom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
