edb8e0f0d2b0a55209a9dcb7e08b528f9c9c83a7
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:30:05 +0000

    doctor Ll give if bury

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def reverse_list(lst):
+        return lst[::-1]
+print([x**2 for x in range(10)])
