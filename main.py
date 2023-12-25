c2c2934029aabbb561b1a99459e3d5c9e56f4211
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:47:13 +0000

    coffee wind neither figure same

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_min(lst):
+        return min(lst)
