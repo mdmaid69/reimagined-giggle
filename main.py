1cab47677c59f5eaf37e406ed95c0a6e7069f034
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:52:59 +0000

    system indeed wire giveout mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
