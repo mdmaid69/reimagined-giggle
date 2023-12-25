819a203b7cafbd90b39dde6e56b47019e274a73e
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:41:45 +0000

    convince car familiar stop giveaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
