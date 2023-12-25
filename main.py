154306a20fb4eaf4559ca997e396a7349c5f05ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:12:30 +0000

    share avoid talk grand control

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
