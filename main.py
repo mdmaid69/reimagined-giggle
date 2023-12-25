f79ec54297eae9b0da225114004920aa30445bb3
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:11:56 +0000

    community rely accidental cross suppose

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
