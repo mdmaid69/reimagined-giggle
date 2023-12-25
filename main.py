1d5cc6ea0c900d9ef241084b223c5b9884ba69a4
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:18:36 +0000

    detail cross myself tense wing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
