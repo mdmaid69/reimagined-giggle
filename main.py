2c01e534579f8ec2ed503395268177a2f0f0a8b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:15:15 +0000

    abroad shake sticky suit roll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
 import array
 def set_array_slice(array, i, j, iterable):
         array[i:j] = iterable
-import array
-def get_array_as_set(array):
-        return set(array)
