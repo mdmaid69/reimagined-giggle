4161aee4f0e93711c312632426bc6853d4cbd365
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:25:59 +0000

    runout Ww murder pocket care

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(10): print(i)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
