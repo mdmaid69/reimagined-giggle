af5cfa2c632be8e41cc012d673497b2f4d9d060f
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:51:51 +0000

    quickly brush boat marry drink

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-for i in range(10): print(i)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
