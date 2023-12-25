0140626a60cd40db7ef8b2bda7387ac02db9d744
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:04:51 +0000

    speed middle office path muscle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
