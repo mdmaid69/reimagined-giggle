d7ab4e5530a5f66f20c4d6757f18cf1dd135e57a
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:41:46 +0000

    child loan suit black pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import getpass
 def get_password(prompt):
         return getpass.getpass(prompt)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def square_number(x):
+        return x**2
