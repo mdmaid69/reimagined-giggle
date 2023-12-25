75a231f57eb1a21c8b7a5ea58579eda7fb13f86a
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:31:56 +0000

    completely serve illness surprising stage

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def add_numbers(x, y):
+        return x + y
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
