7a3ac2ac04543c69eba61bf1d2b7faa45eb06374
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:01:54 +0000

    problem this cry wash rubbish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
