130fa9f5c5e4b10b99e4ac9547f6d1fcf38cc2fb
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:19:21 +0000

    probably squash value distant aware

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
