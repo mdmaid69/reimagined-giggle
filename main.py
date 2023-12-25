d640a7b5d6d9df2c1edcb1b37d626ceb04cf9cbf
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:19:36 +0000

    for care push criticism face

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def subtract_numbers(x, y):
+        return x - y
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
