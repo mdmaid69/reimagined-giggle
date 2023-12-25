062b8f32743817956b38ea49a051eed32b02c574
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:53:45 +0000

    fact alive speak bit project

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
