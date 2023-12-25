4c9671c8c2f6a85fbe7ace123eb4cfa5da7ffc2a
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:40:17 +0000

    turnup them healthy represent throughout

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def get_array_as_list(array):
-        return list(array)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
