44abc9dd26718290d0d5aa84912331203ca6fe1e
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:53:48 +0000

    machine steep brave trap announce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
