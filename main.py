d5bba55c70d6c165938b22197e3d74f1dfac0e61
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:41:49 +0000

    exchange trade science worth give

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
