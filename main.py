bb81d4e950665a28267dd4294ab3c391d36b5c50
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:51:42 +0000

    tool bunch interesting translate stranger

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
