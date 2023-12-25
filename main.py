4d446c675ebc6ebd90e7ffa0fe93b076570d843c
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:34:54 +0000

    happy piece best callup foot

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import array
-def get_array_length(array):
-        return len(array)
+  def subtract_numbers(x, y):
+        return x - y
+def sort_list(lst):
+        return sorted(lst)
