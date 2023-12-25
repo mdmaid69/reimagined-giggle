4a50cad5221788a70ff675758b8027defbe1363d
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:30:26 +0000

    passage scream attractive subject bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
