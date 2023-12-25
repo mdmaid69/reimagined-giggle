157b2331c788845cd22136fb6c7f0df684c3f4b1
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:08:16 +0000

    else criticize dull complaint organization

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+print("Hello, world!")
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
