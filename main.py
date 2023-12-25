1be441988829a0d351d9d80f9643e5771ca1a91a
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:23:52 +0000

    collect switch proud as admire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def reverse_array(array):
+        array.reverse()
