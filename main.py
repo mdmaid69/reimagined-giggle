52e04316011ab31af23980003e2eefbdb0594c9f
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:38:14 +0000

    shine idea immediate Vv Zz

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
