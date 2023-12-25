af265f3180246ecf8b4b39d5c6e01c39938cf226
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:16:44 +0000

    person already notice Uu general

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
