dedaba3cc47e3ec3aad8a107e392ecf41ff712f8
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:57:16 +0000

    hurt wheel upstairs communicate attempt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
