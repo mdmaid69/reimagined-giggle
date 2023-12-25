cee734f807b8c3071a19a04a1ca52ce16f5c1824
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:22:00 +0000

    call tidy minimum touch accidental

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_as_float(array):
+        return float(array[0])
