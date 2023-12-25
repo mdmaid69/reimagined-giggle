e4a20db4bc7a7e17a7aff38ea652bb2b523eaa04
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:01:56 +0000

    relation state send bang front

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import sys
+print(sys.version)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
