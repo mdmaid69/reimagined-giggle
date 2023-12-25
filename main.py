9bcd90a1dc38fb6b4d991175b4f24b328795fc12
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:49:15 +0000

    both minority software forward file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
