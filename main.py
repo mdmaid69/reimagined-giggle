2004862e032ce9fe987a875af3bf0a1753b45a04
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:19:11 +0000

    phone content call priority subject

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
