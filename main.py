8d7f7d5ea9e4394833192be375a5616d3d5076fc
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:25:54 +0000

    convinced persuade type bit bury

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import tensorflow as tf
+print(tf.__version__)
