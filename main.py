d9c1f458d9ba20e0467aad52f3e9514ffa73e799
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:02:23 +0000

    cut add seat thinkover quantity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
