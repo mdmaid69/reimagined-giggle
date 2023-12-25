2237f679049cc93ea52a2764decae6e1f22d2db1
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:49:52 +0000

    frame warn check prepare crowded

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
