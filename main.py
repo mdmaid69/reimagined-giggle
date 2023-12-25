38ad16f319af08df5e4bbeefa00ca595070b25fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:51:24 +0000

    spend shall sign give frame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import math
-def calculate_floor(x):
-        return math.floor(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
