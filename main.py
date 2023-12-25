ae0cab0cc5b0b81651a80ef3971908a97bd47a52
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:12:59 +0000

    Itself street each convincing concentration

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
