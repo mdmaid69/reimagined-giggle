ece582e5439655b4163eed61c80fd9af414faa5b
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:13:07 +0000

    borrow practice welcome favourite mistake

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
