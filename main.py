ee7a5b04b9450939a680e4d7bccf72f1d54bb126
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:06:06 +0000

    feature everybody anywhere corner basis

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
