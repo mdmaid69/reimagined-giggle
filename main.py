5ac32618d9fd20a75092bb49549a4193233b1822
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:46:09 +0000

    less congratulations state bringback study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def cube_number(x):
+        return x**3
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
