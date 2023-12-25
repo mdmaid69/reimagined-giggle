616182a9b3a66eb989a1f6d49d1160a1312f5274
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:08:12 +0000

    light trouble Uu vote Ee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
