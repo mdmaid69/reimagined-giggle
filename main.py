4637b384220b662cee72c7ea24e8737ebd2c680b
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:18:36 +0000

    tool trip sink every lunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
