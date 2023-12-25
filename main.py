59eb5a3676c1f2932b07e5bbf16808acba767ecf
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:44:19 +0000

    turnback before probably charge concentration

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(a, b):
-        return a + b
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
