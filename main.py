d238add6b0d32fcd256f678548bcf6193b86f2f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:35:10 +0000

    concentrate lock finally not float

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def get_array_as_tuple(array):
+        return tuple(array)
