8dfd1b04e6ec931e720f22af2859040650a0c091
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:54:29 +0000

    sleep first detail concentrate turnon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
