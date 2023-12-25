3d00fcb92ed95966f078e50fe920cb4719106138
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:58:25 +0000

    activity shake attractive name attach

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sys
-def exit_program():
-        sys.exit()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def is_even(n):
+        return n % 2 == 0
