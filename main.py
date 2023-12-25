60e9fe9023c27d7a914083628363e40a41aa3c31
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:03:13 +0000

    offer nose straight block skill

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
