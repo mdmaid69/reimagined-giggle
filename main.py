46d988b7fa65b151ae8ed034d170481b6f2ab774
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:11:58 +0000

    difference home aim season he

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import os
-print(os.getcwd())
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_string_from_array(array):
+        return array.tobytes()
