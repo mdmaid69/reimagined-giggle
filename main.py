52bbb0957d3c89bd024b70cf6dc71c2c40113929
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:30:22 +0000

    lend swimming angry finally write

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
