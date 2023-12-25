a801da80827137b05b374ffad1873fc77110e525
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:15:35 +0000

    attempt minor pause life fear

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
