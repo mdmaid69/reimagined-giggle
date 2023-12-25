4c88f8a9a833d621f3699d2a50c5b69394bdc6d0
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:14:41 +0000

    drink turnoff bother inquire deep

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
