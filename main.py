7b2b97f9cd2d7c32d1f9038859e19072807ea9aa
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:40:56 +0000

    fetch week steam blue apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_octal(n):
-        return oct(n)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
