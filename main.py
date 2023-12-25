0bbd5afd4a2a3485959a3ecb3e88f21b4a77167d
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:10:55 +0000

    purpose space uniform hear national

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
