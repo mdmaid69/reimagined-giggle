07c7b8687e80d62c9ae0a27e156c5d5756c8ae26
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:43:03 +0000

    collect strict happen while suggest

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
