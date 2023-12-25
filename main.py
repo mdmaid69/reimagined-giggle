c1d397b95155c5eb1e222a78ab72395762c4591f
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:56:36 +0000

    forward bored embarrassing police seriously

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
