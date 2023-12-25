895a5476d7f067f6c2f820874b2a905a9d8081d6
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:18:03 +0000

    ordinary market mood reliable reaction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
