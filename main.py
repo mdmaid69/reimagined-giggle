9c114789db1a5a61a038a54ff6a96d088ea9f3f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:14:15 +0000

    complete worth gradually laugh company

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def exit_program():
+        sys.exit()
