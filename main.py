6090cf4f46053a13d68ddf596902552d749dd7fb
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:03:05 +0000

    nowhere terrible half job along

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
