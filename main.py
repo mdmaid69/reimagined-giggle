afbf62830d5ccd0c39aef4f85954722364b46bb9
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:42:31 +0000

    definite win plan sort turnover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
