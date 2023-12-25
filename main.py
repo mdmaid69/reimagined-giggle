7a0e17cd944adc482183dacadda65304cae096e8
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:44:51 +0000

    tear song wander mend talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_user_list():
-        return collections.UserList()
+def create_user_string():
+        return collections.UserString()
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
