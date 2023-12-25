1ff530a99ca5fd55db6626dc70c0604dee8a98b5
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:19:56 +0000

    breakup quality hurt Jj one

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
