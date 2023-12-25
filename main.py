1f7184133fa4156193a73aa2491715a35e64545e
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:10:17 +0000

    crime fit mass sight so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def is_odd(n):
+        return n % 2 != 0
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
