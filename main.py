cc7718a9d4588b9daf3cde9ff405c2d5461a63cd
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:42:51 +0000

    free hang intelligent original their

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
