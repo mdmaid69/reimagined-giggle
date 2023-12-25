7fad954e2da2ade7710c58b294e01145531216d2
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:25:12 +0000

    aim or nearly run mine

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
