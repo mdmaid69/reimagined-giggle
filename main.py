52464bdf5573ea5ac0948fb474a95911d08b27b5
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:41:02 +0000

    education commit while turndown cinema

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
