fee36d8755cf615b489d83f64a70a64bb911fcad
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:46:07 +0000

    ourselves list spot habit than

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_exponential(x):
+        return math.exp(x)
