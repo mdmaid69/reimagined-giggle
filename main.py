8e23321ea893d2107b51945e818960eaf7c84611
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:23:00 +0000

    rob on otherwise nurse chase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import array
-def reverse_array(array):
-        array.reverse()
