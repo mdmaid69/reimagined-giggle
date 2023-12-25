ef6e6a15edf04fcc5413d7e8f7ae56e4fa61a94c
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:41:14 +0000

    goodbye suck practice ride kick

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def find_union(list1, list2):
+        return set(list1) | set(list2)
