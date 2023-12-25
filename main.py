e102f65a88b5dc63ba04688e4d00fb6d4d7d4f4d
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:22:57 +0000

    ilovedicks important early transport close

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import random
 def generate_random_number(start, end):
         return random.randint(start, end)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
