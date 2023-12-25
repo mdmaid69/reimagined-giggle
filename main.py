0061b0047d53b8b519b4fa847f3a5a925252e7fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:53:38 +0000

    getaway wipe punish tall far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
