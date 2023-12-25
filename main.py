89c46f91fea87e730186ea6e9f4064909f2b9aa1
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:35:45 +0000

    leg judge beat start from

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
