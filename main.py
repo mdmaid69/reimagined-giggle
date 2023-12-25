4b67d1483b9d7af6f36197d922f332e2506f6bd6
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:46:05 +0000

    sensible Qq familiar seriously callback

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_max(lst):
-        return max(lst)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
