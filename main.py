9c38f6c4b5b1ca667502d48387c493a6f0c7aced
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:25:36 +0000

    wild dull invite trap lend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
