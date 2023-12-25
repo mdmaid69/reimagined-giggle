149a3e8410b6715f95b67c9afca318a97f068aef
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:15:26 +0000

    sport breakthrough neck square married

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_area_rectangle(l, w):
+        return l * w
