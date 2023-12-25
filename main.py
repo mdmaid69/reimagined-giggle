47682a276b89f01c2feef4f3b42ed312fefec6b8
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:09:11 +0000

    all agree transport tap content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
