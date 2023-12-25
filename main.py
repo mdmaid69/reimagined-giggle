67ec2ba442ae60b69f5171045b61d6c6c7cad0cf
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:56:33 +0000

    shut past few command produce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def clear_array(array):
+        array *= 0
+def remove_duplicates(lst):
+        return list(set(lst))
