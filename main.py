c9c226e1a7bb3967bbf6fb898ef49ab32b0db887
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:08:59 +0000

    quick later to success spread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
