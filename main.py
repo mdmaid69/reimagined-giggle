93fb92f6675bb551dafc172e742a94a203cae48f
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:55:45 +0000

    anything print practical appeal environment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
