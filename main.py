ad5d27d204327e66f183b4c99c68312bc9e942e7
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:29:47 +0000

    myself shoulder anxious worth getup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
