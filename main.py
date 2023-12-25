5abee16dcbf70fd6d0ad6cb98bd7e8fbaf626f15
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:44:58 +0000

    light holiday organize indeed shrink

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-print(random.randint(0, 100))
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
