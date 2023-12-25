ed57ba120f515b09e71d70c46a541293fa53d55a
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:19:24 +0000

    pronounce fold maybe talk relationship

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def add_numbers(x, y):
-        return x + y
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
