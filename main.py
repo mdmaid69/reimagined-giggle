4f645394fd828e027ce2ad7b78a598172df016b1
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:40:38 +0000

    model repeat trade shut finally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
