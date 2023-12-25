ab01106eb1194f9e0717ac82dc50ddaa130b8825
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:19:41 +0000

    soak year legal gate dead

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
