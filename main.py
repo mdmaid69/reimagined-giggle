5923e7c665e3e5a696d59a2d3f33226b44626fce
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:32:56 +0000

    deliberate other correction reject new

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import logging
-def log_message(message):
-        logging.info(message)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
