9aad7e64218d6252b04105c6d51b256428afa128
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:15:33 +0000

    improvement slice favourite Ii active

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
