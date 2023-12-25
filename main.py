943dff4802c76a66317566649ff8d89c2bf92624
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:37:12 +0000

    determined fear match town ball

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import getpass
-def get_username():
-        return getpass.getuser()
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
