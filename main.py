2bbda90717919d1f75341d2d06774a0187f6ccbf
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:13:57 +0000

    down way science collection snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import heapq
 def get_smallest_elements(iterable, n):
         return heapq.nsmallest(n, iterable)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
