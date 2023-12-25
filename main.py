5029726e8144bbc75f0d41b7a46407cdc3ccadd7
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:08:16 +0000

    learn pump pleasant refuse punishment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import time
+print(time.time())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
