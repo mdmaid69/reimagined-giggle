3bf50c558d9c8056e5b189f1d76f4ebeb10d054b
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:29:33 +0000

    mass hang protest keep Uu

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
