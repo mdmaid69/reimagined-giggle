ebc2a005bf36524463ac38e43c30de9204131e16
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:36:41 +0000

    blind kill hurt cry bringback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
