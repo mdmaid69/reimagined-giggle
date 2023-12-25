37077af28a34f2aaca7e36ddc47c20042792204e
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:34:49 +0000

    speed near approval rub negative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
