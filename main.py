8313b17dcba18ffee58287f90b3fb4017a49ecfb
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:47:51 +0000

    peace pause reply passenger dinner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
