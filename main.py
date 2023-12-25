c444b753fd14c46a98f17a5ea4a35840abb096ee
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:05:25 +0000

    square aware minimum mess curious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
