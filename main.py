85ebfc3fce7b30b44dde4e2a6184d09acf991887
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:04:27 +0000

    classroom anyone point exam arm

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
