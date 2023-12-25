80662190fb864d510b0eec394760c40662d7a16b
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:34:27 +0000

    out whistle bin manner achieve

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import sys
-  def get_python_version():
-        return sys.version
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
