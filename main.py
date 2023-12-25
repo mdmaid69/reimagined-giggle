69f07c564cea67f9ce7767ae48f8fbaa5506c8b5
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:38:55 +0000

    passage dream sport Qq material

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
