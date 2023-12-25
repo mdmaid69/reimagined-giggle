57504c67b373240678ec51f8a3217c9dbaf349ea
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:37:43 +0000

    cut inquire whatever keep change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
