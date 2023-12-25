8ac26eab1c913b042a347559883fb6424b783e65
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:14:17 +0000

    form sign information deal whenever

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
