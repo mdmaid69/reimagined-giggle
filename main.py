17786b60a3d55312b79e4939a6fc4ffeb302cefe
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:39:37 +0000

    plan leg course appeal strict

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
