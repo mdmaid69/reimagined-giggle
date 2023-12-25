725714288710585631a4120a347fe7044aad4039
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:08:53 +0000

    cinema getback whom 179bite carry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def find_min(numbers):
+        return min(numbers)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
