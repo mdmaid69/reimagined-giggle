8dd77a02d5d7dd5f0f5fd921ceb2546efb6995d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:46:38 +0000

    manner provide port plenty choice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
