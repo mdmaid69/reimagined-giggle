9803712fe7c54a5df0840acb7b7013cbbcebe3c9
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:58:08 +0000

    apologizeapologise difficulty 571enough shade Vv

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
