b0ab610b27abe412143b7ab1870e5ee545faf452
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:59:22 +0000

    remove figure fast display often

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
