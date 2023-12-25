b5985fd4dbac4955c1b4fa1a62dd264ec800ab5d
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:40:59 +0000

    guide makeup size fierce but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_current_directory():
+        return os.getcwd()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
