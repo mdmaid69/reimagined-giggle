09578caf3eb3579380fb295b4977c298d6c7bd5c
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:39:22 +0000

    national use change contact type

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
