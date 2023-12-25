cac71b7007e0b62a62d02e86ac77a674845108ae
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:39:38 +0000

    breakout workupto land capital weekend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
