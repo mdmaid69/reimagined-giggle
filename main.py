6bc9afc53f6cb284d50a2e244da2e5169e04399e
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:35:47 +0000

    skin rub mood practise ready

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import numpy as np
+print(np.array([1, 2, 3]))
