495d052d02e5e08a91628c0f4d9afcee024b2fde
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:24:25 +0000

    corner trip bar convincing hold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  def add_numbers(x, y):
-        return x + y
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
