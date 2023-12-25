88307589875d071b40a3e10e849fd1965efcc5a1
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:10:57 +0000

    spite lookback(on) even successful down

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import platform
+def get_python_version():
+        return platform.python_version()
