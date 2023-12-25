dc1a7e888ab92003630676d5491389e6385a6cc7
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:05:06 +0000

    burn finger keepaway just recognize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import time
+def get_current_time():
+        return time.ctime()
