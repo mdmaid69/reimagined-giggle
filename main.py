0189a877fdfbf6cf3f01443a83023137940d709d
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:42:55 +0000

    imagination hope ourselves Ll short

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
