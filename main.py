dbe0473281ab96fc9d2d55364100518585e44fc1
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:41:35 +0000

    keen offer turnoff set suppose

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
