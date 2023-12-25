50ced9afc0a0ac19f63d26fd2e29f99eecf961f3
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:55:08 +0000

    place prove flat themselves pencil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
