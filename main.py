04048675d03cde83a2bc8d4f32f98a606ecbc571
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:18:30 +0000

    shake smell impatient illness track

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_string_from_array(array):
+        return array.tobytes()
+import sys
+def exit_program():
+        sys.exit()
