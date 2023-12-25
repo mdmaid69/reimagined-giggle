b7faf3d97607a8a98841ad61ce328ebc6ce7a94b
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:52:45 +0000

    cut behave run light almost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
