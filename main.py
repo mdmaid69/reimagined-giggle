fbe3a5b541395bab03c89d1d8919f643eb1b3e5c
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:00:44 +0000

    kind warning goup cruel crash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+name = "Python"
+print("Hello,", name)
