48df88835420185aa5962bc7bcf4c16601c7cb63
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:47:25 +0000

    uponon hurry do change wild

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
