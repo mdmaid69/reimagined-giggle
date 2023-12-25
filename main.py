00066732e09d88a413b132b230fa5d38f1891f62
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:17:58 +0000

    sick stiff trap model swim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_directory_name(path):
+        return os.path.dirname(path)
