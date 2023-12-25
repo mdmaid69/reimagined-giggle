e22ae007c22d25a5aaee4f52236fb4d93ffde948
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:30:28 +0000

    surprise sharp train wide pair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_work(force, distance):
+        return force * distance
