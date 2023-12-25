f20a0008c8721a779368f6f75d8dab4a5135f7d4
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:59:21 +0000

    wise somehow statement believe small

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
