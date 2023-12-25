daf3beafe70c0e5c1d4571dad3e912acb672214d
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:29:27 +0000

    active expect exhibition cook free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  def convert_to_binary(n):
+        return bin(n)
