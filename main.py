9e09955fe1dcb9cb25b4197cf66610c7ede364e8
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:16:30 +0000

    meal interested concentration bush defeat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
