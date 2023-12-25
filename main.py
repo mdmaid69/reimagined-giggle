d3e270213aec0c780e747a681e22860f7f9314fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:27:56 +0000

    pool finish rain check transport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def set_array_item(array, i, item):
+        array[i] = item
