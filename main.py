7be61f99f23d5fc8bac124b0bd0d1aa66081e006
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:06:38 +0000

    gift in thumb value glad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import collections
+def create_counter():
+        return collections.Counter()
