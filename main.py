85cf2bbac1b94f630ed21e14ed306046ef638897
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:54:27 +0000

    sick order after rid tune

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
