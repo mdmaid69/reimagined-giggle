b6a47ac5b6f2556ebc1ae2475a01483dec5b884f
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:55:15 +0000

    rub fact I goodbye step

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
