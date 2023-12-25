ff73d3046582dc729b49db4c14d8ec48328d5a53
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:14:38 +0000

    great callout kill turninto Uu

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_current_directory():
+        return os.getcwd()
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
