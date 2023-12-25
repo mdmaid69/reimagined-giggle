7e38391ceae3cc8bd6c8f412224a431c3bbd687a
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:28:31 +0000

    emphasize accidentally alone attach majority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
