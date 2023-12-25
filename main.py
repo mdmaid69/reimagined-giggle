97014da580b7dd341852c64b49b05ec815767429
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:53:21 +0000

    this offence force surprised change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  def multiply_numbers(x, y):
+        return x * y
