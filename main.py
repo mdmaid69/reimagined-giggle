b0cec0f25594dc82f9b479a71926036e59fd269f
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:45:41 +0000

    bringup plate possibility please high

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 def calculate_energy(mass, c=3*10**8):
         return mass * c**2
+import array
+def get_list_from_array(array):
+        return array.tolist()
