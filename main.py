2b9a74fbdb8523c08842a456140eef70d2bacad5
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:19:22 +0000

    life attention glass beginning thinkof

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def add_numbers(x, y):
+        return x + y
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
