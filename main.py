eee19db1ef6b0ecb88c912564df6225aad274399
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:57:43 +0000

    beginning sister uncomfortable leg teach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import sys
+def exit_program():
+        sys.exit()
