c2dfbe709c8d77c061284abae35fca848ec9f4e7
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:44:09 +0000

    ground reply content course naturally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
