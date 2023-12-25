8721e43592b2e34e56462c6d0e482ff7e41926fb
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:49:01 +0000

    delicate fork OKorokay refer burn

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
