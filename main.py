e8e3582c9250f466a2e512f4ecefbb85677be7ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:31:21 +0000

    flight art feed border measure

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
