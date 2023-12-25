d3d4175085c305b025e41974e73f7b2b01cd3c26
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:24:46 +0000

    matter reliable company certain plant

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
