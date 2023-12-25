1301b5154b7e261a52550ddb5bff8b9c0b3f15fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:31:48 +0000

    smell comeout young store legal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
