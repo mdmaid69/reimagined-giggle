0e6ec777bd09bcae5e80ad1a637293614259423f
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:03:22 +0000

    close squeeze date unable fresh

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
