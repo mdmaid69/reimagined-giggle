4f0968de4989a17f899fbd76e399d0ef40bd4074
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:28:46 +0000

    unfortunate together keepoff attempt beyond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
