ba3c91d41b7e79fd5fb688cbccc81ec7cb8f0248
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:20:47 +0000

    loose experiment information large move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
