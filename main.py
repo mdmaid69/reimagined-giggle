cb42df935c417c4eb7df7dd18e65f7139e522888
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:48:22 +0000

    weekend event strong use phone

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
