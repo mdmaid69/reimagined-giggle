6513bb94022eb31087ad40982575538fe150bba8
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:57:16 +0000

    meeting Dd confidence squash until

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_item(array, i):
+        return array[i]
