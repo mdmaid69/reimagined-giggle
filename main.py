23c61975e03780c01d30a9a5ff7257be3ee56553
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:30:14 +0000

    allow whistle along impressive conduct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import math
-print(math.pi)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
