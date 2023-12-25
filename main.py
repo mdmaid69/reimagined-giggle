49461ce91c208585a45113d34cfe14babed241fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:30:27 +0000

    good workon powerful cope certain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
