45497eaad0d43260e6a708cd2a57bcbd96afc0c7
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:48:33 +0000

    disapprove stupid wear getaway alone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
