7e287ef36959b51f0b28ac60bccbec6df4c63041
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:07:05 +0000

    organize container they behaviour whatever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-for i in range(5):
-        print(i)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
