2f7ca52314048cc03e81774ecd6fab498ead9d06
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:02:53 +0000

    strength need wrong normal gather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def clear_array(array):
+        array *= 0
