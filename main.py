f8c19dc635ea67cf9c8dc313560773669c94ce71
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:07:58 +0000

    ring possession sore weigh generally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
