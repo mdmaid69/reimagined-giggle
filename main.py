5fa90dc08cce64dd8348852df2a690844f676e1f
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:37:51 +0000

    flow lean exam service transparent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
