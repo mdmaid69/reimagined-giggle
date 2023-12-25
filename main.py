108e2adf3a5d724930715433dbd5e363e0546754
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:47:59 +0000

    understand scheme unusual advice fold

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import re
+print(re.match("h.*o", "hello world"))
