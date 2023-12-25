98b4440d71aada4cb1d230f30681cd9f37280d13
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:01:32 +0000

    nervous command confusing strong skirt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
