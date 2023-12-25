c8e2426bb7b1eb38a2daafc69f6e8c2e57fa4e52
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:36:48 +0000

    cool receive count promise pass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
