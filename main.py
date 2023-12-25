9133baeed49498d2d1e3cbd95bd2cb3ac8ed36c1
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:54:30 +0000

    impressive offend something pay course

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
