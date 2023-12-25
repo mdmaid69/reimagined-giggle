06f490c08bad2b20414f6e1115dbe8773c4f1c11
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:53:03 +0000

    fine thumb visit suggestion upwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
