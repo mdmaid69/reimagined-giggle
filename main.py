f796029ae9c435a1925c67e93e5455e8d0578fea
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:55:02 +0000

    concentration zero uniform front punishment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def check_if_array_contains_item(array, item):
+        return item in array
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
