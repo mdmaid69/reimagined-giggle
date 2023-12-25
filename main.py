cc58c072ee74e5d581493700c312410a7e4dcdb2
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:52:12 +0000

    sport opportunity increase teach sight

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
