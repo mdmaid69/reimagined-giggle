2ee52d27055780f0a51f32b925f75b257a764069
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:19:55 +0000

    get typical arrive theirs club

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
