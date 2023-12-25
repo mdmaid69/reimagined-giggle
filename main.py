2b2773b1c25037f63598b4beb124be15accf9cee
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:10:26 +0000

    absolutely choice embarrassing apologizeapologise bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
