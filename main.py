536cb6b2afc5f6566dfe7548398cf1b80af23c7d
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:11:19 +0000

    interfere fat extremely average your

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def is_odd(n):
+        return n % 2 != 0
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
