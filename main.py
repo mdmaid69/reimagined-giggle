ff44882aaf4e3756268022fb6d396d71007455bf
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:31:22 +0000

    walk hurt everywhere hide pleased

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
