a442bff452deebc6091c7103d910a63c947b935e
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:16:28 +0000

    government plenty spot break crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
