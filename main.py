4bebad3a8d86292c08d8c46379deffe04b76ffba
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:45:41 +0000

    content pay stamp live neither

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
