4c2898af8d407a751cb2c6b84f0b49c52fd9f8dc
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:55:43 +0000

    basically tone delicate stamp down

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
