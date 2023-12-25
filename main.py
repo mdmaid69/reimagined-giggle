db73933af0f3c2a47d20eedc6916dab20383b0f4
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:06:17 +0000

    base make result slow sound

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_area_circle(r):
+        return 3.14 * r**2
