d18186d9f2988c7e72bfb262ac2a7e439194b4c7
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:20:42 +0000

    minor too just ground fail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import tensorflow as tf
-print(tf.__version__)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  def remove_duplicates(lst):
+        return list(set(lst))
