f11873eaad0d2baf79473222df9515d5b0608c81
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:00:49 +0000

    party match slice slice refer

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
