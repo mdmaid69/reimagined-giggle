df432e3660e7a95034c87ddd42f24ef92c6367bf
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:56:12 +0000

    shop rule lonely however morning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
