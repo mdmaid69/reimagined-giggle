e7aead50196e278f59aae211d0f24fa7060dcbf2
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:43:21 +0000

    cost before travel rest surprise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
