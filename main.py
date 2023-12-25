2e646da0a7453a34db909f572a0f165e325a2bff
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:56:47 +0000

    set wash issue occasion mail

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def subtract_numbers(x, y):
+        return x - y
