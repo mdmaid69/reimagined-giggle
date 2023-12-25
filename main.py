fd8f3e6818ee00498220facf18b149f9b56d8cc4
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:08:19 +0000

    manage cross down effort various

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
