73a881c78260d93b6c6ee7a571818df92445bb31
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:24:54 +0000

    light goback(to) power already form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
