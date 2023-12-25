468e181685e277cc21bc137247a0ce919b354579
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:27:33 +0000

    trick difficulty release service able

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
