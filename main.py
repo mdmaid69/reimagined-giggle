cdb5a5b73649924f046f752f395d2d8eaba7e144
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:06:21 +0000

    still remain miss watch giveout

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def greet(name):
+        print(f"Hello, {name}!")
+def count_elements(lst):
+        return len(lst)
