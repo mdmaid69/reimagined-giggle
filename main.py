02f8792dc298cf7b974bcb9d7b92fdec819c3678
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:45:05 +0000

    573entertainment yourself keep remark herself

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def is_even(n):
-        return n % 2 == 0
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
