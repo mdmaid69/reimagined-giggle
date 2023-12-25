d614fb7182ba43bd7609389d12b9c4f939518b39
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:01:31 +0000

    confidence takeover upset spot family

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
