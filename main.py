b9c4126c8c1a3eaf9175f47415cf754dfcb1d1bc
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:14:54 +0000

    jump mention sister comfort unless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
