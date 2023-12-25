e18a519d8ccb81930a04ed23086a008385e44c29
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:45:56 +0000

    body account level lock whichever

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
