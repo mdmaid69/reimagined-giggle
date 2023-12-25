4176c0f781a7d5648ca0207223c7893b85bc721e
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:52:24 +0000

    apparently difference paper future science

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def reverse_string(s):
+        return s[::-1]
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_bool(array):
+        return bool(array)
