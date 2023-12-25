b8f4abe934d7fda31df6f7fbc65713e20c7817fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:19:06 +0000

    level market breath nervous board

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
