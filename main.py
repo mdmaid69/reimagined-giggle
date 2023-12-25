9812a277346a48e8b2209ba24b66265fbb4710d2
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:08:17 +0000

    unkind appear emphasis steer turnon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
