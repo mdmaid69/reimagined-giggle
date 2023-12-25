1747e0f0fdbbba3b0eb380a494e37a84c7c97ec5
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:15:59 +0000

    touch laugh part birthday several

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
