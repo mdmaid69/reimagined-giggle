1a377ddaa4d1e43089142198cd3de12bf1fff522
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:17:30 +0000

    pretty pick embarrassed hurry eager

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
