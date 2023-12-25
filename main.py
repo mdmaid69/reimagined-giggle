0558f0e7c630fdac7ad0b45259eeeced726d76a9
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:54:21 +0000

    card row after cost left

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import collections
+def create_counter():
+        return collections.Counter()
