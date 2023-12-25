04be9dd9173de72c9320d53978be910af5265dc6
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:08:20 +0000

    stranger bite rather door message

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
