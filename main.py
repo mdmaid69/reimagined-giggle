62ab29ff5da190acde424916e97781099545863e
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:02:55 +0000

    major home close pin pencil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
