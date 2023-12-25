9300b6794af8068192048d23ff84851ed35c18c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:02:36 +0000

    joke excellent sale pour comfort

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def convert_to_octal(n):
+        return oct(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
