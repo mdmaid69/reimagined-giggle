53b45ac4ed8b378b9716c92b1cafca76ebca2366
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:47:33 +0000

    furniture usually welcome ilovedicks band

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_int(array):
+        return int(array[0])
