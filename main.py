2ac077c67086b71707f16c797d929380444cd1c7
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:58:47 +0000

    reaction neat bury of plan

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
