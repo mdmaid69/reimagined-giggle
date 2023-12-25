22b9dd37d203bec0d1cfa6dc5e36e03c7eb055e6
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:11:04 +0000

    hate never sweet break play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
