f15f5d6b24f43bd300187b9b869bc9467910a45c
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:38:34 +0000

    careless share trap distance to

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
