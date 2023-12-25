96dacd0623d19affee8b3b9c891fa8824320ea6a
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:43:31 +0000

    clever cut bank bring dish

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
