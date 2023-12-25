36cab79e4e858a59945d603ac26a8aa33056498d
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:19:53 +0000

    connection top please loan doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
