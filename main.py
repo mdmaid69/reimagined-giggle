a15f36ead59413ddc1519ffbc8030ff077917d74
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:24:56 +0000

    target look harm talkback throughout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
