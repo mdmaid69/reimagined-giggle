f687b50a61dc45c89daa07dc09f516b21c117d8c
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:05:16 +0000

    skin 567enemy purpose town before

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
