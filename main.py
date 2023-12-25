644f947c10c2f9a2b01a5e49ff6683db510d9a97
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:33:04 +0000

    bang uncomfortable sorry firm list

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
