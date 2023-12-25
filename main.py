620a32d4bb79364ad8b8c9e70153d907f8526cb0
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:09:21 +0000

    left combine imagination that embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
