a3d841cc1be02de100ce896b92bc383f3322c5a4
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:03:13 +0000

    translate top instrument taste weather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def subtract_numbers(x, y):
-        return x - y
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
