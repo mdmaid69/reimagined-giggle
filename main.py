89263b6dca022836aead8056317c3c5a1862bebe
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:23:59 +0000

    worse react 570enjoyable come rather

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def print_python_version():
-        return sys.version
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_speed(distance, time):
+        return distance / time
