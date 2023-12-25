e509bca4fbe955b527c306aef9016fec2976f2d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:38:48 +0000

    proud yes deaf shut which

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
