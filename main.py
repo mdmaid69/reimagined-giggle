ccbe676f69faeac1466cdb933adfcd2130bcd900
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:51:10 +0000

    shame lend risk ahead pretty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
