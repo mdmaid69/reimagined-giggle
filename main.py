6f2970856c07c89615078a9117285ede214f6ab4
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:42:43 +0000

    before quick discovery improvement list

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
