4378936a22c44ca79ceea289fe2bbd191141ae0c
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:30:44 +0000

    Ff single edge good violent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import tensorflow as tf
+print(tf.__version__)
