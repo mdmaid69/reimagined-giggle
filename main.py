e259822cce6c2d2cac1adbc526e9e658df8ddc63
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:23:46 +0000

    lend tidy complete unpleasant pool

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def reverse_list(lst):
+        return lst[::-1]
