5f369d77a08e8d7e36f5930f87ab4e2c50630f3f
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:40:19 +0000

    calm hear develop clothes date

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
