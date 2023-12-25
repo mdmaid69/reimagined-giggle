66697f385e136998b6c03e350b2d7acab87c84bb
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:04:43 +0000

    mistake date chief poison shut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import math
+def calculate_tangent(x):
+        return math.tan(x)
