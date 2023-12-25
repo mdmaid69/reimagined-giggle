cbb057e56b9f37142afc438e6ef6c8fbbea89d08
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:32:52 +0000

    sleep joke improvement sad concern

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
