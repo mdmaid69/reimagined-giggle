52ca83d7ca7b4c74f545a05d40e88e708dd601cb
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:08:31 +0000

    fight professional intelligent among to

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
