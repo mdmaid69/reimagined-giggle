9283f43f79dfcdff40f3fdec778625fa88782b0d
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:47:36 +0000

    watch interruption down design shelter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
