f720a7b31495737cc1eb3da9fa6b25c0fbf99f3f
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:51:56 +0000

    relax next exercise correct 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
