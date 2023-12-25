24b7e4b1fa24d6e03b459448798c24e5dd348480
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:37:48 +0000

    swing lookaround nor last none

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
