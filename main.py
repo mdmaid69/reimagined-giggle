97d8cc5908f0bed8fac992f4a70782fada93ccf5
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:37:26 +0000

    remember earth wash send demand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
