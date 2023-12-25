43dd744c0d6d7af318688d7cdd75a7e3f60a6cd4
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:03:39 +0000

    convenient damp subject still dream

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+def calculate_density(mass, volume):
+        return mass / volume
