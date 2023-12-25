e4c6e6bf008be3f0e32a20394ce31ba5efafc7c8
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:41:45 +0000

    reliable catch school several safe

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def is_odd(n):
-        return n % 2 != 0
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
