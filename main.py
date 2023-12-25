9d54dbb5ece523978b5ab2aaead1e896d07563b5
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:01:23 +0000

    slice breakin study equal clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
