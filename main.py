bf173a7557c4675bc037e1b0a82a95bd50a08d63
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:37:43 +0000

    might mean safety guide brain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
