79cab44d2bb338e718b0eef774cbe264d543a10d
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:50:44 +0000

    cold pretty favourite good silent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_block_size(file_name):
+  def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
