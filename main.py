e9b580b237be6f923a31132a320600d8f7c8e3a2
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:02:08 +0000

    past bottom greeting mad takeaway

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
