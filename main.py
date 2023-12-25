e5dc6d6095f25b9adbecac5641297f80da132b60
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:07:06 +0000

    vanish turn hard side off

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
