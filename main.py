35a52b6a1d2a7e334b478e3ecb25590a85560287
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:03:41 +0000

    detail sorry change coffee season

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import collections
+def create_user_dict():
+        return collections.UserDict()
