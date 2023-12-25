bea11eafb602022681184339e6d34fc4115c3f66
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:44:59 +0000

    visit unless breakout surface clear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
