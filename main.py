5a1c2190b040efb68e5f063a81fafbf34c83113d
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:54:31 +0000

    interview turnout considering communication increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
