df939f131e8e0f9ce51f7a70b1c605f05334103c
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:51:42 +0000

    story fun waste need pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
