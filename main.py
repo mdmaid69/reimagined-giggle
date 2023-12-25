3e0ef423c310a7407bad5e7acba449141a79b28b
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:23:26 +0000

    proper young contribute wrap finally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_speed(distance, time):
+        return distance / time
