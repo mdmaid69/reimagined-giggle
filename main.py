741a39e996badcf13b45881f3ac5214b97b482ea
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:25:50 +0000

    level another charge wet protect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
