18fa9c42f7b7159845c7f1c299d5b3d49d2d551d
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:44:19 +0000

    remove fight length direct steam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import collections
+def create_user_list():
+        return collections.UserList()
