ba3bb54d2622d7af4976fe2d199538f05eb1dcb0
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:36:22 +0000

    type day week traffic crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+x = 10
+y = 20
+print("Sum:", x + y)
