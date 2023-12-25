2afccf394c39c4aacf5996b4057d1bf9e29c4d4e
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:35:06 +0000

    I across copy exist rare

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
