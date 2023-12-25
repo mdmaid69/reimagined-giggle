f27c76c2661af1698b549abf09d03edba1736b0e
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:10:50 +0000

    its stiff just worse artificial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
