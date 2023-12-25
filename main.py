b35c277159a4a8d8df152efe21a41ba68ee9bbd7
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:31:24 +0000

    average twist there break centimetre

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def split_path(path):
+        return os.path.split(path)
