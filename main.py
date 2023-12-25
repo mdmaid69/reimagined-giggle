b74d7a4e988145ec3e811cd03acd511d6c8c4991
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:35:08 +0000

    your shut imagine 179bite interrupt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
