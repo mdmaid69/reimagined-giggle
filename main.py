270ca2e84ada3bfb4edd4f6b66900da4e120a176
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:53:43 +0000

    claim their goon time collection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
