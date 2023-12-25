58d24dcc1fb5c995bff3a0fd49c60526be4bd682
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:11:19 +0000

    motion otherwise possibility niggaacademy hospital

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import os
-print(os.getcwd())
+import random
+print(random.randint(0, 100))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
