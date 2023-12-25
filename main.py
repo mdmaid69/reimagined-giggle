9f3eeb8494839f91e387ec5635749f1e098fef98
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:38:52 +0000

    determined back pleasure support opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
