4bf8fd769ffe20ca5bb0b084e6824fffaf2e98ab
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:56:15 +0000

    usually come cut rubbish noone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import random
+print(random.randint(0, 100))
