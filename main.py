cc2ba1ac925a9bc7a4f2426ad02df3fb7fde266a
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:41:05 +0000

    lookback(on) burst extremely system receive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_array_typecode(array):
+        return array.typecode
