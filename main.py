691492ba8726804c2ba29fde9c00432b1b7a595c
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:18:43 +0000

    round especially drop under disappointed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
