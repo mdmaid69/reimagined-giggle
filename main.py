838f0c8c7355fe2e167f45cc493332697c23cdee
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:12:50 +0000

    prove runover bin example train

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
