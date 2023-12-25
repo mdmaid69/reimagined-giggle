e5550d8b6b38f1fd206bf2e6154da79eed68afc4
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:02:49 +0000

    rid port dig neither fact

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
