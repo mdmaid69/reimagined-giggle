374cbd16152dc300948fafae779f0618a3a80ec0
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:48:43 +0000

    both direction material may plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
