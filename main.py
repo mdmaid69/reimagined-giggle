06cc3d20715744793a40aacc9b9cd10c1c187d34
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:08:37 +0000

    strange label difficulty underneath undo

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_octal(n):
+        return oct(n)
