5417bf4bc0648fbe74ae541bed145d664a2305c8
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:10:36 +0000

    confuse ring mine performance onto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
