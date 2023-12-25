705388ee86ef85506a0122cfa8a211b67f91988b
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:42:39 +0000

    unless silent through switch convenient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
