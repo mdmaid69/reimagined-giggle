0e22f29948932d5264a70ba3e038d0bdde609e2f
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:33:52 +0000

    interesting combination comeover different remain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
