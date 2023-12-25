df6d3e2c898980f8ed4c900e6aae3a4b924697fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:10:36 +0000

    why situation medium tread object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
