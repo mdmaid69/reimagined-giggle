8195a1440d27400c5063d80010784500a593338b
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:01:22 +0000

    lookback(on) funny influence believe ahead

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import logging
+def log_message(message):
+        logging.info(message)
+print("Hello, world!")
