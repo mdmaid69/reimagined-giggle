fc747cf793e73f7e3dc742f38163579da0c35743
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:52:05 +0000

    fall wrap knowledge breakfast supply

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def convert_to_octal(n):
-        return oct(n)
+def find_max(lst):
+        return max(lst)
+def calculate_pressure(force, area):
+        return force / area
