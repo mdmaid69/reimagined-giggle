d4bca3e7d6e2cb60b804a26762b20aec32e44ec1
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:36:40 +0000

    feel marry no record fun

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
