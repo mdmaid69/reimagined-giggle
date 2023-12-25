e56cb3ea2094855d2ddc34183c38674dc720af22
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:36:47 +0000

    pain delight crash appear limit

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def is_palindrome(s):
+        return s == s[::-1]
+print("Hello, world!")
