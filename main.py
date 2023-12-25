922f584475a14abdce86eb72dc57d8a8322ed6ff
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:35:51 +0000

    view remember corner small punish

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_volume(length, width, height):
-        return length * width * height
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
