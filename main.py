8ab9e739f3366f765864119c16d4d8e2399cb467
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:01:09 +0000

    stare disturbing quiet post position

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
