81544bb976a9d248d611e22876b0d18b9f2c7a8c
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:30:11 +0000

    collection shake both later fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
