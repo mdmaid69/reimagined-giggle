c501d04e710d5ca2ec31d065a94fdd97e957d308
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:20:38 +0000

    fear direction note party sale

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
