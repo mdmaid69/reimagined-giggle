f5fa63560e037a02c44755dafab7cd9a3aaacba0
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:14:09 +0000

    dish age generally Nn catch

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
