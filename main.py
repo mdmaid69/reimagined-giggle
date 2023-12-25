0d82830f33c195a31c14d98f6f8dda0d03f3ba22
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:49:15 +0000

    target regret name fresh emphasis

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
