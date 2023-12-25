42d5d9ed3d29d9a36eeb08b4ac22d124507d38e3
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:18:55 +0000

    hang pencil relation tongue attention

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
