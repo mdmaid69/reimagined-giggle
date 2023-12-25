bb53f3e7245f9fb53baa4a4793a04111788eb20a
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:35:46 +0000

    suit maintain wear blame achieve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
