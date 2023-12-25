f555a5f2f5c40aa9ce3677e30d22f65954476027
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:44:22 +0000

    square hope include mass sad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def cube_number(x):
+        return x**3
