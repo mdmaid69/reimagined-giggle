cfd7b929cd1518123cdb96102cd1605ac05a62aa
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:01:52 +0000

    unit gate fond trial opponent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
