0c8c3f3838210d60cc8118f761a4b0309610bfdb
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:09:56 +0000

    closed public vanish gift rescue

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
