7516822e5074fefa8708f6010807b4553204c69e
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:43:32 +0000

    anywhere cause makeup doubt material

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
