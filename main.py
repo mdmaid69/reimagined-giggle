d3b7582e264bc1203c61991fb6bc5e27691965e9
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:20:23 +0000

    surprising takeaway balance what breakin

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def get_array_typecode(array):
+        return array.typecode
