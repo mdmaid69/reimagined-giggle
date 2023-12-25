15033581fbb4c8b921028dc76dece8f5a529f5f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:52:25 +0000

    crowd introduce wall cost health

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def add_numbers(x, y):
-        return x + y
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
