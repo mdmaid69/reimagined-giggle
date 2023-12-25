8500575136fcdebf01ff21a993a2215674927d2d
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:57:40 +0000

    pen buy bored accept immediate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def is_odd(n):
+        return n % 2 != 0
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
