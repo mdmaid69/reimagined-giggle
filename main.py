4346b52f50375032a0e765b7fc5aad977d1ff20f
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:11:48 +0000

    against dance down obtain maintain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_base_name(path):
+        return os.path.basename(path)
