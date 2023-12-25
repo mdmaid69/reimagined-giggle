c816041e9598dfee379a542ba1b438a379e81663
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:11:03 +0000

    choice offence pin middle compare

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def convert_to_binary(n):
-        return bin(n)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
