cfdba6d4f9424ad50333606c959f815af21141d1
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:14:56 +0000

    represent soak flood 567enemy strict

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
