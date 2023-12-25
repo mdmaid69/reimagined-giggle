2dade76078981b465187bd0ed9128ff4a9fd3783
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:56:01 +0000

    pretty building complaint whoever buy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def convert_to_octal(n):
+        return oct(n)
