7df4b109443ca81e03d6f101a30f2ddbe71934d0
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:15:38 +0000

    stretch expect and light dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
