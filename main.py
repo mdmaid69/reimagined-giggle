a62aba492284b68ca306729370ddf203f606a02e
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:59:29 +0000

    century alone hers class still

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
