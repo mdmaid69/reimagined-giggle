e8d3aa4696d98bf37acef4d6bfb049cb73c663c8
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:37:23 +0000

    most turn land another father

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def get_array_length(array):
+        return len(array)
