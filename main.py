a4ae6f7f52dceaaf6608bbef32198107ee31572a
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:13:01 +0000

    wish negative principle noisy couple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import array
+def get_list_from_array(array):
+        return array.tolist()
