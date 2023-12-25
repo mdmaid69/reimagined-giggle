be4533084b9a07d6dbbc574ced175c3dc68b8d81
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:12:56 +0000

    politics bill hate surprised fact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
