b43432cf0a06658dda2460efe3065809ed6e2219
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:53:37 +0000

    leg gas how match file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def calculate_work(force, distance):
+        return force * distance
