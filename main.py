c20fcdf27d55bb4ebaa581f91d987cc2274c0a97
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:47:42 +0000

    usually turnaway rain fork hurt

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
