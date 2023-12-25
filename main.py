bdd4bd9d34ce8dd20590b35aad1cc5527e2fc327
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:10:27 +0000

    familiar must bang feed formal

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_power(work, time):
+        return work / time
