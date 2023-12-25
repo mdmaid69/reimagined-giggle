a3ed3f54e75723f6a64cd0adadf8aed44522da0d
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:18:34 +0000

    interview cover figure car feature

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
