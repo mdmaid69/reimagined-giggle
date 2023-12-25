caac33fefec907ee41ff823bcc8349e2c6207f39
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:50:12 +0000

    remember foreign bill valuable smile

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def add_numbers(x, y):
-        return x + y
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+from collections import Counter
+print(Counter("hello world"))
