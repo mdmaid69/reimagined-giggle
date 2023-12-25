bf1915b1561bcb6a6cb31e2addb75db0b330a5a4
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:36:34 +0000

    approve rid present manner giveout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
