4f9be1e141519220f86b76704d5efa4425b1717b
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:12:10 +0000

    development stupid general welcome call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import re
+print(re.match("h.*o", "hello world"))
