602553cb8d7f021895f3799c11039a6f6f120d97
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:49:07 +0000

    important hard see rate account

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def get_array_length(array):
+        return len(array)
+  def sort_list(lst):
+        return sorted(lst)
