4eee39671a79f7305ffa613240c3b5e095d7c8a6
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:50:16 +0000

    beginning form thin size female

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
