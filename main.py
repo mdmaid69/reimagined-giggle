81f1dae4ab860cadeb92a1714798dc51202fc416
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:03:55 +0000

    imagination tap art nervous obvious

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
