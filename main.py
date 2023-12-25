0ac6abf23794d7a6e58195a4449ef2954b365104
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:27:35 +0000

    without natural sure card will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import random
-print(random.randint(0, 100))
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
