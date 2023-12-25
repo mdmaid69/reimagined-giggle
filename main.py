4b49dc90ce1d7583fd05b6debce8e66f3d473b26
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:54:36 +0000

    either clean inquire story to

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import tensorflow as tf
-print(tf.__version__)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
