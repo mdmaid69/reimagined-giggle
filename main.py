c8989bc373cf78eb96b1fff59c12b4c24589e5f9
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:28:07 +0000

    comfort move scream can road

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
