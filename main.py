12baf66d6cf6a9edb6097107a0ace086becf52cf
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:15:40 +0000

    promise much look nigga forever

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
