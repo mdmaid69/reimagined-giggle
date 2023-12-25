7ef7c271a7b3e93a6470d24eef02a56915ca6ec9
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:45:47 +0000

    cruelty fasten strict laugh conduct

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def is_even(n):
+        return n % 2 == 0
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
