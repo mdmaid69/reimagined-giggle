5b68b89dcafcef1dacd191a8fbcf8eeb8b2c2998
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:40:13 +0000

    many healthy time free club

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
