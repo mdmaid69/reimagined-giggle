28c49d7a168d281c8b09d924696418f08461aeeb
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:34:55 +0000

    note noone lie good platform

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
