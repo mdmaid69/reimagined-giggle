99f70300f535375d6cdb25b1da80518b1a7b5a34
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:52:21 +0000

    protect proof collect whoever class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
