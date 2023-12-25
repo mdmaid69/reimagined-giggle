5f15f66dce04cfb3a25d68ced669f4fc2d215137
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:52:43 +0000

    identify boring easy quick practise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
