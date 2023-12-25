839821924bb5830c333cf83b4a3e427ba5207b40
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:37:21 +0000

    clothes comfort plan entry stone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
