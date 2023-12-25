ac91fff1b87df68037a0370404db4269fb288b34
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:41:23 +0000

    exact dress determined kind government

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def reverse_string(s):
+        return s[::-1]
