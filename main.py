20eb43fb0d1bab484f7e93806820dd1b60a883dc
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:32:05 +0000

    of fine can act major

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
