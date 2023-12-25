edeafdb5c49fdf0a501550e8b8771770329e5fd5
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:41:43 +0000

    pump worth convincing rain loud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_as_str(array):
+        return str(array)
