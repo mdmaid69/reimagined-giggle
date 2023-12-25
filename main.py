6f857fff55636da1bc574cefb12cbfcaa3ea2974
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:18:06 +0000

    post group workon translate attempt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
