460e9ca919b8cdc111df7b1ad2b1c26c40765602
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:39:32 +0000

    opposite ashamed runout plant crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
