83c3344f62ef0eff4496a57dcce85b75cf512cb5
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:52:48 +0000

    key wash move share burst

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
