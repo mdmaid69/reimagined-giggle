64518eac18111fa4e76de569a44b6479865daa45
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:43:00 +0000

    than score long lend spread

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
