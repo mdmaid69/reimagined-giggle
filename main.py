df82edb7df6c7936ec1510f43a78348274ec789d
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:00:21 +0000

    available taste game be test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import array
+def get_array_as_list(array):
+        return list(array)
