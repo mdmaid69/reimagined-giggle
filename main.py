b6dbd780975f6dd2c30494d989a2e32d04884887
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:38:41 +0000

    properly sentence raw in list

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_index(array, item):
+        return array.index(item)
