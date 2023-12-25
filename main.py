450ece4dc9f0794bd434cd570f348c9228639f9e
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:59:16 +0000

    rob range reject dress dish

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
