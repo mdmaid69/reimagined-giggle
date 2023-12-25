fc9ded1d3f6fe2af42aca0557593318e96af0441
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:57:56 +0000

    pair poison ready dish each

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
