b7795ff66d4c51e3e9a40c45712eda3813c9224a
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:52:05 +0000

    eye similar break past of

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
