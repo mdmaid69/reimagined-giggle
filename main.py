18f5a0698ca96f0ec8d484ab02f4f56b1c9cc3f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:51:07 +0000

    mark into watch check name

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def get_array_as_repr(array):
+        return repr(array)
