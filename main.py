1b66a565d4453479c12eb4039bb1f3c276b1838b
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:45:20 +0000

    begin perfect demand thing bringdown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
