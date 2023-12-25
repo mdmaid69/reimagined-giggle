d7f72dda8bc70160e80097c1f9b5db5817c7804d
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:39:51 +0000

    cause doubt proper spite impressive

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def count_words(sentence):
-        return len(sentence.split())
-name = "Python"
-print("Hello,", name)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
