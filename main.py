616bd973287095cea02820a50ec2eac771117def
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:48:57 +0000

    woman whole across together form

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
