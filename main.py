94034ad2eaa775f403fb382dc0f349c67441d86d
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:43:47 +0000

    neither slice lookafter card bring

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
