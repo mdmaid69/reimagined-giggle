0bf1ea505eaeda030380670fd0ac3be046238aad
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:05:41 +0000

    disappointed pretend copy whenever payment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
