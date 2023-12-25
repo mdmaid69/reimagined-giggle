3de911d8b2cce51c126a34f3436ef758699d07fe
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:05:58 +0000

    century beneath interested press flood

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def cube_number(x):
-        return x**3
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+for i in range(10): print(i)
