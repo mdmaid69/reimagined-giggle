33efa111ff0ef7195d5eb5e603b832027425fd79
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:39:43 +0000

    copy right turn poison healthy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
