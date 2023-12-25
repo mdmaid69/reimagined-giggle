62af4ccb28c17cc671d3017f3b75200f8a23f877
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:45:46 +0000

    tear introduction clear pocket nowhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
