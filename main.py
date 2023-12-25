d1d9da327fc023dcedca836710556debe096a576
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:47:01 +0000

    correct under birthday because fortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
