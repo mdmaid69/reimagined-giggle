9e9d3c9962df7d5d9cf7ee9a0fbe98c21b7d4e63
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:42:32 +0000

    regional male cross passage lead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
