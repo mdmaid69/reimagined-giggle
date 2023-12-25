1b1f47d0d5c6fd910176f2784c6a82e13abf2637
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:29:45 +0000

    plant rule country face slowly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
