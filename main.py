fe9c98f11776951139a3fbea766b3b2e2f5e6483
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:08:28 +0000

    stand completely why computer accident

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
