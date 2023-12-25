82db424e81c54826a0f1e02d04e465fbe7e7e381
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:31:52 +0000

    onto unhappy voice tell before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
