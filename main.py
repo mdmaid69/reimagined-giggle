80a9270582e27017bba53004b5c1186366e272c5
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:44:11 +0000

    her until Oo undo announce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_power(work, time):
+        return work / time
+import platform
+def get_os_info():
+        return platform.uname()
