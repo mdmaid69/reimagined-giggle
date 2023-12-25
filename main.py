9fdbcecb81af5944097195207a5a35312dff3822
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:42:41 +0000

    OKorokay suggestion race extraordinary Mm

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
