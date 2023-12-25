bf61eb4368144bba22358b797804d498088e42e2
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:50:55 +0000

    especially conclusion laugh duty quality

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def find_min(numbers):
-        return min(numbers)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def find_max(numbers):
+        return max(numbers)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
