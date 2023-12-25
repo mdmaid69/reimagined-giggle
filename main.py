07bd66048597f8f539f06992843655e42d34f131
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:19:17 +0000

    mistake pause discover refer apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
