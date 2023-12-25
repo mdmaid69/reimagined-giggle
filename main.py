87821c89b3db51610baaeea823923aefc29e39ff
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:33:09 +0000

    tie share job remove suit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+from collections import Counter
+print(Counter("hello world"))
