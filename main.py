a6e6d5ca580079a3048ae361eb62525d2b6019f1
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:49:43 +0000

    sit guilty boat organize on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def split_path(path):
+        return os.path.split(path)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
