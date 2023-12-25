eef960e4b423a276f70c826731e887eaa411ab0b
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:28:47 +0000

    steady search near breakout pick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
