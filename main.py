aa181b873394f4d8e56f895a4033e98c90af1d92
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:01:59 +0000

    instead petrol mine year slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def convert_to_octal(n):
-        return oct(n)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
