6af946f106963201cd7d654b31651dc08d8241d1
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:49:13 +0000

    praise sticky project Dd disturb

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
