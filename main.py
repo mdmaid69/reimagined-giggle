8a6b4abb298afef5a6d0f714492c9e4f6f68f1bb
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:31:00 +0000

    survive shop fire deliver moon

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
