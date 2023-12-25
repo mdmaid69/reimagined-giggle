42e1d14a44625e29b57e56a822267b28cc6c3ced
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:48:25 +0000

    mass least obtain goon custom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
