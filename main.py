dc933bb29532d873c499e7115b674e4879cd340d
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:48:13 +0000

    like risk nonsense mine make

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
