2d1ca1088a514ebd570a963c72b9047b45afeac7
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:09:50 +0000

    treat simple old type meeting

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import sys
-def exit_program():
-        sys.exit()
+x = 10
+y = 20
+print("Sum:", x + y)
+import math
+print(math.pi)
