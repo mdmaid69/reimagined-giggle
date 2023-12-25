f8132cde2facdea3418bb8b604c8380e5b6c2d3b
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:17:14 +0000

    getout breath mystery dance object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
