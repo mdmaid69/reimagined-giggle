b9ac83ed5f416459eb3d2dfc1e5cb265a15ba5f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:12:01 +0000

    common instrument storm middle Gg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def clear_array(array):
+        array *= 0
+def calculate_density(mass, volume):
+        return mass / volume
