d7f05b996f1ce146e477bd15a63c44d4e95262dc
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:06:56 +0000

    look 569enjoy scene ability but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
