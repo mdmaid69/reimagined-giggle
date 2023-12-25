c9e727f52c1f51a1e7a0c7e1349558285eaa6195
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:06:46 +0000

    shine large certainly plan glass

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def convert_to_octal(n):
+        return oct(n)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
