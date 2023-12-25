68b36e9bee82b5515bea0ccc3715d6904d2f2ff2
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:26:06 +0000

    only right trust top dry

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def square_number(x):
-        return x**2
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_speed(distance, time):
+        return distance / time
