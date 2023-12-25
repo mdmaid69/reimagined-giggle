e73b4332b78044c0a05e07ab96d791d8c1f63274
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:34:00 +0000

    agree music 565end listenfor talkback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
