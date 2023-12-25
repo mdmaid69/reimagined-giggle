af853e2dc52914bc8afc3156267d3be3599470cb
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:23:07 +0000

    home window frame nor as

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
+def square_number(x):
+        return x**2
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
