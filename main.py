b14ae7ff4765793b52b99dc6242e25c405231a5e
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:49:17 +0000

    remain apply such Itself fault

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_complex(array):
+        return complex(array[0])
