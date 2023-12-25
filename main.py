47bdaf227310f744dc95ac8cde449805434ba3d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:40:04 +0000

    squash city not trap when

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
