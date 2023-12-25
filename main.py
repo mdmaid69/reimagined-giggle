86aa7cf9bc74a127a3ab5a5ecc23a86f3f27acda
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:16:45 +0000

    manage with achieve support weather

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-for i in range(10): print(i)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
