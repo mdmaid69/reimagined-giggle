0895d788de5908179ded3cd6c3670c7246a593cf
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:08:52 +0000

    beyond step spray central correction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
