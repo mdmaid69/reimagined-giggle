628be373907d82a9af4b2508e69cef82e38fd96b
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:45:28 +0000

    welcome taste direct lunch excellent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
