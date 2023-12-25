b30f5ce5850c174d2d9d2ecc0c9504b973667431
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:52:06 +0000

    tidy common drawing shout effort

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def cube_number(x):
+        return x**3
