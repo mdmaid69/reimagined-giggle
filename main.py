46a0dc877a3f2c3185b372675f01d21a4579911f
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:48:19 +0000

    fault natural threaten steal would

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_item(array, i):
+        return array[i]
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
