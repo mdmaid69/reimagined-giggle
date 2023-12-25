8423e9e14aa07a0585f21f8589b41fbbf3e6bdb9
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:15:44 +0000

    burn exaggerate slice Kk container

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
