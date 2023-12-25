a4c5295af624106112087f07068e1f0b7d005054
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:00:44 +0000

    breakup pleased beginning trick mainly

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
 import math
 def calculate_combinations(n, k):
         return math.comb(n, k)
+  def count_elements(lst):
+        return len(lst)
