db623d47f010183249fe839fe3d2259f3fedd400
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:52:52 +0000

    accordingto mouth rush offer content

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
