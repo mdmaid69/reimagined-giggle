0d51ab63e1bf128cd380a918be497a43291a81a8
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:01:53 +0000

    pronunciation duty introduce instrument surprise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
