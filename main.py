280d0f3820fd26202ffdc22e25f5f42afe7e7fa5
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:45:00 +0000

    earth after undo violent cruel

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
