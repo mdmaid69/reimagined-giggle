fcb8ebf8715a38ef1fd954457639f89383538776
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:10:52 +0000

    until contrast breakthrough slow must

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
