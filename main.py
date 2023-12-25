9cd102c7425e40e1d97760cb73e0a12dead54df0
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:53:25 +0000

    realize calmly duty order decide

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def convert_to_hex(n):
+        return hex(n)
