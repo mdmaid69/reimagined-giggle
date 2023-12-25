dc3eea1ef9187b037e2cc3c0815d544d39056db6
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:37:33 +0000

    respect afraid flow transparent die

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
