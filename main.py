a9267ddf288c5cc1ebc8b2140155115fcfb833d5
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:43:44 +0000

    exam impression much total development

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def greet(name):
+        print(f"Hello, {name}!")
