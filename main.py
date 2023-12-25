5b60a51d08401e70c37f72caad0ad07898d4ce60
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:13:22 +0000

    soon relation anxious sometimes visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def is_odd(n):
+        return n % 2 != 0
