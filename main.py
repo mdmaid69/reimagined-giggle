c6d936270c5fe8998c7ce44e6f190449de30ccb3
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:44:55 +0000

    manner if pot narrow wealth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def count_characters(sentence):
+        return len(sentence)
