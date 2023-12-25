542d75970c8f8fd33bf8e0625f9159b51cefc2d0
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:03:41 +0000

    centimetre surprised advise cheap sky

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
