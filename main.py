1e895d9768b8a7cedb55f90d23ced79b841c8a90
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:51:17 +0000

    choice statement thing little impressed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_min(numbers):
+        return min(numbers)
