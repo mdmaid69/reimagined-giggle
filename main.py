e2189e14d106203abdc627714a765a45dfddfcb2
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:39:52 +0000

    clear how behind become vote

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import re
+print(re.match("h.*o", "hello world"))
+import os
+def remove_directory(path):
+        os.rmdir(path)
