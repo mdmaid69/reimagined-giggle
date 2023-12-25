65b92e5e2fb9f08258661f2855b73747a3d951e1
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:34:56 +0000

    concern drop medium admit apparently

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-def convert_to_hex(n):
-        return hex(n)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def sort_numbers(numbers):
+        return sorted(numbers)
