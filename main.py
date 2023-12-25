7fa31056e16c3dbfaea3b8dcf25ff18024425293
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:36:08 +0000

    alone start want notice comeover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
