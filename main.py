78263eec842256bb48e21eb59b438ff2161f5bb1
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:16:15 +0000

    theirs scheme breakout pain total

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
