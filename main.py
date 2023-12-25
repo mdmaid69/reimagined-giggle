f7fac0c83d8fa690f91730a54f1d24c750a8d255
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:33:35 +0000

    press fill solve thing lonely

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-text = "Hello, world!"
-print("Characters:", len(text))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
