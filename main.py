4daea82a2fb55d9ff5a3cd88b4dc8b75082f7a07
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:13:34 +0000

    mess dark badly tip neck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
