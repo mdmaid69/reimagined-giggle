5cfa65b3757391003f436b70823b286c9215ed0d
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:22:52 +0000

    window mood somewhere vote your

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-print("Hello, world!")
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
