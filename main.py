e05f0edbc10bb06ec955d320a735ae3ca7340d58
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:58:06 +0000

    door task introduction convincing best

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
