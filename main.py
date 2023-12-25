911f430effecc7a989583defc88583594ba40cb7
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:13:27 +0000

    turnback far rank who contrast

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
