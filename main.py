bc060244b27c3237240650c6449d28adc7f216c6
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:49:40 +0000

    love mother should study upwards

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
