7b97e1b0d75115db42706731bc264917d3621b92
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:12:11 +0000

    discussion interfere size how bother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
