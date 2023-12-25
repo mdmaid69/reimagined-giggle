d01bc71963210e46ce6cfa58a48cee5b281eb92f
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:01:36 +0000

    talk tip beautiful noisy cheap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_area_rectangle(l, w):
+        return l * w
