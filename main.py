973b748413a382f45e9f336eed75a8ed13a18de2
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:46:12 +0000

    mine control eye suckme ourselves

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
