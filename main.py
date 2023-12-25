bf828cd152e33cf20a3922f31c08bfc087b0a050
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:33:38 +0000

    equal translate powerful group surprising

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
