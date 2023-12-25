178bb49b8a791d6ec253f714311471e64707225f
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:22:51 +0000

    giveup press suppose bin happy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_pressure(force, area):
-        return force / area
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
