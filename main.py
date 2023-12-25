21b42f04b6d10778e43b96ac8b8c39c1e056abac
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:17:16 +0000

    drop welcome describe round note

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def sort_numbers(numbers):
+        return sorted(numbers)
+import array
+def get_array_as_repr(array):
+        return repr(array)
