e9adccfcc3a26caae5c22c3a91c40d8b955f5bf2
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:32:28 +0000

    whichever authority be patience many

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
