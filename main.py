c69a4d9d40741eedc802a1ebd6eb9e3e2df3bd74
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:10:37 +0000

    whisper family whisper professional instruction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
