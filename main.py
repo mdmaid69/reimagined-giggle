0602a12f6b653682dc82085b2c02c3d6edc599c8
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:41:58 +0000

    breakinto variety long related force

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
