fd57a64c8ef3f9953c0d9af3251cf3cb46f4047a
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:57:20 +0000

    pupil major floor active on

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
