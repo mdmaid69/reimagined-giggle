4a881ca6a6f90781da12f8a01bd6b8611895bf06
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:42:52 +0000

    tap below introduction early insist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
