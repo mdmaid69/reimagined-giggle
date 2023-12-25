d7b7ff920998a1050bcd87d4ddfd45d3e811f379
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:53:33 +0000

    makeup whisper peace substance to

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
