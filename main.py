5341b6164d9eb81095d9bddcb8e090bdebbb1546
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:39:40 +0000

    contact we develop adjective pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
