eb956c9c889ccb96e22713150e1d8dbb28c2ec7e
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:18:08 +0000

    within takeout abroad voice standard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
