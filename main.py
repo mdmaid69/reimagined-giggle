8393a38dfb88958e5b1f3d8ae4c3b26ef673bf9c
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:45:24 +0000

    steep politics yes course number

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
