e8fd7f3eec627302964a00e2d96400b7cb504282
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:55:31 +0000

    battery maintain object breathe hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
