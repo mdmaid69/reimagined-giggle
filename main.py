fe35277a299f1fc3bb248450e6337728f703078c
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:54:35 +0000

    woman flat guard guide lost

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
