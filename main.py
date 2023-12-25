8b3d81e1b68cd62476e0bf9f90a7f5ab70f68f57
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:03:02 +0000

    account similar test concerned opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def is_odd(n):
+        return n % 2 != 0
