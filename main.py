4081d6a306b7830b9e12b2fced30cde7562f3f00
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:24:15 +0000

    getaway earth whole contact tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
