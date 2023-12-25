4c96987fd8fc3048097622965e8f573b94728ee8
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:23:51 +0000

    welcome fast occur health copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
