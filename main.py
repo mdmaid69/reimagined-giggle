6b50067b8ab07ae3803c3ef1e6b3600c600337a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:28:55 +0000

    extremely please list Cc centre

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def reverse_list(lst):
+        return lst[::-1]
+def convert_to_octal(n):
+        return oct(n)
