d1116f47bb1f72695a1c413de353a5d73daecd57
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:18:17 +0000

    above powerful zero table actual

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
