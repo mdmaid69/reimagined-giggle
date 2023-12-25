cdbecea57ca8ab9361d18d71f1b0bba1f6fa198d
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:57:04 +0000

    people mend excited disturb mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_blocks(file_name):
+  def get_file_blocks_allocated(file_name):
         return os.stat(file_name).st_blocks
