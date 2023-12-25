83f6069a943c82fb08e832d074fe115e9be637ad
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:41:40 +0000

    pretty duty nature detail week

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_power(work, time):
+        return work / time
