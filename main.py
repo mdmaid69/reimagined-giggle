4c898aa18fc773ab9ad841b9d4545a9c1fcd101a
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:08:27 +0000

    care performance knock main unfair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
