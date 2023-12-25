6205f16bb64be61a1de505a0ae0a480ebef4a4e3
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:22:51 +0000

    private frame of decide university

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_pressure(force, area):
-        return force / area
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def find_max(lst):
+        return max(lst)
