6b8027fc5034a7f36e922f4a798b6c5514ba179e
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:22:00 +0000

    cook callback hard century issue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
