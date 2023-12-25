c0ca278ed3c8099a2845ea6643addc4de0497e62
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:39:16 +0000

    patient second spare past direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
