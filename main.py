cf2ef100d180105604b062db5d450887ff68004a
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:21:04 +0000

    that hear listen storm charge

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
