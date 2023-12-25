3423d8559798622d5974f5a1a8fa039f7ae594df
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:14:08 +0000

    yesterday every case loose control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
