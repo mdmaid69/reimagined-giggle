7e8d32a7b9305ebade72a75146834082cb733b82
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:27:50 +0000

    picture shame punish whatever lonely

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
