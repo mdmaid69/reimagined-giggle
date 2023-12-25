4ffc7a65127c233120d059bd9f7231574885117f
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:14:26 +0000

    arm disturbing contact willing practise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_volume(length, width, height):
+        return length * width * height
