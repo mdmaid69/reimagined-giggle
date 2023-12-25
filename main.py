6871cb1efb08c517769c9b2f2324dc3898c411e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:36:49 +0000

    display workout weather into tall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  def convert_to_hex(n):
-        return hex(n)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
