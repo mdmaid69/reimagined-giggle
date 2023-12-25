13b22c0ad7f0b070f91218e94ea4eb65f9a16f83
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:43:39 +0000

    maintain strike trip idea study

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
