eeafa034ba3f1e91ae77b90ce76518a4d4908c50
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:09:52 +0000

    same hole lie air way

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import math
-print(math.pi)
+import random
+def roll_die():
+        return random.randint(1, 6)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
