884f284e5896cf3682aac537be557b1dff583965
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:26:23 +0000

    573entertainment behaviour direct attractive embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
