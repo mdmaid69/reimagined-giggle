b377ed1d875c16d9c0fa70643232529e8ba9870f
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:19:22 +0000

    tough minority beginning comeout comein

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_density(mass, volume):
+        return mass / volume
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
