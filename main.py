57603a1a0aeb065fc6f47ddea3fc551e65078cd1
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:43:38 +0000

    understand pour face sink needle

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_power(work, time):
+        return work / time
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
