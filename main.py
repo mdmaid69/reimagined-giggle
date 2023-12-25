225d124ff3d0510298e7bcccd282f176fd325f5f
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:48:17 +0000

    depend mean stretch congratulations flight

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def find_unique_words(sentence):
+        return set(sentence.split())
