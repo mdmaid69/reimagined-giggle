a69e2a5176b440b8b7bfa28908d11ceef9a60fd8
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:41:43 +0000

    listenfor call meanwhile workon outside

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import array
+def get_array_as_int(array):
+        return int(array[0])
