195a4d8b3a8efdc458d814090664d413b1c7a45b
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:27:58 +0000

    quality date Uu Mm education

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
