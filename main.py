96840607d1ea300c61d424cd2f349cac20803f45
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:51:26 +0000

    little scare alive mind education

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+print(sum(range(10)))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
