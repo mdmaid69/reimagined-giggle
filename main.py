4b71873369b9ecfb0ece1cac5ef60b42e6f599a7
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:42:01 +0000

    sport turn good building or

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
