87ceade142629cbbbe5ff20d0e4d8cc8bd0599ce
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:54:46 +0000

    pity city forever ordinary least

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
