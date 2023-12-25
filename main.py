bf9c3a929fbd158ee9e2cbf68c073f2d919d3952
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:59:53 +0000

    always fond himself lend wild

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
