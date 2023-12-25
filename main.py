61f82dac6c75bc5f50441bd37dd669ab6f76cee8
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:18:52 +0000

    be square run basic interesting

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def reverse_list(lst):
+        return lst[::-1]
+def subtract_numbers(x, y):
+        return x - y
