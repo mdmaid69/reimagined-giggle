e2b0b942fd8a98537f6bb68bd9b8976fcbab2a40
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:22:44 +0000

    measure lock combine raw trade

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
