1cbea5525ad5f7054506aca422e509407a72f341
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:20:49 +0000

    order furniture similar real interested

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
