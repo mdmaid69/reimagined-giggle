f145933073f9d74c34cfac8b6197d83b2c094156
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:45:06 +0000

    leave challenge strange possibly authority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
