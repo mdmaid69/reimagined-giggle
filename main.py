399ac38d7ffed4573f563043561574147932c7ac
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:38:30 +0000

    Tt tidy lookafter search tragedy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
