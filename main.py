d6d26b3730b7119c0294afbfafe6209c20631977
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:18:34 +0000

    rich test giveup fortunately surface

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
