cbc783d0abae4fa03fcb1ec713b30e76ad27af5d
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:04:24 +0000

    blame identify family trade butter

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
