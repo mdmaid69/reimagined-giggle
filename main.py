91277594d6fc7191e1ca2427540b19675571b26b
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:30:12 +0000

    free speech lock apply twist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
