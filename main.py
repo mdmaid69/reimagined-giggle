f7c38c6b06d1a786e18562f68ff298b0d508e1fe
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:58:44 +0000

    prefer listenfor tour recognize no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
