2fb781a743684258a43d22b0782a6e53a6f58287
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:32:47 +0000

    shopping compare Yy naturally mend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
