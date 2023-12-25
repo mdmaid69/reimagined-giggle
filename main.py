e1777d84a12c7dcd3f128f51a2e6728f84af9a70
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:32:39 +0000

    sister petrol right should confidence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def square_number(x):
-        return x**2
+  def remove_duplicates(lst):
+        return list(set(lst))
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
