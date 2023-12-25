e3149f0787728e782f73fc0ec7b5dcd9505ec659
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:45:50 +0000

    married hardly belong holiday cover

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
