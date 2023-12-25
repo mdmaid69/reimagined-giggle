9d4ccc66144abdea17c301e8111d59a9ec735310
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:38:05 +0000

    among insist set passage medium

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import sys
+def exit_program():
+        sys.exit()
