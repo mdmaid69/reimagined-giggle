5602079720f562f2265c28f0dbd274954d826f0e
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:20:39 +0000

    flow turndown provide everybody lose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
