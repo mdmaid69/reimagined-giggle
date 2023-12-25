29aaeb1b86a8da50542c050b38cd26b723cc4463
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:27:42 +0000

    see difficult mail ticket separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import platform
+def get_os_info():
+        return platform.uname()
