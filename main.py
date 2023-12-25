ecdbe8a8e43c2f1183dd0d383c6248e149b68865
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:59:56 +0000

    plenty effort cheque damage contain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
