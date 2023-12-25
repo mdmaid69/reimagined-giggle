618dbfec315e3580884407cb7d2edd5f2c402631
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:33:51 +0000

    interest narrow want weigh unlucky

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import sys
+def exit_program():
+        sys.exit()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
