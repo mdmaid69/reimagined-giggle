33b9306c96074876b13934e29ba355681f81270c
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:11:43 +0000

    mother see myself busy importance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
