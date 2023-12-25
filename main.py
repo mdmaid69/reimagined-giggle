e7085d4316c7703495167b49fd12e93a3f442972
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:43:07 +0000

    spread pain over prevent eat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import datetime
+print(datetime.datetime.now())
