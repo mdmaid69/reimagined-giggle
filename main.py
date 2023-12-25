4206a2af8352bebce5b213c0777ec06f4efadec6
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:44:14 +0000

    hospital store raise worried murder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
