10eeb7bd85773dc8319d14714c287fb9f2285e09
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:16:05 +0000

    trial let compare opponent out

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
