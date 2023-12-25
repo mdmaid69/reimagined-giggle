cda157a6b241b17f580b8bf8d6b39d4997095efe
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:22:16 +0000

    extra friend try charge inside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
