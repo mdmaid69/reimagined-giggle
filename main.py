4e37abb0c5962325058aa1c4867da7f73e62ca7f
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:58:43 +0000

    complete kill responsibility its write

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_acceleration(speed, time):
+        return speed / time
