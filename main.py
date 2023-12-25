359af0fe427097619bd88fe2cebb7af051a6608a
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:07:26 +0000

    agreement formal scene instruction cover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_max(lst):
-        return max(lst)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_error_function(x):
+        return math.erf(x)
