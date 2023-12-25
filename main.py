bdcdfc15efbb7fdcfc15b063798f06acce33661a
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:10:26 +0000

    takeover positive know raw secret

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def sort_list(lst):
+        return sorted(lst)
