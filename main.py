92e3142fca7df5589a65e6c5c2064d6a19b3fb16
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:32:39 +0000

    temperature environment news wish perform

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
