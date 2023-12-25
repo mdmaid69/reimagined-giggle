4cf1bc4a180cf3199bebde8981324888129dc5e9
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:02:37 +0000

    save trap onto kick somebody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
