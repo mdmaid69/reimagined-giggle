57d29bfaa989a2c818dc80094a8447f63d6703f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:42:10 +0000

    properly slightly job love slice

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
