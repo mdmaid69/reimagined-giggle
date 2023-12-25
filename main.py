f1cf9f45b99d0b7037d765f509d3fbe277dd2664
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:30:37 +0000

    Vv wire separate comment Yy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
