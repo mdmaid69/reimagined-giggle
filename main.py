064cce8cd71d9f2d2cd8640c3b874c8e39af0c5b
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:11:57 +0000

    ordinary cut finally fact float

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_int(array):
+        return int(array[0])
