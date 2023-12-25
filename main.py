82c3564964e3ca08c6a917e13879b2601153a035
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:32:26 +0000

    shadow police characteristic behave punishment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
