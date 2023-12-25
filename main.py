a2a7999110213dd13afbacd6a3bd1222e88aed55
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:51:11 +0000

    ring system comefrom idea you

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
