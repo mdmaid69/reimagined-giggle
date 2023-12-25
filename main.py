492be6ac4f4f94fbf95bbfd215e2ca37321623d2
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:38:00 +0000

    mistake excited stand mistake mention

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
