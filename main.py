9a98190506cbda3ee737e033e73a53483ed0b431
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:56:28 +0000

    new mark computer hole achieve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-for i in range(10): print(i)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def reverse_array(array):
+        array.reverse()
