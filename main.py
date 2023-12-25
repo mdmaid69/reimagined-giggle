21ebafc75dc954e60630e6dea3d3cd6a76638888
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:08:28 +0000

    who badly ahead possibility 571enough

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-print("Hello, world!")
+def calculate_power(work, time):
+        return work / time
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
