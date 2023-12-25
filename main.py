e4bf8bea11a382f150569648aeaaf49f627866ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:22:47 +0000

    if sort shut command answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_item(array, i):
+        return array[i]
+  def remove_duplicates(lst):
+        return list(set(lst))
