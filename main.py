15acae7b86979edced50dddcd532a0c3d27caf24
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:43:13 +0000

    order comeoff welcome charge talk

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+from collections import Counter
+print(Counter("hello world"))
+for i in range(5):
+        print(i)
