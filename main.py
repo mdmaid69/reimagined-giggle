4ea384f6c8d15b3da9ef455ec7df37d7afba7fe0
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:25:38 +0000

    Cc stone capital letter mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sys
+def print_python_version():
+        return sys.version
