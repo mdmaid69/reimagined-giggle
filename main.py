049cc9c20c2b15fdfe7bcafdf20d3c4a1036b2d1
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:19:59 +0000

    piece narrow complain unlucky fierce

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def convert_to_hex(n):
+        return hex(n)
