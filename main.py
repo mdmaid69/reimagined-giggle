946bdc5bf42d1147f99c3a5cd126ef743bcea7f0
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:39:59 +0000

    wherever basically float address search

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_area_rectangle(l, w):
+        return l * w
+import tensorflow as tf
+print(tf.__version__)
