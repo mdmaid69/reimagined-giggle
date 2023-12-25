d111616a9a6f24d87469c0f19e6eed099572af09
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:21:15 +0000

    lean sticky class calloff foot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
