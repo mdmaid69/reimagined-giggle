cec49386735a285d840efe3d2aff0e17b912beac
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:28:08 +0000

    copy traffic trust Ee question

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
