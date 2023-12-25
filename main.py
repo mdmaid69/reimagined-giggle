67510aaa72dfa3859c3dac47fbb3bf486d64e483
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:21:37 +0000

    takeaway ago advantage personal flight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import sys
-print(sys.version)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
