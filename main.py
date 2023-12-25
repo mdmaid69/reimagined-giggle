93eea880bb741c3a7bf9e5043786bc08618602b5
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:41:07 +0000

    walk tend present turnout indeed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
