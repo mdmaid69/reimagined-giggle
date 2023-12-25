8ceac8cb40f2c7f0ba363f8b7b5796a110c72dae
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:00:35 +0000

    destroy survive mix sure dare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-def get_current_time():
-        return time.time()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
