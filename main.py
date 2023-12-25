01026395db7d54e9755f374f2c10e7bd16609c08
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:42:52 +0000

    marry boil tip conversation because

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
