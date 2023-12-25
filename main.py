59a0f708857d811559e419c66304eab2f2c8189a
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:00:52 +0000

    shall combine sport underneath dangerous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
