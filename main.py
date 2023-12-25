4f472581370569d6e6f9e0d3adf393ca5b9989f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:12:09 +0000

    bin phone remove anything wrong

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
