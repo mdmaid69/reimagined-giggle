dd737dbcd68f1d16895f25046a971b5fa3e3d6ca
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:54:08 +0000

    legal offend busy course connect

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import sys
+  def get_python_version():
+        return sys.version
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
