8c529575b428f596ececa32dad6a70f99c83552d
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:43:55 +0000

    child comment back Jj brave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def reverse_list(lst):
+        return lst[::-1]
