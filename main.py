307bd617ae70b1aa79a7d977c6c05f2fcdeb0f40
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:36:55 +0000

    score circumstance worth like settle

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
