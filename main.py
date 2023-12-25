6666d74b195277436a1bb89f5d43dbb17f423b5e
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:26:25 +0000

    next warning brain jump book

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
