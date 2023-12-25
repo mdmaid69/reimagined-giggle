ab2c40bde61fe9b3b8c3c1a715d518cc2201aa6b
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:17:32 +0000

    person bury moral thank see

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import platform
+def get_python_version():
+        return platform.python_version()
