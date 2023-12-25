037ad2d519378425e6f3aa1df68170246cdf8f35
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:58:42 +0000

    even afford smell speed always

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import sys
+def print_python_version():
+        print(sys.version)
+import os
+def change_working_directory(path):
+        os.chdir(path)
