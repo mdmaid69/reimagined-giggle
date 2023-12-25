56b7ec5aaf3a1cfa1775bd946ef96cecedf124d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:31:05 +0000

    seem character care responsibility foot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
