edb85ca56b00033bfd1a0b3d1817850251138578
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:49:39 +0000

    welcome odd notice identify size

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
