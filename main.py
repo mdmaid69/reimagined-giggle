a8e994b93265be419e5f574120f54dfab79ba84d
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:35:24 +0000

    reduction whenever score recently danger

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
