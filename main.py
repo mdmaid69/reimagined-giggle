e78973b8e9de873128a3e59409475d14eadf25ef
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:39:59 +0000

    meet practice lower slightly shake

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def find_unique_words(sentence):
+        return set(sentence.split())
