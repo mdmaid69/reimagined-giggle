0c5b6515e2d56b0d9eba91808e1bf10570d12bf7
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:32:16 +0000

    she port exam situation pot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
