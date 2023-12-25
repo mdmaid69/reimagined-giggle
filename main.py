dd78cb954461c9406726dff595ac4adbbea69c4d
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:29:27 +0000

    contact to distance safety majority

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
