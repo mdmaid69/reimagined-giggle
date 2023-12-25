b22bcc7cf4649ba3f0c0847caf848ac43edacb55
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:52:13 +0000

    music become finger cum read

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import numpy as np
+print(np.array([1, 2, 3]))
