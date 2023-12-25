99dd07ed77c69af88f4893e4917d9e7a1967d313
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:59:21 +0000

    attempt shout minor official whistle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_error_function(x):
+        return math.erf(x)
