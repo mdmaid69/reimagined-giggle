21f9a333cce1c407f89cf7e138cd10402de09da8
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:26:16 +0000

    wish smooth glass difficulty discover

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+for i in range(10): print(i)
