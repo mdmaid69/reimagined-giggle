b6a53bb7c34f35afef9051cdd49c4a9f47dd9d77
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:13:05 +0000

    here wide Rr principle criticism

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_work(force, distance):
+        return force * distance
+def is_odd(n):
+        return n % 2 != 0
