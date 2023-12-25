7fa4b9ffef28a928e0e0b57e9a2ee5e9c54bd71d
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:02:56 +0000

    rid which difficulty escape hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def print_python_version():
+        print(sys.version)
