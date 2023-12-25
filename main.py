71f127a678150662fbcd25564fe9efe0be0b852e
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:12:44 +0000

    view mean bad valuable zero

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
