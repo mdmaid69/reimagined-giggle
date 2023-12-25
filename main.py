0522d60d6871c99abe4316393f777189ee7f7f0c
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:58:22 +0000

    body door press failure deep

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  def sort_list(lst):
+        return sorted(lst)
