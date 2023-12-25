db42d7b6e2c6bcead30ebee7e20595f82ea5e009
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:44:11 +0000

    argument beat bringdown along open

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def split_path(path):
+        return os.path.split(path)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
