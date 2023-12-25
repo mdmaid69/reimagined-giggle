c49a89b31d30520546e70da609b0f6c01240b9f7
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:32:53 +0000

    them sit order cost amount

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def calculate_work(force, distance):
-        return force * distance
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import collections
+def create_user_list():
+        return collections.UserList()
