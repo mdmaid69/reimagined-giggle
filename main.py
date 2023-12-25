76d630012c1be3b7410cce3d8e9b2147ebd54ad4
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:35:45 +0000

    wall head once Vv poor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
