43e9c44dc77e9ff36f122c3e75a5af5469b95ad7
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:40:21 +0000

    accommodation just better truth turn

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def find_max(numbers):
+        return max(numbers)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
