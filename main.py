0228112147f32b5079dc582cb5701ef5e5e1dbb1
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:48:51 +0000

    must turnback fear score reason

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_base_name(path):
+        return os.path.basename(path)
