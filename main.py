b2b4ccd6ea7d2184b17178ce815cfbabe422c0b3
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:07:57 +0000

    comment behaviour issue appeal unfair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
