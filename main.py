e5dae3f4b3408f1d9b5afe56123992ead61128ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:29:10 +0000

    poison important bye upstairs in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  def convert_to_octal(n):
-        return oct(n)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
