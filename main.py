51b37e8c97736f29f92107d908d7a6d0f84f0d2e
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:32:00 +0000

    past guess shut gooff estimate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import collections
+def create_stack():
+        return collections.deque()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
