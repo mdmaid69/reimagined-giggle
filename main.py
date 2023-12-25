99026c8aca6bcc22034f72bbd894d162ff59a19a
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:57:32 +0000

    feature may except impressed rank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
