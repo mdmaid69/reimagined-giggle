5e2b2179ea10a2e140a98118423190b5dc1d5977
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:18:50 +0000

    accordingto involve bound personal zero

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+print("Hello, world!")
