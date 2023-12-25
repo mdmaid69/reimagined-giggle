b7f056812531d933d898c5f93b1996b695d8a6a6
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:34:17 +0000

    poor connect contrast circumstance 566end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
