22d4d20eb5aa16b0d961844f5d6ef2f1f183b8c7
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:43:53 +0000

    move announce black through negative

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
