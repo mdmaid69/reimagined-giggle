088535e9548395d8119f0cba169c51b66413b5f9
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:30:36 +0000

    train available rest basis else

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+print(sys.version)
