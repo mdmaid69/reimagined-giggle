434d41be1c158889005f333e9300c7c810d2d0c0
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:36:57 +0000

    experience mass formal thorough book

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def convert_to_binary(n):
+        return bin(n)
