5c4b05700bab7dde94ed0e871c37ad4c54c4192d
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:47:40 +0000

    board afternoon voice path radio

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
