7444f76641f20039f8271aaf5ebede7f9372beba
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:22:48 +0000

    match certainly wear able present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def convert_to_hex(n):
-        return hex(n)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
