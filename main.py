9f65aeac0a750b922b9e32f6c5f8220368ad3792
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:34:52 +0000

    task themselves famous Hh pain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-print(sys.version)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
