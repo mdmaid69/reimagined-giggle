55be1e1f8994effba7baf1a304e20c9032c64a83
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:00:35 +0000

    shadow unit below press talkback

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
