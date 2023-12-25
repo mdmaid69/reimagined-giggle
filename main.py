e2997ab60150e0579cb27967d9137249ed8124bd
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:38:01 +0000

    fold nurse tidy level middle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+def sort_list(lst):
+        return sorted(lst)
