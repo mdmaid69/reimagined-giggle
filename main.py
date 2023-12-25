c34abd66e8ab92ed01b26c47197774a72eca180f
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:15:23 +0000

    reliable call fetch top acceptable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
