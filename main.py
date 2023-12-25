9692e14ca0adc52695e25e9a9375b6f1befc08c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:41:38 +0000

    manage whistle usually earn workout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
