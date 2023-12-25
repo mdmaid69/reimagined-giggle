c5b1603a1fa7d8f8016f6744ce87b5fefb57651f
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:44:57 +0000

    case hard average ring prepare

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_density(mass, volume):
+        return mass / volume
