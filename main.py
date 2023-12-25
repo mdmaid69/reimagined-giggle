1be3f060767598e50e84edcdb8cae042695d9089
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:54:40 +0000

    colour gas wrong attention rush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
