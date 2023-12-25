354b69555bdae97f138d676eb5430301461d1f17
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:06:44 +0000

    blame turnover sort needle mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
