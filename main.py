2eaa90617275b695e8deea72564f15a715da6ae9
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:10:21 +0000

    ready well settle thumb tooth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def reverse_string(s):
+        return s[::-1]
