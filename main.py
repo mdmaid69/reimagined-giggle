d9180e180a8426a55ba789d860c0e039c51f7d3a
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:14:48 +0000

    temper shoulder news plant slice

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
