005667a28a5b49f28acd6d2bec1a63f377ac2ee0
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:36:28 +0000

    middle row road branch duty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
