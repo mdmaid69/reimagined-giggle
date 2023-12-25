bea91233c0706b51471b4c9ff1b940f70d90e528
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:05:13 +0000

    single ball answer sensible damp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
