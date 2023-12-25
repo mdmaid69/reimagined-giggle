75840dbc0ecd88c3b4c73f8314c44833ea6141b9
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:34:46 +0000

    ticket precise basic affect hour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
