133572778e7ba37afa97abf7909def4764ca35b3
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:51:38 +0000

    bridge much actually strict arrangement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def convert_to_octal(n):
+        return oct(n)
+import time
+def get_current_time():
+        return time.ctime()
