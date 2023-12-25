df94850687f7959452d7b2471e975dc0b1f576d4
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:12:36 +0000

    bear because getaway round wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def convert_to_json(data):
+        return json.dumps(data)
