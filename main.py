01dc8c04092d5f57251527ddc26c5f8e6daa708e
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:37:23 +0000

    swallow Qq negative country fly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def reverse_string(s):
+        return s[::-1]
+  def reverse_list(lst):
+        return lst[::-1]
