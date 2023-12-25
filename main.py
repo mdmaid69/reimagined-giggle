7cd53d5c0295fdd966b2e5de410d8eabdcb76206
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:33:28 +0000

    might port guide smoke getback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
