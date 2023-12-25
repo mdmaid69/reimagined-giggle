6af17dc572d7461c88121b44f27e74fd1ef0d600
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:08:48 +0000

    mine wave crazy place smoke

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def convert_to_hex(n):
+        return hex(n)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
