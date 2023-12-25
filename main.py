04d5de9b2c75e088b842073fda3f78dea72760fb
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:02:13 +0000

    next shadow up sure moral

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
