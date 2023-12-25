d88a5a6e6a7cf76950d412723748442252a653da
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:56:32 +0000

    narrow clue normal on relief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
