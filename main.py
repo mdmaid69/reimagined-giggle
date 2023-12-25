eb3b9ed9fd9b01c75e4992f6e67b35a4aa2f05db
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:48:37 +0000

    sick effect study comeoff sting

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
