9adb659d49c1fd66d9c4098ee9c2a19ca51440b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:07:54 +0000

    564encourage usual unpleasant reaction swing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
