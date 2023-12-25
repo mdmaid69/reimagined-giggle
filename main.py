69655df09e2575442f374b78e80aa11f33431a09
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:05:12 +0000

    side church pour pile black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
