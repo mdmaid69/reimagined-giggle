7d79fee3155266a5dfd6b9faf2445ee32129e19f
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:49:45 +0000

    list trap appear date reject

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_power(work, time):
-        return work / time
+def sort_list(lst):
+        return sorted(lst)
+import json
+print(json.dumps({"name": "John", "age": 30}))
