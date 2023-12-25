5574c8b3a85eb576d92df85a61ee86e6a7a135b9
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:49:40 +0000

    lost with able young issue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
