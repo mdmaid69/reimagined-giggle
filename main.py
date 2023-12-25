e793e93701fd43e1b4246951e030148a0416ef78
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:23:53 +0000

    priority nigga hate activity community

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
