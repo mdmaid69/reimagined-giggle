cd3a8d528c4d9c817fbb46ac7d85f8047737517a
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:58:26 +0000

    shout hang turnon taste winter

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
