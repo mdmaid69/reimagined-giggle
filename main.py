b23f567f34b74be99a1ddbbf6ed306d385f50f6d
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:01:29 +0000

    practical complicated hair doctor calmly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def count_elements(lst):
+        return len(lst)
