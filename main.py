a1d93384669dabbb723430ad61f9ed9470add5cc
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:45:18 +0000

    surprised breakin match bend proof

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  def add_numbers(x, y):
-        return x + y
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
