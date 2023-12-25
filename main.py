77b2a9b0c8979c0eafdaa782824fb05b30d06ff0
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:48:40 +0000

    represent education around interruption Uu

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
