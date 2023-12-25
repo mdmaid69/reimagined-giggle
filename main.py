eaed141c04a7bef6935d465bd7d0f0ce3b438891
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:29:16 +0000

    conversation force hit worth present

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+print([x**2 for x in range(10)])
