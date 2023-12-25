4d288d3674c8af8bb64eab362e24eb2eae572aa5
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:16:59 +0000

    fortunate mad truth 571enough as

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
