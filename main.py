2eb4cb66a8d837240bbf771f85c6d812aab4b626
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:25:43 +0000

    environment lunch suffer bend story

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
