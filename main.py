2a99de0e0d7ba77ba873cc9d756a03f73a1fc320
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:39:13 +0000

    appeal physical Qq excellent thing

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
