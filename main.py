400be45291bd1f1739df6339e5b8f373f57db0ea
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:46:29 +0000

    set draw warning direct change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import math
+def calculate_floor(x):
+        return math.floor(x)
