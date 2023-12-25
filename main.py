0e123ffa9a280b1eb1a3423b5aa6e19fa4ef789e
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:03:24 +0000

    discovery careful total secret wave

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
