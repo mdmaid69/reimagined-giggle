a7b27088f1bb64a3f36ff6a4d9bf4d4d7775c41d
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:48:26 +0000

    join common refer nerve square

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
