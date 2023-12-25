58b44f968b182bafe6f1f80763686e7d82ace2d4
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:34:15 +0000

    whisper while callback exist keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
