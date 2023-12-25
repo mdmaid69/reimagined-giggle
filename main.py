c9434df4d4069c7debcd05a06eb6dfef9949b3dd
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:19:32 +0000

    coat present before swing exciting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_density(mass, volume):
+        return mass / volume
