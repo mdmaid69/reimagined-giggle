440a45bbea4c74af63ad2e090e23fe515356ab45
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:50:27 +0000

    comfort left standard goaway fine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
