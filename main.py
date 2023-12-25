2040f8fcc3ecc3a9c8de14c228725634a99d44a3
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:41:24 +0000

    body floor appeal frighten mother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
