b873c7430fc777c49e6594bf4ac1ff8e1e94a72b
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:49:45 +0000

    neither cheque dull feel begin

--- main.py
+++ main.py
@@ -3,6 +3,5 @@ with open("some.csv", "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerow(["spam", "eggs", "bacon", "ham"])
         writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
