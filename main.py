f0970da81692b0b462eab2c7a7c7a58b4ea4f205
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:45:57 +0000

    backwards really mail programme maybe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+print(math.pi)
