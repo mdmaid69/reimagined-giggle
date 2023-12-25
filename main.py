0b0c7764cd0c4fefbb08a71fee6a0a0f6a45ab73
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:46:02 +0000

    judge freeze light excellent sign

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
