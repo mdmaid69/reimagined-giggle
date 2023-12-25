ef95b5dd974369ab370d6df13fc97a9969e3c9ee
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:51:56 +0000

    together wait unfair children beat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
