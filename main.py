15e4a3300072b84cd6d3bc7d20b23ae9158d2a4a
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:47:49 +0000

    heart minute noone muscle try

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
