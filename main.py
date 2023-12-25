8535850c277bc02fd027f38a3b296d7f126c3fdd
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:37:26 +0000

    meeting identify criticism burn takeaway

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
