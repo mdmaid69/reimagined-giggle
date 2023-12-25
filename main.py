983fead77dd81d7abc8a9ff042c8328a4fd40d23
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:51:30 +0000

    return wear day issue ride

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-for i in range(5):
-        print(i)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_work(force, distance):
+        return force * distance
