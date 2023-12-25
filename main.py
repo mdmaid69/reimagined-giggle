2c8ae600609db4499293264a278d7b398aa0366b
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:26:45 +0000

    copy confused with move callup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
