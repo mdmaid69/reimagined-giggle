3e3710bb63f284c79e8619f5284d5931ef37ffba
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:48:23 +0000

    flood journey interrupt anybody circle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
