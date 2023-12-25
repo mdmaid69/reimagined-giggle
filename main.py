7f8f6981f2438ec4b09aa00fe3ee43163eb731e6
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:53:07 +0000

    whisper actually autumn demand talkback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
