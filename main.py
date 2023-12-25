84bef0cbb114e81c9338b6dcfdeebe1fd0a1983f
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:11:29 +0000

    involve willing and beautiful would

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_itemsize(array):
+        return array.itemsize
