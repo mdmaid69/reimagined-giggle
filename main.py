6b23ac286e0df8532de5f65c12a12a3ab64db65a
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:35:30 +0000

    unlucky regional middle instruction alone

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def subtract_numbers(x, y):
+        return x - y
