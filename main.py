6f6a2333a5fbbe1682100988b3d708232f8fedba
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:38:58 +0000

    dream abroad share some wander

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
