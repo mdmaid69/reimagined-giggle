f68bf313e74f4e8c07869cd323d2ba455b132741
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:52:00 +0000

    operate left list whistle force

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
 text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+print("Reversed:", text[::-1])
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
