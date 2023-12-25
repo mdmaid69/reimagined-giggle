2978e9a87f0a19400f7cc43299423c27cad08107
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:46:12 +0000

    appearance preparation some low back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
