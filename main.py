be8bde3331ca602a9f950ac1c1cd228d6781e92b
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:46:52 +0000

    wake mark the dig valuable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
