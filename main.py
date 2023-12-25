6134666c7a12751bd1f4dece1bdb591826add8e8
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:22:02 +0000

    recognize miss urgent broken suckme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def get_array_itemsize(array):
+        return array.itemsize
