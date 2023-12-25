55d06e9618bfd7d70379f59964b1b6728dc5aa42
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:32:24 +0000

    comeoff permission proof wake respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
