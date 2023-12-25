ab4f77e13d1c63cb355b99c5d84afc41db10960c
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:24:57 +0000

    poor butter refer usually office

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+print([x**2 for x in range(10)])
+import collections
+def create_queue():
+        return collections.deque()
