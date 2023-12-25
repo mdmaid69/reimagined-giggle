7441c27a1766be1e52d8529ea7682ee835a941a5
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:32:24 +0000

    flat colour still which fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_min(lst):
+        return min(lst)
