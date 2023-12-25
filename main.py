8a180ce78850afa2374111f6151a3afdb20ea3c6
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:28:51 +0000

    change Ll later immediate none

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+print("Hello, world!")
