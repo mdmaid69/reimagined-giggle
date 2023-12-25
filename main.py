cdfb41c2ced625bda73de6c78591642c96690664
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:24:30 +0000

    odd favour brave visit accept

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
