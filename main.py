a0e753b28c0118adf357693975d22798185004fc
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:12:32 +0000

    wild store last drive letter

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def get_array_as_list(array):
+        return list(array)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
