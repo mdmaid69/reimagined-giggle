f34b50b6810d419bd007e36c530344b698ea5c8b
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:14:33 +0000

    regularly drop side cheap what

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
