2b390ac618e63c82af096e0561f641a74edefae8
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:46:21 +0000

    responsibility dare pity talk quantity

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
