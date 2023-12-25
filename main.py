1445624005ee893a01a40f04839b411669360c76
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:15:29 +0000

    radio week show not 569enjoy

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-print(sum(range(10)))
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
