c3513d2e0541b6ea808844821b96e21a46f93373
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:54:34 +0000

    voice attempt nearly independent bother

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
