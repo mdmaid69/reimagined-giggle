341a4dfff3c7a5c270345f0fda0651eb736cc5fb
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:30:04 +0000

    rob over journey smooth brother

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
