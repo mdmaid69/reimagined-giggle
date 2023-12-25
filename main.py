f71a0998bbc58f80a29fe3e2a7c2a13d332da85b
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:39:16 +0000

    wall reasonable scene record communication

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
