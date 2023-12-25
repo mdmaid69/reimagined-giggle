7643392a6e69857ce99ed2f01b2e59333f217d70
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:38:10 +0000

    insist takeover small nervous impatient

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_base_name(path):
+        return os.path.basename(path)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
