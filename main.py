3aeeeacb684a0f6692c539e41428c5428916f769
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:36:21 +0000

    clearly settle bite keen dear

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
