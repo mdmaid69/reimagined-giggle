981150449c1d4bde19c6ab2460e3c786698e4b94
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:25:34 +0000

    basis knock appeal act charge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_index(array, item):
+        return array.index(item)
