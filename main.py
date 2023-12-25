27c9cb7ccd1cc85d802d2acc740321ede79d30e4
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:24:22 +0000

    legal direct face speed cruel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
