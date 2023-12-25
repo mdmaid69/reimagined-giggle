73a642176ac779fe7d6c4f186d1be14f96194571
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:22:24 +0000

    turnaway comeup child after stranger

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
