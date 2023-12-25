0ed4c6efbb8900fe2d85d7d63cd523033d7affe6
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:22:42 +0000

    waste secret cost dry obvious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(10): print(i)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
