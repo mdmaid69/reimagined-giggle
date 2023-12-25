d415ae8fda97782b3d439a623a14483ae49243a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:37:34 +0000

    switch rob notice except professional

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sys
+def exit_program():
+        sys.exit()
+print("Hello, world!")
