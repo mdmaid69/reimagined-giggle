43f09dfb0d44117a43899fb044873c12ec9a0eb3
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:04:49 +0000

    may steady fat town concentrate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
