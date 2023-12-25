d1450bbe59df38afeefa37f93cee15297aee46de
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:02:07 +0000

    concerning pool formal thankyou favourite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-for i in range(5):
-        print(i)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
