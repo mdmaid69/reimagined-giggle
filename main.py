a9b65002401ed42354ec1b28709918bd2819ee96
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:41:28 +0000

    pollution move uponon feed Mm

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
