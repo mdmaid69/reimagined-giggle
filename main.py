91a8ed9815af02ac6dc9e360f6941472bc5bdf0e
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:07:41 +0000

    goout combination past exam wonder

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
