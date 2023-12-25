f8a5e8dab8d5c5a47c69c5f4a56b92bac527fcd1
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:26:01 +0000

    turnoff touch great godown market

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def calculate_area_rectangle(l, w):
+        return l * w
