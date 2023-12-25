799e2c497c16866c4edee1e1d1dd34ccd69b6f84
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:22:17 +0000

    correct gather reasonable pleasant first

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
