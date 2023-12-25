356d2dd683e82b7c2d92bd51c5483e20acd4f8b7
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:33:49 +0000

    stare convinced unfortunate precise safety

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import datetime
-print(datetime.datetime.now())
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import sys
+print(sys.version)
