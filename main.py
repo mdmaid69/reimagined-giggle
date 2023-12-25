7f51dfe6be293c6c7c3b7a6e5fca69af2048a24f
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:08:08 +0000

    disappointed share seem uncertain tense

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
