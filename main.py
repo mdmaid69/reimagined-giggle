3125fdf263e4b1a9747530d52623f57058c2e86b
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:22:09 +0000

    thing silent fault stare butter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
