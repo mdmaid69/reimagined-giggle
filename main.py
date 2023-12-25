dd4492c9d1c8641026de46c92bcb97603c6ca2b9
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:54:31 +0000

    beneath offer transparent connection freedom

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
