2c82b35185b2a4add2669281c76821356be98218
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:53:31 +0000

    rise stage cheap pity about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
