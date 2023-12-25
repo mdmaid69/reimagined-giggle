5473f848cebc0765b0bdcaed9d18744b9daf5441
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:06:43 +0000

    range undo sleep radio watch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def count_elements(lst):
+        return len(lst)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
