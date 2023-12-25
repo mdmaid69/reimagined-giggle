d86c6f5dd4364e4295671e2b94721610fbf8386b
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:35:28 +0000

    wind come off ride image

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
