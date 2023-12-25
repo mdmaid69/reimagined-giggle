48d587762d08e2d0667fd860a38654f90fe9bce3
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:46:50 +0000

    shallow wherever minority crowded tape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
