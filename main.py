5c05fe8a41912fe10b59ffb88e8b8b2677d9b006
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:59:12 +0000

    realize shake fire attach easy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
