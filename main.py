d09daa3a99527c0e4a7e2ebcb665501f9bba5d00
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:06:31 +0000

    hurry correct shake black disgusting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
