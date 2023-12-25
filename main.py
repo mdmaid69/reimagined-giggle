10c52dab984669437001ffb46e7671092370beaf
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:53:38 +0000

    tap precise write add float

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import sys
+def exit_program():
+        sys.exit()
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
