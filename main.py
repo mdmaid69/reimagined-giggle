f37392dab6cfa8684137273e859f03f9e0034fef
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:14:21 +0000

    history wide declare unfair negative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def get_array_as_list(array):
-        return list(array)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
