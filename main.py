0c61c1c5efd2fcc1feed59f72bb0190d67f7dba1
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:32:56 +0000

    loud first formal excellent satisfied

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_as_float(array):
+        return float(array[0])
