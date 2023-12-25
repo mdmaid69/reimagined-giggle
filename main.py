e64e314c78e5d2fc54415ce528450b28179cd91b
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:58:00 +0000

    yes every realize government continue

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def convert_to_octal(n):
+        return oct(n)
+def calculate_density(mass, volume):
+        return mass / volume
