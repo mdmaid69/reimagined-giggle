ae2ffabc0fc72e1531bf8bbcecb867f26987377c
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:40:01 +0000

    between build wide swing sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
