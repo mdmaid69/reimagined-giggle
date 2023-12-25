9994d243a111a01e935ead6b50533521e6967898
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:28:20 +0000

    what shame slice workupto duty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
