f87ad4b156fe2450bc9be3dfd965a244ccc3d847
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:46:15 +0000

    shout supply dick put left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def count_words(sentence):
+        return len(sentence.split())
