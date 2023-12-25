877e698443fb16308236a17aeaa4a8093a1476c6
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:34:30 +0000

    ring war previous on middle

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
