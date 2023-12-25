a1bbe8d0237f1b3c5df1b30b25e2f96de8d28966
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:13:48 +0000

    estimate different nonsense top everyone

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def is_odd(n):
+        return n % 2 != 0
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
