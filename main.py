6e3e6f4730cf9df14acbad2fda7c0f4ab28dc5a3
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:00:26 +0000

    breakout sure hardly breakinto outside

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import functools
 print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
