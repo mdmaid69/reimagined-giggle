635e8aaedf1ebdaae6970e59ef65df6a09feb29d
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:32:30 +0000

    well good chat image bunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
