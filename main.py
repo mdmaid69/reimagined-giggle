b15a5310d61ac571d3506a863266ba1f19ef2dd0
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:20:12 +0000

    relief switch better drop mail

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
