30fef50462d1fdde2475c46862f30420ddb99895
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:02:42 +0000

    view concerning his OKorokay leg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
+def cube_number(x):
+        return x**3
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
