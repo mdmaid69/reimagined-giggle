8a548e6924ba124804e4518fde8c29578c9a8cee
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:25:50 +0000

    again pronunciation translate hungry offend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def change_working_directory(path):
+        os.chdir(path)
