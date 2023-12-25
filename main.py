b281640ee5955b86f817cdb04d6774a19730cdfd
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:05:08 +0000

    conduct than count check but

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def get_array_as_set(array):
-        return set(array)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+print([x**2 for x in range(10)])
