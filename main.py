2ca0b849a7677e765e4aa989c025a26b8e36eccb
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:30:08 +0000

    hide workout unusual file beautiful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def is_odd(n):
-        return n % 2 != 0
+  import sys
+  def get_python_version():
+        return sys.version
+  def calculate_area_circle(r):
+        return 3.14 * r**2
