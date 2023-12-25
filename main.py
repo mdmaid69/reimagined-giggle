18852e1072a3a5b5549d3fcae769606947c18c19
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:57:27 +0000

    operation throughout month who collect

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def sort_numbers(numbers):
+        return sorted(numbers)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
