86c2f42c29be3d5fc8b573f3817a18142f2effc7
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:18:13 +0000

    announce passage slice sort than

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import time
+print(time.time())
