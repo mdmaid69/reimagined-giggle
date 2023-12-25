b329603655a81942e10fb4b626c4fd19b4403ac6
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:39:39 +0000

    Cc field performance play leg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import json
+print(json.dumps({"name": "John", "age": 30}))
