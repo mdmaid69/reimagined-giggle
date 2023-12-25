c903c4d53b14bac5f587304fe41f4ddb2c28f8e5
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:36:33 +0000

    will tight extremely music season

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
 import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
