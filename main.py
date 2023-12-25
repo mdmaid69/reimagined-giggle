83005120bb70a1f2a59e5050cf63c5df55f8e761
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:23:21 +0000

    forget convince goback(to) file dull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
