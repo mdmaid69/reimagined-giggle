4b4b6c6bcafd0e6c4645a1850f121ab4a5f3ee86
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:23:46 +0000

    invitation concern please culture exact

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def is_even(n):
+        return n % 2 == 0
