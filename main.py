caa9765ac92eb517889a7d724db015eea84b0278
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:52:18 +0000

    punishment result opinion judge greeting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
