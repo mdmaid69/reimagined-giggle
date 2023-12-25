7dd7a220c32358796191934a96820493c7cb28ca
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:16:41 +0000

    mean strike breakout system busy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def square_number(x):
+        return x**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
