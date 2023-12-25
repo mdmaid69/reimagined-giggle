1687bba8de3febf1b5e4d82e81d0c56b961081c5
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:55:29 +0000

    manner mess giveback have ilovedicks

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
