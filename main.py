c8cb1c1b00a28191d3c06670bc4ca935f38743a9
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:30:15 +0000

    draw may into money method

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def cube_number(x):
+        return x**3
