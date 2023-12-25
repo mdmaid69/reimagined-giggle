5491c84b04572a1fbc18e618ecec81836cb91c8f
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:06:09 +0000

    soak at dream sweet human

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
