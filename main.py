e6b71a9a8800bc0feb2b8ee1cd91992c91d4426a
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:23:03 +0000

    safe exact impressed voice age

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def reverse_list(lst):
-        return lst[::-1]
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_current_directory():
+        return os.getcwd()
