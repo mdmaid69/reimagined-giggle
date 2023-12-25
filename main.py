c0706a9f0598379ec00bffeb7ee9522438b9fb26
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:19:59 +0000

    free excuse where bus soon

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
