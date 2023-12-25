6ad6f303c0f2355feccbf480d3653d66f6705c10
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:56:13 +0000

    shade meeting mother cover do

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print("Hello, world!")
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
