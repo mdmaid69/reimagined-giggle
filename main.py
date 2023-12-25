a8c96f58d7f54f0d009695cc88ab2ee66a69fc1e
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:08:00 +0000

    everyone whatever run slice phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def remove_duplicates(lst):
+        return list(set(lst))
+import datetime
+def get_current_date():
+        return datetime.date.today()
