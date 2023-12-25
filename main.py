64d469aed84b636e6157cc090ed9fecdffee084d
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:56:49 +0000

    lock bury union motion trust

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
