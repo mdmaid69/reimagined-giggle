e9a346b43bd8de738e737d9751f19e6c6b21313f
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:17:35 +0000

    road thanks neat regard open

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
