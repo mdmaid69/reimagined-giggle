98bc10abf5f6f80e3c2750614071043c441b6fe4
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:39:09 +0000

    original common design odd best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
