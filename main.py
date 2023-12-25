8a924dfcad13c019e90f02bc65fbd7630c46611e
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:44:25 +0000

    path party suggestion throw about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_current_working_directory():
+        return os.getcwd()
+  def sort_list(lst):
+        return sorted(lst)
