4ad05e6c5a25fdf6c6933e1b92d237a3e2b12d0e
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:32:07 +0000

    half giveback spring feeling line

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
