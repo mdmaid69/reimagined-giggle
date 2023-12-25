33cccd1f8f1d0a1adcbe77cc76873e6f4b04b0f9
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:38:36 +0000

    tight live base feature bite

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def calculate_acceleration(speed, time):
-        return speed / time
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
