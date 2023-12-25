7157f745805b8da2cea78e183ffaf80e31ed09c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:56:23 +0000

    begin exact from interrupt amount

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def clear_array(array):
-        array *= 0
+def get_array_as_tuple(array):
+        return tuple(array)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
