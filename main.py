1fdb07a4f66fdc710706553d5053995757f55c02
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:59:14 +0000

    delicate ashamed collection clear along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
