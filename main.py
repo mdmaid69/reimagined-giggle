fa6aff524655c6a5824e4559ea15f3e1164eaf0a
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:28:41 +0000

    tell Rr related pair type

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import os
+def change_working_directory(path):
+        os.chdir(path)
