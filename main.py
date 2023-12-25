0b14cebeb2fdf13005ae130b08fe8a19cfe38b4d
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:16:23 +0000

    speak opponent 565end offer where

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
