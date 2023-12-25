6cb2d6713478f0dd46e017a0ac0201f71d8f2dc2
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:48:11 +0000

    difficulty separate deserve exchange direction

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
         plt.show()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
