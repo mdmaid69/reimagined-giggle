4cec6f00b1eeddea0e5413ce5adbffe1c9e2c2c5
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:28:19 +0000

    exit care pin beginning town

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
