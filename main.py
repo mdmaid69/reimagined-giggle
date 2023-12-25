44289d151449090ccb3323af5cd482bd11d780f0
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:51:46 +0000

    sudden cruel turnup land comeround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sys
+def exit_program():
+        sys.exit()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
