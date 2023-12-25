57a1f504432e4f7f0ebda4d905d695581dad131a
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:57:37 +0000

    freeze innocent nature afraid slip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
