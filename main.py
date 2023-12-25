5bd5f55b7af64fec1b774611eadf8109c8116f98
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:45:04 +0000

    save no test direction cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
