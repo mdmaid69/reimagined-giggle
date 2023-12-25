7635917814db17d2eb8f782c959d0ad553761d88
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:08:08 +0000

    everywhere flavour range length live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def is_even(n):
+        return n % 2 == 0
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
