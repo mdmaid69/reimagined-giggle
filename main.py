e7ddbb832f97ec5b8a8acadd06c096a7ee5ad3dd
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:47:17 +0000

    branch hear blame disagree convinced

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_density(mass, volume):
-        return mass / volume
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
