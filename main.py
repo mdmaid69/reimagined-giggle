5c9b01db97bbcc9051f479f5fea056ca37200eaf
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:22:44 +0000

    come spray rough swim exploration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
