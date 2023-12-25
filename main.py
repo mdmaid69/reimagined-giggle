a2bf47dde933265ed6e8e0042b9ccca41ea7f644
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:35:12 +0000

    after attach busy seat emotion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
