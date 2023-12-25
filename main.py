359213f8bc3f498b4fa6bdebf9b8cb3ab1eb3edf
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:13:11 +0000

    scream fork object guide in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-for i in range(5):
-        print(i)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def count_words(sentence):
+        return len(sentence.split())
