ab892e7b2f911e71e54201837d2ba346b984af5b
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:02:50 +0000

    call suggest sleep stick unlucky

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
