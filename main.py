297df6229d9689fc82be6b2f93aae5aa42905c70
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:19:58 +0000

    star some tomorrow crowd kind

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-print([x**2 for x in range(10)])
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
