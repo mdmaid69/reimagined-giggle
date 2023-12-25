e8c1f88ea87ae139fdf09ee6ce47396dcaae7a3a
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:48:07 +0000

    custom noise advantage ground pair

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
