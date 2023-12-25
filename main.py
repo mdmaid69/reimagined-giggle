5de5302acec8b524cb03ec9eec168c9e8eeb88df
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:17:38 +0000

    else shrink off quantity promise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
