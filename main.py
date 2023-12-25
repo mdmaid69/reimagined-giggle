03ecc4291031b372f5fac33d8728c100b964af4f
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:00:17 +0000

    getaway obtain shop people maintain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
