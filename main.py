de39f456c511c9facbbb0ec3c30b8e46f4ee0ba0
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:43:26 +0000

    number point rush fond story

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def add_numbers(a, b):
-        return a + b
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
 print("Union:", set(list1) | set(list2))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
