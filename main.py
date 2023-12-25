118d75c3b4fc3ab85eca72941404255e9939e789
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:20:01 +0000

    make under another well no

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
