8c14ec5b4640a016303482813bcf6e2b1e4572e2
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:31:37 +0000

    fit constantly hospital brown less

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
