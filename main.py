b187b7e97deebaf3c80c8a26ce9bf5fd8c9a0266
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:55:06 +0000

    public measure become run callback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+for i in range(10): print(i)
