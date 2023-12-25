e4abb151f9666a20528d95d088e4471f9c705128
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:35:30 +0000

    mouth rent instant settle why

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
