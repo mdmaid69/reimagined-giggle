cfcdb0f283411cba4f7d9ce2f8b215b34f70a957
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:09:52 +0000

    contents process part Cc capital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
