e6594ac7b76d4763c4be49c4dc45c9eb2f1d94cc
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:18:52 +0000

    keepfrom squash new exit accidentally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
