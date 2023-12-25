61cfc8489bf916c292d73eac9f8a44739ce9992f
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:59:22 +0000

    brush whoever interview steal big

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import platform
+def get_python_version():
+        return platform.python_version()
