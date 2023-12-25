90de84e914642dfedd640531d34f95c7cdf801ba
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:14:27 +0000

    moral material union however sea

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_sign(x):
+        return math.copysign(1, x)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
