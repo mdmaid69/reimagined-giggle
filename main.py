789c51847e79770129fc6a5b07195fedeb8fa44b
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:51:00 +0000

    place male public various bringback

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
