a6a20e1a7748079974984ca1cf92ac7990100542
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:39:15 +0000

    drive damp apparently mouth stay

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
