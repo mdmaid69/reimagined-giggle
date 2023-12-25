6fd65dd6591e9b40f4123ecdbbd274ac3830cb90
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:34:25 +0000

    pick giveup search its finger

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import sys
+def exit_program():
+        sys.exit()
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_as_str(array):
+        return str(array)
