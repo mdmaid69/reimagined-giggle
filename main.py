e8d618666866bb34be2982b4210cddcb1ba24931
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:42:54 +0000

    watch autumn notice backwards wake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
