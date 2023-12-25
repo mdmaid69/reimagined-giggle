fedf26a4920193f27afb34f04b03c4951204f974
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:27:14 +0000

    mention case before ours convenient

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def is_odd(n):
+        return n % 2 != 0
