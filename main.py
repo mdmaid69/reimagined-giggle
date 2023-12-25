d92298a43cac95e99df469403785f620806b4be6
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:04:45 +0000

    flat suit science introduce possibility

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
