1586538145a4733a7180b9fe1c57e37ed3f297b6
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:55:52 +0000

    female slice common sticky rare

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_speed(distance, time):
+        return distance / time
