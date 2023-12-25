2a859ed0fc6e915e6e6f9dd5d969a1254cec0da0
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:16:32 +0000

    comeup kind exam fast tool

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
