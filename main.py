ab7578f0a947e759399b1725bc374e703b647e80
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:46:53 +0000

    agreement comment television 570enjoyable priority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import platform
+def get_os_info():
+        return platform.uname()
