a08e5d1ba1e666cf2469a7766be405064f9fa5ce
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:38:51 +0000

    start bin find contents gather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
