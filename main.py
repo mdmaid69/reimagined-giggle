358bbc0d256c59b616f75bd51b515086edd9454c
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:49:20 +0000

    giveup bottom variety slice slice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
