5bfeea88616d210dfe8e5366554ff265f6c55ed4
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:56:37 +0000

    comeback neither hide mood total

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def add_numbers(a, b):
-        return a + b
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def count_elements(lst):
+        return len(lst)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
