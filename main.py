9b88c9f94d93945558cd9871428efa0499fe837b
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:36:39 +0000

    line seat bridge suit he

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
