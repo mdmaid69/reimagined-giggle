eb539fd7ff46a1284fa0ec23e1c67c3d15fcd842
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:41:32 +0000

    repair mixture mistake doubt alone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
