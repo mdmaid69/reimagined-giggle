d51593fa54b395b4ebea55d34920b298e62376aa
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:59:30 +0000

    back cross wave stare congratulations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_typecode(array):
+        return array.typecode
