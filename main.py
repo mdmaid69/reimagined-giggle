73e3ceb6ed746f4fef89b454dc28ec8cd721149e
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:39:15 +0000

    later discuss rather middle burn

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_error_function(x):
+        return math.erf(x)
