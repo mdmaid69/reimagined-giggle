8d9173c7ea0a07acdf27cd54f270e95915ea49fc
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:16:29 +0000

    run consult quick seriously mind

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
