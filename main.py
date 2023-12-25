d35403d5a5d530b6e54ae98a8ab3403b3b7898f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:18:25 +0000

    downstairs priority style 573entertainment container

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
