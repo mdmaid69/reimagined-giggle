afdca1bb6f57eb6e06b225b7517ae06c7278281e
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:00:05 +0000

    score point measure interruption basically

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
