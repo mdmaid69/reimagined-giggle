5c83ae73892f9e2e8243a8290ce217051fc5139a
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:15:57 +0000

    awake conduct deserve design broken

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
