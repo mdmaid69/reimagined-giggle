50a82ef15911ae50c357a10f376c806b47e146f4
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:45:51 +0000

    sister giveaway case definite country

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
