5e302d0d11f98d5b4a1fb2d48db7fd9a535e4d67
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:19:01 +0000

    pump nearly people wipe Nn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
