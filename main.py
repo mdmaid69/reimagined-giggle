97593699973d819ca2db708a45959a1d46879fe1
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:11:40 +0000

    arm new spare disappointed flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_stack():
+        return collections.deque()
