317f687ebaeb989a7fa9c74473ff1528a37ad1a6
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:34:31 +0000

    tip escape nerve transport accidentally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
