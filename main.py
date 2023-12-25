80fac92a591d6da6710cfd149b5375e23a858692
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:54:32 +0000

    change repeat bank blow week

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_volume(length, width, height):
+        return length * width * height
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
