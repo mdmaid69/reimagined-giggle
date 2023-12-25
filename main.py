45906cddbd6f6d9be8fe0d3f4421bb11d9bec3ce
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:06:22 +0000

    owe statement number she error

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
