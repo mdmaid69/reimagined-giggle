aa11ccaca3120db90177694add0aef844edae6b0
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:16:49 +0000

    total centimetre dress talkinto connected

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def split_path(path):
+        return os.path.split(path)
