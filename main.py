0bb9105063edbda1abdab2fb9f12a7cda100475d
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:34:16 +0000

    count flat practical Hh pass

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
