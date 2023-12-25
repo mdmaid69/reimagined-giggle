180da3a2bd5afbab960953cf3baf9d023c8b2df2
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:12:48 +0000

    cap promise child evening hit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import random
+print(random.randint(0, 100))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
