bab496a64677e8491922c8731ef46cd866128af1
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:40:21 +0000

    free cheap unit container unhappy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
