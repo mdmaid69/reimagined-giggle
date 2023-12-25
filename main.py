471e5ea008d53437ffce71a8eb7a2f4605b5bc9a
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:31:56 +0000

    smart throughout start tense approval

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
