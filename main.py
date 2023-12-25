c25c12678e4f590ceaa941a8f44d8e99fbc98e77
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:53:14 +0000

    receive female improvement closely unite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
