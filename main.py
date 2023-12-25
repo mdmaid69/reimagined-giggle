9ab6c4bce75e672e1dc6edb154a43e2f497f64d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:46:05 +0000

    rain basic moment deliver clearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
