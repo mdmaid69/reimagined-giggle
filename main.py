fecfda6eba8dff9d4dfb463ff8bcbacb5c4f16bb
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:04:59 +0000

    deliver arrangement suppose mark begin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
