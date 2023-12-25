338b4c139f87fcca9729425f67001c1832284052
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:57:24 +0000

    bringdown Ww represent clever takeover

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
