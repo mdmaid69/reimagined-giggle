61483d2a6850bb55f0c27d188e6df06904e2d1a6
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:04:52 +0000

    mess pure contact target future

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def greet(name):
+        print(f"Hello, {name}!")
+import os
+print(os.getcwd())
