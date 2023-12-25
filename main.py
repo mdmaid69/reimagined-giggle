d44a3a7d29ae0b2ad6f228eac5e3fd3443a4593e
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:22:36 +0000

    rather pipe show keepoff partner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
