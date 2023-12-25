68927377b58c8dac5e097a1ea24283efa8c933d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:39:38 +0000

    various plastic whisper strength test

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  def convert_to_binary(n):
-        return bin(n)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
