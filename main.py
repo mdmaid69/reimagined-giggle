5feb265cada79b00100978a9f03f1ba46ee0f97b
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:47:07 +0000

    rate under price present buy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
