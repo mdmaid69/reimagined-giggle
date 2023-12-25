f1b2ca014e7a80d0a1724c2291f8b55b41fab539
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:48:53 +0000

    key who move human lookaround

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def is_palindrome(s):
-        return s == s[::-1]
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
