2caca95d3069d409c28fe25e73ee3a032e0a0499
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:30:03 +0000

    business sink interview lucky eat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
