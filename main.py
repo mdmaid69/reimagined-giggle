6ffd6c061eecdd6427301fd55f06a7ef17c2d638
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:43:23 +0000

    cap comfort piece stupid modern

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
