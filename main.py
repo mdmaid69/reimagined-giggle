f574f5b227a7ed3c168094120446eccf1df1a1bf
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:42:44 +0000

    size future illness pity give

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
