d8b3fcafecb5d1b54f10a790e5907732af91096d
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:45:37 +0000

    wet Ee cross tune rid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
