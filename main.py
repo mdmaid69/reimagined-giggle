93201d60e47b250dee1dc6b3e829b42088441c35
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:57:45 +0000

    mine house usual emotion know

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def get_array_as_float(array):
+        return float(array[0])
