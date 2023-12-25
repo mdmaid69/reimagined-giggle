10e0a1bb7b4cbfd4f2b96787724961d814c2750f
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:57:12 +0000

    quantity reliable their Hh month

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
