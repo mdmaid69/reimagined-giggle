2d8609a4b729f4b252cb96a3ed87c634c18f42b4
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:49:57 +0000

    quiet discussion trade talkback light

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def convert_to_hex(n):
+        return hex(n)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
