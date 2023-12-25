8d9026355f785e8786755ceaef2f438b6b53472a
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:00:59 +0000

    borrow pass clear single region

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_min(lst):
+        return min(lst)
