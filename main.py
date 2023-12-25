4e312956e28cbe1e43afb9dcab49680699b1f70c
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:26:53 +0000

    ought build rent lead conduct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
