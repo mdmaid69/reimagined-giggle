fe6c0ae722842e1590a012069adbd8bcbeaa1b85
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:10:54 +0000

    I clearly peace store trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
