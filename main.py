fa854754bb5e844dab897f86306b4b05ef624a37
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:11:37 +0000

    of buy demonstrate nor attractive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def count_elements(lst):
-        return len(lst)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
