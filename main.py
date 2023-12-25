517f1c336ab4333602d2bfc159ea93c8c7f9a339
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:32:22 +0000

    grade hollow die list first

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import collections
-def create_stack():
-        return collections.deque()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
