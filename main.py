38406a70eca74e4d34028b9f568348509a979035
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:55:31 +0000

    issue great contribute because history

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
