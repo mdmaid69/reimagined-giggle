70a921d793e3dfe00b5705eb7ba2f5813a62e9d5
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:43:28 +0000

    just combination bridge everywhere day

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_absolute_value(x):
+        return math.fabs(x)
