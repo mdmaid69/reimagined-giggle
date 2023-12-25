6761e63a58a775e0bbf35809f8641459520452b1
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:55:04 +0000

    through fasten that hollow stir

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+text = "Hello, world!"
+print("Words:", len(text.split()))
+def count_words(sentence):
+        return len(sentence.split())
