83568b82bc53931fd9565e5a3f83861f6d39babd
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:19:51 +0000

    middle short either permission quickly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import collections
+def create_user_string():
+        return collections.UserString()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
