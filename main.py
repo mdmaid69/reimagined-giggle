16c866922e8c16c12b2baa79561158f29c94197b
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:18:17 +0000

    stick issue address room seat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def convert_to_hex(n):
+        return hex(n)
