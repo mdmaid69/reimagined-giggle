b889cb2f5ef5eddb07058fe99d6179a3113f3a50
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:49:02 +0000

    shape wish fit can so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import datetime
+def get_current_date():
+        return datetime.date.today()
