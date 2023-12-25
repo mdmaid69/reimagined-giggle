5c9bd21fd5fb73808c5bf033be8e17e10942118a
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:46:21 +0000

    also border begin wind worst

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_as_float(array):
+        return float(array[0])
