27a061365e15cf0f85cb4bb3c8461e65fb924bf0
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:03:39 +0000

    record dry occasion free whoever

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
