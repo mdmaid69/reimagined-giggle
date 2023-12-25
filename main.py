8999c696f22b6924762b9fa57fa53e9977a32bb0
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:53:16 +0000

    face regular front fit present

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
