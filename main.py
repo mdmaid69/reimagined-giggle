04d025531b4c5a1cf0caf9eb6c67f7d558b82050
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:36:28 +0000

    during sit month rob now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
