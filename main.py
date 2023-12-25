17f5b8f03c273ba92401e207aa62359e293e5d69
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:32:12 +0000

    do operate brief business performance

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
