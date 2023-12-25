1b827f69467d4de0315f8647caaad67f67dfc714
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:12:19 +0000

    majority next operation connected degree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-x = 10
-y = 20
-print("Sum:", x + y)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
