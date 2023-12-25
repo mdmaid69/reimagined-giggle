70b72286efe1d2056631d697456ce917ef5b2c5f
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:30:26 +0000

    connect both winter birth shade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
