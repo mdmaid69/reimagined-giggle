3f14a2fdf3786b52ed2c8b0d49fb2f43ec3c6a29
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:02:51 +0000

    piece community breakin over error

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def subtract_numbers(x, y):
+        return x - y
