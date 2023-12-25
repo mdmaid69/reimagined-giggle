94114192d38010d80bcd3ec4a98f1b9829f0cfef
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:09:44 +0000

    shopping cry noone correction turn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
