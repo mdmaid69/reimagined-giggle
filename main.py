735c8f8ad9fbd8e44f2b0baccc2d075fc6c780ca
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:20:02 +0000

    mystery succeed disturbing excuse body

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   def convert_to_binary(n):
         return bin(n)
