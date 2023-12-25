6f0d02d42a5cea6b03200151d06efd461ee33913
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:04:35 +0000

    demonstrate their separate broken machine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def convert_to_octal(n):
+        return oct(n)
