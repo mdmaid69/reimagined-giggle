ee20801fd16aee2271e4ad2226e65892c7401376
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:29:31 +0000

    future lock meet police surface

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
