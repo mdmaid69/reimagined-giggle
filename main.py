39276ebfa56a89dd9ded54e858812a068576aa18
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:46:31 +0000

    test look smile tonight travel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
