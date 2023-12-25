b062a52e423326621f447271c0e025c05a35a0f4
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:54:03 +0000

    geton flavour probably weigh ride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def convert_to_octal(n):
+        return oct(n)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
