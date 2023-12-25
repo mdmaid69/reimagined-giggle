d9cf40eecfad232e5cb77de9b4c4b3908fb7b497
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:29:37 +0000

    afraid carry male section whenever

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
