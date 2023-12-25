ac23a7f6130181b2460c6c1bafc3c4894f0b1702
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:35:31 +0000

    dear yourself forget wear pleased

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
