64a3b3dc4aef69078a9146bfd4e4563553bca08b
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:36:41 +0000

    project sign print burn imagination

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
