44d97ad69055934f91de52364422490f3eaea2cc
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:28:25 +0000

    set old lose fasten empty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def square_number(x):
+        return x**2
