c103c527443d287bceb275a1797590527bd89489
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:36:19 +0000

    recovery broad better smell damp

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_perpetuity(payment, rate):
+        return payment / rate
