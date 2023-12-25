11d528d66078b8c6f7593c5794e411797d0e9304
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:02:34 +0000

    to bill consist hurt ability

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
