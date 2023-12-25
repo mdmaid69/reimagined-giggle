98f4cb73024ee6bc203807c7c219ff68648f022b
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:28:46 +0000

    wipe unfair slowly start tool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
