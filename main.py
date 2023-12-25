52ccc9760cc8cae2441b604f7647656275a74c3f
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:09:25 +0000

    illness throw dress because artificial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_volume(length, width, height):
+        return length * width * height
