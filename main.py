4b74adae39deb5accbc950aa85cbb53de9a61ff6
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:12:25 +0000

    print male treat less shallow

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_max(lst):
-        return max(lst)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def find_unique_words(sentence):
+        return set(sentence.split())
+from collections import Counter
+print(Counter("hello world"))
