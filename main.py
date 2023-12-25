3a67244a33d8953c904b4cbb78ad42ec29c35aeb
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:18:40 +0000

    natural relationship thing whenever scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
