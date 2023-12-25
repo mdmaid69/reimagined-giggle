67fde55372551b0f4dc862b2f21d82cc3654f325
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:00:46 +0000

    bet long furniture to patience

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
