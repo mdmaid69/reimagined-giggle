22152fbd5b99c7b78e63b6fea753365abaa7a8a4
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:21:43 +0000

    statement cover list tap flow

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import os
+def remove_directory(path):
+        os.rmdir(path)
