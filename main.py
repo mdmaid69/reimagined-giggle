a382eb84fd5c8c62c969dedf9b93efacbe2e5255
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:41:24 +0000

    force surprised connect please light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
