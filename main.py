6abb1e8310ca8cc4ee55522d79daade4da003f2c
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:27:00 +0000

    will abroad card emphasis bus

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def count_elements(lst):
-        return len(lst)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
