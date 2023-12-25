740fadb80cc980d47c6cecf73b5d113c04c081cc
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:24:27 +0000

    suddenly Vv pocket can Qq

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def count_words(sentence):
+        return len(sentence.split())
