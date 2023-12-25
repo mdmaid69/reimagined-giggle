888310f0946726cd4e9a6966b4da8a861024ceb0
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:40:37 +0000

    concentration slightly reserve Oo delight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_floor(x):
+        return math.floor(x)
