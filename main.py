2938ca8b64b9a94d9aeebb68be2b22267d5de2cd
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:36:19 +0000

    tool definite attract die fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import sys
+  def get_python_version():
+        return sys.version
