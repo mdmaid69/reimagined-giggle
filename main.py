ebf39a15d22161b84780286f7093c136a2f3f760
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:39:52 +0000

    wheel plenty go formal settle

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
