41ce5e5277c7c13296aa14d50647dce0a0099593
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:56:24 +0000

    tend backwards hospital closed fill

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def find_max(lst):
+        return max(lst)
