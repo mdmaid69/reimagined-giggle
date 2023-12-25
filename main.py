a67ddc2a2630ba300644da17b14905f479d64879
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:52:50 +0000

    approval opportunity warm greeting convince

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
