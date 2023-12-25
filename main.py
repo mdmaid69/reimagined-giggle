e5c7f467b68b55d84b7bb15538c1253cafaeca34
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:33:27 +0000

    afterwards move along star poison

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
