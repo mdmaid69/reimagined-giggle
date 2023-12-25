4bf78f0075ed4f6c1f396810e27281c94c3400d2
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:57:12 +0000

    sail please 564encourage temperature central

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
 import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
