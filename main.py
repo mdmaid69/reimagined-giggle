864a1df295bcd67287af2ac52d6886afe86d2afa
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:28:51 +0000

    tip operate prefer quiet total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
