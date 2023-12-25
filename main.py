6ee77fd14647f70b0643d9a8f445fbb5693188cd
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:35:42 +0000

    stay him draw bringback perform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
