65eb41e8556e6653354f4065603d19de27dfa7a8
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:35:04 +0000

    trade bang turn project water

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def count_elements(lst):
-        return len(lst)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def count_characters(sentence):
+        return len(sentence)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
