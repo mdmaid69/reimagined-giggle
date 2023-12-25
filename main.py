5a9cbff5adb6150182dd2931922c6cbf66c8f453
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:56:26 +0000

    catch move clearly dig flat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import time
+print(time.time())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
