921b7bcd393129dd79d854eee8977a164f9a9851
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:03:11 +0000

    minimum simple fairly book yours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
