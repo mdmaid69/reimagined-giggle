cd1608d1fb99a34a8691f35863d1dddb693f06b4
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:02:51 +0000

    seat down plan sensitive set

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
