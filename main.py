92a94a782e0a722479473b8e68f803eaeba9482e
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:39:04 +0000

    sail off home grateful suggestion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
