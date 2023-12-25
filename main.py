e6b876eff4757ce6e0dded6c0c68020a66ec7230
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:11:27 +0000

    love game under brother earn

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
