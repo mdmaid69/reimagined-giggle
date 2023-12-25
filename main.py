5a2b7b8395473901686d26011e73cccc59816731
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:59:01 +0000

    workout block clue tense disturb

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
