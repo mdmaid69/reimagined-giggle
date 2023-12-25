d7d99ce03fa9116acafeea2aecf635baf63f9995
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:50:29 +0000

    geton clue thorough alive actual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_floor(x):
+        return math.floor(x)
