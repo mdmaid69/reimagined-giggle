c08911dbb3d5dadb2441431fa0866c2ca34025e9
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:09:36 +0000

    division guess prepare scream song

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
