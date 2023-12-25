a487831cef5a52ca809698a3b4fd8c8c8e1e59e1
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:47:55 +0000

    mistake space nothing description boring

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  def sort_list(lst):
+        return sorted(lst)
