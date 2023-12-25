8ee0f80809c02ec4af51b2b8b8e5436d429f6673
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:24:10 +0000

    what lean bang chance car

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
