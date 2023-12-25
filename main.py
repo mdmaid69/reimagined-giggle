0c99ab5a726586728778ed392961d35a86f37029
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:00:20 +0000

    part hide 568energy nervous club

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  def convert_to_binary(n):
+        return bin(n)
