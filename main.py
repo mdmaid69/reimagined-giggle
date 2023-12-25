1f5974bbd89c42c31040383b282b633ec9a26dff
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:01:36 +0000

    lot neat charge belong in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
