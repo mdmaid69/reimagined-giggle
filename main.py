4c2190f4262816d5263e7509346541ecb7db8558
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:26:29 +0000

    arm by distance custom upwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
