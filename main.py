1e5c5eda9363efd75a1d7805bbd50ea51445fa98
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:22:52 +0000

    announce fear represent view try

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
