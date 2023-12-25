14f4ae53e0a20920ec30e302d1c8d0ea518e4c87
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:39:11 +0000

    apply opponent cost breakdown star

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def clear_array(array):
+        array *= 0
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
