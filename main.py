bc08cad81a5f08bb47369088b6830379b222a003
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:02:42 +0000

    equal satisfactory open crash check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_union(list1, list2):
+        return set(list1) | set(list2)
