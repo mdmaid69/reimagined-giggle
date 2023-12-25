7842117c05ae3dcd49da485c38ef5b54aa4235b8
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:24:49 +0000

    radio Ww lose trust discovery

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
