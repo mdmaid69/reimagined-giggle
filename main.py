87d5788d422473bc8bb824b15975a164c5b26963
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:02:16 +0000

    over date disgusting war tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
