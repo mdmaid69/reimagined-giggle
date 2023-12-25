6454c760091a09ef61b0257e1ecb9d194356e2bd
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:03:26 +0000

    appearance later remain hole bye

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_min(lst):
+        return min(lst)
