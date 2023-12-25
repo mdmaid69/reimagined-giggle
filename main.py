6df77a844cdd02405c943ba99f03368d73ddffbf
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:22:58 +0000

    different makeup imagination slide wait

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def reverse_array(array):
+        array.reverse()
