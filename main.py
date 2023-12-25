7ed9dd94e319a8ef019bbfc2d6bbaec5c808ae00
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:54:49 +0000

    Dd generous neck medium sing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
