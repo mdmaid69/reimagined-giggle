baeebcf3869ade92b8afeb258c5ce9a259fe55f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:46:15 +0000

    tidy workupto interview prevent choice

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
