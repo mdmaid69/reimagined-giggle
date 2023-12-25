e811598a4a97abe787125541798b800f43e54850
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:39:43 +0000

    OKorokay positive sure match prepare

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
