0bc3358e9beacf135763d0d9a9a4ceabfbdeb017
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:21:53 +0000

    downstairs loss takeback sense drive

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
