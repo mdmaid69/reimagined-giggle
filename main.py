7aa56a31b43058d744d68f4e0155acca60ca21a5
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:23:47 +0000

    polite have buy attempt shelter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
