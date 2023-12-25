73c01778b42f18a590884c5b715596c92fc24c81
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:41:17 +0000

    protection bother swim divide callback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
