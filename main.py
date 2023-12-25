75831e4bc403530854b11003dec964759b4475fc
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:47:52 +0000

    front slight once you simple

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
