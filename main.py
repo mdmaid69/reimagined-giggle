b9a813d5bf5e2a424f4a5169b081a40697a6e773
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:22:33 +0000

    challenge Qq long strike total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import platform
+def get_os_info():
+        return platform.uname()
