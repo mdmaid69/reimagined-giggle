17a9fef5ec4a3480e01adbe9a10f8b1d3ee28d6d
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:16:53 +0000

    welcome sport disappointed teach sudden

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
