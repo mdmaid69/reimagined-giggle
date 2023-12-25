b6e606ced2dba60e02b5e0dd0ed21717b391d07d
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:37:55 +0000

    reserve firm stiff guess today

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import sys
+  def get_python_version():
+        return sys.version
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
