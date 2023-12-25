fc492a616fc7dcc7b3fb0c3f2b9d0cf9bc419260
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:50:26 +0000

    correction disagree estimate limit lean

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+print([x**2 for x in range(10)])
