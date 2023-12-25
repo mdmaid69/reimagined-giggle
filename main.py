92d1145b678b610308dd462a4c3ebb3980f20d94
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:33:46 +0000

    doctor push tool variety bunch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
