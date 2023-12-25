12bf9477ffb2e547b75f048817303fbeafb70827
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:30:07 +0000

    Bb describe sentence probably general

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
