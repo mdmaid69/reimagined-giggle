a20a387b0fc4396928dd171a2c86e9c44e534e1b
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:26:03 +0000

    shout deal dangerous please formal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
