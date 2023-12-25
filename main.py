f8a882a5e0851ac36aa92031f0319d88714f7124
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:03:24 +0000

    getaway proper charge responsibility relation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
