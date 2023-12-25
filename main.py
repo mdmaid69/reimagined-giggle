188c3e25b472f705e663504304dc8508f4d3e21a
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:49:36 +0000

    confidence takeoff throughout phone throughout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
