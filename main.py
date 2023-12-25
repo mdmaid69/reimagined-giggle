5ee05b96dd1236d1eb96d00cc3a1d69a0fb0d140
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:12:56 +0000

    like seem explain base lesson

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
