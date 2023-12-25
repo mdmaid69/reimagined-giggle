0ec3fa0947611e90769ad30fce5bab2526c0f920
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:05:00 +0000

    here yet right deep phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-for i in range(5):
-        print(i)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
