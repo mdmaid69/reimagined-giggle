627e695d4db7c05fce1e439206b18cb0825ca58d
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:30:56 +0000

    quarter strike office mystery miss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
