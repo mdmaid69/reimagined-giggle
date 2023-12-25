66bbde8f517aadfa148105cf5910b406519fb799
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:15:44 +0000

    quickly represent dark discover rank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
