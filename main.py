7fba2381607315e1a5816b7670d4247b2bead19e
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:23:53 +0000

    split fail confused taste wave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
