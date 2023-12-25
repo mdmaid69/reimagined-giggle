e41aaf48aceac358ec49ba0ac9950f9a12594c46
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:54:35 +0000

    reply fail lucky pay estimate

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def set_array_item(array, i, item):
+        array[i] = item
