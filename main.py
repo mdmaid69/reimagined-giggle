a2f8a5ec36a32235456b1b6bb8894b0411727cbc
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:35:00 +0000

    finally store yesterday experiment charge

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import sys
+  def get_python_version():
+        return sys.version
+def is_palindrome(s):
+        return s == s[::-1]
