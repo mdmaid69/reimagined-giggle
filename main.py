77cd59c2c2816b6906b48ca18b1eb422afbcde7a
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:38:30 +0000

    judge ilovedicks plenty say production

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  def convert_to_octal(n):
-        return oct(n)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
