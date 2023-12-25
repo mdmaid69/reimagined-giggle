010bd81555c98278b69c079c454635408ddf719b
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:01:56 +0000

    pity under march bringdown healthy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
