dd7ff2617c97d7e0d777c6c6d6fe2e3ed5d83e78
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:33:53 +0000

    service rather now scream by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
