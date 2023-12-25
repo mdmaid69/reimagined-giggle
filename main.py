33616a0b381492ca6bb8d84777e533c885895020
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:28:09 +0000

    sore custom float oil rely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def convert_array_to_string(array):
+        return array.tostring()
+  def sort_list(lst):
+        return sorted(lst)
