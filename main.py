04f6f3b3109a7a38dcc0af76cd9e4b60837653e8
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:20:18 +0000

    nice natural cum part deliberate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def append_to_array(array, item):
+        array.append(item)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
