50c86d295e28875bda100dd6ee0f4e6d6052a946
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:40:13 +0000

    majority scrap book prepare he

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
