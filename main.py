b1daec6f519f38cad7245f5841ddb18201137bab
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:08:58 +0000

    inform pump pipe secret deal

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
