8fd365e6f8febe191ea807c9c3dd17d1fc55bfec
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:37:25 +0000

    affect rent tend ticket send

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_max(lst):
+        return max(lst)
