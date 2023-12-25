42edfc0b826db9f801183a15422114b82570ded9
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:06:01 +0000

    understand fear board cheek waste

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import os
-print(os.getcwd())
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def is_odd(n):
+        return n % 2 != 0
