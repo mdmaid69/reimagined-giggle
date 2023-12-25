eb148e617689dc0fcfbbcf8644a84f22613b34d8
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:46:58 +0000

    wipe shame so earn certain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_pressure(force, area):
+        return force / area
