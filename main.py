5dbbf04c7b4098679528c17f4a33a7359dcd1459
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:05:39 +0000

    classroom declare apart over deserve

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_current_directory():
+        return os.getcwd()
