531936ab2731b81c23ac9cfb75ccd1ec43276d94
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:38:52 +0000

    exercise count there judge middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
