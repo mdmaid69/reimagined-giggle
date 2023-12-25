43a02551088e0b961df47876ff3da06c35d78cb5
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:52:02 +0000

    see gather turninto hang bill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
