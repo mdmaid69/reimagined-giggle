98b573903e2252614d3610070b35e9ad6b768293
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:13:25 +0000

    dare nothing teach exhibition mend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+name = "Python"
+print("Hello,", name)
