a2bf3ff72c11f3fa9c8d6763c43b582a579d7d31
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:36:27 +0000

    length hungry few Ll nervous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
