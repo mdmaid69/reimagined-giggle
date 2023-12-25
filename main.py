305e08a38fe075c8bd28b08f48a21bb2ea19d5f4
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:13:35 +0000

    use the far face dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
