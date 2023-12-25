7f1a7240704db5168abc992b378bdd118d19b187
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:01:40 +0000

    getback pinch then and available

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
