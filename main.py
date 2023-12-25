3bd23b09941019a3be625efca210abf67047a11f
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:58:40 +0000

    software chat ours familiar church

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  def is_odd(n):
+        return n % 2 != 0
