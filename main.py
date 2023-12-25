a417664d8b7facaed316ec023bd82c82eec9b8ce
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:20:41 +0000

    please backwards pin just explain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
