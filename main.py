2e7268d31aea26bca295186422a2bc3e862d42e9
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:03:24 +0000

    now past say arrive holdup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_area_circle(r):
+        return 3.14 * r**2
