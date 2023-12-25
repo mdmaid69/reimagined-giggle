8f55f27834e279d334d3210dbc7b31d4f05af76d
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:00:18 +0000

    kick pupil final none warning

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
