f5d073dcabdf50b6960969cd3b4887f6d1074db4
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:24:52 +0000

    congratulations Ww book forgive if

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+for i in range(10): print(i)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
