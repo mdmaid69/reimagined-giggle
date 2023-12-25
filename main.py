cde9427541104f86b3b251eb232508f42b912671
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:13:09 +0000

    crowded control exhibition afterwards experience

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import sys
+def exit_program():
+        sys.exit()
 import array
 def get_array_as_complex(array):
         return complex(array[0])
