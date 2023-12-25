f2d56b34d47f0ac436605274b53f47e74824840d
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:36:49 +0000

    mess finish fasten female spite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import time
+def get_time_since_epoch():
+        return time.time()
+def is_odd(n):
+        return n % 2 != 0
