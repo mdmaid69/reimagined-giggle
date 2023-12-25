5e85cc69c1d3d8d698823d7a2993b3db85b10965
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:43:04 +0000

    connected strict calm complete practise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
