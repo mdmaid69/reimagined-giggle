7c8a3fb66e8c32a7d20c5d71a05f34982abf84dd
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:28:00 +0000

    most keen that everything change

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_max(numbers):
+        return max(numbers)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
