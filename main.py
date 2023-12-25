968c3c3fcf0211a3c3ea7fe7b42795fb60d0e99a
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:33:39 +0000

    effect deep set appearance fear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def square_number(x):
+        return x**2
+import array
+def get_string_from_array(array):
+        return array.tobytes()
