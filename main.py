ac930a08ed3f95ede6a941f2b1f30f98ff726cab
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:28:27 +0000

    attitude tune board 567enemy poison

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
