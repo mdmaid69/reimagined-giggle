0472a41e5065960b2c4d5c9748fa27b8dc5e7280
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:27:16 +0000

    platform trick television safe interview

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
