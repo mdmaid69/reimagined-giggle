da78eb3ed097009f8cb371d1b725c000fbb4b32c
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:09:21 +0000

    frame ground build peace steam

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_queue():
-        return collections.deque()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import platform
+def get_os_info():
+        return platform.uname()
