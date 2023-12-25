985495c11eb2d3f4727a09ab33d51108996b5754
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:14:51 +0000

    convincing programme insist unkind will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def subtract_numbers(x, y):
+        return x - y
