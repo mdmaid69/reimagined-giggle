20a8b1e821e1998a598faead1e363a4215b4c793
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:49:26 +0000

    speed cough light book live

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def count_elements(lst):
-        return len(lst)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
